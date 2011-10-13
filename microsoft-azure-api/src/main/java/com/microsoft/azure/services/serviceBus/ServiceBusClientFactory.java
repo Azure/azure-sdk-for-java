b5f1f88a7599bba6f4a1a4292cb7baba48324ab4
Louis DeJardin <louis.dejardin@gmail.com>
2011-10-12 23:43:55 -0700

    Factory methods for send/receive interfaces

--- microsoft-azure-api/src/main/java/com/microsoft/azure/services/serviceBus/ServiceBusClientFactory.java
+++ microsoft-azure-api/src/main/java/com/microsoft/azure/services/serviceBus/ServiceBusClientFactory.java
@@ -3,5 +3,16 @@ package com.microsoft.azure.services.serviceBus;
 import com.microsoft.azure.http.ClientFactory;
 
 public class ServiceBusClientFactory extends ClientFactory {
-	public ServiceBusClient createClient() throws Exception {throw new Exception();}
+	public ServiceBusClient createClient() {
+		return null;
+	}	
+	public MessageTranceiver openQueue(String queuePath) {
+		return null;
+	}
+	public MessageSender openTopic(String topicPath) {
+		return null;
+	}
+	public MessageReceiver openSubscription(String topicPath, String subscriptionName) {
+		return null;
+	}
 }
