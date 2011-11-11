ab43525988def0fec74aa9b09a3737552bc24d22
Louis DeJardin <lodejard@microsoft.com>
2011-11-11 13:59:39 -0800

    Adding delegate methods to Topic

--- microsoft-azure-api/src/main/java/com/microsoft/windowsazure/services/serviceBus/Topic.java
+++ microsoft-azure-api/src/main/java/com/microsoft/windowsazure/services/serviceBus/Topic.java
@@ -1,6 +1,7 @@
 package com.microsoft.windowsazure.services.serviceBus;
 
 import javax.ws.rs.core.MediaType;
+import javax.xml.datatype.Duration;
 
 import com.microsoft.windowsazure.services.serviceBus.implementation.Content;
 import com.microsoft.windowsazure.services.serviceBus.implementation.Entry;
@@ -19,6 +20,11 @@ public class Topic extends EntryModel<TopicDescription> {
         super(entry, entry.getContent().getTopicDescription());
     }
 
+    public Topic(String name) {
+        this();
+        setName(name);
+    }
+
     public String getName() {
         return getEntry().getTitle();
     }
@@ -27,4 +33,54 @@ public class Topic extends EntryModel<TopicDescription> {
         getEntry().setTitle(value);
         return this;
     }
+
+    public Duration getDefaultMessageTimeToLive() {
+        return getModel().getDefaultMessageTimeToLive();
+    }
+
+    public Topic setDefaultMessageTimeToLive(Duration value) {
+        getModel().setDefaultMessageTimeToLive(value);
+        return this;
+    }
+
+    public Long getMaxSizeInMegabytes() {
+        return getModel().getMaxSizeInMegabytes();
+    }
+
+    public Topic setMaxSizeInMegabytes(Long value) {
+        getModel().setMaxSizeInMegabytes(value);
+        return this;
+    }
+
+    public Boolean isRequiresDuplicateDetection() {
+        return getModel().isRequiresDuplicateDetection();
+    }
+
+    public Topic setRequiresDuplicateDetection(Boolean value) {
+        getModel().setRequiresDuplicateDetection(value);
+        return this;
+    }
+
+    public Duration getDuplicateDetectionHistoryTimeWindow() {
+        return getModel().getDuplicateDetectionHistoryTimeWindow();
+    }
+
+    public Topic setDuplicateDetectionHistoryTimeWindow(Duration value) {
+        getModel().setDuplicateDetectionHistoryTimeWindow(value);
+        return this;
+    }
+
+    public Boolean isEnableBatchedOperations() {
+        return getModel().isEnableBatchedOperations();
+    }
+
+    public Topic setEnableBatchedOperations(Boolean value) {
+        getModel().setEnableBatchedOperations(value);
+        return this;
+    }
+
+    public Long getSizeInBytes() {
+        return getModel().getSizeInBytes();
+    }
+
 }
