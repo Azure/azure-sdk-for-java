068bf45009d5decc1938fef3057b3084072b63cb
Renaud Paquay <renaud.paquay@microsoft.com>
2011-11-11 13:30:54 -0800

    Add support for storage emulator in integration tests

--- microsoft-azure-api/src/test/java/com/microsoft/azure/services/blob/IntegrationTestBase.java
+++ microsoft-azure-api/src/test/java/com/microsoft/azure/services/blob/IntegrationTestBase.java
@@ -11,6 +11,13 @@ public abstract class IntegrationTestBase {
     protected static Configuration createConfiguration() {
         Configuration config = new Configuration();
         Map<String, String> env = System.getenv();
+
+        // Storage emulator support
+        //setConfigValue(config, env, BlobConfiguration.ACCOUNT_NAME, "devstoreaccount1");
+        //setConfigValue(config, env, BlobConfiguration.ACCOUNT_KEY, "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==");
+        //setConfigValue(config, env, BlobConfiguration.URL, "http://127.0.0.1:10000/devstoreaccount1");
+
+        // Storage account support
         setConfigValue(config, env, BlobConfiguration.ACCOUNT_NAME, "xxx");
         setConfigValue(config, env, BlobConfiguration.ACCOUNT_KEY, "xxx");
         setConfigValue(config, env, BlobConfiguration.URL, "http://xxx.blob.core.windows.net");
