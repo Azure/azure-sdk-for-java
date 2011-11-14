98bc0b04ba419d6d576542039c956751ea383d64
Renaud Paquay <renaud.paquay@microsoft.com>
2011-11-14 15:38:39 -0800

    Update class registration

--- microsoft-azure-api/src/main/java/com/microsoft/windowsazure/services/blob/Exports.java
+++ microsoft-azure-api/src/main/java/com/microsoft/windowsazure/services/blob/Exports.java
@@ -1,13 +1,15 @@
 package com.microsoft.windowsazure.services.blob;
 
 import com.microsoft.windowsazure.configuration.builder.Builder;
-import com.microsoft.windowsazure.services.blob.implementation.BlobRestProxy;
 import com.microsoft.windowsazure.services.blob.implementation.BlobExceptionProcessor;
+import com.microsoft.windowsazure.services.blob.implementation.BlobRestProxy;
 import com.microsoft.windowsazure.services.blob.implementation.SharedKeyLiteFilter;
 
 public class Exports implements Builder.Exports {
     public void register(Builder.Registry registry) {
         registry.add(BlobContract.class, BlobExceptionProcessor.class);
+        registry.add(BlobService.class);
+        registry.add(BlobExceptionProcessor.class);
         registry.add(BlobRestProxy.class);
         registry.add(SharedKeyLiteFilter.class);
     }
--- microsoft-azure-api/src/main/java/com/microsoft/windowsazure/services/queue/Exports.java
+++ microsoft-azure-api/src/main/java/com/microsoft/windowsazure/services/queue/Exports.java
@@ -1,13 +1,15 @@
 package com.microsoft.windowsazure.services.queue;
 
 import com.microsoft.windowsazure.configuration.builder.Builder;
-import com.microsoft.windowsazure.services.queue.implementation.QueueRestProxy;
 import com.microsoft.windowsazure.services.queue.implementation.QueueExceptionProcessor;
+import com.microsoft.windowsazure.services.queue.implementation.QueueRestProxy;
 import com.microsoft.windowsazure.services.queue.implementation.SharedKeyLiteFilter;
 
 public class Exports implements Builder.Exports {
     public void register(Builder.Registry registry) {
         registry.add(QueueContract.class, QueueExceptionProcessor.class);
+        registry.add(QueueService.class);
+        registry.add(QueueExceptionProcessor.class);
         registry.add(QueueRestProxy.class);
         registry.add(SharedKeyLiteFilter.class);
     }
