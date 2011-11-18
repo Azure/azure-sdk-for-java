c7a4c345daacdc7f381c757e1fd01ecfac7126f6
Renaud Paquay <renaud.paquay@microsoft.com>
2011-11-17 17:02:49 -0800

    Add "raw" response body to service exceptions

--- microsoft-azure-api/src/main/java/com/microsoft/windowsazure/utils/ServiceExceptionFactory.java
+++ microsoft-azure-api/src/main/java/com/microsoft/windowsazure/utils/ServiceExceptionFactory.java
@@ -24,13 +24,13 @@ public class ServiceExceptionFactory {
         return exception;
     }
 
-    static ServiceException populate(ServiceException exception, String serviceName,
-            UniformInterfaceException cause) {
+    static ServiceException populate(ServiceException exception, String serviceName, UniformInterfaceException cause) {
         exception.setServiceName(serviceName);
 
         if (cause != null) {
             ClientResponse response = cause.getResponse();
             if (response != null) {
+                // Set status
                 Status status = response.getClientResponseStatus();
                 if (status == null) {
                     status = Status.fromStatusCode(response.getStatus());
@@ -42,18 +42,29 @@ public class ServiceExceptionFactory {
                     exception.setHttpStatusCode(status.getStatusCode());
                     exception.setHttpReasonPhrase(status.getReasonPhrase());
                 }
+
+                // Set raw response body
+                if (response.hasEntity()) {
+                    try {
+                        String body = response.getEntity(String.class);
+                        exception.setRawResponseBody(body);
+                    }
+                    catch (Exception e) {
+                        // Skip exceptions as getting the response body as a string is a best effort thing
+                    }
+                }
             }
         }
         return exception;
     }
 
-    static ServiceException populate(ServiceException exception, String serviceName,
-            ServiceException cause) {
+    static ServiceException populate(ServiceException exception, String serviceName, ServiceException cause) {
         exception.setServiceName(cause.getServiceName());
         exception.setHttpStatusCode(cause.getHttpStatusCode());
         exception.setHttpReasonPhrase(cause.getHttpReasonPhrase());
         exception.setErrorCode(cause.getErrorCode());
         exception.setErrorMessage(cause.getErrorMessage());
+        exception.setRawResponseBody(cause.getRawResponseBody());
         exception.setErrorValues(cause.getErrorValues());
         return exception;
     }
