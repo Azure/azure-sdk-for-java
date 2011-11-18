6af17f70d9fa4be46f2069770da7ac8d325388d2
Renaud Paquay <renaud.paquay@microsoft.com>
2011-11-18 10:03:29 -0800

    Implement full ShareKey authorization for Blob Service
    The particular problem we had to solve here is to enable the SharedKey
    filter to have access to the "Content-Length" of the request, since
    that header is needed for authorizing the request.
    
    Since the "Content-Length" of the request is known only very late in the
    pipeline, so we had to introduce a new listener interface
    (EntityStreamingListener) that filters can register into.
    
    Filters then get called back later in the pipeline, just before starting
    sending bytes on the connection. This allows them to access all the
    headers (and also gives them a last chance to update them).

--- microsoft-azure-api/src/main/java/com/microsoft/windowsazure/services/blob/Exports.java
+++ microsoft-azure-api/src/main/java/com/microsoft/windowsazure/services/blob/Exports.java
@@ -1,9 +1,14 @@
 package com.microsoft.windowsazure.services.blob;
 
+import java.util.Map;
+
 import com.microsoft.windowsazure.common.Builder;
 import com.microsoft.windowsazure.services.blob.implementation.BlobExceptionProcessor;
 import com.microsoft.windowsazure.services.blob.implementation.BlobRestProxy;
+import com.microsoft.windowsazure.services.blob.implementation.HttpURLConnectionClient;
+import com.microsoft.windowsazure.services.blob.implementation.SharedKeyFilter;
 import com.microsoft.windowsazure.services.blob.implementation.SharedKeyLiteFilter;
+import com.sun.jersey.api.client.config.ClientConfig;
 
 public class Exports implements Builder.Exports {
     public void register(Builder.Registry registry) {
@@ -12,5 +17,15 @@ public class Exports implements Builder.Exports {
         registry.add(BlobExceptionProcessor.class);
         registry.add(BlobRestProxy.class);
         registry.add(SharedKeyLiteFilter.class);
+        registry.add(SharedKeyFilter.class);
+
+        registry.add(new Builder.Factory<HttpURLConnectionClient>() {
+            public HttpURLConnectionClient create(String profile, Builder builder, Map<String, Object> properties) {
+                ClientConfig clientConfig = (ClientConfig) properties.get("ClientConfig");
+                HttpURLConnectionClient client = HttpURLConnectionClient.create(clientConfig);
+                //client.addFilter(new LoggingFilter());
+                return client;
+            }
+        });
     }
 }
---
+++ microsoft-azure-api/src/main/java/com/microsoft/windowsazure/services/blob/implementation/EntityStreamingListener.java
@@ -0,0 +1,14 @@
+package com.microsoft.windowsazure.services.blob.implementation;
+
+import com.sun.jersey.api.client.ClientRequest;
+
+public interface EntityStreamingListener {
+    /**
+     * This method is called just before the entity is streamed to the underlying connection. This is the last chance
+     * for filters to inspect and modify the headers of the client request if necessary.
+     * 
+     * @param clientRequest
+     *            The client request
+     */
+    void onBeforeStreamingEntity(ClientRequest clientRequest);
+}
