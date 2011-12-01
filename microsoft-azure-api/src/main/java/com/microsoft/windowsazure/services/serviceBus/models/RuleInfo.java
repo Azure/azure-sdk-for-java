4df63865cd3d87c42860e7bb749e0c0260b7a6dc
Louis DeJardin <lodejard@microsoft.com>
2011-11-30 16:33:11 -0800

    Adding method to create SqlFilter for RuleInfo
    Based on clarification of the semantics of the xml data model
    Fixes #161

--- microsoft-azure-api/src/main/java/com/microsoft/windowsazure/services/serviceBus/models/RuleInfo.java
+++ microsoft-azure-api/src/main/java/com/microsoft/windowsazure/services/serviceBus/models/RuleInfo.java
@@ -11,6 +11,7 @@ import com.microsoft.windowsazure.services.serviceBus.implementation.FalseFilter
 import com.microsoft.windowsazure.services.serviceBus.implementation.Filter;
 import com.microsoft.windowsazure.services.serviceBus.implementation.RuleAction;
 import com.microsoft.windowsazure.services.serviceBus.implementation.RuleDescription;
+import com.microsoft.windowsazure.services.serviceBus.implementation.SqlFilter;
 import com.microsoft.windowsazure.services.serviceBus.implementation.SqlRuleAction;
 import com.microsoft.windowsazure.services.serviceBus.implementation.TrueFilter;
 
@@ -124,16 +125,21 @@ public class RuleInfo extends EntryModel<RuleDescription> {
         return setFilter(filter);
     }
 
-    public RuleInfo withTrueSqlExpressionFilter(String sqlExpression) {
-        TrueFilter filter = new TrueFilter();
+    public RuleInfo withSqlExpressionFilter(String sqlExpression) {
+        SqlFilter filter = new SqlFilter();
         filter.setSqlExpression(sqlExpression);
         filter.setCompatibilityLevel(20);
         return setFilter(filter);
     }
 
-    public RuleInfo withFalseSqlExpressionFilter(String sqlExpression) {
+    public RuleInfo withTrueFilter() {
+        TrueFilter filter = new TrueFilter();
+        filter.setCompatibilityLevel(20);
+        return setFilter(filter);
+    }
+
+    public RuleInfo withFalseFilter() {
         FalseFilter filter = new FalseFilter();
-        filter.setSqlExpression(sqlExpression);
         filter.setCompatibilityLevel(20);
         return setFilter(filter);
     }
