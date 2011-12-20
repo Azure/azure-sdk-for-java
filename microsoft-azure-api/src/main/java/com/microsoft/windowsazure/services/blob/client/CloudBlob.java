db4e58a30cb451682b3d8aead5559f365e4cd2b5
Joe Giardino <joegiard@microsoft.com>
2011-12-20 11:29:28 -0800

    CloudBlob Fix: Ensure Client side exceptions are not resumed

--- microsoft-azure-api/src/main/java/com/microsoft/windowsazure/services/blob/client/CloudBlob.java
+++ microsoft-azure-api/src/main/java/com/microsoft/windowsazure/services/blob/client/CloudBlob.java
@@ -809,16 +809,15 @@ public abstract class CloudBlob implements ListBlobItem {
                 final long expectedLength = Long.parseLong(contentLength);
 
                 blob.updatePropertiesFromResponse(request);
-
-                final StreamMd5AndLength descriptor = Utility.writeToOutputStream(streamRef, outStream, -1, false,
-                        validateMD5, this.getResult(), opContext);
-
                 ExecutionEngine.getResponseCode(this.getResult(), request, opContext);
 
                 if (this.getResult().getStatusCode() != HttpURLConnection.HTTP_OK) {
                     this.setNonExceptionedRetryableFailure(true);
                     return null;
                 }
+
+                final StreamMd5AndLength descriptor = Utility.writeToOutputStream(streamRef, outStream, -1, false,
+                        validateMD5, this.getResult(), opContext);
 
                 if (descriptor.getLength() != expectedLength) {
                     throw new StorageException(
@@ -844,10 +843,12 @@ public abstract class CloudBlob implements ListBlobItem {
             opContext.setIntermediateMD5(null);
         }
         catch (final StorageException ex) {
-            // Check if users has any retries specified.
+            // Check if users has any retries specified, Or if the exception is retryable
             final RetryPolicy dummyPolicy = options.getRetryPolicyFactory().createInstance(opContext);
-            if (!dummyPolicy.shouldRetry(0, opContext.getLastResult().getStatusCode(), (Exception) ex.getCause(),
-                    opContext).isShouldRetry()) {
+            if (ex.getHttpStatusCode() == Constants.HeaderConstants.HTTP_UNUSED_306
+                    || ex.getHttpStatusCode() == HttpURLConnection.HTTP_PRECON_FAILED
+                    || !dummyPolicy.shouldRetry(0, opContext.getLastResult().getStatusCode(),
+                            (Exception) ex.getCause(), opContext).isShouldRetry()) {
                 opContext.setIntermediateMD5(null);
                 throw ex;
             }
