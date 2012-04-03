adcf3ad86c73fa8f28595c240131e19a0295f7f3
Jason Cooke <jcooke@microsoft.com>
2012-04-03 12:40:13 -0700

    Added new ISO8601 unit tests Added back "shortFormat" function, needed by Blob

--- microsoft-azure-api/src/main/java/com/microsoft/windowsazure/services/blob/implementation/ISO8601DateConverter.java
+++ microsoft-azure-api/src/main/java/com/microsoft/windowsazure/services/blob/implementation/ISO8601DateConverter.java
@@ -28,12 +28,18 @@ import java.util.TimeZone;
 public class ISO8601DateConverter {
     // Note: because of the trailing "0000000", this is not quite ISO 8601 compatible
     private static final String DATETIME_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+    private static final String SHORT_DATETIME_PATTERN = "yyyy-MM-dd'T'HH:mm:ss'Z'";
     private static final String DATETIME_PATTERN_NO_S = "yyyy-MM-dd'T'HH:mm'Z'";
-    private static final String DATETIME_PATTERN_NO_MS = "yyyy-MM-dd'T'HH:mm:ss'Z'";
     private static final String DATETIME_PATTERN_TO_DECIMAL = "yyyy-MM-dd'T'HH:mm:ss.";
 
     public String format(Date date) {
         DateFormat iso8601Format = new SimpleDateFormat(DATETIME_PATTERN, Locale.US);
+        iso8601Format.setTimeZone(TimeZone.getTimeZone("GMT"));
+        return iso8601Format.format(date);
+    }
+
+    public String shortFormat(Date date) {
+        DateFormat iso8601Format = new SimpleDateFormat(SHORT_DATETIME_PATTERN, Locale.US);
         iso8601Format.setTimeZone(TimeZone.getTimeZone("GMT"));
         return iso8601Format.format(date);
     }
@@ -49,7 +55,7 @@ public class ISO8601DateConverter {
         }
         else if (length == 20) {
             // [2012-01-04T23:21:59Z] length = 20
-            return parseDateFromString(date, DATETIME_PATTERN_NO_MS);
+            return parseDateFromString(date, SHORT_DATETIME_PATTERN);
         }
         else if (length >= 22 && length <= 28) {
             // [2012-01-04T23:21:59.1Z] length = 22
@@ -72,7 +78,7 @@ public class ISO8601DateConverter {
         }
     }
 
-    public static Date parseDateFromString(final String value, final String pattern) throws ParseException {
+    private static Date parseDateFromString(final String value, final String pattern) throws ParseException {
         DateFormat iso8601Format = new SimpleDateFormat(pattern, Locale.US);
         iso8601Format.setTimeZone(TimeZone.getTimeZone("GMT"));
         return iso8601Format.parse(value);
--- microsoft-azure-api/src/test/java/com/microsoft/windowsazure/services/blob/implementation/ISO8601DateConverterTests.java
+++ microsoft-azure-api/src/test/java/com/microsoft/windowsazure/services/blob/implementation/ISO8601DateConverterTests.java
@@ -16,7 +16,9 @@ package com.microsoft.windowsazure.services.blob.implementation;
 
 import static org.junit.Assert.*;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
 import org.junit.Test;
 
@@ -29,9 +31,23 @@ public class ISO8601DateConverterTests {
 
         // Act
         Date result = converter.parse(value);
+        String value2 = converter.format(result);
 
         // Assert
         assertNotNull(result);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(result);
+        calendar.setTimeZone(TimeZone.getTimeZone("GMT"));
+        assertEquals("Year", 2012, calendar.get(Calendar.YEAR));
+        assertEquals("Month", 1, calendar.get(Calendar.MONTH) + 1);
+        assertEquals("Day", 12, calendar.get(Calendar.DAY_OF_MONTH));
+        assertEquals("Hour", 0, calendar.get(Calendar.HOUR));
+        assertEquals("Minute", 35, calendar.get(Calendar.MINUTE));
+        assertEquals("Second", 58, calendar.get(Calendar.SECOND));
+        assertEquals("Millisecond", 0, calendar.get(Calendar.MILLISECOND));
+
+        assertEquals("2012-01-12T00:35:58.000Z", value2);
     }
 
     @Test
@@ -42,9 +58,50 @@ public class ISO8601DateConverterTests {
 
         // Act
         Date result = converter.parse(value);
+        String value2 = converter.format(result);
 
         // Assert
         assertNotNull(result);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(result);
+        calendar.setTimeZone(TimeZone.getTimeZone("GMT"));
+        assertEquals("Year", 2012, calendar.get(Calendar.YEAR));
+        assertEquals("Month", 1, calendar.get(Calendar.MONTH) + 1);
+        assertEquals("Day", 12, calendar.get(Calendar.DAY_OF_MONTH));
+        assertEquals("Hour", 0, calendar.get(Calendar.HOUR));
+        assertEquals("Minute", 35, calendar.get(Calendar.MINUTE));
+        assertEquals("Second", 58, calendar.get(Calendar.SECOND));
+        assertEquals("Millisecond", 123, calendar.get(Calendar.MILLISECOND));
+
+        assertEquals("2012-01-12T00:35:58.123Z", value2);
+    }
+
+    @Test
+    public void mixedFormatWorks() throws Exception {
+        // Arrange
+        ISO8601DateConverter converter = new ISO8601DateConverter();
+        String value = "2012-01-12T00:35:58.12Z";
+
+        // Act
+        Date result = converter.parse(value);
+        String value2 = converter.format(result);
+
+        // Assert
+        assertNotNull(result);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(result);
+        calendar.setTimeZone(TimeZone.getTimeZone("GMT"));
+        assertEquals("Year", 2012, calendar.get(Calendar.YEAR));
+        assertEquals("Month", 1, calendar.get(Calendar.MONTH) + 1);
+        assertEquals("Day", 12, calendar.get(Calendar.DAY_OF_MONTH));
+        assertEquals("Hour", 0, calendar.get(Calendar.HOUR));
+        assertEquals("Minute", 35, calendar.get(Calendar.MINUTE));
+        assertEquals("Second", 58, calendar.get(Calendar.SECOND));
+        assertEquals("Millisecond", 120, calendar.get(Calendar.MILLISECOND));
+
+        assertEquals("2012-01-12T00:35:58.120Z", value2);
     }
 
     @Test
@@ -55,10 +112,12 @@ public class ISO8601DateConverterTests {
 
         // Act
         Date result = converter.parse(value);
-        String value2 = converter.format(result);
+        String value2 = converter.shortFormat(result);
+        String value3 = converter.format(result);
 
         // Assert
         assertNotNull(result);
         assertEquals(value, value2);
+        assertEquals("2012-01-12T00:35:58.000Z", value3);
     }
 }
