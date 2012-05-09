09f0ab227e948087bc68e30a7baf6cd44db9f64f
Colin Robertson <a-colinr@microsoft.com>
2012-05-08 17:36:41 -0700

    Add Javadocs comments to Table service layer files, fix table entry in table client.

--- microsoft-azure-api/src/main/java/com/microsoft/windowsazure/services/table/models/ConstantFilter.java
+++ microsoft-azure-api/src/main/java/com/microsoft/windowsazure/services/table/models/ConstantFilter.java
@@ -14,13 +14,54 @@
  */
 package com.microsoft.windowsazure.services.table.models;
 
+/**
+ * Represents a constant value used as a filter parameter in a table query request.
+ * <p>
+ * Use the static factory method in the {@link Filter} class to create a {@link ConstantFilter}, rather than
+ * constructing one directly.
+ * <p>
+ * Use this class to pass a constant value as a filter parameter. The value is passed as an <code>Object</code> but must
+ * be (or have an implicit conversion to) one of the following for successful serialization:
+ * <ul>
+ * <li><code>null</code></li>
+ * <li><code>byte[]</code></li>
+ * <li><code>Byte[]</code></li>
+ * <li><code>Date</code></li>
+ * <li><code>Long</code></li>
+ * <li><code>String</code></li>
+ * <li><code>UUID</code></li>
+ * </ul>
+ * <p>
+ * A {@link PropertyNameFilter} may be combined in a {@link BinaryFilter} with a comparison operator and a
+ * {@link ConstantFilter} to limit query results to properties with values that match the {@link ConstantFilter} value.
+ * The table service does not support wildcard queries, but you can perform prefix matching by using comparison
+ * operators on the desired prefix created as a {@link String} in a {@link ConstantFilter} instance.
+ * <p>
+ * See the <a href="http://msdn.microsoft.com/en-us/library/windowsazure/dd894031.aspx">Querying Tables and Entities</a>
+ * topic in MSDN for more information on creating table query filter strings.
+ */
 public class ConstantFilter extends Filter {
     private final Object value;
 
+    /**
+     * Creates a table query filter constant from the <em>value</em> parameter.
+     * <p>
+     * Use the static factory method in the {@link Filter} class to create a {@link ConstantFilter}, rather than
+     * constructing one directly.
+     * 
+     * @param propertyName
+     *            An {@link Object} containing the constant value to use as a filter parameter in a table query request.
+     */
     public ConstantFilter(Object value) {
         this.value = value;
     }
 
+    /**
+     * Gets the constant filter value set in this {@link ConstantFilter} instance.
+     * 
+     * @return
+     *         The {@link Object} containing the constant value to use as a filter parameter in a table query request.
+     */
     public Object getValue() {
         return value;
     }
--- microsoft-azure-api/src/main/java/com/microsoft/windowsazure/services/table/models/UpdateEntityResult.java
+++ microsoft-azure-api/src/main/java/com/microsoft/windowsazure/services/table/models/UpdateEntityResult.java
@@ -14,13 +14,46 @@
  */
 package com.microsoft.windowsazure.services.table.models;
 
+import com.microsoft.windowsazure.services.table.TableContract;
+
+/**
+ * Represents the response to a request to modify a single table entity in the storage account returned
+ * from a Table Service REST API Update Entity, Merge Entity, Insert or Replace Entity, or Insert or Merge Entity
+ * operation. This is returned by calls to implementations of {@link TableContract#mergeEntity(String, Entity)},
+ * {@link TableContract#mergeEntity(String, Entity, TableServiceOptions)},
+ * {@link TableContract#insertOrMergeEntity(String, Entity)},
+ * {@link TableContract#insertOrMergeEntity(String, Entity, TableServiceOptions)},
+ * {@link TableContract#insertOrReplaceEntity(String, Entity)},
+ * {@link TableContract#insertOrReplaceEntity(String, Entity, TableServiceOptions)},
+ * {@link TableContract#updateEntity(String, Entity)} and
+ * {@link TableContract#updateEntity(String, Entity, TableServiceOptions)}.
+ * <p>
+ * See the <a href="http://msdn.microsoft.com/en-us/library/windowsazure/dd179427.aspx">Update Entity</a> documentation
+ * on MSDN for details of the underlying Table Service REST API operations.
+ */
 public class UpdateEntityResult {
     private String etag;
 
+    /**
+     * Gets the ETag value of the modified entity returned in the server response.
+     * 
+     * @return
+     *         A {@link String} containing the ETag value of the modified entity.
+     */
     public String getEtag() {
         return etag;
     }
 
+    /**
+     * Reserved for internal use. Sets the ETag value of the modified entity from the <strong>ETag</strong> header value
+     * in the server response.
+     * <p>
+     * This method is invoked by the API to set the value from the Table Service REST API operation response returned by
+     * the server.
+     * 
+     * @param etag
+     *            A {@link String} containing the ETag value of the modified entity.
+     */
     public void setEtag(String etag) {
         this.etag = etag;
     }
