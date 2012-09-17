ea38d271a2d57355d184d0bd5c9e156ceb604e81
Chris Tavares <ctavares@microsoft.com>
2012-09-17 16:14:28 -0700

    Brought in definiton for odata action element, start of test cases

---
+++ microsoft-azure-api/src/test/java/com/microsoft/windowsazure/services/media/implementation/ODataSerializationTest.java
@@ -0,0 +1,14 @@
+package com.microsoft.windowsazure.services.media.implementation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ODataSerializationTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
