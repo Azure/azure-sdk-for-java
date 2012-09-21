f52aa2babd7d040535a3695e93de1a99a2836cbd
Chris Tavares <ctavares@microsoft.com>
2012-09-21 12:10:52 -0700

    missing copyright header

--- microsoft-azure-api/src/main/java/com/microsoft/windowsazure/services/media/models/AssetInfo.java
+++ microsoft-azure-api/src/main/java/com/microsoft/windowsazure/services/media/models/AssetInfo.java
@@ -1,10 +1,25 @@
-package com.microsoft.windowsazure.services.media.models;
+/**
+ * Copyright 2012 Microsoft Corporation
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+package com.microsoft.windowsazure.services.media.models;
 import javax.xml.datatype.XMLGregorianCalendar;
 
 import com.microsoft.windowsazure.services.media.implementation.ODataEntity;
 import com.microsoft.windowsazure.services.media.implementation.atom.EntryType;
 import com.microsoft.windowsazure.services.media.implementation.content.AssetType;
+
 
 /**
  * Data about a Media Services Asset entity.
