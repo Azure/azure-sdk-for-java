927e6bb999d56014bf4ee008bf80b57d41168b7e
Albert Cheng <gongchen@microsoft.com>
2012-09-26 16:30:04 -0700

    adds missing java doc skeleton for UpdateAssetOptions class.

--- microsoft-azure-api/src/main/java/com/microsoft/windowsazure/services/media/models/UpdateAssetOptions.java
+++ microsoft-azure-api/src/main/java/com/microsoft/windowsazure/services/media/models/UpdateAssetOptions.java
@@ -14,44 +14,103 @@
  */
 package com.microsoft.windowsazure.services.media.models;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class UpdateAssetOptions.
+ */
 public class UpdateAssetOptions {
 
+    /** The alternate id. */
     private String alternateId;
+
+    /** The name. */
     private String name;
+
+    /** The options. */
     private EncryptionOption options;
+
+    /** The state. */
     private AssetState state;
 
+    /**
+     * Gets the alternate id.
+     * 
+     * @return the alternate id
+     */
     public String getAlternateId() {
         return alternateId;
     }
 
+    /**
+     * Sets the alternate id.
+     * 
+     * @param alternateId
+     *            the alternate id
+     * @return the update asset options
+     */
     public UpdateAssetOptions setAlternateId(String alternateId) {
         this.alternateId = alternateId;
         return this;
     }
 
+    /**
+     * Gets the name.
+     * 
+     * @return the name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name.
+     * 
+     * @param name
+     *            the name
+     * @return the update asset options
+     */
     public UpdateAssetOptions setName(String name) {
         this.name = name;
         return this;
     }
 
+    /**
+     * Gets the options.
+     * 
+     * @return the options
+     */
     public EncryptionOption getOptions() {
         return options;
     }
 
+    /**
+     * Sets the options.
+     * 
+     * @param options
+     *            the options
+     * @return the update asset options
+     */
     public UpdateAssetOptions setOptions(EncryptionOption options) {
         this.options = options;
         return this;
     }
 
+    /**
+     * Gets the state.
+     * 
+     * @return the state
+     */
     public AssetState getState() {
         return state;
     }
 
+    /**
+     * Sets the state.
+     * 
+     * @param assetState
+     *            the asset state
+     * @return the update asset options
+     */
     public UpdateAssetOptions setState(AssetState assetState) {
         this.state = assetState;
         return this;
