65fc7d39782e447189ff20da0dd814bff47ea1a6
Chris Tavares <ctavares@microsoft.com>
2012-10-16 10:19:44 -0700

    Implementation of blobWriter object, including responding to code review feedback.

--- microsoft-azure-api/src/main/java/com/microsoft/windowsazure/services/media/MediaService.java
+++ microsoft-azure-api/src/main/java/com/microsoft/windowsazure/services/media/MediaService.java
@@ -14,7 +14,14 @@
  */
 package com.microsoft.windowsazure.services.media;
 
+import java.net.URI;
+
 import com.microsoft.windowsazure.services.core.Configuration;
+import com.microsoft.windowsazure.services.core.ServiceException;
+import com.microsoft.windowsazure.services.media.implementation.MediaBlobContainerWriter;
+import com.microsoft.windowsazure.services.media.models.LocatorInfo;
+import com.microsoft.windowsazure.services.media.models.LocatorType;
+import com.sun.jersey.api.client.Client;
 
 /**
  * 
@@ -63,5 +70,57 @@ public class MediaService {
      */
     public static MediaContract create(String profile, Configuration config) {
         return config.create(profile, MediaContract.class);
+    }
+
+    /**
+     * Creates an instance of the <code>WritableBlobContainerContract</code> API that will
+     * write to the blob container given by the provided locator.
+     * 
+     * @param locator
+     *            locator specifying where to upload to
+     * @return the implementation of <code>WritableBlobContainerContract</code>
+     * @throws ServiceException
+     */
+    public static WritableBlobContainerContract createBlobWriter(LocatorInfo locator) throws ServiceException {
+        if (locator.getLocatorType() != LocatorType.SAS) {
+            throw new ServiceException("Can only upload to SAS locators");
+        }
+
+        LocatorParser p = new LocatorParser(locator);
+
+        return new MediaBlobContainerWriter(createUploaderClient(), p.getAccountName(), p.getStorageUri(),
+                p.getContainer(), p.getSASToken());
+    }
+
+    /**
+     * Helper class to encapsulate pulling information out of the locator.
+     */
+    private static class LocatorParser {
+        URI locatorPath;
+
+        LocatorParser(LocatorInfo locator) {
+            locatorPath = URI.create(locator.getPath());
+        }
+
+        String getAccountName() {
+            return locatorPath.getHost().split("\\.")[0];
+        }
+
+        String getStorageUri() {
+            return locatorPath.getScheme() + "://" + locatorPath.getAuthority();
+        }
+
+        String getContainer() {
+            return locatorPath.getPath().substring(1);
+        }
+
+        String getSASToken() {
+            return locatorPath.getRawQuery();
+        }
+    }
+
+    private static Client createUploaderClient() {
+        Client client = Client.create();
+        return client;
     }
 }
