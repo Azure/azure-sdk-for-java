ed53fd26dcd1ad3c42e44163d694dab473e408f8
Albert Cheng <gongchen@microsoft.com>
2013-04-23 14:11:08 -0700

    initial check in for the scaffold of the QueueInfoTest

---
+++ microsoft-azure-api/src/test/java/com/microsoft/windowsazure/services/serviceBus/models/QueueInfoTest.java
@@ -0,0 +1,151 @@
+/**
+ * Copyright Microsoft Corporation
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.microsoft.windowsazure.services.serviceBus.models;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class QueueInfoTest {
+
+    @Test
+    public void testGetSetLockDuration() {
+        // Arrange
+        QueueInfo QueueInfo = new QueueInfo();
+
+        // Act 
+        String actualId = QueueInfo.getId();
+
+        // Assert
+        assertEquals(expectedId, actualId);
+
+    }
+
+    @Test
+    public void testGetSetMaxSizeInMegabytes() {
+
+    }
+
+    @Test
+    public void testGetSetRequiresDuplicateDetection() {
+
+    }
+
+    @Test
+    public void testGetSetRequiresSession() {
+
+    }
+
+    @Test
+    public void testGetSetDefaultMessageTimeToLive() {
+
+    }
+
+    @Test
+    public void testGetSetDeadLetteringOnMessageExpiration() {
+
+    }
+
+    @Test
+    public void testGetSetDuplicateDetectionHistoryTimeWindow() {
+
+    }
+
+    @Test
+    public void testGetSetMaxDeliveryCount() {
+
+    }
+
+    @Test
+    public void testGetSetEnableBatchedOperations() {
+
+    }
+
+    @Test
+    public void testGetSetSizeInBytes() {
+
+    }
+
+    @Test
+    public void testGetSetMessageCount() {
+
+    }
+
+    @Test
+    public void testGetSetIsAnonymousAccessible() {
+
+    }
+
+    @Test
+    public void testGetSetAuthorizationRules() {
+
+    }
+
+    @Test
+    public void testGetSetStatus() {
+
+    }
+
+    @Test
+    public void testGetSetForwardTo() {
+
+    }
+
+    @Test
+    public void testGetSetCreatedAt() {
+
+    }
+
+    @Test
+    public void testGetSetUpdatedAt() {
+
+    }
+
+    @Test
+    public void testGetSetAccessedAt() {
+
+    }
+
+    @Test
+    public void testGetSetUserMetadata() {
+
+    }
+
+    @Test
+    public void testGetSetSupportOrdering() {
+
+    }
+
+    @Test
+    public void testGetSetCountDetails() {
+
+    }
+
+    @Test
+    public void testGetSetAutoDeleteOnIdle() {
+
+    }
+
+    @Test
+    public void testGetSetPartioningPolicy() {
+
+    }
+
+    @Test
+    public void testGetSetEntityAvailabilityStatus() {
+
+    }
+
+}
