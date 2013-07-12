7939e511376fe83125b5f8afc32f967cf7fc2c34
Albert Cheng <gongchen@microsoft.com>
2013-07-12 16:25:47 -0700

    add microsoft copy right.

--- microsoft-azure-api/src/main/java/com/microsoft/windowsazure/services/management/ConnectionCredential.java
+++ microsoft-azure-api/src/main/java/com/microsoft/windowsazure/services/management/ConnectionCredential.java
@@ -1,8 +1,16 @@
-/*
+/**
+ * Copyright Microsoft Corporation
  * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * The author contributes this code to the public domain,
- * retaining no rights and incurring no responsibilities for its use in whole or in part.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.microsoft.windowsazure.services.management;
