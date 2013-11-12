40d91908fbbb99e4c18c943b3b3b022cc63fe08f
velocipedist <velocipedist@gmail.com>
2013-11-12 12:35:41 +0000

    Updated next() method to satisfy the iterator contract and not throw a null pointer exception, if it was called without hasNext() being called.
    The previous implementation did not initialize the *currentSegmentIterator*, unless *hasNext()* has been called, which lead to unexpected *NullPointerException*.
    I've changed the impl to call *hasNext()* prior to the next call.

--- microsoft-azure-api/src/main/java/com/microsoft/windowsazure/services/core/storage/utils/implementation/LazySegmentedIterator.java
+++ microsoft-azure-api/src/main/java/com/microsoft/windowsazure/services/core/storage/utils/implementation/LazySegmentedIterator.java
@@ -130,7 +130,11 @@ public final class LazySegmentedIterator<CLIENT_TYPE, PARENT_TYPE, ENTITY_TYPE> 
      */
     @Override
     public ENTITY_TYPE next() {
-        return this.currentSegmentIterator.next();
+        if(hasNext()) {
+          return this.currentSegmentIterator.next();
+        } else {
+            throw new NoSuchElementException();
+        }
     }
 
     /**
