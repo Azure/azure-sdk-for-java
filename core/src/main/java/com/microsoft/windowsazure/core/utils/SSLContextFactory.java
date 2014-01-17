6a2f0083523945f1215ad0fecd8745efda041fc6
Andre Rodrigues <andrerod@microsoft.com>
2014-01-17 09:30:06 +0100

    Commenting out System.out.println

--- core/src/main/java/com/microsoft/windowsazure/core/utils/SSLContextFactory.java
+++ core/src/main/java/com/microsoft/windowsazure/core/utils/SSLContextFactory.java
@@ -107,20 +107,20 @@ public final class SSLContextFactory
                     {
                         public X509Certificate[] getAcceptedIssuers()
                         {
-                            System.out.println("getAcceptedIssuers =============");
+                            // System.out.println("getAcceptedIssuers =============");
                             return null;
                         }
 
                         public void checkClientTrusted(X509Certificate[] certs,
                                 String authType)
                         {
-                            System.out.println("checkClientTrusted =============");
+                            // System.out.println("checkClientTrusted =============");
                         }
 
                         public void checkServerTrusted(X509Certificate[] certs,
                                 String authType)
                         {
-                            System.out.println("checkServerTrusted =============");
+                            // System.out.println("checkServerTrusted =============");
                         }
                     }
                 }, new SecureRandom());
