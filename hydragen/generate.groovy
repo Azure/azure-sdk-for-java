a1b68adc6830592ce62acd3cc73cba82318194cf
Chris Tavares <ctavares@microsoft.com>
2014-01-20 15:21:58 -0800

    Added groovy script for regenerating code from hydra specs

---
+++ hydragen/generate.groovy
@@ -0,0 +1,159 @@
+/**
+ *
+ * Copyright (c) Microsoft and contributors.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+//
+// List of spec DLLs and generation detail
+// Add to this list to add new specs
+//
+def hydraSpecs = [
+    [
+        spec: "Microsoft.WindowsAzure.Management.Compute.Specification.dll",
+        clientType: "Microsoft.WindowsAzure.Management.Compute.ComputeManagementClient",
+        destDir: "management-compute"
+    ],
+    [
+        spec: "Microsoft.WindowsAzure.Management.Specification.dll",
+        clientType: 'Microsoft.WindowsAzure.Management.ManagementClient',
+        destDir: 'management'
+    ],
+    [
+        spec: "Microsoft.WindowsAzure.Management.Network.Specification.dll",
+        clientType: "Microsoft.WindowsAzure.Management.VirtualNetworks.VirtualNetworkManagementClient",
+        destDir: 'management-virtualnetwork'
+    ],
+    [
+        spec: "Microsoft.WindowsAzure.Management.Scheduler.Specification.dll",
+        clientType: 'Microsoft.WindowsAzure.Management.Scheduler.SchedulerManagementClient',
+        destDir: 'management-scheduler'
+    ],
+    [
+        spec: "Microsoft.WindowsAzure.Management.Scheduler.Specification.dll",
+        clientType: 'Microsoft.WindowsAzure.Scheduler.SchedulerManagementClient',
+        destDir: 'scheduler'
+    ],
+    [
+        spec: "Microsoft.WindowsAzure.Management.ServiceBus.Specification.dll",
+        clientType: 'Microsoft.WindowsAzure.Management.ServiceBus.ServiceBusManagementClient',
+        destDir: 'management-serviceBus'
+    ],
+    [
+        spec: "Microsoft.WindowsAzure.Management.Sql.Specification.dll",
+        clientType: "Microsoft.WindowsAzure.Management.Sql.SqlManagementClient",
+        destDir: "management-sql"
+    ],
+    [
+        spec: "Microsoft.WindowsAzure.Management.Storage.Specification.dll",
+        clientType: "Microsoft.WindowsAzure.Management.Storage.StorageManagementClient",
+        destDir: "management-storage"
+    ],
+    [
+        spec: "Microsoft.WindowsAzure.Management.Store.Specification.dll",
+        clientType: "Microsoft.WindowsAzure.Management.Store.StoreManagementClient",
+        destDir: "management-store"
+    ],
+    [
+        spec: "Microsoft.WindowsAzure.Management.WebSites.Specification.dll",
+        clientType: "Microsoft.WindowsAzure.Management.WebSites.WebSiteManagementClient",
+        destDir: "management-website"
+    ]
+]
+
+//////////////////////////////////////////////////////////////////////
+//
+// Implementation below here
+
+// Download a file from a url
+def download(address)
+{
+  def file = new FileOutputStream(address.tokenize("/")[-1])
+  def out = new BufferedOutputStream(file)
+  out << new URL(address).openStream()
+  out.close()
+}
+
+// Calculates if a CLR exe should run directly or via mono
+def exePrefix() {
+    def isWindows = System.properties['os.name'].toLowerCase().contains('windows')
+    if (isWindows) {
+        return []
+    }
+    ["mono", '--runtime=v4.0.30319']
+}
+
+// Run an executable with the given command line arguments
+def run(String exePath, String... args)
+{
+    def commands = [exePrefix(), exePath, args].flatten()
+    commands.execute()
+}
+
+// Execute nuget.exe with the given command line arguments
+def nuget(String... args) { run('./nuget.exe', args) }
+
+// Execute hydra.exe with the given command line arguments
+def hydra(String hydraExePath, String... args)
+{
+    def commands = [exePrefix(), hydraExePath, args].flatten()
+    commands.execute()
+}
+
+// Generate code for the given specInfo
+def generate(hydraExePath, specInfo)
+{
+    def specDLL = findFile(specInfo.spec)
+    hydra(hydraExePath, '-f', 'java', '-s', 'namespace',
+        '-c', specInfo.clientType,
+        '-d', "../${specInfo.destDir}/src/main/java/com",
+        specDLL)
+}
+
+// Run nuget.exe to restore required nuget packages
+def restorePackages()
+{
+    def env = System.getenv()
+    try {
+        nuget('sources', 'add', '-name', 'download', '-source', env['PRIVATE_FEED_URL'], '-configfile', './restore.config')
+        nuget('sources', 'update', '-name', 'download', '-username', env['PRIVATE_FEED_USER_NAME'], '-configfile', './restore.config')
+        nuget('restore', 'packages.config', '-packagesdirectory', './packages', '-configfile', './restore.config')
+    }
+    finally {
+        // Need to wait a bit, config file stays open while nuget.exe shuts down
+        Thread.sleep(500)
+        new File('./restore.config').delete()
+    }
+}
+
+// Find a file with the given file name (case insensitive match) somewhere under packages
+def findFile(pattern)
+{
+    def path
+    new File('packages').traverse([nameFilter: ~"(?i)${pattern}\$"], { path = it.toString() })
+    path
+
+}
+
+//
+// Main logic here
+//
+download("http://www.nuget.org/nuget.exe")
+restorePackages()
+def hydraPath = findFile('hydra.exe')
+hydraSpecs.each {
+    System.out.println("generating code for ${it.spec}")
+    generate hydraPath, it
+}
