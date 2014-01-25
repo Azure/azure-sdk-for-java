c7b1e1e30390bd648d87f1c6dcffb8bbb6fd8dfd
Andre Rodrigues <andrerod@microsoft.com>
2014-01-25 02:34:03 +0100

    Fix indentation

--- core-test/src/test/java/com/microsoft/windowsazure/ManagementResourceStepdefs.java
+++ core-test/src/test/java/com/microsoft/windowsazure/ManagementResourceStepdefs.java
@@ -231,9 +231,9 @@ public class ManagementResourceStepdefs
     protected static Configuration createConfiguration() throws Exception
     {
         return ManagementConfiguration.configure(
-        		System.getenv(ManagementConfiguration.SUBSCRIPTION_ID),
-                System.getenv(ManagementConfiguration.KEYSTORE_PATH),
-                System.getenv(ManagementConfiguration.KEYSTORE_PASSWORD)
+	        System.getenv(ManagementConfiguration.SUBSCRIPTION_ID),
+	        System.getenv(ManagementConfiguration.KEYSTORE_PATH),
+	        System.getenv(ManagementConfiguration.KEYSTORE_PASSWORD)
         );
     }
 }
