4b98731a7ec870dfd149aa96d2612128a9764502
bibin <bibin>
2014-02-10 17:24:05 -0800

    check junit testing for management that are passed.

---
+++ management/src/test/java/com/microsoft/windowsazure/management/RoleSizeOperationsTest.java
@@ -0,0 +1,26 @@
+package com.microsoft.windowsazure.management;
+
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.microsoft.windowsazure.management.models.RoleSizeListResponse;
+
+public class RoleSizeOperationsTest  extends ManagementIntegrationTestBase { 
+	
+	   @BeforeClass
+	    public static void setup() throws Exception {
+	        createService();	      
+	    }
+	    
+	    @Test
+	    public void listRoleSizeSuccess() throws Exception {	    	   	  	 
+	    	
+	    	 RoleSizeListResponse roleSizeListResponse = managementClient.getRoleSizesOperations().list();
+	    	
+	    	 Assert.assertEquals(200, roleSizeListResponse.getStatusCode());	    	 
+		     Assert.assertNotNull(roleSizeListResponse.getRequestId());		
+		     Assert.assertEquals(10, roleSizeListResponse.getRoleSizes().size());		   
+	    }	    
+}    
