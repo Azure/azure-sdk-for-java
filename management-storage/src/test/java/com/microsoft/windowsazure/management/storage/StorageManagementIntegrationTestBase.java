fc3f72bcfc562090e5637b81fcb9cf56d7bb660e
xuezhai <xuezhain@microsoft.com>
2014-06-09 17:59:32 -0700

    make storage location more flexiable to make it run on MoonCake platform

--- management-storage/src/test/java/com/microsoft/windowsazure/management/storage/StorageManagementIntegrationTestBase.java
+++ management-storage/src/test/java/com/microsoft/windowsazure/management/storage/StorageManagementIntegrationTestBase.java
@@ -15,20 +15,29 @@
 package com.microsoft.windowsazure.management.storage;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.Random;
 
 import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
 
 import com.microsoft.windowsazure.core.pipeline.apache.ApacheConfigurationProperties;
 import com.microsoft.windowsazure.core.utils.KeyStoreType;
+import com.microsoft.windowsazure.management.ManagementClient;
+import com.microsoft.windowsazure.management.ManagementService;
 import com.microsoft.windowsazure.management.configuration.ManagementConfiguration;
 import com.microsoft.windowsazure.Configuration;
+import com.microsoft.windowsazure.management.configuration.PublishSettingsLoader;
+import com.microsoft.windowsazure.management.models.LocationAvailableServiceNames;
+import com.microsoft.windowsazure.management.models.LocationsListResponse;
 
 public abstract class StorageManagementIntegrationTestBase {
 
     protected static String testStorageAccountPrefix = "azurejavatest";
+    protected static String storageLocation = null;
+    
 
     protected static StorageManagementClient storageManagementClient;
+    protected static ManagementClient managementClient;
 
     protected static void createService() throws Exception {
         // reinitialize configuration from known state
@@ -37,16 +46,46 @@ public abstract class StorageManagementIntegrationTestBase {
 
         storageManagementClient = StorageManagementService.create(config);
     }
+    
+    protected static void createManagementClient() throws Exception {
+        Configuration config = createConfiguration();
+        config.setProperty(ApacheConfigurationProperties.PROPERTY_RETRY_HANDLER, new DefaultHttpRequestRetryHandler());
+        managementClient = ManagementService.create(config);
+    }       
   
     protected static Configuration createConfiguration() throws Exception {
-        String baseUri = System.getenv(ManagementConfiguration.URI);
-        return ManagementConfiguration.configure(
-            baseUri != null ? new URI(baseUri) : null,
-            System.getenv(ManagementConfiguration.SUBSCRIPTION_ID),
-            System.getenv(ManagementConfiguration.KEYSTORE_PATH),
-            System.getenv(ManagementConfiguration.KEYSTORE_PASSWORD),
-            KeyStoreType.fromString(System.getenv(ManagementConfiguration.KEYSTORE_TYPE))
-        );
+//        String baseUri = System.getenv(ManagementConfiguration.URI);
+//        return ManagementConfiguration.configure(
+//            baseUri != null ? new URI(baseUri) : null,
+//            System.getenv(ManagementConfiguration.SUBSCRIPTION_ID),
+//            System.getenv(ManagementConfiguration.KEYSTORE_PATH),
+//            System.getenv(ManagementConfiguration.KEYSTORE_PASSWORD),
+//            KeyStoreType.fromString(System.getenv(ManagementConfiguration.KEYSTORE_TYPE))
+//        );
+        return PublishSettingsLoader.createManagementConfiguration("C:\\Users\\xuezhain\\Downloads\\Azure SDK Mooncake sandbox-5-14-2014-credentials.publishsettings","23a32706-03c1-4920-a6ac-1b7c8de24bb4");
+ 
+    }
+    
+    protected static void getLocation() throws Exception {
+        ArrayList<String> serviceName = new ArrayList<String>();       
+        serviceName.add(LocationAvailableServiceNames.STORAGE);       
+
+        LocationsListResponse locationsListResponse = managementClient.getLocationsOperations().list();
+        for (LocationsListResponse.Location location : locationsListResponse) {
+            ArrayList<String> availableServicelist = location.getAvailableServices();
+            String locationName = location.getName();
+            if (availableServicelist.containsAll(serviceName)== true) {  
+                if (locationName.contains("West US") == true)
+                {
+                    storageLocation = locationName;
+                }
+                if (storageLocation==null)
+                {
+                    storageLocation = locationName;
+                }
+            }
+        } 
+        System.out.println("storageLocation = " + storageLocation);
     }
     
     protected static String randomString(int length)
