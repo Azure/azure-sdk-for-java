0d1403abdd0bafcebd0688f0b35637160e0b5054
Jianghao Lu <jianghaolu@users.noreply.github.com>
2015-11-10 17:35:30 -0800

    Initialize autorest generated clients

--- resource-management/azure-mgmt-compute/src/main/readme.md
+++
@@ -1,3 +0,0 @@
-# Readme Compute2
-
-* The scenarios tests for compute2 library has been moved to azure-mgme-utility.
--- services/azure-media/src/main/java/com/microsoft/windowsazure/services/media/implementation/templates/playreadylicense/package-info.java
+++
@@ -1,7 +0,0 @@
-@XmlSchema(
-    namespace = "http://schemas.microsoft.com/Azure/MediaServices/KeyDelivery/PlayReadyTemplate/v1",
-    elementFormDefault = XmlNsForm.QUALIFIED)
-package com.microsoft.windowsazure.services.media.implementation.templates.playreadylicense;
-
-import javax.xml.bind.annotation.XmlNsForm;
-import javax.xml.bind.annotation.XmlSchema;
