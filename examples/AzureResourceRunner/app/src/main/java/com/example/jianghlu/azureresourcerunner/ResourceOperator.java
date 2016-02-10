6a4aec7946a602bd2116a6e0fa104bd065fd8265
Jianghao Lu <jianghlu@microsoft.com>
2016-02-10 13:59:10 -0800

    Add resource group fragment

--- examples/AzureResourceRunner/app/src/main/java/com/example/jianghlu/azureresourcerunner/ResourceOperator.java
+++ examples/AzureResourceRunner/app/src/main/java/com/example/jianghlu/azureresourcerunner/ResourceOperator.java
@@ -1,9 +1,9 @@
 package com.example.jianghlu.azureresourcerunner;
 
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,9 +12,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.microsoft.azure.credentials.UserTokenCredentials;
+import com.microsoft.azure.management.compute.ComputeManagementClient;
+import com.microsoft.azure.management.compute.ComputeManagementClientImpl;
+import com.microsoft.azure.management.resources.ResourceManagementClient;
+import com.microsoft.azure.management.resources.ResourceManagementClientImpl;
+import com.microsoft.azure.management.storage.StorageManagementClient;
+import com.microsoft.azure.management.storage.StorageManagementClientImpl;
 
 public class ResourceOperator extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, ResourceFragment.OnFragmentInteractionListener {
+    SubscriptionInfo subscription;
+    ResourceManagementClient rClient;
+    StorageManagementClient sClient;
+    ComputeManagementClient cClient;
+
+    private final static String CLIENT_ID = "1950a258-227b-4e31-a9cf-717495945fc2";
+    private final static String REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +38,6 @@ public class ResourceOperator extends AppCompatActivity
         setContentView(R.layout.activity_resource_operator);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -41,6 +48,21 @@ public class ResourceOperator extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setItemIconTintList(null);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+        Intent intent = getIntent();
+        subscription = (SubscriptionInfo) intent.getSerializableExtra("subscription");
+        setTitle(subscription.getSubscriptionName());
+
+        UserTokenCredentials credentials = new UserTokenCredentials(
+                this, CLIENT_ID, subscription.getTenantId(), REDIRECT_URI
+        );
+        rClient = new ResourceManagementClientImpl(credentials);
+        sClient = new StorageManagementClientImpl(credentials);
+        cClient = new ComputeManagementClientImpl(credentials);
+        rClient.setSubscriptionId(subscription.getSubscriptionId());
+        sClient.setSubscriptionId(subscription.getSubscriptionId());
+        cClient.setSubscriptionId(subscription.getSubscriptionId());
     }
 
     @Override
@@ -82,7 +104,20 @@ public class ResourceOperator extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_resource) {
+            ResourceFragment fragment = new ResourceFragment();
+            fragment.setRetainInstance(true);
+            fragment.setResourceManagementClient(rClient);
+            FragmentManager fragmentManager = getFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.resource_operator_main_content, fragment)
+                    .commit();
 
+            // Highlight the selected item, update the title, and close the drawer
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+            navigationView.setCheckedItem(0);
+            setTitle(this.getTitle() + " - Resources");
+            drawer.closeDrawer(navigationView);
         } else if (id == R.id.nav_storage) {
 
         } else if (id == R.id.nav_compute) {
@@ -96,5 +131,10 @@ public class ResourceOperator extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
