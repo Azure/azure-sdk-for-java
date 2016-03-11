f530e1e87b207e0269d0eca3f2c1b21c24930f4d
Sreeram Garlapati <sreeramg@microsoft.com>
2016-03-10 20:32:18 -0800

    correct the namespace name

--- azure-eventhubs-log4j-appender/src/main/java/com/microsoft/azure/eventhubs/extensions/EventHubsAppender.java
+++ azure-eventhubs-log4j-appender/src/main/java/com/microsoft/azure/eventhubs/extensions/EventHubsAppender.java
@@ -2,7 +2,7 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project root for full license information.
  */
-package com.microsoft.azure.extensions;
+package com.microsoft.azure.eventhubs.extensions;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
