eaf53dee1795c4eee4b306b55cc74603a3e5b9c3
anuchan <anuamd@gmail.com>
2016-04-21 11:15:52 -0700

    Simplifying AzureResourceGroupImpl using Storage connector

--- azure/src/main/java/com/microsoft/azure/implementation/AzureResourceGroupImpl.java
+++ azure/src/main/java/com/microsoft/azure/implementation/AzureResourceGroupImpl.java
@@ -1,63 +1,26 @@
 package com.microsoft.azure.implementation;
 
-import com.microsoft.azure.management.resources.ResourceGroups;
-import com.microsoft.azure.management.resources.implementation.ResourceGroupsImpl;
 import com.microsoft.azure.management.resources.implementation.api.ResourceManagementClientImpl;
 import com.microsoft.azure.management.resources.ResourceGroup;
 import com.microsoft.azure.management.resources.implementation.ResourceGroupImpl;
 import com.microsoft.azure.management.storage.StorageAccounts;
-import com.microsoft.azure.management.storage.implementation.StorageAccountsImpl;
-import com.microsoft.azure.management.storage.implementation.StorageAccountsInGroupImpl;
-import com.microsoft.azure.management.storage.implementation.api.StorageManagementClientImpl;
-import com.microsoft.rest.credentials.ServiceClientCredentials;
+import com.microsoft.azure.management.storage.implementation.StorageResourceConnector;
 
 public class AzureResourceGroupImpl extends ResourceGroupImpl implements Azure.ResourceGroup {
-    private ResourceManagementClientImpl client;
     private ResourceGroup resourceGroupCore;
-    private ResourceGroups resourceGroupsCore;
-    private StorageAccounts storageAccountsCore;
 
     public AzureResourceGroupImpl(ResourceGroup resourceGroupCore, ResourceManagementClientImpl client) {
         super(resourceGroupCore.inner(), client);
         this.resourceGroupCore = resourceGroupCore;
-        this.client = client;
     }
 
     // StorageAccount collection having a resource resourceGroupCore context.
     //
     public StorageAccounts.InGroup storageAccounts() {
-        return new StorageAccountsInGroupImpl(storageAccountsCore(), resourceGroupCore);
+        return resourceGroupCore.connectToResource(new StorageResourceConnector.Builder()).storageAccounts();
     }
 
     public ResourceGroup resourceGroupCore() {
         return resourceGroupCore;
-    }
-
-    private ResourceGroups resourceGroupsCore() {
-        if (resourceGroupsCore == null) {
-            resourceGroupsCore = new ResourceGroupsImpl(this.client);
-        }
-        return resourceGroupsCore;
-    }
-
-    private StorageAccounts storageAccountsCore() {
-        if (storageAccountsCore == null) {
-            storageAccountsCore = new StorageAccountsImpl(StorageManagementClient(), resourceGroupsCore());
-        }
-        return storageAccountsCore;
-    }
-
-    private ServiceClientCredentials credentials() {
-        return this.client.getCredentials();
-    }
-
-    private String subscriptionId() {
-        return this.client.getSubscriptionId();
-    }
-
-    private StorageManagementClientImpl StorageManagementClient() {
-        StorageManagementClientImpl client = new StorageManagementClientImpl(credentials());
-        client.setSubscriptionId(subscriptionId());
-        return client;
     }
 }
