819be169cf3e9c11cc52a2001b5d122eeb86de87
xingwu1 <xingwu1@yahoo.com>
2016-04-25 16:10:06 -0700

    Fix bugs in the addbulktasks function and use multiple-thread to improve performance.

---
+++ src/main/java/com/microsoft/azure/batch/interceptor/ServerTimeoutInterceptor.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
+package com.microsoft.azure.batch.interceptor;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class ServerTimeoutInterceptor extends RequestInterceptor {
+
+    private final int serverTimeout;
+
+    public ServerTimeoutInterceptor(int timeout) {
+        this.serverTimeout = timeout;
+        this.setHandler(new BatchRequestInterceptHandler() {
+            @Override
+            public void modify(Object request) {
+                Class<?> c = request.getClass();
+                try {
+                    Method timeoutMethod = c.getMethod("setTimeout", new Class[]{Integer.class});
+                    if (timeoutMethod != null) {
+                        timeoutMethod.invoke(request, serverTimeout);
+                    }
+                } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException ex) {
+                }
+            }
+        });
+    }
+}
