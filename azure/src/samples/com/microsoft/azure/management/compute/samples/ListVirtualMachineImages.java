bcca38ee677f91603d72cb477eae39d486bd1cee
Asir Vedamuthu Selvasingh <asirveda@microsoft.com>
2016-06-09 12:48:49 -0700

    Placeholders for samples

---
+++ azure/src/samples/com/microsoft/azure/management/compute/samples/ListVirtualMachineImages.java
@@ -0,0 +1,29 @@
+/**
+ *
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ *
+ */
+
+package com.microsoft.azure.management.compute.samples;
+
+/**
+ * List all virtual machine images by browsing through
+ * locations, publishers, offers, SKUs and images
+ */
+
+public class ListVirtualMachineImages {
+
+    public static void main (String [] args) {
+
+        try {
+
+        // List all virtual machine images by browsing through
+        // locations, publishers, offers, skus and images
+
+        } catch (Exception e)
+        {
+            System.err.println(e.getMessage());
+        }
+    }
+}
