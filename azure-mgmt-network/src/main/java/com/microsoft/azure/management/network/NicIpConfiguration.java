792ca819bac2c78c61bc5cbfee66f4ec28e313ee
anuchan <anuamd@gmail.com>
2016-06-15 16:25:34 -0700

    removing unused imports

--- azure-mgmt-network/src/main/java/com/microsoft/azure/management/network/NicIpConfiguration.java
+++ azure-mgmt-network/src/main/java/com/microsoft/azure/management/network/NicIpConfiguration.java
@@ -3,7 +3,6 @@ package com.microsoft.azure.management.network;
 import com.microsoft.azure.CloudException;
 import com.microsoft.azure.management.network.implementation.api.NetworkInterfaceIPConfiguration;
 import com.microsoft.azure.management.resources.fluentcore.arm.models.ChildResource;
-import com.microsoft.azure.management.resources.fluentcore.model.Appliable;
 import com.microsoft.azure.management.resources.fluentcore.model.Attachable;
 import com.microsoft.azure.management.resources.fluentcore.model.Settable;
 import com.microsoft.azure.management.resources.fluentcore.model.Wrapper;
