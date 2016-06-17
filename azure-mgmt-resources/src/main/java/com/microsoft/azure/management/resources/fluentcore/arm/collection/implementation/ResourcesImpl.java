59c8be59799717ad16a24251e6d7b3584ca8e4bf
unknown <marcins@MARCINS-BRICK.redmond.corp.microsoft.com>
2016-06-17 13:55:40 -0700

    base class for collection classes for shared functionality

---
+++ azure-mgmt-resources/src/main/java/com/microsoft/azure/management/resources/fluentcore/arm/collection/implementation/ResourcesImpl.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+package com.microsoft.azure.management.resources.fluentcore.arm.collection.implementation;
+
+import java.io.IOException;
+
+import com.microsoft.azure.CloudException;
+import com.microsoft.azure.management.resources.fluentcore.arm.ResourceUtils;
+import com.microsoft.azure.management.resources.fluentcore.arm.collection.SupportsGettingByGroup;
+import com.microsoft.azure.management.resources.fluentcore.arm.collection.SupportsGettingById;
+
+/**
+ * Base class for resource collection classes.
+ * (Internal use only)
+ * @param <T> the individual resource type returned
+ */
+public abstract class ResourcesImpl<T>
+    implements
+        SupportsGettingById<T>,
+        SupportsGettingByGroup<T> {
+
+    @Override
+    public abstract T getByGroup(String groupName, String name) throws CloudException, IOException;
+
+    @Override
+    public T getById(String id) throws CloudException, IOException {
+        return this.getByGroup(
+                ResourceUtils.groupFromResourceId(id),
+                ResourceUtils.nameFromResourceId(id));
+    }
+}
