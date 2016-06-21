5eb27f22b9e0b30f401698d9fc4eddd4c414fab5
Asir Vedamuthu Selvasingh <asirveda@microsoft.com>
2016-06-21 08:43:00 -0700

    Finishing List Virtual Machine Images sample

--- azure-samples/src/main/java/com/microsoft/azure/management/compute/samples/ListVirtualMachineImages.java
+++ azure-samples/src/main/java/com/microsoft/azure/management/compute/samples/ListVirtualMachineImages.java
@@ -6,9 +6,20 @@
 
 package com.microsoft.azure.management.compute.samples;
 
+import com.microsoft.azure.Azure;
+import com.microsoft.azure.management.compute.Offer;
+import com.microsoft.azure.management.compute.Publisher;
+import com.microsoft.azure.management.compute.Sku;
+import com.microsoft.azure.management.compute.VirtualMachineImage;
+import com.microsoft.azure.management.resources.fluentcore.arm.Region;
+import okhttp3.logging.HttpLoggingInterceptor;
+
+import java.io.File;
+import java.util.List;
+
 /**
- * List all virtual machine images by browsing through
- * locations, publishers, offers, SKUs and images.
+ * List all virtual machine images (published by Canonical, Red Hat and
+ * Suse) by browsing through locations, publishers, offers, SKUs and images.
  */
 public final class ListVirtualMachineImages {
     /**
@@ -18,10 +29,69 @@ public final class ListVirtualMachineImages {
      */
     public static void main(String[] args) {
         try {
-        // List all virtual machine images by browsing through
-        // locations, publishers, offers, SKUs and images
+
+
+            //=================================================================
+            // Authenticate
+
+            final File credFile = new File("my.azureauth");
+
+            Azure azure = Azure
+                    .configure()
+                    .withLogLevel(HttpLoggingInterceptor.Level.NONE)
+                    .authenticate(credFile)
+                    .withDefaultSubscription();
+
+
+            //=================================================================
+            // List all virtual machine images
+            // (published by Canonical, Red Hat and Suse)
+            // by browsing through locations, publishers, offers, SKUs and images
+
+            List<Publisher> publishers = azure
+                    .virtualMachineImages()
+                    .publishers()
+                    .listByRegion(Region.US_EAST);
+
+            Publisher chosenPublisher;
+
+            System.out.println("Printing list of publishers and images published by \nCanonical, Red Hat and Suse");
+            System.out.println("=======================================================");
+            System.out.println("\n");
+            
+            for (Publisher publisher : publishers) {
+
+                System.out.println("Publisher - " + publisher.name());
+
+                if (publisher.name().equalsIgnoreCase("Canonical")
+                        | publisher.name().equalsIgnoreCase("Suse")
+                        | publisher.name().equalsIgnoreCase("RedHat")) {
+
+                    chosenPublisher = publisher;
+                    System.out.print("\n\n");
+                    System.out.println("=======================================================");
+                    System.out.println("Located " + chosenPublisher.name());
+                    System.out.println("=======================================================");
+                    System.out.println("Printing entries as publisher/offer/sku/image.version()");
+
+                    for (Offer offer : chosenPublisher.offers().list()) {
+                        for (Sku sku: offer.skus().list()) {
+                            for (VirtualMachineImage image : sku.listImages()) {
+                                System.out.println("Image - " + chosenPublisher.name() + "/"
+                                        + offer.name() + "/"
+                                        + sku.name() + "/" + image.version());
+                            }
+                        }
+
+                    }
+
+                    System.out.print("\n\n");
+
+                }
+            }
         } catch (Exception e) {
-            System.err.println(e.getMessage());
+            System.out.println(e.getMessage());
+            e.printStackTrace();
         }
     }
 
