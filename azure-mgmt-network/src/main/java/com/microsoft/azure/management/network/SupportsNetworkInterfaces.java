a87928fdcaae53d65d6923263944d9979ceb5530
unknown <marcins@MARCINS-BRICK.redmond.corp.microsoft.com>
2016-07-15 18:40:48 -0700

    moving NIC-related accessors to a new interface in Networking, extended by VirtualMachine, to enable access to the NICs of a VirtualMachine from Networking APIs

---
+++ azure-mgmt-network/src/main/java/com/microsoft/azure/management/network/SupportsNetworkInterfaces.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+package com.microsoft.azure.management.network;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.microsoft.azure.CloudException;
+
+/**
+ * Interface exposing a list of network interfaces.
+ */
+public interface SupportsNetworkInterfaces {
+    /**
+     * Gets the primary network interface.
+     * <p>
+     * Note that this method can result in a call to the cloud to fetch the network interface information.
+     *
+     * @return the primary network interface associated with this resource
+     * @throws CloudException exceptions thrown from the cloud
+     * @throws IOException exceptions thrown from serialization/deserialization.
+     */
+    NetworkInterface primaryNetworkInterface() throws CloudException, IOException;
+
+    /**
+     * @return the resource id of the primary network interface associated with this resource
+     */
+    String primaryNetworkInterfaceId();
+
+    /**
+     * @return the list of resource IDs of the network interfaces associated with this resource
+     */
+    List<String> networkInterfaceIds();
+}
