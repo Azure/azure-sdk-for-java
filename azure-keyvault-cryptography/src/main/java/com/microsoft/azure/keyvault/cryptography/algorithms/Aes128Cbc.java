4612ba66b0850f52faec4c53b86f408ba555ba60
Hervey Wilson <herveyw@microsoft.com>
2016-08-22 13:46:46 -0700

    Crypto test cases for excess key material support to match .NET

--- azure-keyvault-cryptography/src/main/java/com/microsoft/azure/keyvault/cryptography/algorithms/Aes128Cbc.java
+++ azure-keyvault-cryptography/src/main/java/com/microsoft/azure/keyvault/cryptography/algorithms/Aes128Cbc.java
@@ -18,12 +18,62 @@
 
 package com.microsoft.azure.keyvault.cryptography.algorithms;
 
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.Provider;
+
+import javax.crypto.NoSuchPaddingException;
+
+import com.microsoft.azure.keyvault.cryptography.ICryptoTransform;
+
 public class Aes128Cbc extends AesCbc {
 
     public static final String AlgorithmName = "A128CBC";
+    
+    static final int KeySizeInBytes = 128 >> 3;
 
     public Aes128Cbc() {
         super(AlgorithmName);
     }
+    
+    @Override
+    public ICryptoTransform CreateEncryptor(byte[] key, byte[] iv, byte[] authenticationData) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException {
 
+    	if (key == null || key.length < KeySizeInBytes) {
+    		throw new InvalidKeyException("key must be at least 128 bits in length");
+    	}
+    	
+        return new AesCbcEncryptor(AesCbc.Take(KeySizeInBytes, key), iv, null);
+    }
+
+    @Override
+    public ICryptoTransform CreateEncryptor(byte[] key, byte[] iv, byte[] authenticationData, Provider provider) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException {
+
+    	if (key == null || key.length < KeySizeInBytes) {
+    		throw new InvalidKeyException("key must be at least 128 bits in length");
+    	}
+    	
+        return new AesCbcEncryptor(AesCbc.Take(KeySizeInBytes, key), iv, provider);
+    }
+
+    @Override
+    public ICryptoTransform CreateDecryptor(byte[] key, byte[] iv, byte[] authenticationData) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException {
+
+    	if (key == null || key.length < KeySizeInBytes) {
+    		throw new InvalidKeyException("key must be at least 128 bits in length");
+    	}
+    	
+        return new AesCbcDecryptor(AesCbc.Take(KeySizeInBytes, key), iv, null);
+    }
+
+    @Override
+    public ICryptoTransform CreateDecryptor(byte[] key, byte[] iv, byte[] authenticationData, Provider provider) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException {
+
+    	if (key == null || key.length < KeySizeInBytes) {
+    		throw new InvalidKeyException("key must be at least 128 bits in length");
+    	}
+    	
+        return new AesCbcDecryptor(AesCbc.Take(KeySizeInBytes, key), iv, provider);
+    }
 }
--- azure-keyvault-cryptography/src/test/java/com/microsoft/azure/keyvault/cryptography/test/AesCbcTest.java
+++ azure-keyvault-cryptography/src/test/java/com/microsoft/azure/keyvault/cryptography/test/AesCbcTest.java
@@ -11,6 +11,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.microsoft.azure.keyvault.cryptography.Algorithm;
 import com.microsoft.azure.keyvault.cryptography.ICryptoTransform;
 import com.microsoft.azure.keyvault.cryptography.algorithms.Aes128Cbc;
 
@@ -40,44 +41,36 @@ public class AesCbcTest {
     }
 
     @Test
-    public void testAes128Cbc() {
-        // Arrange: These values are taken from Appendix B of the JWE
-        // specification at
-        // https://tools.ietf.org/html/draft-ietf-jose-json-web-encryption-40#appendix-B
-        // Since the values were intended for use with AES128-CBC-HMAC-SHA2 we
-        // actually take the realCEK from the second half of the CEK data below
-        // in order
-        // that the encrypted result will match the ED value from the example.
-        byte[] CEK = { 4, (byte) 211, 31, (byte) 197, 84, (byte) 157, (byte) 252, (byte) 254, 11, 100, (byte) 157, (byte) 250, 63, (byte) 170, 106, (byte) 206, 107, 124, (byte) 212, 45, 111, 107, 9, (byte) 219, (byte) 200, (byte) 177, 0, (byte) 240, (byte) 143, (byte) 156, 44, (byte) 207 };
-        byte[] PLAIN = { 76, 105, 118, 101, 32, 108, 111, 110, 103, 32, 97, 110, 100, 32, 112, 114, 111, 115, 112, 101, 114, 46 };
-        byte[] IV = { 3, 22, 60, 12, 43, 67, 104, 105, 108, 108, 105, 99, 111, 116, 104, 101 };
-        //byte[] AUTH = { 101, 121, 74, 104, 98, 71, 99, 105, 79, 105, 74, 66, 77, 84, 73, 52, 83, 49, 99, 105, 76, 67, 74, 108, 98, 109, 77, 105, 79, 105, 74, 66, 77, 84, 73, 52, 81, 48, 74, 68, 76, 85, 104, 84, 77, 106, 85, 50, 73, 110, 48 };
-        byte[] ED = { 40, 57, 83, (byte) 181, 119, 33, (byte) 133, (byte) 148, (byte) 198, (byte) 185, (byte) 243, 24, (byte) 152, (byte) 230, 6, 75, (byte) 129, (byte) 223, 127, 19, (byte) 210, 82, (byte) 183, (byte) 230, (byte) 168, 33, (byte) 215, 104, (byte) 143, 112, 56, 102 };
-        //byte[] TAG = { 83, 73, (byte) 191, 98, 104, (byte) 205, (byte) 211, (byte) 128, (byte) 201, (byte) 189, (byte) 199, (byte) 133, 32, 38, (byte) 194, 85 };
+    public void testAes128CbcOneBlock() {
+        // Note that AES128CBC as implemented in this library uses PKCS7 padding mode where the test
+        // vectors from RFC3602 do not use padding.
+        byte[] CEK   = { 0x06, (byte)0xa9, 0x21, 0x40, 0x36, (byte)0xb8, (byte)0xa1, 0x5b, 0x51, 0x2e, 0x03, (byte)0xd5, 0x34, 0x12, 0x00, 0x06 };
+        byte[] PLAIN = "Single block msg".getBytes();
+        byte[] IV    = { 0x3d, (byte)0xaf, (byte)0xba, 0x42, (byte)0x9d, (byte)0x9e, (byte)0xb4, 0x30, (byte)0xb4, 0x22, (byte)0xda, (byte)0x80, 0x2c, (byte)0x9f, (byte)0xac, 0x41 };
+        byte[] ED    = { (byte)0xe3, 0x53, 0x77, (byte)0x9c, 0x10, 0x79, (byte)0xae, (byte)0xb8, 0x27, 0x08, (byte)0x94, 0x2d, (byte)0xbe, 0x77, 0x18, 0x1a };
 
-        Aes128Cbc algo = new Aes128Cbc();
-        byte[] realCEK = new byte[128 >> 3];
-        byte[] encrypted = null;
-
-        // Take the second half of CEK as the AES key
-        System.arraycopy(CEK, 128 >> 3, realCEK, 0, 128 >> 3);
+        Aes128Cbc algo      = new Aes128Cbc();
+        byte[]    encrypted = null;
 
         ICryptoTransform encryptor = null;
         try {
-            encryptor = algo.CreateEncryptor(realCEK, IV, null, _provider);
+            encryptor = algo.CreateEncryptor(CEK, IV, null, _provider);
         } catch (Exception e) {
             fail(e.getMessage());
         }
 
         try {
             encrypted = encryptor.doFinal(PLAIN);
+            
+            // Assert: we only compare the first 16 bytes as this library uses PKCS7 padding
+            assertArrayEquals(Algorithm.Take(16,encrypted),ED);
         } catch (Exception e) {
             fail(e.getMessage());
         }
 
         ICryptoTransform decryptor = null;
         try {
-            decryptor = algo.CreateDecryptor(realCEK, IV, null, _provider);
+            decryptor = algo.CreateDecryptor(CEK, IV, null, _provider);
         } catch (Exception e) {
             fail(e.getMessage());
         }
@@ -86,12 +79,152 @@ public class AesCbcTest {
 
         try {
             decrypted = decryptor.doFinal(encrypted);
+            
+            // Assert: we only compare the first 16 bytes as this library uses PKCS7 padding
+            assertArrayEquals(Algorithm.Take(16, decrypted), PLAIN);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testAes128CbcTwoBlock() {
+        // Note that AES128CBC as implemented in this library uses PKCS7 padding mode where the test
+        // vectors do not use padding.
+        byte[] CEK   = { (byte)0xc2, (byte)0x86, 0x69, 0x6d, (byte)0x88, 0x7c, (byte)0x9a, (byte)0xa0, 0x61, 0x1b, (byte)0xbb, 0x3e, 0x20, 0x25, (byte)0xa4, 0x5a };
+        byte[] PLAIN = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f };
+        byte[] IV    = { 0x56, 0x2e, 0x17, (byte)0x99, 0x6d, 0x09, 0x3d, 0x28, (byte)0xdd, (byte)0xb3, (byte)0xba, 0x69, 0x5a, 0x2e, 0x6f, 0x58 };
+        byte[] ED    = { (byte)0xd2, (byte)0x96, (byte)0xcd, (byte)0x94, (byte)0xc2, (byte)0xcc, (byte)0xcf, (byte)0x8a, 0x3a, (byte)0x86, 0x30, 0x28, (byte)0xb5, (byte)0xe1, (byte)0xdc, 0x0a, 0x75, (byte)0x86, 0x60, 0x2d, 0x25, 0x3c, (byte)0xff, (byte)0xf9, 0x1b, (byte)0x82, 0x66, (byte)0xbe, (byte)0xa6, (byte)0xd6, 0x1a, (byte)0xb1 };
+
+        Aes128Cbc algo      = new Aes128Cbc();
+        byte[]    encrypted = null;
+
+        ICryptoTransform encryptor = null;
+        try {
+            encryptor = algo.CreateEncryptor(CEK, IV, null, _provider);
         } catch (Exception e) {
             fail(e.getMessage());
         }
 
-        // Assert
-        assertArrayEquals(PLAIN, decrypted);
-        assertArrayEquals(ED, encrypted);
+        try {
+            encrypted = encryptor.doFinal(PLAIN);
+            
+            // Assert: we only compare the first 32 bytes as this library uses PKCS7 padding
+            assertArrayEquals(Algorithm.Take(32,encrypted),ED);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+
+        ICryptoTransform decryptor = null;
+        try {
+            decryptor = algo.CreateDecryptor(CEK, IV, null, _provider);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+
+        byte[] decrypted = null;
+
+        try {
+            decrypted = decryptor.doFinal(encrypted);
+            
+            // Assert: we only compare the first 32 bytes as this library uses PKCS7 padding
+            assertArrayEquals(Algorithm.Take(32, decrypted), PLAIN);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testAes128CbcOneBlock_ExcessKeyMaterial() {
+        // Note that AES128CBC as implemented in this library uses PKCS7 padding mode where the test
+        // vectors from RFC3602 do not use padding.
+        byte[] CEK   = { 0x06, (byte)0xa9, 0x21, 0x40, 0x36, (byte)0xb8, (byte)0xa1, 0x5b, 0x51, 0x2e, 0x03, (byte)0xd5, 0x34, 0x12, 0x00, 0x06, (byte)0xc2, (byte)0x86, 0x69, 0x6d, (byte)0x88, 0x7c, (byte)0x9a, (byte)0xa0, 0x61, 0x1b, (byte)0xbb, 0x3e, 0x20, 0x25, (byte)0xa4, 0x5a };
+        byte[] PLAIN = "Single block msg".getBytes();
+        byte[] IV    = { 0x3d, (byte)0xaf, (byte)0xba, 0x42, (byte)0x9d, (byte)0x9e, (byte)0xb4, 0x30, (byte)0xb4, 0x22, (byte)0xda, (byte)0x80, 0x2c, (byte)0x9f, (byte)0xac, 0x41 };
+        byte[] ED    = { (byte)0xe3, 0x53, 0x77, (byte)0x9c, 0x10, 0x79, (byte)0xae, (byte)0xb8, 0x27, 0x08, (byte)0x94, 0x2d, (byte)0xbe, 0x77, 0x18, 0x1a };
+
+        Aes128Cbc algo      = new Aes128Cbc();
+        byte[]    encrypted = null;
+
+        ICryptoTransform encryptor = null;
+        try {
+            encryptor = algo.CreateEncryptor(CEK, IV, null, _provider);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+
+        try {
+            encrypted = encryptor.doFinal(PLAIN);
+            
+            // Assert: we only compare the first 16 bytes as this library uses PKCS7 padding
+            assertArrayEquals(Algorithm.Take(16,encrypted),ED);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+
+        ICryptoTransform decryptor = null;
+        try {
+            decryptor = algo.CreateDecryptor(CEK, IV, null, _provider);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+
+        byte[] decrypted = null;
+
+        try {
+            decrypted = decryptor.doFinal(encrypted);
+            
+            // Assert: we only compare the first 16 bytes as this library uses PKCS7 padding
+            assertArrayEquals(Algorithm.Take(16, decrypted), PLAIN);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testAes128CbcTwoBlock_ExcessKeyMaterial() {
+        // Note that AES128CBC as implemented in this library uses PKCS7 padding mode where the test
+        // vectors do not use padding.
+        byte[] CEK   = { (byte)0xc2, (byte)0x86, 0x69, 0x6d, (byte)0x88, 0x7c, (byte)0x9a, (byte)0xa0, 0x61, 0x1b, (byte)0xbb, 0x3e, 0x20, 0x25, (byte)0xa4, 0x5a, (byte)0xc2, (byte)0x86, 0x69, 0x6d, (byte)0x88, 0x7c, (byte)0x9a, (byte)0xa0, 0x61, 0x1b, (byte)0xbb, 0x3e, 0x20, 0x25, (byte)0xa4, 0x5a };
+        byte[] PLAIN = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f };
+        byte[] IV    = { 0x56, 0x2e, 0x17, (byte)0x99, 0x6d, 0x09, 0x3d, 0x28, (byte)0xdd, (byte)0xb3, (byte)0xba, 0x69, 0x5a, 0x2e, 0x6f, 0x58 };
+        byte[] ED    = { (byte)0xd2, (byte)0x96, (byte)0xcd, (byte)0x94, (byte)0xc2, (byte)0xcc, (byte)0xcf, (byte)0x8a, 0x3a, (byte)0x86, 0x30, 0x28, (byte)0xb5, (byte)0xe1, (byte)0xdc, 0x0a, 0x75, (byte)0x86, 0x60, 0x2d, 0x25, 0x3c, (byte)0xff, (byte)0xf9, 0x1b, (byte)0x82, 0x66, (byte)0xbe, (byte)0xa6, (byte)0xd6, 0x1a, (byte)0xb1 };
+
+        Aes128Cbc algo      = new Aes128Cbc();
+        byte[]    encrypted = null;
+
+        ICryptoTransform encryptor = null;
+        try {
+            encryptor = algo.CreateEncryptor(CEK, IV, null, _provider);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+
+        try {
+            encrypted = encryptor.doFinal(PLAIN);
+            
+            // Assert: we only compare the first 32 bytes as this library uses PKCS7 padding
+            assertArrayEquals(Algorithm.Take(32,encrypted),ED);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+
+        ICryptoTransform decryptor = null;
+        try {
+            decryptor = algo.CreateDecryptor(CEK, IV, null, _provider);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+
+        byte[] decrypted = null;
+
+        try {
+            decrypted = decryptor.doFinal(encrypted);
+            
+            // Assert: we only compare the first 32 bytes as this library uses PKCS7 padding
+            assertArrayEquals(Algorithm.Take(32, decrypted), PLAIN);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
     }
 }
