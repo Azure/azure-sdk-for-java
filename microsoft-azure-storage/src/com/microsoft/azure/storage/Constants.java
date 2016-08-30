14b625deef031dfdcae2de914a7df6fd28227032
Josh Friedman <jofriedm@microsoft.com>
2016-08-30 13:16:18 -0700

    Bumping Version to 4.4.0

--- microsoft-azure-storage/src/com/microsoft/azure/storage/Constants.java
+++ microsoft-azure-storage/src/com/microsoft/azure/storage/Constants.java
@@ -646,7 +646,7 @@ public final class Constants {
         /**
          * Specifies the value to use for UserAgent header.
          */
-        public static final String USER_AGENT_VERSION = "4.3.0";
+        public static final String USER_AGENT_VERSION = "4.4.0";
 
         /**
          * The default type for content-type and accept
