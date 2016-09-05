cc0b48f3d451a51f91cc669708b61d0b4b0a871b
anuchan <anuamd@gmail.com>
2016-09-04 22:46:53 -0700

    unit test for vmaccess extension

--- azure-mgmt-compute/src/test/java/com/microsoft/azure/management/compute/VirtualMachineExtensionOperationsTests.java
+++ azure-mgmt-compute/src/test/java/com/microsoft/azure/management/compute/VirtualMachineExtensionOperationsTests.java
@@ -9,10 +9,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class VirtualMachineExtensionOperationsTests extends ComputeManagementTestBase {
-    private static final String RG_NAME = ResourceNamer.randomResourceName("vmexttest", 15);
-    private static final String LOCATION = "eastus";
-    private static final String VMNAME = "javavm";
-
     @BeforeClass
     public static void setup() throws Exception {
         createClients();
@@ -23,13 +19,56 @@ public class VirtualMachineExtensionOperationsTests extends ComputeManagementTes
     }
 
     @Test
-    public void canInstallUpdateUninstallExtension() throws Exception {
+    public void canResetPasswordUsingVMAccessExtension() throws Exception {
+        final String RG_NAME = ResourceNamer.randomResourceName("vmexttest", 15);
+        final String LOCATION = "eastus";
+        final String VMNAME = "javavm";
+
+        // Create a Linux VM
+        //
+        VirtualMachine vm = computeManager.virtualMachines()
+                .define(VMNAME)
+                .withRegion(LOCATION)
+                .withNewResourceGroup(RG_NAME)
+                .withNewPrimaryNetwork("10.0.0.0/28")
+                .withPrimaryPrivateIpAddressDynamic()
+                .withoutPrimaryPublicIpAddress()
+                .withPopularLinuxImage(KnownLinuxVirtualMachineImage.UBUNTU_SERVER_14_04_LTS)
+                .withRootUserName("Foo12")
+                .withPassword("BaR@12abc!")
+                .withSize(VirtualMachineSizeTypes.STANDARD_D3)
+                .create();
+
+        // Using VMAccess Linux extension to reset the password for the existing user
+        // https://github.com/Azure/azure-linux-extensions/blob/master/VMAccess/README.md
+        //
+        vm.update()
+                .defineNewExtension("VMAccessForLinux")
+                    .withPublisher("Microsoft.OSTCExtensions")
+                    .withType("VMAccessForLinux")
+                    .withVersion("1.4")
+                    .withProtectedSetting("username", "Foo12")
+                    .withProtectedSetting("password", "B12a6@12xyz!")
+                    .withProtectedSetting("reset_ssh", "true")
+                .attach()
+                .apply();
+
+        Assert.assertTrue(vm.extensions().size() > 0);
+        Assert.assertTrue(vm.extensions().containsKey("VMAccessForLinux"));
+    }
+
+    @Test
+    public void canInstallUninstallCustomExtension() throws Exception {
+        final String RG_NAME = ResourceNamer.randomResourceName("vmexttest", 15);
+        final String LOCATION = "eastus";
+        final String VMNAME = "javavm";
+
         final String mySqlInstallScript = "https://raw.githubusercontent.com/Azure/azure-quickstart-templates/4397e808d07df60ff3cdfd1ae40999f0130eb1b3/mysql-standalone-server-ubuntu/scripts/install_mysql_server_5.6.sh";
         final String installCommand = "bash install_mysql_server_5.6.sh Abc.123x(";
         List<String> fileUris = new ArrayList<>();
         fileUris.add(mySqlInstallScript);
 
-        // Create VM and add extension
+        // Create Linux VM with a custom extension to install MySQL
         //
         VirtualMachine vm = computeManager.virtualMachines()
                 .define(VMNAME)
@@ -59,7 +98,7 @@ public class VirtualMachineExtensionOperationsTests extends ComputeManagementTes
         Assert.assertEquals(customScriptExtension.typeName(), "CustomScriptForLinux");
         Assert.assertEquals(customScriptExtension.autoUpgradeMinorVersionEnabled(), true);
 
-        // Remove the extension
+        // Remove the custom extension
         //
         vm.update()
                 .withoutExtension("CustomScriptForLinux")
