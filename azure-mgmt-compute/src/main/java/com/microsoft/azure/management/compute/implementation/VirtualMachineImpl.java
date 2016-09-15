94b238c5720988eeefd6b8a2c42d40fe3e379cff
anuchan <anuamd@gmail.com>
2016-09-15 07:37:28 -0700

    Fix checkstyle error

--- azure-mgmt-compute/src/main/java/com/microsoft/azure/management/compute/implementation/VirtualMachineImpl.java
+++ azure-mgmt-compute/src/main/java/com/microsoft/azure/management/compute/implementation/VirtualMachineImpl.java
@@ -55,7 +55,6 @@ import rx.functions.Func1;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
