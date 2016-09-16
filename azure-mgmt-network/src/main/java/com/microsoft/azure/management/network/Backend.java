03bc0b3ea6d4b2b47bdaf8ce5ee16e234168b6ad
unknown <marcins@MARCINS-BRICK.redmond.corp.microsoft.com>
2016-09-16 07:01:51 -0700

    language annotations on Backend interfaces

--- azure-mgmt-network/src/main/java/com/microsoft/azure/management/network/Backend.java
+++ azure-mgmt-network/src/main/java/com/microsoft/azure/management/network/Backend.java
@@ -8,6 +8,8 @@ package com.microsoft.azure.management.network;
 import java.util.Map;
 import java.util.Set;
 
+import com.microsoft.azure.management.apigeneration.LangDefinition;
+import com.microsoft.azure.management.apigeneration.LangDefinition.MethodConversion;
 import com.microsoft.azure.management.network.implementation.BackendAddressPoolInner;
 import com.microsoft.azure.management.network.model.HasLoadBalancingRules;
 import com.microsoft.azure.management.resources.fluentcore.arm.models.ChildResource;
@@ -18,6 +20,7 @@ import com.microsoft.azure.management.resources.fluentcore.model.Wrapper;
 /**
  * An immutable client-side representation of an load balancer's backend address pool.
  */
+@LangDefinition()
 public interface Backend extends
     Wrapper<BackendAddressPoolInner>,
     ChildResource<LoadBalancer>,
@@ -37,6 +40,7 @@ public interface Backend extends
     /**
      * Grouping of backend definition stages.
      */
+    @LangDefinition(ContainerName = "Definition", ContainerFileName = "IDefinition", IsContainerOnly = true, MethodConversionType = MethodConversion.OnlyMethod)
     interface DefinitionStages {
         /**
          * The first stage of the backend definition.
@@ -59,6 +63,7 @@ public interface Backend extends
     /** The entirety of a backend definition.
      * @param <ParentT> the return type of the final {@link DefinitionStages.WithAttach#attach()}
      */
+    @LangDefinition(ContainerName = "Definition", ContainerFileName = "IDefinition")
     interface Definition<ParentT> extends
         DefinitionStages.Blank<ParentT>,
         DefinitionStages.WithAttach<ParentT> {
@@ -67,12 +72,14 @@ public interface Backend extends
     /**
      * Grouping of backend update stages.
      */
+    @LangDefinition(ContainerName = "Update", ContainerFileName = "IUpdate", IsContainerOnly = true)
     interface UpdateStages {
     }
 
     /**
      * The entirety of a backend update as part of a load balancer update.
      */
+    @LangDefinition(ContainerName = "Update", ContainerFileName = "IUpdate", MethodConversionType = MethodConversion.OnlyMethod)
     interface Update extends
         Settable<LoadBalancer.Update> {
     }
@@ -80,6 +87,7 @@ public interface Backend extends
     /**
      * Grouping of backend definition stages applicable as part of a load balancer update.
      */
+    @LangDefinition(ContainerName = "UpdateDefinition", ContainerFileName = "IUpdateDefinition", IsContainerOnly = true, MethodConversionType = MethodConversion.OnlyMethod)
     interface UpdateDefinitionStages {
         /**
          * The first stage of the backend definition.
@@ -102,6 +110,7 @@ public interface Backend extends
     /** The entirety of a probe definition as part of a load balancer update.
      * @param <ParentT> the return type of the final {@link UpdateDefinitionStages.WithAttach#attach()}
      */
+    @LangDefinition(ContainerName = "UpdateDefinition", ContainerFileName = "IUpdateDefinition")
     interface UpdateDefinition<ParentT> extends
         UpdateDefinitionStages.Blank<ParentT>,
         UpdateDefinitionStages.WithAttach<ParentT> {
