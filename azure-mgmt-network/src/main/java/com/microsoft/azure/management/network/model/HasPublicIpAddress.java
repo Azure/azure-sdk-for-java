bb59287f7b02c3f4aa15a5654d83fb49ab300a63
unknown <marcins@MARCINS-BRICK.redmond.corp.microsoft.com>
2016-09-19 17:36:25 -0700

    factoring out public IP related methods into a standalone interface for better consistency and reuse

---
+++ azure-mgmt-network/src/main/java/com/microsoft/azure/management/network/model/HasPublicIpAddress.java
@@ -0,0 +1,148 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+package com.microsoft.azure.management.network.model;
+
+import com.microsoft.azure.management.apigeneration.LangDefinition;
+import com.microsoft.azure.management.apigeneration.LangDefinition.MethodConversion;
+import com.microsoft.azure.management.network.PublicIpAddress;
+import com.microsoft.azure.management.resources.fluentcore.model.Creatable;
+
+/**
+ * An interface representing a model's ability to reference a public IP address.
+ */
+@LangDefinition()
+public interface HasPublicIpAddress  {
+    /**
+     * @return the resource ID of the associated public IP address
+     */
+    String publicIpAddressId();
+
+    /**
+     * @return the associated public IP address 
+     */
+    PublicIpAddress getPublicIpAddress();
+
+    /**
+     * Grouping of definition stages involving specifying the public IP address.
+     */
+    @LangDefinition(
+                ContainerName = "Definition",
+                ContainerFileName = "IDefinition",
+                IsContainerOnly = true,
+                MethodConversionType = MethodConversion.OnlyMethod)
+    interface DefinitionStages {
+        /**
+         * The stage of the network interface IP configuration definition allowing to associate it with
+         * a public IP address.
+         *
+         * @param <ReturnT> the next stage of the definition
+         */
+        interface WithPublicIpAddress<ReturnT> {
+            /**
+             * Create a new public IP address to associate with the network interface IP configuration,
+             * based on the provided definition.
+             *
+             * @param creatable a creatable definition for a new public IP
+             * @return the next stage of the network interface IP configuration definition
+             */
+            ReturnT withNewPublicIpAddress(Creatable<PublicIpAddress> creatable);
+
+            /**
+             * Creates a new public IP address in the same region and group as the resource and associates it with the resource.
+             * <p>
+             * The internal name and DNS label for the public IP address will be derived from the resource's name.
+             *
+             * @return the next stage of the definition
+             */
+            ReturnT withNewPublicIpAddress();
+
+            /**
+             * Creates a new public IP address in the same region and group as the resource, with the specified DNS label
+             * and associates it with the resource.
+             * <p>
+             * The internal name for the public IP address will be derived from the DNS label.
+             *
+             * @param leafDnsLabel the leaf domain label
+             * @return the next stage of the definition
+             */
+            ReturnT withNewPublicIpAddress(String leafDnsLabel);
+
+            /**
+             * Associates an existing public IP address with the resource.
+             *
+             * @param publicIpAddress an existing public IP address
+             * @return the next stage of the definition
+             */
+            ReturnT withExistingPublicIpAddress(PublicIpAddress publicIpAddress);
+        }
+    }
+
+    /**
+     * Grouping of update stages involving modifying an existing reference to a public IP address.
+     */
+    @LangDefinition(
+                ContainerName = "Update",
+                ContainerFileName = "IUpdate",
+                IsContainerOnly = true,
+                MethodConversionType = MethodConversion.OnlyMethod)
+    interface UpdateStages {
+    }
+
+    /**
+     * Grouping of definition stages applicable as part of a parent resource update, involving specifying a public IP address.
+     */
+    @LangDefinition(
+                ContainerName = "UpdateDefinition",
+                ContainerFileName = "IUpdateDefinition",
+                IsContainerOnly = true,
+                MethodConversionType = MethodConversion.OnlyMethod)
+    interface UpdateDefinitionStages {
+        /**
+         * The stage of the network interface IP configuration definition allowing to associate it with
+         * a public IP address.
+         *
+         * @param <ReturnT> the next stage of the definition
+         */
+        interface WithPublicIpAddress<ReturnT> {
+            /**
+             * Create a new public IP address to associate with the network interface IP configuration,
+             * based on the provided definition.
+             *
+             * @param creatable a creatable definition for a new public IP
+             * @return the next stage of the network interface IP configuration definition
+             */
+            ReturnT withNewPublicIpAddress(Creatable<PublicIpAddress> creatable);
+
+            /**
+             * Creates a new public IP address in the same region and group as the resource and associates it with the resource.
+             * <p>
+             * The internal name and DNS label for the public IP address will be derived from the resource's name.
+             *
+             * @return the next stage of the definition
+             */
+            ReturnT withNewPublicIpAddress();
+
+            /**
+             * Creates a new public IP address in the same region and group as the resource, with the specified DNS label
+             * and associates it with the resource.
+             * <p>
+             * The internal name for the public IP address will be derived from the DNS label.
+             *
+             * @param leafDnsLabel the leaf domain label
+             * @return the next stage of the definition
+             */
+            ReturnT withNewPublicIpAddress(String leafDnsLabel);
+
+            /**
+             * Associates an existing public IP address with the resource.
+             *
+             * @param publicIpAddress an existing public IP address
+             * @return the next stage of the definition
+             */
+            ReturnT withExistingPublicIpAddress(PublicIpAddress publicIpAddress);
+        }
+    }
+}
