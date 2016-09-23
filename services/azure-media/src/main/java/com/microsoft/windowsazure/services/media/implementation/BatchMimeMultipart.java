995aa7bff84dbb2e793201f9c859feacb594ac17
Emanuel Vecchio <vecchioemanuel@gmail.com>
2016-09-23 14:45:46 -0300

    # Fixed tab chars

--- services/azure-media/src/main/java/com/microsoft/windowsazure/services/media/implementation/BatchMimeMultipart.java
+++ services/azure-media/src/main/java/com/microsoft/windowsazure/services/media/implementation/BatchMimeMultipart.java
@@ -9,32 +9,32 @@ import javax.mail.MessagingException;
 import javax.mail.internet.MimeMultipart;
 
 public class BatchMimeMultipart extends MimeMultipart {
-	
-	public BatchMimeMultipart(SetBoundaryMultipartDataSource setBoundaryMultipartDataSource) throws MessagingException {
-		super(setBoundaryMultipartDataSource);
-	}
 
-	@Override
-	public synchronized void writeTo(OutputStream os)
-			throws IOException, MessagingException {
-		
-		resetInputStreams();
-		super.writeTo(os);
-	}
-	
-	// reset all input streams to allow redirect filter to write the output twice
-	private void resetInputStreams() throws IOException, MessagingException {
-		for (int ix = 0; ix < this.getCount(); ix++) {
-			BodyPart part = this.getBodyPart(ix);
-			if (part.getContent() instanceof MimeMultipart) {
-				MimeMultipart subContent = (MimeMultipart) part.getContent();
-				for (int jx = 0; jx < subContent.getCount(); jx++) {
-					BodyPart subPart = subContent.getBodyPart(jx);
-					if (subPart.getContent() instanceof ByteArrayInputStream) {
-						((ByteArrayInputStream) subPart.getContent()).reset();
-					}
-				}
-			}
-		}
-	}
+    public BatchMimeMultipart(SetBoundaryMultipartDataSource setBoundaryMultipartDataSource) throws MessagingException {
+        super(setBoundaryMultipartDataSource);
+    }
+
+    @Override
+    public synchronized void writeTo(OutputStream os)
+            throws IOException, MessagingException {
+
+        resetInputStreams();
+        super.writeTo(os);
+    }
+
+    // reset all input streams to allow redirect filter to write the output twice
+    private void resetInputStreams() throws IOException, MessagingException {
+        for (int ix = 0; ix < this.getCount(); ix++) {
+            BodyPart part = this.getBodyPart(ix);
+            if (part.getContent() instanceof MimeMultipart) {
+                MimeMultipart subContent = (MimeMultipart) part.getContent();
+                for (int jx = 0; jx < subContent.getCount(); jx++) {
+                    BodyPart subPart = subContent.getBodyPart(jx);
+                    if (subPart.getContent() instanceof ByteArrayInputStream) {
+                        ((ByteArrayInputStream) subPart.getContent()).reset();
+                    }
+                }
+            }
+        }
+    }
 }
--- services/azure-media/src/main/java/com/microsoft/windowsazure/services/media/implementation/BatchMimeMultipartBodyWritter.java
+++ services/azure-media/src/main/java/com/microsoft/windowsazure/services/media/implementation/BatchMimeMultipartBodyWritter.java
@@ -15,24 +15,23 @@ import javax.ws.rs.ext.MessageBodyWriter;
 @Produces("multipart/mixed")
 public class BatchMimeMultipartBodyWritter implements MessageBodyWriter<BatchMimeMultipart> {
 
-	@Override
-	public long getSize(BatchMimeMultipart bmm, Class<?> arg1, Type arg2, Annotation[] arg3, MediaType arg4) {
-		return -1;
-	}
+    @Override
+    public long getSize(BatchMimeMultipart bmm, Class<?> arg1, Type arg2, Annotation[] arg3, MediaType arg4) {
+        return -1;
+    }
 
-	@Override
-	public boolean isWriteable(Class<?> type, Type arg1, Annotation[] arg2, MediaType arg3) {
-		return type == BatchMimeMultipart.class;
-	}
+    @Override
+    public boolean isWriteable(Class<?> type, Type arg1, Annotation[] arg2, MediaType arg3) {
+        return type == BatchMimeMultipart.class;
+    }
 
-	@Override
-	public void writeTo(BatchMimeMultipart t, Class<?> arg1, Type arg2, Annotation[] arg3, MediaType arg4,
-			MultivaluedMap<String, Object> arg5, OutputStream entityStream) throws IOException, WebApplicationException {
-		try {
+    @Override
+    public void writeTo(BatchMimeMultipart t, Class<?> arg1, Type arg2, Annotation[] arg3, MediaType arg4,
+            MultivaluedMap<String, Object> arg5, OutputStream entityStream) throws IOException, WebApplicationException {
+        try {
             t.writeTo(entityStream);
         } catch (MessagingException ex) {
             throw new WebApplicationException(ex);
         }	
-	}
-
+    }
 }
