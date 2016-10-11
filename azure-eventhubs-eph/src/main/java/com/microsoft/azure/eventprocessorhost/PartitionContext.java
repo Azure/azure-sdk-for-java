d7a47441080979540deee50a787ddbb7e64b7917
JamesBirdsall <jbird@microsoft.com>
2016-10-11 15:05:42 -0700

    Deprecate PartitionContext.setOffsetAndSequenceNumber for public use.

--- azure-eventhubs-eph/src/main/java/com/microsoft/azure/eventprocessorhost/PartitionContext.java
+++ azure-eventhubs-eph/src/main/java/com/microsoft/azure/eventprocessorhost/PartitionContext.java
@@ -62,33 +62,13 @@ public class PartitionContext
         this.lease = lease;
     }
 
-    /**
-     * Updates the offset/sequenceNumber in the PartitionContext with the values in the received EventData object.
-     *  
-     * Since offset is a string it cannot be compared easily, but sequenceNumber is checked. The new sequenceNumber must be
-     * at least the same as the current value or the entire assignment is aborted. It is assumed that if the new sequenceNumber
-     * is equal or greater, the new offset will be as well.
-     * 
-     * @param event  A received EventData with valid offset and sequenceNumber
-     * @throws IllegalArgumentException  If the sequenceNumber in the provided event is less than the current value
-     */
+    @Deprecated
     public void setOffsetAndSequenceNumber(EventData event) throws IllegalArgumentException
     {
     	setOffsetAndSequenceNumber(event.getSystemProperties().getOffset(), event.getSystemProperties().getSequenceNumber());
     }
     
-    /**
-     * Updates the offset/sequenceNumber in the PartitionContext.
-     * 
-     * These two values are closely tied and must be updated in an atomic fashion, hence the combined setter.
-     * Since offset is a string it cannot be compared easily, but sequenceNumber is checked. The new sequenceNumber must be
-     * at least the same as the current value or the entire assignment is aborted. It is assumed that if the new sequenceNumber
-     * is equal or greater, the new offset will be as well.
-     * 
-     * @param offset  New offset value
-     * @param sequenceNumber  New sequenceNumber value 
-     * @throws IllegalArgumentException  If the new sequenceNumber is less than the current value
-     */
+    @Deprecated
     public void setOffsetAndSequenceNumber(String offset, long sequenceNumber) throws IllegalArgumentException
     {
     	synchronized (this.offsetSynchronizer)
