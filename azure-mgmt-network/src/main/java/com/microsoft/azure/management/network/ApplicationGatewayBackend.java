1a22cedc23d689538259686f7106fb5ee169183c
unknown <marcins@MARCINS-BRICK.redmond.corp.microsoft.com>
2016-10-26 00:50:38 -0700

    initial skeletal application gateway backend interface

---
+++ azure-mgmt-network/src/main/java/com/microsoft/azure/management/network/ApplicationGatewayBackend.java
@@ -0,0 +1,96 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+package com.microsoft.azure.management.network;
+
+import com.microsoft.azure.management.apigeneration.Fluent;
+import com.microsoft.azure.management.network.implementation.ApplicationGatewayBackendAddressPoolInner;
+import com.microsoft.azure.management.resources.fluentcore.arm.models.ChildResource;
+import com.microsoft.azure.management.resources.fluentcore.model.Attachable;
+import com.microsoft.azure.management.resources.fluentcore.model.Settable;
+import com.microsoft.azure.management.resources.fluentcore.model.Wrapper;
+
+/**
+ * An immutable client-side representation of an application gateway backend.
+ */
+@Fluent()
+public interface ApplicationGatewayBackend extends
+    Wrapper<ApplicationGatewayBackendAddressPoolInner>,
+    ChildResource<ApplicationGateway> {
+
+
+    /**
+     * Grouping of appplication gateway backend definition stages.
+     */
+    interface DefinitionStages {
+        /**
+         * The first stage of an application gateway backend definition.
+         * @param <ParentT> the return type of the final {@link WithAttach#attach()}
+         */
+        interface Blank<ParentT> extends WithAttach<ParentT> {
+        }
+
+        /** The final stage of an application gateway backend definition.
+         * <p>
+         * At this stage, any remaining optional settings can be specified, or the definition
+         * can be attached to the parent application gateway definition using {@link WithAttach#attach()}.
+         * @param <ParentT> the return type of {@link WithAttach#attach()}
+         */
+        interface WithAttach<ParentT> extends
+            Attachable.InDefinition<ParentT> {
+        }
+    }
+
+    /** The entirety of an application gateway backend definition.
+     * @param <ParentT> the return type of the final {@link DefinitionStages.WithAttach#attach()}
+     */
+    interface Definition<ParentT> extends
+        DefinitionStages.Blank<ParentT>,
+        DefinitionStages.WithAttach<ParentT> {
+    }
+
+    /**
+     * Grouping of application gateway backend update stages.
+     */
+    interface UpdateStages {
+    }
+
+    /**
+     * The entirety of an application gateway backend update as part of an application gateway update.
+     */
+    interface Update extends
+        Settable<ApplicationGateway.Update> {
+    }
+
+    /**
+     * Grouping of application gateway backend definition stages applicable as part of an application gateway update.
+     */
+    interface UpdateDefinitionStages {
+        /**
+         * The first stage of an application gateway backend definition.
+         * @param <ParentT> the return type of the final {@link WithAttach#attach()}
+         */
+        interface Blank<ParentT> extends WithAttach<ParentT> {
+        }
+
+        /** The final stage of an application gateway backend definition.
+         * <p>
+         * At this stage, any remaining optional settings can be specified, or the definition
+         * can be attached to the parent application gateway definition using {@link WithAttach#attach()}.
+         * @param <ParentT> the return type of {@link WithAttach#attach()}
+         */
+        interface WithAttach<ParentT> extends
+            Attachable.InUpdate<ParentT> {
+        }
+    }
+
+    /** The entirety of an application gateway backend definition as part of an application gateway update.
+     * @param <ParentT> the return type of the final {@link UpdateDefinitionStages.WithAttach#attach()}
+     */
+    interface UpdateDefinition<ParentT> extends
+        UpdateDefinitionStages.Blank<ParentT>,
+        UpdateDefinitionStages.WithAttach<ParentT> {
+    }
+}
