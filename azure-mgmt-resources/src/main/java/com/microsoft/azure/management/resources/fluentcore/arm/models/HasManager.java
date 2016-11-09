40568829fcb24b9484d879eb0e3601b8a375e1c4
unknown <marcins@MARCINS-BRICK.redmond.corp.microsoft.com>
2016-11-08 16:32:30 -0800

    enabling groupable resource collections to expose their manager client

---
+++ azure-mgmt-resources/src/main/java/com/microsoft/azure/management/resources/fluentcore/arm/models/HasManager.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+package com.microsoft.azure.management.resources.fluentcore.arm.models;
+
+import com.microsoft.azure.management.apigeneration.Fluent;
+
+/**
+ * An interface representing a model that exposes a management client.
+ */
+@Fluent
+public interface HasManager<ManagerT> {
+    /**
+     * @return the manager client of this resource type
+     */
+    ManagerT manager();
+}
