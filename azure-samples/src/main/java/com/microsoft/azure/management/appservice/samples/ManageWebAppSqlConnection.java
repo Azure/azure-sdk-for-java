adec0101f23eb3e609a34e0f29f11e8aa75ecea4
Jianghao Lu <jianghaolu@users.noreply.github.com>
2016-12-19 14:45:47 -0800

    Update comments for new sample

--- azure-samples/src/main/java/com/microsoft/azure/management/appservice/samples/ManageWebAppSqlConnection.java
+++ azure-samples/src/main/java/com/microsoft/azure/management/appservice/samples/ManageWebAppSqlConnection.java
@@ -26,9 +26,10 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * Azure App Service basic sample for managing web apps.
- *  - Create a storage account and upload a couple blobs
- *  - Create a web app that contains the connection string to the storage account
- *  - Deploy a Tomcat application that reads from the storage account
+ *  - Create a SQL database in a new SQL server
+ *  - Create a web app deployed with Project Nami (WordPress's SQL Server variant)
+ *      that contains the app settings to connect to the SQL database
+ *  - Update the SQL server's firewall rules to allow the web app to access
  *  - Clean up
  */
 public final class ManageWebAppSqlConnection {
