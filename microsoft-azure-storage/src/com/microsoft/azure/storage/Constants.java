bfeb5056b8e7a8b1ea5dba916816761f32cf4777
Josh Friedman <jofriedm@microsoft.com>
2017-01-18 13:35:10 -0800

    Update version for release

--- microsoft-azure-storage/src/com/microsoft/azure/storage/Constants.java
+++ microsoft-azure-storage/src/com/microsoft/azure/storage/Constants.java
@@ -656,7 +656,7 @@ public final class Constants {
         /**
          * Specifies the value to use for UserAgent header.
          */
-        public static final String USER_AGENT_VERSION = "4.4.0";
+        public static final String USER_AGENT_VERSION = "5.0.0";
 
         /**
          * The default type for content-type and accept
