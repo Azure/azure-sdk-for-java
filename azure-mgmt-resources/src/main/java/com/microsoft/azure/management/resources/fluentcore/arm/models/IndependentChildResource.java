c8a64993f6b41f14e2243323b7c6215d3f7abd9d
unknown <marcins@MARCINS-BRICK.redmond.corp.microsoft.com>
2017-01-23 16:49:42 -0800

    more javadoc checkstyle fun

--- azure-mgmt-resources/src/main/java/com/microsoft/azure/management/resources/fluentcore/arm/models/IndependentChildResource.java
+++ azure-mgmt-resources/src/main/java/com/microsoft/azure/management/resources/fluentcore/arm/models/IndependentChildResource.java
@@ -10,6 +10,8 @@ import com.microsoft.azure.management.apigeneration.Fluent;
 
 /**
  * Interface for the child resource which can be CRUDed independently from the parent resource.
+ *
+ * @param <ManagerT> the client manager type representing the service
  */
 @Fluent
 public interface IndependentChildResource<ManagerT>
