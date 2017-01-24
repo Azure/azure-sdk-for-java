0c1f7549d140de050229a777d38101d38627e0a4
anuchan <anuamd@gmail.com>
2017-01-23 18:39:58 -0800

    Adding managed disk samples (set1)

---
+++ azure-samples/src/main/java/com/microsoft/azure/management/compute/samples/ManageVirtualMachineWithDisk.java
@@ -0,0 +1,25 @@
+/**
+ *
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ *
+ */
+
+package com.microsoft.azure.management.compute.samples;
+
+/**
+ * Azure Compute sample for managing virtual machines -
+ *  - Create a virtual machine
+ *      - two implicit data disks
+ *      - one creatable data disk
+ *      - one existing data disks
+ *  - Update a virtual machine
+ *      - Attach data disks
+ *      - Detach data disks
+ *  - Stop a virtual machine
+ *  - Update a virtual machine
+ *      - Expand the OS disk
+ *      - Expand data disks
+ */
+public class ManageVirtualMachineWithDisk {
+}
