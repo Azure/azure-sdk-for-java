69dac6ea1e818d3a6df9c232f9d1b57d7f0dceec
anuchan <anuamd@gmail.com>
2017-02-06 13:57:29 -0800

    Sample: Deploy VM (with managed disk) using template

--- azure-samples/src/test/java/com/microsoft/azure/management/samples/ResourceSampleTests.java
+++ azure-samples/src/test/java/com/microsoft/azure/management/samples/ResourceSampleTests.java
@@ -10,6 +10,7 @@ package com.microsoft.azure.management.samples;
 import com.microsoft.azure.management.resources.samples.DeployUsingARMTemplate;
 import com.microsoft.azure.management.resources.samples.DeployUsingARMTemplateWithProgress;
 import com.microsoft.azure.management.resources.samples.DeployUsingARMTemplateWithTags;
+import com.microsoft.azure.management.resources.samples.DeployVirtualMachineUsingARMTemplate;
 import com.microsoft.azure.management.resources.samples.ManageResource;
 import com.microsoft.azure.management.resources.samples.ManageResourceGroup;
 import org.junit.Assert;
@@ -39,5 +40,10 @@ public class ResourceSampleTests extends SamplesTestBase {
     @Test
     public void testManageResourceGroup() {
         Assert.assertTrue(ManageResourceGroup.runSample(azure));
+    }
+
+    @Test
+    public void testDeployVirtualMachineUsingARMTemplate() {
+        Assert.assertTrue(DeployVirtualMachineUsingARMTemplate.runSample(azure));
     }
 }
