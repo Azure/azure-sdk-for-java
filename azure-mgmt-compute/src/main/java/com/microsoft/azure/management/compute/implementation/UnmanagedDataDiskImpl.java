a0000367ab0e658fb596a39f93b6c2b54029345a
unknown <marcins@MARCINS-BRICK.redmond.corp.microsoft.com>
2017-02-07 11:44:20 -0800

    more missing copyright notices in sources

--- azure-mgmt-compute/src/main/java/com/microsoft/azure/management/compute/implementation/UnmanagedDataDiskImpl.java
+++ azure-mgmt-compute/src/main/java/com/microsoft/azure/management/compute/implementation/UnmanagedDataDiskImpl.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
 package com.microsoft.azure.management.compute.implementation;
 
 import com.microsoft.azure.management.compute.DataDisk;
--- azure-mgmt-resources/src/main/java/com/microsoft/azure/management/resources/fluentcore/model/CreatedResources.java
+++ azure-mgmt-resources/src/main/java/com/microsoft/azure/management/resources/fluentcore/model/CreatedResources.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
 package com.microsoft.azure.management.resources.fluentcore.model;
 
 import java.util.Map;
