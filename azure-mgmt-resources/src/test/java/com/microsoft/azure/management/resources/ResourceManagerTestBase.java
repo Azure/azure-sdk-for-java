1d6968d94cadf3384d34780e795c1e2f4d6549d2
unknown <marcins@MARCINS-BRICK.redmond.corp.microsoft.com>
2017-02-07 11:23:49 -0800

    missing copyright notices in sources

--- azure-mgmt-resources/src/test/java/com/microsoft/azure/management/resources/ResourceManagerTestBase.java
+++ azure-mgmt-resources/src/test/java/com/microsoft/azure/management/resources/ResourceManagerTestBase.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
 package com.microsoft.azure.management.resources;
 
 import com.microsoft.azure.management.resources.core.TestBase;
