bb23f04039565bc27c93fed0a198a893fb832cde
anuchan <anuamd@gmail.com>
2017-02-08 16:30:09 -0800

    Fixes checkstyle issue

--- azure-mgmt-compute/src/main/java/com/microsoft/azure/management/compute/implementation/ManagedUnmanagedDiskErrors.java
+++ azure-mgmt-compute/src/main/java/com/microsoft/azure/management/compute/implementation/ManagedUnmanagedDiskErrors.java
@@ -13,12 +13,12 @@ import com.microsoft.azure.management.apigeneration.LangDefinition;
  */
 @LangDefinition
 class ManagedUnmanagedDiskErrors {
-    static String VM_BOTH_MANAGED_AND_UNMANAGED_DISK_NOT_ALLOWED = "This virtual machine is based on managed disk(s), both un-managed and managed disk cannot exists together in a virtual machine";
-    static String VMSS_BOTH_MANAGED_AND_UNMANAGED_DISK_NOT_ALLOWED = "This virtual machine scale set is based on managed disk(s), both un-managed and managed cannot exists together in a virtual machine scale set";
-    static String VM_NO_UNMANAGED_DISK_TO_UPDATE = "This virtual machine is based on managed disk(s) and there is no un-managed disk to update";
-    static String VM_NO_MANAGED_DISK_TO_UPDATE = "This virtual machine is based on un-managed disk(s) and there is no managed disk to update";
-    static String VMSS_NO_UNMANAGED_DISK_TO_UPDATE = "This virtual machine scale set is based on managed disk(s) and there is no un-managed disk to update";
-    static String VMSS_NO_MANAGED_DISK_TO_UPDATE = "This virtual machine scale set is based on un-managed disk(s) and there is no managed disk to update";
-    static String VM_BOTH_UNMANAGED_AND_MANAGED_DISK_NOT_ALLOWED = "This virtual machine is based on un-managed disks (s), both un-managed and managed disk cannot exists together in a virtual machine";
-    static String VMSS_BOTH_UNMANAGED_AND_MANAGED_DISK_NOT_ALLOWED = "This virtual machine scale set is based on un-managed disk(s), both un-managed and managed cannot exists together in a virtual machine scale set";
+    static final String VM_BOTH_MANAGED_AND_UNMANAGED_DISK_NOT_ALLOWED = "This virtual machine is based on managed disk(s), both un-managed and managed disk cannot exists together in a virtual machine";
+    static final String VMSS_BOTH_MANAGED_AND_UNMANAGED_DISK_NOT_ALLOWED = "This virtual machine scale set is based on managed disk(s), both un-managed and managed cannot exists together in a virtual machine scale set";
+    static final String VM_NO_UNMANAGED_DISK_TO_UPDATE = "This virtual machine is based on managed disk(s) and there is no un-managed disk to update";
+    static final String VM_NO_MANAGED_DISK_TO_UPDATE = "This virtual machine is based on un-managed disk(s) and there is no managed disk to update";
+    static final String VMSS_NO_UNMANAGED_DISK_TO_UPDATE = "This virtual machine scale set is based on managed disk(s) and there is no un-managed disk to update";
+    static final String VMSS_NO_MANAGED_DISK_TO_UPDATE = "This virtual machine scale set is based on un-managed disk(s) and there is no managed disk to update";
+    static final String VM_BOTH_UNMANAGED_AND_MANAGED_DISK_NOT_ALLOWED = "This virtual machine is based on un-managed disks (s), both un-managed and managed disk cannot exists together in a virtual machine";
+    static final String VMSS_BOTH_UNMANAGED_AND_MANAGED_DISK_NOT_ALLOWED = "This virtual machine scale set is based on un-managed disk(s), both un-managed and managed cannot exists together in a virtual machine scale set";
 }
