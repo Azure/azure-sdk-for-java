975e18866e1b4b9a8ca1f00ddb7719eeef1bdf79
Anudeep Sharma <ans@microsoft.com>
2017-02-21 18:36:19 -0800

    Added List Async operations for Sql Server and Batch

--- azure-mgmt-batch/src/main/java/com/microsoft/azure/management/batch/BatchAccounts.java
+++ azure-mgmt-batch/src/main/java/com/microsoft/azure/management/batch/BatchAccounts.java
@@ -13,12 +13,12 @@ import com.microsoft.azure.management.resources.fluentcore.arm.Region;
 import com.microsoft.azure.management.resources.fluentcore.arm.collection.SupportsDeletingByGroup;
 import com.microsoft.azure.management.resources.fluentcore.arm.collection.SupportsGettingByGroup;
 import com.microsoft.azure.management.resources.fluentcore.arm.collection.SupportsGettingById;
-import com.microsoft.azure.management.resources.fluentcore.arm.collection.SupportsListingByGroup;
+import com.microsoft.azure.management.resources.fluentcore.arm.collection.SupportsListingByGroupAsync;
 import com.microsoft.azure.management.resources.fluentcore.arm.models.HasManager;
 import com.microsoft.azure.management.resources.fluentcore.collection.SupportsBatchCreation;
 import com.microsoft.azure.management.resources.fluentcore.collection.SupportsCreating;
 import com.microsoft.azure.management.resources.fluentcore.collection.SupportsDeletingById;
-import com.microsoft.azure.management.resources.fluentcore.collection.SupportsListing;
+import com.microsoft.azure.management.resources.fluentcore.collection.SupportsListingAsync;
 import com.microsoft.azure.management.resources.fluentcore.model.HasInner;
 
 /**
@@ -27,8 +27,8 @@ import com.microsoft.azure.management.resources.fluentcore.model.HasInner;
 @Fluent
 public interface BatchAccounts extends
         SupportsCreating<BatchAccount.DefinitionStages.Blank>,
-        SupportsListing<BatchAccount>,
-        SupportsListingByGroup<BatchAccount>,
+        SupportsListingAsync<BatchAccount>,
+        SupportsListingByGroupAsync<BatchAccount>,
         SupportsGettingByGroup<BatchAccount>,
         SupportsGettingById<BatchAccount>,
         SupportsDeletingById,
