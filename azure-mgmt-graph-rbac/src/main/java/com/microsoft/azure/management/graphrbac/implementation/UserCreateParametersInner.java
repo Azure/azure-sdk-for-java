a0f300d2e0a81cab84df4c84fa79f738a6144d80
Veerendra <Veerendra.D@happiestminds.com>
2017-02-24 15:41:12 +0530

    added usageLocation attribute in UserCreateParametersInner, this information required when assigning the license to user

--- azure-mgmt-graph-rbac/src/main/java/com/microsoft/azure/management/graphrbac/implementation/UserCreateParametersInner.java
+++ azure-mgmt-graph-rbac/src/main/java/com/microsoft/azure/management/graphrbac/implementation/UserCreateParametersInner.java
@@ -15,163 +15,198 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  * Request parameters for creating a new work or school account user.
  */
 public class UserCreateParametersInner {
-    /**
-     * Whether the account is enabled.
-     */
-    @JsonProperty(required = true)
-    private boolean accountEnabled;
+	/**
+	 * Whether the account is enabled.
+	 */
+	@JsonProperty(required = true)
+	private boolean accountEnabled;
 
-    /**
-     * The display name of the user.
-     */
-    @JsonProperty(required = true)
-    private String displayName;
+	/**
+	 * The display name of the user.
+	 */
+	@JsonProperty(required = true)
+	private String displayName;
 
-    /**
-     * Password Profile.
-     */
-    @JsonProperty(required = true)
-    private PasswordProfile passwordProfile;
+	/**
+	 * Password Profile.
+	 */
+	@JsonProperty(required = true)
+	private PasswordProfile passwordProfile;
 
-    /**
-     * The user principal name (someuser@contoso.com). It must contain one of
-     * the verified domains for the tenant.
-     */
-    @JsonProperty(required = true)
-    private String userPrincipalName;
+	/**
+	 * The user principal name (someuser@contoso.com). It must contain one of
+	 * the verified domains for the tenant.
+	 */
+	@JsonProperty(required = true)
+	private String userPrincipalName;
 
-    /**
-     * The mail alias for the user.
-     */
-    @JsonProperty(required = true)
-    private String mailNickname;
+	/**
+	 * The mail alias for the user.
+	 */
+	@JsonProperty(required = true)
+	private String mailNickname;
 
-    /**
-     * This must be specified if you are using a federated domain for the
-     * user's userPrincipalName (UPN) property when creating a new user
-     * account. It is used to associate an on-premises Active Directory user
-     * account with their Azure AD user object.
-     */
-    private String immutableId;
+	/**
+	 * This must be specified if you are using a federated domain for the user's
+	 * userPrincipalName (UPN) property when creating a new user account. It is
+	 * used to associate an on-premises Active Directory user account with their
+	 * Azure AD user object.
+	 */
+	private String immutableId;
 
-    /**
-     * Get the accountEnabled value.
-     *
-     * @return the accountEnabled value
-     */
-    public boolean accountEnabled() {
-        return this.accountEnabled;
-    }
+	/**
+	 * A two letter country code (ISO standard 3166). Required for users that
+	 * will be assigned licenses due to legal requirement to check for
+	 * availability of services in countries. Examples include: "US", "JP", and
+	 * "GB".
+	 */
+	private String usageLocation;
 
-    /**
-     * Set the accountEnabled value.
-     *
-     * @param accountEnabled the accountEnabled value to set
-     * @return the UserCreateParametersInner object itself.
-     */
-    public UserCreateParametersInner withAccountEnabled(boolean accountEnabled) {
-        this.accountEnabled = accountEnabled;
-        return this;
-    }
+	/**
+	 * Get the accountEnabled value.
+	 *
+	 * @return the accountEnabled value
+	 */
+	public boolean accountEnabled() {
+		return this.accountEnabled;
+	}
 
-    /**
-     * Get the displayName value.
-     *
-     * @return the displayName value
-     */
-    public String displayName() {
-        return this.displayName;
-    }
+	/**
+	 * Set the accountEnabled value.
+	 *
+	 * @param accountEnabled
+	 *            the accountEnabled value to set
+	 * @return the UserCreateParametersInner object itself.
+	 */
+	public UserCreateParametersInner withAccountEnabled(boolean accountEnabled) {
+		this.accountEnabled = accountEnabled;
+		return this;
+	}
 
-    /**
-     * Set the displayName value.
-     *
-     * @param displayName the displayName value to set
-     * @return the UserCreateParametersInner object itself.
-     */
-    public UserCreateParametersInner withDisplayName(String displayName) {
-        this.displayName = displayName;
-        return this;
-    }
+	/**
+	 * Get the displayName value.
+	 *
+	 * @return the displayName value
+	 */
+	public String displayName() {
+		return this.displayName;
+	}
 
-    /**
-     * Get the passwordProfile value.
-     *
-     * @return the passwordProfile value
-     */
-    public PasswordProfile passwordProfile() {
-        return this.passwordProfile;
-    }
+	/**
+	 * Set the displayName value.
+	 *
+	 * @param displayName
+	 *            the displayName value to set
+	 * @return the UserCreateParametersInner object itself.
+	 */
+	public UserCreateParametersInner withDisplayName(String displayName) {
+		this.displayName = displayName;
+		return this;
+	}
 
-    /**
-     * Set the passwordProfile value.
-     *
-     * @param passwordProfile the passwordProfile value to set
-     * @return the UserCreateParametersInner object itself.
-     */
-    public UserCreateParametersInner withPasswordProfile(PasswordProfile passwordProfile) {
-        this.passwordProfile = passwordProfile;
-        return this;
-    }
+	/**
+	 * Get the passwordProfile value.
+	 *
+	 * @return the passwordProfile value
+	 */
+	public PasswordProfile passwordProfile() {
+		return this.passwordProfile;
+	}
 
-    /**
-     * Get the userPrincipalName value.
-     *
-     * @return the userPrincipalName value
-     */
-    public String userPrincipalName() {
-        return this.userPrincipalName;
-    }
+	/**
+	 * Set the passwordProfile value.
+	 *
+	 * @param passwordProfile
+	 *            the passwordProfile value to set
+	 * @return the UserCreateParametersInner object itself.
+	 */
+	public UserCreateParametersInner withPasswordProfile(PasswordProfile passwordProfile) {
+		this.passwordProfile = passwordProfile;
+		return this;
+	}
 
-    /**
-     * Set the userPrincipalName value.
-     *
-     * @param userPrincipalName the userPrincipalName value to set
-     * @return the UserCreateParametersInner object itself.
-     */
-    public UserCreateParametersInner withUserPrincipalName(String userPrincipalName) {
-        this.userPrincipalName = userPrincipalName;
-        return this;
-    }
+	/**
+	 * Get the userPrincipalName value.
+	 *
+	 * @return the userPrincipalName value
+	 */
+	public String userPrincipalName() {
+		return this.userPrincipalName;
+	}
 
-    /**
-     * Get the mailNickname value.
-     *
-     * @return the mailNickname value
-     */
-    public String mailNickname() {
-        return this.mailNickname;
-    }
+	/**
+	 * Set the userPrincipalName value.
+	 *
+	 * @param userPrincipalName
+	 *            the userPrincipalName value to set
+	 * @return the UserCreateParametersInner object itself.
+	 */
+	public UserCreateParametersInner withUserPrincipalName(String userPrincipalName) {
+		this.userPrincipalName = userPrincipalName;
+		return this;
+	}
 
-    /**
-     * Set the mailNickname value.
-     *
-     * @param mailNickname the mailNickname value to set
-     * @return the UserCreateParametersInner object itself.
-     */
-    public UserCreateParametersInner withMailNickname(String mailNickname) {
-        this.mailNickname = mailNickname;
-        return this;
-    }
+	/**
+	 * Get the mailNickname value.
+	 *
+	 * @return the mailNickname value
+	 */
+	public String mailNickname() {
+		return this.mailNickname;
+	}
 
-    /**
-     * Get the immutableId value.
-     *
-     * @return the immutableId value
-     */
-    public String immutableId() {
-        return this.immutableId;
-    }
+	/**
+	 * Set the mailNickname value.
+	 *
+	 * @param mailNickname
+	 *            the mailNickname value to set
+	 * @return the UserCreateParametersInner object itself.
+	 */
+	public UserCreateParametersInner withMailNickname(String mailNickname) {
+		this.mailNickname = mailNickname;
+		return this;
+	}
 
-    /**
-     * Set the immutableId value.
-     *
-     * @param immutableId the immutableId value to set
-     * @return the UserCreateParametersInner object itself.
-     */
-    public UserCreateParametersInner withImmutableId(String immutableId) {
-        this.immutableId = immutableId;
-        return this;
-    }
+	/**
+	 * Get the immutableId value.
+	 *
+	 * @return the immutableId value
+	 */
+	public String immutableId() {
+		return this.immutableId;
+	}
+
+	/**
+	 * Set the immutableId value.
+	 *
+	 * @param immutableId
+	 *            the immutableId value to set
+	 * @return the UserCreateParametersInner object itself.
+	 */
+	public UserCreateParametersInner withImmutableId(String immutableId) {
+		this.immutableId = immutableId;
+		return this;
+	}
+
+	/**
+	 * Get the usageLocation value.
+	 *
+	 * @return the usageLocation value
+	 */
+	public String usageLocation() {
+		return this.mailNickname;
+	}
+
+	/**
+	 * Set the usageLocation value.
+	 *
+	 * @param usageLocation
+	 *            the usageLocation value to set
+	 * @return the UserInner object itself.
+	 */
+	public UserCreateParametersInner withUsageLocation(String usageLocation) {
+		this.usageLocation = usageLocation;
+		return this;
+	}
 
 }
