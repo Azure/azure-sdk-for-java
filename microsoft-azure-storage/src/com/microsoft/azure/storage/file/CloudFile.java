d3f34db2ee441deb233184449c96f7f2c12958ed
Josh Friedman <jofriedm@microsoft.com>
2017-04-25 12:36:26 -0700

    Cleaning up merge from public dev

--- microsoft-azure-storage/src/com/microsoft/azure/storage/file/CloudFile.java
+++ microsoft-azure-storage/src/com/microsoft/azure/storage/file/CloudFile.java
@@ -1881,7 +1881,7 @@ public final class CloudFile implements ListFileItem {
      */
     @DoesServiceRequest
     public FileOutputStream openWriteExisting(AccessCondition accessCondition, FileRequestOptions options,
-            OperationContext opContext) throws StorageException {
+            OperationContext opContext) throws StorageException, URISyntaxException {
         return this.openOutputStreamInternal(null /* length */, accessCondition, options, opContext);
     }
 
