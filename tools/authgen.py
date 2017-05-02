421c309ce466d4f2101c96d55c430b6c85dfd864
Jianghao Lu <jianghaolu@users.noreply.github.com>
2017-05-02 12:01:42 -0700

    Make auth script more robust on Windows (#1637)

--- tools/authgen.py
+++ tools/authgen.py
@@ -7,19 +7,19 @@ import json
 import re
 
 # Endpoints
-p = subprocess.Popen("az cloud show -o json --query '{managementURI: endpoints.management, baseURL: endpoints.resourceManager, authURL: endpoints.activeDirectory, graphURL: endpoints.activeDirectoryGraphResourceId}'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+p = subprocess.Popen('az cloud show -o json --query "{managementURI: endpoints.management, baseURL: endpoints.resourceManager, authURL: endpoints.activeDirectory, graphURL: endpoints.activeDirectoryGraphResourceId}"', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 p.wait()
-cloud = json.loads(p.stdout.read())
+cloud = json.loads(p.stdout.read().decode("utf8"))
 # Subscription
 p = subprocess.Popen('az account show -o json --query "{subscription: id}"', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 p.wait()
-account = json.loads(p.stdout.read())
+account = json.loads(p.stdout.read().decode("utf8"))
 # Service principal
 p = subprocess.Popen('az ad sp create-for-rbac -o json --query "{client: appId, key: password, tenant: tenant}"', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 p.wait()
 out = p.stdout.read()
 out = re.sub(b"[^.]*{", b"{", out)
-sp = json.loads(out)
+sp = json.loads(out.decode("utf8"))
 for key,value in sp.items():
     print(key + "=" + value)
 for key,value in account.items():
