ebdf0b2193cee07df8a86615c7e5c94f278e3fa3
Jianghao Lu <jianghaolu@users.noreply.github.com>
2017-05-08 11:35:51 -0700

    Address cr feedback

--- azure-mgmt-resources/src/main/java/com/microsoft/azure/management/resources/fluentcore/utils/ProviderRegistrationInterceptor.java
+++ azure-mgmt-resources/src/main/java/com/microsoft/azure/management/resources/fluentcore/utils/ProviderRegistrationInterceptor.java
@@ -26,8 +26,8 @@ import java.util.regex.Pattern;
 /**
  * An interceptor for automatic provider registration in Azure.
  */
-public class ProviderRegistrationInterceptor implements Interceptor {
-    private AzureTokenCredentials credentials;
+public final class ProviderRegistrationInterceptor implements Interceptor {
+    private final AzureTokenCredentials credentials;
 
     /**
      * Initialize a provider registration interceptor with a credential that's authorized
@@ -43,24 +43,25 @@ public class ProviderRegistrationInterceptor implements Interceptor {
         Response response = chain.proceed(chain.request());
         if (!response.isSuccessful()) {
             String content = errorBody(response.body());
-            RestClient restClient = new RestClient.Builder()
-                    .withBaseUrl("https://" + chain.request().url().host())
-                    .withCredentials(credentials)
-                    .withSerializerAdapter(new AzureJacksonAdapter())
-                    .withResponseBuilderFactory(new AzureResponseBuilder.Factory())
-                    .build();
-            CloudError cloudError = restClient.serializerAdapter().deserialize(content, CloudError.class);
+            AzureJacksonAdapter jacksonAdapter = new AzureJacksonAdapter();
+            CloudError cloudError = jacksonAdapter.deserialize(content, CloudError.class);
             if ("MissingSubscriptionRegistration".equals(cloudError.code())) {
-                Pattern pattern = Pattern.compile("/subscriptions/([\\w-]+)/");
+                Pattern pattern = Pattern.compile("/subscriptions/([\\w-]+)/", Pattern.CASE_INSENSITIVE);
                 Matcher matcher = pattern.matcher(chain.request().url().toString());
                 matcher.find();
+                RestClient restClient = new RestClient.Builder()
+                        .withBaseUrl("https://" + chain.request().url().host())
+                        .withCredentials(credentials)
+                        .withSerializerAdapter(jacksonAdapter)
+                        .withResponseBuilderFactory(new AzureResponseBuilder.Factory())
+                        .build();
                 ResourceManager resourceManager = ResourceManager.authenticate(restClient)
                         .withSubscription(matcher.group(1));
                 pattern = Pattern.compile(".*'(.*)'");
                 matcher = pattern.matcher(cloudError.message());
                 matcher.find();
                 Provider provider = registerProvider(matcher.group(1), resourceManager);
-                while (provider.registrationState().equals("Unregistered")
+                while (provider.registrationState().equalsIgnoreCase("Unregistered")
                         || provider.registrationState().equalsIgnoreCase("Registering")) {
                     SdkContext.sleep(5 * 1000);
                     provider = resourceManager.providers().getByName(provider.namespace());
@@ -79,7 +80,6 @@ public class ProviderRegistrationInterceptor implements Interceptor {
         BufferedSource source = responseBody.source();
         source.request(Long.MAX_VALUE); // Buffer the entire body.
         Buffer buffer = source.buffer();
-
         return buffer.readUtf8();
     }
 
