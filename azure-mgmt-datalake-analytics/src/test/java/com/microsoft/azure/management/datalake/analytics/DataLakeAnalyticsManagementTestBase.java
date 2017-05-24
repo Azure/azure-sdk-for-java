74b36aa6750a25d62a12784e9aef84d4c7b45606
Martin Sawicki <marcins@microsoft.com>
2017-05-24 10:24:29 -0700

    Attempt to fix transient CI mock test failures  (#1688)
    * attempt to fix transient CI mock test failures by turning some MockIntegrationTestBase statics into instance and following WireMock's guidelines
    
    * another attempt at fixing CI
    
    * another try

--- azure-mgmt-datalake-analytics/src/test/java/com/microsoft/azure/management/datalake/analytics/DataLakeAnalyticsManagementTestBase.java
+++ azure-mgmt-datalake-analytics/src/test/java/com/microsoft/azure/management/datalake/analytics/DataLakeAnalyticsManagementTestBase.java
@@ -59,7 +59,7 @@ public class DataLakeAnalyticsManagementTestBase extends TestBase {
         // the tests are running in.
         String adlaSuffix = "azuredatalakeanalytics.net";
 
-        addTextReplacementRule("https://(.*)." + adlaSuffix, MOCK_URI);
+        addTextReplacementRule("https://(.*)." + adlaSuffix, this.mockUri());
 
         // Generate creds and a set of rest clients for catalog and job
         ApplicationTokenCredentials credentials = new AzureTestCredentials();
@@ -78,7 +78,7 @@ public class DataLakeAnalyticsManagementTestBase extends TestBase {
                     .withBaseUrl("https://{accountName}.{adlaJobDnsSuffix}")
                     .withCredentials(credentials)
                     .withLogLevel(LogLevel.BODY_AND_HEADERS)
-                    .withNetworkInterceptor(interceptor), IS_MOCKED);
+                    .withNetworkInterceptor(this.interceptor()), IS_MOCKED);
 
 
             dataLakeAnalyticsJobManagementClient = new DataLakeAnalyticsJobManagementClientImpl(restClientWithTimeout)
@@ -89,7 +89,7 @@ public class DataLakeAnalyticsManagementTestBase extends TestBase {
                     .withBaseUrl("https://{accountName}.{adlaCatalogDnsSuffix}")
                     .withCredentials(credentials)
                     .withLogLevel(LogLevel.BODY_AND_HEADERS)
-                    .withNetworkInterceptor(interceptor), IS_MOCKED);
+                    .withNetworkInterceptor(this.interceptor()), IS_MOCKED);
 
             dataLakeAnalyticsCatalogManagementClient = new DataLakeAnalyticsCatalogManagementClientImpl(catalogRestClient)
                     .withAdlaCatalogDnsSuffix(adlaSuffix);
