f289a53e6b6fe2b70f47866fe70e37bea105976d
unknown <marcins@MARCINS-BRICK.redmond.corp.microsoft.com>
2017-05-24 12:27:08 -0700

    moving Storage async methods to GA

--- azure-mgmt-storage/src/main/java/com/microsoft/azure/management/storage/StorageAccounts.java
+++ azure-mgmt-storage/src/main/java/com/microsoft/azure/management/storage/StorageAccounts.java
@@ -6,7 +6,6 @@
 
 package com.microsoft.azure.management.storage;
 
-import com.microsoft.azure.management.apigeneration.Beta;
 import com.microsoft.azure.management.apigeneration.Fluent;
 import com.microsoft.azure.management.resources.fluentcore.arm.collection.SupportsBatchDeletion;
 import com.microsoft.azure.management.resources.fluentcore.arm.collection.SupportsDeletingByResourceGroup;
@@ -53,9 +52,8 @@ public interface StorageAccounts extends
      * Checks that account name is valid and is not in use asynchronously.
      *
      * @param name the account name to check
-     * @return whether the name is available and other info if not
+     * @return a representation of the deferred computation of this call, returning whether the name is available and other info if not
      */
-    @Beta
     Observable<CheckNameAvailabilityResult> checkNameAvailabilityAsync(String name);
 
     /**
@@ -65,6 +63,5 @@ public interface StorageAccounts extends
      * @param callback the callback to call on success or failure
      * @return a handle to cancel the request
      */
-    @Beta
     ServiceFuture<CheckNameAvailabilityResult> checkNameAvailabilityAsync(String name, ServiceCallback<CheckNameAvailabilityResult> callback);
 }
