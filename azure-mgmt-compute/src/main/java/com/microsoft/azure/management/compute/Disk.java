9724072ed129d5cbb3812cccff7fb27f981876a4
unknown <marcins@MARCINS-BRICK.redmond.corp.microsoft.com>
2017-05-24 11:58:22 -0700

    checsktyle

--- azure-mgmt-compute/src/main/java/com/microsoft/azure/management/compute/Disk.java
+++ azure-mgmt-compute/src/main/java/com/microsoft/azure/management/compute/Disk.java
@@ -6,7 +6,6 @@
 
 package com.microsoft.azure.management.compute;
 
-import com.microsoft.azure.management.apigeneration.Beta;
 import com.microsoft.azure.management.apigeneration.Fluent;
 import com.microsoft.azure.management.compute.implementation.ComputeManager;
 import com.microsoft.azure.management.compute.implementation.DiskInner;
