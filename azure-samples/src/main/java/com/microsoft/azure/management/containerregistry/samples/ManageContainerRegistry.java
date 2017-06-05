1f523abb6b5fd6d8f0bb176756aff225766bc581
milismsft-mac <adrianmi@microsoft.com>
2017-06-05 09:59:34 -0700

    Address review comments and skip test when running in playback using the new framework

--- azure-samples/src/main/java/com/microsoft/azure/management/containerregistry/samples/ManageContainerRegistry.java
+++ azure-samples/src/main/java/com/microsoft/azure/management/containerregistry/samples/ManageContainerRegistry.java
@@ -29,11 +29,12 @@ import java.util.List;
 /**
  * Azure Container Registry sample for managing container registry.
  *    - Create an Azure Container Registry to be used for holding the Docker images
- *    - Create a Linux virtual machine that will host a Docker engine to be used for this sample if a local one cannot be found
+ *    - If a local Docker engine cannot be found, create a Linux virtual machine that will host a Docker engine
+ *        to be used for this sample
  *    - Use Docker Java to create a Docker client that will push/pull an image to/from Azure Container Registry
  *    - Pull a test image from the public Docker repo (hello-world:latest) to be used as a sample for pushing/pulling
  *        to/from an Azure Container Registry
- *    - Create and run a new Docker container from an image that was pulled from Azure Container Registry
+ *    - Create a new Docker container from an image that was pulled from Azure Container Registry
  */
 public class ManageContainerRegistry {
 
@@ -106,7 +107,7 @@ public class ManageContainerRegistry {
             }
 
             //=============================================================
-            // Commit the new container to the Azure Container Registry from above
+            // Commit the new container
 
             String privateRepoUrl = azureRegistry.loginServerUrl() + "/samples/" + dockerContainerName;
             String dockerImageId = dockerClient.commitCmd(dockerContainerInstance.getId())
--- azure-samples/src/test/java/com/microsoft/azure/management/samples/ContainerRegistryTests.java
+++ azure-samples/src/test/java/com/microsoft/azure/management/samples/ContainerRegistryTests.java
@@ -6,15 +6,18 @@
 
 package com.microsoft.azure.management.samples;
 import com.microsoft.azure.management.containerregistry.samples.ManageContainerRegistry;
+import com.microsoft.azure.management.resources.core.TestBase;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class ContainerRegistryTests extends SamplesTestBase {
+    public ContainerRegistryTests() {
+        // Failing in playback - dependent on Docker client/glassfish jersey library which is expecting a real connection
+        super(TestBase.RunCondition.LIVE_ONLY);
+    }
+
     @Test
     public void testManageContainerRegistry() {
-        if (!IS_MOCKED) {
-            // Failing in playback - dependent on Docker client/glassfish jersey library which is expecting a real connection
-            Assert.assertTrue(ManageContainerRegistry.runSample(azure));
-        }
+        Assert.assertTrue(ManageContainerRegistry.runSample(azure));
     }
 }
