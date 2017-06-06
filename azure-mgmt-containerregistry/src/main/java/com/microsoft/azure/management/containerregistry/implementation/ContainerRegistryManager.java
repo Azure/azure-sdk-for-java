a79ce71f3d1abfe14d8ed9d84548e8d4c4f1d464
Alva Bandy <alvab@microsoft.com>
2017-06-06 16:05:15 -0700

    Add auto registry to container regsitry manager.

--- azure-mgmt-containerregistry/src/main/java/com/microsoft/azure/management/containerregistry/implementation/ContainerRegistryManager.java
+++ azure-mgmt-containerregistry/src/main/java/com/microsoft/azure/management/containerregistry/implementation/ContainerRegistryManager.java
@@ -7,6 +7,7 @@
 package com.microsoft.azure.management.containerregistry.implementation;
 
 import com.microsoft.azure.AzureEnvironment;
+import com.microsoft.azure.AzureResponseBuilder;
 import com.microsoft.azure.credentials.AzureTokenCredentials;
 import com.microsoft.azure.management.apigeneration.Beta;
 import com.microsoft.azure.management.apigeneration.Beta.SinceVersion;
@@ -14,7 +15,9 @@ import com.microsoft.azure.management.containerregistry.Registries;
 import com.microsoft.azure.management.resources.fluentcore.arm.AzureConfigurable;
 import com.microsoft.azure.management.resources.fluentcore.arm.implementation.AzureConfigurableImpl;
 import com.microsoft.azure.management.resources.fluentcore.arm.implementation.Manager;
+import com.microsoft.azure.management.resources.fluentcore.utils.ProviderRegistrationInterceptor;
 import com.microsoft.azure.management.storage.implementation.StorageManager;
+import com.microsoft.azure.serializer.AzureJacksonAdapter;
 import com.microsoft.rest.RestClient;
 
 /**
@@ -46,6 +49,9 @@ public final class ContainerRegistryManager extends Manager<ContainerRegistryMan
         return new ContainerRegistryManager(new RestClient.Builder()
                 .withBaseUrl(credentials.environment(), AzureEnvironment.Endpoint.RESOURCE_MANAGER)
                 .withCredentials(credentials)
+                .withSerializerAdapter(new AzureJacksonAdapter())
+                .withResponseBuilderFactory(new AzureResponseBuilder.Factory())
+                .withInterceptor(new ProviderRegistrationInterceptor(credentials))
                 .build(), subscriptionId);
     }
 
