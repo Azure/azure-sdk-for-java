b99034d33b2e4ad08ed9954f8bc28746d8eca41d
anuchan <anuamd@gmail.com>
2017-06-09 17:44:27 -0700

    Support for executable interface, enable defining dependency between executables and creatables

--- azure-mgmt-resources/src/test/java/com/microsoft/azure/management/resources/fluentcore/dag/IBreadSlice.java
+++ azure-mgmt-resources/src/test/java/com/microsoft/azure/management/resources/fluentcore/dag/IBreadSlice.java
@@ -1,7 +1,19 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
 package com.microsoft.azure.management.resources.fluentcore.dag;
 
+import com.microsoft.azure.management.resources.fluentcore.model.Creatable;
+import com.microsoft.azure.management.resources.fluentcore.model.Executable;
+import com.microsoft.azure.management.resources.fluentcore.model.Indexable;
+
 /**
- * Created by anuthomaschandy on 6/9/17.
+ * Represents a type when executed returns a bread from the store.
  */
-public class IBreadSlice {
+public interface IBreadSlice extends Indexable, Executable<IBreadSlice> {
+    IBreadSlice withAnotherSliceFromStore(Executable<IBreadSlice> breadFetcher);
+    IBreadSlice withNewOrder(Creatable<IOrder> order);
 }
