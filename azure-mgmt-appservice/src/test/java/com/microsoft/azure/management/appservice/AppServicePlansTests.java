35085e750810f01e1c351f436a2756c458c6d5bc
vladimir-shcherbakov <shcherbakov.mailbox@gmail.com>
2017-07-22 16:42:59 -0700

    Import "Ignore"

--- azure-mgmt-appservice/src/test/java/com/microsoft/azure/management/appservice/AppServicePlansTests.java
+++ azure-mgmt-appservice/src/test/java/com/microsoft/azure/management/appservice/AppServicePlansTests.java
@@ -10,6 +10,7 @@ import com.microsoft.azure.management.resources.fluentcore.arm.Region;
 import com.microsoft.rest.RestClient;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.Ignore;
 
 import java.util.List;
 
