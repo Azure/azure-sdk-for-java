c000deaab29f31e6022ed88b83f69b5fb6e28fa4
milismsft-mac <adrianmi@microsoft.com>
2017-08-03 13:36:51 -0700

    add LangDefinition

--- azure-mgmt-search/src/main/java/com/microsoft/azure/management/search/implementation/AdminKeysImpl.java
+++ azure-mgmt-search/src/main/java/com/microsoft/azure/management/search/implementation/AdminKeysImpl.java
@@ -5,12 +5,14 @@
  */
 package com.microsoft.azure.management.search.implementation;
 
+import com.microsoft.azure.management.apigeneration.LangDefinition;
 import com.microsoft.azure.management.resources.fluentcore.model.implementation.WrapperImpl;
 import com.microsoft.azure.management.search.AdminKeys;
 
 /**
  * Response containing the primary and secondary admin API keys for a given Azure Search service.
  */
+@LangDefinition
 class AdminKeysImpl extends WrapperImpl<AdminKeyResultInner> implements AdminKeys {
 
   protected AdminKeysImpl(AdminKeyResultInner innerObject) {
