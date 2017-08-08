e237b5aa298e762c2a7322cbb233c3a431302205
Binzy Wu <binzywu@msn.com>
2017-08-08 15:01:31 -0700

    add javadoc for IMessageSessionEntity

--- azure-servicebus/src/main/java/com/microsoft/azure/servicebus/IMessageSessionEntity.java
+++ azure-servicebus/src/main/java/com/microsoft/azure/servicebus/IMessageSessionEntity.java
@@ -1,12 +1,17 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 package com.microsoft.azure.servicebus;
 
-import java.time.Duration;
 import java.time.Instant;
 import java.util.Collection;
 import java.util.concurrent.CompletableFuture;
 
 import com.microsoft.azure.servicebus.primitives.ServiceBusException;
 
+/**
+ * Represents a session full client entity.
+ */
 public interface IMessageSessionEntity {
 //	int getSessionPrefetchCount();
 //	
@@ -27,12 +32,40 @@ public interface IMessageSessionEntity {
 //    CompletableFuture<IMessageSession> acceptMessageSessionAsync(String sessionId);
 //
 //    CompletableFuture<IMessageSession> acceptMessageSessionAsync(String sessionId, Duration serverWaitTime);
-    
+
+    /**
+     * Gets the message sessions, enabling you to browse sessions on queues.
+     *
+     * @return A collection of sessions.
+     * @throws InterruptedException if the current thread was interrupted while waiting.
+     * @throws ServiceBusException  if get sessions failed.
+     */
     Collection<IMessageSession> getMessageSessions() throws InterruptedException, ServiceBusException;
 
+    /**
+     * Retrieves all message sessions whose session state was updated since lastUpdatedTime.
+     *
+     * @param lastUpdatedTime The time the session was last updated.
+     * @return A collection of sessions.
+     * @throws InterruptedException if the current thread was interrupted while waiting.
+     * @throws ServiceBusException  if get sessions failed.
+     */
     Collection<IMessageSession> getMessageSessions(Instant lastUpdatedTime) throws InterruptedException, ServiceBusException;
 
+    /**
+     * Asynchronously gets the message sessions, enabling you to browse sessions on queues.
+     *
+     * @return a CompletableFuture representing the pending operation to get sessions.
+     * @see IMessageSessionEntity#getMessageSessions
+     */
     CompletableFuture<Collection<IMessageSession>> getMessageSessionsAsync();
 
+    /**
+     * Asynchronously retrieves all message sessions whose session state was updated since lastUpdatedTime.
+     *
+     * @param lastUpdatedTime The time the session was last updated.
+     * @return a CompletableFuture representing the pending operation to get sessions.
+     * @see IMessageSessionEntity#getMessageSessions(Instant)
+     */
     CompletableFuture<Collection<IMessageSession>> getMessageSessionsAsync(Instant lastUpdatedTime);
 }
