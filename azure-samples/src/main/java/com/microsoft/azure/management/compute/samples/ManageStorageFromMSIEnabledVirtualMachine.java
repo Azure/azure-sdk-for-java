22c39bcc08ba76ba16e0e1a6730060888971feb9
anuchan <anuamd@gmail.com>
2017-08-12 16:38:42 -0700

    Using path to the script file [to install az cli and using cli to create storage] in orgin repo

--- azure-samples/src/main/java/com/microsoft/azure/management/compute/samples/ManageStorageFromMSIEnabledVirtualMachine.java
+++ azure-samples/src/main/java/com/microsoft/azure/management/compute/samples/ManageStorageFromMSIEnabledVirtualMachine.java
@@ -44,7 +44,7 @@ public final class ManageStorageFromMSIEnabledVirtualMachine {
         final String password = "12NewPA$$w0rd!";
         final Region region = Region.US_WEST_CENTRAL;
 
-        final String installScript = "https://raw.githubusercontent.com/anuchandy/azure-sdk-for-java/compute-chash-gen/azure-samples/src/main/resources/create_resources_with_msi.sh";
+        final String installScript = "https://raw.githubusercontent.com/Azure/azure-sdk-for-java/master/azure-samples/src/main/resources/create_resources_with_msi.sh";
         String installCommand = "bash create_resources_with_msi.sh {subscriptionID} {port} {stgName} {rgName} {location}";
         List<String> fileUris = new ArrayList<>();
         fileUris.add(installScript);
