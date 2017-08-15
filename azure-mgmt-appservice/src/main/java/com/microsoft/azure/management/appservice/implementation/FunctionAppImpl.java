d2353d13437d770b94a253387e581f975167612c
Jianghao Lu <jianghaolu@users.noreply.github.com>
2017-08-14 17:52:34 -0700

    Fix in function app keys

--- azure-mgmt-appservice/src/main/java/com/microsoft/azure/management/appservice/implementation/FunctionAppImpl.java
+++ azure-mgmt-appservice/src/main/java/com/microsoft/azure/management/appservice/implementation/FunctionAppImpl.java
@@ -29,6 +29,7 @@ import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Headers;
+import retrofit2.http.DELETE;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
