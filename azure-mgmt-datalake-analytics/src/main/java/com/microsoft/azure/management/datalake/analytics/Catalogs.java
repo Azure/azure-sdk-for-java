3600466f5961e8cff94fabd1ca2d787d0ba51dc8
ro-joowan <jooro@microsoft.com>
2017-08-29 17:51:40 -0700

    [CODE REFACTOR] - Removing an import to pass style check
    * Removing the unused import java.io.IOException across files to pass
    Maven style check

--- azure-mgmt-datalake-analytics/src/main/java/com/microsoft/azure/management/datalake/analytics/Catalogs.java
+++ azure-mgmt-datalake-analytics/src/main/java/com/microsoft/azure/management/datalake/analytics/Catalogs.java
@@ -35,7 +35,6 @@ import com.microsoft.azure.PagedList;
 import com.microsoft.rest.ServiceCallback;
 import com.microsoft.rest.ServiceFuture;
 import com.microsoft.rest.ServiceResponse;
-import java.io.IOException;
 import java.util.List;
 import rx.Observable;
 
