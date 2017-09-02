f84b16700c289417b7c38728b3cb3a695c76c64d
unknown <marcins@MARCINS-BRICK.redmond.corp.microsoft.com>
2017-09-02 02:28:34 -0700

    rerecordings and sample tweaks

--- azure-samples/src/main/java/com/microsoft/azure/management/network/samples/ManageVirtualNetwork.java
+++ azure-samples/src/main/java/com/microsoft/azure/management/network/samples/ManageVirtualNetwork.java
@@ -243,7 +243,7 @@ public final class ManageVirtualNetwork {
         } finally {
             try {
                 System.out.println("Deleting Resource Group: " + rgName);
-                azure.resourceGroups().deleteByName(rgName);
+                azure.resourceGroups().beginDeleteByName(rgName);
                 System.out.println("Deleted Resource Group: " + rgName);
             } catch (NullPointerException npe) {
                 System.out.println("Did not create any resources in Azure. No clean up is necessary");
