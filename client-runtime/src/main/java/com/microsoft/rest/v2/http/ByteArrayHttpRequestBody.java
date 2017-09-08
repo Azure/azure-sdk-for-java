e7f680e600fece965ff3e6a1cfe269c018941f25
Dan Schulte <daschult@microsoft.com>
2017-09-08 15:34:55 -0700

    Fix checkstyle errors

--- client-runtime/src/main/java/com/microsoft/rest/v2/http/ByteArrayHttpRequestBody.java
+++ client-runtime/src/main/java/com/microsoft/rest/v2/http/ByteArrayHttpRequestBody.java
@@ -1,11 +1,24 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
 package com.microsoft.rest.v2.http;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
+/**
+ * A HTTP request body that contains a byte[].
+ */
 public class ByteArrayHttpRequestBody implements HttpRequestBody {
     private final byte[] contents;
 
+    /**
+     * Create a new ByteArrayHttpRequestBody with the provided byte[].
+     * @param contents The byte[] to store in this ByteArrayHttpRequestBody.
+     */
     public ByteArrayHttpRequestBody(byte[] contents) {
         this.contents = contents;
     }
