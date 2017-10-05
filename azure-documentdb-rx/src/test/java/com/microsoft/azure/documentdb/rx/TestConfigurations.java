cf4d4e739669115d11513c2d7644e0ca4e63d563
Mohammad Derakhshani <moderakh@users.noreply.github.com>
2017-10-05 09:48:49 -0700

    travis config

--- azure-documentdb-rx/src/test/java/com/microsoft/azure/documentdb/rx/TestConfigurations.java
+++ azure-documentdb-rx/src/test/java/com/microsoft/azure/documentdb/rx/TestConfigurations.java
@@ -1,17 +1,17 @@
 /**
  * The MIT License (MIT)
  * Copyright (c) 2016 Microsoft Corporation
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -29,7 +29,13 @@ public final class TestConfigurations {
     // Replace MASTER_KEY and HOST with values from your DocumentDB account.
     // The default values are credentials of the local emulator, which are not used in any production environment.
     // <!--[SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine")]-->
-    public static final String MASTER_KEY =
-            "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
-    public static final String HOST = "https://localhost:443/";
+    //public static final String MASTER_KEY =
+    //"C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+    //public static final String HOST = "https://localhost:443/";
+
+    public static String MASTER_KEY = System.getProperty("ACCOUNT_KEY");
+    public static String HOST = System.getProperty("ACCOUNT_HOST");
+    static {
+        System.out.println("host is " + System.getProperty("ACCOUNT_HOST"));
+    }
 }
