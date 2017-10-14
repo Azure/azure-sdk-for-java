7598bf8afa6d6d24fee90cb2ce1705a2866fa00c
Rikki Gibson <rigibson@microsoft.com>
2017-10-13 17:06:55 -0700

    Test XML serialization with wrapper

--- client-runtime/src/test/java/com/microsoft/rest/RestProxyXMLTests.java
+++ client-runtime/src/test/java/com/microsoft/rest/RestProxyXMLTests.java
@@ -7,6 +7,14 @@
 
 package com.microsoft.rest;
 
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import com.google.common.base.Charsets;
 import com.google.common.io.ByteStreams;
 import com.google.common.reflect.TypeToken;
@@ -26,7 +34,6 @@ import com.microsoft.rest.serializer.JacksonAdapter;
 import org.joda.time.DateTime;
 import org.junit.Test;
 import rx.Single;
-import sun.misc.IOUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -34,11 +41,36 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.*;
+
+@JacksonXmlRootElement(localName = "SignedIdentifiers")
+class SignedIdentifiersWrapper {
+    @JacksonXmlProperty(localName = "SignedIdentifier")
+    @JacksonXmlElementWrapper(useWrapping = false)
+    private List<SignedIdentifierInner> signedIdentifiers;
+    /**
+     * Get the SignedIdentifiers value.
+     *
+     * @return the SignedIdentifiers value
+     */
+    public List<SignedIdentifierInner> signedIdentifiers() {
+        return signedIdentifiers;
+    }
+    /**
+     * Set the SignedIdentifiers value.
+     *
+     * @param signedIdentifiers the List<SignedIdentifierInner> value to set
+     * @return the SignedIdentifiers object itself.
+     */
+    public SignedIdentifiersWrapper withSignedIdentifiers(List<SignedIdentifierInner> signedIdentifiers) {
+        this.signedIdentifiers = signedIdentifiers;
+        return this;
+    }
+}
+
 
 public class RestProxyXMLTests {
     static class MockXMLHTTPClient extends HttpClient {
@@ -64,16 +96,16 @@ public class RestProxyXMLTests {
     @Host("http://unused")
     interface MyXMLService {
         @GET("GetContainerACLs")
-        List<SignedIdentifierInner> getContainerACLs();
+        SignedIdentifiersWrapper getContainerACLs();
 
         @PUT("SetContainerACLs")
-        void setContainerACLs(@BodyParam("application/xml") List<SignedIdentifierInner> signedIdentifiers);
+        void setContainerACLs(@BodyParam("application/xml") SignedIdentifiersWrapper signedIdentifiers);
     }
 
     @Test
     public void canReadXMLResponse() throws Exception {
         MyXMLService myXMLService = RestProxy.create(MyXMLService.class, null, new MockXMLHTTPClient(), new JacksonAdapter());;
-        List<SignedIdentifierInner> identifiers = myXMLService.getContainerACLs();
+        List<SignedIdentifierInner> identifiers = myXMLService.getContainerACLs().signedIdentifiers();
         assertNotNull(identifiers);
         assertNotEquals(0, identifiers.size());
     }
@@ -118,12 +150,16 @@ public class RestProxyXMLTests {
         JacksonAdapter serializer = new JacksonAdapter();
         MockXMLReceiverClient httpClient = new MockXMLReceiverClient();
         MyXMLService myXMLService = RestProxy.create(MyXMLService.class, null, httpClient, serializer);
-        myXMLService.setContainerACLs(expectedAcls);
+        SignedIdentifiersWrapper wrapper = new SignedIdentifiersWrapper();
+        wrapper.withSignedIdentifiers(expectedAcls);
+        myXMLService.setContainerACLs(wrapper);
 
-        List<SignedIdentifierInner> actualAcls = serializer.deserialize(
+        SignedIdentifiersWrapper actualAclsWrapped = serializer.deserialize(
                 new String(httpClient.receivedBytes, Charsets.UTF_8),
-                new TypeToken<List<SignedIdentifierInner>>() {}.getType(),
+                new TypeToken<SignedIdentifiersWrapper>() {}.getType(),
                 SerializerAdapter.Encoding.XML);
+
+        List<SignedIdentifierInner> actualAcls = actualAclsWrapped.signedIdentifiers();
 
         assertEquals(expectedAcls.size(), actualAcls.size());
         assertEquals(expectedAcls.get(0).id(), actualAcls.get(0).id());
