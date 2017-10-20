ebfa9e62b67a82d8e498d789ef36f4deb58f9982
Emanuel Vecchio (Southworks SRL) <v-envec@microsoft.com>
2017-10-20 08:35:23 -0300

    Fix checkstyle errors

--- services/azure-media/src/main/java/com/microsoft/windowsazure/services/media/authentication/TokenProvider.java
+++ services/azure-media/src/main/java/com/microsoft/windowsazure/services/media/authentication/TokenProvider.java
@@ -2,12 +2,12 @@ package com.microsoft.windowsazure.services.media.authentication;
 
 public interface TokenProvider {
 
-	/**
-	 * Acquire an access token
-	 *
-	 * @return a valid access token
-	 * @throws Exception
-	 */
-	AzureAdAccessToken acquireAccessToken() throws Exception;
+    /**
+     * Acquire an access token
+     *
+     * @return a valid access token
+     * @throws Exception
+     */
+    AzureAdAccessToken acquireAccessToken() throws Exception;
 
 }
