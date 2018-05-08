540a8caa9860b7dc81d0df8011741757af9e926f
Jianghao Lu <jianghaolu@users.noreply.github.com>
2018-05-08 15:46:36 -0700

    Move stuff around

---
+++ mediaservices/data/src/main/java/com/microsoft/windowsazure/services/media/implementation/templates/tokenrestriction/package-info.java
@@ -0,0 +1,7 @@
+@XmlSchema(
+    namespace = "http://schemas.microsoft.com/Azure/MediaServices/KeyDelivery/TokenRestrictionTemplate/v1",
+    elementFormDefault = XmlNsForm.QUALIFIED)
+package com.microsoft.windowsazure.services.media.implementation.templates.tokenrestriction;
+
+import javax.xml.bind.annotation.XmlNsForm;
+import javax.xml.bind.annotation.XmlSchema;
