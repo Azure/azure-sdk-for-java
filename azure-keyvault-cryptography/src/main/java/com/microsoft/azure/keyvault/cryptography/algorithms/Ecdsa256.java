1872c751ca304909417c3d622c536ce87eb6186f
tiffanyachen <tifchen@microsoft.com>
2018-05-09 15:29:25 -0700

    Neglected to add ecdsa256

--- azure-keyvault-cryptography/src/main/java/com/microsoft/azure/keyvault/cryptography/algorithms/Ecdsa256.java
+++ azure-keyvault-cryptography/src/main/java/com/microsoft/azure/keyvault/cryptography/algorithms/Ecdsa256.java
@@ -1,19 +1,12 @@
 package com.microsoft.azure.keyvault.cryptography.algorithms;
 
-import java.security.KeyPair;
-import java.security.Provider;
-
-import com.microsoft.azure.keyvault.cryptography.ISignatureTransform;
-
 public class Ecdsa256 extends Ecdsa {
-	public final static String ALGORITHM_NAME = "NONEwithECDSA";
+    public final static String ALGORITHM_NAME = "NONEwithECDSA";
 	
-	public Ecdsa256() {
-		super(ALGORITHM_NAME);
-	}
-	
-	protected ISignatureTransform createSignatureTransform(KeyPair key, Provider provider) {
-		return createSignatureTransform(key, ALGORITHM_NAME, provider);
-	}
-	
+    @Override
+    public void checkDigestLength(byte[] digest) {
+        if (digest.length != 32) {
+            throw new IllegalArgumentException("Please check the hash of the digest.");
+        }
+    }
 }
