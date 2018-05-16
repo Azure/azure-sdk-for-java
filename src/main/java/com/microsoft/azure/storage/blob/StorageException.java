bbd994a4de4573094ad33c2d83a82f7c50c1e428
rickle-msft <frley@microsoft.com>
2018-05-16 12:06:32 -0700

    Added ErrorCode

--- src/main/java/com/microsoft/azure/storage/blob/StorageException.java
+++ src/main/java/com/microsoft/azure/storage/blob/StorageException.java
@@ -2,12 +2,13 @@ package com.microsoft.azure.storage.blob;
 
 import com.microsoft.azure.storage.blob.models.ResponseError;
 import com.microsoft.azure.storage.blob.models.ResponseErrorException;
+import com.microsoft.azure.storage.blob.models.StorageErrorCode;
 import com.microsoft.rest.v2.RestException;
 
 /**
  * A {@code StorageException} is thrown whenever Azure Storage successfully returns an error code that is not 200-level.
- * Users can inspect the status code and error code to determine the cause of the error response. The response body may
- * also contain more detailed information depending on the type of error. The user may also inspect the raw HTTP
+ * Users can inspect the status code and error code to determine the cause of the error response. The exception message
+ * may also contain more detailed information depending on the type of error. The user may also inspect the raw HTTP
  * response.
  * Note that even some expected "errors" will be thrown as a {@code StorageException}. For example, some users may
  * perform a getProperties request on an entity to determine whether it exists or not. If it does not exists, an
@@ -23,17 +24,8 @@ public final class StorageException extends RestException{
      * @return
      *      The error code returned by the service.
      */
-    public String errorCode() {
-        return super.response().headers().value(Constants.HeaderConstants.ERROR_CODE);
-    }
-
-    /**
-     * @return
-     *      The body of the response which may contain more detailed error information depending on the type of request
-     *      and error. Fields not found in the response will be null.
-     */
-    public ResponseError body() {
-        return ((ResponseError) super.body());
+    public StorageErrorCode errorCode() {
+        return StorageErrorCode.fromString(super.response().headers().value(Constants.HeaderConstants.ERROR_CODE));
     }
 
     /**
