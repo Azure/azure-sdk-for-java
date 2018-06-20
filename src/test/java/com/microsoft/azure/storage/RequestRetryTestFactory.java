ee89655d16a948e29f3f7f0e45f221b75a77cf84
rickle-msft <frley@microsoft.com>
2018-06-19 17:26:35 -0700

    Implemented first test scenario. Not tested.

--- src/test/java/com/microsoft/azure/storage/RequestRetryTestFactory.java
+++ src/test/java/com/microsoft/azure/storage/RequestRetryTestFactory.java
@@ -15,10 +15,18 @@
 
 package com.microsoft.azure.storage;
 
+import com.microsoft.rest.v2.http.HttpHeaders;
 import com.microsoft.rest.v2.http.HttpRequest;
 import com.microsoft.rest.v2.http.HttpResponse;
+import com.microsoft.rest.v2.http.UrlBuilder;
 import com.microsoft.rest.v2.policy.RequestPolicy;
+import com.microsoft.rest.v2.util.FlowableUtil;
+import io.reactivex.Flowable;
 import io.reactivex.Single;
+
+import java.net.MalformedURLException;
+import java.nio.ByteBuffer;
+import java.util.concurrent.TimeoutException;
 
 public class RequestRetryTestFactory {
     public static final int  RETRY_TEST_SCENARIO_RETRY_UNTIL_SUCCESS = 1;
@@ -27,9 +35,25 @@ public class RequestRetryTestFactory {
 
     public static final int RETRY_TEST_SCENARIO_RETRY_UNTIL_MAX_RETRIES = 3;
 
+    // Scenario to actually validate the waiting time and other parameters
+
+    // Scenario non retryable
+
+    // Scenario timeout the whole operation
+
+    // 400 against secondary should not retry
+
+    // return an IOException to represent a network error
+
     private static final String RETRY_TEST_PRIMARY_HOST = "PrimaryDC";
 
     private static final String RETRY_TEST_SECONDARY_HOST = "SecondaryDC";
+
+    private static final String RETRY_TEST_HEADER = "TestHeader";
+
+    private static final String RETRY_TEST_QUERY_PARAM = "TestQueryParam";
+
+    private static final ByteBuffer RETRY_TEST_DEFAULT_DATA = ByteBuffer.wrap("Default data".getBytes());
 
     private int retryTestScenario;
 
@@ -41,12 +65,11 @@ public class RequestRetryTestFactory {
         private RequestPolicy nextPolicy;
         private RequestRetryTestFactory factory;
 
-
         @Override
         public Single<HttpResponse> sendAsync(HttpRequest request) {
             this.factory.tryNumber++;
             if (this.factory.tryNumber > this.factory.maxRetries) {
-                return Single.error(new IllegalArgumentException("Try number has exceeded max tries"));
+                throw new IllegalArgumentException("Try number has exceeded max tries");
             }
 
             // Validate the expected preconditions for each try: The correct host is used.
@@ -61,27 +84,112 @@ public class RequestRetryTestFactory {
                     expectedHost = RETRY_TEST_SECONDARY_HOST;
                 }
             }
-
             if (!request.url().getPath().equals(expectedHost)) {
                 throw new IllegalArgumentException("The host does not match the expected host");
+            }
+
+            /*
+             This policy will add test headers and query parameters. Ensure they are removed/reset for each retry.
+             The retry policy should be starting with a fresh copy of the request for every try.
+             */
+            if (request.headers().value(RETRY_TEST_HEADER) != null ||
+                    request.url().getQuery().contains(RETRY_TEST_QUERY_PARAM) ||
+                    FlowableUtil.collectBytesInBuffer(request.body()).blockingGet() != RETRY_TEST_DEFAULT_DATA) {
+                throw new IllegalArgumentException("The request was not reset from the previous retry");
+            }
+
+            /*
+            Modify the request as policies downstream of the retry policy are likely to do. These must be reset on each
+            try.
+             */
+            request.headers().set(RETRY_TEST_HEADER, "testheader");
+            UrlBuilder builder = UrlBuilder.parse(request.url());
+            builder.setQueryParameter(RETRY_TEST_QUERY_PARAM, "testquery");
+            try {
+                request = request.withUrl(builder.toURL());
+            } catch (MalformedURLException e) {
+                throw new IllegalArgumentException("The URL has been mangled");
+            }
+
+            switch (this.factory.retryTestScenario) {
+                case RETRY_TEST_SCENARIO_RETRY_UNTIL_SUCCESS:
+                    switch (this.factory.tryNumber) {
+                        case 1:
+                            /*
+                            The timer is set with a timeout on the Single used to make the request. If the single
+                            doesn't return success fast enough, it will throw a TimeoutException. We can short circuit
+                            the waiting by simply returning an error. We will validate the time parameter later. Here,
+                            we just test that a timeout is retried.
+                             */
+                            return Single.error(new TimeoutException());
+                        case 2:
+                            return Single.just(new RetryTestResponse(500));
+                        case 3:
+                            return Single.just(new RetryTestResponse(503));
+                        case 4:
+                            /*
+                            By returning 404 when we should be testing against the secondary, we exercise the logic
+                            that should prevent further tries to secondary when the secondary evidently doesn't have the
+                            data.
+                             */
+                            return Single.just(new RetryTestResponse(404));
+                        case 5:
+                            // Just to get to a sixth try where we ensure we should not be trying the secondary again.
+                            return Single.just(new RetryTestResponse(500));
+                        case 6:
+                            return Single.just(new RetryTestResponse(200));
+                        default:
+                            throw new IllegalArgumentException("Continued trying after success");
+                    }
+                    break;
+                case RETRY_TEST_SCENARIO_RETRY_UNTIL_OPERATION_CANCEL:
+
+                    break;
+                case RETRY_TEST_SCENARIO_RETRY_UNTIL_MAX_RETRIES:
+
+                    break;
             }
         }
     }
 
-    private final class retryError {
-        private boolean temporary;
-        private boolean timeout;
 
-        public boolean getTemporary() {
-            return this.temporary;
+    // The retry factory only really cares about the status code.
+    private final class RetryTestResponse extends HttpResponse {
+
+        int statusCode;
+
+        RetryTestResponse(int statusCode) {
+            this.statusCode = statusCode;
         }
 
-        public boolean getTimeout() {
-            return this.temporary;
+        @Override
+        public int statusCode() {
+            return this.statusCode;
         }
 
-        public String toString() {
-            return String.format("Temporary=%b, Timeout = %b", this.temporary, this.timeout);
+        @Override
+        public String headerValue(String headerName) {
+            return null;
+        }
+
+        @Override
+        public HttpHeaders headers() {
+            return null;
+        }
+
+        @Override
+        public Flowable<ByteBuffer> body() {
+            return null;
+        }
+
+        @Override
+        public Single<byte[]> bodyAsByteArray() {
+            return null;
+        }
+
+        @Override
+        public Single<String> bodyAsString() {
+            return null;
         }
     }
 
