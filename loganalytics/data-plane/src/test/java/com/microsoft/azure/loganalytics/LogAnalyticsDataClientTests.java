cefd635e87b6855002c5e7ea82fa14e58256c85b
Ace Eldeib <aleldeib@microsoft.com>
2018-08-09 12:37:32 -0700

    update tests

--- loganalytics/data-plane/src/test/java/com/microsoft/azure/loganalytics/LogAnalyticsDataClientTests.java
+++ loganalytics/data-plane/src/test/java/com/microsoft/azure/loganalytics/LogAnalyticsDataClientTests.java
@@ -4,23 +4,32 @@ import com.microsoft.azure.AzureEnvironment;
 import com.microsoft.azure.loganalytics.implementation.LogAnalyticsDataClientImpl;
 import com.microsoft.azure.loganalytics.models.QueryBody;
 import com.microsoft.azure.loganalytics.models.QueryResults;
-import com.microsoft.azure.credentials.ApplicationTokenCredentials;
+import com.microsoft.azure.credentials.AzureTokenCredentials;
 import com.microsoft.azure.arm.core.TestBase;
 import com.microsoft.rest.RestClient;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class LogAnalyticsDataClientTests {
+public class LogAnalyticsDataClientTests extends TestBase {
     protected static LogAnalyticsDataClientImpl logAnalyticsClient;
     
-    public void initialize() {
-        ApplicationTokenCredentials credentials = new ApplicationTokenCredentials("", "", "", null);
-        logAnalyticsClient = new LogAnalyticsDataClientImpl(credentials);
+    @Override
+    protected String baseUri() {
+        return AzureEnvironment.AZURE.logAnalyticsEndpoint() + "v1/";
+    }
+
+    @Override
+    protected void initializeClients(RestClient restClient, String defaultSubscription, String domain) {
+        logAnalyticsClient = new LogAnalyticsDataClientImpl(restClient);
+    }
+
+    @Override
+    protected void cleanUpResources() {
+        return;
     }
 
     @Test
     public void firstTest() {
-        this.initialize();
         String query = "Heartbeat | take 1";
         String workspaceId = "cab864ad-d0c1-496b-bc5e-4418315621bf";
         QueryResults queryResults = logAnalyticsClient.query(workspaceId, new QueryBody().withQuery(query));
@@ -28,10 +37,11 @@ public class LogAnalyticsDataClientTests {
         
         // Query should return a single table with one row
         Assert.assertEquals(queryResults.tables().size(), 1);
+        Assert.assertEquals(queryResults.tables().get(0).rows().size(), 1);
 
         // Check type behavior on results
+        Assert.assertTrue(queryResults.tables().get(0).rows().get(0).get(1) instanceof String);
         Assert.assertTrue(queryResults.tables().get(0).rows().get(0).get(16) instanceof Double);
         Assert.assertNull(queryResults.tables().get(0).rows().get(0).get(15));
-
     }
 }
