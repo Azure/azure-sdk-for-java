4aeecd9ef87cbc1e1e54d8bd9358acf31672fdc5
Jianghao Lu <jianghaolu@users.noreply.github.com>
2018-08-30 12:35:59 -0700

    [fixes #460] Fix request body logging (#485)
    * [fixes #460] Fix request body logging
    
    * checkstyle

--- client-runtime/src/main/java/com/microsoft/rest/v2/util/FlowableUtil.java
+++ client-runtime/src/main/java/com/microsoft/rest/v2/util/FlowableUtil.java
@@ -7,7 +7,6 @@
 package com.microsoft.rest.v2.util;
 
 import com.microsoft.rest.v2.http.UnexpectedLengthException;
-import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.reactivex.Completable;
 import io.reactivex.Flowable;
@@ -55,7 +54,7 @@ public final class FlowableUtil {
      * @return A Single which emits the concatenation of all the byte buffers given by the source Flowable.
      */
     public static Single<byte[]> collectBytesInArray(Flowable<ByteBuffer> content) {
-        return content.collectInto(Unpooled.buffer(), ByteBuf::writeBytes).map(out -> {
+        return content.collectInto(Unpooled.buffer(), (buf, bb) -> buf.writeBytes(bb.slice())).map(out -> {
             try {
                 if (out.array().length == out.readableBytes()) {
                     return out.array();
