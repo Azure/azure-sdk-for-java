9910420e2b21fed63a10d3479e4e4506a91949ca
Ace Eldeib <alexeldeib@users.noreply.github.com>
2018-10-01 16:34:51 -0700

    App Insights readme and sample (#2442)
    * add App insights java info back
    
    * Migrate example and remove JDK 8 features.
    
    * fix bugs and pom
    
    * change package, add disclaimer.

--- applicationinsights/data-plane/src/test/java/com/microsoft/azure/applicationinsights/query/ApplicationInsightsDataClientTests.java
+++ applicationinsights/data-plane/src/test/java/com/microsoft/azure/applicationinsights/query/ApplicationInsightsDataClientTests.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ *
+ */
 package com.microsoft.azure.applicationinsights.query;
 
 import java.util.ArrayList;
@@ -59,7 +65,6 @@ public class ApplicationInsightsDataClientTests extends TestBase {
         MetricsResult metricResult = applicationInsightsClient.metrics().get(appId, MetricId.AVAILABILITY_RESULTSAVAILABILITY_PERCENTAGE);
         // Validate properties
         Assert.assertNotNull(metricResult.value().additionalProperties());
-        System.out.println(metricResult.value().additionalProperties().keySet());
         Assert.assertNotNull(metricResult.value().start());
         Assert.assertTrue(metricResult.value().start() instanceof DateTime);
     }
