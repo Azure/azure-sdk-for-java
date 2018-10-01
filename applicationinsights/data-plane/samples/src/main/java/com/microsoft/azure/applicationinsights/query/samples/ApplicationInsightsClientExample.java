2c8c9389b830cd142c055325010d00bca6a2fc22
Ace Eldeib <aleldeib@microsoft.com>
2018-10-01 15:41:08 -0700

    change package, add disclaimer.

--- applicationinsights/data-plane/samples/src/main/java/com/microsoft/azure/applicationinsights/query/samples/ApplicationInsightsClientExample.java
+++ applicationinsights/data-plane/samples/src/main/java/com/microsoft/azure/applicationinsights/query/samples/ApplicationInsightsClientExample.java
@@ -1,4 +1,10 @@
-package com.demo.app;
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ *
+ */
+package com.microsoft.azure.applicationinsights.query.samples;
 
 import java.util.ArrayList;
 import java.util.List;
