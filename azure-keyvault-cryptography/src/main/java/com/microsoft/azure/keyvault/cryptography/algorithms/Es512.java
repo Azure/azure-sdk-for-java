6ba1730bf5afe4cf7ea972f86b9e99d08bbc2097
Scott Schaab <sschaab@microsoft.com>
2018-11-16 17:08:48 -0800

    Fix to make local ECDSA signature encoding compatible with service signature encoding   - Added static class SignatureEncoding to covert between ASN.1 DER encoded ECDSA signatures and raw signatures   - FUNCTIONALLY BREAKING CHANGE:  Signatures created with local ECDSA keys from previous versions os     azure-keyvault-cryptography will no longer verify properly on newer versions of azure-keyvault-cryptography.     They must first be decoded using the method SignatureEncoding.fromAsn1Der

--- azure-keyvault-cryptography/src/main/java/com/microsoft/azure/keyvault/cryptography/algorithms/Es512.java
+++ azure-keyvault-cryptography/src/main/java/com/microsoft/azure/keyvault/cryptography/algorithms/Es512.java
@@ -2,12 +2,15 @@ package com.microsoft.azure.keyvault.cryptography.algorithms;
 
 public class Es512 extends Ecdsa {
     
-    public final static String ALGORITHM_NAME = "SHA512withECDSA";
+    public final static String ALGORITHM_NAME = "ES512";
 
     @Override
-    public void checkDigestLength(byte[] digest) {
-        if (digest.length != 64) {
-            throw new IllegalArgumentException("Invalid digest length.");
-        }
+    public int getDigestLength() {
+        return 64;
+    }
+
+    @Override
+    public int getCoordLength() {
+        return 66;
     }
 }
