c3c4e885b099b43926a032297e02a3f91cd8b1ab
vgera <vvinaygera@gmail.com>
2019-01-10 13:25:01 -0800

    Updated Test recordings

--- keyvault/data-plane/azure-keyvault/src/test/java/com/microsoft/azure/keyvault/test/AsyncOperationsTest.java
+++ keyvault/data-plane/azure-keyvault/src/test/java/com/microsoft/azure/keyvault/test/AsyncOperationsTest.java
@@ -84,7 +84,7 @@ public class AsyncOperationsTest extends KeyVaultClientIntegrationTestBase {
 		keyVaultClient.deleteKeyAsync(keyBundle.keyIdentifier().vault(), keyBundle.keyIdentifier().name(), null).get();
 		pollOnKeyDeletion(keyBundle.keyIdentifier().vault(), keyBundle.keyIdentifier().name());
 		keyVaultClient.purgeDeletedKey(keyBundle.keyIdentifier().vault(), keyBundle.keyIdentifier().name());
-		SdkContext.sleep(20000);
+		SdkContext.sleep(40000);
 
 		KeyBundle restoreResult = keyVaultClient.restoreKeyAsync(vault, backupResult.value(), null).get();
 		Assert.assertNotNull(restoreResult);
@@ -187,7 +187,7 @@ public class AsyncOperationsTest extends KeyVaultClientIntegrationTestBase {
 	public void certificateAsyncForAsyncOperationsTest() throws Exception {
 
 		String vault = getVaultUri();
-		String certificateName = "myCertificate2";
+		String certificateName = "tempCertificate2";
 
 		CreateCertificateRequest createCertificateRequest = new CreateCertificateRequest.Builder(vault, certificateName)
 				.withPolicy(new CertificatePolicy()
