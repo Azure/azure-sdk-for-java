cc1a7d820472e3711eab5f4895a714632f84fe4a
Connie <conniey@microsoft.com>
2019-02-11 13:28:08 -0800

    Remove spaces before/after ( ).

--- keyvault/data-plane/azure-keyvault-cryptography/src/main/java/com/microsoft/azure/keyvault/cryptography/algorithms/Rs256.java
+++ keyvault/data-plane/azure-keyvault-cryptography/src/main/java/com/microsoft/azure/keyvault/cryptography/algorithms/Rs256.java
@@ -29,7 +29,7 @@ public class Rs256 extends RsaSignature {
 
             BigInteger modulus = ((RSAPublicKey)_keyPair.getPublic()).getModulus();
 
-            _emLen    = getOctetLength( modulus.bitLength() );
+            _emLen    = getOctetLength(modulus.bitLength());
 
         }
 
@@ -38,7 +38,7 @@ public class Rs256 extends RsaSignature {
             // Signing isn't just a case of encrypting the digest, there is much more to do.
             // For details of the algorithm, see https://tools.ietf.org/html/rfc3447#section-8.2
 
-            if ( _keyPair.getPrivate() == null ) {
+            if (_keyPair.getPrivate() == null) {
                 // TODO
             }
 
@@ -52,14 +52,14 @@ public class Rs256 extends RsaSignature {
             s = RSASP1((RSAPrivateKey)_keyPair.getPrivate(), s);
 
             // Convert to octet sequence
-            return I2OSP(s, _emLen );
+            return I2OSP(s, _emLen);
         }
 
         @Override
         public boolean verify(byte[] digest, byte[] signature) throws NoSuchAlgorithmException {
 
-            if ( signature.length != _emLen ) {
-                throw new IllegalArgumentException( "invalid signature length");
+            if (signature.length != _emLen) {
+                throw new IllegalArgumentException("invalid signature length");
             }
 
             // Convert to integer signature
@@ -68,7 +68,7 @@ public class Rs256 extends RsaSignature {
             // Convert integer message
             BigInteger m = RSAVP1((RSAPublicKey)_keyPair.getPublic(), s);
 
-            byte[] EM  = I2OSP(m, _emLen );
+            byte[] EM  = I2OSP(m, _emLen);
             byte[] EM2 = EMSA_PKCS1_V1_5_ENCODE_HASH(digest, _emLen, "SHA-256");
 
             // Use constant time compare
--- keyvault/data-plane/azure-keyvault-cryptography/src/main/java/com/microsoft/azure/keyvault/cryptography/algorithms/RsaSignature.java
+++ keyvault/data-plane/azure-keyvault-cryptography/src/main/java/com/microsoft/azure/keyvault/cryptography/algorithms/RsaSignature.java
@@ -25,7 +25,7 @@ public abstract class RsaSignature extends AsymmetricSignatureAlgorithm {
     }
 
     protected int getOctetLength(int bits) {
-        return ( bits % 8 > 0 ) ? bits >> 3 + 1 : bits >> 3;
+        return (bits % 8 > 0) ? bits >> 3 + 1 : bits >> 3;
     }
 
     /*
@@ -33,7 +33,7 @@ public abstract class RsaSignature extends AsymmetricSignatureAlgorithm {
      */
     protected BigInteger OS2IP(byte[] x) {
 
-        if ( x == null || x.length == 0 ) {
+        if (x == null || x.length == 0) {
             throw new IllegalArgumentException("x");
         }
 
@@ -45,15 +45,15 @@ public abstract class RsaSignature extends AsymmetricSignatureAlgorithm {
      */
     protected byte[] I2OSP(BigInteger x, int xLen) {
 
-        if ( x == null ) {
+        if (x == null) {
             throw new IllegalArgumentException("x");
         }
 
-        if ( xLen <= 0 ) {
+        if (xLen <= 0) {
             throw new IllegalArgumentException("xLen");
         }
 
-        if ( x.compareTo( twoFiveSix.pow(xLen) ) == 1 ) {
+        if (x.compareTo(twoFiveSix.pow(xLen)) == 1) {
             throw new IllegalArgumentException("integer too large");
         }
 
@@ -66,7 +66,7 @@ public abstract class RsaSignature extends AsymmetricSignatureAlgorithm {
             bytes = bigEndianBytes;
         }
 
-        if ( bytes.length > xLen ) {
+        if (bytes.length > xLen) {
             throw new IllegalArgumentException("integer too large");
         }
 
@@ -82,18 +82,18 @@ public abstract class RsaSignature extends AsymmetricSignatureAlgorithm {
      */
     protected BigInteger RSASP1(RSAPrivateKey K, BigInteger m) {
 
-        if ( K == null ) {
+        if (K == null) {
             throw new IllegalArgumentException("K");
         }
 
-        if ( m == null ) {
+        if (m == null) {
             throw new IllegalArgumentException("m");
         }
 
         BigInteger n = K.getModulus();
         BigInteger d = K.getPrivateExponent();
 
-        if ( m.compareTo(BigInteger.ONE) == -1 || m.compareTo(n) != -1 ) {
+        if (m.compareTo(BigInteger.ONE) == -1 || m.compareTo(n) != -1) {
             throw new IllegalArgumentException("message representative out of range");
         }
 
@@ -105,17 +105,17 @@ public abstract class RsaSignature extends AsymmetricSignatureAlgorithm {
      */
     protected BigInteger RSAVP1(RSAPublicKey K, BigInteger s) {
 
-        if ( K == null ) {
+        if (K == null) {
             throw new IllegalArgumentException("K");
         }
 
-        if ( s == null ) {
+        if (s == null) {
             throw new IllegalArgumentException("s");
         }
         BigInteger n = K.getModulus();
         BigInteger e = K.getPublicExponent();
 
-        if ( s.compareTo(BigInteger.ONE) == -1 || s.compareTo(n) != -1 ) {
+        if (s.compareTo(BigInteger.ONE) == -1 || s.compareTo(n) != -1) {
             throw new IllegalArgumentException("message representative out of range");
         }
 
@@ -128,19 +128,19 @@ public abstract class RsaSignature extends AsymmetricSignatureAlgorithm {
     protected byte[] EMSA_PKCS1_V1_5_ENCODE(byte[] m, int emLen, String algorithm) throws NoSuchAlgorithmException {
 
         // Check m
-        if ( m == null || m.length == 0 ) {
+        if (m == null || m.length == 0) {
             throw new IllegalArgumentException("m");
         }
 
         MessageDigest messageDigest   = null;
 
         // Check algorithm
-        if ( Strings.isNullOrWhiteSpace(algorithm) ) {
+        if (Strings.isNullOrWhiteSpace(algorithm)) {
             throw new IllegalArgumentException("algorithm");
         }
 
         // Only supported algorithms
-        if ( algorithm.equals("SHA-256") ) {
+        if (algorithm.equals("SHA-256")) {
 
             // Initialize digest
             messageDigest   = MessageDigest.getInstance("SHA-256");
@@ -161,24 +161,24 @@ public abstract class RsaSignature extends AsymmetricSignatureAlgorithm {
     protected byte[] EMSA_PKCS1_V1_5_ENCODE_HASH(byte[] h, int emLen, String algorithm) throws NoSuchAlgorithmException {
 
         // Check m
-        if ( h == null || h.length == 0 ) {
+        if (h == null || h.length == 0) {
             throw new IllegalArgumentException("m");
         }
 
         byte[] algorithmPrefix = null;
 
         // Check algorithm
-        if ( Strings.isNullOrWhiteSpace(algorithm) ) {
+        if (Strings.isNullOrWhiteSpace(algorithm)) {
             throw new IllegalArgumentException("algorithm");
         }
 
         // Only supported algorithms
-        if ( algorithm.equals("SHA-256") ) {
+        if (algorithm.equals("SHA-256")) {
 
             // Initialize prefix and digest
             algorithmPrefix = sha256Prefix;
 
-            if ( h.length != 32 ) {
+            if (h.length != 32) {
                 throw new IllegalArgumentException("h is incorrect length for SHA-256");
             }
         } else {
@@ -192,14 +192,14 @@ public abstract class RsaSignature extends AsymmetricSignatureAlgorithm {
         System.arraycopy(algorithmPrefix, 0, T, 0, algorithmPrefix.length);
         System.arraycopy(h, 0, T, algorithmPrefix.length, h.length);
 
-        if ( emLen < T.length + 11 ) {
+        if (emLen < T.length + 11) {
             throw new IllegalArgumentException("intended encoded message length too short");
         }
 
         // Construct PS
         byte[] PS = new byte[emLen - T.length - 3];
 
-        for ( int i = 0; i < PS.length; i++ ) PS[i] = (byte) 0xff;
+        for (int i = 0; i < PS.length; i++) PS[i] = (byte) 0xff;
 
         // Construct EM
         byte[] EM = new byte[PS.length + T.length + 3];
