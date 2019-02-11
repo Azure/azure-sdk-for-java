1eb73931e6f40f27a9a3dd16949b99f1e625a889
Connie <conniey@microsoft.com>
2019-02-11 11:20:43 -0800

    Remove tabs. Trim whitepsace.

--- keyvault/data-plane/azure-keyvault-cryptography/src/main/java/com/microsoft/azure/keyvault/cryptography/algorithms/Es256k.java
+++ keyvault/data-plane/azure-keyvault-cryptography/src/main/java/com/microsoft/azure/keyvault/cryptography/algorithms/Es256k.java
@@ -2,7 +2,7 @@ package com.microsoft.azure.keyvault.cryptography.algorithms;
 
 public class Es256k extends Ecdsa {
     public final static String ALGORITHM_NAME = "ES256K";
-	
+
     @Override
     public int getDigestLength() {
         return 32;
