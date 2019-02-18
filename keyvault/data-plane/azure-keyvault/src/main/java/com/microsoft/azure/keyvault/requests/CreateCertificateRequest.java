3acdfdb9f33780544269de36ca3a0a8888441cdf
Connie <conniey@microsoft.com>
2019-02-17 22:36:54 -0800

    Fixing possible invalid cast

--- keyvault/data-plane/azure-keyvault/src/main/java/com/microsoft/azure/keyvault/requests/CreateCertificateRequest.java
+++ keyvault/data-plane/azure-keyvault/src/main/java/com/microsoft/azure/keyvault/requests/CreateCertificateRequest.java
@@ -15,7 +15,6 @@ import com.microsoft.azure.keyvault.models.KeyProperties;
 import com.microsoft.azure.keyvault.models.LifetimeAction;
 import com.microsoft.azure.keyvault.models.SecretProperties;
 import com.microsoft.azure.keyvault.models.X509CertificateProperties;
-import com.sun.javaws.exceptions.InvalidArgumentException;
 
 /**
  * The create certificate request class.
@@ -75,7 +74,7 @@ public final class CreateCertificateRequest {
          * @return the Builder object itself.
          */
         public Builder withAttributes(Attributes attributes) {
-            if (!attributes.getClass().isAssignableFrom(CertificateAttributes.class)) {
+            if (!(attributes instanceof CertificateAttributes)) {
                 throw new IllegalArgumentException("attributes");
             }
 
