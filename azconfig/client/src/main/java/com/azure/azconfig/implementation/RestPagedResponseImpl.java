213941f09f655c802032f72a3c953358632cdbbd
lenala <lena.lakhno@gmail.com>
2019-02-26 15:35:19 -0800

    Updating license header for AzConfig

--- azconfig/client/src/main/java/com/azure/azconfig/implementation/RestPagedResponseImpl.java
+++ azconfig/client/src/main/java/com/azure/azconfig/implementation/RestPagedResponseImpl.java
@@ -1,8 +1,5 @@
-/**
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for
- * license information.
- */
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 package com.azure.azconfig.implementation;
 
 import com.microsoft.rest.v3.RestPagedResponse;
@@ -34,7 +31,7 @@ public class RestPagedResponseImpl<T> implements RestPagedResponse<T> {
 
     private int statusCode;
 
-    public RestPagedResponseImpl( final List<T> items, final String nextPageLink, final HttpRequest request, final Map<String, String> headers, final int statusCode) {
+    public RestPagedResponseImpl(final List<T> items, final String nextPageLink, final HttpRequest request, final Map<String, String> headers, final int statusCode) {
         this.nextPageLink = nextPageLink;
         this.items = items;
         this.request = request;
