c11b2c1a3aac5459aa6fdbadcead876694cb15af
Connie Yau <conniey@microsoft.com>
2019-03-06 13:30:44 -0800

    Renames com.azure.azconfig -> com.azure.applicationconfig (#3044)
    * Replace package namespace with com.azure.applicationconfig
    
    * Move files to applicationconfig/com/azure/applicationconfig
    
    * Remove unused import from AzConfigClient
    
    * Using HttpPipelineNextPolicy

---
+++ applicationconfig/client/src/main/java/com/azure/applicationconfig/models/LabelFilters.java
@@ -0,0 +1,18 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+package com.azure.applicationconfig.models;
+
+/**
+ * Label filters.
+ */
+public class LabelFilters {
+    /**
+     * Same as 'null' label.
+     */
+    public static final String NULL = "\0";
+    /**
+     * Matches any label.
+     */
+    public static final String ANY = "*";
+}
+
