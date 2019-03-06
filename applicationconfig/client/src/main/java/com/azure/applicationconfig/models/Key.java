18fc6f63dd2602a51aef09098a9216dac83060c0
Connie Yau <conniey@microsoft.com>
2019-03-06 13:30:44 -0800

    Renames com.azure.azconfig -> com.azure.applicationconfig (#3044)
    * Replace package namespace with com.azure.applicationconfig
    
    * Move files to applicationconfig/com/azure/applicationconfig
    
    * Remove unused import from AzConfigClient
    
    * Using HttpPipelineNextPolicy

---
+++ applicationconfig/client/src/main/java/com/azure/applicationconfig/models/Key.java
@@ -0,0 +1,92 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+package com.azure.applicationconfig.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.OffsetDateTime;
+
+/**
+ * Keys that exist in the configuration store.
+ */
+public class Key {
+    @JsonProperty(value = "name")
+    private String name;
+
+    @JsonProperty(value = "kv_count")
+    private int kvCount;
+
+    @JsonProperty(value = "etag")
+    private String etag;
+
+    @JsonProperty(value = "last_modified")
+    private OffsetDateTime lastModified;
+
+    /**
+     * @return label name
+     */
+    public String name() {
+        return name;
+    }
+
+    /**
+     * Sets the name.
+     * @param name key name
+     * @return Key object itself
+     */
+    public Key withName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    /**
+     * @return number of KeyValue's associated with this key
+     */
+    public int kvCount() {
+        return kvCount;
+    }
+
+    /**
+     * Sets the number of KeyValue's associated with this key
+     * @param kvCount number of KeyValue's
+     * @return Key object itself
+     */
+    public Key withKvCount(int kvCount) {
+        this.kvCount = kvCount;
+        return this;
+    }
+
+    /**
+     * @return etag
+     */
+    public String etag() {
+        return etag;
+    }
+
+    /**
+     * Sets the etag.
+     * @param etag etag
+     * @return Key object itself
+     */
+    public Key withEtag(String etag) {
+        this.etag = etag;
+        return this;
+    }
+
+    /**
+     * @return the time when last modified
+     */
+    public OffsetDateTime lastModified() {
+        return lastModified;
+    }
+
+    /**
+     * Sets when Key was last modified.
+     * @param lastModified lastModified
+     * @return Key object itself
+     */
+    public Key withLastModified(OffsetDateTime lastModified) {
+        this.lastModified = lastModified;
+        return this;
+    }
+}
--- azconfig/client/src/main/java/com/azure/azconfig/models/package-info.java
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See License.txt in the project root for
-// license information.
-/**
- * This package contains classes for AzConfig Service SDK.
- */
-package com.azure.azconfig.models;
