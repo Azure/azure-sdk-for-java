03e37c8039350a9d851b93a87612de69e2c86404
shafang <Shawn.Fang@microsoft.com>
2019-03-14 10:02:21 -0700

    refactor(SpotBugs): import static

--- eventhubs/data-plane/azure-eventhubs/src/main/java/com/microsoft/azure/eventhubs/impl/MessageSender.java
+++ eventhubs/data-plane/azure-eventhubs/src/main/java/com/microsoft/azure/eventhubs/impl/MessageSender.java
@@ -34,7 +34,6 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.BufferOverflowException;
-import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.time.Duration;
@@ -54,6 +53,8 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.RejectedExecutionException;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 /**
  * Abstracts all amqp related details
@@ -521,7 +522,7 @@ public final class MessageSender extends ClientEntity implements AmqpSender, Err
     @Override
     public void onSendComplete(final Delivery delivery) {
         final DeliveryState outcome = delivery.getRemoteState();
-        final String deliveryTag = new String(delivery.getTag(), StandardCharsets.UTF_8);
+        final String deliveryTag = new String(delivery.getTag(), UTF_8);
 
         if (TRACE_LOGGER.isTraceEnabled())
             TRACE_LOGGER.trace(
@@ -837,7 +838,7 @@ public final class MessageSender extends ClientEntity implements AmqpSender, Err
                 Exception sendException = null;
 
                 try {
-                    delivery = this.sendLink.delivery(deliveryTag.getBytes(StandardCharsets.UTF_8));
+                    delivery = this.sendLink.delivery(deliveryTag.getBytes(UTF_8));
                     delivery.setMessageFormat(sendData.getMessageFormat());
 
                     sentMsgSize = this.sendLink.send(sendData.getMessage(), 0, sendData.getEncodedMessageSize());
--- eventhubs/data-plane/azure-eventhubs/src/main/java/com/microsoft/azure/eventhubs/impl/SendLinkHandler.java
+++ eventhubs/data-plane/azure-eventhubs/src/main/java/com/microsoft/azure/eventhubs/impl/SendLinkHandler.java
@@ -10,8 +10,9 @@ import org.apache.qpid.proton.engine.Sender;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.nio.charset.StandardCharsets;
 import java.util.Locale;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class SendLinkHandler extends BaseLinkHandler {
     private static final Logger TRACE_LOGGER = LoggerFactory.getLogger(SendLinkHandler.class);
@@ -72,7 +73,7 @@ public class SendLinkHandler extends BaseLinkHandler {
                 TRACE_LOGGER.trace(
                         "onDelivery linkName[" + sender.getName()
                                 + "], unsettled[" + sender.getUnsettled() + "], credit[" + sender.getRemoteCredit() + "], deliveryState[" + delivery.getRemoteState()
-                                + "], delivery.isBuffered[" + delivery.isBuffered() + "], delivery.id[" + new String(delivery.getTag(), StandardCharsets.UTF_8) + "]");
+                                + "], delivery.isBuffered[" + delivery.isBuffered() + "], delivery.id[" + new String(delivery.getTag(), UTF_8) + "]");
             }
 
             msgSender.onSendComplete(delivery);
