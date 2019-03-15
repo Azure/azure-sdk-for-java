d3c2fcda41138e85ffe243d702bfdb4f82e37c1c
Connie Yau <conniey@microsoft.com>
2019-03-14 21:14:45 -0700

    Rename classes to fit App Configuration names (#3121)
    * Rename AzConfigClient -> ConfigurationClient
    
    * Rename KeyValue -> ConfigurationSetting
    
    * Rename AzConfigTest -> ConfigurationClientTest
    
    * Rename AzConfigCredentialsPolicy -> ConfigurationCredentialsPolicy
    
    * Remove unused RequestRetryPolicyFactory

--- applicationconfig/client/src/main/java/com/azure/applicationconfig/package-info.java
+++ applicationconfig/client/src/main/java/com/azure/applicationconfig/package-info.java
@@ -2,6 +2,6 @@
 // Licensed under the MIT License. See License.txt in the project root for
 // license information.
 /**
- * This package contains the classes for AzConfigClient.
+ * This package contains the classes for ConfigurationClient.
  */
 package com.azure.applicationconfig;
