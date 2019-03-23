bb812fff6e5317e3a829104a560e9bcee59834de
Connie <conniey@microsoft.com>
2019-03-23 00:47:54 -0700

    Adding documentation to RequestOptions

--- applicationconfig/client/src/main/java/com/azure/applicationconfig/models/RequestOptions.java
+++ applicationconfig/client/src/main/java/com/azure/applicationconfig/models/RequestOptions.java
@@ -4,17 +4,32 @@ package com.azure.applicationconfig.models;
 
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.EnumSet;
 
 /**
- * Filtering options for Get- requests.
+ * Represents a set of request options for querying App Configuration.
+ *
+ * Providing {@link RequestOptions#label()} will filter {@link ConfigurationSetting}s that match that label name in
+ * conjunction with the key that is passed in to the service request.
+ *
+ * Providing {@link RequestOptions#acceptDateTime()} will return the representation of matching {@link ConfigurationSetting}
+ * at that given {@link OffsetDateTime}.
+ *
+ * Providing {@link RequestOptions#fields()} will populate only those {@link ConfigurationSetting} fields in the response.
  */
-public abstract class KeyValueGenericFilter<T> {
+public abstract class RequestOptions<T extends RequestOptions> {
     private String label;
-    private String fields;
+    private EnumSet<ConfigurationSettingFields> fields;
     private String acceptDatetime;
 
+    protected RequestOptions() {
+        fields = EnumSet.of(ConfigurationSettingFields.DEFAULT);
+    }
+
     /**
-     * @return label
+     * The label used to filter settings based on their {@link ConfigurationSetting#label()} in the service.
+     *
+     * @return label The label used to filter GET requests from the service.
      */
     public String label() {
         return label;
@@ -23,10 +38,10 @@ public abstract class KeyValueGenericFilter<T> {
     /**
      * Sets specific label of the key.
      * @param label the label
-     * @return KeyValueGenericFilter object itself
+     * @return RequestOptions object itself
      */
     @SuppressWarnings("unchecked")
-    public T withLabel(String label) {
+    public T label(String label) {
         this.label = label;
         return (T) this;
     }
@@ -41,10 +56,10 @@ public abstract class KeyValueGenericFilter<T> {
     /**
      * If set, then key values will be retrieved exactly as they existed at the provided time.
      * @param datetime the preferredDateTime
-     * @return KeyValueGenericFilter object itself
+     * @return RequestOptions object itself
      */
     @SuppressWarnings("unchecked")
-    public T withAcceptDatetime(OffsetDateTime datetime) {
+    public T acceptDatetime(OffsetDateTime datetime) {
         this.acceptDatetime = DateTimeFormatter.RFC_1123_DATE_TIME.toFormat().format(datetime);
         return (T) this;
     }
@@ -52,19 +67,18 @@ public abstract class KeyValueGenericFilter<T> {
     /**
      * @return fields
      */
-    public String fields() {
+    public EnumSet<ConfigurationSettingFields> fields() {
         return fields;
     }
 
     /**
-     * Sets fiels that will be returned in the response.
+     * Sets fields that will be returned in the response.
      * @param fields the fields to select
-     * @return KeyValueGenericFilter object itself
+     * @return RequestOptions object itself
      */
     @SuppressWarnings("unchecked")
-    public T withFields(String fields) {
+    public T fields(EnumSet<ConfigurationSettingFields> fields) {
         this.fields = fields;
         return (T) this;
     }
-
 }
