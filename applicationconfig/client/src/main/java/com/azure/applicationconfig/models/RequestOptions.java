5f02fe67610b90fd15cc283bb760e0b32e48a95a
Connie <conniey@microsoft.com>
2019-03-25 11:27:59 -0700

    Adding documentation for RequestOptions

--- applicationconfig/client/src/main/java/com/azure/applicationconfig/models/RequestOptions.java
+++ applicationconfig/client/src/main/java/com/azure/applicationconfig/models/RequestOptions.java
@@ -17,17 +17,18 @@ import java.util.EnumSet;
  *
  * Providing {@link RequestOptions#fields()} will populate only those {@link ConfigurationSetting} fields in the response.
  */
-public abstract class RequestOptions<T extends RequestOptions> {
+public class RequestOptions {
     private String label;
     private EnumSet<ConfigurationSettingFields> fields;
     private String acceptDatetime;
 
-    protected RequestOptions() {
+    public RequestOptions() {
         fields = EnumSet.of(ConfigurationSettingFields.DEFAULT);
     }
 
     /**
-     * The label used to filter settings based on their {@link ConfigurationSetting#label()} in the service.
+     * The label used to filter settings based on their {@link ConfigurationSetting#label()} in the service. If the
+     * value is {@code null} or {@code ""}, all ConfigurationSettings will be returned regardless of their label.
      *
      * @return label The label used to filter GET requests from the service.
      */
@@ -36,18 +37,24 @@ public abstract class RequestOptions<T extends RequestOptions> {
     }
 
     /**
-     * Sets specific label of the key.
-     * @param label the label
-     * @return RequestOptions object itself
+     * Sets the query to match {@link ConfigurationSetting#label()}s in the service.
+     *
+     * @param label The ConfigurationSetting label to match. If the provided value is {@code null} or {@code ""}, all
+     *              ConfigurationSettings will be returned regardless of their label.
+     * @return RequestOptions The updated RequestOptions object.
      */
-    @SuppressWarnings("unchecked")
-    public T label(String label) {
+    public RequestOptions label(String label) {
         this.label = label;
-        return (T) this;
+        return this;
     }
 
     /**
-     * @return preferredDateTime
+     * Gets the date time for the request query. When the query is performed, the state of the
+     * {@link ConfigurationSetting}s at that point in time is returned based on the provided acceptDateTime.
+     *
+     * If the value is not set, then the {@link ConfigurationSetting}s at their current state is returned.
+     *
+     * @return Gets the currently set datetime in {@link DateTimeFormatter#RFC_1123_DATE_TIME} format.
      */
     public String acceptDateTime() {
         return this.acceptDatetime;
@@ -55,30 +62,34 @@ public abstract class RequestOptions<T extends RequestOptions> {
 
     /**
      * If set, then key values will be retrieved exactly as they existed at the provided time.
-     * @param datetime the preferredDateTime
-     * @return RequestOptions object itself
+     *
+     * @param datetime The value of the configuration setting at that given {@link OffsetDateTime}.
+     * @return The updated RequestOptions object.
      */
-    @SuppressWarnings("unchecked")
-    public T acceptDatetime(OffsetDateTime datetime) {
+    public RequestOptions acceptDatetime(OffsetDateTime datetime) {
         this.acceptDatetime = DateTimeFormatter.RFC_1123_DATE_TIME.toFormat().format(datetime);
-        return (T) this;
+        return this;
     }
 
     /**
-     * @return fields
+     * Gets the fields on {@link ConfigurationSetting} to return from the GET request. If none are set, the service
+     * returns the ConfigurationSettings with a default set of properties populated.
+     *
+     * @return The set of {@link ConfigurationSetting} fields to return for a GET request.
      */
     public EnumSet<ConfigurationSettingFields> fields() {
         return fields;
     }
 
     /**
-     * Sets fields that will be returned in the response.
-     * @param fields the fields to select
-     * @return RequestOptions object itself
+     * Sets fields that will be returned in the response corresponding to properties in {@link ConfigurationSetting}.
+     *
+     * @param fields the fields to select for the query response. If none are set, the service will return the
+     *               ConfigurationSettings with a default set of properties.
+     * @return The updated RequestOptions object.
      */
-    @SuppressWarnings("unchecked")
-    public T fields(EnumSet<ConfigurationSettingFields> fields) {
+    public RequestOptions fields(EnumSet<ConfigurationSettingFields> fields) {
         this.fields = fields;
-        return (T) this;
+        return this;
     }
 }
