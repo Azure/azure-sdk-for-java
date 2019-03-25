6e48b5b1ac70630ad53dfc5c05f96c58b9686b13
Connie <conniey@microsoft.com>
2019-03-25 13:33:21 -0700

    Adding RevisionRange so users can select a subset of ConfigurationSetting revisions

--- applicationconfig/client/src/main/java/com/azure/applicationconfig/models/RevisionFilter.java
+++ applicationconfig/client/src/main/java/com/azure/applicationconfig/models/RevisionFilter.java
@@ -2,58 +2,27 @@
 // Licensed under the MIT License.
 package com.azure.applicationconfig.models;
 
-import java.time.OffsetDateTime;
-import java.time.format.DateTimeFormatter;
+public class RevisionFilter extends RequestOptions {
+    private RevisionRange range;
 
-public class RevisionFilter {
-    private String key;
-    private String label;
-    private String fields;
-    private String range;
-    private String acceptDatetime;
+    /**
+     * Gets the range of {@link ConfigurationSetting} revisions to fetch from the service. If none is specified, it will
+     * return all of the revisions of that setting.
+     *
+     * @return The range of revisions for a ConfigurationSetting to fetch.
+     */
+    public RevisionRange range() { return this.range; }
 
-    public RevisionFilter withKey(String key) {
-        this.key = key;
-        return this;
-    }
+    /**
+     * Sets the range of {@link ConfigurationSetting} revisions to fetch from the service. If none is specified, all of
+     * the revisions will be returned.
+     *
+     * @param range The range of revisions for a ConfigurationSetting to fetch.
+     * @return The updated RevisionFilter object.
+     */
+    public RevisionFilter range(RevisionRange range) {
 
-    public String key() {
-        return key;
-    }
-
-    public RevisionFilter withLabel(String label) {
-        this.label = label;
-        return this;
-    }
-
-    public String label() {
-        return label;
-    }
-
-    public RevisionFilter withFields(String fields) {
-        this.fields = fields;
-        return this;
-    }
-
-    public String fields() {
-        return fields;
-    }
-
-    public RevisionFilter withRange(String range) {
         this.range = range;
         return this;
-    }
-
-    public String range() {
-        return range;
-    }
-
-    public RevisionFilter withAcceptDatetime(OffsetDateTime datetime) {
-        this.acceptDatetime = DateTimeFormatter.RFC_1123_DATE_TIME.toFormat().format(datetime);
-        return this;
-    }
-
-    public String acceptDatetime() {
-        return acceptDatetime;
     }
 }
---
+++ applicationconfig/client/src/main/java/com/azure/applicationconfig/models/RevisionRange.java
@@ -0,0 +1,44 @@
+package com.azure.applicationconfig.models;
+
+public class RevisionRange {
+    private final Integer start;
+    private final Integer end;
+
+    /**
+     * Creates a range of revisions starting from {@param start} onwards.
+     *
+     * @param start The revision number to start fetching data from.
+     * @throws IllegalArgumentException If {@code start} is less than 0.
+     */
+    public RevisionRange(int start) {
+        if (start < 0) {
+            throw new IllegalArgumentException("'start' cannot be less than 0.");
+        }
+
+        this.start = start;
+        this.end = null;
+    }
+
+    /**
+     * Creates a range of revisions to fetch {@link ConfigurationSetting}s starting from {@param start} until
+     * {@param end}.
+     *
+     * @param start The revision number to start fetching {@link ConfigurationSetting} changes.
+     * @param end   The revision number to stop fetching {@link ConfigurationSetting} changes.
+     * @throws IllegalArgumentException If {@code start} is less than 0 or {@code end} is less than 0.
+     */
+    public RevisionRange(int start, int end) {
+        if (start < 0) {
+            throw new IllegalArgumentException("'start' cannot be less than 0.");
+        } else if (end < 0) {
+            throw new IllegalArgumentException("'end' cannot be less than 0.");
+        }
+
+        this.start = start;
+        this.end = end;
+    }
+
+    public Integer start() { return start; }
+
+    public Integer end() { return end; }
+}
