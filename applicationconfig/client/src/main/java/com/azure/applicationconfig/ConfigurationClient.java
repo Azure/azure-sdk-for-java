a4a23e2f9b8b5a6d3a57a65c731fad631665a9dd
Connie <conniey@microsoft.com>
2019-03-25 15:00:53 -0700

    Add null check for enums

--- applicationconfig/client/src/main/java/com/azure/applicationconfig/ConfigurationClient.java
+++ applicationconfig/client/src/main/java/com/azure/applicationconfig/ConfigurationClient.java
@@ -447,7 +447,7 @@ public final class ConfigurationClient extends ServiceClient {
     }
 
     private static String getSelectQuery(EnumSet<ConfigurationSettingFields> set) {
-        if (set.isEmpty()) {
+        if (set == null || set.isEmpty()) {
             return null;
         }
 
