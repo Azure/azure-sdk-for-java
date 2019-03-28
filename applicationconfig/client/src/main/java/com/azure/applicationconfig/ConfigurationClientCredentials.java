721c02dd816604c09620db5278f820bea5826910
Connie <conniey@microsoft.com>
2019-03-28 15:47:57 -0700

    Adding license header

--- applicationconfig/client/src/main/java/com/azure/applicationconfig/ConfigurationClientCredentials.java
+++ applicationconfig/client/src/main/java/com/azure/applicationconfig/ConfigurationClientCredentials.java
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 package com.azure.applicationconfig;
 
 import com.azure.common.credentials.AsyncServiceClientCredentials;
