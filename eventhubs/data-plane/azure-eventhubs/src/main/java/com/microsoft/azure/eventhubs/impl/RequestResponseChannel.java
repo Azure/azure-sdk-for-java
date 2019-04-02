ccfbf1f3cd97c57a231a797f9754a04b069209b8
shafang <Shawn.Fang@microsoft.com>
2019-04-02 11:24:16 -0700

    fix(CheckStyle): add parenthesis in RequestResponseChannel

--- eventhubs/data-plane/azure-eventhubs/src/main/java/com/microsoft/azure/eventhubs/impl/RequestResponseChannel.java
+++ eventhubs/data-plane/azure-eventhubs/src/main/java/com/microsoft/azure/eventhubs/impl/RequestResponseChannel.java
@@ -103,15 +103,15 @@ public class RequestResponseChannel implements IOObject {
             final Message message,
             final OperationResult<Message, Exception> onResponse) {
 
-        if (message == null)
+        if (message == null) {
             throw new IllegalArgumentException("message cannot be null");
-
-        if (message.getMessageId() != null)
+        }
+        if (message.getMessageId() != null) {
             throw new IllegalArgumentException("message.getMessageId() should be null");
-
-        if (message.getReplyTo() != null)
+        }
+        if (message.getReplyTo() != null) {
             throw new IllegalArgumentException("message.getReplyTo() should be null");
-
+        }
         message.setMessageId("request" + UnsignedLong.valueOf(this.requestId.incrementAndGet()).toString());
         message.setReplyTo(this.replyTo);
 
@@ -131,12 +131,12 @@ public class RequestResponseChannel implements IOObject {
     private void onLinkOpenComplete(final Exception exception) {
 
         if (openRefCount.decrementAndGet() <= 0 && onOpen != null)
-            if (exception == null && this.sendLink.getRemoteState() == EndpointState.ACTIVE && this.receiveLink.getRemoteState() == EndpointState.ACTIVE)
+            if (exception == null && this.sendLink.getRemoteState() == EndpointState.ACTIVE && this.receiveLink.getRemoteState() == EndpointState.ACTIVE) {
                 onOpen.onComplete(null);
-            else {
-                if (exception != null)
+            } else {
+                if (exception != null) {
                     onOpen.onError(exception);
-                else {
+                } else {
                     final ErrorCondition error = (this.sendLink.getRemoteCondition() != null && this.sendLink.getRemoteCondition().getCondition() != null)
                             ? this.sendLink.getRemoteCondition()
                             : this.receiveLink.getRemoteCondition();
@@ -147,34 +147,39 @@ public class RequestResponseChannel implements IOObject {
 
     private void onLinkCloseComplete(final Exception exception) {
 
-        if (closeRefCount.decrementAndGet() <= 0)
+        if (closeRefCount.decrementAndGet() <= 0) {
             if (exception == null) {
-                if (onClose != null)
+                if (onClose != null) {
                     onClose.onComplete(null);
-                if (onGraceFullClose != null)
+                }
+                if (onGraceFullClose != null) {
                     onGraceFullClose.onComplete(null);
+                }
             } else {
-                if (onClose != null)
+                if (onClose != null) {
                     onClose.onError(exception);
-                if (onGraceFullClose != null)
+                }
+                if (onGraceFullClose != null) {
                     onGraceFullClose.onError(exception);
+                }
             }
+        }
     }
 
     @Override
     public IOObjectState getState() {
 
         if (sendLink.getLocalState() == EndpointState.UNINITIALIZED || receiveLink.getLocalState() == EndpointState.UNINITIALIZED
-                || sendLink.getRemoteState() == EndpointState.UNINITIALIZED || receiveLink.getRemoteState() == EndpointState.UNINITIALIZED)
+                || sendLink.getRemoteState() == EndpointState.UNINITIALIZED || receiveLink.getRemoteState() == EndpointState.UNINITIALIZED) {
             return IOObjectState.OPENING;
-
+        }
         if (sendLink.getRemoteState() == EndpointState.ACTIVE && receiveLink.getRemoteState() == EndpointState.ACTIVE
-                && sendLink.getLocalState() == EndpointState.ACTIVE && receiveLink.getRemoteState() == EndpointState.ACTIVE)
+                && sendLink.getLocalState() == EndpointState.ACTIVE && receiveLink.getRemoteState() == EndpointState.ACTIVE) {
             return IOObjectState.OPENED;
-
-        if (sendLink.getRemoteState() == EndpointState.CLOSED && receiveLink.getRemoteState() == EndpointState.CLOSED)
+        }
+        if (sendLink.getRemoteState() == EndpointState.CLOSED && receiveLink.getRemoteState() == EndpointState.CLOSED) {
             return IOObjectState.CLOSED;
-
+        }
         return IOObjectState.CLOSING; // only left cases are if some are active and some are closed
     }
 
@@ -203,10 +208,11 @@ public class RequestResponseChannel implements IOObject {
         @Override
         public void onClose(ErrorCondition condition) {
 
-            if (condition == null || condition.getCondition() == null)
+            if (condition == null || condition.getCondition() == null) {
                 onLinkCloseComplete(null);
-            else
+            } else {
                 onError(ExceptionUtil.toException(condition));
+            }
         }
 
     }
@@ -226,8 +232,9 @@ public class RequestResponseChannel implements IOObject {
             delivery.settle();
 
             final OperationResult<Message, Exception> responseCallback = inflightRequests.remove(response.getCorrelationId());
-            if (responseCallback != null)
+            if (responseCallback != null) {
                 responseCallback.onComplete(response);
+            }
         }
 
         @Override
@@ -241,8 +248,9 @@ public class RequestResponseChannel implements IOObject {
 
             this.cancelPendingRequests(exception);
 
-            if (onClose != null)
+            if (onClose != null) {
                 onLinkCloseComplete(exception);
+            }
         }
 
         @Override
@@ -254,8 +262,9 @@ public class RequestResponseChannel implements IOObject {
                                 ClientConstants.DEFAULT_IS_TRANSIENT,
                                 "The underlying request-response channel closed, recreate the channel and retry the request."));
 
-                if (onClose != null)
+                if (onClose != null) {
                     onLinkCloseComplete(null);
+                }
             } else {
                 this.onError(ExceptionUtil.toException(condition));
             }
