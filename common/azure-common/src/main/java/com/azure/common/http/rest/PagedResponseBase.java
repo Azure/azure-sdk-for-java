0b5585e957d7f8f4c5e52fc7b819182681ffc5d6
Connie <conniey@microsoft.com>
2019-04-03 14:48:04 -0700

    Add PagedResponseBase class

---
+++ common/azure-common/src/main/java/com/azure/common/http/rest/PagedResponseBase.java
@@ -0,0 +1,74 @@
+package com.azure.common.http.rest;
+
+import com.azure.common.http.HttpHeaders;
+import com.azure.common.http.HttpRequest;
+
+import java.util.List;
+
+public class PagedResponseBase<H, T> implements PagedResponse<T> {
+    private final HttpRequest request;
+    private final int statusCode;
+    private final H deserializedHeaders;
+    private final HttpHeaders headers;
+    private final List<T> items;
+    private final String nextLink;
+
+    public PagedResponseBase(HttpRequest request, int statusCode, HttpHeaders headers, Page<T> page, H deserializedHeaders) {
+        this(request, statusCode, headers, page.items(), page.nextLink(), deserializedHeaders);
+    }
+
+    public PagedResponseBase(HttpRequest request, int statusCode, HttpHeaders headers, List<T> items, String nextLink, H deserializedHeaders) {
+        this.request = request;
+        this.statusCode = statusCode;
+        this.headers = headers;
+        this.items = items;
+        this.nextLink = nextLink;
+        this.deserializedHeaders = deserializedHeaders;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<T> items() { return items; }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String nextLink() { return nextLink; }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int statusCode() { return statusCode; }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public HttpHeaders headers() { return headers; }
+
+    /**
+     * @return the request which resulted in this PagedRequestResponse.
+     */
+    @Override
+    public HttpRequest request() { return request; }
+
+    /**
+     * Get the headers from the HTTP response, transformed into the header type H.
+     *
+     * @return an instance of header type H, containing the HTTP response headers.
+     */
+    public H deserializedHeaders() {
+        return deserializedHeaders;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void close() {
+    }
+}
