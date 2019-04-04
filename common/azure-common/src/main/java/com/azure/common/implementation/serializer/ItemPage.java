8ec0245eb8f7e4dd20c1b1e3162939040c6d82e0
Connie <conniey@microsoft.com>
2019-04-04 14:07:58 -0700

    Use JsonAlias

--- common/azure-common/src/main/java/com/azure/common/implementation/serializer/ItemPage.java
+++ common/azure-common/src/main/java/com/azure/common/implementation/serializer/ItemPage.java
@@ -1,7 +1,9 @@
 package com.azure.common.implementation.serializer;
 
 import com.azure.common.http.rest.Page;
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSetter;
 
 import java.util.List;
@@ -21,45 +23,19 @@ import java.util.List;
  * @param <T>
  */
 class ItemPage<T> implements Page<T> {
+    @JsonAlias({"items", "value"})
     private List<T> items;
 
+    @JsonAlias({"nextLink", "nextPageLink"})
     private String nextLink;
 
     @Override
-    @JsonGetter()
     public List<T> items() {
         return items;
     }
 
     @Override
-    @JsonGetter()
     public String nextLink() {
         return nextLink;
-    }
-
-    @JsonSetter()
-    public void setNextLink(String nextLink) {
-        this.nextLink = nextLink;
-    }
-
-    @JsonSetter()
-    public void setValue(List<T> items) {
-        this.items = items;
-    }
-
-    /**
-     * This private method is to support deserialization where the "nextLink" property is the "nextPageLink" property
-     */
-    @JsonSetter()
-    private void setNextPageLink(String nextLink) {
-        this.nextLink = nextLink;
-    }
-
-    /**
-     * This private setter is to support deserialization cases where the list of T uses "items" and not "value".
-     */
-    @JsonSetter()
-    private void setItems(List<T> items) {
-        this.items = items;
     }
 }
