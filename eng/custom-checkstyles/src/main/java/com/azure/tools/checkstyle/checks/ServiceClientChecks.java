cbee1efbabcdd2a49495d5147c0d0b6060c12404
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2019-04-08 13:21:33 -0700

    Removed azure common dependency and restructure the check logic to leverage TreeWalker traversal

--- eng/custom-checkstyles/src/main/java/com/azure/tools/checkstyle/checks/ServiceClientChecks.java
+++ eng/custom-checkstyles/src/main/java/com/azure/tools/checkstyle/checks/ServiceClientChecks.java
@@ -3,35 +3,37 @@
 
 package com.azure.tools.checkstyle.checks;
 
-import com.azure.common.ServiceClient;
 import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
 import com.puppycrawl.tools.checkstyle.api.FullIdent;
 import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+import com.puppycrawl.tools.checkstyle.utils.ScopeUtil;
 import com.puppycrawl.tools.checkstyle.utils.TokenUtil;
 
-import java.util.Optional;
-
 /**
- * Verifies that subclasses of {@link ServiceClient} meet a set of guidelines.
+ * Verifies that subclasses of ServiceClient meet a set of guidelines.
  * <ol>
- *  <li>They cannot have public or protected constructors</li>
- *  <li>They must implement a public static method named builder</li>
+ *  <li>No public or protected constructors</li>
+ *  <li>Implements a public static method named builder</li>
  * </ol>
  */
 public class ServiceClientChecks extends AbstractCheck {
     private static final String BUILDER_METHOD_NAME = "builder";
+    private static final String SERVICE_CLIENT_CLASS_NAME = "com.azure.common.ServiceClient";
 
     private static final String CONSTRUCTOR_ERROR_MESSAGE = "Descendants of ServiceClient cannot have public or protected constructors.";
     private static final String BUILDER_ERROR_MESSAGE = "Descendants of ServiceClient must have a static method named builder.";
 
-    private static final int[] TOKENS = new int[] { TokenTypes.PACKAGE_DEF };
-    private final Class<?> serviceClientClass;
+    private static final int[] TOKENS = new int[] {
+        TokenTypes.PACKAGE_DEF,
+        TokenTypes.CLASS_DEF,
+        TokenTypes.CTOR_DEF,
+        TokenTypes.METHOD_DEF
+    };
 
-    public ServiceClientChecks() throws ClassNotFoundException {
-        super();
-        this.serviceClientClass = this.getClassLoader().loadClass(ServiceClient.class.getName());
-    }
+    private String packageName;
+    private boolean extendsServiceClient;
+    private boolean hasStaticBuilder;
 
     @Override
     public int[] getDefaultTokens() {
@@ -44,9 +46,8 @@ public class ServiceClientChecks extends AbstractCheck {
     }
 
     /**
-     * Denotes that the PACKAGE_DEF token type is the only one that should trigger visitToken to be called.
-     * This is done to prevent inner classes from accidentally firing checks.
-     * @return AST token types that will trigger visitToken to be called.
+     * Array of tokens that trigger visitToken when the TreeWalker is traversing the AST.
+     * @return The list of tokens that trigger visitToken.
      */
     @Override
     public int[] getRequiredTokens() {
@@ -54,107 +55,110 @@ public class ServiceClientChecks extends AbstractCheck {
     }
 
     /**
-     * Runs the check logic on the tokens we marked as needing to be checked in getRequiredTokens.
-     * @param ast Abstract syntax tree node that will be checked.
+     * Start of the TreeWalker traversal.
+     * @param rootAST Root of the AST.
      */
     @Override
-    public void visitToken(DetailAST ast) {
-        DetailAST classDefNode = CustomCheckUtils.findNextSiblingOfType(ast, node -> node.getType() == TokenTypes.CLASS_DEF);
-        if (classDefNode == null) {
-            return;
+    public void beginTree(DetailAST rootAST) {
+        this.packageName = "";
+        this.extendsServiceClient = false;
+        this.hasStaticBuilder = false;
+    }
+    /**
+     * Processes a token from the required tokens list when the TreeWalker visits it.
+     * @param token Node in the AST.
+     */
+    @Override
+    public void visitToken(DetailAST token) {
+        switch (token.getType()) {
+            case TokenTypes.PACKAGE_DEF:
+                this.packageName = FullIdent.createFullIdent(token.findFirstToken(TokenTypes.DOT)).getText();
+                break;
+            case TokenTypes.CLASS_DEF:
+                if (extendsServiceClient(token)) {
+                    this.extendsServiceClient = true;
+                }
+                break;
+            case TokenTypes.CTOR_DEF:
+                if (this.extendsServiceClient && visibilityIsPublicOrProtected(token)) {
+                    log(token, CONSTRUCTOR_ERROR_MESSAGE);
+                }
+                break;
+            case TokenTypes.METHOD_DEF:
+                if (this.extendsServiceClient && !this.hasStaticBuilder && methodIsStaticBuilder(token)) {
+                    this.hasStaticBuilder = true;
+                }
+                break;
+        }
+    }
+
+    /**
+     * End of the TreeWalker traversal.
+     * @param rootAST Root of the AST.
+     */
+    @Override
+    public void finishTree(DetailAST rootAST) {
+        if (this.extendsServiceClient && !this.hasStaticBuilder) {
+            log(0, BUILDER_ERROR_MESSAGE);
+        }
+    }
+
+    /**
+     * Determines if the class extends ServiceClient.
+     * @param classDefinitionToken Class definition token.
+     * @return True if the class is not an inner class, is not in a track one package, and extends ServiceClient.
+     */
+    private boolean extendsServiceClient(DetailAST classDefinitionToken) {
+        // Prevent inner classes from stating its parent class extends ServiceClient or if it is in a track one package.
+        if (ScopeUtil.isInClassBlock(classDefinitionToken) || this.packageName.startsWith("com.microsoft")) {
+            return false;
         }
 
-        DetailAST objBlockNode = classDefNode.findFirstToken(TokenTypes.OBJBLOCK);
-        Optional<String> fullClassName = getFullClassName(ast, classDefNode);
-        if (!fullClassName.isPresent()) {
-            return;
-        }
-
-        // Attempt to load the class and run the checks if it is a descendant of ServiceClient.
         try {
-            Class<?> classToCheck = this.getClassLoader().loadClass(fullClassName.get());
-            //Class<?> serviceClientClass = this.getClassLoader().loadClass(ServiceClient.class.getName());
+            String className = this.packageName + "." + classDefinitionToken.findFirstToken(TokenTypes.IDENT).getText();
+            Class<?> clazz = this.getClassLoader().loadClass(className);
+            Class<?> serviceClientClass = this.getClassLoader().loadClass(SERVICE_CLIENT_CLASS_NAME);
 
-            if (!this.serviceClientClass.isAssignableFrom(classToCheck)) {
-                return;
-            }
+            return serviceClientClass.isAssignableFrom(clazz);
         } catch (ClassNotFoundException ex) {
-            return;
-        }
-
-        TokenUtil.forEachChild(objBlockNode, TokenTypes.CTOR_DEF, this::checkConstructorIsHidden);
-        if (!TokenUtil.findFirstTokenByPredicate(objBlockNode, this::isMethodStaticBuilder).isPresent()) {
-            log(classDefNode, BUILDER_ERROR_MESSAGE);
+            return false;
         }
     }
 
     /**
-     * Given the package node and the class definition node construct the full class name.
-     * @param packageDefNode Node containing the package name
-     * @param classDefNode Node containing the class definition
-     * @return Full class name, if the file isn't a class an empty optional
+     * Checks if the constructor is using the public or protected scope.
+     * @param constructorToken Construction token.
+     * @return True if the constructor has a public or protected modifier token.
      */
-    private Optional<String> getFullClassName(DetailAST packageDefNode, DetailAST classDefNode) {
-        String packageName = FullIdent.createFullIdent(packageDefNode.findFirstToken(TokenTypes.DOT)).getText();
+    private boolean visibilityIsPublicOrProtected(DetailAST constructorToken) {
+        DetailAST modifierToken = constructorToken.findFirstToken(TokenTypes.MODIFIERS);
 
-        // If the package begins with com.microsoft.* it is still in track one and should skip this check.
-        if (packageName.startsWith("com.microsoft")) {
-            return Optional.empty();
+        // No modifiers means package private.
+        if (modifierToken == null) {
+            return false;
         }
 
-        DetailAST classIdentifierNode = classDefNode.findFirstToken(TokenTypes.LITERAL_CLASS);
-        DetailAST classNameNode = CustomCheckUtils.findNextSiblingOfType(classIdentifierNode, node -> node.getType() == TokenTypes.IDENT);
-        if (classNameNode == null) {
-            classNameNode = CustomCheckUtils.findPreviousSiblingOfType(classIdentifierNode, node -> node.getType() == TokenTypes.IDENT);
-        }
-
-        // If the class name cannot be found we shouldn't attempt to run the check.
-        if (classNameNode == null) {
-            return Optional.empty();
-        }
-
-        return Optional.of(packageName + "." + classNameNode.getText());
+        return TokenUtil.findFirstTokenByPredicate(modifierToken,
+            node -> node.getType() == TokenTypes.LITERAL_PUBLIC || node.getType() == TokenTypes.LITERAL_PROTECTED)
+            .isPresent();
     }
 
     /**
-     * Checks if the constructor node has public or protected modifiers.
-     * @param constructorNode Constructor node
+     * Checks if the method node is public static and named builder.
+     * @param methodToken Method node
+     * @return True if the method is public static and is named builder
      */
-    private void checkConstructorIsHidden(DetailAST constructorNode) {
-        DetailAST modifierNode = constructorNode.findFirstToken(TokenTypes.MODIFIERS);
-        if (modifierNode == null) {
-            return;
-        }
-
-        Optional<DetailAST> disallowedModifierNode = TokenUtil.findFirstTokenByPredicate(modifierNode, node -> {
-            return node.getType() == TokenTypes.LITERAL_PUBLIC || node.getType() == TokenTypes.LITERAL_PROTECTED;
-        });
-
-        if (disallowedModifierNode.isPresent()) {
-            log(constructorNode, CONSTRUCTOR_ERROR_MESSAGE);
-        }
-    }
-
-    /**
-     * Checks if the node is a method node, is public static, and is named builder.
-     * @param node Child node of objBlock
-     * @return True if the node is a method that is public static and is named builder
-     */
-    private boolean isMethodStaticBuilder(DetailAST node) {
-        if (node.getType() != TokenTypes.METHOD_DEF) {
+    private boolean methodIsStaticBuilder(DetailAST methodToken) {
+        DetailAST modifierToken = methodToken.findFirstToken(TokenTypes.MODIFIERS);
+        if (modifierToken == null) {
             return false;
         }
 
-        DetailAST modifierNode = node.findFirstToken(TokenTypes.MODIFIERS);
-        if (modifierNode == null) {
+        if (modifierToken.findFirstToken(TokenTypes.LITERAL_STATIC) == null
+            || modifierToken.findFirstToken(TokenTypes.LITERAL_PUBLIC) == null) {
             return false;
         }
 
-        if (modifierNode.findFirstToken(TokenTypes.LITERAL_STATIC) == null
-            || modifierNode.findFirstToken(TokenTypes.LITERAL_PUBLIC) == null) {
-            return false;
-        }
-
-        return node.findFirstToken(TokenTypes.LPAREN).getPreviousSibling().getText().equals(BUILDER_METHOD_NAME);
+        return methodToken.findFirstToken(TokenTypes.IDENT).getText().equals(BUILDER_METHOD_NAME);
     }
 }
