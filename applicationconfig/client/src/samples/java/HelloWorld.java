ae5978a4153189ceaa060dfafeefc8eb229da9a0
Connie <conniey@microsoft.com>
2019-04-10 13:36:44 -0700

    Update documentation

--- applicationconfig/client/src/samples/java/HelloWorld.java
+++ applicationconfig/client/src/samples/java/HelloWorld.java
@@ -21,25 +21,23 @@ public class HelloWorld {
             .credentials(new ConfigurationClientCredentials(connectionString))
             .build();
 
-        // Create a setting to be stored by the configuration service.
-        ConfigurationSetting settingToAdd = new ConfigurationSetting().key("hello").value("world");
+        // Name of the key to add to the configuration service.
+        String key = "hello";
 
-        // setSetting adds or updates a setting to Azure Application Configuration store.
-        // Alternatively, you can call addSetting which only succeeds if the setting does not exist in the store. Or,
-        // you can call updateSetting to update a setting that is already present in the store.
-        Mono<Response<ConfigurationSetting>> setSetting = client.setSetting(settingToAdd);
-
-        // We subscribe and wait for the service call to be complete then print out the contents.
-        // If an error occurs, we print out that error and on the completion of the subscription, we delete the setting.
+        // setSetting adds or updates a setting to Azure Application Configuration store. Alternatively, you can call
+        // addSetting which only succeeds if the setting does not exist in the store. Or, you can call updateSetting to
+        // update a setting that is already present in the store.
+        // We subscribe and wait for the service call to complete then print out the contents of our newly added setting.
+        // If an error occurs, we print out that error. On completion of the subscription, we delete the setting.
         // .block() exists there so the program does not end before the deletion has completed.
-        setSetting.subscribe(result -> {
+        client.setSetting(key, "world").subscribe(result -> {
             ConfigurationSetting setting = result.value();
-            System.out.println(String.format("Key: %s, Value: %s", setting.value(), setting.value()));
+            System.out.println(String.format("Key: %s, Value: %s", setting.key(), setting.value()));
         }, error -> {
             System.err.println("There was an error adding the setting: " + error.toString());
         }, () -> {
             System.out.println("Completed. Deleting setting...");
-            client.deleteSetting(settingToAdd.key()).block();
+            client.deleteSetting(key).block();
         });
     }
 }
