278c4cf3a08c8e27420064fea0b89d2d6197576d
Sima Zhu <sizhu@microsoft.com>
2019-04-11 11:48:10 -0700

    Fixed copyright based on checkstyle.

--- common/azure-common/src/main/java/com/azure/common/annotations/package-info.java
+++ common/azure-common/src/main/java/com/azure/common/annotations/package-info.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 /**
  * Package containing annotations for client side methods that maps to REST APIs.
  */
