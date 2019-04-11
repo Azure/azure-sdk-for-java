bb06f718565fb779e52b48d812f82802ccc65f9f
Sima Zhu <sizhu@microsoft.com>
2019-04-11 14:52:42 -0700

    More imports

--- common/azure-common-mgmt/src/test/java/com/azure/common/mgmt/AzureProxyTests.java
+++ common/azure-common-mgmt/src/test/java/com/azure/common/mgmt/AzureProxyTests.java
@@ -35,7 +35,10 @@ import java.io.IOException;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class AzureProxyTests {
     private long delayInMillisecondsBackup;
--- common/azure-common-mgmt/src/test/java/com/azure/common/mgmt/AzureProxyToRestProxyTests.java
+++ common/azure-common-mgmt/src/test/java/com/azure/common/mgmt/AzureProxyToRestProxyTests.java
@@ -34,7 +34,12 @@ import reactor.core.publisher.Mono;
 
 import java.util.LinkedHashMap;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public abstract class AzureProxyToRestProxyTests {
     /**
