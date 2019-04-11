646b5cbd0030f1a8f4b1f1405a651352ff73b3cc
Sima Zhu <48036328+sima-zhu@users.noreply.github.com>
2019-04-11 14:16:33 -0700

    Fixed copyright based on checkstyle. (#3354)
    This is the first part of checkstyle work.

--- common/azure-common-mgmt/src/main/java/com/azure/common/mgmt/annotations/package-info.java
+++ common/azure-common-mgmt/src/main/java/com/azure/common/mgmt/annotations/package-info.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 /**
  * Annotations used on Swagger generated interfaces that are specific to Azure ARM REST APIs.
  */
