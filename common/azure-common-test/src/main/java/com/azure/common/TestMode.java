c0f5a7aa2480bef6ae011872e928d006ff4244e7
Connie <conniey@microsoft.com>
2019-04-11 21:21:23 -0700

    Add docs to TestMode.java

--- common/azure-common-test/src/main/java/com/azure/common/TestMode.java
+++ common/azure-common-test/src/main/java/com/azure/common/TestMode.java
@@ -1,6 +1,15 @@
 package com.azure.common;
 
+/**
+ * The possible testing modes.
+ */
 public enum TestMode {
+    /**
+     * Record data from a live test.
+     */
     RECORD,
+    /**
+     * Playback data from an existing test session.
+     */
     PLAYBACK,
 }
