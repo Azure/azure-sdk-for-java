41568a627b4b4023be843961cfabb2c33050911c
Connie <conniey@microsoft.com>
2019-04-12 12:31:36 -0700

    Adding documetnation to InterceptorManager

--- common/azure-common-test/src/main/java/com/azure/common/InterceptorManager.java
+++ common/azure-common-test/src/main/java/com/azure/common/InterceptorManager.java
@@ -4,6 +4,8 @@ package com.azure.common;
 
 import com.azure.common.http.HttpClient;
 import com.azure.common.http.PlaybackClient;
+import com.azure.common.http.policy.HttpPipelinePolicy;
+import com.azure.common.models.NetworkCallRecord;
 import com.azure.common.models.RecordedData;
 import com.azure.common.policy.RecordNetworkCallPolicy;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,12 +18,28 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
+/**
+ * A class that keeps track of network calls by either reading the data from an existing test session record or
+ * recording the network calls in memory. Test session records are saved or read from:
+ * "<i>session-records/{@code testName}.json</i>"
+ *
+ * <ul>
+ *     <li>If the {@code testMode} is {@link TestMode#PLAYBACK}, the manager tries to find an existing test session
+ *     record to read network calls from.</li>
+ *     <li>If the {@code testMode} is {@link TestMode#RECORD}, the manager creates a new test session record and saves
+ *     all the network calls to it.</li>
+ * </ul>
+ *
+ * When the {@link InterceptorManager} is disposed, if the {@code testMode} is {@link TestMode#RECORD}, the network
+ * calls that were recorded are persisted to: "<i>session-records/{@code testName}.json</i>"
+ */
 public class InterceptorManager implements AutoCloseable {
     private final static String RECORD_FOLDER = "session-records/";
 
     private final Logger logger = LoggerFactory.getLogger(InterceptorManager.class);
-    private final Map<String, String> textReplacementRules = new HashMap<>();
+    private final Map<String, String> textReplacementRules;
     private final String testName;
     private final TestMode testMode;
 
@@ -29,27 +47,101 @@ public class InterceptorManager implements AutoCloseable {
     // A state machine ensuring a test is always reset before another one is setup
     private final RecordedData recordedData;
 
+    /**
+     * Creates a new InterceptorManager that either replays test-session records or saves them.
+     *
+     * <ul>
+     *     <li>If {@code testMode} is {@link TestMode#PLAYBACK}, the manager tries to find an existing test session
+     *     record to read network calls from.</li>
+     *     <li>If {@code testMode} is {@link TestMode#RECORD}, the manager creates a new test session record and saves
+     *     all the network calls to it.</li>
+     * </ul>
+     *
+     * The test session records are persisted in the path: "<i>session-records/{@code testName}.json</i>"
+     *
+     * @param testName Name of the test session record.
+     * @param testMode The {@link TestMode} for this interceptor.
+     * @throws IOException If {@code testMode} is {@link TestMode#PLAYBACK} and an existing test session record could
+     * not be located or the data could not be deserialized into an instance of {@link RecordedData}.
+     * @throws NullPointerException If {@code testName} is {@code null}.
+     */
     public InterceptorManager(String testName, TestMode testMode) throws IOException {
+        Objects.requireNonNull(testName);
+
         this.testName = testName;
         this.testMode = testMode;
+        this.textReplacementRules = new HashMap<>();
 
         this.recordedData = testMode == TestMode.PLAYBACK
             ? readDataFromFile()
             : new RecordedData();
     }
 
+    /**
+     * Creates a new InterceptorManager that replays test session records. It takes a set of
+     * {@code textReplacementRules}, that can be used by {@link PlaybackClient} to replace values in a
+     * {@link NetworkCallRecord#response()}.
+     *
+     * The test session records are read from: "<i>session-records/{@code testName}.json</i>"
+     *
+     * @param testName Name of the test session record.
+     * @param textReplacementRules A set of rules to replace text in {@link NetworkCallRecord#response()} when playing
+     * back network calls.
+     * @throws IOException An existing test session record could not be located or the data could not be deserialized
+     * into an instance of {@link RecordedData}.
+     * @throws NullPointerException If {@code testName} or {@code textReplacementRules} is {@code null}.
+     */
+    public InterceptorManager(String testName, Map<String, String> textReplacementRules) throws IOException {
+        Objects.requireNonNull(testName);
+        Objects.requireNonNull(textReplacementRules);
+
+        this.testName = testName;
+        this.testMode = TestMode.PLAYBACK;
+
+        this.recordedData = readDataFromFile();
+        this.textReplacementRules = textReplacementRules;
+    }
+
+    /**
+     * Gets whether this InterceptorManager is in playback mode.
+     *
+     * @return true if the InterceptorManager is in playback mode and false otherwise.
+     */
     public boolean isPlaybackMode() {
         return testMode == TestMode.PLAYBACK;
     }
 
-    public RecordNetworkCallPolicy getRecordPolicy() {
+    /**
+     * Gets the recorded data InterceptorManager is keeping track of.
+     *
+     * @return
+     */
+    public RecordedData getRecordedData() {
+        return recordedData;
+    }
+
+    /**
+     * Gets a new HTTP pipeline policy that records network calls and its data is managed by {@link InterceptorManager}.
+     */
+    public HttpPipelinePolicy getRecordPolicy() {
         return new RecordNetworkCallPolicy(recordedData);
     }
 
+    /**
+     * Gets a new HTTP client that plays back test session records managed by {@link InterceptorManager}.
+     *
+     * @return An HTTP client that plays back network calls from its recorded data.
+     */
     public HttpClient getPlaybackClient() {
         return new PlaybackClient(recordedData, textReplacementRules);
     }
 
+    /**
+     * Disposes of resources used by this InterceptorManager.
+     *
+     * If {@code testMode} is {@link TestMode#RECORD}, all the network calls are persisted to:
+     * "<i>session-records/{@code testName}.json</i>"
+     */
     @Override
     public void close() {
         switch (testMode) {
@@ -64,7 +156,8 @@ public class InterceptorManager implements AutoCloseable {
                 // Do nothing
                 break;
             default:
-                System.out.println("==> Unknown AZURE_TEST_MODE: " + testMode);
+                logger.error("==> Unknown AZURE_TEST_MODE: {}", testMode);
+                break;
         }
     }
 
