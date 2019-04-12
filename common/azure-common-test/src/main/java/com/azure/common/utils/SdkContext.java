7bbdc4a1711e5c53f8ec3f17ad82245bcb051513
Connie <conniey@microsoft.com>
2019-04-12 00:28:56 -0700

    Add license headers

--- common/azure-common-test/src/main/java/com/azure/common/utils/SdkContext.java
+++ common/azure-common-test/src/main/java/com/azure/common/utils/SdkContext.java
@@ -5,6 +5,7 @@ package com.azure.common.utils;
 
 import com.azure.common.TestMode;
 import com.azure.common.models.RecordedData;
+import com.azure.common.TestResourceNamer;
 
 import java.util.Objects;
 
