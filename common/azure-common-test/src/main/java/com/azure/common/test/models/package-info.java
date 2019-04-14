c41e856145c7c32660de7e6d8a2fb5587e134348
Connie <conniey@microsoft.com>
2019-04-13 21:49:11 -0700

    Adding package information.

--- common/azure-common-test/src/main/java/com/azure/common/test/models/package-info.java
+++ common/azure-common-test/src/main/java/com/azure/common/test/models/package-info.java
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 /**
- * Package contains common test classes for Azure client libraries.
+ * Package contains models used to test Azure client libraries.
  */
-package com.azure.common.test;
+package com.azure.common.test.models;
