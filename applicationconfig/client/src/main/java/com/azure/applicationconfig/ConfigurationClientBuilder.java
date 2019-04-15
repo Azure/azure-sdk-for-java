bf5caefb99ef6e04989eb5b0e8b2dbf4ab0b963d
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2019-04-15 12:44:54 -0700

    JavaDocs for ConfigurationClientBuilder

--- applicationconfig/client/src/main/java/com/azure/applicationconfig/ConfigurationClientBuilder.java
+++ applicationconfig/client/src/main/java/com/azure/applicationconfig/ConfigurationClientBuilder.java
@@ -3,16 +3,66 @@ package com.azure.applicationconfig;
 import com.azure.applicationconfig.credentials.ConfigurationClientCredentials;
 import com.azure.applicationconfig.models.ConfigurationSetting;
 import com.azure.common.http.HttpClient;
+import com.azure.common.http.HttpHeaders;
 import com.azure.common.http.HttpPipeline;
 import com.azure.common.http.policy.HttpLogDetailLevel;
 import com.azure.common.http.policy.HttpPipelinePolicy;
 
 import java.net.MalformedURLException;
 
+/**
+ * Builds instances of {@link ConfigurationClient} based on the configuration options stored in the builder when
+ * {@link ConfigurationClientBuilder#build()} is called.
+ *
+ * <p>
+ *     To build ConfigurationAsyncClients that can interact with Azure App Configuration the service endpoint and
+ *     authentication {@link HttpHeaders} are required. This information can be passed to the builder using two primary
+ *     means, first being a {@link ConfigurationClientCredentials} and second using a {@link HttpPipeline} and the
+ *     service endpoint.
+ * </p>
+ *
+ * <p>
+ *     The ConfigurationClientCredentials object contains the service endpoint and a method to retrieve the authentication
+ *     headers. {@link ConfigurationClientBuilder#credentials(ConfigurationClientCredentials)} sets the
+ *     {@code serviceEndpoint} member variable to {@link ConfigurationClientCredentials#baseUri()} and {@code credentials}
+ *     member variable to the passed ConfigurationClientCredentials. When building the ConfigurationAsyncClient the
+ *     builder will use default {@link HttpPipelinePolicy policies}, including the ConfigurationClientCredentials, to
+ *     construct a HttpPipeline that will be used by the client to interact with the service; a new pipeline will be
+ *     constructed every build.
+ *
+ * <pre>
+ * {@code ConfigurationClientCredentials credentials = new ConfigurationClientCredentials(connectionString);
+ *    ConfigurationAsyncClient.builder()
+ *        .credentials(credentials)
+ *        .build();}
+ * </pre>
+ * </p>
+ *
+ * <p>
+ *     An HttpPipeline performs the communication between the client and service. Using an HttpPipeline to construct a
+ *     ConfigurationAsyncClient requires additional setup as it doesn't generate default policies, but it allows for finer
+ *     controller. {@link ConfigurationClientBuilder#pipeline(HttpPipeline)} sets the {@code pipeline} but unlike credentials
+ *     doesn't set the service endpoint as a pipeline doesn't have that information, {@link ConfigurationClientBuilder#serviceEndpoint(String)}
+ *     must be called to set the service endpoint. When building the ConfigurationAsyncClient the HttpPipeline and service
+ *     endpoint as simply passed into the client constructor.
+ *
+ * <pre>
+ * {@code String serviceEndpoint = <App-Configuration-URL>;
+ *    HttpPipeline pipeline = new HttpPipeline(<policies>);
+ *    ConfigurationClient client = ConfigurationClient.builder()
+ *        .pipeline(pipeline)
+ *        .serviceEndpoint(serviceEndpoint)
+ *        .build();}
+ * </pre>
+ * </p>
+ *
+ * @see ConfigurationAsyncClient
+ * @see ConfigurationClientCredentials
+ */
 public final class ConfigurationClientBuilder {
     private final ConfigurationAsyncClientBuilder builder;
 
-    public ConfigurationClientBuilder() {
+    ConfigurationClientBuilder() {
         builder = ConfigurationAsyncClient.builder();
     }
 
@@ -23,7 +73,7 @@ public final class ConfigurationClientBuilder {
      * <p>
      * If {@link ConfigurationClientBuilder#pipeline(HttpPipeline) pipeline} is set, then the {@code pipeline} and
      * {@link ConfigurationClientBuilder#serviceEndpoint(String) serviceEndpoint} are used to create the
-     * {@link ConfigurationAsyncClient client}. All other builder settings are ignored.
+     * {@link ConfigurationClient client}. All other builder settings are ignored.
      * </p>
      *
      * @return A ConfigurationAsyncClient with the options set from the builder.
@@ -76,7 +126,7 @@ public final class ConfigurationClientBuilder {
 
     /**
      * Adds a policy to the set of existing policies that are executed after
-     * {@link ConfigurationAsyncClient} required policies.
+     * {@link ConfigurationClient} required policies.
      *
      * @param policy The retry policy for service requests.
      * @return The updated ConfigurationClientBuilder object.
