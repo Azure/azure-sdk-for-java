c43aef7f34dbb74170e411e37b40202e5859f7dd
Connie <conniey@microsoft.com>
2019-04-16 13:06:10 -0700

    HttpBinJSON: Add getters/setters to member variables.

--- common/azure-common/src/test/java/com/azure/common/entities/HttpBinJSON.java
+++ common/azure-common/src/test/java/com/azure/common/entities/HttpBinJSON.java
@@ -3,13 +3,44 @@
 
 package com.azure.common.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.Map;
 
 /**
  * Maps to the JSON return values from http://httpbin.org.
  */
 public class HttpBinJSON {
-    public String url;
-    public Map<String, String> headers;
-    public Object data;
+    @JsonProperty()
+    private String url;
+
+    @JsonProperty()
+    private Map<String, String> headers;
+
+    @JsonProperty()
+    private Object data;
+
+    public String url() {
+        return url;
+    }
+
+    public void url(String url) {
+        this.url = url;
+    }
+
+    public Map<String, String> headers() {
+        return headers;
+    }
+
+    public void headers(Map<String, String> headers) {
+        this.headers = headers;
+    }
+
+    public Object data() {
+        return data;
+    }
+
+    public void data(Object data) {
+        this.data = data;
+    }
 }
