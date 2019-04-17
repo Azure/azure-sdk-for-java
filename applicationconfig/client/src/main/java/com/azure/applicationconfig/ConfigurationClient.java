7603ffffa45bc92aa58a7058ed2c3c5d36b9c949
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2019-04-17 09:49:01 -0700

    Checkstyle cleanup

--- applicationconfig/client/src/main/java/com/azure/applicationconfig/ConfigurationClient.java
+++ applicationconfig/client/src/main/java/com/azure/applicationconfig/ConfigurationClient.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.azure.applicationconfig;
 
 import com.azure.applicationconfig.credentials.ConfigurationClientCredentials;
--- applicationconfig/client/src/main/java/com/azure/applicationconfig/ConfigurationClientBuilder.java
+++ applicationconfig/client/src/main/java/com/azure/applicationconfig/ConfigurationClientBuilder.java
@@ -1,9 +1,11 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.azure.applicationconfig;
 
 import com.azure.applicationconfig.credentials.ConfigurationClientCredentials;
 import com.azure.applicationconfig.models.ConfigurationSetting;
 import com.azure.common.http.HttpClient;
-import com.azure.common.http.HttpHeaders;
 import com.azure.common.http.HttpPipeline;
 import com.azure.common.http.policy.HttpLogDetailLevel;
 import com.azure.common.http.policy.HttpPipelinePolicy;
