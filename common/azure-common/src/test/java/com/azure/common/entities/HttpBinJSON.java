459b3a856157634850017a8cfbefc83312cdc929
Connie <conniey@microsoft.com>
2019-04-19 09:38:46 -0700

    Adding javadocs to HttpBinJSON

--- common/azure-common/src/test/java/com/azure/common/entities/HttpBinJSON.java
+++ common/azure-common/src/test/java/com/azure/common/entities/HttpBinJSON.java
@@ -20,26 +20,56 @@ public class HttpBinJSON {
     @JsonProperty()
     private Object data;
 
+    /**
+     * Gets the URL associated with this request.
+     *
+     * @return he URL associated with the request.
+     */
     public String url() {
         return url;
     }
 
+    /**
+     * Sets the URL associated with this request.
+     *
+     * @param url The URL associated with the request.
+     */
     public void url(String url) {
         this.url = url;
     }
 
+    /**
+     * Gets the response headers.
+     *
+     * @return The response headers.
+     */
     public Map<String, String> headers() {
         return headers;
     }
 
+    /**
+     * Sets the response headers.
+     *
+     * @param headers The response headers.
+     */
     public void headers(Map<String, String> headers) {
         this.headers = headers;
     }
 
+    /**
+     * Gets the response body.
+     *
+     * @return The response body.
+     */
     public Object data() {
         return data;
     }
 
+    /**
+     * Sets the response body.
+     *
+     * @param data The response body.
+     */
     public void data(Object data) {
         this.data = data;
     }
