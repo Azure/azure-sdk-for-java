ebcc121e1ca0d927ec1f79b66a513e83b579ada4
Connie <conniey@microsoft.com>
2019-04-25 17:08:06 -0700

    Use slf4j logger instead of console since we have slf4j logger enabled for tests.

--- eventhubs/data-plane/azure-eventhubs-eph/src/test/java/com/microsoft/azure/eventprocessorhost/TestBase.java
+++ eventhubs/data-plane/azure-eventhubs-eph/src/test/java/com/microsoft/azure/eventprocessorhost/TestBase.java
@@ -31,43 +31,24 @@ public class TestBase {
     static final int NO_CHECKS = -2; // do no checks at all, used for tests which are expected fail in startup
     static final int ANY_NONZERO_COUNT = -1; // if expectedEvents is -1, just check for > 0
 
-    static boolean logInfo = false;
-    static boolean logConsole = false;
     static final Logger TRACE_LOGGER = LoggerFactory.getLogger("servicebus.test-eph.trace");
 
     @Rule
     public final TestName name = new TestName();
-
-    @BeforeClass
-    public static void allTestStart() {
-        String env = System.getenv("VERBOSELOG");
-        if (env != null) {
-            TestBase.logInfo = true;
-            if (env.compareTo("CONSOLE") == 0) {
-                TestBase.logConsole = true;
-            }
-        }
-    }
 
     @AfterClass
     public static void allTestFinish() {
     }
 
     static void logError(String message) {
-        if (TestBase.logConsole) {
-            System.err.println("TEST ERROR: " + message);
-        } else {
-            TestBase.TRACE_LOGGER.error(message);
+        if (TRACE_LOGGER.isErrorEnabled()) {
+            TRACE_LOGGER.error(message);
         }
     }
 
     static void logInfo(String message) {
-        if (TestBase.logInfo) {
-            if (TestBase.logConsole) {
-                System.err.println("TEST INFO: " + message);
-            } else {
-                TestBase.TRACE_LOGGER.info(message);
-            }
+        if (TRACE_LOGGER.isInfoEnabled()) {
+            TRACE_LOGGER.info(message);
         }
     }
 
@@ -77,22 +58,12 @@ public class TestBase {
 
     @Before
     public void logCaseStart() {
-        String usemsg = "CASE START: " + this.name.getMethodName();
-        if (TestBase.logConsole) {
-            System.err.println(usemsg);
-        } else {
-            TestBase.TRACE_LOGGER.info(usemsg);
-        }
+        logInfo("CASE START: " + this.name.getMethodName());
     }
 
     @After
     public void logCaseEnd() {
-        String usemsg = "CASE END: " + this.name.getMethodName();
-        if (TestBase.logConsole) {
-            System.err.println(usemsg);
-        } else {
-            TestBase.TRACE_LOGGER.info(usemsg);
-        }
+        logInfo("CASE END: " + this.name.getMethodName());
     }
 
     PerTestSettings testSetup(PerTestSettings settings) throws Exception {
