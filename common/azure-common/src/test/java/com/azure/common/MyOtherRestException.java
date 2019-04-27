0306fbb38bdd7393ee5dad94e7ca534049301823
Sima Zhu <sizhu@microsoft.com>
2019-04-26 20:25:06 -0700

    Change the http request related exception from ServiceRequestException to HttpRequestException.

--- common/azure-common/src/test/java/com/azure/common/MyOtherRestException.java
+++ common/azure-common/src/test/java/com/azure/common/MyOtherRestException.java
@@ -5,9 +5,9 @@ package com.azure.common;
 
 import com.azure.common.entities.HttpBinJSON;
 import com.azure.common.http.HttpResponse;
-import com.azure.common.exception.ServiceRequestException;
+import com.azure.common.exception.HttpRequestException;
 
-public class MyOtherRestException extends ServiceRequestException {
+public class MyOtherRestException extends HttpRequestException {
     /**
      * Creates a service request exception.
      *
--- common/azure-common/src/test/java/com/azure/common/MyRestException.java
+++ common/azure-common/src/test/java/com/azure/common/MyRestException.java
@@ -4,10 +4,10 @@
 package com.azure.common;
 
 import com.azure.common.entities.HttpBinJSON;
-import com.azure.common.exception.ServiceRequestException;
+import com.azure.common.exception.HttpRequestException;
 import com.azure.common.http.HttpResponse;
 
-public class MyRestException extends ServiceRequestException {
+public class MyRestException extends HttpRequestException {
     /**
      * Creates a service request exception.
      *
