feecc50d0894103da5b731cf4981d966c9ea736c
Connie <conniey@microsoft.com>
2019-04-28 23:40:13 -0700

    Fix last method names.

--- keyvault/data-plane/azure-keyvault-extensions/src/test/java/com/microsoft/azure/keyvault/extensions/test/KeyVaultKeyResolverDefaultProviderTest.java
+++ keyvault/data-plane/azure-keyvault-extensions/src/test/java/com/microsoft/azure/keyvault/extensions/test/KeyVaultKeyResolverDefaultProviderTest.java
@@ -75,7 +75,7 @@ public class KeyVaultKeyResolverDefaultProviderTest extends KeyVaultClientIntegr
     private static final Base64 BASE_64 = new Base64(-1, null, true);
 
     @Test
-    public void KeyKeyVaultKeyResolverDefaultProviderTest() throws InterruptedException, ExecutionException {
+    public void keyKeyVaultKeyResolverDefaultProviderTest() throws InterruptedException, ExecutionException {
         String testKeyName = KEY_NAME + "1";
         try {
             // Create a key on a vault.
@@ -105,7 +105,7 @@ public class KeyVaultKeyResolverDefaultProviderTest extends KeyVaultClientIntegr
      * Test resolving a key from a 128bit secret encoded as base64 in a vault using various KeyVaultKeyResolver constructors.
      */
     @Test
-    public void Secret128Base64KeyVaultKeyResolverDefaultProviderTest() throws InterruptedException, ExecutionException {
+    public void secret128Base64KeyVaultKeyResolverDefaultProviderTest() throws InterruptedException, ExecutionException {
         // Arrange
         byte[] keyBytes = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F};
         byte[] cek = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, (byte) 0x88, (byte) 0x99, (byte) 0xAA, (byte) 0xBB, (byte) 0xCC, (byte) 0xDD, (byte) 0xEE, (byte) 0xFF};
@@ -164,7 +164,7 @@ public class KeyVaultKeyResolverDefaultProviderTest extends KeyVaultClientIntegr
      * Test resolving a key from a 128bit secret encoded as base64 in a vault using various KeyVaultKeyResolver constructors.
      */
     @Test
-    public void Secret192Base64KeyVaultKeyResolverDefaultProviderTest() throws InterruptedException, ExecutionException {
+    public void secret192Base64KeyVaultKeyResolverDefaultProviderTest() throws InterruptedException, ExecutionException {
         // Arrange
         byte[] keyBytes = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17};
         byte[] cek = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, (byte) 0x88, (byte) 0x99, (byte) 0xAA, (byte) 0xBB, (byte) 0xCC, (byte) 0xDD, (byte) 0xEE, (byte) 0xFF};
@@ -240,7 +240,7 @@ public class KeyVaultKeyResolverDefaultProviderTest extends KeyVaultClientIntegr
      * Test resolving a key from a 256bit secret encoded as base64 in a vault using various KeyVaultKeyResolver constructors.
      */
     @Test
-    public void Secret256Base64KeyVaultKeyResolverDefaultProviderTest() throws InterruptedException, ExecutionException {
+    public void secret256Base64KeyVaultKeyResolverDefaultProviderTest() throws InterruptedException, ExecutionException {
         // Arrange
         byte[] keyBytes = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F};
         byte[] cek = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, (byte) 0x88, (byte) 0x99, (byte) 0xAA, (byte) 0xBB, (byte) 0xCC, (byte) 0xDD, (byte) 0xEE, (byte) 0xFF};
