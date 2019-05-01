b3aa03905ab2647f35cab6d276574043bb07dc21
Connie <conniey@microsoft.com>
2019-05-01 14:06:32 -0700

    Expose isProxyConfigured and ctor documentation to ProxyConfiguration.

--- sdk/eventhubs/microsoft-azure-eventhubs/src/main/java/com/microsoft/azure/eventhubs/ProxyConfiguration.java
+++ sdk/eventhubs/microsoft-azure-eventhubs/src/main/java/com/microsoft/azure/eventhubs/ProxyConfiguration.java
@@ -1,13 +1,13 @@
 package com.microsoft.azure.eventhubs;
 
 import java.net.Authenticator;
+import java.net.PasswordAuthentication;
 import java.util.Objects;
 
 public class ProxyConfiguration {
     private final String proxyAddress;
-    private final String username;
-    private final char[] password;
     private final ProxyConfiguration.ProxyAuthType authType;
+    private final PasswordAuthentication credentials;
 
     /**
      * Creates a proxy configuration that uses the system configured proxy and authentication.
@@ -15,50 +15,52 @@ public class ProxyConfiguration {
     private ProxyConfiguration() {
         this.authType = ProxyAuthType.USE_DEFAULT_AUTHENTICATOR;
         this.proxyAddress = null;
-        this.username = null;
-        this.password = null;
+        this.credentials = null;
     }
 
     /**
-     * Creates a proxy configuration that uses the {@code proxyAddress} and requires no authentication.
-     * {@link ProxyAuthType#NONE} is used.
+     * Creates a proxy configuration that uses the {@code proxyAddress} and uses system configured authenticator to
+     * provide a username or password if one is needed.
      *
      * @throws NullPointerException if {@code proxyAddress} is {@code null}.
      */
     public ProxyConfiguration(String proxyAddress) {
-        this(proxyAddress, null, null, ProxyAuthType.NONE);
+        this(proxyAddress, null, null, ProxyAuthType.USE_DEFAULT_AUTHENTICATOR);
+
+        Objects.requireNonNull(proxyAddress);
     }
 
     /**
      * Creates a proxy configuration that uses the {@code proxyAddress} and authenticates with provided
      * {@code username}, {@code password} and {@code authType}.
      *
-     * @param proxyAddress Required. URL of the proxy
+     * @param proxyAddress URL of the proxy. If {@code null} is passed in, then the system configured proxy url is used.
      * @param username Username used to authenticate with proxy. Optional if {@code authType} is
      * {@link ProxyAuthType#NONE} or {@link ProxyAuthType#USE_DEFAULT_AUTHENTICATOR}.
      * @param password Password used to authenticate with proxy. Optional if {@code authType} is
      * {@link ProxyAuthType#NONE} or {@link ProxyAuthType#USE_DEFAULT_AUTHENTICATOR}.
      * @param authType Authentication method to use with proxy.
      *
-     * @throws NullPointerException if {@code proxyAddress} or {@code authType} is {@code null}.
+     * @throws NullPointerException if {@code authType} is {@code null}.
      * @throws IllegalArgumentException if {@code authType} is {@link ProxyAuthType#BASIC} or
      * {@link ProxyAuthType#DIGEST} and {@code username} or {@code password} are {@code null}.
      */
     public ProxyConfiguration(String proxyAddress, String username, String password, ProxyAuthType authType) {
-        Objects.requireNonNull(proxyAddress);
         Objects.requireNonNull(authType);
 
+        // If the user is authenticating with BASIC or DIGEST, they do not want to use the system-configured
+        // authenticator, so we require these values.
         if (authType == ProxyAuthType.BASIC || authType == ProxyAuthType.DIGEST) {
             Objects.requireNonNull(username);
             Objects.requireNonNull(password);
+
+            this.credentials = new PasswordAuthentication(username, password.toCharArray());
+        } else {
+            this.credentials = null;
         }
 
         this.proxyAddress = proxyAddress;
-        this.username = username;
         this.authType = authType;
-        this.password = password != null
-            ? password.toCharArray()
-            : new char[0];
     }
 
     public static ProxyConfiguration useSystemConfiguration() {
@@ -69,8 +71,17 @@ public class ProxyConfiguration {
         return proxyAddress;
     }
 
-    String username() {
-        return username;
+    PasswordAuthentication credentials() {
+        return credentials;
+    }
+
+    /**
+     * Gets whether the proxy address has been configured.
+     *
+     * @return true if the proxy url has been set, and false otherwise.
+     */
+    boolean isProxyAddressConfigured() {
+        return proxyAddress != null && !proxyAddress.equals("");
     }
 
     /**
