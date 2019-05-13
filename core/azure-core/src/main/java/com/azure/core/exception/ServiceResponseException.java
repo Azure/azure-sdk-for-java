2e1d8914fbe56facf1e8524261e4830e5eadc09d
Sima Zhu <sizhu@microsoft.com>
2019-05-13 09:56:51 -0700

    Remove the httpResponse field.

--- core/azure-core/src/main/java/com/azure/core/exception/ServiceResponseException.java
+++ core/azure-core/src/main/java/com/azure/core/exception/ServiceResponseException.java
@@ -14,11 +14,6 @@ import com.azure.core.http.HttpResponse;
 public class ServiceResponseException extends AzureException {
 
     /**
-     * Information about the associated HTTP response.
-     */
-    private HttpResponse response;
-
-    /**
      * Initializes a new instance of the ServiceResponseException class.
      *
      * @param message the exception message or the response content if a message is not available
@@ -36,36 +31,4 @@ public class ServiceResponseException extends AzureException {
     public ServiceResponseException(final String message, final Throwable cause) {
         super(message, cause);
     }
-
-    /**
-     * Initializes a new instance of the ServiceResponseException class.
-     *
-     * @param message the exception message or the response content if a message is not available
-     * @param response the Http response received from Azure service
-     */
-    public ServiceResponseException(final String message, final HttpResponse response) {
-        super(message);
-        this.response = response;
-    }
-
-
-    /**
-     * Initializes a new instance of the ServiceResponseException class.
-     *
-     * @param message the exception message or the response content if a message is not available
-     * @param response the Http response received from Azure service
-     * @param cause the Throwable which caused the creation of this ServiceResponseException
-     */
-    public ServiceResponseException(final String message, final HttpResponse response, final Throwable cause) {
-        super(message, cause);
-        this.response = response;
-    }
-
-    /**
-     * @return information about the associated HTTP response
-     */
-    public HttpResponse response() {
-        return response;
-    }
-
 }
