ad9fd3259afc9eacb8e435e43a9154581ad037e6
Sima Zhu <sizhu@microsoft.com>
2019-05-14 10:11:02 -0700

    Remove the httprequest from ServiceRequestException

--- core/azure-core/src/main/java/com/azure/core/exception/ServiceRequestException.java
+++ core/azure-core/src/main/java/com/azure/core/exception/ServiceRequestException.java
@@ -14,11 +14,6 @@ import com.azure.core.http.HttpRequest;
 public class ServiceRequestException extends AzureException {
 
     /**
-     * Information about the associated HTTP response.
-     */
-    private HttpRequest request;
-
-    /**
      * Initializes a new instance of the ServiceRequestException class.
      *
      * @param message the exception message or the response content if a message is not available
@@ -26,34 +21,14 @@ public class ServiceRequestException extends AzureException {
     public ServiceRequestException(final String message) {
         super(message);
     }
-
+    
     /**
      * Initializes a new instance of the ServiceRequestException class.
      *
      * @param message the exception message or the response content if a message is not available
-     * @param request the Http request sends to the Azure service
-     */
-    public ServiceRequestException(final String message, final HttpRequest request) {
-        super(message);
-        this.request = request;
-    }
-
-    /**
-     * Initializes a new instance of the ServiceRequestException class.
-     *
-     * @param message the exception message or the response content if a message is not available
-     * @param request the HTTP request sends to the Azure service
      * @param cause the Throwable which caused the creation of this ServiceRequestException
      */
-    public ServiceRequestException(final String message, final HttpRequest request, final Throwable cause) {
+    public ServiceRequestException(final String message, final Throwable cause) {
         super(message, cause);
-        this.request = request;
-    }
-
-    /**
-     * @return information about the associated HTTP response
-     */
-    public HttpRequest request() {
-        return request;
     }
 }
