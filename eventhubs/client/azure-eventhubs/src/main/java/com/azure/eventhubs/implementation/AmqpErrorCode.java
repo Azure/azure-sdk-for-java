e53cc7d4306705f4c10be4db4310e719abadb0c2
Connie <conniey@microsoft.com>
2019-05-16 11:21:35 -0700

    Fix constant names.

--- eventhubs/client/azure-eventhubs/src/main/java/com/azure/eventhubs/implementation/AmqpErrorCode.java
+++ eventhubs/client/azure-eventhubs/src/main/java/com/azure/eventhubs/implementation/AmqpErrorCode.java
@@ -7,23 +7,23 @@ import org.apache.qpid.proton.amqp.Symbol;
 
 public final class AmqpErrorCode {
 
-    public static final Symbol NotFound = Symbol.getSymbol("amqp:not-found");
-    public static final Symbol UnauthorizedAccess = Symbol.getSymbol("amqp:unauthorized-access");
-    public static final Symbol ResourceLimitExceeded = Symbol.getSymbol("amqp:resource-limit-exceeded");
-    public static final Symbol NotAllowed = Symbol.getSymbol("amqp:not-allowed");
-    public static final Symbol InternalError = Symbol.getSymbol("amqp:internal-error");
-    public static final Symbol IllegalState = Symbol.getSymbol("amqp:illegal-state");
-    public static final Symbol NotImplemented = Symbol.getSymbol("amqp:not-implemented");
+    public static final Symbol NOT_FOUND = Symbol.getSymbol("amqp:not-found");
+    public static final Symbol UNAUTHORIZED_ACCESS = Symbol.getSymbol("amqp:unauthorized-access");
+    public static final Symbol RESOURCE_LIMIT_EXCEEDED = Symbol.getSymbol("amqp:resource-limit-exceeded");
+    public static final Symbol NOT_ALLOWED = Symbol.getSymbol("amqp:not-allowed");
+    public static final Symbol INTERNAL_ERROR = Symbol.getSymbol("amqp:internal-error");
+    public static final Symbol ILLEGAL_STATE = Symbol.getSymbol("amqp:illegal-state");
+    public static final Symbol NOT_IMPLEMENTED = Symbol.getSymbol("amqp:not-implemented");
 
     // link errors
-    public static final Symbol Stolen = Symbol.getSymbol("amqp:link:stolen");
-    public static final Symbol PayloadSizeExceeded = Symbol.getSymbol("amqp:link:message-size-exceeded");
-    public static final Symbol AmqpLinkDetachForced = Symbol.getSymbol("amqp:link:detach-forced");
+    public static final Symbol STOLEN = Symbol.getSymbol("amqp:link:stolen");
+    public static final Symbol PAYLOAD_SIZE_EXCEEDED = Symbol.getSymbol("amqp:link:message-size-exceeded");
+    public static final Symbol AMQP_LINK_DETACH_FORCED = Symbol.getSymbol("amqp:link:detach-forced");
 
     // connection errors
-    public static final Symbol ConnectionForced = Symbol.getSymbol("amqp:connection:forced");
+    public static final Symbol CONNECTION_FORCED = Symbol.getSymbol("amqp:connection:forced");
 
-    // proton library introduced this amqpsymbol in their code-base to communicate IOExceptions
+    // proton library introduced this AMQP symbol in their code-base to communicate IOExceptions
     // while performing operations on SocketChannel (in IOHandler.java)
     public static final Symbol PROTON_IO_ERROR = Symbol.getSymbol("proton:io");
 }
