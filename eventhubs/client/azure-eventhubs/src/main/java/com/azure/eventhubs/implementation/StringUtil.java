7dcda737cb5e367cfb426577148eed1ea208e08d
Connie <conniey@microsoft.com>
2019-05-16 11:38:38 -0700

    Move StringUtil to ImplUtils.

--- eventhubs/client/azure-eventhubs/src/main/java/com/azure/eventhubs/implementation/StringUtil.java
+++ eventhubs/client/azure-eventhubs/src/main/java/com/azure/eventhubs/implementation/StringUtil.java
@@ -8,26 +8,6 @@ import java.util.Locale;
 import java.util.UUID;
 
 public final class StringUtil {
-    public static final String EMPTY = "";
-    public static final String SEPARATOR = "_";
-
-    public static boolean isNullOrEmpty(String string) {
-        return (string == null || string.isEmpty());
-    }
-
-    public static boolean isNullOrWhiteSpace(String string) {
-        if (string == null) {
-            return true;
-        }
-        for (int index = 0; index < string.length(); index++) {
-            if (!Character.isWhitespace(string.charAt(index))) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
     public static String getRandomString(String prefix) {
         return String.format(Locale.US, "%s_%s_%s", prefix, UUID.randomUUID().toString().substring(0, 6), Instant.now().toEpochMilli());
     }
