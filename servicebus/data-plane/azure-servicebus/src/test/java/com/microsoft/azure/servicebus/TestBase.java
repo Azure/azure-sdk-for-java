3e8e8e66d33d72a4c0ceeef8b29c55cfb9b3c626
shafang <Shawn.Fang@microsoft.com>
2019-05-20 15:23:50 -0700

    remove azure core test dependency completely

--- servicebus/data-plane/azure-servicebus/src/test/java/com/microsoft/azure/servicebus/TestBase.java
+++ servicebus/data-plane/azure-servicebus/src/test/java/com/microsoft/azure/servicebus/TestBase.java
@@ -3,7 +3,6 @@
 
 package com.microsoft.azure.servicebus;
 
-import com.azure.core.test.TestMode;
 import org.junit.Assume;
 import org.junit.BeforeClass;
 import org.slf4j.Logger;
@@ -12,27 +11,28 @@ import org.slf4j.LoggerFactory;
 import java.util.Locale;
 
 public class TestBase {
-
+    private static final String RECORD = "RECORD";
+    private static final String PLAYBACK = "PLAYBACK";
     private static final String AZURE_TEST_MODE = "AZURE_TEST_MODE";
 
     @BeforeClass
     public static void skipIfNotConfigured() {
-        Assume.assumeTrue("The test only runs in Live mode.", TestBase.getTestMode() == TestMode.RECORD);
+        Assume.assumeTrue("The test only runs in Live mode.", RECORD.equals(TestBase.getTestMode()));
     }
 
-    public static TestMode getTestMode() {
+    public static String getTestMode() {
 
         final Logger logger = LoggerFactory.getLogger(com.microsoft.azure.servicebus.TestBase.class);
         final String azureTestMode = System.getenv(AZURE_TEST_MODE);
 
         if (azureTestMode != null) {
             try {
-                return TestMode.valueOf(azureTestMode.toUpperCase(Locale.US));
+                return azureTestMode.toUpperCase(Locale.US);
             } catch (IllegalArgumentException e) {
                 if (logger.isErrorEnabled()) {
                     logger.error("Could not parse '{}' into TestEnum. Using 'Playback' mode.", azureTestMode);
                 }
-                return TestMode.PLAYBACK;
+                return PLAYBACK;
             }
         }
 
@@ -40,6 +40,6 @@ public class TestBase {
             logger.info("Environment variable '{}' has not been set yet. Using 'Playback' mode.", AZURE_TEST_MODE);
         }
 
-        return TestMode.PLAYBACK;
+        return PLAYBACK;
     }
 }
