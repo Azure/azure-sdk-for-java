2e40a8179f56add7fd837ef2719c61e15ffd4170
Connie <conniey@microsoft.com>
2019-06-06 11:52:30 -0700

    Adding documentation.

--- eventhubs/client/azure-eventhubs/src/main/java/com/azure/eventhubs/implementation/EventHubManagementNode.java
+++ eventhubs/client/azure-eventhubs/src/main/java/com/azure/eventhubs/implementation/EventHubManagementNode.java
@@ -9,8 +9,22 @@ import reactor.core.publisher.Mono;
 
 import java.io.Closeable;
 
+/**
+ * The management node for fetching metadata about the Event Hub and its partitions.
+ */
 public interface EventHubManagementNode extends Closeable {
+    /**
+     * Gets the metadata associated with the Event Hub.
+     *
+     * @return Metadata associated with the Event Hub.
+     */
     Mono<EventHubProperties> getEventHubProperties();
 
+    /**
+     * Gets the metadata associated with a particular partition in the Event Hub.
+     *
+     * @param partitionId The identifier of the partition.
+     * @return The metadata associated with the partition.
+     */
     Mono<PartitionProperties> getPartitionProperties(String partitionId);
 }
