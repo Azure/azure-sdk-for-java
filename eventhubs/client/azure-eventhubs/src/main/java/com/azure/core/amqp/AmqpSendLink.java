2574c5d87712026ab2c6de0d1773b3095d8a8024
Connie <conniey@microsoft.com>
2019-06-06 11:29:16 -0700

    Remove SendWithResponse.

--- eventhubs/client/azure-eventhubs/src/main/java/com/azure/core/amqp/AmqpSendLink.java
+++ eventhubs/client/azure-eventhubs/src/main/java/com/azure/core/amqp/AmqpSendLink.java
@@ -24,13 +24,4 @@ public interface AmqpSendLink extends AmqpLink {
      * @return A Mono that completes when all the messages have been sent.
      */
     Mono<Void> send(Publisher<Message> messages);
-
-    /**
-     * A send operation that returns associated responses from messages that were sent.
-     *
-     * @param messages Messages to send to the service.
-     * @param mappingFunction Function to map response {@link Message} to a result.
-     * @return A stream of mapped responses.
-     */
-    <T> Publisher<T> sendWithResponse(Publisher<Message> messages, Function<Message, T> mappingFunction);
 }
