e4b0feae088dada0c668867822f673dd2b5d81b1
Connie <conniey@microsoft.com>
2019-06-10 11:24:17 -0700

    Adding client-side error that could be thrown, OperationCancelledException.

---
+++ eventhubs/client/azure-eventhubs/src/main/java/com/azure/core/amqp/OperationCancelledException.java
@@ -0,0 +1,22 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.core.amqp;
+
+import com.azure.core.amqp.exception.AmqpException;
+
+/**
+ * This exception is thrown when the underlying AMQP layer encounter an abnormal link abort or disconnect of connection
+ * in an unexpected fashion.
+ */
+public class OperationCancelledException extends AmqpException {
+    private static final long serialVersionUID = 1L;
+
+    public OperationCancelledException(final String message) {
+        super(false, message);
+    }
+
+    public OperationCancelledException(final String message, final Throwable cause) {
+        super(false, message, cause);
+    }
+}
