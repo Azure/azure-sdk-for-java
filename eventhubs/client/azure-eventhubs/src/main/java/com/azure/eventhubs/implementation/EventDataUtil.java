637a2557ac80892c982b930f9795637ee66d2c8a
Connie <conniey@microsoft.com>
2019-06-11 14:17:36 -0700

    Move AmqpUtil methods into EventDataUtil.

--- eventhubs/client/azure-eventhubs/src/main/java/com/azure/eventhubs/implementation/EventDataUtil.java
+++ eventhubs/client/azure-eventhubs/src/main/java/com/azure/eventhubs/implementation/EventDataUtil.java
@@ -6,21 +6,34 @@ import com.azure.eventhubs.EventData;
 import org.apache.qpid.proton.Proton;
 import org.apache.qpid.proton.amqp.Binary;
 import org.apache.qpid.proton.amqp.Symbol;
+import org.apache.qpid.proton.amqp.messaging.AmqpValue;
 import org.apache.qpid.proton.amqp.messaging.ApplicationProperties;
 import org.apache.qpid.proton.amqp.messaging.Data;
 import org.apache.qpid.proton.amqp.messaging.MessageAnnotations;
 import org.apache.qpid.proton.message.Message;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
-class EventDataUtil {
+/**
+ * Utility class for converting {@link EventData} to {@link Message}.
+ */
+public class EventDataUtil {
+    /**
+     * Maps the set of events given to a collection of AMQP messages.
+     */
+    public static List<Message> toAmqpMessage(String partitionKey, List<EventData> events) {
+        return events.stream().map(event -> toAmqpMessage(partitionKey, event)).collect(Collectors.toList());
+    }
+
     /**
      * Creates the AMQP message represented by this EventData.
      *
      * @return A new AMQP message for this EventData.
      */
-    private static Message toAmqpMessage(EventData eventData, String partitionKey) {
+    public static Message toAmqpMessage(String partitionKey, EventData eventData) {
         final Message message = Proton.message();
 
         if (eventData.properties() != null && !eventData.properties().isEmpty()) {
@@ -42,6 +55,47 @@ class EventDataUtil {
         }
 
         return message;
+    }
+
+    /**
+     * Gets the serialized size of the AMQP message.
+     */
+    public static int getDataSerializedSize(Message amqpMessage) {
+
+        if (amqpMessage == null) {
+            return 0;
+        }
+
+        int payloadSize = getPayloadSize(amqpMessage);
+
+        // EventData - accepts only PartitionKey - which is a String & stuffed into MessageAnnotation
+        final MessageAnnotations messageAnnotations = amqpMessage.getMessageAnnotations();
+        final ApplicationProperties applicationProperties = amqpMessage.getApplicationProperties();
+
+        int annotationsSize = 0;
+        int applicationPropertiesSize = 0;
+
+        if (messageAnnotations != null) {
+            for (Symbol value : messageAnnotations.getValue().keySet()) {
+                annotationsSize += sizeof(value);
+            }
+
+            for (Object value : messageAnnotations.getValue().values()) {
+                annotationsSize += sizeof(value);
+            }
+        }
+
+        if (applicationProperties != null) {
+            for (Object value : applicationProperties.getValue().keySet()) {
+                applicationPropertiesSize += sizeof(value);
+            }
+
+            for (Object value : applicationProperties.getValue().values()) {
+                applicationPropertiesSize += sizeof(value);
+            }
+        }
+
+        return annotationsSize + applicationPropertiesSize + payloadSize;
     }
 
     /*
@@ -111,6 +165,74 @@ class EventDataUtil {
                 message.setMessageAnnotations(messageAnnotations);
             }
         });
+    }
+
+    private static int getPayloadSize(Message msg) {
+
+        if (msg == null || msg.getBody() == null) {
+            return 0;
+        }
+
+        if (msg.getBody() instanceof Data) {
+            final Data payloadSection = (Data) msg.getBody();
+            if (payloadSection == null) {
+                return 0;
+            }
+
+            final Binary payloadBytes = payloadSection.getValue();
+            if (payloadBytes == null) {
+                return 0;
+            }
+
+            return payloadBytes.getLength();
+        }
+
+        if (msg.getBody() instanceof AmqpValue) {
+            final AmqpValue amqpValue = (AmqpValue) msg.getBody();
+            if (amqpValue == null) {
+                return 0;
+            }
+
+            return amqpValue.getValue().toString().length() * 2;
+        }
+
+        return 0;
+    }
+
+    private static int sizeof(Object obj) {
+        if (obj instanceof String) {
+            return obj.toString().length() << 1;
+        }
+
+        if (obj instanceof Symbol) {
+            return ((Symbol) obj).length() << 1;
+        }
+
+        if (obj instanceof Integer) {
+            return Integer.BYTES;
+        }
+
+        if (obj instanceof Long) {
+            return Long.BYTES;
+        }
+
+        if (obj instanceof Short) {
+            return Short.BYTES;
+        }
+
+        if (obj instanceof Character) {
+            return Character.BYTES;
+        }
+
+        if (obj instanceof Float) {
+            return Float.BYTES;
+        }
+
+        if (obj instanceof Double) {
+            return Double.BYTES;
+        }
+
+        throw new IllegalArgumentException(String.format(Locale.US, "Encoding Type: %s is not supported", obj.getClass()));
     }
 
     private EventDataUtil() {
