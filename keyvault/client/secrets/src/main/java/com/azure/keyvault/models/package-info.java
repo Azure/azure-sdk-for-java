65633ce090f05d6f13bc2f177fc9bd3d2ba900c7
Srikanta <51379715+srnagar@users.noreply.github.com>
2019-06-11 12:26:43 -0700

    Update/Add package-info.java for all track 2 packages (#3875)

--- keyvault/client/secrets/src/main/java/com/azure/keyvault/models/package-info.java
+++ keyvault/client/secrets/src/main/java/com/azure/keyvault/models/package-info.java
@@ -1,6 +1,7 @@
 
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+
 /**
  * This package contains classes used for representing secrets, deleted secrets and their attributes in the Azure
  * Key Vault.
