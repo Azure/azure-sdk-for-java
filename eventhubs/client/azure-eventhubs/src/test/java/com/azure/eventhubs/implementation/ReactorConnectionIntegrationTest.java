7af466ae30b51a0ad5034ad77bf2497930a3ea1c
Connie <conniey@microsoft.com>
2019-06-11 16:53:37 -0700

    Verify that we can authorize with cbs-node.

---
+++ eventhubs/client/azure-eventhubs/src/test/java/com/azure/eventhubs/implementation/ReactorConnectionIntegrationTest.java
@@ -0,0 +1,63 @@
+package com.azure.eventhubs.implementation;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+import java.util.Locale;
+
+import static org.mockito.Mockito.mock;
+
+public class ReactorConnectionIntegrationTest extends ApiTestBase {
+    private ReactorHandlerProvider handlerProvider;
+
+    @Rule
+    public TestName testName = new TestName();
+    private ReactorConnection connection;
+
+    @Override
+    protected String testName() {
+        return testName.getMethodName();
+    }
+
+    @Override
+    protected void beforeTest() {
+        handlerProvider = new ReactorHandlerProvider(getReactorProvider());
+        connection = new ReactorConnection("test-connection-id", getConnectionParameters(), getReactorProvider(), handlerProvider, mock(AmqpResponseMapper.class));
+    }
+
+    @Override
+    protected void afterTest() {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+
+    @Test
+    public void getCbsNode() {
+        skipIfNotRecordMode();
+
+        StepVerifier.create(connection.getCBSNode())
+            .assertNext(node -> {
+                Assert.assertTrue(node instanceof CBSChannel);
+            })
+            .verifyComplete();
+    }
+
+    @Test
+    public void getCbsNodeAuthorize() {
+        skipIfNotRecordMode();
+
+        // Arrange
+        final String tokenAudience = String.format(Locale.US, ClientConstants.TOKEN_AUDIENCE_FORMAT,
+            getCredentialInfo().endpoint().getHost(), getCredentialInfo().eventHubPath());
+        final Duration tokenDuration = Duration.ofMinutes(5);
+
+        // Act & Assert.
+        StepVerifier.create(connection.getCBSNode().flatMap(node -> node.authorize(tokenAudience, tokenDuration)))
+            .verifyComplete();
+    }
+}
