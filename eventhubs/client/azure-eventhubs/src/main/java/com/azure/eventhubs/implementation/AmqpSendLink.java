e2261aa7cfc0eca58e55437d2b19e237b06e876d
Connie Yau <conniey@microsoft.com>
2019-06-12 23:50:39 -0700

    Apply suggestions from code review
    Applying doc fixes.
    
    Co-Authored-By: Jonathan Giles <jonathan@jonathangiles.net>

--- eventhubs/client/azure-eventhubs/src/main/java/com/azure/eventhubs/implementation/AmqpSendLink.java
+++ eventhubs/client/azure-eventhubs/src/main/java/com/azure/eventhubs/implementation/AmqpSendLink.java
@@ -19,7 +19,7 @@ public interface AmqpSendLink extends AmqpLink {
      *
      * @param message Message to send.
      * @return A Mono that completes when the message has been sent.
-     * @throws AmqpException if the serialised {@code message} exceed the link's capacity for a single message.
+     * @throws AmqpException if the serialised {@code message} exceed the links capacity for a single message.
      */
     Mono<Void> send(Message message);
 
@@ -27,7 +27,7 @@ public interface AmqpSendLink extends AmqpLink {
      * Batches the messages given into a single proton-j message that is sent down the wire.
      *
      * @param messageBatch The batch of messages to send to the service.
-     * @return A Mono that completes when all the batched message is successfully transmitted to Event Hub.
+     * @return A Mono that completes when all the batched messages are successfully transmitted to Event Hub.
      * @throws AmqpException if the serialised contents of {@code messageBatch} exceed the link's capacity for a single
      * message.
      */
