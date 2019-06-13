2ac0e73d6bb8da2d4fab1b4a203860de16382cad
Connie <conniey@microsoft.com>
2019-06-12 17:35:53 -0700

    Adding implementation of EventReceiver.

--- eventhubs/client/azure-eventhubs/src/main/java/com/azure/eventhubs/EventReceiver.java
+++ eventhubs/client/azure-eventhubs/src/main/java/com/azure/eventhubs/EventReceiver.java
@@ -3,8 +3,19 @@
 
 package com.azure.eventhubs;
 
+import com.azure.core.amqp.AmqpLink;
+import com.azure.core.implementation.logging.ServiceLogger;
+import com.azure.eventhubs.implementation.AmqpReceiveLink;
 import reactor.core.publisher.ConnectableFlux;
+import reactor.core.publisher.EmitterProcessor;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
@@ -15,17 +26,73 @@ import static java.nio.charset.StandardCharsets.UTF_8;
  * A {@link EventReceiver#receive()} is tied to a Event Hub PartitionId + consumer group combination.
  *
  * <ul>
- *      <li>If the {@link EventReceiver} is created where {@link EventReceiverOptions#exclusiveReceiverPriority()} has a
- *      value, then Event Hubs service will guarantee only 1 active receiver exists per partitionId and consumer group
- *      combination. This is the recommended approach to create a {@link EventReceiver}.</li>
- *      <li>Multiple receivers per partitionId and consumer group combination can be created using non-epoch receivers.</li>
+ * <li>If the {@link EventReceiver} is created where {@link EventReceiverOptions#exclusiveReceiverPriority()} has a
+ * value, then Event Hubs service will guarantee only 1 active receiver exists per partitionId and consumer group
+ * combination. This is the recommended approach to create a {@link EventReceiver}.</li>
+ * <li>Multiple receivers per partitionId and consumer group combination can be created using non-epoch receivers.</li>
  * </ul>
  *
  * @see EventHubClient#createReceiver(String)
  * @see EventHubClient#createReceiver(String, EventReceiverOptions)
  */
-public class EventReceiver {
-    private PartitionProperties partitionInformation = null;
+public class EventReceiver implements Closeable {
+    private static final AtomicReferenceFieldUpdater<EventReceiver, AmqpReceiveLink> RECEIVE_LINK_FIELD_UPDATER =
+        AtomicReferenceFieldUpdater.newUpdater(EventReceiver.class, AmqpReceiveLink.class, "receiveLink");
+
+    private final Mono<AmqpReceiveLink> receiveLinkMono;
+    private final EventReceiverOptions options;
+    private final Duration operationTimeout;
+    private final AtomicBoolean isDisposed = new AtomicBoolean();
+    private final ServiceLogger logger = new ServiceLogger(EventReceiver.class);
+    private final EmitterProcessor<EventData> emitterProcessor;
+    private Flux<EventData> eventDataConnectableFlux;
+
+    private volatile PartitionProperties partitionInformation = null;
+    private volatile AmqpReceiveLink receiveLink;
+
+    EventReceiver(Mono<AmqpReceiveLink> receiveLinkMono, EventReceiverOptions options, Duration operationTimeout) {
+        this.emitterProcessor = EmitterProcessor.create(options.prefetchCount(), false);
+
+        // Caching the created link so we don't invoke another link creation.
+        this.receiveLinkMono = receiveLinkMono.map(link -> {
+            if (RECEIVE_LINK_FIELD_UPDATER.compareAndSet(this, null, link)) {
+                logger.asInfo().log("Created AMQP receive link. Subscribing to events.");
+
+                //TODO (conniey): subscribe to on EndpointState and errors, etc.
+
+                eventDataConnectableFlux = link.receive().map(EventData::new).subscribeWith(emitterProcessor)
+                    .doOnSubscribe(subscription -> {
+                        logger.asInfo().log("Subscription received. Adding credits.");
+                    }).doOnRequest(request -> {
+                        logger.asInfo().log("More events requested: {}", request);
+                        final int credits = link.credits();
+                        if (credits < request) {
+                            logger.asInfo().log("Adding more credits. Current: [{}]. Requested: [{}]", credits, request);
+                            link.addCredits((int) request);
+                        }
+                    });
+            }
+
+            return RECEIVE_LINK_FIELD_UPDATER.get(this);
+        }).cache();
+        this.options = options;
+        this.operationTimeout = operationTimeout;
+    }
+
+    /**
+     * Disposes of the EventReceiver by closing the underlying connection to the service.
+     *
+     * @throws IOException if the underlying {@link AmqpLink} and its resources could not be disposed.
+     */
+    @Override
+    public void close() throws IOException {
+        if (!isDisposed.getAndSet(true)) {
+            final AmqpReceiveLink block = receiveLinkMono.block(operationTimeout);
+            if (block != null) {
+                block.close();
+            }
+        }
+    }
 
     /**
      * Gets the most recent information about a partition by the current receiver.
@@ -43,15 +110,6 @@ public class EventReceiver {
      * @return A stream of events for this receiver.
      */
     public Flux<EventData> receive() {
-        final ConnectableFlux<EventData> publish = receiveFromPartition().flatMap(x -> {
-            String data = String.format("Something %s", x);
-            return Flux.just(new EventData(data.getBytes(UTF_8)));
-        }).publish();
-
-        return publish.refCount();
-    }
-
-    private Flux<EventData> receiveFromPartition() {
-        return Flux.empty();
+        return eventDataConnectableFlux;
     }
 }
