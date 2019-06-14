0461585295c0d8d5b24a0759a66f3bfe490665be
Connie <conniey@microsoft.com>
2019-06-14 15:45:06 -0700

    Remove unused import.

--- eventhubs/client/azure-eventhubs/src/test/java/com/azure/eventhubs/implementation/ReactorReceiverTest.java
+++ eventhubs/client/azure-eventhubs/src/test/java/com/azure/eventhubs/implementation/ReactorReceiverTest.java
@@ -22,7 +22,6 @@ import java.time.Duration;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
