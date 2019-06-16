f46e81033ea0c5444ac6a31a98d039e5834ef074
Connie <conniey@microsoft.com>
2019-06-16 15:45:27 -0700

    Adding retry and error logic.

--- eventhubs/client/azure-eventhubs/src/main/java/com/azure/eventhubs/implementation/ActiveClientTokenManager.java
+++ eventhubs/client/azure-eventhubs/src/main/java/com/azure/eventhubs/implementation/ActiveClientTokenManager.java
@@ -4,7 +4,9 @@
 package com.azure.eventhubs.implementation;
 
 import com.azure.core.amqp.CBSNode;
+import com.azure.core.amqp.exception.AmqpException;
 import com.azure.core.amqp.exception.AmqpResponseCode;
+import com.azure.core.exception.AzureException;
 import com.azure.core.implementation.logging.ServiceLogger;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.FluxSink;
@@ -22,6 +24,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 class ActiveClientTokenManager implements Closeable {
     private final ServiceLogger logger = new ServiceLogger(ActiveClientTokenManager.class);
     private final AtomicBoolean hasScheduled = new AtomicBoolean();
+    private final AtomicBoolean hasDisposed = new AtomicBoolean();
     private final Mono<CBSNode> cbsNode;
     private final String tokenAudience;
     private final Duration refreshInterval;
@@ -34,7 +37,13 @@ class ActiveClientTokenManager implements Closeable {
         this.cbsNode = cbsNode;
         this.tokenAudience = tokenAudience;
         this.refreshInterval = refreshInterval;
-        this.authorizationResults = Flux.create(sink -> this.sink = sink);
+        this.authorizationResults = Flux.create(sink -> {
+            if (hasDisposed.get()) {
+                sink.complete();
+            } else {
+                this.sink = sink;
+            }
+        });
     }
 
     /**
@@ -51,6 +60,10 @@ class ActiveClientTokenManager implements Closeable {
      * Invokes an authorization call on the CBS node.
      */
     Mono<Void> authorize() {
+        if (hasDisposed.get()) {
+            return Mono.error(new AzureException("Cannot authorize with CBS node when this token manager has been disposed of."));
+        }
+
         return cbsNode.flatMap(cbsNode -> cbsNode.authorize(tokenAudience))
             .then()
             .doOnSuccess(x -> {
@@ -63,17 +76,41 @@ class ActiveClientTokenManager implements Closeable {
 
     @Override
     public void close() {
-        this.timer.cancel();
-        this.sink.complete();
+        if (!hasDisposed.getAndSet(true)) {
+            this.timer.cancel();
+
+            if (this.sink != null) {
+                this.sink.complete();
+            }
+        }
     }
 
     private class RefreshAuthorizationToken extends TimerTask {
         @Override
         public void run() {
+            logger.asInfo().log("Refreshing authorization token.");
             authorize().subscribe(
-                (Void response) -> sink.next(AmqpResponseCode.ACCEPTED),
-                error -> sink.error(error),
-                () -> sink.next(AmqpResponseCode.ACCEPTED));
+                (Void response) -> {
+                    logger.asInfo().log("Response acquired.");
+                }, error -> {
+                    if ((error instanceof AmqpException) && ((AmqpException) error).isTransient()) {
+                        logger.asError().log("Error is transient. Rescheduling authorization task.", error);
+                        timer.schedule(new RefreshAuthorizationToken(), refreshInterval.toMillis());
+                    } else {
+                        logger.asError().log("Error occurred while refreshing token that is not retriable. Not scheduling"
+                            + " refresh task. Use ActiveClientTokenManager.authorize() to schedule task again.", error);
+                        hasScheduled.set(false);
+                    }
+
+                    sink.error(error);
+                }, () -> {
+                    logger.asInfo().log("Success. Rescheduling refresh authorization task.");
+                    sink.next(AmqpResponseCode.ACCEPTED);
+
+                    if (hasScheduled.getAndSet(true)) {
+                        timer.schedule(new RefreshAuthorizationToken(), refreshInterval.toMillis());
+                    }
+                });
         }
     }
 }
---
+++ eventhubs/client/azure-eventhubs/src/test/java/com/azure/eventhubs/implementation/ActiveClientTokenManagerTest.java
@@ -0,0 +1,129 @@
+package com.azure.eventhubs.implementation;
+
+import com.azure.core.amqp.CBSNode;
+import com.azure.core.amqp.exception.AmqpException;
+import com.azure.core.amqp.exception.AmqpResponseCode;
+import com.azure.core.amqp.exception.ErrorCondition;
+import com.azure.core.exception.AzureException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+public class ActiveClientTokenManagerTest {
+    private static final String AUDIENCE = "an-audience-test";
+    private static Duration REFRESH_INTERVAL = Duration.ofSeconds(2);
+    private static Duration TIMEOUT = Duration.ofSeconds(4);
+
+    @Mock
+    private CBSNode cbsNode;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @After
+    public void teardown() {
+        Mockito.framework().clearInlineMocks();
+        cbsNode = null;
+    }
+
+    /**
+     * Verify that we can get successes and errors from CBS node.
+     */
+    @Test
+    public void getAuthorizationResults() {
+        // Arrange
+        final Mono<CBSNode> cbsNodeMono = Mono.fromCallable(() -> cbsNode);
+        when(cbsNode.authorize(any())).thenReturn(Mono.empty());
+
+        final ActiveClientTokenManager tokenManager = new ActiveClientTokenManager(cbsNodeMono, AUDIENCE, REFRESH_INTERVAL);
+
+        // Act & Assert
+        StepVerifier.create(tokenManager.getAuthorizationResults())
+            .then(() -> tokenManager.authorize().block(TIMEOUT))
+            .expectNext(AmqpResponseCode.ACCEPTED)
+            .expectNext(AmqpResponseCode.ACCEPTED)
+            .then(tokenManager::close)
+            .verifyComplete();
+    }
+
+    /**
+     * Verify that we can get successes and errors from CBS node. This un-retriable error will not allow it to be
+     * rescheduled.
+     */
+    @SuppressWarnings("unchecked")
+    @Test
+    public void getAuthorizationResultsSuccessFailure() {
+        // Arrange
+        final Mono<CBSNode> cbsNodeMono = Mono.fromCallable(() -> cbsNode);
+        final IllegalArgumentException error = new IllegalArgumentException("Some error");
+        when(cbsNode.authorize(any())).thenReturn(Mono.empty(), Mono.empty(), Mono.error(error));
+
+        // Act & Assert
+        try (ActiveClientTokenManager tokenManager = new ActiveClientTokenManager(cbsNodeMono, AUDIENCE, REFRESH_INTERVAL)) {
+            StepVerifier.create(tokenManager.getAuthorizationResults())
+                .then(() -> tokenManager.authorize().block(TIMEOUT))
+                .expectNext(AmqpResponseCode.ACCEPTED)
+                .expectError(IllegalArgumentException.class)
+                .verifyThenAssertThat()
+                .hasNotDroppedElements()
+                .hasNotDroppedElements()
+                .hasNotDroppedErrors();
+        }
+    }
+
+    /**
+     * Verify that we cannot authorize with CBS node when it has already been disposed of.
+     */
+    @Test
+    public void cannotAuthorizeDisposedInstance() {
+        // Arrange
+        final Mono<CBSNode> cbsNodeMono = Mono.fromCallable(() -> cbsNode);
+        when(cbsNode.authorize(any())).thenReturn(Mono.empty());
+
+        final ActiveClientTokenManager tokenManager = new ActiveClientTokenManager(cbsNodeMono, AUDIENCE, REFRESH_INTERVAL);
+        tokenManager.authorize().then(Mono.fromRunnable(tokenManager::close)).block();
+
+        // Act & Assert
+        StepVerifier.create(tokenManager.authorize())
+            .expectError(AzureException.class)
+            .verify();
+    }
+
+    /**
+     * Verify that the ActiveClientTokenManager reschedules the authorization task.
+     */
+    @SuppressWarnings("unchecked")
+    @Test
+    public void getAuthorizationResultsRetriableError() {
+        // Arrange
+        final Mono<CBSNode> cbsNodeMono = Mono.fromCallable(() -> cbsNode);
+        final AmqpException error = new AmqpException(true, ErrorCondition.TIMEOUT_ERROR, "Timed out");
+        when(cbsNode.authorize(any())).thenReturn(Mono.empty(), Mono.error(error), Mono.empty(), Mono.empty(), Mono.empty());
+
+        // Act & Assert
+        try (ActiveClientTokenManager tokenManager = new ActiveClientTokenManager(cbsNodeMono, AUDIENCE, REFRESH_INTERVAL)) {
+            StepVerifier.create(tokenManager.getAuthorizationResults())
+                .then(() -> tokenManager.authorize().block(TIMEOUT))
+                .expectError(AmqpException.class)
+                .verify();
+
+            StepVerifier.create(tokenManager.getAuthorizationResults())
+                .expectNext(AmqpResponseCode.ACCEPTED)
+                .expectNext(AmqpResponseCode.ACCEPTED)
+                .then(tokenManager::close)
+                .verifyComplete();
+        }
+    }
+}
