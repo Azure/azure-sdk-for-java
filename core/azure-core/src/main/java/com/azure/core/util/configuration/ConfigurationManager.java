0bb07ff78550dfdabda332b895905bc3292f47c5
Hemant Tanwar <hemant_tanwar@hotmail.com>
2019-06-17 18:42:42 -0700

    move core configuration to util configuration package[Draft] (#3958)
    * move core configuration to util.configuration package

--- core/azure-core/src/main/java/com/azure/core/util/configuration/ConfigurationManager.java
+++ core/azure-core/src/main/java/com/azure/core/util/configuration/ConfigurationManager.java
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.azure.core.configuration;
+package com.azure.core.util.configuration;
 
 /**
  * Manages the global configuration store.
--- core/azure-core/src/main/java/com/azure/core/util/configuration/NoopConfiguration.java
+++ core/azure-core/src/main/java/com/azure/core/util/configuration/NoopConfiguration.java
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-package com.azure.core.configuration;
+package com.azure.core.util.configuration;
 
 import java.util.function.Function;
 
