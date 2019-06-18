965c5f04aa6335684e3cb8d742a7486aa77efaf8
James Birdsall <jbird@microsoft.com>
2019-06-17 17:46:11 -0700

    Fix Managed Identity provider

--- eventhubs/data-plane/azure-eventhubs/src/main/java/com/microsoft/azure/eventhubs/ManagedIdentityTokenProvider.java
+++ eventhubs/data-plane/azure-eventhubs/src/main/java/com/microsoft/azure/eventhubs/ManagedIdentityTokenProvider.java
@@ -7,15 +7,16 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
 
 import com.microsoft.azure.credentials.MSICredentials;
+import com.microsoft.azure.eventhubs.impl.ClientConstants;
 
 public class ManagedIdentityTokenProvider implements ITokenProvider {
+	final static MSICredentials credentials = new MSICredentials();
+	
     @Override
     public CompletableFuture<SecurityToken> getToken(final String resource, final Duration timeout) {
-    	final MSICredentials credentials = new MSICredentials();
-
     	return CompletableFuture.supplyAsync(() -> {
 			try {
-				String rawToken = credentials.getToken(resource);
+				String rawToken = ManagedIdentityTokenProvider.credentials.getToken(ClientConstants.EVENTHUBS_AUDIENCE);
 				return new JsonSecurityToken(rawToken, resource);
 			} catch (IOException | ParseException e) {
 				throw new CompletionException(e);
