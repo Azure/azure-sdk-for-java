be6f4f65814c041f90d14816786504c8c4e6095e
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2019-06-19 12:09:49 -0700

    Javadocs for rest of the model classes

--- storage/client/src/main/java/com/azure/storage/queue/models/QueueProperties.java
+++ storage/client/src/main/java/com/azure/storage/queue/models/QueueProperties.java
@@ -4,28 +4,34 @@ package com.azure.storage.queue.models;
 
 import java.util.Map;
 
+/**
+ * Model class containing properties of a specific queue in the storage Queue service.
+ */
 public final class QueueProperties {
-    /**
-     * The metadata property.
-     */
     private final Map<String, String> metadata;
-
-    /**
-     * The approximate number of messages in the queue. This number is not
-     * lower than the actual number of messages in the queue, but could be
-     * higher.
-     */
     private final int approximateMessagesCount;
 
+    /**
+     * Creates an instance that contains properties of a queue.
+     *
+     * @param metadata Metadata associated with the queue
+     * @param approximateMessagesCount Approximate number of messages contained in the queue
+     */
     public QueueProperties(Map<String, String> metadata, int approximateMessagesCount) {
         this.metadata = metadata;
         this.approximateMessagesCount = approximateMessagesCount;
     }
 
+    /**
+     * @return the user-defined metadata associated with the queue
+     */
     public Map<String, String> metadata() {
-        return this.metadata; // Clone this?
+        return this.metadata;
     }
 
+    /**
+     * @return the approximate number of messages contained in the queue at the time of properties retrieval
+     */
     public int approximateMessagesCount() {
         return approximateMessagesCount;
     }
--- storage/client/src/main/java/com/azure/storage/queue/models/UpdatedMessage.java
+++ storage/client/src/main/java/com/azure/storage/queue/models/UpdatedMessage.java
@@ -4,27 +4,35 @@ package com.azure.storage.queue.models;
 
 import java.time.OffsetDateTime;
 
+/**
+ * Response from the service when updating a message. Contains the information needed to continue working with
+ * the specific message.
+ */
 public final class UpdatedMessage {
-    /*
-     * The pop receipt of the queue message.
-     */
     private final String popReceipt;
-
-    /*
-     * A UTC date/time value that represents when the message will be visible
-     * on the queue.
-     */
     private final OffsetDateTime timeNextVisible;
 
+    /**
+     * Creates an instance of updated message information.
+     *
+     * @param popReceipt Unique identifier that verifies the operation on a message is valid
+     * @param timeNextVisible The next time the message will be visible to other operations in the queue
+     */
     public UpdatedMessage(String popReceipt, OffsetDateTime timeNextVisible) {
         this.popReceipt = popReceipt;
         this.timeNextVisible = timeNextVisible;
     }
 
+    /**
+     * @return the unique identifier used to verify that the operation is allowed on the message
+     */
     public String popReceipt() {
         return popReceipt;
     }
 
+    /**
+     * @return the next time the message will be visible to other operations in the queue
+     */
     public OffsetDateTime timeNextVisible() {
         return timeNextVisible;
     }
