89da622ad289089196a8a4680a62e939da4f4acd
Connie <conniey@microsoft.com>
2019-06-20 12:24:53 -0700

    Moving OperationCancelledException to azure-core-amqp

--- core/azure-core-amqp/src/test/java/com/azure/core/amqp/exception/OperationCancelledExceptionTest.java
+++ core/azure-core-amqp/src/test/java/com/azure/core/amqp/exception/OperationCancelledExceptionTest.java
@@ -1,16 +1,22 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+<<<<<<< HEAD:eventhubs/client/azure-eventhubs/src/test/java/com/azure/messaging/eventhubs/OperationCancelledExceptionTest.java
 package com.azure.messaging.eventhubs;
+=======
+package com.azure.core.amqp.exception;
+>>>>>>> 433c62408b... Moving OperationCancelledException to azure-core-amqp:core/azure-core-amqp/src/test/java/com/azure/core/amqp/exception/OperationCancelledExceptionTest.java
 
 import org.junit.Assert;
 import org.junit.Test;
 
 public class OperationCancelledExceptionTest {
+    private final ErrorContext errorContext = new ErrorContext("Test namespace");
+
     @Test
     public void correctMessage() {
         final String message = "A test message.";
-        final OperationCancelledException exception = new OperationCancelledException(message);
+        final OperationCancelledException exception = new OperationCancelledException(message, errorContext);
 
         Assert.assertEquals(message, exception.getMessage());
     }
@@ -22,7 +28,7 @@ public class OperationCancelledExceptionTest {
         final Throwable innerException = new IllegalArgumentException("An argument");
 
         // Act
-        final OperationCancelledException exception = new OperationCancelledException(message, innerException);
+        final OperationCancelledException exception = new OperationCancelledException(message, innerException, errorContext);
 
         // Arrange
         Assert.assertEquals(message, exception.getMessage());
