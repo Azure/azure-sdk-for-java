a6f5dde8cce9dbae27a706560bfaebccf8eb400b
Connie <conniey@microsoft.com>
2019-06-20 11:33:27 -0700

    Fix documentation on ErrorContext

--- core/azure-core-amqp/src/main/java/com/azure/core/amqp/exception/ErrorContext.java
+++ core/azure-core-amqp/src/main/java/com/azure/core/amqp/exception/ErrorContext.java
@@ -2,17 +2,25 @@
 // Licensed under the MIT License.
 package com.azure.core.amqp.exception;
 
+import com.azure.core.amqp.AmqpConnection;
+import com.azure.core.amqp.AmqpLink;
+import com.azure.core.amqp.AmqpSession;
 import com.azure.core.implementation.util.ImplUtils;
 
 import java.io.Serializable;
 import java.util.Locale;
 
 /**
- * Provides context for an {@link AmqpException}.
+ * Provides context for an {@link AmqpException} that occurs in an {@link AmqpConnection}, {@link AmqpSession},
+ * or {@link AmqpLink}.
  *
  * @see AmqpException
+ * @see SessionErrorContext
+ * @see LinkErrorContext
  */
 public class ErrorContext implements Serializable {
+    static final String MESSAGE_PARAMETER_DELIMITER = ", ";
+
     private static final long serialVersionUID = -2819764407122954922L;
 
     private final String namespaceName;
@@ -20,8 +28,7 @@ public class ErrorContext implements Serializable {
     /**
      * Creates a new instance with the provided {@code namespaceName}.
      *
-     * @param namespaceName Event Hub namespace of the error context.
-     * @throws NullPointerException when {@code exception} is {@code null}.
+     * @param namespaceName The service namespace of the error.
      * @throws IllegalArgumentException when {@code namespaceName} is {@code null} or empty.
      */
     public ErrorContext(String namespaceName) {
@@ -37,15 +44,17 @@ public class ErrorContext implements Serializable {
      *
      * @return The namespace for this error.
      */
-    public String namespaceName() {
+    public String getNamespace() {
         return namespaceName;
     }
 
     /**
-     * {@inheritDoc}
+     * Creates a string representation of this ErrorContext.
+     *
+     * @return A string representation of this ErrorContext.
      */
     @Override
     public String toString() {
-        return String.format(Locale.US, "Namespace: %s.", namespaceName);
+        return String.format(Locale.US, "NAMESPACE: %s", getNamespace());
     }
 }
