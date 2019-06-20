d87dc41725368f2f118fafb91b6359b184d9d6d6
Connie Yau <conniey@microsoft.com>
2019-06-20 15:02:20 -0700

    Update Event Hubs namespace (#4018)
    * Move packages to new namespace
    
    * Rename package to com.azure.messaging.eventhubs
    
    * Adding azure-messaging-eventhubs to spotbugs report, javadoc, and jacoco.

---
+++ eventhubs/client/azure-eventhubs/src/main/java/com/azure/messaging/eventhubs/EventHubConsumer.java
@@ -0,0 +1,121 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.messaging.eventhubs;
+
+import com.azure.core.util.logging.ClientLogger;
+import com.azure.messaging.eventhubs.implementation.AmqpReceiveLink;
+import reactor.core.publisher.EmitterProcessor;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
+
+/**
+ * This is a logical representation of receiving from an Event Hub partition.
+ *
+ * <p>
+ * A {@link EventHubConsumer#receive()} is tied to a Event Hub partitionId + consumer group combination.
+ *
+ * <ul>
+ * <li>If {@link EventHubConsumer} is created where {@link EventHubConsumerOptions#ownerLevel()} has a
+ * value, then Event Hubs service will guarantee only 1 active receiver exists per partitionId and consumer group
+ * combination. This is the recommended approach to create a {@link EventHubConsumer}.</li>
+ * <li>Multiple consumers per partitionId and consumer group combination can be created by not setting
+ * {@link EventHubConsumerOptions#ownerLevel()} when creating receivers.</li>
+ * </ul>
+ *
+ * @see EventHubClient#createConsumer(String, String, EventPosition)
+ * @see EventHubClient#createConsumer(String, String, EventPosition, EventHubConsumerOptions)
+ */
+public class EventHubConsumer implements Closeable {
+    private static final AtomicReferenceFieldUpdater<EventHubConsumer, AmqpReceiveLink> RECEIVE_LINK_FIELD_UPDATER =
+        AtomicReferenceFieldUpdater.newUpdater(EventHubConsumer.class, AmqpReceiveLink.class, "receiveLink");
+
+    private final Mono<AmqpReceiveLink> receiveLinkMono;
+    private final Duration operationTimeout;
+    private final AtomicInteger creditsToRequest = new AtomicInteger(1);
+    private final AtomicBoolean isDisposed = new AtomicBoolean();
+    private final ClientLogger logger = new ClientLogger(EventHubConsumer.class);
+    private final EmitterProcessor<EventData> emitterProcessor;
+    private final Flux<EventData> messageFlux;
+
+    private volatile PartitionProperties partitionInformation = null;
+    private volatile AmqpReceiveLink receiveLink;
+
+    EventHubConsumer(Mono<AmqpReceiveLink> receiveLinkMono, EventHubConsumerOptions options, Duration operationTimeout) {
+        this.receiveLinkMono = receiveLinkMono;
+        this.emitterProcessor = EmitterProcessor.create(options.prefetchCount(), false);
+        this.operationTimeout = operationTimeout;
+
+        // Caching the created link so we don't invoke another link creation.
+        this.messageFlux = receiveLinkMono.flatMapMany(link -> {
+            if (RECEIVE_LINK_FIELD_UPDATER.compareAndSet(this, null, link)) {
+                logger.asInfo().log("Created AMQP receive link. Initialising prefetch credit: {}", options.prefetchCount());
+                link.addCredits(options.prefetchCount());
+
+                link.setEmptyCreditListener(() -> {
+                    if (emitterProcessor.hasDownstreams()) {
+                        return creditsToRequest.get();
+                    } else {
+                        logger.asVerbose().log("Emitter has no downstream subscribers. Not adding credits.");
+                        return 0;
+                    }
+                });
+            }
+
+            return link.receive().map(EventData::new);
+        }).subscribeWith(emitterProcessor);
+
+        emitterProcessor.doOnSubscribe(subscription -> {
+            if (receiveLink.getCredits() == 0) {
+                logger.asInfo().log("Subscription received and there are no remaining credits on the link. Adding more.");
+                receiveLink.addCredits(creditsToRequest.get());
+            }
+        }).doOnRequest(request -> {
+            logger.asInfo().log("Back pressure requested. Old value: {}. New value: {}", creditsToRequest.get(), request);
+            creditsToRequest.set((int) request);
+        });
+    }
+
+    /**
+     * Disposes of the consumer by closing the underlying connection to the service.
+     *
+     * @throws IOException if the underlying transport and its resources could not be disposed.
+     */
+    @Override
+    public void close() throws IOException {
+        if (!isDisposed.getAndSet(true)) {
+            final AmqpReceiveLink receiveLink = RECEIVE_LINK_FIELD_UPDATER.getAndSet(this, null);
+            if (receiveLink != null) {
+                receiveLink.close();
+            }
+
+            emitterProcessor.dispose();
+        }
+    }
+
+    /**
+     * Gets the most recent information about a partition by the current receiver.
+     *
+     * @return If {@link EventHubConsumerOptions}
+     */
+    public PartitionProperties partitionInformation() {
+        return partitionInformation;
+    }
+
+    /**
+     * Begin receiving events until there are no longer any subscribers, or the parent
+     * {@link EventHubClient#close() EventHubClient.close()} is called.
+     *
+     * @return A stream of events for this receiver.
+     */
+    public Flux<EventData> receive() {
+        return messageFlux;
+    }
+}
---
+++ eventhubs/client/azure-eventhubs/src/main/java/com/azure/messaging/eventhubs/implementation/handler/LinkHandler.java
@@ -0,0 +1,110 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.messaging.eventhubs.implementation.handler;
+
+import com.azure.core.amqp.exception.ErrorContext;
+import com.azure.core.amqp.exception.ExceptionUtil;
+import com.azure.core.util.logging.ClientLogger;
+import com.azure.messaging.eventhubs.implementation.ClientConstants;
+import org.apache.qpid.proton.amqp.transport.ErrorCondition;
+import org.apache.qpid.proton.engine.EndpointState;
+import org.apache.qpid.proton.engine.Event;
+import org.apache.qpid.proton.engine.Link;
+import org.apache.qpid.proton.engine.Session;
+
+class LinkHandler extends Handler {
+
+    ClientLogger logger;
+
+    LinkHandler(final String connectionId, final String hostname, ClientLogger logger) {
+        super(connectionId, hostname);
+        this.logger = logger;
+    }
+
+    @Override
+    public void onLinkLocalClose(Event event) {
+        final Link link = event.getLink();
+        final ErrorCondition condition = link.getCondition();
+
+        logger.asInfo().log("onLinkLocalClose connectionId[{}], linkName[{}], errorCondition[{}], errorDescription[{}]",
+            getConnectionId(), link.getName(),
+            condition != null ? condition.getCondition() : ClientConstants.NOT_APPLICABLE,
+            condition != null ? condition.getDescription() : ClientConstants.NOT_APPLICABLE);
+
+        closeSession(link, link.getCondition());
+    }
+
+    @Override
+    public void onLinkRemoteClose(Event event) {
+        final Link link = event.getLink();
+        final ErrorCondition condition = link.getRemoteCondition();
+
+        logger.asInfo().log("onLinkRemoteClose connectionId[{}], linkName[{}], errorCondition[{}], errorDescription[{}]",
+            getConnectionId(), link.getName(),
+            condition != null ? condition.getCondition() : ClientConstants.NOT_APPLICABLE,
+            condition != null ? condition.getDescription() : ClientConstants.NOT_APPLICABLE);
+
+        handleRemoteLinkClosed(event);
+    }
+
+    @Override
+    public void onLinkRemoteDetach(Event event) {
+        final Link link = event.getLink();
+        final ErrorCondition condition = link.getCondition();
+
+        logger.asInfo().log("onLinkRemoteClose connectionId[{}], linkName[{}], errorCondition[{}], errorDescription[{}]",
+            getConnectionId(), link.getName(),
+            condition != null ? condition.getCondition() : ClientConstants.NOT_APPLICABLE,
+            condition != null ? condition.getDescription() : ClientConstants.NOT_APPLICABLE);
+
+        handleRemoteLinkClosed(event);
+    }
+
+    @Override
+    public void onLinkFinal(Event event) {
+        logger.asInfo().log("onLinkFinal clientName[{}],  linkName[{}]", getConnectionId(), event.getLink().getName());
+        close();
+    }
+
+    private void processOnClose(Link link, ErrorCondition condition) {
+        logger.asInfo().log("processOnClose connectionId[{}], linkName[{}], errorCondition[{}], errorDescription[{}]",
+            getConnectionId(), link.getName(),
+            condition != null ? condition.getCondition() : ClientConstants.NOT_APPLICABLE,
+            condition != null ? condition.getDescription() : ClientConstants.NOT_APPLICABLE);
+
+        if (condition != null) {
+            final Throwable exception = ExceptionUtil.toException(condition.getCondition().toString(), condition.getDescription());
+            onNext(new ErrorContext(exception, getHostname()));
+        }
+
+        onNext(EndpointState.CLOSED);
+    }
+
+    private void closeSession(Link link, ErrorCondition condition) {
+        final Session session = link.getSession();
+
+        if (session != null && session.getLocalState() != EndpointState.CLOSED) {
+            logger.asInfo().log("closeSession connectionId[{}], linkName[{}], errorCondition[{}], errorDescription[{}]",
+                getConnectionId(), link.getName(),
+                condition != null ? condition.getCondition() : ClientConstants.NOT_APPLICABLE,
+                condition != null ? condition.getDescription() : ClientConstants.NOT_APPLICABLE);
+
+            session.setCondition(condition);
+            session.close();
+        }
+    }
+
+    private void handleRemoteLinkClosed(final Event event) {
+        final Link link = event.getLink();
+        final ErrorCondition condition = link.getRemoteCondition();
+
+        if (link.getLocalState() != EndpointState.CLOSED) {
+            link.setCondition(condition);
+            link.close();
+        }
+
+        processOnClose(link, condition);
+        closeSession(link, condition);
+    }
+}
