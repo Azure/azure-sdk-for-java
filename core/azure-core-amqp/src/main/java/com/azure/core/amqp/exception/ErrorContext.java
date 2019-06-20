5a827c09419641d8592504ca701f699f330f981c
Connie <conniey@microsoft.com>
2019-06-20 10:53:47 -0700

    ErrorContext no longer knows about Throwable. Removes that circular dependency.

--- core/azure-core-amqp/src/main/java/com/azure/core/amqp/exception/ErrorContext.java
+++ core/azure-core-amqp/src/main/java/com/azure/core/amqp/exception/ErrorContext.java
@@ -6,7 +6,6 @@ import com.azure.core.implementation.util.ImplUtils;
 
 import java.io.Serializable;
 import java.util.Locale;
-import java.util.Objects;
 
 /**
  * Provides context for an {@link AmqpException}.
@@ -17,25 +16,20 @@ public class ErrorContext implements Serializable {
     private static final long serialVersionUID = -2819764407122954922L;
 
     private final String namespaceName;
-    private final Throwable exception;
 
     /**
      * Creates a new instance with the provided {@code namespaceName}.
      *
-     * @param exception Exception that caused this error.
      * @param namespaceName Event Hub namespace of the error context.
      * @throws NullPointerException when {@code exception} is {@code null}.
      * @throws IllegalArgumentException when {@code namespaceName} is {@code null} or empty.
      */
-    public ErrorContext(final Throwable exception, final String namespaceName) {
-        Objects.requireNonNull(exception);
-
+    public ErrorContext(String namespaceName) {
         if (ImplUtils.isNullOrEmpty(namespaceName)) {
             throw new IllegalArgumentException("'namespaceName' cannot be null or empty");
         }
 
         this.namespaceName = namespaceName;
-        this.exception = exception;
     }
 
     /**
@@ -48,19 +42,10 @@ public class ErrorContext implements Serializable {
     }
 
     /**
-     * Gets the exception wrapped in this context.
-     *
-     * @return The exception that caused the error.
-     */
-    public Throwable exception() {
-        return exception;
-    }
-
-    /**
      * {@inheritDoc}
      */
     @Override
     public String toString() {
-        return String.format(Locale.US, "NS: %s. Exception: %s", namespaceName, exception);
+        return String.format(Locale.US, "Namespace: %s.", namespaceName);
     }
 }
