ceae510678c8a1c9525e18ef6ab88aaabd41b198
Sima Zhu <48036328+sima-zhu@users.noreply.github.com>
2019-06-21 19:04:00 +0000

    Azure core fix for storage playback tests. (#4035)
    * Make changes to not override the header content length

--- core/azure-core-test/src/main/java/com/azure/core/test/http/PlaybackClient.java
+++ core/azure-core-test/src/main/java/com/azure/core/test/http/PlaybackClient.java
@@ -100,7 +100,7 @@ public final class PlaybackClient implements HttpClient {
         HttpHeaders headers = new HttpHeaders();
 
         for (Map.Entry<String, String> pair : networkCallRecord.response().entrySet()) {
-            if (!pair.getKey().equals("StatusCode") && !pair.getKey().equals("Body") && !pair.getKey().equals("Content-Length")) {
+            if (!pair.getKey().equals("StatusCode") && !pair.getKey().equals("Body")) {
                 String rawHeader = pair.getValue();
                 for (Map.Entry<String, String> rule : textReplacementRules.entrySet()) {
                     if (rule.getValue() != null) {
@@ -122,7 +122,9 @@ public final class PlaybackClient implements HttpClient {
             }
 
             bytes = rawBody.getBytes(StandardCharsets.UTF_8);
-            headers.put("Content-Length", String.valueOf(bytes.length));
+            if (bytes.length > 0) {
+                headers.put("Content-Length", String.valueOf(bytes.length));
+            }
         }
 
         HttpResponse response = new MockHttpResponse(request, recordStatusCode, headers, bytes);
