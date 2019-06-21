d82ac4e9ba470705633c096099990cffb22faf74
Srikanta Nagaraja <srnagar@microsoft.com>
2019-06-21 12:44:26 -0700

    Update unit tests and javadocs

--- core/azure-core/src/main/java/com/azure/core/http/rest/PagedFlux.java
+++ core/azure-core/src/main/java/com/azure/core/http/rest/PagedFlux.java
@@ -10,18 +10,22 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 /**
- * TODO:
- * @param <T> The type for paginated flux
+ * This class is a flux that can operate on a {@link PagedResponse} and
+ * also provides the ability to operate on individual items.
+ *
+ * When used with paged responses, each response will contain the items in the page as
+ * well as the request details like status code and headers
+ *
+ * @param <T> The type of items in a page
  */
 public class PagedFlux<T> extends Flux<T> {
-
     private final Mono<PagedResponse<T>> firstPage;
     private final Function<String, Mono<PagedResponse<T>>> pager;
 
     /**
-     * TODO
-     *
-     * @param pager Function to get the next page for a given next page link
+     * Creates an instance of {@link PagedFlux}
+     * @param firstPage The first page
+     * @param pager Function that retrieves the page for a given continuation token
      */
     public PagedFlux(Mono<PagedResponse<T>> firstPage,
         Function<String, Mono<PagedResponse<T>>> pager) {
@@ -29,18 +33,22 @@ public class PagedFlux<T> extends Flux<T> {
         this.pager = pager;
     }
 
+    /**
+     * Creates a flux of {@link PagedResponse} starting from the given continuation token
+     * @param continuationToken The continuation token from which to start the flux
+     * @return A {@link PagedFlux} starting from the continuation token
+     */
     public Flux<PagedResponse<T>> byPage(String continuationToken) {
-        Mono<PagedResponse<T>> page = pager.apply(continuationToken);
-        return page.flatMapMany(pagedResponse -> extractAndFetchPage(pagedResponse));
+        return pager.apply(continuationToken).flatMapMany(this::extractAndFetchPage);
     }
 
     /**
-     * TODO:
+     * Creates a flux of {@link PagedResponse} starting from the first page
      *
-     * @return Returns a flux of paged response
+     * @return A {@link PagedFlux} starting from the first page
      */
     public Flux<PagedResponse<T>> byPage() {
-        return firstPage.flatMapMany(r -> extractAndFetchPage(r));
+        return firstPage.flatMapMany(this::extractAndFetchPage);
     }
 
     /**
@@ -52,26 +60,44 @@ public class PagedFlux<T> extends Flux<T> {
         byT(null).subscribe(coreSubscriber);
     }
 
-    private Flux<T> byT(String nextPageLink) {
-        if (nextPageLink == null) {
-            return firstPage.flatMapMany(page -> extractAndFetchT(page));
+    /**
+     * Helper method to return the flux of items contained in the page associated with
+     * given continuation token
+     * @param continuationToken The continuation token that is used to fetch the next page
+     * @return A {@link Flux} of items in this page
+     */
+    private Flux<T> byT(String continuationToken) {
+        if (continuationToken == null) {
+            return firstPage.flatMapMany(this::extractAndFetchT);
         }
-        return pager.apply(nextPageLink).flatMapMany(page -> extractAndFetchT(page));
+        return pager.apply(continuationToken).flatMapMany(this::extractAndFetchT);
     }
 
+    /**
+     * Helper method to string together a flux of items transparently extracting items from
+     * next pages, if available.
+     * @param page Starting page
+     * @return A {@link Flux} of items
+     */
     private Publisher<T> extractAndFetchT(PagedResponse<T> page) {
-        String nextPageLink = page.nextLink();
-        if (nextPageLink == null) {
+        String continuationToken = page.nextLink();
+        if (continuationToken == null) {
             return Flux.fromIterable(page.items());
         }
-        return Flux.fromIterable(page.items()).concatWith(byT(nextPageLink));
+        return Flux.fromIterable(page.items()).concatWith(byT(continuationToken));
     }
 
-    private Publisher<? extends PagedResponse<T>> extractAndFetchPage(PagedResponse<T> r) {
-        String nextPageLink = r.nextLink();
-        if (nextPageLink == null) {
-            return Flux.just(r);
+    /**
+     * Helper method to string together a flux of {@link PagedResponse} transparently
+     * fetching next pages, if available
+     * @param page Starting page
+     * @return A {@link Flux} of {@link PagedResponse}
+     */
+    private Publisher<? extends PagedResponse<T>> extractAndFetchPage(PagedResponse<T> page) {
+        String continuationToken = page.nextLink();
+        if (continuationToken == null) {
+            return Flux.just(page);
         }
-        return Flux.just(r).concatWith(byPage(nextPageLink));
+        return Flux.just(page).concatWith(byPage(continuationToken));
     }
 }
--- core/azure-core/src/test/java/com/azure/core/http/rest/PagedFluxTest.java
+++ core/azure-core/src/test/java/com/azure/core/http/rest/PagedFluxTest.java
@@ -17,13 +17,22 @@ import org.junit.Test;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+/**
+ * Unit tests for {@link PagedFlux}
+ */
 public class PagedFluxTest {
-
     private List<PagedResponse<Integer>> pagedResponses;
+
+    @Test
+    public void testEmptyFirstPage() {
+        PagedFlux<Integer> pagedFlux = new PagedFlux<>(Mono.empty(),
+            pageLink -> getNextPage(pageLink, pagedResponses));
+        StepVerifier.create(pagedFlux).verifyComplete();
+    }
+
     @Test
     public void testPagedFluxSubscribeToItems() throws MalformedURLException {
         PagedFlux<Integer> pagedFlux = getIntegerPagedFlux();
-        pagedFlux.log().subscribe(x -> System.out.println(x));
         StepVerifier.create(pagedFlux)
             .expectNext(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14)
             .verifyComplete();
@@ -32,7 +41,6 @@ public class PagedFluxTest {
     @Test
     public void testPagedFluxSubscribeToPages() throws MalformedURLException {
         PagedFlux<Integer> pagedFlux = getIntegerPagedFlux();
-        pagedFlux.byPage().log().subscribe(x -> System.out.println(x.items()));
         StepVerifier.create(pagedFlux.byPage())
             .expectNext(pagedResponses.get(0), pagedResponses.get(1), pagedResponses.get(2),
                 pagedResponses.get(3), pagedResponses.get(4))
@@ -42,30 +50,33 @@ public class PagedFluxTest {
     @Test
     public void testPagedFluxSubscribeToPageWithContinuationToken() throws MalformedURLException {
         PagedFlux<Integer> pagedFlux = getIntegerPagedFlux();
-        pagedFlux.byPage("3").log().subscribe(x -> System.out.println(x.items()));
         StepVerifier.create(pagedFlux.byPage("3"))
             .expectNext(pagedResponses.get(3), pagedResponses.get(4))
             .verifyComplete();
     }
 
     private PagedFlux<Integer> getIntegerPagedFlux() throws MalformedURLException {
-        HttpRequest httpRequest = new HttpRequest(HttpMethod.GET, new URL("http://localhost"));
         HttpHeaders httpHeaders = new HttpHeaders().put("header1", "value1")
             .put("header2", "value2");
+        HttpRequest httpRequest = new HttpRequest(HttpMethod.GET, new URL("http://localhost"));
 
         String deserializedHeaders = "header1,value1,header2,value2";
         pagedResponses = IntStream.range(0, 5)
             .boxed()
-            .map(
-                i -> new PagedResponseBase<>(httpRequest, HttpResponseStatus.OK.code(),
-                    httpHeaders,
-                    getItems(i),
-                    i < 4 ? String.valueOf(i + 1) : null,
-                    deserializedHeaders)).
-                collect(Collectors.toList());
+            .map(i -> createPagedResponse(httpRequest, httpHeaders, deserializedHeaders, i))
+            .collect(Collectors.toList());
         Mono<PagedResponse<Integer>> firstPage = Mono.just(pagedResponses.get(0));
         return new PagedFlux<>(firstPage,
             pageLink -> getNextPage(pageLink, pagedResponses));
+    }
+
+    private PagedResponseBase<String, Integer> createPagedResponse(HttpRequest httpRequest,
+        HttpHeaders httpHeaders, String deserializedHeaders, Integer i) {
+        return new PagedResponseBase<>(httpRequest, HttpResponseStatus.OK.code(),
+            httpHeaders,
+            getItems(i),
+            i < 4 ? String.valueOf(i + 1) : null,
+            deserializedHeaders);
     }
 
     private Mono<PagedResponse<Integer>> getNextPage(String pageLink,
@@ -73,10 +84,7 @@ public class PagedFluxTest {
         if (pageLink == null || pageLink.isEmpty()) {
             return Mono.empty();
         }
-
-        Mono<PagedResponse<Integer>> result = Mono
-            .just(pagedResponses.get(Integer.valueOf(pageLink)));
-        return result;
+        return Mono.just(pagedResponses.get(Integer.valueOf(pageLink)));
     }
 
     private List<Integer> getItems(Integer i) {
