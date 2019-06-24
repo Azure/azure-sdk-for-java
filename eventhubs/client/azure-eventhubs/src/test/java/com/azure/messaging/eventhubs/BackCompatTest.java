7cabe0dbae270bd7421b540016d3f97930b8bf63
Connie <conniey@microsoft.com>
2019-06-23 18:16:11 -0700

    Re-enabling BackCompatTest.

--- eventhubs/client/azure-eventhubs/src/test/java/com/azure/messaging/eventhubs/BackCompatTest.java
+++ eventhubs/client/azure-eventhubs/src/test/java/com/azure/messaging/eventhubs/BackCompatTest.java
@@ -14,28 +14,24 @@ import org.apache.qpid.proton.amqp.messaging.Data;
 import org.apache.qpid.proton.amqp.messaging.MessageAnnotations;
 import org.apache.qpid.proton.message.Message;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestName;
 import reactor.test.StepVerifier;
 
 import java.nio.ByteBuffer;
-import java.time.Duration;
 import java.util.HashMap;
+import java.util.Map;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class BackCompatTest extends ApiTestBase {
     private static final String PARTITION_ID = "0";
-    private static final String APPLICATION_PROPERTY = "firstProp";
-    private static final String INT_APPLICATION_PROPERTY = "intProp";
-    private static final String PAYLOAD = "testmsg";
+    private static final String PAYLOAD = "test-message";
 
     private EventHubClient client;
     private EventHubProducer producer;
     private EventHubConsumer consumer;
-    private ReactorHandlerProvider handlerProvider;
 
     public BackCompatTest() {
         super(new ClientLogger(BackCompatTest.class));
@@ -51,10 +47,14 @@ public class BackCompatTest extends ApiTestBase {
 
     @Override
     protected void beforeTest() {
-        handlerProvider = new ReactorHandlerProvider(getReactorProvider());
+        final ReactorHandlerProvider handlerProvider = new ReactorHandlerProvider(getReactorProvider());
+
         client = new EventHubClient(getConnectionOptions(), getReactorProvider(), handlerProvider);
         consumer = client.createConsumer(EventHubClient.DEFAULT_CONSUMER_GROUP_NAME, PARTITION_ID, EventPosition.latest());
-        producer = client.createProducer(new EventHubProducerOptions().partitionId(PARTITION_ID).retry(Retry.getNoRetry()).timeout(Duration.ofSeconds(30)));
+        producer = client.createProducer(new EventHubProducerOptions()
+            .partitionId(PARTITION_ID)
+            .retry(Retry.getNoRetry())
+            .timeout(TIMEOUT));
     }
 
     @Override
@@ -63,44 +63,50 @@ public class BackCompatTest extends ApiTestBase {
     }
 
     /**
-     * Verifies test work with versions before 0.11.0.
+     * Verifies test work with SDK versions before 0.11.0.
      */
-    @Ignore
     @Test
     public void backCompatWithJavaSDKOlderThan0110() {
         skipIfNotRecordMode();
 
         // Arrange
-        final Message originalMessage = Proton.message();
+        final String matchingMessageKey = "messageKey";
+        final String matchingMessageValue = "backCompatWithJavaSDKOlderThan0110";
 
         // until version 0.10.0 - we used to have Properties as HashMap<String,String>
         // This specific combination is intended to test the back compat - with the new Properties type as HashMap<String, Object>
-        final HashMap<String, Object> appProperties = new HashMap<>();
-        appProperties.put(APPLICATION_PROPERTY, "value1");
-        appProperties.put(INT_APPLICATION_PROPERTY, "3");
-        // back compat end
-        final ApplicationProperties applicationProperties = new ApplicationProperties(appProperties);
+        final HashMap<String, Object> applicationProperties = new HashMap<>();
+        applicationProperties.put("firstProperty", "value1");
+        applicationProperties.put("intProperty", "3");
 
-        originalMessage.setApplicationProperties(applicationProperties);
-        originalMessage.setBody(new Data(Binary.create(ByteBuffer.wrap(PAYLOAD.getBytes()))));
-        originalMessage.setMessageAnnotations(new MessageAnnotations(new HashMap<>()));
-        final EventData msgEvent = new EventData(originalMessage);
+        // We want to ensure that we fetch the event data corresponding to this test and not some other test case.
+        applicationProperties.put(matchingMessageKey, matchingMessageValue);
+
+        final Message message = Proton.message();
+        message.setApplicationProperties(new ApplicationProperties(applicationProperties));
+        message.setBody(new Data(Binary.create(ByteBuffer.wrap(PAYLOAD.getBytes(UTF_8)))));
+        message.setMessageAnnotations(new MessageAnnotations(new HashMap<>()));
+
+        final EventData eventData = new EventData(message);
 
         // Act & Assert
-        StepVerifier.create(consumer.receive().take(1))
-            .then(() -> producer.send(msgEvent).block(TIMEOUT))
-            .assertNext(event -> validateAmqpPropertiesInEventData(event, originalMessage))
+        StepVerifier.create(consumer.receive().filter(received -> {
+            return received.properties() != null
+                && received.properties().containsKey(matchingMessageKey)
+                && matchingMessageValue.equals(received.properties().get(matchingMessageKey));
+        }).take(1))
+            .then(() -> producer.send(eventData).block(TIMEOUT))
+            .assertNext(event -> validateAmqpProperties(applicationProperties, event))
             .verifyComplete();
     }
 
-    private void validateAmqpPropertiesInEventData(EventData eData, Message originalMessage) {
-        Assert.assertTrue(eData.properties().containsKey(APPLICATION_PROPERTY));
-        Assert.assertEquals(originalMessage.getApplicationProperties().getValue().get(APPLICATION_PROPERTY), eData.properties().get(APPLICATION_PROPERTY));
+    private void validateAmqpProperties(Map<String, Object> expected, EventData event) {
+        Assert.assertEquals(expected.size(), event.properties().size());
+        Assert.assertEquals(PAYLOAD, UTF_8.decode(event.body()).toString());
 
-        Assert.assertTrue(eData.properties().containsKey(INT_APPLICATION_PROPERTY));
-        Assert.assertEquals(originalMessage.getApplicationProperties().getValue().get(INT_APPLICATION_PROPERTY), eData.properties().get(INT_APPLICATION_PROPERTY));
-
-        Assert.assertEquals(2, eData.properties().size());
-        Assert.assertEquals(PAYLOAD, UTF_8.decode(eData.body()).toString());
+        expected.forEach((key, value) -> {
+            Assert.assertTrue(event.properties().containsKey(key));
+            Assert.assertEquals(value, event.properties().get(key));
+        });
     }
 }
