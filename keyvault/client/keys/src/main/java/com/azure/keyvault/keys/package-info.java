b23f17b589a4d7253d5b8f23865455859b690c5b
Vinay Gera <vigera@microsoft.com>
2019-06-26 14:12:25 -0700

    Keyvault Updates (#4062)
    Keyvault Config + Logging + Iterable + Tests Updates + Samples updates + README Updates

--- keyvault/client/keys/src/main/java/com/azure/keyvault/keys/package-info.java
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-/**
- * Package containing classes for creating {@link com.azure.keyvault.keys.KeyAsyncClient} and
- * {@link com.azure.keyvault.keys.KeyClient} to perform operations on Azure Key Vault.
- */
-package com.azure.keyvault.keys;
---
+++ keyvault/client/keys/src/samples/java/com/azure/security/keyvault/keys/KeyClientJavaDocCodeSnippets.java
@@ -0,0 +1,63 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.security.keyvault.keys;
+
+import com.azure.core.credentials.TokenCredential;
+import com.azure.identity.credential.DefaultAzureCredential;
+import com.azure.security.keyvault.keys.models.Key;
+import com.azure.security.keyvault.keys.models.KeyBase;
+import com.azure.security.keyvault.keys.models.webkey.KeyType;
+
+/**
+ * This class contains code samples for generating javadocs through doclets for {@link KeyClient}
+ */
+public final class KeyClientJavaDocCodeSnippets {
+
+    /**
+     * Generates code sample for creating a {@link KeyClient}
+     * @return An instance of {@link KeyClient}
+     */
+    public KeyClient createClient() {
+        // BEGIN: com.azure.keyvault.keys.keyclient.instantiation
+        KeyClient keyClient = KeyClient.builder()
+            .endpoint("https://myvault.azure.net/")
+            .credential(new DefaultAzureCredential())
+            .build();
+        // END: com.azure.keyvault.keys.keyclient.instantiation
+        return keyClient;
+    }
+
+    /**
+     * Generates a code sample for using {@link KeyClient#createKey(String, KeyType)}
+     */
+    public void createKey() {
+        KeyClient keyClient = createClient();
+        // BEGIN: com.azure.keyvault.keys.keyclient.createKey#string-keyType
+        Key retKey = keyClient.createKey("keyName", KeyType.EC).value();
+        System.out.printf("Key is created with name %s and id %s %n", retKey.name(), retKey.id());
+        // END: com.azure.keyvault.keys.keyclient.createKey#string-keyType
+    }
+
+    /**
+     * Generates code sample for using {@link KeyClient#listKeyVersions(String)}
+     */
+    public void listKeyVersions() {
+        KeyClient keyClient = createClient();
+        // BEGIN: com.azure.keyvault.keys.keyclient.listKeyVersions
+        for (KeyBase key : keyClient.listKeyVersions("keyName")) {
+            Key keyWithMaterial  = keyClient.getKey(key).value();
+            System.out.printf("Received key's version with name %s, type %s and version %s", keyWithMaterial.name(),
+                    keyWithMaterial.keyMaterial().kty(), keyWithMaterial.version());
+        }
+        // END: com.azure.keyvault.keys.keyclient.listKeyVersions
+    }
+
+    /**
+     * Implementation not provided for this method
+     * @return {@code null}
+     */
+    private TokenCredential getKeyVaultCredential() {
+        return null;
+    }
+}
