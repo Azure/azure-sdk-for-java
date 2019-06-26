592100a81260fd289723a8cf7d1789bab2b9d10c
Connie <conniey@microsoft.com>
2019-06-25 21:57:00 -0700

    Fix broken codesnippet

--- eventhubs/client/azure-eventhubs/src/main/java/com/azure/messaging/eventhubs/EventHubClient.java
+++ eventhubs/client/azure-eventhubs/src/main/java/com/azure/messaging/eventhubs/EventHubClient.java
@@ -38,11 +38,11 @@ import java.util.concurrent.atomic.AtomicBoolean;
  *
  * <p><strong>Creating an {@link EventHubClient} using Event Hubs namespace connection string</strong></p>
  *
- * {@codesnippet com.azure.messaging.eventhubs.eventhubclient.instantiation}
+ * {@codesnippet com.azure.messaging.eventhubs.eventhubclientbuilder.instantiation}
  *
  * <p><strong>Creating an {@link EventHubClient} using Event Hub instance connection string</strong></p>
  *
- * {@codesnippet com.azure.messaging.eventhubs.eventhubclient.instantiationEventHub}
+ * {@codesnippet com.azure.messaging.eventhubs.eventhubclientbuilder.instantiationEventHub}
  *
  * @see EventHubClientBuilder
  * @see <a href="https://docs.microsoft.com/en-us/Azure/event-hubs/event-hubs-about">About Azure Event Hubs</a>
