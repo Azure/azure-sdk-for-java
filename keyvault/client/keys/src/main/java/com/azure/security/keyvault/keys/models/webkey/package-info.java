f170c0561dc83b806dca4b2936b0bb3aecc6b903
Vinay Gera <vigera@microsoft.com>
2019-06-26 14:12:25 -0700

    Keyvault Updates (#4062)
    Keyvault Config + Logging + Iterable + Tests Updates + Samples updates + README Updates

---
+++ keyvault/client/keys/src/main/java/com/azure/security/keyvault/keys/models/webkey/package-info.java
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+/**
+ * Package containing classes for key vault JSON web key.
+ */
+package com.azure.security.keyvault.keys.models.webkey;
