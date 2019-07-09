734d25e16a01a76eceea165115fb6f9a92380391
Sima Zhu <sizhu@microsoft.com>
2019-07-09 11:03:39 -0700

    Added Azure Identity for storage sample and readme

---
+++ storage/client/blob/src/samples/java/blob/AzureIdentityExample.java
@@ -0,0 +1,33 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package blob;
+
+import com.azure.identity.credential.DefaultAzureCredential;
+import com.azure.storage.blob.StorageClient;
+import java.util.Locale;
+
+import static blob.SampleHelper.getAccountName;
+
+public class AzureIdentityExample {
+    public static void main(String[] args) {
+        String accountName = getAccountName();
+
+        /**
+         * From the Azure portal, get your Storage account blob service URL endpoint.
+         * The URL typically looks like this:
+         */
+        String endpoint = String.format(Locale.ROOT, "https://%s.blob.core.windows.net", accountName);
+
+        /**
+         * Create a storage client using the Azure Identity credentials.
+         */
+        StorageClient storageClient = StorageClient.storageClientBuilder()
+                .endpoint(endpoint)
+                .credential(new DefaultAzureCredential())
+                .buildClient();
+        System.out.println("Successfully setup client using the Azure Identity, please check the service version: "
+            + storageClient.getProperties().value().defaultServiceVersion());
+
+    }
+}
