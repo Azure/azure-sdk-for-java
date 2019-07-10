41fdc67dd7b3db8dd9ad89166e7c0d1ae77ae6cc
Pareekshit Singh <pareekshit.singh@microsoft.com>
2019-07-09 17:24:04 -0700

    Add more debug infoi

--- eng/pipelines/scripts/mgmt_sdk_compiler.sh
+++ eng/pipelines/scripts/mgmt_sdk_compiler.sh
@@ -7,6 +7,9 @@ mvn --version
 
 echo "java is at : $JAVA_HOME"
 
+echo "versions of java available:" 
+ls /usr/lib/jvm/zulu*
+
 #TODO:
 #for some reason the workingdirectory dos not seem to work...
 #fix the following cd cmd once we figure out how to get it to work
@@ -16,10 +19,10 @@ cd ../../..
 for i in `ls -d */*/v20* | grep -v "node_modules/*/*"`; 
 do 
   echo "building folder $i"
-  cd $i; 
-  mvn clean compile -Dgpg.skip; 
-  if [ $? != 0 ]; 
-    then cd -; exit -1; 
-    else cd -; 
-  fi; 
+  #cd $i; 
+  #mvn clean compile --batch-mode -Dgpg.skip; 
+  #if [ $? != 0 ]; 
+  #  then cd -; exit -1; 
+  #  else cd -; 
+  #fi; 
 done
