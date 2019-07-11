63a59f51784e559d0946912a915aa83538bfd208
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2019-07-11 16:45:40 -0700

    credentials -> credential, serviceEndpoint -> endpoint, endpoint no longer throws MalformedUrlException

--- sdk/appconfiguration/azure-data-appconfiguration/src/samples/java/PipelineSample.java
+++ sdk/appconfiguration/azure-data-appconfiguration/src/samples/java/PipelineSample.java
@@ -44,7 +44,7 @@ class PipelineSample {
         // We add in a policy to track the type of HTTP method calls we make.
         // We also want to see the Header information of our HTTP requests, so we specify the detail level.
         final ConfigurationAsyncClient client = new ConfigurationClientBuilder()
-                .credentials(new ConfigurationClientCredentials(connectionString))
+                .credential(new ConfigurationClientCredentials(connectionString))
                 .addPolicy(new HttpMethodRequestTrackingPolicy(tracker))
                 .httpLogDetailLevel(HttpLogDetailLevel.HEADERS)
                 .buildAsyncClient();
