97ced78f40a9c7b62ad502592702936eb89318ea
Connie <conniey@microsoft.com>
2019-07-12 11:14:52 -0700

    Updating the sample.

--- sdk/eventhubs/azure-eventhubs/src/samples/java/com/azure/messaging/eventhubs/EventHubProducerJavaDocCodeSamples.java
+++ sdk/eventhubs/azure-eventhubs/src/samples/java/com/azure/messaging/eventhubs/EventHubProducerJavaDocCodeSamples.java
@@ -7,9 +7,7 @@ import reactor.core.publisher.Flux;
 
 import java.io.IOException;
 import java.time.Duration;
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
@@ -81,8 +79,7 @@ public class EventHubProducerJavaDocCodeSamples {
         final EventHubProducer producer = client.createProducer();
 
         // BEGIN: com.azure.messaging.eventhubs.eventhubproducer.send#eventdatabatch
-        final AtomicInteger current = new AtomicInteger();
-        final List<EventData> events = Arrays.asList(
+        final Flux<EventData> telemetryEvents = Flux.just(
             new EventData("92".getBytes(UTF_8)).addProperty("telemetry", "latency"),
             new EventData("98".getBytes(UTF_8)).addProperty("telemetry", "cpu-temperature"),
             new EventData("120".getBytes(UTF_8)).addProperty("telemetry", "fps")
@@ -91,21 +88,25 @@ public class EventHubProducerJavaDocCodeSamples {
         final BatchOptions options = new BatchOptions()
             .partitionKey("telemetry")
             .maximumSizeInBytes(256);
+        final AtomicReference<EventDataBatch> currentBatch = new AtomicReference<>(
+            producer.createBatch(options).block());
 
-        producer.createBatch(options).flatMap(batch -> {
-            int index = current.get();
-            while (index < events.size()) {
-                if (!batch.tryAdd(events.get(index))) {
-                    break;
+        // The sample Flux contains three events, but it could be an infinite stream of telemetry events.
+        telemetryEvents.subscribe(event -> {
+                final EventDataBatch batch = currentBatch.get();
+                if (!batch.tryAdd(event)) {
+                    producer.createBatch(options).map(newBatch -> {
+                        currentBatch.set(newBatch);
+                        return producer.send(batch);
+                    }).block();
                 }
-
-                index = current.incrementAndGet();
-            }
-
-            return producer.send(batch);
-        }).subscribe(ignored -> System.out.println("Batch sent."),
-            error -> System.err.println("Error received:" + error),
-            () -> System.out.println("Send complete."));
+            }, error -> System.err.println("Error received:" + error),
+            () -> {
+                final EventDataBatch batch = currentBatch.getAndSet(null);
+                if (batch != null) {
+                    producer.send(batch).block();
+                }
+            });
         // END: com.azure.messaging.eventhubs.eventhubproducer.send#eventdatabatch
     }
 }
