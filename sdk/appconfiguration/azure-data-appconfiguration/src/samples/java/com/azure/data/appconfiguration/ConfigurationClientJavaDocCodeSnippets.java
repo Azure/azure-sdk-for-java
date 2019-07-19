5a1ae0bf267ca138068d6b9478aa83dd449cfe11
Hemant Tanwar <hetanwar@users.noreply.github.com>
2019-07-19 16:01:56 -0700

    added samples in snippet files

--- sdk/appconfiguration/azure-data-appconfiguration/src/samples/java/com/azure/data/appconfiguration/ConfigurationClientJavaDocCodeSnippets.java
+++ sdk/appconfiguration/azure-data-appconfiguration/src/samples/java/com/azure/data/appconfiguration/ConfigurationClientJavaDocCodeSnippets.java
@@ -6,7 +6,6 @@ package com.azure.data.appconfiguration;
 import com.azure.core.http.HttpPipeline;
 import com.azure.core.test.models.RecordedData;
 import com.azure.core.test.policy.RecordNetworkCallPolicy;
-import com.azure.core.util.Context;
 import com.azure.data.appconfiguration.credentials.ConfigurationClientCredentials;
 import com.azure.data.appconfiguration.models.ConfigurationSetting;
 import com.azure.data.appconfiguration.models.SettingSelector;
