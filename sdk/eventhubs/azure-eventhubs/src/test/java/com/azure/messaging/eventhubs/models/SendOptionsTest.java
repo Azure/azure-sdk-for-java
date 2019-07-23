6a6e38fbca979a98b70e3471afe151c88dc8d9ef
Connie <conniey@microsoft.com>
2019-07-23 10:39:49 -0700

    Move tests into their respective folders.

--- sdk/eventhubs/azure-eventhubs/src/test/java/com/azure/messaging/eventhubs/models/SendOptionsTest.java
+++ sdk/eventhubs/azure-eventhubs/src/test/java/com/azure/messaging/eventhubs/models/SendOptionsTest.java
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.azure.messaging.eventhubs;
+package com.azure.messaging.eventhubs.models;
 
 import com.azure.messaging.eventhubs.models.SendOptions;
 import org.junit.Assert;
