78328bb9546ce5b8cda8e78562e775304968ab24
shafang <Shawn.Fang@microsoft.com>
2019-07-30 15:59:32 -0700

    use leaveToken instead of self-tree-traversal

--- eng/code-quality-reports/src/main/java/com/azure/tools/checkstyle/checks/ThrowFromClientLoggerCheck.java
+++ eng/code-quality-reports/src/main/java/com/azure/tools/checkstyle/checks/ThrowFromClientLoggerCheck.java
@@ -8,6 +8,9 @@ import com.puppycrawl.tools.checkstyle.api.DetailAST;
 import com.puppycrawl.tools.checkstyle.api.FullIdent;
 import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * To throw an exception,
  * <ol>
@@ -25,6 +28,9 @@ import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 public class ThrowFromClientLoggerCheck extends AbstractCheck {
     private static final String LOGGER_LOG_AND_THROW = "logger.logAndThrow";
 
+    private Deque<Boolean> classStaticDeque = new ArrayDeque<>();
+    private Deque<Boolean> methodStaticDeque = new ArrayDeque<>();
+
     @Override
     public int[] getDefaultTokens() {
         return getRequiredTokens();
@@ -38,16 +44,42 @@ public class ThrowFromClientLoggerCheck extends AbstractCheck {
     @Override
     public int[] getRequiredTokens() {
         return new int[] {
+            TokenTypes.CLASS_DEF,
             TokenTypes.LITERAL_THROW,
+            TokenTypes.METHOD_DEF,
             TokenTypes.METHOD_CALL
         };
     }
 
     @Override
+    public void leaveToken(DetailAST token) {
+        switch (token.getType()) {
+            case TokenTypes.CLASS_DEF:
+                classStaticDeque.pop();
+                break;
+            case TokenTypes.METHOD_DEF:
+                methodStaticDeque.pop();
+                break;
+            default:
+                // Checkstyle complains if there's no default block in switch
+                break;
+        }
+    }
+
+    @Override
     public void visitToken(DetailAST token) {
         switch (token.getType()) {
+            case TokenTypes.CLASS_DEF:
+                DetailAST modifiersToken = token.findFirstToken(TokenTypes.MODIFIERS);
+                classStaticDeque.addLast(modifiersToken.branchContains(TokenTypes.LITERAL_STATIC));
+                break;
+            case TokenTypes.METHOD_DEF:
+                DetailAST methodModifiersToken = token.findFirstToken(TokenTypes.MODIFIERS);
+                methodStaticDeque.addLast(methodModifiersToken.branchContains(TokenTypes.LITERAL_STATIC));
+                break;
             case TokenTypes.LITERAL_THROW:
-                if (!isValidScope(token)) {
+                if (classStaticDeque.isEmpty() || methodStaticDeque.isEmpty()
+                    || classStaticDeque.peekLast() || methodStaticDeque.peekLast()) {
                     return;
                 }
                 log(token, String.format("Directly throwing an exception is disallowed. Replace the throw statement with" +
@@ -59,7 +91,8 @@ public class ThrowFromClientLoggerCheck extends AbstractCheck {
                     return;
                 }
 
-                if (!isValidScope(token)) {
+                if (classStaticDeque.isEmpty() || methodStaticDeque.isEmpty() ||
+                    classStaticDeque.peekLast() || methodStaticDeque.peekLast()) {
                     return;
                 }
 
@@ -73,43 +106,5 @@ public class ThrowFromClientLoggerCheck extends AbstractCheck {
                 // Checkstyle complains if there's no default block in switch
                 break;
         }
-    }
-
-    /**
-     * Given a token as a root node, traversal back to find the if the token is belong to a static method.
-     * If not, check if it is in the constructor or a static class. Use this method to define the scope of the token.
-     *
-     * @param token the given token as a root to traversal back to one of the ancestors.
-     * @return false if the token is in a scope of static method, constructor or static class. otherwise, return true.
-     */
-    private boolean isValidScope(DetailAST token) {
-        DetailAST parentToken = token.getParent();
-        boolean throwsFromMethod = false;
-        while (parentToken != null) {
-            int parentType = parentToken.getType();
-            // Skip check if the token is from static method
-            if (parentType == TokenTypes.METHOD_DEF) {
-                DetailAST modifiersToken = parentToken.findFirstToken(TokenTypes.MODIFIERS);
-                if (modifiersToken.branchContains(TokenTypes.LITERAL_STATIC)) {
-                    return false;
-                }
-                throwsFromMethod = true;
-            }
-            // Skip check if the token is from static class
-            if (parentType == TokenTypes.CLASS_DEF) {
-                DetailAST modifiersToken = parentToken.findFirstToken(TokenTypes.MODIFIERS);
-                if (modifiersToken.branchContains(TokenTypes.LITERAL_STATIC)) {
-                    return false;
-                }
-                break;
-            }
-            // Skip check if the token from constructor
-            if (!throwsFromMethod && parentType == TokenTypes.CTOR_DEF) {
-                return false;
-            }
-            parentToken = parentToken.getParent();
-        }
-
-        return true;
     }
 }
