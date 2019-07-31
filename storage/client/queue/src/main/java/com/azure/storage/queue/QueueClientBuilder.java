13c2431dbea8d227e251d51ddddd3b9fe2c26424
Sima Zhu <sizhu@microsoft.com>
2019-07-30 18:12:33 -0700

    merge mainline and checkstyle fix

--- storage/client/queue/src/main/java/com/azure/storage/queue/QueueClientBuilder.java
+++ storage/client/queue/src/main/java/com/azure/storage/queue/QueueClientBuilder.java
@@ -30,7 +30,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 import java.util.Objects;
 
 /**
--- storage/client/queue/src/main/java/com/azure/storage/queue/QueueServiceClientBuilder.java
+++ storage/client/queue/src/main/java/com/azure/storage/queue/QueueServiceClientBuilder.java
@@ -27,7 +27,6 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Locale;
 import java.util.Objects;
 
