7fc63987a759ab49477618d58757f9c36af946a4
Jonathan Cardenas <JonathanCrd@users.noreply.github.com>
2019-07-31 18:52:16 -0700

    Create CosmosDB files

---
+++ samples/SmokeTest/src/main/java/com/azure/CosmosDB.java
@@ -0,0 +1,29 @@
+package com.azure;
+
+import com.azure.data.cosmos.*;
+
+public class CosmosDB {
+    public static void createDatabase(){
+
+    }
+
+    public static void createCollection(){
+
+    }
+
+    public static void createDocuments(){
+
+    }
+
+    public static void simpleQuery(){
+
+    }
+
+    public static void deleteDatabase(){
+
+    }
+
+    public static void main(String[] args){
+
+    }
+}
