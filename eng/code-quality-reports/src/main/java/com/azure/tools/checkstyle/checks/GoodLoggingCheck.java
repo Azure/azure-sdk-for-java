6b1c8668e5a138956f0ada2a012777bc9c7e50eb
shafang <Shawn.Fang@microsoft.com>
2019-08-02 11:30:35 -0700

    refactor and fix bugs

--- eng/code-quality-reports/src/main/java/com/azure/tools/checkstyle/checks/GoodLoggingCheck.java
+++ eng/code-quality-reports/src/main/java/com/azure/tools/checkstyle/checks/GoodLoggingCheck.java
@@ -18,27 +18,32 @@ import java.util.Stack;
 
 /**
  * Check Style Rule: Good Logging Practice
- *  I. Non-static Logger instance
- *  II. Logger variable name should be 'logger'
+ *  <ol>
+ *      <li>Non-static Logger instance.</li>
+ *      <li>Logger variable name should be 'logger' in public class and not exist in an implementation package.</li>
+ *      <li>Should not use any external logger class, only use ClientLogger. No slf4j, log4j, or other logging imports are allowed.</li>
+ *      <li>Should refer to the containing class in its constructor:
+ *      public class A {
+ *          private ClientLogger logger = new ClientLogger(A.class);
+ *      }
+ *      </li>
+ *  </ol>
  */
 public class GoodLoggingCheck extends AbstractCheck {
-    private static final String PACKAGE_PATH = "com.azure";
-    private static final String IMPLEMENTATION_PATH = "com.azure.core.implementation";
-    private static final String LOGGER_CLASS = "com.azure.core.implementation.logging.ServiceLogger";
-    private static final String SERVICE_LOGGER = "ServiceLogger";
+    private static final String CLIENT_LOOGER_PATH = "com.azure.core.util.logging.ClientLogger";
+    private static final String CLIENT_LOGGER = "ClientLogger";
     private static final String SLF4J = "org.slf4j";
     private static final String LOG4J = "org.apache.logging.log4j";
 
-    private static final String LOGGER_NAME_ERR = "ServiceLogger instance naming: use \"%s\" instead of \"%s\" for consistency.";
-    private static final String STATIC_LOGGER_ERR = "Reference to ServiceLogger should not be static: remove static modifier.";
-    private static final String ONLY_LOGGER_CLASS_ERR = "Use \"%s\" as a logging mechanism instead of \"%s\".";
+    private static final String LOGGER_NAME_ERROR = "ClientLogger instance naming: use ''%s'' instead of ''%s'' for consistency.";
+    private static final String STATIC_LOGGER_ERROR = "Reference to ClientLogger should not be static: remove static modifier.";
+    private static final String NOT_CLIENT_LOGGER_ERROR = "Do not use external logger class. Use ''%s'' as a logging mechanism instead of ''%s''.";
 
-    private static boolean isPathCorrect; // track 2
-    private static boolean isImplPackage;
-    private static boolean hasSlf4jImported;
-    private static boolean hasLog4jimported;
-    private static boolean hasLoggerImported;
-    private static String loggerRequiredName;
+    private boolean hasSlf4jImported;
+    private boolean hasLog4jimported;
+    private boolean hasClientLoggerImported;
+    private String loggerRequiredName;
+    private String className;
 
     /**
      * Setter to specifies valid identifiers
@@ -61,114 +66,107 @@ public class GoodLoggingCheck extends AbstractCheck {
     @Override
     public int[] getRequiredTokens() {
         return new int[] {
-            TokenTypes.PACKAGE_DEF,
             TokenTypes.IMPORT,
             TokenTypes.LITERAL_CLASS,
+            TokenTypes.LITERAL_NEW,
             TokenTypes.VARIABLE_DEF
         };
     }
 
     @Override
-    public void beginTree(DetailAST rootAST) {
-        this.hasLoggerImported = false;
-        this.isPathCorrect = false;
-        this.isImplPackage = false;
+    public void finishTree(DetailAST ast) {
+        hasSlf4jImported = false;
+        hasLog4jimported = false;
+        hasClientLoggerImported = false;
     }
 
     @Override
     public void visitToken(DetailAST ast) {
-
-        if (ast.getType() == TokenTypes.PACKAGE_DEF) {
-            String packageName = FullIdent.createFullIdent(ast.findFirstToken(TokenTypes.DOT)).getText();
-            this.isPathCorrect = packageName.startsWith(PACKAGE_PATH);
-            this.isImplPackage = packageName.startsWith(IMPLEMENTATION_PATH);
-
-        } else {
-            if (this.isPathCorrect) {
-                if (this.isImplPackage) {
-                    return;
-                }
-            } else {
-                return;
-            }
-        }
-
         switch (ast.getType()) {
             case TokenTypes.IMPORT:
                 String importClassPath = FullIdent.createFullIdentBelow(ast).getText();
-                if (importClassPath != null) {
-                    this.hasLoggerImported |= importClassPath.equals(LOGGER_CLASS);
-                    this.hasSlf4jImported |= importClassPath.startsWith(SLF4J);
-                    this.hasLog4jimported |= importClassPath.startsWith(LOG4J);
-                }
+                hasClientLoggerImported |= importClassPath.equals(CLIENT_LOOGER_PATH);
+                hasSlf4jImported |= importClassPath.startsWith(SLF4J);
+                hasLog4jimported |= importClassPath.startsWith(LOG4J);
                 break;
             case TokenTypes.LITERAL_CLASS:
                 if (ast.getNextSibling() == null) {
                     break;
                 }
-                String className = ast.getNextSibling().getText();
-                // No check for ServiceLogger class
-                if (!SERVICE_LOGGER.equals(className)) {
-                    if (this.hasSlf4jImported) {
-                        log(ast, String.format(ONLY_LOGGER_CLASS_ERR, LOGGER_CLASS, SLF4J));
-                    } else if (this.hasLog4jimported) {
-                        log(ast, String.format(ONLY_LOGGER_CLASS_ERR, LOGGER_CLASS, LOG4J));
-                    } else {
-                        // do nothing
-                    }
+                // Checks no use any external logger class.
+                if (hasSlf4jImported) {
+                    log(ast, String.format(NOT_CLIENT_LOGGER_ERROR, CLIENT_LOOGER_PATH, SLF4J));
+                }
+                if (hasLog4jimported) {
+                    log(ast, String.format(NOT_CLIENT_LOGGER_ERROR, CLIENT_LOOGER_PATH, LOG4J));
+                }
+
+                className = ast.getNextSibling().getText();
+
+                break;
+            case TokenTypes.LITERAL_NEW:
+                // Check if the containing class matched with class name itself
+
+                DetailAST identToken = ast.findFirstToken(TokenTypes.IDENT);
+                if (identToken == null) {
+                    return;
+                }
+
+                String newClientLogger = identToken.getText();
+                if (!newClientLogger.equals(CLIENT_LOGGER)) {
+                    return;
+                }
+
+                DetailAST elistToken = ast.findFirstToken(TokenTypes.ELIST);
+                DetailAST exprToken = elistToken.findFirstToken(TokenTypes.EXPR);
+                if (exprToken.getFirstChild().getType() != TokenTypes.DOT) {
+                    return;
+                }
+                String containerClassName = FullIdent.createFullIdentBelow(exprToken).getText();
+                containerClassName = containerClassName.substring(0, containerClassName.length() - 6);
+                if (!containerClassName.equals(className)) {
+                    log(ast, String.format("Not newing a ClientLogger with matching class name. Use ''%s.class'' instead of ''%s.class''", className, containerClassName));
                 }
                 break;
             case TokenTypes.VARIABLE_DEF:
-                if (this.hasLoggerImported && this.isLoggerType(ast)) {
-                    logLoggerInstanceNamingErrorIfFound(ast);
-                    logStaticLoggerInstanceErrorIfFound(ast);
+                if (!hasClientLoggerImported || !isTypeClientLogger(ast)) {
+                    return;
                 }
+                // Check if the Logger instance named as 'logger'.
+                DetailAST identAST = ast.findFirstToken(TokenTypes.IDENT);
+                if (identAST != null && !identAST.getText().equals(loggerRequiredName)) {
+                    log(ast, String.format(LOGGER_NAME_ERROR, loggerRequiredName, identAST.getText()));
+                }
+                // Check if the Logger is static instance, log as error if it is static instance logger.
+                DetailAST modifierAST = ast.findFirstToken(TokenTypes.MODIFIERS);
+                if (modifierAST != null && modifierAST.branchContains(TokenTypes.LITERAL_STATIC)) {
+                    log(ast, STATIC_LOGGER_ERROR);
+                }
+                break;
+            default:
+                // Checkstyle complains if there's no default block in switch
                 break;
         }
     }
 
     /**
-     * Check if the Logger instance named as logger, log error if not
-     * @param varDefAST the VARIABLE_DEF node
-     */
-    private void logLoggerInstanceNamingErrorIfFound(DetailAST varDefAST) {
-        if (varDefAST == null) {
-            return;
-        }
-        DetailAST identAST = varDefAST.findFirstToken(TokenTypes.IDENT);
-        if (identAST != null && !identAST.getText().equals(this.loggerRequiredName)) {
-            log(varDefAST, String.format(LOGGER_NAME_ERR, this.loggerRequiredName, identAST.getText()));
-        }
-    }
-
-    /**
-     * Check if the Logger is static instance, log as error if it is static instance logger,
-     * @param varDefAST the VARIABLE_DEF node
-     */
-    private void logStaticLoggerInstanceErrorIfFound(DetailAST varDefAST) {
-        DetailAST modifierAST = varDefAST.findFirstToken(TokenTypes.MODIFIERS);
-        if (modifierAST != null && modifierAST.branchContains(TokenTypes.LITERAL_STATIC)) {
-            log(varDefAST, STATIC_LOGGER_ERR);
-        }
-    }
-
-    /**
-     * check if the VARIABLE_DEF AST node type is Logger
+     * check if the VARIABLE_DEF AST node type is 'ClientLogger'.
      * @param varDefAST VARIABLE_DEF AST node
-     * @return true if the variable type is Logger
+     * @return true if the variable type is 'ClientLogger'.
      */
-    private boolean isLoggerType(DetailAST varDefAST) {
+    private boolean isTypeClientLogger(DetailAST varDefAST) {
         DetailAST typeAST = varDefAST.findFirstToken(TokenTypes.TYPE);
         DetailAST varNameAST = varDefAST.findFirstToken(TokenTypes.IDENT);
         if (typeAST == null || varNameAST == null) {
             return false;
         }
         DetailAST typeNameAST = typeAST.findFirstToken(TokenTypes.IDENT);
-        if (typeNameAST != null) {
-            String typeName = typeNameAST.getText();
-            if (typeName != null && typeName.equals(SERVICE_LOGGER)) {
-                return true;
-            }
+        if (typeNameAST == null) {
+            return false;
+        }
+        String typeName = typeNameAST.getText();
+        if (typeName != null && typeName.equals(CLIENT_LOGGER)) {
+            return true;
         }
         return false;
     }
