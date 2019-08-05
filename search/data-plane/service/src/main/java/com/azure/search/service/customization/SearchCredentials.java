9a2308b8f126506810e688c9d3c1c71b25a45515
Rabeea Abu-Saleh <rabeea.abusaleh@gmail.com>
2019-08-05 15:18:06 +0300

    Search service autorest (#2)
    Adding auto-generated Search Service (using AutoRest v2.0) with customization, to be used by tests of Search Data model to create Indexes and Documents in Azure Search.

---
+++ search/data-plane/service/src/main/java/com/azure/search/service/customization/SearchCredentials.java
@@ -0,0 +1,24 @@
+package com.azure.search.service.customization;
+
+import com.microsoft.rest.credentials.ServiceClientCredentials;
+import okhttp3.OkHttpClient;
+
+/**
+ * api key credential that is put into a header to authorize requests.
+ */
+public class SearchCredentials implements ServiceClientCredentials {
+
+    private String apiKey;
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public void applyCredentialsFilter(OkHttpClient.Builder clientBuilder) {
+        clientBuilder.interceptors().add(new SearchInterceptor(this));
+    }
+
+    public SearchCredentials(String apiKey) {
+        this.apiKey = apiKey;
+    }
+}
