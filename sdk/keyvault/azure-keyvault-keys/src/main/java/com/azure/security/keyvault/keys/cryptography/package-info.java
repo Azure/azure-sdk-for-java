8fb784db00e64105357cff2f31a1de6461080139
vinay <vigera@microsoft.com>
2019-08-06 17:47:06 +0000

    package-info

--- sdk/keyvault/azure-keyvault-keys/src/main/java/com/azure/security/keyvault/keys/cryptography/package-info.java
+++ sdk/keyvault/azure-keyvault-keys/src/main/java/com/azure/security/keyvault/keys/cryptography/package-info.java
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 /**
- * This package contains cryptographic functions and implementations.
+ * Package containing classes for creating {@link com.azure.security.keyvault.keys.cryptography.CryptographyAsyncClient} and
+ * {@link com.azure.security.keyvault.keys.cryptography.CryptographyClient} to perform cryptography operations.
  */
 package com.azure.security.keyvault.keys.cryptography;
