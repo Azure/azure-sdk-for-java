52ebbd668e3ec254b0b1846bc2e473cc568766a4
vinay <vigera@microsoft.com>
2019-08-06 20:13:24 +0000

    spotbugs fix

--- sdk/keyvault/azure-keyvault-keys/src/main/java/com/azure/security/keyvault/keys/cryptography/AesCbcHmacSha2.java
+++ sdk/keyvault/azure-keyvault-keys/src/main/java/com/azure/security/keyvault/keys/cryptography/AesCbcHmacSha2.java
@@ -78,6 +78,74 @@ abstract class AesCbcHmacSha2 extends SymmetricEncryptionAlgorithm {
 
             return inner.doFinal(input);
         }
+
+        private static byte[] toBigEndian(long i) {
+
+            byte[] shortRepresentation = BigInteger.valueOf(i).toByteArray();
+            byte[] longRepresentation = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+
+            System.arraycopy(shortRepresentation, 0, longRepresentation, longRepresentation.length - shortRepresentation.length, shortRepresentation.length);
+
+            return longRepresentation;
+        }
+
+        private static Triplet<byte[], byte[], Mac> getAlgorithmParameters(String algorithm, byte[] key) throws InvalidKeyException, NoSuchAlgorithmException {
+
+            byte[] aesKey;
+            byte[] hmacKey;
+            Mac hmac;
+
+            if (algorithm.equalsIgnoreCase(Aes128CbcHmacSha256.ALGORITHM_NAME)) {
+                if ((key.length << 3) < 256) {
+                    throw new IllegalArgumentException(String.format("%s key length in bits %d < 256", algorithm, key.length << 3));
+                }
+
+                hmacKey = new byte[128 >> 3];
+                aesKey = new byte[128 >> 3];
+
+                // The HMAC key precedes the AES key
+                System.arraycopy(key, 0, hmacKey, 0, 128 >> 3);
+                System.arraycopy(key, 128 >> 3, aesKey, 0, 128 >> 3);
+
+                hmac = Mac.getInstance("HmacSHA256");
+                hmac.init(new SecretKeySpec(hmacKey, "HmacSHA256"));
+
+            } else if (algorithm.equalsIgnoreCase(Aes192CbcHmacSha384.ALGORITHM_NAME)) {
+
+                if ((key.length << 3) < 384) {
+                    throw new IllegalArgumentException(String.format("%s key length in bits %d < 384", algorithm, key.length << 3));
+                }
+
+                hmacKey = new byte[192 >> 3];
+                aesKey = new byte[192 >> 3];
+
+                // The HMAC key precedes the AES key
+                System.arraycopy(key, 0, hmacKey, 0, 192 >> 3);
+                System.arraycopy(key, 192 >> 3, aesKey, 0, 192 >> 3);
+
+                hmac = Mac.getInstance("HmacSHA384");
+                hmac.init(new SecretKeySpec(hmacKey, "HmacSHA384"));
+            } else if (algorithm.equalsIgnoreCase(Aes256CbcHmacSha512.ALGORITHM_NAME)) {
+
+                if ((key.length << 3) < 512) {
+                    throw new IllegalArgumentException(String.format("%s key length in bits %d < 512", algorithm, key.length << 3));
+                }
+
+                hmacKey = new byte[256 >> 3];
+                aesKey = new byte[256 >> 3];
+
+                // The HMAC key precedes the AES key
+                System.arraycopy(key, 0, hmacKey, 0, 256 >> 3);
+                System.arraycopy(key, 256 >> 3, aesKey, 0, 256 >> 3);
+
+                hmac = Mac.getInstance("HmacSHA512");
+                hmac.init(new SecretKeySpec(hmacKey, "HmacSHA512"));
+            } else {
+                throw new IllegalArgumentException(String.format("Unsupported algorithm: %s", algorithm));
+            }
+
+            return new Triplet<>(aesKey, hmacKey, hmac);
+        }
     }
 
     static class AesCbcHmacSha2Encryptor implements IAuthenticatedCryptoTransform {
@@ -106,6 +174,74 @@ abstract class AesCbcHmacSha2 extends SymmetricEncryptionAlgorithm {
             // Prime the hash.
             hmac.update(authenticationData);
             hmac.update(iv);
+        }
+
+        private static byte[] toBigEndian(long i) {
+
+            byte[] shortRepresentation = BigInteger.valueOf(i).toByteArray();
+            byte[] longRepresentation = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+
+            System.arraycopy(shortRepresentation, 0, longRepresentation, longRepresentation.length - shortRepresentation.length, shortRepresentation.length);
+
+            return longRepresentation;
+        }
+
+        private static Triplet<byte[], byte[], Mac> getAlgorithmParameters(String algorithm, byte[] key) throws InvalidKeyException, NoSuchAlgorithmException {
+
+            byte[] aesKey;
+            byte[] hmacKey;
+            Mac hmac;
+
+            if (algorithm.equalsIgnoreCase(Aes128CbcHmacSha256.ALGORITHM_NAME)) {
+                if ((key.length << 3) < 256) {
+                    throw new IllegalArgumentException(String.format("%s key length in bits %d < 256", algorithm, key.length << 3));
+                }
+
+                hmacKey = new byte[128 >> 3];
+                aesKey = new byte[128 >> 3];
+
+                // The HMAC key precedes the AES key
+                System.arraycopy(key, 0, hmacKey, 0, 128 >> 3);
+                System.arraycopy(key, 128 >> 3, aesKey, 0, 128 >> 3);
+
+                hmac = Mac.getInstance("HmacSHA256");
+                hmac.init(new SecretKeySpec(hmacKey, "HmacSHA256"));
+
+            } else if (algorithm.equalsIgnoreCase(Aes192CbcHmacSha384.ALGORITHM_NAME)) {
+
+                if ((key.length << 3) < 384) {
+                    throw new IllegalArgumentException(String.format("%s key length in bits %d < 384", algorithm, key.length << 3));
+                }
+
+                hmacKey = new byte[192 >> 3];
+                aesKey = new byte[192 >> 3];
+
+                // The HMAC key precedes the AES key
+                System.arraycopy(key, 0, hmacKey, 0, 192 >> 3);
+                System.arraycopy(key, 192 >> 3, aesKey, 0, 192 >> 3);
+
+                hmac = Mac.getInstance("HmacSHA384");
+                hmac.init(new SecretKeySpec(hmacKey, "HmacSHA384"));
+            } else if (algorithm.equalsIgnoreCase(Aes256CbcHmacSha512.ALGORITHM_NAME)) {
+
+                if ((key.length << 3) < 512) {
+                    throw new IllegalArgumentException(String.format("%s key length in bits %d < 512", algorithm, key.length << 3));
+                }
+
+                hmacKey = new byte[256 >> 3];
+                aesKey = new byte[256 >> 3];
+
+                // The HMAC key precedes the AES key
+                System.arraycopy(key, 0, hmacKey, 0, 256 >> 3);
+                System.arraycopy(key, 256 >> 3, aesKey, 0, 256 >> 3);
+
+                hmac = Mac.getInstance("HmacSHA512");
+                hmac.init(new SecretKeySpec(hmacKey, "HmacSHA512"));
+            } else {
+                throw new IllegalArgumentException(String.format("Unsupported algorithm: %s", algorithm));
+            }
+
+            return new Triplet<>(aesKey, hmacKey, hmac);
         }
 
         @Override
@@ -186,73 +322,5 @@ abstract class AesCbcHmacSha2 extends SymmetricEncryptionAlgorithm {
 
         // Create the Encryptor
         return new AesCbcHmacSha2Encryptor(getName(), key, iv, authenticationData, provider);
-    }
-
-    private static Triplet<byte[], byte[], Mac> getAlgorithmParameters(String algorithm, byte[] key) throws InvalidKeyException, NoSuchAlgorithmException {
-
-        byte[] aesKey;
-        byte[] hmacKey;
-        Mac hmac;
-
-        if (algorithm.equalsIgnoreCase(Aes128CbcHmacSha256.ALGORITHM_NAME)) {
-            if ((key.length << 3) < 256) {
-                throw new IllegalArgumentException(String.format("%s key length in bits %d < 256", algorithm, key.length << 3));
-            }
-
-            hmacKey = new byte[128 >> 3];
-            aesKey = new byte[128 >> 3];
-
-            // The HMAC key precedes the AES key
-            System.arraycopy(key, 0, hmacKey, 0, 128 >> 3);
-            System.arraycopy(key, 128 >> 3, aesKey, 0, 128 >> 3);
-
-            hmac = Mac.getInstance("HmacSHA256");
-            hmac.init(new SecretKeySpec(hmacKey, "HmacSHA256"));
-
-        } else if (algorithm.equalsIgnoreCase(Aes192CbcHmacSha384.ALGORITHM_NAME)) {
-
-            if ((key.length << 3) < 384) {
-                throw new IllegalArgumentException(String.format("%s key length in bits %d < 384", algorithm, key.length << 3));
-            }
-
-            hmacKey = new byte[192 >> 3];
-            aesKey = new byte[192 >> 3];
-
-            // The HMAC key precedes the AES key
-            System.arraycopy(key, 0, hmacKey, 0, 192 >> 3);
-            System.arraycopy(key, 192 >> 3, aesKey, 0, 192 >> 3);
-
-            hmac = Mac.getInstance("HmacSHA384");
-            hmac.init(new SecretKeySpec(hmacKey, "HmacSHA384"));
-        } else if (algorithm.equalsIgnoreCase(Aes256CbcHmacSha512.ALGORITHM_NAME)) {
-
-            if ((key.length << 3) < 512) {
-                throw new IllegalArgumentException(String.format("%s key length in bits %d < 512", algorithm, key.length << 3));
-            }
-
-            hmacKey = new byte[256 >> 3];
-            aesKey = new byte[256 >> 3];
-
-            // The HMAC key precedes the AES key
-            System.arraycopy(key, 0, hmacKey, 0, 256 >> 3);
-            System.arraycopy(key, 256 >> 3, aesKey, 0, 256 >> 3);
-
-            hmac = Mac.getInstance("HmacSHA512");
-            hmac.init(new SecretKeySpec(hmacKey, "HmacSHA512"));
-        } else {
-            throw new IllegalArgumentException(String.format("Unsupported algorithm: %s", algorithm));
-        }
-
-        return new Triplet<>(aesKey, hmacKey, hmac);
-    }
-
-    private static byte[] toBigEndian(long i) {
-
-        byte[] shortRepresentation = BigInteger.valueOf(i).toByteArray();
-        byte[] longRepresentation = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
-
-        System.arraycopy(shortRepresentation, 0, longRepresentation, longRepresentation.length - shortRepresentation.length, shortRepresentation.length);
-
-        return longRepresentation;
     }
 }
