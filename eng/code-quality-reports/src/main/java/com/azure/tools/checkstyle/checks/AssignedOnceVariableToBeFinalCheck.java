bf46e02390f918fdd2f57c4e613fc6cc8158b390
Victor Vazquez <victor.vazquez@wizeline.com>
2019-08-07 15:18:09 -0700

    use TokenUtil for child looping instead of manual for function

--- eng/code-quality-reports/src/main/java/com/azure/tools/checkstyle/checks/AssignedOnceVariableToBeFinalCheck.java
+++ eng/code-quality-reports/src/main/java/com/azure/tools/checkstyle/checks/AssignedOnceVariableToBeFinalCheck.java
@@ -6,9 +6,11 @@ package com.azure.tools.checkstyle.checks;
 import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
 import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+import com.puppycrawl.tools.checkstyle.utils.TokenUtil;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -25,19 +27,22 @@ import java.util.Set;
 public class AssignedOnceVariableToBeFinalCheck extends AbstractCheck {
     private static final String ERROR_MSG = "Field \"%s\" is only assigned in constructor and it is not final. " +
         "Make field final";
-    private static final Set<Integer> INVALID_FINAL_COMBINATION = new HashSet<>(Arrays.asList(
+    private static final Set<Integer> INVALID_FINAL_COMBINATION = Collections
+        .unmodifiableSet(new HashSet<>(Arrays.asList(
         TokenTypes.LITERAL_TRANSIENT,
         TokenTypes.LITERAL_VOLATILE
-    ));
-    private static final Set<String> INVALID_FINAL_ANNOTATIONS = new HashSet<>(Arrays.asList(
+    )));
+    private static final Set<String> INVALID_FINAL_ANNOTATIONS = Collections
+        .unmodifiableSet(new HashSet<>(Arrays.asList(
         "JsonProperty"
-    ));
+    )));
 
 
-    private static ArrayList<DetailAST> nonFinalFields;
-    private static Set<String> assignmentsFromConstructor;
-    private static Set<String> assignmentsFromMethods;
-    private static DetailAST scopeParent = null;
+    private ArrayList<DetailAST> nonFinalFields;
+    private Set<String> assignmentsFromConstructor;
+    private Set<String> assignmentsFromMethods;
+    private DetailAST scopeParent = null;
+    private Set<String> currentScopeParameterSet = null;
 
     @Override
     public int[] getDefaultTokens() {
@@ -106,7 +111,6 @@ public class AssignedOnceVariableToBeFinalCheck extends AbstractCheck {
                 checkAssignation(token);
                 break;
             case TokenTypes.METHOD_DEF:
-                scopeParent = token;
             case TokenTypes.CTOR_DEF:
                 scopeParent = token;
             default:
@@ -121,6 +125,7 @@ public class AssignedOnceVariableToBeFinalCheck extends AbstractCheck {
             case TokenTypes.METHOD_DEF:
             case TokenTypes.CTOR_DEF:
                 scopeParent = null;
+                currentScopeParameterSet = null;
             default:
                 break;
         }
@@ -128,7 +133,7 @@ public class AssignedOnceVariableToBeFinalCheck extends AbstractCheck {
 
     @Override
     public void finishTree(DetailAST token) {
-        for (DetailAST field: nonFinalFields) {
+        for (DetailAST field : nonFinalFields) {
             final String fieldName = field.findFirstToken(TokenTypes.IDENT).getText();
             if (assignmentsFromConstructor.contains(fieldName) && !assignmentsFromMethods.contains(fieldName)) {
                 log(field, String.format(ERROR_MSG, fieldName));
@@ -136,6 +141,47 @@ public class AssignedOnceVariableToBeFinalCheck extends AbstractCheck {
         }
     }
 
+    /*
+    * Get the field token from an assignation token.
+    * This method handles cases for fields referenced as `this.field` or only `field`
+    * It will get parameters from the method definition to ignore assignations to those parameters
+    */
+    private DetailAST getAssignedField(final DetailAST assignationToken) {
+        final Set<String> scopeParentParameterSet = getParameterSet(scopeParent.findFirstToken(
+            TokenTypes.PARAMETERS));
+        final DetailAST assignationWithDot = assignationToken.findFirstToken(TokenTypes.DOT);
+
+        if (assignationWithDot != null) {
+            if (assignationWithDot.branchContains(TokenTypes.LITERAL_THIS)) {
+                return assignationWithDot.findFirstToken(TokenTypes.IDENT);
+            }
+        } else {
+            final DetailAST variableNameToken = assignationToken.getFirstChild();
+            // make sure the assignation is not for a method parameter
+            if (!scopeParentParameterSet.contains(variableNameToken.getText())) {
+                return variableNameToken;
+            }
+        }
+
+        return null;
+    }
+
+    /*
+    * Saves a field name to a container depending on the provided type
+    */
+    private void saveField(String fieldName, int scopeParentType) {
+        if (scopeParentType == TokenTypes.METHOD_DEF) {
+            assignmentsFromMethods.add(fieldName);
+        } else if (scopeParentType == TokenTypes.CTOR_DEF) {
+            assignmentsFromConstructor.add(fieldName);
+        }
+    }
+
+    /*
+     * Review an assignation to save fields that gets assigned in constructor or in any method
+     *
+     * @param assignationToken an assignation token
+     */
     private void checkAssignation(final DetailAST assignationToken) {
         if (scopeParent == null || assignationToken.getChildCount() == 0) {
             // not inside any method or constructor definition. No need to check anything
@@ -149,35 +195,14 @@ public class AssignedOnceVariableToBeFinalCheck extends AbstractCheck {
             return;
         }
 
-        final HashSet<String> scopeParentParameterList = getParameterList(scopeParent.findFirstToken(
-            TokenTypes.PARAMETERS));
-        final int scopeParentType = scopeParent.getType();
+        DetailAST fieldToken = getAssignedField(assignationToken);
 
-        final DetailAST assignationWithDot = assignationToken.getFirstChild();
-        DetailAST fieldToken = null;
-        if (assignationWithDot != null && assignationWithDot.getType() == TokenTypes.DOT) {
-            if (assignationWithDot.branchContains(TokenTypes.LITERAL_THIS)) {
-                fieldToken = assignationWithDot.findFirstToken(TokenTypes.IDENT);
-            }
-        } else {
-            final DetailAST variableNameToken = assignationToken.getFirstChild();
-            // make sure the assignation is not for a method parameter
-            if (!scopeParentParameterList.contains(variableNameToken.getText())) {
-                fieldToken = variableNameToken;
-            }
-        }
         if (fieldToken != null) {
-            final String fieldName = fieldToken.getText();
-
-            if (scopeParentType == TokenTypes.METHOD_DEF && !assignmentsFromMethods.contains(fieldName)) {
-                assignmentsFromMethods.add(fieldName);
-            } else if (scopeParentType == TokenTypes.CTOR_DEF && !assignmentsFromConstructor.contains(fieldName)) {
-                assignmentsFromConstructor.add(fieldName);
-            }
+            saveField(fieldToken.getText(), scopeParent.getType());
         }
     }
 
-    /**
+    /*
      * Check if variable modifiers contains any of the illegal combination with final modifier
      * For instance, we don't want to combine transient or volatile with final
      *
@@ -200,40 +225,42 @@ public class AssignedOnceVariableToBeFinalCheck extends AbstractCheck {
         return false;
     }
 
-    /**
+    /*
      * Check each non-final field definition from a class and fill nonFinalFields
      *
      * @param classDefinitionAST a class definition AST
      */
     private void fillClassFieldDefinitions(DetailAST classDefinitionAST) {
         final DetailAST classObjBlockAst = classDefinitionAST.findFirstToken(TokenTypes.OBJBLOCK);
-        for (DetailAST astChild = classObjBlockAst.getFirstChild(); astChild != null;
-             astChild = astChild.getNextSibling()) {
-            if (TokenTypes.VARIABLE_DEF == astChild.getType()) {
-                final DetailAST variableModifiersAst = astChild.findFirstToken(TokenTypes.MODIFIERS);
-                if (!variableModifiersAst.branchContains(TokenTypes.FINAL)
-                    && !hasIllegalCombination(variableModifiersAst)) {
-                    nonFinalFields.add(astChild);
-                }
+
+        TokenUtil.forEachChild(classObjBlockAst, TokenTypes.VARIABLE_DEF, (definitionToken) -> {
+            final DetailAST variableModifiersAst = definitionToken.findFirstToken(TokenTypes.MODIFIERS);
+            if (!variableModifiersAst.branchContains(TokenTypes.FINAL)
+                && !hasIllegalCombination(variableModifiersAst)) {
+                nonFinalFields.add(definitionToken);
             }
-        }
+        });
     }
 
-    /**
+    /*
      * Get a node AST with parameters definition and return the list of all parameter names
+     * The set of parameters is created the first time an assignation is check within a method or constructor
+     * and we don't need to generate it again until visiting a different method or constructor.
+     * Field `currentScopeParameterSet` ensures we don't create the set multiple times for the same method/constructor
      *
      * @param parametersAST a TokenTypes.PARAMETERS
      * @return a set of parameter names
      */
-    private HashSet<String> getParameterList (DetailAST parametersAST) {
-        final HashSet<String> parameterList = new HashSet<>();
-        for (DetailAST parameter = parametersAST.findFirstToken(TokenTypes.PARAMETER_DEF); parameter != null;
-             parameter = parameter.getNextSibling()) {
-            if (parameter.getType() == TokenTypes.PARAMETER_DEF) {
-                final String parameterName = parameter.findFirstToken(TokenTypes.IDENT).getText();
-                parameterList.add(parameterName);
-            }
+    private Set<String> getParameterSet (DetailAST parametersAST) {
+        if (currentScopeParameterSet != null) {
+            return currentScopeParameterSet;
         }
-        return parameterList;
+        currentScopeParameterSet = new HashSet<>();
+        TokenUtil.forEachChild(parametersAST, TokenTypes.PARAMETER_DEF, (paramDefToken) -> {
+            final String parameterName = paramDefToken.findFirstToken(TokenTypes.IDENT).getText();
+            currentScopeParameterSet.add(parameterName);
+        });
+
+        return currentScopeParameterSet;
     }
 }
