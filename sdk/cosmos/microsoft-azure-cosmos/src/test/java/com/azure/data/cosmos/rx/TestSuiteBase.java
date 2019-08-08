6e13b4afd3cf954bac42f0aee842c8406761a5c9
Kushagra Thapar <kuthapar@microsoft.com>
2019-08-07 21:00:37 -0700

    Cosmos Key Credential (#4885)
    Cosmos Key Credential holds the key credentials, and supports key rotations. User can update the key in CosmosKeyCredential object, and that will be reflected in SDK on the fly.

--- sdk/cosmos/microsoft-azure-cosmos/src/test/java/com/azure/data/cosmos/rx/TestSuiteBase.java
+++ sdk/cosmos/microsoft-azure-cosmos/src/test/java/com/azure/data/cosmos/rx/TestSuiteBase.java
@@ -23,6 +23,7 @@ import com.azure.data.cosmos.CosmosDatabaseResponse;
 import com.azure.data.cosmos.CosmosItem;
 import com.azure.data.cosmos.CosmosItemProperties;
 import com.azure.data.cosmos.CosmosItemResponse;
+import com.azure.data.cosmos.CosmosKeyCredential;
 import com.azure.data.cosmos.CosmosResponse;
 import com.azure.data.cosmos.CosmosResponseValidator;
 import com.azure.data.cosmos.CosmosStoredProcedureRequestOptions;
@@ -99,6 +100,8 @@ public class TestSuiteBase extends CosmosClientTest {
     private static final ImmutableList<ConsistencyLevel> desiredConsistencies;
     private static final ImmutableList<Protocol> protocols;
 
+    protected static final CosmosKeyCredential cosmosKeyCredential;
+
     protected int subscriberValidationTimeout = TIMEOUT;
 
     private static CosmosDatabase SHARED_DATABASE;
@@ -113,7 +116,7 @@ public class TestSuiteBase extends CosmosClientTest {
     protected static CosmosDatabase getSharedCosmosDatabase(CosmosClient client) {
         return CosmosBridgeInternal.getCosmosDatabaseWithNewClient(SHARED_DATABASE, client);
     }
-    
+
     protected static CosmosContainer getSharedMultiPartitionCosmosContainer(CosmosClient client) {
         return CosmosBridgeInternal.getCosmosContainerWithNewClient(SHARED_MULTI_PARTITION_COLLECTION, SHARED_DATABASE, client);
     }
@@ -140,6 +143,8 @@ public class TestSuiteBase extends CosmosClientTest {
         objectMapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
         objectMapper.configure(JsonParser.Feature.ALLOW_TRAILING_COMMA, true);
         objectMapper.configure(JsonParser.Feature.STRICT_DUPLICATE_DETECTION, true);
+
+        cosmosKeyCredential = new CosmosKeyCredential(TestConfigurations.MASTER_KEY);
     }
 
     protected TestSuiteBase() {
@@ -519,7 +524,7 @@ public class TestSuiteBase extends CosmosClientTest {
                                                       .flatMap(page -> Flux.fromIterable(page.results()))
                                                       .collectList()
                                                       .block();
-        
+
         if (!res.isEmpty()) {
             deleteCollection(database, collectionId);
         }
@@ -803,7 +808,7 @@ public class TestSuiteBase extends CosmosClientTest {
     private static Object[][] simpleClientBuildersWithDirect(Protocol... protocols) {
         logger.info("Max test consistency to use is [{}]", accountConsistency);
         List<ConsistencyLevel> testConsistencies = ImmutableList.of(ConsistencyLevel.EVENTUAL);
-        
+
         boolean isMultiMasterEnabled = preferredLocations != null && accountConsistency == ConsistencyLevel.SESSION;
 
         List<CosmosClientBuilder> cosmosConfigurations = new ArrayList<>();
@@ -876,7 +881,7 @@ public class TestSuiteBase extends CosmosClientTest {
     static List<ConsistencyLevel> allEqualOrLowerConsistencies(ConsistencyLevel accountConsistency) {
         List<ConsistencyLevel> testConsistencies = new ArrayList<>();
         switch (accountConsistency) {
-        
+
             case STRONG:
                 testConsistencies.add(ConsistencyLevel.STRONG);
             case BOUNDED_STALENESS:
@@ -924,7 +929,7 @@ public class TestSuiteBase extends CosmosClientTest {
         options.maxRetryWaitTimeInSeconds(SUITE_SETUP_TIMEOUT);
         connectionPolicy.retryOptions(options);
         return CosmosClient.builder().endpoint(TestConfigurations.HOST)
-                .key(TestConfigurations.MASTER_KEY)
+                .cosmosKeyCredential(cosmosKeyCredential)
                 .connectionPolicy(connectionPolicy)
                 .consistencyLevel(ConsistencyLevel.SESSION);
     }
@@ -935,7 +940,7 @@ public class TestSuiteBase extends CosmosClientTest {
         connectionPolicy.usingMultipleWriteLocations(multiMasterEnabled);
         connectionPolicy.preferredLocations(preferredLocations);
         return CosmosClient.builder().endpoint(TestConfigurations.HOST)
-                .key(TestConfigurations.MASTER_KEY)
+                .cosmosKeyCredential(cosmosKeyCredential)
                 .connectionPolicy(connectionPolicy)
                 .consistencyLevel(consistencyLevel);
     }
@@ -963,7 +968,7 @@ public class TestSuiteBase extends CosmosClientTest {
         doAnswer((Answer<Protocol>)invocation -> protocol).when(configs).getProtocol();
 
         CosmosClientBuilder builder = CosmosClient.builder().endpoint(TestConfigurations.HOST)
-                .key(TestConfigurations.MASTER_KEY)
+                .cosmosKeyCredential(cosmosKeyCredential)
                 .connectionPolicy(connectionPolicy)
                 .consistencyLevel(consistencyLevel);
 
