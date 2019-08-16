e91fbba7432d207e3895b7b489d01733b93f9ed6
Jonathan Cardenas <JonathanCrd@users.noreply.github.com>
2019-08-16 16:06:39 -0700

    MIT headers

--- eng/smoke-tests/src/main/java/com/azure/App.java
+++ eng/smoke-tests/src/main/java/com/azure/App.java
@@ -1,7 +1,5 @@
-// ------------------------------------
-// Copyright(c) Microsoft Corporation.
+// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-// ------------------------------------
 package com.azure;
 
 import java.io.IOException;
