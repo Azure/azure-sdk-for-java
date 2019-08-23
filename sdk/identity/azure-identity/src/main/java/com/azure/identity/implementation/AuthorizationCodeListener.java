1d221a2f4a35ac769494fd9f8fbea6c4852e7490
vinay <vigera@microsoft.com>
2019-08-23 02:55:32 +0000

    update code

--- sdk/identity/azure-identity/src/main/java/com/azure/identity/implementation/AuthorizationCodeListener.java
+++ sdk/identity/azure-identity/src/main/java/com/azure/identity/implementation/AuthorizationCodeListener.java
@@ -4,19 +4,22 @@
 package com.azure.identity.implementation;
 
 import com.azure.core.implementation.http.UrlBuilder;
+import com.sun.net.httpserver.HttpServer;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.MonoProcessor;
-import reactor.netty.DisposableServer;
-import reactor.netty.http.server.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 
 /**
  * A local HTTP server that listens to the authorization code response from Azure Active Directory.
  */
 public final class AuthorizationCodeListener {
-    private DisposableServer server;
+    private HttpServer server;
     private MonoProcessor<String> authorizationCodeEmitter;
 
-    private AuthorizationCodeListener(DisposableServer server, MonoProcessor<String> authorizationCodeEmitter) {
+    private AuthorizationCodeListener(HttpServer server, MonoProcessor<String> authorizationCodeEmitter) {
         this.server = server;
         this.authorizationCodeEmitter = authorizationCodeEmitter;
     }
@@ -28,14 +31,24 @@ public final class AuthorizationCodeListener {
      */
     public static Mono<AuthorizationCodeListener> create(int port) {
         MonoProcessor<String> monoProcessor = MonoProcessor.create();
-        return HttpServer.create()
-            .port(port)
-            .handle((inbound, outbound) -> {
-                monoProcessor.onNext(getCodeFromUri(inbound.uri()));
-                return inbound.receive().then();
-            })
-            .bind()
-            .map(server -> new AuthorizationCodeListener(server, monoProcessor));
+        try {
+            HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+            server.createContext("/", httpExchange -> {
+                String url = httpExchange.getRequestURI().toString();
+                System.out.println(url);
+                monoProcessor.onNext(getCodeFromUri(url));
+
+                String response = "";
+                httpExchange.sendResponseHeaders(200, response.getBytes().length);
+                OutputStream os = httpExchange.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            });
+            server.setExecutor(null);
+            return Mono.just(new AuthorizationCodeListener(server, monoProcessor));
+        } catch (IOException e) {
+            return Mono.error(e);
+        }
     }
 
     /**
@@ -43,7 +56,7 @@ public final class AuthorizationCodeListener {
      * @return a Publisher signaling the completion
      */
     public Mono<Void> dispose() {
-        return Mono.fromRunnable(() -> server.disposeNow());
+        return Mono.fromRunnable(() -> server.stop(0));
     }
 
     /**
@@ -51,11 +64,12 @@ public final class AuthorizationCodeListener {
      * @return a Publisher emitting an authorization code
      */
     public Mono<String> listen() {
+        server.start();
         return authorizationCodeEmitter;
     }
 
-    private static String getCodeFromUri(String uri) {
-        UrlBuilder urlBuilder = UrlBuilder.parse(uri);
+    private static String getCodeFromUri(String url) {
+        UrlBuilder urlBuilder = UrlBuilder.parse(url);
         for (String query : urlBuilder.query().keySet()) {
             if ("code".equalsIgnoreCase(query)) {
                 return urlBuilder.query().get(query);
