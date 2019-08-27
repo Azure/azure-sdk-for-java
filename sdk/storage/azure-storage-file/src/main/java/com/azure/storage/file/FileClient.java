9b33a3b3f031d4a8e0431b9de97202f6292ee7ff
shafang <Shawn.Fang@microsoft.com>
2019-08-27 10:02:51 -0700

    storage codesnippet ID should match with begin and end code sample id, case sensitive

--- sdk/storage/azure-storage-file/src/main/java/com/azure/storage/file/FileClient.java
+++ sdk/storage/azure-storage-file/src/main/java/com/azure/storage/file/FileClient.java
@@ -429,7 +429,7 @@ public class FileClient {
      *
      * <p>Set the metadata to "file:updatedMetadata"</p>
      *
-     * {@codesnippet com.azure.storage.file.FileClient.setMetadataWithResponse#Map-Context}
+     * {@codesnippet com.azure.storage.file.fileClient.setMetadataWithResponse#map-Context}
      *
      * <p>Clear the metadata of the file</p>
      *
