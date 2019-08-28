bb3fa674f7346afc5b15ed103a0b441f2587e8ce
Gauri Prasad <gapra@microsoft.com>
2019-08-28 15:53:52 -0700

    Changed get mode back to check env variable

--- sdk/storage/azure-storage-queue/src/test/java/com/azure/storage/queue/spock/APISpec.groovy
+++ sdk/storage/azure-storage-queue/src/test/java/com/azure/storage/queue/spock/APISpec.groovy
@@ -84,18 +84,18 @@ class APISpec extends Specification {
      * </ul>
      */
     def getTestMode() {
-//        def azureTestMode = ConfigurationManager.getConfiguration().get(AZURE_TEST_MODE)
-//
-//        if (azureTestMode != null) {
-//            try {
-//                return TestMode.valueOf(azureTestMode.toUpperCase(Locale.US))
-//            } catch (IllegalArgumentException e) {
-//                logger.error("Could not parse '{}' into TestEnum. Using 'Playback' mode.", azureTestMode)
-//                return TestMode.PLAYBACK
-//            }
-//        }
-//
-//        logger.info("Environment variable '{}' has not been set yet. Using 'Playback' mode.", AZURE_TEST_MODE)
+        def azureTestMode = ConfigurationManager.getConfiguration().get(AZURE_TEST_MODE)
+
+        if (azureTestMode != null) {
+            try {
+                return TestMode.valueOf(azureTestMode.toUpperCase(Locale.US))
+            } catch (IllegalArgumentException e) {
+                logger.error("Could not parse '{}' into TestEnum. Using 'Playback' mode.", azureTestMode)
+                return TestMode.PLAYBACK
+            }
+        }
+
+        logger.info("Environment variable '{}' has not been set yet. Using 'Playback' mode.", AZURE_TEST_MODE)
         return TestMode.PLAYBACK
     }
 
