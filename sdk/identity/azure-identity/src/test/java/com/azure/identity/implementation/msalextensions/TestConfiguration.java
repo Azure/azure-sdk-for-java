34c8446ccd07c5e9c5032cd2d77b777ebef2b608
Fatima Qarni <fatima.q832@gmail.com>
2019-08-29 22:29:36 +0000

    style

---
+++ sdk/identity/azure-identity/src/test/java/com/azure/identity/implementation/msalextensions/TestConfiguration.java
@@ -0,0 +1,19 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.s
+
+package com.azure.identity.implementation.msalextensions;
+
+public class TestConfiguration {
+
+    final static String TENANT_SPECIFIC_AUTHORITY = "https://login.microsoftonline.com/[insert here]/";
+
+    final static String PUBLIC_CLIENT_ID = "";
+
+    final static String GRAPH_DEFAULT_SCOPE = "https://graph.windows.net/.default";
+
+    final static String CONFIDENTIAL_CLIENT_ID = "";
+    final static String CONFIDENTIAL_CLIENT_ID_2 = "";
+
+    final static String CONFIDENTIAL_CLIENT_SECRET = "";
+    final static String CONFIDENTIAL_CLIENT_SECRET_2 = "";
+}
