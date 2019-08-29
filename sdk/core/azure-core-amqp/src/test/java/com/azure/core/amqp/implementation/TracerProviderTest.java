7a3a4957dbd3f9f719e2e1040c6fa4ef1b04b9ef
Connie <conniey@microsoft.com>
2019-08-29 13:52:45 -0700

    Testing with multiple Tracers

--- sdk/core/azure-core-amqp/src/test/java/com/azure/core/amqp/implementation/TracerProviderTest.java
+++ sdk/core/azure-core-amqp/src/test/java/com/azure/core/amqp/implementation/TracerProviderTest.java
@@ -8,10 +8,16 @@ import com.azure.core.amqp.exception.ErrorCondition;
 import com.azure.core.implementation.tracing.ProcessKind;
 import com.azure.core.implementation.tracing.Tracer;
 import com.azure.core.util.Context;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import reactor.core.publisher.Signal;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -20,96 +26,116 @@ import static com.azure.core.implementation.tracing.Tracer.OPENTELEMETRY_SPAN_KE
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.isNull;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class TracerProviderTest {
+    private static final String METHOD_NAME = "Azure.eventhubs.send";
+
+    @Mock
+    private Tracer tracer;
+    @Mock
+    private Tracer tracer2;
+
+    private List<Tracer> tracers;
+    private TracerProvider tracerProvider;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+
+        tracers = Arrays.asList(tracer, tracer2);
+        tracerProvider = new TracerProvider(tracers);
+    }
+
+    @After
+    public void teardown() {
+        Mockito.framework().clearInlineMocks();
+    }
 
     @Test
     public void startSpan() {
-        // Arrange
-        final Tracer tracer1 = mock(Tracer.class);
-        List<Tracer> tracers = Collections.singletonList(tracer1);
-        final TracerProvider tracerProvider = new TracerProvider(tracers);
-
         // Act
         tracerProvider.startSpan(Context.NONE, ProcessKind.SEND);
 
         // Assert
-        verify(tracer1, times(1)).start(eq("Azure.eventhubs.send"), any(), eq(ProcessKind.SEND));
+        for (Tracer t : tracers) {
+            verify(t, times(1))
+                .start(eq(METHOD_NAME), any(), eq(ProcessKind.SEND));
+        }
     }
 
     @Test
-    public void isEnabled() {
+    public void notEnabledWhenNoTracers() {
         // Arrange
-        List<Tracer> tracers = Collections.emptyList();
-        final TracerProvider tracerProvider = new TracerProvider(tracers);
+        final TracerProvider provider = new TracerProvider(Collections.emptyList());
 
         // Act & Assert
-        Assert.assertFalse(tracerProvider.isEnabled());
+        Assert.assertFalse(provider.isEnabled());
     }
 
     @Test
     public void startSpanReturnsUpdatedContext() {
         // Arrange
-        final Tracer tracer1 = mock(Tracer.class);
-        List<Tracer> tracers = Collections.singletonList(tracer1);
-        final TracerProvider tracerProvider = new TracerProvider(tracers);
-        when(tracer1.start("Azure.eventhubs.send", Context.NONE, ProcessKind.SEND)).thenAnswer(
+        final String parentKey = "parent-key";
+        final String parentValue = "parent-value";
+        final String childKey = "child-key";
+        final String childValue = "child-value";
+        final Context startingContext = Context.NONE;
+        when(tracer.start(METHOD_NAME, startingContext, ProcessKind.SEND)).thenAnswer(
             invocation -> {
                 Context passed = invocation.getArgument(1, Context.class);
-                return passed.addData(OPENTELEMETRY_SPAN_KEY, "value");
+                return passed.addData(parentKey, parentValue);
+            }
+        );
+        when(tracer2.start(eq(METHOD_NAME), any(), eq(ProcessKind.SEND))).thenAnswer(
+            invocation -> {
+                Context passed = invocation.getArgument(1, Context.class);
+                return passed.addData(childKey, childValue);
             }
         );
 
         // Act
-        Context updatedContext = tracerProvider.startSpan(Context.NONE, ProcessKind.SEND);
+        final Context updatedContext = tracerProvider.startSpan(startingContext, ProcessKind.SEND);
 
         // Assert
-        Assert.assertEquals(Context.class, updatedContext.getClass());
+        // Want to ensure that the data added to the parent and child are available.
+        final Optional<Object> parentData = updatedContext.getData(parentKey);
+        Assert.assertTrue(parentData.isPresent());
+        Assert.assertEquals(parentValue, parentData.get());
 
-        final Optional<Object> actual = updatedContext.getData(OPENTELEMETRY_SPAN_KEY);
-        Assert.assertTrue(actual.isPresent());
-        Assert.assertEquals(actual.get(), "value");
+        final Optional<Object> childData = updatedContext.getData(childKey);
+        Assert.assertTrue(childData.isPresent());
+        Assert.assertEquals(childValue, childData.get());
     }
 
     @Test
     public void endSpanSuccess() {
-        // Arrange
-        final Tracer tracer1 = mock(Tracer.class);
-        final List<Tracer> tracers = Collections.singletonList(tracer1);
-        final TracerProvider tracerProvider = new TracerProvider(tracers);
-
         // Act
         tracerProvider.endSpan(new Context(OPENTELEMETRY_SPAN_KEY, "value"), Signal.complete());
 
         // Assert
-        verify(tracer1, times(1)).end(eq("success"), isNull(), any(Context.class));
+        for (Tracer t : tracers) {
+            verify(t, times(1)).end(eq("success"), isNull(), any(Context.class));
+        }
     }
 
     @Test
     public void endSpanNoKey() {
-        // Arrange
-        final Tracer tracer1 = mock(Tracer.class);
-        final List<Tracer> tracers = Collections.singletonList(tracer1);
-        final TracerProvider tracerProvider = new TracerProvider(tracers);
-
         // Act
         tracerProvider.endSpan(Context.NONE, Signal.complete());
 
         // Assert
-        verify(tracer1, never()).end("", null, Context.NONE);
+        for (Tracer t : tracers) {
+            verify(t, never()).end("", null, Context.NONE);
+        }
     }
 
     @Test
     public void endSpanError() {
         // Arrange
-        final Tracer tracer1 = mock(Tracer.class);
-        final List<Tracer> tracers = Collections.singletonList(tracer1);
-        final TracerProvider tracerProvider = new TracerProvider(tracers);
         Throwable testThrow = new Throwable("testError");
         Context sendContext = new Context(OPENTELEMETRY_SPAN_KEY, "value");
 
@@ -117,15 +143,14 @@ public class TracerProviderTest {
         tracerProvider.endSpan(sendContext, Signal.error(testThrow));
 
         // Assert
-        verify(tracer1, times(1)).end("", testThrow, sendContext);
+        for (Tracer t : tracers) {
+            verify(t, times(1)).end("", testThrow, sendContext);
+        }
     }
 
     @Test
     public void endSpanOnSubscribe() {
         // Arrange
-        final Tracer tracer1 = mock(Tracer.class);
-        final List<Tracer> tracers = Collections.singletonList(tracer1);
-        final TracerProvider tracerProvider = new TracerProvider(tracers);
         Throwable testThrow = new Throwable("testError");
         Context sendContext = new Context(OPENTELEMETRY_SPAN_KEY, "value");
 
@@ -133,15 +158,14 @@ public class TracerProviderTest {
         tracerProvider.endSpan(sendContext, Signal.error(testThrow));
 
         // Assert
-        verify(tracer1, times(1)).end("", testThrow, sendContext);
+        for (Tracer t : tracers) {
+            verify(t, times(1)).end("", testThrow, sendContext);
+        }
     }
 
     @Test
     public void endSpanAmqpException() {
         // Arrange
-        final Tracer tracer1 = mock(Tracer.class);
-        final List<Tracer> tracers = Collections.singletonList(tracer1);
-        final TracerProvider tracerProvider = new TracerProvider(tracers);
         final ErrorCondition errorCondition = ErrorCondition.NOT_FOUND;
         final Exception exception = new AmqpException(true, errorCondition, "", null);
         Context sendContext = new Context(OPENTELEMETRY_SPAN_KEY, "value");
@@ -150,6 +174,24 @@ public class TracerProviderTest {
         tracerProvider.endSpan(sendContext, Signal.error(exception));
 
         // Assert
-        verify(tracer1, times(1)).end(errorCondition.getErrorCondition(), exception, sendContext);
+        for (Tracer t : tracers) {
+            verify(t, times(1))
+                .end(errorCondition.getErrorCondition(), exception, sendContext);
+        }
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void addSpanLinksNoContext() {
+        // Act
+        tracerProvider.addSpanLinks(null);
+    }
+
+    /**
+     * Verify that we add spans for all the tracers.
+     */
+    @Test(expected = NullPointerException.class)
+    public void addSpanLinks() {
+        // Act
+        tracerProvider.addSpanLinks(null);
     }
 }
