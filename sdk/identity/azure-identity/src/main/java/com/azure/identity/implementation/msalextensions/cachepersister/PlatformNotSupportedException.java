9003a1d7fad784388252775564b7bb079fa9780a
Fatima Qarni <fatima.q832@gmail.com>
2019-08-30 17:38:13 +0000

    make platform not supported a runtime exception

--- sdk/identity/azure-identity/src/main/java/com/azure/identity/implementation/msalextensions/cachepersister/PlatformNotSupportedException.java
+++ sdk/identity/azure-identity/src/main/java/com/azure/identity/implementation/msalextensions/cachepersister/PlatformNotSupportedException.java
@@ -7,7 +7,7 @@ package com.azure.identity.implementation.msalextensions.cachepersister;
  * Exception for when the current OS is not supported by {@link CachePersister}
  * so the OS specific DPAPI cannot be used to encrypt the token cache.
  * */
-public class PlatformNotSupportedException extends Exception {
+public class PlatformNotSupportedException extends RuntimeException {
 
     /**
      * Initializes PlatformNotSupportedException
