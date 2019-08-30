51bb3f72e965c1168d4fa43c818e8034daaab356
Sima Zhu <sizhu@microsoft.com>
2019-08-29 18:17:55 -0700

    Rename to the one we used in identity

--- sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/UnexpectedLengthException.java
+++ sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/UnexpectedLengthException.java
@@ -10,7 +10,7 @@ public final class UnexpectedLengthException extends IllegalStateException {
     private final long bytesRead;
     private final long bytesExpected;
 
-    UnexpectedLengthException(String message, long bytesRead, long bytesExpected) {
+    public UnexpectedLengthException(String message, long bytesRead, long bytesExpected) {
         super(message);
         this.bytesRead = bytesRead;
         this.bytesExpected = bytesExpected;
