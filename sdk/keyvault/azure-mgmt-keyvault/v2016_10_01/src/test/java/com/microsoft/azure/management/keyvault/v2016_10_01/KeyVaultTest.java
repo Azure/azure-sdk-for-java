3ffdf8a39632b72bf23ec72e2555f916bd062c17
weidongxu-microsoft <weidxu@microsoft.com>
2019-09-04 15:21:40 +0800

    test

--- sdk/keyvault/azure-mgmt-keyvault/v2016_10_01/src/test/java/com/microsoft/azure/management/keyvault/v2016_10_01/KeyVaultTest.java
+++ sdk/keyvault/azure-mgmt-keyvault/v2016_10_01/src/test/java/com/microsoft/azure/management/keyvault/v2016_10_01/KeyVaultTest.java
@@ -17,6 +17,10 @@ import org.junit.Assert;
 import com.microsoft.azure.arm.resources.Region;
 import com.microsoft.azure.management.resources.ResourceGroup;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
 public class  KeyVaultTest extends TestBase {
     protected static ResourceManager resourceManager;
     protected static KeyVaultManager keyVaultManager;
@@ -28,18 +32,62 @@ public class  KeyVaultTest extends TestBase {
                 .withSubscription(defaultSubscription);
         keyVaultManager = KeyVaultManager
                 .authenticate(restClient, defaultSubscription);
+
+        this.domain = domain;
     }
     @Override
     protected void cleanUpResources() {
         resourceManager.resourceGroups().deleteByName(rgName);
     }
     @Test
-    public void firstTest() {
+    public void basicTest() {
         rgName = SdkContext.randomResourceName("rg", 20);
         ResourceGroup group = resourceManager.resourceGroups()
-                .define(rgName)
-                .withRegion(Region.US_WEST.toString())
-                .create();
+            .define(rgName)
+            .withRegion(Region.US_WEST.toString())
+            .create();
         Assert.assertNotNull(group);
+
+        String tenantId = domain;
+        String vaultName = SdkContext.randomResourceName("kv", 10);
+        try {
+            AccessPolicyEntry accessPolicy = new AccessPolicyEntry()
+                .withTenantId(UUID.fromString(tenantId))
+                .withObjectId(PRINCIPAL_ID)
+                .withPermissions(new Permissions().withKeys(Arrays.asList(KeyPermissions.GET, KeyPermissions.CREATE, KeyPermissions.UPDATE)));
+
+            Vault vault = keyVaultManager.vaults()
+                .define(vaultName)
+                .withRegion(Region.US_WEST)
+                .withExistingResourceGroup(rgName)
+                .withProperties(
+                    new VaultProperties()
+                        .withTenantId(UUID.fromString(tenantId))
+                        .withSku(new Sku().withName(SkuName.STANDARD))
+                        .withEnabledForDeployment(true)
+                        .withAccessPolicies(Arrays.asList(accessPolicy))
+                )
+                .create();
+
+            Assert.assertNotNull(vault);
+            String vaultId = vault.id();
+
+            List<Vault> vaults = keyVaultManager.vaults().listByResourceGroup(rgName);
+            Assert.assertEquals(1, vaults.size());
+            vault = vaults.iterator().next();
+            Assert.assertEquals(vaultName, vault.name());
+            Assert.assertEquals(vaultId, vault.id());
+            Assert.assertEquals(SkuName.STANDARD, vault.properties().sku().name());
+            Assert.assertTrue(vault.properties().enabledForDeployment());
+
+            keyVaultManager.vaults().deleteByIds(vaultId);
+            vaults = keyVaultManager.vaults().listByResourceGroup(rgName);
+            Assert.assertEquals(0, vaults.size());
+        } finally {
+            resourceManager.resourceGroups().beginDeleteByName(rgName);
+        }
     }
+
+    protected String domain;
+    private static String PRINCIPAL_ID = "5ae156d4-2a10-47dc-a5c7-89fa4cd313ce";    // if it fails, user your clientId in AZURE_AUTH_LOCATION
 }
