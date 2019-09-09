3f6826b4c6403fafc6fbb606aee8f4b0f5b1c68f
Srikanta <51379715+srnagar@users.noreply.github.com>
2019-09-09 15:38:55 -0700

    Partition Manager implementation with Storage Blob (#5278)
    * Checkpoint store implementation using Storage Blobs

---
+++ sdk/eventhubs/azure-messaging-eventhubs-checkpointstore-blob/CONTRIBUTING.md
@@ -0,0 +1,83 @@
+# Contributing
+
+This project welcomes contributions and suggestions. Most contributions require you to agree to a Contributor License
+Agreement (CLA) declaring that you have the right to, and actually do, grant us the rights to use your contribution. For
+details, visit https://cla.microsoft.com.
+
+When you submit a pull request, a CLA-bot will automatically determine whether you need to provide a CLA and decorate
+the PR appropriately (e.g., label, comment). Simply follow the instructions provided by the bot. You will only need to
+do this once across all repositories using our CLA.
+
+## Code of conduct
+
+This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/).
+For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or contact
+[opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments.
+
+## Getting started
+
+Before working on a contribution, it would be beneficial to familiarize yourself with the process and guidelines used
+for the Azure SDKs so that your submission is consistent with the project standards and is ready to be accepted with
+fewer changes requested. In particular, it is recommended to review:
+
+- [Azure SDK README][github-general], to learn more about the overall project and processes used.
+- [Azure SDK Design Guidelines][design-guidelines], to understand the general guidelines for the SDKs across all
+  languages and platforms
+- [Azure SDK Design Guidelines for Java][java-spec], to understand the guidelines specific to the Azure SDKs for Java.
+
+## Development environment setup
+
+### Prerequisites
+
+- Java Development Kit (JDK) with version 8 or above
+- [Maven][maven]
+- Git
+- Microsoft Azure subscription
+    - You can create a free account at: https://azure.microsoft.com
+- Azure Event Hubs instance
+    - Step-by-step guide for [creating an Event Hub using the Azure Portal][event_hubs_create]
+- Azure Storage account
+    - Step-by-step guide for [creating a Storage account using the Azure Portal][storage_account]
+
+## Building all the client libraries
+
+Open a command prompt/terminal:
+1. Execute `git clone https://github.com/Azure/azure-sdk-for-java.git`
+1. Traverse to the repository root.
+1. Execute `mvn compile -f pom.client.xml`
+1. Install the tooling and build the product by executing:
+    * `mvn install -Dinclude-non-shipping-modules -DskipTests -Dgpg.skip -f pom.client.xml`
+
+## Building only the Azure SDK client library for Storage Blob Checkpoint Store
+
+After building the tooling and solution once from the section, 
+[Building all the client libraries](#building-all-the-client-libraries), you can build just the Azure SDK client library
+ for Checkpoint store using Storage Blobs by executing:
+1. `mvn compile -f sdk\eventhubs\azure-messaging-eventhubs-checkpointstore-blob\pom.xml`
+
+## Running tests
+
+After following instructions in [Building all the client libraries](#building-all-the-client-libraries), you can run the
+unit tests by executing:
+1. `mvn test -f sdk\eventhubs\azure-messaging-eventhubs-checkpointstore-blob\pom.xml`
+
+For unit tests, there are no special considerations; these are self-contained and execute locally without any reliance
+on external resources. These tests are run for all PR validations.
+
+## Logging output
+
+Log messages can be seen in the output window by:
+1. Setting `AZURE_LOG_LEVEL` to the desired verbosity. Log levels can be found in [ClientLogger][log-level]
+1. Adding an implementation of [slf4j][slf4j] to the classpath. Implementations can be found under section "[Binding with a
+   logging framework at deployment time][slf4j-implementations]".
+
+<!-- Links -->
+[design-guidelines]: https://azure.github.io/azure-sdk/java_design.html#
+[event_hubs_create]: https://docs.microsoft.com/en-us/azure/event-hubs/event-hubs-create
+[github-general]: https://github.com/Azure/azure-sdk
+[java-spec]: https://azure.github.io/azure-sdk/java_introduction.html
+[log-level]: https://github.com/Azure/azure-sdk-for-java/blob/master/core/azure-core/src/main/java/com/azure/core/util/logging/ClientLogger.java#L40
+[maven]: https://maven.apache.org/
+[slf4j]: https://www.slf4j.org/
+[slf4j-implementations]: https://www.slf4j.org/manual.html#swapping
+[storage_account]: https://docs.microsoft.com/azure/storage/common/storage-quickstart-create-account?tabs=azure-portal
---
+++ sdk/eventhubs/azure-messaging-eventhubs-checkpointstore-blob/src/test/java/com/azure/messaging/eventhubs/checkpointstore/blob/BlobPartitionManagerTest.java
@@ -0,0 +1,212 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.messaging.eventhubs.checkpointstore.blob;
+
+import static junit.framework.TestCase.assertNull;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.Mockito.when;
+
+import com.azure.core.exception.ResourceModifiedException;
+import com.azure.core.http.HttpHeaders;
+import com.azure.core.http.rest.PagedFlux;
+import com.azure.core.http.rest.ResponseBase;
+import com.azure.core.http.rest.VoidResponse;
+import com.azure.core.implementation.http.PagedResponseBase;
+import com.azure.messaging.eventhubs.models.Checkpoint;
+import com.azure.messaging.eventhubs.models.PartitionOwnership;
+import com.azure.storage.blob.BlobAsyncClient;
+import com.azure.storage.blob.BlockBlobAsyncClient;
+import com.azure.storage.blob.ContainerAsyncClient;
+import com.azure.storage.blob.models.BlobAccessConditions;
+import com.azure.storage.blob.models.BlobItem;
+import com.azure.storage.blob.models.BlobProperties;
+import com.azure.storage.blob.models.ListBlobsOptions;
+import com.azure.storage.blob.models.Metadata;
+import java.net.SocketTimeoutException;
+import java.nio.ByteBuffer;
+import java.time.OffsetDateTime;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+/**
+ * Unit tests for {@link BlobPartitionManager}.
+ */
+public class BlobPartitionManagerTest {
+
+    @Mock
+    private ContainerAsyncClient containerAsyncClient;
+
+    @Mock
+    private BlockBlobAsyncClient blockBlobAsyncClient;
+
+    @Mock
+    private BlobAsyncClient blobAsyncClient;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testListOwnerShip() {
+        BlobPartitionManager blobPartitionManager = new BlobPartitionManager(containerAsyncClient);
+        BlobItem blobItem = getBlobItem("owner1", "1", "230", "etag", "eh/cg/0");
+        PagedFlux<BlobItem> response = new PagedFlux<BlobItem>(() -> Mono.just(new PagedResponseBase<HttpHeaders,
+            BlobItem>(null, 200, null,
+            Arrays.asList(blobItem), null,
+            null)));
+        when(containerAsyncClient.listBlobsFlat(any(ListBlobsOptions.class))).thenReturn(response);
+
+        StepVerifier.create(blobPartitionManager.listOwnership("eh", "cg"))
+            .assertNext(partitionOwnership -> {
+                assertEquals("owner1", partitionOwnership.ownerId());
+                assertEquals("0", partitionOwnership.partitionId());
+                assertEquals(1, (long) partitionOwnership.sequenceNumber());
+                assertEquals(230, (long) partitionOwnership.offset());
+                assertEquals("eh", partitionOwnership.eventHubName());
+                assertEquals("cg", partitionOwnership.consumerGroupName());
+                assertEquals("etag", partitionOwnership.eTag());
+            }).verifyComplete();
+    }
+
+    @Test
+    public void testUpdateCheckpoint() {
+        Checkpoint checkpoint = new Checkpoint()
+            .eventHubName("eh")
+            .consumerGroupName("cg")
+            .ownerId("owner1")
+            .partitionId("0")
+            .eTag("etag")
+            .sequenceNumber(2L)
+            .offset(100L);
+
+        Map<String, String> headers = new HashMap<>();
+        headers.put("eTag", "etag2");
+        when(containerAsyncClient.getBlobAsyncClient("eh/cg/0")).thenReturn(blobAsyncClient);
+        when(blobAsyncClient.setMetadataWithResponse(any(Metadata.class), any(BlobAccessConditions.class)))
+            .thenReturn(Mono.just(new VoidResponse(null, 200, new HttpHeaders(headers))));
+
+        BlobPartitionManager blobPartitionManager = new BlobPartitionManager(containerAsyncClient);
+        StepVerifier.create(blobPartitionManager.updateCheckpoint(checkpoint))
+            .assertNext(etag -> assertEquals("etag2", etag)).verifyComplete();
+    }
+
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testClaimOwnership() {
+        PartitionOwnership po = createPartitionOwnership("eh", "cg", "0", "owner1");
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.put("eTag", "etag2");
+
+        when(containerAsyncClient.getBlobAsyncClient("eh/cg/0")).thenReturn(blobAsyncClient);
+        when(blobAsyncClient.asBlockBlobAsyncClient()).thenReturn(blockBlobAsyncClient);
+        when(blockBlobAsyncClient.uploadWithResponse(ArgumentMatchers.<Flux<ByteBuffer>>any(), eq(0L),
+            isNull(), any(Metadata.class), any(BlobAccessConditions.class)))
+            .thenReturn(Mono.just(new ResponseBase<>(null, 200, httpHeaders, null, null)));
+
+        BlobPartitionManager blobPartitionManager = new BlobPartitionManager(containerAsyncClient);
+        StepVerifier.create(blobPartitionManager.claimOwnership(po))
+            .assertNext(partitionOwnership -> {
+                assertEquals("owner1", partitionOwnership.ownerId());
+                assertEquals("0", partitionOwnership.partitionId());
+                assertEquals("eh", partitionOwnership.eventHubName());
+                assertEquals("cg", partitionOwnership.consumerGroupName());
+                assertEquals("etag2", partitionOwnership.eTag());
+                assertNull(partitionOwnership.sequenceNumber());
+                assertNull(partitionOwnership.offset());
+            }).verifyComplete();
+    }
+
+
+    @Test
+    public void testListOwnershipError() {
+        BlobPartitionManager blobPartitionManager = new BlobPartitionManager(containerAsyncClient);
+        PagedFlux<BlobItem> response = new PagedFlux<>(() -> Mono.error(new SocketTimeoutException()));
+        when(containerAsyncClient.listBlobsFlat(any(ListBlobsOptions.class))).thenReturn(response);
+
+        StepVerifier.create(blobPartitionManager.listOwnership("eh", "cg"))
+            .expectError(SocketTimeoutException.class).verify();
+    }
+
+    @Test
+    public void testUpdateCheckpointError() {
+        Checkpoint checkpoint = new Checkpoint()
+            .eventHubName("eh")
+            .consumerGroupName("cg")
+            .ownerId("owner1")
+            .partitionId("0")
+            .eTag("etag")
+            .sequenceNumber(2L)
+            .offset(100L);
+
+        Map<String, String> headers = new HashMap<>();
+        headers.put("eTag", "etag2");
+        when(containerAsyncClient.getBlobAsyncClient("eh/cg/0")).thenReturn(blobAsyncClient);
+        when(blobAsyncClient.setMetadataWithResponse(any(Metadata.class), any(BlobAccessConditions.class)))
+            .thenReturn(Mono.error(new SocketTimeoutException()));
+
+        BlobPartitionManager blobPartitionManager = new BlobPartitionManager(containerAsyncClient);
+        StepVerifier.create(blobPartitionManager.updateCheckpoint(checkpoint))
+                .expectError(SocketTimeoutException.class).verify();
+    }
+
+    @Test
+    public void testFailedOwnershipClaim() {
+        PartitionOwnership po = createPartitionOwnership("eh", "cg", "0", "owner1");
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.put("eTag", "etag2");
+
+        when(containerAsyncClient.getBlobAsyncClient("eh/cg/0")).thenReturn(blobAsyncClient);
+        when(blobAsyncClient.asBlockBlobAsyncClient()).thenReturn(blockBlobAsyncClient);
+        when(blockBlobAsyncClient.uploadWithResponse(ArgumentMatchers.<Flux<ByteBuffer>>any(), eq(0L),
+            isNull(), any(Metadata.class), any(BlobAccessConditions.class)))
+            .thenReturn(Mono.error(new ResourceModifiedException("Etag did not match", null)));
+        BlobPartitionManager blobPartitionManager = new BlobPartitionManager(containerAsyncClient);
+        StepVerifier.create(blobPartitionManager.claimOwnership(po)).verifyComplete();
+    }
+
+    private PartitionOwnership createPartitionOwnership(String eventHubName, String consumerGroupName,
+        String partitionId, String ownerId) {
+        return new PartitionOwnership()
+            .eventHubName(eventHubName)
+            .consumerGroupName(consumerGroupName)
+            .partitionId(partitionId)
+            .ownerId(ownerId);
+    }
+
+    private BlobItem getBlobItem(String owner, String sequenceNumber, String offset, String etag, String blobName) {
+        Metadata metadata = getMetadata(owner, sequenceNumber, offset);
+
+        BlobProperties properties = new BlobProperties()
+            .lastModified(OffsetDateTime.now())
+            .etag(etag);
+
+        return new BlobItem()
+            .name(blobName)
+            .metadata(metadata)
+            .properties(properties);
+    }
+
+    private Metadata getMetadata(String owner, String sequenceNumber, String offset) {
+        Metadata metadata = new Metadata();
+        metadata.put("OwnerId", owner);
+        metadata.put("SequenceNumber", sequenceNumber);
+        metadata.put("Offset", offset);
+        return metadata;
+    }
+}
