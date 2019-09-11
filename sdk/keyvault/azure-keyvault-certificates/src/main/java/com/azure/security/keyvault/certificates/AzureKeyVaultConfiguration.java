0368bb29eb8983ed6c5ae2fe1a97ad96bdef2137
Connie <conniey@microsoft.com>
2019-09-11 10:56:59 -0700

    Bump KeyVault versions to 4.0.0-preview.4

--- sdk/keyvault/azure-keyvault-certificates/src/main/java/com/azure/security/keyvault/certificates/AzureKeyVaultConfiguration.java
+++ sdk/keyvault/azure-keyvault-certificates/src/main/java/com/azure/security/keyvault/certificates/AzureKeyVaultConfiguration.java
@@ -6,5 +6,5 @@ package com.azure.security.keyvault.certificates;
 class AzureKeyVaultConfiguration {
     //TODO: Eventually remove these hardcoded strings with https://github.com/Azure/azure-sdk-for-java/issues/3141
     static final String SDK_NAME = "Azure-Keyvault";
-    static final String SDK_VERSION = "4.0.0-preview.3";
+    static final String SDK_VERSION = "4.0.0-preview.4";
 }
