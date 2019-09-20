42fbbc76a569562b4e98e3117f5078a7a7c21947
Connie <conniey@microsoft.com>
2019-09-20 13:57:13 -0700

    Add MessageSerializer interface.

---
+++ sdk/eventhubs/azure-messaging-eventhubs/src/main/java/com/azure/messaging/eventhubs/implementation/MessageSerializer.java
@@ -0,0 +1,38 @@
+package com.azure.messaging.eventhubs.implementation;
+
+import org.apache.qpid.proton.message.Message;
+
+/**
+ * Serializer to translate objects to and from proton-j {@link Message messages}.
+ */
+public interface MessageSerializer {
+    /**
+     * Gets the serialized size of an AMQP message
+     *
+     * @param amqpMessage AMQP message to serialize.
+     * @return The size, in bytes, of the serialized AMQP message.
+     */
+    int getSize(Message amqpMessage);
+
+    /**
+     * Serializes the given {@code object} into an AMQP message.
+     *
+     * @param object Object to send to AMQP service.
+     * @param clazz Class of the {@code object} to serialize.
+     * @param <T> Type of the object to serialize into an AMQP message.
+     *
+     * @return An AMQP message that can be sent to the service.
+     */
+    <T> Message serialize(T object, Class<T> clazz);
+
+    /**
+     * Deserializes the AMQP message to a concrete object.
+     *
+     * @param message AMQP message to deserialize.
+     * @param clazz Class to deserialize object into.
+     * @param <T> Type of the deserialized message.
+     *
+     * @return The deserialized object from an AMQP message.
+     */
+    <T> T deserialize(Message message, Class<T> clazz);
+}
