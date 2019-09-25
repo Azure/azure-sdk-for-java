23c32f3b5500819d9ae00acc2933f6c8a414ebe3
Christopher Anderson <chrande@microsoft.com>
2019-09-25 15:27:43 -0700

    4.0.0-preview.4

--- sdk/cosmos/microsoft-azure-cosmos/src/main/java/com/azure/data/cosmos/internal/HttpConstants.java
+++ sdk/cosmos/microsoft-azure-cosmos/src/main/java/com/azure/data/cosmos/internal/HttpConstants.java
@@ -252,7 +252,7 @@ public class HttpConstants {
         // TODO: FIXME we can use maven plugin for generating a version file
         // @see
         // https://stackoverflow.com/questions/2469922/generate-a-version-java-file-in-maven
-        public static final String SDK_VERSION = "3.2.0";
+        public static final String SDK_VERSION = "4.0.0-preview.4";
         public static final String SDK_NAME = "cosmosdb-java-sdk";
     }
 
