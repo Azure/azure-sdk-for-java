3c433bb8018b7efbb8d94090cde0bddd87717dca
Gauri Prasad <gapra@microsoft.com>
2019-09-25 17:16:59 -0700

    Fixed bug with servicebuilder and removed ADLS from CI

--- sdk/storage/azure-storage-file-datalake/src/main/java/com/azure/storage/file/datalake/implementation/DataLakeStorageClientBuilder.java
+++ sdk/storage/azure-storage-file-datalake/src/main/java/com/azure/storage/file/datalake/implementation/DataLakeStorageClientBuilder.java
@@ -6,7 +6,7 @@ package com.azure.storage.file.datalake.implementation;
 
 import com.azure.core.http.HttpPipeline;
 import com.azure.core.implementation.RestProxy;
-import com.azure.core.implementation.annotation.ServiceClientBuilder;
+import com.azure.core.annotation.ServiceClientBuilder;
 
 /**
  * A builder for creating a new instance of the DataLakeStorageClient type.
