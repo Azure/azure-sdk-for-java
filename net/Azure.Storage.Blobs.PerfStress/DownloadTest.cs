c45acd0469c13405ab7b1e9f19d83d91fe0fecc9
Mike Harder <mharder@microsoft.com>
2019-10-01 16:10:24 -0700

    Pass ParallelTransferOptions

--- net/Azure.Storage.Blobs.PerfStress/DownloadTest.cs
+++ net/Azure.Storage.Blobs.PerfStress/DownloadTest.cs
@@ -9,17 +9,25 @@ namespace Azure.Storage.Blobs.PerfStress
     {
         public DownloadTest(ParallelTransferOptionsOptions options) : base(options)
         {
+            try
+            {
+                BlobClient.Delete();
+            }
+            catch (StorageRequestFailedException)
+            {
+            }
+
             BlobClient.Upload(RandomStream);
         }
 
         public override void Run(CancellationToken cancellationToken)
         {
-            BlobClient.Download(Stream.Null, cancellationToken);
+            BlobClient.Download(Stream.Null, parallelTransferOptions: ParallelTransferOptions, cancellationToken: cancellationToken);
         }
 
         public override Task RunAsync(CancellationToken cancellationToken)
         {
-            return BlobClient.DownloadAsync(Stream.Null, cancellationToken);
+            return BlobClient.DownloadAsync(Stream.Null, parallelTransferOptions: ParallelTransferOptions, cancellationToken: cancellationToken);
         }
 
         public override void Dispose()
