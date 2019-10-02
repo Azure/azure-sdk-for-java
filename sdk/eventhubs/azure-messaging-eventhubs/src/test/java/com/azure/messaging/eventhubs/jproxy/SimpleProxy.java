5bdfef897b2fa4a4fe9728891a24b4de9b0a954f
Connie <conniey@microsoft.com>
2019-10-02 08:44:14 -0700

    Cleaning up simple proxy.

--- sdk/eventhubs/azure-messaging-eventhubs/src/test/java/com/azure/messaging/eventhubs/jproxy/SimpleProxy.java
+++ sdk/eventhubs/azure-messaging-eventhubs/src/test/java/com/azure/messaging/eventhubs/jproxy/SimpleProxy.java
@@ -11,6 +11,7 @@ import java.nio.channels.AsynchronousServerSocketChannel;
 import java.nio.channels.AsynchronousSocketChannel;
 import java.nio.channels.CompletionHandler;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
 
 /**
@@ -23,25 +24,22 @@ public class SimpleProxy implements ProxyServer {
 
     private final AtomicBoolean isRunning;
 
-    private volatile Consumer<Throwable> onErrorHandler;
+    private Consumer<Throwable> onErrorHandler;
     private volatile AsynchronousServerSocketChannel serverSocket;
 
     public SimpleProxy(final String hostname, final int port) {
         this.port = port;
         this.hostname = hostname;
-
         this.isRunning = new AtomicBoolean(false);
     }
 
     @Override
     public void start(final Consumer<Throwable> onError) throws IOException {
-        if (this.isRunning.get()) {
+        if (isRunning.getAndSet(true)) {
             throw new IllegalStateException("ProxyServer is already running");
         }
 
-        this.isRunning.set(true);
-        this.onErrorHandler = onError;
-
+        onErrorHandler = onError;
         serverSocket = AsynchronousServerSocketChannel.open();
         serverSocket.bind(new InetSocketAddress("localhost", port));
         scheduleListener(serverSocket);
@@ -49,40 +47,56 @@ public class SimpleProxy implements ProxyServer {
 
     @Override
     public void stop() throws IOException {
-        if (this.isRunning.getAndSet(false)) {
-            this.serverSocket.close();
+        if (isRunning.getAndSet(false)) {
+            serverSocket.close();
         }
     }
 
     private void scheduleListener(final AsynchronousServerSocketChannel serverSocket) {
-        serverSocket.accept(
-            serverSocket,
-            new CompletionHandler<AsynchronousSocketChannel, AsynchronousServerSocketChannel>() {
-                @Override
-                public void completed(
-                    AsynchronousSocketChannel client,
-                    AsynchronousServerSocketChannel serverSocket) {
+        serverSocket.accept(serverSocket, new SocketListener(logger, onErrorHandler, isRunning));
+    }
 
-                    try {
-                        logger.warning("Client connected from: {}", client.getRemoteAddress().toString());
-                    } catch (IOException e) {
-                        logger.error("Error occurred while getting remote address.", e);
-                    }
+    private static class SocketListener implements
+        CompletionHandler<AsynchronousSocketChannel, AsynchronousServerSocketChannel> {
 
-                    serverSocket.accept(serverSocket, this);
+        private final ClientLogger logger;
+        private final Consumer<Throwable> onErrorHandler;
+        private final AtomicBoolean isRunning;
+        private final AtomicReference<ProxyNegotiationHandler> negotiationHandler = new AtomicReference<>();
 
-                    try {
-                        new ProxyNegotiationHandler(client);
-                    } catch (IOException e) {
-                        onErrorHandler.accept(e);
-                    }
-                }
+        private SocketListener(ClientLogger logger, Consumer<Throwable> onErrorHandler, AtomicBoolean isRunning) {
+            this.logger = logger;
+            this.onErrorHandler = onErrorHandler;
+            this.isRunning = isRunning;
+        }
 
-                @Override
-                public void failed(Throwable exc, AsynchronousServerSocketChannel attachment) {
-                    isRunning.set(false);
-                    onErrorHandler.accept(exc);
-                }
-            });
+        @Override
+        public void completed(AsynchronousSocketChannel client, AsynchronousServerSocketChannel serverSocket) {
+            try {
+                logger.warning("Client connected from: {}", client.getRemoteAddress().toString());
+            } catch (IOException e) {
+                logger.error("Error occurred while getting remote address.", e);
+            }
+
+            if (negotiationHandler.get() != null) {
+                logger.error("Negotiation handler has already been set. We are not setting another.");
+                return;
+            }
+
+            logger.info("Setting negotiation handler.");
+            serverSocket.accept(serverSocket, this);
+
+            try {
+                negotiationHandler.set(new ProxyNegotiationHandler(client));
+            } catch (IOException e) {
+                onErrorHandler.accept(e);
+            }
+        }
+
+        @Override
+        public void failed(Throwable exception, AsynchronousServerSocketChannel attachment) {
+            isRunning.set(false);
+            onErrorHandler.accept(exception);
+        }
     }
 }
