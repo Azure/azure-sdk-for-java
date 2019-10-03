7849b7e4c0023f64711abadcf0ee8215378fe477
Hemant Tanwar <hetanwar@users.noreply.github.com>
2019-10-03 10:49:36 -0700

    Incorporated API review comments

--- sdk/core/azure-core-amqp/src/main/java/com/azure/core/amqp/EndpointStateNotifier.java
+++ sdk/core/azure-core-amqp/src/main/java/com/azure/core/amqp/EndpointStateNotifier.java
@@ -12,28 +12,24 @@ public interface EndpointStateNotifier {
 
     /**
      * Gets the current state of the endpoint.
-     *
      * @return The current state of the endpoint.
      */
     AmqpEndpointState getCurrentState();
 
     /**
      * Gets the errors that occurred in the AMQP endpoint.
-     *
      * @return A stream of errors that occurred in the AMQP endpoint.
      */
     Flux<Throwable> getErrors();
 
     /**
      * Gets the endpoint states for the AMQP endpoint.
-     *
      * @return A stream of endpoint states as they occur in the endpoint.
      */
     Flux<AmqpEndpointState> getConnectionStates();
 
     /**
      * Gets any shutdown signals that occur in the AMQP endpoint.
-     *
      * @return A stream of shutdown signals that occur in the AMQP endpoint.
      */
     Flux<AmqpShutdownSignal> getShutdownSignals();
