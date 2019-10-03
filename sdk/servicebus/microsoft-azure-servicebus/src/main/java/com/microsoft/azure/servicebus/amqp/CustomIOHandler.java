ba91b7d4ff291dfc606e356aeb1954c17ae3a6d7
Vijaya Gopal Yarramneni <viyarr@microsoft.com>
2019-10-02 17:17:56 -0700

    Minor tweak to the comment

--- sdk/servicebus/microsoft-azure-servicebus/src/main/java/com/microsoft/azure/servicebus/amqp/CustomIOHandler.java
+++ sdk/servicebus/microsoft-azure-servicebus/src/main/java/com/microsoft/azure/servicebus/amqp/CustomIOHandler.java
@@ -19,7 +19,7 @@ public class CustomIOHandler extends IOHandler {
         }
 
         Transport transport = Proton.transport();
-        //To detect connection drops in worst case in idleTimeout, which otherwise can take a non deterministic
+        // To detect connection drops in worst case in idleTimeout, which otherwise can take a non deterministic
         // amount of time to get detected. In practice, we saw 15-20 minutes, on Linux docker containers when 
         // simulating packet drops with iptables rules.
         transport.setIdleTimeout(AmqpConstants.TRANSPORT_IDLE_TIMEOUT_MILLIS);
