4e54cc3e413f636e19e8c5579c16449b04ebfd34
Kushagra Thapar <kuthapar@microsoft.com>
2019-10-03 11:36:34 -0700

    Fixed doOnError in Store Client and Consistency Writer to handle unwrapped exceptions

--- sdk/cosmos/microsoft-azure-cosmos/src/main/java/com/azure/data/cosmos/internal/directconnectivity/ConsistencyWriter.java
+++ sdk/cosmos/microsoft-azure-cosmos/src/main/java/com/azure/data/cosmos/internal/directconnectivity/ConsistencyWriter.java
@@ -21,6 +21,7 @@ import com.azure.data.cosmos.internal.Utils;
 import org.apache.commons.collections4.ComparatorUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reactor.core.Exceptions;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
@@ -164,7 +165,8 @@ public class ConsistencyWriter {
                                            .doOnError(
                                                t -> {
                                                    try {
-                                                       CosmosClientException ex = Utils.as(t, CosmosClientException.class);
+                                                       Throwable unwrappedException = Exceptions.unwrap(t);
+                                                       CosmosClientException ex = Utils.as(unwrappedException, CosmosClientException.class);
                                                        try {
                                                            BridgeInternal.recordResponse(request.requestContext.cosmosResponseDiagnostics, request,
                                                                storeReader.createStoreResult(null, ex, false, false, primaryUri));
