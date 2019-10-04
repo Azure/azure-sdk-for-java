ffe914471c1045909f1551eaf10fab3b3cacdba4
Mike Harder <mharder@microsoft.com>
2019-10-04 10:34:30 -0700

    Add options and perf tracing

--- HttpMock/Log.cs
+++ HttpMock/Log.cs
@@ -8,58 +8,60 @@ namespace HttpMock
     {
         public static void LogRequest(HttpRequest request)
         {
-#if DEBUG
-            var uriBuilder = new UriBuilder()
+            if (Program.Options.Debug)
             {
-                Scheme = request.Scheme,
-                Host = request.Host.Host,
-                Path = request.Path.Value,
-                Query = request.QueryString.Value,
-            };
+                var uriBuilder = new UriBuilder()
+                {
+                    Scheme = request.Scheme,
+                    Host = request.Host.Host,
+                    Path = request.Path.Value,
+                    Query = request.QueryString.Value,
+                };
 
-            if (request.Host.Port.HasValue)
-            {
-                uriBuilder.Port = request.Host.Port.Value;
+                if (request.Host.Port.HasValue)
+                {
+                    uriBuilder.Port = request.Host.Port.Value;
+                }
+
+                var uri = uriBuilder.Uri;
+
+                Console.WriteLine($"{request.Method} {uri.ToString()} {request.Protocol}");
+
+                foreach (var header in request.Headers)
+                {
+                    Console.WriteLine($"{header.Key}: {header.Value}");
+                }
+
+                Console.WriteLine();
             }
-
-            var uri = uriBuilder.Uri;
-
-            Console.WriteLine($"{request.Method} {uri.ToString()} {request.Protocol}");
-
-            foreach (var header in request.Headers)
-            {
-                Console.WriteLine($"{header.Key}: {header.Value}");
-            }
-
-            Console.WriteLine();
-#endif
         }
 
         public static void LogUpstreamRequest(HttpRequestMessage requestMessage)
         {
-#if DEBUG
-            Console.WriteLine(requestMessage);
-            Console.WriteLine();
-#endif
+            if (Program.Options.Debug)
+            {
+                Console.WriteLine(requestMessage);
+                Console.WriteLine();
+            }
         }
 
         public static void LogUpstreamResponse(HttpResponseMessage responseMessage, bool cached)
         {
-            if (cached)
+            if (Program.Options.Debug)
             {
-                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(responseMessage);
+                Console.WriteLine();
             }
-
-#if DEBUG
-            Console.WriteLine(responseMessage);
-            Console.WriteLine();
-#else
-            Console.Write(".");
-#endif
-
-            if (cached)
+            else if (!Program.Options.Trace)
             {
-                Console.ResetColor();
+                if (cached)
+                {
+                    Console.Write(".");
+                }
+                else
+                {
+                    Console.Write("*");
+                }
             }
         }
     }
--- HttpMock/Program.cs
+++ HttpMock/Program.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using CommandLine;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using System;
 using System.Collections.Concurrent;
 using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Runtime;
 
 namespace HttpMock
 {
@@ -13,7 +15,33 @@ namespace HttpMock
         private static ConcurrentDictionary<RequestCacheKey, HttpResponseMessage> _cache =
             new ConcurrentDictionary<RequestCacheKey, HttpResponseMessage>();
 
+
+        public class HttpMockOptions
+        {
+            [Option("debug")]
+            public bool Debug { get; set; }
+
+            [Option("trace")]
+            public bool Trace { get; set; }
+        }
+
+        public static HttpMockOptions Options { get; private set; }
+
         static void Main(string[] args)
+        {
+            if (!GCSettings.IsServerGC)
+            {
+                throw new InvalidOperationException("Requires server GC");
+            }
+
+            Parser.Default.ParseArguments<HttpMockOptions>(args).WithParsed(o =>
+            {
+                Options = o;
+                Run();
+            });
+        }
+
+        static void Run()
         {
             Console.WriteLine("Connection String (unencrypted): \"BlobEndpoint=http://<hostname>:5000\"");
             Console.WriteLine("Connection String (encrypted): \"BlobEndpoint=https://<hostname>:5001\"");
@@ -31,28 +59,56 @@ namespace HttpMock
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .Configure(app => app.Run(async context =>
                 {
+                    Trace("Start Request");
+
                     var request = context.Request;
                     var response = context.Response;
 
                     Log.LogRequest(request);
 
                     var key = new RequestCacheKey(request);
+
+                    Trace("Created Cache Key");
+
                     if (_cache.TryGetValue(key, out var upstreamResponse))
                     {
+                        Trace("Cache Hit");
                         Log.LogUpstreamResponse(upstreamResponse, cached: true);
+                        
                         await Proxy.SendDownstreamResponse(upstreamResponse, response);
+
+                        Trace("Sent Downstream Response");
                     }
                     else
                     {
+                        Trace("Cache Miss");
+                        
                         upstreamResponse = await Proxy.SendUpstreamRequest(request);
+
+                        Trace("Received Upstream Response");                        
                         Log.LogUpstreamResponse(upstreamResponse, cached: false);
+
                         await Proxy.SendDownstreamResponse(upstreamResponse, response);
+
+                        Trace("Sent Downstream Response");
+
                         _cache.AddOrUpdate(key, upstreamResponse, (k, r) => upstreamResponse);
+
+                        Trace("Updated Cache");
                     }
 
+                    Trace("End Request" + Environment.NewLine);
                 }))
                 .Build()
                 .Run();
+        }
+         
+        private static void Trace(string message)
+        {
+            if (Options.Trace)
+            {
+                Console.WriteLine($"[{DateTime.Now.ToString("o")}] {message}");
+            }
         }
     }
 }
