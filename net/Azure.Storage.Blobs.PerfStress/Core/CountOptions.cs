35625c2dad732fa7d40de0b38d4f744989d5dfd6
Mike Harder <mharder@microsoft.com>
2019-10-09 10:51:35 -0700

    Increase default count to 500

--- net/Azure.Storage.Blobs.PerfStress/Core/CountOptions.cs
+++ net/Azure.Storage.Blobs.PerfStress/Core/CountOptions.cs
@@ -5,7 +5,7 @@ namespace Azure.Storage.Blobs.PerfStress.Core
 {
     public class CountOptions : PerfStressOptions
     {
-        [Option('c', "count", Default = 100, HelpText = "Number of blobs")]
+        [Option('c', "count", Default = 500, HelpText = "Number of blobs")]
         public int Count { get; set; }
     }
 }
