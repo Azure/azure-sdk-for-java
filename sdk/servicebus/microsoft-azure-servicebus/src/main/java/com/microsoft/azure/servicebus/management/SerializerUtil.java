787ce73c5fba72d660b00c8adafb70308d44564c
Neeraj Makam <neeraj.makam@microsoft.com>
2019-10-11 17:05:59 -0700

    Disabling DTD (#5706)
    XML Serialization had security issues regarding DTD. Disabling those features which are not required.
    Fixes #4466 .

--- sdk/servicebus/microsoft-azure-servicebus/src/main/java/com/microsoft/azure/servicebus/management/SerializerUtil.java
+++ sdk/servicebus/microsoft-azure-servicebus/src/main/java/com/microsoft/azure/servicebus/management/SerializerUtil.java
@@ -3,11 +3,32 @@
 
 package com.microsoft.azure.servicebus.management;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.time.Duration;
 
 // Fields that require special serializations
 public class SerializerUtil {
-    
+    private static final String XML_FEATURE_DISALLOW_DOC_TYPE = "http://apache.org/xml/features/disallow-doctype-decl";
+    private static final String XML_FEATURE_EXTERNAL_GENERAL_ENTITIES = "http://xml.org/sax/features/external-general-entities";
+    private static final String XML_FEATURE_EXTERNAL_PARAM_ENTITIES = "http://xml.org/sax/features/external-parameter-entities";
+    private static final String XML_FEATURE_LOAD_EXTERNAL_DTD = "http://apache.org/xml/features/nonvalidating/load-external-dtd";
+
+    public static DocumentBuilderFactory getDocumentBuilderFactory() throws ParserConfigurationException {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+        // Disallow DTDs (doctypes) to prevent most XML attacks.
+        dbf.setFeature(XML_FEATURE_DISALLOW_DOC_TYPE, true);
+        dbf.setFeature(XML_FEATURE_EXTERNAL_GENERAL_ENTITIES, false);
+        dbf.setFeature(XML_FEATURE_EXTERNAL_PARAM_ENTITIES, false);
+        dbf.setFeature(XML_FEATURE_LOAD_EXTERNAL_DTD, false);
+        // as per Timothy Morgan's 2014 paper: "XML Schema, DTD, and Entity Attacks"
+        dbf.setXIncludeAware(false);
+        dbf.setExpandEntityReferences(false);
+
+        return dbf;
+    }
+
     public static String serializeDuration(Duration duration) {
         if (duration == null || duration.isNegative() || duration.isZero()) {
             return "";
@@ -26,19 +47,19 @@ public class SerializerUtil {
                 sb.append(hours).append("H");
                 remainingTime = duration.minusHours(hours);
             }
-            
+
             long minutes = remainingTime.toMinutes();
             if (minutes > 0) {
                 sb.append(minutes).append("M");
                 remainingTime = duration.minusMinutes(minutes);
             }
-            
+
             long seconds = remainingTime.getSeconds();
             if (seconds > 0) {
                 sb.append(seconds).append("S");
             }
         }
-        
+
         return sb.toString();
     }
 }
