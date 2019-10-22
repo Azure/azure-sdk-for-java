034f71f4820a5fa4ef5e7e6c439d4c31630c9669
Rick Ley <frley@microsoft.com>
2019-10-22 14:42:15 -0700

    Revert "Optimizations for default block size on HTBB"
    This reverts commit b8206459115c469fae4a34a12d68213e31c260d9.

--- sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/specialized/BlobAsyncClientBase.java
+++ sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/specialized/BlobAsyncClientBase.java
@@ -3,7 +3,6 @@
 
 package com.azure.storage.blob.specialized;
 
-import static com.azure.core.implementation.util.FluxUtil.withContext;
 import com.azure.core.http.HttpPipeline;
 import com.azure.core.http.HttpResponse;
 import com.azure.core.http.RequestConditions;
@@ -66,6 +65,7 @@ import java.util.concurrent.locks.ReentrantLock;
 
 import static com.azure.core.implementation.util.FluxUtil.fluxError;
 import static com.azure.core.implementation.util.FluxUtil.monoError;
+import static com.azure.core.implementation.util.FluxUtil.withContext;
 
 /**
  * This class provides a client that contains all operations that apply to any blob type.
@@ -747,9 +747,11 @@ public class BlobAsyncClientBase {
     // TODO (gapra) : Investigate if this is can be parallelized, and include the parallelTransfers parameter.
     Mono<Response<BlobProperties>> downloadToFileWithResponse(String filePath, BlobRange range,
         ParallelTransferOptions parallelTransferOptions, ReliableDownloadOptions options,
-        BlobRequestConditions accessConditions, boolean rangeGetContentMD5, Context context) {
-        ParallelTransferOptions finalParallelTransferOptions = new ParallelTransferOptions();
-        finalParallelTransferOptions.populateAndApplyDefaults(parallelTransferOptions);
+        BlobRequestConditions accessConditions, boolean rangeGetContentMd5, Context context) {
+        final ParallelTransferOptions finalParallelTransferOptions = parallelTransferOptions == null
+            ? new ParallelTransferOptions()
+            : parallelTransferOptions;
+        ProgressReceiver progressReceiver = finalParallelTransferOptions.getProgressReceiver();
 
         // See ProgressReporter for an explanation on why this lock is necessary and why we use AtomicLong.
         AtomicLong totalProgress = new AtomicLong(0);
@@ -758,9 +760,9 @@ public class BlobAsyncClientBase {
         return Mono.using(() -> downloadToFileResourceSupplier(filePath),
             channel -> getPropertiesWithResponse(accessConditions)
                 .flatMap(response -> processInRange(channel, response,
-                range, finalParallelTransferOptions.getBlockSize(), options, accessConditions, rangeGetContentMD5,
-                context, totalProgress, progressLock, finalParallelTransferOptions.getProgressReceiver())),
-            this::downloadToFileCleanup);
+                    range, finalParallelTransferOptions.getBlockSize(), options, accessConditions, rangeGetContentMd5,
+                    context, totalProgress, progressLock, progressReceiver)), this::downloadToFileCleanup);
+
     }
 
     private Mono<Response<BlobProperties>> processInRange(AsynchronousFileChannel channel,
