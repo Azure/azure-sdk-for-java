ff9461f79dffee0423c231cc20595702acb770c4
Mike Harder <mharder@microsoft.com>
2019-10-23 17:38:32 -0700

    Use ByteArrayInputStream instead of CircularStream - Workaround for Azure/azure-sdk-for-java#6020

--- java/azure-perfstress/src/main/java/com/azure/perfstress/RandomStream.java
+++ java/azure-perfstress/src/main/java/com/azure/perfstress/RandomStream.java
@@ -1,5 +1,6 @@
 package com.azure.perfstress;
 
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.Random;
 
@@ -7,11 +8,14 @@ public class RandomStream {
     private static final byte[] _randomBytes;
 
     static {
-        _randomBytes = new byte[1024 * 1024];
+        // _randomBytes = new byte[1024 * 1024];
+        _randomBytes = new byte[Integer.MAX_VALUE];
         (new Random(0)).nextBytes(_randomBytes);
     }
     
     public static InputStream create(long size) {
-        return CircularStream.create(_randomBytes, size);
+        // Workaround for Azure/azure-sdk-for-java#6020
+        // return CircularStream.create(_randomBytes, size);
+        return new ByteArrayInputStream(_randomBytes, 0, (int)size);
     }
 }
