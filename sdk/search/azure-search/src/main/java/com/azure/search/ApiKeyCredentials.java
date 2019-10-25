106e72900626838f6aba11bc1fc049cc21aa55bf
Noel <noelbundick@gmail.com>
2019-10-25 15:11:16 -0700

    Add javadocs for ApiKeyCredentials (#192)

--- sdk/search/azure-search/src/main/java/com/azure/search/ApiKeyCredentials.java
+++ sdk/search/azure-search/src/main/java/com/azure/search/ApiKeyCredentials.java
@@ -3,13 +3,29 @@
 
 package com.azure.search;
 
+/**
+ * Credentials that authorize requests to Azure Cognitive Search. It uses content within the HTTP request to
+ * generate the correct "api-key" header value
+ *
+ * @see SearchIndexClientBuilder
+ * @see SearchServiceClientBuilder
+ */
 public class ApiKeyCredentials {
     private final String apiKey;
 
+    /**
+     * Creates an instance that is able to authorize requests to Azure Cognitive Search.
+     *
+     * @param apiKey a query or admin API key
+     */
     public ApiKeyCredentials(String apiKey) {
         this.apiKey = apiKey;
     }
 
+    /**
+     * Gets the API key
+     * @return the API key
+     */
     public String getApiKey() {
         return apiKey;
     }
