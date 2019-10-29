eea4b100df64e1f36df7abd0c31fa208f64b272a
sima <sizhu@microsoft.com>
2019-10-28 17:33:29 -0700

    Remove the storage file module.

--- sdk/storage/azure-storage-file-share/src/main/java/module-info.java
+++ sdk/storage/azure-storage-file-share/src/main/java/module-info.java
@@ -1,21 +1,21 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-module com.azure.storage.file {
+module com.azure.storage.file.share {
     requires transitive com.azure.core;
     requires transitive com.azure.storage.common;
     requires com.fasterxml.jackson.dataformat.xml;
 
-    exports com.azure.storage.file;
-    exports com.azure.storage.file.models;
+    exports com.azure.storage.file.share;
+    exports com.azure.storage.file.share.models;
 
-    opens com.azure.storage.file.models to
+    opens com.azure.storage.file.share.models to
         com.fasterxml.jackson.databind,
         com.azure.core;
-    opens com.azure.storage.file.implementation to
+    opens com.azure.storage.file.share.implementation to
         com.fasterxml.jackson.databind,
         com.azure.core;
-    opens com.azure.storage.file.implementation.models to
+    opens com.azure.storage.file.share.implementation.models to
         com.fasterxml.jackson.databind,
         com.azure.core;
 }
