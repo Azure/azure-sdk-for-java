8be5e487d8168ca8e9befa2156b9a20b15b6c375
sima <sizhu@microsoft.com>
2019-10-29 10:48:31 -0700

    Remove unnessary

--- sdk/core/azure-core-test/src/main/java/com/azure/core/test/InterceptorManager.java
+++ sdk/core/azure-core-test/src/main/java/com/azure/core/test/InterceptorManager.java
@@ -16,7 +16,6 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
