dda3e65480c8887b4fed8a8ba05a095fb2918efb
Connie <conniey@microsoft.com>
2019-10-30 00:22:39 -0700

    Add documentation.

--- sdk/eventhubs/azure-messaging-eventhubs/src/main/java/com/azure/messaging/eventhubs/models/PartitionEvent.java
+++ sdk/eventhubs/azure-messaging-eventhubs/src/main/java/com/azure/messaging/eventhubs/models/PartitionEvent.java
@@ -6,20 +6,42 @@ package com.azure.messaging.eventhubs.models;
 import com.azure.core.annotation.Immutable;
 import com.azure.messaging.eventhubs.EventData;
 
+import java.util.Objects;
+
+/**
+ * An event received within the context of an Event Hub partition.
+ */
 @Immutable
 public class PartitionEvent {
     private final PartitionContext partitionContext;
     private final EventData eventData;
 
+    /**
+     * Creates a new instance with the context and event data set.
+     *
+     * @param partitionContext Information about the partition the event was in.
+     * @param eventData Event received from the partition.
+     * @throws NullPointerException if {@code partitionContext} or {@code eventData} is null.
+     */
     public PartitionEvent(PartitionContext partitionContext, EventData eventData) {
-        this.partitionContext = partitionContext;
-        this.eventData = eventData;
+        this.partitionContext = Objects.requireNonNull(partitionContext, "'partitionContext' cannot be null.");
+        this.eventData = Objects.requireNonNull(eventData, "'eventData' cannot be null.");;
     }
 
+    /**
+     * Gets information about the partition the received event was in.
+     *
+     * @return Information about the partition the received event was in.
+     */
     public PartitionContext getPartitionContext() {
         return partitionContext;
     }
 
+    /**
+     * Gets the event received from the partition.
+     *
+     * @return Event received from the partition.
+     */
     public EventData getEventData() {
         return eventData;
     }
