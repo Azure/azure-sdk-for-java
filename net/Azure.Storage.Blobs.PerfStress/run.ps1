2314ca008391c6eff3d9d42bd4b4c81c5f4d870b
Mike Harder <mharder@microsoft.com>
2019-11-01 20:43:15 +0000

    Add env var

--- net/Azure.Storage.Blobs.PerfStress/run.ps1
+++ net/Azure.Storage.Blobs.PerfStress/run.ps1
@@ -1,3 +1,3 @@
 #!/usr/bin/env pwsh
 
-docker run -it --rm --network host azure-storage-blobs-perfstress @args
+docker run -it --rm --network host -e STORAGE_CONNECTION_STRING azure-storage-blobs-perfstress @args
