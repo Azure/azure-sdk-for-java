379a0ee46f9e079b8b074e6694d39e85c7928c45
Mike Harder <mharder@microsoft.com>
2019-11-01 13:36:04 -0700

    Add build scripts and dockerfile

---
+++ net/Azure.Storage.Blobs.V11.PerfStress/build.ps1
@@ -0,0 +1,3 @@
+#!/usr/bin/env pwsh
+
+docker build -t azure-storage-blobs-v11-perfstress -f Dockerfile ..
---
+++ net/Azure.Storage.Blobs.V11.PerfStress/run.ps1
@@ -0,0 +1,3 @@
+#!/usr/bin/env pwsh
+
+docker run -it --rm --network host azure-storage-blobs-v11-perfstress @args
