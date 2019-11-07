8313a1b2d8142571fe632dd5bbccb8c1f32c586a
James Suplizio <jasupliz@microsoft.com>
2019-11-07 15:34:13 -0800

    This is just a test and will NEVER be checked in

--- sdk/appconfiguration/azure-data-appconfiguration/src/main/java/com/azure/data/appconfiguration/AzureConfiguration.java
+++ sdk/appconfiguration/azure-data-appconfiguration/src/main/java/com/azure/data/appconfiguration/AzureConfiguration.java
@@ -1,15 +1,15 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.azure.storage.file.share;
+package com.azure.data.appconfiguration;
 
-/*
+/**
  * Gets the SDK information for this library component.
  */
-class FileConfiguration {
+class AzureConfiguration {
     //TODO: Eventually remove these hardcoded strings with https://github.com/Azure/azure-sdk-for-java/issues/3141
-    static final String NAME = "azure-storage-file-share";
-    // {x-version-update-start;com.azure:azure-storage-file-share;current}
-    static final String VERSION = "12.0.0-preview.6";
+    static final String NAME = "application-configuration";
+    // {x-version-update-start;com.azure:azure-data-appconfiguration;current}
+    static final String VERSION = "1.0.0-preview.7";
     // {x-version-update-end}
 }
