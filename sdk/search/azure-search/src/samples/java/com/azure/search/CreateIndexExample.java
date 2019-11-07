00585ab95515082e58be5d0e22bf28cb96aaace9
srikanthraju2016 <32080857+srikanthraju2016@users.noreply.github.com>
2019-11-07 10:52:59 -0800

    Create index example (#229)
    * simple example of create index
    
    * updated

---
+++ sdk/search/azure-search/src/samples/java/com/azure/search/CreateIndexExample.java
@@ -0,0 +1,44 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.search;
+
+import com.azure.search.models.DataType;
+import com.azure.search.models.Field;
+import com.azure.search.models.Index;
+
+import java.util.Arrays;
+
+public class CreateIndexExample {
+    /**
+     * From the Azure portal, get your Azure Cognitive Search service name and API key and
+     * populate API_KEY and SEARCH_SERVICE_NAME.
+     */
+
+    public static final String API_KEY = "<Your Search API Key>";
+    public static final String SEARCH_SERVICE_NAME = "<Your Search Service Name>";
+
+    public static void main( String[] args ) {
+        String endpoint = String.format("https://%s.%s", SEARCH_SERVICE_NAME, "search.windows.net"); // search.windows.net is the default DNS SUFFIX
+        ApiKeyCredentials apiKeyCredentials = new ApiKeyCredentials( API_KEY);
+
+        SearchServiceClient client = new SearchServiceClientBuilder()
+            .endpoint(endpoint)
+            .credential(apiKeyCredentials)
+            .buildClient();
+
+        Index newIndex  = new Index()
+            .setName("good-food")
+            .setFields(
+                Arrays.asList( new Field()
+                    .setName("Name")
+                    .setType(DataType.EDM_STRING)
+                    .setKey(Boolean.TRUE) ,
+                    new Field()
+                        .setName("Cuisine")
+                        .setType(DataType.EDM_STRING)));
+
+        client.createIndex(newIndex);
+    }
+
+}
