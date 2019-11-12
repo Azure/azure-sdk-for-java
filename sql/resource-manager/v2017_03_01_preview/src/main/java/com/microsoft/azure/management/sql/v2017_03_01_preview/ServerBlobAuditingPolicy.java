a04464988bd564be418329b6b729971f9f0a7705
Tanyi Chen <tanchen@microsoft.com>
2019-11-12 16:19:59 +0800

    Sql: update 2017 03 01 preview (#6276)
    * Sql: update 2017 03 01 preview
    
    * fix: compile error
    
    * fix: SensitivityLabelSource parse from id in https://github.com/Azure/azure-rest-api-specs/blob/de100e16eabbd5d41551cba8b1d8cd97f3701bf9/specification/sql/resource-manager/Microsoft.Sql/preview/2017-03-01-preview/SensitivityLabels.json#L284

--- sql/resource-manager/v2017_03_01_preview/src/main/java/com/microsoft/azure/management/sql/v2017_03_01_preview/ServerBlobAuditingPolicy.java
+++ sql/resource-manager/v2017_03_01_preview/src/main/java/com/microsoft/azure/management/sql/v2017_03_01_preview/ServerBlobAuditingPolicy.java
@@ -183,7 +183,7 @@ public interface ServerBlobAuditingPolicy extends HasInner<ServerBlobAuditingPol
             /**
              * Specifies isAzureMonitorTargetEnabled.
              * @param isAzureMonitorTargetEnabled Specifies whether audit events are sent to Azure Monitor.
- In order to send the events to Azure Monitor, specify 'State' as 'Enabled' and 'IsAzureMonitorTargetEnabled' as true.
+ In order to send the events to Azure Monitor, specify 'state' as 'Enabled' and 'isAzureMonitorTargetEnabled' as true.
  When using REST API to configure auditing, Diagnostic Settings with 'SQLSecurityAuditEvents' diagnostic logs category on the database should be also created.
  Note that for server level audit you should use the 'master' database as {databaseName}.
  Diagnostic Settings URI format:
@@ -336,7 +336,7 @@ public interface ServerBlobAuditingPolicy extends HasInner<ServerBlobAuditingPol
             /**
              * Specifies isAzureMonitorTargetEnabled.
              * @param isAzureMonitorTargetEnabled Specifies whether audit events are sent to Azure Monitor.
- In order to send the events to Azure Monitor, specify 'State' as 'Enabled' and 'IsAzureMonitorTargetEnabled' as true.
+ In order to send the events to Azure Monitor, specify 'state' as 'Enabled' and 'isAzureMonitorTargetEnabled' as true.
  When using REST API to configure auditing, Diagnostic Settings with 'SQLSecurityAuditEvents' diagnostic logs category on the database should be also created.
  Note that for server level audit you should use the 'master' database as {databaseName}.
  Diagnostic Settings URI format:
