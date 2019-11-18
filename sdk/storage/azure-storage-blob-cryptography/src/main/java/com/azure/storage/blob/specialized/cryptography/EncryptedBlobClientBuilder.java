bbdd519e661ef270dd89f00647d8166e0d4c36a1
Rick Ley <frley@microsoft.com>
2019-11-18 11:35:26 -0800

    Update sdk/storage/azure-storage-blob-cryptography/src/main/java/com/azure/storage/blob/specialized/cryptography/EncryptedBlobClientBuilder.java
    Co-Authored-By: Alan Zimmer <48699787+alzimmermsft@users.noreply.github.com>

--- sdk/storage/azure-storage-blob-cryptography/src/main/java/com/azure/storage/blob/specialized/cryptography/EncryptedBlobClientBuilder.java
+++ sdk/storage/azure-storage-blob-cryptography/src/main/java/com/azure/storage/blob/specialized/cryptography/EncryptedBlobClientBuilder.java
@@ -406,7 +406,7 @@ public final class EncryptedBlobClientBuilder {
     }
 
     /**
-     * Adds a pipeline policy to apply on each request sent. The policy will be added after the retry policy. If
+     * Adds a {@link HttpPipelinePolicy} to apply on each request sent. The policy will be added after the retry policy. If
      * the method is called multiple times, all policies will be added and their order preserved.
      *
      * @param pipelinePolicy a pipeline policy
