7334385ac3ea9986e73095b000fa3d4f741b45ff
Rick Ley <frley@microsoft.com>
2019-11-18 11:36:11 -0800

    Update sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/ProgressReceiver.java
    Co-Authored-By: Alan Zimmer <48699787+alzimmermsft@users.noreply.github.com>

--- sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/ProgressReceiver.java
+++ sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/ProgressReceiver.java
@@ -12,7 +12,7 @@ import reactor.core.publisher.Flux;
  * transfer operations, the {@code reportProgress} method will be called periodically with the total number of bytes
  * transferred. The user may configure this method to report progress in whatever format desired. It is recommended
  * that this type be used in conjunction with {@link ProgressReporter#addProgressReporting(Flux, ProgressReceiver)} to
- * enable reporting on sequential transfers. Note that any method accepting a {@link ParallelTransferOptions} with use
+ * enable reporting on sequential transfers. Note that any method accepting a {@link ParallelTransferOptions} will use
  * the {@code ProgressReceiver} specified there and will handle the logic to coordinate the reporting between parallel
  * operations.
  */
