7931167b9eaf8340254b362d789bf211e8146b9d
Naveen Kumar Singh <simplynaveen20@gmail.com>
2019-11-19 14:27:55 -0500

    resolving comments

--- sdk/cosmos/azure-cosmos/src/test/java/com/azure/cosmos/CosmosResponseDiagnosticsTest.java
+++ sdk/cosmos/azure-cosmos/src/test/java/com/azure/cosmos/CosmosResponseDiagnosticsTest.java
@@ -18,12 +18,9 @@ import static org.assertj.core.api.Assertions.fail;
 
 public class CosmosResponseDiagnosticsTest extends TestSuiteBase {
     private CosmosClient gatewayClient;
-    private CosmosDatabase createdDatabase;
-    private CosmosContainer cosmosContainer;
     private CosmosClient directClient;
-
-    private final static String DATABASE_NAME = "TestDiagnosticDB";
-    private final static String CONTAINER_NAME = "TestDiagnosticContainer";
+    private CosmosContainer container;
+    private CosmosAsyncContainer cosmosAsyncContainer;
 
     @Factory(dataProvider = "clientBuilders")
     public CosmosResponseDiagnosticsTest(CosmosClientBuilder clientBuilder) {
@@ -36,15 +33,16 @@ public class CosmosResponseDiagnosticsTest extends TestSuiteBase {
         ConnectionPolicy connectionPolicy = new ConnectionPolicy();
         connectionPolicy.setConnectionMode(ConnectionMode.GATEWAY);
         gatewayClient = clientBuilder().setConnectionPolicy(connectionPolicy).buildClient();
-        createdDatabase = TestSuiteBase.createSyncDatabase(gatewayClient, DATABASE_NAME);
-        CosmosContainerProperties cosmosContainerProperties = new CosmosContainerProperties(CONTAINER_NAME, "/id");
-        cosmosContainer = createdDatabase.createContainer(cosmosContainerProperties).getContainer();
+        connectionPolicy = new ConnectionPolicy();
+        connectionPolicy.setConnectionMode(ConnectionMode.DIRECT);
+        directClient = clientBuilder().setConnectionPolicy(connectionPolicy).buildClient();
+        cosmosAsyncContainer = getSharedMultiPartitionCosmosContainer(this.gatewayClient.asyncClient());
+        container = gatewayClient.getDatabase(cosmosAsyncContainer.getDatabase().getId()).getContainer(cosmosAsyncContainer.getId());
     }
 
     @AfterClass(groups = {"simple"}, timeOut = SHUTDOWN_TIMEOUT, alwaysRun = true)
     public void afterClass() throws CosmosClientException {
         assertThat(this.gatewayClient).isNotNull();
-        createdDatabase.delete();
         this.gatewayClient.close();
         if (this.directClient != null) {
             this.directClient.close();
@@ -52,30 +50,26 @@ public class CosmosResponseDiagnosticsTest extends TestSuiteBase {
     }
 
     @Test(groups = {"simple"})
-    public void testGatewayDiagnostics() {
-        CosmosItemProperties cosmosItemProperties = new CosmosItemProperties();
-        cosmosItemProperties.setId(UUID.randomUUID().toString());
-        try {
-            CosmosItemResponse createResponse = cosmosContainer.createItem(cosmosItemProperties);
-            String diagnostics = createResponse.getCosmosResponseDiagnostics().toString();
-            assertThat(diagnostics).contains("Connection Mode : " + ConnectionMode.GATEWAY);
-            assertThat(diagnostics).contains("Gateway statistics");
-            assertThat(diagnostics).contains("Operation Type : " + OperationType.Create);
-            assertThat(createResponse.getCosmosResponseDiagnostics().getRequestLatency()).isNotNull();
-        } catch (CosmosClientException e) {
-            fail(e.getMessage());
-        }
+    public void gatewayDiagnostics() throws CosmosClientException {
+        CosmosItemProperties cosmosItemProperties = getCosmosItemProperties();
+        CosmosItemResponse createResponse = container.createItem(cosmosItemProperties);
+        String diagnostics = createResponse.getCosmosResponseDiagnostics().toString();
+        assertThat(diagnostics).contains("Connection Mode : " + ConnectionMode.GATEWAY);
+        assertThat(diagnostics).contains("Gateway statistics");
+        assertThat(diagnostics).contains("Operation Type : " + OperationType.Create);
+        assertThat(createResponse.getCosmosResponseDiagnostics().getRequestLatency()).isNotNull();
+        deleteItem(createResponse);
     }
 
     @Test(groups = {"simple"})
-    public void testGatewayDiagnosticsOnException() {
-        CosmosItemProperties cosmosItemProperties = new CosmosItemProperties();
-        cosmosItemProperties.setId(UUID.randomUUID().toString());
+    public void gatewayDiagnosticsOnException() throws CosmosClientException {
+        CosmosItemProperties cosmosItemProperties = getCosmosItemProperties();
+        CosmosItemResponse createResponse = null;
         try {
-            CosmosItemResponse createResponse = this.cosmosContainer.createItem(cosmosItemProperties);
+            createResponse = this.container.createItem(cosmosItemProperties);
             CosmosItemRequestOptions cosmosItemRequestOptions = new CosmosItemRequestOptions();
             cosmosItemRequestOptions.setPartitionKey(new PartitionKey("wrongPartitionKey"));
-            CosmosItemResponse readResponse = this.cosmosContainer.getItem(createResponse.getItem().getId(), null).read(cosmosItemRequestOptions);
+            CosmosItemResponse readResponse = this.container.getItem(createResponse.getItem().getId(), null).read(cosmosItemRequestOptions);
             fail("request should fail as partition key is wrong");
         } catch (CosmosClientException exception) {
             String diagnostics = exception.getCosmosResponseDiagnostics().toString();
@@ -86,55 +80,46 @@ public class CosmosResponseDiagnosticsTest extends TestSuiteBase {
             assertThat(diagnostics).contains("Operation Type : " + OperationType.Read);
             assertThat(exception.getCosmosResponseDiagnostics().getRequestLatency()).isNotNull();
             System.out.println(diagnostics);
+        } finally {
+            deleteItem(createResponse);
         }
     }
 
     @Test(groups = {"simple"})
-    public void testSystemDiagnosticsForSystemStateInformation() throws CosmosClientException {
-        CosmosItemProperties cosmosItemProperties = new CosmosItemProperties();
-        cosmosItemProperties.setId(UUID.randomUUID().toString());
-        CosmosItemResponse createResponse = this.cosmosContainer.createItem(cosmosItemProperties);
+    public void systemDiagnosticsForSystemStateInformation() throws CosmosClientException {
+        CosmosItemProperties cosmosItemProperties = getCosmosItemProperties();
+        CosmosItemResponse createResponse = this.container.createItem(cosmosItemProperties);
         String diagnostics = createResponse.getCosmosResponseDiagnostics().toString();
         assertThat(diagnostics).contains("System State Information ------");
         assertThat(diagnostics).contains("Used Memory :");
         assertThat(diagnostics).contains("Available Memory :");
         assertThat(diagnostics).contains("CPU Process Load :");
         assertThat(diagnostics).contains("CPU System Load :");
-
         assertThat(createResponse.getCosmosResponseDiagnostics().getRequestLatency()).isNotNull();
+        deleteItem(createResponse);
     }
 
     @Test(groups = {"simple"})
-    public void testDirectDiagnostics() {
-        ConnectionPolicy connectionPolicy = new ConnectionPolicy();
-        connectionPolicy.setConnectionMode(ConnectionMode.DIRECT);
-        CosmosClient directClient = clientBuilder().setConnectionPolicy(connectionPolicy).buildClient();
-        CosmosContainer cosmosContainer = directClient.getDatabase(DATABASE_NAME).getContainer(CONTAINER_NAME);
-        CosmosItemProperties cosmosItemProperties = new CosmosItemProperties();
-        cosmosItemProperties.setId(UUID.randomUUID().toString());
-        try {
-            CosmosItemResponse createResponse = cosmosContainer.createItem(cosmosItemProperties);
-            String diagnostics = createResponse.getCosmosResponseDiagnostics().toString();
-            assertThat(diagnostics).contains("Connection Mode : " + ConnectionMode.DIRECT);
-            assertThat(diagnostics).contains("StoreResponseStatistics");
-            assertThat(diagnostics).doesNotContain("Gateway request URI :");
-            assertThat(diagnostics).contains("AddressResolutionStatistics");
-            assertThat(createResponse.getCosmosResponseDiagnostics().getRequestLatency()).isNotNull();
-        } catch (CosmosClientException e) {
-            fail(e.getMessage());
-        }
+    public void directDiagnostics() throws CosmosClientException {
+        CosmosContainer cosmosContainer = directClient.getDatabase(cosmosAsyncContainer.getDatabase().getId()).getContainer(cosmosAsyncContainer.getId());
+        CosmosItemProperties cosmosItemProperties = getCosmosItemProperties();
+        CosmosItemResponse createResponse = cosmosContainer.createItem(cosmosItemProperties);
+        String diagnostics = createResponse.getCosmosResponseDiagnostics().toString();
+        assertThat(diagnostics).contains("Connection Mode : " + ConnectionMode.DIRECT);
+        assertThat(diagnostics).contains("StoreResponseStatistics");
+        assertThat(diagnostics).doesNotContain("Gateway request URI :");
+        assertThat(diagnostics).contains("AddressResolutionStatistics");
+        assertThat(createResponse.getCosmosResponseDiagnostics().getRequestLatency()).isNotNull();
+        deleteItem(createResponse);
     }
 
     @Test(groups = {"simple"})
-    public void testDirectDiagnosticsOnException() {
-        ConnectionPolicy connectionPolicy = new ConnectionPolicy();
-        connectionPolicy.setConnectionMode(ConnectionMode.DIRECT);
-        CosmosClient directClient = clientBuilder().setConnectionPolicy(connectionPolicy).buildClient();
-        CosmosContainer cosmosContainer = directClient.getDatabase(DATABASE_NAME).getContainer(CONTAINER_NAME);
-        CosmosItemProperties cosmosItemProperties = new CosmosItemProperties();
-        cosmosItemProperties.setId(UUID.randomUUID().toString());
+    public void directDiagnosticsOnException() throws CosmosClientException {
+        CosmosContainer cosmosContainer = directClient.getDatabase(cosmosAsyncContainer.getDatabase().getId()).getContainer(cosmosAsyncContainer.getId());
+        CosmosItemProperties cosmosItemProperties = getCosmosItemProperties();
+        CosmosItemResponse createResponse = null;
         try {
-            CosmosItemResponse createResponse = this.cosmosContainer.createItem(cosmosItemProperties);
+            createResponse = this.container.createItem(cosmosItemProperties);
             CosmosItemRequestOptions cosmosItemRequestOptions = new CosmosItemRequestOptions();
             cosmosItemRequestOptions.setPartitionKey(new PartitionKey("wrongPartitionKey"));
             CosmosItemResponse readResponse = cosmosContainer.getItem(createResponse.getItem().getId(), null).read(cosmosItemRequestOptions);
@@ -144,6 +129,21 @@ public class CosmosResponseDiagnosticsTest extends TestSuiteBase {
             assertThat(exception.getStatusCode()).isEqualTo(HttpConstants.StatusCodes.NOTFOUND);
             assertThat(diagnostics).contains("Connection Mode : " + ConnectionMode.DIRECT);
             assertThat(exception.getCosmosResponseDiagnostics().getRequestLatency()).isNotNull();
+        } finally {
+            deleteItem(createResponse);
         }
+    }
+
+    private CosmosItemProperties getCosmosItemProperties() {
+        CosmosItemProperties cosmosItemProperties = new CosmosItemProperties();
+        cosmosItemProperties.setId(UUID.randomUUID().toString());
+        cosmosItemProperties.set("mypk", "test");
+        return cosmosItemProperties;
+    }
+
+    private void deleteItem(CosmosItemResponse cosmosItemResponse) throws CosmosClientException {
+        CosmosItemRequestOptions cosmosItemRequestOptions = new CosmosItemRequestOptions();
+        cosmosItemRequestOptions.setPartitionKey(new PartitionKey("test"));
+        cosmosItemResponse.getItem().delete(cosmosItemRequestOptions);
     }
 }
