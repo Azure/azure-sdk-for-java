ae161b643902102e34a90606338850ebfdacbbe8
Gauri Prasad <gapra@microsoft.com>
2019-11-20 09:17:54 -0800

    updated tests to be parameterized

--- sdk/storage/azure-storage-file-datalake/src/test/java/com/azure/storage/file/datalake/UrlTests.groovy
+++ sdk/storage/azure-storage-file-datalake/src/test/java/com/azure/storage/file/datalake/UrlTests.groovy
@@ -1,98 +1,62 @@
 package com.azure.storage.file.datalake
 
+import spock.lang.Unroll
+
 class UrlTests extends APISpec {
 
-    def "test custom url"() {
+    @Unroll
+    def "test urls that should not change for datalake"() {
         when:
-        def customUrl = "https://www.customstorageurl.com"
         DataLakeServiceClient client = new DataLakeServiceClientBuilder()
-            .endpoint(customUrl)
+            .endpoint(endpoint)
             .credential(primaryCredential)
             .buildClient()
         then:
         client.getAccountUrl() == client.blobServiceClient.getAccountUrl()
+
+        where:
+        endpoint                                 | _
+        "https://www.customstorageurl.com"       | _
+        "https://account.core.windows.net"       | _
+        "https://0.0.0.0/account"                | _
+        "https://account.file.core.windows.net"  | _
     }
 
-    def "test short host url"() {
-        when:
-        def shortHostUrl = "https://account.core.windows.net"
-        DataLakeServiceClient client = new DataLakeServiceClientBuilder()
-            .endpoint(shortHostUrl)
-            .credential(primaryCredential)
-            .buildClient()
-        then:
-        client.getAccountUrl() == client.blobServiceClient.getAccountUrl()
-    }
-
-    def "test ip style url"() {
-        when:
-        def ipStyleUrl = "https://0.0.0.0/account"
-        DataLakeServiceClient client = new DataLakeServiceClientBuilder()
-            .endpoint(ipStyleUrl)
-            .credential(primaryCredential)
-            .buildClient()
-        then:
-        client.getAccountUrl() == client.blobServiceClient.getAccountUrl()
-    }
-
-    def "test invalid service url"() {
-        when:
-        def invalidServiceUrl = "https://account.file.core.windows.net"
-        DataLakeServiceClient client = new DataLakeServiceClientBuilder()
-            .endpoint(invalidServiceUrl)
-            .credential(primaryCredential)
-            .buildClient()
-        then:
-        client.getAccountUrl() == client.blobServiceClient.getAccountUrl()
-    }
-
-    def "test blob service url"() {
+    @Unroll
+    def "test correct service url set"() {
         when:
         def blobUrl = "https://account.blob.core.windows.net"
         def dfsUrl = "https://account.dfs.core.windows.net"
+
+        def testUrl = blobUrl
+        if (useDfsurl) {
+            testUrl = dfsUrl
+        }
+
         DataLakeServiceClient serviceClient = new DataLakeServiceClientBuilder()
-            .endpoint(blobUrl)
+            .endpoint(testUrl)
             .credential(primaryCredential)
             .buildClient()
         DataLakeFileSystemClient fileSystemClient = new DataLakeFileSystemClientBuilder()
-            .endpoint(blobUrl + "/container")
+            .endpoint(testUrl + "/container")
             .credential(primaryCredential)
             .buildClient()
         DataLakeFileClient pathClient = new DataLakePathClientBuilder()
-            .endpoint(blobUrl + "/container/blob")
+            .endpoint(testUrl + "/container/blob")
             .credential(primaryCredential)
             .buildFileClient()
         then:
+        // In either case the dfs url should be set to the dfs client and blob url set to the blob client
         serviceClient.getAccountUrl() == dfsUrl
         serviceClient.blobServiceClient.getAccountUrl() == blobUrl
         fileSystemClient.getFileSystemUrl() == dfsUrl + "/container"
         fileSystemClient.blobContainerClient.getBlobContainerUrl() == blobUrl + "/container"
         pathClient.getPathUrl() == dfsUrl + "/container/blob"
         pathClient.blockBlobClient.getBlobUrl() == blobUrl + "/container/blob"
-    }
 
-    def "test dfs service url"() {
-        when:
-        def blobUrl = "https://account.blob.core.windows.net"
-        def dfsUrl = "https://account.dfs.core.windows.net"
-        DataLakeServiceClient serviceClient = new DataLakeServiceClientBuilder()
-            .endpoint(blobUrl)
-            .credential(primaryCredential)
-            .buildClient()
-        DataLakeFileSystemClient fileSystemClient = new DataLakeFileSystemClientBuilder()
-            .endpoint(blobUrl + "/container")
-            .credential(primaryCredential)
-            .buildClient()
-        DataLakeFileClient pathClient = new DataLakePathClientBuilder()
-            .endpoint(blobUrl + "/container/blob")
-            .credential(primaryCredential)
-            .buildFileClient()
-        then:
-        serviceClient.getAccountUrl() == dfsUrl
-        serviceClient.blobServiceClient.getAccountUrl() == blobUrl
-        fileSystemClient.getFileSystemUrl() == dfsUrl + "/container"
-        fileSystemClient.blobContainerClient.getBlobContainerUrl() == blobUrl + "/container"
-        pathClient.getPathUrl() == dfsUrl + "/container/blob"
-        pathClient.blockBlobClient.getBlobUrl() == blobUrl + "/container/blob"
+        where:
+        useDfsurl | _
+        true      | _
+        false     | _
     }
 }
