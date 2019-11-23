792806e9f3541a60e4cbae94048d74b58464b7f3
Hemant Tanwar <hetanwar@users.noreply.github.com>
2019-11-22 23:25:33 -0800

    Rollback RequestIDPolicy API to support custom header.

--- sdk/core/azure-core/src/test/java/com/azure/core/http/policy/RequestIdPolicyTests.java
+++ sdk/core/azure-core/src/test/java/com/azure/core/http/policy/RequestIdPolicyTests.java
@@ -115,37 +115,4 @@ public class RequestIdPolicyTests {
 
         pipeline.send(new HttpRequest(HttpMethod.GET, new URL("http://localhost/"))).block();
     }
-
-    @Test
-    public void clientProvidedRequestIdForRetry() throws Exception {
-
-        String customRequestIdHeaderName = "x-ms-client-custom-request-id";
-        String clientProvidedRequestId = UUID.randomUUID().toString();
-
-        Supplier<HttpHeaders> requestIdSupplier = () -> new HttpHeaders().put(customRequestIdHeaderName, clientProvidedRequestId);
-        final HttpPipeline pipeline = new HttpPipelineBuilder()
-            .httpClient(new NoOpHttpClient() {
-                String firstRequestId = null;
-
-                @Override
-                public Mono<HttpResponse> send(HttpRequest request) {
-                    if (firstRequestId != null) {
-                        String newRequestId = request.getHeaders().getValue(customRequestIdHeaderName);
-                        Assertions.assertNotNull(newRequestId);
-                        Assertions.assertEquals(newRequestId, firstRequestId);
-                        Assertions.assertEquals(newRequestId, clientProvidedRequestId);
-                    }
-                    firstRequestId = request.getHeaders().getValue(customRequestIdHeaderName);
-                    if (firstRequestId == null) {
-                        Assertions.fail();
-                    }
-                    return Mono.just(mockResponse);
-                }
-            })
-            .policies(new RequestIdPolicy(requestIdSupplier), new RetryPolicy(new FixedDelay(1, Duration.of(0, ChronoUnit.SECONDS))))
-            .build();
-
-        pipeline.send(new HttpRequest(HttpMethod.GET, new URL("http://localhost/"))).block();
-    }
-
 }
