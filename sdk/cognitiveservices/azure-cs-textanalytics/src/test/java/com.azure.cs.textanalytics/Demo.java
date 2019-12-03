0d4a8f37761786a3c3fc58d3b67e4ad1f230af50
shafang <Shawn.Fang@microsoft.com>
2019-12-02 22:31:48 -0800

    update test Demo

--- sdk/cognitiveservices/azure-cs-textanalytics/src/test/java/com.azure.cs.textanalytics/Demo.java
+++ sdk/cognitiveservices/azure-cs-textanalytics/src/test/java/com.azure.cs.textanalytics/Demo.java
@@ -2,13 +2,53 @@
 // Licensed under the MIT License.
 package com.azure.cs.textanalytics;
 
+import com.azure.core.http.rest.SimpleResponse;
+import com.azure.core.util.Configuration;
+import com.azure.core.util.Context;
+import com.azure.core.util.logging.ClientLogger;
+import com.azure.cs.textanalytics.implementation.TextAnalyticsClientImpl;
+import com.azure.cs.textanalytics.implementation.TextAnalyticsClientImplBuilder;
+import com.azure.cs.textanalytics.implementation.models.LanguageBatchInput;
+import com.azure.cs.textanalytics.implementation.models.LanguageResult;
+import com.azure.cs.textanalytics.models.DetectLanguageInput;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Demo {
+    private final ClientLogger logger = new ClientLogger(Demo.class);
 
+    private static final String AZURE_TEXT_ANALYTICS_SUBSCRIPTION_KEY = "AZURE_TEXT_ANALYTICS_SUBSCRIPTION_KEY";
+    private String subscriptionKey;
 
     @Test
     public void test() {
-        System.out.println("Test Demo");
+
+        String endpoint = "https://shawnjavatextanalytics.cognitiveservices.azure.com/";
+
+        subscriptionKey = Configuration.getGlobalConfiguration().get(AZURE_TEXT_ANALYTICS_SUBSCRIPTION_KEY);
+        System.out.println("Subscription Key = " + subscriptionKey);
+        TextAnalyticsClientImpl ta = new TextAnalyticsClientImplBuilder()
+            .endpoint(endpoint)
+//            .pipeline()
+            .build();
+
+        System.out.println("Endpoint = " + ta.getEndpoint());
+
+        final List<DetectLanguageInput> inputs = Arrays.asList(
+            new DetectLanguageInput("1", "This is written in English","US"),
+            new DetectLanguageInput("2", "Este es un document escrito en Español.", "es")
+        );
+        final LanguageBatchInput languageBatchInput = new LanguageBatchInput().setDocuments(inputs);
+        Mono<SimpleResponse<LanguageResult>> result = ta.languagesWithRestResponseAsync(languageBatchInput, Context.NONE)
+             .doOnSubscribe(ignoredValue -> logger.info("A batch of language input - {}", languageBatchInput))
+             .doOnSuccess(response -> logger.info("A batch of detected language output - {}", languageBatchInput))
+             .doOnError(error -> logger.warning("Failed to detected languages - {}", languageBatchInput))
+             .map(response -> new SimpleResponse<>(response, response.getValue()));
+
+
     }
 }
