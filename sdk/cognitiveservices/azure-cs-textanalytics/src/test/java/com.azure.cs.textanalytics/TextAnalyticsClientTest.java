64857cb89d744d728aab18f47a8fef938fbd466e
shafang <Shawn.Fang@microsoft.com>
2019-12-04 15:33:31 -0800

    use AAD and add partial test base

---
+++ sdk/cognitiveservices/azure-cs-textanalytics/src/test/java/com.azure.cs.textanalytics/TextAnalyticsClientTest.java
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.cs.textanalytics;
+
+public class TextAnalyticsClientTest {
+
+
+}
