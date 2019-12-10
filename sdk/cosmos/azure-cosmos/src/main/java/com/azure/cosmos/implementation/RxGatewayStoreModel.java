1c8f7fabb942affa101b61e2fdb4dbebcd3af9ba
Naveen Singh <simplynaveen20@gmail.com>
2019-12-10 16:02:52 -0500

    Adding memory/cpu statistics and gateway information (#6405)
    * Adding system information and gateway diagnostics
    
    * Adding more data for gateway
    
    * reverting redundant change
    
    * renaming variable name
    
    * resolving comments
    
    * creating OperatingSystemMXBean during class initialization
    
    * merging and resolving comment
    
    * Adding system information and gateway diagnostics
    
    * Adding more data for gateway
    
    * reverting redundant change
    
    * renaming variable name
    
    * resolving comments
    
    * creating OperatingSystemMXBean during class initialization
    
    * merging and resolving comment
    
    * resolving comments
    
    * Update CosmosResponseDiagnosticsTest.java
    
    Removing redundant system out

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/RxGatewayStoreModel.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/RxGatewayStoreModel.java
@@ -2,7 +2,6 @@
 // Licensed under the MIT License.
 package com.azure.cosmos.implementation;
 
-
 import com.azure.cosmos.implementation.directconnectivity.HttpUtils;
 import com.azure.cosmos.implementation.directconnectivity.StoreResponse;
 import com.azure.cosmos.implementation.http.HttpClient;
@@ -13,6 +12,7 @@ import com.azure.cosmos.BridgeInternal;
 import com.azure.cosmos.ConsistencyLevel;
 import com.azure.cosmos.CosmosClientException;
 import com.azure.cosmos.CosmosError;
+import com.azure.cosmos.implementation.directconnectivity.DirectBridgeInternal;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.handler.codec.http.HttpMethod;
@@ -138,6 +138,12 @@ class RxGatewayStoreModel implements RxStoreModel {
     public Flux<RxDocumentServiceResponse> performRequest(RxDocumentServiceRequest request, HttpMethod method) {
 
         try {
+
+            if (request.getResourceType().equals(ResourceType.Document) &&
+                request.requestContext.cosmosResponseDiagnostics == null) {
+                request.requestContext.cosmosResponseDiagnostics = BridgeInternal.createCosmosResponseDiagnostics();
+            }
+
             URI uri = getUri(request);
 
             HttpHeaders httpHeaders = this.getHttpRequestHeaders(request.getHeaders());
@@ -271,6 +277,11 @@ class RxGatewayStoreModel implements RxStoreModel {
                                StoreResponse rsp = new StoreResponse(httpResponseStatus,
                                    HttpUtils.unescape(httpResponseHeaders.toMap().entrySet()),
                                    content);
+                               if (request.requestContext.cosmosResponseDiagnostics != null &&
+                                   request.getResourceType().equals(ResourceType.Document)) {
+                                   BridgeInternal.recordGatewayResponse(request.requestContext.cosmosResponseDiagnostics, request, rsp, null);
+                                   DirectBridgeInternal.setCosmosResponseDiagnostics(rsp, request.requestContext.cosmosResponseDiagnostics);
+                               }
                                return Flux.just(rsp);
                            } catch (Exception e) {
                                return Flux.error(e);
@@ -294,6 +305,12 @@ class RxGatewayStoreModel implements RxStoreModel {
                            CosmosClientException dce = BridgeInternal.createCosmosClientException(0, exception);
                            BridgeInternal.setRequestHeaders(dce, request.getHeaders());
                            return Mono.error(dce);
+                       }
+
+                       if (request.requestContext.cosmosResponseDiagnostics != null &&
+                           request.getResourceType().equals(ResourceType.Document)) {
+                           BridgeInternal.recordGatewayResponse(request.requestContext.cosmosResponseDiagnostics, request, null, (CosmosClientException)exception);
+                           BridgeInternal.setCosmosResponseDiagnostics((CosmosClientException)exception, request.requestContext.cosmosResponseDiagnostics);
                        }
 
                        return Mono.error(exception);
