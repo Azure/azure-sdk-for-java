0ff10b1c8c1f08b52efde70a63e03fecf2fd8472
Sima Zhu <sizhu@microsoft.com>
2019-12-09 21:53:15 -0800

    More tests fixed

--- sdk/storage/azure-storage-blob-cryptography/src/test/java/com/azure/storage/blob/specialized/cryptography/BlobCryptographyUserAgentPropertiesTest.groovy
+++ sdk/storage/azure-storage-blob-cryptography/src/test/java/com/azure/storage/blob/specialized/cryptography/BlobCryptographyUserAgentPropertiesTest.groovy
@@ -10,7 +10,7 @@ class BlobCryptographyUserAgentPropertiesTest extends Specification {
 
     def "User agent properties not null"() {
         given:
-        Map<String, String> properties = CoreUtils.getProperties("azure-storage-blob-cyptography.properties")
+        Map<String, String> properties = CoreUtils.getProperties("azure-storage-blob-cryptography.properties")
         expect:
         properties.get("name") == "azure-storage-blob-cryptography"
         properties.get("version").matches("(\\d)+.(\\d)+.(\\d)+([-a-zA-Z0-9.])*")
