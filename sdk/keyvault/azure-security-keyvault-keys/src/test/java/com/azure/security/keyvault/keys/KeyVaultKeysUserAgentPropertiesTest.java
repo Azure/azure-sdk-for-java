f57f6e7f97dbf87da93494ec4a9bd1d05b3887d4
Sima Zhu <sizhu@microsoft.com>
2019-12-09 22:12:48 -0800

    Keyvault test fixed

--- sdk/keyvault/azure-security-keyvault-keys/src/test/java/com/azure/security/keyvault/keys/KeyVaultKeysUserAgentPropertiesTest.java
+++ sdk/keyvault/azure-security-keyvault-keys/src/test/java/com/azure/security/keyvault/keys/KeyVaultKeysUserAgentPropertiesTest.java
@@ -14,7 +14,7 @@ public class KeyVaultKeysUserAgentPropertiesTest {
     @Test
     public void testAzureConfiguration() {
         Map<String, String> properties = CoreUtils.getProperties("azure-key-vault-keys.properties");
-        assertTrue(properties.get("name").matches("azure-key-vault-keys"));
+        assertTrue(properties.get("name").matches("azure-security-keyvault-keys"));
         assertTrue(properties.get("version").matches("(\\d)+.(\\d)+.(\\d)+([-a-zA-Z0-9.])*"));
     }
 }
