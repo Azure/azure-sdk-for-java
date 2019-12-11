ffa969aa6d8e4c7564cd8e069c7ef857e7919b81
Sima Zhu <sizhu@microsoft.com>
2019-12-11 14:05:35 -0800

    More tests on fluxUtil and Base64Util

--- sdk/core/azure-core/src/test/java/com/azure/core/util/FluxUtilTest.java
+++ sdk/core/azure-core/src/test/java/com/azure/core/util/FluxUtilTest.java
@@ -3,6 +3,35 @@
 
 package com.azure.core.util;
 
+import com.azure.core.http.HttpHeaders;
+import com.azure.core.http.HttpMethod;
+import com.azure.core.http.HttpRequest;
+import com.azure.core.http.rest.Response;
+import com.azure.core.http.rest.SimpleResponse;
+import com.azure.core.util.logging.ClientLogger;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousFileChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import com.azure.core.http.HttpHeaders;
+import com.azure.core.http.HttpMethod;
+import com.azure.core.http.HttpRequest;
+import com.azure.core.http.rest.Response;
+import com.azure.core.http.rest.SimpleResponse;
+import com.azure.core.util.logging.ClientLogger;
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+import java.nio.ByteBuffer;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -10,8 +39,10 @@ import reactor.core.publisher.Mono;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import reactor.test.StepVerifier;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FluxUtilTest {
@@ -60,6 +91,77 @@ public class FluxUtilTest {
         assertEquals("value2", reactorContext.get("key2"));
     }
 
+    @Test
+    public void testIsFluxByteBufferInvalidType() {
+        assertFalse(FluxUtil.isFluxByteBuffer(Mono.class));
+    }
+
+    @Test
+    public void testIsFluxByteBufferValidType() throws Exception {
+        Method method = FluxUtilTest.class.getMethod("mockReturnType");
+        Type returnType = method.getGenericReturnType();
+        assertTrue(FluxUtil.isFluxByteBuffer(returnType));
+    }
+
+    @Test
+    public void testToMono() {
+        String testValue = "some value";
+        Response<String> response = new SimpleResponse<String>(new HttpRequest(HttpMethod.GET, "http://www.test.com"),
+            202, new HttpHeaders(), testValue);
+        StepVerifier.create(FluxUtil.toMono(response))
+            .assertNext(val -> assertEquals(val, testValue))
+            .verifyComplete();
+    }
+
+    @Test
+    public void testMonoError() {
+        String errMsg = "It is an error message";
+        RuntimeException ex = new RuntimeException(errMsg);
+        ClientLogger logger = new ClientLogger(FluxUtilTest.class);
+        StepVerifier.create(FluxUtil.monoError(logger, ex))
+            .verifyErrorMessage(errMsg);
+    }
+
+    @Test
+    public void testFluxError() {
+        String errMsg = "It is an error message";
+        RuntimeException ex = new RuntimeException(errMsg);
+        ClientLogger logger = new ClientLogger(FluxUtilTest.class);
+        StepVerifier.create(FluxUtil.fluxError(logger, ex))
+            .verifyErrorMessage(errMsg);
+    }
+
+    @Test
+    public void testPageFluxError() {
+        String errMsg = "It is an error message";
+        RuntimeException ex = new RuntimeException(errMsg);
+        ClientLogger logger = new ClientLogger(FluxUtilTest.class);
+        StepVerifier.create(FluxUtil.pagedFluxError(logger, ex))
+            .verifyErrorMessage(errMsg);
+    }
+    
+    @Test
+    public void testWriteFile() throws Exception {
+        String toReplace = "test";
+        String original = "hello there";
+        String target = "testo there";
+
+        Flux<ByteBuffer> body = Flux.just(ByteBuffer.wrap(toReplace.getBytes(StandardCharsets.UTF_8)));
+        File file = createFileIfNotExist("target/test1");
+        FileOutputStream stream = new FileOutputStream(file);
+        stream.write(original.getBytes(StandardCharsets.UTF_8));
+        stream.close();
+        try (AsynchronousFileChannel channel = AsynchronousFileChannel.open(file.toPath(), StandardOpenOption.WRITE)) {
+            FluxUtil.writeFile(body, channel).block();
+            byte[] outputStream = Files.readAllBytes(file.toPath());
+            assertTrue(Arrays.equals(outputStream, target.getBytes(StandardCharsets.UTF_8)));
+        }
+    }
+
+    public Flux<ByteBuffer> mockReturnType() {
+        return Flux.just(ByteBuffer.wrap(new byte[0]));
+    }
+
     private Mono<String> getSingle() {
         return FluxUtil.withContext(this::serviceCallSingle);
     }
@@ -84,5 +186,14 @@ public class FluxUtilTest {
             + context.getData("LastName").orElse("");
 
         return Flux.just(msg.split(" "));
+    }
+
+    private File createFileIfNotExist(String fileName) throws IOException {
+        File file = new File(fileName);
+        if (file.getParentFile() != null) {
+            file.getParentFile().mkdirs();
+        }
+        file.createNewFile();
+        return file;
     }
 }
