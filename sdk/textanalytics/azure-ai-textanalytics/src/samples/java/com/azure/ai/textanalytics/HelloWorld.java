5f5891dc6f6dd1d127a9a1c62bfc51d8ca185af5
shafang <Shawn.Fang@microsoft.com>
2019-12-13 12:55:03 -0800

    sample for simple cases

--- sdk/textanalytics/azure-ai-textanalytics/src/samples/java/com/azure/ai/textanalytics/HelloWorld.java
+++ sdk/textanalytics/azure-ai-textanalytics/src/samples/java/com/azure/ai/textanalytics/HelloWorld.java
@@ -5,6 +5,7 @@ package com.azure.ai.textanalytics;
 
 import com.azure.ai.textanalytics.models.DetectLanguageResult;
 import com.azure.ai.textanalytics.models.DetectedLanguage;
+import com.azure.core.util.Configuration;
 
 import java.util.List;
 
@@ -13,14 +14,16 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Instantiate a client that will be used to call the service.
         TextAnalyticsClient client = new TextAnalyticsClientBuilder()
-            .subscriptionKey("subscription-key")
-            .endpoint("https://servicename.cognitiveservices.azure.com/")
+//            .subscriptionKey("subscription-key")
+//            .endpoint("https://servicename.cognitiveservices.azure.com/")
+            .subscriptionKey(Configuration.getGlobalConfiguration().get("AZURE_TEXT_ANALYTICS_SUBSCRIPTION_KEY"))
+            .endpoint(Configuration.getGlobalConfiguration().get("AZURE_TEXT_ANALYTICS_ENDPOINT"))
             .buildClient();
 
         // The text that need be analysed.
         String text = "hello world";
 
-        final DetectLanguageResult detectLanguageResult = client.detectLanguage(text, "en");
+        final DetectLanguageResult detectLanguageResult = client.detectLanguage(text, "US");
         final DetectedLanguage detectedDocumentLanguage = detectLanguageResult.getPrimaryLanguage();
         System.out.printf("Detected Primary Language: %s, ISO 6391 Name: %s, Score: %s%n",
             detectedDocumentLanguage.getName(),
