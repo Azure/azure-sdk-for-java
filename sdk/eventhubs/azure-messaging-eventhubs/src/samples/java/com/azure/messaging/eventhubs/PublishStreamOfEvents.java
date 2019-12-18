8195ea6f6f769c9c56763062b796653779d96938
Connie <conniey@microsoft.com>
2019-12-18 10:50:16 -0800

    Updating sample to emit batch at an interval or when batch is full.

--- sdk/eventhubs/azure-messaging-eventhubs/src/samples/java/com/azure/messaging/eventhubs/PublishStreamOfEvents.java
+++ sdk/eventhubs/azure-messaging-eventhubs/src/samples/java/com/azure/messaging/eventhubs/PublishStreamOfEvents.java
@@ -2,22 +2,27 @@
 // Licensed under the MIT License.
 package com.azure.messaging.eventhubs;
 
-import reactor.core.Exceptions;
+import com.azure.messaging.eventhubs.models.CreateBatchOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.DirectProcessor;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
+import java.util.Objects;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
-
-import static java.nio.charset.StandardCharsets.UTF_8;
+import java.util.stream.Collectors;
 
 /**
  * Demonstrates how to continuously publish events using {@link EventHubProducerAsyncClient}.
  */
 public class PublishStreamOfEvents {
-    private static final Duration OPERATION_TIMEOUT = Duration.ofSeconds(30);
+    private static final String EVENT_NUMBER = "EVENT_NUMBER";
 
     /**
      * Main method to invoke this demo about how continuously publish a stream of events to Event Hubs.
@@ -30,104 +35,100 @@ public class PublishStreamOfEvents {
         // 2. Creating an Event Hub instance.
         // 3. Creating a "Shared access policy" for your Event Hub instance.
         // 4. Copying the connection string from the policy's properties.
-        String connectionString = "Endpoint={endpoint};SharedAccessKeyName={sharedAccessKeyName};" +
-            "SharedAccessKey={sharedAccessKey};EntityPath={eventHubName}";
+        String connectionString = "Endpoint={endpoint};SharedAccessKeyName={sharedAccessKeyName};"
+            + "SharedAccessKey={sharedAccessKey};EntityPath={eventHubName}";
 
-        MyPublisher publisher = new MyPublisher(connectionString);
+        // Limit the size of the batches to 1024 bytes instead of using the default max size.
+        final CreateBatchOptions batchOptions = new CreateBatchOptions().setMaximumSizeInBytes(1024);
+        final CustomPublisher publisher = new CustomPublisher(connectionString, Duration.ofSeconds(1), batchOptions);
 
-        // We will publish three events based on simple sentences.
-        Flux<EventData> events = Flux.just(
-            new EventData("This is the first event.".getBytes(UTF_8)),
-            new EventData("This is the second event.".getBytes(UTF_8)),
-            new EventData("This is the third event.".getBytes(UTF_8)));
+        // This represents a stream of events that we want to publish.
+        final DirectProcessor<EventData> events = DirectProcessor.create();
 
-        publisher.publish(events);
+        System.out.println("Publishing events...");
+        publisher.publish(events).subscribe(unused -> System.out.println("Completed."),
+            error -> System.err.println("Error sending events: " + error),
+            () -> System.out.println("Completed sending events."));
 
-        // We try to add as many events as a batch can fit based on the event size and send to Event Hub when
-        // the batch can hold no more events. Create a new batch for next set of events and repeat until all events
-        // are sent.
-        events.flatMap(event -> {
-            final EventDataBatch batch = currentBatch.get();
-            if (batch.tryAdd(event)) {
-                return Mono.empty();
-            }
-
-            // The batch is full, so we create a new batch and send the batch. Mono.when completes when both operations
-            // have completed.
-            return Mono.when(
-                producer.send(batch),
-                producer.createBatch(options).map(newBatch -> {
-                    currentBatch.set(newBatch);
-
-                    // Add that event that we couldn't before.
-                    if (!newBatch.tryAdd(event)) {
-                        throw Exceptions.propagate(new IllegalArgumentException(String.format(
-                            "Event is too large for an empty batch. Max size: %s. Event: %s",
-                            newBatch.getMaxSizeInBytes(), event.getBodyAsString())));
-                    }
-
-                    return newBatch;
-                }));
-        }).then()
-            .doFinally(signal -> {
-                final EventDataBatch batch = currentBatch.getAndSet(null);
-                if (batch != null) {
-                    producer.send(batch).block(OPERATION_TIMEOUT);
-                }
-            })
-            .subscribe(unused -> System.out.println("Complete"),
-                error -> System.out.println("Error sending events: " + error),
-                () -> System.out.println("Completed sending events."));
+        emitEvents(events.sink());
 
         // The .subscribe() creation and assignment is not a blocking call. For the purpose of this example, we sleep
-        // the thread so the program does not end before the send operation is complete. Using .block() instead of
-        // .subscribe() will turn this into a synchronous call.
+        // the thread so the program does not end before the send operation is complete.
         try {
-            TimeUnit.SECONDS.sleep(5);
+            TimeUnit.SECONDS.sleep(10);
         } catch (InterruptedException ignored) {
         } finally {
-            // Disposing of our producer.
-            producer.close();
+            System.out.println("Closing custom publisher.");
+            publisher.close();
         }
     }
 
-    static class MyPublisher implements AutoCloseable {
-        private final EventHubProducerAsyncClient producer;
-        private final AtomicBoolean isDisposed = new AtomicBoolean();
-        private final Object lock = new Object();
-        private final AtomicReference<EventDataBatch> currentBatch = new AtomicReference<>();
+    /**
+     * Helper function that emits 50 events. The interval between each event is randomly selected between 0 - 250ms and
+     * is a random substring of the lorem ipsum text.
+     *
+     * @param sink Sink for generated events.
+     */
+    private static void emitEvents(FluxSink<EventData> sink) {
+        final String contents = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do "
+            + "eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis "
+            + "nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure "
+            + "dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur"
+            + " sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est "
+            + "laborum.";
+        final Random random = new Random();
 
-        MyPublisher(String connectionString) {
+        for (int i = 0; i < 50; i++) {
+            int milliseconds = random.nextInt(250);
+
+            try {
+                TimeUnit.MILLISECONDS.sleep(milliseconds);
+            } catch (InterruptedException ignored) {
+            }
+
+            final int size = random.nextInt(contents.length());
+            final int endIndex = size < 1 ? contents.length() : size;
+            final EventData event = new EventData(contents.substring(0, endIndex));
+            event.getProperties().put(EVENT_NUMBER, String.valueOf(i));
+
+            sink.next(event);
+        }
+
+        sink.complete();
+    }
+
+    /**
+     * Aggregates events into batches based on the given {@link CreateBatchOptions} and sends the batches to the Event
+     * Hubs service when either:
+     *
+     * <ul>
+     *     <li>The batch has reached its max size and no more events can be added to it.</li>
+     *     <li>The timeout window has elapsed.</li>
+     * </ul>
+     */
+    private static class CustomPublisher implements AutoCloseable {
+        private final Logger logger = LoggerFactory.getLogger(CustomPublisher.class);
+        private final AtomicBoolean isDisposed = new AtomicBoolean();
+        private final AtomicReference<EventDataBatch> currentBatch = new AtomicReference<>();
+        private final EventHubProducerAsyncClient producer;
+        private final CreateBatchOptions batchOptions;
+        private final Duration windowDuration;
+
+        /**
+         * Creates a new instance of {@link CustomPublisher}.
+         *
+         * @param connectionString Connection string for the specific Event Hub.
+         * @param windowDuration Intervals to check for an available {@link EventDataBatch} to send.
+         * @param batchOptions Options to use when creating the {@link EventDataBatch}. If {@code null}, the default
+         * batch options are used.
+         */
+        CustomPublisher(String connectionString, Duration windowDuration, CreateBatchOptions batchOptions) {
             producer = new EventHubClientBuilder()
                 .connectionString(connectionString)
                 .buildAsyncProducerClient();
-        }
 
-        /**
-         * Adds an event to the current batch. When the batch is full, then the batch is sent to Event Hubs.
-         *
-         * @param event Event to publish to the service.
-         *
-         * @return Mono that completes or errors when the event is added to the service.
-         */
-        Mono<Void> publish(EventData event) {
-            return getOrCreateBatch().flatMap(batch -> {
-                if (batch.tryAdd(event)) {
-                    return Mono.empty();
-                }
-
-                return Mono.when(
-                    producer.send(batch),
-                    createBatch().flatMap(newBatch -> {
-                        if (!newBatch.tryAdd(event)) {
-                            throw Exceptions.propagate(new IllegalArgumentException(String.format(
-                                "Event is too large for an empty batch. Max size: %s. Event: %s",
-                                newBatch.getMaxSizeInBytes(), event.getBodyAsString())));
-                        }
-
-                        return Mono.<Void>empty();
-                    }));
-            });
+            this.batchOptions = batchOptions != null ? batchOptions : new CreateBatchOptions();
+            this.windowDuration = Objects.requireNonNull(windowDuration, "'windowDuration' cannot be null.");
         }
 
         /**
@@ -143,10 +144,12 @@ public class PublishStreamOfEvents {
          * @return Mono that completes when all the events have been published.
          */
         Mono<Void> publish(Flux<EventData> events) {
-            final Flux<EventDataBatch> fullBatchFlux = Flux.create(sink -> {
+            final Flux<EventDataBatch> fullBatchFlux = Flux.<EventDataBatch>create(sink -> {
                 events.subscribe(event -> {
                     // For each event, tries to add it to the current batch. If the current batch is full, then we emit
-                    // that. Otherwise an empty, completed, Mono is emitted.
+                    // that. Otherwise an empty Mono is emitted.
+                    // Blocking at the very end of the method to ensure that the event is added to a batch before
+                    // requesting another event.
                     getOrCreateBatch()
                         .flatMap(batch -> batch.tryAdd(event) ? Mono.empty() : Mono.just(batch))
                         .flatMap(fullBatch -> {
@@ -171,18 +174,31 @@ public class PublishStreamOfEvents {
 
                     sink.complete();
                 });
-            });
+            }).publish().autoConnect();
 
-            final Duration windowPeriod = Duration.ofSeconds(1);
-            final Flux<EventDataBatch> emitAtIntervals = Flux.interval(windowPeriod)
-                .takeUntilOther(fullBatchFlux.then())
+            // Periodically checks to see if there is an available, not full, batch to send. If there is, it sends that
+            // batch. It keeps checking for an available batch until the last event in `events` has been published.
+            final Flux<EventDataBatch> emitAtIntervals = Flux.interval(windowDuration)
+                .takeUntilOther(events.then())
                 .flatMap(v -> {
                     final EventDataBatch batch = currentBatch.getAndSet(null);
+                    logger.info("Interval check. Has items? {}", batch != null && batch.getCount() > 0);
                     return batch != null ? Mono.just(batch) : Mono.empty();
                 });
 
+            // Merge the two fluxes together so the results
             return Flux.merge(fullBatchFlux, emitAtIntervals)
-                .flatMap(batchToSend -> producer.send(batchToSend))
+                .flatMap(batchToSend -> {
+                    final String eventNumbers = batchToSend.getEvents()
+                        .stream()
+                        .map(e -> e.getProperties().getOrDefault(EVENT_NUMBER, "n/a").toString())
+                        .collect(Collectors.joining(", "));
+
+                    logger.info("Sending batch with {} events. Size: {} bytes. Event numbers in batch [{}]",
+                        batchToSend.getCount(), batchToSend.getSizeInBytes(), eventNumbers);
+
+                    return producer.send(batchToSend);
+                })
                 .then();
         }
 
@@ -194,7 +210,7 @@ public class PublishStreamOfEvents {
         }
 
         private Mono<EventDataBatch> createBatch() {
-            return producer.createBatch().map(batch -> {
+            return producer.createBatch(batchOptions).map(batch -> {
                 currentBatch.set(batch);
                 return batch;
             });
@@ -203,6 +219,11 @@ public class PublishStreamOfEvents {
         public void close() {
             if (isDisposed.getAndSet(true)) {
                 return;
+            }
+
+            final EventDataBatch batch = currentBatch.getAndSet(null);
+            if (batch != null) {
+                producer.send(batch).block(Duration.ofSeconds(30));
             }
 
             producer.close();
