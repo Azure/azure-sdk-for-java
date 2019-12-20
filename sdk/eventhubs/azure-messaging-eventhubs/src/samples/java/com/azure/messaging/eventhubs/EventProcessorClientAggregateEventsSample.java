0eba5b5a7b26dc8ebf21df59aa027fb5559881b4
Connie <conniey@microsoft.com>
2019-12-20 09:58:07 -0800

    Adding simpler sample.

--- sdk/eventhubs/azure-messaging-eventhubs/src/samples/java/com/azure/messaging/eventhubs/EventProcessorClientAggregateEventsSample.java
+++ sdk/eventhubs/azure-messaging-eventhubs/src/samples/java/com/azure/messaging/eventhubs/EventProcessorClientAggregateEventsSample.java
@@ -52,7 +52,7 @@ import java.util.function.Consumer;
  *
  * Every 5 seconds, {@link MachineInformation} reports the current average temperature of the machine.
  */
-public class EventProcessorClientWithStateManagement {
+public class EventProcessorClientAggregateEventsSample {
     private static final Duration REPORTING_INTERVAL = Duration.ofSeconds(5);
     private static final String EH_CONNECTION_STRING = "Endpoint={endpoint};SharedAccessKeyName={sharedAccessKeyName};"
         + "SharedAccessKey={sharedAccessKey};EntityPath={eventHubName}";
