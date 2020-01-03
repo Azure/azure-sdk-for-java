ebcee09fae5763733772f5688eceba93d5fc0dcb
Connie <conniey@microsoft.com>
2020-01-03 14:24:50 -0800

    Fix error in update_version script since files.txt is removed.

--- eng/versioning/update_versions.py
+++ eng/versioning/update_versions.py
@@ -148,26 +148,6 @@ def update_versions_all(update_type, build_type, target_file, skip_readme):
                 if (file_name.endswith('.md') and not skip_readme) or (file_name.startswith('pom.') and file_name.endswith('.xml')):
                     update_versions(version_map, file_path)
 
-    # This is a temporary stop gap to deal with versions hard coded in java files.
-    # Everything within the begin/end tags below can be deleted once
-    # https://github.com/Azure/azure-sdk-for-java/issues/3141 has been fixed.
-    # version_*_java_files.txt
-    # BEGIN:Versions_in_java_files
-    if not target_file and BuildType.none != build_type:
-        # the good thing here is that the java files only contain library versions, not
-        # external versions
-        version_java_file = os.path.normpath('eng/versioning/version_' + build_type.name + '_java_files.txt')
-        with open(version_java_file) as f:
-            for raw_line in f:
-                java_file_to_update = raw_line.strip()
-                if not java_file_to_update or java_file_to_update.startswith('#'):
-                    continue
-                if os.path.isfile(java_file_to_update):
-                    update_versions(version_map, java_file_to_update)
-                else:
-                    raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), java_file_to_update)
-    # END:Versions_in_java_files
-
 def main():
     parser = argparse.ArgumentParser(description='Replace version numbers in poms and READMEs.')
     parser.add_argument('--update-type', '--ut', type=UpdateType, choices=list(UpdateType))
