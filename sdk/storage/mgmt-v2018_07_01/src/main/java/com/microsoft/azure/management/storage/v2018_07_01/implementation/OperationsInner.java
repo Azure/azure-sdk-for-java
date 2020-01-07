872be1941c0a5cc96b28e9e0045b1bfd1c822afc
SDK Automation <sdkautomation@microsoft.com>
2020-01-07 01:48:47 +0000

    Generated from 40703e6be5fb9549d3484bf41b7c3bffa740b2b5
    Add default response

--- sdk/storage/mgmt-v2018_07_01/src/main/java/com/microsoft/azure/management/storage/v2018_07_01/implementation/OperationsInner.java
+++ sdk/storage/mgmt-v2018_07_01/src/main/java/com/microsoft/azure/management/storage/v2018_07_01/implementation/OperationsInner.java
@@ -102,10 +102,8 @@ public class OperationsInner {
      * @return the observable to the List&lt;OperationInner&gt; object
      */
     public Observable<ServiceResponse<List<OperationInner>>> listWithServiceResponseAsync() {
-        if (this.client.apiVersion() == null) {
-            throw new IllegalArgumentException("Parameter this.client.apiVersion() is required and cannot be null.");
-        }
-        return service.list(this.client.apiVersion(), this.client.acceptLanguage(), this.client.userAgent())
+        final String apiVersion = "2018-07-01";
+        return service.list(apiVersion, this.client.acceptLanguage(), this.client.userAgent())
             .flatMap(new Func1<Response<ResponseBody>, Observable<ServiceResponse<List<OperationInner>>>>() {
                 @Override
                 public Observable<ServiceResponse<List<OperationInner>>> call(Response<ResponseBody> response) {
@@ -124,7 +122,7 @@ public class OperationsInner {
             });
     }
 
-    private ServiceResponse<PageImpl<OperationInner>> listDelegate(Response<ResponseBody> response) throws CloudException, IOException, IllegalArgumentException {
+    private ServiceResponse<PageImpl<OperationInner>> listDelegate(Response<ResponseBody> response) throws CloudException, IOException {
         return this.client.restClient().responseBuilderFactory().<PageImpl<OperationInner>, CloudException>newInstance(this.client.serializerAdapter())
                 .register(200, new TypeToken<PageImpl<OperationInner>>() { }.getType())
                 .registerError(CloudException.class)
