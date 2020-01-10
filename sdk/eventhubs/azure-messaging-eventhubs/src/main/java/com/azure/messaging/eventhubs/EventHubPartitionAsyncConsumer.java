d57f3c7a3ffd22a672525ba6153b1cece4f0aa06
Connie <conniey@microsoft.com>
2020-01-09 19:27:34 -0800

    Add AmqpLinkMessageProcessor and partition async consumer.

--- sdk/eventhubs/azure-messaging-eventhubs/src/main/java/com/azure/messaging/eventhubs/EventHubPartitionAsyncConsumer.java
+++ sdk/eventhubs/azure-messaging-eventhubs/src/main/java/com/azure/messaging/eventhubs/EventHubPartitionAsyncConsumer.java
@@ -3,9 +3,8 @@
 
 package com.azure.messaging.eventhubs;
 
-import com.azure.core.amqp.implementation.AmqpReceiveLink;
 import com.azure.core.amqp.implementation.MessageSerializer;
-import com.azure.core.util.logging.ClientLogger;
+import com.azure.messaging.eventhubs.implementation.AmqpLinkMessageProcessor;
 import com.azure.messaging.eventhubs.models.LastEnqueuedEventProperties;
 import com.azure.messaging.eventhubs.models.PartitionContext;
 import com.azure.messaging.eventhubs.models.PartitionEvent;
@@ -13,117 +12,43 @@ import com.azure.messaging.eventhubs.models.ReceiveOptions;
 import org.apache.qpid.proton.message.Message;
 import reactor.core.publisher.EmitterProcessor;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
-import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 
 /**
  * A package-private consumer responsible for reading {@link EventData} from a specific Event Hub partition in the
  * context of a specific consumer group.
  */
 class EventHubPartitionAsyncConsumer implements AutoCloseable {
-    private static final AtomicReferenceFieldUpdater<EventHubPartitionAsyncConsumer, AmqpReceiveLink>
-        RECEIVE_LINK_FIELD_UPDATER = AtomicReferenceFieldUpdater.newUpdater(
-        EventHubPartitionAsyncConsumer.class, AmqpReceiveLink.class, "receiveLink");
-
-    // We don't want to dump too many credits on the link at once. It's easy enough to ask for more.
-    private static final int MINIMUM_REQUEST = 0;
-    private static final int MAXIMUM_REQUEST = 100;
-
-    private final AtomicInteger creditsToRequest = new AtomicInteger(1);
     private final AtomicBoolean isDisposed = new AtomicBoolean();
     private final AtomicReference<LastEnqueuedEventProperties> lastEnqueuedEventProperties = new AtomicReference<>();
-    private final ClientLogger logger = new ClientLogger(EventHubPartitionAsyncConsumer.class);
+    private final AmqpLinkMessageProcessor amqpLinkMessageProcessor;
     private final MessageSerializer messageSerializer;
     private final String fullyQualifiedNamespace;
     private final String eventHubName;
     private final String consumerGroup;
     private final String partitionId;
-    private final EmitterProcessor<PartitionEvent> emitterProcessor;
-    private final Flux<PartitionEvent> messageFlux;
     private final boolean trackLastEnqueuedEventProperties;
+    private final EmitterProcessor<PartitionEvent> emitterProcessor;
 
-    private volatile AmqpReceiveLink receiveLink;
-
-    EventHubPartitionAsyncConsumer(Mono<AmqpReceiveLink> receiveLinkMono, MessageSerializer messageSerializer,
-        String fullyQualifiedNamespace, String eventHubName, String consumerGroup, String partitionId,
-        int prefetchCount, boolean trackLastEnqueuedEventProperties) {
+    EventHubPartitionAsyncConsumer(AmqpLinkMessageProcessor amqpLinkMessageProcessor,
+        MessageSerializer messageSerializer, String fullyQualifiedNamespace, String eventHubName, String consumerGroup,
+        String partitionId, int prefetchCount, boolean trackLastEnqueuedEventProperties) {
+        this.amqpLinkMessageProcessor = amqpLinkMessageProcessor;
         this.messageSerializer = messageSerializer;
         this.fullyQualifiedNamespace = fullyQualifiedNamespace;
         this.eventHubName = eventHubName;
         this.consumerGroup = consumerGroup;
         this.partitionId = partitionId;
-        this.emitterProcessor = EmitterProcessor.create(prefetchCount, false);
+        this.emitterProcessor = amqpLinkMessageProcessor
+            .map(message -> onMessageReceived(message))
+            .subscribeWith(EmitterProcessor.create(prefetchCount, false));;
         this.trackLastEnqueuedEventProperties = trackLastEnqueuedEventProperties;
 
         if (trackLastEnqueuedEventProperties) {
             lastEnqueuedEventProperties.set(new LastEnqueuedEventProperties(null, null, null, null));
         }
-
-        // Caching the created link so we don't invoke another link creation.
-        this.messageFlux = receiveLinkMono.cache().flatMapMany(link -> {
-            if (RECEIVE_LINK_FIELD_UPDATER.compareAndSet(this, null, link)) {
-                logger.info("Created AMQP receive link. Initializing prefetch credits: {}", prefetchCount);
-                link.addCredits(prefetchCount);
-
-                link.setEmptyCreditListener(() -> {
-                    if (emitterProcessor.hasDownstreams()) {
-                        return creditsToRequest.get();
-                    } else {
-                        logger.verbose("Emitter has no downstream subscribers. Not adding credits.");
-                        return 0;
-                    }
-                });
-
-                link.getEndpointStates().subscribe(
-                    state -> {
-                        logger.verbose("Endpoint state: {}", state);
-                    },
-                    error -> {
-                        logger.info("Error received in AmqpReceiveLink. {}", error.toString());
-
-                        //TODO (conniey): Propagate error to emitter and re-resubscribe for a link if it is transient.
-                        close();
-                    },
-                    () -> {
-                        logger.info("Amqp receive link shutting down.");
-                        close();
-                    });
-            }
-
-            return link.receive().map(message -> onMessageReceived(message));
-        }).subscribeWith(emitterProcessor)
-            .doOnSubscribe(subscription -> {
-                AmqpReceiveLink existingLink = RECEIVE_LINK_FIELD_UPDATER.get(this);
-                if (existingLink == null) {
-                    logger.info("AmqpReceiveLink not set yet.");
-                    return;
-                }
-
-                logger.verbose("Subscription received for consumer.");
-                if (existingLink.getCredits() == 0) {
-                    logger.info("Subscription received and there are no remaining credits on the link. Adding more.");
-                    existingLink.addCredits(creditsToRequest.get());
-                }
-            })
-            .doOnRequest(request -> {
-                if (request < MINIMUM_REQUEST) {
-                    logger.warning(Messages.REQUEST_VALUE_NOT_VALID,
-                        MINIMUM_REQUEST, MAXIMUM_REQUEST);
-                    return;
-                }
-
-                final int newRequest = request > MAXIMUM_REQUEST
-                    ? MAXIMUM_REQUEST
-                    : (int) request;
-
-                logger.verbose("Back pressure request. Old value: {}. New value: {}", creditsToRequest.get(),
-                    newRequest);
-                creditsToRequest.set(newRequest);
-            });
     }
 
     /**
@@ -132,12 +57,8 @@ class EventHubPartitionAsyncConsumer implements AutoCloseable {
     @Override
     public void close() {
         if (!isDisposed.getAndSet(true)) {
-            final AmqpReceiveLink receiveLink = RECEIVE_LINK_FIELD_UPDATER.getAndSet(this, null);
-            if (receiveLink != null) {
-                receiveLink.close();
-            }
-
             emitterProcessor.onComplete();
+            amqpLinkMessageProcessor.cancel();
         }
     }
 
@@ -147,7 +68,7 @@ class EventHubPartitionAsyncConsumer implements AutoCloseable {
      * @return A stream of events received from the partition.
      */
     Flux<PartitionEvent> receive() {
-        return messageFlux;
+        return emitterProcessor;
     }
 
     /**
---
+++ sdk/eventhubs/azure-messaging-eventhubs/src/main/java/com/azure/messaging/eventhubs/implementation/AmqpLinkMessageProcessor.java
@@ -0,0 +1,346 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.messaging.eventhubs.implementation;
+
+import com.azure.core.amqp.AmqpEndpointState;
+import com.azure.core.amqp.AmqpRetryPolicy;
+import com.azure.core.amqp.implementation.AmqpReceiveLink;
+import com.azure.core.util.logging.ClientLogger;
+import com.azure.messaging.eventhubs.Messages;
+import org.apache.qpid.proton.message.Message;
+import org.reactivestreams.Subscription;
+import reactor.core.CoreSubscriber;
+import reactor.core.Disposable;
+import reactor.core.Disposables;
+import reactor.core.publisher.FluxProcessor;
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.Operators;
+
+import java.time.Duration;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Processes AMQP receive links into a stream of AMQP messages.
+ */
+public class AmqpLinkMessageProcessor extends FluxProcessor<AmqpReceiveLink, Message> implements Subscription {
+    // We don't want to dump too many credits on the link at once. It's easy enough to ask for more.
+    private static final int MINIMUM_REQUEST = 0;
+    private static final int MAXIMUM_REQUEST = 100;
+
+    private final ClientLogger logger = new ClientLogger(AmqpLinkMessageProcessor.class);
+    private final Object lock = new Object();
+    private final AtomicBoolean isTerminated = new AtomicBoolean();
+    private final AtomicBoolean hasDownstream = new AtomicBoolean();
+    private final AtomicInteger retryAttempts = new AtomicInteger();
+    private final AtomicInteger linkCreditRequest = new AtomicInteger(1);
+
+    private final int prefetch;
+    private final AmqpRetryPolicy retryPolicy;
+
+    private volatile Subscription upstream;
+    private volatile CoreSubscriber<? super Message> downstream;
+
+    private volatile Throwable lastError;
+    private volatile AmqpReceiveLink currentLink;
+    private volatile Disposable currentLinkSubscriptions;
+    private volatile Disposable retrySubscription;
+
+    /**
+     * Creates an instance of {@link AmqpLinkMessageProcessor}.
+     *
+     * @param prefetch The number if messages to initially fetch.
+     * @param retryPolicy Retry policy to apply when fetching a new AMQP channel.
+     *
+     * @throws NullPointerException if {@code retryPolicy} is null.
+     * @throws IllegalArgumentException if {@code prefetch} is less than 0.
+     */
+    public AmqpLinkMessageProcessor(int prefetch, AmqpRetryPolicy retryPolicy) {
+        this.retryPolicy = Objects.requireNonNull(retryPolicy, "'retryPolicy' cannot be null.");
+        if (prefetch < 0) {
+            throw logger.logExceptionAsError(new IllegalArgumentException("'prefetch' cannot be less than 0."));
+        }
+
+        this.prefetch = prefetch;
+    }
+
+    /**
+     * Gets the error associated with this processor.
+     *
+     * @return Error associated with this processor. {@code null} if there is no error.
+     */
+    @Override
+    public Throwable getError() {
+        return lastError;
+    }
+
+    /**
+     * Gets whether or not the processor is terminated.
+     *
+     * @return {@code true} if the processor has terminated; false otherwise.
+     */
+    @Override
+    public boolean isTerminated() {
+        return isTerminated.get();
+    }
+
+    /**
+     * When a subscription is obtained from upstream publisher.
+     *
+     * @param subscription Subscription to upstream publisher.
+     */
+    @Override
+    public void onSubscribe(Subscription subscription) {
+        Objects.requireNonNull(subscription, "'subscription' cannot be null");
+
+        if (isTerminated()) {
+            return;
+        }
+
+        logger.verbose("Subscribing to upstream.");
+
+        this.upstream = subscription;
+
+        // Don't request from upstream until there is a downstream subscriber.
+        subscription.request(0);
+    }
+
+    /**
+     * When the next AMQP link is fetched.
+     *
+     * @param next The next AMQP receive link.
+     */
+    @Override
+    public void onNext(AmqpReceiveLink next) {
+        Objects.requireNonNull(next, "'next' cannot be null.");
+
+        if (isTerminated()) {
+            logger.warning("Got another link when we have already terminated processor. Link: {}",
+                next.getEntityPath());
+            return;
+        }
+
+        logger.info("Setting next AMQP receive link.");
+
+        final AmqpReceiveLink oldChannel;
+        final Disposable oldSubscription;
+        synchronized (lock) {
+            oldChannel = currentLink;
+            oldSubscription = currentLinkSubscriptions;
+
+            currentLink = next;
+
+            next.addCredits(prefetch);
+            next.setEmptyCreditListener(() -> {
+                if (hasDownstream.get()) {
+                    return linkCreditRequest.get();
+                } else {
+                    logger.verbose("Emitter has no downstream subscribers. Not adding credits.");
+                    return 0;
+                }
+            });
+
+            currentLinkSubscriptions = Disposables.composite(
+                next.getEndpointStates().subscribe(
+                    state -> {
+                        // Connection was successfully opened, we can reset the retry interval.
+                        if (state == AmqpEndpointState.ACTIVE) {
+                            retryAttempts.set(0);
+                        }
+                    },
+                    error -> {
+                        currentLink = null;
+                        onError(error);
+                    },
+                    () -> {
+                        if (isTerminated()) {
+                            logger.info("Connection is disposed.");
+                        } else {
+                            logger.info("Connection closed.");
+                            currentLink = null;
+                        }
+                    }),
+                next.receive().subscribe(message -> {
+                    logger.info("Pushing next message downstream.");
+                    downstream.onNext(message);
+                }));
+        }
+
+        if (oldChannel != null) {
+            try {
+                oldChannel.close();
+            } catch (Exception e) {
+                logger.info("Error encountered closing old connection.", e);
+            }
+        }
+
+        if (oldSubscription != null) {
+            oldSubscription.dispose();
+        }
+    }
+
+    /**
+     * Sets up the downstream subscriber.
+     *
+     * @param actual The downstream subscriber.
+     * @throws IllegalStateException if there is already a downstream subscriber.
+     */
+    @Override
+    public void subscribe(CoreSubscriber<? super Message> actual) {
+        Objects.requireNonNull(actual, "'actual' cannot be null.");
+
+        if (isTerminated()) {
+            logger.info("Processor is already terminated.");
+            if (hasError()) {
+                actual.onError(lastError);
+            } else {
+                actual.onComplete();
+            }
+
+            return;
+        }
+
+        if (!hasDownstream.getAndSet(true)) {
+            this.downstream = actual;
+            requestUpstream();
+        } else {
+            Operators.error(actual, logger.logExceptionAsError(new IllegalStateException(
+                "There is already one downstream subscriber.'")));
+        }
+    }
+
+    /**
+     * When an error occurs from the upstream publisher. If the {@code throwable} is a transient failure, another AMQP
+     * element is requested if the {@link AmqpRetryPolicy} allows. Otherwise, the processor closes.
+     *
+     * @param throwable Error that occurred in upstream publisher.
+     */
+    @Override
+    public void onError(Throwable throwable) {
+        Objects.requireNonNull(throwable, "'throwable' is required.");
+
+        if (isTerminated()) {
+            return;
+        }
+
+        final int attempt = retryAttempts.incrementAndGet();
+        final Duration retryInterval = retryPolicy.calculateRetryDelay(throwable, attempt);
+
+        if (retryInterval != null) {
+            logger.warning("Transient error occurred. Attempt: {}. Retrying after {} ms.",
+                attempt, retryInterval.toMillis(), throwable);
+
+            retrySubscription = Mono.delay(retryInterval).subscribe(i -> {
+                requestUpstream();
+            });
+
+            return;
+        }
+
+        logger.warning("Non-retryable error occurred in connection.", throwable);
+        lastError = throwable;
+        isTerminated.set(true);
+
+        synchronized (lock) {
+            if (downstream != null) {
+                downstream.onError(throwable);
+            }
+        }
+    }
+
+    /**
+     * Terminates the
+     */
+    @Override
+    public void onComplete() {
+        if (!isTerminated.getAndSet(true)) {
+            terminate();
+        }
+    }
+
+    /**
+     * When downstream subscriber makes a back-pressure request.
+     */
+    @Override
+    public void request(long request) {
+        if (isTerminated.get()) {
+            logger.warning("Cannot request more from AMQP link processor that is disposed.");
+            return;
+        }
+
+        if (request < MINIMUM_REQUEST) {
+            logger.warning(Messages.REQUEST_VALUE_NOT_VALID,
+                MINIMUM_REQUEST, MAXIMUM_REQUEST);
+            return;
+        }
+
+        final int newRequest = request > MAXIMUM_REQUEST
+            ? MAXIMUM_REQUEST
+            : (int) request;
+
+        logger.verbose("Back pressure request. Old value: {}. New value: {}", linkCreditRequest.get(),
+            newRequest);
+        linkCreditRequest.set(newRequest);
+    }
+
+    /**
+     * When downstream subscriber cancels their subscription.
+     */
+    @Override
+    public void cancel() {
+        if (isTerminated.getAndSet(true)) {
+            return;
+        }
+
+        terminate();
+    }
+
+    private void requestUpstream() {
+        if (isTerminated()) {
+            return;
+        }
+
+        synchronized (lock) {
+            if (currentLink != null) {
+                logger.info("AmqpReceiveLink exists, not requesting another.");
+                return;
+            } else if (isTerminated.get() || upstream == null) {
+                logger.verbose("Terminated. Not requesting another.");
+                return;
+            }
+        }
+
+        logger.info("Connection not requested, yet. Requesting one.");
+        upstream.request(1);
+    }
+
+    private void terminate() {
+        if (isTerminated.getAndSet(true)) {
+            return;
+        }
+
+        if (hasDownstream.get()) {
+            downstream.onComplete();
+        }
+
+        if (retrySubscription != null && !retrySubscription.isDisposed()) {
+            retrySubscription.dispose();
+        }
+
+        if (currentLink != null) {
+            try {
+                currentLink.close();
+            } catch (Exception error) {
+                logger.warning("Error encountered closing connection.", error);
+            }
+        }
+
+        currentLink = null;
+
+        if (currentLinkSubscriptions != null) {
+            currentLinkSubscriptions.dispose();
+        }
+    }
+}
