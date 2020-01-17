4bad0d4101c05b53fc704365af799963c4241fc6
rickle-msft <frley@microsoft.com>
2020-01-16 17:29:41 -0800

    More path methods

--- sdk/storage/azure-storage-blob-nio/src/main/java/com/azure/storage/blob/nio/AzurePath.java
+++ sdk/storage/azure-storage-blob-nio/src/main/java/com/azure/storage/blob/nio/AzurePath.java
@@ -3,6 +3,8 @@
 
 package com.azure.storage.blob.nio;
 
+import com.azure.core.util.logging.ClientLogger;
+import com.azure.storage.blob.nio.implementation.util.Utility;
 import reactor.core.publisher.Flux;
 
 import java.io.File;
@@ -17,11 +19,13 @@ import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
 import java.util.Iterator;
+import java.util.Stack;
 
 /**
  * {@inheritDoc}
  */
 public final class AzurePath implements Path {
+    private final ClientLogger logger = new ClientLogger(AzurePath.class);
     private static final String ROOT_DIR_SUFFIX = ":";
 
     private final AzureFileSystem parentFileSystem;
@@ -39,8 +43,8 @@ public final class AzurePath implements Path {
         if (this.pathString.contains(ROOT_DIR_SUFFIX)
             && (this.pathString.indexOf(ROOT_DIR_SUFFIX) >
             this.pathString.indexOf(this.parentFileSystem.getSeparator()))) {
-            throw new InvalidPathException(this.pathString, ROOT_DIR_SUFFIX + " is an invalid character except to " +
-                "identify the root element of this path if there is one.");
+            throw Utility.logError(logger, new InvalidPathException(this.pathString, ROOT_DIR_SUFFIX +
+                " is an invalid character except to identify the root element of this path if there is one."));
         }
     }
 
@@ -93,7 +97,7 @@ public final class AzurePath implements Path {
             return null;
         } else {
             return this.parentFileSystem.getPath(
-                Flux.fromArray(this.pathString.split(this.parentFileSystem.getSeparator())).last().block());
+                Flux.fromArray(this.splitToElements()).last().block());
         }
     }
 
@@ -106,7 +110,7 @@ public final class AzurePath implements Path {
         If this path only has one element, there is no parent. Note the root is included in the parent, so we don't
         use getNameCount here.
          */
-        if (this.pathString.split(this.parentFileSystem.getSeparator()).length == 1) {
+        if (this.splitToElements().length == 1) {
             return null;
         }
 
@@ -168,7 +172,22 @@ public final class AzurePath implements Path {
         There can only be one instance of a file system with a given id, so comparing object identity is equivalent
         to checking ids here.
          */
-        return path.toString().equals(this.toString()) && path.getFileSystem() == this.parentFileSystem;
+        if (path.getFileSystem() != this.parentFileSystem) {
+            return false;
+        }
+
+        String[] thisPathElements = this.splitToElements();
+        String[] otherPathElements = ((AzurePath) path).pathString.split(this.parentFileSystem.getSeparator());
+        if (otherPathElements.length > thisPathElements.length) {
+            return false;
+        }
+        for (int i = 0; i < otherPathElements.length; i++) {
+            if (!otherPathElements[i].equals(thisPathElements[i])) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     /**
@@ -184,7 +203,30 @@ public final class AzurePath implements Path {
      */
     @Override
     public boolean endsWith(Path path) {
-        return false;
+        /*
+        There can only be one instance of a file system with a given id, so comparing object identity is equivalent
+        to checking ids here.
+         */
+        if (path.getFileSystem() != this.parentFileSystem) {
+            return false;
+        }
+
+        String[] thisPathElements = this.splitToElements();
+        String[] otherPathElements = ((AzurePath) path).pathString.split(this.parentFileSystem.getSeparator());
+        if (otherPathElements.length > thisPathElements.length) {
+            return false;
+        }
+        // If the given path has a root component, the paths must be equal.
+        if (path.getRoot() != null && otherPathElements.length != thisPathElements.length) {
+            return false;
+        }
+        for (int i = 1; i <= otherPathElements.length; i++) {
+            if (!otherPathElements[otherPathElements.length - i]
+                .equals(thisPathElements[thisPathElements.length - i])) {
+                return false;
+            }
+        }
+        return true;
     }
 
     /**
@@ -192,7 +234,7 @@ public final class AzurePath implements Path {
      */
     @Override
     public boolean endsWith(String s) {
-        return false;
+        return this.endsWith(this.parentFileSystem.getPath(s));
     }
 
     /**
@@ -200,7 +242,39 @@ public final class AzurePath implements Path {
      */
     @Override
     public Path normalize() {
-        return null;
+        Stack<String> stack = new Stack<>();
+        String[] pathElements = this.splitToElements();
+        Path root = this.getRoot();
+        String rootStr = root == null ? null : root.toString();
+        rootStr = rootStr == null ? null : rootStr.substring(0, rootStr.length() -1); // Strip the '/' for comparison.
+        for (String element : pathElements) {
+            if (element.equals(".")) {
+                continue;
+            } else if (element.equals("..")) {
+                if (rootStr != null) {
+                    // Root path. We never push "..".
+                    if (stack.peek().equals(rootStr)) {
+                        // Cannot go higher than root. Ignore.
+                        continue;
+                    } else {
+                        stack.pop();
+                    }
+                } else {
+                    // Relative paths can have an arbitrary number of ".." at the beginning.
+                    if (stack.empty()) {
+                        stack.push(element);
+                    } else if (stack.peek().equals("..")) {
+                        stack.push(element);
+                    } else {
+                        stack.pop();
+                    }
+                }
+            } else {
+                stack.push(element);
+            }
+        }
+
+        return this.parentFileSystem.getPath(String.join(this.parentFileSystem.getSeparator(), stack));
     }
 
     /**
@@ -208,7 +282,13 @@ public final class AzurePath implements Path {
      */
     @Override
     public Path resolve(Path path) {
-        return null;
+        if (path.isAbsolute()) {
+            return path;
+        }
+        if (path.getNameCount() == 0) {
+            return this;
+        }
+        return this.parentFileSystem.getPath(this.toString(), path.toString());
     }
 
     /**
@@ -216,7 +296,7 @@ public final class AzurePath implements Path {
      */
     @Override
     public Path resolve(String s) {
-        return null;
+        return this.resolve(this.parentFileSystem.getPath(s));
     }
 
     /**
@@ -224,7 +304,12 @@ public final class AzurePath implements Path {
      */
     @Override
     public Path resolveSibling(Path path) {
-        return null;
+        if (path.isAbsolute()) {
+            return path;
+        }
+
+        Path parent = this.getParent();
+        return parent == null ? path : parent.resolve(path);
     }
 
     /**
@@ -232,7 +317,7 @@ public final class AzurePath implements Path {
      */
     @Override
     public Path resolveSibling(String s) {
-        return null;
+        return this.resolveSibling(this.parentFileSystem.getPath(s));
     }
 
     /**
@@ -240,6 +325,10 @@ public final class AzurePath implements Path {
      */
     @Override
     public Path relativize(Path path) {
+        if (path.getRoot() == null ^ this.getRoot() == null) {
+            throw Utility.logError(logger,
+                new IllegalArgumentException("Both paths must be absolute or neither can be"));
+        }
         return null;
     }
 
@@ -328,5 +417,9 @@ public final class AzurePath implements Path {
         }
 
         return this.pathString;
+    }
+
+    private String[] splitToElements() {
+        return this.pathString.split(this.parentFileSystem.getSeparator());
     }
 }
