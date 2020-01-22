4da89f9f128aadb30fd82f4538f06b025d53fb25
Nava Vaisman Levy <navalev@users.noreply.github.com>
2020-01-22 13:24:36 +0200

    undo all jsonwrapper changes

--- sdk/core/azure-core/src/main/java/com/azure/core/implementation/serializer/jsonwrapper/spi/package-info.java
+++ sdk/core/azure-core/src/main/java/com/azure/core/implementation/serializer/jsonwrapper/spi/package-info.java
@@ -1,7 +1,1 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-/**
- * Package containing jsonwrapper serialization tool
- */
 package com.azure.core.implementation.serializer.jsonwrapper.spi;
