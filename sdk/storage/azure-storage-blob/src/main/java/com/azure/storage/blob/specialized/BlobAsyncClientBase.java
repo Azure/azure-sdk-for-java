9874bd28d294dd912bbdeb74a26ff80d48f41ac7
Gauri Prasad <gapra@microsoft.com>
2020-01-27 10:57:29 -0800

    Removed unused constants in BlobAsyncClientBase

--- sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/specialized/BlobAsyncClientBase.java
+++ sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/specialized/BlobAsyncClientBase.java
@@ -90,9 +90,6 @@ import static java.lang.StrictMath.toIntExact;
  */
 public class BlobAsyncClientBase {
 
-    private static final int BLOB_DEFAULT_DOWNLOAD_BLOCK_SIZE = 4 * Constants.MB;
-    private static final int BLOB_MAX_DOWNLOAD_BLOCK_SIZE = 100 * Constants.MB;
-
     private final ClientLogger logger = new ClientLogger(BlobAsyncClientBase.class);
 
     protected final AzureBlobStorageImpl azureBlobStorage;
