17c52f96782a4aa61c1f6790c595680417734283
Gauri Prasad <gapra@microsoft.com>
2020-01-29 12:03:42 -0800

    Added test

--- sdk/storage/azure-storage-blob/src/test/java/com/azure/storage/blob/BlockBlobInputOutputStreamTest.groovy
+++ sdk/storage/azure-storage-blob/src/test/java/com/azure/storage/blob/BlockBlobInputOutputStreamTest.groovy
@@ -26,7 +26,6 @@ class BlockBlobInputOutputStreamTest extends APISpec {
 
         then:
         def inputStream = bc.openInputStream()
-        def propertiesBefore = inputStream.getProperties()
         int b
         def outputStream = new ByteArrayOutputStream()
         try {
@@ -38,8 +37,36 @@ class BlockBlobInputOutputStreamTest extends APISpec {
         }
         def propertiesAfter = inputStream.getProperties()
         propertiesAfter.getBlobType() == BlobType.BLOCK_BLOB
-        propertiesAfter.getBlobSize() == 5 * Constants.MB
-        propertiesBefore.getETag() == propertiesAfter.getETag()
+        propertiesAfter.getBlobSize() == 4 * Constants.MB
+        byte[] randomBytes2 = outputStream.toByteArray()
+        assert randomBytes2 == Arrays.copyOfRange(randomBytes, 1 * Constants.MB, 6 * Constants.MB)
+    }
+
+    // Only run this test in live mode as BlobOutputStream dynamically assigns blocks
+    @Requires({ liveMode() })
+    def "Get properties before"() {
+        when:
+        int length = 6 * Constants.MB
+        byte[] randomBytes = getRandomByteArray(length)
+
+        BlobOutputStream outStream = bc.getBlobOutputStream()
+        outStream.write(randomBytes, 1 * Constants.MB, 5 * Constants.MB)
+        outStream.close()
+
+        then:
+        def inputStream = bc.openInputStream()
+        def propertiesBefore = inputStream.getProperties()
+        int b
+        def outputStream = new ByteArrayOutputStream()
+        try {
+            while ((b = inputStream.read()) != -1) {
+                outputStream.write(b)
+            }
+        } catch (IOException ex) {
+            throw new UncheckedIOException(ex)
+        }
+        propertiesBefore.getBlobType() == BlobType.BLOCK_BLOB
+        propertiesBefore.getBlobSize() == 5 * Constants.MB
         byte[] randomBytes2 = outputStream.toByteArray()
         assert randomBytes2 == Arrays.copyOfRange(randomBytes, 1 * Constants.MB, 6 * Constants.MB)
     }
