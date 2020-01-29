05e4aa38f57f80abe0fd3c5b58a9940f46d7aba5
Anu Thomas Chandy <anuamd@hotmail.com>
2020-01-29 09:57:42 -0800

    Making BufferedResponse to do deep caching (it was doing shallow caching)

--- sdk/core/azure-core/src/main/java/com/azure/core/implementation/http/BufferedHttpResponse.java
+++ sdk/core/azure-core/src/main/java/com/azure/core/implementation/http/BufferedHttpResponse.java
@@ -28,7 +28,10 @@ public final class BufferedHttpResponse extends HttpResponse {
     public BufferedHttpResponse(HttpResponse innerHttpResponse) {
         super(innerHttpResponse.getRequest());
         this.innerHttpResponse = innerHttpResponse;
-        this.cachedBody = innerHttpResponse.getBody().cache();
+        this.cachedBody = FluxUtil.collectBytesInByteBufferStream(innerHttpResponse.getBody())
+            .map(bytes -> ByteBuffer.wrap(bytes))
+            .flux()
+            .cache();
     }
 
     @Override
@@ -53,7 +56,7 @@ public final class BufferedHttpResponse extends HttpResponse {
 
     @Override
     public Mono<byte[]> getBodyAsByteArray() {
-        return FluxUtil.collectBytesInByteBufferStream(cachedBody.map(ByteBuffer::duplicate));
+        return cachedBody.next().map(byteBuffer -> byteBuffer.array());
     }
 
     @Override
