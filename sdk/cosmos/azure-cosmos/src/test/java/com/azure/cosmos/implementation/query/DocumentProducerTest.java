b9f9a458e936792e8a53da27245f6be989851ee8
Mohammad Derakhshani <moderakh@users.noreply.github.com>
2020-02-07 00:01:04 -0800

    increase timeout

--- sdk/cosmos/azure-cosmos/src/test/java/com/azure/cosmos/implementation/query/DocumentProducerTest.java
+++ sdk/cosmos/azure-cosmos/src/test/java/com/azure/cosmos/implementation/query/DocumentProducerTest.java
@@ -63,7 +63,7 @@ import static org.mockito.Mockito.times;
 
 public class DocumentProducerTest {
     private final static Logger logger = LoggerFactory.getLogger(DocumentProducerTest.class);
-    private static final long TIMEOUT = 20000;
+    private static final long TIMEOUT = 30000;
     private final static String OrderByPayloadFieldName = "payload";
     private final static String OrderByItemsFieldName = "orderByItems";
 
