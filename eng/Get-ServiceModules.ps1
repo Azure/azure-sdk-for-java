bff0e6067c1fc556c8288fb4977f21aa3b748fdb
Connie <conniey@microsoft.com>
2020-02-11 17:29:26 -0800

    Adding documentation.

--- eng/Get-ServiceModules.ps1
+++ eng/Get-ServiceModules.ps1
@@ -3,6 +3,25 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+<#
+    .Synopsis
+    Gets the modules that are built from the given service directory and SDK type.
+
+    .Parameter ServiceDirectory
+    Name of the service being built. For example, "eventhubs".
+    .Parameter SdkType
+    Type of SDK to find modules for.
+
+    .Example
+    .\eng\Get-ServiceModules "eventhubs" "client"
+    Will output "azure-messaging-eventhubs,azure-messaging-eventhubs-checkpointstore-blob" because
+    those are modules that are built by the 'client' SDK type.
+
+    .Example
+    .\eng\Get-ServiceModules "eventhubs" "data"
+    Will output "microsoft-azure-eventhubs,microsoft-azure-eventhubs-eph,microsoft-azure-eventhubs-extensions"
+    because those are modules that are built by the 'data' SDK type.
+#>
 [CmdletBinding()]
 param(
     [Parameter(Mandatory = $true, Position = 0)]
