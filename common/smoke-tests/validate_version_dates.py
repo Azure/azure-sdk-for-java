bc6c7f377f63de85e4413c8196111077cc780d2b
Daniel Jurek <djurek@microsoft.com>
2020-02-13 11:42:16 -0800

    extract get_xml_root from smoke test validate_version_dates.py

--- common/smoke-tests/validate_version_dates.py
+++ common/smoke-tests/validate_version_dates.py
@@ -1,19 +1,5 @@
-import xml.etree.ElementTree as ET
-from io import StringIO
 import re
-
-# namespaces in xml really mess with xmlTree: https://bugs.python.org/issue18304
-# this function provides a workaround for both parsing an xml file as well as REMOVING said namespaces
-def get_xml_root(file_path):
-    with open(file_path) as f:
-        xml = f.read()
-
-    it = ET.iterparse(StringIO(xml))
-    for _, el in it:
-        if '}' in el.tag:
-            # strip all namespaces
-            el.tag = el.tag.split('}', 1)[1]
-    return it.root
+from util import get_xml_root
 
 def main():
     root = get_xml_root('./pom.xml')
