1a8058071399154f1917d09ea01ecdf21f24e024
Gauri Prasad <gapra@microsoft.com>
2020-02-24 13:36:48 -0800

    converted to java classes

---
+++ sdk/storage/azure-storage-common/src/test/java/com/azure/storage/common/CompareTestUtils.java
@@ -0,0 +1,92 @@
+package com.azure.storage.common;
+
+import com.azure.storage.common.implementation.Constants;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.List;
+
+public class CompareTestUtils {
+    /**
+     * Compares two files for having equivalent content.
+     *
+     * @param file1
+     *     File used to upload data to the service
+     * @param file2
+     *     File used to download data from the service
+     * @param offset
+     *     Write offset from the upload file
+     * @param count
+     *     Size of the download from the service
+     *
+     * @return Whether the files have equivalent content based on offset and read count
+     */
+    public static Boolean compareFiles(File file1, File file2, long offset, long count) throws IOException {
+        Long pos = 0L;
+        int readBuffer = 8 * Constants.KB;
+        FileInputStream stream1 = new FileInputStream(file1);
+        stream1.skip(offset);
+        FileInputStream stream2 = new FileInputStream(file2);
+
+        try {
+            while (pos < count) {
+                Integer bufferSize = (int) Math.min(readBuffer, count - pos);
+                byte[] buffer1 = new byte[bufferSize];
+                byte[] buffer2 = new byte[bufferSize];
+
+                int readCount1 = stream1.read(buffer1);
+                int readCount2 = stream2.read(buffer2);
+
+                assert readCount1 == readCount2 && Arrays.equals(buffer1, buffer2);
+
+                pos += bufferSize;
+            }
+
+
+            int verificationRead = stream2.read();
+            return pos == count && verificationRead == -1;
+        } finally {
+            stream1.close();
+            stream2.close();
+        }
+
+    }
+
+    public static byte[] convertInputStreamToByteArray(InputStream inputStream) {
+        int b;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        try {
+            while ((b = inputStream.read()) != -1) {
+                outputStream.write(b);
+            }
+
+        } catch (IOException ex) {
+            throw new UncheckedIOException(ex);
+        }
+
+
+        return outputStream.toByteArray();
+    }
+
+    public static Boolean compareListToBuffer(List<ByteBuffer> buffers, ByteBuffer result) {
+        result.position(0);
+        for (ByteBuffer buffer : buffers) {
+            buffer.position(0);
+            result.limit(result.position() + buffer.remaining());
+            if (!buffer.equals(result)) {
+                return false;
+            }
+
+            result.position(result.position() + buffer.remaining());
+        }
+
+        return result.remaining() == 0;
+    }
+
+}
---
+++ sdk/storage/azure-storage-common/src/test/java/com/azure/storage/common/MockRequestResponse.java
@@ -0,0 +1,132 @@
+package com.azure.storage.common;
+
+import com.azure.core.http.HttpHeaders;
+import com.azure.core.http.HttpMethod;
+import com.azure.core.http.HttpPipelineCallContext;
+import com.azure.core.http.HttpPipelineNextPolicy;
+import com.azure.core.http.HttpRequest;
+import com.azure.core.http.HttpResponse;
+import com.azure.core.http.policy.HttpPipelinePolicy;
+import com.azure.storage.common.implementation.Constants;
+import groovy.lang.Closure;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+
+public class MockRequestResponse {
+    public HttpRequest getMockRequest() throws MalformedURLException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.put(Constants.HeaderConstants.CONTENT_ENCODING, "en-US");
+        URL url = new URL("http://devtest.blob.core.windows.net/test-container/test-blob");
+        HttpRequest request = new HttpRequest(HttpMethod.POST, url, headers, null);
+        return request;
+    }
+
+    public static HttpResponse getStubResponse(final int code, final HttpRequest request) {
+        return new HttpResponse(request) {
+            @Override
+            public int getStatusCode() {
+                return code;
+            }
+
+            @Override
+            public String getHeaderValue(String s) {
+                return null;
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                return new HttpHeaders();
+            }
+
+            @Override
+            public Flux<ByteBuffer> getBody() {
+                return Flux.empty();
+            }
+
+            @Override
+            public Mono<byte[]> getBodyAsByteArray() {
+                return Mono.just(new byte[0]);
+            }
+
+            @Override
+            public Mono<String> getBodyAsString() {
+                return Mono.just("");
+            }
+
+            @Override
+            public Mono<String> getBodyAsString(Charset charset) {
+                return Mono.just("");
+            }
+
+        };
+    }
+
+    public class MockRetryRangeResponsePolicy implements HttpPipelinePolicy {
+        @Override
+        public Mono<HttpResponse> process(HttpPipelineCallContext context, HttpPipelineNextPolicy next) {
+            return next.process().flatMap(response -> {
+                if (response.getRequest().getHeaders().getValue("x-ms-range") != "bytes=2-6") {
+                    return Mono.error(new IllegalArgumentException("The range header was not set correctly on retry."));
+                } else {
+                    // ETag can be a dummy value. It's not validated, but DownloadResponse requires one
+                    return Mono.just(new MockDownloadHttpResponse(response, 206, Flux.error(new IOException())));
+                }
+            });
+        }
+
+    }
+
+    public class MockDownloadHttpResponse extends HttpResponse {
+        public MockDownloadHttpResponse(HttpResponse response, int statusCode, Flux<ByteBuffer> body) {
+            super(response.getRequest());
+            this.statusCode = statusCode;
+            this.headers = response.getHeaders();
+            this.body = body;
+        }
+
+        @Override
+        public int getStatusCode() {
+            return statusCode;
+        }
+
+        @Override
+        public String getHeaderValue(String s) {
+            return headers.getValue(s);
+        }
+
+        @Override
+        public HttpHeaders getHeaders() {
+            return headers;
+        }
+
+        @Override
+        public Flux<ByteBuffer> getBody() {
+            return body;
+        }
+
+        @Override
+        public Mono<byte[]> getBodyAsByteArray() {
+            return Mono.error(new IOException());
+        }
+
+        @Override
+        public Mono<String> getBodyAsString() {
+            return Mono.error(new IOException());
+        }
+
+        @Override
+        public Mono<String> getBodyAsString(Charset charset) {
+            return Mono.error(new IOException());
+        }
+
+        private final int statusCode;
+        private final HttpHeaders headers;
+        private final Flux<ByteBuffer> body;
+    }
+}
