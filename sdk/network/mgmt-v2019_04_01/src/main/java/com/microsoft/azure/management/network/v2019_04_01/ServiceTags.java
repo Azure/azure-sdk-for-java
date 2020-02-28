3f596eaa01ea0508c7c3ed9e42fbbdab00c55928
SDK Automation <sdkautomation@microsoft.com>
2020-02-28 05:52:59 +0000

    Generated from aee76f6f20cccd50de6dfe022d205c698f0a291a
    in64 -> int64

--- sdk/network/mgmt-v2019_04_01/src/main/java/com/microsoft/azure/management/network/v2019_04_01/ServiceTags.java
+++ sdk/network/mgmt-v2019_04_01/src/main/java/com/microsoft/azure/management/network/v2019_04_01/ServiceTags.java
@@ -9,15 +9,17 @@
 package com.microsoft.azure.management.network.v2019_04_01;
 
 import rx.Observable;
+import com.microsoft.azure.management.network.v2019_04_01.implementation.ServiceTagsInner;
+import com.microsoft.azure.arm.model.HasInner;
 
 /**
  * Type representing ServiceTags.
  */
-public interface ServiceTags {
+public interface ServiceTags extends HasInner<ServiceTagsInner> {
     /**
      * Gets a list of service tag information resources.
      *
-     * @param location The location.
+     * @param location The location that will be used as a reference for version (not as a filter based on location, you will get the list of service tags with prefix details across all regions but limited to the cloud that your subscription belongs to).
      * @throws IllegalArgumentException thrown if parameters fail the validation
      * @return the observable for the request
      */
