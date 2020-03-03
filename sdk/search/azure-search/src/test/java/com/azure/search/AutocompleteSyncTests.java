dd09d2056c8cd55a74ee2da737484b0121bb16ef
Sima Zhu <48036328+sima-zhu@users.noreply.github.com>
2020-03-03 14:33:06 -0800

    Update AutocompleteSyncTests.java

--- sdk/search/azure-search/src/test/java/com/azure/search/AutocompleteSyncTests.java
+++ sdk/search/azure-search/src/test/java/com/azure/search/AutocompleteSyncTests.java
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft Corporation. All rights reservgited.
+// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 package com.azure.search;
 
