a2296bc811aeea4c13136715885aabd57b8b19e7
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2020-03-03 08:37:06 -0800

    Add test for change

--- sdk/search/azure-search/src/test/java/com/azure/search/SearchServiceSyncTests.java
+++ sdk/search/azure-search/src/test/java/com/azure/search/SearchServiceSyncTests.java
@@ -2,10 +2,15 @@
 // Licensed under the MIT License.
 package com.azure.search;
 
+import com.azure.core.http.rest.Response;
 import com.azure.core.util.Context;
+import com.azure.search.models.RequestOptions;
 import com.azure.search.models.ServiceStatistics;
 import org.junit.jupiter.api.Test;
 
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.unitils.reflectionassert.ReflectionAssert.assertReflectionEquals;
 import static org.unitils.reflectionassert.ReflectionComparatorMode.IGNORE_DEFAULTS;
 
@@ -25,5 +30,17 @@ public class SearchServiceSyncTests extends SearchServiceTestBase {
 
         ServiceStatistics serviceStatistics = serviceClient.getServiceStatisticsWithResponse(generateRequestOptions(), Context.NONE).getValue();
         assertReflectionEquals(serviceStatistics, getExpectedServiceStatistics(), IGNORE_DEFAULTS);
+    }
+
+    @Test
+    public void getServiceStatsReturnsRequestId() {
+        SearchServiceClient serviceClient = getSearchServiceClientBuilder().buildClient();
+
+        UUID expectedUuid = UUID.randomUUID();
+        RequestOptions requestOptions = new RequestOptions().setClientRequestId(expectedUuid);
+        Response<ServiceStatistics> response = serviceClient.getServiceStatisticsWithResponse(requestOptions, Context.NONE);
+
+        assertEquals(expectedUuid.toString(), response.getHeaders().getValue("client-request-id"));
+        assertReflectionEquals(response.getValue(), getExpectedServiceStatistics(), IGNORE_DEFAULTS);
     }
 }
