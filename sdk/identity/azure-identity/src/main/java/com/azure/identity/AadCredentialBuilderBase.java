526a4c73fcc997e2e0dbad10d7c403dab375a7c5
g2vinay <vinay_work@hotmail.com>
2020-03-09 10:37:30 -0700

    update javadoc

--- sdk/identity/azure-identity/src/main/java/com/azure/identity/AadCredentialBuilderBase.java
+++ sdk/identity/azure-identity/src/main/java/com/azure/identity/AadCredentialBuilderBase.java
@@ -50,12 +50,16 @@ public abstract class AadCredentialBuilderBase<T extends AadCredentialBuilderBas
     }
 
     /**
-     * Specifies the  ExecutorService to be used to execute the requests.
+     * Specifies the ExecutorService to be used to execute the authentication requests.
      * Developer is responsible for maintaining the lifecycle of the ExecutorService.
      *
-     * <p> The executor service is required whenever the application is looking to completely or
-     * exclusively consume the {@link ForkJoinPool#commonPool()}. The executor service
-     * and can be safely shutdown if the TokenCredential is no longer being used by the
+     * <p>
+     * If this is not configured, the {@link ForkJoinPool#commonPool()} will be used which is
+     * also shared with other application tasks. If the common pool is heavily used for other tasks, authentication
+     * requests might starve and setting up this executor service should be considered.
+     * </p>
+     *
+     * <p> The executor service and can be safely shutdown if the TokenCredential is no longer being used by the
      * Azure SDK clients and should be shutdown before the application exits. </p>
      *
      * @param executorService the executor service to use for executing authentication requests.
