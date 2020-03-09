d0036776f1113d9b04ef776acffcc4654d2d82b7
Sima Zhu <sizhu@microsoft.com>
2020-03-09 12:04:33 -0700

    Move to test helper

--- sdk/appconfiguration/azure-data-appconfiguration/src/test/java/com/azure/data/appconfiguration/AadCredentialTest.java
+++ sdk/appconfiguration/azure-data-appconfiguration/src/test/java/com/azure/data/appconfiguration/AadCredentialTest.java
@@ -74,22 +74,4 @@ public class AadCredentialTest extends TestBase {
         Assertions.assertEquals(addedSetting.getValue(), value);
     }
 
-    /**
-     * Returns a stream of arguments that includes all combinations of eligible {@link HttpClient HttpClients} and
-     * service versions that should be tested.
-     *
-     * @return A stream of HttpClient and service version combinations to test.
-     */
-    protected Stream<Arguments> getTestParameters() {
-        // when this issues is closed, the newer version of junit will have better support for
-        // cartesian product of arguments - https://github.com/junit-team/junit5/issues/1427
-        List<Arguments> argumentsList = new ArrayList<>();
-        getHttpClients()
-            .forEach(httpClient -> {
-                for (ConfigurationServiceVersion serviceVersion : ConfigurationServiceVersion.values()) {
-                    argumentsList.add(Arguments.of(httpClient, serviceVersion));
-                }
-            });
-        return argumentsList.stream();
-    }
 }
