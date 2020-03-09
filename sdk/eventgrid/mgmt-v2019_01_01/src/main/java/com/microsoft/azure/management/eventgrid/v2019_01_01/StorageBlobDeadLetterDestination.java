27a92e7142578b6cbc1de0af912a923118a1e23c
SDK Automation <sdkautomation@microsoft.com>
2020-03-09 02:35:39 +0000

    Generated from 99ff0d7b0e6a074c3d06bb6eddb1f7e805859e06
    fix merge conflict

--- sdk/eventgrid/mgmt-v2019_01_01/src/main/java/com/microsoft/azure/management/eventgrid/v2019_01_01/StorageBlobDeadLetterDestination.java
+++ sdk/eventgrid/mgmt-v2019_01_01/src/main/java/com/microsoft/azure/management/eventgrid/v2019_01_01/StorageBlobDeadLetterDestination.java
@@ -16,13 +16,13 @@ import com.microsoft.rest.serializer.JsonFlatten;
 /**
  * Information about the storage blob based dead letter destination.
  */
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "endpointType")
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "endpointType", defaultImpl = StorageBlobDeadLetterDestination.class)
 @JsonTypeName("StorageBlob")
 @JsonFlatten
 public class StorageBlobDeadLetterDestination extends DeadLetterDestination {
     /**
-     * The Azure Resource ID of the storage blob container that is the
-     * destination of the deadletter events. For example:
+     * The Azure Resource ID of the storage account that is the destination of
+     * the deadletter events. For example:
      * /subscriptions/{AzureSubscriptionId}/resourceGroups/{ResourceGroupName}/providers/microsoft.Storage/storageAccounts/{StorageAccountName}.
      */
     @JsonProperty(value = "properties.resourceId")
@@ -36,7 +36,7 @@ public class StorageBlobDeadLetterDestination extends DeadLetterDestination {
     private String blobContainerName;
 
     /**
-     * Get the Azure Resource ID of the storage blob container that is the destination of the deadletter events. For example: /subscriptions/{AzureSubscriptionId}/resourceGroups/{ResourceGroupName}/providers/microsoft.Storage/storageAccounts/{StorageAccountName}.
+     * Get the Azure Resource ID of the storage account that is the destination of the deadletter events. For example: /subscriptions/{AzureSubscriptionId}/resourceGroups/{ResourceGroupName}/providers/microsoft.Storage/storageAccounts/{StorageAccountName}.
      *
      * @return the resourceId value
      */
@@ -45,7 +45,7 @@ public class StorageBlobDeadLetterDestination extends DeadLetterDestination {
     }
 
     /**
-     * Set the Azure Resource ID of the storage blob container that is the destination of the deadletter events. For example: /subscriptions/{AzureSubscriptionId}/resourceGroups/{ResourceGroupName}/providers/microsoft.Storage/storageAccounts/{StorageAccountName}.
+     * Set the Azure Resource ID of the storage account that is the destination of the deadletter events. For example: /subscriptions/{AzureSubscriptionId}/resourceGroups/{ResourceGroupName}/providers/microsoft.Storage/storageAccounts/{StorageAccountName}.
      *
      * @param resourceId the resourceId value to set
      * @return the StorageBlobDeadLetterDestination object itself.
