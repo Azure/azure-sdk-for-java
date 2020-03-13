db18f6b33c2a3f7d1a057575cc8835419b146eee
Alan Zimmer <48699787+alzimmermsft@users.noreply.github.com>
2020-03-13 15:37:09 -0700

    Replace HttpBin Usage with WireMock (#8975)
    * Cleanup RestProxyTests
    
    * Prototype of WireMock server
    
    * Prototype with ResponseDefinitionTransformer
    
    * Stubbed and fixed most HttpBin tests
    
    * Removed RestProxyStressTests, MockServer, and Jetty dependencies
    
    * Fixing additional tests
    
    * Change server startup and shutdown guards
    
    * Disambiguate test from WireMock stub
    
    * Additional test fixes for Linux
    
    * Additional mocking changes in attempt to resolve Linux deadlocking issue
    
    * Use WireMock dynamic port, cleaned up transformer code, added API to get port to RestProxyTests
    
    * Fixed mock RestProxyTests
    
    * Fixed issue where Linux would deadlock on a test

--- sdk/core/azure-core-http-okhttp/src/test/java/com/azure/core/http/okhttp/implementation/RestProxyWithOkHttpTests.java
+++ sdk/core/azure-core-http-okhttp/src/test/java/com/azure/core/http/okhttp/implementation/RestProxyWithOkHttpTests.java
@@ -5,9 +5,32 @@ package com.azure.core.http.okhttp.implementation;
 
 import com.azure.core.http.HttpClient;
 import com.azure.core.http.okhttp.OkHttpAsyncHttpClientBuilder;
+import com.azure.core.test.RestProxyTestsWireMockServer;
 import com.azure.core.test.implementation.RestProxyTests;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 
 public class RestProxyWithOkHttpTests extends RestProxyTests {
+    private static WireMockServer server;
+
+    @BeforeAll
+    public static void getWireMockServer() {
+        server = RestProxyTestsWireMockServer.getRestProxyTestsServer();
+        server.start();
+    }
+
+    @AfterAll
+    public static void shutdownWireMockServer() {
+        if (server != null) {
+            server.shutdown();
+        }
+    }
+
+    @Override
+    protected int getWireMockPort() {
+        return server.port();
+    }
 
     @Override
     protected HttpClient createHttpClient() {
