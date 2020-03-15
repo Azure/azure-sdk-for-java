2d211e946afb3c850467dea0c6d063cb7a2db423
Anu Thomas Chandy <anuamd@hotmail.com>
2020-03-14 18:48:44 -0700

    Fix: CN_IDIOM_NO_SUPER_CALL

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/DocumentServiceRequestContext.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/DocumentServiceRequestContext.java
@@ -14,7 +14,7 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DocumentServiceRequestContext implements Cloneable{
+public class DocumentServiceRequestContext implements Cloneable {
     public volatile boolean forceAddressRefresh;
     public volatile boolean forceRefreshAddressCache;
     public volatile RequestChargeTracker requestChargeTracker;
