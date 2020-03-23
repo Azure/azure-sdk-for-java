73a8f4cad55cd9816ead6c486bb7ba0b3c23b011
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2020-03-23 15:15:04 -0700

    Update test name printing format

--- sdk/core/azure-core/src/test/java/com/azure/core/TestNamePrinter.java
+++ sdk/core/azure-core/src/test/java/com/azure/core/TestNamePrinter.java
@@ -6,13 +6,35 @@ package com.azure.core;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInfo;
 
+import java.util.Objects;
+
 /**
  * This class is a simple test class super class that will print out the name of the currently running test. This is
  * for test classes that don't extend 'TestBaseClass'.
  */
 public class TestNamePrinter {
+    /**
+     * Prints the name of the currently running test. This will print the fully qualified test name, if the test is a
+     * parameterized test the display name will be printed in parentheses next to the fully qualified name.
+     *
+     * @param testInfo Information about the currently running test.
+     */
     @BeforeEach
     public void printTestName(TestInfo testInfo) {
-        System.out.printf("========================= %s =========================%n", testInfo.getDisplayName());
+        String displayName = testInfo.getDisplayName();
+
+        String testName = "";
+        String fullyQualifiedTestName = "";
+        if (testInfo.getTestMethod().isPresent()) {
+            testName = testInfo.getTestMethod().get().getName();
+            fullyQualifiedTestName = testInfo.getTestMethod().get().getDeclaringClass().getName() + "." + testName;
+        }
+
+        if (!Objects.equals(displayName, testName)) {
+            System.out.printf("========================= %s (%s) =========================%n", fullyQualifiedTestName,
+                displayName);
+        } else {
+            System.out.printf("========================= %s =========================%n", fullyQualifiedTestName);
+        }
     }
 }
