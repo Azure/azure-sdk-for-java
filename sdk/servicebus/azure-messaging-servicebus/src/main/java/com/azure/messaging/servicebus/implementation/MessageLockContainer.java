cebad66bcae6973d265a7dc681f342c3d572385b
Connie <conniey@microsoft.com>
2020-03-24 08:25:02 -0700

    Updates documentation.

--- sdk/servicebus/azure-messaging-servicebus/src/main/java/com/azure/messaging/servicebus/implementation/MessageLockContainer.java
+++ sdk/servicebus/azure-messaging-servicebus/src/main/java/com/azure/messaging/servicebus/implementation/MessageLockContainer.java
@@ -11,20 +11,18 @@ import java.util.concurrent.ConcurrentHashMap;
  * Container of Message lock and related metadata related.
  */
 public class MessageLockContainer {
-    private final ConcurrentHashMap<UUID, Instant> lockTokenExpirationMap;
+    private final ConcurrentHashMap<UUID, Instant> lockTokenExpirationMap = new ConcurrentHashMap<>();
 
-    public MessageLockContainer() {
-        lockTokenExpirationMap = new ConcurrentHashMap<>();
-    }
-
-    public Instant getLockTokenExpiration(UUID lockToken) {
-        return lockTokenExpirationMap.get(lockToken);
-    }
-
-    public void remove(UUID lockToken) {
-        lockTokenExpirationMap.remove(lockToken);
-    }
-
+    /**
+     * Adds or updates the expiration time on a lock token. If the expiration time in the container is larger than
+     * {@code lockTokenExpiration}, then the current container value is used.
+     *
+     * @param lockToken Token to update associated lock expiration.
+     * @param lockTokenExpiration Time at which the lock token expires.
+     *
+     * @return The updated value in the container. If the expiration time in the container is larger than
+     *     {@code lockTokenExpiration}, then the current container value is used.
+     */
     public Instant addOrUpdate(UUID lockToken, Instant lockTokenExpiration) {
         return lockTokenExpirationMap.compute(lockToken, (key, existing) -> {
             if (existing == null) {
@@ -35,5 +33,25 @@ public class MessageLockContainer {
                     : existing;
             }
         });
+    }
+
+    /**
+     * Gets the lock expiration given a lock token.
+     *
+     * @param lockToken Token to get associated lock expiration for.
+     *
+     * @return An {@link Instant} for when the lock expires or {@code null} if the {@code lockToken} does not exist.
+     */
+    public Instant getLockTokenExpiration(UUID lockToken) {
+        return lockTokenExpirationMap.get(lockToken);
+    }
+
+    /**
+     * Removes the lock token from the map.
+     *
+     * @param lockToken Token to remove.
+     */
+    public void remove(UUID lockToken) {
+        lockTokenExpirationMap.remove(lockToken);
     }
 }
