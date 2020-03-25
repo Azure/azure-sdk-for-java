00a05a646b789eae5ee1e785a607fd7bea1b6f91
Sima Zhu <sizhu@microsoft.com>
2020-03-25 07:37:27 -0700

    Regen code

--- sdk/search/azure-search/src/main/java/com/azure/search/documents/implementation/package-info.java
+++ sdk/search/azure-search/src/main/java/com/azure/search/documents/implementation/package-info.java
@@ -5,8 +5,9 @@
 // regenerated.
 
 /**
- * Package containing the data models for SearchIndexRestClient.
- * Client that can be used to query an index and upload, merge, or delete
- * documents.
+ * Package containing the implementations and inner classes for
+ * SearchServiceRestClient.
+ * Client that can be used to manage and query indexes and documents, as well
+ * as manage other resources, on a search service.
  */
 package com.azure.search.documents.implementation;
