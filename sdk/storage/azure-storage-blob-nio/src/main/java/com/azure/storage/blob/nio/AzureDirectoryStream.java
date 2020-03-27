3514dfc88f9c6d12eb5eeacf13a478f0463299e1
Rick Ley <frley@microsoft.com>
2020-03-27 16:28:52 -0700

    Added lots of tests

--- sdk/storage/azure-storage-blob-nio/src/main/java/com/azure/storage/blob/nio/AzureDirectoryStream.java
+++ sdk/storage/azure-storage-blob-nio/src/main/java/com/azure/storage/blob/nio/AzureDirectoryStream.java
@@ -5,13 +5,17 @@ package com.azure.storage.blob.nio;
 
 import com.azure.core.util.logging.ClientLogger;
 import com.azure.storage.blob.models.BlobItem;
+import com.azure.storage.blob.models.BlobListDetails;
+import com.azure.storage.blob.models.ListBlobsOptions;
 
 import java.io.IOException;
 import java.nio.file.DirectoryIteratorException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Path;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+import java.util.Set;
 
 /**
  * Is asynchronously closeable. Closing the stream from any thread will cause the stream to stop returning elements.
@@ -21,13 +25,13 @@ import java.util.NoSuchElementException;
 public class AzureDirectoryStream implements DirectoryStream<Path> {
     private final ClientLogger logger = new ClientLogger(AzureDirectoryStream.class);
 
-    private final AzureResource azureResource;
+    private final Path path;
     private final DirectoryStream.Filter<? super Path> filter;
     private boolean iteratorRequested = false;
     boolean closed = false;
 
-    AzureDirectoryStream(Path path, DirectoryStream.Filter<? super Path> filter) throws IOException {
-        this.azureResource = new AzureResource(path);
+    AzureDirectoryStream(Path path, DirectoryStream.Filter<? super Path> filter) {
+        this.path = path;
         this.filter = filter;
     }
 
@@ -38,7 +42,7 @@ public class AzureDirectoryStream implements DirectoryStream<Path> {
                 new IllegalStateException("Only one iterator may be requested from a given directory stream"));
         }
         this.iteratorRequested = true;
-        return new AzureDirectoryIterator(this, this.azureResource, this.filter);
+        return new AzureDirectoryIterator(this, this.path, this.filter);
     }
 
     @Override
@@ -52,17 +56,26 @@ public class AzureDirectoryStream implements DirectoryStream<Path> {
         private final AzureDirectoryStream parentStream;
         private final DirectoryStream.Filter<? super Path> filter;
         private final Iterator<BlobItem> blobIterator;
-        private final AzureResource azureResource;
+        private final Path path;
         private Path bufferedNext = null;
+        private final Set<String> directoryPaths;
 
-        AzureDirectoryIterator(AzureDirectoryStream parentStream, AzureResource resource,
-            DirectoryStream.Filter<? super Path> filter)  {
+        AzureDirectoryIterator(AzureDirectoryStream parentStream, Path path,
+            DirectoryStream.Filter<? super Path> filter) throws IOException {
             this.parentStream = parentStream;
             this.filter = filter;
-            this.azureResource = resource;
-            this.blobIterator = resource.getContainerClient()
-                .listBlobsByHierarchy(resource.getBlobClient().getBlobName() + AzureFileSystem.PATH_SEPARATOR)
-                .iterator();
+            this.path = path;
+            directoryPaths = new HashSet<>();
+            if (path.isRoot()) {
+                this.blobIterator = resource.getContainerClient().listBlobs().iterator();
+            } else {
+                AzureResource azureResource = new AzureResource(path);
+                ListBlobsOptions listOptions = new ListBlobsOptions()
+                    .setPrefix(azureResource.getBlobClient().getBlobName() + AzureFileSystem.PATH_SEPARATOR)
+                    .setDetails(new BlobListDetails().setRetrieveMetadata(true));
+                this.blobIterator = azureResource.getContainerClient()
+                    .listBlobsByHierarchy(AzureFileSystem.PATH_SEPARATOR, listOptions, null).iterator();
+            }
         }
 
         @Override
@@ -81,10 +94,11 @@ public class AzureDirectoryStream implements DirectoryStream<Path> {
             Search for a new element that passes the filter and buffer it when found. If no such element is found,
             return false.
              */
-            while (blobIterator.hasNext()) {
-                Path nextPath = getNextListResult();
+            while (this.blobIterator.hasNext()) {
+                BlobItem nextBlob = this.blobIterator.next();
+                Path nextPath = getNextListResult(nextBlob);
                 try {
-                    if (filter.accept(nextPath)) {
+                    if (this.filter.accept(nextPath) && passesDirectoryDuplicateFilter(nextPath, nextBlob)) {
                         this.bufferedNext = nextPath;
                         return true;
                     }
@@ -112,23 +126,48 @@ public class AzureDirectoryStream implements DirectoryStream<Path> {
             throw new UnsupportedOperationException();
         }
 
-        private Path getNextListResult() {
+        private Path getNextListResult(BlobItem blobItem) {
             // Strip the root if it is present so we can relativize the list result, which never has a root.
-            Path withoutRoot = azureResource.getPath();
+            Path withoutRoot = this.path;
             if (withoutRoot.isAbsolute()) {
-                withoutRoot = azureResource.getPath().getRoot().relativize(azureResource.getPath());
+                withoutRoot = this.path.getRoot().relativize(this.path);
             }
 
             /*
             Listing results return the full blob path, and we don't want to duplicate the path we listed off of, so
             we relativize to remove it.
              */
-            String blobName = this.blobIterator.next().getName();
+            String blobName = blobItem.getName();
             Path relativeResult = withoutRoot.relativize(
-                this.azureResource.getPath().getFileSystem().getPath(blobName));
+                this.path.getFileSystem().getPath(blobName));
 
             // Resolve the cleaned list result against the original path for the final result.
-            return azureResource.getPath().resolve(relativeResult);
+            return this.path.resolve(relativeResult);
+        }
+
+        /*
+        If there is a concrete directory with children, a given path will be returned twice: once as the marker blob
+        and once as the prefix for its children. We don't want to return the item twice, and we have no guarantees on
+        result ordering, so we have to maintain a cache of directory paths we've seen in order to de-dup.
+         */
+        private boolean passesDirectoryDuplicateFilter(Path path, BlobItem blob) {
+            /*
+            If the blob is not a prefix and the blob does not contain the directory metadata marker, it is a normal blob
+            and therefore will not be duplicated.
+             */
+            if (!(blob.isPrefix() != null && blob.isPrefix())
+                && !(blob.getMetadata() != null && blob.getMetadata().containsKey(AzureResource.DIR_METADATA_MARKER))) {
+                return true;
+            }
+
+            // If the set contains this path, it means we've seen it before and we shouldn't return it again.
+            if (this.directoryPaths.contains(path.toString())) {
+                return false;
+            }
+
+            // We haven't seen this before. Track it and indicate it should be returned.
+            this.directoryPaths.add(path.toString());
+            return true;
         }
     }
 }
