fd3bc7be72b828d8762305a1cb15977daf297d2a
g2vinay <vinay_work@hotmail.com>
2020-03-28 19:37:35 -0700

    add hosts:

---
+++ sdk/identity/azure-identity/src/main/java/com/azure/identity/KnownAuthorityHosts.java
@@ -0,0 +1,27 @@
+package com.azure.identity;
+
+/**
+ * Defines fields exposing the well known authority hosts for the Azure Public Cloud and sovereign clouds.
+ */
+public class KnownAuthorityHosts {
+
+    /**
+     * The host of the Azure Active Directory authority for tenants in the Azure Public Cloud.
+     */
+    public static final String AZURE_CLOUD = "https://login.microsoftonline.com/";
+
+    /**
+     * The host of the Azure Active Directory authority for tenants in the Azure China Cloud.
+     */
+    public static final String AZURE_CHINA_CLOUD = "https://login.chinacloudapi.cn/";
+
+    /**
+     * The host of the Azure Active Directory authority for tenants in the Azure German Cloud.
+     */
+    public static final String AZURE_GERMAN_CLOUD = "https://login.microsoftonline.de/";
+
+    /**
+     * The host of the Azure Active Directory authority for tenants in the Azure US Government Cloud.
+     */
+    public static final String AZURE_US_GOVERNMENT = "https://login.microsoftonline.us/";
+}
