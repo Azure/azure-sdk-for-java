76690a6ed16d7d54b3d2c92c246a13da8e31983c
Gauri Prasad <51212198+gapra-msft@users.noreply.github.com>
2020-04-03 15:22:56 -0700

    Added base avro parser package (#9806)

---
+++ sdk/storage/azure-storage-internal-avro/src/main/java/com/azure/storage/internal/avro/package-info.java
@@ -0,0 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+/**
+ * Internal Avro base package.
+ */
+package com.azure.storage.internal.avro;
---
+++ sdk/storage/azure-storage-internal-avro/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+module com.azure.storage.internal.avro {
+}
