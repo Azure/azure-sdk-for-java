a765c87be1bd85c1c9baff4907089036101077d3
Sima Zhu <sizhu@microsoft.com>
2020-04-06 09:14:53 -0700

    Print out the version and httpclient

--- sdk/core/azure-core-test/src/main/java/com/azure/core/test/TestBase.java
+++ sdk/core/azure-core-test/src/main/java/com/azure/core/test/TestBase.java
@@ -196,7 +196,7 @@ public abstract class TestBase implements BeforeEachCallback {
      * @return A list of {@link HttpClient HttpClients} to be tested.
      */
     public static List<HttpClient> getHttpClients() {
-        if (testMode == TestMode.PLAYBACK) {
+        if (testMode != TestMode.PLAYBACK) {
             // Call to @MethodSource method happens @BeforeEach call, so the interceptorManager is
             // not yet initialized. So, playbackClient will not be available until later.
             return Arrays.asList(new HttpClient[]{null});
