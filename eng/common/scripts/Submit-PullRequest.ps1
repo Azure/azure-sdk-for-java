4ec023762a30266a341c704fad45cb73ac78c75a
Chidozie Ononiwu <chononiw@microsoft.com>
2020-04-06 19:43:37 -0700

    Revert Submit-PullRequest.ps1 script

--- eng/common/scripts/Submit-PullRequest.ps1
+++ eng/common/scripts/Submit-PullRequest.ps1
@@ -1,4 +1,4 @@
-#!/usr/bin/env pwsh -c
+ #!/usr/bin/env pwsh -c
 
 <#
 .DESCRIPTION
@@ -41,27 +41,18 @@ param(
   $PRBody = $PRTitle
 )
 
-Write-Host "Got Here 1"
-
 $query = "state=open&head=${PROwner}:${PRBranch}&base=${BaseBranch}"
-
-Write-Host "Got Here 2"
 
 $resp = Invoke-RestMethod "https://api.github.com/repos/$RepoOwner/$RepoName/pulls?$query"
 $resp | Write-Verbose
 
-Write-Host "Got Here 3"
-
-if ($resp.Count -gt 0)
-{
-  Write-Host -f green "Pull request already exists $($resp[0].html_url)"
-} else
-{
-  Write-Host "Got Here 4"
+if ($resp.Count -gt 0) {
+    Write-Host -f green "Pull request already exists $($resp[0].html_url)"
+}
+else {
   $headers = @{
     Authorization = "bearer $AuthToken"
   }
-  Write-Host "Got Here 5"
 
   $data = @{
     title                 = $PRTitle
@@ -70,13 +61,10 @@ if ($resp.Count -gt 0)
     body                  = $PRBody
     maintainer_can_modify = $true
   }
-  Write-Host "Got Here 6"
 
   $resp = Invoke-RestMethod -Method POST -Headers $headers `
-    https://api.github.com/repos/$RepoOwner/$RepoName/pulls `
-    -Body ($data | ConvertTo-Json)
-  Write-Host "Got Here 7"
+                            https://api.github.com/repos/$RepoOwner/$RepoName/pulls `
+                            -Body ($data | ConvertTo-Json)
   $resp | Write-Verbose
-  Write-Host "Got Here 8"
   Write-Host -f green "Pull request created https://github.com/$RepoOwner/$RepoName/pull/$($resp.number)"
 }
