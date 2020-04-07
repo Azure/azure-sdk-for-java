461c1e166325810bce11f3a3f2032399e91930d5
xccc-msft <chuxi@microsoft.com>
2020-04-07 13:27:15 +0800

    Update javadoc for ETagState

--- sdk/resources/mgmt/src/main/java/com/azure/management/resources/fluentcore/utils/ETagState.java
+++ sdk/resources/mgmt/src/main/java/com/azure/management/resources/fluentcore/utils/ETagState.java
@@ -3,12 +3,20 @@
 
 package com.azure.management.resources.fluentcore.utils;
 
+/**
+ * Utility class for ETag state handling.
+ */
 public class ETagState {
     private boolean doImplicitETagCheckOnCreate;
     private boolean doImplicitETagCheckOnUpdate;
     private String eTagOnUpdate;
     private String eTagOnDelete;
 
+    /**
+     * Specifies if it is in create or update with implicit ETag check.
+     * @param isInCreateMode The boolean flag to indicate if it is in create mode.
+     * @return the ETag state.
+     */
     public ETagState withImplicitETagCheckOnCreateOrUpdate(boolean isInCreateMode) {
         if (isInCreateMode) {
             this.doImplicitETagCheckOnCreate = true;
@@ -18,17 +26,31 @@ public class ETagState {
         return this;
     }
 
+    /**
+     * Specifies the explicit ETag value on update.
+     * @param eTagValue The explicit ETag value.
+     * @return the ETag state.
+     */
     public ETagState withExplicitETagCheckOnUpdate(String eTagValue) {
         this.eTagOnUpdate = eTagValue;
         return this;
     }
 
+    /**
+     * Specifies the explicit ETag value on delete.
+     * @param eTagValue The explicit ETag value.
+     * @return the ETag state.
+     */
     public ETagState withExplicitETagCheckOnDelete(String eTagValue) {
         this.eTagOnDelete = eTagValue;
         return this;
     }
 
 
+    /**
+     * Clears the stored values in ETag state
+     * @return the ETag state.
+     */
     public ETagState clear() {
         this.doImplicitETagCheckOnCreate = false;
         this.doImplicitETagCheckOnUpdate = false;
@@ -37,6 +59,11 @@ public class ETagState {
         return this;
     }
 
+    /**
+     * Specifies the ETag value for If-Match header on update.
+     * @param currentETagValue The current ETag value.
+     * @return the ETag value.
+     */
     public String ifMatchValueOnUpdate(String currentETagValue) {
         String eTagValue = null;
         if (this.doImplicitETagCheckOnUpdate) {
@@ -48,10 +75,18 @@ public class ETagState {
         return eTagValue;
     }
 
+    /**
+     * Specifies the ETag value for If-Match header on delete.
+     * @return the ETag value.
+     */
     public String ifMatchValueOnDelete() {
         return this.eTagOnDelete;
     }
 
+    /**
+     * Specifies the ETag value for If-None-Match header on create.
+     * @return the ETag value.
+     */
     public String ifNonMatchValueOnCreate() {
         if (this.doImplicitETagCheckOnCreate) {
             return "*";
