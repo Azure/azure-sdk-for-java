eb816af74670b7c8a48c36335b724b73ddcb3de3
Hemant Tanwar <hetanwar@users.noreply.github.com>
2020-04-09 16:55:29 -0700

    added sample

--- sdk/servicebus/azure-messaging-servicebus/src/samples/java/com/azure/messaging/servicebus/ServiceBusMultiSessionProcessorSample.java
+++ sdk/servicebus/azure-messaging-servicebus/src/samples/java/com/azure/messaging/servicebus/ServiceBusMultiSessionProcessorSample.java
@@ -1,16 +1,24 @@
 package com.azure.messaging.servicebus;
 
 import com.azure.messaging.servicebus.models.ReceiveMode;
+import com.azure.messaging.servicebus.models.ServiceBusErrorContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.time.Duration;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ServiceBusMultiSessionProcessorSample {
 
     public static void main(String[] args) throws Exception {
-
+        final MessageProcessor processor = new MessageProcessor();
         ServiceBusMultiSessionProcessorClient multiSessionProcessorClient = new ServiceBusClientBuilder()
             .connectionString("connectionString")
             .receiverMultiSession()
+            .processMessage((receivedMessage, sessionManager) -> processor.onMessage(receivedMessage, sessionManager))
             .receiveMode(ReceiveMode.PEEK_LOCK)
             .queueName("<<queue-name>>")
             .buildMultiSessionProcessorClient();
@@ -23,5 +31,39 @@ public class ServiceBusMultiSessionProcessorSample {
         multiSessionProcessorClient.stop();
 
         System.out.println("Exiting process");
+    }
+    static class MessageProcessor implements AutoCloseable {
+        private final Logger logger = LoggerFactory.getLogger(MessageProcessor.class);
+        private final AtomicBoolean isDisposed = new AtomicBoolean();
+
+        private final ConcurrentHashMap<String, Set<String>> partitionsProcessing = new ConcurrentHashMap<>();
+
+        void onMessage(ServiceBusReceivedMessage message, SessionManager manager) {
+
+        }
+
+
+        /**
+         * When an occurs, reports that error to a log.
+         *
+         * @param errorContext Error that occurred while processing events.
+         */
+        void onError(ServiceBusErrorContext errorContext) {
+
+        }
+
+
+
+        /**
+         * Disposes of all the machine information and closes the processor.
+         */
+        @Override
+        public void close() {
+            if (isDisposed.getAndSet(true)) {
+                return;
+            }
+
+
+        }
     }
 }
