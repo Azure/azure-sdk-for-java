8032017f51b7197092d2fa1866ecfcc2e1958b41
Hemant Tanwar <hetanwar@users.noreply.github.com>
2020-04-09 15:29:31 -0700

    initial proposal for processor model for multi session

--- sdk/servicebus/azure-messaging-servicebus/src/main/java/com/azure/messaging/servicebus/ServiceBusMultiSessionProcessorClient.java
+++ sdk/servicebus/azure-messaging-servicebus/src/main/java/com/azure/messaging/servicebus/ServiceBusMultiSessionProcessorClient.java
@@ -6,8 +6,7 @@ package com.azure.messaging.servicebus;
 public class ServiceBusMultiSessionProcessorClient {
     public void start(){}
     public void stop(){}
-    public  boolean isRunning() {
-        return true;
+    public  boolean isRunning() { return true;
     }
 }
 
