721651bb013c49e82150bf47ca09b429ddf697e5
Sima Zhu <sizhu@microsoft.com>
2020-04-15 10:05:42 -0700

    regen the code

--- sdk/search/azure-search-documents/src/main/java/com/azure/search/documents/models/Field.java
+++ sdk/search/azure-search-documents/src/main/java/com/azure/search/documents/models/Field.java
@@ -227,7 +227,7 @@ public final class Field {
 
     /*
      * A value indicating whether the field will be returned in a search
-     * result. This property must be true for key fields, and must be null for
+     * result. This property must be false for key fields, and must be null for
      * complex fields. You can hide a field from search results if you want to
      * use it only as a filter, for sorting, or for scoring. This property can
      * also be changed on existing fields and enabling it does not cause an
@@ -788,7 +788,7 @@ public final class Field {
 
     /**
      * Set the hidden property: A value indicating whether the field will be
-     * returned in a search result. This property must be true for key fields,
+     * returned in a search result. This property must be false for key fields,
      * and must be null for complex fields. You can hide a field from search
      * results if you want to use it only as a filter, for sorting, or for
      * scoring. This property can also be changed on existing fields and
