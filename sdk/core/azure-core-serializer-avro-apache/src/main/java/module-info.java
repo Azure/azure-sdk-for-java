a6b654d3284cf72e673a2db4f1ae5a5a92ce2981
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2020-04-14 17:02:56 -0700

    Removed Avro interface and implementations

--- sdk/core/azure-core-serializer-avro-apache/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-module com.azure.core.serializer.avro.apache {
-    requires transitive com.azure.core;
-    requires transitive org.apache.avro;
-}
--- sdk/core/azure-core-serializer-avro-jackson/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-module com.azure.core.serializer.avro.jackson {
-    requires transitive com.azure.core;
-    requires transitive com.fasterxml.jackson.dataformat.avro;
-}
