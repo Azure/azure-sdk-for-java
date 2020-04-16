675adffbea33d938001e618734cd679a0dcfae0b
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2020-04-15 17:44:34 -0700

    Update test casing expectation

--- sdk/core/azure-core/src/test/java/com/azure/core/http/rest/PercentEscaperTests.java
+++ sdk/core/azure-core/src/test/java/com/azure/core/http/rest/PercentEscaperTests.java
@@ -15,8 +15,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * Tests {@link PercentEscaper}.
  */
 public class PercentEscaperTests {
-    String japaneseCharacters = "日本語";
-
     @ParameterizedTest
     @MethodSource("escapeSupplier")
     public void escape(PercentEscaper escaper, String original, String expected) {
@@ -32,7 +30,9 @@ public class PercentEscaperTests {
             Arguments.arguments(defaultEscaper, "ह", "%E0%A4%B9"),
             Arguments.arguments(defaultEscaper, "€", "%E2%82%AC"),
             Arguments.arguments(defaultEscaper, "한", "%ED%95%9C"),
+            Arguments.arguments(defaultEscaper, "円", "%E5%86%86"),
             Arguments.arguments(defaultEscaper, "\uD800\uDF48", "%F0%90%8D%88"),
+            Arguments.arguments(defaultEscaper, "日本語", "%E6%97%A5%E6%9C%AC%E8%AA%9E"),
             Arguments.arguments(defaultEscaper, " ", "%20"),
             Arguments.arguments(new PercentEscaper(null, true), " ", "+"),
             Arguments.arguments(new PercentEscaper("$", false), "$", "$")
