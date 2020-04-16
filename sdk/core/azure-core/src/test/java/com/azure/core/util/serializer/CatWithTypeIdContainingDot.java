14715214f0bf97316d3bf536047be8202200fb57
Anu Thomas Chandy <anuamd@hotmail.com>
2020-04-16 13:57:40 -0700

    Adding more code coverage to flattening and typeId scenarios.

---
+++ sdk/core/azure-core/src/test/java/com/azure/core/util/serializer/CatWithTypeIdContainingDot.java
@@ -0,0 +1,27 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.core.util.serializer;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME,
+    include = JsonTypeInfo.As.PROPERTY,
+    property = "@odata\\.type",
+    defaultImpl = CatWithTypeIdContainingDot.class)
+@JsonTypeName("#Favourite.Pet.CatWithTypeIdContainingDot")
+public class CatWithTypeIdContainingDot extends AnimalWithTypeIdContainingDot {
+    @JsonProperty(value = "breed", required = true)
+    private String breed;
+
+    public String breed() {
+        return this.breed;
+    }
+
+    public CatWithTypeIdContainingDot withBreed(String presetName) {
+        this.breed = presetName;
+        return this;
+    }
+}
