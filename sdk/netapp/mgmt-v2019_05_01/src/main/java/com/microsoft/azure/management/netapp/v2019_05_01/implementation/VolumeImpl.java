96aa7e3578d8728a8ceb561f81e18298589c0568
SDK Automation <sdkautomation@microsoft.com>
2020-04-17 12:33:02 +0000

    Generated from a5c7df9961238af61a400aeeb35fe1042e2b5b4f
    Added missing properties to mountTargetProperties for 2019-11-01

--- sdk/netapp/mgmt-v2019_05_01/src/main/java/com/microsoft/azure/management/netapp/v2019_05_01/implementation/VolumeImpl.java
+++ sdk/netapp/mgmt-v2019_05_01/src/main/java/com/microsoft/azure/management/netapp/v2019_05_01/implementation/VolumeImpl.java
@@ -15,7 +15,7 @@ import com.microsoft.azure.management.netapp.v2019_05_01.VolumePatch;
 import com.microsoft.azure.management.netapp.v2019_05_01.ServiceLevel;
 import com.microsoft.azure.management.netapp.v2019_05_01.VolumePropertiesExportPolicy;
 import java.util.List;
-import com.microsoft.azure.management.netapp.v2019_05_01.MountTargetList;
+import com.microsoft.azure.management.netapp.v2019_05_01.MountTargetProperties;
 import java.util.Map;
 import com.microsoft.azure.management.netapp.v2019_05_01.VolumePatchPropertiesExportPolicy;
 import rx.functions.Func1;
@@ -130,7 +130,7 @@ class VolumeImpl extends CreatableUpdatableImpl<Volume, VolumeInner, VolumeImpl>
     }
 
     @Override
-    public List<MountTargetList> mountTargets() {
+    public List<MountTargetProperties> mountTargets() {
         return this.inner().mountTargets();
     }
 
@@ -218,7 +218,7 @@ class VolumeImpl extends CreatableUpdatableImpl<Volume, VolumeInner, VolumeImpl>
     }
 
     @Override
-    public VolumeImpl withMountTargets(List<MountTargetList> mountTargets) {
+    public VolumeImpl withMountTargets(List<MountTargetProperties> mountTargets) {
         this.inner().withMountTargets(mountTargets);
         return this;
     }
