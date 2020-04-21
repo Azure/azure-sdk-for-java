ca5261d8df4ab45a86b36cc2f4046a09a76044b5
Sameeksha Vaity <savaity@microsoft.com>
2020-04-20 22:30:00 -0700

    Update Form Recognizer Readme and samples (#10218)

--- sdk/formrecognizer/azure-ai-formrecognizer/src/samples/README.md
+++ sdk/formrecognizer/azure-ai-formrecognizer/src/samples/README.md
@@ -19,30 +19,64 @@ Key concepts are explained in detail [here][SDK_README_KEY_CONCEPTS].
 Getting started explained in detail [here][SDK_README_GETTING_STARTED].
 
 ## Examples
-The following sections provide several code snippets covering some of the most common configuration service tasks, including:
-
-- [Extract receipt information - InputStream][extract_receipt_sync]
-- [Extract receipt information - SourceUrl][extract_receipt_async]
+The following sections provide code samples covering common scenario operations with the Azure Form Recognizer client library.
+- Recognize data from a file of a US sales receipt using a prebuilt model
+  - [RecognizeReceipts][recognize_receipts] and [RecognizeReceiptsAsync][recognize_receipts_async]
+- Recognize data from a URL of a US sales receipt using a prebuilt model
+  - [RecognizeReceiptsFromUrl][recognize_receipts_from_url] and [RecognizeReceiptsFromUrlAsync][recognize_receipts_from_url_async]
+- Recognize text and table structures of a document
+  - [RecognizeContent][recognize_content] and [RecognizeContentAsync][recognize_content_async]
+- Recognize forms with your custom model
+  - [RecognizeCustomForms][recognize_custom_forms] and [RecognizeCustomFormsAsync][recognize_custom_forms_async]
+- Train a custom model with labeled data
+  - [TrainLabeledModel][train_labeled_model] and [TrainLabeledModelAsync][train_labeled_model_async]
+- Train a custom model with unlabeled data
+  - [TrainUnlabeledModel][train_unlabeled_model] and [TrainUnlabeledModelAsync][train_unlabeled_model_async]
+- Manage the custom models in your account
+  - [ManageCustomModels][manage_custom_models] and [ManageCustomModelsAsync][manage_custom_models_async]
 
 ## Troubleshooting
 Troubleshooting steps can be found [here][SDK_README_TROUBLESHOOTING].
 
 ## Next steps
-See [Next steps][SDK_README_NEXT_STEPS]. 
+Check out the [API reference documentation][java_fr_ref_docs] to learn more about
+what you can do with the Azure Form Recognizer client library.
+Following section provides code samples for some of the advanced scenarios in Form Recognizer Client library:
+- Get info to visualize the outlines of form content and fields, which can be used for manual validation
+  - [GetBoundingBoxes][get_bounding_boxes] and [GetBoundingBoxesAsync][get_bounding_boxes_async]
+- See the differences in output when using a custom model trained with labeled data and one trained with unlabeled data
+  - [AdvancedDiffCustomFormsLabeledUnlabeledData][differentiate_custom_forms_with_labeled_and_unlabeled_models] and [AdvancedDiffCustomFormsLabeledUnlabeledDataAsync][differentiate_custom_forms_with_labeled_and_unlabeled_models_async]
 
 ## Contributing
 If you would like to become an active contributor to this project please refer to our [Contribution
-Guidelines](../../CONTRIBUTING.md) for more information.
+Guidelines][SDK_README_CONTRIBUTING] for more information.
 
 <!-- LINKS -->
-[KEYS_SDK_README]: ../../README.md
 [SDK_README_CONTRIBUTING]: ../../README.md#contributing
 [SDK_README_GETTING_STARTED]: ../../README.md#getting-started
 [SDK_README_TROUBLESHOOTING]: ../../README.md#troubleshooting
 [SDK_README_KEY_CONCEPTS]: ../../README.md#key-concepts
-[SDK_README_DEPENDENCY]: ../../README.md#adding-the-package-to-your-product
+[SDK_README_DEPENDENCY]: ../../README.md#include-the-package
 [SDK_README_NEXT_STEPS]: ../../README.md#next-steps
+[java_fr_ref_docs]: https://aka.ms/azsdk-java-formrecognizer-ref-docs
 
-[extract_receipt_sync]: java/com/azure/ai/formrecognizer/ExtractPrebuiltReceiptSync.java
-[extract_receipt_async]: java/com/azure/ai/formrecognizer/ExtractPrebuiltReceiptAsync.java
+[differentiate_custom_forms_with_labeled_and_unlabeled_models]: java/com/azure/ai/formrecognizer/AdvancedDiffLabeledUnlabeledData.java
+[differentiate_custom_forms_with_labeled_and_unlabeled_models_async]: java/com/azure/ai/formrecognizer/AdvancedDiffLabeledUnlabeledDataAsync.java
+[get_bounding_boxes]: java/com/azure/ai/formrecognizer/GetBoundingBoxes.java
+[get_bounding_boxes_async]: java/com/azure/ai/formrecognizer/GetBoundingBoxesAsync.java
+[manage_custom_models]: java/com/azure/ai/formrecognizer/ManageCustomModels.java
+[manage_custom_models_async]: java/com/azure/ai/formrecognizer/ManageCustomModelsAsync.java
+[recognize_receipts]: java/com/azure/ai/formrecognizer/RecognizeReceipts.java
+[recognize_receipts_async]: java/com/azure/ai/formrecognizer/RecognizeReceiptsAsync.java
+[recognize_receipts_from_url]: java/com/azure/ai/formrecognizer/RecognizeReceiptsFromUrl.java
+[recognize_receipts_from_url_async]: java/com/azure/ai/formrecognizer/RecognizeReceiptsFromUrlAsync.java
+[recognize_content]: java/com/azure/ai/formrecognizer/RecognizeContent.java
+[recognize_content_async]: java/com/azure/ai/formrecognizer/RecognizeContentAsync.java
+[recognize_custom_forms]: java/com/azure/ai/formrecognizer/RecognizeCustomForms.java
+[recognize_custom_forms_async]: java/com/azure/ai/formrecognizer/RecognizeCustomFormsAsync.java
+[train_unlabeled_model]: java/com/azure/ai/formrecognizer/TrainUnlabeledModel.java
+[train_unlabeled_model_async]: java/com/azure/ai/formrecognizer/TrainUnlabeledModelAsync.java
+[train_labeled_model]: java/com/azure/ai/formrecognizer/TrainLabeledModel.java
+[train_labeled_model_async]: java/com/azure/ai/formrecognizer/TrainLabeledModelAsync.java
+
 ![Impressions](https://azure-sdk-impressions.azurewebsites.net/api/impressions/azure-sdk-for-java%2Fsdk%2Fformrecognizer%2Fazure-ai-formrecognizer%2FREADME.png)
--- sdk/formrecognizer/azure-ai-formrecognizer/src/samples/java/com/azure/ai/formrecognizer/ReadmeSamples.java
+++ sdk/formrecognizer/azure-ai-formrecognizer/src/samples/java/com/azure/ai/formrecognizer/ReadmeSamples.java
@@ -3,14 +3,21 @@
 
 package com.azure.ai.formrecognizer;
 
+import com.azure.ai.formrecognizer.models.AccountProperties;
+import com.azure.ai.formrecognizer.models.CustomFormModel;
+import com.azure.ai.formrecognizer.models.CustomFormModelInfo;
+import com.azure.ai.formrecognizer.models.ErrorResponseException;
+import com.azure.ai.formrecognizer.models.FormPage;
 import com.azure.ai.formrecognizer.models.OperationResult;
+import com.azure.ai.formrecognizer.models.RecognizedForm;
 import com.azure.ai.formrecognizer.models.RecognizedReceipt;
 import com.azure.ai.formrecognizer.models.USReceipt;
 import com.azure.core.credential.AzureKeyCredential;
-import com.azure.core.http.HttpClient;
-import com.azure.core.http.netty.NettyAsyncHttpClientBuilder;
+import com.azure.core.http.rest.PagedIterable;
 import com.azure.core.util.IterableStream;
 import com.azure.core.util.polling.SyncPoller;
+
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * WARNING: MODIFYING THIS FILE WILL REQUIRE CORRESPONDING UPDATES TO README.md FILE. LINE NUMBERS ARE USED TO EXTRACT
@@ -21,16 +28,7 @@ import com.azure.core.util.polling.SyncPoller;
  */
 public class ReadmeSamples {
     private FormRecognizerClient formRecognizerClient = new FormRecognizerClientBuilder().buildClient();
-
-    /**
-     * Code snippet for configuring http client.
-     */
-    public void configureHttpClient() {
-        HttpClient client = new NettyAsyncHttpClientBuilder()
-            .port(8080)
-            .wiretap(true)
-            .build();
-    }
+    private FormTrainingClient formTrainingClient = formRecognizerClient.getFormTrainingClient();
 
     /**
      * Code snippet for getting sync client using the API key authentication.
@@ -40,16 +38,6 @@ public class ReadmeSamples {
             .apiKey(new AzureKeyCredential("{api_key}"))
             .endpoint("{endpoint}")
             .buildClient();
-    }
-
-    /**
-     * Code snippet for getting async client using API key authentication.
-     */
-    public void useApiKeyAsyncClient() {
-        FormRecognizerAsyncClient formRecognizerAsyncClient = new FormRecognizerClientBuilder()
-            .apiKey(new AzureKeyCredential("{api_key}"))
-            .endpoint("{endpoint}")
-            .buildAsyncClient();
     }
 
     /**
@@ -65,8 +53,53 @@ public class ReadmeSamples {
         credential.update("{new_api_key}");
     }
 
-    public void extractReceipt() {
-        String receiptSourceUrl = "https://docs.microsoft.com/en-us/azure/cognitive-services/form-recognizer/media/contoso-allinone.jpg";
+    public void recognizeCustomForm() {
+        String analyzeFilePath = "{file_source_url}";
+        String modelId = "{custom_trained_model_id}";
+        SyncPoller<OperationResult, IterableStream<RecognizedForm>> recognizeFormPoller =
+            formRecognizerClient.beginRecognizeCustomFormsFromUrl(analyzeFilePath, modelId);
+
+        IterableStream<RecognizedForm> recognizedForms = recognizeFormPoller.getFinalResult();
+
+        recognizedForms.forEach(form -> {
+            System.out.println("----------- Recognized Form -----------");
+            System.out.printf("Form type: %s%n", form.getFormType());
+            form.getFields().forEach((label, formField) -> {
+                System.out.printf("Field %s has value %s with confidence score of %d.%n", label,
+                    formField.getFieldValue(),
+                    formField.getConfidence());
+            });
+            System.out.print("-----------------------------------");
+        });
+    }
+
+    public void recognizeContent() {
+        String analyzeFilePath = "{file_source_url}";
+        SyncPoller<OperationResult, IterableStream<FormPage>> recognizeLayoutPoller =
+            formRecognizerClient.beginRecognizeContentFromUrl(analyzeFilePath);
+
+        IterableStream<FormPage> layoutPageResults = recognizeLayoutPoller.getFinalResult();
+
+        layoutPageResults.forEach(formPage -> {
+            // Table information
+            System.out.println("----Recognizing content ----");
+            System.out.printf("Has width: %d and height: %d, measured with unit: %s.%n", formPage.getWidth(),
+                formPage.getHeight(),
+                formPage.getUnit());
+            formPage.getTables().forEach(formTable -> {
+                System.out.printf("Table has %d rows and %d columns.%n", formTable.getRowCount(),
+                    formTable.getColumnCount());
+                formTable.getCells().forEach(formTableCell -> {
+                    System.out.printf("Cell has text %s.%n", formTableCell.getText());
+                });
+                System.out.println();
+            });
+        });
+    }
+
+    public void recognizeReceipt() {
+        String receiptSourceUrl = "https://docs.microsoft.com/en-us/azure/cognitive-services/form-recognizer/media"
+            + "/contoso-allinone.jpg";
         SyncPoller<OperationResult, IterableStream<RecognizedReceipt>> syncPoller =
             formRecognizerClient.beginRecognizeReceiptsFromUrl(receiptSourceUrl);
         IterableStream<RecognizedReceipt> receiptPageResults = syncPoller.getFinalResult();
@@ -83,5 +116,73 @@ public class ReadmeSamples {
             System.out.printf("Total: %s%n", usReceipt.getTotal().getName());
             System.out.printf("Total Value: %s%n", usReceipt.getTotal().getFieldValue());
         });
+    }
+
+    public void trainModel() {
+        String trainingSetSource = "{unlabeled_training_set_SAS_URL}";
+        SyncPoller<OperationResult, CustomFormModel> trainingPoller =
+            formTrainingClient.beginTraining(trainingSetSource, false);
+
+        CustomFormModel customFormModel = trainingPoller.getFinalResult();
+
+        // Model Info
+        System.out.printf("Model Id: %s%n", customFormModel.getModelId());
+        System.out.printf("Model Status: %s%n", customFormModel.getModelStatus());
+        System.out.printf("Model created on: %s%n", customFormModel.getCreatedOn());
+        System.out.printf("Model last updated: %s%n%n", customFormModel.getLastUpdatedOn());
+
+        System.out.println("Recognized Fields:");
+        // looping through the sub-models, which contains the fields they were trained on
+        // Since the given training documents are unlabeled, we still group them but they do not have a label.
+        customFormModel.getSubModels().forEach(customFormSubModel -> {
+            // Since the training data is unlabeled, we are unable to return the accuracy of this model
+            customFormSubModel.getFieldMap().forEach((field, customFormModelField) ->
+                System.out.printf("Field: %s Field Label: %s%n",
+                    field, customFormModelField.getLabel()));
+        });
+    }
+
+    public void manageModels() {
+        AtomicReference<String> modelId = new AtomicReference<>();
+        // First, we see how many custom models we have, and what our limit is
+        AccountProperties accountProperties = formTrainingClient.getAccountProperties();
+        System.out.printf("The account has %s custom models, and we can have at most %s custom models",
+            accountProperties.getCount(), accountProperties.getLimit());
+
+        // Next, we get a paged list of all of our custom models
+        PagedIterable<CustomFormModelInfo> customModels = formTrainingClient.getModelInfos();
+        System.out.println("We have following models in the account:");
+        customModels.forEach(customFormModelInfo -> {
+            System.out.printf("Model Id: %s%n", customFormModelInfo.getModelId());
+            // get custom model info
+            modelId.set(customFormModelInfo.getModelId());
+            CustomFormModel customModel = formTrainingClient.getCustomModel(customFormModelInfo.getModelId());
+            System.out.printf("Model Status: %s%n", customModel.getModelStatus());
+            System.out.printf("Created on: %s%n", customModel.getCreatedOn());
+            System.out.printf("Updated on: %s%n", customModel.getLastUpdatedOn());
+            customModel.getSubModels().forEach(customFormSubModel -> {
+                System.out.printf("Custom Model Form type: %s%n", customFormSubModel.getFormType());
+                System.out.printf("Custom Model Accuracy: %d%n", customFormSubModel.getAccuracy());
+                if (customFormSubModel.getFieldMap() != null) {
+                    customFormSubModel.getFieldMap().forEach((fieldText, customFormModelField) -> {
+                        System.out.printf("Field Text: %s%n", fieldText);
+                        System.out.printf("Field Accuracy: %d%n", customFormModelField.getAccuracy());
+                    });
+                }
+            });
+        });
+        // Delete Custom Model
+        formTrainingClient.deleteModel(modelId.get());
+    }
+
+    /**
+     * Code snippet for handling exception
+     */
+    public void handlingException() {
+        try {
+            formRecognizerClient.beginRecognizeContentFromUrl("invalidSourceUrl");
+        } catch (ErrorResponseException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
