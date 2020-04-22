dcde901d2f61311235ccd75829087aec69434e08
Connie Yau <conniey@microsoft.com>
2020-04-22 07:51:23 -0700

    Adding support for completing messages on receiver link. Moving logic out of azure-core-amqp. Adding more status codes (#10385)
    * Adding missing AmqpResponseCode from .NET's AMQP library.
    
    * Using a processor rather than a sink that could be null.
    
    * Not throwing IllegalArgumentException when AMQP error condition not found. Being more versatile in parsing response codes.
    
    * Adding flexibility to decoding RequestResponse messages.
    
    * Move message deserialization and validation out of RequestResponseChannel. Just validate a matching message.
    
    * Fixing error handling in RequestResponseChannel.
    
    * Update message lock container to expire lock tokens.
    
    * Add methods for reading RequestResponse status codes and error messages.
    
    * Add comment for service bus session and unnamed session.
    
    * MessageLockContainer removed from constructor.
    
    * Adding support for keeping sessions open.
    
    * Adding support for settling messages on receive link.

--- sdk/servicebus/azure-messaging-servicebus/src/main/java/com/azure/messaging/servicebus/implementation/ServiceBusSession.java
+++ sdk/servicebus/azure-messaging-servicebus/src/main/java/com/azure/messaging/servicebus/implementation/ServiceBusSession.java
@@ -28,7 +28,7 @@ public interface ServiceBusSession extends AmqpSession {
      * @param entityPath The entity path this link connects to, so that it may read events from the message broker.
      * @param timeout Timeout required for creating and opening an AMQP link.
      * @param retryPolicy The retry policy to use when consuming messages.
-     * @param receiveMode  The {@link ReceiveMode} for the messages to be received.
+     * @param receiveMode The {@link ReceiveMode} for the messages to be received.
      *
      * @return A newly created AMQP link.
      */
@@ -42,8 +42,9 @@ public interface ServiceBusSession extends AmqpSession {
      * @param entityPath The entity path this link connects to, so that it may read events from the message broker.
      * @param timeout Timeout required for creating and opening an AMQP link.
      * @param retryPolicy The retry policy to use when consuming messages.
-     * @param receiveMode  The {@link ReceiveMode} for the messages to be received.
-     * @param sessionId  The sessionId for the messages to be received.
+     * @param receiveMode The {@link ReceiveMode} for the messages to be received.
+     * @param sessionId The sessionId for the messages to be received. If {@code null}, then the next, unnamed session
+     *     is retrieved.
      *
      * @return A newly created AMQP link.
      */
