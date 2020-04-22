75e12d73f027e0abed0bf79dd7c933d099253ed9
Gauri Prasad <gapra@microsoft.com>
2020-04-22 14:10:37 -0700

    trying to get java 11 to build

--- sdk/storage/azure-storage-blob-changefeed/src/main/java/module-info.java
+++ sdk/storage/azure-storage-blob-changefeed/src/main/java/module-info.java
@@ -2,7 +2,7 @@
 // Licensed under the MIT License.
 
 module azure.storage.blob.changefeed {
-    requires com.azure.storage.blob;
+    requires transitive com.azure.storage.blob;
 
     exports com.azure.storage.blob.changefeed;
 }
