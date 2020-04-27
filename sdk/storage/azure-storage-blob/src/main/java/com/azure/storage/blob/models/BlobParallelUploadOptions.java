454580a64019d867da83a8ab6879979c0458b8de
rickle-msft <frley@microsoft.com>
2020-04-27 14:16:40 -0700

    Added docs to new types

--- sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/models/BlobParallelUploadOptions.java
+++ sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/models/BlobParallelUploadOptions.java
@@ -3,10 +3,14 @@
 
 package com.azure.storage.blob.models;
 
-import com.azure.core.http.RequestConditions;
+import com.azure.core.annotation.Fluent;
 
 import java.util.Map;
 
+/**
+ * Extended options that may be passed when uploading a Block Blob in parallel.
+ */
+@Fluent
 public class BlobParallelUploadOptions {
     private ParallelTransferOptions parallelTransferOptions;
     private BlobHttpHeaders headers;
@@ -15,55 +19,97 @@ public class BlobParallelUploadOptions {
     private AccessTier tier;
     private BlobRequestConditions requestConditions;
 
+    /**
+     * @return {@link ParallelTransferOptions}
+     */
     public ParallelTransferOptions getParallelTransferOptions() {
         return parallelTransferOptions;
     }
 
+    /**
+     * @param parallelTransferOptions {@link ParallelTransferOptions}
+     * @return The updated options.
+     */
     public BlobParallelUploadOptions setParallelTransferOptions(ParallelTransferOptions parallelTransferOptions) {
         this.parallelTransferOptions = parallelTransferOptions;
         return this;
     }
 
+    /**
+     * @return {@link BlobHttpHeaders}
+     */
     public BlobHttpHeaders getHeaders() {
         return headers;
     }
 
+    /**
+     * @param headers {@link BlobHttpHeaders}
+     * @return The updated options
+     */
     public BlobParallelUploadOptions setHeaders(BlobHttpHeaders headers) {
         this.headers = headers;
         return this;
     }
 
+    /**
+     * @return The metadata to associate with the blob.
+     */
     public Map<String, String> getMetadata() {
         return metadata;
     }
 
+    /**
+     * @param metadata The metadata to associate with the blob.
+     * @return The updated options.
+     */
     public BlobParallelUploadOptions setMetadata(Map<String, String> metadata) {
         this.metadata = metadata;
         return this;
     }
 
+    /**
+     * @return The tags to associate with the blob.
+     */
     public Map<String, String> getTags() {
         return tags;
     }
 
+    /**
+     * @param tags The tags to associate with the blob.
+     * @return The updated options.
+     */
     public BlobParallelUploadOptions setTags(Map<String, String> tags) {
         this.tags = tags;
         return this;
     }
 
+    /**
+     * @return {@link AccessTier}
+     */
     public AccessTier getTier() {
         return tier;
     }
 
+    /**
+     * @param tier {@link AccessTier}
+     * @return The updated options.
+     */
     public BlobParallelUploadOptions setTier(AccessTier tier) {
         this.tier = tier;
         return this;
     }
 
+    /**
+     * @return {@link BlobRequestConditions}
+     */
     public BlobRequestConditions getRequestConditions() {
         return requestConditions;
     }
 
+    /**
+     * @param requestConditions {@link BlobRequestConditions}
+     * @return The updated options.
+     */
     public BlobParallelUploadOptions setRequestConditions(BlobRequestConditions requestConditions) {
         this.requestConditions = requestConditions;
         return this;
