9369a62d220c4b1fa033c6cc75e84907a2e595c1
Gauri Prasad <gapra@microsoft.com>
2020-04-28 14:17:32 -0700

    Implemented an Avro Parser

---
+++ sdk/storage/azure-storage-internal-avro/src/main/java/com/azure/storage/internal/avro/implementation/schema/AvroSimpleSchema.java
@@ -0,0 +1,35 @@
+package com.azure.storage.internal.avro.implementation.schema;
+
+import com.azure.storage.internal.avro.implementation.AvroParserState;
+
+import java.util.function.Consumer;
+
+/**
+ * An abstract class that represents a simple Avro schema that can return an Object result.
+ *
+ * Simple avro schemas directly consume bytes from the state to populate the result.
+ *
+ * @see AvroSchema
+ */
+public abstract class AvroSimpleSchema extends AvroSchema {
+    /**
+     * Constructs a new Schema.
+     *
+     * @param state    The state of the parser.
+     * @param onResult The result handler.
+     */
+    public AvroSimpleSchema(AvroParserState state, Consumer<Object> onResult) {
+        super(state, onResult);
+    }
+
+    /**
+     * @return Whether or not progress can be made for this schema.
+     */
+    public abstract boolean canProgress();
+
+    /**
+     * Makes some progress in parsing the type.
+     */
+    public abstract void progress();
+
+}
---
+++ sdk/storage/azure-storage-internal-avro/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+module com.azure.storage.internal.avro {
+    requires transitive com.azure.storage.common;
+    requires com.azure.core;
+}
