46e52f8e62a6a408ba43eaaa9f2f2d39a5d4a10b
Sima Zhu <48036328+sima-zhu@users.noreply.github.com>
2020-04-29 11:19:08 -0700

    Update sdk/search/azure-search-documents/src/main/java/com/azure/search/documents/indexes/package-info.java
    Co-Authored-By: Srikanta <51379715+srnagar@users.noreply.github.com>

--- sdk/search/azure-search-documents/src/main/java/com/azure/search/documents/indexes/package-info.java
+++ sdk/search/azure-search-documents/src/main/java/com/azure/search/documents/indexes/package-info.java
@@ -2,7 +2,7 @@
 // Licensed under the MIT License.
 
 /**
- * Package containing the annotations used by FieldBuilder which help covert
+ * Package containing the annotations used by FieldBuilder which help convert
  * model class to a list of Field classes for building Index.
  */
 package com.azure.search.documents.indexes;
