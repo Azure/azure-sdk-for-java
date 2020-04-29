30d3d576501551e1c1c87d3760e6a74422bcf932
Weidong Xu <weidxu@microsoft.com>
2020-04-29 15:30:48 +0800

    add a missing one

--- sdk/appservice/mgmt/src/test/java/com/azure/management/appservice/WebAppsWebDeployTests.java
+++ sdk/appservice/mgmt/src/test/java/com/azure/management/appservice/WebAppsWebDeployTests.java
@@ -52,7 +52,7 @@ public class WebAppsWebDeployTests extends AppServiceTest {
             webApp1
                 .deploy()
                 .withPackageUri(
-                    "https://github.com/Azure/azure-sdk-for-java/raw/master/sdk/appservice/mgmt/src/test/resources/webapps.zip")
+                    "https://raw.githubusercontent.com/Azure/azure-sdk-for-java/master/sdk/appservice/mgmt/src/test/resources/webapps.zip")
                 .withExistingDeploymentsDeleted(true)
                 .execute();
 
