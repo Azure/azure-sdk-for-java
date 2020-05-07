4a75226e630892b76edbde0d7486f3cd81afdd6e
Kushagra Thapar <kuthapar@microsoft.com>
2020-05-06 17:28:31 -0700

    Fixed more tests client builders with content response on write disabled

--- sdk/cosmos/azure-cosmos-examples/src/test/java/com/azure/cosmos/rx/examples/CosmosAuthorizationTokenResolverTest.java
+++ sdk/cosmos/azure-cosmos-examples/src/test/java/com/azure/cosmos/rx/examples/CosmosAuthorizationTokenResolverTest.java
@@ -69,7 +69,8 @@ public class CosmosAuthorizationTokenResolverTest extends DocumentClientTest {
             .withServiceEndpoint(TestConfigurations.HOST)
             .withMasterKeyOrResourceToken(TestConfigurations.MASTER_KEY)
             .withConnectionPolicy(connectionPolicy)
-            .withConsistencyLevel(ConsistencyLevel.SESSION);
+            .withConsistencyLevel(ConsistencyLevel.SESSION)
+            .withContentResponseOnWriteEnabled(true);
 
         this.client = this.clientBuilder().build();
 
