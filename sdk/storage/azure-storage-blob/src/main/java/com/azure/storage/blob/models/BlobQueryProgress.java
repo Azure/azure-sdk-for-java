13fcf2ee1285db2ac87223d185b180cc9fee03dd
Gauri Prasad <gapra@microsoft.com>
2020-05-18 12:10:54 -0700

    Added heades

--- sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/models/BlobQueryProgress.java
+++ sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/models/BlobQueryProgress.java
@@ -1,7 +1,10 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.azure.storage.blob.models;
 
 /**
- * Defines a blob query error that can be returned on parsing a blob  query request.
+ * Defines a blob query error that can be returned on parsing a blob query request.
  */
 public class BlobQueryProgress {
 
