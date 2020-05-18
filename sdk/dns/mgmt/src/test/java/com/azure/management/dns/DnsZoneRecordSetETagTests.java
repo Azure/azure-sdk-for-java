6ec44956379cb5ed05a1b142612d32589926e3be
Tanyi Chen <tanchen@microsoft.com>
2020-05-18 15:34:59 +0800

    eTag -> etag

--- sdk/dns/mgmt/src/test/java/com/azure/management/dns/DnsZoneRecordSetETagTests.java
+++ sdk/dns/mgmt/src/test/java/com/azure/management/dns/DnsZoneRecordSetETagTests.java
@@ -46,7 +46,7 @@ public class DnsZoneRecordSetETagTests extends TestBase {
 
         DnsZone dnsZone =
             zoneManager.zones().define(topLevelDomain).withNewResourceGroup(rgName, region).withETagCheck().create();
-        Assertions.assertNotNull(dnsZone.eTag());
+        Assertions.assertNotNull(dnsZone.etag());
 
         Runnable runnable =
             () ->
@@ -66,11 +66,11 @@ public class DnsZoneRecordSetETagTests extends TestBase {
 
         final DnsZone dnsZone =
             zoneManager.zones().define(topLevelDomain).withNewResourceGroup(rgName, region).withETagCheck().create();
-        Assertions.assertNotNull(dnsZone.eTag());
+        Assertions.assertNotNull(dnsZone.etag());
 
-        Runnable runnable = () -> dnsZone.update().withETagCheck(dnsZone.eTag() + "-foo").apply();
+        Runnable runnable = () -> dnsZone.update().withETagCheck(dnsZone.etag() + "-foo").apply();
         ensureETagExceptionIsThrown(runnable);
-        dnsZone.update().withETagCheck(dnsZone.eTag()).apply();
+        dnsZone.update().withETagCheck(dnsZone.etag()).apply();
     }
 
     @Test
@@ -80,11 +80,11 @@ public class DnsZoneRecordSetETagTests extends TestBase {
 
         final DnsZone dnsZone =
             zoneManager.zones().define(topLevelDomain).withNewResourceGroup(rgName, region).withETagCheck().create();
-        Assertions.assertNotNull(dnsZone.eTag());
+        Assertions.assertNotNull(dnsZone.etag());
 
-        Runnable runnable = () -> zoneManager.zones().deleteById(dnsZone.id(), dnsZone.eTag() + "-foo");
+        Runnable runnable = () -> zoneManager.zones().deleteById(dnsZone.id(), dnsZone.etag() + "-foo");
         ensureETagExceptionIsThrown(runnable);
-        zoneManager.zones().deleteById(dnsZone.id(), dnsZone.eTag());
+        zoneManager.zones().deleteById(dnsZone.id(), dnsZone.etag());
     }
 
     @Test
@@ -220,27 +220,27 @@ public class DnsZoneRecordSetETagTests extends TestBase {
         PagedIterable<ARecordSet> aRecordSets = dnsZone.aRecordSets().list();
         Assertions.assertTrue(TestUtilities.getSize(aRecordSets) == 1);
         ARecordSet aRecordSet = aRecordSets.iterator().next();
-        Assertions.assertNotNull(aRecordSet.eTag());
+        Assertions.assertNotNull(aRecordSet.etag());
 
         // Check AAAA records
         PagedIterable<AaaaRecordSet> aaaaRecordSets = dnsZone.aaaaRecordSets().list();
         Assertions.assertTrue(TestUtilities.getSize(aaaaRecordSets) == 1);
         AaaaRecordSet aaaaRecordSet = aaaaRecordSets.iterator().next();
-        Assertions.assertNotNull(aaaaRecordSet.eTag());
+        Assertions.assertNotNull(aaaaRecordSet.etag());
 
         // by default zone access type should be public
         Assertions.assertEquals(ZoneType.PUBLIC, dnsZone.accessType());
-        // Try updates with invalid eTag
+        // Try updates with invalid etag
         //
         Exception compositeException = null;
         try {
             dnsZone
                 .update()
                 .updateARecordSet("www")
-                .withETagCheck(aRecordSet.eTag() + "-foo")
+                .withETagCheck(aRecordSet.etag() + "-foo")
                 .parent()
                 .updateAaaaRecordSet("www")
-                .withETagCheck(aaaaRecordSet.eTag() + "-foo")
+                .withETagCheck(aaaaRecordSet.etag() + "-foo")
                 .parent()
                 .apply();
         } catch (Exception exception) {
@@ -261,10 +261,10 @@ public class DnsZoneRecordSetETagTests extends TestBase {
             .update()
             .updateARecordSet("www")
             .withIPv4Address("24.97.105.45")
-            .withETagCheck(aRecordSet.eTag())
+            .withETagCheck(aRecordSet.etag())
             .parent()
             .updateAaaaRecordSet("www")
-            .withETagCheck(aaaaRecordSet.eTag())
+            .withETagCheck(aaaaRecordSet.etag())
             .parent()
             .apply();
 
@@ -272,14 +272,14 @@ public class DnsZoneRecordSetETagTests extends TestBase {
         aRecordSets = dnsZone.aRecordSets().list();
         Assertions.assertTrue(TestUtilities.getSize(aRecordSets) == 1);
         aRecordSet = aRecordSets.iterator().next();
-        Assertions.assertNotNull(aRecordSet.eTag());
+        Assertions.assertNotNull(aRecordSet.etag());
         Assertions.assertTrue(aRecordSet.ipv4Addresses().size() == 3);
 
         // Check AAAA records
         aaaaRecordSets = dnsZone.aaaaRecordSets().list();
         Assertions.assertTrue(TestUtilities.getSize(aaaaRecordSets) == 1);
         aaaaRecordSet = aaaaRecordSets.iterator().next();
-        Assertions.assertNotNull(aaaaRecordSet.eTag());
+        Assertions.assertNotNull(aaaaRecordSet.etag());
     }
 
     @Test
@@ -309,22 +309,22 @@ public class DnsZoneRecordSetETagTests extends TestBase {
         PagedIterable<ARecordSet> aRecordSets = dnsZone.aRecordSets().list();
         Assertions.assertTrue(TestUtilities.getSize(aRecordSets) == 1);
         ARecordSet aRecordSet = aRecordSets.iterator().next();
-        Assertions.assertNotNull(aRecordSet.eTag());
+        Assertions.assertNotNull(aRecordSet.etag());
 
         // Check AAAA records
         PagedIterable<AaaaRecordSet> aaaaRecordSets = dnsZone.aaaaRecordSets().list();
         Assertions.assertTrue(TestUtilities.getSize(aaaaRecordSets) == 1);
         AaaaRecordSet aaaaRecordSet = aaaaRecordSets.iterator().next();
-        Assertions.assertNotNull(aaaaRecordSet.eTag());
+        Assertions.assertNotNull(aaaaRecordSet.etag());
 
-        // Try delete with invalid eTag
+        // Try delete with invalid etag
         //
         Exception compositeException = null;
         try {
             dnsZone
                 .update()
-                .withoutARecordSet("www", aRecordSet.eTag() + "-foo")
-                .withoutAaaaRecordSet("www", aaaaRecordSet.eTag() + "-foo")
+                .withoutARecordSet("www", aRecordSet.etag() + "-foo")
+                .withoutAaaaRecordSet("www", aaaaRecordSet.etag() + "-foo")
                 .apply();
         } catch (Exception exception) {
             compositeException = exception;
@@ -342,8 +342,8 @@ public class DnsZoneRecordSetETagTests extends TestBase {
         // Try delete with correct etags
         dnsZone
             .update()
-            .withoutARecordSet("www", aRecordSet.eTag())
-            .withoutAaaaRecordSet("www", aaaaRecordSet.eTag())
+            .withoutARecordSet("www", aRecordSet.etag())
+            .withoutAaaaRecordSet("www", aaaaRecordSet.etag())
             .apply();
 
         // Check A records
