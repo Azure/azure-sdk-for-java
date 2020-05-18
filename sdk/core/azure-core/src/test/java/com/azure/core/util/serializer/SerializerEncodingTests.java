a78949c7fc33e58eb123d485980b80071a5468f2
Connie Yau <conniey@microsoft.com>
2020-05-18 15:28:43 -0700

    Adding flexibility to parsing serializer encodings. (#11255)
    * Adding more flexible support for encodings.

---
+++ sdk/core/azure-core/src/test/java/com/azure/core/util/serializer/SerializerEncodingTests.java
@@ -0,0 +1,57 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+package com.azure.core.util.serializer;
+
+import com.azure.core.http.HttpHeaders;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Collections;
+
+/**
+ * Tests for {@link SerializerEncoding}.
+ */
+class SerializerEncodingTests {
+    private static final String CONTENT_TYPE = "Content-Type";
+
+    @ParameterizedTest
+    @ValueSource(strings = {"application/xml", "application/atom+xml", "text/xml", "application/foo+XML", "TEXT/XML"})
+    void recognizeXml(String mimeType) {
+        // Arrange
+        HttpHeaders headers = new HttpHeaders(Collections.singletonMap(CONTENT_TYPE, mimeType));
+
+        // Act & Assert
+        Assertions.assertEquals(SerializerEncoding.XML, SerializerEncoding.fromHeaders(headers));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"application/json", "application/kv+json", "APPLICATION/JSON", "application/FOO+JSON"})
+    void recognizeJson(String mimeType) {
+        // Arrange
+        HttpHeaders headers = new HttpHeaders(Collections.singletonMap(CONTENT_TYPE, mimeType));
+
+        // Act & Assert
+        Assertions.assertEquals(SerializerEncoding.JSON, SerializerEncoding.fromHeaders(headers));
+    }
+
+    @Test
+    void defaultNoContentType() {
+        // Arrange
+        HttpHeaders headers = new HttpHeaders(Collections.singletonMap("Http-Method", "GET"));
+
+        // Act & Assert
+        Assertions.assertEquals(SerializerEncoding.JSON, SerializerEncoding.fromHeaders(headers));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"application/binary", "invalid-mime-type"})
+    void defaultUnsupportedType(String mimeType) {
+        // Arrange
+        HttpHeaders headers = new HttpHeaders(Collections.singletonMap(CONTENT_TYPE, mimeType));
+
+        // Act & Assert
+        Assertions.assertEquals(SerializerEncoding.JSON, SerializerEncoding.fromHeaders(headers));
+    }
+}
