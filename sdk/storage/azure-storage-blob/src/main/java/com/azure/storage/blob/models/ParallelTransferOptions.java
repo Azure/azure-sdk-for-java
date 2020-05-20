6d0162a2eda29f3daaa599fda3529b2c427f3a6a
Rick Ley <frley@microsoft.com>
2020-05-20 11:37:28 -0700

    Barely started

--- sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/models/ParallelTransferOptions.java
+++ sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/models/ParallelTransferOptions.java
@@ -19,6 +19,7 @@ public final class ParallelTransferOptions {
     private final Integer numBuffers;
     private final ProgressReceiver progressReceiver;
     private final Integer maxSingleUploadSize;
+    private final Integer maxConcurrency;
 
     /**
      * Creates a new {@link ParallelTransferOptions} with default parameters applied.
@@ -62,7 +63,35 @@ public final class ParallelTransferOptions {
      * {@link BlockBlobAsyncClient#MAX_UPLOAD_BLOB_BYTES}.
      */
     public ParallelTransferOptions(Integer blockSize, Integer numBuffers, ProgressReceiver progressReceiver,
-        Integer maxSingleUploadSize) {
+            Integer maxSingleUploadSize) {
+        this(blockSize, numBuffers, progressReceiver, maxSingleUploadSize, null);
+    }
+
+    /**
+     * Creates a new {@link ParallelTransferOptions} with default parameters applied.
+     *
+     * @param blockSize The block size.
+     * For upload, The block size is the size of each block that will be staged. This value also determines the number
+     * of requests that need to be made. If block size is large, upload will make fewer network calls, but each
+     * individual call will send more data and will therefore take longer. This parameter also determines the size
+     * that each buffer uses when buffering is required and consequently amount of memory consumed by such methods may
+     * be up to blockSize * numBuffers.
+     * @param numBuffers For buffered upload only, the number of buffers is the maximum number of buffers this method
+     * should allocate. Memory will be allocated lazily as needed. Must be at least two. Typically, the larger the
+     * number of buffers, the more parallel, and thus faster, the upload portion  of this operation will be.
+     * The amount of memory consumed by methods using this value may be up to blockSize * numBuffers.
+     * @param progressReceiver {@link ProgressReceiver}
+     * @param maxSingleUploadSize If the size of the data is less than or equal to this value, it will be uploaded in a
+     * single put rather than broken up into chunks. If the data is uploaded in a single shot, the block size will be
+     * ignored. Some constraints to consider are that more requests cost more, but several small or mid-sized requests
+     * may sometimes perform better. In the case of buffered upload, up to this amount of data may be buffered before
+     * any data is sent. Must be greater than 0. May be null to accept default behavior, which is the maximum value the
+     * service accepts for uploading in a single requests and is represented by
+     * {@link BlockBlobAsyncClient#MAX_UPLOAD_BLOB_BYTES}.
+     * @param maxConcurrency The maximum number of parallel requests
+     */
+    public ParallelTransferOptions(Integer blockSize, Integer numBuffers, ProgressReceiver progressReceiver,
+        Integer maxSingleUploadSize, Integer maxConcurrency) {
         if (blockSize != null) {
             StorageImplUtils.assertInBounds("blockSize", blockSize, 1, BlockBlobAsyncClient.MAX_STAGE_BLOCK_BYTES);
         }
