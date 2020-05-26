18ccc2833433ac7cfcb803c0e143b525a436d06f
Sima Zhu <48036328+sima-zhu@users.noreply.github.com>
2020-05-26 09:48:47 -0700

    Update sdk/search/azure-search-documents/src/main/java/com/azure/search/documents/implementation/util/Constants.java
    Co-authored-by: Srikanta <51379715+srnagar@users.noreply.github.com>

--- sdk/search/azure-search-documents/src/main/java/com/azure/search/documents/implementation/util/Constants.java
+++ sdk/search/azure-search-documents/src/main/java/com/azure/search/documents/implementation/util/Constants.java
@@ -5,7 +5,7 @@ package com.azure.search.documents.implementation.util;
 
 public class Constants {
     public static final String ENUM_INTERNAL_ERROR_MSG = "The enum does not exist in internal used model %s.";
-    public static final String ENUM_EXTERNAL_ERROR_MSG = "The enum does not exist in external used model %s.";
+    public static final String ENUM_EXTERNAL_ERROR_MSG = "The enum does not exist in externally used model %s.";
 
     public static final String ABSTRACT_INTERNAL_ERROR_MSG = "The subclass does not exist in internal used model %s.";
     public static final String ABSTRACT_EXTERNAL_ERROR_MSG = "The subclass does not exist in external used model %s.";
