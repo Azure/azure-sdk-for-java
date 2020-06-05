cbef4522e8789e578c7f8be0a47c0ba4e1ecd17f
shafang <Shawn.Fang@microsoft.com>
2020-06-05 12:35:54 -0700

    using Function Apply

--- sdk/core/azure-core-test/src/main/java/com/azure/core/test/models/RecordingRedactor.java
+++ sdk/core/azure-core-test/src/main/java/com/azure/core/test/models/RecordingRedactor.java
@@ -4,8 +4,11 @@
 package com.azure.core.test.models;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
 import java.util.UUID;
+import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,17 +16,15 @@ import java.util.regex.Pattern;
  * This class used to redact the sensitive information when recording
  */
 public class RecordingRedactor {
-    private static final String ACCESS_TOKEN = "accessToken";
     private static final String REDACTED = "REDACTED";
     private static final String REDACTED_UTF_8 = Base64.getEncoder().encodeToString("REDACTED".getBytes(StandardCharsets.UTF_8));
-    private static final String USER_DELEGATION_KEY = "UserDelegationKey";
 
     private static final Pattern ACCESS_TOKEN_KEY_PATTERN = Pattern.compile("(?:\"accessToken\":\")(.*?)(?:\",|\"})");
     private static final Pattern DELEGATIONKEY_KEY_PATTERN = Pattern.compile("(?:<Value>)(.*)(?:</Value>)");
     private static final Pattern DELEGATIONKEY_CLIENTID_PATTERN = Pattern.compile("(?:<SignedOid>)(.*)(?:</SignedOid>)");
     private static final Pattern DELEGATIONKEY_TENANTID_PATTERN = Pattern.compile("(?:<SignedTid>)(.*)(?:</SignedTid>)");
 
-    private static final String[] STOP_WORDS = {ACCESS_TOKEN, USER_DELEGATION_KEY};
+    private static final List<Function<String, String>> RECORDING_REDACTORS = loadRedactor();
 
     /**
      * Redact the sensitive information.
@@ -32,37 +33,33 @@ public class RecordingRedactor {
      * @return the redacted content
      */
     public String redact(String redactableString) {
-
-        for (String stopWord : STOP_WORDS) {
-            if (redactableString.contains(stopWord)) {
-                switch (stopWord) {
-                    case USER_DELEGATION_KEY:
-                        redactableString = redactUserDelegationKey(redactableString);
-                        break;
-                    case ACCESS_TOKEN:
-                        redactableString = redactAccessToken(redactableString);
-                        break;
-                    default:
-                        break;
-                }
-            }
+        String redactedString = redactableString;
+        for (Function<String, String> redactor : RECORDING_REDACTORS) {
+            redactedString = redactor.apply(redactedString);
         }
-        return redactableString;
+        return redactedString;
     }
 
-    private String redactAccessToken(String content) {
+    private static List<Function<String, String>> loadRedactor() {
+        List<Function<String, String>> redactors = new ArrayList<>();
+        redactors.add(RecordingRedactor::redactAccessToken);
+        redactors.add(RecordingRedactor::redactUserDelegationKey);
+        return redactors;
+    }
+
+    private static String redactAccessToken(String content) {
         content = redactionReplacement(content, ACCESS_TOKEN_KEY_PATTERN.matcher(content), REDACTED);
         return content;
     }
 
-    private String redactUserDelegationKey(String content) {
+    private static String redactUserDelegationKey(String content) {
         content = redactionReplacement(content, DELEGATIONKEY_KEY_PATTERN.matcher(content), REDACTED_UTF_8);
         content = redactionReplacement(content, DELEGATIONKEY_CLIENTID_PATTERN.matcher(content), UUID.randomUUID().toString());
         content = redactionReplacement(content, DELEGATIONKEY_TENANTID_PATTERN.matcher(content), UUID.randomUUID().toString());
         return content;
     }
 
-    private String redactionReplacement(String content, Matcher matcher, String replacement) {
+    private static String redactionReplacement(String content, Matcher matcher, String replacement) {
         while (matcher.find()) {
             content = content.replace(matcher.group(1), replacement);
         }
