279def8b2105796ae2f66bef5ad6802e46a57eb4
shafang <Shawn.Fang@microsoft.com>
2020-06-05 11:47:43 -0700

    add an unit test for non-sensitive data contend

--- sdk/core/azure-core-test/src/test/java/com/azure/core/test/RecordingRedactorTests.java
+++ sdk/core/azure-core-test/src/test/java/com/azure/core/test/RecordingRedactorTests.java
@@ -15,6 +15,11 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 public class RecordingRedactorTests {
     private static final String DUMMY_SENSITIVE_INFORMATION = "sensitiveInformation";
 
+    // Non-sensitive content data, keep the content as it is
+    private static final String NON_SENSITIVE_DATA_CONTENT = "\"Response\" : {\n"
+        + "      \"Body\" : \"{\"a\":\"a\",\"expirationDateTimeTicks\":637270217074441783}\",\n"
+        + "    },";
+
     // Access token value pair at Body
     private static final String ACCESS_TOKEN_FIRST_PAIR = "\"Response\" : {\n"
         + "      \"Body\" : \"{\"accessToken\":\"sensitiveData\",\"expirationDateTimeTicks\":637270217074441783}\",\n"
@@ -49,6 +54,14 @@ public class RecordingRedactorTests {
     private static final String USER_DELEGATION_KEY_FOR_SIGNED_TID_RESPONSE = "\"Response\" : {\n"
         + "   \"Body\" : <UserDelegationKey><SignedTid>sensitiveInformation</SignedTid></UserDelegationKey>\",\n"
         + "    },";
+
+    /**
+     * Verify if the given content has no key in black list, no change will be required.
+     */
+    @Test
+    public void nonSensitiveDataInContent() {
+        assertEquals(NON_SENSITIVE_DATA_CONTENT, new RecordingRedactor().redact(NON_SENSITIVE_DATA_CONTENT));
+    }
 
     /**
      * Verify if the {@code accessToken} pair that is before last pair value is redacted successfully.
