1e295c319f71f24bc02da03336dcb946d24f8c26
Connie <conniey@microsoft.com>
2020-06-08 00:03:04 -0700

    Add test for QueueDescriptionFeed.

--- sdk/servicebus/azure-messaging-servicebus/src/test/java/com/azure/messaging/servicebus/ServiceBusManagementAsyncClientIntegrationTest.java
+++ sdk/servicebus/azure-messaging-servicebus/src/test/java/com/azure/messaging/servicebus/ServiceBusManagementAsyncClientIntegrationTest.java
@@ -3,25 +3,27 @@
 
 package com.azure.messaging.servicebus;
 
+import com.azure.core.exception.ResourceExistsException;
+import com.azure.core.http.HttpClient;
 import com.azure.core.http.policy.HttpLogDetailLevel;
 import com.azure.core.http.policy.HttpLogOptions;
+import com.azure.core.http.policy.RetryPolicy;
 import com.azure.core.test.TestBase;
-import com.azure.core.test.TestMode;
-import com.azure.messaging.servicebus.implementation.models.ServiceBusManagementErrorException;
 import com.azure.messaging.servicebus.models.QueueDescription;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import reactor.test.StepVerifier;
 
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ServiceBusManagementAsyncClientIntegrationTest extends TestBase {
-    private ServiceBusManagementAsyncClient client;
 
     @BeforeAll
     static void beforeAll() {
@@ -33,41 +35,55 @@ class ServiceBusManagementAsyncClientIntegrationTest extends TestBase {
         StepVerifier.resetDefaultTimeout();
     }
 
-    @Override
-    protected void beforeTest() {
-        Assumptions.assumeTrue(getTestMode() != TestMode.PLAYBACK,
-            "Current record/playback does not support persisting XML calls.");
-
-        client = new ServiceBusManagementClientBuilder()
-            .connectionString(TestUtils.getConnectionString())
-            .httpLogOptions(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BODY_AND_HEADERS))
-            .buildAsyncClient();
-    }
-
-    @Test
-    void getQueue() {
+    @ParameterizedTest
+    @MethodSource("com.azure.core.test.TestBase#getHttpClients")
+    void getQueue(HttpClient httpClient) {
         // Arrange
-        String queueName = TestUtils.getQueueBaseName();
+        final ServiceBusManagementAsyncClient client = createClient(httpClient);
+        final String queueName = TestUtils.getEntityName(TestUtils.getQueueBaseName(), 5);
 
         // Act & Assert
         StepVerifier.create(client.getQueue(queueName))
             .assertNext(queueDescription -> {
                 assertEquals(queueName, queueDescription.getName());
+                assertFalse(queueDescription.isRequiresSession());
+                assertNotNull(queueDescription.getLockDuration());
             })
             .verifyComplete();
     }
 
-    @Test
-    void createQueueExistingName() {
+    @ParameterizedTest
+    @MethodSource("com.azure.core.test.TestBase#getHttpClients")
+    void createQueueExistingName(HttpClient httpClient) {
         // Arrange
         String queueName = TestUtils.getEntityName(TestUtils.getQueueBaseName(), 5);
         QueueDescription queueDescription = new QueueDescription().setName(queueName);
+        final ServiceBusManagementAsyncClient client = createClient(httpClient);
+        final String queueName = TestUtils.getQueueName();
+        final QueueDescription queueDescription = new QueueDescription().setName(queueName);
 
         // Act & Assert
         StepVerifier.create(client.createQueue(queueDescription))
             .consumeErrorWith(error -> {
-                assertTrue(error instanceof ServiceBusManagementErrorException);
+                assertTrue(error instanceof ResourceExistsException);
             })
             .verify();
+    }
+
+    private ServiceBusManagementAsyncClient createClient(HttpClient httpClient) {
+        final String connectionString = interceptorManager.isPlaybackMode()
+            ? "Endpoint=sb://foo.servicebus.windows.net;SharedAccessKeyName=dummyKey;SharedAccessKey=dummyAccessKey"
+            : TestUtils.getConnectionString();
+        final ServiceBusManagementClientBuilder builder = new ServiceBusManagementClientBuilder()
+            .httpLogOptions(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BODY_AND_HEADERS))
+            .connectionString(connectionString)
+            .httpClient(httpClient);
+
+        if (!interceptorManager.isPlaybackMode()) {
+            builder.addPolicy(interceptorManager.getRecordPolicy());
+            builder.addPolicy(new RetryPolicy());
+        }
+
+        return builder.buildAsyncClient();
     }
 }
