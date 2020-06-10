0636e82b376f24168c7bd48bc0486068ca0659ce
Xiaolu Dai <31124698+saragluna@users.noreply.github.com>
2020-06-10 13:01:23 +0800

    Spring Boot migration - AAD related samples (#10614)
    * add samples for aad
    
    * remove all en-us part of URL, fix codestyle, rename readmes
    
    * fix README.md files
    
    * fix README.md
    
    * add azure-spring-boot in jacoco-test-coverage pom file to make build-from-source successful
    
    * move Spring versions to version_client.txt to keep consistency
    
    * add additional modules to build in ci.yml
    
    * add configurations for JavaDoc plugins and entries in jacoco-test-coverage
    
    * remove dependencyManagement and move bom file to sdk/boms
    
    * remove azure-spring-boot-bom in spring ci
    
    * remove current tags from azure-spring-boot-bom
    
    Co-authored-by: Xiaolu Dai <xiada@microsoft.com>
    Co-authored-by: Yi Liu <yiliu6@microsoft.com>

---
+++ sdk/spring/azure-spring-boot-samples/azure-spring-boot-sample-active-directory-backend-v2/src/main/java/microsoft/azure/aad/AzureADOAuth2V2BackendSampleApplication.java
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package microsoft.azure.aad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureADOAuth2V2BackendSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzureADOAuth2V2BackendSampleApplication.class, args);
+    }
+}
