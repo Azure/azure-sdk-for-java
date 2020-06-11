26a638035dce602be5ebc2373050cd66803935dc
Rick Ley <frley@microsoft.com>
2020-06-11 15:50:01 -0700

    Updated beta release based on 73 apiview feedback

--- sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/options/BlobParallelUploadOptions.java
+++ sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/options/BlobParallelUploadOptions.java
@@ -1,10 +1,18 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.azure.storage.blob.models;
+package com.azure.storage.blob.options;
 
 import com.azure.core.annotation.Fluent;
+import com.azure.storage.blob.models.AccessTier;
+import com.azure.storage.blob.models.BlobHttpHeaders;
+import com.azure.storage.blob.models.BlobRequestConditions;
+import com.azure.storage.blob.models.ParallelTransferOptions;
+import reactor.core.publisher.Flux;
 
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.time.Duration;
 import java.util.Map;
 
 /**
@@ -12,11 +20,39 @@ import java.util.Map;
  */
 @Fluent
 public class BlobParallelUploadOptions {
+    private final InputStream dataStream;
+    private final long length;
     private ParallelTransferOptions parallelTransferOptions;
     private BlobHttpHeaders headers;
     private Map<String, String> metadata;
     private AccessTier tier;
     private BlobRequestConditions requestConditions;
+    private Duration timeout;
+
+    /**
+     * @param dataStream The data to write to the blob.
+     * @param length The exact length of the data. It is important that this value match precisely the length of the
+     * data provided in the {@link InputStream}.
+     */
+    public BlobParallelUploadOptions(InputStream dataStream, long length) {
+        this.dataStream = dataStream;
+        this.length = length;
+    }
+
+    /**
+     * @return The data to write to the blob.
+     */
+    public InputStream getDataStream() {
+        return this.dataStream;
+    }
+
+    /**
+     * @return The exact length of the data. It is important that this value match precisely the length of the
+     * data provided in the {@link InputStream}.
+     */
+    public long getLength() {
+        return length;
+    }
 
     /**
      * @return {@link ParallelTransferOptions}
@@ -95,6 +131,19 @@ public class BlobParallelUploadOptions {
      */
     public BlobParallelUploadOptions setRequestConditions(BlobRequestConditions requestConditions) {
         this.requestConditions = requestConditions;
+        return this;
+    }
+
+    public Duration getTimeout() {
+        return this.timeout;
+    }
+
+    /**
+     * @param timeout An optional timeout value beyond which a {@link RuntimeException} will be raised.
+     * @return The updated options.
+     */
+    public BlobParallelUploadOptions setTimeout(Duration timeout) {
+        this.timeout = timeout;
         return this;
     }
 }
