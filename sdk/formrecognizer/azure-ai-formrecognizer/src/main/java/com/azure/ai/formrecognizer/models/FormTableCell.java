d70c56774dd82b8e33927fa35d05b26ac6a40be7
Sameeksha Vaity <savaity@microsoft.com>
2020-06-12 10:38:16 -0700

    Remove TextContentType model (#12080)

--- sdk/formrecognizer/azure-ai-formrecognizer/src/main/java/com/azure/ai/formrecognizer/models/FormTableCell.java
+++ sdk/formrecognizer/azure-ai-formrecognizer/src/main/java/com/azure/ai/formrecognizer/models/FormTableCell.java
@@ -39,10 +39,10 @@ public final class FormTableCell extends FormContent {
     private final float confidence;
 
     /*
-     * When includeTextDetails is set to true, a list of references to the text
+     * When includeTextContent is set to true, a list of references to the text
      * elements constituting this table cell.
      */
-    private final List<FormContent> elements;
+    private final List<FormContent> textContent;
 
     /*
      * Is the current cell a header cell?
@@ -73,7 +73,7 @@ public final class FormTableCell extends FormContent {
         final Integer columnSpan, final String text, final BoundingBox boundingBox,
         final float confidence, final boolean isHeader, final boolean isFooter, final int pageNumber,
         final List<FormContent> textContent) {
-        super(text, boundingBox, pageNumber, null);
+        super(text, boundingBox, pageNumber);
         this.rowIndex = rowIndex;
         this.columnIndex = columnIndex;
         this.rowSpan = rowSpan;
@@ -81,7 +81,7 @@ public final class FormTableCell extends FormContent {
         this.confidence = confidence;
         this.isHeader = isHeader;
         this.isFooter = isFooter;
-        this.elements = textContent;
+        this.textContent = textContent;
     }
 
     /**
@@ -156,12 +156,12 @@ public final class FormTableCell extends FormContent {
 
     /**
      * Get the list of references to the text elements constituting this table cell
-     * When includeTextDetails is set to true.
+     * When includeTextContent is set to true.
      *
-     * @return the elements value.
+     * @return the {@code textContent} value.
      */
-    public List<FormContent> getElements() {
-        return this.elements;
+    public List<FormContent> getTextContent() {
+        return this.textContent;
     }
 
     /**
