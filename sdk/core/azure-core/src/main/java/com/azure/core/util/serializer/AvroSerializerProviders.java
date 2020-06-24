61a85640c61b994b020cee763c39f801e8925d37
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2020-06-24 15:21:34 -0700

    Moved .avro files into test resources, renamed AvroSerializerProxy to AvroSerializerProviders

--- sdk/core/azure-core/src/main/java/com/azure/core/util/serializer/AvroSerializerProviders.java
+++ sdk/core/azure-core/src/main/java/com/azure/core/util/serializer/AvroSerializerProviders.java
@@ -9,13 +9,14 @@ import java.util.ServiceLoader;
 /**
  * Service Provider Interface (SPI) proxy for {@link AvroSerializerProvider}.
  */
-public final class AvroSerializerProxy {
-    private static boolean attemptedLoading;
+public final class AvroSerializerProviders {
+    private static boolean loadAttempted;
     private static AvroSerializerProvider defaultProvider;
-    private static final String CANNOT_FIND_AVRO_SERIALIZER = "Cannot find any AvroSerializerProvider on the classpath "
-        + "- unable to create a default Avro ObjectSerializer instance";
 
-    private AvroSerializerProxy() {
+    private static final IllegalStateException NO_AVRO_SERIALIZER_EXCEPTION =
+        new IllegalStateException("No AvroSerializerProvicer found on the classpath.");
+
+    private AvroSerializerProviders() {
         // no-op
     }
 
@@ -23,16 +24,20 @@ public final class AvroSerializerProxy {
      * Given that not all SDKs will be using an Avro serializer interface the classpath instances are loaded lazily.
      */
     private static synchronized void load() {
-        if (attemptedLoading) {
+        if (loadAttempted) {
+            if (defaultProvider == null) {
+                throw NO_AVRO_SERIALIZER_EXCEPTION;
+            }
+
             return;
         }
 
-        attemptedLoading = true;
+        loadAttempted = true;
         Iterator<AvroSerializerProvider> it = ServiceLoader.load(AvroSerializerProvider.class).iterator();
         if (it.hasNext()) {
             defaultProvider = it.next();
         } else {
-            throw new IllegalStateException(CANNOT_FIND_AVRO_SERIALIZER);
+            throw NO_AVRO_SERIALIZER_EXCEPTION;
         }
     }
 
