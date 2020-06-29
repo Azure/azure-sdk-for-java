f9fc201f7cd792accf10a03037b4f9f6f99fb93f
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2020-06-29 13:22:33 -0700

    Add module to test coverage, fixed module name, removed dead configuration in POM

--- sdk/core/azure-core-experimental/src/main/java/module-info.java
+++ sdk/core/azure-core-experimental/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-module azure.core.experimental {
+module com.azure.core.experimental {
     requires transitive com.azure.core;
 
     exports com.azure.core.experimental.serializer;
