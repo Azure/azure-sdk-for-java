7a0fc351d37e69958bd3f8dd032e24ea195a8ef2
Arthur Erlendsson <arerlend@microsoft.com>
2020-07-06 14:12:45 -0700

    mono TryAdd

--- sdk/eventhubs/azure-messaging-eventhubs/src/main/java/com/azure/messaging/eventhubs/ObjectBatch.java
+++ sdk/eventhubs/azure-messaging-eventhubs/src/main/java/com/azure/messaging/eventhubs/ObjectBatch.java
@@ -5,28 +5,23 @@ import com.azure.core.amqp.implementation.ErrorContextProvider;
 import com.azure.core.amqp.implementation.TracerProvider;
 import com.azure.core.util.logging.ClientLogger;
 import com.azure.core.experimental.serializer.ObjectSerializer;
+import reactor.core.publisher.Mono;
 
 import java.io.ByteArrayOutputStream;
 import java.util.Objects;
 
+/**
+ * A class for aggregating Java objects into a single, size-limited, batch. Objects are serialized into EventData
+ * objects and are added to the batch.  It is treated as a single message when sent to the Azure Event Hubs service.
+ *
+ * @param <T> type of objects in the batch.  Multi-type batches are not permitted.
+ */
 public final class ObjectBatch<T> extends EventDataBatchBase {
     private final ClientLogger logger = new ClientLogger(ObjectBatch.class);
     private final Object lock = new Object();
     private final Class<T> batchType;
     private final ObjectSerializer objectSerializer;
 
-    /**
-     *
-     * @param maxMessageSize
-     * @param partitionId
-     * @param partitionKey
-     * @param batchType
-     * @param contextProvider
-     * @param tracerProvider
-     * @param objectSerializer
-     * @param entityPath
-     * @param hostname
-     */
     ObjectBatch(int maxMessageSize, String partitionId, String partitionKey, Class<T> batchType,
                     ErrorContextProvider contextProvider, TracerProvider tracerProvider,
                 ObjectSerializer objectSerializer, String entityPath, String hostname) {
@@ -45,7 +40,7 @@ public final class ObjectBatch<T> extends EventDataBatchBase {
      * @throws AmqpException if serialized object as {@link EventData} is larger than the maximum size
      *      of the {@link EventDataBatch}.
      */
-    public Boolean tryAdd(final T object) {
+    public Mono<Boolean> tryAdd(final T object) {
         if (object == null) {
             throw logger.logExceptionAsWarning(new IllegalArgumentException("object cannot be null"));
         }
@@ -55,6 +50,6 @@ public final class ObjectBatch<T> extends EventDataBatchBase {
             EventData eventData = new EventData(s.toByteArray());
             EventData event = tracerProvider.isEnabled() ? traceMessageSpan(eventData) : eventData;
             return tryAdd(event);
-        }).block();
+        });
     }
 }
