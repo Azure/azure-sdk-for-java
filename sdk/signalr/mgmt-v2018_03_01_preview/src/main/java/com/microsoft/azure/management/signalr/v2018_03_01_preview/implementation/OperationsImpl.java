56bf15b0a2b8f411b6d58485364e16e6a87f0efa
SDK Automation <sdkautomation@microsoft.com>
2020-07-08 07:45:39 +0000

    Generated from ce9cff5e66ca14a7149660f03fc5aa75afa569ce
    fix go sdk generation

--- sdk/signalr/mgmt-v2018_03_01_preview/src/main/java/com/microsoft/azure/management/signalr/v2018_03_01_preview/implementation/OperationsImpl.java
+++ sdk/signalr/mgmt-v2018_03_01_preview/src/main/java/com/microsoft/azure/management/signalr/v2018_03_01_preview/implementation/OperationsImpl.java
@@ -17,52 +17,33 @@ import com.microsoft.azure.Page;
 import com.microsoft.azure.management.signalr.v2018_03_01_preview.Operation;
 
 class OperationsImpl extends WrapperImpl<OperationsInner> implements Operations {
-    private final SignalRManager manager;
+    private final SignalRServiceManager manager;
 
-    OperationsImpl(SignalRManager manager) {
+    OperationsImpl(SignalRServiceManager manager) {
         super(manager.inner().operations());
         this.manager = manager;
     }
 
-    public SignalRManager manager() {
+    public SignalRServiceManager manager() {
         return this.manager;
     }
 
-    private Observable<Page<OperationInner>> listNextInnerPageAsync(String nextLink) {
-        if (nextLink == null) {
-            Observable.empty();
-        }
-        OperationsInner client = this.inner();
-        return client.listNextAsync(nextLink)
-        .flatMap(new Func1<Page<OperationInner>, Observable<Page<OperationInner>>>() {
-            @Override
-            public Observable<Page<OperationInner>> call(Page<OperationInner> page) {
-                return Observable.just(page).concatWith(listNextInnerPageAsync(page.nextPageLink()));
-            }
-        });
-    }
     @Override
     public Observable<Operation> listAsync() {
         OperationsInner client = this.inner();
         return client.listAsync()
-        .flatMap(new Func1<Page<OperationInner>, Observable<Page<OperationInner>>>() {
-            @Override
-            public Observable<Page<OperationInner>> call(Page<OperationInner> page) {
-                return listNextInnerPageAsync(page.nextPageLink());
-            }
-        })
         .flatMapIterable(new Func1<Page<OperationInner>, Iterable<OperationInner>>() {
             @Override
             public Iterable<OperationInner> call(Page<OperationInner> page) {
                 return page.items();
             }
-       })
+        })
         .map(new Func1<OperationInner, Operation>() {
             @Override
             public Operation call(OperationInner inner) {
                 return new OperationImpl(inner, manager());
             }
-       });
+        });
     }
 
 }
