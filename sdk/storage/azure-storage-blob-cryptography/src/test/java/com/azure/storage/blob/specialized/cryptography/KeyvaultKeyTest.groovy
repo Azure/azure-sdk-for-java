b12eba114671bfa02423661412abe99698f03d75
Gauri Prasad <gapra@microsoft.com>
2020-07-10 09:28:02 -0700

    Rerecorded tests

--- sdk/storage/azure-storage-blob-cryptography/src/test/java/com/azure/storage/blob/specialized/cryptography/KeyvaultKeyTest.groovy
+++ sdk/storage/azure-storage-blob-cryptography/src/test/java/com/azure/storage/blob/specialized/cryptography/KeyvaultKeyTest.groovy
@@ -1,18 +1,35 @@
 package com.azure.storage.blob.specialized.cryptography
 
+import com.azure.core.credential.TokenCredential
 import com.azure.core.cryptography.AsyncKeyEncryptionKey
+import com.azure.core.http.HttpClient
+import com.azure.core.http.HttpPipeline
+import com.azure.core.http.HttpPipelineBuilder
+import com.azure.core.http.netty.NettyAsyncHttpClientBuilder
+import com.azure.core.http.policy.BearerTokenAuthenticationPolicy
+import com.azure.core.http.policy.ExponentialBackoff
+import com.azure.core.http.policy.HttpLogDetailLevel
+import com.azure.core.http.policy.HttpLogOptions
+import com.azure.core.http.policy.HttpLoggingPolicy
+import com.azure.core.http.policy.HttpPipelinePolicy
+import com.azure.core.http.policy.HttpPolicyProviders
+import com.azure.core.http.policy.RetryPolicy
+import com.azure.core.http.policy.RetryStrategy
+import com.azure.core.http.policy.UserAgentPolicy
 import com.azure.core.test.TestMode
 import com.azure.core.util.Configuration
+import com.azure.identity.ClientSecretCredentialBuilder
 import com.azure.identity.DefaultAzureCredentialBuilder
-import com.azure.identity.EnvironmentCredentialBuilder
 import com.azure.security.keyvault.keys.KeyClient
 import com.azure.security.keyvault.keys.KeyClientBuilder
+import com.azure.security.keyvault.keys.KeyServiceVersion
 import com.azure.security.keyvault.keys.cryptography.KeyEncryptionKeyClientBuilder
 import com.azure.security.keyvault.keys.models.CreateRsaKeyOptions
 import com.azure.security.keyvault.keys.models.KeyVaultKey
 import com.azure.storage.blob.BlobContainerClient
 import com.azure.storage.common.implementation.Constants
 
+import java.time.Duration
 import java.time.OffsetDateTime
 
 class KeyvaultKeyTest extends APISpec {
@@ -23,21 +40,13 @@ class KeyvaultKeyTest extends APISpec {
     String keyId
 
     def setup() {
-        def keyVaultUrl = "https://azstoragesdkvailt.vault.azure.net/"
+        def keyVaultUrl = "https://azstoragesdkvault.vault.azure.net/"
         if (testMode != TestMode.PLAYBACK) {
             keyVaultUrl = Configuration.getGlobalConfiguration().get("KEYVAULT_URL")
         }
-        KeyClientBuilder builder = new KeyClientBuilder()
 
-        if (testMode != TestMode.PLAYBACK) {
-            if (testMode == TestMode.RECORD) {
-                builder.addPolicy(interceptorManager.getRecordPolicy())
-            }
-            // AZURE_TENANT_ID, AZURE_CLIENT_ID, AZURE_CLIENT_SECRET
-            builder.credential(new EnvironmentCredentialBuilder().build())
-        } else {
-            builder.credential(new DefaultAzureCredentialBuilder().build())
-        }
+        KeyClientBuilder builder = new KeyClientBuilder()
+            .pipeline(getHttpPipeline(getHttpClient(), KeyServiceVersion.V7_0))
 
         keyClient = builder
             .httpClient(getHttpClient())
@@ -98,6 +107,49 @@ class KeyvaultKeyTest extends APISpec {
 
         then:
         inputArray != os.toByteArray()
+    }
+
+    HttpPipeline getHttpPipeline(HttpClient httpClient, KeyServiceVersion serviceVersion) {
+        TokenCredential credential = null;
+
+        if (!interceptorManager.isPlaybackMode()) {
+            String clientId = System.getenv("AZURE_CLIENT_ID");
+            String clientKey = System.getenv("AZURE_CLIENT_SECRET");
+            String tenantId = System.getenv("AZURE_TENANT_ID");
+            Objects.requireNonNull(clientId, "The client id cannot be null");
+            Objects.requireNonNull(clientKey, "The client key cannot be null");
+            Objects.requireNonNull(tenantId, "The tenant id cannot be null");
+            credential = new ClientSecretCredentialBuilder()
+                .clientSecret(clientKey)
+                .clientId(clientId)
+                .tenantId(tenantId)
+                .build();
+        }
+
+        // Closest to API goes first, closest to wire goes last.
+        final List<HttpPipelinePolicy> policies = new ArrayList<>();
+        policies.add(new UserAgentPolicy("client_name", "client_version",  Configuration.getGlobalConfiguration().clone(), serviceVersion));
+        HttpPolicyProviders.addBeforeRetryPolicies(policies);
+        RetryStrategy strategy = new ExponentialBackoff(5, Duration.ofSeconds(2), Duration.ofSeconds(16));
+        policies.add(new RetryPolicy(strategy));
+        if (credential != null) {
+            policies.add(new BearerTokenAuthenticationPolicy(credential, "https://vault.azure.net/.default"));
+        }
+        HttpPolicyProviders.addAfterRetryPolicies(policies);
+        policies.add(new HttpLoggingPolicy(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BODY_AND_HEADERS)));
+
+        if (!interceptorManager.isPlaybackMode()) {
+            if (testMode == TestMode.RECORD) {
+                policies.add(interceptorManager.getRecordPolicy());
+            }
+        }
+
+        HttpPipeline pipeline = new HttpPipelineBuilder()
+            .policies(policies.toArray(new HttpPipelinePolicy[0]))
+            .httpClient(httpClient == null ? interceptorManager.getPlaybackClient() : httpClient)
+            .build();
+
+        return pipeline;
     }
 
 }
