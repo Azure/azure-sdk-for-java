7010ab85d210eb09896897cd0bfd6a36d8f8f8c3
Rujun Chen <rujche@microsoft.com>
2020-07-16 16:57:33 +0800

    Add license text in every java file.

--- sdk/appconfiguration/spring-cloud-azure-feature-management/src/test/java/com/microsoft/azure/spring/cloud/feature/manager/feature/filters/TargetingFilterTest.java
+++ sdk/appconfiguration/spring-cloud-azure-feature-management/src/test/java/com/microsoft/azure/spring/cloud/feature/manager/feature/filters/TargetingFilterTest.java
@@ -1,8 +1,5 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for
- * license information.
- */
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 package com.microsoft.azure.spring.cloud.feature.manager.feature.filters;
 
 import static org.junit.Assert.assertFalse;
