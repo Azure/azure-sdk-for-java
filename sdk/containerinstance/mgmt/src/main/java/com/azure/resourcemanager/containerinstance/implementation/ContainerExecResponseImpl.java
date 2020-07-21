663fd4459fc2f7dbeb552ed80fa5256568cfb211
Tanyi Chen <tanchen@microsoft.com>
2020-07-21 14:10:38 +0800

    auto fix some checkstyle

--- sdk/containerinstance/mgmt/src/main/java/com/azure/resourcemanager/containerinstance/implementation/ContainerExecResponseImpl.java
+++ sdk/containerinstance/mgmt/src/main/java/com/azure/resourcemanager/containerinstance/implementation/ContainerExecResponseImpl.java
@@ -1,7 +1,6 @@
-/**
- * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT License. See License.txt in the
- * project root for license information.
- */
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.azure.resourcemanager.containerinstance.implementation;
 
 import com.azure.resourcemanager.containerinstance.fluent.inner.ContainerExecResponseInner;
--- sdk/management/samples/src/main/java/com/azure/resourcemanager/containerinstance/samples/ManageContainerInstanceZeroToOneAndOneToManyUsingContainerServiceOrchestrator.java
+++ sdk/management/samples/src/main/java/com/azure/resourcemanager/containerinstance/samples/ManageContainerInstanceZeroToOneAndOneToManyUsingContainerServiceOrchestrator.java
@@ -45,9 +45,7 @@ import org.apache.commons.codec.binary.Base64;
 
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
-import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
