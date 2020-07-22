563cd445482169354d35c42f2fd8d22ceeffb865
Azad Abbasi <azabbasi@microsoft.com>
2020-07-22 15:19:19 -0700

    Use auto rest v4 to generate the code.

--- sdk/digitaltwins/azure-digitaltwins-core/autorest.md
+++ sdk/digitaltwins/azure-digitaltwins-core/autorest.md
@@ -12,6 +12,7 @@ Run `generate.ps1` in this directory to generate the code.
 input-file: swagger/digitaltwins.json
 output-folder: "./"
 license-header: MICROSOFT_MIT_SMALL
+use: C:/Users/azad_/Enlistments/autorest.java
 java:
     add-context-parameter: true
     namespace: com.azure.digitaltwins.core
--- sdk/digitaltwins/azure-digitaltwins-core/generate.ps1
+++ sdk/digitaltwins/azure-digitaltwins-core/generate.ps1
@@ -1,1 +1,3 @@
-autorest --java autorest.md
+autorest autorest.md
+
+# autorest --java --use=C:/Users/azad_/Enlistments/autorest.java --input-file=swagger/digitaltwins.json --output-folder=./ --license-header=MICROSOFT_MIT_SMALL --add-context-parameter=true    --namespace=com.azure.digitaltwins.core    --add-credentials=true    --sync-methods=none
