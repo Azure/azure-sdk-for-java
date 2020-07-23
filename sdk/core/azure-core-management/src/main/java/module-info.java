6047218132d28d340b6219a0420c2d9c88eeeb8b
xccc-msft <chuxi@microsoft.com>
2020-07-23 13:04:11 +0800

    update module-info

--- sdk/core/azure-core-management/src/main/java/module-info.java
+++ sdk/core/azure-core-management/src/main/java/module-info.java
@@ -9,6 +9,7 @@ module com.azure.core.management {
     exports com.azure.core.management.serializer;
     exports com.azure.core.management.exception;
     exports com.azure.core.management.profile;
+    exports com.azure.core.management.provider;
 
     opens com.azure.core.management to
         com.fasterxml.jackson.databind,
