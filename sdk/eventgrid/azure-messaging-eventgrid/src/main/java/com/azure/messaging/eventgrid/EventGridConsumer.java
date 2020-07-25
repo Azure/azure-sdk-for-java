d63b717a0b720c5669c0e546bb65a8e023f75d18
Soren Dahl <Soreloser2@users.noreply.github.com>
2020-07-24 17:09:17 -0700

    Remove consumer deserialization functionality
    As modules need to be added, so depend on Jackson and return custom data formats (like xml) as Strings, possibly change in future.

--- sdk/eventgrid/azure-messaging-eventgrid/src/main/java/com/azure/messaging/eventgrid/EventGridConsumer.java
+++ sdk/eventgrid/azure-messaging-eventgrid/src/main/java/com/azure/messaging/eventgrid/EventGridConsumer.java
@@ -4,11 +4,13 @@
 
 package com.azure.messaging.eventgrid;
 
+import com.azure.core.annotation.Fluent;
 import com.azure.core.experimental.serializer.JsonArray;
 import com.azure.core.experimental.serializer.JsonSerializer;
-import com.azure.core.experimental.serializer.JsonSerializerProviders;
-import com.azure.core.experimental.serializer.ObjectSerializer;
+import com.azure.core.serializer.json.jackson.JacksonJsonSerializerBuilder;
 import com.azure.core.util.CoreUtils;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import reactor.core.publisher.Flux;
 
 import java.io.ByteArrayInputStream;
@@ -23,32 +25,22 @@ import java.util.Map;
  * @see EventGridEvent
  * @see CloudEvent
  */
+@Fluent
 public final class EventGridConsumer {
 
     private final Map<String, Class<?>> typeMappings;
 
-    private JsonSerializer deserializer;
+
+    private final JsonSerializer deserializer;
 
     /**
      * Creates a new instance of the consumer with a default deserializer and system event mappings.
      */
     public EventGridConsumer() {
-        this(null);
-    }
-
-    /**
-     * Creates an instance of the consumer using a provided custom {@link JsonSerializer}. Contains all system
-     * event mappings.
-     * @param deserializer the serializer/deserializer to use. If none is provided a default will be loaded
-     *                     from the classpath.
-     */
-    public EventGridConsumer(JsonSerializer deserializer) {
         this.typeMappings = new HashMap<>(SystemEventMappings.getSystemEventMappings());
-        if (deserializer == null) {
-            this.deserializer = JsonSerializerProviders.createInstance();
-        } else {
-            this.deserializer = deserializer;
-        }
+        this.deserializer = new JacksonJsonSerializerBuilder()
+            .serializer(new ObjectMapper().registerModule(new JavaTimeModule()))
+            .build();
     }
 
     /**
@@ -69,22 +61,6 @@ public final class EventGridConsumer {
         return this;
     }
 
-    /**
-     * Add a custom deserializer to interpret the json as a rich event object. If this value is not set, the default
-     * is loaded from the classpath.
-     * @param deserializer the deserializer to use.
-     *
-     * @return the builder itself.
-     */
-    public EventGridConsumer deserializer(JsonSerializer deserializer) {
-        if (deserializer == null) {
-            // reset to default
-            this.deserializer = JsonSerializerProviders.createInstance();
-        } else {
-            this.deserializer = deserializer;
-        }
-        return this;
-    }
 
     /**
      * Deserialize the given JSON using the available mappings and return the rich EventGridEvent Object, with the
@@ -125,29 +101,13 @@ public final class EventGridConsumer {
     }
 
     /**
-     * Deserialize the given JSON using the available mappings and return the rich CloudEvent, with the
-     * data field as a rich object, if possible. Binary data will be directly serialized
-     * @param json the information to be deserialize as a JSON string.
-     *
-     * @return the deserialized events in a List.
-     */
-    public List<CloudEvent> deserializeCloudEvents(String json) {
-        return deserializeCloudEvents(json, null);
-    }
-
-    /**
-     * Deserialize the given JSON using the set deserializer and return the rich CloudEvent, with the data field as a
-     * rich object, if possible. This overload is to be used with data serialized not in "application/json" format,
-     * for example as an xml string or with the use of the {@link CloudEvent#setData(Object, ObjectSerializer, String)}
-     * overload. There should be a custom event mapping for the expected event type field which
-     * corresponds to the deserialized type expected in the data field.
-     * @param json                   the JSON string containing the event that was recieved from the EventGrid service.
-     * @param customDataDeserializer The deserializer to decode the data field, or null to use the same JSON
-     *                               deserializer behavior as the rest of the event.
+     * Deserialize the given JSON and return the rich CloudEvent, with the data field as a
+     * rich object, if possible.
+     * @param json the JSON string containing the event that was received from the EventGrid service.
      *
      * @return the deserialized cloud events in a list, with rich object data.
      */
-    public List<CloudEvent> deserializeCloudEvents(String json, ObjectSerializer customDataDeserializer) {
+    public List<CloudEvent> deserializeCloudEvents(String json) {
         return deserializer.toTree(new ByteArrayInputStream(json.getBytes(StandardCharsets.UTF_8)))
             .flatMapMany(jsonNode -> {
                 if (jsonNode.isArray()) {
@@ -160,22 +120,14 @@ public final class EventGridConsumer {
                         .as(Flux::from);
                 }
             })
-            .map(cloudEvent -> richDataAndConvert(cloudEvent, customDataDeserializer))
+            .map(this::richDataAndConvert)
             .collectList()
             .block();
     }
 
-    private CloudEvent richDataAndConvert(com.azure.messaging.eventgrid.implementation.models.CloudEvent
-                                              event, ObjectSerializer objectSerializer) {
+    private CloudEvent richDataAndConvert(com.azure.messaging.eventgrid.implementation.models.CloudEvent event) {
         String eventType = SystemEventMappings.canonicalizeEventType(event.getType());
         if (typeMappings.containsKey(eventType)) {
-            if (objectSerializer != null) {
-                // data was custom serialized, expect a json string as data
-                objectSerializer.deserialize(new ByteArrayInputStream(((String) event.getData())
-                        .getBytes(StandardCharsets.UTF_8)),
-                    typeMappings.get(eventType))
-                    .subscribe(event::setData);
-            }
             deserializer.toTree(event.getData())
                 .flatMap(jsonNode -> deserializer.deserializeTree(jsonNode, typeMappings.get(eventType)))
                 .subscribe(event::setData);
