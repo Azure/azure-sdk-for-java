1314c5bd879b3f6b4387d7527bef77b4e0561969
Sima Zhu <sizhu@microsoft.com>
2020-07-27 08:48:46 -0700

    Fixed linting issue

--- sdk/search/azure-search-documents/src/main/java/com/azure/search/documents/indexes/implementation/SearchServiceClientImplBuilder.java
+++ sdk/search/azure-search-documents/src/main/java/com/azure/search/documents/indexes/implementation/SearchServiceClientImplBuilder.java
@@ -12,8 +12,6 @@ import com.azure.core.http.HttpPipelineBuilder;
 import com.azure.core.http.policy.CookiePolicy;
 import com.azure.core.http.policy.RetryPolicy;
 import com.azure.core.http.policy.UserAgentPolicy;
-import com.azure.core.util.serializer.JacksonAdapter;
-import com.azure.core.util.serializer.SerializerAdapter;
 
 /** A builder for creating a new instance of the SearchServiceClient type. */
 @ServiceClientBuilder(serviceClients = {SearchServiceClientImpl.class})
