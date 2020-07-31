1aa38288669ee78e32473e3a0d17f2d9691b9826
Moary Chen <moary@126.com>
2020-07-31 14:33:09 +0800

    migrate spring integration azure repo

---
+++ sdk/spring/azure-spring-integration-eventhubs/src/test/java/com/microsoft/azure/spring/integration/eventhub/converter/EventHubMessageConverterTest.java
@@ -0,0 +1,26 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.microsoft.azure.spring.integration.eventhub.converter;
+
+import com.azure.messaging.eventhubs.EventData;
+import com.microsoft.azure.spring.integration.core.converter.AzureMessageConverter;
+import com.microsoft.azure.spring.integration.test.support.AzureMessageConverterTest;
+
+public class EventHubMessageConverterTest extends AzureMessageConverterTest<EventData> {
+
+    @Override
+    protected EventData getInstance() {
+        return new EventData(this.payload.getBytes());
+    }
+
+    @Override
+    public AzureMessageConverter<EventData> getConverter() {
+        return new EventHubMessageConverter();
+    }
+
+    @Override
+    protected Class<EventData> getTargetClass() {
+        return EventData.class;
+    }
+}
---
+++ sdk/spring/azure-spring-integration-test/README.md
@@ -0,0 +1,9 @@
+# Azure Spring Integration test library for Java
+
+## Key concepts
+## Getting started
+## Key concepts
+## Examples
+## Troubleshooting
+## Next steps
+## Contributing
