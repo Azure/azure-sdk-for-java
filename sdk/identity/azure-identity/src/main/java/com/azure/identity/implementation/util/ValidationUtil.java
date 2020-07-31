8e2e86f6a8b8bb7136b318836628a552034498c4
g2vinay <vinay_work@hotmail.com>
2020-07-31 10:02:33 -0700

    fix checkstyle

--- sdk/identity/azure-identity/src/main/java/com/azure/identity/implementation/util/ValidationUtil.java
+++ sdk/identity/azure-identity/src/main/java/com/azure/identity/implementation/util/ValidationUtil.java
@@ -11,7 +11,6 @@ import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
@@ -66,7 +65,8 @@ public final class ValidationUtil {
         if (id != null) {
             if (!tenantIdentifierCharPattern.matcher(id).matches()) {
                 throw logger.logExceptionAsError(
-                    new IllegalArgumentException("Tenant id must have characters in the range of [0-9], [a-z], '-', '.'"));
+                    new IllegalArgumentException(
+                        "Tenant id must have characters in the range of [0-9], [a-z], '-', '.'"));
             }
         }
     }
@@ -81,7 +81,8 @@ public final class ValidationUtil {
                 if (!absolutePath.equals(normalizedPath)) {
                     throw logger.logExceptionAsError(
                         new IllegalArgumentException(
-                            String.format("%s is not valid. The path contains invalid characters `.` or `..`", pathName)));
+                            String.format(
+                                "%s is not valid. The path contains invalid characters `.` or `..`", pathName)));
                 }
             }
         }
