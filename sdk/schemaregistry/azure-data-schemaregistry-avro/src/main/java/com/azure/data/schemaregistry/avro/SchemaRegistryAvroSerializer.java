a35565ad083aad393cebe74770b35df52a56346b
Arthur Erlendsson <30675661+arerlend@users.noreply.github.com>
2020-08-04 10:38:18 -0700

    Introduce aggregate serializer (#13779)
    * initial work
    
    * aggregate serializer
    
    * aggregate serializer

--- sdk/schemaregistry/azure-data-schemaregistry-avro/src/main/java/com/azure/data/schemaregistry/avro/SchemaRegistryAvroSerializer.java
+++ sdk/schemaregistry/azure-data-schemaregistry-avro/src/main/java/com/azure/data/schemaregistry/avro/SchemaRegistryAvroSerializer.java
@@ -3,15 +3,27 @@
 
 package com.azure.data.schemaregistry.avro;
 
-import com.azure.core.experimental.serializer.ObjectSerializer;
-import com.azure.core.experimental.serializer.TypeReference;
 import com.azure.core.util.logging.ClientLogger;
+import com.azure.core.util.serializer.ObjectSerializer;
+import com.azure.core.util.serializer.TypeReference;
 import com.azure.data.schemaregistry.SchemaRegistrySerializer;
 import com.azure.data.schemaregistry.SchemaRegistryAsyncClient;
+import com.azure.data.schemaregistry.models.SerializationType;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericDatumReader;
+import org.apache.avro.generic.GenericDatumWriter;
+import org.apache.avro.io.*;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.specific.SpecificDatumWriter;
+import org.apache.avro.specific.SpecificRecord;
 import reactor.core.publisher.Mono;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Collections;
+import java.util.Objects;
 
 /**
  * Asynchronous registry-based serializer implementation.
@@ -19,17 +31,68 @@ import java.util.Collections;
 public final class SchemaRegistryAvroSerializer extends SchemaRegistrySerializer implements ObjectSerializer {
     private final ClientLogger logger = new ClientLogger(SchemaRegistryAvroSerializer.class);
 
+    private static final EncoderFactory ENCODER_FACTORY = EncoderFactory.get();
+    private static final DecoderFactory DECODER_FACTORY = DecoderFactory.get();
+    private static final Boolean AVRO_SPECIFIC_READER_DEFAULT = false;
+
+    private final Boolean avroSpecificReader;
+
     /**
      *
      * @param registryClient
-     * @param codec
+     * @param avroSpecificReader
      * @param schemaGroup
      * @param autoRegisterSchemas
      */
-    SchemaRegistryAvroSerializer(SchemaRegistryAsyncClient registryClient, AvroSchemaRegistryCodec codec,
+    SchemaRegistryAvroSerializer(SchemaRegistryAsyncClient registryClient, Boolean avroSpecificReader,
                                       String schemaGroup, Boolean autoRegisterSchemas) {
-        super(registryClient, codec, Collections.singletonList(codec), autoRegisterSchemas, schemaGroup);
+        super(registryClient, autoRegisterSchemas, schemaGroup);
+
+        if (avroSpecificReader == null) {
+            this.avroSpecificReader = SchemaRegistryAvroSerializer.AVRO_SPECIFIC_READER_DEFAULT;
+        }
+        else {
+            this.avroSpecificReader = avroSpecificReader;
+        }
     }
+
+    /**
+     *
+     * @return serialization type
+     */
+    protected SerializationType getSerializationType() {
+        return SerializationType.AVRO;
+    };
+
+
+    /**
+     * Returns schema name for storing schemas in schema registry store.
+     *
+     * @param object Schema object used to generate schema path
+     * @return schema name as string
+     */
+    protected String getSchemaName(Object object) {
+        return AvroSchemaUtils.getSchema(object).getFullName();
+    }
+
+    /**
+     * @param object Schema object used to generate schema string
+     * @see AvroSchemaUtils for distinction between primitive and Avro schema generation
+     * @return string representation of schema
+     */
+    protected String getSchemaString(Object object) {
+        Schema schema = AvroSchemaUtils.getSchema(object);
+        return schema.toString();
+    }
+
+    /**
+     * @param schemaString string representation of schema
+     * @return avro schema
+     */
+    Schema parseSchemaString(String schemaString) {
+        return (new Schema.Parser()).parse(schemaString);
+    }
+
 
     @Override
     public <T> T deserialize(InputStream stream, TypeReference<T> typeReference) {
@@ -37,8 +100,7 @@ public final class SchemaRegistryAvroSerializer extends SchemaRegistrySerializer
     }
 
     @Override
-    public <T> Mono<T> deserializeAsync(InputStream stream,
-        TypeReference<T> typeReference) {
+    public <T> Mono<T> deserializeAsync(InputStream stream, TypeReference<T> typeReference) {
         return null;
     }
 
@@ -66,5 +128,87 @@ public final class SchemaRegistryAvroSerializer extends SchemaRegistrySerializer
 //                throw logger.logExceptionAsError(new IllegalStateException("Deserialized object not of class %s"));
 //            });
 //    }
+
+    /**
+     * Returns ByteArrayOutputStream containing Avro encoding of object parameter
+     * @param object Object to be encoded into byte stream
+     * @return closed ByteArrayOutputStream
+     */
+    protected byte[] encode(Object object) {
+        Schema schema = AvroSchemaUtils.getSchema(object);
+
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            if (object instanceof byte[]) {
+                out.write((byte[]) object); // todo: real avro byte arrays require writing array size to buffer
+            } else {
+                BinaryEncoder encoder = ENCODER_FACTORY.directBinaryEncoder(out, null);
+                DatumWriter<Object> writer;
+                if (object instanceof SpecificRecord) {
+                    writer = new SpecificDatumWriter<>(schema);
+                } else {
+                    writer = new GenericDatumWriter<>(schema);
+                }
+                writer.write(object, encoder);
+                encoder.flush();
+            }
+            return out.toByteArray();
+        } catch (IOException | RuntimeException e) {
+            // Avro serialization can throw AvroRuntimeException, NullPointerException, ClassCastException, etc
+            throw logger.logExceptionAsError(
+                new IllegalStateException("Error serializing Avro message", e));
+        }
+    }
+
+
+    /**
+     * @param b byte array containing encoded bytes
+     * @param object schema for Avro reader read - fetched from Azure Schema Registry
+     * @return deserialized object
+     */
+    protected Object decode(byte[] b, Object object) {
+        Objects.requireNonNull(object, "Schema must not be null.");
+
+        if (!(object instanceof Schema)) {
+            throw logger.logExceptionAsError(
+                new IllegalStateException("Object must be an Avro schema."));
+        }
+        Schema schema = (Schema) object;
+
+        if (schema.getType().equals(Schema.Type.BYTES)) {
+            return b;
+        }
+
+        DatumReader<?> reader = getDatumReader(schema);
+
+        try {
+            Object result = reader.read(null, DECODER_FACTORY.binaryDecoder(b, null));
+
+            if (schema.getType().equals(Schema.Type.STRING)) {
+                return result.toString();
+            }
+
+            return result;
+        } catch (IOException | RuntimeException e) {
+            // avro deserialization may throw AvroRuntimeException, NullPointerException, etc
+            throw logger.logExceptionAsError(new IllegalStateException("Error deserializing Avro message.", e));
+        }
+    }
+
+    /**
+     * Returns correct reader for decoding payload.
+     *
+     * @param writerSchema Avro schema fetched from schema registry store
+     * @return correct Avro DatumReader object given encoder configuration
+     */
+    private DatumReader<?> getDatumReader(Schema writerSchema) {
+        boolean writerSchemaIsPrimitive = AvroSchemaUtils.getPrimitiveSchemas().values().contains(writerSchema);
+        // do not use SpecificDatumReader if writerSchema is a primitive
+        if (avroSpecificReader && !writerSchemaIsPrimitive) {
+            return new SpecificDatumReader<>(writerSchema);
+        } else {
+            return new GenericDatumReader<>(writerSchema);
+        }
+    }
 }
 
---
+++ sdk/schemaregistry/azure-data-schemaregistry/src/main/java/com/azure/data/schemaregistry/AggregateSchemaRegistrySerializer.java
@@ -0,0 +1,115 @@
+package com.azure.data.schemaregistry;
+
+import com.azure.core.util.logging.ClientLogger;
+import com.azure.core.util.serializer.ObjectSerializer;
+import com.azure.core.util.serializer.TypeReference;
+import com.azure.data.schemaregistry.models.SerializationType;
+import reactor.core.publisher.Mono;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentSkipListMap;
+
+public class AggregateSchemaRegistrySerializer implements ObjectSerializer {
+    private final ClientLogger logger = new ClientLogger(AggregateSchemaRegistrySerializer.class);
+
+    private SchemaRegistryAsyncClient schemaRegistryClient;
+    private SchemaRegistrySerializer serializer;
+    private Map<String, SchemaRegistrySerializer> deserializerMap =
+        new ConcurrentSkipListMap<>(String.CASE_INSENSITIVE_ORDER);
+
+    public AggregateSchemaRegistrySerializer(SchemaRegistrySerializer serializer,
+                                             List<SchemaRegistrySerializer> deserializerList) {
+        this.serializer = serializer;
+        this.schemaRegistryClient = serializer.schemaRegistryClient;
+        for (SchemaRegistrySerializer d : deserializerList) {
+            if (this.deserializerMap.containsKey(d.getSerializationType())) {
+                throw logger.logExceptionAsError(
+                    new IllegalArgumentException("Only on Codec can be provided per schema serialization type."));
+            }
+            this.deserializerMap.put(d.getSerializationType().toString(), d);
+        }
+    }
+
+    @Override
+    public <T> T deserialize(InputStream stream, TypeReference<T> typeReference) {
+        return deserializeAsync(stream, typeReference).block();
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T> Mono<T> deserializeAsync(InputStream stream, TypeReference<T> typeReference) {
+        Objects.requireNonNull(stream, "'stream' cannot be null.");
+
+        if (!Object.class.equals(typeReference.getJavaType())) {
+            throw logger.logExceptionAsError(new IllegalArgumentException(
+                "Aggregate deserialization requires TypeReference<Object> type reference parameter."));
+        }
+
+        return Mono.fromCallable(() -> {
+            byte[] payload = new byte[stream.available()];
+            while (stream.read(payload) != -1) {}
+            return payload;
+        }).flatMap(payload -> {
+            if (payload == null || payload.length == 0) {
+                return Mono.empty();
+            }
+
+            ByteBuffer buffer = ByteBuffer.wrap(payload);
+            byte[] schemaGuidByteArray = new byte[SchemaRegistrySerializer.SCHEMA_ID_SIZE];
+            buffer.get(schemaGuidByteArray);
+
+            String schemaId = new String(schemaGuidByteArray, StandardCharsets.UTF_8);
+
+            return this.schemaRegistryClient.getSchema(schemaId)
+//                    .onErrorMap(IOException.class,
+//                        e -> logger.logExceptionAsError(new SerializationException(e.getMessage(), e)))
+                .handle((registryObject, sink) -> {
+                    Object payloadSchema = registryObject.getSchema();
+
+                    if (payloadSchema == null) {
+                        sink.error(logger.logExceptionAsError(
+                            new NullPointerException(
+                                String.format("Payload schema returned as null. Schema type: %s, Schema ID: %s",
+                                    registryObject.getSerializationType(), registryObject.getSchemaId()))));
+                        return;
+                    }
+
+                    int start = buffer.position() + buffer.arrayOffset();
+                    int length = buffer.limit() - SchemaRegistrySerializer.SCHEMA_ID_SIZE;
+                    byte[] b = Arrays.copyOfRange(buffer.array(), start, start + length);
+
+                    SchemaRegistrySerializer innerDeserializer = getDeserializer(registryObject.getSerializationType());
+                    Object deserialized = innerDeserializer.decode(b, payloadSchema);
+                    sink.next((T)deserialized);
+                });
+        });
+    }
+
+    @Override
+    public <S extends OutputStream> S serialize(S stream, Object value) {
+        return serializer.serializeAsync(stream, value).block();
+    }
+
+    @Override
+    public <S extends OutputStream> Mono<S> serializeAsync(S stream, Object value) {
+        return serializer.serializeAsync(stream, value);
+    }
+
+    private SchemaRegistrySerializer getDeserializer(SerializationType type) {
+        SchemaRegistrySerializer serializer = deserializerMap.get(type);
+        if (serializer == null) {
+            throw logger.logExceptionAsError(
+                new NullPointerException(
+                    String.format("No deserializer class found for serialization type '%s'.", type)
+                ));
+        }
+        return serializer;
+    }
+}
