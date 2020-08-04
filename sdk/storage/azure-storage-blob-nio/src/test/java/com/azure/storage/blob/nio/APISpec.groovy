b5ea843dcfe67c28f025d41762f5156ddfc88c99
Rick Ley <frley@microsoft.com>
2020-08-04 13:48:10 -0700

    Trying to fix github

--- sdk/storage/azure-storage-blob-nio/src/test/java/com/azure/storage/blob/nio/APISpec.groovy
+++ sdk/storage/azure-storage-blob-nio/src/test/java/com/azure/storage/blob/nio/APISpec.groovy
@@ -159,8 +159,7 @@ class APISpec extends Specification {
 
         interceptorManager.close()
     }
-
-    //TODO: Should this go in core.
+    
     static Mono<ByteBuffer> collectBytesInBuffer(Flux<ByteBuffer> content) {
         return FluxUtil.collectBytesInByteBufferStream(content).map { bytes -> ByteBuffer.wrap(bytes) }
     }
