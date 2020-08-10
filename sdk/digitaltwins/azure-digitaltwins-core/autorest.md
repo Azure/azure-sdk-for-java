36463f780df1914d87faaad0d3b5a5793a3cd0eb
Azad Abbasi <azabbasi@microsoft.com>
2020-08-10 09:55:00 -0700

    Run autorest off of the source instead of local build

--- sdk/digitaltwins/azure-digitaltwins-core/autorest.md
+++ sdk/digitaltwins/azure-digitaltwins-core/autorest.md
@@ -12,7 +12,7 @@ Run `generate.ps1` in this directory to generate the code.
 input-file: swagger/digitaltwins.json
 output-folder: "./"
 license-header: MICROSOFT_MIT_SMALL
-use: C:/Users/azad_/Enlistments/autorest.java
+use: '@autorest/java@4.0.1'
 java:
     add-context-parameter: true
     namespace: com.azure.digitaltwins.core
--- sdk/digitaltwins/azure-digitaltwins-core/generate.ps1
+++ sdk/digitaltwins/azure-digitaltwins-core/generate.ps1
@@ -1,3 +1,2 @@
+npm i -g autorest
 autorest autorest.md
-
-# autorest --java --use=C:/Users/azad_/Enlistments/autorest.java --input-file=swagger/digitaltwins.json --output-folder=./ --license-header=MICROSOFT_MIT_SMALL --add-context-parameter=true    --namespace=com.azure.digitaltwins.core    --add-credentials=true    --sync-methods=none
