172651932a0484f23e4b5a63dac26d0947625b1e
Jack Lu <dbqp99@msn.com>
2020-08-10 15:28:54 +0800

    Modified according to Checkstyle

--- sdk/spring/azure-spring-cloud-test-eventhubs/src/test/java/com/microsoft/azure/eventhubs/Application.java
+++ sdk/spring/azure-spring-cloud-test-eventhubs/src/test/java/com/microsoft/azure/eventhubs/Application.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.microsoft.azure.eventhubs;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
