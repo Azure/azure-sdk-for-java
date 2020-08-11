0a2ff3755f8fd75f88652ac3108b3216df3252e8
timtay-microsoft <timtay@microsoft.com>
2020-08-11 14:46:12 -0700

    squash

--- sdk/digitaltwins/azure-digitaltwins-core/src/main/java/com/azure/digitaltwins/core/DigitalTwinsServiceVersion.java
+++ sdk/digitaltwins/azure-digitaltwins-core/src/main/java/com/azure/digitaltwins/core/DigitalTwinsServiceVersion.java
@@ -1,7 +1,13 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.azure.digitaltwins.core;
 
 import com.azure.core.util.ServiceVersion;
 
+/**
+ * The service API versions of Azure Digital Twins that are supported by this client.
+ */
 public enum DigitalTwinsServiceVersion implements ServiceVersion {
     V2020_05_31_preview("2020-05-31-preview");
 
@@ -11,12 +17,19 @@ public enum DigitalTwinsServiceVersion implements ServiceVersion {
         this.version = version;
     }
 
-    public static DigitalTwinsServiceVersion getLatest() {
-        return V2020_05_31_preview;
-    }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String getVersion() {
         return this.version;
+    }
+
+    /**
+     * Gets the latest service API version of Azure Digital Twins that is supported by this client.
+     * @return The latest service API version of Azure Digital Twins that is supported by this client.
+     */
+    public static DigitalTwinsServiceVersion getLatest() {
+        return V2020_05_31_preview;
     }
 }
