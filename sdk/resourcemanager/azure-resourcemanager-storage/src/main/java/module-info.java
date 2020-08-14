b829c164fce309281cf2a9c24563b938b4e5f8fe
xccc-msft <chuxi@microsoft.com>
2020-08-14 11:54:44 +0800

    add module-info for storage

---
+++ sdk/resourcemanager/azure-resourcemanager-storage/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.azure.resourcemanager.storage {
+    requires transitive com.azure.resourcemanager.resources;
+
+    exports com.azure.resourcemanager.storage;
+    exports com.azure.resourcemanager.storage.fluent;
+    exports com.azure.resourcemanager.storage.fluent.inner;
+    exports com.azure.resourcemanager.storage.models;
+}
