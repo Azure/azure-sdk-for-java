80db3dc1d7563115c771390c4616cae65c0928fe
xccc-msft <chuxi@microsoft.com>
2020-08-14 11:51:16 +0800

    add azure-resourcemanager-test

---
+++ sdk/resourcemanager/azure-resourcemanager-test/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.azure.resourcemanager.test {
+    requires com.azure.core.management;
+    requires com.azure.core.test;
+    requires com.azure.http.netty;
+    requires com.azure.identity;
+
+    exports com.azure.resourcemanager.test;
+}
