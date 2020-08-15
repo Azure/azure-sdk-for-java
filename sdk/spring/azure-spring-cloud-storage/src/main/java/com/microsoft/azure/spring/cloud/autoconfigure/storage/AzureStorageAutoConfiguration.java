06d8aa933e7936d2bf3ffd63cb4a6e82d114f9b9
Yev Bronshteyn <yebronsh@microsoft.com>
2020-08-14 21:45:21 -0400

    Adding storage resource sample, autoconfiguration for blob storage with default identity

--- sdk/spring/azure-spring-cloud-storage/src/main/java/com/microsoft/azure/spring/cloud/autoconfigure/storage/AzureStorageAutoConfiguration.java
+++ sdk/spring/azure-spring-cloud-storage/src/main/java/com/microsoft/azure/spring/cloud/autoconfigure/storage/AzureStorageAutoConfiguration.java
@@ -3,16 +3,12 @@
 
 package com.microsoft.azure.spring.cloud.autoconfigure.storage;
 
-import com.azure.core.http.policy.HttpLogOptions;
-import com.azure.storage.blob.BlobServiceClientBuilder;
-import com.azure.storage.file.share.ShareServiceClientBuilder;
-import com.microsoft.azure.management.storage.StorageAccount;
-import com.microsoft.azure.spring.cloud.autoconfigure.context.AzureContextAutoConfiguration;
-import com.microsoft.azure.spring.cloud.context.core.api.EnvironmentProvider;
-import com.microsoft.azure.spring.cloud.context.core.api.ResourceManagerProvider;
-import com.microsoft.azure.spring.cloud.context.core.storage.StorageConnectionStringProvider;
-import com.microsoft.azure.spring.cloud.storage.AzureStorageProtocolResolver;
-import com.microsoft.azure.spring.cloud.telemetry.TelemetryCollector;
+import static com.microsoft.azure.spring.cloud.context.core.util.Constants.SPRING_CLOUD_STORAGE_BLOB_APPLICATION_ID;
+import static com.microsoft.azure.spring.cloud.context.core.util.Constants.SPRING_CLOUD_STORAGE_FILE_SHARE_APPLICATION_ID;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,11 +20,20 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.env.Environment;
 
-import javax.annotation.PostConstruct;
-
-import static com.microsoft.azure.spring.cloud.context.core.util.Constants.SPRING_CLOUD_STORAGE_BLOB_APPLICATION_ID;
-import static com.microsoft.azure.spring.cloud.context.core.util.Constants.SPRING_CLOUD_STORAGE_FILE_SHARE_APPLICATION_ID;
+import com.azure.core.credential.TokenCredential;
+import com.azure.core.http.policy.HttpLogOptions;
+import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.azure.storage.file.share.ShareServiceClientBuilder;
+import com.microsoft.azure.identity.spring.SpringEnvironmentTokenBuilder;
+import com.microsoft.azure.management.storage.StorageAccount;
+import com.microsoft.azure.spring.cloud.autoconfigure.context.AzureContextAutoConfiguration;
+import com.microsoft.azure.spring.cloud.context.core.api.EnvironmentProvider;
+import com.microsoft.azure.spring.cloud.context.core.api.ResourceManagerProvider;
+import com.microsoft.azure.spring.cloud.context.core.storage.StorageConnectionStringProvider;
+import com.microsoft.azure.spring.cloud.storage.AzureStorageProtocolResolver;
+import com.microsoft.azure.spring.cloud.telemetry.TelemetryCollector;
 
 /**
  * An auto-configuration for Azure Storage Account
@@ -37,7 +42,7 @@ import static com.microsoft.azure.spring.cloud.context.core.util.Constants.SPRIN
  */
 @Configuration
 @AutoConfigureAfter(AzureContextAutoConfiguration.class)
-@ConditionalOnClass({BlobServiceClientBuilder.class, ShareServiceClientBuilder.class})
+@ConditionalOnClass({ BlobServiceClientBuilder.class, ShareServiceClientBuilder.class })
 @ConditionalOnProperty(name = "spring.cloud.azure.storage.account")
 @EnableConfigurationProperties(AzureStorageProperties.class)
 public class AzureStorageAutoConfiguration {
@@ -53,54 +58,63 @@ public class AzureStorageAutoConfiguration {
         TelemetryCollector.getInstance().addService(STORAGE);
     }
 
+    @Autowired
+    private Environment environment;
+
     @Bean
     @ConditionalOnMissingBean
     public BlobServiceClientBuilder blobServiceClientBuilder(AzureStorageProperties storageProperties,
-                                                             EnvironmentProvider environmentProvider) {
-        String connectionString;
+            EnvironmentProvider environmentProvider) {
 
-        if (resourceManagerProvider != null) {
-            String accountName = storageProperties.getAccount();
+        BlobServiceClientBuilder authenticatedClientBuilder = null;
 
-            StorageAccount storageAccount = resourceManagerProvider.getStorageAccountManager().getOrCreate(accountName);
-            connectionString = StorageConnectionStringProvider.getConnectionString(storageAccount,
-                environmentProvider.getEnvironment(), storageProperties.isSecureTransfer());
+        // Use storage credentials where provided, default identity otherwise.
+        if (StringUtils.isNotBlank(storageProperties.getAccessKey())) {
+            String connectionString = null;
+            if (resourceManagerProvider != null) {
+                StorageAccount storageAccount = resourceManagerProvider.getStorageAccountManager()
+                        .getOrCreate(storageProperties.getAccount());
+                connectionString = StorageConnectionStringProvider.getConnectionString(storageAccount,
+                        environmentProvider.getEnvironment(), storageProperties.isSecureTransfer());
+            } else {
+                connectionString = StorageConnectionStringProvider.getConnectionString(storageProperties.getAccount(),
+                        storageProperties.getAccessKey(), environmentProvider.getEnvironment());
+                TelemetryCollector.getInstance().addProperty(STORAGE, ACCOUNT_NAME, storageProperties.getAccount());
+            }
+            authenticatedClientBuilder = new BlobServiceClientBuilder().connectionString(connectionString);
+
         } else {
-            connectionString = StorageConnectionStringProvider
-                .getConnectionString(storageProperties.getAccount(), storageProperties.getAccessKey(),
-                    environmentProvider.getEnvironment());
-            TelemetryCollector.getInstance().addProperty(STORAGE, ACCOUNT_NAME, storageProperties.getAccount());
+            TokenCredential defaultIdentityCredential = new SpringEnvironmentTokenBuilder().fromEnvironment(environment)
+                    .defaultCredential().build();
+            authenticatedClientBuilder = new BlobServiceClientBuilder().credential(defaultIdentityCredential);
         }
 
-
-        return new BlobServiceClientBuilder().connectionString(connectionString)
-            .httpLogOptions(new HttpLogOptions().setApplicationId(SPRING_CLOUD_STORAGE_BLOB_APPLICATION_ID));
+        return authenticatedClientBuilder
+                .httpLogOptions(new HttpLogOptions().setApplicationId(SPRING_CLOUD_STORAGE_BLOB_APPLICATION_ID));
     }
 
     @Bean
     @ConditionalOnMissingBean
     public ShareServiceClientBuilder shareServiceClientBuilder(AzureStorageProperties storageProperties,
-                                                               EnvironmentProvider environmentProvider) {
+            EnvironmentProvider environmentProvider) {
         String connectionString;
-
+        String connectionString1;
         if (resourceManagerProvider != null) {
             String accountName = storageProperties.getAccount();
 
             StorageAccount storageAccount = resourceManagerProvider.getStorageAccountManager().getOrCreate(accountName);
-
-            connectionString = StorageConnectionStringProvider
-                .getConnectionString(storageAccount, environmentProvider.getEnvironment(),
-                    storageProperties.isSecureTransfer());
+            connectionString1 = StorageConnectionStringProvider.getConnectionString(storageAccount,
+                    environmentProvider.getEnvironment(), storageProperties.isSecureTransfer());
         } else {
-            connectionString = StorageConnectionStringProvider
-                .getConnectionString(storageProperties.getAccount(), storageProperties.getAccessKey(),
-                    environmentProvider.getEnvironment());
+            connectionString1 = StorageConnectionStringProvider.getConnectionString(storageProperties.getAccount(),
+                    storageProperties.getAccessKey(), environmentProvider.getEnvironment());
             TelemetryCollector.getInstance().addProperty(STORAGE, ACCOUNT_NAME, storageProperties.getAccount());
         }
 
+        connectionString = connectionString1;
 
         return new ShareServiceClientBuilder().connectionString(connectionString)
-            .httpLogOptions(new HttpLogOptions().setApplicationId(SPRING_CLOUD_STORAGE_FILE_SHARE_APPLICATION_ID));
+                .httpLogOptions(new HttpLogOptions().setApplicationId(SPRING_CLOUD_STORAGE_FILE_SHARE_APPLICATION_ID));
     }
 
     @Configuration
