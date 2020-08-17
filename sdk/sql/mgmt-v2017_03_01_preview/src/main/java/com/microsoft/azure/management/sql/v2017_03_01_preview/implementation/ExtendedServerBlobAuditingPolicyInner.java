7631c4abbf2667bea76a3211b186975b6b7f0bf5
SDK Automation <sdkautomation@microsoft.com>
2020-08-17 19:06:21 +0000

    Generated from 91d8290a5bc2faac20a299e3b799813843c228b8
    Renaming the optional parameter to storageAccountType

--- sdk/sql/mgmt-v2017_03_01_preview/src/main/java/com/microsoft/azure/management/sql/v2017_03_01_preview/implementation/ExtendedServerBlobAuditingPolicyInner.java
+++ sdk/sql/mgmt-v2017_03_01_preview/src/main/java/com/microsoft/azure/management/sql/v2017_03_01_preview/implementation/ExtendedServerBlobAuditingPolicyInner.java
@@ -37,15 +37,23 @@ public class ExtendedServerBlobAuditingPolicyInner extends ProxyResource {
     /**
      * Specifies the blob storage endpoint (e.g.
      * https://MyAccount.blob.core.windows.net). If state is Enabled,
-     * storageEndpoint is required.
+     * storageEndpoint or isAzureMonitorTargetEnabled is required.
      */
     @JsonProperty(value = "properties.storageEndpoint")
     private String storageEndpoint;
 
     /**
-     * Specifies the identifier key of the auditing storage account. If state
-     * is Enabled and storageEndpoint is specified, storageAccountAccessKey is
-     * required.
+     * Specifies the identifier key of the auditing storage account.
+     * If state is Enabled and storageEndpoint is specified, not specifying the
+     * storageAccountAccessKey will use SQL server system-assigned managed
+     * identity to access the storage.
+     * Prerequisites for using managed identity authentication:
+     * 1. Assign SQL Server a system-assigned managed identity in Azure Active
+     * Directory (AAD).
+     * 2. Grant SQL Server identity access to the storage account by adding
+     * 'Storage Blob Data Contributor' RBAC role to the server identity.
+     * For more information, see [Auditing to storage using Managed Identity
+     * authentication](https://go.microsoft.com/fwlink/?linkid=2114355).
      */
     @JsonProperty(value = "properties.storageAccountAccessKey")
     private String storageAccountAccessKey;
@@ -170,6 +178,15 @@ public class ExtendedServerBlobAuditingPolicyInner extends ProxyResource {
     private Boolean isAzureMonitorTargetEnabled;
 
     /**
+     * Specifies the amount of time in milliseconds that can elapse before
+     * audit actions are forced to be processed.
+     * The default minimum value is 1000 (1 second). The maximum is
+     * 2,147,483,647.
+     */
+    @JsonProperty(value = "properties.queueDelayMs")
+    private Integer queueDelayMs;
+
+    /**
      * Get specifies condition of where clause when creating an audit.
      *
      * @return the predicateExpression value
@@ -210,7 +227,7 @@ public class ExtendedServerBlobAuditingPolicyInner extends ProxyResource {
     }
 
     /**
-     * Get specifies the blob storage endpoint (e.g. https://MyAccount.blob.core.windows.net). If state is Enabled, storageEndpoint is required.
+     * Get specifies the blob storage endpoint (e.g. https://MyAccount.blob.core.windows.net). If state is Enabled, storageEndpoint or isAzureMonitorTargetEnabled is required.
      *
      * @return the storageEndpoint value
      */
@@ -219,7 +236,7 @@ public class ExtendedServerBlobAuditingPolicyInner extends ProxyResource {
     }
 
     /**
-     * Set specifies the blob storage endpoint (e.g. https://MyAccount.blob.core.windows.net). If state is Enabled, storageEndpoint is required.
+     * Set specifies the blob storage endpoint (e.g. https://MyAccount.blob.core.windows.net). If state is Enabled, storageEndpoint or isAzureMonitorTargetEnabled is required.
      *
      * @param storageEndpoint the storageEndpoint value to set
      * @return the ExtendedServerBlobAuditingPolicyInner object itself.
@@ -230,7 +247,12 @@ public class ExtendedServerBlobAuditingPolicyInner extends ProxyResource {
     }
 
     /**
-     * Get specifies the identifier key of the auditing storage account. If state is Enabled and storageEndpoint is specified, storageAccountAccessKey is required.
+     * Get specifies the identifier key of the auditing storage account.
+     If state is Enabled and storageEndpoint is specified, not specifying the storageAccountAccessKey will use SQL server system-assigned managed identity to access the storage.
+     Prerequisites for using managed identity authentication:
+     1. Assign SQL Server a system-assigned managed identity in Azure Active Directory (AAD).
+     2. Grant SQL Server identity access to the storage account by adding 'Storage Blob Data Contributor' RBAC role to the server identity.
+     For more information, see [Auditing to storage using Managed Identity authentication](https://go.microsoft.com/fwlink/?linkid=2114355).
      *
      * @return the storageAccountAccessKey value
      */
@@ -239,7 +261,12 @@ public class ExtendedServerBlobAuditingPolicyInner extends ProxyResource {
     }
 
     /**
-     * Set specifies the identifier key of the auditing storage account. If state is Enabled and storageEndpoint is specified, storageAccountAccessKey is required.
+     * Set specifies the identifier key of the auditing storage account.
+     If state is Enabled and storageEndpoint is specified, not specifying the storageAccountAccessKey will use SQL server system-assigned managed identity to access the storage.
+     Prerequisites for using managed identity authentication:
+     1. Assign SQL Server a system-assigned managed identity in Azure Active Directory (AAD).
+     2. Grant SQL Server identity access to the storage account by adding 'Storage Blob Data Contributor' RBAC role to the server identity.
+     For more information, see [Auditing to storage using Managed Identity authentication](https://go.microsoft.com/fwlink/?linkid=2114355).
      *
      * @param storageAccountAccessKey the storageAccountAccessKey value to set
      * @return the ExtendedServerBlobAuditingPolicyInner object itself.
@@ -448,6 +475,28 @@ public class ExtendedServerBlobAuditingPolicyInner extends ProxyResource {
      */
     public ExtendedServerBlobAuditingPolicyInner withIsAzureMonitorTargetEnabled(Boolean isAzureMonitorTargetEnabled) {
         this.isAzureMonitorTargetEnabled = isAzureMonitorTargetEnabled;
+        return this;
+    }
+
+    /**
+     * Get specifies the amount of time in milliseconds that can elapse before audit actions are forced to be processed.
+     The default minimum value is 1000 (1 second). The maximum is 2,147,483,647.
+     *
+     * @return the queueDelayMs value
+     */
+    public Integer queueDelayMs() {
+        return this.queueDelayMs;
+    }
+
+    /**
+     * Set specifies the amount of time in milliseconds that can elapse before audit actions are forced to be processed.
+     The default minimum value is 1000 (1 second). The maximum is 2,147,483,647.
+     *
+     * @param queueDelayMs the queueDelayMs value to set
+     * @return the ExtendedServerBlobAuditingPolicyInner object itself.
+     */
+    public ExtendedServerBlobAuditingPolicyInner withQueueDelayMs(Integer queueDelayMs) {
+        this.queueDelayMs = queueDelayMs;
         return this;
     }
 
