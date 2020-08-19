30ec84de60d1c1e0ce3940fd94914be7c2ed68cb
kirankumarkolli <kirankk@microsoft.com>
2020-08-19 14:48:15 +0530

    Little more refractoring

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/BaseAuthorizationTokenProvider.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/BaseAuthorizationTokenProvider.java
@@ -5,13 +5,11 @@ package com.azure.cosmos.implementation;
 
 import com.azure.core.credential.AzureKeyCredential;
 import com.azure.cosmos.implementation.apachecommons.lang.StringUtils;
-import com.azure.cosmos.implementation.directconnectivity.HttpUtils;
 import com.azure.cosmos.models.ModelBridgeInternal;
 
 import javax.crypto.Mac;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
-import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
@@ -19,6 +17,8 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Locale;
 import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * This class is used internally by both client (for generating the auth header with master/system key) and by the GATEWAY when
@@ -29,12 +29,14 @@ public class BaseAuthorizationTokenProvider implements AuthorizationTokenProvide
     private static final String AUTH_PREFIX = "type=master&ver=1.0&sig=";
     private final AzureKeyCredential credential;
     private volatile Mac macInstance;
+    private final Lock macInstanceLock = new ReentrantLock();
 
     //  stores current master key's hashcode for performance reasons.
     private volatile int masterKeyHashCode;
 
     public BaseAuthorizationTokenProvider(AzureKeyCredential credential) {
         this.credential = credential;
+        reInitializeIfPossible();
     }
 
     private static String getResourceSegment(ResourceType resourceType) {
@@ -198,32 +200,51 @@ public class BaseAuthorizationTokenProvider implements AuthorizationTokenProvide
     }
 
     private Mac getMacInstance() {
-        int masterKeyLatestHashCode = this.credential.getKey().hashCode();
-
-        //  Master key has changed, or this is the first time we are getting mac instance
-        if (masterKeyLatestHashCode != this.masterKeyHashCode) {
-            synchronized (this.credential) {
-                if (masterKeyLatestHashCode != this.masterKeyHashCode) {
-                    byte[] masterKeyBytes = this.credential.getKey().getBytes(StandardCharsets.UTF_8);
-                    byte[] masterKeyDecodedBytes = Utils.Base64Decoder.decode(masterKeyBytes);
-                    SecretKey signingKey = new SecretKeySpec(masterKeyDecodedBytes, "HMACSHA256");
-                    try {
-                        Mac macInstance = Mac.getInstance("HMACSHA256");
-                        macInstance.init(signingKey);
-                        //  Update the master key hash code
-                        this.masterKeyHashCode = masterKeyLatestHashCode;
-                        this.macInstance = macInstance;
-                    } catch (NoSuchAlgorithmException | InvalidKeyException e) {
-                        throw new IllegalStateException(e);
-                    }
-                }
-            }
-        }
+        reInitializeIfPossible();
 
         try {
             return (Mac)this.macInstance.clone();
         } catch (CloneNotSupportedException e) {
             throw new IllegalStateException(e);
+        }
+    }
+
+    /*
+     * Ensures that this.macInstance is initialized
+     * In-case of credential change, optimistically will try to refresh the macInstance
+     *
+     * Implementation is non-blocking, the one which acquire the lock will try to refresh
+     * with new credentials
+     *
+     * NOTE: Calling it CTOR ensured that default is initialized.
+     */
+    private void reInitializeIfPossible() {
+        int masterKeyLatestHashCode = this.credential.getKey().hashCode();
+
+        //  Master key has changed, or this is the first time we are getting mac instance
+        if (masterKeyLatestHashCode != this.masterKeyHashCode) {
+            // Try to acquire the lock, the one who got lock will try to refresh the macInstance
+            boolean lockAcquired = this.macInstanceLock.tryLock();
+            if (lockAcquired) {
+                try {
+                    if (masterKeyLatestHashCode != this.masterKeyHashCode) {
+                        byte[] masterKeyBytes = this.credential.getKey().getBytes(StandardCharsets.UTF_8);
+                        byte[] masterKeyDecodedBytes = Utils.Base64Decoder.decode(masterKeyBytes);
+                        SecretKey signingKey = new SecretKeySpec(masterKeyDecodedBytes, "HMACSHA256");
+                        try {
+                            Mac macInstance = Mac.getInstance("HMACSHA256");
+                            macInstance.init(signingKey);
+                            //  Update the master key hash code
+                            this.masterKeyHashCode = masterKeyLatestHashCode;
+                            this.macInstance = macInstance;
+                        } catch (NoSuchAlgorithmException | InvalidKeyException e) {
+                            throw new IllegalStateException(e);
+                        }
+                    }
+                } finally {
+                    this.macInstanceLock.unlock();
+                }
+            }
         }
     }
 }
