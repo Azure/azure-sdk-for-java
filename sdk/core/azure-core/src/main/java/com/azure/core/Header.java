dc4ddfa2b70ac9bfc61afe0965df52c1fd29428e
Hemant Tanwar <hetanwar@users.noreply.github.com>
2020-08-18 22:49:37 -0700

    continue to design

---
+++ sdk/core/azure-core/src/main/java/com/azure/core/Header.java
@@ -0,0 +1,73 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.core;
+
+/**
+ * A single header within for a request.
+ *
+ * If multiple header values are added to a HTTP request or response with
+ * the same name (case-insensitive), then the values will be appended
+ * to the end of the same Header with commas separating them.
+ */
+public class Header {
+    private final String name;
+    private String value;
+
+    /**
+     * Create a HttpHeader instance using the provided name and value.
+     *
+     * @param name the name
+     * @param value the value
+     */
+    public Header(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    /**
+     * Get the header name.
+     *
+     * @return the name of this Header
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Get the header value.
+     *
+     * @return the value of this Header
+     */
+    public String getValue() {
+        return value;
+    }
+
+    /**
+     * Get the comma separated value as an array.
+     *
+     * @return the values of this Header that are separated by a comma
+     */
+    public String[] getValues() {
+        return value == null ? null : value.split(",");
+    }
+
+    /**
+     * Add a new value to the end of the Header.
+     *
+     * @param value the value to add
+     */
+    public void addValue(String value) {
+        this.value += "," + value;
+    }
+
+    /**
+     * Get the String representation of the header.
+     *
+     * @return the String representation of this HttpHeader
+     */
+    @Override
+    public String toString() {
+        return name + ":" + value;
+    }
+}
---
+++ sdk/core/azure-core/src/main/java/com/azure/core/Headers.java
@@ -0,0 +1,156 @@
+package com.azure.core;
+
+import com.azure.core.http.HttpHeader;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Locale;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Headers implements Iterable<Header> {
+    private final Map<String, Header> headers = new ConcurrentHashMap<>();
+
+    /**
+     * Create an empty HttpHeaders instance.
+     */
+    public Headers() {
+    }
+
+    /**
+     * Create a HttpHeaders instance with the provided initial headers.
+     *
+     * @param headers the map of initial headers
+     */
+    public Headers(Map<String, String> headers) {
+        for (final Map.Entry<String, String> header : headers.entrySet()) {
+            this.put(header.getKey(), header.getValue());
+        }
+    }
+
+    /**
+     * Create a HttpHeaders instance with the provided initial headers.
+     *
+     * @param headers the collection of initial headers
+     */
+    public Headers(Iterable<HttpHeader> headers) {
+        this();
+
+        for (final HttpHeader header : headers) {
+            this.put(header.getName(), header.getValue());
+        }
+    }
+
+    /**
+     * Gets the number of headers in the collection.
+     *
+     * @return the number of headers in this collection.
+     */
+    public int getSize() {
+        return headers.size();
+    }
+
+    /**
+     * Sets a {@link HttpHeader header} with the given name and value.
+     *
+     * <p>If header with same name already exists then the value will be overwritten.</p>
+     *
+     * @param name the name
+     * @param value the value
+     * @return The updated HttpHeaders object
+     */
+    public Headers put(String name, String value) {
+        headers.put(formatKey(name), new HttpHeader(name, value));
+        return this;
+    }
+
+    /**
+     * Gets the {@link HttpHeader header} for the provided header name. {@code Null} is returned if the header isn't
+     * found.
+     *
+     * @param name the name of the header to find.
+     * @return the header if found, null otherwise.
+     */
+    public Header get(String name) {
+        return headers.get(formatKey(name));
+    }
+
+    /**
+     * Removes the {@link HttpHeader header} with the provided header name. {@code Null} is returned if the header
+     * isn't found.
+     *
+     * @param name the name of the header to remove.
+     * @return the header if removed, null otherwise.
+     */
+    public Header remove(String name) {
+        return headers.remove(formatKey(name));
+    }
+
+    /**
+     * Get the value for the provided header name. {@code Null} is returned if the header name isn't found.
+     *
+     * @param name the name of the header whose value is being retrieved.
+     * @return the value of the header, or null if the header isn't found
+     */
+    public String getValue(String name) {
+        final Header header = get(name);
+        return header == null ? null : header.getValue();
+    }
+
+    /**
+     * Get the values for the provided header name. {@code Null} is returned if the header name isn't found.
+     *
+     * <p>This returns {@link #getValue(String) getValue} split by {@code comma}.</p>
+     *
+     * @param name the name of the header whose value is being retrieved.
+     * @return the values of the header, or null if the header isn't found
+     */
+    public String[] getValues(String name) {
+        final Header header = get(name);
+        return header == null ? null : header.getValues();
+    }
+
+    private String formatKey(final String key) {
+        return key.toLowerCase(Locale.ROOT);
+    }
+
+    /**
+     * Gets a {@link Map} representation of the HttpHeaders collection.
+     *
+     * @return the headers as map
+     */
+    public Map<String, String> toMap() {
+        final Map<String, String> result = new HashMap<>();
+        for (final Header header : headers.values()) {
+            result.put(header.getName(), header.getValue());
+        }
+        return result;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Iterator<Header> iterator() {
+        return headers.values().iterator();
+    }
+
+    /**
+     * Get a {@link Stream} representation of the HttpHeader values in this instance.
+     *
+     * @return A {@link Stream} of all header values in this instance.
+     */
+    public Stream<Header> stream() {
+        return headers.values().stream();
+    }
+
+    @Override
+    public String toString() {
+        return this.stream()
+            .map(header -> header.getName() + "=" + header.getValue())
+            .collect(Collectors.joining(", "));
+    }
+
+}
