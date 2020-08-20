a7c3acbd89d268dd085e05a28a8a87242dc2036e
Sameeksha Vaity <savaity@microsoft.com>
2020-08-20 13:07:47 -0700

    Rename bounding box model to FieldBoundingBox (#14241)

--- sdk/formrecognizer/azure-ai-formrecognizer/src/main/java/com/azure/ai/formrecognizer/models/FieldBoundingBox.java
+++ sdk/formrecognizer/azure-ai-formrecognizer/src/main/java/com/azure/ai/formrecognizer/models/FieldBoundingBox.java
@@ -13,19 +13,19 @@ import java.util.stream.Collectors;
  * Quadrangle bounding box, with coordinates specified relative to the top-left of the original image
  */
 @Immutable
-public final class BoundingBox {
+public final class FieldBoundingBox {
 
     /**
-     * The list of coordinates of the Bounding box.
+     * The list of coordinates of the field's bounding box.
      */
     private final List<Point> points;
 
     /**
-     * Constructs a Bounding box object.
+     * Constructs a Field Bounding box object.
      *
-     * @param points The list of coordinates of the Bounding box.
+     * @param points The list of coordinates of the field's bounding box.
      */
-    public BoundingBox(final List<Point> points) {
+    public FieldBoundingBox(final List<Point> points) {
         if (points == null) {
             this.points = null;
         } else {
@@ -36,16 +36,16 @@ public final class BoundingBox {
     /**
      * Gets the list of all point coordinates of the bounding box.
      *
-     * @return The unmodifiable list of all point coordinates of the Bounding box.
+     * @return The unmodifiable list of all point coordinates of the field's bounding box.
      */
     public List<Point> getPoints() {
         return this.points;
     }
 
     /**
-     * Returns a string representation of the {@link BoundingBox}.
+     * Returns a string representation of the {@link FieldBoundingBox}.
      *
-     * @return the string representation of the {@link BoundingBox}.
+     * @return the string representation of the {@link FieldBoundingBox}.
      */
     @Override
     public String toString() {
