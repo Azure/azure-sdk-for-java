7a420f3c4fbc05531b30c821370a70f4e422f567
Connie <conniey@microsoft.com>
2020-08-20 12:24:53 -0700

    Adding create rules.

---
+++ sdk/servicebus/azure-messaging-servicebus/src/main/java/com/azure/messaging/servicebus/administration/models/CreateRuleOptions.java
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.messaging.servicebus.administration.models;
+
+/**
+ * A set of options for creating a rule.
+ */
+public class CreateRuleOptions {
+}
