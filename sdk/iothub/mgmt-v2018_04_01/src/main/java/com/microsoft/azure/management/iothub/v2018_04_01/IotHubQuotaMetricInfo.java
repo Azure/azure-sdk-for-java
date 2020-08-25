3932db9bf39bfaf26e3a8ba662686983d4cdf605
SDK Automation <sdkautomation@microsoft.com>
2020-08-25 02:42:17 +0000

    Generated from 964d3cc7016e8103bb10ef41c227935af79357b8
    Add Missing Descriptions

--- sdk/iothub/mgmt-v2018_04_01/src/main/java/com/microsoft/azure/management/iothub/v2018_04_01/IotHubQuotaMetricInfo.java
+++ sdk/iothub/mgmt-v2018_04_01/src/main/java/com/microsoft/azure/management/iothub/v2018_04_01/IotHubQuotaMetricInfo.java
@@ -11,12 +11,12 @@ package com.microsoft.azure.management.iothub.v2018_04_01;
 import com.microsoft.azure.arm.model.HasInner;
 import com.microsoft.azure.management.iothub.v2018_04_01.implementation.IotHubQuotaMetricInfoInner;
 import com.microsoft.azure.arm.resources.models.HasManager;
-import com.microsoft.azure.management.iothub.v2018_04_01.implementation.IoTHubManager;
+import com.microsoft.azure.management.iothub.v2018_04_01.implementation.DevicesManager;
 
 /**
  * Type representing IotHubQuotaMetricInfo.
  */
-public interface IotHubQuotaMetricInfo extends HasInner<IotHubQuotaMetricInfoInner>, HasManager<IoTHubManager> {
+public interface IotHubQuotaMetricInfo extends HasInner<IotHubQuotaMetricInfoInner>, HasManager<DevicesManager> {
     /**
      * @return the currentValue value.
      */
