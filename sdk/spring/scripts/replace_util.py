33b726cd0e6be621c88dc8cf23398c332b5d84e2
Moary Chen <moary@126.com>
2020-08-27 11:27:06 +0800

    refer to reviewer' comment and update

--- sdk/spring/scripts/replace_util.py
+++ sdk/spring/scripts/replace_util.py
@@ -9,24 +9,29 @@ import time
 import in_place
 
 from log import log, Log
+from artifact_id_pair import ArtifactIdPair
+from version_update_item import VersionUpdateItem
+
+X_VERSION_UPDATE = 'x-version-update'
+X_INCLUDE_UPDATE = 'x-include-update'
 
 config = {
     'cosmos': {
         'sdk/cosmos/azure-spring-data-cosmos-test/pom.xml': {
-            'artifact_id_tuple': (
-                ('azure-spring-data-2-3-cosmos', 'azure-spring-data-2-2-cosmos'),
+            'artifact_id_pairs': (
+                ArtifactIdPair('azure-spring-data-2-3-cosmos', 'azure-spring-data-2-2-cosmos'),
             )
         }
     },
     'spring': {
         'sdk/spring/azure-spring-boot-test-cosmosdb/pom.xml': {
-            'artifact_id_tuple': (
-                ('azure-cosmosdb-spring-boot-2-3-starter', 'azure-cosmosdb-spring-boot-2-2-starter'),
+            'artifact_id_pairs': (
+                ArtifactIdPair('azure-cosmosdb-spring-boot-2-3-starter', 'azure-cosmosdb-spring-boot-2-2-starter'),
             ),
-            'version_tuple': (
-                ('org.springframework.boot:spring-boot-starter-web', '2.2.9.RELEASE'),
-                ('org.springframework.boot:spring-boot-starter-actuator', '2.2.9.RELEASE'),
-                ('org.springframework.boot:spring-boot-starter-test', '2.2.9.RELEASE')
+            'version_update_items': (
+                VersionUpdateItem('org.springframework.boot:spring-boot-starter-web', '2.2.9.RELEASE'),
+                VersionUpdateItem('org.springframework.boot:spring-boot-starter-actuator', '2.2.9.RELEASE'),
+                VersionUpdateItem('org.springframework.boot:spring-boot-starter-test', '2.2.9.RELEASE')
             )
         }
     }
@@ -99,10 +104,9 @@ def init_log(args):
 def replace(module):
     """
     Replace action
-    :param module:
+    :param module: module name
     :return:
     """
-    module = 'spring'
     for pom in config[module].keys():
         replace_artifact_id(module, pom)
         replace_version(module, pom)
@@ -111,66 +115,62 @@ def replace(module):
 def replace_artifact_id(module, pom):
     """
     Replace artifactId in dependency
-    :param module module name
+    :param module: module name
     :param pom: pom file path
     :return:
     """
-
-    log.info('Replacing artifact id in file: {}'.format(pom, module))
-    artifact_id_tuple = config[module][pom]['artifact_id_tuple']
-    log.info('Module: {}, artifact ids: {}'.format(module, artifact_id_tuple))
+    log.debug('Replacing artifact id in file: {}'.format(pom, module))
+    artifact_id_pairs = config[module][pom]['artifact_id_pairs']
+    log.debug('Module: {}, artifact ids: {}'.format(module, artifact_id_pairs))
     with in_place.InPlace(pom) as file:
         line_num = 0
         for line in file:
             line_num = line_num + 1
-            for artifact_id_mapping_tuple in artifact_id_tuple:
-                new_line = line.replace(artifact_id_mapping_tuple[0], artifact_id_mapping_tuple[1])
+            for artifact_id_pair in artifact_id_pairs:
+                new_line = line.replace(artifact_id_pair.old_artifact_id, artifact_id_pair.new_artifact_id)
                 if line != new_line:
-                    log.debug('Updated artifact id line {}'.format(line_num))
+                    line = new_line
+                    log.debug('Updated artifact id in line {}'.format(line_num))
                     log.debug('    old_line = {}.'.format(line.strip('\n')))
                     log.debug('    new_line = {}.'.format(new_line.strip('\n')))
-                    line = new_line
             file.write(line)
 
 
 def replace_version(module, pom):
     """
     Replace version, contains dependency and plugin parts.
-    :param module module name
+    :param module: module name
     :param pom: pom file path
     :return:
     """
-
-    log.info('Replacing version in file: {}'.format(pom))
-    version_tuple = config[module][pom]['version_tuple']
-    log.info('Module: {}, versions: {}'.format(module, version_tuple))
+    log.debug('Replacing version in file: {}'.format(pom))
+    version_update_items = config[module][pom]['version_update_items']
+    log.debug('Module: {}, versions: {}'.format(module, version_update_items))
     with in_place.InPlace(pom) as file:
         line_num = 0
         for line in file:
             line_num = line_num + 1
-            for version_mapping_tuple in version_tuple:
-                dependency_update_flag = '<!-- {x-version-update;' + version_mapping_tuple[0] + ';'
-                plugin_update_flag = '<!-- {x-include-update;' + version_mapping_tuple[0] + ';'
-                # update version in dependency part
-                if dependency_update_flag in line:
-                    old_version = line[(line.index('<version>') + 9):line.index('</version>')]
-                    log.debug('Updated version line {}'.format(line_num))
-                    new_line = line.replace(old_version, version_mapping_tuple[1])
-                    if line != new_line:
-                        log.debug('Updated dependency line {}'.format(line_num))
-                        log.debug('    old_line = {}.'.format(line.strip('\n')))
-                        log.debug('    new_line = {}.'.format(new_line.strip('\n')))
-                        line = new_line
-                # update version in plugin part
-                elif plugin_update_flag in line:
-                    old_version = line[(line.index('[')+1):line.index(']')]
-                    log.debug('Updated version line {}'.format(line_num))
-                    new_line = line.replace(old_version, version_mapping_tuple[1])
-                    if line != new_line:
-                        log.debug('Updated plugin line {}'.format(line_num))
-                        log.debug('    old_line = {}.'.format(line.strip('\n')))
-                        log.debug('    new_line = {}.'.format(new_line.strip('\n')))
-                        line = new_line
+            for version_update_item in version_update_items:
+                if version_update_item.id in line:
+                    log.debug('Updated line {}'.format(line_num))
+                    # update version in dependency part
+                    if X_VERSION_UPDATE in line:
+                        old_version = line[(line.index('<version>') + 9):line.index('</version>')]
+                        new_line = line.replace(old_version, version_update_item.new_version)
+                        if line != new_line:
+                            line = new_line
+                            log.debug('Updated version of dependency in line {}'.format(line_num))
+                            log.debug('    old_line = {}.'.format(line.strip('\n')))
+                            log.debug('    new_line = {}.'.format(new_line.strip('\n')))
+                    # update version in plugin part
+                    elif X_INCLUDE_UPDATE in line:
+                        old_version = line[(line.index('[')+1):line.index(']')]
+                        new_line = line.replace(old_version, version_update_item.new_version)
+                        if line != new_line:
+                            line = new_line
+                            log.debug('Updated version of plugin in line {}'.format(line_num))
+                            log.debug('    old_line = {}.'.format(line.strip('\n')))
+                            log.debug('    new_line = {}.'.format(new_line.strip('\n')))
             file.write(line)
 
 
