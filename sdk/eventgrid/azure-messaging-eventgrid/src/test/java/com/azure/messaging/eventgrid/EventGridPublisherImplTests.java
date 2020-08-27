a47fa35c6ef8f1d6b06b278feb2ae4bd97b3cf41
Soren Dahl <Soreloser2@users.noreply.github.com>
2020-08-26 17:22:38 -0700

    Fix tests and pom
    tests now renamed, use StepVerifier. Core dependency changed, added reactor test as dependency and remove build configs in pom. Refactor swagger readme for captialization.

--- sdk/eventgrid/azure-messaging-eventgrid/src/test/java/com/azure/messaging/eventgrid/EventGridPublisherImplTests.java
+++ sdk/eventgrid/azure-messaging-eventgrid/src/test/java/com/azure/messaging/eventgrid/EventGridPublisherImplTests.java
@@ -8,48 +8,46 @@ import com.azure.core.http.HttpHeaders;
 import com.azure.core.http.HttpPipelineBuilder;
 import com.azure.core.http.policy.AddHeadersPolicy;
 import com.azure.core.http.policy.RetryPolicy;
-import com.azure.core.http.rest.Response;
 import com.azure.core.test.TestBase;
 import com.azure.messaging.eventgrid.implementation.EventGridPublisherClientImpl;
 import com.azure.messaging.eventgrid.implementation.EventGridPublisherClientImplBuilder;
 import com.azure.messaging.eventgrid.implementation.models.CloudEvent;
 import com.azure.messaging.eventgrid.implementation.models.EventGridEvent;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
 import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
 
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
 import java.time.OffsetDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 public class EventGridPublisherImplTests extends TestBase {
 
-    HttpPipelineBuilder builder;
+    private HttpPipelineBuilder pipelineBuilder;
 
-    private static final String EVENTGRID_ENDPOINT = "EG_ENDPOINT";
+    private EventGridPublisherClientImplBuilder clientBuilder;
 
-    private static final String CLOUD_ENDPOINT = "EG_CLOUD_ENDPOINT";
+    // Event Grid endpoint for a topic accepting EventGrid schema events
+    private static final String EVENTGRID_ENDPOINT = "AZURE_EVENTGRID_EVENTGRID_ENDPOINT";
 
-    private static final String CUSTOM_ENDPOINT = "EG_CUSTOM_ENDPOINT";
+    // Event Grid endpoint for a topic accepting CloudEvents schema events
+    private static final String CLOUD_ENDPOINT = "AZURE_EVENTGRID_CLOUD_ENDPOINT";
 
-    private static final String EVENTGRID_KEY = "EG_KEY";
+    // Event Grid endpoint for a topic accepting custom schema events
+    private static final String CUSTOM_ENDPOINT = "AZURE_EVENTGRID_CUSTOM_ENDPOINT";
 
-    private static final String CLOUD_KEY = "EG_CLOUD_KEY";
+    // Event Grid access key for a topic accepting EventGrid schema events
+    private static final String EVENTGRID_KEY = "AZURE_EVENTGRID_EVENTGRID_KEY";
 
-    private static final String CUSTOM_KEY = "EG_CUSTOM_KEY";
+    // Event Grid access key for a topic accepting CloudEvents schema events
+    private static final String CLOUD_KEY = "AZURE_EVENTGRID_CLOUD_KEY";
+
+    // Event Grid access key for a topic accepting custom schema events
+    private static final String CUSTOM_KEY = "AZURE_EVENTGRID_CUSTOM_KEY";
 
     private static final String DUMMY_ENDPOINT = "https://www.dummyEndpoint.com";
 
@@ -57,77 +55,86 @@ public class EventGridPublisherImplTests extends TestBase {
 
     @Override
     protected void beforeTest() {
-        builder = new HttpPipelineBuilder();
+        StepVerifier.setDefaultTimeout(Duration.ofSeconds(30));
+
+        pipelineBuilder = new HttpPipelineBuilder();
+
+        clientBuilder = new EventGridPublisherClientImplBuilder();
 
         if (interceptorManager.isPlaybackMode()) {
-            builder.httpClient(interceptorManager.getPlaybackClient());
+            pipelineBuilder.httpClient(interceptorManager.getPlaybackClient());
         } else {
-            builder.policies(interceptorManager.getRecordPolicy(), new RetryPolicy());
+            pipelineBuilder.policies(interceptorManager.getRecordPolicy(), new RetryPolicy());
         }
     }
 
+    @Override
+    protected void afterTest() {
+        StepVerifier.resetDefaultTimeout();
+    }
+
     @Test
-    public void testPublishEventGridEventsImpl() throws MalformedURLException {
-        EventGridPublisherClientImpl egClient = new EventGridPublisherClientImplBuilder()
-            .pipeline(builder.policies(
+    public void publishEventGridEventsImpl() throws MalformedURLException {
+        EventGridPublisherClientImpl egClient = clientBuilder
+            .pipeline(pipelineBuilder.policies(
                 new AddHeadersPolicy(new HttpHeaders().put("aeg-sas-key", getKey(EVENTGRID_KEY))))
                 .build())
             .buildClient();
 
-        List<EventGridEvent> events = new ArrayList<>();
-        events.add(new EventGridEvent()
-            .setId(UUID.randomUUID().toString())
-            .setSubject("Test")
-            .setEventType("Microsoft.MockPublisher.TestEvent")
-            .setData(new HashMap<String, String>() {{
-                put("Field1", "Value1");
-                put("Field2", "Value2");
-                put("Field3", "Value3");
-            }})
-            .setDataVersion("1.0")
-            .setEventTime(OffsetDateTime.now()));
+        List<EventGridEvent> events = Collections.singletonList(
+            new EventGridEvent()
+                .setId(UUID.randomUUID().toString())
+                .setSubject("Test")
+                .setEventType("Microsoft.MockPublisher.TestEvent")
+                .setData(new HashMap<String, String>() {{
+                    put("Field1", "Value1");
+                    put("Field2", "Value2");
+                    put("Field3", "Value3");
+                }})
+                .setDataVersion("1.0")
+                .setEventTime(OffsetDateTime.now())
+        );
 
         String hostname = new URL(getEndpoint(EVENTGRID_ENDPOINT)).getHost();
-        Response<Void> response = egClient.publishEventsWithResponseAsync(hostname, events).block();
 
-        assertNotNull(response);
-        assertEquals(response.getStatusCode(), 200);
-
+        StepVerifier.create(egClient.publishEventsWithResponseAsync(hostname, events))
+            .expectNextMatches(voidResponse -> voidResponse.getStatusCode() == 200)
+            .verifyComplete();
     }
 
     @Test
-    public void testPublishCloudEventsImpl() throws MalformedURLException {
-        EventGridPublisherClientImpl egClient = new EventGridPublisherClientImplBuilder()
-            .pipeline(builder.policies(
+    public void publishCloudEventsImpl() throws MalformedURLException {
+        EventGridPublisherClientImpl egClient = clientBuilder
+            .pipeline(pipelineBuilder.policies(
                 new AddHeadersPolicy(new HttpHeaders().put("aeg-sas-key", getKey(CLOUD_KEY))))
                 .build())
             .buildClient();
 
-        List<CloudEvent> events = new ArrayList<>();
-        events.add(new CloudEvent()
-            .setId(UUID.randomUUID().toString())
-            .setSubject("Test")
-            .setType("Microsoft.MockPublisher.TestEvent")
-            .setData(new HashMap<String, String>() {{
-                put("Field1", "Value1");
-                put("Field2", "Value2");
-                put("Field3", "Value3");
-            }})
-            .setSpecversion("1.0")
-            .setTime(OffsetDateTime.now()));
+        List<CloudEvent> events = Collections.singletonList(
+            new CloudEvent()
+                .setId(UUID.randomUUID().toString())
+                .setSubject("Test")
+                .setType("Microsoft.MockPublisher.TestEvent")
+                .setData(new HashMap<String, String>() {{
+                    put("Field1", "Value1");
+                    put("Field2", "Value2");
+                    put("Field3", "Value3");
+                }})
+                .setSpecversion("1.0")
+                .setTime(OffsetDateTime.now())
+        );
 
         String hostname = new URL(getEndpoint(CLOUD_ENDPOINT)).getHost();
-        Response<Void> response = egClient.publishCloudEventEventsWithResponseAsync(hostname, events).block();
 
-        assertNotNull(response);
-        assertEquals(response.getStatusCode(), 200);
+        StepVerifier.create(egClient.publishCloudEventEventsWithResponseAsync(hostname, events))
+            .expectNextMatches(voidResponse -> voidResponse.getStatusCode() == 200)
+            .verifyComplete();
     }
 
     @Test
-    public void TestPublishCustomEventsImpl() throws MalformedURLException {
-
-        EventGridPublisherClientImpl egClient = new EventGridPublisherClientImplBuilder()
-            .pipeline(builder.policies(
+    public void publishCustomEventsImpl() throws MalformedURLException {
+        EventGridPublisherClientImpl egClient = clientBuilder
+            .pipeline(pipelineBuilder.policies(
                 new AddHeadersPolicy(new HttpHeaders().put("aeg-sas-key", getKey(CUSTOM_KEY))))
                 .build())
             .buildClient();
@@ -143,10 +150,10 @@ public class EventGridPublisherImplTests extends TestBase {
         }
 
         String hostname = new URL(getEndpoint(CUSTOM_ENDPOINT)).getHost();
-        Response<Void> response = egClient.publishCustomEventEventsWithResponseAsync(hostname, events).block();
 
-        assertNotNull(response);
-        assertEquals(response.getStatusCode(), 200);
+        StepVerifier.create(egClient.publishCustomEventEventsWithResponseAsync(hostname, events))
+            .expectNextMatches(voidResponse -> voidResponse.getStatusCode() == 200)
+            .verifyComplete();
     }
 
 
@@ -154,13 +161,17 @@ public class EventGridPublisherImplTests extends TestBase {
         if (interceptorManager.isPlaybackMode()) {
             return DUMMY_ENDPOINT;
         }
-        return System.getenv(liveEnvName);
+        String endpoint = System.getenv(liveEnvName);
+        assertNotNull(endpoint, "System environment variable " + liveEnvName + "is null");
+        return endpoint;
     }
 
     private String getKey(String liveEnvName) {
         if (interceptorManager.isPlaybackMode()) {
             return DUMMY_KEY;
         }
-        return System.getenv(liveEnvName);
+        String key = System.getenv(liveEnvName);
+        assertNotNull(key, "System environment variable " + liveEnvName + "is null");
+        return key;
     }
 }
