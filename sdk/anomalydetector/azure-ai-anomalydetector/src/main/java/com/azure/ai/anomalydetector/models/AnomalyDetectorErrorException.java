be21b3ab48a763cf4e629d70df0572ffd72515ff
Srikanta <srnagar@microsoft.com>
2020-08-26 17:13:05 -0700

    Codegen from updated swagger

--- sdk/anomalydetector/azure-ai-anomalydetector/src/main/java/com/azure/ai/anomalydetector/models/AnomalyDetectorErrorException.java
+++ sdk/anomalydetector/azure-ai-anomalydetector/src/main/java/com/azure/ai/anomalydetector/models/AnomalyDetectorErrorException.java
@@ -7,31 +7,31 @@ package com.azure.ai.anomalydetector.models;
 import com.azure.core.exception.HttpResponseException;
 import com.azure.core.http.HttpResponse;
 
-/** Exception thrown for an invalid response with ApiError information. */
-public final class ApiErrorException extends HttpResponseException {
+/** Exception thrown for an invalid response with AnomalyDetectorError information. */
+public final class AnomalyDetectorErrorException extends HttpResponseException {
     /**
-     * Initializes a new instance of the ApiErrorException class.
+     * Initializes a new instance of the AnomalyDetectorErrorException class.
      *
      * @param message the exception message or the response content if a message is not available.
      * @param response the HTTP response.
      */
-    public ApiErrorException(String message, HttpResponse response) {
+    public AnomalyDetectorErrorException(String message, HttpResponse response) {
         super(message, response);
     }
 
     /**
-     * Initializes a new instance of the ApiErrorException class.
+     * Initializes a new instance of the AnomalyDetectorErrorException class.
      *
      * @param message the exception message or the response content if a message is not available.
      * @param response the HTTP response.
      * @param value the deserialized response value.
      */
-    public ApiErrorException(String message, HttpResponse response, ApiError value) {
+    public AnomalyDetectorErrorException(String message, HttpResponse response, AnomalyDetectorError value) {
         super(message, response, value);
     }
 
     @Override
-    public ApiError getValue() {
-        return (ApiError) super.getValue();
+    public AnomalyDetectorError getValue() {
+        return (AnomalyDetectorError) super.getValue();
     }
 }
