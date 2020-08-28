be9721fdca7503f7f5b8e5d8b56f6a1e0d42c499
shafang <Shawn.Fang@microsoft.com>
2020-08-28 14:19:13 -0700

    don't mock on input

--- sdk/textanalytics/azure-ai-textanalytics/src/test/java/com/azure/ai/textanalytics/ReferencePointerParseTest.java
+++ sdk/textanalytics/azure-ai-textanalytics/src/test/java/com/azure/ai/textanalytics/ReferencePointerParseTest.java
@@ -14,11 +14,11 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.when;
 
 /**
  * Unit tests for Aspect relation's reference parse
@@ -28,29 +28,9 @@ public class ReferencePointerParseTest {
     @Mock
     private TextAnalyticsClientImpl textAnalyticsClientImpl;
 
-    @Mock
-    private List<DocumentSentiment> documentSentiments;
-
-    @Mock
-    private DocumentSentiment documentSentiment;
-
-    @Mock
-    private List<SentenceSentiment> sentenceSentiments;
-
-    @Mock
-    private SentenceSentiment sentenceSentiment;
-
-    @Mock
-    private List<SentenceOpinion> sentenceOpinions;
-
-    @Mock
-    private SentenceOpinion sentenceOpinion;
-
-
     @BeforeEach
     void setup() {
         MockitoAnnotations.initMocks(this);
-
     }
 
     @AfterEach
@@ -100,53 +80,57 @@ public class ReferencePointerParseTest {
 
     @Test
     public void findSentimentOpinion() {
-        when(documentSentiments.size()).thenReturn(2);
-        when(documentSentiments.get(1)).thenReturn(documentSentiment);
-        when(documentSentiment.getSentences()).thenReturn(sentenceSentiments);
-        when(sentenceSentiments.size()).thenReturn(4);
-        when(sentenceSentiments.get(3)).thenReturn(sentenceSentiment);
-        when(sentenceSentiment.getOpinions()).thenReturn(sentenceOpinions);
-        when(sentenceOpinions.size()).thenReturn(6);
-        when(sentenceOpinions.get(5)).thenReturn(sentenceOpinion);
-        assertEquals(sentenceOpinion,
-            analyzeSentimentAsyncClient.findSentimentOpinion(VALID_OPINION_POINTER, documentSentiments));
+        final SentenceOpinion sentenceOpinion = analyzeSentimentAsyncClient.findSentimentOpinion(VALID_OPINION_POINTER, getDocumentSentiments());
+        assertEquals(SentenceOpinion.class, sentenceOpinion.getClass());
     }
 
     @Test
     public void findSentimentOpinionWithInvalidDocumentIndex() {
-        when(documentSentiments.size()).thenReturn(2);
-        when(documentSentiments.get(1)).thenReturn(documentSentiment);
         final String referencePointer = "#/documents/2/sentences/1/opinions/1";
         final IllegalStateException illegalStateException = assertThrows(IllegalStateException.class, () ->
-            analyzeSentimentAsyncClient.findSentimentOpinion(referencePointer, documentSentiments));
+            analyzeSentimentAsyncClient.findSentimentOpinion(referencePointer, getDocumentSentiments()));
         assertEquals(String.format(INVALID_DOCUMENT_INDEX_EXCEPTION, 2, referencePointer), illegalStateException.getMessage());
     }
 
     @Test
     public void findSentimentOpinionWithInvalidSentenceIndex() {
-        when(documentSentiments.size()).thenReturn(2);
-        when(documentSentiments.get(1)).thenReturn(documentSentiment);
-        when(documentSentiment.getSentences()).thenReturn(sentenceSentiments);
-        when(sentenceSentiments.size()).thenReturn(4);
         final String referencePointer = "#/documents/1/sentences/4/opinions/1";
         final IllegalStateException illegalStateException = assertThrows(IllegalStateException.class, () ->
-            analyzeSentimentAsyncClient.findSentimentOpinion(referencePointer, documentSentiments));
+            analyzeSentimentAsyncClient.findSentimentOpinion(referencePointer, getDocumentSentiments()));
         assertEquals(String.format(INVALID_SENTENCE_INDEX_EXCEPTION, 4, referencePointer), illegalStateException.getMessage());
     }
 
     @Test
     public void findSentimentOpinionWithInvalidOpinionIndex() {
-        when(documentSentiments.size()).thenReturn(2);
-        when(documentSentiments.get(1)).thenReturn(documentSentiment);
-        when(documentSentiment.getSentences()).thenReturn(sentenceSentiments);
-        when(sentenceSentiments.size()).thenReturn(4);
-        when(sentenceSentiments.get(3)).thenReturn(sentenceSentiment);
-        when(sentenceSentiment.getOpinions()).thenReturn(sentenceOpinions);
-        when(sentenceOpinions.size()).thenReturn(6);
-        when(sentenceOpinions.get(5)).thenReturn(sentenceOpinion);
         final String referencePointer = "#/documents/1/sentences/3/opinions/6";
         final IllegalStateException illegalStateException = assertThrows(IllegalStateException.class, () ->
-            analyzeSentimentAsyncClient.findSentimentOpinion(referencePointer, documentSentiments));
+            analyzeSentimentAsyncClient.findSentimentOpinion(referencePointer, getDocumentSentiments()));
         assertEquals(String.format(INVALID_OPINION_INDEX_EXCEPTION, 6, referencePointer), illegalStateException.getMessage());
+    }
+
+    private List<DocumentSentiment> getDocumentSentiments() {
+        List<DocumentSentiment> documentSentiments = new ArrayList<>();
+        for (int i = 0; i < 2; i++) {
+            documentSentiments.add(new DocumentSentiment()
+                .setId(Integer.toString(i))
+                .setSentences(getSentenceSentiments()));
+        }
+        return documentSentiments;
+    }
+
+    private List<SentenceSentiment> getSentenceSentiments() {
+        List<SentenceSentiment> sentenceSentiments = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            sentenceSentiments.add(new SentenceSentiment().setOpinions(getSentenceOpinions()));
+        }
+        return sentenceSentiments;
+    }
+
+    private List<SentenceOpinion> getSentenceOpinions() {
+        final List<SentenceOpinion> sentenceOpinions = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            sentenceOpinions.add(new SentenceOpinion());
+        }
+        return sentenceOpinions;
     }
 }
