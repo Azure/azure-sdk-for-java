f16b387c9021e84189082413206d2c279e404e36
Xia Zhu <zhuxia@microsoft.com>
2020-08-28 17:55:47 +0800

    add samples and sample data

---
+++ sdk/anomalydetector/azure-ai-anomalydetector/src/samples/java/com/azure/ai/anomalydetector/DetectAnomaliesLastPoint.java
@@ -0,0 +1,69 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.ai.anomalydetector;
+
+import com.azure.ai.anomalydetector.models.DetectRequest;
+import com.azure.ai.anomalydetector.models.LastDetectResponse;
+import com.azure.ai.anomalydetector.models.TimeGranularity;
+import com.azure.ai.anomalydetector.models.TimeSeriesPoint;
+import com.azure.core.credential.AzureKeyCredential;
+import com.azure.core.http.*;
+import com.azure.core.http.policy.AddHeadersPolicy;
+import com.azure.core.http.policy.AzureKeyCredentialPolicy;
+import com.azure.core.http.policy.HttpPipelinePolicy;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DetectAnomaliesLastPoint {
+
+    public static void main(final String[] args) throws IOException {
+        String endpoint = "<anomaly-detector-resource-endpoint>";
+        String key = "<anomaly-detector-resource-key>";
+        HttpHeaders headers = new HttpHeaders()
+            .put("Accept", ContentType.APPLICATION_JSON);
+
+        HttpPipelinePolicy authPolicy = new AzureKeyCredentialPolicy("Ocp-Apim-Subscription-Key",
+            new AzureKeyCredential(key));
+        AddHeadersPolicy addHeadersPolicy = new AddHeadersPolicy(headers);
+
+        HttpPipeline httpPipeline = new HttpPipelineBuilder().httpClient(HttpClient.createDefault())
+            .policies(authPolicy, addHeadersPolicy).build();
+        AnomalyDetectorClient anomalyDetectorClient = new AnomalyDetectorClientBuilder()
+            .pipeline(httpPipeline)
+            .endpoint(endpoint)
+            .buildClient();
+
+        Path path = Paths.get("./src/samples/java/sample_data/request-data.csv");
+        List<String> requestData = Files.readAllLines(path);
+        List<TimeSeriesPoint> series = requestData.stream()
+            .map(e -> e.trim())
+            .filter(e -> e.length() > 0)
+            .map(e -> e.split(",", 2))
+            .filter(e -> e.length == 2)
+            .map(e -> {
+                TimeSeriesPoint p = new TimeSeriesPoint();
+                p.setTimestamp(OffsetDateTime.parse(e[0]));
+                p.setValue(Float.parseFloat(e[1]));
+                return p;
+            })
+            .collect(Collectors.toList());
+
+        System.out.println("Determining if latest data point is an anomaly...");
+        DetectRequest request = new DetectRequest();
+        request.setSeries(series);
+        request.setGranularity(TimeGranularity.DAILY);
+        LastDetectResponse response = anomalyDetectorClient.detectLastPoint(request);
+        if (response.isAnomaly()) {
+            System.out.println("The latest point was detected as an anomaly.");
+        } else {
+            System.out.println("The latest point was not detected as an anomaly.");
+        }
+    }
+}
---
+++ sdk/anomalydetector/azure-ai-anomalydetector/src/samples/java/com/azure/ai/anomalydetector/DetectChangePoints.java
@@ -0,0 +1,75 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.ai.anomalydetector;
+
+import com.azure.ai.anomalydetector.models.ChangePointDetectRequest;
+import com.azure.ai.anomalydetector.models.ChangePointDetectResponse;
+import com.azure.ai.anomalydetector.models.TimeGranularity;
+import com.azure.ai.anomalydetector.models.TimeSeriesPoint;
+import com.azure.core.credential.AzureKeyCredential;
+import com.azure.core.http.*;
+import com.azure.core.http.policy.AddHeadersPolicy;
+import com.azure.core.http.policy.AzureKeyCredentialPolicy;
+import com.azure.core.http.policy.HttpPipelinePolicy;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DetectChangePoints {
+
+    public static void main(final String[] args) throws IOException {
+        String endpoint = "<anomaly-detector-resource-endpoint>";
+        String key = "<anomaly-detector-resource-key>";
+        HttpHeaders headers = new HttpHeaders()
+            .put("Accept", ContentType.APPLICATION_JSON);
+
+        HttpPipelinePolicy authPolicy = new AzureKeyCredentialPolicy("Ocp-Apim-Subscription-Key",
+            new AzureKeyCredential(key));
+        AddHeadersPolicy addHeadersPolicy = new AddHeadersPolicy(headers);
+
+        HttpPipeline httpPipeline = new HttpPipelineBuilder().httpClient(HttpClient.createDefault())
+            .policies(authPolicy, addHeadersPolicy).build();
+        AnomalyDetectorClient anomalyDetectorClient = new AnomalyDetectorClientBuilder()
+            .pipeline(httpPipeline)
+            .endpoint(endpoint)
+            .buildClient();
+
+        Path path = Paths.get("./src/samples/java/sample_data/request-data.csv");
+        List<String> requestData = Files.readAllLines(path);
+        List<TimeSeriesPoint> series = requestData.stream()
+            .map(e -> e.trim())
+            .filter(e -> e.length() > 0)
+            .map(e -> e.split(",", 2))
+            .filter(e -> e.length == 2)
+            .map(e -> {
+                TimeSeriesPoint p = new TimeSeriesPoint();
+                p.setTimestamp(OffsetDateTime.parse(e[0]));
+                p.setValue(Float.parseFloat(e[1]));
+                return p;
+            })
+            .collect(Collectors.toList());
+
+        System.out.println("Detecting change points...");
+        ChangePointDetectRequest request = new ChangePointDetectRequest();
+        request.setSeries(series);
+        request.setGranularity(TimeGranularity.DAILY);
+        ChangePointDetectResponse response = anomalyDetectorClient.detectChangePoint(request);
+        if (response.getIsChangePoint().contains(true)) {
+            System.out.println("Change points found in the following data positions:");
+            for (int i = 0; i < request.getSeries().size(); ++i) {
+                if (response.getIsChangePoint().get(i)) {
+                    System.out.print(i + " ");
+                }
+            }
+            System.out.println();
+        } else {
+            System.out.println("No change points were found in the series.");
+        }
+    }
+}
