2fcf6243c6b7e876b070a097eec135008c2942e9
Brandon Siegel <brandon.siegel@microsoft.com>
2020-08-27 20:00:01 -0700

    Fix deserialization of single entities for getEntity

--- sdk/tables/azure-data-tables/src/main/java/com/azure/data/tables/implementation/TablesJacksonSerializer.java
+++ sdk/tables/azure-data-tables/src/main/java/com/azure/data/tables/implementation/TablesJacksonSerializer.java
@@ -6,7 +6,6 @@ import com.azure.core.util.logging.ClientLogger;
 import com.azure.core.util.serializer.JacksonAdapter;
 import com.azure.core.util.serializer.SerializerEncoding;
 import com.azure.data.tables.implementation.models.TableEntityQueryResponse;
-import com.azure.data.tables.models.TableEntity;
 import com.fasterxml.jackson.databind.JsonNode;
 
 import java.io.ByteArrayInputStream;
@@ -53,26 +52,46 @@ public class TablesJacksonSerializer extends JacksonAdapter {
         String odataMetadata = null;
         List<Map<String, Object>> values = new ArrayList<>();
 
+        // Represents the entries in the response. It's possible that it is a single or multiple response.
         final JsonNode node = super.serializer().readTree(inputStream);
+        Map<String, Object> singleValue = null;
+
         for (Iterator<Map.Entry<String, JsonNode>> it = node.fields(); it.hasNext();) {
             final Map.Entry<String, JsonNode> entry = it.next();
+            final String fieldName = entry.getKey();
             final JsonNode childNode = entry.getValue();
 
-            if (entry.getKey().equals(TablesConstants.ODATA_METADATA_KEY)) {
+            if (fieldName.equals(TablesConstants.ODATA_METADATA_KEY)) {
                 odataMetadata = childNode.asText();
-                continue;
-            }
-
-            // Represents the entries in the response. It's possible that it is a single or multiple response.
-            if (entry.getKey().equals("value")) {
+            } else if (fieldName.equals("value")) {
                 if (childNode.isArray()) {
+                    // This is a multiple-entity response.
                     for (JsonNode childEntry : childNode) {
                         values.add(getEntityFieldsAsMap(childEntry));
                     }
                 } else {
-                    values.add(getEntityFieldsAsMap(childNode));
+                    // This is a single-entity response where the user just happened to use the key "value".
+                    if (singleValue == null) {
+                        singleValue = new HashMap<>();
+                    }
+                    singleValue.put(fieldName, getEntityFieldAsObject(node, fieldName));
                 }
+            } else {
+                // This is a single-entity response.
+                if (singleValue == null) {
+                    singleValue = new HashMap<>();
+                }
+                singleValue.put(fieldName, getEntityFieldAsObject(node, fieldName));
             }
+        }
+
+        if (singleValue != null) {
+            if (values.size() > 0) {
+                throw logger.logExceptionAsError(new IllegalStateException(
+                    "Unexpected response format. Response containing a 'value' array must not contain other properties."
+                ));
+            }
+            values.add(singleValue);
         }
 
         return (U) new TableEntityQueryResponse()
@@ -82,34 +101,33 @@ public class TablesJacksonSerializer extends JacksonAdapter {
 
     private Map<String, Object> getEntityFieldsAsMap(JsonNode node) {
         Map<String, Object> result = new HashMap<>();
-        for (Iterator<Map.Entry<String, JsonNode>> it = node.fields(); it.hasNext();) {
-            Map.Entry<String, JsonNode> entry = it.next();
-
-            String fieldName = entry.getKey();
-            if (!TablesConstants.METADATA_KEYS.contains(fieldName) && !fieldName.endsWith("@odata.type")) {
-                JsonNode typeNode = node.get(fieldName + "@odata.type");
-                if (typeNode != null) {
-                    String type = typeNode.asText();
-                    switch (type) {
-                        case "Edm.DateTime":
-                            try {
-                                result.put(fieldName, OffsetDateTime.parse(entry.getValue().asText()));
-                            } catch (DateTimeParseException e) {
-                                throw logger.logExceptionAsError(new IllegalArgumentException(String.format(
-                                    "'%s' value is not a valid OffsetDateTime.", TablesConstants.TIMESTAMP_KEY), e));
-                            }
-                            break;
-                        default:
-                            result.put(fieldName, entry.getValue().asText());
-                            break;
-                    }
-                } else {
-                    result.put(fieldName, entry.getValue().asText());
-                }
-            } else {
-                result.put(fieldName, entry.getValue().asText());
-            }
+        for (Iterator<String> it = node.fieldNames(); it.hasNext();) {
+            String fieldName = it.next();
+            result.put(fieldName, getEntityFieldAsObject(node, fieldName));
         }
         return result;
+    }
+
+    private Object getEntityFieldAsObject(JsonNode parentNode, String fieldName) {
+        JsonNode valueNode = parentNode.get(fieldName);
+        if (!TablesConstants.METADATA_KEYS.contains(fieldName) && !fieldName.endsWith("@odata.type")) {
+            JsonNode typeNode = parentNode.get(fieldName + "@odata.type");
+            if (typeNode != null) {
+                String type = typeNode.asText();
+                switch (type) {
+                    case "Edm.DateTime":
+                        try {
+                            return OffsetDateTime.parse(valueNode.asText());
+                        } catch (DateTimeParseException e) {
+                            throw logger.logExceptionAsError(new IllegalArgumentException(String.format(
+                                "'%s' value is not a valid OffsetDateTime.", TablesConstants.TIMESTAMP_KEY), e));
+                        }
+                    default:
+                        logger.warning(String.format("'%s' value has unknown OData type '%s'", fieldName, type));
+                        break; // Fallthrough to the default return value
+                }
+            }
+        }
+        return valueNode.asText();
     }
 }
