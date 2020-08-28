2bace5ff22b4549d399b3fb73e43d1d773ef3b9f
Jack Lu <dbqp99@msn.com>
2020-08-28 17:51:47 +0800

    remove unused import

--- sdk/spring/azure-spring-boot-test-servicebus-jms/src/test/java/com/microsoft/azure/servicebus/jms/Receiver.java
+++ sdk/spring/azure-spring-boot-test-servicebus-jms/src/test/java/com/microsoft/azure/servicebus/jms/Receiver.java
@@ -3,8 +3,6 @@
 
 package com.microsoft.azure.servicebus.jms;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
