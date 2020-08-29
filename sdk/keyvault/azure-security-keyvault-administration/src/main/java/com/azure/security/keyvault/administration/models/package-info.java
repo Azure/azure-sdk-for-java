e9480ec816dbc074074d076fb895ef3c261e113a
Victor Colin Amador <vicolina@microsoft.com>
2020-08-28 17:15:54 -0700

    Added package-info.java and module-info.java

---
+++ sdk/keyvault/azure-security-keyvault-administration/src/main/java/com/azure/security/keyvault/administration/models/package-info.java
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+/**
+ * Package containing classes used for representing backup and restore operations in Azure Key Vault.
+ */
+package com.azure.security.keyvault.administration.models;
--- sdk/keyvault/azure-security-keyvault-administration/src/main/java/module-info.java
+++ sdk/keyvault/azure-security-keyvault-administration/src/main/java/module-info.java
@@ -1,6 +1,14 @@
-module azure.security.keyvault.administration {
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+module com.azure.security.keyvault.administration {
     requires transitive com.azure.core;
 
     exports com.azure.security.keyvault.administration;
     exports com.azure.security.keyvault.administration.models;
+    exports com.azure.security.keyvault.administration.implementation.models;
+
+    opens com.azure.security.keyvault.administration to com.fasterxml.jackson.databind;
+    opens com.azure.security.keyvault.administration.models to com.fasterxml.jackson.databind;
+    opens com.azure.security.keyvault.administration.implementation to com.fasterxml.jackson.databind;
 }
