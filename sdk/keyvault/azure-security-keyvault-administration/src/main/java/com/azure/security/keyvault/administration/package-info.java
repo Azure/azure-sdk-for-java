5e5389269da35262ed36b43a7dcf9418b4741a4f
Victor Colin Amador <vicolina@microsoft.com>
2020-08-28 17:48:29 -0700

    Added missing package-info.java files.

---
+++ sdk/keyvault/azure-security-keyvault-administration/src/main/java/com/azure/security/keyvault/administration/package-info.java
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+/**
+ * Package containing classes for creating
+ * {@link com.azure.security.keyvault.administration.KeyVaultAccessControlAsyncClient}
+ * and {@link com.azure.security.keyvault.administration.KeyVaultAccessControlClient} to perform operations on Azure
+ * Key Vault.
+ */
+package com.azure.security.keyvault.administration;
