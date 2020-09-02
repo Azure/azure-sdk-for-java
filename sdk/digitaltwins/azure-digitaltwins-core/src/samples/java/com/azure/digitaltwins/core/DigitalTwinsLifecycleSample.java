243fe60c10edf1ebd7a720c9809d59b982717b91
Abhipsa Misra <abhipsa.misra@microsoft.com>
2020-09-02 09:42:53 -0700

    some small modifications

--- sdk/digitaltwins/azure-digitaltwins-core/src/samples/java/com/azure/digitaltwins/core/DigitalTwinsLifecycleSample.java
+++ sdk/digitaltwins/azure-digitaltwins-core/src/samples/java/com/azure/digitaltwins/core/DigitalTwinsLifecycleSample.java
@@ -5,7 +5,6 @@ package com.azure.digitaltwins.core;
 
 import com.azure.core.http.policy.HttpLogDetailLevel;
 import com.azure.core.http.policy.HttpLogOptions;
-import com.azure.core.util.logging.ClientLogger;
 import com.azure.digitaltwins.core.implementation.models.ErrorResponseException;
 import com.azure.identity.ClientSecretCredentialBuilder;
 
@@ -19,14 +18,12 @@ import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
 public class DigitalTwinsLifecycleSample {
-    private static final ClientLogger logger = new ClientLogger(DigitalTwinsLifecycleSample.class);
-
     private static final String tenantId = System.getenv("TENANT_ID");
     private static final String clientId = System.getenv("CLIENT_ID");
     private static final String clientSecret = System.getenv("CLIENT_SECRET");
     private static final String endpoint = System.getenv("DIGITAL_TWINS_ENDPOINT");
 
-    private static final int MaxTimeForTwinOperationsInSeconds = 10;
+    private static final int MaxWaitTimeAsyncOperationsInSeconds = 10;
 
     private static final URL DtdlDirectoryUrl = DigitalTwinsLifecycleSample.class.getClassLoader().getResource("DTDL");
     private static final Path DtDlDirectoryPath;
@@ -41,7 +38,7 @@ public class DigitalTwinsLifecycleSample {
             assert DtdlDirectoryUrl != null;
             DtDlDirectoryPath = Paths.get(DtdlDirectoryUrl.toURI());
         } catch (URISyntaxException e) {
-            logger.error("Unable to convert the DTDL directory URL to URI: ", e);
+            System.err.println("Unable to convert the DTDL directory URL to URI: " + e);
             throw new RuntimeException(e);
         }
         TwinsPath = Paths.get(DtDlDirectoryPath.toString(), "DigitalTwins");
@@ -59,7 +56,7 @@ public class DigitalTwinsLifecycleSample {
             .endpoint(endpoint)
             .httpLogOptions(
                 new HttpLogOptions()
-                    .setLogLevel(HttpLogDetailLevel.BODY_AND_HEADERS))
+                    .setLogLevel(HttpLogDetailLevel.NONE))
             .buildAsyncClient();
     }
 
@@ -76,22 +73,21 @@ public class DigitalTwinsLifecycleSample {
         Map<String, String> twins = FileHelper.loadAllFilesInPath(TwinsPath);
         final Semaphore deleteTwinsSemaphore = new Semaphore(0);
 
+        // Call APIs to delete the twins. For each async operation, once the operation is completed successfully, a semaphore is released.
         twins
             .forEach((twinId, twinContent) -> client.deleteDigitalTwin(twinId)
-                .subscribe(
-                    aVoid -> { },
-                    throwable -> {
-                        // If digital twin does not exist, ignore.
-                        if (!(throwable instanceof ErrorResponseException) || !((ErrorResponseException) throwable).getValue().getError().getCode().equals("DigitalTwinNotFound")) {
-                            throw new RuntimeException(throwable);
-                        }
-                    },
-                    () -> {
-                        System.out.println("Deleted digital twin: " + twinId);
-                        deleteTwinsSemaphore.release();
-                    }));
+                .doOnSuccess(aVoid -> System.out.println("Deleted digital twin: " + twinId))
+                .doOnError(throwable -> {
+                    // If digital twin does not exist, ignore.
+                    if (!(throwable instanceof ErrorResponseException) || !((ErrorResponseException) throwable).getValue().getError().getCode().equals("DigitalTwinNotFound")) {
+                        System.err.println("Could not delete digital twin " + twinId + " due to " + throwable);
+                    }
+                })
+                .doOnTerminate(deleteTwinsSemaphore::release)
+                .subscribe());
 
-        boolean created = deleteTwinsSemaphore.tryAcquire(twins.size(), MaxTimeForTwinOperationsInSeconds, TimeUnit.SECONDS);
+        // Verify that a semaphore has been released for each async operation, signifying that the async call has completed.
+        boolean created = deleteTwinsSemaphore.tryAcquire(twins.size(), MaxWaitTimeAsyncOperationsInSeconds, TimeUnit.SECONDS);
         System.out.println("Twins deleted: " + created);
     }
 
@@ -100,19 +96,16 @@ public class DigitalTwinsLifecycleSample {
         Map<String, String> twins = FileHelper.loadAllFilesInPath(TwinsPath);
         final Semaphore createTwinsSemaphore = new Semaphore(0);
 
-        // Call APIs to create the twins.
+        // Call APIs to create the twins. For each async operation, once the operation is completed successfully, a semaphore is released.
         twins
-            .forEach(
-                (twinId, twinContent) -> client.createDigitalTwinWithResponse(twinId, twinContent)
-                    .subscribe(
-                        stringDigitalTwinsResponse -> {
-                            System.out.println("Created digital twin: " + twinId);
-                            System.out.println("\t Body: " + stringDigitalTwinsResponse.getValue());
-                        },
-                        throwable -> System.err.println("Could not create digital twin " + twinId + " due to " + throwable),
-                        createTwinsSemaphore::release));
+            .forEach((twinId, twinContent) -> client.createDigitalTwinWithResponse(twinId, twinContent)
+                .doOnSuccess(response -> System.out.println("Created digital twin: " + twinId + "\n\t Body: " + response.getValue()))
+                .doOnError(throwable -> System.err.println("Could not create digital twin " + twinId + " due to " + throwable))
+                .doOnTerminate(createTwinsSemaphore::release)
+                .subscribe());
 
-        boolean created = createTwinsSemaphore.tryAcquire(twins.size(), MaxTimeForTwinOperationsInSeconds, TimeUnit.SECONDS);
+        // Verify that a semaphore has been released for each async operation, signifying that the async call has completed.
+        boolean created = createTwinsSemaphore.tryAcquire(twins.size(), MaxWaitTimeAsyncOperationsInSeconds, TimeUnit.SECONDS);
         System.out.println("Twins created: " + created);
     }
 }
