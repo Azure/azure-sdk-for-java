fbd12ff65b416c483f9b0b320e34ad50e4c916dc
Chuang <54572251+xccc-msft@users.noreply.github.com>
2020-09-02 17:20:13 +0800

    mgmt update maven profile (#14728)
    * mgmt update maven build profile
    
    * move TestUtilities to azure-resourcemanager-test
    
    * update TestUtilities

--- sdk/resourcemanager/azure-resourcemanager-compute/src/test/java/com/azure/resourcemanager/compute/VirtualMachineScaleSetOperationsTests.java
+++ sdk/resourcemanager/azure-resourcemanager-compute/src/test/java/com/azure/resourcemanager/compute/VirtualMachineScaleSetOperationsTests.java
@@ -46,7 +46,7 @@ import com.azure.resourcemanager.network.models.VirtualMachineScaleSetNetworkInt
 import com.azure.resourcemanager.network.models.VirtualMachineScaleSetNicIpConfiguration;
 import com.azure.resourcemanager.resources.fluentcore.utils.SdkContext;
 import com.azure.resourcemanager.resources.models.ResourceGroup;
-import com.azure.resourcemanager.resources.core.TestUtilities;
+import com.azure.resourcemanager.test.utils.TestUtilities;
 import com.azure.resourcemanager.resources.fluentcore.arm.AvailabilityZoneId;
 import com.azure.resourcemanager.resources.fluentcore.arm.Region;
 import com.azure.core.management.profile.AzureProfile;
