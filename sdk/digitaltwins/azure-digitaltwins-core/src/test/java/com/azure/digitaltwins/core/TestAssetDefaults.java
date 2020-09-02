24d42c70db3a42e43ac11104da5d789ac8f2acc9
timtay-microsoft <timtay@microsoft.com>
2020-09-02 12:17:55 -0700

    to squash

--- sdk/digitaltwins/azure-digitaltwins-core/src/test/java/com/azure/digitaltwins/core/TestAssetDefaults.java
+++ sdk/digitaltwins/azure-digitaltwins-core/src/test/java/com/azure/digitaltwins/core/TestAssetDefaults.java
@@ -1,31 +1,31 @@
 package com.azure.digitaltwins.core;
 
 public class TestAssetDefaults {
-    public final String FloorTwinIdPrefix = "floorTwin";
+    public static final String FloorTwinIdPrefix = "floorTwin";
 
-    public final String RoomTwinIdPrefix = "roomTwin";
+    public static final String RoomTwinIdPrefix = "roomTwin";
 
-    public final String HvacTwinIdPrefix = "hvacTwin";
+    public static final String HvacTwinIdPrefix = "hvacTwin";
 
-    public final String RoomWithWifiTwinIdPrefix = "roomWithWifiTwin";
+    public static final String RoomWithWifiTwinIdPrefix = "roomWithWifiTwin";
 
-    public final String WifiModelIdPrefix = "dtmi:example:wifi;1";
+    public static final String WifiModelIdPrefix = "dtmi:example:wifi;1";
 
-    public final String RoomModelIdPrefix = "dtmi:example:room;1";
+    public static final String RoomModelIdPrefix = "dtmi:example:room;1";
 
-    public final String FloorModelIdPrefix = "dtmi:example:floor;1";
+    public static final String FloorModelIdPrefix = "dtmi:example:floor;1";
 
-    public final String HvacModelIdPrefix = "dtmi:example:hvac;1";
+    public static final String HvacModelIdPrefix = "dtmi:example:hvac;1";
 
-    public final String RoomWithWifiModelIdPrefix = "dtmi:example:wifiroom;1";
+    public static final String RoomWithWifiModelIdPrefix = "dtmi:example:wifiroom;1";
 
-    public final String FloorModelId = "dtmi:example:Floor;1";
+    public static final String FloorModelId = "dtmi:example:Floor;1";
 
-    public final String RoomModelId = "dtmi:example:Room;1";
+    public static final String RoomModelId = "dtmi:example:Room;1";
 
-    public final String HvacModelId = "dtmi:example:Hvac;1";
+    public static final String HvacModelId = "dtmi:example:Hvac;1";
 
-    public final String BuildingModelId = "dtmi:example:Building;1";
+    public static final String BuildingModelId = "dtmi:example:Building;1";
 
-    public final String WardModelId = "dtmi:example:Ward;1";
+    public static final String WardModelId = "dtmi:example:Ward;1";
 }
