fddbee20822140dde137e871e1745a0808adb292
Rujun Chen <rujche@microsoft.com>
2020-09-04 10:59:10 +0800

    reinstall 2 more modules.

--- sdk/spring/azure-spring-boot-test-keyvault/install_java.sh
+++ sdk/spring/azure-spring-boot-test-keyvault/install_java.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt remove --purge openjdk-8-jre-headless
+sudo apt remove --purge openjdk-8-jre-headless openjdk-8-jre openjdk-8-jdk-headless openjdk-8-jdk
 sudo apt autoremove
-sudo apt install --reinstall openjdk-8-jre-headless
-sudo apt-get install -y openjdk-8-jdk
-# sudo apt-get install -y openjdk-8-jdk maven git
+sudo apt install --reinstall openjdk-8-jre-headless openjdk-8-jre openjdk-8-jdk-headless openjdk-8-jdk
