a76dac9a8950f30dcad30528cf9cc3254b0eadba
Rujun Chen <rujche@microsoft.com>
2020-09-04 10:10:14 +0800

    Reinstall openjsk-9-jre-headless.

--- sdk/spring/azure-spring-boot-test-keyvault/install_java.sh
+++ sdk/spring/azure-spring-boot-test-keyvault/install_java.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 sudo apt-get update
+sudo apt remove --purge openjdk-8-jre-headless
+sudo apt autoremove
+sudo apt install --reinstall openjdk-8-jre-headless
 sudo apt-get install -y openjdk-8-jdk
 # sudo apt-get install -y openjdk-8-jdk maven git
