eecf0ed92b41d7157bae7761387c9ee6ee1f43b4
Shawn Fang <45607042+mssfang@users.noreply.github.com>
2020-09-03 17:22:01 -0700

    [TA] Add domainFilter feature to PII endpoint (#14714)
    * add domainFilter feature

--- sdk/textanalytics/azure-ai-textanalytics/src/samples/java/com/azure/ai/textanalytics/batch/RecognizePiiEntitiesBatchDocuments.java
+++ sdk/textanalytics/azure-ai-textanalytics/src/samples/java/com/azure/ai/textanalytics/batch/RecognizePiiEntitiesBatchDocuments.java
@@ -7,7 +7,7 @@ import com.azure.ai.textanalytics.TextAnalyticsClient;
 import com.azure.ai.textanalytics.TextAnalyticsClientBuilder;
 import com.azure.ai.textanalytics.models.PiiEntityCollection;
 import com.azure.ai.textanalytics.models.RecognizePiiEntitiesResult;
-import com.azure.ai.textanalytics.models.TextAnalyticsRequestOptions;
+import com.azure.ai.textanalytics.models.RecognizePiiEntityOptions;
 import com.azure.ai.textanalytics.models.TextDocumentBatchStatistics;
 import com.azure.ai.textanalytics.models.TextDocumentInput;
 import com.azure.ai.textanalytics.util.RecognizePiiEntitiesResultCollection;
@@ -42,12 +42,12 @@ public class RecognizePiiEntitiesBatchDocuments {
             new TextDocumentInput("2", "Visa card 4111 1111 1111 1111").setLanguage("en")
         );
 
-        // Request options: show statistics and model version
-        TextAnalyticsRequestOptions requestOptions = new TextAnalyticsRequestOptions().setIncludeStatistics(true).setModelVersion("latest");
+        // Show statistics and model version
+        RecognizePiiEntityOptions options = new RecognizePiiEntityOptions().setIncludeStatistics(true).setModelVersion("latest");
 
         // Recognizing Personally Identifiable Information entities for each document in a batch of documents
         Response<RecognizePiiEntitiesResultCollection> piiEntitiesBatchResultResponse =
-            client.recognizePiiEntitiesBatchWithResponse(documents, requestOptions, Context.NONE);
+            client.recognizePiiEntitiesBatchWithResponse(documents, options, Context.NONE);
 
         // Response's status code
         System.out.printf("Status code of request response: %d%n", piiEntitiesBatchResultResponse.getStatusCode());
