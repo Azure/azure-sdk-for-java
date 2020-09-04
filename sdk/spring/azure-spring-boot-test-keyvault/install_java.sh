aa7b5ef2f35eb9aa8c483f554aa7b7ebfdab275a
Rujun Chen <rujche@microsoft.com>
2020-09-04 11:16:30 +0800

    Install default-jdk

--- sdk/spring/azure-spring-boot-test-keyvault/install_java.sh
+++ sdk/spring/azure-spring-boot-test-keyvault/install_java.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt remove --purge openjdk-8-jre-headless openjdk-8-jre openjdk-8-jdk-headless openjdk-8-jdk
-sudo apt autoremove
-sudo apt install --reinstall openjdk-8-jre-headless openjdk-8-jre openjdk-8-jdk-headless openjdk-8-jdk
+sudo apt-get install default-jdk
