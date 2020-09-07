e36a873a0f650b411c66ab775ebb3f822804f37d
Tanyi Chen <tanchen@microsoft.com>
2020-09-07 14:19:24 +0800

    service bus

--- sdk/resourcemanager/azure-resourcemanager-servicebus/src/main/java/com/azure/resourcemanager/servicebus/fluent/OperationsClient.java
+++ sdk/resourcemanager/azure-resourcemanager-servicebus/src/main/java/com/azure/resourcemanager/servicebus/fluent/OperationsClient.java
@@ -25,7 +25,6 @@ import com.azure.core.management.exception.ManagementException;
 import com.azure.core.util.Context;
 import com.azure.core.util.FluxUtil;
 import com.azure.core.util.logging.ClientLogger;
-import com.azure.resourcemanager.servicebus.ServiceBusManagementClient;
 import com.azure.resourcemanager.servicebus.fluent.inner.OperationInner;
 import com.azure.resourcemanager.servicebus.fluent.inner.OperationListResultInner;
 import reactor.core.publisher.Mono;
--- sdk/resourcemanager/azure-resourcemanager-servicebus/src/main/java/com/azure/resourcemanager/servicebus/fluent/SubscriptionsClient.java
+++ sdk/resourcemanager/azure-resourcemanager-servicebus/src/main/java/com/azure/resourcemanager/servicebus/fluent/SubscriptionsClient.java
@@ -28,7 +28,6 @@ import com.azure.core.management.exception.ManagementException;
 import com.azure.core.util.Context;
 import com.azure.core.util.FluxUtil;
 import com.azure.core.util.logging.ClientLogger;
-import com.azure.resourcemanager.servicebus.ServiceBusManagementClient;
 import com.azure.resourcemanager.servicebus.fluent.inner.SubscriptionListResultInner;
 import com.azure.resourcemanager.servicebus.fluent.inner.SubscriptionResourceInner;
 import com.azure.resourcemanager.servicebus.models.SubscriptionCreateOrUpdateParameters;
