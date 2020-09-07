c053511b4187c6455c92b006993131cc6b1241bb
Tanyi Chen <tanchen@microsoft.com>
2020-09-07 14:18:12 +0800

    network

--- sdk/resourcemanager/azure-resourcemanager-network/src/main/java/com/azure/resourcemanager/network/fluent/BastionHostsClient.java
+++ sdk/resourcemanager/azure-resourcemanager-network/src/main/java/com/azure/resourcemanager/network/fluent/BastionHostsClient.java
@@ -31,7 +31,6 @@ import com.azure.core.util.FluxUtil;
 import com.azure.core.util.logging.ClientLogger;
 import com.azure.core.util.polling.PollerFlux;
 import com.azure.core.util.polling.SyncPoller;
-import com.azure.resourcemanager.network.NetworkManagementClient;
 import com.azure.resourcemanager.network.fluent.inner.BastionHostInner;
 import com.azure.resourcemanager.network.fluent.inner.BastionHostListResultInner;
 import com.azure.resourcemanager.resources.fluentcore.collection.InnerSupportsDelete;
