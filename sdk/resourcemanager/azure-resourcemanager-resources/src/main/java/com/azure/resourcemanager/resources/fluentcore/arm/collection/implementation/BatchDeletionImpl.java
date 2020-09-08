6b0b7237c47e06416aa92711c511f12335828d63
Weidong Xu <weidxu@microsoft.com>
2020-09-08 14:17:18 +0800

    use flatMapDelayError

--- sdk/resourcemanager/azure-resourcemanager-resources/src/main/java/com/azure/resourcemanager/resources/fluentcore/arm/collection/implementation/BatchDeletionImpl.java
+++ sdk/resourcemanager/azure-resourcemanager-resources/src/main/java/com/azure/resourcemanager/resources/fluentcore/arm/collection/implementation/BatchDeletionImpl.java
@@ -8,7 +8,6 @@ import com.azure.resourcemanager.resources.fluentcore.utils.ReactorMapper;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.function.BiFunction;
 
@@ -17,21 +16,17 @@ import java.util.function.BiFunction;
  */
 public class BatchDeletionImpl {
 
-    @SuppressWarnings({"unchecked", "rawtypes"})
     public static Flux<String> deleteByIdsAsync(Collection<String> ids,
                                                 BiFunction<String, String, Mono<Void>> deleteByIdAsync) {
         if (ids == null || ids.isEmpty()) {
             return Flux.empty();
+        } else {
+            return Flux.fromIterable(ids)
+                .flatMapDelayError(id -> {
+                    final String resourceGroupName = ResourceUtils.groupFromResourceId(id);
+                    final String name = ResourceUtils.nameFromResourceId(id);
+                    return ReactorMapper.map(deleteByIdAsync.apply(resourceGroupName, name), id);
+                }, 32, 32);
         }
-
-        Collection<Mono<String>> observables = new ArrayList<>();
-        for (String id : ids) {
-            final String resourceGroupName = ResourceUtils.groupFromResourceId(id);
-            final String name = ResourceUtils.nameFromResourceId(id);
-            Mono<String> o = ReactorMapper.map(deleteByIdAsync.apply(resourceGroupName, name), id);
-            observables.add(o);
-        }
-
-        return Flux.mergeDelayError(32, observables.toArray(new Mono[0]));
     }
 }
