a6a6b0144d423ca79dd57231ae010583ac38ef16
Azad Abbasi <azabbasi@microsoft.com>
2020-09-08 11:22:12 -0700

    Update update-recorded-sessions.ps1

--- sdk/digitaltwins/azure-digitaltwins-core/update-recorded-sessions.ps1
+++ sdk/digitaltwins/azure-digitaltwins-core/update-recorded-sessions.ps1
@@ -8,5 +8,4 @@ if (-not (Test-Path -Path $sourceSessionRecordPath))
     Write-Host("Ensure you have run the tests in Record mode. $sessionRecordPath does not exist`n")
 }
 
-
 Copy-Item -Path $sourceSessionRecordPath -Destination $targetSessionRecordPath -Force -Recurse -Container
