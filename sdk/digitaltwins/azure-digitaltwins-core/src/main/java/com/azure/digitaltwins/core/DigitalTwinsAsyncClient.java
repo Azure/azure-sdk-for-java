ff38b3b8d49ef3ef979e0c9036a1f1eaedf22606
Azad Abbasi <azabbasi@microsoft.com>
2020-09-10 14:04:45 -0700

    Update DigitalTwinsAsyncClient.java

--- sdk/digitaltwins/azure-digitaltwins-core/src/main/java/com/azure/digitaltwins/core/DigitalTwinsAsyncClient.java
+++ sdk/digitaltwins/azure-digitaltwins-core/src/main/java/com/azure/digitaltwins/core/DigitalTwinsAsyncClient.java
@@ -1383,10 +1383,19 @@ public final class DigitalTwinsAsyncClient {
     }
 
     Mono<Response<Void>> publishTelemetryWithResponse(String digitalTwinId, String payload, PublishTelemetryRequestOptions publishTelemetryRequestOptions, Context context) {
+        Object payloadObject = null;
+        try {
+            payloadObject = mapper.readValue(payload, Object.class);
+        }
+        catch (JsonProcessingException e) {
+            logger.error("Could not parse the payload [%s]: %s", payload, e);
+            return Mono.error(e);
+        }
+
         return protocolLayer.getDigitalTwins().sendTelemetryWithResponseAsync(
             digitalTwinId,
             publishTelemetryRequestOptions.getMessageId(),
-            payload,
+            payloadObject,
             publishTelemetryRequestOptions.getTimestamp().toString(),
             context);
     }
