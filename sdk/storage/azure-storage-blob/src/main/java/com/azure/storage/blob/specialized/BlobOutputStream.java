de70e52efd58abf32114da6e6aa70374a814c8dc
Gauri Prasad <gapra@microsoft.com>
2020-09-10 13:02:58 -0700

    Fixed error

--- sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/specialized/BlobOutputStream.java
+++ sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/specialized/BlobOutputStream.java
@@ -225,7 +225,12 @@ public abstract class BlobOutputStream extends StorageOutputStream {
                 .setTags(tags).setTier(tier).setRequestConditions(requestConditions))
                 // This allows the operation to continue while maintaining the error that occurred.
                 .onErrorResume(e -> {
-                    this.lastError = new IOException(e);
+                    if (e instanceof IOException) {
+                        this.lastError = (IOException) e;
+                    } else {
+                        this.lastError = new IOException(e);
+                    }
+
                     return Mono.empty();
                 })
                 .doOnTerminate(() -> {
