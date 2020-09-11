9a9ee77d0aa616a3a69d9ba19b6438fb125984f9
Azad Abbasi <azabbasi@microsoft.com>
2020-09-11 12:14:21 -0700

    Update module-info.java

--- sdk/digitaltwins/azure-digitaltwins-core/src/main/java/module-info.java
+++ sdk/digitaltwins/azure-digitaltwins-core/src/main/java/module-info.java
@@ -3,20 +3,17 @@
 
 module come.azure.digitaltwins.core {
     requires transitive com.azure.core;
-    requires com.fasterxml.jackson.annotation;
-    requires com.azure.http.netty;
-
-    opens com.azure.digitaltwins.core to come.azure.core, com.azure.http.netty;
-    opens com.azure.digitaltwins.core.models to com.fasterxml.jackson.annotation, come.azure.core;
-    opens com.azure.digitaltwins.core.util to com.fasterxml.jackson.annotation, come.azure.core;
-    opens com.azure.digitaltwins.core.serialization to com.fasterxml.jackson.annotation, come.azure.core;
-    opens com.azure.digitaltwins.core.implementation to com.fasterxml.jackson.annotation, come.azure.core;
-    opens com.azure.digitaltwins.core.implementation.models to com.fasterxml.jackson.annotation, com.azure.core;
-    opens com.azure.digitaltwins.core.implementation.serializer to com.fasterxml.jackson.annotation, com.azure.core;
-    opens com.azure.digitaltwins.core.implementation.converters to com.fasterxml.jackson.annotation, com.azure.core;
 
     exports com.azure.digitaltwins.core;
     exports com.azure.digitaltwins.core.models;
     exports com.azure.digitaltwins.core.serialization;
     exports com.azure.digitaltwins.core.util;
+
+    opens com.azure.digitaltwins.core.implementation to com.fasterxml.jackson.databind;
+    opens com.azure.digitaltwins.core.implementation.models to com.fasterxml.jackson.databind, com.azure.core;
+    opens com.azure.digitaltwins.core.models to com.fasterxml.jackson.databind;
+    opens com.azure.digitaltwins.core.util to com.fasterxml.jackson.databind;
+    opens com.azure.digitaltwins.core.serialization to com.fasterxml.jackson.databind;
+    opens com.azure.digitaltwins.core.implementation.serializer to com.fasterxml.jackson.databind, com.azure.core;
+    opens com.azure.digitaltwins.core.implementation.converters to com.fasterxml.jackson.databind, com.azure.core;
 }
