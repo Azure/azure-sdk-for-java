0e694ed05c4564dc008bcb6d6862e0a325c35c1b
Weidong Xu <weidxu@microsoft.com>
2020-09-11 10:05:41 +0800

    mgmt, appservice redegisn (#14890)
    * bump netty version
    
    * add WebSiteBase
    
    * redesign WebApps.List
    
    * FunctionAppBasic
    
    * add test case for WebAppBasic
    
    * add WebDeploymentSlotBasic and FunctionDeploymentSlotBasic
    
    * simplify FunctionRuntimeStack
    
    * changelog
    
    * add test for batch flux
    
    * delete ReactorMapper

--- sdk/resourcemanager/azure-resourcemanager-appservice/src/main/java/com/azure/resourcemanager/appservice/implementation/WebAppsImpl.java
+++ sdk/resourcemanager/azure-resourcemanager-appservice/src/main/java/com/azure/resourcemanager/appservice/implementation/WebAppsImpl.java
@@ -4,23 +4,29 @@
 package com.azure.resourcemanager.appservice.implementation;
 
 import com.azure.core.http.rest.PagedFlux;
+import com.azure.core.http.rest.PagedIterable;
 import com.azure.resourcemanager.appservice.AppServiceManager;
 import com.azure.resourcemanager.appservice.fluent.WebAppsClient;
 import com.azure.resourcemanager.appservice.fluent.inner.SiteConfigResourceInner;
 import com.azure.resourcemanager.appservice.fluent.inner.SiteInner;
 import com.azure.resourcemanager.appservice.fluent.inner.SiteLogsConfigInner;
 import com.azure.resourcemanager.appservice.models.WebApp;
+import com.azure.resourcemanager.appservice.models.WebAppBasic;
 import com.azure.resourcemanager.appservice.models.WebApps;
-import com.azure.resourcemanager.resources.fluentcore.arm.collection.implementation.TopLevelModifiableResourcesImpl;
-import com.azure.resourcemanager.resources.fluentcore.utils.PagedConverter;
+import com.azure.resourcemanager.resources.fluentcore.arm.collection.SupportsBatchDeletion;
+import com.azure.resourcemanager.resources.fluentcore.arm.collection.implementation.BatchDeletionImpl;
+import com.azure.resourcemanager.resources.fluentcore.arm.collection.implementation.GroupableResourcesImpl;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 
 /** The implementation for WebApps. */
 public class WebAppsImpl
-    extends TopLevelModifiableResourcesImpl<WebApp, WebAppImpl, SiteInner, WebAppsClient, AppServiceManager>
-    implements WebApps {
+    extends GroupableResourcesImpl<WebApp, WebAppImpl, SiteInner, WebAppsClient, AppServiceManager>
+    implements WebApps, SupportsBatchDeletion {
 
     public WebAppsImpl(final AppServiceManager manager) {
         super(manager.inner().getWebApps(), manager);
@@ -30,8 +36,7 @@ public class WebAppsImpl
     public Mono<WebApp> getByResourceGroupAsync(final String groupName, final String name) {
         final WebAppsImpl self = this;
         return this
-            .inner()
-            .getByResourceGroupAsync(groupName, name)
+            .getInnerAsync(groupName, name)
             .flatMap(
                 siteInner ->
                     Mono
@@ -40,6 +45,16 @@ public class WebAppsImpl
                             self.inner().getDiagnosticLogsConfigurationAsync(groupName, name),
                             (SiteConfigResourceInner siteConfigResourceInner, SiteLogsConfigInner logsConfigInner) ->
                                 wrapModel(siteInner, siteConfigResourceInner, logsConfigInner)));
+    }
+
+    @Override
+    protected Mono<SiteInner> getInnerAsync(String resourceGroupName, String name) {
+        return this.inner().getByResourceGroupAsync(resourceGroupName, name);
+    }
+
+    @Override
+    protected Mono<Void> deleteInnerAsync(String resourceGroupName, String name) {
+        return inner().deleteAsync(resourceGroupName, name).then();
     }
 
     @Override
@@ -60,29 +75,51 @@ public class WebAppsImpl
     }
 
     @Override
-    protected PagedFlux<WebApp> wrapPageAsync(PagedFlux<SiteInner> innerPage) {
-        return PagedConverter
-            .flatMapPage(
-                innerPage,
-                siteInner -> {
-                    if (siteInner.kind() == null || Arrays.asList(siteInner.kind().split(",")).contains("app")) {
-                        return Mono
-                            .zip(
-                                this.inner().getConfigurationAsync(siteInner.resourceGroup(), siteInner.name()),
-                                this
-                                    .inner()
-                                    .getDiagnosticLogsConfigurationAsync(
-                                        siteInner.resourceGroup(), siteInner.name()),
-                                (siteConfigResourceInner, logsConfigInner) ->
-                                    this.wrapModel(siteInner, siteConfigResourceInner, logsConfigInner));
-                    } else {
-                        return Mono.empty();
-                    }
-                });
+    public WebAppImpl define(String name) {
+        return wrapModel(name);
     }
 
     @Override
-    public WebAppImpl define(String name) {
-        return wrapModel(name);
+    public Flux<String> deleteByIdsAsync(Collection<String> ids) {
+        return BatchDeletionImpl.deleteByIdsAsync(ids, this::deleteInnerAsync);
+    }
+
+    @Override
+    public Flux<String> deleteByIdsAsync(String... ids) {
+        return this.deleteByIdsAsync(new ArrayList<>(Arrays.asList(ids)));
+    }
+
+    @Override
+    public void deleteByIds(Collection<String> ids) {
+        if (ids != null && !ids.isEmpty()) {
+            this.deleteByIdsAsync(ids).blockLast();
+        }
+    }
+
+    @Override
+    public void deleteByIds(String... ids) {
+        this.deleteByIds(new ArrayList<>(Arrays.asList(ids)));
+    }
+
+    @Override
+    public PagedIterable<WebAppBasic> listByResourceGroup(String resourceGroupName) {
+        return new PagedIterable<>(this.listByResourceGroupAsync(resourceGroupName));
+    }
+
+    @Override
+    public PagedFlux<WebAppBasic> listByResourceGroupAsync(String resourceGroupName) {
+        return inner().listByResourceGroupAsync(resourceGroupName)
+            .mapPage(inner -> new WebAppBasicImpl(inner, this.manager()));
+    }
+
+    @Override
+    public PagedIterable<WebAppBasic> list() {
+        return new PagedIterable<>(this.listAsync());
+    }
+
+    @Override
+    public PagedFlux<WebAppBasic> listAsync() {
+        return inner().listAsync()
+            .mapPage(inner -> new WebAppBasicImpl(inner, this.manager()));
     }
 }
