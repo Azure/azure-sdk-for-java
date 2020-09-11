fd97441fd63968256692c9ddb674ff7e12d27527
Azad Abbasi <azabbasi@microsoft.com>
2020-09-11 12:26:34 -0700

    Testing the pipeline

--- sdk/digitaltwins/azure-digitaltwins-core/src/main/java/module-info.java
+++ sdk/digitaltwins/azure-digitaltwins-core/src/main/java/module-info.java
@@ -9,6 +9,7 @@ module com.azure.digitaltwins.core {
     exports com.azure.digitaltwins.core.serialization;
     exports com.azure.digitaltwins.core.util;
 
+    opens com.azure.digitaltwins.core to java.util;
     opens com.azure.digitaltwins.core.implementation to com.fasterxml.jackson.databind;
     opens com.azure.digitaltwins.core.implementation.models to com.fasterxml.jackson.databind, com.azure.core;
     opens com.azure.digitaltwins.core.models to com.fasterxml.jackson.databind;
