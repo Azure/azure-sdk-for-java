82d4ab9b8947e347464971314573edb5060431fc
Rujun Chen <Rujun.Chen@microsoft.com>
2020-09-11 11:29:18 +0800

    No logic change, just make code easier to read. (#14973)
    * No logic change, just make code easier to read.
    
    * No logic change, just make code easier to read.
    
    * No logic change, just make code easier to read.
    
    * No logic change, just make code easier to read.
    
    * No logic change, just make code easier to read.
    
    * No logic change, just make code easier to read.
    
    * Remove useledd code 'authentication.setAuthenticated(true);', because it
    is already executed in constructor.
    
    * No logic change, just make code easier to read.
    
    * No logic change, just make code easier to read.
    
    * No logic change, just change class name.
    
    * No logic change, just change class name.
    
    * Delete AADGraphApiObjectType.java, use Constants instead.
    
    * No logic change, just make code easier to read.
    
    * No logic change, just make code easier to read.
    
    * No logic change, just make code easier to read.
    
    * No logic change, just make code easier to read.
    
    * Fix build error.
    
    Co-authored-by: Rujun Chen <rujche@microsoft.com>

--- sdk/spring/azure-spring-boot/src/main/java/com/microsoft/azure/spring/autoconfigure/aad/Constants.java
+++ sdk/spring/azure-spring-boot/src/main/java/com/microsoft/azure/spring/autoconfigure/aad/Constants.java
@@ -3,10 +3,11 @@
 
 package com.microsoft.azure.spring.autoconfigure.aad;
 
-public class AADConstantsHelper {
+public class Constants {
     public static final String CONDITIONAL_ACCESS_POLICY = "conditional_access_policy";
     public static final String SAVED_REQUEST = "SPRING_SECURITY_SAVED_REQUEST";
     public static final String CAP_CLAIMS = "CAP_Claims";
     public static final String CLAIMS = "claims";
     public static final String FAILURE_DEFAULT_URL = "/login?error";
+    public static final String OBJECT_TYPE_GROUP = "Group";
 }
