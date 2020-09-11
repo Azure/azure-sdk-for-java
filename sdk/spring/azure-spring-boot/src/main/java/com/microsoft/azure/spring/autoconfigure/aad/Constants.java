eb622c8ca2fdbf386628918112a1bc6663d7b437
Rujun Chen <rujche@microsoft.com>
2020-09-11 17:49:43 +0800

    No logic change, just make code easier to read.

--- sdk/spring/azure-spring-boot/src/main/java/com/microsoft/azure/spring/autoconfigure/aad/Constants.java
+++ sdk/spring/azure-spring-boot/src/main/java/com/microsoft/azure/spring/autoconfigure/aad/Constants.java
@@ -7,6 +7,5 @@ public class Constants {
     public static final String SAVED_REQUEST = "SPRING_SECURITY_SAVED_REQUEST";
     public static final String CAP_CLAIMS = "CAP_Claims";
     public static final String CLAIMS = "claims";
-    public static final String FAILURE_DEFAULT_URL = "/login?error";
     public static final String OBJECT_TYPE_GROUP = "Group";
 }
