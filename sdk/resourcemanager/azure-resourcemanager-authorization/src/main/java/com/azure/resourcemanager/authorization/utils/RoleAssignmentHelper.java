2e3665e4c974cc87468bfb4f165317838a587616
xccc-msft <chuxi@microsoft.com>
2020-09-14 16:06:36 +0800

    add module-info for azure-resourcemanager-authorization

--- sdk/resourcemanager/azure-resourcemanager-authorization/src/main/java/com/azure/resourcemanager/authorization/utils/RoleAssignmentHelper.java
+++ sdk/resourcemanager/azure-resourcemanager-authorization/src/main/java/com/azure/resourcemanager/authorization/utils/RoleAssignmentHelper.java
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.azure.resourcemanager.authorization.implementation;
+package com.azure.resourcemanager.authorization.utils;
 
 import com.azure.core.management.exception.ManagementException;
 import com.azure.resourcemanager.authorization.AuthorizationManager;
