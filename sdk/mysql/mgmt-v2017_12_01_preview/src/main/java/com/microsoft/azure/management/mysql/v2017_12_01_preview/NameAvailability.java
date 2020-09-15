c189a054f1ae84b29fb520fd6a8f11c11dd91a88
SDK Automation <sdkautomation@microsoft.com>
2020-09-15 00:10:40 +0000

    Generated from 1b5edee19d240ffefbc58c741738fd88d138c821

--- sdk/mysql/mgmt-v2017_12_01_preview/src/main/java/com/microsoft/azure/management/mysql/v2017_12_01_preview/NameAvailability.java
+++ sdk/mysql/mgmt-v2017_12_01_preview/src/main/java/com/microsoft/azure/management/mysql/v2017_12_01_preview/NameAvailability.java
@@ -10,13 +10,13 @@ package com.microsoft.azure.management.mysql.v2017_12_01_preview;
 
 import com.microsoft.azure.arm.model.HasInner;
 import com.microsoft.azure.arm.resources.models.HasManager;
-import com.microsoft.azure.management.mysql.v2017_12_01_preview.implementation.MySQLManager;
+import com.microsoft.azure.management.mysql.v2017_12_01_preview.implementation.DBForMySQLManager;
 import com.microsoft.azure.management.mysql.v2017_12_01_preview.implementation.NameAvailabilityInner;
 
 /**
  * Type representing NameAvailability.
  */
-public interface NameAvailability extends HasInner<NameAvailabilityInner>, HasManager<MySQLManager> {
+public interface NameAvailability extends HasInner<NameAvailabilityInner>, HasManager<DBForMySQLManager> {
     /**
      * @return the message value.
      */
--- sdk/mysql/mgmt-v2017_12_01_preview/src/main/java/com/microsoft/azure/management/mysql/v2017_12_01_preview/implementation/CheckNameAvailabilitysInner.java
+++ sdk/mysql/mgmt-v2017_12_01_preview/src/main/java/com/microsoft/azure/management/mysql/v2017_12_01_preview/implementation/CheckNameAvailabilitysInner.java
@@ -55,7 +55,7 @@ public class CheckNameAvailabilitysInner {
      */
     interface CheckNameAvailabilitysService {
         @Headers({ "Content-Type: application/json; charset=utf-8", "x-ms-logging-context: com.microsoft.azure.management.mysql.v2017_12_01_preview.CheckNameAvailabilitys execute" })
-        @POST("subscriptions/{subscriptionId}/providers/Microsoft.DBforMySQL/checkNameAvailability")
+        @POST("subscriptions/{subscriptionId}/providers/Microsoft.DBForMySQL/checkNameAvailability")
         Observable<Response<ResponseBody>> execute(@Path("subscriptionId") String subscriptionId, @Query("api-version") String apiVersion, @Body NameAvailabilityRequest nameAvailabilityRequest, @Header("accept-language") String acceptLanguage, @Header("User-Agent") String userAgent);
 
     }
