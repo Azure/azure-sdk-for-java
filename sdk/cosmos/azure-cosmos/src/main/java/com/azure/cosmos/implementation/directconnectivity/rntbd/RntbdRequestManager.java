4a6ca1419ec73f067176a5f6b05bc60ca2d7d13a
Fabian Meiswinkel <fabianm@microsoft.com>
2020-09-16 03:29:33 +0200

    Optimizing scheduling of the AcquireTask completion callbacks (#15225)
    * rntbd improvements
    
    * fixed latency issue and a race condition on close
    
    * fixed race condition in connection management resulting in creating more connections, fixed a infinite loop issue
    
    * fixed compilation warning
    
    * ensure the channel is servicable
    
    * increase monitoring period
    
    * removed info debug logs
    
    * cleanup
    
    * update condition
    
    * Reacting to code review comments
    
    * improved queue pending task monitoring, fixed spotbug complain
    
    * Defense in-depth against releaseChannel race condition
    
    * cancels pending acquisition tasks which are expired
    
    * cancels pending acquisition tasks which are expired
    
    * Fixing compiler warnings
    
    * Fixing SpotBug warning
    
    * Fixing build issue
    
    * Fixing SpotBug issue
    
    * Moving AcquireTask completion callbacks away from the  channel pool executor's event loop
    
    * Further iterating on the scheduling for AcuireTask completions
    
    * Cleanup
    
    * Stop ignoring maxRequest limit
    
    * Missed porting one change when resolving merge conflicts
    
    Co-authored-by: Mohammad Derakhshani <moderakh@users.noreply.github.com>

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/directconnectivity/rntbd/RntbdRequestManager.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/directconnectivity/rntbd/RntbdRequestManager.java
@@ -60,8 +60,6 @@ import java.util.UUID;
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import static com.azure.cosmos.implementation.HttpConstants.StatusCodes;
 import static com.azure.cosmos.implementation.HttpConstants.SubStatusCodes;
@@ -548,10 +546,10 @@ public final class RntbdRequestManager implements ChannelHandler, ChannelInbound
         return this.contextFuture.getNow(null) != null;
     }
 
-    boolean isServiceable(final int demand, boolean ignoreMaxRequestPerChannel) {
+    boolean isServiceable(final int demand) {
         reportIssueUnless(this.hasRequestedRntbdContext(), this, "Direct TCP context request was not issued");
         final int limit = this.hasRntbdContext() ? this.pendingRequestLimit : Math.min(this.pendingRequestLimit, demand);
-        return ignoreMaxRequestPerChannel || this.pendingRequests.size() < limit;
+        return this.pendingRequests.size() < limit;
     }
 
     void pendWrite(final ByteBuf out, final ChannelPromise promise) {
