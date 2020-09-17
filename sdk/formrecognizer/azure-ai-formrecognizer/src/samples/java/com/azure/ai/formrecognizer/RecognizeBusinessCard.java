04629ca7ced6df14fde9e1f6bdb6a0ebea38e5b9
shafang <Shawn.Fang@microsoft.com>
2020-09-17 10:09:53 -0700

    fixes field name

--- sdk/formrecognizer/azure-ai-formrecognizer/src/samples/java/com/azure/ai/formrecognizer/RecognizeBusinessCard.java
+++ sdk/formrecognizer/azure-ai-formrecognizer/src/samples/java/com/azure/ai/formrecognizer/RecognizeBusinessCard.java
@@ -186,7 +186,7 @@ public class RecognizeBusinessCard {
                 }
             }
 
-            FormField companyName = recognizedFields.get("CompanyName");
+            FormField companyName = recognizedFields.get("CompanyNames");
             if (companyName != null) {
                 if (FieldValueType.LIST == companyName.getValue().getValueType()) {
                     List<FormField> companyNameItems = companyName.getValue().asList();
