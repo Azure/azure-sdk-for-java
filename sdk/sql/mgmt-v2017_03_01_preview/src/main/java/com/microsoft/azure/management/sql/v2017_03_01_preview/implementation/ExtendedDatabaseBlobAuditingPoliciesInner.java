5d46cf13420295158bd1f54f1c0900f28ed45c6a
SDK Automation <sdkautomation@microsoft.com>
2020-09-21 09:42:32 +0000

    Generated from 509d6bed4c687511c7b734904243f83f94127b77

--- sdk/sql/mgmt-v2017_03_01_preview/src/main/java/com/microsoft/azure/management/sql/v2017_03_01_preview/implementation/ExtendedDatabaseBlobAuditingPoliciesInner.java
+++ sdk/sql/mgmt-v2017_03_01_preview/src/main/java/com/microsoft/azure/management/sql/v2017_03_01_preview/implementation/ExtendedDatabaseBlobAuditingPoliciesInner.java
@@ -10,12 +10,17 @@ package com.microsoft.azure.management.sql.v2017_03_01_preview.implementation;
 
 import retrofit2.Retrofit;
 import com.google.common.reflect.TypeToken;
+import com.microsoft.azure.AzureServiceFuture;
 import com.microsoft.azure.CloudException;
+import com.microsoft.azure.ListOperationCallback;
+import com.microsoft.azure.Page;
+import com.microsoft.azure.PagedList;
 import com.microsoft.rest.ServiceCallback;
 import com.microsoft.rest.ServiceFuture;
 import com.microsoft.rest.ServiceResponse;
 import com.microsoft.rest.Validator;
 import java.io.IOException;
+import java.util.List;
 import okhttp3.ResponseBody;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
@@ -24,6 +29,7 @@ import retrofit2.http.Headers;
 import retrofit2.http.Path;
 import retrofit2.http.PUT;
 import retrofit2.http.Query;
+import retrofit2.http.Url;
 import retrofit2.Response;
 import rx.functions.Func1;
 import rx.Observable;
@@ -61,6 +67,14 @@ public class ExtendedDatabaseBlobAuditingPoliciesInner {
         @Headers({ "Content-Type: application/json; charset=utf-8", "x-ms-logging-context: com.microsoft.azure.management.sql.v2017_03_01_preview.ExtendedDatabaseBlobAuditingPolicies createOrUpdate" })
         @PUT("subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Sql/servers/{serverName}/databases/{databaseName}/extendedAuditingSettings/{blobAuditingPolicyName}")
         Observable<Response<ResponseBody>> createOrUpdate(@Path("resourceGroupName") String resourceGroupName, @Path("serverName") String serverName, @Path("databaseName") String databaseName, @Path("blobAuditingPolicyName") String blobAuditingPolicyName, @Path("subscriptionId") String subscriptionId, @Body ExtendedDatabaseBlobAuditingPolicyInner parameters, @Query("api-version") String apiVersion, @Header("accept-language") String acceptLanguage, @Header("User-Agent") String userAgent);
+
+        @Headers({ "Content-Type: application/json; charset=utf-8", "x-ms-logging-context: com.microsoft.azure.management.sql.v2017_03_01_preview.ExtendedDatabaseBlobAuditingPolicies listByDatabase" })
+        @GET("subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Sql/servers/{serverName}/databases/{databaseName}/extendedAuditingSettings")
+        Observable<Response<ResponseBody>> listByDatabase(@Path("resourceGroupName") String resourceGroupName, @Path("serverName") String serverName, @Path("databaseName") String databaseName, @Path("subscriptionId") String subscriptionId, @Query("api-version") String apiVersion, @Header("accept-language") String acceptLanguage, @Header("User-Agent") String userAgent);
+
+        @Headers({ "Content-Type: application/json; charset=utf-8", "x-ms-logging-context: com.microsoft.azure.management.sql.v2017_03_01_preview.ExtendedDatabaseBlobAuditingPolicies listByDatabaseNext" })
+        @GET
+        Observable<Response<ResponseBody>> listByDatabaseNext(@Url String nextUrl, @Header("accept-language") String acceptLanguage, @Header("User-Agent") String userAgent);
 
     }
 
@@ -257,6 +271,248 @@ public class ExtendedDatabaseBlobAuditingPoliciesInner {
         return this.client.restClient().responseBuilderFactory().<ExtendedDatabaseBlobAuditingPolicyInner, CloudException>newInstance(this.client.serializerAdapter())
                 .register(200, new TypeToken<ExtendedDatabaseBlobAuditingPolicyInner>() { }.getType())
                 .register(201, new TypeToken<ExtendedDatabaseBlobAuditingPolicyInner>() { }.getType())
+                .registerError(CloudException.class)
+                .build(response);
+    }
+
+    /**
+     * Lists extended auditing settings of a database.
+     *
+     * @param resourceGroupName The name of the resource group that contains the resource. You can obtain this value from the Azure Resource Manager API or the portal.
+     * @param serverName The name of the server.
+     * @param databaseName The name of the database.
+     * @throws IllegalArgumentException thrown if parameters fail the validation
+     * @throws CloudException thrown if the request is rejected by server
+     * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent
+     * @return the PagedList&lt;ExtendedDatabaseBlobAuditingPolicyInner&gt; object if successful.
+     */
+    public PagedList<ExtendedDatabaseBlobAuditingPolicyInner> listByDatabase(final String resourceGroupName, final String serverName, final String databaseName) {
+        ServiceResponse<Page<ExtendedDatabaseBlobAuditingPolicyInner>> response = listByDatabaseSinglePageAsync(resourceGroupName, serverName, databaseName).toBlocking().single();
+        return new PagedList<ExtendedDatabaseBlobAuditingPolicyInner>(response.body()) {
+            @Override
+            public Page<ExtendedDatabaseBlobAuditingPolicyInner> nextPage(String nextPageLink) {
+                return listByDatabaseNextSinglePageAsync(nextPageLink).toBlocking().single().body();
+            }
+        };
+    }
+
+    /**
+     * Lists extended auditing settings of a database.
+     *
+     * @param resourceGroupName The name of the resource group that contains the resource. You can obtain this value from the Azure Resource Manager API or the portal.
+     * @param serverName The name of the server.
+     * @param databaseName The name of the database.
+     * @param serviceCallback the async ServiceCallback to handle successful and failed responses.
+     * @throws IllegalArgumentException thrown if parameters fail the validation
+     * @return the {@link ServiceFuture} object
+     */
+    public ServiceFuture<List<ExtendedDatabaseBlobAuditingPolicyInner>> listByDatabaseAsync(final String resourceGroupName, final String serverName, final String databaseName, final ListOperationCallback<ExtendedDatabaseBlobAuditingPolicyInner> serviceCallback) {
+        return AzureServiceFuture.fromPageResponse(
+            listByDatabaseSinglePageAsync(resourceGroupName, serverName, databaseName),
+            new Func1<String, Observable<ServiceResponse<Page<ExtendedDatabaseBlobAuditingPolicyInner>>>>() {
+                @Override
+                public Observable<ServiceResponse<Page<ExtendedDatabaseBlobAuditingPolicyInner>>> call(String nextPageLink) {
+                    return listByDatabaseNextSinglePageAsync(nextPageLink);
+                }
+            },
+            serviceCallback);
+    }
+
+    /**
+     * Lists extended auditing settings of a database.
+     *
+     * @param resourceGroupName The name of the resource group that contains the resource. You can obtain this value from the Azure Resource Manager API or the portal.
+     * @param serverName The name of the server.
+     * @param databaseName The name of the database.
+     * @throws IllegalArgumentException thrown if parameters fail the validation
+     * @return the observable to the PagedList&lt;ExtendedDatabaseBlobAuditingPolicyInner&gt; object
+     */
+    public Observable<Page<ExtendedDatabaseBlobAuditingPolicyInner>> listByDatabaseAsync(final String resourceGroupName, final String serverName, final String databaseName) {
+        return listByDatabaseWithServiceResponseAsync(resourceGroupName, serverName, databaseName)
+            .map(new Func1<ServiceResponse<Page<ExtendedDatabaseBlobAuditingPolicyInner>>, Page<ExtendedDatabaseBlobAuditingPolicyInner>>() {
+                @Override
+                public Page<ExtendedDatabaseBlobAuditingPolicyInner> call(ServiceResponse<Page<ExtendedDatabaseBlobAuditingPolicyInner>> response) {
+                    return response.body();
+                }
+            });
+    }
+
+    /**
+     * Lists extended auditing settings of a database.
+     *
+     * @param resourceGroupName The name of the resource group that contains the resource. You can obtain this value from the Azure Resource Manager API or the portal.
+     * @param serverName The name of the server.
+     * @param databaseName The name of the database.
+     * @throws IllegalArgumentException thrown if parameters fail the validation
+     * @return the observable to the PagedList&lt;ExtendedDatabaseBlobAuditingPolicyInner&gt; object
+     */
+    public Observable<ServiceResponse<Page<ExtendedDatabaseBlobAuditingPolicyInner>>> listByDatabaseWithServiceResponseAsync(final String resourceGroupName, final String serverName, final String databaseName) {
+        return listByDatabaseSinglePageAsync(resourceGroupName, serverName, databaseName)
+            .concatMap(new Func1<ServiceResponse<Page<ExtendedDatabaseBlobAuditingPolicyInner>>, Observable<ServiceResponse<Page<ExtendedDatabaseBlobAuditingPolicyInner>>>>() {
+                @Override
+                public Observable<ServiceResponse<Page<ExtendedDatabaseBlobAuditingPolicyInner>>> call(ServiceResponse<Page<ExtendedDatabaseBlobAuditingPolicyInner>> page) {
+                    String nextPageLink = page.body().nextPageLink();
+                    if (nextPageLink == null) {
+                        return Observable.just(page);
+                    }
+                    return Observable.just(page).concatWith(listByDatabaseNextWithServiceResponseAsync(nextPageLink));
+                }
+            });
+    }
+
+    /**
+     * Lists extended auditing settings of a database.
+     *
+    ServiceResponse<PageImpl<ExtendedDatabaseBlobAuditingPolicyInner>> * @param resourceGroupName The name of the resource group that contains the resource. You can obtain this value from the Azure Resource Manager API or the portal.
+    ServiceResponse<PageImpl<ExtendedDatabaseBlobAuditingPolicyInner>> * @param serverName The name of the server.
+    ServiceResponse<PageImpl<ExtendedDatabaseBlobAuditingPolicyInner>> * @param databaseName The name of the database.
+     * @throws IllegalArgumentException thrown if parameters fail the validation
+     * @return the PagedList&lt;ExtendedDatabaseBlobAuditingPolicyInner&gt; object wrapped in {@link ServiceResponse} if successful.
+     */
+    public Observable<ServiceResponse<Page<ExtendedDatabaseBlobAuditingPolicyInner>>> listByDatabaseSinglePageAsync(final String resourceGroupName, final String serverName, final String databaseName) {
+        if (resourceGroupName == null) {
+            throw new IllegalArgumentException("Parameter resourceGroupName is required and cannot be null.");
+        }
+        if (serverName == null) {
+            throw new IllegalArgumentException("Parameter serverName is required and cannot be null.");
+        }
+        if (databaseName == null) {
+            throw new IllegalArgumentException("Parameter databaseName is required and cannot be null.");
+        }
+        if (this.client.subscriptionId() == null) {
+            throw new IllegalArgumentException("Parameter this.client.subscriptionId() is required and cannot be null.");
+        }
+        if (this.client.apiVersion() == null) {
+            throw new IllegalArgumentException("Parameter this.client.apiVersion() is required and cannot be null.");
+        }
+        return service.listByDatabase(resourceGroupName, serverName, databaseName, this.client.subscriptionId(), this.client.apiVersion(), this.client.acceptLanguage(), this.client.userAgent())
+            .flatMap(new Func1<Response<ResponseBody>, Observable<ServiceResponse<Page<ExtendedDatabaseBlobAuditingPolicyInner>>>>() {
+                @Override
+                public Observable<ServiceResponse<Page<ExtendedDatabaseBlobAuditingPolicyInner>>> call(Response<ResponseBody> response) {
+                    try {
+                        ServiceResponse<PageImpl<ExtendedDatabaseBlobAuditingPolicyInner>> result = listByDatabaseDelegate(response);
+                        return Observable.just(new ServiceResponse<Page<ExtendedDatabaseBlobAuditingPolicyInner>>(result.body(), result.response()));
+                    } catch (Throwable t) {
+                        return Observable.error(t);
+                    }
+                }
+            });
+    }
+
+    private ServiceResponse<PageImpl<ExtendedDatabaseBlobAuditingPolicyInner>> listByDatabaseDelegate(Response<ResponseBody> response) throws CloudException, IOException, IllegalArgumentException {
+        return this.client.restClient().responseBuilderFactory().<PageImpl<ExtendedDatabaseBlobAuditingPolicyInner>, CloudException>newInstance(this.client.serializerAdapter())
+                .register(200, new TypeToken<PageImpl<ExtendedDatabaseBlobAuditingPolicyInner>>() { }.getType())
+                .registerError(CloudException.class)
+                .build(response);
+    }
+
+    /**
+     * Lists extended auditing settings of a database.
+     *
+     * @param nextPageLink The NextLink from the previous successful call to List operation.
+     * @throws IllegalArgumentException thrown if parameters fail the validation
+     * @throws CloudException thrown if the request is rejected by server
+     * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent
+     * @return the PagedList&lt;ExtendedDatabaseBlobAuditingPolicyInner&gt; object if successful.
+     */
+    public PagedList<ExtendedDatabaseBlobAuditingPolicyInner> listByDatabaseNext(final String nextPageLink) {
+        ServiceResponse<Page<ExtendedDatabaseBlobAuditingPolicyInner>> response = listByDatabaseNextSinglePageAsync(nextPageLink).toBlocking().single();
+        return new PagedList<ExtendedDatabaseBlobAuditingPolicyInner>(response.body()) {
+            @Override
+            public Page<ExtendedDatabaseBlobAuditingPolicyInner> nextPage(String nextPageLink) {
+                return listByDatabaseNextSinglePageAsync(nextPageLink).toBlocking().single().body();
+            }
+        };
+    }
+
+    /**
+     * Lists extended auditing settings of a database.
+     *
+     * @param nextPageLink The NextLink from the previous successful call to List operation.
+     * @param serviceFuture the ServiceFuture object tracking the Retrofit calls
+     * @param serviceCallback the async ServiceCallback to handle successful and failed responses.
+     * @throws IllegalArgumentException thrown if parameters fail the validation
+     * @return the {@link ServiceFuture} object
+     */
+    public ServiceFuture<List<ExtendedDatabaseBlobAuditingPolicyInner>> listByDatabaseNextAsync(final String nextPageLink, final ServiceFuture<List<ExtendedDatabaseBlobAuditingPolicyInner>> serviceFuture, final ListOperationCallback<ExtendedDatabaseBlobAuditingPolicyInner> serviceCallback) {
+        return AzureServiceFuture.fromPageResponse(
+            listByDatabaseNextSinglePageAsync(nextPageLink),
+            new Func1<String, Observable<ServiceResponse<Page<ExtendedDatabaseBlobAuditingPolicyInner>>>>() {
+                @Override
+                public Observable<ServiceResponse<Page<ExtendedDatabaseBlobAuditingPolicyInner>>> call(String nextPageLink) {
+                    return listByDatabaseNextSinglePageAsync(nextPageLink);
+                }
+            },
+            serviceCallback);
+    }
+
+    /**
+     * Lists extended auditing settings of a database.
+     *
+     * @param nextPageLink The NextLink from the previous successful call to List operation.
+     * @throws IllegalArgumentException thrown if parameters fail the validation
+     * @return the observable to the PagedList&lt;ExtendedDatabaseBlobAuditingPolicyInner&gt; object
+     */
+    public Observable<Page<ExtendedDatabaseBlobAuditingPolicyInner>> listByDatabaseNextAsync(final String nextPageLink) {
+        return listByDatabaseNextWithServiceResponseAsync(nextPageLink)
+            .map(new Func1<ServiceResponse<Page<ExtendedDatabaseBlobAuditingPolicyInner>>, Page<ExtendedDatabaseBlobAuditingPolicyInner>>() {
+                @Override
+                public Page<ExtendedDatabaseBlobAuditingPolicyInner> call(ServiceResponse<Page<ExtendedDatabaseBlobAuditingPolicyInner>> response) {
+                    return response.body();
+                }
+            });
+    }
+
+    /**
+     * Lists extended auditing settings of a database.
+     *
+     * @param nextPageLink The NextLink from the previous successful call to List operation.
+     * @throws IllegalArgumentException thrown if parameters fail the validation
+     * @return the observable to the PagedList&lt;ExtendedDatabaseBlobAuditingPolicyInner&gt; object
+     */
+    public Observable<ServiceResponse<Page<ExtendedDatabaseBlobAuditingPolicyInner>>> listByDatabaseNextWithServiceResponseAsync(final String nextPageLink) {
+        return listByDatabaseNextSinglePageAsync(nextPageLink)
+            .concatMap(new Func1<ServiceResponse<Page<ExtendedDatabaseBlobAuditingPolicyInner>>, Observable<ServiceResponse<Page<ExtendedDatabaseBlobAuditingPolicyInner>>>>() {
+                @Override
+                public Observable<ServiceResponse<Page<ExtendedDatabaseBlobAuditingPolicyInner>>> call(ServiceResponse<Page<ExtendedDatabaseBlobAuditingPolicyInner>> page) {
+                    String nextPageLink = page.body().nextPageLink();
+                    if (nextPageLink == null) {
+                        return Observable.just(page);
+                    }
+                    return Observable.just(page).concatWith(listByDatabaseNextWithServiceResponseAsync(nextPageLink));
+                }
+            });
+    }
+
+    /**
+     * Lists extended auditing settings of a database.
+     *
+    ServiceResponse<PageImpl<ExtendedDatabaseBlobAuditingPolicyInner>> * @param nextPageLink The NextLink from the previous successful call to List operation.
+     * @throws IllegalArgumentException thrown if parameters fail the validation
+     * @return the PagedList&lt;ExtendedDatabaseBlobAuditingPolicyInner&gt; object wrapped in {@link ServiceResponse} if successful.
+     */
+    public Observable<ServiceResponse<Page<ExtendedDatabaseBlobAuditingPolicyInner>>> listByDatabaseNextSinglePageAsync(final String nextPageLink) {
+        if (nextPageLink == null) {
+            throw new IllegalArgumentException("Parameter nextPageLink is required and cannot be null.");
+        }
+        String nextUrl = String.format("%s", nextPageLink);
+        return service.listByDatabaseNext(nextUrl, this.client.acceptLanguage(), this.client.userAgent())
+            .flatMap(new Func1<Response<ResponseBody>, Observable<ServiceResponse<Page<ExtendedDatabaseBlobAuditingPolicyInner>>>>() {
+                @Override
+                public Observable<ServiceResponse<Page<ExtendedDatabaseBlobAuditingPolicyInner>>> call(Response<ResponseBody> response) {
+                    try {
+                        ServiceResponse<PageImpl<ExtendedDatabaseBlobAuditingPolicyInner>> result = listByDatabaseNextDelegate(response);
+                        return Observable.just(new ServiceResponse<Page<ExtendedDatabaseBlobAuditingPolicyInner>>(result.body(), result.response()));
+                    } catch (Throwable t) {
+                        return Observable.error(t);
+                    }
+                }
+            });
+    }
+
+    private ServiceResponse<PageImpl<ExtendedDatabaseBlobAuditingPolicyInner>> listByDatabaseNextDelegate(Response<ResponseBody> response) throws CloudException, IOException, IllegalArgumentException {
+        return this.client.restClient().responseBuilderFactory().<PageImpl<ExtendedDatabaseBlobAuditingPolicyInner>, CloudException>newInstance(this.client.serializerAdapter())
+                .register(200, new TypeToken<PageImpl<ExtendedDatabaseBlobAuditingPolicyInner>>() { }.getType())
                 .registerError(CloudException.class)
                 .build(response);
     }
