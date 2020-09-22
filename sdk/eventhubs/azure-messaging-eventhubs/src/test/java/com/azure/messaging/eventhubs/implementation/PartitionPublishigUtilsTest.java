1d2b29de221702e3d514969777ed6fd317db7b17
Yijun Xie <48257664+YijunXieMS@users.noreply.github.com>
2020-09-22 12:32:57 -0700

    Update for code review

--- sdk/eventhubs/azure-messaging-eventhubs/src/test/java/com/azure/messaging/eventhubs/implementation/PartitionPublishigUtilsTest.java
+++ sdk/eventhubs/azure-messaging-eventhubs/src/test/java/com/azure/messaging/eventhubs/implementation/PartitionPublishigUtilsTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PartitionPublishigUtilsTest {
     @ParameterizedTest
