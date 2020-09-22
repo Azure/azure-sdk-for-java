2980a37d93a5d960d9162981e53936a530df2d5c
Weidong Xu <weidxu@microsoft.com>
2020-09-22 12:02:28 +0800

    batch replace

--- sdk/resourcemanager/azure-resourcemanager-monitor/src/main/java/com/azure/resourcemanager/monitor/fluent/MonitorClientBuilder.java
+++ sdk/resourcemanager/azure-resourcemanager-monitor/src/main/java/com/azure/resourcemanager/monitor/fluent/MonitorClientBuilder.java
@@ -11,7 +11,7 @@ import com.azure.core.http.policy.CookiePolicy;
 import com.azure.core.http.policy.RetryPolicy;
 import com.azure.core.http.policy.UserAgentPolicy;
 import com.azure.core.management.AzureEnvironment;
-import com.azure.core.management.serializer.AzureJacksonAdapter;
+import com.azure.core.management.serializer.SerializerFactory;
 import com.azure.core.util.serializer.SerializerAdapter;
 import java.time.Duration;
 
@@ -136,7 +136,7 @@ public final class MonitorClientBuilder {
                     .build();
         }
         if (serializerAdapter == null) {
-            this.serializerAdapter = new AzureJacksonAdapter();
+            this.serializerAdapter = SerializerFactory.createDefaultManagementSerializerAdapter();
         }
         MonitorClient client =
             new MonitorClient(pipeline, serializerAdapter, defaultPollInterval, environment, subscriptionId, endpoint);
