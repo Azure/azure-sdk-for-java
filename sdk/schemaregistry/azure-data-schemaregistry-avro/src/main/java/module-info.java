9f051cee2a77c61bf1ee58ddd3d43aba6caef540
Srikanta <51379715+srnagar@users.noreply.github.com>
2020-09-25 14:49:55 -0700

    [Schema Registry] Move module-info.java to the right directory (#15726)
    * Move module-info.java to the right directory
    
    * Fix module-info related changes to compile and run tests
    
    * Missing module-info

--- sdk/schemaregistry/azure-data-schemaregistry-avro/src/main/java/module-info.java
+++ sdk/schemaregistry/azure-data-schemaregistry-avro/src/main/java/module-info.java
@@ -3,6 +3,10 @@
 
 module com.azure.data.schemaregistry.avro {
     requires transitive com.azure.data.schemaregistry;
+    requires transitive com.azure.core.serializer.avro.apache;
 
     exports com.azure.data.schemaregistry.avro;
+
+    opens com.azure.data.schemaregistry.avro to com.fasterxml.jackson.databind, com.azure.core;
+
 }
