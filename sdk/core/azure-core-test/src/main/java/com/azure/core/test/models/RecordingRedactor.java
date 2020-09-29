4eac7fb6911b45b4f3771edce446ef75620be6f2
Sameeksha Vaity <savaity@microsoft.com>
2020-09-29 14:20:24 -0700

    redact json key patterns from test recordings

--- sdk/core/azure-core-test/src/main/java/com/azure/core/test/models/RecordingRedactor.java
+++ sdk/core/azure-core-test/src/main/java/com/azure/core/test/models/RecordingRedactor.java
@@ -20,9 +20,21 @@ public class RecordingRedactor {
     private static final String REDACTED_UTF_8 = Base64.getEncoder().encodeToString(REDACTED.getBytes(StandardCharsets.UTF_8));
 
     private static final Pattern ACCESS_TOKEN_KEY_PATTERN = Pattern.compile("(?:\"accessToken\":\")(.*?)(?:\",|\"})");
+    private static final Pattern AUTH_HEADER_KEY_PATTERN = Pattern.compile("(?:\"authHeader\":\")(.*?)(?:\",|\"})");
+    private static final Pattern ACCOUNT_NAME_KEY_PATTERN = Pattern.compile("(?:\"accountName\":\")(.*?)(?:\",|\"})");
+    private static final Pattern ACCOUNT_KEY_PATTERN = Pattern.compile("(?:\"accountKey\":\")(.*?)(?:\",|\"})");
+    private static final Pattern APPLICATION_ID_KEY_PATTERN = Pattern.compile("(?:\"applicationId\":\")(.*?)(?:\",|\"})");
+    private static final Pattern API_KEY_PATTERN = Pattern.compile("(?:\"apiKey\":\")(.*?)(?:\",|\"})");
+    private static final Pattern CONNECTION_STRING_KEY_PATTERN = Pattern.compile("(?:\"connectionString\":\")(.*?)(?:\",|\"})");
     private static final Pattern DELEGATIONKEY_KEY_PATTERN = Pattern.compile("(?:<Value>)(.*)(?:</Value>)");
     private static final Pattern DELEGATIONKEY_CLIENTID_PATTERN = Pattern.compile("(?:<SignedOid>)(.*)(?:</SignedOid>)");
     private static final Pattern DELEGATIONKEY_TENANTID_PATTERN = Pattern.compile("(?:<SignedTid>)(.*)(?:</SignedTid>)");
+    private static final Pattern HTTP_URL_PATTERN = Pattern.compile("(?:\"url\":\")(.*?)(?:\",|\"})");
+    private static final Pattern HOST_KEY_PATTERN = Pattern.compile("(?:\"host\":\")(.*?)(?:\",|\"})");
+    private static final Pattern PASSWORD_KEY_PATTERN = Pattern.compile("(?:Password=)(.*?)(?:;)");
+    private static final Pattern PWD_KEY_PATTERN = Pattern.compile("(?:\"password\":\")(.*?)(?:\",|\"})");
+    private static final Pattern USERNAME_KEY_PATTERN = Pattern.compile("(?:\"userName\":\")(.*?)(?:\",|\"})");
+    private static final Pattern USER_ID_KEY_PATTERN = Pattern.compile("(?:User ID=)(.*?)(?:;)");
 
     private static final List<Function<String, String>> RECORDING_REDACTORS = loadRedactor();
 
@@ -43,12 +55,41 @@ public class RecordingRedactor {
     private static List<Function<String, String>> loadRedactor() {
         List<Function<String, String>> redactors = new ArrayList<>();
         redactors.add(RecordingRedactor::redactAccessToken);
+        redactors.add(RecordingRedactor::redactJsonKeyPatterns);
         redactors.add(RecordingRedactor::redactUserDelegationKey);
+        redactors.add(RecordingRedactor::redactUsernameKeyPatterns);
+        redactors.add(RecordingRedactor::redactPasswordKeyPatterns);
         return redactors;
     }
 
+
     private static String redactAccessToken(String content) {
         content = redactionReplacement(content, ACCESS_TOKEN_KEY_PATTERN.matcher(content), REDACTED);
+        return content;
+    }
+
+    private static String redactUsernameKeyPatterns(String content) {
+        content = redactionReplacement(content, USERNAME_KEY_PATTERN.matcher(content), REDACTED);
+        content = redactionReplacement(content, USER_ID_KEY_PATTERN.matcher(content), REDACTED);
+        return content;
+    }
+
+    private static String redactPasswordKeyPatterns(String content) {
+        content = redactionReplacement(content, PASSWORD_KEY_PATTERN.matcher(content), REDACTED);
+        content = redactionReplacement(content, PWD_KEY_PATTERN.matcher(content), REDACTED);
+        return content;
+    }
+
+    private static String redactJsonKeyPatterns(String content) {
+        content = redactionReplacement(content, AUTH_HEADER_KEY_PATTERN.matcher(content), REDACTED);
+        content = redactionReplacement(content, ACCESS_TOKEN_KEY_PATTERN.matcher(content), REDACTED);
+        content = redactionReplacement(content, ACCOUNT_NAME_KEY_PATTERN.matcher(content), REDACTED);
+        content = redactionReplacement(content, ACCOUNT_KEY_PATTERN.matcher(content), REDACTED);
+        content = redactionReplacement(content, API_KEY_PATTERN.matcher(content), REDACTED);
+        content = redactionReplacement(content, APPLICATION_ID_KEY_PATTERN.matcher(content), REDACTED);
+        content = redactionReplacement(content, CONNECTION_STRING_KEY_PATTERN.matcher(content), REDACTED);
+        content = redactionReplacement(content, HTTP_URL_PATTERN.matcher(content), REDACTED);
+        content = redactionReplacement(content, HOST_KEY_PATTERN.matcher(content), REDACTED);
         return content;
     }
 
