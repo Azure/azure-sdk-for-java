739dfa8f65f2ae62e852871b7a42b5340b726216
SDK Automation <sdkautomation@microsoft.com>
2020-09-29 07:33:17 +0000

    Generated from 9bec05bb05059e34078c4049e251b551d9d0fb16

--- sdk/cdn/mgmt-v2020_04_15/src/main/java/com/microsoft/azure/management/cdn/v2020_04_15/RequestHeaderMatchConditionParameters.java
+++ sdk/cdn/mgmt-v2020_04_15/src/main/java/com/microsoft/azure/management/cdn/v2020_04_15/RequestHeaderMatchConditionParameters.java
@@ -24,7 +24,7 @@ public class RequestHeaderMatchConditionParameters {
     /**
      * Name of Header to be matched.
      */
-    @JsonProperty(value = "selector", required = true)
+    @JsonProperty(value = "selector")
     private String selector;
 
     /**
@@ -44,7 +44,7 @@ public class RequestHeaderMatchConditionParameters {
     /**
      * The match value for the condition of the delivery rule.
      */
-    @JsonProperty(value = "matchValues", required = true)
+    @JsonProperty(value = "matchValues")
     private List<String> matchValues;
 
     /**
@@ -55,9 +55,7 @@ public class RequestHeaderMatchConditionParameters {
 
     /**
      * Creates an instance of RequestHeaderMatchConditionParameters class.
-     * @param selector name of Header to be matched.
      * @param operator describes operator to be matched. Possible values include: 'Any', 'Equal', 'Contains', 'BeginsWith', 'EndsWith', 'LessThan', 'LessThanOrEqual', 'GreaterThan', 'GreaterThanOrEqual'.
-     * @param matchValues the match value for the condition of the delivery rule.
      */
     public RequestHeaderMatchConditionParameters() {
         odatatype = "#Microsoft.Azure.Cdn.Models.DeliveryRuleRequestHeaderConditionParameters";
