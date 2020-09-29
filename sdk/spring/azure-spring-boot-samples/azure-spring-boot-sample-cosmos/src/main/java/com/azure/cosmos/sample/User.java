63f7f00e80c28a4ef1f613aa72242f6508572adb
Moary Chen <v-moaryc@microsoft.com>
2020-09-29 09:26:39 +0800

    fix reactiveCosmosTemplate bean creating failure

--- sdk/spring/azure-spring-boot-samples/azure-spring-boot-sample-cosmos/src/main/java/com/azure/cosmos/sample/User.java
+++ sdk/spring/azure-spring-boot-samples/azure-spring-boot-sample-cosmos/src/main/java/com/azure/cosmos/sample/User.java
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.azure.cosmos;
+package com.azure.cosmos.sample;
 
 import com.azure.spring.data.cosmos.core.mapping.Container;
 import com.azure.spring.data.cosmos.core.mapping.PartitionKey;
