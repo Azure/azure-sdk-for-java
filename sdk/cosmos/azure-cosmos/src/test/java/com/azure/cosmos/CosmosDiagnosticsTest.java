97f95273a025bb853a52b564cd86e9fc0acac3af
Fabian Meiswinkel <fabian@meiswinkel.com>
2020-09-30 18:10:02 +0000

    Fixing unit test flakiness

--- sdk/cosmos/azure-cosmos/src/test/java/com/azure/cosmos/CosmosDiagnosticsTest.java
+++ sdk/cosmos/azure-cosmos/src/test/java/com/azure/cosmos/CosmosDiagnosticsTest.java
@@ -618,16 +618,24 @@ public class CosmosDiagnosticsTest extends TestSuiteBase {
         assertThat(serviceEndpointStatistics.get("isClosed").asBoolean()).isEqualTo(false);
 
         // first request initialized the rntbd service endpoint
-        assertThat(Instant.parse(serviceEndpointStatistics.get("createdTime").asText())).isAfterOrEqualTo(beforeInitializingRntbdServiceEndpoint);
+        Instant beforeInitializationThreshold = beforeInitializingRntbdServiceEndpoint.minusMillis(1);
+        assertThat(Instant.parse(serviceEndpointStatistics.get("createdTime").asText()))
+            .isAfterOrEqualTo(beforeInitializationThreshold);
 
         // Adding 1 ms to cover for rounding errors (only 3 fractional digits)
         Instant afterInitializationThreshold = afterInitializingRntbdServiceEndpoint.plusMillis(1);
-        assertThat(Instant.parse(serviceEndpointStatistics.get("createdTime").asText())).isBeforeOrEqualTo(afterInitializationThreshold);
+        assertThat(Instant.parse(serviceEndpointStatistics.get("createdTime").asText()))
+            .isBeforeOrEqualTo(afterInitializationThreshold);
 
         // Adding 1 ms to cover for rounding errors (only 3 fractional digits)
         Instant afterOperation2Threshold = afterOperation2.plusMillis(1);
-        assertThat(Instant.parse(serviceEndpointStatistics.get("lastRequestTime").asText())).isAfterOrEqualTo(beforeOperation2).isBeforeOrEqualTo(afterOperation2Threshold);
-        assertThat(Instant.parse(serviceEndpointStatistics.get("lastSuccessfulRequestTime").asText())).isAfterOrEqualTo(beforeOperation2).isBeforeOrEqualTo(afterOperation2Threshold);
+        Instant beforeOperation2Threshold = beforeOperation2.minusMillis(1);
+        assertThat(Instant.parse(serviceEndpointStatistics.get("lastRequestTime").asText()))
+            .isAfterOrEqualTo(beforeOperation2Threshold)
+            .isBeforeOrEqualTo(afterOperation2Threshold);
+        assertThat(Instant.parse(serviceEndpointStatistics.get("lastSuccessfulRequestTime").asText()))
+            .isAfterOrEqualTo(beforeOperation2Threshold)
+            .isBeforeOrEqualTo(afterOperation2Threshold);
     }
 
     private void validate(CosmosDiagnostics cosmosDiagnostics, int expectedRequestPayloadSize, int expectedResponsePayloadSize) throws Exception {
