7720e9c3a85af9ebbef866874c536482b2edadaa
Manfred Riem <mriem@manorrock.com>
2020-09-30 09:00:18 -0600

    Cleaned up sample

--- sdk/spring/azure-spring-boot-samples/azure-spring-boot-sample-keyvault-certificates/src/main/java/com/azure/keyvault/certificates/sample/SampleApplication.java
+++ sdk/spring/azure-spring-boot-samples/azure-spring-boot-sample-keyvault-certificates/src/main/java/com/azure/keyvault/certificates/sample/SampleApplication.java
@@ -1,12 +1,14 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 package com.azure.keyvault.certificates.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SampleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(SampleApplication.class, args);
     }
 }
