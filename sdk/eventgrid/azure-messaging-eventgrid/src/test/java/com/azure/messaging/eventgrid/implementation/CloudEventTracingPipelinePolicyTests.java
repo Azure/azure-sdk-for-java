66070e9ecf19c2ac89710f588c7a5b9bfdf3087a
Yijun Xie <48257664+YijunXieMS@users.noreply.github.com>
2020-09-30 17:40:12 -0700

    Test different headers

--- sdk/eventgrid/azure-messaging-eventgrid/src/test/java/com/azure/messaging/eventgrid/implementation/CloudEventTracingPipelinePolicyTests.java
+++ sdk/eventgrid/azure-messaging-eventgrid/src/test/java/com/azure/messaging/eventgrid/implementation/CloudEventTracingPipelinePolicyTests.java
@@ -16,11 +16,10 @@ public class CloudEventTracingPipelinePolicyTests {
     @BeforeEach
     public void setup() throws MalformedURLException {
         httpRequest = new HttpRequest(HttpMethod.POST, new URL("https://something.com"));
-        httpRequest.setHeader(Constants.CONTENT_TYPE, Constants.CLOUD_EVENT_CONTENT_TYPE);
     }
 
     @Test
-    void processBodyWithPlaceHolder() {
+    void processBodyWithNoHeader() {
         String testBodyString = "[{\"id\":\"313ac785-2dca-467e-a6a7-623f1baa2890\",\"source\":\"source\"," +
             "\"type\":\"json\",\"specversion\":\"1.0\",\"tracestate\":\"TS-14b6b15b-74b6-4178-847e-d142aa2727b2\"," +
             "\"traceparent\":\"TP-14b6b15b-74b6-4178-847e-d142aa2727b2\"}]";
@@ -37,14 +36,38 @@ public class CloudEventTracingPipelinePolicyTests {
     }
 
     @Test
-    void processBodyWithoutPlaceHolder() {
+    void processBodyWithTraceParentHeader() {
+        httpRequest.setHeader(Constants.TRACE_PARENT, "aTraceParent");
         String testBodyString = "[{\"id\":\"313ac785-2dca-467e-a6a7-623f1baa2890\",\"source\":\"source\"," +
-            "\"type\":\"json\",\"specversion\":\"1.0\"}]";
+            "\"type\":\"json\",\"specversion\":\"1.0\",\"tracestate\":\"TS-14b6b15b-74b6-4178-847e-d142aa2727b2\"," +
+            "\"traceparent\":\"TP-14b6b15b-74b6-4178-847e-d142aa2727b2\"}]";
+        String expectedNewBody = "[{\"id\":\"313ac785-2dca-467e-a6a7-623f1baa2890\",\"source\":\"source\"," +
+            "\"type\":\"json\",\"specversion\":\"1.0\"," +
+            "\"traceparent\":\"aTraceParent\"}]";
         httpRequest.setBody(testBodyString);
         httpRequest.setHeader(Constants.CONTENT_LENGTH, testBodyString);
         String newBody = CloudEventTracingPipelinePolicy.replaceTracingPlaceHolder(
             httpRequest, ByteBuffer.wrap(testBodyString.getBytes()));
-        Assertions.assertEquals(testBodyString, newBody);
+        Assertions.assertEquals(expectedNewBody, newBody);
+        Assertions.assertEquals(httpRequest.getHeaders().get(Constants.CONTENT_LENGTH).getValue(),
+            String.valueOf(newBody.length()));
+    }
+
+    @Test
+    void processBodyWithTraceStateHeader() {
+        httpRequest.setHeader(Constants.TRACE_STATE, "aTraceState");
+        String testBodyString = "[{\"id\":\"313ac785-2dca-467e-a6a7-623f1baa2890\",\"source\":\"source\"," +
+            "\"type\":\"json\",\"specversion\":\"1.0\",\"tracestate\":\"TS-14b6b15b-74b6-4178-847e-d142aa2727b2\"," +
+            "\"traceparent\":\"TP-14b6b15b-74b6-4178-847e-d142aa2727b2\"}]";
+        String expectedNewBody = "[{\"id\":\"313ac785-2dca-467e-a6a7-623f1baa2890\",\"source\":\"source\"," +
+            "\"type\":\"json\",\"specversion\":\"1.0\"," +
+            "\"tracestate\":\"aTraceState\"}]";
+
+        httpRequest.setBody(testBodyString);
+        httpRequest.setHeader(Constants.CONTENT_LENGTH, testBodyString);
+        String newBody = CloudEventTracingPipelinePolicy.replaceTracingPlaceHolder(
+            httpRequest, ByteBuffer.wrap(testBodyString.getBytes()));
+        Assertions.assertEquals(expectedNewBody, newBody);
         Assertions.assertEquals(httpRequest.getHeaders().get(Constants.CONTENT_LENGTH).getValue(),
             String.valueOf(newBody.length()));
     }
