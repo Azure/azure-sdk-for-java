d86628a8a86efe0aa6a60ba33df77d3535f0bc88
Sameeksha Vaity <savaity@microsoft.com>
2020-10-01 10:19:50 -0700

    opens update

--- sdk/core/azure-core-test/src/main/java/module-info.java
+++ sdk/core/azure-core-test/src/main/java/module-info.java
@@ -16,7 +16,7 @@ module com.azure.core.test {
 
     opens com.azure.core.test.implementation to com.fasterxml.jackson.databind, com.azure.core;
     opens com.azure.core.test.implementation.entities to com.fasterxml.jackson.databind, com.azure.core;
-    opens com.azure.core.test to com.fasterxml.jackson.databind, org.junit.platform.commons;
+    opens com.azure.core.test to com.fasterxml.jackson.databind;
 
     uses com.azure.core.http.HttpClientProvider;
 }
