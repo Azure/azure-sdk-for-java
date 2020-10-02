42be18843bec43fe815717479de0c0286e127b4d
Yijun Xie <48257664+YijunXieMS@users.noreply.github.com>
2020-10-01 18:16:05 -0700

    opens com.azure.messaging.eventgrid.implementation in module-info.java

--- sdk/eventgrid/azure-messaging-eventgrid/src/main/java/module-info.java
+++ sdk/eventgrid/azure-messaging-eventgrid/src/main/java/module-info.java
@@ -7,6 +7,7 @@ module com.azure.messaging.eventgrid {
     exports com.azure.messaging.eventgrid;
     exports com.azure.messaging.eventgrid.systemevents;
 
+    opens com.azure.messaging.eventgrid.implementation;
     opens com.azure.messaging.eventgrid.implementation.models to com.fasterxml.jackson.databind;
     opens com.azure.messaging.eventgrid.systemevents to com.fasterxml.jackson.databind;
 }
