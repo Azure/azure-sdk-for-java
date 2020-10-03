61cb905a930e8aaf70219aee14a1e80c03d2688c
vcolin7 <vicolina@microsoft.com>
2020-10-02 23:23:31 -0500

    Added Key Vault APIs for passing specific polling intervals for long running operations. (#15885)
    * Added APIs for LROs that accept a specific polling interval.
    
    * Modified tests.
    
    * Added samples.
    
    * Made small corrections.
    
    * Fixed checkstyle issues.
    
    * Fixed Javadoc.

--- sdk/keyvault/azure-security-keyvault-administration/src/main/java/com/azure/security/keyvault/administration/KeyVaultBackupClient.java
+++ sdk/keyvault/azure-security-keyvault-administration/src/main/java/com/azure/security/keyvault/administration/KeyVaultBackupClient.java
@@ -8,9 +8,10 @@ import com.azure.core.annotation.ServiceClient;
 import com.azure.core.annotation.ServiceMethod;
 import com.azure.core.util.polling.PollerFlux;
 import com.azure.core.util.polling.SyncPoller;
-import com.azure.security.keyvault.administration.implementation.models.KeyVaultErrorException;
 import com.azure.security.keyvault.administration.models.KeyVaultBackupOperation;
 import com.azure.security.keyvault.administration.models.KeyVaultRestoreOperation;
+
+import java.time.Duration;
 
 /**
  * The {@link KeyVaultBackupClient} provides synchronous methods to perform full backup and restore of an Azure Key
@@ -52,10 +53,24 @@ public final class KeyVaultBackupClient {
     }
 
     /**
+     * Initiates a full backup of the Key Vault.
+     *
+     * @param blobStorageUrl The URL for the Blob Storage resource where the backup will be located.
+     * @param sasToken A Shared Access Signature (SAS) token to authorize access to the blob.
+     * @param pollingInterval The interval at which the operation status will be polled for.
+     * @return A {@link SyncPoller} polling on the {@link KeyVaultBackupOperation backup operation} status.
+     * @throws NullPointerException if the {@code blobStorageUrl} or {@code sasToken} are {@code null}.
+     */
+    @ServiceMethod(returns = ReturnType.SINGLE)
+    public SyncPoller<KeyVaultBackupOperation, String> beginBackup(String blobStorageUrl, String sasToken, Duration pollingInterval) {
+        return asyncClient.beginBackup(blobStorageUrl, sasToken, pollingInterval).getSyncPoller();
+    }
+
+    /**
      * Gets a pending {@link KeyVaultBackupOperation backup operation} from the Key Vault.
      *
      * @param jobId The operation identifier.
-     * @throws KeyVaultErrorException when a backup operation for a given {@code jobId} doesn't exist.
+     * @throws NullPointerException if the {@code jobId} is null.
      * @return A {@link SyncPoller} to poll on the backup operation status.
      */
     @ServiceMethod(returns = ReturnType.SINGLE)
@@ -79,10 +94,26 @@ public final class KeyVaultBackupClient {
     }
 
     /**
+     * Initiates a full restore of the Key Vault.
+     *
+     * @param blobStorageUrl The URL for the Blob Storage resource where the backup is located.
+     * @param sasToken A Shared Access Signature (SAS) token to authorize access to the blob.
+     * @param folderName The name of the folder containing the backup data to restore.
+     * @param pollingInterval The interval at which the operation status will be polled for.
+     * @return A {@link SyncPoller} polling on the {@link KeyVaultRestoreOperation backup operation} status.
+     * @throws NullPointerException if the {@code blobStorageUrl}, {@code sasToken} or {@code folderName} are {@code
+     * null}.
+     */
+    @ServiceMethod(returns = ReturnType.SINGLE)
+    public SyncPoller<KeyVaultRestoreOperation, Void> beginRestore(String blobStorageUrl, String sasToken, String folderName, Duration pollingInterval) {
+        return asyncClient.beginRestore(blobStorageUrl, sasToken, folderName, pollingInterval).getSyncPoller();
+    }
+
+    /**
      * Gets a pending {@link KeyVaultRestoreOperation full or selective restore operation} from the Key Vault.
      *
      * @param jobId The operation identifier.
-     * @throws KeyVaultErrorException when a restore operation for a given {@code jobId} doesn't exist.
+     * @throws NullPointerException if the {@code jobId} is null.
      * @return A {@link SyncPoller} to poll on the restore operation status.
      */
     @ServiceMethod(returns = ReturnType.SINGLE)
@@ -105,5 +136,23 @@ public final class KeyVaultBackupClient {
     @ServiceMethod(returns = ReturnType.SINGLE)
     public SyncPoller<KeyVaultRestoreOperation, Void> beginSelectiveRestore(String keyName, String blobStorageUrl, String sasToken, String folderName) {
         return asyncClient.beginSelectiveRestore(keyName, blobStorageUrl, sasToken, folderName).getSyncPoller();
+    }
+
+    /**
+     * Restores all versions of a given key using the supplied SAS token pointing to a previously stored Azure Blob
+     * storage backup folder.
+     *
+     * @param keyName The name of the key to be restored.
+     * @param blobStorageUrl The URL for the Blob Storage resource where the backup is located.
+     * @param sasToken A Shared Access Signature (SAS) token to authorize access to the blob.
+     * @param folderName The name of the folder containing the backup data to restore.
+     * @param pollingInterval The interval at which the operation status will be polled for.
+     * @return A {@link PollerFlux} polling on the {@link KeyVaultRestoreOperation backup operation} status.
+     * @throws NullPointerException if the {@code keyName}, {@code blobStorageUrl}, {@code sasToken} or {@code
+     * folderName} are {@code null}.
+     */
+    @ServiceMethod(returns = ReturnType.SINGLE)
+    public SyncPoller<KeyVaultRestoreOperation, Void> beginSelectiveRestore(String keyName, String blobStorageUrl, String sasToken, String folderName, Duration pollingInterval) {
+        return asyncClient.beginSelectiveRestore(keyName, blobStorageUrl, sasToken, folderName, pollingInterval).getSyncPoller();
     }
 }
