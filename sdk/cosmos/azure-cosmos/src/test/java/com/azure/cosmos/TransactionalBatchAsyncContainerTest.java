2735e339d39f4a4796eafb7fe3c00cd20ba93c40
Rakesh Kumar <rakkuma@microsoft.com>
2020-10-07 14:42:36 +0530

    test
    Signed-off-by: Rakesh Kumar <rakkuma@microsoft.com>

--- sdk/cosmos/azure-cosmos/src/test/java/com/azure/cosmos/TransactionalBatchAsyncContainerTest.java
+++ sdk/cosmos/azure-cosmos/src/test/java/com/azure/cosmos/TransactionalBatchAsyncContainerTest.java
@@ -4,6 +4,7 @@
 package com.azure.cosmos;
 
 import com.azure.cosmos.implementation.HttpConstants;
+import com.azure.cosmos.implementation.ISessionToken;
 import com.azure.cosmos.models.CosmosItemResponse;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import org.assertj.core.api.Assertions;
@@ -117,6 +118,100 @@ public class TransactionalBatchAsyncContainerTest extends BatchTestBase {
                 assertThat(ex.getStatusCode()).isEqualTo(HttpResponseStatus.NOT_FOUND.code());
                 assertThat(ex.getSubStatusCode()).isEqualTo(HttpConstants.SubStatusCodes.READ_SESSION_NOT_AVAILABLE);
             }
+        }
+    }
+
+    @Test(groups = {"simple"}, timeOut = TIMEOUT)
+    public void batchSessionTokenPropertiesTest() throws Exception {
+        CosmosAsyncContainer container = batchAsyncContainer;
+        this.createJsonTestDocs(container);
+
+        TestDoc sampleDoc = this.populateTestDoc(this.partitionKey1);
+
+        CosmosItemResponse<TestDoc> createResponse =  container.createItem(
+            sampleDoc,
+            this.getPartitionKey(this.partitionKey1),
+            null).block();
+
+        String ownerIdCreate = createResponse.getResponseHeaders().get(HttpConstants.HttpHeaders.OWNER_ID);
+        assertThat(createResponse.getStatusCode()).isEqualTo(HttpResponseStatus.CREATED.code());
+        assertThat(ownerIdCreate).isNotEmpty();
+
+        CosmosItemResponse<TestDoc> readResponse = container.readItem(
+            this.TestDocPk1ExistingC.getId(),
+            this.getPartitionKey(this.partitionKey1),
+            TestDoc.class).block();
+
+        assertThat(readResponse.getStatusCode()).isEqualTo(HttpResponseStatus.OK.code());
+
+        ISessionToken beforeRequestSessionToken = this.getSessionToken(readResponse.getSessionToken());
+
+        String readEtagValue = readResponse.getETag();
+        TransactionalBatchItemRequestOptions readRequestOption = new TransactionalBatchItemRequestOptions()
+            .setIfMatchETag(readEtagValue);
+
+        String oldSessionToken = this.getDifferentLSNToken(readResponse.getSessionToken(), -10);
+
+        {
+            // Batch with only Read operation
+            TransactionalBatchResponse batchResponse = container.executeTransactionalBatch(
+                TransactionalBatch.createTransactionalBatch(this.getPartitionKey(this.partitionKey1))
+                    .readItem(this.TestDocPk1ExistingA.getId())
+                    .readItem(this.TestDocPk1ExistingC.getId(), readRequestOption),
+                new TransactionalBatchRequestOptions().setSessionToken(oldSessionToken)).block();
+
+            this.verifyBatchProcessed(batchResponse, 2, HttpResponseStatus.MULTI_STATUS);
+
+            assertThat(batchResponse.get(0).getStatusCode()).isEqualTo(HttpResponseStatus.OK.code());
+            assertThat(batchResponse.get(1).getStatusCode()).isEqualTo(HttpResponseStatus.NOT_MODIFIED.code());
+
+            ISessionToken afterRequestSessionToken = this.getSessionToken(batchResponse.getSessionToken());
+
+            assertThat(afterRequestSessionToken.getLSN())
+                .as("Response session token should be more than or equal to request session token")
+                .isGreaterThanOrEqualTo(beforeRequestSessionToken.getLSN());
+
+            String ownerIdBatch = batchResponse.getResponseHeaders().get(HttpConstants.HttpHeaders.OWNER_ID);
+            assertThat(ownerIdBatch).isNotEmpty();
+            assertThat(ownerIdBatch).isEqualTo(ownerIdCreate);
+        }
+
+        {
+            // Batch with write-read operations
+            TestDoc testDocToCreate = this.populateTestDoc(this.partitionKey1);
+
+            TestDoc testDocToReplace = this.getTestDocCopy(this.TestDocPk1ExistingB);
+            testDocToReplace.setCost(testDocToReplace.getCost() + 1);
+            TestDoc testDocToUpsert = this.populateTestDoc(this.partitionKey1);
+
+            TransactionalBatchResponse batchResponse = container.executeTransactionalBatch(
+                TransactionalBatch.createTransactionalBatch(this.getPartitionKey(this.partitionKey1))
+                    .createItem(testDocToCreate)
+                    .replaceItem(testDocToReplace.getId(), testDocToReplace)
+                    .upsertItem(testDocToUpsert)
+                    .deleteItem(this.TestDocPk1ExistingD.getId())
+                    .readItem(this.TestDocPk1ExistingA.getId())
+                    .readItem(this.TestDocPk1ExistingC.getId(), readRequestOption),
+                new TransactionalBatchRequestOptions().setSessionToken(oldSessionToken)).block();
+
+            this.verifyBatchProcessed(batchResponse, 6, HttpResponseStatus.MULTI_STATUS);
+
+            assertThat(batchResponse.get(0).getStatusCode()).isEqualTo(HttpResponseStatus.CREATED.code());
+            assertThat(batchResponse.get(1).getStatusCode()).isEqualTo(HttpResponseStatus.OK.code());
+            assertThat(batchResponse.get(2).getStatusCode()).isEqualTo(HttpResponseStatus.CREATED.code());
+            assertThat(batchResponse.get(3).getStatusCode()).isEqualTo(HttpResponseStatus.NO_CONTENT.code());
+            assertThat(batchResponse.get(4).getStatusCode()).isEqualTo(HttpResponseStatus.OK.code());
+            assertThat(batchResponse.get(5).getStatusCode()).isEqualTo(HttpResponseStatus.NOT_MODIFIED.code());
+
+            ISessionToken afterRequestSessionToken = this.getSessionToken(batchResponse.getSessionToken());
+
+            assertThat(afterRequestSessionToken.getLSN())
+                .as("Response session token should be more than request session token")
+                .isGreaterThan(beforeRequestSessionToken.getLSN());
+
+            String ownerIdBatch = batchResponse.getResponseHeaders().get(HttpConstants.HttpHeaders.OWNER_ID);
+            assertThat(ownerIdBatch).isNotEmpty();
+            assertThat(ownerIdBatch).isEqualTo(ownerIdCreate);
         }
     }
 }
