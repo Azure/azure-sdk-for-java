baa408fdd45e827853760c6d7ccff10c6d55f3f8
Jianghao Lu <jianghaolu@users.noreply.github.com>
2020-10-07 11:30:58 -0700

    Add test base

---
+++ sdk/api-learn/azure-learn-appconfig/src/test/java/com/azure/learn/appconfig/LearnAppConfigTestBase.java
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.learn.appconfig;
+
+import com.azure.core.test.TestBase;
+
+public class LearnAppConfigTestBase extends TestBase {
+}
