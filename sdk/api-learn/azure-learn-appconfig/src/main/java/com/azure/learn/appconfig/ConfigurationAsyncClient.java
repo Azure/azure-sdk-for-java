3cf96d1c00f0bb2f2326f40e59c01d7bc4b25589
Srikanta <srnagar@microsoft.com>
2020-10-09 03:12:15 -0700

    update with response apis

--- sdk/api-learn/azure-learn-appconfig/src/main/java/com/azure/learn/appconfig/ConfigurationAsyncClient.java
+++ sdk/api-learn/azure-learn-appconfig/src/main/java/com/azure/learn/appconfig/ConfigurationAsyncClient.java
@@ -3,6 +3,7 @@ package com.azure.learn.appconfig;
 import com.azure.core.annotation.ReturnType;
 import com.azure.core.annotation.ServiceClient;
 import com.azure.core.annotation.ServiceMethod;
+import com.azure.core.http.rest.Response;
 import com.azure.learn.appconfig.models.ConfigurationSetting;
 import reactor.core.publisher.Mono;
 
@@ -24,7 +25,8 @@ public final class ConfigurationAsyncClient {
     }
 
     @ServiceMethod(returns = ReturnType.SINGLE)
-    public Mono<ConfigurationSetting> getConfigurationSetting(ConfigurationSetting setting, boolean ifChanged) {
+    public Mono<Response<ConfigurationSetting>> getConfigurationSettingWithResponse(ConfigurationSetting setting,
+                                                                                    boolean ifChanged) {
         throw new UnsupportedOperationException("not yet implemented");
     }
 }
--- sdk/api-learn/azure-learn-appconfig/src/main/java/com/azure/learn/appconfig/ConfigurationClient.java
+++ sdk/api-learn/azure-learn-appconfig/src/main/java/com/azure/learn/appconfig/ConfigurationClient.java
@@ -3,6 +3,8 @@ package com.azure.learn.appconfig;
 import com.azure.core.annotation.ReturnType;
 import com.azure.core.annotation.ServiceClient;
 import com.azure.core.annotation.ServiceMethod;
+import com.azure.core.http.rest.Response;
+import com.azure.core.util.Context;
 import com.azure.learn.appconfig.models.ConfigurationSetting;
 
 @ServiceClient(builder = ConfigurationClientBuilder.class)
@@ -23,7 +25,8 @@ public final class ConfigurationClient {
     }
 
     @ServiceMethod(returns = ReturnType.SINGLE)
-    public ConfigurationSetting getConfigurationSetting(ConfigurationSetting setting, boolean ifChanged) {
+    public Response<ConfigurationSetting> getConfigurationSettingWithResponse(ConfigurationSetting setting,
+                                                                              boolean ifChanged, Context context) {
         throw new UnsupportedOperationException("not yet implemented");
     }
 }
