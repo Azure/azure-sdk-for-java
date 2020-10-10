dc843cd075f9dd90ad21eee79dc24491eb5723fa
Srikanta <51379715+srnagar@users.noreply.github.com>
2020-10-09 19:25:47 -0700

    Api design starter (#16131)
    * App config API design tutorial initial commit (#14844)
    
    * App Config API design tutorial initial commit
    
    * Add test resources

---
+++ sdk/api-learn/azure-learn-appconfig/src/samples/java/com/azure/learn/appconfig/HelloWorldSample.java
@@ -0,0 +1,18 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.learn.appconfig;
+
+/**
+ * A sample for demonstrating App Configuration client library usage.
+ */
+public class HelloWorldSample {
+
+    /**
+     * Main method to run the sample.
+     * @param args Input args to the program.
+     */
+    public static void main(String[] args) {
+        // TODO: write sample to create configuration client and get configs
+    }
+}
