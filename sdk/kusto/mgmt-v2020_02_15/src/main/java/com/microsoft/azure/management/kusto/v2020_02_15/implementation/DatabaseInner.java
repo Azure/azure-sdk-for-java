62a04ca9b2cc1510af3ddb6599afcc90c9efec4b
SDK Automation <sdkautomation@microsoft.com>
2020-10-11 01:32:58 +0000

    Generated from 107bc96b5f9b28633b5f99549f983421e66d8ddc

--- sdk/kusto/mgmt-v2020_02_15/src/main/java/com/microsoft/azure/management/kusto/v2020_02_15/implementation/DatabaseInner.java
+++ sdk/kusto/mgmt-v2020_02_15/src/main/java/com/microsoft/azure/management/kusto/v2020_02_15/implementation/DatabaseInner.java
@@ -13,8 +13,6 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.microsoft.azure.ProxyResource;
-import com.microsoft.azure.management.kusto.v2020_02_15.ReadOnlyFollowingDatabase;
-import com.microsoft.azure.management.kusto.v2020_02_15.ReadWriteDatabase;
 
 /**
  * Class representing a Kusto database.
