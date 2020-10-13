ddf3130b939a2e5ad054799a4f550706d097c7e5
Connie Yau <conniey@microsoft.com>
2020-10-13 14:31:49 -0700

    Add support to verify peer name when connecting to Event Hubs (#16207)
    * Adding VerifyMode in ConnectionHandler.
    
    * Making StrictTlsContext package-private.
    
    * Adding StrictTlsContext and StrictTlsContextSpi.
    
    * Add ConnectionHandler logic.
    
    * Cleaning up options and moving them to a new line,.
    
    * Adding package private overload to create MessagingFactory.
    
    * Move SendTest to implementation package so it can access package-private methods.
    
    * Add test to verify peer name.

--- sdk/eventhubs/microsoft-azure-eventhubs/src/main/java/com/microsoft/azure/eventhubs/impl/StrictTlsContext.java
+++ sdk/eventhubs/microsoft-azure-eventhubs/src/main/java/com/microsoft/azure/eventhubs/impl/StrictTlsContext.java
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.azure.core.amqp.implementation.handler;
+package com.microsoft.azure.eventhubs.impl;
 
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLContextSpi;
