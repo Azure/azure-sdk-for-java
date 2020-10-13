2af7825851806f569a7fa59b373c3721a6008db7
Xiaobing Zhu <v-xiaozhu@microsoft.com>
2020-10-13 18:05:04 +0800

    * update checkstyle-suppressions.xml

---
+++ sdk/spring/azure-spring-cloud-servicebus-queue-stream-binder/src/main/java/com/azure/servicebus/stream/binder/package-info.java
@@ -0,0 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+/**
+ * Package com.azure.servicebus.stream.binder
+ */
+package com.azure.servicebus.stream.binder;
