b32a466f288818b1ad1eb400428edb5eba5d5d3e
Srikanta <51379715+srnagar@users.noreply.github.com>
2020-10-14 22:29:24 -0700

    Update ConditionalAccessSample.java

--- sdk/api-learn/azure-learn-appconfig/src/samples/java/com/azure/learn/appconfig/ConditionalAccessSample.java
+++ sdk/api-learn/azure-learn-appconfig/src/samples/java/com/azure/learn/appconfig/ConditionalAccessSample.java
@@ -35,12 +35,12 @@ public class ConditionalAccessSample {
     private static void updateCache(ConfigurationClient configurationClient, Map<String, ConfigurationSetting> configCache) {
         ConfigurationSetting newFontColor = configurationClient.getConfigurationSettingWithResponse(configCache.get(FONT_COLOR), true, Context.NONE).getValue();
         if (newFontColor != null) {
-            configCache.putIfAbsent(FONT_COLOR, newFontColor);
+            configCache.put(FONT_COLOR, newFontColor);
         }
 
         ConfigurationSetting newGreetingText = configurationClient.getConfigurationSettingWithResponse(configCache.get(GREETING_TEXT), true, Context.NONE).getValue();
         if (newGreetingText != null) {
-            configCache.putIfAbsent(GREETING_TEXT, newGreetingText);
+            configCache.put(GREETING_TEXT, newGreetingText);
         }
     }
 
