b6f494bb66ef2ba0c3b70fc3f2e428caa3e8c963
Bhaskar Mallapragada <bhaskar.mallapragada@microsoft.com>
2020-10-15 11:14:53 -0700

    Cleanup

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/RxDocumentClientImpl.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/RxDocumentClientImpl.java
@@ -3567,8 +3567,9 @@ public class RxDocumentClientImpl implements AsyncDocumentClient, IAuthorization
         } else {
             if ((request.getOperationType() == OperationType.Query || request.getOperationType() == OperationType.SqlQuery) &&
                     Utils.isCollectionChild(request.getResourceType())) {
-                // Go to gateway only if there is no partition key range or partition key set. This should be very rare
-                if (request.getPartitionKeyRangeIdentity() == null && request.getHeaders().get(HttpConstants.HttpHeaders.PARTITION_KEY) == null) {
+                // Go to gateway only when partition key range and partition key are not set. This should be very rare
+                if (request.getPartitionKeyRangeIdentity() == null &&
+                        request.getHeaders().get(HttpConstants.HttpHeaders.PARTITION_KEY) == null) {
                     return this.gatewayProxy;
                 }
             }
