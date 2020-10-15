37f42c3e18a37697d9d4b00b8bf260eaec6bc1ab
v-gaoh <v-gaoh@Microsoft.com>
2020-10-15 12:47:42 +0800

    add failureUrl

--- sdk/spring/azure-spring-boot-samples/azure-spring-boot-sample-active-directory-backend/src/main/java/com/azure/aad/security/AADOAuth2LoginSecurityConfig.java
+++ sdk/spring/azure-spring-boot-samples/azure-spring-boot-sample-active-directory-backend/src/main/java/com/azure/aad/security/AADOAuth2LoginSecurityConfig.java
@@ -4,9 +4,11 @@
 package com.azure.aad.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
@@ -20,6 +22,9 @@ public class AADOAuth2LoginSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService;
 
+    @Value("${spring.security.oauth2.authorization.failureUrl}")
+    private String errorPageUrl;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -27,6 +32,15 @@ public class AADOAuth2LoginSecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
             .oauth2Login()
             .userInfoEndpoint()
-            .oidcUserService(oidcUserService);
+            .oidcUserService(oidcUserService)
+            .and()
+            .failureUrl(errorPageUrl);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+            .ignoring()
+            .antMatchers(errorPageUrl);
     }
 }
