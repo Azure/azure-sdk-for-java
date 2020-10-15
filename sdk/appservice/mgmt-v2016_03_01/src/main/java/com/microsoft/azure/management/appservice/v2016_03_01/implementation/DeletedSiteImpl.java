10159d63546f34a0d7feb65ea604c08fb1f7213b
SDK Automation <sdkautomation@microsoft.com>
2020-10-15 02:22:22 +0000

    Generated from f2bd37682d6b6068049dd003fc37c4a8512c8116

--- sdk/appservice/mgmt-v2016_03_01/src/main/java/com/microsoft/azure/management/appservice/v2016_03_01/implementation/DeletedSiteImpl.java
+++ sdk/appservice/mgmt-v2016_03_01/src/main/java/com/microsoft/azure/management/appservice/v2016_03_01/implementation/DeletedSiteImpl.java
@@ -12,14 +12,14 @@ import com.microsoft.azure.management.appservice.v2016_03_01.DeletedSite;
 import com.microsoft.azure.arm.model.implementation.WrapperImpl;
 
 class DeletedSiteImpl extends WrapperImpl<DeletedSiteInner> implements DeletedSite {
-    private final AppServiceManager manager;
-    DeletedSiteImpl(DeletedSiteInner inner, AppServiceManager manager) {
+    private final WebManager manager;
+    DeletedSiteImpl(DeletedSiteInner inner, WebManager manager) {
         super(inner);
         this.manager = manager;
     }
 
     @Override
-    public AppServiceManager manager() {
+    public WebManager manager() {
         return this.manager;
     }
 
