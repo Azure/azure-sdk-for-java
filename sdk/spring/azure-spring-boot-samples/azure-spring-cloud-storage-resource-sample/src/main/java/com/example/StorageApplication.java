525c5b466ab18ade3ab893e218c17461b7fb7840
Yev Bronshteyn <yebronsh@microsoft.com>
2020-10-14 21:12:51 -0400

    Resource sample works with environment parameters

--- sdk/spring/azure-spring-boot-samples/azure-spring-cloud-storage-resource-sample/src/main/java/com/example/StorageApplication.java
+++ sdk/spring/azure-spring-boot-samples/azure-spring-cloud-storage-resource-sample/src/main/java/com/example/StorageApplication.java
@@ -15,10 +15,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StorageApplication {
 
     public static void main(String[] args) {
-        System.out.println("Running in " + FileSystems.getDefault().getPath("").toAbsolutePath().toString());
-        System.getenv().forEach((key, val) -> {
-            System.err.println(key + ":" + val);
-        });
         SpringApplication.run(StorageApplication.class, args);
     }
 }
