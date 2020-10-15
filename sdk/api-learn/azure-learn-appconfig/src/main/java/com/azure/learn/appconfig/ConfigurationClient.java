87567f50b156e183f29a2f6ae4d0c076406d763d
Andrei Giurgiu <agiurg@microsoft.com>
2020-10-15 14:47:54 -0700

    Implemented builder and clients

--- sdk/api-learn/azure-learn-appconfig/src/main/java/com/azure/learn/appconfig/ConfigurationClient.java
+++ sdk/api-learn/azure-learn-appconfig/src/main/java/com/azure/learn/appconfig/ConfigurationClient.java
@@ -1,5 +1,6 @@
 package com.azure.learn.appconfig;
 
+import com.azure.core.util.logging.ClientLogger;
 import com.azure.core.annotation.ReturnType;
 import com.azure.core.annotation.ServiceClient;
 import com.azure.core.annotation.ServiceMethod;
@@ -7,26 +8,32 @@ import com.azure.core.http.rest.Response;
 import com.azure.core.util.Context;
 import com.azure.learn.appconfig.models.ConfigurationSetting;
 
+import static com.azure.core.util.FluxUtil.monoError;
+import static com.azure.core.util.FluxUtil.withContext;
+
 @ServiceClient(builder = ConfigurationClientBuilder.class)
 public final class ConfigurationClient {
 
-    ConfigurationClient() {
-        // package-private constructor
+    private final ClientLogger logger = new ClientLogger(ConfigurationAsyncClient.class);
+    private final ConfigurationAsyncClient asyncClient;
+
+    ConfigurationClient(ConfigurationAsyncClient asyncClient) {
+        this.asyncClient = asyncClient;
     }
 
     @ServiceMethod(returns = ReturnType.SINGLE)
     public ConfigurationSetting getConfigurationSetting(String key) {
-        throw new UnsupportedOperationException("not yet implemented");
+        return asyncClient.getConfigurationSetting(key).block();
     }
 
     @ServiceMethod(returns = ReturnType.SINGLE)
     public ConfigurationSetting getConfigurationSetting(String key, String label) {
-        throw new UnsupportedOperationException("not yet implemented");
+        return asyncClient.getConfigurationSetting(key, label).block();
     }
 
     @ServiceMethod(returns = ReturnType.SINGLE)
     public Response<ConfigurationSetting> getConfigurationSettingWithResponse(ConfigurationSetting setting,
                                                                               boolean ifChanged, Context context) {
-        throw new UnsupportedOperationException("not yet implemented");
+        return asyncClient.getConfigurationSettingWithResponse(setting, ifChanged, context).block();
     }
 }
--- sdk/api-learn/azure-learn-appconfig/src/main/java/com/azure/learn/appconfig/ConfigurationClientBuilder.java
+++ sdk/api-learn/azure-learn-appconfig/src/main/java/com/azure/learn/appconfig/ConfigurationClientBuilder.java
@@ -3,6 +3,13 @@
 
 package com.azure.learn.appconfig;
 
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Objects;
+import java.util.List;
+import java.net.MalformedURLException;
+import java.net.URL;
+import com.azure.core.util.logging.ClientLogger;
 import com.azure.core.annotation.ServiceClientBuilder;
 import com.azure.core.credential.TokenCredential;
 import com.azure.core.http.HttpClient;
@@ -13,66 +20,77 @@ import com.azure.core.http.policy.RetryPolicy;
 import com.azure.core.util.Configuration;
 import com.azure.core.util.ServiceVersion;
 
+import com.azure.learn.appconfig.implementation.AzureAppConfigurationImpl;
+import com.azure.learn.appconfig.implementation.AzureAppConfigurationImplBuilder;
+
 @ServiceClientBuilder(serviceClients = {ConfigurationClient.class, ConfigurationAsyncClient.class})
 public final class ConfigurationClientBuilder {
 
+    private final AzureAppConfigurationImplBuilder internalBuilder;
+    private final ClientLogger logger = new ClientLogger(ConfigurationClientBuilder.class);
+
+    public ConfigurationClientBuilder() {
+        this.internalBuilder = new AzureAppConfigurationImplBuilder();
+    }
+
     public ConfigurationClient buildClient() {
-        // implementation
-        return new ConfigurationClient();
+        ConfigurationAsyncClient asyncClient = buildAsyncClient();
+        return new ConfigurationClient(asyncClient);
     }
 
     public ConfigurationAsyncClient buildAsyncClient() {
-        // implementation
-        return new ConfigurationAsyncClient();
+        AzureAppConfigurationImpl internalClient = internalBuilder.buildClient();
+        return new ConfigurationAsyncClient(internalClient);
     }
 
     public ConfigurationClientBuilder credential(TokenCredential credential) {
-        // implementation
+        internalBuilder.credential(credential);
         return this;
     }
 
     public ConfigurationClientBuilder httpClient(HttpClient httpClient) {
-        // implementation
+        internalBuilder.httpClient(httpClient);
         return this;
     }
 
     public ConfigurationClientBuilder endpoint(String endpoint) {
-        // implementation
+        Objects.requireNonNull(endpoint, "'endpoint' cannot be null");
+        try {
+            URL url = new URL(endpoint);
+        } catch (MalformedURLException exception) {
+            logger.logExceptionAsError(new IllegalArgumentException(exception));
+        }
+        internalBuilder.endpoint(endpoint);
         return this;
     }
 
     public ConfigurationClientBuilder httpLogOptions(HttpLogOptions logOptions) {
-        // implementation
+        internalBuilder.httpLogOptions(logOptions);
         return this;
     }
 
     public ConfigurationClientBuilder addPolicy(HttpPipelinePolicy policy) {
-        // implementation
+        internalBuilder.addPolicy(policy);
         return this;
     }
 
     public ConfigurationClientBuilder pipeline(HttpPipeline pipeline) {
-        // implementation
+        internalBuilder.pipeline(pipeline);
         return this;
     }
 
     public ConfigurationClientBuilder configuration(Configuration configuration) {
-        // implementation
+        internalBuilder.configuration(configuration);
         return this;
     }
 
     public ConfigurationClientBuilder retryPolicy(RetryPolicy retryPolicy) {
-        // implementation
+        internalBuilder.retryPolicy(retryPolicy);
         return this;
     }
 
     public ConfigurationClientBuilder serviceVersion(ServiceVersion version) {
-        // implementation
-        return this;
-    }
-
-    public ConfigurationClientBuilder connectionString(String connectionString) {
-        // implementation
+        // no-op, we just have a single version and generated client is tied to a specific version
         return this;
     }
 }
