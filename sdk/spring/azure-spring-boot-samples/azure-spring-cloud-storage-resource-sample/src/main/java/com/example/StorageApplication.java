7d7adb15320f0c0483682a90f9bd9746bdae06cc
Yev Bronshteyn <yebronsh@microsoft.com>
2020-10-14 23:04:58 -0400

    Removing debugging code

--- sdk/spring/azure-spring-boot-samples/azure-spring-cloud-storage-resource-sample/src/main/java/com/example/StorageApplication.java
+++ sdk/spring/azure-spring-boot-samples/azure-spring-cloud-storage-resource-sample/src/main/java/com/example/StorageApplication.java
@@ -3,8 +3,6 @@
 
 package com.example;
 
-import java.nio.file.FileSystems;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
