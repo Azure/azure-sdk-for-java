6867d33121ae8521811d5e081af1ce892e10f98b
Rujun Chen <rujche@microsoft.com>
2020-10-19 15:42:19 +0800

    No logic change.

--- sdk/spring/azure-spring-boot-samples/azure-spring-boot-sample-keyvault-certificates/src/main/java/com/azure/spring/security/keyvault/certificates/sample/SampleApplication.java
+++ sdk/spring/azure-spring-boot-samples/azure-spring-boot-sample-keyvault-certificates/src/main/java/com/azure/spring/security/keyvault/certificates/sample/SampleApplication.java
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+
 package com.azure.spring.security.keyvault.certificates.sample;
 
 import org.springframework.boot.SpringApplication;
