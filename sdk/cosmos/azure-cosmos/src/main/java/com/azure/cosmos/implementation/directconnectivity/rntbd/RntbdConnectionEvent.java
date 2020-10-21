f0445e4ada1629e52037420e6dc52c109581397b
Annie Liang <64233642+xinlian12@users.noreply.github.com>
2020-10-21 22:51:35 +0000

    ConnectionStateListener change (#15991)
    * ConnectionStateListener change
    Co-authored-by: Annie Liang <xinlian@microsoft.com>

---
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/directconnectivity/rntbd/RntbdConnectionEvent.java
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.cosmos.implementation.directconnectivity.rntbd;
+
+public enum RntbdConnectionEvent {
+    READ_EOF,
+    READ_FAILURE;
+}
