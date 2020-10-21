c8caea59a29beba2e7097fdc0573f9dd4ca6d8df
g2vinay <vinay_work@hotmail.com>
2020-10-20 22:03:44 -0700

    rollback Client cert credential InputSream API

--- sdk/identity/azure-identity/src/main/java/com/azure/identity/ClientCertificateCredentialBuilder.java
+++ sdk/identity/azure-identity/src/main/java/com/azure/identity/ClientCertificateCredentialBuilder.java
@@ -38,7 +38,7 @@ public class ClientCertificateCredentialBuilder extends AadCredentialBuilderBase
      * @param certificate the input stream containing the PEM certificate
      * @return An updated instance of this builder.
      */
-    public ClientCertificateCredentialBuilder pemCertificate(InputStream certificate) {
+    ClientCertificateCredentialBuilder pemCertificate(InputStream certificate) {
         this.clientCertificate = certificate;
         return this;
     }
@@ -65,7 +65,7 @@ public class ClientCertificateCredentialBuilder extends AadCredentialBuilderBase
      * @param clientCertificatePassword the password protecting the PFX file
      * @return An updated instance of this builder.
      */
-    public ClientCertificateCredentialBuilder pfxCertificate(InputStream certificate,
+    ClientCertificateCredentialBuilder pfxCertificate(InputStream certificate,
                                                              String clientCertificatePassword) {
         this.clientCertificate = certificate;
         this.clientCertificatePassword = clientCertificatePassword;
