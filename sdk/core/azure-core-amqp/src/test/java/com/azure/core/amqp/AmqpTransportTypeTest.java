0e537f66f92951dfaac2aca303c3ded1aa13a5fa
krunal satwara <krunalsatwara@qlogic.io>
2020-10-26 17:04:29 +0530

    fix: fix AmqpTranportTypeTest changes

--- sdk/core/azure-core-amqp/src/test/java/com/azure/core/amqp/AmqpTransportTypeTest.java
+++ sdk/core/azure-core-amqp/src/test/java/com/azure/core/amqp/AmqpTransportTypeTest.java
@@ -6,7 +6,6 @@ package com.azure.core.amqp;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class AmqpTransportTypeTest {
