c584e8dbd36b7db06011f044ca2d508d1b4024b1
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2020-10-26 16:24:50 -0700

    Change JsonPatchDocument and JsonPatchOperation serialization to use a module hooked into JacksonAdapter

--- sdk/core/azure-core/src/main/java/com/azure/core/util/serializer/JsonPatchOperationSerializer.java
+++ sdk/core/azure-core/src/main/java/com/azure/core/util/serializer/JsonPatchOperationSerializer.java
@@ -1,11 +1,14 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.azure.core.util;
+package com.azure.core.util.serializer;
 
+import com.azure.core.util.JsonPatchOperation;
+import com.azure.core.util.Option;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 
 import java.io.IOException;
 
@@ -13,6 +16,21 @@ import java.io.IOException;
  * Handles serialization of a {@link JsonPatchOperation}.
  */
 final class JsonPatchOperationSerializer extends JsonSerializer<JsonPatchOperation> {
+    private static final SimpleModule MODULE;
+
+    static {
+        MODULE = new SimpleModule().addSerializer(JsonPatchOperation.class, new JsonPatchOperationSerializer());
+    }
+
+    /**
+     * Gets a module wrapping this serializer as an adapter for the Jackson ObjectMapper.
+     *
+     * @return a simple module to be plugged onto Jackson ObjectMapper.
+     */
+    public static SimpleModule getModule() {
+        return MODULE;
+    }
+
     @Override
     public void serialize(JsonPatchOperation value, JsonGenerator gen, SerializerProvider serializers)
         throws IOException {
