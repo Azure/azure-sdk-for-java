d01190a50674acfb431452c33ad261596a6e538b
Hemant Tanwar <hetanwar@users.noreply.github.com>
2020-10-26 15:56:19 -0700

    Adding error handling for Error source

--- sdk/servicebus/azure-messaging-servicebus/src/main/java/module-info.java
+++ sdk/servicebus/azure-messaging-servicebus/src/main/java/module-info.java
@@ -8,6 +8,6 @@ module com.azure.messaging.servicebus {
     exports com.azure.messaging.servicebus.administration;
     exports com.azure.messaging.servicebus.administration.models;
     exports com.azure.messaging.servicebus.models;
-    
+
     uses com.azure.core.util.tracing.Tracer;
 }
