42e15287fdd54b03959a6d3eb24b3e4db71287d2
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2020-10-27 16:36:16 -0700

    Initial structure for AMQP Experimental package

---
+++ sdk/core/azure-core-amqp-experimental/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+module com.azure.core.amqp.experimental {
+    requires transitive com.azure.core.amqp;
+}
