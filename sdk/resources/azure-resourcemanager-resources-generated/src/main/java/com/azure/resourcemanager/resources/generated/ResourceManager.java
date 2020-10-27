d4ae7eac668a47fa99b2268eb51d311116c7585f
Weidong Xu <weidxu@microsoft.com>
2020-10-27 16:42:37 +0800

    regen

--- sdk/resources/azure-resourcemanager-resources-generated/src/main/java/com/azure/resourcemanager/resources/generated/ResourceManager.java
+++ sdk/resources/azure-resourcemanager-resources-generated/src/main/java/com/azure/resourcemanager/resources/generated/ResourceManager.java
@@ -152,7 +152,7 @@ public final class ResourceManager {
         }
 
         /**
-         * Sets the default poll interval, used when service does not provide .
+         * Sets the default poll interval, used when service does not provide "Retry-After" header.
          *
          * @param defaultPollInterval the default poll interval.
          * @return the configurable object itself.
