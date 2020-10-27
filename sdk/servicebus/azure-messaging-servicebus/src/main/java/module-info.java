53427b361331fb9360340f2cfb07fa975db4cd12
Srikanta <srnagar@microsoft.com>
2020-10-27 01:45:12 -0700

    Open SB models package to azure core

--- sdk/servicebus/azure-messaging-servicebus/src/main/java/module-info.java
+++ sdk/servicebus/azure-messaging-servicebus/src/main/java/module-info.java
@@ -9,8 +9,8 @@ module com.azure.messaging.servicebus {
     exports com.azure.messaging.servicebus.administration.models;
     exports com.azure.messaging.servicebus.models;
 
-    opens com.azure.messaging.servicebus.implementation.models to com.fasterxml.jackson.databind;
-    opens com.azure.messaging.servicebus.administration.models to com.fasterxml.jackson.databind;
+    opens com.azure.messaging.servicebus.implementation.models to com.fasterxml.jackson.databind, com.azure.core;
+    opens com.azure.messaging.servicebus.administration.models to com.fasterxml.jackson.databind, com.azure.core;
 
     uses com.azure.core.util.tracing.Tracer;
 }
