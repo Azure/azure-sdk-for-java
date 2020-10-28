3e96d9edc36905e8051fe8f15cbe1f0d887f7c75
Alan Zimmer <48699787+alzimmermsft@users.noreply.github.com>
2020-10-27 19:03:22 -0700

    Move JsonPatchDocument into com.azure.core.models (#16904)
    * Moved JsonPatchDocument into com.azure.core.models
    
    * Add package to Javadoc configuration, fixed some incorrect Javadoc grouping configuration

--- sdk/core/azure-core/src/samples/java/com/azure/core/models/JsonPatchDocumentJavaDocCodeSnippet.java
+++ sdk/core/azure-core/src/samples/java/com/azure/core/models/JsonPatchDocumentJavaDocCodeSnippet.java
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.azure.core.util;
+package com.azure.core.models;
 
 import java.util.Collections;
 
