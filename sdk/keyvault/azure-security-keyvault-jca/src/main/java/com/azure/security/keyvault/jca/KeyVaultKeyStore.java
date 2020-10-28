8dacd02317efbd59b250f247b7cfbb2a03442829
Manfred Riem <manfred.riem@microsoft.com>
2020-10-28 11:10:08 -0600

    Remove unnecessary null check. False positive on Java 11, works fine on Java 8

--- sdk/keyvault/azure-security-keyvault-jca/src/main/java/com/azure/security/keyvault/jca/KeyVaultKeyStore.java
+++ sdk/keyvault/azure-security-keyvault-jca/src/main/java/com/azure/security/keyvault/jca/KeyVaultKeyStore.java
@@ -259,13 +259,13 @@ public final class KeyVaultKeyStore extends KeyStoreSpi {
         List<String> filenames = new ArrayList<>();
         if (path != null) {
             try (InputStream in = getClass().getResourceAsStream(path)) {
-                try (BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"))) {
-                    String resource;
-                    while ((resource = br.readLine()) != null) {
-                        filenames.add(resource);
+                    try (BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"))) {
+                        String resource;
+                        while ((resource = br.readLine()) != null) {
+                            filenames.add(resource);
+                        }
                     }
                 }
-            }
         }
         return filenames.toArray(new String[0]);
     }
