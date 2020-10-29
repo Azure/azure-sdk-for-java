3ac492876be89aa998e26770efd4f819ab9495a9
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2020-10-28 19:25:46 -0700

    Remove JsonPatchDocument.getOperations(), JsonPatchOperation, JsonPatchOperationKind, and Option from azure-core public API

--- sdk/core/azure-core/src/test/java/com/azure/core/implementation/OptionSerializerTests.java
+++ sdk/core/azure-core/src/test/java/com/azure/core/implementation/OptionSerializerTests.java
@@ -1,9 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.azure.core.util.serializer;
+package com.azure.core.implementation;
 
-import com.azure.core.util.Option;
+import com.azure.core.util.serializer.JacksonAdapter;
+import com.azure.core.util.serializer.SerializerEncoding;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -14,23 +15,32 @@ import java.io.IOException;
  * Tests for {@link Option} that can represent tri-sate (non-null-value, null-value, or no-value).
  */
 public class OptionSerializerTests {
+    private static final JacksonAdapter ADAPTER;
+
+    static {
+        JacksonAdapter adapter = new JacksonAdapter();
+        adapter.serializer().registerModule(new OptionModule());
+
+        ADAPTER = adapter;
+    }
+
     @Test
     public void canSerializeExplicitNull() throws IOException {
         PatchModel model = new PatchModel();
 
         model.setSku(Option.of(null));
-        String serialized = new JacksonAdapter().serialize(model, SerializerEncoding.JSON);
+        String serialized = ADAPTER.serialize(model, SerializerEncoding.JSON);
         Assertions.assertEquals("{\"sku\":null}", serialized);
 
         model.setSku(Option.empty());
-        serialized = new JacksonAdapter().serialize(model, SerializerEncoding.JSON);
+        serialized = ADAPTER.serialize(model, SerializerEncoding.JSON);
         Assertions.assertEquals("{\"sku\":null}", serialized);
     }
 
     @Test
     public void shouldIgnoreImplicitNull() throws IOException {
         PatchModel model = new PatchModel();
-        String serialized = new JacksonAdapter().serialize(model, SerializerEncoding.JSON);
+        String serialized = ADAPTER.serialize(model, SerializerEncoding.JSON);
         Assertions.assertEquals("{}", serialized);
     }
 
@@ -38,7 +48,7 @@ public class OptionSerializerTests {
     public void shouldIgnoreUninitialized() throws IOException {
         PatchModel model = new PatchModel();
         model.setSku(Option.uninitialized());
-        String serialized = new JacksonAdapter().serialize(model, SerializerEncoding.JSON);
+        String serialized = ADAPTER.serialize(model, SerializerEncoding.JSON);
         Assertions.assertEquals("{}", serialized);
     }
 
@@ -46,7 +56,7 @@ public class OptionSerializerTests {
     public void canSerializeNonNullValue() throws IOException {
         PatchModel model = new PatchModel();
         model.setSku(Option.of("basic"));
-        String serialized = new JacksonAdapter().serialize(model, SerializerEncoding.JSON);
+        String serialized = ADAPTER.serialize(model, SerializerEncoding.JSON);
         Assertions.assertEquals("{\"sku\":\"basic\"}", serialized);
     }
 
@@ -54,12 +64,12 @@ public class OptionSerializerTests {
     public void canSerializeRawType() throws IOException {
         @SuppressWarnings("rawtypes")
         final Option rawOption = Option.of(new RawModel().setName("test"));
-        String serialized = new JacksonAdapter().serialize(rawOption, SerializerEncoding.JSON);
+        String serialized = ADAPTER.serialize(rawOption, SerializerEncoding.JSON);
         Assertions.assertEquals("{\"name\":\"test\"}", serialized);
 
         @SuppressWarnings("rawtypes")
         final Option rawOption1 = Option.of("test");
-        String serialized1 = new JacksonAdapter().serialize(rawOption1, SerializerEncoding.JSON);
+        String serialized1 = ADAPTER.serialize(rawOption1, SerializerEncoding.JSON);
         Assertions.assertEquals("\"test\"", serialized1);
     }
 
