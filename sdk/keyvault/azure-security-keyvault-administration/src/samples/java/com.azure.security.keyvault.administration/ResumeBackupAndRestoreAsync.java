092df7b332c83db76b82725d9c0da778d61191f1
Victor Colin Amador <vicolina@microsoft.com>
2020-11-02 12:38:00 -0800

    Added missing license header to ResumeBackupAndRestoreAsync.

--- sdk/keyvault/azure-security-keyvault-administration/src/samples/java/com.azure.security.keyvault.administration/ResumeBackupAndRestoreAsync.java
+++ sdk/keyvault/azure-security-keyvault-administration/src/samples/java/com.azure.security.keyvault.administration/ResumeBackupAndRestoreAsync.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.azure.security.keyvault.administration;
 
 import com.azure.core.util.polling.AsyncPollResponse;
