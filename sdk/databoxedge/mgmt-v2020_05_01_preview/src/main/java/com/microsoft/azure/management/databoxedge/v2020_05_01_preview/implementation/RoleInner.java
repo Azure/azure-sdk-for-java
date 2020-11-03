386c01f9ce64c58c366e0ceb555bac077933fac1
SDK Automation <sdkautomation@microsoft.com>
2020-11-03 05:22:02 +0000

    Generated from 245ca85b8f2632cc262497491cc4783c9f1ca17d

--- sdk/databoxedge/mgmt-v2020_05_01_preview/src/main/java/com/microsoft/azure/management/databoxedge/v2020_05_01_preview/implementation/RoleInner.java
+++ sdk/databoxedge/mgmt-v2020_05_01_preview/src/main/java/com/microsoft/azure/management/databoxedge/v2020_05_01_preview/implementation/RoleInner.java
@@ -12,7 +12,6 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.microsoft.azure.management.databoxedge.v2020_05_01_preview.ARMBaseModel;
-import com.microsoft.azure.management.databoxedge.v2020_05_01_preview.IoTRole;
 
 /**
  * Compute role.
