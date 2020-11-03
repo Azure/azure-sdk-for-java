5ebda56c75f49195d1e6550bbbd275ee0f0edd99
SDK Automation <sdkautomation@microsoft.com>
2020-11-03 02:36:54 +0000

    Generated from d7216ae183c55f5117ed2417449f62635c834253

--- sdk/sql/mgmt-v2017_03_01_preview/src/main/java/com/microsoft/azure/management/sql/v2017_03_01_preview/JobStepExecutions.java
+++ sdk/sql/mgmt-v2017_03_01_preview/src/main/java/com/microsoft/azure/management/sql/v2017_03_01_preview/JobStepExecutions.java
@@ -11,8 +11,6 @@ package com.microsoft.azure.management.sql.v2017_03_01_preview;
 import rx.Observable;
 import com.microsoft.azure.management.sql.v2017_03_01_preview.ExecutionJobJobAgentServerJobExecution;
 
-import java.util.UUID;
-
 /**
  * Type representing JobStepExecutions.
  */
