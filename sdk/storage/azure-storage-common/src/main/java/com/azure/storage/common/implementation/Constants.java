24d920628301332cda78dc336b76106aea2d5d22
Rick Ley <frley@microsoft.com>
2020-11-02 17:08:04 -0800

    Updated recordings with new service version (#17077)
    * Updated recordings with new service version
    
    * Updated latest service version
    
    * Updated versions for other services
    
    * A few more constants
    
    * Updated blob sas version

--- sdk/storage/azure-storage-common/src/main/java/com/azure/storage/common/implementation/Constants.java
+++ sdk/storage/azure-storage-common/src/main/java/com/azure/storage/common/implementation/Constants.java
@@ -196,7 +196,7 @@ public final class Constants {
         /**
          * The current storage version header value.
          */
-        public static final String TARGET_STORAGE_VERSION = "2020-02-10";
+        public static final String TARGET_STORAGE_VERSION = "2020-04-08";
 
         /**
          * Error code returned from the service.
--- sdk/storage/azure-storage-file-datalake/src/main/java/com/azure/storage/file/datalake/DataLakeServiceVersion.java
+++ sdk/storage/azure-storage-file-datalake/src/main/java/com/azure/storage/file/datalake/DataLakeServiceVersion.java
@@ -12,7 +12,8 @@ public enum DataLakeServiceVersion implements ServiceVersion {
     V2019_02_02("2019-02-02"),
     V2019_07_07("2019-07-07"),
     V2019_12_12("2019-12-12"),
-    V2020_02_10("2020-02-10");
+    V2020_02_10("2020-02-10"),
+    V2020_04_08("2020-04-08");
 
     private final String version;
 
@@ -34,6 +35,6 @@ public enum DataLakeServiceVersion implements ServiceVersion {
      * @return the latest {@link DataLakeServiceVersion}
      */
     public static DataLakeServiceVersion getLatest() {
-        return V2020_02_10;
+        return V2020_04_08;
     }
 }
