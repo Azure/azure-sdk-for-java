a19c91e135488f5b4e7b8318acb38fbc27ec4e89
Richard Park <richard.park@microsoft.com>
2020-11-10 13:07:29 -0800

    * Fixing problem where I was throwing the exception directly rather than returning it through the Flux. * Adding in test to make sure we throw a reasonable error when the user tries to complete a peeked message.

--- sdk/servicebus/azure-messaging-servicebus/src/main/java/com/azure/messaging/servicebus/ServiceBusReceiverAsyncClient.java
+++ sdk/servicebus/azure-messaging-servicebus/src/main/java/com/azure/messaging/servicebus/ServiceBusReceiverAsyncClient.java
@@ -988,7 +988,9 @@ public final class ServiceBusReceiverAsyncClient implements AutoCloseable {
                 new IllegalArgumentException("The message has either been deleted or already settled.")));
         } else if (message.getLockToken() == null) {
             // message must be a peeked message (or somehow they created a message w/o a lock token)
-            throw new IllegalArgumentException("This operation is not supported for peeked messages. Only messages received using receiveMessages() or receiveMessagesWithContext() in PEEK_LOCK mode can be settled.");
+            return Mono.error(
+                logger.logExceptionAsError(new UnsupportedOperationException("This operation is not supported for peeked messages. Only messages received using receiveMessages() or receiveMessagesWithContext() in PEEK_LOCK mode can be settled."))
+            );
         }
 
         final String sessionIdToUse;
