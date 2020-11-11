5c3a6b57330640c28dcc099d406f5de401115d94
SDKAuto <sdkautomation@microsoft.com>
2020-11-11 07:01:48 +0000

    CodeGen from PR 11275 in Azure/azure-rest-api-specs [T2]containerservice for track2 python sdk (#11275)

--- sdk/containerservice/mgmt-v2019_08_01/src/main/java/com/microsoft/azure/management/containerservice/v2019_08_01/implementation/ManagedClusterInner.java
+++ sdk/containerservice/mgmt-v2019_08_01/src/main/java/com/microsoft/azure/management/containerservice/v2019_08_01/implementation/ManagedClusterInner.java
@@ -103,7 +103,9 @@ public class ManagedClusterInner extends Resource {
     private Boolean enableRBAC;
 
     /**
-     * (PREVIEW) Whether to enable Kubernetes Pod security policy.
+     * (DEPRECATING) Whether to enable Kubernetes pod security policy
+     * (preview). This feature is set for removal on October 15th, 2020. Learn
+     * more at aka.ms/aks/azpodpolicy.
      */
     @JsonProperty(value = "properties.enablePodSecurityPolicy")
     private Boolean enablePodSecurityPolicy;
@@ -340,7 +342,7 @@ public class ManagedClusterInner extends Resource {
     }
 
     /**
-     * Get (PREVIEW) Whether to enable Kubernetes Pod security policy.
+     * Get (DEPRECATING) Whether to enable Kubernetes pod security policy (preview). This feature is set for removal on October 15th, 2020. Learn more at aka.ms/aks/azpodpolicy.
      *
      * @return the enablePodSecurityPolicy value
      */
@@ -349,7 +351,7 @@ public class ManagedClusterInner extends Resource {
     }
 
     /**
-     * Set (PREVIEW) Whether to enable Kubernetes Pod security policy.
+     * Set (DEPRECATING) Whether to enable Kubernetes pod security policy (preview). This feature is set for removal on October 15th, 2020. Learn more at aka.ms/aks/azpodpolicy.
      *
      * @param enablePodSecurityPolicy the enablePodSecurityPolicy value to set
      * @return the ManagedClusterInner object itself.
