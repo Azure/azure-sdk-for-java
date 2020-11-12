cb2f4bbcd430a42f7b19a92cc15f6a73ec236e06
Connie <conniey@microsoft.com>
2020-11-12 12:22:13 -0800

    Cleaning up overlapping options.

--- sdk/servicebus/azure-messaging-servicebus-track2-perf/src/main/java/com/azure/messaging/servicebus/perf/App.java
+++ sdk/servicebus/azure-messaging-servicebus-track2-perf/src/main/java/com/azure/messaging/servicebus/perf/App.java
@@ -9,16 +9,14 @@ import com.azure.perf.test.core.PerfStressProgram;
  * Runs the ServiceBus performance test.
  *
  * <p>To run from command line. Package the project into a jar with dependencies via mvn clean package.
- * Then run the program via java -jar 'compiled-jar-with-dependencies-path' </p>
+ * Then run the program via <code>java -jar 'compiled-jar-with-dependencies-path'</code></p>
  *
- * <p> To run from IDE, set all the required environment variables in IntelliJ via Run -&gt; EditConfigurations
- * section.
- * Then run the App's main method via IDE.</p>
+ * <p>To run from IDE, set all the required environment variables in IntelliJ via Run -&gt; EditConfigurations
+ * section. Then run the App's main method via IDE.</p>
  */
 public class App {
-
     /**
-     *  main function.
+     * Main function.
      * @param args args
      * @throws RuntimeException If not able to load test classes.
      */
--- sdk/servicebus/azure-messaging-servicebus-track2-perf/src/main/java/com/azure/messaging/servicebus/perf/ServiceBusStressOptions.java
+++ sdk/servicebus/azure-messaging-servicebus-track2-perf/src/main/java/com/azure/messaging/servicebus/perf/ServiceBusStressOptions.java
@@ -12,26 +12,4 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
  */
 @JsonPropertyOrder(alphabetic = true)
 public class ServiceBusStressOptions extends PerfStressOptions {
-
-    @Parameter(names = { "-mr", "--maxReceive" }, description = "MaxReceive messages")
-    private int messagesToReceive = 10;
-
-    @Parameter(names = { "-ms", "--messageSend" }, description = "Messages to send")
-    private int messagesToSend = 10;
-
-    /**
-     * Get the configured messagesToSend option for performance test.
-     * @return The size.
-     */
-    public int getMessagesToSend() {
-        return messagesToSend;
-    }
-
-    /**
-     * Get the configured messagesToReceive option for performance test.
-     * @return The size.
-     */
-    public int getMessagesToReceive() {
-        return messagesToReceive;
-    }
 }
