db4527b3a8a6e4f404a827fb7146292b05f898d7
Jack Lu <dbqp99@msn.com>
2020-11-18 15:20:43 +0800

    add Copyright

--- sdk/spring/azure-spring-boot/src/test/java/com/azure/spring/autoconfigure/aad/MembershipsTest.java
+++ sdk/spring/azure-spring-boot/src/test/java/com/azure/spring/autoconfigure/aad/MembershipsTest.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.azure.spring.autoconfigure.aad;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
