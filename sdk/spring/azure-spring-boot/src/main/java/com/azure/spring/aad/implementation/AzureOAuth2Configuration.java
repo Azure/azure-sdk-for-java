ed03d8757db002442ffb287666bee1ad947b9510
Rujun Chen <Rujun.Chen@microsoft.com>
2020-11-19 14:34:39 +0800

    Keep aad legacy code (#17664)
    * checkout jialin's commit and make build pass.
    
    * Change new property prefix from 'azure.activedirectory' to 'azure.active.directory'.

--- sdk/spring/azure-spring-boot/src/main/java/com/azure/spring/aad/implementation/AzureOAuth2Configuration.java
+++ sdk/spring/azure-spring-boot/src/main/java/com/azure/spring/aad/implementation/AzureOAuth2Configuration.java
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.azure.spring.autoconfigure.aad;
+package com.azure.spring.aad.implementation;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,15 +10,10 @@ import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationC
 import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
 import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
 
-import java.util.Optional;
-
-/**
- * The main purpose of this class is to make AzureOAuth2AuthorizationCodeGrantRequestEntityConverter take effect.
- */
-public abstract class AzureOAuth2WebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+public abstract class AzureOAuth2Configuration extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private AzureClientRegistrationRepository azureClientRegistrationRepository;
+    private AzureClientRegistrationRepository repo;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -27,10 +22,7 @@ public abstract class AzureOAuth2WebSecurityConfigurerAdapter extends WebSecurit
 
     protected OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> accessTokenResponseClient() {
         DefaultAuthorizationCodeTokenResponseClient result = new DefaultAuthorizationCodeTokenResponseClient();
-        Optional.ofNullable(azureClientRegistrationRepository)
-                .map(AzureClientRegistrationRepository::defaultClient)
-                .map(AzureOAuth2AuthorizationCodeGrantRequestEntityConverter::new)
-                .ifPresent(result::setRequestEntityConverter);
+        result.setRequestEntityConverter(new AuthzCodeGrantRequestEntityConverter(repo.defaultClient()));
         return result;
     }
 }
--- sdk/spring/azure-spring-boot/src/test/java/com/azure/spring/autoconfigure/aad/UserPrincipalAzureADGraphTest.java
+++ sdk/spring/azure-spring-boot/src/test/java/com/azure/spring/autoconfigure/aad/UserPrincipalAzureADGraphTest.java
@@ -6,13 +6,24 @@ package com.azure.spring.autoconfigure.aad;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import com.nimbusds.jose.JWSObject;
+import com.nimbusds.jwt.JWTClaimsSet;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.util.StringUtils;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.text.ParseException;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
@@ -29,29 +40,26 @@ import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
 import static com.github.tomakehurst.wiremock.client.WireMock.verify;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.HttpHeaders.ACCEPT;
-import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
-public class GraphOboClientAzureADGraphTest {
+
+public class UserPrincipalAzureADGraphTest {
     @Rule
     public WireMockRule wireMockRule = new WireMockRule(9519);
 
-    private GraphOboClient graphOboClient;
+    private AzureADGraphClient graphClientMock;
     private AADAuthenticationProperties aadAuthenticationProperties;
     private ServiceEndpointsProperties serviceEndpointsProperties;
+    private String accessToken;
     private static String userGroupsJson;
 
     static {
         try {
             final ObjectMapper objectMapper = new ObjectMapper();
-            final Map<String, Object> json = objectMapper.readValue(
-                GraphOboClientAzureADGraphTest.class
-                    .getClassLoader()
-                    .getResourceAsStream("aad/azure-ad-graph-user-groups.json"),
-                new TypeReference<HashMap<String, Object>>() {
-                }
-            );
+            final Map<String, Object> json = objectMapper.readValue(UserPrincipalAzureADGraphTest.class.getClassLoader()
+                .getResourceAsStream("aad/azure-ad-graph-user-groups.json"),
+                new TypeReference<HashMap<String, Object>>() { });
             userGroupsJson = objectMapper.writeValueAsString(json);
         } catch (IOException e) {
             e.printStackTrace();
@@ -62,6 +70,7 @@ public class GraphOboClientAzureADGraphTest {
 
     @Before
     public void setup() {
+        accessToken = TestConstants.ACCESS_TOKEN;
         aadAuthenticationProperties = new AADAuthenticationProperties();
         serviceEndpointsProperties = new ServiceEndpointsProperties();
         final ServiceEndpoints serviceEndpoints = new ServiceEndpoints();
@@ -72,7 +81,7 @@ public class GraphOboClientAzureADGraphTest {
     @Test
     public void getAuthoritiesByUserGroups() throws Exception {
         aadAuthenticationProperties.getUserGroup().setAllowedGroups(Collections.singletonList("group1"));
-        this.graphOboClient = new GraphOboClient(aadAuthenticationProperties, serviceEndpointsProperties);
+        this.graphClientMock = new AzureADGraphClient(aadAuthenticationProperties, serviceEndpointsProperties);
 
         stubFor(get(urlEqualTo("/memberOf"))
             .withHeader(ACCEPT, equalTo("application/json;odata=minimalmetadata"))
@@ -81,21 +90,19 @@ public class GraphOboClientAzureADGraphTest {
                 .withHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
                 .withBody(userGroupsJson)));
 
-        assertThat(graphOboClient.getGrantedAuthorities(TestConstants.ACCESS_TOKEN))
-            .isNotEmpty()
-            .extracting(GrantedAuthority::getAuthority)
-            .containsExactly("ROLE_group1");
+        assertThat(graphClientMock.getGrantedAuthorities(TestConstants.ACCESS_TOKEN)).isNotEmpty()
+                                                                                     .extracting(GrantedAuthority::getAuthority).containsExactly("ROLE_group1");
 
         verify(getRequestedFor(urlMatching("/memberOf"))
-            .withHeader(AUTHORIZATION, equalTo(TestConstants.BEARER_TOKEN))
+            .withHeader(HttpHeaders.AUTHORIZATION, equalTo(String.format("Bearer %s", accessToken)))
             .withHeader(ACCEPT, equalTo("application/json;odata=minimalmetadata"))
             .withHeader("api-version", equalTo("1.6")));
     }
 
     @Test
     public void getGroups() throws Exception {
-        aadAuthenticationProperties.getUserGroup().setAllowedGroups(Arrays.asList("group1", "group2", "group3"));
-        this.graphOboClient = new GraphOboClient(aadAuthenticationProperties, serviceEndpointsProperties);
+        aadAuthenticationProperties.setActiveDirectoryGroups(Arrays.asList("group1", "group2", "group3"));
+        this.graphClientMock = new AzureADGraphClient(aadAuthenticationProperties, serviceEndpointsProperties);
 
         stubFor(get(urlEqualTo("/memberOf"))
             .withHeader(ACCEPT, equalTo("application/json;odata=minimalmetadata"))
@@ -104,17 +111,43 @@ public class GraphOboClientAzureADGraphTest {
                 .withHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
                 .withBody(userGroupsJson)));
 
-        final Collection<? extends GrantedAuthority> authorities = graphOboClient
+        final Collection<? extends GrantedAuthority> authorities = graphClientMock
             .getGrantedAuthorities(TestConstants.ACCESS_TOKEN);
 
-        assertThat(authorities)
-            .isNotEmpty()
-            .extracting(GrantedAuthority::getAuthority)
-            .containsExactlyInAnyOrder("ROLE_group1", "ROLE_group2", "ROLE_group3");
+        assertThat(authorities).isNotEmpty().extracting(GrantedAuthority::getAuthority)
+                               .containsExactlyInAnyOrder("ROLE_group1", "ROLE_group2", "ROLE_group3");
 
         verify(getRequestedFor(urlMatching("/memberOf"))
-            .withHeader(AUTHORIZATION, equalTo(TestConstants.BEARER_TOKEN))
+            .withHeader(HttpHeaders.AUTHORIZATION, equalTo(String.format("Bearer %s", accessToken)))
             .withHeader(ACCEPT, equalTo("application/json;odata=minimalmetadata"))
             .withHeader("api-version", equalTo("1.6")));
+    }
+
+    @Test
+    public void userPrincipalIsSerializable() throws ParseException, IOException, ClassNotFoundException {
+        final File tmpOutputFile = File.createTempFile("test-user-principal", "txt");
+
+        try (FileOutputStream fileOutputStream = new FileOutputStream(tmpOutputFile);
+             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+             FileInputStream fileInputStream = new FileInputStream(tmpOutputFile);
+             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)) {
+
+            final JWSObject jwsObject = JWSObject.parse(TestConstants.JWT_TOKEN);
+            final JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder().subject("fake-subject").build();
+            final UserPrincipal principal = new UserPrincipal("", jwsObject, jwtClaimsSet);
+
+            objectOutputStream.writeObject(principal);
+
+            final UserPrincipal serializedPrincipal = (UserPrincipal) objectInputStream.readObject();
+
+            Assert.assertNotNull("Serialized UserPrincipal not null", serializedPrincipal);
+            Assert.assertFalse("Serialized UserPrincipal kid not empty",
+                StringUtils.isEmpty(serializedPrincipal.getKid()));
+            Assert.assertNotNull("Serialized UserPrincipal claims not null.", serializedPrincipal.getClaims());
+            Assert.assertTrue("Serialized UserPrincipal claims not empty.",
+                    serializedPrincipal.getClaims().size() > 0);
+        } finally {
+            Files.deleteIfExists(tmpOutputFile.toPath());
+        }
     }
 }
