282cd32a425df29baa3af0176c372a45a53e6a6e
samvaity <savaity@microsoft.com>
2020-11-22 23:07:00 -0800

    remove wait time

--- sdk/formrecognizer/test-resources-post.ps1
+++
@@ -1,6 +0,0 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-
-# This script is used to wait until XCache is refreshed for the service properties (30s), and role assignment takes effect (300s).
-
-Start-Sleep -s 600
