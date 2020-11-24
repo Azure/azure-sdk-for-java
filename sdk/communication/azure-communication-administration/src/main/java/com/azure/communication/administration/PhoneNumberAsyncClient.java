ed4c8ccc6860192860eb596aa48032020dc863da
Ankit Arora <ankita@microsoft.com>
2020-11-24 19:43:04 +0530

    Phone number API definition

--- sdk/communication/azure-communication-administration/src/main/java/com/azure/communication/administration/PhoneNumberAsyncClient.java
+++ sdk/communication/azure-communication-administration/src/main/java/com/azure/communication/administration/PhoneNumberAsyncClient.java
@@ -2,7 +2,17 @@
 // Licensed under the MIT License.
 package com.azure.communication.administration;
 
+import com.azure.communication.administration.models.*;
+import com.azure.core.annotation.ReturnType;
 import com.azure.core.annotation.ServiceClient;
+import com.azure.core.annotation.ServiceMethod;
+import com.azure.core.http.rest.Response;
+import com.azure.core.util.Context;
+import com.azure.core.util.polling.PollerFlux;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 /**
  * Asynchronous client for Communication service phone number operations
@@ -12,4 +22,102 @@ public final class PhoneNumberAsyncClient {
 
     PhoneNumberAsyncClient() {}
 
+    @ServiceMethod(returns = ReturnType.COLLECTION)
+    public Flux<PhoneNumberCountry> listAllSupportedCountries(String locale) {
+        return Flux.error(new UnsupportedOperationException("not yet implemented"));
+    }
+
+    @ServiceMethod(returns = ReturnType.SINGLE)
+    public Mono<TollFreeAreaCodes> getTollFreeAreaCodes(String countryCode) {
+        return Mono.error(new UnsupportedOperationException("not yet implemented"));
+    }
+
+    @ServiceMethod(returns = ReturnType.SINGLE)
+    public Mono<GeographicAreaCodes> getGeographicAreaCodes(String countryCode) {
+        return Mono.error(new UnsupportedOperationException("not yet implemented"));
+    }
+
+    @ServiceMethod(returns = ReturnType.SINGLE)
+    public Mono<Response<GeographicAreaCodes>> getGeographicAreaCodesWithResponse(
+        String countryCode, String locale, String locationPath) {
+        return Mono.error(new UnsupportedOperationException("not yet implemented"));
+    }
+
+    @ServiceMethod(returns = ReturnType.COLLECTION)
+    public Flux<CountryOffering> listAllOfferings(String countryCode) {
+        return Flux.error(new UnsupportedOperationException("not yet implemented"));
+    }
+
+    @ServiceMethod(returns = ReturnType.COLLECTION)
+    public Flux<Response<CountryOffering>> listAllOfferingsWithResponse(
+        String countryCode, PhoneNumberType phoneNumberType, AssignmentType assignmentType) {
+        return Flux.error(new UnsupportedOperationException("not yet implemented"));
+    }
+
+    @ServiceMethod(returns = ReturnType.COLLECTION)
+    public PollerFlux<Operation, SearchResult> beginPhoneNumberSearch(
+        SearchRequest searchRequest,  String countryCode) {
+        return new PollerFlux<Operation, SearchResult>(
+            Duration.ofMillis(1000),
+            (context) -> Mono.empty(),
+            (context) -> Mono.empty(),
+            (activationResponse, context) -> Mono.empty(),
+            (context) -> Mono.empty());
+    }
+
+    @ServiceMethod(returns = ReturnType.COLLECTION)
+    public PollerFlux<Operation, SearchResult> beginPhoneNumberPurchase(String searchId) {
+        return new PollerFlux<Operation, SearchResult>(
+            Duration.ofMillis(1000),
+            (context) -> Mono.empty(),
+            (context) -> Mono.empty(),
+            (activationResponse, context) -> Mono.empty(),
+            (context) -> Mono.empty());
+    }
+
+    @ServiceMethod(returns = ReturnType.SINGLE)
+    public Mono<SearchResult> getSearchResults(String searchId) {
+        return Mono.error(new UnsupportedOperationException("not yet implemented"));
+    }
+
+    @ServiceMethod(returns = ReturnType.SINGLE)
+    public Mono<Operation> getOperation(String operationId) {
+        return Mono.error(new UnsupportedOperationException("not yet implemented"));
+    }
+
+    @ServiceMethod(returns = ReturnType.SINGLE)
+    public Mono<Void> cancelOperation(String operationId) {
+        return Mono.error(new UnsupportedOperationException("not yet implemented"));
+    }
+
+    @ServiceMethod(returns = ReturnType.COLLECTION)
+    public Flux<AcquiredPhoneNumbers> listAllPhoneNumbers() {
+        return Flux.error(new UnsupportedOperationException("not yet implemented"));
+    }
+
+    @ServiceMethod(returns = ReturnType.SINGLE)
+    public Mono<AcquiredPhoneNumber> getPhoneNumber(String phoneNumber) {
+        return Mono.error(new UnsupportedOperationException("not yet implemented"));
+    }
+
+    @ServiceMethod(returns = ReturnType.COLLECTION)
+    public PollerFlux<Operation, AcquiredPhoneNumber> beginUpdatePhoneNumber(
+        String phoneNumber, AcquiredPhoneNumberUpdate update) {
+        return new PollerFlux<Operation, AcquiredPhoneNumber>(
+            Duration.ofMillis(1000),
+            (context) -> Mono.empty(),
+            (context) -> Mono.empty(),
+            (activationResponse, context) -> Mono.empty(),
+            (context) -> Mono.empty());
+    }
+
+    @ServiceMethod(returns = ReturnType.COLLECTION)
+    public PollerFlux<Operation, Void> beginReleasePhoneNumber(String phoneNumber) {
+        return new PollerFlux<Operation, Void>(
+            Duration.ofMillis(1000),
+            (context) -> Mono.empty(),
+            (context) -> Mono.empty(),
+            (activationResponse, context) -> Mono.empty(),
+            (context) -> Mono.empty());
+    }
 }
--- sdk/communication/azure-communication-administration/src/main/java/com/azure/communication/administration/PhoneNumberClient.java
+++ sdk/communication/azure-communication-administration/src/main/java/com/azure/communication/administration/PhoneNumberClient.java
@@ -2,7 +2,16 @@
 // Licensed under the MIT License.
 package com.azure.communication.administration;
 
+import com.azure.communication.administration.models.*;
+import com.azure.core.annotation.ReturnType;
 import com.azure.core.annotation.ServiceClient;
+import com.azure.core.annotation.ServiceMethod;
+import com.azure.core.http.rest.PagedIterable;
+import com.azure.core.http.rest.Response;
+import com.azure.core.util.Context;
+import com.azure.core.util.polling.SyncPoller;
+
+import java.time.Duration;
 
 /**
  * Synchronous client for Communication service phone number operations
@@ -11,4 +20,83 @@ import com.azure.core.annotation.ServiceClient;
 public final class PhoneNumberClient {
 
     PhoneNumberClient() {}
+
+    @ServiceMethod(returns = ReturnType.COLLECTION)
+    public PagedIterable<PhoneNumberCountry> listAllSupportedCountries(String locale) {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    @ServiceMethod(returns = ReturnType.SINGLE)
+    public TollFreeAreaCodes getTollFreeAreaCodes(String countryCode) {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    @ServiceMethod(returns = ReturnType.SINGLE)
+    public GeographicAreaCodes getGeographicAreaCodes(String countryCode) {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    @ServiceMethod(returns = ReturnType.SINGLE)
+    public Response<GeographicAreaCodes> getGeographicAreaCodesWithResponse(
+        String countryCode, String locale, String locationPath, Context context) {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    @ServiceMethod(returns = ReturnType.COLLECTION)
+    public PagedIterable<CountryOffering> listAllOfferings(String countryCode) {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    @ServiceMethod(returns = ReturnType.COLLECTION)
+    public PagedIterable<Response<CountryOffering>> listAllOfferingsWithResponse(
+        String countryCode, PhoneNumberType phoneNumberType, AssignmentType assignmentType, Context context) {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    @ServiceMethod(returns = ReturnType.COLLECTION)
+    public SyncPoller<Operation, SearchResult> beginPhoneNumberSearch(
+        SearchRequest searchRequest,  String countryCode) {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    @ServiceMethod(returns = ReturnType.COLLECTION)
+    public SyncPoller<Operation, SearchResult> beginPhoneNumberPurchase(String searchId) {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    @ServiceMethod(returns = ReturnType.SINGLE)
+    public SearchResult getSearchResults(String searchId) {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    @ServiceMethod(returns = ReturnType.SINGLE)
+    public Operation getOperation(String operationId) {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    @ServiceMethod(returns = ReturnType.SINGLE)
+    public Void cancelOperation(String operationId) {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    @ServiceMethod(returns = ReturnType.COLLECTION)
+    public PagedIterable<AcquiredPhoneNumbers> listAllPhoneNumbers() {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    @ServiceMethod(returns = ReturnType.SINGLE)
+    public AcquiredPhoneNumber getPhoneNumber(String phoneNumber) {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    @ServiceMethod(returns = ReturnType.COLLECTION)
+    public SyncPoller<Operation, AcquiredPhoneNumber> beginUpdatePhoneNumber(
+        String phoneNumber, AcquiredPhoneNumberUpdate update) {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    @ServiceMethod(returns = ReturnType.COLLECTION)
+    public SyncPoller<Operation, Void> beginReleasePhoneNumber(String phoneNumber) {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
 }
