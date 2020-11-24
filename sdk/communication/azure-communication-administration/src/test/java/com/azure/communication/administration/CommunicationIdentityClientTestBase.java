b11c309870d30d793d1e0cdb77b5f255b95babe0
Jorge Beauregard <jbeauregardb@microsoft.com>
2020-11-24 15:54:09 -0600

    Using DefaultAzureCredential for managed identity

--- sdk/communication/azure-communication-administration/src/test/java/com/azure/communication/administration/CommunicationIdentityClientTestBase.java
+++ sdk/communication/azure-communication-administration/src/test/java/com/azure/communication/administration/CommunicationIdentityClientTestBase.java
@@ -3,18 +3,13 @@
 
 package com.azure.communication.administration;
 
-import com.azure.core.credential.AccessToken;
-import com.azure.core.credential.TokenCredential;
-import com.azure.core.credential.TokenRequestContext;
 import com.azure.core.http.HttpClient;
 import com.azure.core.test.TestBase;
 import com.azure.core.test.TestMode;
 import com.azure.core.util.logging.ClientLogger;
 import com.azure.core.util.Configuration;
 import com.azure.identity.DefaultAzureCredentialBuilder;
-import reactor.core.publisher.Mono;
 
-import java.time.OffsetDateTime;
 import java.util.Base64;
 import java.util.Locale;
 
