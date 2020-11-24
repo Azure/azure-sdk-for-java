eb906c106cbf80b853fefde9cf085ca78cbd6081
Mohammad Derakhshani <moderakh@users.noreply.github.com>
2020-11-24 08:18:34 -0800

    spark filter to cosmos query predicate

--- sdk/cosmos/azure-cosmos-spark_3-0_2-12/src/main/scala/com/azure/cosmos/spark/CosmosParametrizedQuery.scala
+++ sdk/cosmos/azure-cosmos-spark_3-0_2-12/src/main/scala/com/azure/cosmos/spark/CosmosParametrizedQuery.scala
@@ -2,16 +2,14 @@
 // Licensed under the MIT License.
 package com.azure.cosmos.models
 
-// scalastyle:off underscore.import
-import scala.collection.JavaConverters._
-// scalastyle:on underscore.import
-
-case class CosmosParametrizedQuery(val queryTest : String,
+case class CosmosParametrizedQuery(val queryTest: String,
                                    val parameterNames: List[String],
                                    val parameterValues: List[Any])
   extends Serializable {
-  def toSqlQuerySpec() : SqlQuerySpec = {
-    new SqlQuerySpec(queryTest, new SqlParameter(parameterNames.asJava.get(0), parameterValues.asJava.get(0)))
+  def toSqlQuerySpec(): SqlQuerySpec = {
+    new SqlQuerySpec(queryTest, new SqlParameter(parameterNames.zip(parameterValues) {
+      case (parameterName, parameterValue) => new SqlParameter(parameterName, parameterValue)
+    }))
   }
 }
 
