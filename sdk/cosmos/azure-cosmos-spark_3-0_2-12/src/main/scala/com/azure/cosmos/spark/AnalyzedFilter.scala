81105136e6125496d02779d6aab82f103653a5d3
Mohammad Derakhshani <moderakh@users.noreply.github.com>
2020-11-24 17:14:42 -0800

    added comment for future optimization

--- sdk/cosmos/azure-cosmos-spark_3-0_2-12/src/main/scala/com/azure/cosmos/spark/AnalyzedFilter.scala
+++ sdk/cosmos/azure-cosmos-spark_3-0_2-12/src/main/scala/com/azure/cosmos/spark/AnalyzedFilter.scala
@@ -6,7 +6,7 @@ package com.azure.cosmos.spark
 import com.azure.cosmos.models.CosmosParametrizedQuery
 import org.apache.spark.sql.sources.Filter
 
-// TODO: moderakh, thought for later optimization:
+// TODO: moderakh, thought for future optimization:
 //  if we can identify if the user filter is a equality on cosmos partitionKeyValue
 //  then we can set partitionKeyValue in the CosmosQueryOption
 //  the benfit is that if the partitionKeyValue is set in the CosmosQueryOption
