64506af55ad5fd12e5212048f05f1f33b4fb5a1a
Mohammad Derakhshani <moderakh@users.noreply.github.com>
2020-11-24 17:15:14 -0800

    added comment for future optimization

--- sdk/cosmos/azure-cosmos-spark_3-0_2-12/src/main/scala/com/azure/cosmos/spark/AnalyzedFilter.scala
+++ sdk/cosmos/azure-cosmos-spark_3-0_2-12/src/main/scala/com/azure/cosmos/spark/AnalyzedFilter.scala
@@ -9,7 +9,7 @@ import org.apache.spark.sql.sources.Filter
 // TODO: moderakh, thought for future optimization:
 //  if we can identify if the user filter is a equality on cosmos partitionKeyValue
 //  then we can set partitionKeyValue in the CosmosQueryOption
-//  the benfit is that if the partitionKeyValue is set in the CosmosQueryOption
+//  the benefit is that if the partitionKeyValue is set in the CosmosQueryOption
 //  the antlr query parsing support can eliminate the need for query plan fetch from GW
 case class AnalyzedFilters(cosmosParametrizedQuery: CosmosParametrizedQuery,
                            filtersToBePushedDownToCosmos: Array[Filter],
