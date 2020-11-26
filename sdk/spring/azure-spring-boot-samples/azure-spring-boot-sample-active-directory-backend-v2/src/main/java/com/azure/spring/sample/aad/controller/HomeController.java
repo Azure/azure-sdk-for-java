a8ccda274059722b8f54d9e0185fd61ebd2bafcf
Rujun Chen <rujche@microsoft.com>
2020-11-26 08:25:12 +0800

    Make 'azure-spring-boot-sample-active-directory-backend-v2' work well in new aad implementation.

--- sdk/spring/azure-spring-boot-samples/azure-spring-boot-sample-active-directory-backend-v2/src/main/java/com/azure/spring/sample/aad/controller/HomeController.java
+++ sdk/spring/azure-spring-boot-samples/azure-spring-boot-sample-active-directory-backend-v2/src/main/java/com/azure/spring/sample/aad/controller/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -33,11 +34,11 @@ public class HomeController {
     }
 
     @GetMapping("/")
-    public String index(Model model, OAuth2AuthenticationToken authentication) {
-        final OAuth2AuthorizedClient authorizedClient =
-                this.authorizedClientService.loadAuthorizedClient(
-                        authentication.getAuthorizedClientRegistrationId(),
-                        authentication.getName());
+    public String index(
+        Model model,
+        OAuth2AuthenticationToken authentication,
+        @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient
+    ) {
         model.addAttribute("userName", authentication.getName());
         model.addAttribute("clientName", authorizedClient.getClientRegistration().getClientName());
         return "index";
