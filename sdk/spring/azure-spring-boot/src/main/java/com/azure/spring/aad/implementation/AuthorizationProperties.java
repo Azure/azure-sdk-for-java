dcce3350a8410d59a5733b6351847c6a6942e230
lzc-1997-abel <70368631+lzc-1997-abel@users.noreply.github.com>
2020-11-26 17:11:24 +0800

    add on demand properties

--- sdk/spring/azure-spring-boot/src/main/java/com/azure/spring/aad/implementation/AuthorizationProperties.java
+++ sdk/spring/azure-spring-boot/src/main/java/com/azure/spring/aad/implementation/AuthorizationProperties.java
@@ -9,11 +9,21 @@ public class AuthorizationProperties {
 
     private List<String> scopes;
 
+    private Boolean onDemand = false;
+
     public void setScopes(List<String> scopes) {
         this.scopes = scopes;
     }
 
     public List<String> getScopes() {
         return scopes;
+    }
+
+    public Boolean getOnDemand() {
+        return onDemand;
+    }
+
+    public void setOnDemand(Boolean onDemand) {
+        this.onDemand = onDemand;
     }
 }
