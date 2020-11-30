d21181e0010080eaf0cb56fa29e1d8c52dc1ca6b
Tanyi Chen <tanchen@microsoft.com>
2020-11-30 16:23:23 +0800

    use unmodifiableList

--- sdk/resourcemanager/azure-resourcemanager-cosmos/src/main/java/com/azure/resourcemanager/cosmos/implementation/CosmosDBAccountImpl.java
+++ sdk/resourcemanager/azure-resourcemanager-cosmos/src/main/java/com/azure/resourcemanager/cosmos/implementation/CosmosDBAccountImpl.java
@@ -35,6 +35,7 @@ import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -86,7 +87,7 @@ class CosmosDBAccountImpl
 
     @Override
     public List<IpAddressOrRange> ipRules() {
-        return this.innerModel().ipRules();
+        return Collections.unmodifiableList(this.innerModel().ipRules());
     }
 
     @Override
