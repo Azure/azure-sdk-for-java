bf36292f75b370e917462353a29512cd7b6b504d
ZhuXiaoBing-cn <zhu13865245671@163.com>
2020-12-03 15:33:46 +0800

    * Update sample

--- sdk/spring/azure-spring-boot-samples/azure-spring-boot-sample-active-directory-spring-security-resource-server/src/main/java/com/azure/spring/sample/aad/controller/HomeController.java
+++ sdk/spring/azure-spring-boot-samples/azure-spring-boot-sample-active-directory-spring-security-resource-server/src/main/java/com/azure/spring/sample/aad/controller/HomeController.java
@@ -5,24 +5,24 @@ package com.azure.spring.sample.aad.controller;
 
 
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 public class HomeController {
 
     @GetMapping("/file")
     @ResponseBody
-    @PreAuthorize("hasRole('SCOPE_File.read')")
+    @PreAuthorize("hasAuthority('SCOPE_File.read')")
     public String group1() {
         return "file read success.";
     }
 
     @GetMapping("/user")
     @ResponseBody
-    @PreAuthorize("hasRole('SCOPE_User.read')")
+    @PreAuthorize("hasAuthority('SCOPE_User.read')")
     public String group2() {
         return "user read success.";
     }
