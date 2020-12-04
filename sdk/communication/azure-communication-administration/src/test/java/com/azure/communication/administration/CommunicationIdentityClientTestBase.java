fe8cf286f867858a62c23876f527e407492e7475
Jorge Beauregard <jbeauregardb@microsoft.com>
2020-12-04 14:10:44 -0600

    Added FakeCredential for playback tests

--- sdk/communication/azure-communication-administration/src/test/java/com/azure/communication/administration/CommunicationIdentityClientTestBase.java
+++ sdk/communication/azure-communication-administration/src/test/java/com/azure/communication/administration/CommunicationIdentityClientTestBase.java
@@ -3,13 +3,18 @@
 
 package com.azure.communication.administration;
 
+import com.azure.core.credential.AccessToken;
+import com.azure.core.credential.TokenCredential;
+import com.azure.core.credential.TokenRequestContext;
 import com.azure.core.http.HttpClient;
 import com.azure.core.test.TestBase;
 import com.azure.core.test.TestMode;
 import com.azure.core.util.logging.ClientLogger;
 import com.azure.core.util.Configuration;
 import com.azure.identity.DefaultAzureCredentialBuilder;
+import reactor.core.publisher.Mono;
 
+import java.time.OffsetDateTime;
 import java.util.Base64;
 import java.util.Locale;
 
@@ -44,8 +49,13 @@ public class CommunicationIdentityClientTestBase extends TestBase {
         builder
             .endpoint(ENDPOINT)
             .httpClient(httpClient == null ? interceptorManager.getPlaybackClient() : httpClient);
-
-        builder.credential(new DefaultAzureCredentialBuilder().build());
+        
+        if (getTestMode() == TestMode.PLAYBACK) {
+            builder.credential(new FakeCredentials());
+        }
+        else {
+            builder.credential(new DefaultAzureCredentialBuilder().build());
+        }
 
         if (getTestMode() == TestMode.RECORD) {
             builder.addPolicy(interceptorManager.getRecordPolicy());
@@ -81,6 +91,13 @@ public class CommunicationIdentityClientTestBase extends TestBase {
         } else {
             logger.info("Environment variable '{}' has not been set yet. Using 'Playback' mode.", "AZURE_TEST_MODE");
             return TestMode.PLAYBACK;
+        }
+    }
+
+    static class FakeCredentials implements TokenCredential {
+        @Override
+        public Mono<AccessToken> getToken(TokenRequestContext tokenRequestContext) {
+            return Mono.just(new AccessToken("someFakeToken", OffsetDateTime.MAX));
         }
     }
 }
