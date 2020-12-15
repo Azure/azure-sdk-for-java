fc1878d8b2dcf58a7878cb60cdd329de8939459e
minnieliu <minnieliu96@hotmail.com>
2020-12-15 15:00:34 -0800

    [Communication] - Common - Adding TokenRefreshOptions to CommunicationTokenCredential ctor (#18060)
    * Adding TokenRefreshOptions
    
    * Update changelog
    
    * Update changelog
    
    Co-authored-by: Minnie Liu <peiliu@microsoft.com>

---
+++ sdk/communication/azure-communication-common/src/main/java/com/azure/communication/common/CommunicationTokenRefreshOptions.java
@@ -0,0 +1,64 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+package com.azure.communication.common;
+
+/**
+ * Options for refreshing CommunicationTokenCredential
+ */
+public class CommunicationTokenRefreshOptions {
+    private final TokenRefresher tokenRefresher;
+    private final boolean refreshProactively;
+    private final String token;
+
+    /**
+     * Creates a CommunicationTokenRefreshOptions object
+     * 
+     * @param tokenRefresher the token refresher to provide capacity to fetch fresh token
+     * @param refreshProactively when set to true, turn on proactive fetching to call
+     *                           tokenRefresher before token expiry by minutes set
+     *                           with setCallbackOffsetMinutes or default value of
+     *                           two minutes
+     */
+    public CommunicationTokenRefreshOptions(TokenRefresher tokenRefresher, boolean refreshProactively) {
+        this.tokenRefresher = tokenRefresher;
+        this.refreshProactively = refreshProactively;
+        this.token = null;
+    }
+
+     /**
+     * Creates a CommunicationTokenRefreshOptions object
+     * 
+     * @param tokenRefresher the token refresher to provide capacity to fetch fresh token
+     * @param refreshProactively when set to true, turn on proactive fetching to call
+     *                           tokenRefresher before token expiry by minutes set
+     *                           with setCallbackOffsetMinutes or default value of
+     *                           two minutes
+     * @param token the optional serialized JWT token
+     */
+    public CommunicationTokenRefreshOptions(TokenRefresher tokenRefresher, boolean refreshProactively, String token) {
+        this.tokenRefresher = tokenRefresher;
+        this.refreshProactively = refreshProactively;
+        this.token = token;
+    }
+
+    /**
+     * @return the token refresher to provide capacity to fetch fresh token
+     */
+    public TokenRefresher getTokenRefresher() {
+        return tokenRefresher;
+    }
+
+    /**
+     * @return whether or not to refresh token proactively
+     */
+    public boolean getRefreshProactively() {
+        return refreshProactively;
+    }
+
+    /**
+     * @return the serialized JWT token
+     */
+    public String getToken() {
+        return token;
+    }
+}
