50ae6fb2c3fef20b7cfea1760cf741e58ef18d6e
Blackbaud-EricSlater <eric.slater@blackbaud.com>
2020-12-15 14:04:54 -0600

    make class fully static

--- sdk/cosmos/azure-spring-data-cosmos/src/main/java/com/azure/spring/data/cosmos/repository/support/IndexPolicyCompareService.java
+++ sdk/cosmos/azure-spring-data-cosmos/src/main/java/com/azure/spring/data/cosmos/repository/support/IndexPolicyCompareService.java
@@ -16,10 +16,6 @@ import java.util.stream.Collectors;
 public class IndexPolicyCompareService {
 
     public static boolean policyNeedsUpdate(IndexingPolicy existingPolicy, IndexingPolicy newPolicy) {
-        return new IndexPolicyCompareService().needsUpdate(existingPolicy, newPolicy);
-    }
-
-    private boolean needsUpdate(IndexingPolicy existingPolicy, IndexingPolicy newPolicy) {
         return !hasSameIncludedPaths(existingPolicy.getIncludedPaths(), newPolicy.getIncludedPaths())
             || !hasSameExcludedPaths(existingPolicy.getExcludedPaths(), newPolicy.getExcludedPaths())
             || !existingPolicy.getIndexingMode().equals(newPolicy.getIndexingMode())
@@ -27,7 +23,7 @@ public class IndexPolicyCompareService {
     }
 
     // Returns true if the lists are the same or the only difference is that the existing paths contain "/*"
-    private boolean hasSameIncludedPaths(List<IncludedPath> existingPaths, List<IncludedPath> newPaths) {
+    private static boolean hasSameIncludedPaths(List<IncludedPath> existingPaths, List<IncludedPath> newPaths) {
         List<IncludedPath> existingListDiff = existingPaths.stream()
             .filter(element -> !newPaths.contains(element))
             .collect(Collectors.toList());
@@ -43,7 +39,7 @@ public class IndexPolicyCompareService {
     }
 
     // Returns true if the lists are the same or the only difference is that the existing paths contain the etag field
-    private boolean hasSameExcludedPaths(List<ExcludedPath> existingPaths, List<ExcludedPath> newPaths) {
+    private static boolean hasSameExcludedPaths(List<ExcludedPath> existingPaths, List<ExcludedPath> newPaths) {
         List<ExcludedPath> existingListDiff = existingPaths.stream()
             .filter(element -> !newPaths.contains(element))
             .collect(Collectors.toList());
@@ -57,6 +53,5 @@ public class IndexPolicyCompareService {
             && existingListDiff.size() == 1
             && existingListDiff.get(0).getPath().equals("/\"_etag\"/?"));
     }
-
 
 }
