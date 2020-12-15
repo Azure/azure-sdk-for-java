024c1baf675fafe9c77c962e07f34a28c4c1e90e
lzc-1997-abel <70368631+lzc-1997-abel@users.noreply.github.com>
2020-12-15 18:08:10 +0800

    add scopes for default client

--- sdk/spring/azure-spring-boot/src/main/java/com/azure/spring/aad/webapp/AzureActiveDirectoryConfiguration.java
+++ sdk/spring/azure-spring-boot/src/main/java/com/azure/spring/aad/webapp/AzureActiveDirectoryConfiguration.java
@@ -126,6 +126,13 @@ public class AzureActiveDirectoryConfiguration {
 
     private ClientRegistration createClientBuilder(String id, AuthorizationProperties authz) {
         ClientRegistration.Builder result = createClientBuilder(id);
+        List<String> scopes = authz.getScopes();
+        if(authz.isOnDemand() && !scopes.contains("openid")){
+            scopes.add("openid");
+        }
+        if(authz.isOnDemand() && !scopes.contains("profile")){
+            scopes.add("profile");
+        }
         result.scope(authz.getScopes());
         return result.build();
     }
