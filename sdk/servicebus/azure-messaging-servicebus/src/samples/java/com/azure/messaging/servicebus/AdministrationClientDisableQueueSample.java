b3ec95f9f72e879e991fe67f5a643d85da85482b
Hemant Tanwar <hetanwar@users.noreply.github.com>
2020-12-17 22:06:08 -0800

    Adding sample based on user query

---
+++ sdk/servicebus/azure-messaging-servicebus/src/samples/java/com/azure/messaging/servicebus/AdministrationClientDisableQueueSample.java
@@ -0,0 +1,53 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.messaging.servicebus;
+
+import com.azure.messaging.servicebus.administration.ServiceBusAdministrationClient;
+import com.azure.messaging.servicebus.administration.ServiceBusAdministrationClientBuilder;
+import com.azure.messaging.servicebus.administration.models.EntityStatus;
+import com.azure.messaging.servicebus.administration.models.QueueProperties;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Sample example showing how disable queue in Service Bus Queue.
+ */
+public class AdministrationClientDisableQueueSample {
+    /**
+     * Main method to how disable queue in Service Bus Queue.
+     *
+     * @param args Unused arguments to the program.
+     * @throws InterruptedException If the program is unable to sleep while waiting for the receive to complete.
+     */
+    public static void main(String[] args) throws InterruptedException {
+        // The connection string value can be obtained by:
+        // 1. Going to your Service Bus namespace in Azure Portal.
+        // 2. Go to "Shared access policies"
+        // 3. Copy the connection string for the "RootManageSharedAccessKey" policy.
+
+        String connectionString = "Endpoint={fully-qualified-namespace};SharedAccessKeyName={policy-name};"
+            + "SharedAccessKey={key}";
+
+        // Create a administrator client using connection string.
+        ServiceBusAdministrationClient administration = new ServiceBusAdministrationClientBuilder()
+            .connectionString(connectionString)
+            .buildClient();
+
+        // "<<queue-name>>" will be the name of the Service Bus queue instance you created
+        // inside the Service Bus namespace.
+        QueueProperties queueProperties = administration.getQueue("<< queue-name >>");
+
+        System.out.println(" Queue status: " + queueProperties.getStatus());
+
+        QueueProperties updatedQueueProperties = administration.updateQueue(queueProperties.setStatus(EntityStatus.DISABLED));
+
+        System.out.println(" Queue status after update: " + updatedQueueProperties.getStatus());
+
+        // Subscribe is not a blocking call so we sleep here so the program does not end while finishing
+        // the peek operation.
+        TimeUnit.SECONDS.sleep(10);
+
+    }
+
+}
