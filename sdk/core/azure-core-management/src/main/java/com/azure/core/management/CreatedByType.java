fa58baf1766bafdf3b3afe0b5f33ce742babe39a
Weidong Xu <weidxu@microsoft.com>
2020-12-22 13:28:31 +0800

    nit

--- sdk/core/azure-core-management/src/main/java/com/azure/core/management/CreatedByType.java
+++ sdk/core/azure-core-management/src/main/java/com/azure/core/management/CreatedByType.java
@@ -10,13 +10,13 @@ import java.util.Collection;
 /** Defines values for CreatedByType. */
 public final class CreatedByType extends ExpandableStringEnum<CreatedByType> {
 
-    /** Static value User for CreatedByType, an AAD user. */
+    /** Static value User for CreatedByType, represents an AAD user. */
     public static final CreatedByType USER = fromString("User");
 
-    /** Static value Application for CreatedByType, an AAD application. */
+    /** Static value Application for CreatedByType, represents an AAD application. */
     public static final CreatedByType APPLICATION = fromString("Application");
 
-    /** Static value ManagedIdentity for CreatedByType, a Managed Identity. */
+    /** Static value ManagedIdentity for CreatedByType, represents a Managed Identity. */
     public static final CreatedByType MANAGED_IDENTITY = fromString("ManagedIdentity");
 
     /** Static value Key for CreatedByType. */
