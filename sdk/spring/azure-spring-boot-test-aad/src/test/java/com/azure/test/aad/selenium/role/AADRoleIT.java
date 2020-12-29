f2cb8f1a940469d7575f511cb23ccc894348a8eb
Rujun Chen <rujche@microsoft.com>
2020-12-29 09:13:19 +0800

    Change package name for AADRoleIT.

--- sdk/spring/azure-spring-boot-test-aad/src/test/java/com/azure/test/aad/selenium/role/AADRoleIT.java
+++ sdk/spring/azure-spring-boot-test-aad/src/test/java/com/azure/test/aad/selenium/role/AADRoleIT.java
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.azure.test.aad.selenium.login;
+package com.azure.test.aad.selenium.role;
 
 import com.azure.test.aad.selenium.AADSeleniumITHelper;
 import org.junit.Assert;
