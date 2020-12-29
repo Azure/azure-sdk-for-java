faeb4d7edda0c81f3bb4102a497d772021e8c7c2
ZhuXiaoBing-cn <zhu13865245671@163.com>
2020-12-29 17:26:32 +0800

    Update the resource-server-obo README.md file and modify part of the code.

--- sdk/spring/azure-spring-boot-samples/azure-spring-boot-sample-active-directory-resource-server-obo/src/main/java/com/azure/spring/sample/aad/controller/SampleController.java
+++ sdk/spring/azure-spring-boot-samples/azure-spring-boot-sample-active-directory-resource-server-obo/src/main/java/com/azure/spring/sample/aad/controller/SampleController.java
@@ -57,23 +57,21 @@ public class SampleController {
      * @return Response with graph data
      */
     @GetMapping("call-graph-only-with-annotation")
-    @PreAuthorize("hasAuthority('SCOPE_Obo.GraphAndObo.File.Read')")
+    @PreAuthorize("hasAuthority('SCOPE_Obo.Graph.Read')")
     public String callGraphOnlyWithAnnotation(@RegisteredOAuth2AuthorizedClient("graph") OAuth2AuthorizedClient graph) {
         return callMicrosoftGraphMeEndpoint(graph);
     }
 
     /**
-     * Call the graph and custom(local) resources, combine all the response and return.
-     * @param graph authorized client for Graph
+     * Call custom resources, combine all the response and return.
      * @param custom authorized client for Custom
      * @return Response Graph and Custom data.
      */
+    @GetMapping("call-custom-resources")
     @PreAuthorize("hasAuthority('SCOPE_Obo.File.Read')")
-    @GetMapping("call-graph-and-custom-resources")
     public String callGraphAndCustomResources(
-        @RegisteredOAuth2AuthorizedClient("graph") OAuth2AuthorizedClient graph,
         @RegisteredOAuth2AuthorizedClient("custom") OAuth2AuthorizedClient custom) {
-        return callMicrosoftGraphMeEndpoint(graph) + " " + callCustomLocalFileEndpoint(custom);
+        return callCustomLocalFileEndpoint(custom);
     }
 
     /**
@@ -106,7 +104,7 @@ public class SampleController {
             .retrieve()
             .bodyToMono(String.class)
             .block();
-        LOGGER.info("Response from Custom(local): {}", body);
-        return "Web API B response " + (null != body ? "success." : "failed.");
+        LOGGER.info("Response from Custom: {}", body);
+        return "Custom response " + (null != body ? "success." : "failed.");
     }
 }
