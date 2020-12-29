22aa07f799d07a885c15e7bda917ef5329d96f40
Rujun Chen <Rujun.Chen@microsoft.com>
2020-12-29 15:49:40 +0800

    Make allowed groups optional (#18373)
    * Make it valid if "azure.activedirectory.user-group.user-group" is not configured.
    
    * Add additional scope "https://graph.microsoft.com/User.Read" when：
    1. authorizationCodeScopes have more than one resource server.
    2. accessTokenScopes have no resource server

--- sdk/spring/azure-spring-boot/src/main/java/com/azure/spring/aad/AADAuthorizationServerEndpoints.java
+++ sdk/spring/azure-spring-boot/src/main/java/com/azure/spring/aad/AADAuthorizationServerEndpoints.java
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.azure.spring.aad.webapp;
+package com.azure.spring.aad;
 
 import com.nimbusds.oauth2.sdk.util.StringUtils;
 
--- sdk/spring/azure-spring-boot/src/main/java/com/azure/spring/autoconfigure/aad/AADAuthenticationFilter.java
+++ sdk/spring/azure-spring-boot/src/main/java/com/azure/spring/autoconfigure/aad/AADAuthenticationFilter.java
@@ -3,7 +3,7 @@
 
 package com.azure.spring.autoconfigure.aad;
 
-import com.azure.spring.aad.webapp.AADAuthorizationServerEndpoints;
+import com.azure.spring.aad.AADAuthorizationServerEndpoints;
 import com.microsoft.aad.msal4j.MsalServiceException;
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.jwk.source.JWKSetCache;
