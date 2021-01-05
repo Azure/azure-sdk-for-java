3298216868a4386a62704269103c82e16ca3eb3d
Tanyi Chen <tanchen@microsoft.com>
2021-01-05 15:28:07 +0800

    use singletonMap

--- sdk/resourcemanager/azure-resourcemanager-authorization/src/main/java/com/azure/resourcemanager/authorization/implementation/ActiveDirectoryGroupImpl.java
+++ sdk/resourcemanager/azure-resourcemanager-authorization/src/main/java/com/azure/resourcemanager/authorization/implementation/ActiveDirectoryGroupImpl.java
@@ -24,7 +24,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
@@ -180,10 +180,9 @@ class ActiveDirectoryGroupImpl
     public ActiveDirectoryGroupImpl withMember(String objectId) {
         // https://docs.microsoft.com/en-us/graph/api/group-post-members
         String membersKey = "@odata.id";
-        membersToAdd.add(new HashMap<String, Object>() {{
-                put(membersKey,
-                    String.format("%s/directoryObjects/%s", manager().serviceClient().getEndpoint(), objectId));
-            }});
+        membersToAdd.add(
+            Collections.singletonMap(membersKey,
+                String.format("%s/directoryObjects/%s", manager().serviceClient().getEndpoint(), objectId)));
         return this;
     }
 
