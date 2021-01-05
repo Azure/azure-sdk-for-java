3d2eaa8e5dead6e1fb576ca88df3e7a97c2caafd
Tanyi Chen <tanchen@microsoft.com>
2021-01-05 11:04:46 +0800

    fix checkstyle

--- sdk/resourcemanager/azure-resourcemanager-authorization/src/main/java/com/azure/resourcemanager/authorization/implementation/ActiveDirectoryGroupImpl.java
+++ sdk/resourcemanager/azure-resourcemanager-authorization/src/main/java/com/azure/resourcemanager/authorization/implementation/ActiveDirectoryGroupImpl.java
@@ -177,7 +177,8 @@ class ActiveDirectoryGroupImpl
         // https://docs.microsoft.com/en-us/graph/api/group-post-members
         String membersKey = "@odata.id";
         membersToAdd.add(new HashMap<String, Object>() {{
-                put(membersKey, String.format("%s/directoryObjects/%s", manager().serviceClient().getEndpoint(), objectId));
+                put(membersKey,
+                    String.format("%s/directoryObjects/%s", manager().serviceClient().getEndpoint(), objectId));
             }});
         return this;
     }
