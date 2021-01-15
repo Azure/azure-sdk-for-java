5a8a075d9ee555ebb4f8aa687a141e8aa08b0e69
Max Vollmer <mavollme@microsoft.com>
2021-01-15 12:31:24 +0000

    Initial commit: * Generated java source files from swagger * Simple maven project

---
+++ sdk/mixedreality/remoterendering/azure-mixedreality-remoterendering/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+module com.azure.mixedreality.remoterendering {
+    requires com.azure.core;
+    exports com.azure.mixedreality.remoterendering;
+}
