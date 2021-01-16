eec855b806ee4ff1948d287e30b5041c0053b270
Minnie Liu <peiliu@microsoft.com>
2021-01-15 17:48:09 -0800

    Add java doc

--- sdk/communication/azure-communication-administration/src/main/java/com/azure/communication/administration/models/CommunicationUserIdentifierWithTokenResult.java
+++ sdk/communication/azure-communication-administration/src/main/java/com/azure/communication/administration/models/CommunicationUserIdentifierWithTokenResult.java
@@ -10,6 +10,13 @@ public final class CommunicationUserIdentifierWithTokenResult {
     private final CommunicationUserIdentifier communicationUser;
     private final CommunicationUserToken userToken;
 
+    /**
+     * Creates a CommunicationUserIdentifierWithTokenResult object
+     * 
+     * @param communicationUser the communication user identifier
+     * @param userToken the user token of the communication user
+     * @throws IllegalArgumentException thrown if id parameter fail the validation.
+     */
     public CommunicationUserIdentifierWithTokenResult(CommunicationUserIdentifier communicationUser, CommunicationUserToken userToken)
     {
         this.communicationUser = communicationUser;
