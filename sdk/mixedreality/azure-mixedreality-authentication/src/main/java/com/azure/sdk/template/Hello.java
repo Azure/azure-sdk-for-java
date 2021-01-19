1874b43e4e1888f8e183da5bea83cbae5d2e1098
Craig Treasure <crtreasu@microsoft.com>
2021-01-18 16:10:50 -0800

    Added the Mixed Reality Authentication library

---
+++ sdk/mixedreality/azure-mixedreality-authentication/src/main/java/com/azure/sdk/template/Hello.java
@@ -0,0 +1,16 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+package com.azure.sdk.template;
+
+/**
+ * This is a sample class for template app.
+ */
+public class Hello {
+    /**
+     * Sample method.
+     * @return message
+     */
+    public final String getMessage() {
+        return "hello";
+    }
+}
