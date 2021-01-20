582119083668c9af28c33c2ed81e3b0aa160b204
Craig Treasure <crtreasu@microsoft.com>
2021-01-19 17:07:45 -0800

    Added public API structure

---
+++ sdk/mixedreality/azure-mixedreality-authentication/src/main/java/com/azure/mixedreality/authentication/MixedRealityAccountKeyCredential.java
@@ -0,0 +1,30 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.mixedreality.authentication;
+
+import com.azure.core.credential.AccessToken;
+import com.azure.core.credential.AzureKeyCredential;
+import com.azure.core.credential.TokenCredential;
+import com.azure.core.credential.TokenRequestContext;
+import reactor.core.publisher.Mono;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+class MixedRealityAccountKeyCredential implements TokenCredential {
+    private final UUID accountId;
+    private final AzureKeyCredential keyCredential;
+
+    public MixedRealityAccountKeyCredential(UUID accountId, AzureKeyCredential keyCredential) {
+        this.accountId = accountId;
+        this.keyCredential = keyCredential;
+    }
+
+    @Override
+    public Mono<AccessToken> getToken(TokenRequestContext tokenRequestContext) {
+        AccessToken result = new AccessToken(String.format("%s:%s", this.accountId.toString(), this.keyCredential.getKey()), OffsetDateTime.MAX);
+
+        return Mono.just(result);
+    }
+}
