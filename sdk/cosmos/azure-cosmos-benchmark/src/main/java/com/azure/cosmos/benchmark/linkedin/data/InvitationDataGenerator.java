1a1462a37c494b4ef5060106e8e9eadb24cb419e
amar.athavale <amar.athavale@gmail.com>
2021-01-19 19:55:15 -0800

    Adding copyright header

--- sdk/cosmos/azure-cosmos-benchmark/src/main/java/com/azure/cosmos/benchmark/linkedin/data/InvitationDataGenerator.java
+++ sdk/cosmos/azure-cosmos-benchmark/src/main/java/com/azure/cosmos/benchmark/linkedin/data/InvitationDataGenerator.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.azure.cosmos.benchmark.linkedin.data;
 
 import com.azure.cosmos.benchmark.linkedin.impl.Constants;
--- sdk/cosmos/azure-cosmos-benchmark/src/main/java/com/azure/cosmos/benchmark/linkedin/impl/models/Result.java
+++ sdk/cosmos/azure-cosmos-benchmark/src/main/java/com/azure/cosmos/benchmark/linkedin/impl/models/Result.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.azure.cosmos.benchmark.linkedin.impl.models;
 
 import com.google.common.base.Preconditions;
