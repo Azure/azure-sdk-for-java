7f920d004b7561ed5b932311291a9ae47e3435a4
Jianghao Lu <jianghaolu@users.noreply.github.com>
2021-01-20 12:14:40 -0800

    Remove synapse stuff

--- sdk/synapse/azure-analytics-synapse-spark/src/test/java/com/azure/analytics/synapse/spark/SparkBatchClientTest.java
+++ sdk/synapse/azure-analytics-synapse-spark/src/test/java/com/azure/analytics/synapse/spark/SparkBatchClientTest.java
@@ -4,15 +4,10 @@
 package com.azure.analytics.synapse.spark;
 
 import com.azure.analytics.synapse.spark.models.SparkBatchJob;
-import com.azure.analytics.synapse.spark.models.SparkBatchJobOptions;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 public class SparkBatchClientTest extends SparkClientTestBase {
+
     private SparkBatchClient client;
 
     @Override
@@ -24,54 +19,14 @@ public class SparkBatchClientTest extends SparkClientTestBase {
             .buildSparkBatchClient());
     }
 
+    /**
+     * Tests that Spark jobs can be listed in the Spark pool.
+     */
     @Test
     public void getSparkBatchJob() {
         for (SparkBatchJob expectedSparkJob : client.getSparkBatchJobs().getSessions()) {
             SparkBatchJob actualSparkJob = client.getSparkBatchJob(expectedSparkJob.getId());
-            assertSparkJobEquals(expectedSparkJob, actualSparkJob);
-        }
-    }
-
-    @Test
-    public void crudSparkBatchJob() {
-        // arrange
-        String jobName = testResourceNamer.randomName("spark-job-", 20);
-        String file = String.format("abfss://%s@%s.dfs.core.windows.net/wordcount.jar", getStorageContainerName(), getStorageAccountName());
-        SparkBatchJobOptions options = new SparkBatchJobOptions()
-            .setName(jobName)
-            .setFile(file)
-            .setClassName("WordCount")
-            .setArguments(Arrays.asList(
-                String.format("abfss://%s@%s.dfs.core.windows.net/shakespeare.txt", getStorageContainerName(), getStorageAccountName()),
-                String.format("abfss://%s@%s.dfs.core.windows.net/java-result/", getStorageContainerName(), getStorageAccountName())
-            ))
-            .setDriverMemory("28g")
-            .setDriverCores(4)
-            .setExecutorMemory("28g")
-            .setExecutorCores(4)
-            .setExecutorCount(2);
-
-        // act
-        SparkBatchJob expected = null;
-
-        try {
-            expected = client.createSparkBatchJob(options, true);
-
-            // assert
-            assertNotNull(expected);
-            assertEquals(jobName, expected.getName());
-            assertEquals(getSparkPoolName(), expected.getSparkPoolName());
-
-            // act
-            SparkBatchJob actual = client.getSparkBatchJob(expected.getId(), true);
-
-            // assert
-            assertSparkJobEquals(expected, actual);
-        } finally {
-            // clean up
-            if (expected != null) {
-                client.cancelSparkBatchJob(expected.getId());
-            }
+            validateSparkBatchJob(expectedSparkJob, actualSparkJob);
         }
     }
 }
