b37be5d6602a9b29cfb6122194ae5868eaf20abf
Mike Harder <mharder@microsoft.com>
2021-01-21 12:08:23 -0800

    Suppress spotbugs for azure-template-perf

--- sdk/template/azure-template-perf/src/main/java/com/azure/sdk/template/perf/GetMessageTest.java
+++ sdk/template/azure-template-perf/src/main/java/com/azure/sdk/template/perf/GetMessageTest.java
@@ -19,7 +19,7 @@ public class GetMessageTest extends PerfStressTest<PerfStressOptions> {
 
     @Override
     public void run() {
-        String message = hello.getMessage();
+        hello.getMessage();
     }
 
     @Override
