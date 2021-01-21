e114af30dc38573c605dad0fbf06425d7fc80fc1
Daniel Gerlag <daniel@gerlag.ca>
2021-01-21 10:34:40 -0800

    hide implementation

--- sdk/communication/azure-communication-chat/src/main/java/com/azure/communication/chat/models/ChatMessageContent.java
+++ sdk/communication/azure-communication-chat/src/main/java/com/azure/communication/chat/models/ChatMessageContent.java
@@ -4,7 +4,6 @@
 
 package com.azure.communication.chat.models;
 
-import com.azure.communication.chat.implementation.models.ChatParticipant;
 import com.azure.core.annotation.Fluent;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
