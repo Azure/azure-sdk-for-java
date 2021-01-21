25afcdee46b799fb16012a29871f44b5421b62de
Rick Ley <frley@microsoft.com>
2021-01-21 10:40:35 -0800

    Nio output stream open options (#18735)
    * Updated the flags
    
    * Added tests
    
    * Changelog

--- sdk/storage/azure-storage-blob-nio/CHANGELOG.md
+++ sdk/storage/azure-storage-blob-nio/CHANGELOG.md
@@ -3,6 +3,7 @@
 ## 12.0.0-beta.3 (Unreleased)
 - Added support for FileSystemProvider.checkAccess method
 - Added support for file key on AzureBasicFileAttributes and AzureBlobFileAttributes
+- Adjusted the required flags for opening an outputstream
 
 ## 12.0.0-beta.2 (2020-08-13)
 - Added checks to ensure file system has not been closed before operating on data
--- sdk/storage/azure-storage-blob-nio/src/test/java/com/azure/storage/blob/nio/CompositeTest.groovy
+++ sdk/storage/azure-storage-blob-nio/src/test/java/com/azure/storage/blob/nio/CompositeTest.groovy
@@ -1,6 +1,7 @@
 package com.azure.storage.blob.nio
 
 import java.nio.file.Files
+import java.nio.file.StandardCopyOption
 
 /**
  * This test class is for testing static helper methods provided by the JDK. Customers often rely on these methods
@@ -26,5 +27,39 @@ class CompositeTest extends APISpec {
         Files.isDirectory(fs.getPath('mydir1'))
         Files.isDirectory(fs.getPath('mydir1/mydir2'))
         Files.isDirectory(fs.getPath('mydir1/mydir2/mydir3'))
+    }
+
+    def "Files copy"() {
+        setup:
+        def fs = createFS(config)
+        def dest = fs.getPath("dest")
+        def resultArr = new byte[defaultDataSize]
+
+        when:
+        Files.copy(defaultInputStream.get(), dest)
+        fs.provider().newInputStream(dest).read(resultArr)
+
+        then:
+        resultArr == defaultData.array()
+
+        when:
+        def dest2 = fs.getPath("dest2")
+        def outStream = fs.provider().newOutputStream(dest2)
+        Files.copy(dest, outStream)
+        outStream.close()
+        resultArr = new byte[defaultDataSize]
+        fs.provider().newInputStream(dest2).read(resultArr)
+
+        then:
+        resultArr == defaultData.array()
+
+        when:
+        def dest3 = fs.getPath("dest3")
+        Files.copy(dest, dest3, StandardCopyOption.COPY_ATTRIBUTES)
+        resultArr = new byte[defaultDataSize]
+        fs.provider().newInputStream(dest3).read(resultArr)
+
+        then:
+        resultArr == defaultData.array()
     }
 }
