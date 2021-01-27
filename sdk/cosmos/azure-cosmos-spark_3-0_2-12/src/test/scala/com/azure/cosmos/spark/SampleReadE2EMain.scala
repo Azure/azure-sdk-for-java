cb09c73ed2d6d8f61deeac18108979cbe79ebc4a
Matias Quaranta <ealsur@hotmail.com>
2021-01-27 08:58:07 -0800

    Updating test

--- sdk/cosmos/azure-cosmos-spark_3-0_2-12/src/test/scala/com/azure/cosmos/spark/SampleReadE2EMain.scala
+++ sdk/cosmos/azure-cosmos-spark_3-0_2-12/src/test/scala/com/azure/cosmos/spark/SampleReadE2EMain.scala
@@ -28,7 +28,8 @@ object SampleReadE2EMain {
     val cfg = Map("spark.cosmos.accountEndpoint" -> cosmosEndpoint,
       "spark.cosmos.accountKey" -> cosmosMasterKey,
       "spark.cosmos.database" -> cosmosDatabase,
-      "spark.cosmos.container" -> cosmosContainer
+      "spark.cosmos.container" -> cosmosContainer,
+      "spark.cosmos.read.inferSchemaEnabled" -> "true"
     )
 
     val spark = SparkSession.builder()
