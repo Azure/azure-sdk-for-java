2595a4bebdbac99d4d0fa47d55d4280707a37495
Connie <conniey@microsoft.com>
2021-01-28 15:47:54 -0800

    Adding opens for testing and cleaning up imports.

--- sdk/core/azure-core-amqp/src/test/java/com/azure/core/amqp/AmqpRetryOptionsTest.java
+++ sdk/core/azure-core-amqp/src/test/java/com/azure/core/amqp/AmqpRetryOptionsTest.java
@@ -6,9 +6,7 @@ package com.azure.core.amqp;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.Duration;
 import java.util.stream.Stream;
