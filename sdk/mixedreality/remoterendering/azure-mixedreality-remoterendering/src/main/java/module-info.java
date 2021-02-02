20d5251017d57e263e13cb8e65b98f983778eee2
Malcolm Tyrrell <matyrr@microsoft.com>
2021-02-02 13:12:47 +0000

    module-info fixes

--- sdk/mixedreality/remoterendering/azure-mixedreality-remoterendering/src/main/java/module-info.java
+++ sdk/mixedreality/remoterendering/azure-mixedreality-remoterendering/src/main/java/module-info.java
@@ -2,9 +2,11 @@
 // Licensed under the MIT License.
 
 module com.azure.mixedreality.remoterendering {
-    requires com.azure.core;
-    requires azure.mixedreality.authentication;
-    opens com.azure.mixedreality.remoterendering.implementation.models;
+    requires transitive com.azure.core;
+    requires com.azure.mixedreality.authentication;
+
+    opens com.azure.mixedreality.remoterendering.implementation.models to com.fasterxml.jackson.databind;
+
     exports com.azure.mixedreality.remoterendering;
     exports com.azure.mixedreality.remoterendering.models;
 }
