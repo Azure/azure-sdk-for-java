e42fc62740109497d1069e2b2ae1c168d9fea058
zhihaoguo <zhihaoguo@microsoft.com>
2021-02-05 13:02:30 +0800

    remove FunctionalInterface

--- sdk/spring/azure-spring-boot-test-selenium-common/src/main/java/com/azure/test/aad/common/JarUtil.java
+++ sdk/spring/azure-spring-boot-test-selenium-common/src/main/java/com/azure/test/aad/common/JarUtil.java
@@ -3,22 +3,6 @@
 
 package com.azure.test.aad.common;
 
-/*******************************************************************************
- *     Copyright (C) 2017 wysohn
- *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *******************************************************************************/
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -34,27 +18,23 @@ public class JarUtil {
     public static final char JAR_SEPARATOR = '/';
 
     public static void copyFolderFromJar(String folderName, File destFolder, CopyOption option) throws IOException {
-        copyFolderFromJar(folderName, destFolder, option, null);
-    }
-
-    public static void copyFolderFromJar(String folderName, File destFolder, CopyOption option, PathTrimmer trimmer) throws IOException {
         if (!destFolder.exists())
             destFolder.mkdirs();
 
         byte[] buffer = new byte[1024];
 
         File fullPath = null;
-        String path = JarUtil.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-        if (trimmer != null)
-            path = trimmer.trim(path);
-        try {
-            if (!path.startsWith("file"))
-                path = "file://" + path;
+        String path = JarUtil.class.getProtectionDomain().getCodeSource().getLocation().getPath().trim();
+        if (!path.startsWith("file")) {
+            path = "file://" + path;
+        }
 
+        try {
             fullPath = new File(new URI(path));
         } catch (URISyntaxException e) {
             e.printStackTrace();
         }
+
         ZipInputStream zis = new ZipInputStream(new FileInputStream(fullPath));
 
         ZipEntry entry;
@@ -99,11 +79,6 @@ public class JarUtil {
 
     public enum CopyOption {
         COPY_IF_NOT_EXIST, REPLACE_IF_EXIST;
-    }
-
-    @FunctionalInterface
-    public interface PathTrimmer {
-        String trim(String original);
     }
 
 }
