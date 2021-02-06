0b31a8ca791724ef3d6aafd3f966c0bc5feeddfe
Mohammad Derakhshani <moderakh@users.noreply.github.com>
2021-02-05 19:21:26 -0800

    ensure classes loaded

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/ImplementationBridgeHelpers.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/ImplementationBridgeHelpers.java
@@ -6,16 +6,22 @@ package com.azure.cosmos.implementation;
 import com.azure.cosmos.CosmosClientBuilder;
 import com.azure.cosmos.implementation.routing.PartitionKeyInternal;
 import com.azure.cosmos.models.PartitionKey;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ImplementationBridgeHelpers {
+    private static Logger logger = LoggerFactory.getLogger(ImplementationBridgeHelpers.class);
     public static final class CosmosClientBuilderHelper {
         private static CosmosClientBuilderAccessor accessor;
 
         private CosmosClientBuilderHelper() {}
+        static {
+            ensureClassLoaded(CosmosClientBuilder.class);
+        }
 
         public static void setCosmosClientBuilderAccessor(final CosmosClientBuilderAccessor newAccessor) {
             if (accessor != null) {
-                throw new IllegalStateException("accessor already initialized!");
+                throw new IllegalStateException("CosmosClientBuilderHelper accessor already initialized!");
             }
 
             accessor = newAccessor;
@@ -23,7 +29,7 @@ public class ImplementationBridgeHelpers {
 
         static CosmosClientBuilderAccessor getCosmosClientBuilderAccessor() {
             if (accessor == null) {
-                throw new IllegalStateException("accessor is not initialized yet!");
+                throw new IllegalStateException("CosmosClientBuilderHelper accessor is not initialized yet!");
             }
 
             return accessor;
@@ -38,13 +44,16 @@ public class ImplementationBridgeHelpers {
     }
 
     public static final class PartitionKeyHelper {
+        static {
+            ensureClassLoaded(PartitionKey.class);
+        }
         private static PartitionKeyAccessor accessor;
 
         private PartitionKeyHelper() {}
 
         public static void setPartitionKeyAccessor(final PartitionKeyAccessor newAccessor) {
             if (accessor != null) {
-                throw new IllegalStateException("accessor already initialized!");
+                throw new IllegalStateException("PartitionKeyHelper accessor already initialized!");
             }
 
             accessor = newAccessor;
@@ -52,7 +61,7 @@ public class ImplementationBridgeHelpers {
 
         public static PartitionKeyAccessor getPartitionKeyAccessor() {
             if (accessor == null) {
-                throw new IllegalStateException("accessor is not initialized yet!");
+                throw new IllegalStateException("PartitionKeyHelper accessor already initialized!");
             }
 
             return accessor;
@@ -60,6 +69,16 @@ public class ImplementationBridgeHelpers {
 
         public interface PartitionKeyAccessor {
             PartitionKey toPartitionKey(PartitionKeyInternal partitionKeyInternal);
+        }
+    }
+
+    private static <T> void ensureClassLoaded(Class<T> classType) {
+        try {
+            // ensures the class is loaded
+            Class.forName(classType.getName());
+        } catch (ClassNotFoundException e) {
+            logger.error("cannot load class {}", classType.getName());
+            throw new RuntimeException(e);
         }
     }
 }
