300250423a4e954670b954fb3ea5bbd8ee73e0dd
Yijun Xie <48257664+YijunXieMS@users.noreply.github.com>
2021-02-08 11:17:56 -0800

    Update doc

--- sdk/eventgrid/azure-messaging-eventgrid-perf/src/main/java/com/azure/messaging/eventgrid/perf/App.java
+++ sdk/eventgrid/azure-messaging-eventgrid-perf/src/main/java/com/azure/messaging/eventgrid/perf/App.java
@@ -9,7 +9,7 @@ import com.azure.perf.test.core.PerfStressProgram;
  * Runs the Event Grid performance test.
  *
  * <p>To run from command line. Package the project into a jar with dependencies via mvn clean package.
- * Then run the program via java -jar 'compiled-jar-with-dependencies-path' </p>
+ * Then run the program with 'java -jar azure-messaging-eventgrid-perf-1.0.0-beta.1-jar-with-dependencies.jar' </p>
  *
  * <p> To run from IDE, set all the required environment variables in IntelliJ via Run -&gt; EditConfigurations section.
  * Then run the App's main method via IDE.</p>
