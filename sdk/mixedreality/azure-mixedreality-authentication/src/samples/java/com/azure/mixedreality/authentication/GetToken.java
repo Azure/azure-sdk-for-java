7314f9385dacecc57b9d26832cb74d77607f7618
Craig Treasure <crtreasu@microsoft.com>
2021-02-11 12:07:12 -0800

    Added javadoc to the sample

--- sdk/mixedreality/azure-mixedreality-authentication/src/samples/java/com/azure/mixedreality/authentication/GetToken.java
+++ sdk/mixedreality/azure-mixedreality-authentication/src/samples/java/com/azure/mixedreality/authentication/GetToken.java
@@ -6,8 +6,19 @@ package com.azure.mixedreality.authentication;
 import com.azure.core.credential.AccessToken;
 import com.azure.core.credential.AzureKeyCredential;
 
+/**
+ * Sample demonstrates how to get an access token from the Mixed Reality security
+ * token service (STS).
+ */
 public final class GetToken {
+    /**
+     * Runs the sample and demonstrates to get an access token from the Mixed
+     * Reality security token service (STS).
+     * @param args Unused. Arguments to the program.
+     */
     public static void main(String[] args) {
+        // You can get your account domain, Id, and key by viewing your Mixed
+        // Reality resource in the Azure portal.
         final String accountDomain = "";
         final String accountId = "00000000-0000-0000-0000-000000000000";
         final String accountKey = "";
