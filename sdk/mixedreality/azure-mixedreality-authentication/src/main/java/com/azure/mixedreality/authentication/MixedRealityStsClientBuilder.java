9073c4f683c4e9398ea26a8cf3e03945cd03636d
Craig Treasure <crtreasu@microsoft.com>
2021-02-11 12:30:49 -0800

    Fixed formatting

--- sdk/mixedreality/azure-mixedreality-authentication/src/main/java/com/azure/mixedreality/authentication/MixedRealityStsClientBuilder.java
+++ sdk/mixedreality/azure-mixedreality-authentication/src/main/java/com/azure/mixedreality/authentication/MixedRealityStsClientBuilder.java
@@ -174,13 +174,13 @@ public final class MixedRealityStsClientBuilder {
             this.pipeline = createHttpPipeline(this.httpClient, authPolicy, this.customPolicies);
         }
 
-         MixedRealityStsServiceVersion version;
+        MixedRealityStsServiceVersion version;
 
-         if (this.apiVersion != null) {
-             version = this.apiVersion;
-         } else {
-             version = MixedRealityStsServiceVersion.getLatest();
-         }
+        if (this.apiVersion != null) {
+            version = this.apiVersion;
+        } else {
+            version = MixedRealityStsServiceVersion.getLatest();
+        }
 
         MixedRealityStsRestClientImpl serviceClient = new MixedRealityStsRestClientImplBuilder()
             .apiVersion(version.getVersion())
