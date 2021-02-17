127045224ce36345d99f0d6edd072df9b44a06f9
minnieliu <minnieliu96@hotmail.com>
2021-02-16 16:57:53 -0800

    [Communication] - Identity - Rename IssueToken to GetToken (#19271)
    * Rename IssueToken to GetToken
    
    * Update chat tests and CHANGELOG
    
    * Add classnames
    
    * Rename token to initialTOken
    
    * Rename test files
    
    Co-authored-by: Minnie Liu <peiliu@microsoft.com>

--- sdk/communication/azure-communication-chat/src/test/java/com/azure/communication/chat/ChatClientTest.java
+++ sdk/communication/azure-communication-chat/src/test/java/com/azure/communication/chat/ChatClientTest.java
@@ -54,7 +54,7 @@ public class ChatClientTest extends ChatClientTestBase {
         secondThreadMember = communicationClient.createUser();
 
         List<CommunicationTokenScope> scopes = Arrays.asList(CommunicationTokenScope.CHAT);
-        AccessToken response = communicationClient.issueToken(firstThreadMember, scopes);
+        AccessToken response = communicationClient.getToken(firstThreadMember, scopes);
 
         ChatClientBuilder chatBuilder = getChatClientBuilder(response.getToken(), httpClient);
         client = addLoggingPolicyForIdentityClientBuilder(chatBuilder, testName).buildClient();
