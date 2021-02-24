2dd27611ee7ef3287a1bc6f66e4d9df04e8a19f0
ZhuXiaoBing <zhu13865245671@163.com>
2021-02-24 14:48:09 +0800

    For AAD resource-server, create grantedAuthority by both "roles" and "claims" by default.

--- sdk/spring/azure-spring-boot/src/main/java/com/azure/spring/aad/webapi/AADJwtBearerTokenAuthenticationConverter.java
+++ sdk/spring/azure-spring-boot/src/main/java/com/azure/spring/aad/webapi/AADJwtBearerTokenAuthenticationConverter.java
@@ -2,41 +2,22 @@
 // Licensed under the MIT License.
 package com.azure.spring.aad.webapi;
 
-import java.util.Collection;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthentication;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
-import org.springframework.util.Assert;
+
+import java.util.Collection;
 
 /**
  * A {@link Converter} that takes a {@link Jwt} and converts it into a {@link BearerTokenAuthentication}.
  */
 public class AADJwtBearerTokenAuthenticationConverter implements Converter<Jwt, AbstractAuthenticationToken> {
 
-    private static final String DEFAULT_AUTHORITY_PREFIX = "SCOPE_";
-
     private Converter<Jwt, Collection<GrantedAuthority>> converter
-        = new JwtGrantedAuthoritiesConverter();
-
-    public AADJwtBearerTokenAuthenticationConverter() {
-    }
-
-    public AADJwtBearerTokenAuthenticationConverter(String authoritiesClaimName) {
-        this(authoritiesClaimName, DEFAULT_AUTHORITY_PREFIX);
-    }
-
-    public AADJwtBearerTokenAuthenticationConverter(String authoritiesClaimName, String authorityPrefix) {
-        Assert.notNull(authoritiesClaimName, "authoritiesClaimName cannot be null");
-        Assert.notNull(authorityPrefix, "authorityPrefix cannot be null");
-        JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
-        jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName(authoritiesClaimName);
-        jwtGrantedAuthoritiesConverter.setAuthorityPrefix(authorityPrefix);
-        this.converter = jwtGrantedAuthoritiesConverter;
-    }
+        = new AADJwtGrantedAuthoritiesConverter();
 
     protected Collection<GrantedAuthority> extractAuthorities(Jwt jwt) {
         return this.converter.convert(jwt);
---
+++ sdk/spring/azure-spring-boot/src/main/java/com/azure/spring/aad/webapi/AADJwtGrantedAuthoritiesConverter.java
@@ -0,0 +1,61 @@
+package com.azure.spring.aad.webapi;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Extracts the {@link GrantedAuthority}s from scope attributes typically found in a {@link Jwt}.
+ *
+ * @author Eric Deandrea
+ * @since 5.2
+ */
+public class AADJwtGrantedAuthoritiesConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+
+    private static final String DEFAULT_SCP_AUTHORITY_PREFIX = "SCOPE_";
+
+    private static final String DEFAULT_ROLES_AUTHORITY_PREFIX = "APPROLE_";
+
+    private static final Collection<String> WELL_KNOWN_AUTHORITIES_CLAIM_NAMES = Arrays.asList("scp", "roles");
+
+    @Override
+    public Collection<GrantedAuthority> convert(Jwt jwt) {
+        Collection<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        for (String authority : getAuthorities(jwt)) {
+            grantedAuthorities.add(new SimpleGrantedAuthority(authority));
+        }
+        return grantedAuthorities;
+    }
+
+    private Collection<String> getAuthorities(Jwt jwt) {
+        List<String> authoritiesList = new ArrayList();
+        for (String claimName : WELL_KNOWN_AUTHORITIES_CLAIM_NAMES) {
+            if (jwt.containsClaim(claimName)) {
+                Object authorities = jwt.getClaim(claimName);
+                if (authorities instanceof String) {
+                    if (StringUtils.hasText((String) authorities)) {
+                        authoritiesList.addAll(Arrays.asList(((String) authorities).split(" "))
+                                                     .stream()
+                                                     .map(s -> DEFAULT_SCP_AUTHORITY_PREFIX + s)
+                                                     .collect(Collectors.toList()));
+                    }
+                } else if (authorities instanceof Collection) {
+                    authoritiesList.addAll(((Collection<?>) authorities)
+                        .stream()
+                        .filter(s -> StringUtils.hasText((String) s))
+                        .map(s -> DEFAULT_ROLES_AUTHORITY_PREFIX + s)
+                        .collect(Collectors.toList()));
+                }
+            }
+        }
+        return authoritiesList;
+    }
+}
