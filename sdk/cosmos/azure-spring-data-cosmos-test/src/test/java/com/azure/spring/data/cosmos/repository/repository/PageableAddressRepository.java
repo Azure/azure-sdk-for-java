2997020b073905291a9d28c28f464cefbe26b6fe
Michael Lueders <Mike.Lueders@blackbaud.com>
2021-02-25 03:22:13 -0600

    eliminate duplicate class PageableAddress

--- sdk/cosmos/azure-spring-data-cosmos-test/src/test/java/com/azure/spring/data/cosmos/repository/repository/PageableAddressRepository.java
+++ sdk/cosmos/azure-spring-data-cosmos-test/src/test/java/com/azure/spring/data/cosmos/repository/repository/PageableAddressRepository.java
@@ -2,15 +2,15 @@
 // Licensed under the MIT License.
 package com.azure.spring.data.cosmos.repository.repository;
 
-import com.azure.spring.data.cosmos.domain.PageableAddress;
+import com.azure.spring.data.cosmos.domain.Address;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PageableAddressRepository extends PagingAndSortingRepository<PageableAddress, String> {
-    Page<PageableAddress> findByStreet(String street, Pageable pageable);
+public interface PageableAddressRepository extends PagingAndSortingRepository<Address, String> {
+    Page<Address> findByStreet(String street, Pageable pageable);
 
-    Page<PageableAddress> findByCity(String city, Pageable pageable);
+    Page<Address> findByCity(String city, Pageable pageable);
 }
