cb09813d63ab7eb01889b25c01e6bd90af1b17af
Kamil Sobol <61715331+kasobol-msft@users.noreply.github.com>
2021-02-24 17:21:21 -0800

    [Storage] Queue message encoding (#19328)
    * settings.
    
    * internal model.
    
    * wip.
    
    * decoding.
    
    * encode message.
    
    * enqueue/dequeue tests.
    
    * recording.
    
    * undo models.
    
    * more tests.
    
    * handler tests.
    
    * bundle failure args.
    
    * checkstyle.
    
    * checkstyle.
    
    * readme.
    
    * expose queue client.
    
    * handler sample.
    
    * CHANGELOG.md
    
    * fix npe.
    
    * some of pr feedback.
    
    * one more test.
    
    * two handlers.
    
    * pr feedback.
    
    * pr feedback.

--- sdk/storage/azure-storage-queue/src/main/java/com/azure/storage/queue/implementation/models/MessagesPeekResponse.java
+++ sdk/storage/azure-storage-queue/src/main/java/com/azure/storage/queue/implementation/models/MessagesPeekResponse.java
@@ -7,11 +7,10 @@ package com.azure.storage.queue.implementation.models;
 import com.azure.core.http.HttpHeaders;
 import com.azure.core.http.HttpRequest;
 import com.azure.core.http.rest.ResponseBase;
-import com.azure.storage.queue.models.PeekedMessageItem;
 import java.util.List;
 
 /** Contains all response data for the peek operation. */
-public final class MessagesPeekResponse extends ResponseBase<MessagesPeekHeaders, List<PeekedMessageItem>> {
+public final class MessagesPeekResponse extends ResponseBase<MessagesPeekHeaders, List<PeekedMessageItemInternal>> {
     /**
      * Creates an instance of MessagesPeekResponse.
      *
@@ -25,14 +24,14 @@ public final class MessagesPeekResponse extends ResponseBase<MessagesPeekHeaders
             HttpRequest request,
             int statusCode,
             HttpHeaders rawHeaders,
-            List<PeekedMessageItem> value,
+            List<PeekedMessageItemInternal> value,
             MessagesPeekHeaders headers) {
         super(request, statusCode, rawHeaders, value, headers);
     }
 
     /** @return the deserialized response body. */
     @Override
-    public List<PeekedMessageItem> getValue() {
+    public List<PeekedMessageItemInternal> getValue() {
         return super.getValue();
     }
 }
