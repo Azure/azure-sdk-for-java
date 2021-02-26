4b9d9cc677d7d3999aca32e4024718a45c6f04c0
Luyunmt <2534523917@qq.com>
2021-02-26 14:25:56 +0800

    update readme

--- sdk/storage/azure-storage-file-share/src/samples/java/com/azure/storage/file/share/ReadmeSamples.java
+++ sdk/storage/azure-storage-file-share/src/samples/java/com/azure/storage/file/share/ReadmeSamples.java
@@ -61,7 +61,7 @@ public class ReadmeSamples {
         ShareClient shareClient = new ShareClientBuilder().endpoint(shareURL)
             .sasToken(SAS_TOKEN).shareName(shareName).buildClient();
     }
-    
+
     public void createShareClientWithConnectionString() {
         String shareName = "testshare";
         String shareURL = String.format("https://%s.file.core.windows.net", ACCOUNT_NAME);
