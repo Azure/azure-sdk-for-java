8c627cb07645a90bcbc859e4864de484c851b50f
Yijun Xie <48257664+YijunXieMS@users.noreply.github.com>
2021-03-01 10:23:00 -0800

    Fix a deserialization problem for data BinaryData.from(null)

--- sdk/core/azure-core/src/test/java/com/azure/core/models/CloudEventTests.java
+++ sdk/core/azure-core/src/test/java/com/azure/core/models/CloudEventTests.java
@@ -385,7 +385,7 @@ public class CloudEventTests {
             SERIALIZER.serialize(bos, cloudEvent);
             final String serializedString = bos.toString();
             final CloudEvent deserializedCloudEvent = CloudEvent.fromString(serializedString).get(0);
-            assertNull(deserializedCloudEvent.getData());
+            assertNull(deserializedCloudEvent.getData().toObject(Object.class, SERIALIZER));  // any class works here.
             assertEquals("value", deserializedCloudEvent.getExtensionAttributes().get("foo"));
             compareCloudEventContent(cloudEvent, deserializedCloudEvent);
         }
