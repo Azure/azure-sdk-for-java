03418e00ae596e0e061530d75b2f326a33a378c6
Azad Abbasi <azabbasi@microsoft.com>
2021-03-03 15:34:29 -0800

    Update descriptions

--- sdk/modelsrepository/azure-iot-modelsrepository/src/main/java/com/azure/iot/modelsrepository/implementation/package-info.java
+++ sdk/modelsrepository/azure-iot-modelsrepository/src/main/java/com/azure/iot/modelsrepository/implementation/package-info.java
@@ -2,7 +2,6 @@
 // Licensed under the MIT License.
 
 /**
- * Package containing the implementations for AzureDigitalTwinsAPI. A service for managing and querying digital twins
- * and digital twin models.
+ * Package containing the implementations for ModelsRepositoryAPI.
  */
 package com.azure.iot.modelsrepository.implementation;
