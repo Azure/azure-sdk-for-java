2b6de91d0e11e5228c9782733a9820e9ed354f94
amarathavale <amarathavale@users.noreply.github.com>
2021-03-09 14:32:12 -0800

    Adding query execution test capability (#19682)
    Co-authored-by: Amar Athavale <aathaval@linkedin.com>

--- sdk/cosmos/azure-cosmos-benchmark/src/main/java/com/azure/cosmos/benchmark/linkedin/QueryTestRunner.java
+++ sdk/cosmos/azure-cosmos-benchmark/src/main/java/com/azure/cosmos/benchmark/linkedin/QueryTestRunner.java
@@ -7,9 +7,12 @@ import com.azure.cosmos.CosmosAsyncClient;
 import com.azure.cosmos.benchmark.Configuration;
 import com.azure.cosmos.benchmark.linkedin.data.EntityConfiguration;
 import com.azure.cosmos.benchmark.linkedin.data.Key;
+import com.azure.cosmos.benchmark.linkedin.impl.Constants;
 import com.azure.cosmos.benchmark.linkedin.impl.exceptions.AccessorException;
-import com.azure.cosmos.benchmark.linkedin.impl.models.GetRequestOptions;
+import com.azure.cosmos.benchmark.linkedin.impl.models.QueryOptions;
+import com.azure.cosmos.models.SqlParameter;
 import com.codahale.metrics.MetricRegistry;
+import com.google.common.collect.ImmutableList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,19 +24,32 @@ import org.slf4j.LoggerFactory;
  *
  * The Latency tracking and reposting is handled within the Accessor, and not in this method.
  */
-public class GetTestRunner extends TestRunner {
+public class QueryTestRunner extends TestRunner {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(GetTestRunner.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(QueryTestRunner.class);
+    private static final String PARTITIONING_KEY_BINDING_PARAMETER = "@" + Constants.PARTITION_KEY;
+    private static final String QUERY = String.format("SELECT * FROM c WHERE c.%s = %s",
+        Constants.PARTITION_KEY, PARTITIONING_KEY_BINDING_PARAMETER);
 
-    GetTestRunner(final Configuration configuration,
+    QueryTestRunner(final Configuration configuration,
         final CosmosAsyncClient client,
         final MetricRegistry metricsRegistry,
         final EntityConfiguration entityConfiguration) {
         super(configuration, client, metricsRegistry, entityConfiguration);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void testOperation(final Key key) throws AccessorException {
-        _accessor.get(key, GetRequestOptions.EMPTY_REQUEST_OPTIONS);
+        final SqlParameter sqlParameter = new SqlParameter(PARTITIONING_KEY_BINDING_PARAMETER,
+            key.getPartitioningKey());
+        final QueryOptions queryOptions = new QueryOptions.Builder()
+            .setDocumentDBQuery(QUERY)
+            .setSqlParameterList(ImmutableList.of(sqlParameter))
+            .setPartitioningKey(key.getPartitioningKey())
+            .build();
+        _accessor.query(queryOptions);
     }
 }
