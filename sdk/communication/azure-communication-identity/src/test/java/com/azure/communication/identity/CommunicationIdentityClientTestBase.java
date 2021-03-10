4d3b8c32282c03e010691e6d5e0dbfd75ccdb8e2
sacheu <sacheu@microsoft.com>
2021-03-10 14:22:18 -0800

    Add client side log

--- sdk/communication/azure-communication-identity/src/test/java/com/azure/communication/identity/CommunicationIdentityClientTestBase.java
+++ sdk/communication/azure-communication-identity/src/test/java/com/azure/communication/identity/CommunicationIdentityClientTestBase.java
@@ -65,6 +65,19 @@ public class CommunicationIdentityClientTestBase extends TestBase {
         return builder;
     }
 
+    //Helper method
+    private static HttpPipeline setupPipeline(HttpClient httpClient) {
+        List<HttpPipelinePolicy> policies = new ArrayList<>();
+        HttpLogOptions httpLogOptions = new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BODY_AND_HEADERS);
+        HttpPolicyProviders.addBeforeRetryPolicies(policies);
+        policies.add(new RetryPolicy());
+        HttpPolicyProviders.addAfterRetryPolicies(policies);
+        policies.add(new HttpLoggingPolicy(httpLogOptions));
+        
+        return new HttpPipelineBuilder().httpClient(httpClient)
+            .policies(policies.toArray(new HttpPipelinePolicy[0])).build();
+    }
+
     protected CommunicationIdentityClientBuilder getCommunicationIdentityClientBuilderUsingManagedIdentity(HttpClient httpClient) {
         CommunicationIdentityClientBuilder builder = new CommunicationIdentityClientBuilder();
         builder
@@ -74,7 +87,7 @@ public class CommunicationIdentityClientTestBase extends TestBase {
         if (getTestMode() == TestMode.PLAYBACK) {
             builder.credential(new FakeCredentials());
         } else {
-            builder.credential(new DefaultAzureCredentialBuilder().authorityHost("https://login.windows-ppe.net").build());
+            builder.credential(new DefaultAzureCredentialBuilder().authorityHost("https://login.windows-ppe.net").httpPipeline(setupPipeline(HttpClient.createDefault())).build());
         }
 
         if (getTestMode() == TestMode.RECORD) {
