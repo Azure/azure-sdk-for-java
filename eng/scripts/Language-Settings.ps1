c09c2655425b48bcbaed039a419f8334c87acce9
praveenkuttappan <55455725+praveenkuttappan@users.noreply.github.com>
2021-03-10 12:50:11 -0800

    Use common API review crete step and reorder step

--- eng/scripts/Language-Settings.ps1
+++ eng/scripts/Language-Settings.ps1
@@ -238,19 +238,21 @@ function Update-java-CIConfig($pkgs, $ciRepo, $locationInDocRepo, $monikerId=$nu
 }
 
 # function is used to filter packages to submit to API view tool
-function Find-java-Artifacts-For-Apireview($artifactDir, $pkgName = "")
+function Find-java-Artifacts-For-Apireview($artifactDir, $pkgName)
 {
-  Write-Host "Checking for source jar in artifact path $($artifactDir)"
   # Find all source jar files in given artifact directory
-  $files = Get-ChildItem "${artifactDir}" | Where-Object -FilterScript {$_.Name.EndsWith("sources.jar")}
+  # Filter for package only in track2 data plane or management directory. Currently track1 package APIs are not reviewed/enforced by arch board"
+  $artifactPath = Join-Path $artifactDir "com.azure*" $pkgName
+  Write-Host "Checking for source jar in artifact path $($artifactPath)"
+  $files = Get-ChildItem -Recurse "${artifactPath}" | Where-Object -FilterScript {$_.Name.EndsWith("sources.jar")}
   if (!$files)
   {
-    Write-Host "$($artifactDir) does not have any package"
+    Write-Host "$($artifactPath) does not have any package"
     return $null
   }
   elseif($files.Count -ne 1)
   {
-    Write-Host "$($artifactDir) should contain only one (1) published source jar package"
+    Write-Host "$($artifactPath) should contain only one (1) published source jar package"
     Write-Host "No of Packages $($files.Count)"
     return $null
   }
