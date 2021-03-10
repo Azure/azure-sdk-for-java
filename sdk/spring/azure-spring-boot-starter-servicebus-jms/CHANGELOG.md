43698d283e694b9bf1f3b5eaa21fed943f49cc70
Yi Liu <yiliu6@microsoft.com>
2021-03-10 10:03:18 +0800

    add changelog

--- sdk/spring/azure-spring-boot-starter-servicebus-jms/CHANGELOG.md
+++ sdk/spring/azure-spring-boot-starter-servicebus-jms/CHANGELOG.md
@@ -1,7 +1,8 @@
 # Release History
 
 ## 3.3.0-beta.1 (Unreleased)
-
+### Key Bug Fixes
+- Disable caching of `MessageProducer` and `MessageConsumer`.
 
 ## 3.2.0 (2021-03-03)
 ### Breaking Changes
--- sdk/spring/azure-spring-boot/src/main/java/com/azure/spring/autoconfigure/jms/NonPremiumServiceBusJMSAutoConfiguration.java
+++ sdk/spring/azure-spring-boot/src/main/java/com/azure/spring/autoconfigure/jms/NonPremiumServiceBusJMSAutoConfiguration.java
@@ -14,7 +14,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.config.JmsListenerContainerFactory;
-import org.springframework.jms.connection.CachingConnectionFactory;
 
 import javax.jms.ConnectionFactory;
 
