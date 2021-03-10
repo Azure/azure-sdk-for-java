9c6b404f0fe5f4df7aae47677cfc67b4a1de3cbb
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2021-03-10 14:50:45 -0800

    Customize SearchIndexStatistics

--- sdk/search/azure-search-documents/src/main/java/com/azure/search/documents/indexes/models/InputFieldMappingEntry.java
+++ sdk/search/azure-search-documents/src/main/java/com/azure/search/documents/indexes/models/InputFieldMappingEntry.java
@@ -10,7 +10,6 @@ import com.azure.core.annotation.Fluent;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSetter;
-
 import java.util.List;
 
 /** Input field mapping for a skill. */
