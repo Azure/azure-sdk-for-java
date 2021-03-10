058008df191482a84894644b6a1edcaf5b8a43ae
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2021-03-09 16:45:19 -0800

    Customizations for data policies

--- sdk/search/azure-search-documents/swagger/src/main/java/SearchServiceCustomizations.java
+++ sdk/search/azure-search-documents/swagger/src/main/java/SearchServiceCustomizations.java
@@ -9,6 +9,10 @@ import com.azure.autorest.customization.PackageCustomization;
 import java.lang.reflect.Modifier;
 
 public class SearchServiceCustomizations extends Customization {
+    // Common modifier combinations
+    private static final int PUBLIC_ABSTRACT = Modifier.PUBLIC | Modifier.ABSTRACT;
+    private static final int PUBLIC_FINAL = Modifier.PUBLIC | Modifier.FINAL;
+
     // Packages
     private static final String IMPLEMENTATION_MODELS = "com.azure.search.documents.indexes.implementation.models";
     private static final String MODELS = "com.azure.search.documents.indexes.models";
@@ -22,6 +26,13 @@ public class SearchServiceCustomizations extends Customization {
     private static final String BM_25_SIMILARITY_ALGORITHM = "BM25SimilarityAlgorithm";
     private static final String CLASSIC_SIMILARITY_ALGORITHM = "ClassicSimilarityAlgorithm";
 
+    private static final String DATA_CHANGE_DETECTION_POLICY = "DataChangeDetectionPolicy";
+    private static final String HIGH_WATER_MARK_CHANGE_DETECTION_POLICY = "HighWaterMarkChangeDetectionPolicy";
+    private static final String SQL_INTEGRATED_CHANGE_TRACKING_POLICY = "SqlIntegratedChangeTrackingPolicy";
+
+    private static final String DATA_DELETION_DETECTION_POLICY = "DataDeletionDetectionPolicy";
+    private static final String SOFT_DELETE_COLUMN_DELETION_DETECTION_POLICY = "SoftDeleteColumnDeletionDetectionPolicy";
+
     @Override
     public void customize(LibraryCustomization libraryCustomization) {
         customizeImplementationModelsPackage(libraryCustomization.getPackage(IMPLEMENTATION_MODELS));
@@ -32,13 +43,28 @@ public class SearchServiceCustomizations extends Customization {
     }
 
     private void customizeModelsPackage(PackageCustomization packageCustomization) {
-        customizeScoringFunction(packageCustomization.getClass(SCORING_FUNCTION));
+        // Change ScoringFunction to an abstract class.
+        changeClassModifier(packageCustomization.getClass(SCORING_FUNCTION), PUBLIC_ABSTRACT);
         customizeMagnitudeScoringParameters(packageCustomization.getClass(MAGNITUDE_SCORING_PARAMETERS));
         customizeSearchFieldDataType(packageCustomization.getClass(SEARCH_FIELD_DATA_TYPE));
 
         customizeSimilarityAlgorithm(packageCustomization.getClass(SIMILARITY_ALGORITHM));
-        customizeBM25SimilarityAlgorithm(packageCustomization.getClass(BM_25_SIMILARITY_ALGORITHM));
-        customizeClassicSimilarityAlgorithm(packageCustomization.getClass(CLASSIC_SIMILARITY_ALGORITHM));
+        // Change BM25SimilarityAlgorithm to a final class.
+        changeClassModifier(packageCustomization.getClass(BM_25_SIMILARITY_ALGORITHM), PUBLIC_FINAL);
+        // Change ClassicSimilarityAlgorithm to a final class.
+        changeClassModifier(packageCustomization.getClass(CLASSIC_SIMILARITY_ALGORITHM), PUBLIC_FINAL);
+
+        // Change DataChangeDetectionPolicy to an abstract class.
+        changeClassModifier(packageCustomization.getClass(DATA_CHANGE_DETECTION_POLICY), PUBLIC_ABSTRACT);
+        // Change HighWaterMarkChangeDetectionPolicy to a final class.
+        changeClassModifier(packageCustomization.getClass(HIGH_WATER_MARK_CHANGE_DETECTION_POLICY), PUBLIC_FINAL);
+        // Change SqlIntegratedChangeTrackingPolicy to a final class.
+        changeClassModifier(packageCustomization.getClass(SQL_INTEGRATED_CHANGE_TRACKING_POLICY), PUBLIC_FINAL);
+
+        // Change DataDeletionDetectionPolicy to an abstract class.
+        changeClassModifier(packageCustomization.getClass(DATA_DELETION_DETECTION_POLICY), PUBLIC_ABSTRACT);
+        // Change SoftDeleteColumnDeletionDetectionPolicy to a final class.
+        changeClassModifier(packageCustomization.getClass(SOFT_DELETE_COLUMN_DELETION_DETECTION_POLICY), PUBLIC_FINAL);
     }
 
     private void customizeSearchFieldDataType(ClassCustomization classCustomization) {
@@ -53,26 +79,18 @@ public class SearchServiceCustomizations extends Customization {
             .setReturn("a Collection of the corresponding SearchFieldDataType");
     }
 
-    private void customizeScoringFunction(ClassCustomization classCustomization) {
-        classCustomization.setModifier(Modifier.PUBLIC | Modifier.ABSTRACT);
-    }
-
     private void customizeMagnitudeScoringParameters(ClassCustomization classCustomization) {
         classCustomization.getMethod("isShouldBoostBeyondRangeByConstant")
             .rename("shouldBoostBeyondRangeByConstant");
     }
 
     private void customizeSimilarityAlgorithm(ClassCustomization classCustomization) {
-        classCustomization.setModifier(Modifier.PUBLIC | Modifier.ABSTRACT);
+        changeClassModifier(classCustomization, Modifier.PUBLIC | Modifier.ABSTRACT);
         classCustomization.removeAnnotation("@JsonTypeName");
         classCustomization.addAnnotation("@JsonTypeName(\"Similarity\")");
     }
 
-    private void customizeBM25SimilarityAlgorithm(ClassCustomization classCustomization) {
-        classCustomization.setModifier(Modifier.PUBLIC | Modifier.FINAL);
-    }
-
-    private void customizeClassicSimilarityAlgorithm(ClassCustomization classCustomization) {
-        classCustomization.setModifier(Modifier.PUBLIC | Modifier.FINAL);
+    private static void changeClassModifier(ClassCustomization classCustomization, int modifier) {
+        classCustomization.setModifier(modifier);
     }
 }
