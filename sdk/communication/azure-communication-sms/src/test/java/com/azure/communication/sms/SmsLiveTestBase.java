c869616e5c048f335a7a247d750faee6711c1e5d
sacheu <sacheu@microsoft.com>
2021-03-11 14:25:17 -0800

    Fix compile error

--- sdk/communication/azure-communication-sms/src/test/java/com/azure/communication/sms/SmsLiveTestBase.java
+++ sdk/communication/azure-communication-sms/src/test/java/com/azure/communication/sms/SmsLiveTestBase.java
@@ -66,8 +66,6 @@ public class SmsLiveTestBase extends TestBase {
         if (getTestMode() == TestMode.PLAYBACK) {
             builder.credential(new FakeCredentials());
         } else {
-           // builder.credential(new ClientSecretCredentialBuilder().tenantId("3b78b39a-ac6c-465a-a010-5a3f47148ff0").clientId("19ea75db-1ade-473a-9f60-c3fb905f1f56").clientSecret("wxJC4-t.~G2Xb6gnTrXU9~t~F41hrBl42l").authorityHost("https://login.windows-ppe.net").build());
-
             builder.credential(new DefaultAzureCredentialBuilder().build());
         }
 
