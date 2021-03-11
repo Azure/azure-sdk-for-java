893b14dec488bd88ed4c411ea186ade22f252fb8
sacheu <sacheu@microsoft.com>
2021-03-10 18:35:28 -0800

    Add tenantId in DefaultAzureCredentialBuilder

--- sdk/communication/azure-communication-identity/src/test/java/com/azure/communication/identity/CommunicationIdentityClientTestBase.java
+++ sdk/communication/azure-communication-identity/src/test/java/com/azure/communication/identity/CommunicationIdentityClientTestBase.java
@@ -87,7 +87,7 @@ public class CommunicationIdentityClientTestBase extends TestBase {
         if (getTestMode() == TestMode.PLAYBACK) {
             builder.credential(new FakeCredentials());
         } else {
-            builder.credential(new DefaultAzureCredentialBuilder().authorityHost("https://login.windows-ppe.net").httpPipeline(setupPipeline(HttpClient.createDefault())).build());
+            builder.credential(new DefaultAzureCredentialBuilder().tenantId("3b78b39a-ac6c-465a-a010-5a3f47148ff0").authorityHost("https://login.windows-ppe.net").httpPipeline(setupPipeline(HttpClient.createDefault())).build());
         }
 
         if (getTestMode() == TestMode.RECORD) {
