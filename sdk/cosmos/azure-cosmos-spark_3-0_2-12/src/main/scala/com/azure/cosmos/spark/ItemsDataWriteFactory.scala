aa4071b35a383904dd2a0d4946af4e177c78416e
Mohammad Derakhshani <moderakh@users.noreply.github.com>
2021-03-10 23:04:33 -0800

    Async point write and hugely increase perf for point write (#19781)
    - Add support for async parallelism of point write (this should hugely improve the point write perf)
    - added `spark.cosmos.write.maxConcurrency` which controls the parallelism for point write.
    - Factored out the point write code to a new class `PointWriter`
    - refactored the code `PointWriter` and `BulkWriter` both implement a new trait `AsyncItemWriter`
    - the `CosmosWriter` is simplified
    - added support for "id" validation and better error message if id is missing.
    
    TODO:
    `PointWriter` is written to support async parallelism. There is some blocking code which can become nonblocking using reactive stream api (will go different PR)
    Concurrency control and any Bulk related improvement will come later

--- sdk/cosmos/azure-cosmos-spark_3-0_2-12/src/main/scala/com/azure/cosmos/spark/ItemsDataWriteFactory.scala
+++ sdk/cosmos/azure-cosmos-spark_3-0_2-12/src/main/scala/com/azure/cosmos/spark/ItemsDataWriteFactory.scala
@@ -2,10 +2,8 @@
 // Licensed under the MIT License.
 package com.azure.cosmos.spark
 
-import com.azure.cosmos.CosmosException
+
 import com.azure.cosmos.implementation.CosmosClientMetadataCachesSnapshot
-import com.azure.cosmos.models.{CosmosItemRequestOptions, PartitionKey}
-import com.fasterxml.jackson.databind.node.ObjectNode
 import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.connector.write.{DataWriter, DataWriterFactory, WriterCommitMessage}
@@ -23,7 +21,6 @@ private class ItemsDataWriteFactory(userConfig: Map[String, String],
 
   private class CosmosWriter(inputSchema: StructType) extends DataWriter[InternalRow] {
     logInfo(s"Instantiated ${this.getClass.getSimpleName}")
-
     private val cosmosTargetContainerConfig = CosmosContainerConfig.parseCosmosContainerConfig(userConfig)
     private val cosmosWriteConfig = CosmosWriteConfig.parseWriteConfig(userConfig)
 
@@ -34,7 +31,11 @@ private class ItemsDataWriteFactory(userConfig: Map[String, String],
     private val containerDefinition = container.read().block().getProperties
     private val partitionKeyDefinition = containerDefinition.getPartitionKeyDefinition
 
-    private lazy val bulkWriter = new BulkWriter(container, cosmosWriteConfig)
+    private val writer = if (cosmosWriteConfig.bulkEnabled) {
+      new BulkWriter(container, cosmosWriteConfig)
+    } else {
+      new PointWriter(container, cosmosWriteConfig)
+    }
 
     override def write(internalRow: InternalRow): Unit = {
       val objectNode = CosmosRowConverter.fromInternalRowToObjectNode(internalRow, inputSchema)
@@ -43,84 +44,30 @@ private class ItemsDataWriteFactory(userConfig: Map[String, String],
       // TODO moderakh support patch?
       // TODO moderakh bulkWrite in another PR
 
+      if (!objectNode.has(CosmosConstants.Properties.Id) ||
+        !objectNode.get(CosmosConstants.Properties.Id).isTextual) {
+        logError(s"${CosmosConstants.Properties.Id} is a mandatory field. " +
+          s"But it is missing or it is not a string")
+      }
       val partitionKeyValue = PartitionKeyHelper.getPartitionKeyPath(objectNode, partitionKeyDefinition)
-
-      if (cosmosWriteConfig.bulkEnabled) {
-        bulkWriter.scheduleWrite(partitionKeyValue, objectNode)
-      } else {
-        cosmosWriteConfig.itemWriteStrategy match {
-          case ItemWriteStrategy.ItemOverwrite => upsertWithRetry(partitionKeyValue, objectNode)
-          case ItemWriteStrategy.ItemAppend => createWithRetry(partitionKeyValue, objectNode)
-        }
-      }
+      writer.scheduleWrite(partitionKeyValue, objectNode)
     }
-
-    // scalastyle:off return
-    private def createWithRetry(partitionKeyValue: PartitionKey,
-                                objectNode: ObjectNode): Unit = {
-
-      var exceptionOpt = Option.empty[Exception]
-      for (attempt <- 1 to cosmosWriteConfig.maxRetryCount + 1) {
-        try {
-          container.createItem(objectNode, partitionKeyValue, new CosmosItemRequestOptions()).block()
-          return
-        } catch {
-          case e: CosmosException if Exceptions.isResourceExistsException(e) =>
-            // TODO: what should we do on unique index violation? should we ignore or throw?
-            // TODO moderakh we need to add log messages extract identifier (id, pk) and log
-            return
-          case e: CosmosException if Exceptions.canBeTransientFailure(e) =>
-            logWarning(
-              s"create item attempt #$attempt max remaining retries"
-                  + s"${cosmosWriteConfig.maxRetryCount + 1 - attempt}, encountered ${e.getMessage}")
-            exceptionOpt = Option.apply(e)
-        }
-      }
-
-      assert(exceptionOpt.isDefined)
-      throw exceptionOpt.get
-    }
-
-    private def upsertWithRetry(partitionKeyValue: PartitionKey,
-                                objectNode: ObjectNode): Unit = {
-      var exceptionOpt = Option.empty[Exception]
-      for (attempt <- 1 to cosmosWriteConfig.maxRetryCount + 1) {
-
-        try {
-          container.upsertItem(objectNode, partitionKeyValue, new CosmosItemRequestOptions()).block()
-          return
-        } catch {
-          case e: CosmosException if Exceptions.canBeTransientFailure(e) =>
-            logWarning(
-              s"upsert item attempt #$attempt max remaining retries "
-                  + s"${cosmosWriteConfig.maxRetryCount + 1 - attempt}, encountered ${e.getMessage}")
-            exceptionOpt = Option.apply(e)
-        }
-      }
-
-      assert(exceptionOpt.isDefined)
-      throw exceptionOpt.get
-    }
-    // scalastyle:on return
 
     override def commit(): WriterCommitMessage = {
-      if (cosmosWriteConfig.bulkEnabled) {
-        bulkWriter.flushAndClose()
-      }
+      logInfo("commit invoked!!!")
+      writer.flushAndClose()
 
       new WriterCommitMessage {}
     }
 
     override def abort(): Unit = {
-      if (cosmosWriteConfig.bulkEnabled) {
-        bulkWriter.flushAndClose()
-      }
+      logInfo("abort invoked!!!")
+      writer.flushAndClose()
     }
 
     override def close(): Unit = {
-      if (cosmosWriteConfig.bulkEnabled) {
-        bulkWriter.flushAndClose()
-      }
+      logInfo("close invoked!!!")
+      writer.flushAndClose()
     }
   }
 }
