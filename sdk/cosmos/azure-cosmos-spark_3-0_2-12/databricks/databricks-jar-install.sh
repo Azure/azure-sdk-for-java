7fb0d17598781c3691241c124cc79ea84df91d90
Matias Quaranta <ealsur@hotmail.com>
2021-03-16 15:02:12 -0700

    paths

--- sdk/cosmos/azure-cosmos-spark_3-0_2-12/databricks/databricks-jar-install.sh
+++ sdk/cosmos/azure-cosmos-spark_3-0_2-12/databricks/databricks-jar-install.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 CLUSTER_NAME=$1
-JARFILE=$2
+JARPATH=$2
+JARFILE=$3
 [[ -z "$CLUSTER_NAME" ]] && exit 1
+[[ -z "$JARFILE" ]] && exit 1
 [[ -z "$JARFILE" ]] && exit 1
 
 CLUSTER_ID=$(databricks clusters list --output json | jq -r --arg N "$CLUSTER_NAME" '.clusters[] | select(.cluster_name == $N) | .cluster_id')
 
-databricks libraries uninstall --cluster-id $cluster_id --jar dbfs:/tmp/sparkconnector
+databricks libraries uninstall --cluster-id $cluster_id --jar dbfs:/tmp/sparkconnector/$JARFILE
 
-dbfs cp $JARFILE dbfs:/tmp/sparkconnector
-dbfs ls dbfs:/tmp
+dbfs cp $JARPATH/$JARFILE dbfs:/tmp/sparkconnector
+dbfs ls dbfs:/tmp/sparkconnector
 
-databricks libraries install --cluster-id $cluster_id --jar dbfs:/tmp/sparkconnector
+databricks libraries install --cluster-id $cluster_id --jar dbfs:/tmp/sparkconnector/$JARFILE
