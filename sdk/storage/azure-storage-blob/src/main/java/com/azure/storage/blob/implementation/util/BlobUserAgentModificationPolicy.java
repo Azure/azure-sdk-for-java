84f1d2b8e2c070021aab51f8d822dcb63dd00ebe
gapra <gapra@microsoft.com>
2021-03-17 12:19:49 -0700

    Made changes to user agent regex

--- sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/implementation/util/BlobUserAgentModificationPolicy.java
+++ sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/implementation/util/BlobUserAgentModificationPolicy.java
@@ -25,7 +25,7 @@ public class BlobUserAgentModificationPolicy implements HttpPipelinePolicy {
     private final String clientVersion;
 
     private static final String USER_AGENT = "User-Agent";
-    private static final String REGEX = "azsdk-java-azure-storage-blob/12\\.\\d{1,2}\\.\\d{1,2}(?:-beta\\.\\d{1,2})?";
+    private static final String REGEX = "(.*? )?(azsdk-java-azure-storage-blob/12\\.\\d{1,2}\\.\\d{1,2}(?:-beta\\.\\d{1,2})?)( .*?)?";
     private static final Pattern PATTERN = Pattern.compile(REGEX);
 
     /**
@@ -45,15 +45,11 @@ public class BlobUserAgentModificationPolicy implements HttpPipelinePolicy {
         String userAgent = context.getHttpRequest().getHeaders().getValue(USER_AGENT);
         Matcher matcher = PATTERN.matcher(userAgent);
         StringBuilder builder = new StringBuilder();
-        if (matcher.find()) {
-            int insertAfter = matcher.end() + 1; // Add a 1 for the extra space
-            String[] ua = userAgent.split(" ");
-            for (int i = 0; i < ua.length; i++) {
-                builder.append(ua[i]).append(" ");
-                if (builder.length() == insertAfter) {
-                    builder.append("azsdk-java-").append(clientName).append("/").append(clientVersion).append(" ");
-                }
-            }
+        if (matcher.matches()) {
+            builder.append(matcher.group(1) == null ? "" : matcher.group(1))
+                .append(matcher.group(2) == null ? "" : matcher.group(2))
+                .append(" ").append("azsdk-java-").append(clientName).append("/").append(clientVersion)
+                .append(matcher.group(3) == null ? "" : matcher.group(3));
         } else {
             builder.append(userAgent);
         }
