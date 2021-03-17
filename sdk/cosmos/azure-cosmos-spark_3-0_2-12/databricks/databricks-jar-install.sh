28ff7a9ebaaef02ac34059afcc45c080f7571c8f
Matias Quaranta <ealsur@hotmail.com>
2021-03-17 14:11:41 -0700

    restart

--- sdk/cosmos/azure-cosmos-spark_3-0_2-12/databricks/databricks-jar-install.sh
+++ sdk/cosmos/azure-cosmos-spark_3-0_2-12/databricks/databricks-jar-install.sh
@@ -14,9 +14,24 @@ export CLUSTER_ID=$(databricks clusters list --output json | jq -r --arg N "$CLU
 echo "Uninstalling previous $JARFILE in $CLUSTER_ID"
 databricks libraries uninstall --cluster-id $CLUSTER_ID --jar dbfs:/tmp/sparkconnector/$JARFILE
 
+echo "Restarting $CLUSTER_ID"
+databricks clusters restart --cluster-id $CLUSTER_ID
+export STATE="PENDING"
+while [[ "$STATE" != "PENDING" ]]
+do
+	export STATE=$(databricks clusters list --output json | jq -r --arg I "$CLUSTER_ID" '.clusters[] | select(.cluster_name == $I) | .state')
+done
+
 echo "Copying files to DBFS $JARPATH/$JARFILE"
-dbfs cp $JARPATH/$JARFILE dbfs:/tmp/sparkconnector
+dbfs cp --overwrite $JARPATH/$JARFILE dbfs:/tmp/sparkconnector/$JARFILE
 dbfs ls dbfs:/tmp/sparkconnector
 
 echo "Installing $JARFILE in $CLUSTER_ID"
 databricks libraries install --cluster-id $CLUSTER_ID --jar dbfs:/tmp/sparkconnector/$JARFILE
+echo "Restarting $CLUSTER_ID"
+databricks clusters restart --cluster-id $CLUSTER_ID
+export STATE="PENDING"
+while [[ "$STATE" != "PENDING" ]]
+do
+	export STATE=$(databricks clusters list --output json | jq -r --arg I "$CLUSTER_ID" '.clusters[] | select(.cluster_name == $I) | .state')
+done
