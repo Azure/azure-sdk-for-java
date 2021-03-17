3ffc9c2a5c492e646828fe8b5c875ba76488723e
Mohammad Derakhshani <moderakh@users.noreply.github.com>
2021-03-17 16:16:33 -0700

    improves reliablity of test infra

--- sdk/cosmos/azure-cosmos-spark_3-1_2-12/src/test/scala/com/azure/cosmos/spark/TestUtils.scala
+++ sdk/cosmos/azure-cosmos-spark_3-1_2-12/src/test/scala/com/azure/cosmos/spark/TestUtils.scala
@@ -2,20 +2,22 @@
 // Licensed under the MIT License.
 package com.azure.cosmos.spark
 
-import java.util.UUID
 import com.azure.cosmos.implementation.TestConfigurations
-import com.azure.cosmos.models.{ChangeFeedPolicy, CosmosContainerProperties, CosmosItemRequestOptions, ThroughputProperties}
-import com.azure.cosmos.{CosmosAsyncClient, CosmosClientBuilder, CosmosException}
+import com.azure.cosmos.models.{ChangeFeedPolicy, CosmosContainerProperties, PartitionKey, ThroughputProperties}
+import com.azure.cosmos.{BulkOperations, CosmosAsyncClient, CosmosClientBuilder, CosmosException, CosmosItemOperation}
 import com.fasterxml.jackson.databind.node.ObjectNode
 import org.apache.commons.lang3.RandomStringUtils
 import org.apache.spark.sql.SparkSession
 import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, Suite}
+import reactor.core.publisher.EmitterProcessor
+import reactor.core.scala.publisher.SMono.PimpJFlux
 
 import java.time.Duration
+import java.util.UUID
+import java.util.concurrent.atomic.AtomicInteger
 import scala.collection.mutable.ListBuffer
 import scala.jdk.CollectionConverters.iterableAsScalaIterableConverter
 // scalastyle:off underscore.import
-
 // scalastyle:on underscore.import
 
 // extending class will have a pre-created spark session
@@ -136,6 +138,18 @@ trait CosmosContainer extends CosmosDatabase {
 
   var cosmosContainer = UUID.randomUUID().toString
 
+  val partitionKeyPath = "/id"
+
+  def getPartitionKeyValue(objectNode: ObjectNode) : Object = {
+    // assumes partitionKeyPath being "/id" hence pkValue is always string
+    objectNode.get("id").textValue()
+  }
+
+  def getId(objectNode: ObjectNode) : String = {
+    // assumes partitionKeyPath being "/id" hence pkValue is always string
+    objectNode.get("id").textValue()
+  }
+
   override def beforeAll(): Unit = {
     super.beforeAll()
     this.createContainerCore()
@@ -157,7 +171,7 @@ trait CosmosContainer extends CosmosDatabase {
     val throughputProperties = ThroughputProperties.createManualThroughput(Defaults.DefaultContainerThroughput)
     cosmosClient
       .getDatabase(cosmosDatabase)
-      .createContainerIfNotExists(cosmosContainer, "/id", throughputProperties).block()
+      .createContainerIfNotExists(cosmosContainer, partitionKeyPath, throughputProperties).block()
   }
 
   override def afterAll(): Unit = {
@@ -186,9 +200,10 @@ trait CosmosContainerWithRetention extends CosmosContainer {
   this: Suite =>
   //scalastyle:off
 
+
   override def createContainerCore(): Unit = {
     val properties: CosmosContainerProperties =
-      new CosmosContainerProperties(cosmosContainer, "/id")
+      new CosmosContainerProperties(cosmosContainer, partitionKeyPath)
     properties.setChangeFeedPolicy(
       ChangeFeedPolicy.createFullFidelityPolicy(Duration.ofMinutes(10)))
 
@@ -203,15 +218,46 @@ trait CosmosContainerWithRetention extends CosmosContainer {
 trait AutoCleanableCosmosContainer extends CosmosContainer with BeforeAndAfterEach {
   this: Suite =>
 
+  private val MaxConcurrencyForItemDeletion = 10
+
+
+
   override def afterEach(): Unit = {
-    try super.afterEach() // To be stackable, must call super.afterAll
-    finally {
+
+    try {
       // wait for data to get replicated
       Thread.sleep(1000)
+      System.out.println(s"cleaning the items in container ${cosmosContainer}")
       val container = cosmosClient.getDatabase(cosmosDatabase).getContainer(cosmosContainer)
-      container.queryItems("SELECT * FROM r", classOf[ObjectNode]).flatMap(
-        item => container.deleteItem(item, new CosmosItemRequestOptions())
-      ).blockLast()
+
+      try {
+        val emitter = EmitterProcessor.create[CosmosItemOperation]()
+        val bulkDeleteFlux = container.processBulkOperations(emitter)
+
+        val cnt = new AtomicInteger(0)
+        container.queryItems("SELECT * FROM r", classOf[ObjectNode])
+          .asScala
+          .doOnNext(item => {
+            val operation = BulkOperations.getDeleteItemOperation(getId(item), new PartitionKey(getPartitionKeyValue(item)))
+            cnt.incrementAndGet()
+            emitter.onNext(operation)
+
+          }).doOnComplete(
+          () => {
+            emitter.onComplete()
+          }).subscribe()
+
+        bulkDeleteFlux.blockLast()
+
+        System.out.println(s"Deleted ${cnt.get()} in container ${cosmosContainer}")
+      } catch {
+        case e: Exception => {
+          System.err.println(s"${this.getClass.getName}#afterEach: failed:" + e.getMessage)
+          throw e
+        }
+      }
+    } finally {
+      super.afterEach() // To be stackable, must call super.afterEach
     }
   }
 }
