0927f353191757b1c257bac9fd9ee91c0f87e30a
annie-mac <annie-mac@TT-V-ROPILA-WS5.redmond.corp.microsoft.com>
2021-03-24 07:12:52 -0700

    change

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/changefeed/CancellationToken.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/changefeed/CancellationToken.java
@@ -2,26 +2,28 @@
 // Licensed under the MIT License.
 package com.azure.cosmos.implementation.changefeed;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 /**
  * Propagates notification that operations should be canceled..
  */
 public class CancellationToken {
     private final CancellationTokenSource tokenSource;
-    private volatile boolean cancellationRequested;
+    private AtomicBoolean cancellationRequested;
 
     public CancellationToken(CancellationTokenSource source) {
         this.tokenSource = source;
-        this.cancellationRequested = false;
+        this.cancellationRequested = new AtomicBoolean(false);
     }
 
-    public synchronized void cancel() {
-        this.cancellationRequested = true;
+    public void cancel() {
+        this.cancellationRequested.set(true);
     }
 
     /**
      * @return true if the cancellation was requested from the source.
      */
     public boolean isCancellationRequested() {
-        return tokenSource.isCancellationRequested() || this.cancellationRequested;
+        return tokenSource.isCancellationRequested() || this.cancellationRequested.get();
     }
 }
