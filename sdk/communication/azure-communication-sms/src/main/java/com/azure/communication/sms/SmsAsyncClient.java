221def1ebf3ae2795293b8f4dcda47415e895e16
ankitarorabit <48968483+ankitarorabit@users.noreply.github.com>
2021-03-25 18:28:15 -0700

    [Communication] Revert "[Communication] Change SMS APIs to be pageable (#20042)" (#20104)
    * Revert "[Communication] Change SMS APIs to be pageable (#20042)"
    
    This reverts commit 9c5d7bfa0ceb76218478dcb463f1af0e67307262.
    
    * Changes to keep
    
    * Update versions in the package POM
    
    * Include other valid APIView feedback
    
    * PR feedback
    
    * remove unused logger

--- sdk/communication/azure-communication-sms/src/main/java/com/azure/communication/sms/SmsAsyncClient.java
+++ sdk/communication/azure-communication-sms/src/main/java/com/azure/communication/sms/SmsAsyncClient.java
@@ -14,8 +14,6 @@ import com.azure.communication.sms.models.SmsSendResult;
 import com.azure.core.annotation.ReturnType;
 import com.azure.core.annotation.ServiceClient;
 import com.azure.core.annotation.ServiceMethod;
-import com.azure.core.http.rest.PagedFlux;
-import com.azure.core.http.rest.PagedResponseBase;
 import com.azure.core.http.rest.Response;
 import com.azure.core.http.rest.SimpleResponse;
 import com.azure.core.util.Context;
@@ -97,9 +95,10 @@ public final class SmsAsyncClient {
      * @param message message to send to recipient.
      * @return response for a successful send Sms request.
      */
-    @ServiceMethod(returns = ReturnType.COLLECTION)
-    public PagedFlux<SmsSendResult> send(String from, Iterable<String> to, String message) {
-        return send(from, to, message, null, null);
+    @ServiceMethod(returns = ReturnType.SINGLE)
+    public Mono<Iterable<SmsSendResult>> send(String from, Iterable<String> to, String message) {
+        return sendWithResponse(from, to, message, null)
+            .map(response -> response.getValue());
     }
 
     /**
@@ -112,39 +111,28 @@ public final class SmsAsyncClient {
      * for this message to the Azure Resource Event Grid.
      * @return response for a successful send Sms request.
      */
-    @ServiceMethod(returns = ReturnType.COLLECTION)
-    public PagedFlux<SmsSendResult> send(String from, Iterable<String> to, String message, SmsSendOptions options) {
-        return send(from, to, message, options, null);
+    @ServiceMethod(returns = ReturnType.SINGLE)
+    public Mono<Response<Iterable<SmsSendResult>>> sendWithResponse(String from, Iterable<String> to, String message, SmsSendOptions options) {
+        return sendWithResponse(from, to, message, options, null);
     }
 
-    PagedFlux<SmsSendResult> send(String from, Iterable<String> to, String message, SmsSendOptions options, Context context) {
+    Mono<Response<Iterable<SmsSendResult>>> sendWithResponse(String from, Iterable<String> to, String message, SmsSendOptions options, Context context) {
         try {
             Objects.requireNonNull(from, "'from' cannot be null.");
             Objects.requireNonNull(to, "'to' cannot be null.");
             SendMessageRequest request = createSendMessageRequest(from, to, message, options);
-
-            Mono<Response<List<SmsSendResult>>> responseMono = withContext(contextValue -> {
+            return withContext(contextValue -> {
                 if (context != null) {
                     contextValue = context;
                 }
                 return this.smsClient.sendWithResponseAsync(request, contextValue)
                     .flatMap((Response<SmsSendResponse> response) -> {
-                        List<SmsSendResult> smsSendResults = convertSmsSendResults(response.getValue().getValue());
-                        return Mono.just(new SimpleResponse<List<SmsSendResult>>(response, smsSendResults));
+                        Iterable<SmsSendResult> smsSendResults = convertSmsSendResults(response.getValue().getValue());
+                        return Mono.just(new SimpleResponse<Iterable<SmsSendResult>>(response, smsSendResults));
                     });
             });
-
-            return new PagedFlux<>(() -> responseMono.map(response -> new PagedResponseBase<Void, SmsSendResult>(
-                response.getRequest(),
-                response.getStatusCode(),
-                response.getHeaders(),
-                response.getValue(),
-                null,
-                null
-            )));
-
         } catch (RuntimeException ex) {
-            return new PagedFlux<>(() -> monoError(logger, ex));
+            return monoError(logger, ex);
         }
     }
 
