ef839496ec3c2634c4a4d333ad0e3bf29f4c3efd
Rick Ley <frley@microsoft.com>
2021-03-29 13:10:23 -0700

    Updated poms for current version of core

--- sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/BlobServiceAsyncClient.java
+++ sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/BlobServiceAsyncClient.java
@@ -55,7 +55,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 
 import static com.azure.core.util.FluxUtil.monoError;
