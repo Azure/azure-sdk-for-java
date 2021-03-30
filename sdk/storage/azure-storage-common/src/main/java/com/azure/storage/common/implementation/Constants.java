02b96cae6027bd2e6648b05f5f7c81715eea6479
Rick Ley <frley@microsoft.com>
2021-03-30 10:32:34 -0700

    updated service version

--- sdk/storage/azure-storage-common/src/main/java/com/azure/storage/common/implementation/Constants.java
+++ sdk/storage/azure-storage-common/src/main/java/com/azure/storage/common/implementation/Constants.java
@@ -198,7 +198,7 @@ public final class Constants {
         /**
          * The current storage version header value.
          */
-        public static final String TARGET_STORAGE_VERSION = "2020-06-12";
+        public static final String TARGET_STORAGE_VERSION = "2020-08-04";
 
         /**
          * Error code returned from the service.
