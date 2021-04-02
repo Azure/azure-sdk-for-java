12f5fad764790c1e2d2c4cfe9e383f97e587ef08
Azad Abbasi <azabbasi@microsoft.com>
2021-04-02 13:06:39 -0700

    Update module-info.java

--- sdk/modelparser/azure-digitaltwins-parser/src/main/java/module-info.java
+++ sdk/modelparser/azure-digitaltwins-parser/src/main/java/module-info.java
@@ -7,6 +7,4 @@ module com.azure.digitaltwins.parser {
     exports com.azure.digitaltwins.parser;
 
     opens com.azure.digitaltwins.parser to com.fasterxml.jackson.databind;
-    opens com.azure.digitaltwins.parser.implementation to com.fasterxml.jackson.databind, com.azure.core;
-    opens com.azure.digitaltwins.parser.implementation.models to com.fasterxml.jackson.databind, com.azure.core;
 }
