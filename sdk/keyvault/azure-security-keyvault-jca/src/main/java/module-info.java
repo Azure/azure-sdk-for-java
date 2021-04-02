bb2542f7bfa16054cef619fbe472016c598a8177
Rujun Chen <rujche@microsoft.com>
2021-04-02 08:11:26 +0800

    Make sure the module-info only open/exports our package and not the shaded package names.

--- sdk/keyvault/azure-security-keyvault-jca/src/main/java/module-info.java
+++ sdk/keyvault/azure-security-keyvault-jca/src/main/java/module-info.java
@@ -2,11 +2,6 @@
 // Licensed under the MIT License.
 
 module com.azure.security.keyvault.jca {
-    requires transitive java.logging;
-    requires transitive com.fasterxml.jackson.databind;
-
-    requires org.apache.httpcomponents.core5.httpcore5;
-    requires org.apache.httpcomponents.client5.httpclient5;
 
     exports com.azure.security.keyvault.jca;
     exports com.azure.security.keyvault.jca.model;
