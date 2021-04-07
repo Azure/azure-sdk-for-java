a091a427a585df7d2152fc4447478e2c79d0f2f1
Moary Chen <v-moaryc@microsoft.com>
2021-04-07 10:00:39 +0800

    Add timeout process for eventhub IT test case

--- sdk/spring/azure-spring-cloud-test-eventhubs/src/test/java/com/azure/spring/test/eventhubs/EventHubIT.java
+++ sdk/spring/azure-spring-cloud-test-eventhubs/src/test/java/com/azure/spring/test/eventhubs/EventHubIT.java
@@ -5,15 +5,23 @@ package com.azure.spring.test.eventhubs;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.GenericMessage;
 import reactor.core.publisher.Sinks;
 
-//@EnableBinding(Source.class)
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
 @SpringBootTest
-class EventHubIT {
+public class EventHubIT {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EventHubIT.class);
 
     private static final String MESSAGE = "Azure Spring Cloud EventHub Test";
 
@@ -21,14 +29,35 @@ class EventHubIT {
     private Sinks.Many<Message<String>> many;
 
     @Test
-    void integrationTest() throws InterruptedException {
-        // Wait for eventhub initialization to complete
-        Thread.sleep(9000);
-        many.emitNext(new GenericMessage<>(MESSAGE), Sinks.EmitFailureHandler.FAIL_FAST);
-        Thread.sleep(6000);
-        String msg = Application.EXCHANGER.exchange(MESSAGE);
-        Assertions.assertEquals(MESSAGE, msg);
-        msg = Application.EXCHANGER.exchange("");
-        Assertions.assertEquals("ERROR!", msg);
+    void integrationTest() {
+        ExecutorService executorService = null;
+        boolean hasException = false;
+        try {
+            executorService = Executors.newFixedThreadPool(1);
+            Future<?> future = executorService.submit(() -> {
+                try {
+                    // Wait for eventhub initialization to complete
+                    Thread.sleep(9000);
+                    many.emitNext(new GenericMessage<>(MESSAGE), Sinks.EmitFailureHandler.FAIL_FAST);
+                    Thread.sleep(6000);
+                    String msg = Application.EXCHANGER.exchange(MESSAGE);
+                    Assertions.assertEquals(MESSAGE, msg);
+                    msg = Application.EXCHANGER.exchange("");
+                    Assertions.assertEquals("ERROR!", msg);
+                } catch (InterruptedException e) {
+                    LOGGER.error("InterruptedException found:", e);
+                    new RuntimeException(e);
+                }
+            });
+            future.get(35, TimeUnit.SECONDS);
+        } catch (Exception ex) {
+            hasException = true;
+            LOGGER.error("Exception found:", ex);
+            if (executorService != null) {
+                executorService.shutdown();
+            }
+        } finally {
+            Assertions.assertEquals(hasException, false);
+        }
     }
 }
