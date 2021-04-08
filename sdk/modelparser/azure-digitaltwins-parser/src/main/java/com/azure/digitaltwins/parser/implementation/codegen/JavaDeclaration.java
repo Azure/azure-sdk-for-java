436d02c882e06a834337e3bb7f6c211e8d8495a4
Azad Abbasi <azabbasi@microsoft.com>
2021-04-08 16:22:28 -0700

    Update JavaDeclaration.java

--- sdk/modelparser/azure-digitaltwins-parser/src/main/java/com/azure/digitaltwins/parser/implementation/codegen/JavaDeclaration.java
+++ sdk/modelparser/azure-digitaltwins-parser/src/main/java/com/azure/digitaltwins/parser/implementation/codegen/JavaDeclaration.java
@@ -95,6 +95,7 @@ public class JavaDeclaration {
     /**
      * Gets the value of the name decorated with appropriate keywords.
      *
+     * @param typeParams Compiled list of all typed parameters for declaration. i.e. <T1, T2>
      * @return The value of the name decorated with appropriate keywords.
      */
     public String getDecoratedName(String typeParams) {
