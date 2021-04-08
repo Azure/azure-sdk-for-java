9d2b03648017ac6609b35bae835f61816a1b2a54
gapra <gapra@microsoft.com>
2021-04-08 16:28:04 -0700

    removed unused param

--- sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/specialized/BlobOutputStream.java
+++ sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/specialized/BlobOutputStream.java
@@ -197,8 +197,6 @@ public abstract class BlobOutputStream extends StorageOutputStream {
 
     private static final class BlockBlobOutputStream extends BlobOutputStream {
 
-        private final ClientLogger logger = new ClientLogger(BlockBlobOutputStream.class);
-
         private final Lock lock;
         private final Condition transferComplete;
         private final StorageBlockingSink sink;
