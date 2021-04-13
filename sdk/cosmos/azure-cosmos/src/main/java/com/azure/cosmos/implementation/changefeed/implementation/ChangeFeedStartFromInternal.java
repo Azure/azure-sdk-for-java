eb879629833e490266feaf4315628ac14f861d00
Fabian Meiswinkel <fabian@meiswinkel.com>
2021-04-13 02:31:08 +0000

    Fixes retry policy for ChangeFeed pull model

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/changefeed/implementation/ChangeFeedStartFromInternal.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/changefeed/implementation/ChangeFeedStartFromInternal.java
@@ -43,8 +43,16 @@ public abstract class ChangeFeedStartFromInternal extends JsonSerializable {
     }
 
     @Override
-    public String toString() {
-        return this.toJson();
+    public String toString()
+    {
+        String json = this.toJson();
+
+        if (json.indexOf("\"Type\":") != json.lastIndexOf("\"Type\":")) {
+            // TODO @fabianm Remove as soon as root caused
+            throw new IllegalStateException("There shouldn't be any duplicate json properties!");
+        }
+
+        return json;
     }
 
     public abstract boolean supportsFullFidelityRetention();
