2e3ff7b47489550f9a673323aafdb47a2d23499d
Rujun Chen <rujche@microsoft.com>
2021-04-16 16:31:53 +0800

    Fix integration test: principalName not right.

--- sdk/spring/azure-spring-boot-test-aad-resource-server/src/test/java/com/azure/test/aad/resource/server/AADWeiResourceServerIT.java
+++ sdk/spring/azure-spring-boot-test-aad-resource-server/src/test/java/com/azure/test/aad/resource/server/AADWeiResourceServerIT.java
@@ -3,22 +3,24 @@
 
 package com.azure.test.aad.resource.server;
 
+import com.azure.spring.aad.webapi.AADJwtBearerTokenAuthenticationConverter;
+import com.azure.spring.aad.webapi.AADResourceServerWebSecurityConfigurerAdapter;
 import com.azure.spring.test.aad.AADWebApiITHelper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
 
+import java.security.Principal;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -26,6 +28,7 @@ import java.util.Map;
 import static com.azure.spring.test.Constant.MULTI_TENANT_SCOPE_GRAPH_READ;
 import static com.azure.spring.test.EnvironmentVariable.AAD_MULTI_TENANT_CLIENT_ID;
 import static com.azure.spring.test.EnvironmentVariable.AAD_MULTI_TENANT_CLIENT_SECRET;
+import static com.azure.spring.test.EnvironmentVariable.AAD_USER_NAME_1;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -58,18 +61,25 @@ public class AADWeiResourceServerIT {
             () -> aadWebApiITHelper.httpGetStringByAccessToken("notExist"));
     }
 
+    @Test
+    public void testPrincipalName() {
+        assertEquals(AAD_USER_NAME_1, aadWebApiITHelper.httpGetStringByAccessToken("principalName"));
+    }
+
     @EnableWebSecurity
     @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
     @SpringBootApplication
     @RestController
-    public static class DumbApp extends WebSecurityConfigurerAdapter {
+    public static class DumbApp extends AADResourceServerWebSecurityConfigurerAdapter {
 
         @Override
         protected void configure(HttpSecurity http) throws Exception {
-            http.authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+            super.configure(http);
+            AADJwtBearerTokenAuthenticationConverter converter = new AADJwtBearerTokenAuthenticationConverter();
+            converter.setPrincipalClaimName("upn");
+            http.oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(converter);
         }
 
         @GetMapping("graph")
@@ -82,6 +92,11 @@ public class AADWeiResourceServerIT {
         @PreAuthorize("hasAuthority('SCOPE_NotExist')")
         public String notExist() {
             return "notExist";
+        }
+
+        @GetMapping(value = "/principalName")
+        public ResponseEntity<String> principalName(Principal principal) {
+            return ResponseEntity.ok(principal.getName());
         }
     }
 }
