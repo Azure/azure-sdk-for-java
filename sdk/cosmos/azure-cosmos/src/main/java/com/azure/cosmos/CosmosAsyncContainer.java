e8e276e926dbcdf46cc6170b3c9aeeb58fa931b3
Omar Al-Safi <omarsmak@gmail.com>
2021-04-16 16:43:06 +0200

    Fix CosmosQueryRequestOptions null check in readAllItem method

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/CosmosAsyncContainer.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/CosmosAsyncContainer.java
@@ -971,14 +971,14 @@ public class CosmosAsyncContainer {
         PartitionKey partitionKey,
         CosmosQueryRequestOptions options,
         Class<T> classType) {
-
+        final CosmosQueryRequestOptions requestOptions = options == null ? new CosmosQueryRequestOptions() : options;
         return UtilBridgeInternal.createCosmosPagedFlux(pagedFluxOptions -> {
             pagedFluxOptions.setTracerAndTelemetryInformation(this.readAllItemsSpanName, database.getId(),
                 this.getId(), OperationType.ReadFeed, ResourceType.Document, this.getDatabase().getClient());
-            setContinuationTokenAndMaxItemCount(pagedFluxOptions, options);
+            setContinuationTokenAndMaxItemCount(pagedFluxOptions, requestOptions);
             return getDatabase()
                 .getDocClientWrapper()
-                .readAllDocuments(getLink(), partitionKey, options)
+                .readAllDocuments(getLink(), partitionKey, requestOptions)
                 .map(response -> prepareFeedResponse(response, false, classType));
         });
     }
