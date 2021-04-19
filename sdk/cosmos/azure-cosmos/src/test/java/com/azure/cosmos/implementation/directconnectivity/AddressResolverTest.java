97c468b5b8560526742cd54bf3d24091dce6769d
Kushagra Thapar <kuthapar@microsoft.com>
2021-04-19 10:09:32 -0700

    Updated mockito API names

--- sdk/cosmos/azure-cosmos/src/test/java/com/azure/cosmos/implementation/directconnectivity/AddressResolverTest.java
+++ sdk/cosmos/azure-cosmos/src/test/java/com/azure/cosmos/implementation/directconnectivity/AddressResolverTest.java
@@ -321,7 +321,7 @@ public class AddressResolverTest {
         this.collectionCacheRefreshedCount = 0;
 
         Mockito.doAnswer(invocationOnMock -> {
-            RxDocumentServiceRequest request = invocationOnMock.getArgument(1, RxDocumentServiceRequest.class);
+            RxDocumentServiceRequest request = invocationOnMock.getArgumentAt(1, RxDocumentServiceRequest.class);
             if (request.forceNameCacheRefresh && collectionAfterRefresh != null) {
                 currentCollection.setValue(collectionAfterRefresh);
                 AddressResolverTest.this.collectionCacheRefreshedCount++;
@@ -354,8 +354,8 @@ public class AddressResolverTest {
         this.routingMapRefreshCount = new HashMap<>();
 
         Mockito.doAnswer(invocationOnMock -> {
-            String collectionRid = invocationOnMock.getArgument(1, String.class);
-            CollectionRoutingMap previousValue = invocationOnMock.getArgument(2, CollectionRoutingMap.class);
+            String collectionRid = invocationOnMock.getArgumentAt(1, String.class);
+            CollectionRoutingMap previousValue = invocationOnMock.getArgumentAt(2, CollectionRoutingMap.class);
 
             return collectionRoutingMapCache.tryLookupAsync(null, collectionRid, previousValue, false, null);
         }).when(this.collectionRoutingMapCache).tryLookupAsync(
@@ -366,8 +366,8 @@ public class AddressResolverTest {
 
         // Refresh case
         Mockito.doAnswer(invocationOnMock -> {
-            String collectionRid = invocationOnMock.getArgument(1, String.class);
-            CollectionRoutingMap previousValue = invocationOnMock.getArgument(2, CollectionRoutingMap.class);
+            String collectionRid = invocationOnMock.getArgumentAt(1, String.class);
+            CollectionRoutingMap previousValue = invocationOnMock.getArgumentAt(2, CollectionRoutingMap.class);
 
             if (previousValue == null) {
                 return Mono.just(new Utils.ValueHolder<>(currentRoutingMap.get(collectionRid)));
@@ -414,9 +414,9 @@ public class AddressResolverTest {
         // No refresh case
         //
         Mockito.doAnswer(invocationOnMock -> {
-            RxDocumentServiceRequest request = invocationOnMock.getArgument(0, RxDocumentServiceRequest.class);
-            PartitionKeyRangeIdentity pkri = invocationOnMock.getArgument(1, PartitionKeyRangeIdentity.class);
-            Boolean forceRefresh = invocationOnMock.getArgument(2, Boolean.class);
+            RxDocumentServiceRequest request = invocationOnMock.getArgumentAt(0, RxDocumentServiceRequest.class);
+            PartitionKeyRangeIdentity pkri = invocationOnMock.getArgumentAt(1, PartitionKeyRangeIdentity.class);
+            Boolean forceRefresh = invocationOnMock.getArgumentAt(2, Boolean.class);
 
             if (!forceRefresh) {
                 return Mono.just(new Utils.ValueHolder<>(currentAddresses.get(findMatchingServiceIdentity(currentAddresses, pkri))));
