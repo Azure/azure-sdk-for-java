eedb7dc41bdfbe54808b57712acb5859ea69ff25
Weidong Xu <weidxu@microsoft.com>
2021-04-19 17:00:37 +0800

    record

--- sdk/resourcemanager/azure-resourcemanager-samples/src/main/java/com/azure/resourcemanager/compute/samples/CreateVirtualMachineUsingCustomImageFromVM.java
+++ sdk/resourcemanager/azure-resourcemanager-samples/src/main/java/com/azure/resourcemanager/compute/samples/CreateVirtualMachineUsingCustomImageFromVM.java
@@ -52,7 +52,7 @@ public final class CreateVirtualMachineUsingCustomImageFromVM {
         final String publicIpDnsLabel = Utils.randomResourceName(azureResourceManager, "pip", 15);
         final String userName = "tirekicker";
         final String password = Utils.password();
-        final Region region = Region.US_WEST_CENTRAL;
+        final Region region = Region.US_WEST2;
 
         final String apacheInstallScript = "https://raw.githubusercontent.com/Azure/azure-sdk-for-java/master/sdk/resourcemanager/azure-resourcemanager-samples/src/main/resources/install_apache.sh";
         final String apacheInstallCommand = "bash install_apache.sh";
