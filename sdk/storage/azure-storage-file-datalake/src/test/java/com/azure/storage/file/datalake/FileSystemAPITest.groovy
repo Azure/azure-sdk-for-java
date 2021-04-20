e77936e54f2abce5b1b66514ba6fa6b6eef726c4
gapra <gapra@microsoft.com>
2021-04-20 12:45:49 -0700

    trying again

--- sdk/storage/azure-storage-file-datalake/src/test/java/com/azure/storage/file/datalake/FileSystemAPITest.groovy
+++ sdk/storage/azure-storage-file-datalake/src/test/java/com/azure/storage/file/datalake/FileSystemAPITest.groovy
@@ -20,7 +20,6 @@ import com.azure.storage.file.datalake.models.PathItem
 import com.azure.storage.file.datalake.models.PublicAccessType
 import spock.lang.Unroll
 
-import java.nio.charset.StandardCharsets
 import java.time.OffsetDateTime
 import java.time.ZoneId
 import java.time.temporal.ChronoUnit
@@ -1073,7 +1072,7 @@ class FileSystemAPITest extends APISpec {
         def deletionId = fsc.listDeletedPaths().first().getDeletionId()
 
         when:
-        fsc.restorePath(URLEncoder.encode(dir.getDirectoryName(), StandardCharsets.UTF_8), deletionId)
+        fsc.restorePath(Utility.urlEncode(dir.getDirectoryName()), deletionId)
 
         then:
         dir.getProperties() != null
