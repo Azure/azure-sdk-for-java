3e97d8555a5da20c37f4d7068536306c1ebd71c5
Sima Zhu <sizhu@microsoft.com>
2021-04-22 17:30:03 -0700

    Have the right packages

--- eng/scripts/Language-Settings.ps1
+++ eng/scripts/Language-Settings.ps1
@@ -325,9 +325,9 @@ function Update-java-CIConfig($ciRepo, $locationInDocRepo)
   }
   # remove package from package.json
   for ($i=0; $i -lt $jsonRepresentation.Length; $i++) {
-    $packages = $jsonRepresentation[$i]
+    $packages = $jsonRepresentation[$i].packages
     for ($j=0; $j -lt $packages.Length; $j++) {
-      Write-Host "$($packages[$j].packageGroupId):$($packages[$j].packageArtifactId)"
+      Write-Host $packages
       if ($ignorePackages -contains "$($packages[$j].packageGroupId):$($packages[$j].packageArtifactId)") {
         Write-Host "The package $($packages[$j].packageGroupId):$($packages[$j].packageGroupId) exists in ignore list."
         $jsonRepresentation[$i].packages -= $packages[$j]
