2b1bcfa2c9bf778c423617c0b095427eaff71e84
Fabian Meiswinkel <fabian@meiswinkel.com>
2021-04-27 23:24:13 +0000

    Fixing StackOverflow error due to unintentional recursion

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/changefeed/implementation/ChangeFeedStartFromNowImpl.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/changefeed/implementation/ChangeFeedStartFromNowImpl.java
@@ -22,11 +22,6 @@ class ChangeFeedStartFromNowImpl extends ChangeFeedStartFromInternal {
             this,
             Constants.Properties.CHANGE_FEED_START_FROM_TYPE,
             ChangeFeedStartFromTypes.NOW);
-
-        // TODO @fabianm Remove as soon as root caused - https://github.com/Azure/azure-sdk-for-java/issues/20635
-        // Just used to trigger the additional detection for invalid json (duplicate Type property
-        // "StartFrom":{"Type":"NOW","Type":"NOW"}
-        this.toJson();
     }
 
     @Override
