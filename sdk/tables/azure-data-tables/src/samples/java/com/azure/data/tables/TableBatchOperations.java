813ce9e5d184b6dacb657437a19933a3f18a80f5
Victor Colin Amador <victor.y.asi@gmail.com>
2021-04-28 16:05:08 -0500

    Updated the TableBatchOperations sample.

--- sdk/tables/azure-data-tables/src/samples/java/com/azure/data/tables/TableBatchOperations.java
+++ sdk/tables/azure-data-tables/src/samples/java/com/azure/data/tables/TableBatchOperations.java
@@ -5,6 +5,7 @@ package com.azure.data.tables;
 
 import com.azure.data.tables.models.BatchOperationResponse;
 import com.azure.data.tables.models.TableEntity;
+import com.azure.data.tables.models.TableOperationsTransactionalBatch;
 
 import java.util.List;
 
@@ -26,45 +27,45 @@ public class TableBatchOperations {
 
         // Now let's create a batch to add operations to.
         String partitionKey = "markers4";
-        TableBatch tableBatch = tableClient.createBatch(partitionKey);
+        TableOperationsTransactionalBatch batch = new TableOperationsTransactionalBatch(partitionKey);
 
         // Let's create a couple entities.
         String firstEntityRowKey = "m001";
         String secondEntityRowKey = "m002";
 
         TableEntity firstEntity = new TableEntity(partitionKey, firstEntityRowKey)
-            .addProperty("Brand", "Crayola")
-            .addProperty("Color", "Red");
-        tableBatch.createEntity(firstEntity);
+            .setProperty("Brand", "Crayola")
+            .setProperty("Color", "Red");
+        batch.createEntity(firstEntity);
         System.out.printf("Added create operation to batch for entity with partition key: %s, and row key: %s\n",
             partitionKey, firstEntityRowKey);
 
         TableEntity secondEntity = new TableEntity(partitionKey, secondEntityRowKey)
-            .addProperty("Brand", "Crayola")
-            .addProperty("Color", "Blue");
-        tableBatch.createEntity(secondEntity);
+            .setProperty("Brand", "Crayola")
+            .setProperty("Color", "Blue");
+        batch.createEntity(secondEntity);
         System.out.printf("Added create operation to batch for entity with partition key: %s, and row key: %s\n",
             partitionKey, secondEntityRowKey);
 
         // Now let's update a different entity.
         String rowKeyForUpdate = "m003";
         TableEntity entityToUpdate = new TableEntity(partitionKey, rowKeyForUpdate)
-            .addProperty("Brand", "Crayola")
-            .addProperty("Color", "Blue");
-        tableBatch.updateEntity(entityToUpdate);
+            .setProperty("Brand", "Crayola")
+            .setProperty("Color", "Blue");
+        batch.updateEntity(entityToUpdate);
         System.out.printf("Added update operation to batch for entity with partition key: %s, and row key: %s\n",
             partitionKey, rowKeyForUpdate);
 
         // And delete another one.
         String rowKeyForDelete = "m004";
         TableEntity entityToDelete = new TableEntity(partitionKey, rowKeyForDelete)
-            .addProperty("Brand", "Crayola")
-            .addProperty("Color", "Blue");
-        tableBatch.updateEntity(entityToDelete);
+            .setProperty("Brand", "Crayola")
+            .setProperty("Color", "Blue");
+        batch.updateEntity(entityToDelete);
         System.out.printf("Added delete operation to batch for entity with partition key: %s, and row key: %s\n",
             partitionKey, rowKeyForDelete);
 
         // Finally, let's submit the batch of operations and inspect all the responses.
-        List<BatchOperationResponse> batchOperationResponses = tableBatch.submitTransaction();
+        List<BatchOperationResponse> batchOperationResponses = tableClient.submitTransaction(batch);
     }
 }
