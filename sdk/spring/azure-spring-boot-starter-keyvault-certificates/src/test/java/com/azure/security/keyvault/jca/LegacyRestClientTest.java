1979d8a83acd56a1a56dc655f08633946b19eced
Rujun Chen <rujche@microsoft.com>
2021-04-28 17:13:19 +0800

    Fix pipeline failure.

--- sdk/spring/azure-spring-boot-starter-keyvault-certificates/src/test/java/com/azure/security/keyvault/jca/LegacyRestClientTest.java
+++ sdk/spring/azure-spring-boot-starter-keyvault-certificates/src/test/java/com/azure/security/keyvault/jca/LegacyRestClientTest.java
@@ -5,7 +5,6 @@ package com.azure.security.keyvault.jca;
 
 import org.junit.jupiter.api.Test;
 
-import static com.azure.security.keyvault.jca.LegacyRestClient.DEFAULT_USER_AGENT_VALUE_PREFIX;
 import static com.azure.security.keyvault.jca.LegacyRestClient.DEFAULT_VERSION;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
