8a3a296c4381d2e7de0327cfaa5efcd6d1d36c89
hemanttanwar <hetanwar@users.noreply.github.com>
2021-04-29 12:58:51 -0700

    fix check style error

--- sdk/servicebus/azure-messaging-servicebus/src/samples/java/com/azure/messaging/servicebus/ReceiveMessageUsingSasSample.java
+++ sdk/servicebus/azure-messaging-servicebus/src/samples/java/com/azure/messaging/servicebus/ReceiveMessageUsingSasSample.java
@@ -80,10 +80,10 @@ public class ReceiveMessageUsingSasSample {
         try {
             String stringToSign = URLEncoder.encode(resourceUri, StandardCharsets.UTF_8.toString()) + "\n" + expiry;
             String signature = getHMAC256(key, stringToSign);
-            sasToken = "SharedAccessSignature sr=" + URLEncoder.encode(resourceUri, StandardCharsets.UTF_8) + "&sig=" +
-                URLEncoder.encode(signature, StandardCharsets.UTF_8.toString()) + "&se=" + expiry + "&skn=" + keyName;
-        } catch(Exception e) {
-            System.err.println(" Could not url encode " +  e.getMessage());
+            sasToken = "SharedAccessSignature sr=" + URLEncoder.encode(resourceUri, StandardCharsets.UTF_8) + "&sig="
+                + URLEncoder.encode(signature, StandardCharsets.UTF_8.toString()) + "&se=" + expiry + "&skn=" + keyName;
+        } catch (Exception e) {
+            System.err.println(" Could not url encode " + e.getMessage());
         }
         return sasToken;
     }
