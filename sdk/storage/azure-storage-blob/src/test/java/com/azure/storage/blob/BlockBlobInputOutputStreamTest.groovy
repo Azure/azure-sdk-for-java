f0427d4439d1b1b42a2d792d544e8cb52e9b418e
Kamil Sobol <kasobol@microsoft.com>
2021-05-01 00:50:39 -0700

    wip

--- sdk/storage/azure-storage-blob/src/test/java/com/azure/storage/blob/BlockBlobInputOutputStreamTest.groovy
+++ sdk/storage/azure-storage-blob/src/test/java/com/azure/storage/blob/BlockBlobInputOutputStreamTest.groovy
@@ -264,7 +264,7 @@ class BlockBlobInputOutputStreamTest extends APISpec {
         assert randomBytes1 == randomBytes
     }
 
-    def "IS consistent read etag user provides version client chooses etag"() {
+    def "IS consistent read etag provided version chosen etag"() {
         setup:
         int length = Constants.KB
         byte[] randomBytes = getRandomByteArray(length)
@@ -394,7 +394,7 @@ class BlockBlobInputOutputStreamTest extends APISpec {
         assert randomBytes2 == randomBytes
     }
 
-    def "IS consistent read version user provides etag client chooses version"() {
+    def "IS consistent read version provided chosen version"() {
         setup:
         int length = Constants.KB
         byte[] randomBytes = getRandomByteArray(length)
