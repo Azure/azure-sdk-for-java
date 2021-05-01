21b96000d33f14167a98769c680cbf718c491635
Kamil Sobol <kasobol@microsoft.com>
2021-05-01 11:14:54 -0700

    these require exclusive access.

--- sdk/storage/azure-storage-blob-nio/src/test/java/com/azure/storage/blob/nio/AzurePathTest.groovy
+++ sdk/storage/azure-storage-blob-nio/src/test/java/com/azure/storage/blob/nio/AzurePathTest.groovy
@@ -3,11 +3,12 @@
 
 package com.azure.storage.blob.nio
 
-import spock.lang.Shared
+import spock.lang.ResourceLock
 import spock.lang.Unroll
 
 import java.nio.file.FileSystems
 
+@ResourceLock("AzurePathTest")
 class AzurePathTest extends APISpec {
     AzureFileSystem fs
 
