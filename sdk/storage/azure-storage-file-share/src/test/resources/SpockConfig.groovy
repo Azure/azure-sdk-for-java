f82bdc0fc8f678cfd54519a5774a7f16d21e8f65
Kamil Sobol <kasobol@microsoft.com>
2021-05-10 07:44:23 -0700

    let's see.

--- sdk/storage/azure-storage-file-share/src/test/resources/SpockConfig.groovy
+++ sdk/storage/azure-storage-file-share/src/test/resources/SpockConfig.groovy
@@ -1,6 +1,11 @@
+def isParallelEnabled = System.getenv().getOrDefault("AZURE_STORAGE_TESTS_PARALLEL_ENABLED", "true").toBoolean()
+def factor = System.getenv().getOrDefault("AZURE_STORAGE_TESTS_PARALLEL_FACTOR", "1").toBigDecimal()
+
+printf("Parallelization is enabled=%b, factor=%.2f, config=%s%n" , isParallelEnabled, factor, this.class.protectionDomain.codeSource.location)
+
 runner {
     parallel {
-        enabled true
-        dynamic(5)
+        enabled isParallelEnabled
+        dynamic(factor)
     }
 }
