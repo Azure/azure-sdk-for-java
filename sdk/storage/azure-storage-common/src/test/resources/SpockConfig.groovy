fc0c86692ccbccc5ca4655faf4f213b5b3532258
Kamil Sobol <kasobol@microsoft.com>
2021-05-10 08:29:26 -0700

    factor 2.

--- sdk/storage/azure-storage-common/src/test/resources/SpockConfig.groovy
+++ sdk/storage/azure-storage-common/src/test/resources/SpockConfig.groovy
@@ -1,5 +1,5 @@
 def isParallelEnabled = System.getenv().getOrDefault("AZURE_STORAGE_TESTS_PARALLEL_ENABLED", "true").toBoolean()
-def factor = System.getenv().getOrDefault("AZURE_STORAGE_TESTS_PARALLEL_FACTOR", "1").toBigDecimal()
+def factor = System.getenv().getOrDefault("AZURE_STORAGE_TESTS_PARALLEL_FACTOR", "2").toBigDecimal()
 def numberOfProcessors = Runtime.getRuntime().availableProcessors()
 
 printf("Parallelization is enabled=%b, factor=%.2f, processors=%d config=%s%n",
