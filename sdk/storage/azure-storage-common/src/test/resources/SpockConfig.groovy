319856fcacfe0f17509471379489c1da0d83d341
Kamil Sobol <kasobol@microsoft.com>
2021-05-10 08:27:49 -0700

    centralized spock config.

--- sdk/storage/azure-storage-common/src/test/resources/SpockConfig.groovy
+++ sdk/storage/azure-storage-common/src/test/resources/SpockConfig.groovy
@@ -1,7 +1,9 @@
 def isParallelEnabled = System.getenv().getOrDefault("AZURE_STORAGE_TESTS_PARALLEL_ENABLED", "true").toBoolean()
 def factor = System.getenv().getOrDefault("AZURE_STORAGE_TESTS_PARALLEL_FACTOR", "1").toBigDecimal()
+def numberOfProcessors = Runtime.getRuntime().availableProcessors()
 
-printf("Parallelization is enabled=%b, factor=%.2f, config=%s%n" , isParallelEnabled, factor, this.class.protectionDomain.codeSource.location)
+printf("Parallelization is enabled=%b, factor=%.2f, processors=%d config=%s%n",
+    isParallelEnabled, factor, numberOfProcessors, this.class.protectionDomain.codeSource.location)
 
 runner {
     parallel {
