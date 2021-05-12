9613a7dc47e6632f5e577ff5168c6cc02dec80cb
ZhuXiaoBing <zhu13865245671@163.com>
2021-05-12 11:37:11 +0800

    * Add ServiceBus binder integration tests.

---
+++ sdk/spring/azure-spring-cloud-test-servicebus-binder/src/test/java/com/azure/spring/sample/servicebus/binder/ServiceBusQueueAndTopicBinderIT.java
@@ -0,0 +1,104 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+package com.azure.spring.sample.servicebus.binder;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.GenericMessage;
+import org.springframework.test.context.TestPropertySource;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
+
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+@SpringBootTest(classes = { ServiceBusQueueAndTopicBinderIT.TestQueueConfig.class,
+    ServiceBusQueueAndTopicBinderIT.TestTopicConfig.class })
+@TestPropertySource(locations = "classpath:application.yml")
+public class ServiceBusQueueAndTopicBinderIT {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ServiceBusQueueAndTopicBinderIT.class);
+
+    private static String message = UUID.randomUUID().toString();
+
+    private static final AtomicInteger count = new AtomicInteger(0);
+
+    @Autowired
+    private Sinks.Many<Message<String>> manyQueue;
+
+    @Autowired
+    private Sinks.Many<Message<String>> manyTopic;
+
+    @EnableAutoConfiguration
+    public static class TestQueueConfig {
+
+        @Bean
+        public Sinks.Many<Message<String>> manyQueue() {
+            return Sinks.many().unicast().onBackpressureBuffer();
+        }
+
+        @Bean
+        public Supplier<Flux<Message<String>>> queueSupply(Sinks.Many<Message<String>> manyQueue) {
+            return () -> manyQueue.asFlux()
+                                  .doOnNext(m -> LOGGER.info("Manually sending message {}", m))
+                                  .doOnError(t -> LOGGER.error("Error encountered", t));
+        }
+
+        @Bean
+        public Consumer<Message<String>> queueConsume() {
+            return message -> {
+                LOGGER.info("New message received: '{}'", message);
+                Assertions.assertEquals(message.getPayload(), ServiceBusQueueAndTopicBinderIT.message);
+                count.addAndGet(1);
+            };
+        }
+    }
+
+    @EnableAutoConfiguration
+    public static class TestTopicConfig {
+
+        @Bean
+        public Sinks.Many<Message<String>> manyTopic() {
+            return Sinks.many().unicast().onBackpressureBuffer();
+        }
+
+        @Bean
+        public Supplier<Flux<Message<String>>> topicSupply(Sinks.Many<Message<String>> manyTopic) {
+            return () -> manyTopic.asFlux()
+                                  .doOnNext(m -> LOGGER.info("Manually sending message {}", m))
+                                  .doOnError(t -> LOGGER.error("Error encountered", t));
+        }
+
+        @Bean
+        public Consumer<Message<String>> topicConsume() {
+            return message -> {
+                LOGGER.info("New message received: '{}'", message);
+                Assertions.assertEquals(message.getPayload(), ServiceBusQueueAndTopicBinderIT.message);
+                count.addAndGet(1);
+            };
+        }
+    }
+
+    @Test
+    public void testSendAndReceiveMessage() throws InterruptedException {
+        Thread.sleep(1500);
+        GenericMessage<String> stringGenericMessage = new GenericMessage<>(message);
+        manyQueue.emitNext(stringGenericMessage, Sinks.EmitFailureHandler.FAIL_FAST);
+        Thread.sleep(5000);
+        Assertions.assertEquals(1, count.get());
+        Thread.sleep(1500);
+        manyTopic.emitNext(stringGenericMessage, Sinks.EmitFailureHandler.FAIL_FAST);
+        Thread.sleep(5000);
+        Assertions.assertEquals(2, count.get());
+    }
+
+}
