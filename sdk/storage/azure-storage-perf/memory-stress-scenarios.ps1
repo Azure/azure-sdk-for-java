fac73ea07ce9fee12d5c458084f0543bb7845634
Kamil Sobol <kasobol@microsoft.com>
2021-05-17 17:09:29 -0700

    try this.

--- sdk/storage/azure-storage-perf/memory-stress-scenarios.ps1
+++ sdk/storage/azure-storage-perf/memory-stress-scenarios.ps1
@@ -1,3 +1,8 @@
 $JavaPath = Join-Path -Path $Env:JAVA_HOME -ChildPath "bin" | Join-Path -ChildPath "java"
+$TargetDirPath = Join-Path -Path $PSScriptRoot -ChildPath "target"
+$PerfJarPath = Get-ChildItem -Path (Join-Path -Path $TargetDirPath -ChildPath "*") -Include azure-storage-perf-*-jar-with-dependencies.jar -File
 
 Invoke-Expression "& '$JavaPath' -version"
+
+Invoke-Expression "& '$JavaPath' -Xms100m -Xmx100m -jar '$PerfJarPath' uploadoutputstream --warmup 0 --duration 120 --size 1048576000 --sync"
+Invoke-Expression "& '$JavaPath' -Xms50m -Xmx50m -jar '$PerfJarPath' uploadblob --warmup 0 --duration 120 --size 1048576000"
