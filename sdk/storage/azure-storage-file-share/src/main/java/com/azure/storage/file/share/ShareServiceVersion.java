768e313e1c965d8a1401d92726eae91409c9f4ea
Gauri Prasad <51212198+gapra-msft@users.noreply.github.com>
2021-05-18 14:29:15 -0700

    Set up STG78 branch (#21634)

--- sdk/storage/azure-storage-file-share/src/main/java/com/azure/storage/file/share/ShareServiceVersion.java
+++ sdk/storage/azure-storage-file-share/src/main/java/com/azure/storage/file/share/ShareServiceVersion.java
@@ -16,7 +16,8 @@ public enum ShareServiceVersion implements ServiceVersion {
     V2020_02_10("2020-02-10"),
     V2020_04_08("2020-04-08"),
     V2020_06_12("2020-06-12"),
-    V2020_08_04("2020-08-04");
+    V2020_08_04("2020-08-04"),
+    V2020_10_02("2020-10-02");
 
     private final String version;
 
@@ -38,6 +39,6 @@ public enum ShareServiceVersion implements ServiceVersion {
      * @return the latest {@link ShareServiceVersion}
      */
     public static ShareServiceVersion getLatest() {
-        return V2020_08_04;
+        return V2020_10_02;
     }
 }
