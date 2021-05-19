e29ac62b1b112e2e0b2b87e267d8ca294081ae2c
shafang <Shawn.Fang@microsoft.com>
2021-05-19 12:48:54 -0700

    PII and healthcare endpoint default value is true

--- sdk/textanalytics/azure-ai-textanalytics/src/main/java/com/azure/ai/textanalytics/models/AnalyzeHealthcareEntitiesOptions.java
+++ sdk/textanalytics/azure-ai-textanalytics/src/main/java/com/azure/ai/textanalytics/models/AnalyzeHealthcareEntitiesOptions.java
@@ -43,8 +43,7 @@ public final class AnalyzeHealthcareEntitiesOptions extends TextAnalyticsRequest
     /**
      * Set the value of {@code disableServiceLogs}.
      *
-     * @param disableServiceLogs The default value of this property is 'false', except for methods like
-     * 'beginAnalyzeHealthcareEntities' and 'recognizePiiEntities'. This means, Text Analytics service logs
+     * @param disableServiceLogs The default value of this property is 'true'. This means, Text Analytics service logs
      * your input text for 48 hours, solely to allow for troubleshooting issues. Setting this property to true,
      * disables input logging and may limit our ability to investigate issues that occur.
      *
