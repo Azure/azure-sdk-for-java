d58ea0b85b5eeb54639cf9aa7646237a2923d311
gapra <gapra@microsoft.com>
2021-05-24 12:07:03 -0700

    Start preparing for stg78 tests

--- sdk/storage/azure-storage-blob/src/test/java/com/azure/storage/blob/ImmutableStorageWithVersioningTest.groovy
+++ sdk/storage/azure-storage-blob/src/test/java/com/azure/storage/blob/ImmutableStorageWithVersioningTest.groovy
@@ -3,9 +3,30 @@
 
 package com.azure.storage.blob
 
-import com.azure.storage.common.test.shared.extensions.RequiredServiceVersion
+import com.azure.core.credential.TokenCredential
+import com.azure.identity.EnvironmentCredentialBuilder
+import com.azure.resourcemanager.storage.generated.fluent.models.BlobContainerInner
+import com.azure.resourcemanager.storage.generated.implementation.StorageManagementClientBuilder
+import com.azure.resourcemanager.storage.generated.implementation.StorageManagementClientImpl
+import com.azure.resourcemanager.storage.generated.models.PublicAccess
 
 class ImmutableStorageWithVersioningTest extends APISpec {
 
+    BlobContainerClient containerClient;
+
+
+    def setup() {
+
+        TokenCredential credential = new EnvironmentCredentialBuilder().build()
+//        StorageManager manager = StorageManager.authenticate(credential, new AzureProfile(AzureEnvironment.AZURE))
+
+        StorageManagementClientImpl client = new StorageManagementClientBuilder()
+            .buildClient()
+
+        def resourceGroupName = "XClient"
+        def inner = new BlobContainerInner().withPublicAccess(PublicAccess.CONTAINER)
+//        client.getBlobContainers().createWithResponse(resourceGroupName, env.primaryAccount.name, generateContainerName())
+
+    }
 
 }
