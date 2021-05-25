b81caedc4b6226578000532c4642c4edbe2894c9
John Mannix <jmannix@onetrust.com>
2021-05-25 10:37:08 +0100

    Add autoscale RU support for azure-spring-data-cosmos
    Resolves #12711

---
+++ sdk/cosmos/azure-spring-data-cosmos-test/src/test/java/com/azure/spring/data/cosmos/domain/AutoScaleSample.java
@@ -0,0 +1,25 @@
+package com.azure.spring.data.cosmos.domain;
+
+import com.azure.spring.data.cosmos.common.TestConstants;
+import com.azure.spring.data.cosmos.core.mapping.Container;
+
+@Container(ru = TestConstants.AUTOSCALE_MAX_THROUGHPUT, autoScale = true)
+public class AutoScaleSample {
+    private String id;
+
+    public AutoScaleSample() {
+    }
+
+    public AutoScaleSample(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+}
