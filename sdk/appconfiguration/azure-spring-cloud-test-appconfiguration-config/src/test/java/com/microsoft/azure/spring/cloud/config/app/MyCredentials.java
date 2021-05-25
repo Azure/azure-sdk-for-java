d3c8374f9f16fb6c0ed15d21ea46f35d566b72b2
mrm9084 <mrm9084@gmail.com>
2021-05-25 10:17:01 -0700

    Added Basic Test and ARM file

---
+++ sdk/appconfiguration/azure-spring-cloud-test-appconfiguration-config/src/test/java/com/microsoft/azure/spring/cloud/config/app/MyCredentials.java
@@ -0,0 +1,25 @@
+package com.microsoft.azure.spring.cloud.config.app;
+
+import com.azure.core.credential.TokenCredential;
+import com.azure.identity.EnvironmentCredentialBuilder;
+//import com.azure.identity.AzureCliCredentialBuilder;
+import com.microsoft.azure.spring.cloud.config.AppConfigurationCredentialProvider;
+import com.microsoft.azure.spring.cloud.config.KeyVaultCredentialProvider;
+
+public class MyCredentials implements AppConfigurationCredentialProvider, KeyVaultCredentialProvider{
+
+    @Override
+    public TokenCredential getKeyVaultCredential(String uri) {
+        return buildCredential();
+    }
+
+    @Override
+    public TokenCredential getAppConfigCredential(String uri) {
+        return buildCredential();
+    }
+
+    TokenCredential buildCredential() {
+            return  new EnvironmentCredentialBuilder().build();
+    }
+
+}
