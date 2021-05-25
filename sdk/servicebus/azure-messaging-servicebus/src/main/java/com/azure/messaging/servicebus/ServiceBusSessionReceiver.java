983f25a784b1ace671d4adcd3ecb6bca80b0a1d9
Yijun Xie <48257664+YijunXieMS@users.noreply.github.com>
2021-05-25 15:42:13 -0700

    Implements AutoClosable

--- sdk/servicebus/azure-messaging-servicebus/src/main/java/com/azure/messaging/servicebus/ServiceBusSessionReceiver.java
+++ sdk/servicebus/azure-messaging-servicebus/src/main/java/com/azure/messaging/servicebus/ServiceBusSessionReceiver.java
@@ -29,7 +29,7 @@ import java.util.function.Function;
 /**
  * Represents an session that is received when "any" session is accepted from the service.
  */
-class ServiceBusSessionReceiver implements AsyncAutoCloseable {
+class ServiceBusSessionReceiver implements AsyncAutoCloseable, AutoCloseable {
     private final AtomicBoolean isDisposed = new AtomicBoolean();
     private final LockContainer<OffsetDateTime> lockContainer;
     private final AtomicReference<OffsetDateTime> sessionLockedUntil = new AtomicReference<>();
@@ -212,6 +212,7 @@ class ServiceBusSessionReceiver implements AsyncAutoCloseable {
         return receiveLink.closeAsync().doFinally(signal -> subscriptions.dispose());
     }
 
+    @Override
     public void close() {
         closeAsync().block(retryOptions.getTryTimeout());
     }
