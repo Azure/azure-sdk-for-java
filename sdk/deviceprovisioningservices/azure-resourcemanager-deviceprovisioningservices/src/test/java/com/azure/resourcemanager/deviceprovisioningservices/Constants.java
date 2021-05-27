82c868e8270c7ddb6d1bb88056c4e8e615df96f8
timtay-microsoft <timtay@microsoft.com>
2021-05-26 17:37:24 -0700

    Add tests for device provisioning resource manager

--- sdk/deviceprovisioningservices/azure-resourcemanager-deviceprovisioningservices/src/test/java/com/azure/resourcemanager/deviceprovisioningservices/Constants.java
+++ sdk/deviceprovisioningservices/azure-resourcemanager-deviceprovisioningservices/src/test/java/com/azure/resourcemanager/deviceprovisioningservices/Constants.java
@@ -3,22 +3,46 @@
 
 package com.azure.resourcemanager.deviceprovisioningservices;
 
+import com.azure.resourcemanager.deviceprovisioningservices.models.AllocationPolicy;
+import com.azure.resourcemanager.deviceprovisioningservices.models.IotDpsSku;
+import com.azure.resourcemanager.deviceprovisioningservices.models.IotDpsSkuInfo;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Constants
 {
-    public static final String ACCESS_KEY_NAME = "provisioningserviceowner";
-
-    public static final String DEFAULT_LOCATION = "WestUS2";
+    static final String DEFAULT_LOCATION = "WestUS2";
+    static final String OWNER_ACCESS_KEY_NAME = "provisioningserviceowner";
+    static final String IOTHUB_OWNER_ACCESS_KEY_NAME = "iothubowner";
 
     public static class DefaultSku
     {
-        public static final String NAME = "S1";
-        public static final int CAPACITY = 1;
-        public static final String TIER = "S1";
+        static final String NAME = "S1";
+        static final Long CAPACITY = 1L;
+        static IotDpsSkuInfo INSTANCE = new IotDpsSkuInfo()
+            .withCapacity(Constants.DefaultSku.CAPACITY)
+            .withName(IotDpsSku.fromString(Constants.DefaultSku.NAME));
     }
 
-    public static final String[] ALLOCATION_POLICIES = { "Hashed", "GeoLatency", "Static" };
+    public static class Certificate
+    {
+        static final String CONTENT =
+            "MIIBvjCCAWOgAwIBAgIQG6PoBFT6GLJGNKn/EaxltTAKBggqhkjOPQQDAjAcMRow"
+            + "GAYDVQQDDBFBenVyZSBJb1QgUm9vdCBDQTAeFw0xNzExMDMyMDUyNDZaFw0xNzEy"
+            + "MDMyMTAyNDdaMBwxGjAYBgNVBAMMEUF6dXJlIElvVCBSb290IENBMFkwEwYHKoZI"
+            + "zj0CAQYIKoZIzj0DAQcDQgAE+CgpnW3K+KRNIi/U6Zqe/Al9m8PExHX2KgakmGTf"
+            + "E04nNBwnSoygWb0ekqpT+Lm+OP56LMMe9ynVNryDEr9OSKOBhjCBgzAOBgNVHQ8B"
+            + "Af8EBAMCAgQwHQYDVR0lBBYwFAYIKwYBBQUHAwIGCCsGAQUFBwMBMB8GA1UdEQQY"
+            + "MBaCFENOPUF6dXJlIElvVCBSb290IENBMBIGA1UdEwEB/wQIMAYBAf8CAQwwHQYD"
+            + "VR0OBBYEFDjiklfHQzw1G0A33BcmRQTjAivTMAoGCCqGSM49BAMCA0kAMEYCIQCt"
+            + "jJ4bAvoYuDhwr92Kk+OkvpPF+qBFiRfrA/EC4YGtzQIhAO79WPtbUnBQ5fsQnW2a"
+            + "UAT4yJGWL+7l4/qfmqblb96n";
 
-    public static final int ARM_ATTEMPT_WAIT_MS = 500;
-    public static final int RANDOM_ALLOCATION_WEIGHT = 870084357;
-    public static final int ARM_ATTEMPT_LIMIT = 5;
+        static final String NAME = "DPStestCert";
+        static final String SUBJECT = "Azure IoT Root CA";
+        static final String THUMBPRINT = "9F0983E8F2DB2DB3582997FEF331247D872DEE32";
+    }
+
+    static final List<AllocationPolicy> ALLOCATION_POLICIES = Arrays.asList(AllocationPolicy.GEO_LATENCY, AllocationPolicy.HASHED, AllocationPolicy.STATIC);
 }
