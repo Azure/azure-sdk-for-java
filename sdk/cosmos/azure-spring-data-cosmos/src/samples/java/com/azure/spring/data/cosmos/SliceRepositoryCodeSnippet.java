56b5a0a5daa8d9c23ed6ef72ed42091ab9452990
John Mannix <jmannix@onetrust.com>
2021-05-28 13:17:44 +0100

    Add Slice<T> support for azure-spring-data-cosmos
    Feedback from PR
    
    Resolves #20950

--- sdk/cosmos/azure-spring-data-cosmos/src/samples/java/com/azure/spring/data/cosmos/SliceRepositoryCodeSnippet.java
+++ sdk/cosmos/azure-spring-data-cosmos/src/samples/java/com/azure/spring/data/cosmos/SliceRepositoryCodeSnippet.java
@@ -1,4 +1,12 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 package com.azure.spring.data.cosmos;
+
+/**
+ * WARNING: MODIFYING THIS FILE WILL REQUIRE CORRESPONDING UPDATES TO README.md FILE. LINE NUMBERS
+ * ARE USED TO EXTRACT APPROPRIATE CODE SEGMENTS FROM THIS FILE. ADD NEW CODE AT THE BOTTOM TO AVOID CHANGING
+ * LINE NUMBERS OF EXISTING CODE SAMPLES.
+ */
 
 import com.azure.spring.data.cosmos.core.query.CosmosPageRequest;
 import org.springframework.beans.factory.annotation.Autowired;
