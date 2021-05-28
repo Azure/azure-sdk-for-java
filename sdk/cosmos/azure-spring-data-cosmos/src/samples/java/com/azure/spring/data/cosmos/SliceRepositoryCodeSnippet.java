be6631610c44b4062aaa00fdab1af787964be462
Kushagra Thapar <kuthapar@microsoft.com>
2021-05-28 12:40:22 -0700

    Updated readme and fixed code snippet unused import

--- sdk/cosmos/azure-spring-data-cosmos/src/samples/java/com/azure/spring/data/cosmos/SliceRepositoryCodeSnippet.java
+++ sdk/cosmos/azure-spring-data-cosmos/src/samples/java/com/azure/spring/data/cosmos/SliceRepositoryCodeSnippet.java
@@ -10,7 +10,6 @@ package com.azure.spring.data.cosmos;
 
 import com.azure.spring.data.cosmos.core.query.CosmosPageRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
