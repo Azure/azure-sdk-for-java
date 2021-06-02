ed7e7d9868f493cfa3217beff8601a37ee495cb2
Sameeksha Vaity <sameeksha.v91@gmail.com>
2021-06-02 16:01:41 -0700

    Fix pipeline error - export admin models (#8)

--- sdk/metricsadvisor/azure-ai-metricsadvisor/src/main/java/module-info.java
+++ sdk/metricsadvisor/azure-ai-metricsadvisor/src/main/java/module-info.java
@@ -6,7 +6,7 @@ module com.azure.ai.metricsadvisor {
 
     exports com.azure.ai.metricsadvisor;
     exports com.azure.ai.metricsadvisor.models;
-    exports com.azure.ai.metricsadvisor.administration;
+    exports com.azure.ai.metricsadvisor.administration.models;
 
     opens com.azure.ai.metricsadvisor.implementation to com.fasterxml.jackson.databind;
     opens com.azure.ai.metricsadvisor.administration.models to com.fasterxml.jackson.databind;
