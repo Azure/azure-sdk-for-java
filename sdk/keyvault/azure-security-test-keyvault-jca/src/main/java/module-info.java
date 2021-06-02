6771ffb1d831c44c67ef2db8d176843c353e8c75
Michael Qi <v-michaelqi@microsoft.com>
2021-06-02 09:57:22 +0800

    minor change

--- sdk/keyvault/azure-security-test-keyvault-jca/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-/**
- * This is for testing the modularization of module azure.security.keyvault.jca
- */
-module azure.security.test.keyvault.jca {
-    requires azure.security.keyvault.jca;
-}
