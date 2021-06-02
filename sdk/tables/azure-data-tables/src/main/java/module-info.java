475bd88e2a7ccd8a3e0320b03595d0b5e841f86c
Victor Colin Amador <victor.y.asi@gmail.com>
2021-06-02 18:53:18 -0500

    Removed unnecessary exports for implementation packages in module-info.java

--- sdk/tables/azure-data-tables/src/main/java/module-info.java
+++ sdk/tables/azure-data-tables/src/main/java/module-info.java
@@ -9,9 +9,6 @@ module com.azure.data.tables {
     exports com.azure.data.tables.models;
     exports com.azure.data.tables.sas;
 
-    exports com.azure.data.tables.implementation to com.azure.core;
-    exports com.azure.data.tables.implementation.models to com.azure.core;
-
     // exporting some packages specifically for Jackson
     opens com.azure.data.tables to com.fasterxml.jackson.databind, com.azure.core;
     opens com.azure.data.tables.implementation to com.fasterxml.jackson.databind, com.azure.core;
