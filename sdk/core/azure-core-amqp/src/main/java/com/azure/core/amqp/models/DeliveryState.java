9db79903d918b8d42682ec3117ade49cf31914b9
Connie <conniey@microsoft.com>
2021-06-03 16:10:42 -0700

    Change deliverystate to expandable enum

--- sdk/core/azure-core-amqp/src/main/java/com/azure/core/amqp/models/DeliveryState.java
+++ sdk/core/azure-core-amqp/src/main/java/com/azure/core/amqp/models/DeliveryState.java
@@ -3,6 +3,8 @@
 
 package com.azure.core.amqp.models;
 
+import com.azure.core.util.ExpandableStringEnum;
+
 /**
  * States for a message delivery.
  *
@@ -11,15 +13,29 @@ package com.azure.core.amqp.models;
  * @see <a href="http://docs.oasis-open.org/amqp/core/v1.0/os/amqp-core-transactions-v1.0-os.html#section-txn-work">Transactional
  *     work</a>
  */
-public enum DeliveryState {
-    // indicates successful processing at the receiver.
-    ACCEPTED,
-    // indicates an invalid and unprocessable message.
-    REJECTED,
-    // indicates that the message was not (and will not be) processed.
-    RELEASED,
-    // indicates that the message was modified, but not processed.
-    MODIFIED,
-    // indicates partial message data seen by the receiver as well as the starting point for a resumed transfer.
-    RECEIVED,
+public class DeliveryState extends ExpandableStringEnum<DeliveryState> {
+    /**
+     * Indicates successful processing at the receiver.
+     */
+    public static DeliveryState ACCEPTED = fromString("ACCEPTED", DeliveryState.class);
+    /**
+     * Indicates an invalid and unprocessable message.
+     */
+    public static DeliveryState REJECTED = fromString("REJECTED", DeliveryState.class);
+    /**
+     * Indicates that the message was not (and will not be) processed.
+     */
+    public static DeliveryState RELEASED = fromString("RELEASED", DeliveryState.class);
+    /**
+     * indicates that the message was modified, but not processed.
+     */
+    public static DeliveryState MODIFIED = fromString("MODIFIED", DeliveryState.class);
+    /**
+     * indicates partial message data seen by the receiver as well as the starting point for a resumed transfer.
+     */
+    public static DeliveryState RECEIVED = fromString("RECEIVED", DeliveryState.class);
+    /**
+     * Indicates that this delivery is part of a transaction.
+     */
+    public static DeliveryState TRANSACTIONAL = fromString("TRANSACTIONAL", DeliveryState.class);
 }
