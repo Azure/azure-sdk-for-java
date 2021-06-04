cd75ed49f4d05e79bff1100eb582c6a1763fcff8
Arif Saikat <71465659+arifsaikat-microsoft@users.noreply.github.com>
2021-06-03 17:46:18 -0700

    Fix review suggestions (#22068)

--- sdk/communication/azure-communication-callingserver/src/samples/java/com/azure/communication/callingserver/ReadmeSamples.java
+++ sdk/communication/azure-communication-callingserver/src/samples/java/com/azure/communication/callingserver/ReadmeSamples.java
@@ -3,8 +3,8 @@
 
 package com.azure.communication.callingserver;
 
-import com.azure.communication.callingserver.implementation.models.CallModality;
-import com.azure.communication.callingserver.implementation.models.EventSubscriptionType;
+import com.azure.communication.callingserver.models.CallModality;
+import com.azure.communication.callingserver.models.EventSubscriptionType;
 import com.azure.communication.callingserver.models.CreateCallOptions;
 import com.azure.communication.callingserver.models.CreateCallResponse;
 import com.azure.communication.common.CommunicationIdentifier;
