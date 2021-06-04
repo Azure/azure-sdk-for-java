5e27c83fbd078cf4be4a34fb68614843972d36ff
Connie Yau <conniey@microsoft.com>
2021-06-04 16:51:32 -0700

    Add javadocs to ReceivedDeliveryOutcome.

--- sdk/core/azure-core-amqp/src/main/java/com/azure/core/amqp/models/ReceivedDeliveryOutcome.java
+++ sdk/core/azure-core-amqp/src/main/java/com/azure/core/amqp/models/ReceivedDeliveryOutcome.java
@@ -16,6 +16,10 @@ public class ReceivedDeliveryOutcome extends DeliveryOutcome {
 
     /**
      * Creates an instance of the delivery outcome with its state.
+     *
+     * @param sectionNumber Section number within the message that can be resent or may not have been received.
+     * @param sectionOffset First byte of the section where data can be resent, or first byte of the section where
+     *     it may not have been received.
      */
     public ReceivedDeliveryOutcome(int sectionNumber, long sectionOffset) {
         super(DeliveryState.RECEIVED);
@@ -25,11 +29,11 @@ public class ReceivedDeliveryOutcome extends DeliveryOutcome {
 
     /**
      * Gets the section number.
-     *
+     * <p>
      * When sent by the sender this indicates the first section of the message (with section-number 0 being the first
      * section) for which data can be resent. Data from sections prior to the given section cannot be retransmitted for
      * this delivery.
-     *
+     * <p>
      * When sent by the receiver this indicates the first section of the message for which all data might not yet have
      * been received.
      *
@@ -41,11 +45,11 @@ public class ReceivedDeliveryOutcome extends DeliveryOutcome {
 
     /**
      * Gets the section offset.
-     *
+     * <p>
      * When sent by the sender this indicates the first byte of the encoded section data of the section given by
      * section-number for which data can be resent (with section-offset 0 being the first byte). Bytes from the same
      * section prior to the given offset section cannot be retransmitted for this delivery.
-     *
+     * <p>
      * When sent by the receiver this indicates the first byte of the given section which has not yet been received.
      * Note that if a receiver has received all of section number X (which contains N bytes of data), but none of
      * section number X + 1, then it can indicate this by sending either Received(section-number=X, section-offset=N) or
