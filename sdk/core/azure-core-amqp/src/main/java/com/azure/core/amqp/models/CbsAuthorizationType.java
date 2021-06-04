a466b3d474290eacdbd1a7afcf211d431df7356f
Connie Yau <conniey@microsoft.com>
2021-06-04 08:10:11 -0700

    Expose CbsAuthorizationType (#22072)
    * Adding CbsAuthorizationType model.
    
    * Deleting implementation CbsAuthorizationType.
    
    * Update AzureTokenManagerProvider to use ExpandableEnum CbsAuthorizationType.
    
    * Fix CbsAuthorizationType imports.
    
    * Fixing Event Hubs breaks.
    
    * Fix Service Bus breaks.

--- sdk/core/azure-core-amqp/src/main/java/com/azure/core/amqp/models/CbsAuthorizationType.java
+++ sdk/core/azure-core-amqp/src/main/java/com/azure/core/amqp/models/CbsAuthorizationType.java
@@ -1,38 +1,27 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.azure.core.amqp.implementation;
+package com.azure.core.amqp.models;
 
 import com.azure.core.amqp.ClaimsBasedSecurityNode;
+import com.azure.core.util.ExpandableStringEnum;
 
 /**
  * An enumeration of supported authorization methods with the {@link ClaimsBasedSecurityNode}.
  */
-public enum CbsAuthorizationType {
+public final class CbsAuthorizationType extends ExpandableStringEnum<CbsAuthorizationType> {
     /**
      * Authorize with CBS through a shared access signature.
      */
-    SHARED_ACCESS_SIGNATURE("servicebus.windows.net:sastoken"),
+    public static final CbsAuthorizationType SHARED_ACCESS_SIGNATURE =
+        fromString("servicebus.windows.net:sastoken", CbsAuthorizationType.class);
+
     /**
      * Authorize with CBS using a JSON web token.
      *
      * This is used in the case where Azure Active Directory is used for authentication and the authenticated user
      * wants to authorize with Azure Event Hubs.
      */
-    JSON_WEB_TOKEN("jwt");
-
-    private final String scheme;
-
-    CbsAuthorizationType(String scheme) {
-        this.scheme = scheme;
-    }
-
-    /**
-     * Gets the token type scheme.
-     *
-     * @return The token type scheme.
-     */
-    public String getTokenType() {
-        return scheme;
-    }
+    public static final CbsAuthorizationType JSON_WEB_TOKEN =
+        fromString("jwt", CbsAuthorizationType.class);
 }
