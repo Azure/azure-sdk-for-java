03e9b85e7c26d6e46823b7a2df5363f5e3ffedba
Yijun Xie <48257664+YijunXieMS@users.noreply.github.com>
2021-06-07 15:54:09 -0700

    Add 4.4.0 new system events.

--- sdk/eventgrid/azure-messaging-eventgrid/src/main/java/com/azure/messaging/eventgrid/implementation/SystemEventMappingNames.java
+++ sdk/eventgrid/azure-messaging-eventgrid/src/main/java/com/azure/messaging/eventgrid/implementation/SystemEventMappingNames.java
@@ -24,6 +24,21 @@ import com.azure.messaging.eventgrid.systemevents.AcsChatThreadWithUserDeletedEv
 import com.azure.messaging.eventgrid.systemevents.AcsRecordingFileStatusUpdatedEventData;
 import com.azure.messaging.eventgrid.systemevents.AcsSmsDeliveryReportReceivedEventData;
 import com.azure.messaging.eventgrid.systemevents.AcsSmsReceivedEventData;
+import com.azure.messaging.eventgrid.systemevents.AgriFoodFarmingApplicationDataChangedEventData;
+import com.azure.messaging.eventgrid.systemevents.AgriFoodFarmingBoundaryChangedEventData;
+import com.azure.messaging.eventgrid.systemevents.AgriFoodFarmingCropChangedEventData;
+import com.azure.messaging.eventgrid.systemevents.AgriFoodFarmingCropVarietyChangedEventData;
+import com.azure.messaging.eventgrid.systemevents.AgriFoodFarmingFarmChangedEventData;
+import com.azure.messaging.eventgrid.systemevents.AgriFoodFarmingFarmOperationDataIngestionJobStatusChangedEventData;
+import com.azure.messaging.eventgrid.systemevents.AgriFoodFarmingFarmerChangedEventData;
+import com.azure.messaging.eventgrid.systemevents.AgriFoodFarmingFieldChangedEventData;
+import com.azure.messaging.eventgrid.systemevents.AgriFoodFarmingHarvestDataChangedEventData;
+import com.azure.messaging.eventgrid.systemevents.AgriFoodFarmingPlantingDataChangedEventData;
+import com.azure.messaging.eventgrid.systemevents.AgriFoodFarmingSatelliteDataIngestionJobStatusChangedEventData;
+import com.azure.messaging.eventgrid.systemevents.AgriFoodFarmingSeasonChangedEventData;
+import com.azure.messaging.eventgrid.systemevents.AgriFoodFarmingSeasonalFieldChangedEventData;
+import com.azure.messaging.eventgrid.systemevents.AgriFoodFarmingTillageDataChangedEventData;
+import com.azure.messaging.eventgrid.systemevents.AgriFoodFarmingWeatherDataIngestionJobStatusChangedEventData;
 import com.azure.messaging.eventgrid.systemevents.AppConfigurationKeyValueDeletedEventData;
 import com.azure.messaging.eventgrid.systemevents.AppConfigurationKeyValueModifiedEventData;
 import com.azure.messaging.eventgrid.systemevents.ContainerRegistryChartDeletedEventData;
@@ -103,6 +118,7 @@ import com.azure.messaging.eventgrid.systemevents.SignalRServiceClientConnection
 import com.azure.messaging.eventgrid.systemevents.StorageAsyncOperationInitiatedEventData;
 import com.azure.messaging.eventgrid.systemevents.StorageBlobCreatedEventData;
 import com.azure.messaging.eventgrid.systemevents.StorageBlobDeletedEventData;
+import com.azure.messaging.eventgrid.systemevents.StorageBlobInventoryPolicyCompletedEventData;
 import com.azure.messaging.eventgrid.systemevents.StorageBlobRenamedEventData;
 import com.azure.messaging.eventgrid.systemevents.StorageBlobTierChangedEventData;
 import com.azure.messaging.eventgrid.systemevents.StorageDirectoryCreatedEventData;
@@ -230,6 +246,65 @@ public final class SystemEventMappingNames {
 
     /** Schema of the Data property of an EventGridEvent for a Microsoft.Communication.SMSReceived event. */
     public static final String ACS_SMS_RECEIVED = "Microsoft.Communication.SMSReceived";
+
+    /** Schema of the Data property of an EventGridEvent for a Microsoft.AgFoodPlatform.ApplicationDataChanged event. */
+    public static final String AGRI_FOOD_FARMING_APPLICATION_DATA_CHANGED =
+            "Microsoft.AgFoodPlatform.ApplicationDataChanged";
+
+    /** Schema of the Data property of an EventGridEvent for a Microsoft.AgFoodPlatform.BoundaryChanged event. */
+    public static final String AGRI_FOOD_FARMING_BOUNDARY_CHANGED = "Microsoft.AgFoodPlatform.BoundaryChanged";
+
+    /** Schema of the Data property of an EventGridEvent for a Microsoft.AgFoodPlatform.CropChanged event. */
+    public static final String AGRI_FOOD_FARMING_CROP_CHANGED = "Microsoft.AgFoodPlatform.CropChanged";
+
+    /** Schema of the Data property of an EventGridEvent for a Microsoft.AgFoodPlatform.CropVarietyChanged event. */
+    public static final String AGRI_FOOD_FARMING_CROP_VARIETY_CHANGED = "Microsoft.AgFoodPlatform.CropVarietyChanged";
+
+    /** Schema of the Data property of an EventGridEvent for a Microsoft.AgFoodPlatform.FarmChanged event. */
+    public static final String AGRI_FOOD_FARMING_FARM_CHANGED = "Microsoft.AgFoodPlatform.FarmChanged";
+
+    /**
+     * Schema of the Data property of an EventGridEvent for a
+     * Microsoft.AgFoodPlatform.FarmOperationDataIngestionJobStatusChanged event.
+     */
+    public static final String AGRI_FOOD_FARMING_FARM_OPERATION_DATA_INGESTION_JOB_STATUS_CHANGED =
+            "Microsoft.AgFoodPlatform.FarmOperationDataIngestionJobStatusChanged";
+
+    /** Schema of the Data property of an EventGridEvent for a Microsoft.AgFoodPlatform.FarmerChanged event. */
+    public static final String AGRI_FOOD_FARMING_FARMER_CHANGED = "Microsoft.AgFoodPlatform.FarmerChanged";
+
+    /** Schema of the Data property of an EventGridEvent for a Microsoft.AgFoodPlatform.FieldChanged event. */
+    public static final String AGRI_FOOD_FARMING_FIELD_CHANGED = "Microsoft.AgFoodPlatform.FieldChanged";
+
+    /** Schema of the Data property of an EventGridEvent for a Microsoft.AgFoodPlatform.HarvestDataChanged event. */
+    public static final String AGRI_FOOD_FARMING_HARVEST_DATA_CHANGED = "Microsoft.AgFoodPlatform.HarvestDataChanged";
+
+    /** Schema of the Data property of an EventGridEvent for a Microsoft.AgFoodPlatform.PlantingDataChanged event. */
+    public static final String AGRI_FOOD_FARMING_PLANTING_DATA_CHANGED = "Microsoft.AgFoodPlatform.PlantingDataChanged";
+
+    /**
+     * Schema of the Data property of an EventGridEvent for a
+     * Microsoft.AgFoodPlatform.SatelliteDataIngestionJobStatusChanged event.
+     */
+    public static final String AGRI_FOOD_FARMING_SATELLITE_DATA_INGESTION_JOB_STATUS_CHANGED =
+            "Microsoft.AgFoodPlatform.SatelliteDataIngestionJobStatusChanged";
+
+    /** Schema of the Data property of an EventGridEvent for a Microsoft.AgFoodPlatform.SeasonChanged event. */
+    public static final String AGRI_FOOD_FARMING_SEASON_CHANGED = "Microsoft.AgFoodPlatform.SeasonChanged";
+
+    /** Schema of the Data property of an EventGridEvent for a Microsoft.AgFoodPlatform.SeasonalFieldChanged event. */
+    public static final String AGRI_FOOD_FARMING_SEASONAL_FIELD_CHANGED =
+            "Microsoft.AgFoodPlatform.SeasonalFieldChanged";
+
+    /** Schema of the Data property of an EventGridEvent for a Microsoft.AgFoodPlatform.TillageDataChanged event. */
+    public static final String AGRI_FOOD_FARMING_TILLAGE_DATA_CHANGED = "Microsoft.AgFoodPlatform.TillageDataChanged";
+
+    /**
+     * Schema of the Data property of an EventGridEvent for a
+     * Microsoft.AgFoodPlatform.WeatherDataIngestionJobStatusChanged event.
+     */
+    public static final String AGRI_FOOD_FARMING_WEATHER_DATA_INGESTION_JOB_STATUS_CHANGED =
+            "Microsoft.AgFoodPlatform.WeatherDataIngestionJobStatusChanged";
 
     /** Schema of the Data property of an EventGridEvent for a Microsoft.AppConfiguration.KeyValueDeleted event. */
     public static final String APP_CONFIGURATION_KEY_VALUE_DELETED = "Microsoft.AppConfiguration.KeyValueDeleted";
@@ -602,6 +677,10 @@ public final class SystemEventMappingNames {
     /** Schema of the Data property of an EventGridEvent for a Microsoft.Storage.BlobDeleted event. */
     public static final String STORAGE_BLOB_DELETED = "Microsoft.Storage.BlobDeleted";
 
+    /** Schema of the Data property of an EventGridEvent for an Microsoft.Storage.BlobInventoryPolicyCompleted event. */
+    public static final String STORAGE_BLOB_INVENTORY_POLICY_COMPLETED =
+            "Microsoft.Storage.BlobInventoryPolicyCompleted";
+
     /** Schema of the Data property of an EventGridEvent for a Microsoft.Storage.BlobRenamed event. */
     public static final String STORAGE_BLOB_RENAMED = "Microsoft.Storage.BlobRenamed";
 
@@ -693,6 +772,29 @@ public final class SystemEventMappingNames {
                     put(ACS_RECORDING_FILE_STATUS_UPDATED, AcsRecordingFileStatusUpdatedEventData.class);
                     put(ACS_SMS_DELIVERY_REPORT_RECEIVED, AcsSmsDeliveryReportReceivedEventData.class);
                     put(ACS_SMS_RECEIVED, AcsSmsReceivedEventData.class);
+                    put(
+                            AGRI_FOOD_FARMING_APPLICATION_DATA_CHANGED,
+                            AgriFoodFarmingApplicationDataChangedEventData.class);
+                    put(AGRI_FOOD_FARMING_BOUNDARY_CHANGED, AgriFoodFarmingBoundaryChangedEventData.class);
+                    put(AGRI_FOOD_FARMING_CROP_CHANGED, AgriFoodFarmingCropChangedEventData.class);
+                    put(AGRI_FOOD_FARMING_CROP_VARIETY_CHANGED, AgriFoodFarmingCropVarietyChangedEventData.class);
+                    put(AGRI_FOOD_FARMING_FARM_CHANGED, AgriFoodFarmingFarmChangedEventData.class);
+                    put(
+                            AGRI_FOOD_FARMING_FARM_OPERATION_DATA_INGESTION_JOB_STATUS_CHANGED,
+                            AgriFoodFarmingFarmOperationDataIngestionJobStatusChangedEventData.class);
+                    put(AGRI_FOOD_FARMING_FARMER_CHANGED, AgriFoodFarmingFarmerChangedEventData.class);
+                    put(AGRI_FOOD_FARMING_FIELD_CHANGED, AgriFoodFarmingFieldChangedEventData.class);
+                    put(AGRI_FOOD_FARMING_HARVEST_DATA_CHANGED, AgriFoodFarmingHarvestDataChangedEventData.class);
+                    put(AGRI_FOOD_FARMING_PLANTING_DATA_CHANGED, AgriFoodFarmingPlantingDataChangedEventData.class);
+                    put(
+                            AGRI_FOOD_FARMING_SATELLITE_DATA_INGESTION_JOB_STATUS_CHANGED,
+                            AgriFoodFarmingSatelliteDataIngestionJobStatusChangedEventData.class);
+                    put(AGRI_FOOD_FARMING_SEASON_CHANGED, AgriFoodFarmingSeasonChangedEventData.class);
+                    put(AGRI_FOOD_FARMING_SEASONAL_FIELD_CHANGED, AgriFoodFarmingSeasonalFieldChangedEventData.class);
+                    put(AGRI_FOOD_FARMING_TILLAGE_DATA_CHANGED, AgriFoodFarmingTillageDataChangedEventData.class);
+                    put(
+                            AGRI_FOOD_FARMING_WEATHER_DATA_INGESTION_JOB_STATUS_CHANGED,
+                            AgriFoodFarmingWeatherDataIngestionJobStatusChangedEventData.class);
                     put(APP_CONFIGURATION_KEY_VALUE_DELETED, AppConfigurationKeyValueDeletedEventData.class);
                     put(APP_CONFIGURATION_KEY_VALUE_MODIFIED, AppConfigurationKeyValueModifiedEventData.class);
                     put(CONTAINER_REGISTRY_CHART_DELETED, ContainerRegistryChartDeletedEventData.class);
@@ -798,6 +900,7 @@ public final class SystemEventMappingNames {
                     put(STORAGE_ASYNC_OPERATION_INITIATED, StorageAsyncOperationInitiatedEventData.class);
                     put(STORAGE_BLOB_CREATED, StorageBlobCreatedEventData.class);
                     put(STORAGE_BLOB_DELETED, StorageBlobDeletedEventData.class);
+                    put(STORAGE_BLOB_INVENTORY_POLICY_COMPLETED, StorageBlobInventoryPolicyCompletedEventData.class);
                     put(STORAGE_BLOB_RENAMED, StorageBlobRenamedEventData.class);
                     put(STORAGE_BLOB_TIER_CHANGED, StorageBlobTierChangedEventData.class);
                     put(STORAGE_DIRECTORY_CREATED, StorageDirectoryCreatedEventData.class);
@@ -831,7 +934,5 @@ public final class SystemEventMappingNames {
         return Collections.unmodifiableMap(SYSTEM_EVENT_MAPPINGS);
     }
 
-    private SystemEventMappingNames() {
-        // No instances allowed
-    }
+    private SystemEventMappingNames() {}
 }
--- sdk/eventgrid/azure-messaging-eventgrid/swagger/README.md
+++ sdk/eventgrid/azure-messaging-eventgrid/swagger/README.md
@@ -23,8 +23,6 @@ in this folder, with the following tags:
 
 `--use=<path to autorest clone>`
 
-`--api-spec-location=<path to rest-api-spec clone>`
-
 If you are adding or updating swagger files, please make sure that they are included 
 in the input file list. Also, if you are adding an additional service event, make sure that 
 its `eventType` string is added as a constant and a mapping to the event data model in the
@@ -82,23 +80,24 @@ custom-types: CloudEvent,EventGridEvent
 model-override-setter-from-superclass: true
 
 input-file:
-- $(api-spec-location)/specification/eventgrid/data-plane/Microsoft.Storage/stable/2018-01-01/Storage.json
-- $(api-spec-location)/specification/eventgrid/data-plane/Microsoft.EventHub/stable/2018-01-01/EventHub.json
-- $(api-spec-location)/specification/eventgrid/data-plane/Microsoft.Resources/stable/2018-01-01/Resources.json
-- $(api-spec-location)/specification/eventgrid/data-plane/Microsoft.EventGrid/stable/2018-01-01/EventGrid.json
-- $(api-spec-location)/specification/eventgrid/data-plane/Microsoft.Devices/stable/2018-01-01/IotHub.json
-- $(api-spec-location)/specification/eventgrid/data-plane/Microsoft.ContainerRegistry/stable/2018-01-01/ContainerRegistry.json
-- $(api-spec-location)/specification/eventgrid/data-plane/Microsoft.ServiceBus/stable/2018-01-01/ServiceBus.json
-- $(api-spec-location)/specification/eventgrid/data-plane/Microsoft.Media/stable/2018-01-01/MediaServices.json
-- $(api-spec-location)/specification/eventgrid/data-plane/Microsoft.Maps/stable/2018-01-01/Maps.json
-- $(api-spec-location)/specification/eventgrid/data-plane/Microsoft.AppConfiguration/stable/2018-01-01/AppConfiguration.json
-- $(api-spec-location)/specification/eventgrid/data-plane/Microsoft.SignalRService/stable/2018-01-01/SignalRService.json
-- $(api-spec-location)/specification/eventgrid/data-plane/Microsoft.KeyVault/stable/2018-01-01/KeyVault.json
-- $(api-spec-location)/specification/eventgrid/data-plane/Microsoft.MachineLearningServices/stable/2018-01-01/MachineLearningServices.json
-- $(api-spec-location)/specification/eventgrid/data-plane/Microsoft.Cache/stable/2018-01-01/RedisCache.json
-- $(api-spec-location)/specification/eventgrid/data-plane/Microsoft.Web/stable/2018-01-01/Web.json
-- $(api-spec-location)/specification/eventgrid/data-plane/Microsoft.Communication/stable/2018-01-01/AzureCommunicationServices.json
-- $(api-spec-location)/specification/eventgrid/data-plane/Microsoft.PolicyInsights/stable/2018-01-01/PolicyInsights.json
+- https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master/specification/eventgrid/data-plane/Microsoft.Storage/stable/2018-01-01/Storage.json
+- https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master/specification/eventgrid/data-plane/Microsoft.EventHub/stable/2018-01-01/EventHub.json
+- https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master/specification/eventgrid/data-plane/Microsoft.Resources/stable/2018-01-01/Resources.json
+- https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master/specification/eventgrid/data-plane/Microsoft.EventGrid/stable/2018-01-01/EventGrid.json
+- https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master/specification/eventgrid/data-plane/Microsoft.Devices/stable/2018-01-01/IotHub.json
+- https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master/specification/eventgrid/data-plane/Microsoft.ContainerRegistry/stable/2018-01-01/ContainerRegistry.json
+- https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master/specification/eventgrid/data-plane/Microsoft.ServiceBus/stable/2018-01-01/ServiceBus.json
+- https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master/specification/eventgrid/data-plane/Microsoft.Media/stable/2018-01-01/MediaServices.json
+- https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master/specification/eventgrid/data-plane/Microsoft.Maps/stable/2018-01-01/Maps.json
+- https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master/specification/eventgrid/data-plane/Microsoft.AppConfiguration/stable/2018-01-01/AppConfiguration.json
+- https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master/specification/eventgrid/data-plane/Microsoft.SignalRService/stable/2018-01-01/SignalRService.json
+- https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master/specification/eventgrid/data-plane/Microsoft.KeyVault/stable/2018-01-01/KeyVault.json
+- https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master/specification/eventgrid/data-plane/Microsoft.MachineLearningServices/stable/2018-01-01/MachineLearningServices.json
+- https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master/specification/eventgrid/data-plane/Microsoft.Cache/stable/2018-01-01/RedisCache.json
+- https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master/specification/eventgrid/data-plane/Microsoft.Web/stable/2018-01-01/Web.json
+- https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master/specification/eventgrid/data-plane/Microsoft.Communication/stable/2018-01-01/AzureCommunicationServices.json
+- https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master/specification/eventgrid/data-plane/Microsoft.PolicyInsights/stable/2018-01-01/PolicyInsights.json
+- https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master/specification/eventgrid/data-plane/Microsoft.AgFoodPlatform/stable/2018-01-01/AzureFarmBeats.json
 ```
 
 ### Customization
@@ -143,7 +142,6 @@ public class EventGridCustomization extends Customization {
         "    }\n" +
         "\n" +
         "    private SystemEventMappingNames() {\n" +
-        "         // No instances allowed" +
         "    }";
 
     @Override
