c3f814cd44441d471365af14404fb4ac2b0b1cd4
Connie <conniey@microsoft.com>
2021-06-07 10:24:03 -0700

    Adding documentation to DeliveryOutcome.

--- sdk/core/azure-core-amqp/src/main/java/com/azure/core/amqp/models/DeliveryOutcome.java
+++ sdk/core/azure-core-amqp/src/main/java/com/azure/core/amqp/models/DeliveryOutcome.java
@@ -8,13 +8,17 @@ import com.azure.core.annotation.Fluent;
 /**
  * There are different outcomes accepted by the AMQP protocol layer.
  *
- * Outcomes that don't have any other fields
- * http://docs.oasis-open.org/amqp/core/v1.0/os/amqp-core-messaging-v1.0-os.html#type-accepted
- * http://docs.oasis-open.org/amqp/core/v1.0/os/amqp-core-messaging-v1.0-os.html#type-released
+ * @see <a href="http://docs.oasis-open.org/amqp/core/v1.0/os/amqp-core-messaging-v1.0-os.html#type-accepted">Delivery
+ *     State: Accepted</a>
+ * @see <a href="http://docs.oasis-open.org/amqp/core/v1.0/os/amqp-core-messaging-v1.0-os.html#type-released">Delivery
+ *     State: Released</a>
+ * @see ModifiedDeliveryOutcome
+ * @see RejectedDeliveryOutcome
+ * @see TransactionalDeliveryOutcome
  */
 @Fluent
 public class DeliveryOutcome {
-    private DeliveryState deliveryState;
+    private final DeliveryState deliveryState;
 
     /**
      * Creates an instance of the delivery outcome with its state.
@@ -32,14 +36,5 @@ public class DeliveryOutcome {
      */
     public DeliveryState getDeliveryState() {
         return deliveryState;
-    }
-
-    /**
-     * Sets the delivery state.
-     *
-     * @param deliveryState The delivery state.
-     */
-    void setDeliveryState(DeliveryState deliveryState) {
-        this.deliveryState = deliveryState;
     }
 }
