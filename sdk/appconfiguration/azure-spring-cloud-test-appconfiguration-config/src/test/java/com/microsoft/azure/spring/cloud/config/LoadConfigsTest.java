a7f7b41332565b51aafd67aee107b315b5c05aa2
mrm9084 <mrm9084@gmail.com>
2021-06-08 09:56:19 -0700

    Fixed import

--- sdk/appconfiguration/azure-spring-cloud-test-appconfiguration-config/src/test/java/com/microsoft/azure/spring/cloud/config/LoadConfigsTest.java
+++ sdk/appconfiguration/azure-spring-cloud-test-appconfiguration-config/src/test/java/com/microsoft/azure/spring/cloud/config/LoadConfigsTest.java
@@ -2,7 +2,7 @@ package com.microsoft.azure.spring.cloud.config;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
