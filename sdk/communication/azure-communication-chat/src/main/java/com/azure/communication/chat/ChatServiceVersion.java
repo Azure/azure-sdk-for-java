dbfd41cf4d52eef35a8b73eee18ed9b031ff5e3e
Andrei Giurgiu <agiurg@microsoft.com>
2021-06-08 23:31:07 -0700

    Re-added V2021_03_07

--- sdk/communication/azure-communication-chat/src/main/java/com/azure/communication/chat/ChatServiceVersion.java
+++ sdk/communication/azure-communication-chat/src/main/java/com/azure/communication/chat/ChatServiceVersion.java
@@ -9,6 +9,7 @@ import com.azure.core.util.ServiceVersion;
  * The versions of Chat Service supported by this client library.
  */
 public enum ChatServiceVersion implements ServiceVersion {
+    V2021_03_07("2021-03-07"),
     V2021_04_05_preview6("2021-04-05-preview6");
 
     private final String version;
