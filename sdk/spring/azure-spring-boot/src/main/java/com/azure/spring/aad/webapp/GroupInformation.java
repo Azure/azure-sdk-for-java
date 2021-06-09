1efdf233509290001184de86644c18d10392492a
Xiaobing Zhu <71206407+ZhuXiaoBing-cn@users.noreply.github.com>
2021-06-09 14:58:52 +0800

    * Add the javadoc.

--- sdk/spring/azure-spring-boot/src/main/java/com/azure/spring/aad/webapp/GroupInformation.java
+++ sdk/spring/azure-spring-boot/src/main/java/com/azure/spring/aad/webapp/GroupInformation.java
@@ -1,8 +1,14 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.azure.spring.aad.webapp;
 
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Contains information about the group.
+ */
 public class GroupInformation {
 
     private Set<String> groupsId = new HashSet<>();
