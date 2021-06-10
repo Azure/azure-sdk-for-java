9e06b1aeb8e31ce089f8145f02f748f26f06d4f7
Moary Chen <v-moaryc@microsoft.com>
2021-06-10 18:23:45 +0800

    fix code smell

--- sdk/spring/azure-spring-cloud-storage/src/main/java/com/azure/spring/cloud/autoconfigure/storage/AzureStorageQueueAutoConfiguration.java
+++ sdk/spring/azure-spring-cloud-storage/src/main/java/com/azure/spring/cloud/autoconfigure/storage/AzureStorageQueueAutoConfiguration.java
@@ -26,7 +26,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.PostConstruct;
 import java.util.Optional;
 
 /**
