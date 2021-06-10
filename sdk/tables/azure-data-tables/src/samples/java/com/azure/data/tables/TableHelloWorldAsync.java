6f8ce560eb8b00349dcc90cf21fa1e45472dfd5d
Victor Colin Amador <victor.y.asi@gmail.com>
2021-06-10 02:10:31 -0500

    Applied PR feedback.

--- sdk/tables/azure-data-tables/src/samples/java/com/azure/data/tables/TableHelloWorldAsync.java
+++ sdk/tables/azure-data-tables/src/samples/java/com/azure/data/tables/TableHelloWorldAsync.java
@@ -4,6 +4,8 @@ package com.azure.data.tables;
 
 import com.azure.data.tables.models.TableEntity;
 
+import java.util.concurrent.CountDownLatch;
+
 /**
  * This sample demonstrates how to create, update, upsert, get, list and delete table entities asynchronously on a
  * table.
@@ -15,7 +17,7 @@ public class TableHelloWorldAsync {
      *
      * @param args Unused. Arguments to the program.
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         /* Instantiate a TableAsyncClient that will be used to call the service. Notice that the client is using a
         connection string. For information on how to obtain a connection string, please refer to this project samples'
         README.*/
@@ -24,65 +26,70 @@ public class TableHelloWorldAsync {
             .tableName("myTable")
             .buildAsyncClient();
 
-        // We'll first create the table this client will be associated with.
-        tableAsyncClient.createTable()
-            .doOnSuccess(tableItem -> System.out.printf("Created table with name '%s'.\n", tableItem.getName()))
-            .block();
-
-        // We will then create an entity on the table.
         String partitionKey = "OfficeSupplies";
         String rowKey = "s001";
+
         TableEntity entityToCreate = new TableEntity(partitionKey, rowKey)
             .addProperty("Type", "Pen");
 
-        tableAsyncClient.createEntity(entityToCreate)
-            .doOnSuccess(unused ->
-                System.out.printf("Created entity with partition key '%s' and row key '%s'.\n", partitionKey, rowKey))
-            .block();
-
-        // Let's now update said entity on the table. By default, the provided entity will be merged with the one on
-        // the table.
         TableEntity entityForUpdate = new TableEntity(partitionKey, rowKey)
             .addProperty("Type", "Pen")
             .addProperty("Color", "Red");
 
-        tableAsyncClient.updateEntity(entityForUpdate)
-            .doOnSuccess(unused ->
-                System.out.printf("Updated entity with partition key '%s' and row key '%s'.\n", partitionKey, rowKey))
-            .block();
-
-        // Let's retrieve our updated entity to see how it looks now.
-        tableAsyncClient.getEntity(partitionKey, rowKey)
-            .doOnSuccess(retrievedEntity ->
-                System.out.printf("Retrieved entity with partition key '%s' and row key '%s'.\n",
-                    retrievedEntity.getPartitionKey(), retrievedEntity.getRowKey()))
-            .block();
-
-        // Let's now use the upsert command to update an entity (or insert it, if the entity does not exist on the
-        // table already).
         String anotherRowKey = "s002";
 
         TableEntity entityForUpsert = new TableEntity(partitionKey, anotherRowKey)
             .addProperty("Type", "Marker")
             .addProperty("Color", "Blue");
 
-        tableAsyncClient.updateEntity(entityForUpsert)
-            .doOnSuccess(unused ->
-                System.out.printf("Upserted entity with partition key '%s' and row key '%s'.\n", partitionKey, rowKey))
-            .block();
+        CountDownLatch latch = new CountDownLatch(1);
 
-        // Let's now retrieve all the entities on the table and print their partition and row keys.
-        tableAsyncClient.listEntities()
-            .doOnNext(entity ->
+        // We'll first create the table this client will be associated with.
+        tableAsyncClient.createTable()
+            .map(tableItem -> System.out.printf("Created table with name '%s'.\n", tableItem.getName()))
+            // We will then create an entity on the table.
+            .then(tableAsyncClient.createEntity(entityToCreate))
+            .map(unused ->
+                System.out.printf("Created entity with partition key '%s' and row key '%s'.\n", partitionKey, rowKey))
+            // Let's now update said entity on the table. By default, the provided entity will be merged with the one on
+            // the table.
+            .then(tableAsyncClient.updateEntity(entityForUpdate))
+            .map(unused ->
+                System.out.printf("Updated entity with partition key '%s' and row key '%s'.\n", partitionKey, rowKey))
+            // Let's retrieve our updated entity to see how it looks now.
+            .then(tableAsyncClient.getEntity(partitionKey, rowKey))
+            .map(retrievedEntity ->
                 System.out.printf("Retrieved entity with partition key '%s' and row key '%s'.\n",
-                    entity.getPartitionKey(), entity.getRowKey()))
-            .blockLast();
+                    retrievedEntity.getPartitionKey(), retrievedEntity.getRowKey()))
+            // Let's now use the upsert command to update an entity (or insert it, if the entity does not exist on the
+            // table already).
+            .then(tableAsyncClient.upsertEntity(entityForUpsert))
+            .map(unused ->
+                System.out.printf("Upserted entity with partition key '%s' and row key '%s'.\n", partitionKey, rowKey))
+            // Let's now retrieve all the entities on the table and print their partition and row keys.
+            .thenMany(tableAsyncClient.listEntities())
+            .map(currentEntity -> {
+                System.out.printf("Retrieved entity with partition key '%s' and row key '%s'.\n",
+                    currentEntity.getPartitionKey(), currentEntity.getRowKey());
+                return currentEntity;
+            })
+            .filter(tableEntity ->
+                tableEntity.getPartitionKey().equals(partitionKey) && tableEntity.getRowKey().equals(rowKey))
+            // Finally, let's delete the entity we upserted above.
+            .flatMap(tableAsyncClient::deleteEntity)
+            .subscribe(
+                ignored ->
+                    System.out.printf("Deleted entity with partition key '%s' and row key '%s'.\n", partitionKey,
+                        anotherRowKey),
+                ex -> {
+                    System.out.println("Got an error running the sample: " + ex.getMessage());
+                    latch.countDown();
+                },
+                () -> {
+                    System.out.println("Successfully completed running the sample.");
+                    latch.countDown();
+                });
 
-        // Finally, let's delete the entity we upserted above.
-        tableAsyncClient.deleteEntity(partitionKey, anotherRowKey)
-            .doOnSuccess(unused ->
-                System.out.printf("Deleted entity with partition key '%s' and row key '%s'.\n", partitionKey,
-                    anotherRowKey))
-            .block();
+        latch.await();
     }
 }
--- sdk/tables/azure-data-tables/src/samples/java/com/azure/data/tables/TableServiceHelloWorldAsync.java
+++ sdk/tables/azure-data-tables/src/samples/java/com/azure/data/tables/TableServiceHelloWorldAsync.java
@@ -2,6 +2,8 @@
 // Licensed under the MIT License.
 package com.azure.data.tables;
 
+import java.util.concurrent.CountDownLatch;
+
 /**
  * This sample demonstrates how to create, list and delete tables asynchronously.
  */
@@ -11,31 +13,43 @@ public class TableServiceHelloWorldAsync {
      *
      * @param args Unused. Arguments to the program.
      */
-    public static void main(String[] args) {
-        /* Instantiate a TableServiceAsyncClient that will be used to call the service. Notice that the client is using a
-        connection string. For information on how to obtain a connection string, please refer to this project samples'
+    public static void main(String[] args) throws InterruptedException {
+        /* Instantiate a TableServiceAsyncClient that will be used to call the service. Notice that the client is using
+        a connection string. For information on how to obtain a connection string, please refer to this project samples'
         README.*/
         TableServiceAsyncClient tableServiceAsyncClient = new TableServiceClientBuilder()
             .connectionString("DefaultEndpointsProtocol=https;AccountName=dummyAccount;AccountKey=xyzDummy;EndpointSuffix=core.windows.net")
             .buildAsyncClient();
 
+        String tableName = "myTable";
+        CountDownLatch latch = new CountDownLatch(1);
+
         // We'll first create a table. This operation produces a TableClient that can be used to perform operations
         // directly on the table.
-        String tableName = "myTable";
-
         tableServiceAsyncClient.createTable(tableName)
-            .doOnSuccess(tableClient ->
-                System.out.printf("Created table with name '%s'.\n", tableClient.getTableName()))
-            .block();
+            .map(tableAsyncClient ->
+                System.out.printf("Created table with name '%s'.\n", tableAsyncClient.getTableName()))
+            // We will then retrieve all tables for this account's Table service and print their names.
+            .thenMany(tableServiceAsyncClient.listTables())
+            .map(tableItem -> {
+                System.out.printf("Retrieved table with name '%s'.\n", tableItem.getName());
+                return tableItem;
+            })
+            .filter(tableItem -> tableItem.getName().equals(tableName))
+            // Finally, let's delete the table we created above.
+            .flatMap(tableItem -> tableServiceAsyncClient.deleteTable(tableItem.getName()))
+            .subscribe(
+                ignored -> System.out.printf("Deleted table with name '%s'.\n", tableName),
+                ex -> {
+                    System.out.println("An error occurred running the sample: " + ex.getMessage());
+                    latch.countDown();
+                },
+                () -> {
+                    System.out.println("Successfully completed running the sample");
+                    latch.countDown();
+                }
+            );
 
-        // We will then retrieve all tables for this account's Table service and print their names.
-        tableServiceAsyncClient.listTables()
-            .doOnNext(tableItem -> System.out.printf("Retrieved table with name '%s'.\n", tableItem.getName()))
-            .blockLast();
-
-        // Finally, let's delete the table we created above.
-        tableServiceAsyncClient.deleteTable(tableName)
-            .doOnSuccess(unused -> System.out.printf("Deleted table with name '%s'.\n", tableName))
-            .block();
+        latch.await();
     }
 }
