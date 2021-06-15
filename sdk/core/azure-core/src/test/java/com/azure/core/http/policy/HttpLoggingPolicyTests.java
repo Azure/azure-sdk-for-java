a390cede76e9593eb257269073d00a4e321c4245
Dragos Daian <v-ddaian@microsoft.com>
2021-06-15 14:12:22 +0200

    Remove extra line.

--- sdk/core/azure-core/src/test/java/com/azure/core/http/policy/HttpLoggingPolicyTests.java
+++ sdk/core/azure-core/src/test/java/com/azure/core/http/policy/HttpLoggingPolicyTests.java
@@ -105,7 +105,6 @@ public class HttpLoggingPolicyTests {
 
         StepVerifier.create(pipeline.send(new HttpRequest(HttpMethod.POST, requestUrl), CONTEXT))
             .verifyComplete();
-
         String result = convertOutputStreamToString(logCaptureStream);
         assertTrue(convertOutputStreamToString(logCaptureStream).contains(expectedQueryString));
     }
