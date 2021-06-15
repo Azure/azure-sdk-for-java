f4d35d3c0b56672a2508e572363d8450c30a8c2c
lzc-1997-abel <70368631+lzc-1997-abel@users.noreply.github.com>
2021-06-15 17:46:35 +0800

    add test for File system and classpath

--- sdk/keyvault/azure-security-keyvault-jca/src/test/java/com/azure/security/keyvault/jca/FileSystemCertificatesTest.java
+++ sdk/keyvault/azure-security-keyvault-jca/src/test/java/com/azure/security/keyvault/jca/FileSystemCertificatesTest.java
@@ -6,17 +6,6 @@ package com.azure.security.keyvault.jca;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.security.ProviderException;
-import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-
 public class FileSystemCertificatesTest {
 
     FileSystemCertificates fileSystemCertificates;
@@ -28,37 +17,8 @@ public class FileSystemCertificatesTest {
 
     @Test
     public void testSetCertificateEntry() {
-        fileSystemCertificates = FileSystemCertificates.FileSystemCertificatesFactory.getCustomFileSystemCertificates(getFilePath("custom\\"));
+        fileSystemCertificates = FileSystemCertificates.getFileSystemCertificates(getFilePath("custom\\"));
         fileSystemCertificates.loadCertificatesFromFileSystem();
         Assertions.assertTrue(fileSystemCertificates.getAliases().contains("sideload"));
     }
-
-    @Test
-    public void testFileSystemCertificatePath() {
-        // Test them together cause System.setProperty will interdependent
-        System.setProperty("azure.keyvault.uri", "https://fake-vaule.vault.azure.net/");
-        System.setProperty("azure.cert-path.well-known", getFilePath("well-known\\"));
-        System.setProperty("azure.cert-path.custom", getFilePath("custom\\"));
-        KeyVaultKeyStore ks = new KeyVaultKeyStore();
-        ks.engineLoad(null);
-        X509Certificate customCertificate = getCertificateByFile(new File(getFilePath("custom\\sideload.x509")));
-        X509Certificate wellKnownCertificate = getCertificateByFile(new File(getFilePath("well-known\\sideload.pem")));
-        assertEquals(wellKnownCertificate, ks.engineGetCertificate("sideload"));
-        assertNotEquals(customCertificate, ks.engineGetCertificate("sideload"));
-
-    }
-
-
-    public X509Certificate getCertificateByFile(File file) {
-        X509Certificate certificate;
-        try (InputStream inputStream = new FileInputStream(file);
-            BufferedInputStream bytes = new BufferedInputStream(inputStream)) {
-            CertificateFactory cf = CertificateFactory.getInstance("X.509");
-            certificate = (X509Certificate) cf.generateCertificate(bytes);
-        } catch (Exception e) {
-            throw new ProviderException(e);
-        }
-        return certificate;
-    }
-
 }
--- sdk/keyvault/azure-security-test-keyvault-jca/src/test/java/com/azure/security/keyvault/jca/test/FileSystemCertificatesTest.java
+++ sdk/keyvault/azure-security-test-keyvault-jca/src/test/java/com/azure/security/keyvault/jca/test/FileSystemCertificatesTest.java
@@ -3,36 +3,83 @@
 
 package com.azure.security.keyvault.jca.test;
 
+import com.azure.security.keyvault.jca.KeyVaultKeyStore;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.File;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.ProviderException;
 import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @EnabledIfEnvironmentVariable(named = "AZURE_KEYVAULT_CERTIFICATE_NAME", matches = "myalias")
 public class FileSystemCertificatesTest {
 
     @BeforeAll
     public static void setEnvironmentProperty() {
-        System.setProperty("azure.cert-path.custom", getFilePath());
         PropertyConvertorUtils.putEnvironmentPropertyToSystemPropertyForKeyVaultJca();
         PropertyConvertorUtils.addKeyVaultJcaProvider();
     }
 
-    public static String getFilePath() {
-        String filepath = "\\src\\test\\resources\\custom\\";
+    public static String getFilePath(String packageName) {
+        String filepath = "\\src\\test\\resources\\" + packageName;
         return System.getProperty("user.dir") + filepath.replace("\\", System.getProperty("file.separator"));
     }
 
     @Test
     public void testGetFileSystemCertificate() throws CertificateException, NoSuchAlgorithmException, KeyStoreException, IOException {
+        System.setProperty("azure.cert-path.custom", getFilePath("custom"));
         KeyStore keyStore = PropertyConvertorUtils.getKeyVaultKeyStore();
         Assertions.assertNotNull(keyStore.getCertificate("sideload"));
+    }
+
+    @Test
+    public void testCertificateLevelBetweenFileSystems() {
+        System.setProperty("azure.cert-path.well-known", getFilePath("well-known\\"));
+        System.setProperty("azure.cert-path.custom", getFilePath("custom\\"));
+        KeyVaultKeyStore ks = new KeyVaultKeyStore();
+        ks.engineLoad(null);
+        X509Certificate customCertificate = getCertificateByFile(new File(getFilePath("custom\\sideload.x509")));
+        X509Certificate wellKnownCertificate = getCertificateByFile(new File(getFilePath("well-known\\sideload.pem")));
+        assertEquals(wellKnownCertificate, ks.engineGetCertificate("sideload"));
+        assertNotEquals(customCertificate, ks.engineGetCertificate("sideload"));
+    }
+
+    @Test
+    public void testCertificateLevelBetweenFileSystemAndClassPath() {
+        System.setProperty("azure.cert-path.custom", getFilePath("custom\\"));
+        KeyVaultKeyStore ks = new KeyVaultKeyStore();
+        ks.engineLoad(null);
+        X509Certificate customCertificateCompareWithClassPath = getCertificateByFile(new File(getFilePath("custom\\sideload2.pem")));
+        X509Certificate classPathCertificate = getCertificateByFile(new File(getFilePath("keyvault\\sideload2.pem")));
+        assertEquals(customCertificateCompareWithClassPath, ks.engineGetCertificate("sideload2"));
+        assertNotEquals(classPathCertificate, ks.engineGetCertificate("sideload2"));
+
+    }
+
+    public X509Certificate getCertificateByFile(File file) {
+        X509Certificate certificate;
+        try (InputStream inputStream = new FileInputStream(file);
+             BufferedInputStream bytes = new BufferedInputStream(inputStream)) {
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            certificate = (X509Certificate) cf.generateCertificate(bytes);
+        } catch (Exception e) {
+            throw new ProviderException(e);
+        }
+        return certificate;
     }
 
 }
