a0560621391cae21a837d9a11499577b520f8a8f
lzc-1997-abel <70368631+lzc-1997-abel@users.noreply.github.com>
2021-06-15 17:03:39 +0800

    remove unused import

--- sdk/keyvault/azure-security-test-keyvault-jca/src/test/java/com/azure/security/keyvault/jca/test/ServerSocketTest.java
+++ sdk/keyvault/azure-security-test-keyvault-jca/src/test/java/com/azure/security/keyvault/jca/test/ServerSocketTest.java
@@ -35,7 +35,6 @@ import java.security.Security;
 import java.security.cert.X509Certificate;
 import java.util.Map;
 
-import static com.azure.security.keyvault.jca.test.PropertyConvertorUtils.SYSTEM_PROPERTIES;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
