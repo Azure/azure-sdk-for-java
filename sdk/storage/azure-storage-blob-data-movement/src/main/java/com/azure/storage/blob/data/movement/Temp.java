1c47ab81f21180a9a0e496a8dd5b2f26727358e1
gapra <gapra@microsoft.com>
2021-06-16 12:13:00 -0700

    Fixed analyze step

--- sdk/storage/azure-storage-blob-data-movement/src/main/java/com/azure/storage/blob/data/movement/Temp.java
+++ sdk/storage/azure-storage-blob-data-movement/src/main/java/com/azure/storage/blob/data/movement/Temp.java
@@ -1,4 +1,7 @@
-package com.azure.storage.blob.data.movement;
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.storage.common.data.movement;
 
 /**
  * Temporary Class
