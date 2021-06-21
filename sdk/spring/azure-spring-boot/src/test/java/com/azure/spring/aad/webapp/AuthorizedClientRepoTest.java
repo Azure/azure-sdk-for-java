679a8cde21f6303cfac966381090902f82e53a4c
Moary Chen <v-moaryc@microsoft.com>
2021-06-21 12:47:52 +0800

    Add isolated annotation and junit-platform properties file.

--- sdk/spring/azure-spring-boot/src/test/java/com/azure/spring/aad/webapp/AuthorizedClientRepoTest.java
+++ sdk/spring/azure-spring-boot/src/test/java/com/azure/spring/aad/webapp/AuthorizedClientRepoTest.java
@@ -4,6 +4,7 @@
 package com.azure.spring.aad.webapp;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Isolated;
 import org.springframework.boot.test.context.assertj.AssertableWebApplicationContext;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
@@ -24,6 +25,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Isolated
 public class AuthorizedClientRepoTest {
 
     private ClientRegistration azure;
