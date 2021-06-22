18724206a46bf6226bf6275b4899e5ee7ff03833
gapra <gapra@microsoft.com>
2021-06-22 16:33:23 -0700

    Analyze step

--- sdk/storage/azure-storage-common/src/test-shared/java/com/azure/storage/common/test/shared/policy/MockFailureResponsePolicy.java
+++ sdk/storage/azure-storage-common/src/test-shared/java/com/azure/storage/common/test/shared/policy/MockFailureResponsePolicy.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.azure.storage.common.test.shared.policy;
 
 import com.azure.core.http.HttpPipelineCallContext;
