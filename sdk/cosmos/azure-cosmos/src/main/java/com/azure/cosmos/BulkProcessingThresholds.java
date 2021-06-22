fd5de1432613280a6754003b9c63e1cdc3e32339
Fabian Meiswinkel <fabian@meiswinkel.com>
2021-06-22 13:18:16 +0000

    Update BulkProcessingThresholds.java

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/BulkProcessingThresholds.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/BulkProcessingThresholds.java
@@ -4,13 +4,16 @@
 package com.azure.cosmos;
 
 import com.azure.cosmos.implementation.batch.PartitionScopeThresholds;
+import com.azure.cosmos.util.Beta;
 
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
+@Beta(value = Beta.SinceVersion.V4_17_0, warningText = Beta.PREVIEW_SUBJECT_TO_CHANGE_WARNING)
 public class BulkProcessingThresholds<TContext> {
     private final ConcurrentMap<String, PartitionScopeThresholds<TContext>> partitionScopeThresholds;
 
+    @Beta(value = Beta.SinceVersion.V4_17_0, warningText = Beta.PREVIEW_SUBJECT_TO_CHANGE_WARNING)
     public BulkProcessingThresholds() {
         this.partitionScopeThresholds = new ConcurrentHashMap<>();
     }
