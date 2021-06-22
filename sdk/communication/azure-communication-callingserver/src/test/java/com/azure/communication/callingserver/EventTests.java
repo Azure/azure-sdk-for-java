7893b1310d27fb4c02f7cc63dc2fccd31117940a
Arif Saikat <71465659+arifsaikat-microsoft@users.noreply.github.com>
2021-06-21 19:35:16 -0700

    event related unit test

--- sdk/communication/azure-communication-callingserver/src/test/java/com/azure/communication/callingserver/EventTests.java
+++ sdk/communication/azure-communication-callingserver/src/test/java/com/azure/communication/callingserver/EventTests.java
@@ -3,8 +3,12 @@
 
 package com.azure.communication.callingserver;
 
+import com.azure.communication.callingserver.implementation.models.AddParticipantResultEventInternal;
 import com.azure.communication.callingserver.implementation.models.CallConnectionStateChangedEventInternal;
+import com.azure.communication.callingserver.implementation.models.ResultInfoInternal;
 import com.azure.communication.callingserver.models.CallConnectionState;
+import com.azure.communication.callingserver.models.OperationStatus;
+import com.azure.communication.callingserver.models.events.AddParticipantResultEvent;
 import com.azure.communication.callingserver.models.events.CallConnectionStateChangedEvent;
 import com.azure.core.util.BinaryData;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -12,10 +16,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import org.junit.jupiter.api.Test;
 
+import java.security.InvalidParameterException;
 import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class EventTests {
     @Test
@@ -28,15 +35,43 @@ public class EventTests {
                 .setServerCallId(serverCallId)
                 .setCallConnectionState(CallConnectionState.CONNECTED);
 
-        ObjectWriter objectWriter = new ObjectMapper().writer().withDefaultPrettyPrinter();
-        String jsonData = objectWriter.writeValueAsString(internalEvent);
-        BinaryData binaryData = BinaryData.fromString(jsonData);
-
-        CallConnectionStateChangedEvent event = CallConnectionStateChangedEvent.deserialize(binaryData);
+        BinaryData binaryData = getBinaryData(internalEvent);
+        CallConnectionStateChangedEvent event =
+            assertDoesNotThrow(() -> CallConnectionStateChangedEvent.deserialize(binaryData));
 
         assertNotNull(event);
         assertEquals(event.getCallConnectionId(), callConnectionId);
         assertEquals(event.getServerCallId(), serverCallId);
         assertEquals(event.getCallConnectionState(), CallConnectionState.CONNECTED);
+    }
+
+    @Test
+    public void addParticipantResultEventTest() throws JsonProcessingException {
+        var operationContext = UUID.randomUUID().toString();
+        var message = "Participant added.";
+        AddParticipantResultEventInternal internalEvent =
+            new AddParticipantResultEventInternal()
+                .setOperationContext(operationContext)
+                .setStatus(OperationStatus.COMPLETED)
+                .setResultInfo(new ResultInfoInternal().setCode(100).setSubcode(200).setMessage(message));
+
+        BinaryData binaryData = getBinaryData(internalEvent);
+        AddParticipantResultEvent event =
+            assertDoesNotThrow(() -> AddParticipantResultEvent.deserialize(binaryData));
+
+        assertNotNull(event);
+        assertEquals(event.getOperationContext(), operationContext);
+        assertEquals(event.getStatus(), OperationStatus.COMPLETED);
+        assertNotNull(event.getResultInfo());
+        assertEquals(event.getResultInfo().getCode(), 100);
+        assertEquals(event.getResultInfo().getSubcode(), 200);
+        assertEquals(event.getResultInfo().getMessage(), message);
+
+    }
+
+    private BinaryData getBinaryData(Object eventObject) throws JsonProcessingException {
+        ObjectWriter objectWriter = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        String jsonData = objectWriter.writeValueAsString(eventObject);
+        return BinaryData.fromString(jsonData);
     }
 }
