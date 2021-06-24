521b748f0fc4f34cd0aa680c84f97dd635670a12
Pallavi Taneja <pallavit@users.noreply.github.com>
2021-06-24 13:09:16 -0700

    Add support for the perf library for ACR.

---
+++ sdk/containerregistry/azure-containers-containerregistry-perf/src/main/java/com/azure/containers/containerregistry/perf/App.java
@@ -0,0 +1,25 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.containers.containerregistry.perf;
+
+import com.azure.perf.test.core.PerfStressProgram;
+
+/**
+ * Runs the Azure Container Registry performance test.
+ *
+ * <p>To run from command line. Package the project into a jar with dependencies via mvn clean package.
+ * Then run the program via java -jar 'compiled-jar-with-dependencies-path' </p>
+ *
+ * <p> To run from IDE, set all the required environment variables in IntelliJ via Run -&gt; EditConfigurations
+ * section.
+ * Then run the App's main method via IDE.</p>
+ */
+public class App {
+    public static void main(String[] args) {
+        PerfStressProgram.run(new Class<?>[]{
+            GetManifestPropertiesTest.class,
+            ListRepositoryTests.class,
+        }, args);
+    }
+}
