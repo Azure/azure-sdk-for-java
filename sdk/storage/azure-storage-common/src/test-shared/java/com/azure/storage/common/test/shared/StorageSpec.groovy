73221a95a03cfb4bff57fe622bfd1fc111edfea3
James <41338290+jaschrep-msft@users.noreply.github.com>
2021-06-25 10:42:36 -0700

    Feature/oauth copyfromurl (#22086)
    * Set up STG78 branch (#21634)
    
    * Generated off latest version of blob swagger (#21644)
    
    * swagger generation and public API overloads
    
    * blob clients obey new generation
    
    * minor changes to bearer token logic
    
    * TEST WORKING
    
    * extra testing
    
    * ported oauthcopy source to blobs
    
    * options API changes and docstrings
    
    * fixed constructor issues
    
    * fix missed source auth misses
    
    * script tags on pom
    
    * removes arg checks in option model constructors
    
    rerecorded tests
    
    * pom fix
    
    * fix misnamed page blob tests
    
    * Test fixes
    
    adds missing recording.
    extra playback logic for oauth tokens that should fix a CI-specific
    issue.
    
    * fixed pom script comment
    
    * downgrade some depenency verions
    
    * rerecord sas tests
    
    * snippets and checkstyle
    
    * rerecorded tests
    
    fixed recording infra for file share tests
    
    * test fixes
    
    * rerecorded versioning tests
    
    * HttpAuthorization added to core-experimental
    
    azure-storage-blob and azure-storage-file-share now depend on
    core-experimental for new class and updated their APIs to use the new
    class.
    
    * refactored options type names
    
    * Reenabled quick query parquet support (#21785)
    
    * reidentified samples for options class name change
    
    * Implemented Immutable Storage with Versioning (#21718)
    
    * File listing v2 (#21720)
    
    * swagger generation and hookup
    
    regenerated files swagger for listing
    hooked up generated and handwritten models
    expanded client API to support new models
    
    * testing and fixes
    
    expanded test parameters for file listing.
    swagger transform added.
    
    * rerecorded file listing test
    
    * more testing
    
    also fixed a method name casing
    
    * docstrings and samples
    
    * checkstyle
    
    * reverted primative boolean extended info arg
    
    REST api accepts true or an absence of value. Alligning with .NET for
    now on Boolean vs boolean.
    
    * checkpoint
    
    * pr feedback
    
    * misused variable fix
    
    * fixed test
    
    * send nothing, not false: include-extended-info
    
    other minor fixes
    rerecorded tests
    
    * import cleanup
    
    * swagger regeneration and test fixes
    
    * API comments
    
    extendedInfo boolean => Boolean
    renamed options class
    
    * reidentified samples for options class name change
    
    Co-authored-by: jschrepp-MSFT <41338290+jschrepp-MSFT@users.noreply.github.com>
    
    * Fix test.
    
    * remove HttpAuthorization in prep for merge
    
    added in separate PR. removing to be ready for that offical addition.
    
    * fix build from merge
    
    * fix pom versioning CI issues
    
    * snippet line length
    
    * fix merge error with immutable versioning
    
    * PR feedback
    
    * Minor cleanup
    
    * generator cleanup and regeneration
    
    * Keep files-shares tests dependent on latest blob
    
    * test refactor/rerecord
    
    * deleted old tests that were copied instead of moved
    
    * min service version reformat
    
    * import cleanup
    
    * post-merge.
    
    * getOauthToken refactor
    
    * reformatting
    
    Co-authored-by: Gauri Prasad <51212198+gapra-msft@users.noreply.github.com>
    Co-authored-by: jschrepp-MSFT <41338290+jschrepp-MSFT@users.noreply.github.com>
    Co-authored-by: gapra <gapra@microsoft.com>
    Co-authored-by: Kamil Sobol <kasobol@microsoft.com>
    Co-authored-by: Kamil Sobol <61715331+kasobol-msft@users.noreply.github.com>

--- sdk/storage/azure-storage-common/src/test-shared/java/com/azure/storage/common/test/shared/StorageSpec.groovy
+++ sdk/storage/azure-storage-common/src/test-shared/java/com/azure/storage/common/test/shared/StorageSpec.groovy
@@ -3,12 +3,14 @@
 
 package com.azure.storage.common.test.shared
 
+import com.azure.core.credential.TokenRequestContext
 import com.azure.core.http.HttpClient
 import com.azure.core.http.netty.NettyAsyncHttpClientBuilder
 import com.azure.core.http.policy.HttpPipelinePolicy
 import com.azure.core.test.InterceptorManager
 import com.azure.core.test.TestMode
 import com.azure.core.util.ServiceVersion
+import com.azure.identity.EnvironmentCredentialBuilder
 import spock.lang.Specification
 
 class StorageSpec extends Specification {
@@ -74,6 +76,17 @@ class StorageSpec extends Specification {
         } else {
             return interceptorManager.getPlaybackClient()
         }
+    }
+
+    private static String getAuthToken() {
+        if (env.testMode == TestMode.PLAYBACK) {
+            // we just need some string to satisfy SDK for playback mode. Recording framework handles this fine.
+            return "recordingBearerToken"
+        }
+        return new EnvironmentCredentialBuilder().build()
+            .getToken(new TokenRequestContext().setScopes(["https://storage.azure.com/.default"]))
+            .map { it.getToken() }
+            .block()
     }
 
     private String getTestName() {
