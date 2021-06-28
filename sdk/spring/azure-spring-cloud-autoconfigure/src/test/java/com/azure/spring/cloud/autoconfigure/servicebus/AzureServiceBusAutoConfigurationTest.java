414c8646286bb0cd423be56eec8b1c78d67ac70c
lzc-1997-abel <70368631+lzc-1997-abel@users.noreply.github.com>
2021-06-28 14:18:57 +0800

    nothing changed, just delete blank line

--- sdk/spring/azure-spring-cloud-autoconfigure/src/test/java/com/azure/spring/cloud/autoconfigure/servicebus/AzureServiceBusAutoConfigurationTest.java
+++ sdk/spring/azure-spring-cloud-autoconfigure/src/test/java/com/azure/spring/cloud/autoconfigure/servicebus/AzureServiceBusAutoConfigurationTest.java
@@ -97,7 +97,6 @@ public class AzureServiceBusAutoConfigurationTest {
             });
     }
 
-
     @Test
     public void testWithAzureResourceManagerProvided() {
         this.contextRunner.withUserConfiguration(TestConfigWithAzureResourceManager.class)
