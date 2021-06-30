c848f61390e2fc87c838fc61cebcee8af31a54e6
Moary Chen <v-moaryc@microsoft.com>
2021-06-30 14:50:54 +0800

    [FEATURE REQ] Make it easier to debug aad-starter (#22289)

--- sdk/spring/azure-spring-boot/src/main/java/com/azure/spring/aad/AbstractOAuth2AuthorizationCodeGrantRequestEntityConverter.java
+++ sdk/spring/azure-spring-boot/src/main/java/com/azure/spring/aad/AbstractOAuth2AuthorizationCodeGrantRequestEntityConverter.java
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.azure.spring.common;
+package com.azure.spring.aad;
 
 import com.azure.spring.utils.ApplicationId;
 import org.springframework.http.HttpEntity;
