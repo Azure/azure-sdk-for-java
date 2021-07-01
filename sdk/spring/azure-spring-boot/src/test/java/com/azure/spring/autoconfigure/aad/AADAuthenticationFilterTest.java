1206dcf99d0456858c3bf7f54f3db6333e6bf6a0
yiliuTo <yiliu6@microsoft.com>
2021-07-01 22:23:27 +0800

    add two tests

--- sdk/spring/azure-spring-boot/src/test/java/com/azure/spring/autoconfigure/aad/AADAuthenticationFilterTest.java
+++ sdk/spring/azure-spring-boot/src/test/java/com/azure/spring/autoconfigure/aad/AADAuthenticationFilterTest.java
@@ -147,28 +147,63 @@ public class AADAuthenticationFilterTest {
             LOG.info("================1==================");
             LOG.info("azure.activedirectory.client-id = {}", context.getEnvironment().getProperty("azure.activedirectory.client-id"));
             LOG.info("spring.cloud.azure.client-id = {}", context.getEnvironment().getProperty("spring.cloud.azure.client-id"));
+            LOG.info("test.prop = {}", context.getEnvironment().getProperty("test.prop"));
             assertThat(context).doesNotHaveBean(AADAuthenticationFilterAutoConfiguration.class);
         });
 
+//        this.contextRunner.withPropertyValues(
+//                "azure.activedirectory.client-id=" + TestConstants.CLIENT_ID
+//            ).run(context -> {
+//            LOG.info("================2==================");
+//            LOG.info("azure.activedirectory.client-id = {}", context.getEnvironment().getProperty("azure.activedirectory.client-id"));
+//            LOG.info("spring.cloud.azure.client-id = {}", context.getEnvironment().getProperty("spring.cloud.azure.client-id"));
+//
+//            assertThat(context).hasSingleBean(AADAuthenticationFilterAutoConfiguration.class);
+//            });
+
         this.contextRunner.withPropertyValues(
-                "azure.activedirectory.client-id=" + TestConstants.CLIENT_ID
+                "spring.cloud.azure.client-id=66666", "test.prop=123456"
             ).run(context -> {
-            LOG.info("================2==================");
+            LOG.info("================3==================");
             LOG.info("azure.activedirectory.client-id = {}", context.getEnvironment().getProperty("azure.activedirectory.client-id"));
             LOG.info("spring.cloud.azure.client-id = {}", context.getEnvironment().getProperty("spring.cloud.azure.client-id"));
 
             assertThat(context).hasSingleBean(AADAuthenticationFilterAutoConfiguration.class);
             });
 
+    }
+
+    @Test
+    public void testAutoConfiguration2() {
+
+        this.contextRunner.run(context -> {
+            LOG.info("================4==================");
+            LOG.info("azure.activedirectory.client-id = {}", context.getEnvironment().getProperty("azure.activedirectory.client-id"));
+            LOG.info("spring.cloud.azure.client-id = {}", context.getEnvironment().getProperty("spring.cloud.azure.client-id"));
+            LOG.info("test.prop = {}", context.getEnvironment().getProperty("test.prop"));
+
+            assertThat(context).doesNotHaveBean(AADAuthenticationFilterAutoConfiguration.class);
+        });
+
 //        this.contextRunner.withPropertyValues(
-//                "spring.cloud.azure.client-id=" + TestConstants.CLIENT_ID
-//            ).run(context -> {
+//            "azure.activedirectory.client-id=" + TestConstants.CLIENT_ID
+//        ).run(context -> {
+//            LOG.info("================2==================");
+//            LOG.info("azure.activedirectory.client-id = {}", context.getEnvironment().getProperty("azure.activedirectory.client-id"));
+//            LOG.info("spring.cloud.azure.client-id = {}", context.getEnvironment().getProperty("spring.cloud.azure.client-id"));
+//
+//            assertThat(context).hasSingleBean(AADAuthenticationFilterAutoConfiguration.class);
+//        });
+//
+//        this.contextRunner.withPropertyValues(
+//            "spring.cloud.azure.client-id=" + TestConstants.CLIENT_ID
+//        ).run(context -> {
 //            LOG.info("================3==================");
 //            LOG.info("azure.activedirectory.client-id = {}", context.getEnvironment().getProperty("azure.activedirectory.client-id"));
 //            LOG.info("spring.cloud.azure.client-id = {}", context.getEnvironment().getProperty("spring.cloud.azure.client-id"));
 //
 //            assertThat(context).hasSingleBean(AADAuthenticationFilterAutoConfiguration.class);
-//            });
+//        });
 
     }
 }
