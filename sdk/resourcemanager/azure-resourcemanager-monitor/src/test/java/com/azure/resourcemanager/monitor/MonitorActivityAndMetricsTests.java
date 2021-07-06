b0e5756089ac03a8e126c7a71d415b511b71ea09
Weidong Xu <weidxu@microsoft.com>
2021-07-06 12:52:17 +0800

    fix tests

--- sdk/resourcemanager/azure-resourcemanager-monitor/src/test/java/com/azure/resourcemanager/monitor/MonitorActivityAndMetricsTests.java
+++ sdk/resourcemanager/azure-resourcemanager-monitor/src/test/java/com/azure/resourcemanager/monitor/MonitorActivityAndMetricsTests.java
@@ -20,7 +20,11 @@ import org.junit.jupiter.api.Test;
 public class MonitorActivityAndMetricsTests extends MonitorManagementTest {
     @Test
     public void canListEventsAndMetrics() throws Exception {
-        OffsetDateTime recordDateTime = OffsetDateTime.parse("2020-12-28T21:44:57.424+08:00").minusDays(40);
+        // make sure there exists a VM
+
+        OffsetDateTime recordDateTime = this.isPlaybackMode()
+            ? OffsetDateTime.parse("2020-12-28T21:44:57.424+08:00").minusDays(40)
+            : OffsetDateTime.now().minusDays(40);
         VirtualMachine vm = computeManager.virtualMachines().list().iterator().next();
 
         // Metric Definition
