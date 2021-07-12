f9fe9e7af558ca3f534fa6fa4ad8c94ca105210d
Weidong Xu <weidxu@microsoft.com>
2021-07-12 10:17:08 +0800

    regen postgresql

--- sdk/postgresql/azure-resourcemanager-postgresql/src/main/java/com/azure/resourcemanager/postgresql/models/Server.java
+++ sdk/postgresql/azure-resourcemanager-postgresql/src/main/java/com/azure/resourcemanager/postgresql/models/Server.java
@@ -447,4 +447,22 @@ public interface Server {
      * @return the refreshed resource.
      */
     Server refresh(Context context);
+
+    /**
+     * Restarts a server.
+     *
+     * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
+     * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
+     */
+    void restart();
+
+    /**
+     * Restarts a server.
+     *
+     * @param context The context to associate with this operation.
+     * @throws IllegalArgumentException thrown if parameters fail the validation.
+     * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
+     * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
+     */
+    void restart(Context context);
 }
