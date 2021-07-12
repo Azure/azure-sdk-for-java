92f26ce2ce7f14c7c29f0e05685e210c2e716705
Mike Harder <mharder@microsoft.com>
2021-07-12 13:32:04 -0700

    Add package

--- common/perf-test-core/src/main/java/com/azure/perf/test/core/TestProxyPolicy.java
+++ common/perf-test-core/src/main/java/com/azure/perf/test/core/TestProxyPolicy.java
@@ -1,3 +1,8 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.perf.test.core;
+
 import com.azure.core.http.HttpPipelineCallContext;
 import com.azure.core.http.HttpPipelineNextPolicy;
 import com.azure.core.http.HttpPipelinePosition;
