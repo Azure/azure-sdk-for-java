3d953d99a023542e04a67224fe0b113ca26e0eb2
Mike Harder <mharder@microsoft.com>
2021-07-12 19:53:00 -0700

    Move options to separate class

--- common/perf-test-core/src/main/java/com/azure/perf/test/core/HttpPipelineTest.java
+++ common/perf-test-core/src/main/java/com/azure/perf/test/core/HttpPipelineTest.java
@@ -9,15 +9,14 @@ import com.azure.core.http.HttpPipelineBuilder;
 import com.azure.core.http.HttpRequest;
 import com.azure.core.http.netty.NettyAsyncHttpClientBuilder;
 import com.azure.core.http.policy.HttpPipelinePolicy;
-import com.beust.jcommander.Parameter;
 import reactor.core.publisher.Mono;
 
-import java.net.URL;
+import java.util.ArrayList;
 
-public class HttpPipelineTest extends PerfStressTest<HttpPipelineTest.HttpPipelineOptions> {
+public class HttpPipelineTest extends PerfStressTest<HttpPipelineOptions> {
     private final HttpPipeline httpPipeline;
 
-    public HttpPipelineTest(HttpPipelineTest.HttpPipelineOptions options) {
+    public HttpPipelineTest(HttpPipelineOptions options) {
         super(options);
 
         HttpPipelineBuilder builder = new HttpPipelineBuilder();
@@ -27,9 +26,11 @@ public class HttpPipelineTest extends PerfStressTest<HttpPipelineTest.HttpPipeli
         }
 
         if (policies != null) {
+            ArrayList<HttpPipelinePolicy> policyList = new ArrayList<HttpPipelinePolicy>();
             for (HttpPipelinePolicy policy : policies) {
-                builder.addPolicy(policy);
+                policyList.add(policy);
             }
+            builder.policies(policyList.toArray(new HttpPipelinePolicy[0]));
         }
 
         httpPipeline = builder.build();
@@ -45,14 +46,5 @@ public class HttpPipelineTest extends PerfStressTest<HttpPipelineTest.HttpPipeli
     public Mono<Void> runAsync() {
         HttpRequest request = new HttpRequest(HttpMethod.GET, options.getUrl());
         return httpPipeline.send(request).then();
-    }
-
-    public class HttpPipelineOptions extends PerfStressOptions {
-        @Parameter(names = { "-u", "--url" }, description = "URL to fetch")
-        private URL url;
-
-        public URL getUrl() {
-            return url;
-        }
     }
 }
