551d30727979c897d0fda8c63f392fcb612adbaa
Alan Zimmer <48699787+alzimmermsft@users.noreply.github.com>
2021-07-21 07:03:48 -0700

    Add Custom Annotation to Parameterize Tests with more Complex Scenarios (#20484)
    Add Custom Annotation to Parameterize Tests with more Complex Scenarios

--- sdk/containerregistry/azure-containers-containerregistry/src/test/java/com/azure/containers/containerregistry/ContainerRepositoryAnonymousAccessTests.java
+++ sdk/containerregistry/azure-containers-containerregistry/src/test/java/com/azure/containers/containerregistry/ContainerRepositoryAnonymousAccessTests.java
@@ -4,7 +4,6 @@
 package com.azure.containers.containerregistry;
 
 import com.azure.core.http.HttpClient;
-import com.azure.core.test.implementation.ImplUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -22,7 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class ContainerRepositoryAnonymousAccessTests extends ContainerRegistryClientsTestBase {
     @BeforeEach
     void beforeEach() {
-        TestUtils.importImageAsync(ImplUtils.getTestMode(), ANONYMOUS_REGISTRY_NAME, HELLO_WORLD_REPOSITORY_NAME, Arrays.asList("latest", "v1", "v2", "v3", "v4")).block();
+        TestUtils.importImageAsync(getTestMode(), ANONYMOUS_REGISTRY_NAME, HELLO_WORLD_REPOSITORY_NAME, Arrays.asList("latest", "v1", "v2", "v3", "v4")).block();
     }
 
     @ParameterizedTest(name = DISPLAY_NAME_WITH_ARGUMENTS)
@@ -30,6 +29,6 @@ public class ContainerRepositoryAnonymousAccessTests extends ContainerRegistryCl
     public void listAnonymousRepositories(HttpClient httpClient) {
         ContainerRegistryClient client = getContainerRegistryBuilder(httpClient, null, ANONYMOUS_REGISTRY_ENDPOINT).buildClient();
         List<String> repositories = client.listRepositoryNames().stream().collect(Collectors.toList());
-        assertTrue(repositories.stream().anyMatch(a -> HELLO_WORLD_REPOSITORY_NAME.equals(a)));
+        assertTrue(repositories.stream().anyMatch(HELLO_WORLD_REPOSITORY_NAME::equals));
     }
 }
