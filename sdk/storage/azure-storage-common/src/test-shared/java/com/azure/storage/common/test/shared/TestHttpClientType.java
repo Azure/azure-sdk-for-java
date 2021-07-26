ef92f1d05cb0beb2856d0de87a22b06217017ede
Kamil Sobol <61715331+kasobol-msft@users.noreply.github.com>
2021-07-26 15:02:03 -0700

    [Storage] wire ok http client in tests. (#23179)
    * does this work?
    
    * print.
    
    * move this to test scope.
    
    * fix build.

---
+++ sdk/storage/azure-storage-common/src/test-shared/java/com/azure/storage/common/test/shared/TestHttpClientType.java
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.storage.common.test.shared;
+
+public enum TestHttpClientType {
+    NETTY,
+    OK_HTTP
+}
