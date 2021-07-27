ae606a54900ea21b1b1c778bfc890ad10e72d579
Franko Morales <67804607+cochi2@users.noreply.github.com>
2021-07-27 13:37:52 -0700

    FIX: Editing ChangeLog and Readme for CallingServer. (#23167)
    Also, Applying new comments received for the RedirectPolicy

--- sdk/communication/azure-communication-callingserver/src/test/java/com/azure/communication/callingserver/implementation/RedirectPolicyTests.java
+++ sdk/communication/azure-communication-callingserver/src/test/java/com/azure/communication/callingserver/implementation/RedirectPolicyTests.java
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.azure.communication.common.implementation;
+package com.azure.communication.callingserver.implementation;
 
 import com.azure.core.http.HttpClient;
 import com.azure.core.http.HttpMethod;
