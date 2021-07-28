03f84bb8be1b301f7a9c9643810e500461c24fc3
shafang <Shawn.Fang@microsoft.com>
2021-07-27 22:52:44 -0700

    simplify the samples

--- sdk/textanalytics/azure-ai-textanalytics/src/samples/java/com/azure/ai/textanalytics/lro/AnalyzeExtractiveSummarization.java
+++ sdk/textanalytics/azure-ai-textanalytics/src/samples/java/com/azure/ai/textanalytics/lro/AnalyzeExtractiveSummarization.java
@@ -7,18 +7,14 @@ import com.azure.ai.textanalytics.TextAnalyticsClient;
 import com.azure.ai.textanalytics.TextAnalyticsClientBuilder;
 import com.azure.ai.textanalytics.models.AnalyzeActionsOperationDetail;
 import com.azure.ai.textanalytics.models.AnalyzeActionsOptions;
-import com.azure.ai.textanalytics.models.AnalyzeActionsResult;
 import com.azure.ai.textanalytics.models.ExtractSummaryAction;
 import com.azure.ai.textanalytics.models.ExtractSummaryActionResult;
 import com.azure.ai.textanalytics.models.ExtractSummaryResult;
 import com.azure.ai.textanalytics.models.SummarySentence;
 import com.azure.ai.textanalytics.models.SummarySentencesOrder;
 import com.azure.ai.textanalytics.models.TextAnalyticsActions;
-import com.azure.ai.textanalytics.models.TextDocumentInput;
 import com.azure.ai.textanalytics.util.AnalyzeActionsResultPagedIterable;
 import com.azure.core.credential.AzureKeyCredential;
-import com.azure.core.http.rest.PagedResponse;
-import com.azure.core.util.Context;
 import com.azure.core.util.polling.SyncPoller;
 
 import java.util.ArrayList;
@@ -38,31 +34,28 @@ public class AnalyzeExtractiveSummarization {
                                          .endpoint("{endpoint}")
                                          .buildClient();
 
-        List<TextDocumentInput> documents = new ArrayList<>();
-        for (int i = 0; i < 21; i++) {
-            documents.add(new TextDocumentInput(Integer.toString(i),
-                "The government of British Prime Minster Theresa May has been plunged into turmoil with "
-                    + "the resignation of two senior Cabinet ministers in a deep split over her Brexit strategy."
-                    + "The Foreign Secretary Boris Johnson, quit on Monday, hours after the resignation late on"
-                    + "Sunday night of the minister in charge of Brexit negotiations, David Davis.  Their"
-                    + "decision to leave the government came three days after May appeared to have agreed a"
-                    + "deal with her fractured Cabinet on the UK's post-Brexit relationship with the EU."
-                    + "That plan is now in tatters and her political future appears uncertain."
-                    + "May appeared in Parliament on Monday afternoon to defend her plan, minutes after"
-                    + "Downing Street confirmed the departure of Johnson. May acknowledged the splits in"
-                    + "her statement to MPs, saying of the ministers who quit: \"We do not agree about the"
-                    + "best way of delivering our shared commitment to honoring the result of the referendum.\""
-                    + "The Prime Minister's latest plitical drama began late on Sunday night when Davis quit,"
-                    + "declaring he could not support May's Brexit plan.  He said it involved too close a "
-                    + "relationship with the EU and gave only an illusion of control being returned to the UK"
-                    + "after it left the EU. \"It seems to me we're giving too much away, too easily, and"
-                    + "that's a dangerous strategy at this time,\" Davis said in a BBC radio interview Monday"
-                    + "morning. Johnson's resignation came Monday afternoon local time, just before the Prime"
-                    + " Minister was due to make a scheduled statement in Parliament. \"This afternoon, the Prime"
-                    + "Minister accepted the resignation of Boris Johnson as Foreign Secretary,\" a"
-                    + "statement from Downing Street said."
-            ));
-        }
+        List<String> documents = new ArrayList<>();
+        documents.add(
+            "The government of British Prime Minster Theresa May has been plunged into turmoil with "
+                + "the resignation of two senior Cabinet ministers in a deep split over her Brexit strategy."
+                + "The Foreign Secretary Boris Johnson, quit on Monday, hours after the resignation late on"
+                + "Sunday night of the minister in charge of Brexit negotiations, David Davis.  Their "
+                + "decision to leave the government came three days after May appeared to have agreed a"
+                + "deal with her fractured Cabinet on the UK's post-Brexit relationship with the EU."
+                + "That plan is now in tatters and her political future appears uncertain."
+                + "May appeared in Parliament on Monday afternoon to defend her plan, minutes after"
+                + "Downing Street confirmed the departure of Johnson. May acknowledged the splits in"
+                + "her statement to MPs, saying of the ministers who quit: \"We do not agree about the"
+                + "best way of delivering our shared commitment to honoring the result of the referendum.\""
+                + "The Prime Minister's latest plitical drama began late on Sunday night when Davis quit,"
+                + "declaring he could not support May's Brexit plan.  He said it involved too close a "
+                + "relationship with the EU and gave only an illusion of control being returned to the UK"
+                + "after it left the EU. \"It seems to me we're giving too much away, too easily, and"
+                + "that's a dangerous strategy at this time,\" Davis said in a BBC radio interview Monday"
+                + "morning. Johnson's resignation came Monday afternoon local time, just before the Prime"
+                + " Minister was due to make a scheduled statement in Parliament. \"This afternoon, the Prime"
+                + "Minister accepted the resignation of Boris Johnson as Foreign Secretary,\" a"
+                + "statement from Downing Street said.");
 
         SyncPoller<AnalyzeActionsOperationDetail, AnalyzeActionsResultPagedIterable> syncPoller =
             client.beginAnalyzeActions(documents,
@@ -71,22 +64,12 @@ public class AnalyzeExtractiveSummarization {
                         new ExtractSummaryAction()
                             .setMaxSentenceCount(2)
                             .setSentencesOrderBy(SummarySentencesOrder.RANK)),
-                new AnalyzeActionsOptions(),
-                Context.NONE);
+                "en",
+                new AnalyzeActionsOptions());
 
         syncPoller.waitForCompletion();
 
-        Iterable<PagedResponse<AnalyzeActionsResult>> pagedResults = syncPoller.getFinalResult().iterableByPage();
-        for (PagedResponse<AnalyzeActionsResult> perPage : pagedResults) {
-            processAnalyzeActionsResult(perPage);
-        }
-    }
-
-    private static void processAnalyzeActionsResult(PagedResponse<AnalyzeActionsResult> perPage) {
-        System.out.printf("Response code: %d, Continuation Token: %s.%n",
-            perPage.getStatusCode(), perPage.getContinuationToken());
-
-        for (AnalyzeActionsResult actionsResult : perPage.getElements()) {
+        syncPoller.getFinalResult().forEach(actionsResult -> {
             System.out.println("Extractive Summarization action results:");
             for (ExtractSummaryActionResult actionResult : actionsResult.getExtractSummaryResults()) {
                 if (!actionResult.isError()) {
@@ -109,6 +92,6 @@ public class AnalyzeExtractiveSummarization {
                         actionResult.getError().getMessage());
                 }
             }
-        }
+        });
     }
 }
