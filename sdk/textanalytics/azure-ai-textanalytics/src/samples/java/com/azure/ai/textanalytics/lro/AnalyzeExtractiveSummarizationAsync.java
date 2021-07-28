37f418ccc3cb629a25d08955c45977e8a2cf386d
shafang <Shawn.Fang@microsoft.com>
2021-07-27 22:54:30 -0700

    custom credential

--- sdk/textanalytics/azure-ai-textanalytics/src/samples/java/com/azure/ai/textanalytics/lro/AnalyzeExtractiveSummarizationAsync.java
+++ sdk/textanalytics/azure-ai-textanalytics/src/samples/java/com/azure/ai/textanalytics/lro/AnalyzeExtractiveSummarizationAsync.java
@@ -15,7 +15,6 @@ import com.azure.ai.textanalytics.models.SummarySentence;
 import com.azure.ai.textanalytics.models.SummarySentencesOrder;
 import com.azure.ai.textanalytics.models.TextAnalyticsActions;
 import com.azure.core.credential.AzureKeyCredential;
-import com.azure.core.util.Configuration;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,8 +31,8 @@ public class AnalyzeExtractiveSummarizationAsync {
      */
     public static void main(String[] args) {
         TextAnalyticsAsyncClient client = new TextAnalyticsClientBuilder()
-                                              .credential(new AzureKeyCredential(Configuration.getGlobalConfiguration().get("AZURE_TEXT_ANALYTICS_API_KEY")))
-                                              .endpoint(Configuration.getGlobalConfiguration().get("AZURE_TEXT_ANALYTICS_ENDPOINT"))
+                                              .credential(new AzureKeyCredential("{key}"))
+                                              .endpoint("{endpoint}")
                                               .buildAsyncClient();
 
         List<String> documents = new ArrayList<>();
