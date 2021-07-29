b8ad26b76dd5a807f2795b58cf45e078a68ad578
Mike Harder <mharder@microsoft.com>
2021-07-29 15:28:45 -0700

    Consume response body

--- common/perf-test-core/src/main/java/com/azure/perf/test/core/HttpPipelineTest.java
+++ common/perf-test-core/src/main/java/com/azure/perf/test/core/HttpPipelineTest.java
@@ -7,14 +7,17 @@ import com.azure.core.http.HttpMethod;
 import com.azure.core.http.HttpPipeline;
 import com.azure.core.http.HttpPipelineBuilder;
 import com.azure.core.http.HttpRequest;
-import com.azure.core.http.netty.NettyAsyncHttpClientBuilder;
+import com.azure.core.http.HttpResponse;
 import com.azure.core.http.policy.HttpPipelinePolicy;
 import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
 
 public class HttpPipelineTest extends PerfStressTest<HttpPipelineOptions> {
+    private static final int BUFFER_SIZE = 16 * 1024 * 1024;
+
     private final HttpPipeline httpPipeline;
+    private final byte[] buffer = new byte[BUFFER_SIZE];
 
     public HttpPipelineTest(HttpPipelineOptions options) {
         super(options);
@@ -38,13 +41,26 @@ public class HttpPipelineTest extends PerfStressTest<HttpPipelineOptions> {
 
     @Override
     public void run() {
-        HttpRequest request = new HttpRequest(HttpMethod.GET, options.getUrl());
-        httpPipeline.send(request).block();
+        runAsync().block();
     }
 
     @Override
     public Mono<Void> runAsync() {
         HttpRequest request = new HttpRequest(HttpMethod.GET, options.getUrl());
-        return httpPipeline.send(request).then();
+        return httpPipeline
+            .send(request)
+            .flatMapMany(HttpResponse::getBody)
+            .map(b -> {
+                int readCount = 0;
+                int remaining = b.remaining();
+                while (readCount < remaining) {
+                    int expectedReadCount = Math.min(remaining - readCount, BUFFER_SIZE);
+                    b.get(buffer, 0, expectedReadCount);
+                    readCount += expectedReadCount;
+                }
+
+                return 1;
+            })
+            .then();
     }
 }
