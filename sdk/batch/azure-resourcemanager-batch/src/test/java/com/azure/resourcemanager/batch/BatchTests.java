9024512ab6c670ff288d28eead8d2e3746fa67d7
Haoling Dong <haolingdong@microsoft.com>
2021-07-29 15:21:39 +0800

    add one test case for batch service

---
+++ sdk/batch/azure-resourcemanager-batch/src/test/java/com/azure/resourcemanager/batch/BatchTests.java
@@ -0,0 +1,78 @@
+package com.azure.resourcemanager.batch;
+
+import com.azure.core.http.policy.HttpLogDetailLevel;
+import com.azure.core.http.policy.HttpLogOptions;
+import com.azure.core.management.AzureEnvironment;
+import com.azure.core.management.Region;
+import com.azure.core.management.profile.AzureProfile;
+import com.azure.core.test.TestBase;
+import com.azure.core.test.annotation.DoNotRecord;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.resourcemanager.batch.models.AutoStorageBaseProperties;
+import com.azure.resourcemanager.batch.models.BatchAccount;
+import com.azure.resourcemanager.storage.StorageManager;
+import com.azure.resourcemanager.storage.models.StorageAccount;
+import org.junit.jupiter.api.Test;
+
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BatchTests extends TestBase {
+
+    private static final Random RANDOM = new Random();
+
+    private static final Region REGION = Region.US_WEST2;
+    private static final String RESOURCE_GROUP = "rg" + randomPadding();
+    private static final String STORAGE_ACCOUNT = "sa" + randomPadding();
+    private static final String BATCH_ACCOUNT = "ba" + randomPadding();
+
+    @Test
+    @DoNotRecord(skipInPlayback = true)
+    public void testCreateBatchAccount() {
+        BatchManager batchManager = BatchManager
+            .configure().withLogOptions(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BODY_AND_HEADERS))
+            .authenticate(new DefaultAzureCredentialBuilder().build(), new AzureProfile(AzureEnvironment.AZURE));
+
+        StorageManager storageManager = StorageManager
+            .configure().withLogOptions(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BODY_AND_HEADERS))
+            .authenticate(new DefaultAzureCredentialBuilder().build(), new AzureProfile(AzureEnvironment.AZURE));
+
+        storageManager.resourceManager().resourceGroups().define(RESOURCE_GROUP)
+            .withRegion(REGION)
+            .create();
+
+        // storage account
+        StorageAccount storageAccount = storageManager.storageAccounts().define(STORAGE_ACCOUNT)
+            .withRegion(REGION)
+            .withExistingResourceGroup(RESOURCE_GROUP)
+            .create();
+
+        // batch account
+        BatchAccount account = batchManager
+            .batchAccounts()
+            .define(BATCH_ACCOUNT)
+            .withRegion(REGION)
+            .withExistingResourceGroup(RESOURCE_GROUP)
+            .withAutoStorage(
+                new AutoStorageBaseProperties()
+                    .withStorageAccountId(storageAccount.id()))
+            .create();
+
+
+        assertNotNull(account);
+
+        BatchAccount batchAccount = batchManager.batchAccounts().getByResourceGroup(RESOURCE_GROUP, BATCH_ACCOUNT);
+        assertEquals(BATCH_ACCOUNT, batchAccount.name());
+        assertEquals(REGION.toString(), batchAccount.location());
+
+        batchManager.batchAccounts().deleteById(batchAccount.id());
+    }
+
+    private static String randomPadding() {
+        return String.format("%05d", Math.abs(RANDOM.nextInt() % 100000));
+    }
+
+
+}
