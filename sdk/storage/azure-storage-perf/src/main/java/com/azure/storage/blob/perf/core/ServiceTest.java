fe91bce10dfb966c857d45a27440b9f27dc6d147
Mike Harder <mharder@microsoft.com>
2021-07-29 17:48:35 -0700

    Remove unused import

--- sdk/storage/azure-storage-perf/src/main/java/com/azure/storage/blob/perf/core/ServiceTest.java
+++ sdk/storage/azure-storage-perf/src/main/java/com/azure/storage/blob/perf/core/ServiceTest.java
@@ -3,7 +3,6 @@
 
 package com.azure.storage.blob.perf.core;
 
-import com.azure.core.http.policy.HttpPipelinePolicy;
 import com.azure.core.util.Configuration;
 import com.azure.core.util.CoreUtils;
 import com.azure.perf.test.core.PerfStressOptions;
