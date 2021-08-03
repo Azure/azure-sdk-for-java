ec71e86fd74f03b855d2f67badb2232cd135de57
Feiyue Yu <iamyfy@163.com>
2021-08-03 15:56:10 +0800

    Fix RequestOptions

--- sdk/core/azure-core/src/main/java/com/azure/core/http/rest/RequestOptions.java
+++ sdk/core/azure-core/src/main/java/com/azure/core/http/rest/RequestOptions.java
@@ -65,9 +65,10 @@ import java.util.function.Consumer;
  *
  * {@codesnippet com.azure.core.http.rest.requestoptions.postrequest}
  */
-final class RequestOptions {
+public final class RequestOptions {
     private Consumer<HttpRequest> requestCallback = request -> { };
     private boolean throwOnError = true;
+    private BinaryData requestBody;
 
     /**
      * Gets the request callback, applying all the configurations set on this RequestOptions.
