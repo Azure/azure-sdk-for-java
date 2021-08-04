5d65ba0b2e92f820a87b173ccfbf39151a36e0f9
Fabian Meiswinkel <fabian@meiswinkel.com>
2021-08-04 23:43:16 +0000

    Update ItemsDataWriteFactory.scala

--- sdk/cosmos/azure-cosmos-spark_3-1_2-12/src/main/scala/com/azure/cosmos/spark/ItemsDataWriteFactory.scala
+++ sdk/cosmos/azure-cosmos-spark_3-1_2-12/src/main/scala/com/azure/cosmos/spark/ItemsDataWriteFactory.scala
@@ -74,6 +74,7 @@ private class ItemsDataWriteFactory(userConfig: Map[String, String],
     private val client = CosmosClientCache(CosmosClientConfiguration(userConfig, useEventualConsistency = true), Some(cosmosClientStateHandle))
 
     private val container = ThroughputControlHelper.getContainer(userConfig, cosmosTargetContainerConfig, client)
+
     private val containerDefinition = container.read().block().getProperties
     private val partitionKeyDefinition = containerDefinition.getPartitionKeyDefinition
 
