1eec9b9b029f9a56bb72f6e17e617547b550818c
Feiyue Yu <iamyfy@163.com>
2021-08-04 18:14:27 +0800

    test

---
+++ sdk/purview/azure-analytics-purview-catalog/src/test/java/com/azure/analytics/purview/catalog/GlossaryBaseClientTests.java
@@ -0,0 +1,33 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.analytics.purview.catalog;
+
+import com.azure.core.util.BinaryData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+public class GlossaryBaseClientTests extends PurviewCatalogClientTestBase {
+    private GlossaryClient client;
+
+    @Override
+    protected void beforeTest() {
+        client = clientSetup(httpPipeline -> new PurviewCatalogClientBuilder()
+            .endpoint(getEndpoint())
+            .pipeline(httpPipeline)
+            .buildGlossaryClient());
+    }
+
+    @Test
+    public void testListGlossaries() {
+        BinaryData binaryData = client.listGlossaries(null);
+        List<?> glossaries = binaryData.toObject(List.class);
+        Assertions.assertEquals(1, glossaries.size());
+        Map<?, ?> map = (Map<?, ?>)glossaries.get(0);
+        List<?> terms = (List<?>) map.get("terms");
+        Assertions.assertEquals(1, terms.size());
+    }
+}
---
+++ sdk/purview/azure-analytics-purview-scanning/src/test/java/com/azure/analytics/purview/scanning/SystemScanRulesetsBaseClientTests.java
@@ -0,0 +1,34 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.analytics.purview.scanning;
+
+import com.azure.core.http.rest.PagedIterable;
+import com.azure.core.util.BinaryData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+
+public class SystemScanRulesetsBaseClientTests extends PurviewScanningClientTestBase {
+    private SystemScanRulesetsClient client;
+
+    @Override
+    protected void beforeTest() {
+        client = clientSetup(httpPipeline -> new PurviewScanningClientBuilder()
+            .endpoint(getEndpoint())
+            .pipeline(httpPipeline)
+            .buildSystemScanRulesetsClient());
+    }
+
+    @Test
+    public void testListAll() {
+        PagedIterable<BinaryData> response = client.listAll(null);
+        List<BinaryData> list = response.stream().collect(Collectors.toList());
+        System.err.println(list);
+        Assertions.assertTrue(list.size() > 0);
+    }
+}
