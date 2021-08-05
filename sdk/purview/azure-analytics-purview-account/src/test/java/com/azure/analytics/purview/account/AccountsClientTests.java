fa2345548e3030be5ab0ac0b9a0b1275e06e3836
Feiyue Yu <iamyfy@163.com>
2021-08-05 11:30:59 +0800

    Account test

---
+++ sdk/purview/azure-analytics-purview-account/src/test/java/com/azure/analytics/purview/account/AccountsClientTests.java
@@ -0,0 +1,21 @@
+package com.azure.analytics.purview.account;
+
+import com.azure.core.util.BinaryData;
+import org.junit.jupiter.api.Test;
+
+public class AccountsClientTests extends PurviewAccountClientTestBase {
+    private AccountsClient client;
+
+    @Override
+    protected void beforeTest() {
+        client = clientSetup(httpPipeline -> new PurviewAccountClientBuilder()
+            .host(getEndpoint())
+            .pipeline(httpPipeline)
+            .buildAccountsClient());
+    }
+
+    @Test
+    public void testGetAccount() {
+        BinaryData response = client.get(null);
+    }
+}
