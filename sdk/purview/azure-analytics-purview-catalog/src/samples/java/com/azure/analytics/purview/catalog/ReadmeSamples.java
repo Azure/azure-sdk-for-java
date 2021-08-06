8d3417d417988e0eb28ae76c3fa6ec25cd392223
Feiyue Yu <iamyfy@163.com>
2021-08-06 13:28:05 +0800

    samples

--- sdk/purview/azure-analytics-purview-catalog/src/samples/java/com/azure/analytics/purview/catalog/ReadmeSamples.java
+++ sdk/purview/azure-analytics-purview-catalog/src/samples/java/com/azure/analytics/purview/catalog/ReadmeSamples.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.azure.analytics.purview.catalog;
 
 import com.azure.identity.DefaultAzureCredentialBuilder;
--- sdk/purview/azure-analytics-purview-scanning/src/samples/java/com/azure/analytics/purview/scanning/ReadmeSamples.java
+++ sdk/purview/azure-analytics-purview-scanning/src/samples/java/com/azure/analytics/purview/scanning/ReadmeSamples.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.azure.analytics.purview.scanning;
 
 import com.azure.identity.DefaultAzureCredentialBuilder;
