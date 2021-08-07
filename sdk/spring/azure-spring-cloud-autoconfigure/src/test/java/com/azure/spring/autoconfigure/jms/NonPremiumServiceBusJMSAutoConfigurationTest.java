e8be15da4945c2ed4d1a2c08c796f4701e44ec40
Yi Liu <yiliu6@microsoft.com>
2021-08-07 11:43:44 +0800

    Mapping spring legacy props (#23166)
    * use file for property mapping
    
    * add environmentpostprocessor after kv
    
    * map multiple key vault use case

--- sdk/spring/azure-spring-cloud-autoconfigure/src/test/java/com/azure/spring/autoconfigure/jms/NonPremiumServiceBusJMSAutoConfigurationTest.java
+++ sdk/spring/azure-spring-cloud-autoconfigure/src/test/java/com/azure/spring/autoconfigure/jms/NonPremiumServiceBusJMSAutoConfigurationTest.java
@@ -104,7 +104,7 @@ public class NonPremiumServiceBusJMSAutoConfigurationTest {
             context -> {
                 assertThat(context).hasBean(AZURE_PROPERTY_BEAN_NAME);
 
-                assertThat(((AzureProperties) context.getBean(AZURE_PROPERTY_BEAN_NAME)).getCredential().getCertificatePassword())
+                assertThat(((AzureProperties) context.getBean(AZURE_PROPERTY_BEAN_NAME)).getCredential().getClientCertificatePassword())
                     .isEqualTo("for-test-purpose");
                 assertThat(((AzureProperties) context.getBean(AZURE_PROPERTY_BEAN_NAME)).getEnvironment().getAuthorityHost())
                     .isEqualTo("for-test-purpose");
@@ -134,7 +134,7 @@ public class NonPremiumServiceBusJMSAutoConfigurationTest {
                 "spring.jms.servicebus.pricing-tier=basic",
                 "spring.jms.servicebus.credential.client-secret=for-test-purpose",
                 "spring.jms.servicebus.environment.cloud=AzureGermany",
-                "spring.cloud.azure.credential.certificate-password=for-test-purpose",
+                "spring.cloud.azure.credential.client-certificate-password=for-test-purpose",
                 "spring.cloud.azure.environment.authority-host=for-test-purpose"
             );
     }
