a1657c1d73170a55683eff2f514890ac3c3800c0
Larry Osterman <LarryOsterman@users.noreply.github.com>
2021-08-10 10:28:21 -0700

    Fixed samples; hooked samples into build

--- sdk/attestation/azure-security-attestation/src/samples/java/com/azure/security/attestation/ReadMeSamples.java
+++ sdk/attestation/azure-security-attestation/src/samples/java/com/azure/security/attestation/ReadMeSamples.java
@@ -1,26 +1,26 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 package com.azure.security.attestation;
 
 import com.azure.core.http.HttpClient;
 import com.azure.core.test.TestMode;
-import com.azure.security.attestation.models.AttestSgxEnclaveRequest;
-import com.azure.security.attestation.models.AttestationResponse;
+import com.azure.security.attestation.models.AttestationOptions;
+import com.azure.security.attestation.models.AttestationResult;
+import com.azure.security.attestation.models.AttestationSigner;
 import com.azure.security.attestation.models.AttestationType;
-import com.azure.security.attestation.models.JsonWebKeySet;
 import com.azure.security.attestation.models.PolicyResponse;
 import com.nimbusds.jose.JOSEObject;
-import com.nimbusds.jwt.JWTClaimsSet;
-import org.junit.jupiter.api.Assertions;
 
 import java.nio.charset.StandardCharsets;
 import java.text.ParseException;
 import java.util.Base64;
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ReadmeSamples extends AttestationClientTestBase {
-
 
     void testAttestSgxEnclave(HttpClient httpClient, String clientUri) {
 
@@ -31,25 +31,18 @@ class ReadmeSamples extends AttestationClientTestBase {
         byte[] decodedRuntimeData = new byte[1];
         byte[] decodedSgxQuote = new byte[1];
 
-        AttestSgxEnclaveRequest request = new AttestSgxEnclaveRequest();
-        request.setQuote(decodedSgxQuote);
-        RuntimeData runtimeData = new RuntimeData();
-        runtimeData.setDataType(DataType.BINARY);
-        runtimeData.setData(decodedRuntimeData);
-        request.setRuntimeData(runtimeData);
-        AttestationResponse response = client.attestSgxEnclave(request);
+        AttestationOptions options = AttestationOptions
+            .fromEvidence(decodedSgxQuote)
+                .setRunTimeData(decodedRuntimeData)
+                    .interpretRunTimeDataAsBinary();
+        AttestationResult result = client.attestSgxEnclave(options);
 
-        JWTClaimsSet claims = null;
-        claims = verifyAttestationToken(httpClient, clientUri, response.getToken()).block();
-
-        assertNotNull(claims);
-        assertTrue(claims.getClaims().containsKey("iss"));
+        assertNotNull(result.getIssuer());
 
         // In playback mode, the client URI is bogus and thus cannot be relied on for test purposes.
         if (testContextManager.getTestMode() != TestMode.PLAYBACK) {
-            Assertions.assertEquals(clientUri, claims.getClaims().get("iss"));
+            assertEquals(clientUri, result.getIssuer());
         }
-
     }
 
     void attestationPolicyGet(HttpClient httpClient, String clientUri) {
@@ -87,7 +80,7 @@ class ReadmeSamples extends AttestationClientTestBase {
 
         AttestationClientBuilder attestationBuilder = getBuilder(httpClient, clientUri);
 
-        JsonWebKeySet certs = attestationBuilder.buildSigningCertificatesClient().get();
+        AttestationSigner[] certs = attestationBuilder.buildAttestationClient().getAttestationSigners();
     }
 
 }
