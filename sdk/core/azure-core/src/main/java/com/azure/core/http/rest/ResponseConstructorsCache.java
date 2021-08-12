fdf2db0fe21b1dbd65309519af85853717b6ebb2
Alan Zimmer <48699787+alzimmermsft@users.noreply.github.com>
2021-08-12 12:44:05 -0700

    Replace Reflection Calls with MethodHandles (#19093)
    Update JacksonAdapter and ResponseConstructorCache to use MethodHandles

--- sdk/core/azure-core/src/main/java/com/azure/core/http/rest/ResponseConstructorsCache.java
+++ sdk/core/azure-core/src/main/java/com/azure/core/http/rest/ResponseConstructorsCache.java
@@ -6,10 +6,13 @@ package com.azure.core.http.rest;
 import com.azure.core.http.HttpHeaders;
 import com.azure.core.http.HttpRequest;
 import com.azure.core.http.HttpResponse;
+import com.azure.core.implementation.ReflectionUtils;
 import com.azure.core.implementation.serializer.HttpResponseDecoder;
 import com.azure.core.util.logging.ClientLogger;
 import reactor.core.publisher.Mono;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Constructor;
 import java.util.Arrays;
 import java.util.Comparator;
@@ -19,7 +22,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import static com.azure.core.util.FluxUtil.monoError;
 
 /**
- * A concurrent cache of {@link Response} constructors.
+ * A concurrent cache of {@link Response} {@link MethodHandle} constructors.
  */
 final class ResponseConstructorsCache {
     private static final String THREE_PARAM_ERROR = "Failed to deserialize 3-parameter response.";
@@ -27,34 +30,52 @@ final class ResponseConstructorsCache {
     private static final String FIVE_PARAM_ERROR = "Failed to deserialize 5-parameter response.";
     private static final String INVALID_PARAM_COUNT = "Response constructor with expected parameters not found.";
 
+    private static final Map<Class<?>, MethodHandle> CACHE = new ConcurrentHashMap<>();
+
     private final ClientLogger logger = new ClientLogger(ResponseConstructorsCache.class);
-    private final Map<Class<?>, Constructor<? extends Response<?>>> cache = new ConcurrentHashMap<>();
 
     /**
-     * Identify the suitable constructor for the given response class.
+     * Identify the suitable {@link MethodHandle} to construct the given response class.
      *
-     * @param responseClass the response class
-     * @return identified constructor, null if there is no match
+     * @param responseClass The response class.
+     * @return The {@link MethodHandle} that is capable of constructing an instance of the class or null if no handle is
+     * found.
      */
-    Constructor<? extends Response<?>> get(Class<? extends Response<?>> responseClass) {
-        return this.cache.computeIfAbsent(responseClass, this::locateResponseConstructor);
+    MethodHandle get(Class<? extends Response<?>> responseClass) {
+        return CACHE.computeIfAbsent(responseClass, this::locateResponseConstructor);
     }
 
     /**
-     * Identify the most specific constructor for the given response class.
-     *
-     * The most specific constructor is looked up following order: 1. (httpRequest, statusCode, headers, body,
-     * decodedHeaders) 2. (httpRequest, statusCode, headers, body) 3. (httpRequest, statusCode, headers)
+     * Identify the most specific {@link MethodHandle} to construct the given response class.
+     * <p>
+     * Lookup is the following order:
+     * <ol>
+     * <li>(httpRequest, statusCode, headers, body, decodedHeaders)</li>
+     * <li>(httpRequest, statusCode, headers, body)</li>
+     * <li>(httpRequest, statusCode, headers)</li>
+     * </ol>
      *
      * Developer Note: This method logic can be easily replaced with Java.Stream and associated operators but we're
      * using basic sort and loop constructs here as this method is in hot path and Stream route is consuming a fair
      * amount of resources.
      *
-     * @param responseClass the response class
-     * @return identified constructor, null if there is no match
+     * @param responseClass The response class.
+     * @return The {@link MethodHandle} that is capable of constructing an instance of the class or null if no handle is
+     * found.
      */
-    @SuppressWarnings("unchecked")
-    private Constructor<? extends Response<?>> locateResponseConstructor(Class<?> responseClass) {
+    private MethodHandle locateResponseConstructor(Class<?> responseClass) {
+        MethodHandles.Lookup lookupToUse;
+        try {
+            lookupToUse = ReflectionUtils.getLookupToUse(responseClass);
+        } catch (Throwable t) {
+            throw logger.logExceptionAsError(new RuntimeException(t));
+        }
+
+        /*
+         * Now that the MethodHandles.Lookup has been found to create the MethodHandle instance begin searching for
+         * the most specific MethodHandle that can be used to create the response class (as mentioned in the method
+         * Javadocs).
+         */
         Constructor<?>[] constructors = responseClass.getDeclaredConstructors();
         // Sort constructors in the "descending order" of parameter count.
         Arrays.sort(constructors, Comparator.comparing(Constructor::getParameterCount, (a, b) -> b - a));
@@ -62,7 +83,16 @@ final class ResponseConstructorsCache {
             final int paramCount = constructor.getParameterCount();
             if (paramCount >= 3 && paramCount <= 5) {
                 try {
-                    return (Constructor<? extends Response<?>>) constructor;
+                    /*
+                     * From here we have three, possibly more options, to resolve this.
+                     *
+                     * 1) setAccessible to true in the response class (requires doPrivilege).
+                     * 2) Use Java 9+ Module class to add reads in com.azure.core and the SDK library exports to
+                     * com.azure.core for implementation.
+                     * 3) SDK libraries create an accessible MethodHandles.Lookup which com.azure.core can use to spoof
+                     * as the SDK library.
+                     */
+                    return lookupToUse.unreflectConstructor(constructor);
                 } catch (Throwable t) {
                     throw logger.logExceptionAsError(new RuntimeException(t));
                 }
@@ -72,39 +102,44 @@ final class ResponseConstructorsCache {
     }
 
     /**
-     * Invoke the constructor this type represents.
+     * Invoke the {@link MethodHandle} to construct and instance of the response class.
      *
-     * @param constructor the constructor type
-     * @param decodedResponse the decoded http response
-     * @param bodyAsObject the http response content
-     * @return an instance of a {@link Response} implementation
+     * @param handle The {@link MethodHandle} capable of constructing an instance of the response class.
+     * @param decodedResponse The decoded HTTP response.
+     * @param bodyAsObject The HTTP response body.
+     * @return An instance of the {@link Response} implementation.
      */
-    Mono<Response<?>> invoke(final Constructor<? extends Response<?>> constructor,
+    Mono<Response<?>> invoke(final MethodHandle handle,
         final HttpResponseDecoder.HttpDecodedResponse decodedResponse, final Object bodyAsObject) {
         final HttpResponse httpResponse = decodedResponse.getSourceResponse();
         final HttpRequest httpRequest = httpResponse.getRequest();
         final int responseStatusCode = httpResponse.getStatusCode();
         final HttpHeaders responseHeaders = httpResponse.getHeaders();
 
-        final int paramCount = constructor.getParameterCount();
+        final int paramCount = handle.type().parameterCount();
         switch (paramCount) {
             case 3:
-                return constructResponse(constructor, THREE_PARAM_ERROR, logger, httpRequest, responseStatusCode,
+                return constructResponse(handle, THREE_PARAM_ERROR, logger, httpRequest, responseStatusCode,
                     responseHeaders);
             case 4:
-                return constructResponse(constructor, FOUR_PARAM_ERROR, logger, httpRequest, responseStatusCode,
+                return constructResponse(handle, FOUR_PARAM_ERROR, logger, httpRequest, responseStatusCode,
                     responseHeaders, bodyAsObject);
             case 5:
-                return constructResponse(constructor, FIVE_PARAM_ERROR, logger, httpRequest, responseStatusCode,
+                return constructResponse(handle, FIVE_PARAM_ERROR, logger, httpRequest, responseStatusCode,
                     responseHeaders, bodyAsObject, decodedResponse.getDecodedHeaders());
             default:
                 return monoError(logger, new IllegalStateException(INVALID_PARAM_COUNT));
         }
     }
 
-    private static Mono<Response<?>> constructResponse(Constructor<? extends Response<?>> constructor,
-        String exceptionMessage, ClientLogger logger, Object... params) {
-        return Mono.<Response<?>>fromCallable(() -> constructor.newInstance(params))
-            .onErrorMap(ex -> logger.logExceptionAsError(new RuntimeException(exceptionMessage, ex)));
+    private static Mono<Response<?>> constructResponse(MethodHandle handle, String exceptionMessage,
+        ClientLogger logger, Object... params) {
+        return Mono.defer(() -> {
+            try {
+                return Mono.just((Response<?>) handle.invokeWithArguments(params));
+            } catch (Throwable throwable) {
+                return monoError(logger, new RuntimeException(exceptionMessage, throwable));
+            }
+        });
     }
 }
