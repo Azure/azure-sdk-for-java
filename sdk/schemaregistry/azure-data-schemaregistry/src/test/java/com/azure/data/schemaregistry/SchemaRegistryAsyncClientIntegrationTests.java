87cee52c57c09f361794aa17df55270c6d082e9a
Connie <conniey@microsoft.com>
2021-08-13 14:12:12 -0700

    Adding integration tests.

---
+++ sdk/schemaregistry/azure-data-schemaregistry/src/test/java/com/azure/data/schemaregistry/SchemaRegistryAsyncClientIntegrationTests.java
@@ -0,0 +1,156 @@
+package com.azure.data.schemaregistry;
+
+import com.azure.core.credential.AccessToken;
+import com.azure.core.credential.TokenCredential;
+import com.azure.core.credential.TokenRequestContext;
+import com.azure.core.http.policy.HttpLogDetailLevel;
+import com.azure.core.http.policy.HttpLogOptions;
+import com.azure.core.http.policy.RetryPolicy;
+import com.azure.core.test.TestBase;
+import com.azure.data.schemaregistry.models.SerializationType;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.nio.charset.StandardCharsets;
+import java.time.OffsetDateTime;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class SchemaRegistryAsyncClientIntegrationTests extends TestBase {
+    private static final int RESOURCE_LENGTH = 16;
+    private static final String AZURE_EVENTHUBS_FULLY_QUALIFIED_DOMAIN_NAME = "AZURE_EVENTHUBS_FULLY_QUALIFIED_DOMAIN_NAME";
+    private static final String SCHEMA_CONTENT = "{\"type\" : \"record\",\"namespace\" : \"TestSchema\",\"name\" : \"Employee\",\"fields\" : [{ \"name\" : \"Name\" , \"type\" : \"string\" },{ \"name\" : \"Age\", \"type\" : \"int\" }]}";
+    private static final String SCHEMA_GROUP = "at";
+
+    SchemaRegistryClientBuilder builder;
+
+    @Override
+    protected void afterTest() {
+        Mockito.framework().clearInlineMocks();
+        super.afterTest();
+    }
+
+    /**
+     * Verifies that we can register a schema and then get it by its schemaId.
+     */
+    @Test
+    public void registerAndGetSchema() {
+        // Arrange
+        initializeBuilder();
+
+        final String schemaName = testResourceNamer.randomName("sch", RESOURCE_LENGTH);
+        final SchemaRegistryAsyncClient client1 = builder.buildAsyncClient();
+        final SchemaRegistryAsyncClient client2 = builder.buildAsyncClient();
+
+        final AtomicReference<String> schemaId = new AtomicReference<>();
+
+        // Act & Assert
+        StepVerifier.create(client1.registerSchema(SCHEMA_GROUP, schemaName, SCHEMA_CONTENT, SerializationType.AVRO))
+            .assertNext(response -> {
+                assertEquals(schemaName, response.getSchemaName());
+                assertNotNull(response.getSchemaId());
+                schemaId.set(response.getSchemaId());
+
+                final String contents = new String(response.getSchema(), StandardCharsets.UTF_8);
+                assertEquals(SCHEMA_CONTENT, contents);
+            }).verifyComplete();
+
+
+        // Assert that we can get a schema based on its id. We registered a schema with client1 and its response is
+        // cached, so it won't make a network call when getting the schema. client2 will not have this information.
+        final String schemaIdToGet = schemaId.get();
+        assertNotNull(schemaIdToGet);
+
+        // Act & Assert
+        StepVerifier.create(client2.getSchema(schemaIdToGet))
+            .assertNext(schema -> {
+                assertEquals(schemaIdToGet, schema.getSchemaId());
+                assertEquals(SerializationType.AVRO, schema.getSerializationType());
+
+                final String contents = new String(schema.getSchema(), StandardCharsets.UTF_8);
+                assertEquals(SCHEMA_CONTENT, contents);
+            })
+            .verifyComplete();
+    }
+
+    /**
+     * Verifies that we can register a schema and then get it by its schemaId.
+     */
+    @Test
+    public void registerAndGetCachedSchema() {
+        // Arrange
+        initializeBuilder();
+
+        final String schemaName = testResourceNamer.randomName("sch", RESOURCE_LENGTH);
+        final SchemaRegistryAsyncClient client1 = builder.buildAsyncClient();
+
+        final AtomicReference<String> schemaId = new AtomicReference<>();
+
+        // Act & Assert
+        StepVerifier.create(client1.registerSchema(SCHEMA_GROUP, schemaName, SCHEMA_CONTENT, SerializationType.AVRO))
+            .assertNext(response -> {
+                assertEquals(schemaName, response.getSchemaName());
+                assertNotNull(response.getSchemaId());
+                schemaId.set(response.getSchemaId());
+
+                final String contents = new String(response.getSchema(), StandardCharsets.UTF_8);
+                assertEquals(SCHEMA_CONTENT, contents);
+            }).verifyComplete();
+
+        // Assert that we can get a schema based on its id. We registered a schema with client1 and its response is
+        // cached, so it won't make a network call when getting the schema. client2 will not have this information.
+        final String schemaIdToGet = schemaId.get();
+        assertNotNull(schemaIdToGet);
+
+        // Act & Assert
+        StepVerifier.create(client1.getSchema(schemaIdToGet))
+            .assertNext(schema -> {
+                assertEquals(schemaIdToGet, schema.getSchemaId());
+                assertEquals(SerializationType.AVRO, schema.getSerializationType());
+
+                final String contents = new String(schema.getSchema(), StandardCharsets.UTF_8);
+                assertEquals(SCHEMA_CONTENT, contents);
+            })
+            .verifyComplete();
+    }
+
+    void initializeBuilder() {
+        final String endpoint;
+        TokenCredential tokenCredential;
+        if (interceptorManager.isPlaybackMode()) {
+            tokenCredential = mock(TokenCredential.class);
+            when(tokenCredential.getToken(any(TokenRequestContext.class)))
+                .thenReturn(Mono.fromCallable(() -> {
+                    return new AccessToken("foo", OffsetDateTime.now().plusMinutes(20));
+                }));
+            endpoint = "https://foo.servicebus.windows.net";
+        } else {
+            tokenCredential = new DefaultAzureCredentialBuilder().build();
+            endpoint = System.getenv(AZURE_EVENTHUBS_FULLY_QUALIFIED_DOMAIN_NAME);
+
+            assertNotNull(endpoint, "'endpoint' cannot be null in LIVE/RECORD mode.");
+        }
+
+        builder = new SchemaRegistryClientBuilder()
+            .credential(tokenCredential)
+            .httpLogOptions(new HttpLogOptions()
+                .setLogLevel(HttpLogDetailLevel.BODY_AND_HEADERS)
+                .setPrettyPrintBody(true))
+            .endpoint(endpoint);
+
+        if (interceptorManager.isPlaybackMode()) {
+            builder.httpClient(interceptorManager.getPlaybackClient());
+        } else {
+            builder.addPolicy(new RetryPolicy())
+                .addPolicy(interceptorManager.getRecordPolicy());
+        }
+    }
+}
