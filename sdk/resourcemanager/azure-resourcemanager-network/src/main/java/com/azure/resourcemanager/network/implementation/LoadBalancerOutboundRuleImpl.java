f6659455d8fcadc3398049a8415d1c10258217f7
Haoling Dong <haolingdong@microsoft.com>
2021-08-16 13:48:04 +0800

    add copyright

--- sdk/resourcemanager/azure-resourcemanager-network/src/main/java/com/azure/resourcemanager/network/implementation/LoadBalancerOutboundRuleImpl.java
+++ sdk/resourcemanager/azure-resourcemanager-network/src/main/java/com/azure/resourcemanager/network/implementation/LoadBalancerOutboundRuleImpl.java
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 package com.azure.resourcemanager.network.implementation;
 
 import com.azure.core.management.SubResource;
