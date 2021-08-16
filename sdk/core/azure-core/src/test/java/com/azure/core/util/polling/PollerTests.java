e9e45e07acb2ce4a5a870d9c4543b6be726f839e
Jianghao Lu <jianghaolu@users.noreply.github.com>
2021-08-16 12:31:52 -0700

    Unused imports

--- sdk/core/azure-core/src/test/java/com/azure/core/util/polling/PollerTests.java
+++ sdk/core/azure-core/src/test/java/com/azure/core/util/polling/PollerTests.java
@@ -21,7 +21,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BiFunction;
 import java.util.function.Function;
