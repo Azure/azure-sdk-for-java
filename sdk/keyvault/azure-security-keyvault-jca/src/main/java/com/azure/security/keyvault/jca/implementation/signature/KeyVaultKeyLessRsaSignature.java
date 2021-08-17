f77a9763e000f15242234c4bd918e48c0b71e25d
lzc-1997-abel <70368631+lzc-1997-abel@users.noreply.github.com>
2021-08-17 11:02:52 +0800

    fix error import

--- sdk/keyvault/azure-security-keyvault-jca/src/main/java/com/azure/security/keyvault/jca/implementation/signature/KeyVaultKeyLessRsaSignature.java
+++ sdk/keyvault/azure-security-keyvault-jca/src/main/java/com/azure/security/keyvault/jca/implementation/signature/KeyVaultKeyLessRsaSignature.java
@@ -3,7 +3,6 @@
 
 package com.azure.security.keyvault.jca.implementation.signature;
 
-import java.security.AlgorithmParameters;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.InvalidAlgorithmParameterException;
