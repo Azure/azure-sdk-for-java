a7d53c5c7d1e5630f9e659abf992192abcbe6b55
Feiyue Yu <iamyfy@163.com>
2021-08-19 16:50:49 +0800

    comment

--- sdk/purview/azure-analytics-purview-account/src/test/java/com/azure/analytics/purview/account/PurviewAccountClientTestBase.java
+++ sdk/purview/azure-analytics-purview-account/src/test/java/com/azure/analytics/purview/account/PurviewAccountClientTestBase.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.azure.analytics.purview.account;
 
 import com.azure.core.credential.TokenCredential;
