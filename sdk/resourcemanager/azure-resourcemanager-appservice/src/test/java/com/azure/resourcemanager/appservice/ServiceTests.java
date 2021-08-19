12c2d3a32fb6568d900d7c2640045a0cc053d468
Weidong Xu <weidxu@microsoft.com>
2021-08-19 13:26:38 +0800

    copyright

--- sdk/resourcemanager/azure-resourcemanager-appservice/src/test/java/com/azure/resourcemanager/appservice/ServiceTests.java
+++ sdk/resourcemanager/azure-resourcemanager-appservice/src/test/java/com/azure/resourcemanager/appservice/ServiceTests.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.azure.resourcemanager.appservice;
 
 import com.azure.core.test.annotation.DoNotRecord;
