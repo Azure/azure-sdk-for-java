d4fccd1fe90c04ee56de5fe95fc021a964783d11
j82w <j82w@users.noreply.github.com>
2021-08-24 09:57:26 -0700

    Adding links to client initialization failed
    There has been ICM incidents where customers did not find the TSG page causing a CRI. Adding the link will help customers mitigate the issue by themselves in the future.

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/RxDocumentClientImpl.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/RxDocumentClientImpl.java
@@ -388,9 +388,9 @@ public class RxDocumentClientImpl implements AsyncDocumentClient, IAuthorization
         // hence asserting it
         if (databaseAccount == null) {
             logger.error("Client initialization failed."
-                + " Check if the endpoint is reachable and if your auth token is valid");
+                + " Check if the endpoint is reachable and if your auth token is valid. More info: https://aka.ms/cosmosdb-tsg-service-unavailable-java");
             throw new RuntimeException("Client initialization failed."
-                + " Check if the endpoint is reachable and if your auth token is valid");
+                + " Check if the endpoint is reachable and if your auth token is valid. More info: https://aka.ms/cosmosdb-tsg-service-unavailable-java");
         }
 
         this.useMultipleWriteLocations = this.connectionPolicy.isMultipleWriteRegionsEnabled() && BridgeInternal.isEnableMultipleWriteLocations(databaseAccount);
@@ -603,7 +603,11 @@ public class RxDocumentClientImpl implements AsyncDocumentClient, IAuthorization
         try {
 
             if (database == null) {
-                throw new IllegalArgumentException("Database");
+                
+                
+                
+                
+                IllegalArgumentException("Database");
             }
 
             logger.debug("Creating a Database. id: [{}]", database.getId());
