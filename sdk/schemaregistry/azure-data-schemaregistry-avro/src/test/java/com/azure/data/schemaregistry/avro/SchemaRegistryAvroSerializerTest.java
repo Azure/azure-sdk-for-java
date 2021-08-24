e64dffc319f6c3890661a2ab6457bcf0fbf86c52
Connie <conniey@microsoft.com>
2021-08-24 15:06:59 -0700

    Closing resources after usage.

--- sdk/schemaregistry/azure-data-schemaregistry-avro/src/test/java/com/azure/data/schemaregistry/avro/SchemaRegistryAvroSerializerTest.java
+++ sdk/schemaregistry/azure-data-schemaregistry-avro/src/test/java/com/azure/data/schemaregistry/avro/SchemaRegistryAvroSerializerTest.java
@@ -14,7 +14,9 @@ import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.io.BinaryEncoder;
 import org.apache.avro.io.DecoderFactory;
 import org.apache.avro.io.EncoderFactory;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -29,12 +31,14 @@ import java.io.IOException;
 import java.nio.BufferUnderflowException;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 
 import static com.azure.data.schemaregistry.avro.SchemaRegistryAvroSerializer.RECORD_FORMAT_INDICATOR_SIZE;
 import static com.azure.data.schemaregistry.avro.SchemaRegistryAvroSerializer.SCHEMA_ID_SIZE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -58,6 +62,16 @@ public class SchemaRegistryAvroSerializerTest {
     @Mock
     private SchemaRegistryAsyncClient client;
 
+    @BeforeAll
+    public static void beforeAll() {
+        StepVerifier.setDefaultTimeout(Duration.ofSeconds(30));
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        StepVerifier.resetDefaultTimeout();
+    }
+
     @BeforeEach
     public void beforeEach() {
         mocksCloseable = MockitoAnnotations.openMocks(this);
@@ -72,13 +86,13 @@ public class SchemaRegistryAvroSerializerTest {
     }
 
     @Test
-    void testRegistryGuidPrefixedToPayload() {
+    void testRegistryGuidPrefixedToPayload() throws IOException {
         // manually add SchemaRegistryObject into mock registry client cache
         AvroSchemaRegistryUtils encoder = new AvroSchemaRegistryUtils(false, parser, ENCODER_FACTORY,
             DECODER_FACTORY);
 
         SchemaProperties registered = new SchemaProperties(MOCK_GUID,
-            encoder.getSerializationType(),
+            SerializationType.AVRO,
             encoder.getSchemaName(null),
             encoder.getSchemaString(null).getBytes());
 
@@ -91,9 +105,11 @@ public class SchemaRegistryAvroSerializerTest {
         SchemaRegistryAvroSerializer serializer = new SchemaRegistryAvroSerializer(
             client, encoder, MOCK_SCHEMA_GROUP, false);
 
-        try {
-            ByteArrayOutputStream payload = new ByteArrayOutputStream();
-            serializer.serializeAsync(payload, 1).block();
+        try (ByteArrayOutputStream payload = new ByteArrayOutputStream()) {
+
+            StepVerifier.create(serializer.serializeAsync(payload, 1))
+                .verifyComplete();
+
             ByteBuffer buffer = ByteBuffer.wrap(payload.toByteArray());
             buffer.get(new byte[RECORD_FORMAT_INDICATOR_SIZE]);
             byte[] schemaGuidByteArray = new byte[SCHEMA_ID_SIZE];
@@ -110,39 +126,36 @@ public class SchemaRegistryAvroSerializerTest {
 
     @Test
     void testNullPayloadThrowsSerializationException() {
+        // Arrange
         AvroSchemaRegistryUtils encoder = new AvroSchemaRegistryUtils(false, parser, ENCODER_FACTORY,
             DECODER_FACTORY);
         SchemaRegistryAvroSerializer serializer = new SchemaRegistryAvroSerializer(
             client, encoder, MOCK_SCHEMA_GROUP, false);
 
+        // Act & Assert
         StepVerifier.create(serializer.serializeAsync(new ByteArrayOutputStream(), null))
             .verifyError(NullPointerException.class);
     }
 
     @Test
     void testIfRegistryNullThenThrow() {
-        try {
-            AvroSchemaRegistryUtils encoder = new AvroSchemaRegistryUtils(false, parser, ENCODER_FACTORY,
-                DECODER_FACTORY);
-            SchemaRegistryAvroSerializer serializer = new SchemaRegistryAvroSerializer(
-                null, encoder, MOCK_SCHEMA_GROUP, false);
-            fail("Building serializer instance with null registry client failed to throw");
-        } catch (NullPointerException e) {
-            assertTrue(true);
-        } catch (Exception e) {
-            fail("Building serializer instance with null registry client should throw null pointer exception");
-        }
+        // Arrange
+        AvroSchemaRegistryUtils encoder = new AvroSchemaRegistryUtils(false, parser, ENCODER_FACTORY,
+            DECODER_FACTORY);
+
+        // Act & Assert
+        assertThrows(NullPointerException.class,
+            () -> new SchemaRegistryAvroSerializer(null, encoder, MOCK_SCHEMA_GROUP, false));
     }
 
     @Test
     void testAddUtils() throws IOException {
-
         // manually add SchemaRegistryObject to cache
         AvroSchemaRegistryUtils decoder = new AvroSchemaRegistryUtils(false, parser, ENCODER_FACTORY,
             DECODER_FACTORY);
 
         SchemaProperties registered = new SchemaProperties(MOCK_GUID,
-            decoder.getSerializationType(),
+            SerializationType.AVRO,
             decoder.getSchemaName(null),
             MOCK_AVRO_SCHEMA_STRING.getBytes());
 
@@ -170,46 +183,62 @@ public class SchemaRegistryAvroSerializerTest {
             client, new AvroSchemaRegistryUtils(false, parser, ENCODER_FACTORY, DECODER_FACTORY),
             MOCK_SCHEMA_GROUP, true);
 
+        // Null payload should just complete the mono.
         StepVerifier.create(deserializer.deserializeAsync(null, null))
             .verifyComplete();
     }
 
     @Test
-    void testIfTooShortPayloadThrow() {
+    void testNullPayloadSync() {
         SchemaRegistryAvroSerializer deserializer = new SchemaRegistryAvroSerializer(
             client, new AvroSchemaRegistryUtils(false, parser, ENCODER_FACTORY, DECODER_FACTORY),
             MOCK_SCHEMA_GROUP, true);
 
-        StepVerifier.create(deserializer.deserializeAsync(new ByteArrayInputStream("aaa".getBytes()),
-            TypeReference.createInstance(String.class)))
-            .verifyError(BufferUnderflowException.class);
+        // Null payload should return null.
+        final Object actual = deserializer.deserialize(null, null);
 
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        try {
-            out.write(new byte[]{0x00, 0x00, 0x00, 0x00});
-            out.write("aa".getBytes());
-        } catch (IOException e) {
-            fail();
+        // Assert
+        assertNull(actual);
+    }
+
+    @Test
+    void testIfTooShortPayloadThrow() throws IOException {
+        SchemaRegistryAvroSerializer deserializer = new SchemaRegistryAvroSerializer(
+            client, new AvroSchemaRegistryUtils(false, parser, ENCODER_FACTORY, DECODER_FACTORY),
+            MOCK_SCHEMA_GROUP, true);
+
+        try (ByteArrayInputStream inputStream = new ByteArrayInputStream("aaa".getBytes())) {
+            StepVerifier.create(deserializer.deserializeAsync(inputStream, TypeReference.createInstance(String.class)))
+                .verifyError(BufferUnderflowException.class);
         }
 
-        ByteArrayInputStream stream = new ByteArrayInputStream(out.toByteArray());
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+            outputStream.write(new byte[]{0x00, 0x00, 0x00, 0x00});
+            outputStream.write("aa".getBytes());
 
-        StepVerifier.create(deserializer.deserializeAsync(stream, TypeReference.createInstance(String.class)))
-            .verifyError(BufferUnderflowException.class);
+            try (ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray())){
+                StepVerifier.create(deserializer.deserializeAsync(inputStream,
+                    TypeReference.createInstance(String.class)))
+                    .verifyError(BufferUnderflowException.class);
+            }
+        }
     }
 
     private static byte[] getPayload() throws IOException {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        out.write(new byte[]{0x00, 0x00, 0x00, 0x00});
-        out.write(ByteBuffer.allocate(SCHEMA_ID_SIZE)
-            .put(MOCK_GUID.getBytes(StandardCharsets.UTF_8))
-            .array());
-        GenericRecord record = getAvroRecord();
-        BinaryEncoder encoder = ENCODER_FACTORY.directBinaryEncoder(out, null);
-        GenericDatumWriter<GenericRecord> writer = new GenericDatumWriter<>(MOCK_AVRO_SCHEMA);
-        writer.write(record, encoder);
-        encoder.flush();
-        return out.toByteArray();
+        try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+            out.write(new byte[]{0x00, 0x00, 0x00, 0x00});
+            out.write(ByteBuffer.allocate(SCHEMA_ID_SIZE)
+                .put(MOCK_GUID.getBytes(StandardCharsets.UTF_8))
+                .array());
+
+            GenericRecord record = getAvroRecord();
+            BinaryEncoder encoder = ENCODER_FACTORY.directBinaryEncoder(out, null);
+            GenericDatumWriter<GenericRecord> writer = new GenericDatumWriter<>(MOCK_AVRO_SCHEMA);
+            writer.write(record, encoder);
+            encoder.flush();
+
+            return out.toByteArray();
+        }
     }
 
     private static GenericRecord getAvroRecord() {
