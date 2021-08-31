66098105cbb8db55617104b01331844f87456771
Connie Yau <conniey@microsoft.com>
2021-08-31 14:01:39 -0600

    Adding tests for primitive types.

--- sdk/schemaregistry/azure-data-schemaregistry-avro/src/test/java/com/azure/data/schemaregistry/avro/AvroSchemaUtilsTest.java
+++ sdk/schemaregistry/azure-data-schemaregistry-avro/src/test/java/com/azure/data/schemaregistry/avro/AvroSchemaUtilsTest.java
@@ -5,6 +5,7 @@ package com.azure.data.schemaregistry.avro;
 
 import org.apache.avro.AvroRuntimeException;
 import org.apache.avro.Schema;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -15,6 +16,7 @@ import java.util.Objects;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
@@ -48,5 +50,21 @@ public class AvroSchemaUtilsTest {
 
         final Schema actual = primitiveTypes.get(type);
         assertEquals(expectedSchema, actual);
+    }
+
+    /**
+     * Asserts that we can't modify the primitive types.
+     */
+    @Test
+    public void cannotModifyPrimitives() {
+        // Arrange
+        final Schema testTypesSchema = Schema.createEnum("TestType", "Different test types",
+            "org.example", Arrays.asList("UNIT", "INTEGRATION", "REGRESSION", "PERFORMANCE"));
+
+        final Map<Schema.Type, Schema> primitives = AvroSchemaUtils.getPrimitiveSchemas();
+
+        // Act & Assert
+        assertThrows(UnsupportedOperationException.class, () -> primitives.put(Schema.Type.RECORD, testTypesSchema));
+        assertThrows(UnsupportedOperationException.class, () -> primitives.remove(Schema.Type.BOOLEAN));
     }
 }
