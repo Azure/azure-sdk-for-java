74ce74267bc128dfa9a80c7f76865365bc2ffe9d
vcolin7 <vicolina@microsoft.com>
2021-08-31 12:06:33 -0700

    Update sdk/keyvault/azure-security-keyvault-jca/src/main/java/module-info.java
    Co-authored-by: Alan Zimmer <48699787+alzimmermsft@users.noreply.github.com>

--- sdk/keyvault/azure-security-keyvault-jca/src/main/java/module-info.java
+++ sdk/keyvault/azure-security-keyvault-jca/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-module azure.security.keyvault.jca {
+module com.azure.security.keyvault.jca {
     exports com.azure.security.keyvault.jca;
     exports com.azure.security.keyvault.jca.implementation to com.azure.security.keyvault.jca.test;
 }
