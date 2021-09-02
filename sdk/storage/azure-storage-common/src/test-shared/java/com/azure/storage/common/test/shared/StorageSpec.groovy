bda53cb3ccf987c093a01987901d99bb4b0c3269
Kamil Sobol <kasobol@microsoft.com>
2021-09-02 10:20:30 -0700

    undo that.

--- sdk/storage/azure-storage-common/src/test-shared/java/com/azure/storage/common/test/shared/StorageSpec.groovy
+++ sdk/storage/azure-storage-common/src/test-shared/java/com/azure/storage/common/test/shared/StorageSpec.groovy
@@ -16,17 +16,9 @@ import com.azure.core.util.ServiceVersion
 import com.azure.core.util.logging.ClientLogger
 import com.azure.identity.EnvironmentCredentialBuilder
 import okhttp3.ConnectionPool
-import org.jetbrains.annotations.NotNull
 import spock.lang.Specification
 
-import java.lang.management.ManagementFactory
-import java.lang.management.ThreadInfo
-import java.lang.management.ThreadMXBean
 import java.time.Duration
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
-import java.util.concurrent.ScheduledExecutorService
-import java.util.concurrent.ThreadFactory
 import java.util.concurrent.TimeUnit
 import java.util.function.Predicate
 import java.util.function.Supplier
@@ -44,47 +36,6 @@ class StorageSpec extends Specification {
 
     private InterceptorManager interceptorManager
     private StorageResourceNamer namer
-
-    private static final ScheduledExecutorService EXECUTOR = Executors.newScheduledThreadPool(1, new ThreadFactory() {
-        @Override
-        Thread newThread(@NotNull Runnable r) {
-            Thread t = new Thread(r);
-            t.setDaemon(true);
-            return t;
-        }
-    })
-
-    static {
-        EXECUTOR.scheduleAtFixedRate(new Runnable() {
-            @Override
-            void run() {
-                printThreadStacks()
-            }
-        }, 0, 1, TimeUnit.MINUTES)
-    }
-
-
-    static void printThreadStacks() {
-        final StringBuilder dump = new StringBuilder("============= THREAD DUMP START =========");
-        final ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
-        final ThreadInfo[] threadInfos = threadMXBean.getThreadInfo(threadMXBean.getAllThreadIds(), 100);
-        for (ThreadInfo threadInfo : threadInfos) {
-            dump.append('"');
-            dump.append(threadInfo.getThreadName());
-            dump.append("\" ");
-            final Thread.State state = threadInfo.getThreadState();
-            dump.append("\n   java.lang.Thread.State: ");
-            dump.append(state);
-            final StackTraceElement[] stackTraceElements = threadInfo.getStackTrace();
-            for (final StackTraceElement stackTraceElement : stackTraceElements) {
-                dump.append("\n        at ");
-                dump.append(stackTraceElement);
-            }
-            dump.append("\n\n");
-        }
-        dump.append("============= THREAD DUMP START =========");
-        println dump.toString();
-    }
 
     def setup() {
         def testName = TestNameProvider.getTestName(specificationContext.getCurrentIteration());
