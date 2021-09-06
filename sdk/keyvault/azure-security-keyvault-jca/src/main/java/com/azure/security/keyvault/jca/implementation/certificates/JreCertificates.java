acafd6b28199392856983b4150106a7f571d9a07
michaelqi793 <archer@Archers-MacBook-Pro.local>
2021-09-06 14:25:07 +0800

    Build the SSL context with jre key store

--- sdk/keyvault/azure-security-keyvault-jca/src/main/java/com/azure/security/keyvault/jca/implementation/certificates/JreCertificates.java
+++ sdk/keyvault/azure-security-keyvault-jca/src/main/java/com/azure/security/keyvault/jca/implementation/certificates/JreCertificates.java
@@ -3,6 +3,8 @@
 
 package com.azure.security.keyvault.jca.implementation.certificates;
 
+import com.azure.security.keyvault.jca.implementation.JREKeyStore;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -52,7 +54,7 @@ public final class JreCertificates implements AzureCertificates {
     private final  Map<String, Key> keys;
 
     /**
-     * Stores the singleton
+     * Stores the instance of JreCertificates.
      */
     private static final JreCertificates INSTANCE = new JreCertificates();
 
@@ -87,7 +89,7 @@ public final class JreCertificates implements AzureCertificates {
 
     /**
      *
-     * @return the singleton.
+     * @return the instance of JreCertificates.
      */
     public static JreCertificates getInstance() {
         return INSTANCE;
@@ -112,57 +114,5 @@ public final class JreCertificates implements AzureCertificates {
     @Override
     public void deleteEntry(String alias) {
 
-    }
-
-    private static class JREKeyStore {
-        private static final String JAVA_HOME =  privilegedGetProperty("java.home", "");
-        private static final Path STORE_PATH = Paths.get(JAVA_HOME).resolve("lib").resolve("security");
-        private static final Path DEFAULT_STORE = STORE_PATH.resolve("cacerts");
-        private static final Path JSSE_DEFAULT_STORE = STORE_PATH.resolve("jssecacerts");
-        private static final String KEY_STORE_PASSWORD = privilegedGetProperty("javax.net.ssl.keyStorePassword", "changeit");
-
-        private static KeyStore getDefault() {
-            KeyStore defaultKeyStore = null;
-            try {
-                defaultKeyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-                loadKeyStore(defaultKeyStore);
-            } catch (KeyStoreException e) {
-                LOGGER.log(WARNING, "Unable to get the jre key store.", e);
-            }
-            return defaultKeyStore;
-        }
-
-        private static void loadKeyStore(KeyStore ks) {
-            try (InputStream inputStream = Files.newInputStream(getKeyStoreFile())) {
-                ks.load(inputStream, KEY_STORE_PASSWORD.toCharArray());
-            } catch (IOException | NoSuchAlgorithmException | CertificateException e) {
-                LOGGER.log(WARNING, "unable to load the jre key store", e);
-            }
-        }
-
-        private static Path getKeyStoreFile() {
-            return Stream.of(getConfiguredKeyStorePath(), JSSE_DEFAULT_STORE, DEFAULT_STORE)
-                .filter(Objects::nonNull)
-                .filter(Files::exists)
-                .filter(Files::isReadable)
-                .findFirst()
-                .orElse(null);
-        }
-
-        private static Path getConfiguredKeyStorePath() {
-            String configuredKeyStorePath = privilegedGetProperty("javax.net.ssl.keyStore", "");
-            return Optional.of(configuredKeyStorePath)
-                .filter(path -> !path.isEmpty())
-                .map(Paths::get)
-                .orElse(null);
-        }
-
-        private static String  privilegedGetProperty(String theProp, String defaultVal) {
-            return AccessController.doPrivileged(
-                (PrivilegedAction<String>) () -> {
-                    String value = System.getProperty(theProp, "");
-                    return (value.isEmpty()) ? defaultVal : value;
-                });
-        }
     }
 }
