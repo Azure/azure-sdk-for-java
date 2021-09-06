713b4b0a0d9b4a31269fc60c36f1d7a5f76c7446
Haoling Dong <haolingdong@microsoft.com>
2021-09-06 12:01:42 +0800

    Add README to explain eventgrid example

--- sdk/eventgrid/azure-resourcemanager-eventgrid/src/main/samples/java/com/azure/resourcemanager/eventgrid/EventGridPublishAndConsumeExample.java
+++ sdk/eventgrid/azure-resourcemanager-eventgrid/src/main/samples/java/com/azure/resourcemanager/eventgrid/EventGridPublishAndConsumeExample.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.azure.resourcemanager.eventgrid;
 
 import com.azure.core.credential.AzureKeyCredential;
