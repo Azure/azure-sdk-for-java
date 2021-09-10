634d6bb9548e095fb9bdb6bb6a9aaa2cc4cfa4dd
michaelqi793 <archer@Archers-MacBook-Pro.local>
2021-09-10 15:24:08 +0800

    Added copy right

--- sdk/keyvault/azure-security-keyvault-jca/src/test/java/com/azure/security/keyvault/jca/implementation/JREKeyStoreFactoryTest.java
+++ sdk/keyvault/azure-security-keyvault-jca/src/test/java/com/azure/security/keyvault/jca/implementation/JREKeyStoreFactoryTest.java
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 package com.azure.security.keyvault.jca.implementation;
 
 import com.azure.security.keyvault.jca.KeyVaultKeyStore;
