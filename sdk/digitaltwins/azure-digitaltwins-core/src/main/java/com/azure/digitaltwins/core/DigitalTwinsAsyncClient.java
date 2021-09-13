3ee7c3799c174bbb3e0b47eb49128acddf9d0f7a
timtay-microsoft <timtay@microsoft.com>
2021-09-13 14:11:34 -0700

    Remove "en-us" from links in digital twins SDK (#24100)

--- sdk/digitaltwins/azure-digitaltwins-core/src/main/java/com/azure/digitaltwins/core/DigitalTwinsAsyncClient.java
+++ sdk/digitaltwins/azure-digitaltwins-core/src/main/java/com/azure/digitaltwins/core/DigitalTwinsAsyncClient.java
@@ -1274,7 +1274,7 @@ public final class DigitalTwinsAsyncClient {
      *
      * Note that there may be a delay between before changes in your instance are reflected in queries.
      * For more details on query limitations, see
-     * <a href="https://docs.microsoft.com/en-us/azure/digital-twins/how-to-query-graph#query-limitations">Query limitations</a>
+     * <a href="https://docs.microsoft.com/azure/digital-twins/how-to-query-graph#query-limitations">Query limitations</a>
      *
      * @param query The query string, in SQL-like syntax.
      * @param clazz The model class to deserialize each queried digital twin into. Since the queried twins may not all
@@ -1302,7 +1302,7 @@ public final class DigitalTwinsAsyncClient {
      *
      * Note that there may be a delay between before changes in your instance are reflected in queries.
      * For more details on query limitations, see
-     * <a href="https://docs.microsoft.com/en-us/azure/digital-twins/how-to-query-graph#query-limitations">Query limitations</a>
+     * <a href="https://docs.microsoft.com/azure/digital-twins/how-to-query-graph#query-limitations">Query limitations</a>
      *
      * @param query The query string, in SQL-like syntax.
      * @param clazz The model class to deserialize each queried digital twin into. Since the queried twins may not all
