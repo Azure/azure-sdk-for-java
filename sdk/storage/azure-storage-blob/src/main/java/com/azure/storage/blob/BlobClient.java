bcfc0794379871bcda3d5f4ab1fafb455fcb7918
Andriy Redko <andriy.redko@aiven.io>
2021-09-13 09:22:40 -0400

    Fixing BlobClient::uploadWithResponse method to follow the convention and return the response back

--- sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/BlobClient.java
+++ sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/BlobClient.java
@@ -243,12 +243,13 @@ public class BlobClient extends BlobClientBase {
      * @param requestConditions {@link BlobRequestConditions}
      * @param timeout An optional timeout value beyond which a {@link RuntimeException} will be raised.
      * @param context Additional context that is passed through the Http pipeline during the service call.
+     * @return Information about the uploaded block blob.
      */
     @ServiceMethod(returns = ReturnType.SINGLE)
-    public void uploadWithResponse(InputStream data, long length, ParallelTransferOptions parallelTransferOptions,
+    public Response<BlockBlobItem> uploadWithResponse(InputStream data, long length, ParallelTransferOptions parallelTransferOptions,
         BlobHttpHeaders headers, Map<String, String> metadata, AccessTier tier, BlobRequestConditions requestConditions,
         Duration timeout, Context context) {
-        this.uploadWithResponse(new BlobParallelUploadOptions(data, length)
+        return this.uploadWithResponse(new BlobParallelUploadOptions(data, length)
             .setParallelTransferOptions(parallelTransferOptions).setHeaders(headers).setMetadata(metadata).setTier(tier)
             .setRequestConditions(requestConditions), timeout, context);
     }
