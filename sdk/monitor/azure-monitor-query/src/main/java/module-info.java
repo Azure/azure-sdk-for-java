8a4a4505382aae0c0502ba68c7b0d4bd9d1a4b39
Srikanta <srnagar@microsoft.com>
2021-09-16 11:48:40 -0700

    revert experimental changes

--- sdk/monitor/azure-monitor-query/src/main/java/module-info.java
+++ sdk/monitor/azure-monitor-query/src/main/java/module-info.java
@@ -6,6 +6,7 @@
  */
 module com.azure.monitor.query {
     requires transitive com.azure.core;
+    requires transitive com.azure.core.experimental;
     exports com.azure.monitor.query;
     exports com.azure.monitor.query.models;
     opens com.azure.monitor.query.models to com.fasterxml.jackson.databind, com.azure.core;
