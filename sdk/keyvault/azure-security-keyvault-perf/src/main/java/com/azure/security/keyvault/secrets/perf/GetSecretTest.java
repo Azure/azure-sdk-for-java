99372978d710969cf4760fc2244a7b497ac28a00
Mike Harder <mharder@microsoft.com>
2021-09-20 17:16:19 -0700

    Rename static final per naming convention

--- sdk/keyvault/azure-security-keyvault-perf/src/main/java/com/azure/security/keyvault/secrets/perf/GetSecretTest.java
+++ sdk/keyvault/azure-security-keyvault-perf/src/main/java/com/azure/security/keyvault/secrets/perf/GetSecretTest.java
@@ -9,7 +9,7 @@ import reactor.core.publisher.Mono;
 import java.util.UUID;
 
 public class GetSecretTest extends SecretsTest<PerfStressOptions> {
-    private static final String secretName = "getSecretPerfTest-" + UUID.randomUUID();
+    private static final String SECRET_NAME = "getSecretPerfTest-" + UUID.randomUUID();
 
     public GetSecretTest(PerfStressOptions options) {
         super(options);
@@ -18,23 +18,23 @@ public class GetSecretTest extends SecretsTest<PerfStressOptions> {
     @Override
     public Mono<Void> globalSetupAsync() {
         return super.globalSetupAsync()
-            .then(secretAsyncClient.setSecret(secretName, secretName))
+            .then(secretAsyncClient.setSecret(SECRET_NAME, SECRET_NAME))
             .then();
     }
 
     @Override
     public Mono<Void> globalCleanupAsync() {
-        return deleteAndPurgeSecretsAsync(secretName)
+        return deleteAndPurgeSecretsAsync(SECRET_NAME)
             .then(super.globalCleanupAsync());
     }
 
     @Override
     public void run() {
-        secretClient.getSecret(secretName);
+        secretClient.getSecret(SECRET_NAME);
     }
 
     @Override
     public Mono<Void> runAsync() {
-        return secretAsyncClient.getSecret(secretName).then();
+        return secretAsyncClient.getSecret(SECRET_NAME).then();
     }
 }
