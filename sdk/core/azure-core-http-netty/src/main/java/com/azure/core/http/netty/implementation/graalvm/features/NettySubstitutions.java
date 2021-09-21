44f35d51a37a1a6341166ccdc6a9dc50ebb99ab4
Srikanta <srnagar@microsoft.com>
2021-09-21 13:13:40 -0700

    address comments

--- sdk/core/azure-core-http-netty/src/main/java/com/azure/core/http/netty/implementation/graalvm/features/NettySubstitutions.java
+++ sdk/core/azure-core-http-netty/src/main/java/com/azure/core/http/netty/implementation/graalvm/features/NettySubstitutions.java
@@ -1,6 +1,20 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
+/*
+ * Copyright (C) 2010 The Quarkus Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 
+/*
+ * Portions Copyright (c) Microsoft Corporation
+ */
 package com.azure.core.http.netty.implementation.graalvm.features;
 
 import com.oracle.svm.core.annotate.Alias;
@@ -22,8 +36,6 @@ import io.netty.handler.ssl.ClientAuth;
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextOption;
 import io.netty.handler.ssl.SslProvider;
-import io.netty.util.internal.logging.InternalLoggerFactory;
-import io.netty.util.internal.logging.JdkLoggerFactory;
 
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLEngine;
