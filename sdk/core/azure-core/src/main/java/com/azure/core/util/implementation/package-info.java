dd4c6787987acb5d51702c6c11e16d0264e7c566
Alan Zimmer <48699787+alzimmermsft@users.noreply.github.com>
2021-09-24 18:03:11 -0700

    Fix failures when running failsafe directly

---
+++ sdk/core/azure-core/src/main/java/com/azure/core/util/implementation/package-info.java
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+/**
+ * Package containing implementation-specific utility APIs that should not be used by end-users.
+ */
+package com.azure.core.util.implementation;
