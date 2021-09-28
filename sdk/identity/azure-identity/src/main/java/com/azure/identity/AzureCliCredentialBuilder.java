5d074a5f8fde5154c4c5de553a4ee6a76f8134c6
g2vinay <vinaygera@Vinays-MacBook-Pro.local>
2021-09-28 23:04:24 +0530

    update

--- sdk/identity/azure-identity/src/main/java/com/azure/identity/AzureCliCredentialBuilder.java
+++ sdk/identity/azure-identity/src/main/java/com/azure/identity/AzureCliCredentialBuilder.java
@@ -3,12 +3,28 @@
 
 package com.azure.identity;
 
+import com.azure.identity.implementation.util.ValidationUtil;
+
 /**
  * Fluent credential builder for instantiating a {@link AzureCliCredential}.
  *
  * @see AzureCliCredential
  */
 public class AzureCliCredentialBuilder extends CredentialBuilderBase<AzureCliCredentialBuilder> {
+    private String tenantId;
+
+    /**
+     * Sets the tenant ID of the application.
+     *
+     * @param tenantId the tenant ID of the application.
+     * @return An updated instance of this builder with the tenant id set as specified.
+     */
+    public AzureCliCredentialBuilder tenantId(String tenantId) {
+        ValidationUtil.validateTenantIdCharacterRange(getClass().getSimpleName(), tenantId);
+        this.tenantId = tenantId;
+        return this;
+    }
+
      /**
      * Creates a new {@link AzureCliCredential} with the current configurations.
      *
--- sdk/identity/azure-identity/src/main/java/com/azure/identity/AzurePowerShellCredentialBuilder.java
+++ sdk/identity/azure-identity/src/main/java/com/azure/identity/AzurePowerShellCredentialBuilder.java
@@ -3,12 +3,27 @@
 
 package com.azure.identity;
 
+import com.azure.identity.implementation.util.ValidationUtil;
+
 /**
  * Fluent credential builder for instantiating a {@link AzurePowerShellCredential}.
  *
  * @see AzurePowerShellCredential
  */
 public class AzurePowerShellCredentialBuilder extends CredentialBuilderBase<AzurePowerShellCredentialBuilder> {
+    private String tenantId;
+
+    /**
+     * Sets the tenant ID of the application.
+     *
+     * @param tenantId the tenant ID of the application.
+     * @return An updated instance of this builder with the tenant id set as specified.
+     */
+    public AzurePowerShellCredentialBuilder tenantId(String tenantId) {
+        ValidationUtil.validateTenantIdCharacterRange(getClass().getSimpleName(), tenantId);
+        this.tenantId = tenantId;
+        return this;
+    }
 
      /**
      * Creates a new {@link AzurePowerShellCredential} with the current configurations.
