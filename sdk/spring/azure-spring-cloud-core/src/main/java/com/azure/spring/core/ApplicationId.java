4afd30db4f9e52bd3b865ef130e0c76f4b082990
Moary Chen <v-moaryc@microsoft.com>
2021-09-28 15:14:46 +0800

    fix build error

--- sdk/spring/azure-spring-cloud-core/src/main/java/com/azure/spring/core/ApplicationId.java
+++ sdk/spring/azure-spring-cloud-core/src/main/java/com/azure/spring/core/ApplicationId.java
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.azure.spring.utils;
+package com.azure.spring.core;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
