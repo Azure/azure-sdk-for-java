9c0c3399aa0dbaf8471dd5a93c151ecb08b9c765
Feiyue Yu <iamyfy@163.com>
2021-09-29 11:35:00 +0800

    sample & test

--- sdk/purview/azure-analytics-purview-catalog/src/test/java/com/azure/analytics/purview/catalog/GlossaryClientTests.java
+++ sdk/purview/azure-analytics-purview-catalog/src/test/java/com/azure/analytics/purview/catalog/GlossaryClientTests.java
@@ -4,11 +4,8 @@
 package com.azure.analytics.purview.catalog;
 
 import com.azure.core.util.BinaryData;
-import com.azure.core.util.polling.SyncPoller;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import java.nio.file.Path;
 import java.util.List;
 
 public class GlossaryClientTests extends PurviewCatalogClientTestBase {
@@ -27,24 +24,5 @@ public class GlossaryClientTests extends PurviewCatalogClientTestBase {
         BinaryData binaryData = client.listGlossariesWithResponse(null, null).getValue();
         List<?> list = binaryData.toObject(List.class);
         System.out.println(list);
-//        DynamicResponse res = client.listGlossaries().send();
-//        assertEquals(200, res.getStatusCode());
-//
-//        JsonReader jsonReader = Json.createReader(new StringReader(res.getBody().toString()));
-//        JsonArray glossaries = jsonReader.readArray();
-//        assertEquals(1, glossaries.size());
-//
-//        JsonObject glossary = glossaries.get(0).asJsonObject();
-//        assertEquals("Glossary", glossary.getString("name"));
-//
-//        JsonArray terms = glossary.getJsonArray("terms");
-//        assertEquals(1, terms.size());
-    }
-
-    @Disabled
-    public void testImportTerm() {
-        BinaryData binaryData = BinaryData.fromFile(Path.of("C:\\Users\\fey\\Downloads\\import-terms-template-System-default.csv"));
-        System.out.println(binaryData);
-        client.beginImportGlossaryTermsViaCsvByGlossaryName("Glossary", binaryData, null, null).waitForCompletion();
     }
 }
