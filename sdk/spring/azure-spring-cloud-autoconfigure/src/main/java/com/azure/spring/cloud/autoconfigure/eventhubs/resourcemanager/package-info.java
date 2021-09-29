ef7ef20483a914d12f32e37ff96e05d444bb79b6
Xiaolu Dai <xiada@microsoft.com>
2021-09-29 21:46:41 +0800

    fix pipeline

--- sdk/spring/azure-spring-cloud-autoconfigure/src/main/java/com/azure/spring/cloud/autoconfigure/eventhubs/resourcemanager/package-info.java
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-/**
- * Package com.azure.spring.cloud.autoconfigure.eventhub.resourcemanager
- */
-package com.azure.spring.cloud.autoconfigure.eventhubs.resourcemanager;
