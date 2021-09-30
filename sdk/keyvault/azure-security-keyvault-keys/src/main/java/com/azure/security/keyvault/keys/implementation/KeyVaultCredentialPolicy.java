938c74c6b10db871271e5e1ee7f9247efc4c29de
Victor Colin Amador <vicolina@microsoft.com>
2021-09-29 17:05:30 -0700

    Applied PR feedback.

--- sdk/keyvault/azure-security-keyvault-keys/src/main/java/com/azure/security/keyvault/keys/implementation/KeyVaultCredentialPolicy.java
+++ sdk/keyvault/azure-security-keyvault-keys/src/main/java/com/azure/security/keyvault/keys/implementation/KeyVaultCredentialPolicy.java
@@ -31,8 +31,8 @@ import java.util.concurrent.ConcurrentMap;
 public class KeyVaultCredentialPolicy extends BearerTokenAuthenticationPolicy {
     private static final String BEARER_TOKEN_PREFIX = "Bearer ";
     private static final String CONTENT_LENGTH_HEADER = "Content-Length";
-    private static final String KEY_VAULT_STASHED_CONTENT_KEY = "KeyVaultBody";
-    private static final String KEY_VAULT_STASHED_CONTENT_LENGTH_KEY = "KeyVaultBodyContentLength";
+    private static final String KEY_VAULT_STASHED_CONTENT_KEY = "KeyVaultCredentialPolicyStashedBody";
+    private static final String KEY_VAULT_STASHED_CONTENT_LENGTH_KEY = "KeyVaultCredentialPolicyStashedContentLength";
     private static final String WWW_AUTHENTICATE = "WWW-Authenticate";
     private static final ConcurrentMap<String, String> SCOPE_CACHE = new ConcurrentHashMap<>();
     private String scope;
@@ -89,79 +89,83 @@ public class KeyVaultCredentialPolicy extends BearerTokenAuthenticationPolicy {
 
     @Override
     public Mono<Void> authorizeRequest(HttpPipelineCallContext context) {
-        HttpRequest request = context.getHttpRequest();
+        return Mono.defer(() -> {
+            HttpRequest request = context.getHttpRequest();
 
-        // If this policy doesn't have an authorityScope cached try to get it from the static challenge cache.
-        if (this.scope == null) {
-            String authority = getRequestAuthority(request);
-            this.scope = SCOPE_CACHE.get(authority);
-        }
-
-        if (this.scope != null) {
-            // We fetched the scope from the cache, but we have not initialized the scopes in the base yet.
-            TokenRequestContext tokenRequestContext = new TokenRequestContext().addScopes(this.scope);
-
-            return setAuthorizationHeader(context, tokenRequestContext);
-        }
-
-        // The body is removed from the initial request because Key Vault supports other authentication schemes which
-        // also protect the body of the request. As a result, before we know the auth scheme we need to avoid sending
-        // an unprotected body to Key Vault. We don't currently support this enhanced auth scheme in the SDK but we
-        // still don't want to send any unprotected data to vaults which require it.
-
-        // Do not overwrite previous contents if retrying after initial request failed (e.g. timeout).
-        if (!context.getData(KEY_VAULT_STASHED_CONTENT_KEY).isPresent()) {
-            if (request.getBody() != null) {
-                context.setData(KEY_VAULT_STASHED_CONTENT_KEY, request.getBody());
-                context.setData(KEY_VAULT_STASHED_CONTENT_LENGTH_KEY,
-                    request.getHeaders().getValue(CONTENT_LENGTH_HEADER));
-                request.setHeader(CONTENT_LENGTH_HEADER, "0");
-                request.setBody((Flux<ByteBuffer>) null);
+            // If this policy doesn't have an authorityScope cached try to get it from the static challenge cache.
+            if (this.scope == null) {
+                String authority = getRequestAuthority(request);
+                this.scope = SCOPE_CACHE.get(authority);
             }
-        }
 
-        return Mono.empty();
+            if (this.scope != null) {
+                // We fetched the scope from the cache, but we have not initialized the scopes in the base yet.
+                TokenRequestContext tokenRequestContext = new TokenRequestContext().addScopes(this.scope);
+
+                return setAuthorizationHeader(context, tokenRequestContext);
+            }
+
+            // The body is removed from the initial request because Key Vault supports other authentication schemes which
+            // also protect the body of the request. As a result, before we know the auth scheme we need to avoid sending
+            // an unprotected body to Key Vault. We don't currently support this enhanced auth scheme in the SDK but we
+            // still don't want to send any unprotected data to vaults which require it.
+
+            // Do not overwrite previous contents if retrying after initial request failed (e.g. timeout).
+            if (!context.getData(KEY_VAULT_STASHED_CONTENT_KEY).isPresent()) {
+                if (request.getBody() != null) {
+                    context.setData(KEY_VAULT_STASHED_CONTENT_KEY, request.getBody());
+                    context.setData(KEY_VAULT_STASHED_CONTENT_LENGTH_KEY,
+                        request.getHeaders().getValue(CONTENT_LENGTH_HEADER));
+                    request.setHeader(CONTENT_LENGTH_HEADER, "0");
+                    request.setBody((Flux<ByteBuffer>) null);
+                }
+            }
+
+            return Mono.empty();
+        });
     }
 
     @SuppressWarnings("unchecked")
     @Override
     public Mono<Boolean> authorizeRequestOnChallenge(HttpPipelineCallContext context, HttpResponse response) {
-        HttpRequest request = context.getHttpRequest();
-        Optional<Object> contentOptional = context.getData(KEY_VAULT_STASHED_CONTENT_KEY);
-        Optional<Object> contentLengthOptional = context.getData(KEY_VAULT_STASHED_CONTENT_LENGTH_KEY);
+        return Mono.defer(() -> {
+            HttpRequest request = context.getHttpRequest();
+            Optional<Object> contentOptional = context.getData(KEY_VAULT_STASHED_CONTENT_KEY);
+            Optional<Object> contentLengthOptional = context.getData(KEY_VAULT_STASHED_CONTENT_LENGTH_KEY);
 
-        if (request.getBody() == null && contentOptional.isPresent() && contentLengthOptional.isPresent()) {
-            request.setBody((Flux<ByteBuffer>) contentOptional.get());
-            request.setHeader(CONTENT_LENGTH_HEADER, (String) contentLengthOptional.get());
-        }
-
-        String authority = getRequestAuthority(request);
-        Map<String, String> challengeAttributes =
-            extractChallengeAttributes(response.getHeaderValue(WWW_AUTHENTICATE), BEARER_TOKEN_PREFIX);
-        String scope = challengeAttributes.get("resource");
-
-        if (scope != null) {
-            scope = scope + "/.default";
-        } else {
-            scope = challengeAttributes.get("scope");
-        }
-
-        if (scope == null) {
-            this.scope = SCOPE_CACHE.get(authority);
-
-            if (this.scope == null) {
-                return Mono.just(false);
+            if (request.getBody() == null && contentOptional.isPresent() && contentLengthOptional.isPresent()) {
+                request.setBody((Flux<ByteBuffer>) contentOptional.get());
+                request.setHeader(CONTENT_LENGTH_HEADER, (String) contentLengthOptional.get());
             }
-        } else {
-            this.scope = scope;
 
-            SCOPE_CACHE.put(authority, this.scope);
-        }
+            String authority = getRequestAuthority(request);
+            Map<String, String> challengeAttributes =
+                extractChallengeAttributes(response.getHeaderValue(WWW_AUTHENTICATE), BEARER_TOKEN_PREFIX);
+            String scope = challengeAttributes.get("resource");
 
-        TokenRequestContext tokenRequestContext = new TokenRequestContext().addScopes(this.scope);
+            if (scope != null) {
+                scope = scope + "/.default";
+            } else {
+                scope = challengeAttributes.get("scope");
+            }
 
-        return setAuthorizationHeader(context, tokenRequestContext)
-            .then(Mono.just(true));
+            if (scope == null) {
+                this.scope = SCOPE_CACHE.get(authority);
+
+                if (this.scope == null) {
+                    return Mono.just(false);
+                }
+            } else {
+                this.scope = scope;
+
+                SCOPE_CACHE.put(authority, this.scope);
+            }
+
+            TokenRequestContext tokenRequestContext = new TokenRequestContext().addScopes(this.scope);
+
+            return setAuthorizationHeader(context, tokenRequestContext)
+                .then(Mono.just(true));
+        });
     }
 
     static void clearCache() {
