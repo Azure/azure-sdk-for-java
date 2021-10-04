c0c6a74bc39defb195242582021f452e344f8b4c
Alan Zimmer <48699787+alzimmermsft@users.noreply.github.com>
2021-10-04 16:02:01 -0700

    Fix Java 17 Javadoc errors

--- sdk/search/azure-search-documents/src/main/java/com/azure/search/documents/SearchServiceVersion.java
+++ sdk/search/azure-search-documents/src/main/java/com/azure/search/documents/SearchServiceVersion.java
@@ -9,7 +9,14 @@ import com.azure.core.util.ServiceVersion;
  * The versions of Azure Cognitive Search supported by this client library.
  */
 public enum SearchServiceVersion implements ServiceVersion {
+    /**
+     * {@code 2020-06-30} service version.
+     */
     V2020_06_30("2020-06-30"),
+
+    /**
+     * {@code 2021-04-30-Preview} service version.
+     */
     V2021_04_30_PREVIEW("2021-04-30-Preview");
 
     private final String version;
