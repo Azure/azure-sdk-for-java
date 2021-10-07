dcf86f5bce146574f62dc3eb57594d3505ab6783
Alan Zimmer <48699787+alzimmermsft@users.noreply.github.com>
2021-10-07 09:20:24 -0700

    Reduce Memory Usage with Better Patterns (#24591)

--- sdk/core/azure-core/src/main/java/com/azure/core/util/polling/PollerFlux.java
+++ sdk/core/azure-core/src/main/java/com/azure/core/util/polling/PollerFlux.java
@@ -374,7 +374,8 @@ public final class PollerFlux<T, U> extends Flux<AsyncPollResponse<T, U>> {
                 // or duration specified in the last retry-after response header elapses.
                 return pollOnceMono.delaySubscription(getDelay(cxt.getLatestResponse()));
             })
-                .switchIfEmpty(Mono.error(new IllegalStateException("PollOperation returned Mono.empty().")))
+                .switchIfEmpty(Mono.defer(() -> Mono.error(new IllegalStateException(
+                    "PollOperation returned Mono.empty()."))))
                 .repeat()
                 .takeUntil(currentPollResponse -> currentPollResponse.getStatus().isComplete())
                 .concatMap(currentPollResponse -> {
