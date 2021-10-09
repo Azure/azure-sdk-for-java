d4b8bba49367f5d4bae7ed52503a8e332b203f10
Xiaolu Dai <xiada@microsoft.com>
2021-10-09 12:42:31 +0800

    remove package-info.java files for now

--- sdk/spring/azure-spring-cloud-autoconfigure/src/main/java/com/azure/spring/cloud/autoconfigure/storage/queue/package-info.java
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-/**
- * Package com.azure.spring.cloud.autoconfigure.storage.queue;
- */
-package com.azure.spring.cloud.autoconfigure.storage.queue;
-
