728dca0458f2192c070a07cb0700d0cc7edb8341
yiliu6 <yiliu6@microsoft.com>
2021-10-20 09:27:41 +0800

    add condition for AzureMessagingListenerConfiguration

--- sdk/spring/spring-cloud-azure-autoconfigure/src/main/java/com/azure/spring/cloud/autoconfigure/messaging/AzureMessagingListenerConfiguration.java
+++ sdk/spring/spring-cloud-azure-autoconfigure/src/main/java/com/azure/spring/cloud/autoconfigure/messaging/AzureMessagingListenerConfiguration.java
@@ -19,6 +19,7 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnClass(EnableAzureMessaging.class)
+@ConditionalOnBean(SubscribeByGroupOperation.class)
 public class AzureMessagingListenerConfiguration {
 
     @Bean
