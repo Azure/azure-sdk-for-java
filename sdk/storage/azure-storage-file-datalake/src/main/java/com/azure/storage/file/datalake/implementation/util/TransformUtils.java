081c40edb754a794295c6849273d43199faadcb8
Rick Ley <frley@microsoft.com>
2021-10-21 15:55:40 -0700

    Updated datalake version conversion

--- sdk/storage/azure-storage-file-datalake/src/main/java/com/azure/storage/file/datalake/implementation/util/TransformUtils.java
+++ sdk/storage/azure-storage-file-datalake/src/main/java/com/azure/storage/file/datalake/implementation/util/TransformUtils.java
@@ -30,6 +30,8 @@ public class TransformUtils {
             return BlobServiceVersion.V2020_10_02;
         } else if (DataLakeServiceVersion.V2020_12_06.ordinal() == version.ordinal()) {
             return BlobServiceVersion.V2020_12_06;
+        } else if (DataLakeServiceVersion.V2021_02_12.ordinal() == version.ordinal()) {
+            return BlobServiceVersion.V2021_02_12;
         }
         return null;
     }
