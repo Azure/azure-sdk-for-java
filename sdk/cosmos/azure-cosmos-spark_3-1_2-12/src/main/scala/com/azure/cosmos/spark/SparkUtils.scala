fd5c8cab25b24274d3ef94b93f932c7a73aa2aca
Mohammad Derakhshani <moderakh@users.noreply.github.com>
2021-10-26 13:21:57 -0700

    cosmos client warm up

--- sdk/cosmos/azure-cosmos-spark_3-1_2-12/src/main/scala/com/azure/cosmos/spark/SparkUtils.scala
+++ sdk/cosmos/azure-cosmos-spark_3-1_2-12/src/main/scala/com/azure/cosmos/spark/SparkUtils.scala
@@ -4,10 +4,12 @@
 package com.azure.cosmos.spark
 
 import com.azure.cosmos.CosmosAsyncContainer
+import com.azure.cosmos.models.{CosmosQueryRequestOptions, SqlParameter, SqlQuerySpec}
 import com.azure.cosmos.spark.diagnostics.ILogger
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.node.ObjectNode
 
+import java.util.UUID
 import java.util.concurrent.ThreadFactory
 import java.util.concurrent.atomic.AtomicInteger
 
@@ -33,9 +35,20 @@ private object SparkUtils {
 
   def safeOpenConnectionInitCaches(container: CosmosAsyncContainer, logger: (String, Exception) => Unit): Unit = {
     try {
-      container.openConnectionsAndInitCaches().block()
+      val sqlQuery = new SqlQuerySpec(s"SELECT * FROM r WHERE r['${UUID.randomUUID().toString}'] = @param",
+        new SqlParameter("@param", UUID.randomUUID().toString)
+      )
+
+      val res = container.queryItems(sqlQuery,
+        new CosmosQueryRequestOptions(),
+        classOf[ObjectNode])
+        .collectList()
+        .block()
+
+      res.clear()
     } catch {
       case e: Exception => {
+        e.printStackTrace()
         logger("ignoring openConnectionsAndInitCaches failure", e)
       }
     }
