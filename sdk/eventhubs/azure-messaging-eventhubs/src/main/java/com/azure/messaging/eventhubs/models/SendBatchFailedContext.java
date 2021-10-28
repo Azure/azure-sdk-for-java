b354a570fdf3da3b9c43dae1e04bcedb8dab53a5
Connie Yau <conniey@microsoft.com>
2021-10-27 18:22:45 -0700

    Adding documentation to buffered producer models.

--- sdk/eventhubs/azure-messaging-eventhubs/src/main/java/com/azure/messaging/eventhubs/models/SendBatchFailedContext.java
+++ sdk/eventhubs/azure-messaging-eventhubs/src/main/java/com/azure/messaging/eventhubs/models/SendBatchFailedContext.java
@@ -3,5 +3,59 @@
 
 package com.azure.messaging.eventhubs.models;
 
+import com.azure.messaging.eventhubs.EventData;
+import com.azure.messaging.eventhubs.EventHubBufferedProducerAsyncClient;
+import com.azure.messaging.eventhubs.EventHubBufferedProducerClient;
+
+/**
+ * Contains information about a batch that was unable to be published, as well as the exception that occurred and the
+ * partition that the batch was being published to.
+ *
+ * @see EventHubBufferedProducerClient
+ * @see EventHubBufferedProducerAsyncClient
+ */
 public final class SendBatchFailedContext {
+    private final Iterable<EventData> events;
+    private final String partitionId;
+    private final Throwable throwable;
+
+    /**
+     * Creates a new instance.
+     *
+     * @param events Events associated with the failed batch.
+     * @param partitionId Partition that the events went to.
+     * @param throwable Error associated with the failed batch.
+     */
+    public SendBatchFailedContext(Iterable<EventData> events, String partitionId, Throwable throwable) {
+        this.events = events;
+        this.partitionId = partitionId;
+        this.throwable = throwable;
+    }
+
+    /**
+     * Gets the events that failed to send.
+     *
+     * @return The events that failed to send.
+     */
+    public Iterable<EventData> getEvents() {
+        return events;
+    }
+
+    /**
+     * Gets the partition id that the failed batch went to.
+     *
+     * @return The partition id that the failed batch went to.
+     */
+    public String getPartitionId() {
+        return partitionId;
+    }
+
+    /**
+     * Gets the error that occurred when sending the batch.
+     *
+     * @return The error that occurred when sending the batch.
+     */
+    public Throwable getThrowable() {
+        return throwable;
+    }
 }
--- sdk/eventhubs/azure-messaging-eventhubs/src/main/java/com/azure/messaging/eventhubs/models/SendBatchSucceededContext.java
+++ sdk/eventhubs/azure-messaging-eventhubs/src/main/java/com/azure/messaging/eventhubs/models/SendBatchSucceededContext.java
@@ -3,5 +3,46 @@
 
 package com.azure.messaging.eventhubs.models;
 
+import com.azure.messaging.eventhubs.EventData;
+import com.azure.messaging.eventhubs.EventHubBufferedProducerAsyncClient;
+import com.azure.messaging.eventhubs.EventHubBufferedProducerClient;
+
+/**
+ * Contains information about a batch that was published and the partition that it was published to.
+ *
+ * @see EventHubBufferedProducerAsyncClient
+ * @see EventHubBufferedProducerClient
+ */
 public final class SendBatchSucceededContext {
+    private final String partitionId;
+    private final Iterable<EventData> events;
+
+    /**
+     * Initializes a new instance of the class.
+     *
+     * @param events The set of events in the batch that was published.
+     * @param partitionId The identifier of the partition that the batch was published to.
+     */
+    public SendBatchSucceededContext(Iterable<EventData> events, String partitionId) {
+        this.events = events;
+        this.partitionId = partitionId;
+    }
+
+    /**
+     * Gets the set of events in the batch that was published.
+     *
+     * @return The set of events in the batch that was published.
+     */
+    public Iterable<EventData> getEvents() {
+        return events;
+    }
+
+    /**
+     * Gets the identifier of the partition that the batch was published to.
+     *
+     * @return The identifier of the partition that the batch was published to.
+     */
+    public String getPartitionId() {
+        return partitionId;
+    }
 }
