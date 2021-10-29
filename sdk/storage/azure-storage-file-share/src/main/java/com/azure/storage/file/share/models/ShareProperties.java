700c7cd7fea86dd4c71d8936904fe32ac957bcd1
Rick Ley <frley@microsoft.com>
2021-10-29 13:58:38 -0700

    Removed unused import

--- sdk/storage/azure-storage-file-share/src/main/java/com/azure/storage/file/share/models/ShareProperties.java
+++ sdk/storage/azure-storage-file-share/src/main/java/com/azure/storage/file/share/models/ShareProperties.java
@@ -6,7 +6,6 @@ package com.azure.storage.file.share.models;
 
 import com.azure.core.annotation.Fluent;
 import com.azure.core.util.DateTimeRfc1123;
-import com.azure.storage.file.share.implementation.models.SharePropertiesInternal;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import java.time.OffsetDateTime;
