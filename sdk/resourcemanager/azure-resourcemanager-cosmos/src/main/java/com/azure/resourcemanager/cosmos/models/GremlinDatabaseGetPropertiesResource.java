78967588a11508d0851646f686a394280697ce67
Weidong Xu <weidxu@microsoft.com>
2021-11-01 10:17:18 +0800

    mgmt, upgrade cosmos (#25081)
    * cosmos 2021-10
    
    * regen
    
    * auto update sessions
    
    * changelog
    
    * manual fix for invalid server response

--- sdk/resourcemanager/azure-resourcemanager-cosmos/src/main/java/com/azure/resourcemanager/cosmos/models/GremlinDatabaseGetPropertiesResource.java
+++ sdk/resourcemanager/azure-resourcemanager-cosmos/src/main/java/com/azure/resourcemanager/cosmos/models/GremlinDatabaseGetPropertiesResource.java
@@ -4,13 +4,13 @@
 
 package com.azure.resourcemanager.cosmos.models;
 
-import com.azure.core.annotation.Immutable;
+import com.azure.core.annotation.Fluent;
 import com.azure.core.util.logging.ClientLogger;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /** The GremlinDatabaseGetPropertiesResource model. */
-@Immutable
+@Fluent
 public final class GremlinDatabaseGetPropertiesResource extends GremlinDatabaseResource {
     @JsonIgnore private final ClientLogger logger = new ClientLogger(GremlinDatabaseGetPropertiesResource.class);
 
