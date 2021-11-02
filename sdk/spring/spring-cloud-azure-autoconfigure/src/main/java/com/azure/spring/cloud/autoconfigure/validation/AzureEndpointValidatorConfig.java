7c6cd71f87bfa80a576cfe93bae4ae59330d58da
zhihaoguo <zhihaoguo@microsoft.com>
2021-11-02 22:14:34 +0800

    add configuration in spring.factories

--- sdk/spring/spring-cloud-azure-autoconfigure/src/main/java/com/azure/spring/cloud/autoconfigure/validation/AzureEndpointValidatorConfig.java
+++ sdk/spring/spring-cloud-azure-autoconfigure/src/main/java/com/azure/spring/cloud/autoconfigure/validation/AzureEndpointValidatorConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 /**
  */
-@Configuration
+
 public class AzureEndpointValidatorConfig {
     @Bean
     public static AzureEndpointValidator configurationPropertiesValidator() {
