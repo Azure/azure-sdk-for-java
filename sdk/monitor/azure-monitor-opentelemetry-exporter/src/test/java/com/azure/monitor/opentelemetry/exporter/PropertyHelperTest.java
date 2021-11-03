d5a7c3d06b6d3eab2d8b78a920358a4120ebba09
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-11-02 19:48:09 -0700

    Convert asserts to assertj

--- sdk/monitor/azure-monitor-opentelemetry-exporter/src/test/java/com/azure/monitor/opentelemetry/exporter/PropertyHelperTest.java
+++ sdk/monitor/azure-monitor-opentelemetry-exporter/src/test/java/com/azure/monitor/opentelemetry/exporter/PropertyHelperTest.java
@@ -1,22 +1,20 @@
 package com.azure.monitor.opentelemetry.exporter;
 
 import com.azure.monitor.opentelemetry.exporter.utils.PropertyHelper;
+import io.opentelemetry.api.OpenTelemetry;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PropertyHelperTest {
     @Test
     void testGetSDKVersionNumber() {
-        String sdkVersionNumber = PropertyHelper.getSdkVersionNumber();
-        String regex = "[0-9].[0-9].[0-9].*";
-        assertTrue(sdkVersionNumber.matches(regex));
+        assertThat(PropertyHelper.getSdkVersionNumber()).matches("[0-9].[0-9].[0-9].*");
     }
 
     @Test
     void testGetQualifiedSDKVersionString() {
-        String sdkVersionString = PropertyHelper.getQualifiedSdkVersionString();
-        String regex = "java[0-9].?[0-9]:ot.*:ext.*";
-        assertTrue(sdkVersionString.matches(regex));
+        assertThat(PropertyHelper.getQualifiedSdkVersionString()).matches("java[0-9].?[0-9]:ot.*:ext.*");
     }
 }
