1a0510d7019fef000fd827afdc9ae3874317b35e
Rujun Chen <rujche@microsoft.com>
2021-11-10 14:16:20 +0800

    1. Move active directory related code from main branch to 4.0 branch. 2. Change package name. 3. Resolve the errors reported by "mvn clean install".

---
+++ sdk/spring/spring-cloud-azure-autoconfigure/src/main/java/com/azure/spring/cloud/autoconfigure/active/directory/implementation/aad/webapp/package-info.java
@@ -0,0 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+/**
+ * Package com.azure.spring.cloud.autoconfigure.active.directory.implementation.aad.webapp
+ */
+package com.azure.spring.cloud.autoconfigure.active.directory.implementation.aad.webapp;
