48f5f1ccbc93651bb7b3abac49f40966fbc1acb9
kryalama <66494519+kryalama@users.noreply.github.com>
2021-11-11 17:36:01 -0800

    make tests accessible

--- sdk/monitor/azure-monitor-opentelemetry-exporter/src/test/java/com/azure/monitor/opentelemetry/exporter/implementation/VersionTest.java
+++ sdk/monitor/azure-monitor-opentelemetry-exporter/src/test/java/com/azure/monitor/opentelemetry/exporter/implementation/VersionTest.java
@@ -7,17 +7,17 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class VersionTest {
     @Test
-    void testArtifactName() {
+    public void testArtifactName() {
         assertEquals("azure-monitor-opentelemetry-exporter", VersionGenerator.getArtifactName());
     }
 
     @Test
-    void testArtifactVersion() {
+    public void testArtifactVersion() {
         assertTrue(VersionGenerator.getArtifactVersion().matches("[0-9].[0-9].[0-9].*"));
     }
 
     @Test
-    void testSdkVersion() {
+    public void testSdkVersion() {
         // OpenTelemetry added version.properties files in 1.3.0
         // but testing against OpenTelemetry 1.0.0, so it's unknown in this test
         assertTrue(VersionGenerator.getSdkVersion().matches("java[0-9._]+:ot([0-9.]+|unknown):ext[0-9.]+.*"));
