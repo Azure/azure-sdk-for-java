9b80a2430219bba164a382d7649c58328ddef7dc
kryalama <66494519+kryalama@users.noreply.github.com>
2021-11-12 09:35:54 -0800

    Bump otel version and add sdkversionstring to telemetry (#25162)
    * initial commit
    
    * Convert asserts to assertj
    
    * Some updates
    
    * Read otel version
    
    * Oops
    
    * More oops
    
    * More oops
    
    * address comments
    
    * fix indentation
    
    * make tests accessible
    
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>

--- sdk/monitor/azure-monitor-opentelemetry-exporter/src/main/java/com/azure/monitor/opentelemetry/exporter/implementation/FormattedDuration.java
+++ sdk/monitor/azure-monitor-opentelemetry-exporter/src/main/java/com/azure/monitor/opentelemetry/exporter/implementation/FormattedDuration.java
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.azure.monitor.opentelemetry.exporter.utils;
+package com.azure.monitor.opentelemetry.exporter.implementation;
 
 import static java.util.concurrent.TimeUnit.DAYS;
 import static java.util.concurrent.TimeUnit.HOURS;
