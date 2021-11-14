2672314cb3f24b20c61a6383c112437e47736b0c
Xiaolu Dai <xiada@microsoft.com>
2021-11-15 01:02:20 +0800

    refactor code

--- sdk/spring/spring-cloud-azure-core/src/main/java/com/azure/spring/core/properties/AbstractAzureSdkProperties.java
+++ sdk/spring/spring-cloud-azure-core/src/main/java/com/azure/spring/core/properties/AbstractAzureSdkProperties.java
@@ -4,64 +4,23 @@
 package com.azure.spring.core.properties;
 
 import com.azure.spring.core.properties.authentication.TokenCredentialProperties;
-import com.azure.spring.core.properties.client.ClientProperties;
 import com.azure.spring.core.properties.profile.AzureProfile;
-import com.azure.spring.core.properties.proxy.ProxyProperties;
-import com.azure.spring.core.properties.retry.RetryProperties;
 
 /**
  * Unified properties for Azure SDK clients.
  */
 public abstract class AbstractAzureSdkProperties implements AzureProperties {
 
-    private ClientProperties client = new ClientProperties();
-    private ProxyProperties proxy = new ProxyProperties();
-    private RetryProperties retry = new RetryProperties();
-    private TokenCredentialProperties credential = new TokenCredentialProperties();
-    private AzureProfile profile = new AzureProfile();
-
-    @Override
-    public ClientProperties getClient() {
-        return client;
-    }
-
-    public void setClient(ClientProperties client) {
-        this.client = client;
-    }
-
-    @Override
-    public ProxyProperties getProxy() {
-        return proxy;
-    }
-
-    public void setProxy(ProxyProperties proxy) {
-        this.proxy = proxy;
-    }
-
-    @Override
-    public RetryProperties getRetry() {
-        return retry;
-    }
-
-    public void setRetry(RetryProperties retry) {
-        this.retry = retry;
-    }
+    protected final TokenCredentialProperties credential = new TokenCredentialProperties();
+    protected final AzureProfile profile = new AzureProfile();
 
     @Override
     public TokenCredentialProperties getCredential() {
         return credential;
     }
 
-    public void setCredential(TokenCredentialProperties credential) {
-        this.credential = credential;
-    }
-
     @Override
     public AzureProfile getProfile() {
         return profile;
-    }
-
-    public void setProfile(AzureProfile profile) {
-        this.profile = profile;
     }
 }
