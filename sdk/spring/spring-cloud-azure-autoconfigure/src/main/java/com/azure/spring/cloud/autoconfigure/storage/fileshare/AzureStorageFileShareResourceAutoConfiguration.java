87e1969821a8f4418f9f0671209b64907b43e61f
zhihaoguo <zhihaoguo@microsoft.com>
2021-11-16 12:18:06 +0800

    Set content-type when using Azure Storage Resource (#25253)

--- sdk/spring/spring-cloud-azure-autoconfigure/src/main/java/com/azure/spring/cloud/autoconfigure/storage/fileshare/AzureStorageFileShareResourceAutoConfiguration.java
+++ sdk/spring/spring-cloud-azure-autoconfigure/src/main/java/com/azure/spring/cloud/autoconfigure/storage/fileshare/AzureStorageFileShareResourceAutoConfiguration.java
@@ -5,7 +5,6 @@ package com.azure.spring.cloud.autoconfigure.storage.fileshare;
 
 import com.azure.spring.cloud.autoconfigure.condition.ConditionalOnAnyProperty;
 import com.azure.spring.core.resource.AzureStorageFileProtocolResolver;
-import com.azure.storage.file.share.ShareServiceClient;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -14,7 +13,7 @@ import org.springframework.context.annotation.Bean;
 /**
  * Auto-configuration for a {@link AzureStorageFileProtocolResolver}.
  */
-@ConditionalOnClass({ ShareServiceClient.class, AzureStorageFileProtocolResolver.class })
+@ConditionalOnClass({ AzureStorageFileProtocolResolver.class })
 @ConditionalOnProperty(value = "spring.cloud.azure.storage.fileshare.enabled", havingValue = "true", matchIfMissing = true)
 @ConditionalOnAnyProperty(prefix = "spring.cloud.azure.storage.fileshare", name = { "account-name", "endpoint", "connection-string" })
 public class AzureStorageFileShareResourceAutoConfiguration  {
