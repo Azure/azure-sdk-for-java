cbe2aae712002d62f57e9dd39e17ff346609e2dd
Strong Liu <shaozliu@microsoft.com>
2021-11-17 21:57:01 +0800

    remove unused import

--- sdk/spring/spring-cloud-azure-core/src/main/java/com/azure/spring/core/factory/credential/AbstractAzureCredentialBuilderFactory.java
+++ sdk/spring/spring-cloud-azure-core/src/main/java/com/azure/spring/core/factory/credential/AbstractAzureCredentialBuilderFactory.java
@@ -21,7 +21,6 @@ import java.util.Collections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.time.Duration;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
