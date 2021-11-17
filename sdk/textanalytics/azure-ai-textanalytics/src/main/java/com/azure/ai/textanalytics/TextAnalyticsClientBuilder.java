94cf05136da02cdf5493e0d6bcd6ecde9d43aeef
Sameeksha Vaity <savaity@microsoft.com>
2021-11-17 10:49:50 -0800

    Update Text Analytics to code snippet tooling (#25481)

--- sdk/textanalytics/azure-ai-textanalytics/src/main/java/com/azure/ai/textanalytics/TextAnalyticsClientBuilder.java
+++ sdk/textanalytics/azure-ai-textanalytics/src/main/java/com/azure/ai/textanalytics/TextAnalyticsClientBuilder.java
@@ -52,11 +52,25 @@ import java.util.Objects;
  *
  * <p><strong>Instantiating an asynchronous Text Analytics Client</strong></p>
  *
- * {@codesnippet com.azure.ai.textanalytics.TextAnalyticsAsyncClient.instantiation}
+ * <!-- src_embed com.azure.ai.textanalytics.TextAnalyticsAsyncClient.instantiation -->
+ * <pre>
+ * TextAnalyticsAsyncClient textAnalyticsAsyncClient = new TextAnalyticsClientBuilder&#40;&#41;
+ *     .credential&#40;new AzureKeyCredential&#40;&quot;&#123;key&#125;&quot;&#41;&#41;
+ *     .endpoint&#40;&quot;&#123;endpoint&#125;&quot;&#41;
+ *     .buildAsyncClient&#40;&#41;;
+ * </pre>
+ * <!-- end com.azure.ai.textanalytics.TextAnalyticsAsyncClient.instantiation -->
  *
  * <p><strong>Instantiating a synchronous Text Analytics Client</strong></p>
  *
- * {@codesnippet com.azure.ai.textanalytics.TextAnalyticsClient.instantiation}
+ * <!-- src_embed com.azure.ai.textanalytics.TextAnalyticsClient.instantiation -->
+ * <pre>
+ * TextAnalyticsClient textAnalyticsClient = new TextAnalyticsClientBuilder&#40;&#41;
+ *     .credential&#40;new AzureKeyCredential&#40;&quot;&#123;key&#125;&quot;&#41;&#41;
+ *     .endpoint&#40;&quot;&#123;endpoint&#125;&quot;&#41;
+ *     .buildClient&#40;&#41;;
+ * </pre>
+ * <!-- end com.azure.ai.textanalytics.TextAnalyticsClient.instantiation -->
  *
  * <p>
  * Another way to construct the client is using a {@link HttpPipeline}. The pipeline gives the client an authenticated
@@ -65,7 +79,19 @@ import java.util.Objects;
  * on how the {@link TextAnalyticsClient} and {@link TextAnalyticsAsyncClient} is built.
  * </p>
  *
- * {@codesnippet com.azure.ai.textanalytics.TextAnalyticsClient.pipeline.instantiation}
+ * <!-- src_embed com.azure.ai.textanalytics.TextAnalyticsClient.pipeline.instantiation -->
+ * <pre>
+ * HttpPipeline pipeline = new HttpPipelineBuilder&#40;&#41;
+ *     .policies&#40;&#47;* add policies *&#47;&#41;
+ *     .build&#40;&#41;;
+ *
+ * TextAnalyticsClient textAnalyticsClient = new TextAnalyticsClientBuilder&#40;&#41;
+ *     .credential&#40;new AzureKeyCredential&#40;&quot;&#123;key&#125;&quot;&#41;&#41;
+ *     .endpoint&#40;&quot;&#123;endpoint&#125;&quot;&#41;
+ *     .pipeline&#40;pipeline&#41;
+ *     .buildClient&#40;&#41;;
+ * </pre>
+ * <!-- end com.azure.ai.textanalytics.TextAnalyticsClient.pipeline.instantiation -->
  *
  * @see TextAnalyticsAsyncClient
  * @see TextAnalyticsClient
