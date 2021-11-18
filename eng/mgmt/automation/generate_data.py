21b92ee9a3b0d8651bb77b2789632d84512596be
Weidong Xu <weidxu@microsoft.com>
2021-11-18 13:08:54 +0800

    llc, sdk automation for data-plane

--- eng/mgmt/automation/generate_data.py
+++ eng/mgmt/automation/generate_data.py
@@ -4,14 +4,68 @@ import sys
 import shutil
 import logging
 import argparse
+import re
+import glob
+from typing import List
 
-pwd = os.getcwd()
-os.chdir(os.path.abspath(os.path.dirname(sys.argv[0])))
 from parameters import *
-os.chdir(pwd)
 
 
 LLC_ARGUMENTS = '--java --low-level-client --sdk-integration --generate-samples'
+
+
+def sdk_automation(config: dict) -> List[dict]:
+    base_dir = os.path.abspath(os.path.dirname(sys.argv[0]))
+    sdk_root = os.path.abspath(os.path.join(base_dir, SDK_ROOT))
+    spec_root = config['specFolder']
+
+    packages = []
+
+    for file_path in config['changedFiles']:
+        match = re.search(
+            'specification/([^/]+)/data-plane/.*/([^/]+).json',
+            file_path,
+            re.IGNORECASE,
+        )
+        if match and '/examples/' not in file_path:
+            group = match.group(1)
+            file_name = match.group(2)
+            file_name_sans = ''.join(c for c in file_name if c.isalnum())
+            module = 'azure-{0}-{1}'.format(group, file_name_sans)
+            input_file = os.path.join(spec_root, file_path)
+            # placeholder, for lack of information
+            credential_types = 'tokencredential'
+            credential_scopes = 'https://{0}.azure.com/.default'.format(group)
+
+            succeeded = generate(sdk_root, input_file,
+                                 group, module, credential_types, credential_scopes,
+                                 AUTOREST_CORE_VERSION, AUTOREST_JAVA, '')
+
+            generated_folder = 'sdk/{0}/{1}'.format(group, module)
+
+            if succeeded:
+                install_build_tools(sdk_root)
+                succeeded = compile_package(os.path.join(sdk_root, generated_folder))
+
+            artifacts = [
+                '{0}/pom.xml'.format(generated_folder)
+            ]
+            artifacts += [
+                jar for jar in glob.glob('{0}/target/*.jar'.format(
+                    generated_folder))
+            ]
+            result = 'succeeded' if succeeded else 'failed'
+
+            packages.append({
+                'packageName': module,
+                'path': [generated_folder],
+                'artifacts': artifacts,
+                'result': result,
+            })
+        else:
+            logging.info('[Skip] changed file {0}'.format(file_path))
+
+    return packages
 
 
 def generate(
@@ -31,8 +85,9 @@ def generate(
         sdk_root,
         'sdk', group, module
     )
-    shutil.rmtree(os.path.join(output_dir, 'src/main'), ignore_errors = True)
-    shutil.rmtree(os.path.join(output_dir, 'src/samples/java', namespace.replace('.', '/'), 'generated'), ignore_errors = True)
+    shutil.rmtree(os.path.join(output_dir, 'src/main'), ignore_errors=True)
+    shutil.rmtree(os.path.join(output_dir, 'src/samples/java', namespace.replace('.', '/'), 'generated'),
+                  ignore_errors=True)
 
     credential_arguments = '--java.credential-types={0}'.format(credential_types)
     if credential_scopes:
@@ -56,7 +111,16 @@ def generate(
     return True
 
 
-def compile_package(output_dir):
+def install_build_tools(sdk_root: str):
+    command = 'mvn --no-transfer-progress clean install -f {0} -pl com.azure:sdk-build-tools'.format(os.path.join(sdk_root, 'pom.xml'))
+    logging.info(command)
+    if os.system(command) != 0:
+        logging.error('[COMPILE] Maven build fail for sdk-build-tools')
+        return False
+    return True
+
+
+def compile_package(output_dir: str):
     command = 'mvn --no-transfer-progress clean verify package -f {0}'.format(os.path.join(output_dir, 'pom.xml'))
     logging.info(command)
     if os.system(command) != 0:
@@ -127,6 +191,7 @@ def main():
         sdk_root,
         'sdk', args['group'], args['module']
     )
+    install_build_tools(sdk_root)
     compile_package(output_dir)
 
 
