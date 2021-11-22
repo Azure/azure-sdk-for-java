260b245029e019e98d137093626ab0654a355bbe
Meha Kaushik <mekaushi@microsoft.com>
2021-11-21 22:48:03 -0800

    nit

--- sdk/cosmos/azure-cosmos/src/test/java/com/azure/cosmos/implementation/RxGatewayStoreModelTest.java
+++ sdk/cosmos/azure-cosmos/src/test/java/com/azure/cosmos/implementation/RxGatewayStoreModelTest.java
@@ -7,6 +7,7 @@ import com.azure.cosmos.ConsistencyLevel;
 import com.azure.cosmos.CosmosException;
 import com.azure.cosmos.implementation.directconnectivity.ReflectionUtils;
 import com.azure.cosmos.implementation.http.HttpClient;
+import com.azure.cosmos.implementation.http.HttpHeaders;
 import com.azure.cosmos.implementation.http.HttpRequest;
 import io.netty.handler.codec.http.HttpMethod;
 import io.netty.handler.timeout.ReadTimeoutException;
