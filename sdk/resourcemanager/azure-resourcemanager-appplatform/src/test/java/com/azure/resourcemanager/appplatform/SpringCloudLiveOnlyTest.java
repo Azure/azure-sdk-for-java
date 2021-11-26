484bd4ce0de161dd722de48d44dabdf15fa4faf9
Weidong Xu <weidxu@microsoft.com>
2021-11-26 09:42:42 +0800

    fix checkstyle

--- sdk/resourcemanager/azure-resourcemanager-appplatform/src/test/java/com/azure/resourcemanager/appplatform/SpringCloudLiveOnlyTest.java
+++ sdk/resourcemanager/azure-resourcemanager-appplatform/src/test/java/com/azure/resourcemanager/appplatform/SpringCloudLiveOnlyTest.java
@@ -405,13 +405,13 @@ public class SpringCloudLiveOnlyTest extends AppPlatformTest {
         HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
     }
 
-    private static final char[] hexCode = "0123456789ABCDEF".toCharArray();
+    private static final char[] HEX_CODE = "0123456789ABCDEF".toCharArray();
 
     private static String printHexBinary(byte[] data) {
         StringBuilder r = new StringBuilder(data.length * 2);
         for (byte b : data) {
-            r.append(hexCode[(b >> 4) & 0xF]);
-            r.append(hexCode[(b & 0xF)]);
+            r.append(HEX_CODE[(b >> 4) & 0xF]);
+            r.append(HEX_CODE[(b & 0xF)]);
         }
         return r.toString();
     }
