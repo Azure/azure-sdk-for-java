c470c686a55bd872053825df927f4446ae8e4822
Rujun Chen <rujche@microsoft.com>
2021-11-26 08:33:08 +0800

    Fix error: duplicated bean definition.

--- sdk/spring/spring-cloud-azure-autoconfigure/src/main/java/com/azure/spring/cloud/autoconfigure/aad/AADAutoConfiguration.java
+++ sdk/spring/spring-cloud-azure-autoconfigure/src/main/java/com/azure/spring/cloud/autoconfigure/aad/AADAutoConfiguration.java
@@ -8,6 +8,7 @@ import com.azure.spring.cloud.autoconfigure.aad.properties.AADAuthenticationProp
 import com.azure.spring.cloud.autoconfigure.aad.properties.AADResourceServerProperties;
 import com.azure.spring.cloud.autoconfigure.aad.webapi.AADResourceServerConfiguration;
 import com.azure.spring.cloud.autoconfigure.aad.webapp.AADWebApplicationConfiguration;
+import com.azure.spring.cloud.autoconfigure.context.AzureGlobalPropertiesAutoConfiguration;
 import com.azure.spring.cloud.autoconfigure.properties.AzureGlobalProperties;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -22,11 +23,11 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @ConditionalOnProperty(value = "spring.cloud.azure.active-directory.enabled", havingValue = "true")
 @EnableConfigurationProperties({
-    AzureGlobalProperties.class,
     AADAuthenticationProperties.class,
     AADResourceServerProperties.class
 })
 @Import({
+    AzureGlobalPropertiesAutoConfiguration.class,
     AADWebApplicationConfiguration.class,
     AADResourceServerConfiguration.class,
     AADOAuth2ClientConfiguration.class
