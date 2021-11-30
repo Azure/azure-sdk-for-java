228842462ace501fc5d1a6152ca43de41eeaec40
Fabian Meiswinkel <fabian@meiswinkel.com>
2021-11-30 20:55:49 +0000

    Adding introduction/comment to the Retry iterator

--- sdk/cosmos/azure-cosmos-spark_3-1_2-12/src/main/scala/com/azure/cosmos/spark/TransientIOErrorsRetryingIterator.scala
+++ sdk/cosmos/azure-cosmos-spark_3-1_2-12/src/main/scala/com/azure/cosmos/spark/TransientIOErrorsRetryingIterator.scala
@@ -13,6 +13,21 @@ import java.util.concurrent.locks.ReentrantReadWriteLock
 import scala.util.Random
 import scala.util.control.Breaks
 
+// This iterator exists to allow adding more extensive retries for transient
+// IO errors when draining a query or change feed query
+// The Java SDK has built-in retry policies - but those retry policies are
+// pretty restrictive - only allowing retries for 30 seconds for example in eventual consistency mode
+// The built-in SDK policies are optimized for OLTP scenarios - assuming that client machines
+// are not resource/CPU exhausted etc. In Spark scenarios it is much more common
+// that executors at least temporarily have pegged CPU, often queries are very IO intense,
+// use large page sizes etc. The retry policy against transient IO errors needs to be more robust
+// as a consequence for Spark scenarios.
+// The iterator below allows retries based on the continuation token of the previously received response
+// because we know that IO errors cannot happen iterating over documents of one page it is safe
+// to use the continuation token to keep draining on the retry
+// TODO @fabianm - we should still have a discussion whether it would be worth to allow tweaking
+//  the retry policy of the SDK. But having the Spark specific retries for now to get some experience
+//  can help making the right decisions if/how to expose this in the SDK
 private class TransientIOErrorsRetryingIterator
 (
   cosmosPagedFluxFactory: String => CosmosPagedFlux[ObjectNode],
