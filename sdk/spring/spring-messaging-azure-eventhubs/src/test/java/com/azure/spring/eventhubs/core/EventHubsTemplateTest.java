0938968c967222e37a974cfa0e6b2923f66c276e
Gary Liu <1392029926@qq.com>
2021-12-03 11:21:37 +0800

    sending batch issue
    refactor the implementation and test case

--- sdk/spring/spring-messaging-azure-eventhubs/src/test/java/com/azure/spring/eventhubs/core/EventHubsTemplateTest.java
+++ sdk/spring/spring-messaging-azure-eventhubs/src/test/java/com/azure/spring/eventhubs/core/EventHubsTemplateTest.java
@@ -3,6 +3,8 @@
 
 package com.azure.spring.eventhubs.core;
 
+import com.azure.core.amqp.exception.AmqpErrorCondition;
+import com.azure.core.amqp.exception.AmqpException;
 import com.azure.messaging.eventhubs.EventData;
 import com.azure.messaging.eventhubs.EventDataBatch;
 import com.azure.messaging.eventhubs.EventHubProducerAsyncClient;
@@ -14,8 +16,10 @@ import org.springframework.messaging.Message;
 import org.springframework.messaging.support.GenericMessage;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -35,23 +39,19 @@ class EventHubsTemplateTest {
 
         EventHubsProducerFactory producerFactory = mock(EventHubsProducerFactory.class);
         this.mockProducerClient = mock(EventHubProducerAsyncClient.class);
-        CreateBatchOptions options =
-                new CreateBatchOptions().setMaximumSizeInBytes(1024).setPartitionKey("test");
-
         when(producerFactory.createProducer(this.destination)).thenReturn(this.mockProducerClient);
 
         //spy EventHusTemplate just to mock partial method
         this.eventHubsTemplate = spy(new EventHubsTemplate(producerFactory));
-        when(this.eventHubsTemplate.buildCreateBatchOptions(any())).thenReturn(options);
         when(this.mockProducerClient.send(any(EventDataBatch.class))).thenReturn(this.mono);
     }
 
 
     /**
-     * test the multiple batches case
+     * test the three batches case
      */
     @Test
-    void testSendAsyncForMessagesWithMoreThanOneBatch() {
+    void testSendAsyncForMessagesWithThreeBatch() {
         EventDataBatch eventDataBatch = mock(EventDataBatch.class);
 
         when(this.mockProducerClient.createBatch(any(CreateBatchOptions.class)))
@@ -88,9 +88,100 @@ class EventHubsTemplateTest {
         List<Message<String>> messages =
                 messagesList.stream().map((Function<String, GenericMessage<String>>) GenericMessage::new).collect(Collectors.toList());
 
-        this.eventHubsTemplate.sendAsync(this.destination, messages, null).block();
+        this.eventHubsTemplate.sendAsync(this.destination, messages, null).doOnSuccess(t -> {
+            System.out.println("do on success:" + t);
+        }).block();
         verify(this.mockProducerClient, times(1)).send(any(EventDataBatch.class));
     }
 
 
+    /**
+     * test the normal one batch case with one exception at the first
+     */
+    @Test
+    void testSendAsyncForMessagesOneBatchAndSendCompletelyWithException() {
+        EventDataBatch eventDataBatch = mock(EventDataBatch.class);
+
+        when(this.mockProducerClient.createBatch(any(CreateBatchOptions.class)))
+                .thenReturn(Mono.just(eventDataBatch));
+        when(eventDataBatch.tryAdd(any(EventData.class))).thenThrow(new AmqpException(false, AmqpErrorCondition.LINK_PAYLOAD_SIZE_EXCEEDED,
+                String.format(Locale.US, "Size of the payload exceeded maximum message size: %s kb",
+                        1024*1024 / 1024),
+                null)).thenReturn(true, true, true, true);
+        when(eventDataBatch.getCount()).thenReturn(5);
+        List<String> messagesList = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            messagesList.add("abcde");
+        }
+        List<Message<String>> messages =
+                messagesList.stream().map((Function<String, GenericMessage<String>>) GenericMessage::new).collect(Collectors.toList());
+
+        this.eventHubsTemplate.sendAsync(this.destination, messages, null).doOnError(ex -> {
+            System.out.println("do on Error");
+            ex.printStackTrace();
+        }).doOnSuccess(t -> {
+            System.out.println("do on success:" + t);
+        }).block(Duration.ofSeconds(10));
+        verify(this.mockProducerClient, times(1)).send(any(EventDataBatch.class));
+    }
+
+    /**
+     * test the normal two batch case with one exception in the middle
+     */
+    @Test
+    void testSendAsyncForMessagesTwoBatchAndSendCompletelyWithException() {
+        EventDataBatch eventDataBatch = mock(EventDataBatch.class);
+
+        when(this.mockProducerClient.createBatch(any(CreateBatchOptions.class)))
+                .thenReturn(Mono.just(eventDataBatch));
+        when(eventDataBatch.tryAdd(any(EventData.class))).thenReturn(true, false, true).
+                thenThrow(new AmqpException(false,
+                AmqpErrorCondition.LINK_PAYLOAD_SIZE_EXCEEDED,
+                String.format(Locale.US, "Size of the payload exceeded maximum message size: %s kb",
+                        1024*1024 / 1024),
+                null)).thenReturn(true, true);
+        when(eventDataBatch.getCount()).thenReturn(5);
+        List<String> messagesList = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            messagesList.add("abcde");
+        }
+        List<Message<String>> messages =
+                messagesList.stream().map((Function<String, GenericMessage<String>>) GenericMessage::new).collect(Collectors.toList());
+
+        this.eventHubsTemplate.sendAsync(this.destination, messages, null).doOnError(ex -> {
+            System.out.println("do on Error");
+            ex.printStackTrace();
+        }).doOnSuccess(t -> {
+            System.out.println("do on success:" + t);
+        }).block(Duration.ofSeconds(10));
+        verify(this.mockProducerClient, times(2)).send(any(EventDataBatch.class));
+    }
+
+    /**
+     * test the case that the second event is too large for one new batch
+     */
+    @Test
+    void testSendAsyncForMessagesWithTheSecondEventTooLargeForOneNewBatch() {
+        EventDataBatch eventDataBatch = mock(EventDataBatch.class);
+
+        when(this.mockProducerClient.createBatch(any(CreateBatchOptions.class)))
+                .thenReturn(Mono.just(eventDataBatch));
+        when(eventDataBatch.tryAdd(any(EventData.class))).thenReturn(true, false, false).thenReturn(true, true, true)
+                 ;
+        when(eventDataBatch.getCount()).thenReturn(5);
+        List<String> messagesList = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            messagesList.add("abcde");
+        }
+        List<Message<String>> messages =
+                messagesList.stream().map((Function<String, GenericMessage<String>>) GenericMessage::new).collect(Collectors.toList());
+
+        this.eventHubsTemplate.sendAsync(this.destination, messages, null).doOnError(ex -> {
+            System.out.println("do on Error" + ex.getMessage());
+            ex.printStackTrace();
+        }).doOnSuccess(t -> {
+            System.out.println("do on success:" + t);
+        }).block(Duration.ofSeconds(10));
+        verify(this.mockProducerClient, times(2)).send(any(EventDataBatch.class));
+    }
 }
