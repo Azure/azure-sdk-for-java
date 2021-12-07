6f34016b406a84e0af953c8db8a46ebc4abff442
Srikanta <srnagar@microsoft.com>
2021-12-07 15:38:05 -0800

    remove netty-all

--- sdk/aot/azure-aot-graalvm-support-netty/src/main/java/module-info.java
+++ sdk/aot/azure-aot-graalvm-support-netty/src/main/java/module-info.java
@@ -3,9 +3,10 @@
 
 module com.azure.aot.graalvm.support.netty {
     requires com.azure.aot.graalvm.support;
-    requires io.netty.transport;
-    requires io.netty.codec;
     requires io.netty.buffer;
-    requires io.netty.handler;
+    requires io.netty.codec;
     requires io.netty.codec.http;
+    requires io.netty.common;
+    requires io.netty.handler;
+    requires io.netty.transport;
 }
