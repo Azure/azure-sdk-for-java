10027478fd5896cf4e7697cb80a66c24d45b0afc
Fabian Meiswinkel <fabian@meiswinkel.com>
2021-12-09 00:45:48 +0000

    Added a unit test to validate key is not persisted in view definition

--- sdk/cosmos/azure-cosmos-spark_3-1_2-12/src/main/scala/com/azure/cosmos/spark/CosmosCatalog.scala
+++ sdk/cosmos/azure-cosmos-spark_3-1_2-12/src/main/scala/com/azure/cosmos/spark/CosmosCatalog.scala
@@ -799,7 +799,9 @@ class CosmosCatalog
 
   private def redactAuthInfo(cfg: Map[String, String]): Map[String, String] = {
     cfg.filter((kvp) => !CosmosConfigNames.AccountEndpoint.equalsIgnoreCase(kvp._1) &&
-      !CosmosConfigNames.AccountKey.equalsIgnoreCase(kvp._1)
+      !CosmosConfigNames.AccountKey.equalsIgnoreCase(kvp._1) &&
+      !kvp._1.toLowerCase.contains(CosmosConfigNames.AccountEndpoint.toLowerCase()) &&
+      !kvp._1.toLowerCase.contains(CosmosConfigNames.AccountKey.toLowerCase())
     )
   }
 
@@ -904,105 +906,6 @@ class CosmosCatalog
       props.asScala.toMap
     }
   }
-
-  private object ViewDefinitionEnvelopeSerializer {
-    private val idPropertyName: String = "id"
-    private val viewsPropertyName: String = "views"
-    private val keyPropertyName: String = "k"
-    private val valuePropertyName: String = "v"
-    private val databasePropertyName: String = "database"
-    private val viewPropertyName: String = "name"
-    private val customSchemaPropertyName: String = "customSchema"
-    private val optionsPropertyName: String = "options"
-    val V1Identifier: String = "spark.cosmos.catalog.viewDefinitions.v1"
-    private val objectMapper = new ObjectMapper()
-
-    def fromJson(json: String): Array[ViewDefinition] = {
-      val parsedNode = objectMapper.readTree(json)
-      if (isValidJson(parsedNode)) {
-        val viewArrayNode = parsedNode.get(viewsPropertyName).asInstanceOf[ArrayNode]
-
-        val views = new scala.collection.mutable.ArrayBuffer[ViewDefinition]()
-        for (i <- 0 until viewArrayNode.size()) {
-          val viewNode = viewArrayNode.get(i)
-          val databaseName = viewNode.get(databasePropertyName).asText()
-          val name = viewNode.get(viewPropertyName).asText()
-          val customSchema: Option[StructType] = if (viewNode.has(customSchemaPropertyName)) {
-            Some(StructType.fromDDL(viewNode.get(customSchemaPropertyName).asText))
-          } else {
-            None
-          }
-          val options = if (viewNode.has(optionsPropertyName)) {
-            val optionsArrayNode = viewNode.get(optionsPropertyName).asInstanceOf[ArrayNode]
-            val temp = new scala.collection.mutable.HashMap[String, String]
-            for (i <- 0 until optionsArrayNode.size) {
-              val kvpNode = optionsArrayNode.get(i)
-              temp += (kvpNode.get(keyPropertyName).asText -> kvpNode.get(valuePropertyName).asText)
-            }
-
-            temp.toMap
-          } else {
-            Map.empty[String, String]
-          }
-
-          views += ViewDefinition(databaseName, name, customSchema, options)
-        }
-        views.toArray
-      } else {
-        val message = s"Unable to deserialize view definitions '$json'."
-        throw new IllegalArgumentException(message)
-      }
-    }
-
-    def toJson(viewDefinitions: Array[ViewDefinition]): String = {
-      val root = objectMapper.createObjectNode()
-      root.put(idPropertyName, V1Identifier)
-      val viewsArray: ArrayNode = root.putArray(viewsPropertyName)
-
-      for (viewDefinition <- viewDefinitions) {
-
-        val view = objectMapper.createObjectNode()
-        view.put(databasePropertyName, viewDefinition.databaseName)
-        view.put(viewPropertyName, viewDefinition.viewName)
-
-        viewDefinition.userProvidedSchema match {
-          case Some(schema) => view.put(customSchemaPropertyName, schema.toDDL)
-          case None =>
-        }
-
-        if (viewDefinition.options.size > 0) {
-          val options = view.putArray(optionsPropertyName)
-          viewDefinition.options.foreach((kvp) => {
-            val option = objectMapper.createObjectNode()
-            option.put(keyPropertyName, kvp._1)
-            option.put(valuePropertyName, kvp._2)
-            options.add(option)
-          })
-        }
-
-        viewsArray.add(view)
-      }
-
-      objectMapper.writeValueAsString(root)
-    }
-
-    private[this] def isValidJson(parsedNode: JsonNode): Boolean = {
-      parsedNode != null &&
-        parsedNode.isObject &&
-        parsedNode.get(idPropertyName) != null &&
-        parsedNode.get(idPropertyName).asText("") == V1Identifier &&
-        parsedNode.get(viewsPropertyName) != null &&
-        parsedNode.get(viewsPropertyName).isArray
-    }
-  }
-
-  private case class ViewDefinition
-  (
-    databaseName: String,
-    viewName: String,
-    userProvidedSchema: Option[StructType],
-    options: Map[String, String]
-  )
 }
 // scalastyle:on multiple.string.literals
 // scalastyle:on number.of.methods
