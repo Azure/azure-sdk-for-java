8b03c6e20c1df1dbb5149936d4d31e11d6fa35be
Rujun Chen <rujche@microsoft.com>
2021-12-14 14:59:04 +0800

    Change the package name of BlobServiceUserAgentTest.

--- sdk/spring/spring-cloud-azure-autoconfigure/src/test/java/com/azure/spring/cloud/autoconfigure/user/agent/http/storage/BlobServiceUserAgentTest.java
+++ sdk/spring/spring-cloud-azure-autoconfigure/src/test/java/com/azure/spring/cloud/autoconfigure/user/agent/http/storage/BlobServiceUserAgentTest.java
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.azure.spring.cloud.autoconfigure.user.agent.http;
+package com.azure.spring.cloud.autoconfigure.user.agent.http.storage;
 
 import com.azure.core.http.policy.HttpPipelinePolicy;
 import com.azure.core.http.policy.UserAgentPolicy;
