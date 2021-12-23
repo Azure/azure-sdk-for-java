65b60419a3890f15ee347f63e36093dc44f5a09d
Muyao Feng <v-muyaofeng@microsoft.com>
2021-12-23 08:40:20 +0800

    add script to skip unsupported module 3

--- sdk/spring/scripts/ci_exclude_modules.py
+++ sdk/spring/scripts/ci_exclude_modules.py
@@ -11,6 +11,8 @@ def exclude_modules_in_txt():
         for i in unsupported_list:
             pos = content.find(i)
             if pos != -1:
+                print("processing:" + file_path)
+                print("noted unsupported module")
                 content = content[:pos] +'# '+ content[pos:]
                 with open(file_path, "r+", encoding="utf-8") as f:
                     f.writelines(content)
@@ -26,6 +28,8 @@ def exclude_modules_in_pom():
         for i in unsupported_list_for_pom:
             pos = content.find(i)
             if pos != -1:
+                print("processing:" + file_path)
+                print("noted unsupported module")
                 content = content[:pos-8] +'<!--'+ content[pos-8:]
                 content = content[:pos+len(i)+13] +'-->'+ content[pos+len(i)+13:]
                 with open(file_path, "r+", encoding="utf-8") as f:
