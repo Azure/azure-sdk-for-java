e7aeeeac393601361d1fbf22b350ab9f5b1cd125
Alan Zimmer <48699787+alzimmermsft@users.noreply.github.com>
2022-01-04 14:17:44 -0800

    Clean up imports

--- sdk/core/azure-core/src/main/java9/com/azure/core/implementation/ReflectionUtils.java
+++ sdk/core/azure-core/src/main/java9/com/azure/core/implementation/ReflectionUtils.java
@@ -3,13 +3,7 @@
 
 package com.azure.core.implementation;
 
-import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
-import java.lang.reflect.AccessibleObject;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
 
 /**
  * Utility methods that aid in performing reflective operations.
