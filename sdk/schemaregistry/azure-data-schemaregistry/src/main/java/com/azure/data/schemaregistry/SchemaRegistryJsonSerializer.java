26f994f8837e309f1b67dab97a1c98dc2cf2c581
Connie <conniey@microsoft.com>
2022-01-05 08:17:23 -0800

    Moving JsonSerializer to package-private.

--- sdk/schemaregistry/azure-data-schemaregistry/src/main/java/com/azure/data/schemaregistry/SchemaRegistryJsonSerializer.java
+++ sdk/schemaregistry/azure-data-schemaregistry/src/main/java/com/azure/data/schemaregistry/SchemaRegistryJsonSerializer.java
@@ -1,4 +1,7 @@
-package com.azure.data.schemaregistry.implementation;
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.data.schemaregistry;
 
 import com.azure.core.http.HttpHeaders;
 import com.azure.core.util.serializer.CollectionFormat;
@@ -10,9 +13,15 @@ import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.List;
 
-public class SchemaRegistryJsonSerializer implements SerializerAdapter {
+/**
+ * Serializer for Schema Registry.
+ */
+class SchemaRegistryJsonSerializer implements SerializerAdapter {
     private final SerializerAdapter adapter = JacksonAdapter.createDefaultSerializerAdapter();
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String serialize(Object object, SerializerEncoding encoding) throws IOException {
         if (encoding != SerializerEncoding.JSON) {
@@ -26,22 +35,34 @@ public class SchemaRegistryJsonSerializer implements SerializerAdapter {
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String serializeRaw(Object object) {
         return adapter.serializeRaw(object);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String serializeList(List<?> list, CollectionFormat format) {
         return adapter.serializeList(list, format);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public <T> T deserialize(String value, Type type, SerializerEncoding encoding) throws IOException {
         //TODO (conniey): Test this.
         return adapter.deserialize(value, type, encoding);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public <T> T deserialize(HttpHeaders headers, Type type) throws IOException {
         return adapter.deserialize(headers, type);
