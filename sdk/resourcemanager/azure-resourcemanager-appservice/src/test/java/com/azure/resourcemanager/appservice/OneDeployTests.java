213ec629f5b655044c9894ebe792627b0bef4968
Weidong Xu <weidxu@microsoft.com>
2022-01-10 13:22:08 +0800

    fix test

--- sdk/resourcemanager/azure-resourcemanager-appservice/src/test/java/com/azure/resourcemanager/appservice/OneDeployTests.java
+++ sdk/resourcemanager/azure-resourcemanager-appservice/src/test/java/com/azure/resourcemanager/appservice/OneDeployTests.java
@@ -9,7 +9,6 @@ import com.azure.core.http.HttpResponse;
 import com.azure.core.management.AzureEnvironment;
 import com.azure.core.management.Region;
 import com.azure.core.test.annotation.DoNotRecord;
-import com.azure.core.util.Configuration;
 import com.azure.core.util.serializer.JacksonAdapter;
 import com.azure.core.util.serializer.SerializerAdapter;
 import com.azure.core.util.serializer.SerializerEncoding;
@@ -19,23 +18,43 @@ import com.azure.resourcemanager.appservice.models.DeployType;
 import com.azure.resourcemanager.appservice.models.JavaVersion;
 import com.azure.resourcemanager.appservice.models.KuduDeploymentResult;
 import com.azure.resourcemanager.appservice.models.PricingTier;
+import com.azure.resourcemanager.appservice.models.RuntimeStack;
 import com.azure.resourcemanager.appservice.models.WebApp;
 import com.azure.resourcemanager.appservice.models.WebContainer;
 import com.azure.resourcemanager.resources.fluentcore.utils.ResourceManagerUtils;
+import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.time.Duration;
 import java.util.Map;
 
 public class OneDeployTests extends AppServiceTest {
 
+    private static final String GATEWAY_JAR_URL = "https://github.com/weidongxu-microsoft/azure-sdk-for-java-management-tests/raw/master/spring-cloud/gateway.jar";
+
     @Test
     @DoNotRecord(skipInPlayback = true)
-    public void canDeployZip() throws Exception {
+    public void canDeployZip() {
         String webAppName1 = generateRandomResourceName("webapp", 10);
-        WebApp webApp1 = createWebApp(webAppName1);
+
+        WebApp webApp1 =
+            appServiceManager
+                .webApps()
+                .define(webAppName1)
+                .withRegion(Region.US_WEST3)
+                .withNewResourceGroup(rgName)
+                .withNewWindowsPlan(PricingTier.STANDARD_S1)
+                .withJavaVersion(JavaVersion.JAVA_8_NEWEST)
+                .withWebContainer(WebContainer.TOMCAT_8_5_NEWEST)
+                .withHttpsOnly(true)
+                .create();
 
         File zipFile = new File(OneDeployTests.class.getResource("/webapps.zip").getPath());
         webApp1.deploy(DeployType.ZIP, zipFile);
@@ -52,36 +71,58 @@ public class OneDeployTests extends AppServiceTest {
     @DoNotRecord(skipInPlayback = true)
     public void canPushDeployZip() throws Exception {
         String webAppName1 = generateRandomResourceName("webapp", 10);
-        WebApp webApp1 = createWebApp(webAppName1);
 
-        File zipFile = new File(OneDeployTests.class.getResource("/webapps.zip").getPath());
+        WebApp webApp1 =
+            appServiceManager
+                .webApps()
+                .define(webAppName1)
+                .withRegion(Region.US_WEST3)
+                .withNewResourceGroup(rgName)
+                .withNewLinuxPlan(PricingTier.STANDARD_S1)
+                .withBuiltInImage(RuntimeStack.JAVA_11_JAVA11)
+                .withHttpsOnly(true)
+                .create();
+
+        // deploy
+        File jarFile = new File("gateway.jar");
+        if (!jarFile.exists()) {
+            HttpURLConnection connection = (HttpURLConnection) new URL(GATEWAY_JAR_URL).openConnection();
+            connection.connect();
+            try (InputStream inputStream = connection.getInputStream();
+                 OutputStream outputStream = new FileOutputStream(jarFile)) {
+                IOUtils.copy(inputStream, outputStream);
+            }
+            connection.disconnect();
+        }
         KuduDeploymentResult deployResult =
-            webApp1.pushDeploy(DeployType.ZIP, zipFile, new DeployOptions().withTrackDeployment(true));
+            webApp1.pushDeploy(DeployType.JAR, jarFile, new DeployOptions().withTrackDeployment(true));
 
         Assertions.assertNotNull(deployResult.deploymentId());
 
+        // poll deployment status
         String deploymentStatusUrl = AzureEnvironment.AZURE.getResourceManagerEndpoint()
-            + "subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Web/sites/{name}/deploymentStatus/{deploymentId}";
+            + "subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Web/sites/{name}/deploymentStatus/{deploymentId}?api-version=2021-03-01";
         deploymentStatusUrl = deploymentStatusUrl
-            .replace("{subscriptionId}", Configuration.getGlobalConfiguration().get(Configuration.PROPERTY_AZURE_SUBSCRIPTION_ID))
+            .replace("{subscriptionId}", appServiceManager.subscriptionId())
             .replace("{resourceGroupName}", rgName)
             .replace("{name}", webAppName1)
             .replace("{deploymentId}", deployResult.deploymentId());
 
-        // wait for RUNTIME_SUCCESSFUL
+        // wait for RuntimeSuccessful
         String buildStatus = null;
         SerializerAdapter serializerAdapter = JacksonAdapter.createDefaultSerializerAdapter();
         while (!"RuntimeSuccessful".equals(buildStatus)) {
-            ResourceManagerUtils.sleep(Duration.ofSeconds(5));
+            ResourceManagerUtils.sleep(Duration.ofSeconds(10));
 
             HttpRequest request = new HttpRequest(HttpMethod.GET, deploymentStatusUrl);
             HttpResponse response = appServiceManager.httpPipeline().send(request).block();
-            Assertions.assertEquals(200, response.getStatusCode());
+            Assertions.assertTrue(response.getStatusCode() / 100 == 2);
 
             String body = response.getBodyAsString().block();
             Assertions.assertNotNull(body);
             Map<String, Object> bodyJson = serializerAdapter.deserialize(body,
-                new TypeReference<Map<String, Object>>() {}.getJavaType(),
+                new TypeReference<Map<String, Object>>() {
+                }.getJavaType(),
                 SerializerEncoding.JSON);
             Assertions.assertNotNull(bodyJson);
             if (bodyJson.containsKey("properties")) {
@@ -90,24 +131,12 @@ public class OneDeployTests extends AppServiceTest {
                     buildStatus = (String) propertiesJson.get("status");
                 }
             }
+
+            if (buildStatus != null && buildStatus.contains("Failed")) {
+                // failed
+                break;
+            }
         }
-
-        String response = curl("https://" + webAppName1 + ".azurewebsites.net/" + "helloworld/").getValue();
-        Assertions.assertTrue(response.contains("Hello"));
-    }
-
-    private WebApp createWebApp(String webAppName1) {
-        WebApp webApp1 =
-            appServiceManager
-                .webApps()
-                .define(webAppName1)
-                .withRegion(Region.US_CENTRAL)
-                .withNewResourceGroup(rgName)
-                .withNewWindowsPlan(PricingTier.BASIC_B1)
-                .withJavaVersion(JavaVersion.JAVA_8_NEWEST)
-                .withWebContainer(WebContainer.TOMCAT_8_5_NEWEST)
-                .withHttpsOnly(true)
-                .create();
-        return webApp1;
+        Assertions.assertEquals("RuntimeSuccessful", buildStatus);
     }
 }
