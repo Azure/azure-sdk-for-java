f970ade207858f271ad77daf027035665598d0f3
Haoling Dong <haolingdong@microsoft.com>
2022-01-11 16:35:59 +0800

    Update tests to avoid using DEFAULT_SCOPE

--- sdk/deviceupdate/azure-iot-deviceupdate/src/test/java/com/azure/iot/deviceupdate/ManagementClientTests.java
+++ sdk/deviceupdate/azure-iot-deviceupdate/src/test/java/com/azure/iot/deviceupdate/ManagementClientTests.java
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 package com.azure.iot.deviceupdate;
 
+import com.azure.core.credential.AccessToken;
 import com.azure.core.credential.TokenCredential;
 import com.azure.core.http.ContentType;
 import com.azure.core.http.HttpClient;
@@ -18,52 +19,34 @@ import com.azure.core.test.TestBase;
 import com.azure.core.test.TestMode;
 import com.azure.core.util.BinaryData;
 import com.azure.identity.AzureCliCredentialBuilder;
+import com.azure.identity.DefaultAzureCredentialBuilder;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+
+import java.time.OffsetDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ManagementClientTests extends TestBase {
-    private static final String DEFAULT_SCOPE = "https://api.adu.microsoft.com/.default";
 
     private ManagementAsyncClient createClient() {
-        TokenCredential credentials;
-        HttpClient httpClient;
-        HttpPipelinePolicy recordingPolicy = null;
-        HttpPipeline httpPipeline;
-
-        HttpHeaders headers = new HttpHeaders().put("Accept", ContentType.APPLICATION_JSON);
-        AddHeadersPolicy addHeadersPolicy = new AddHeadersPolicy(headers);
-
-        if (getTestMode() != TestMode.PLAYBACK) {
-            // Record & Live
-            credentials = new AzureCliCredentialBuilder().build();
-            httpClient = HttpClient.createDefault();
-            if (getTestMode() == TestMode.RECORD) {
-                recordingPolicy = interceptorManager.getRecordPolicy();
-            }
-            BearerTokenAuthenticationPolicy bearerTokenAuthenticationPolicy =
-                new BearerTokenAuthenticationPolicy(credentials, DEFAULT_SCOPE);
-            httpPipeline = new HttpPipelineBuilder()
-                .httpClient(httpClient)
-                .policies(bearerTokenAuthenticationPolicy, addHeadersPolicy, recordingPolicy)
-                .build();
+        DeviceUpdateClientBuilder builder =
+            new DeviceUpdateClientBuilder()
+                .endpoint(TestData.ACCOUNT_ENDPOINT)
+                .instanceId(TestData.INSTANCE_ID)
+                .httpClient(HttpClient.createDefault())
+                .httpLogOptions(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BODY_AND_HEADERS));
+        if (getTestMode() == TestMode.PLAYBACK) {
+            builder.httpClient(interceptorManager.getPlaybackClient())
+                .credential(request -> Mono.just(new AccessToken("this_is_a_token", OffsetDateTime.MAX)));
+        } else if (getTestMode() == TestMode.RECORD) {
+            builder.addPolicy(interceptorManager.getRecordPolicy())
+                .credential(new DefaultAzureCredentialBuilder().build());
+        } else if (getTestMode() == TestMode.LIVE) {
+            builder.credential(new DefaultAzureCredentialBuilder().build());
         }
-        else {
-            // Playback
-            httpClient = interceptorManager.getPlaybackClient();
-            httpPipeline = new HttpPipelineBuilder()
-                .httpClient(httpClient)
-                .policies(addHeadersPolicy)
-                .build();
-        }
-
-        return new DeviceUpdateClientBuilder()
-            .endpoint(TestData.ACCOUNT_ENDPOINT)
-            .instanceId(TestData.INSTANCE_ID)
-            .pipeline(httpPipeline)
-            .httpLogOptions(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BODY_AND_HEADERS))
-            .buildManagementAsyncClient();
+        return builder.buildManagementAsyncClient();
     }
 
     @Test
