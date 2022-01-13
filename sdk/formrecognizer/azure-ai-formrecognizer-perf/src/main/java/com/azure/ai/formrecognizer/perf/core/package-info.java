1506ddb964ba9f144558857f841162f7a8fed12b
samvaity <savaity@microsoft.com>
2022-01-12 16:33:07 -0800

    add missing javadoc package info

---
+++ sdk/formrecognizer/azure-ai-formrecognizer-perf/src/main/java/com/azure/ai/formrecognizer/perf/core/package-info.java
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+/**
+ * Package contains the base class for the Form Recognizer performance tests.
+ */
+package com.azure.ai.formrecognizer.perf.core;
