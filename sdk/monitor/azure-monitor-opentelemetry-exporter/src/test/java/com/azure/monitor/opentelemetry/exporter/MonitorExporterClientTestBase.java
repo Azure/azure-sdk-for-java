29fabaab70b79f6f871e054350043740f1946945
kryalama <66494519+kryalama@users.noreply.github.com>
2022-01-23 21:23:11 -0800

    Fix live tests failures (#26082)
    * This is a blank commit
    
    * This is a blank commit
    
    * please ignore sample commit
    
    * add redirect policy to test builders
    
    * remove redundant redirect policy
    
    * add temporary status code 307 support to redirect policy
    
    * add 307 support
    
    * add role assignment to test azure resource group
    
    * fix template json
    
    * fix name issue in json
    
    * remove calls to add before and after retry policies
    
    * remove aad auth tests
    
    * fix spotbugs and undo auth related changes
    
    * remove unused imports
    
    * address comments
    
    * remove unused imports

--- sdk/monitor/azure-monitor-opentelemetry-exporter/src/test/java/com/azure/monitor/opentelemetry/exporter/MonitorExporterClientTestBase.java
+++ sdk/monitor/azure-monitor-opentelemetry-exporter/src/test/java/com/azure/monitor/opentelemetry/exporter/MonitorExporterClientTestBase.java
@@ -3,14 +3,12 @@
 
 package com.azure.monitor.opentelemetry.exporter;
 
-import com.azure.core.credential.TokenCredential;
 import com.azure.core.http.HttpClient;
 import com.azure.core.http.HttpPipeline;
 import com.azure.core.http.HttpPipelineBuilder;
 import com.azure.core.test.TestBase;
 import com.azure.core.test.TestMode;
 import com.azure.core.util.Configuration;
-import com.azure.identity.ClientSecretCredentialBuilder;
 import com.azure.monitor.opentelemetry.exporter.implementation.models.MonitorBase;
 import com.azure.monitor.opentelemetry.exporter.implementation.models.MonitorDomain;
 import com.azure.monitor.opentelemetry.exporter.implementation.models.RequestData;
@@ -41,36 +39,10 @@ public class MonitorExporterClientTestBase extends TestBase {
 
         HttpPipeline httpPipeline = new HttpPipelineBuilder()
             .httpClient(httpClient)
-            .policies(interceptorManager.getRecordPolicy()).build();
+            .policies(new AzureMonitorRedirectPolicy(), interceptorManager.getRecordPolicy())
+            .build();
 
         return new AzureMonitorExporterBuilder().pipeline(httpPipeline);
-    }
-
-    AzureMonitorExporterBuilder getClientBuilderWithAuthentication() {
-        TokenCredential credential = null;
-        HttpClient httpClient;
-        if (getTestMode() == TestMode.RECORD || getTestMode() == TestMode.LIVE) {
-            httpClient = HttpClient.createDefault();
-            credential =
-                new ClientSecretCredentialBuilder()
-                    .tenantId(System.getenv("AZURE_TENANT_ID"))
-                    .clientSecret(System.getenv("AZURE_CLIENT_SECRET"))
-                    .clientId(System.getenv("AZURE_CLIENT_ID"))
-                    .build();
-        } else {
-            httpClient = interceptorManager.getPlaybackClient();
-        }
-
-        if (credential != null) {
-            return new AzureMonitorExporterBuilder()
-                .credential(credential)
-                .httpClient(httpClient)
-                .addPolicy(interceptorManager.getRecordPolicy());
-        } else {
-            return new AzureMonitorExporterBuilder()
-                .httpClient(httpClient)
-                .addPolicy(interceptorManager.getRecordPolicy());
-        }
     }
 
     List<TelemetryItem> getAllInvalidTelemetryItems() {
