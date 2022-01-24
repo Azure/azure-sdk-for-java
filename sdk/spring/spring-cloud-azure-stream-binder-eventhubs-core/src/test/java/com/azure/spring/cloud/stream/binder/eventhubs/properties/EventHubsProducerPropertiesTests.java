7ce5a6e6034b6de97fef55e355e4b5498f79f13a
Moary Chen <moary@126.com>
2022-01-24 20:59:43 +0800

    Add UTs for binder-eventhubs-core

---
+++ sdk/spring/spring-cloud-azure-stream-binder-eventhubs-core/src/test/java/com/azure/spring/cloud/stream/binder/eventhubs/properties/EventHubsProducerPropertiesTests.java
@@ -0,0 +1,89 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.spring.cloud.stream.binder.eventhubs.properties;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class EventHubsProducerPropertiesTests {
+
+    private static final String CONNECTION_STRING = "Endpoint=sb://test.servicebus.windows.net/;SharedAccessKeyName=test;SharedAccessKey=accessKey;EntityPath=testeh";
+
+    private EventHubsProducerProperties producerProperties;
+
+    @BeforeEach
+    void beforeEach() {
+        producerProperties = new EventHubsProducerProperties();
+    }
+
+    @Test
+    void sendTimeoutDefaultsTo10000() {
+        assertEquals(10000, producerProperties.getSendTimeout());
+    }
+
+    @Test
+    void customSendTimeout() {
+        producerProperties.setSendTimeout(15000);
+        assertEquals(15000, producerProperties.getSendTimeout());
+    }
+
+    @Test
+    void syncDefaultsToFalse() {
+        assertEquals(false, producerProperties.isSync());
+    }
+
+    @Test
+    void customSync() {
+        producerProperties.setSync(true);
+        assertEquals(true, producerProperties.isSync());
+    }
+
+    @Test
+    void domainNameDefaultsToFalse() {
+        assertEquals("servicebus.windows.net", producerProperties.getDomainName());
+    }
+
+    @Test
+    void customDomainName() {
+        producerProperties.setDomainName("new.servicebus.windows.net");
+        assertEquals("new.servicebus.windows.net", producerProperties.getDomainName());
+    }
+
+    @Test
+    void getFqnWhenNamespaceIsNull() {
+        producerProperties.setConnectionString(CONNECTION_STRING);
+        assertEquals("test.servicebus.windows.net", producerProperties.getFullyQualifiedNamespace());
+    }
+
+    @Test
+    void getFqnWhenNamespaceIsNotNull() {
+        producerProperties.setNamespace("dev");
+        assertEquals("dev.servicebus.windows.net", producerProperties.getFullyQualifiedNamespace());
+    }
+
+    @Test
+    void getFqnReturnNullWhenNamespaceAndConnectionStringAreNull() {
+        assertNull(producerProperties.getFullyQualifiedNamespace());
+    }
+
+    @Test
+    void getEventHubNameWhenNamespaceIsNull() {
+        producerProperties.setConnectionString(CONNECTION_STRING);
+        assertEquals("testeh", producerProperties.getEventHubName());
+    }
+
+    @Test
+    void getEventHubNameWhenNamespaceIsNotNull() {
+        producerProperties.setEventHubName("test");
+        assertEquals("test", producerProperties.getEventHubName());
+    }
+
+    @Test
+    void getEventHubNameReturnNullWhenNamespaceAndConnectionStringAreNull() {
+        assertNull(producerProperties.getFullyQualifiedNamespace());
+    }
+}
