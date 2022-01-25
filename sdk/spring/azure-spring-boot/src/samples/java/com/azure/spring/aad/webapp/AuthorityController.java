dfecfcc7d7737d5f7e22fe6dd4fffa77ef7ea705
Alan Zimmer <48699787+alzimmermsft@users.noreply.github.com>
2022-01-25 14:54:25 -0800

    Remove embedme (#26639)
    Remove embedme

--- sdk/spring/azure-spring-boot/src/samples/java/com/azure/spring/aad/webapp/AuthorityController.java
+++ sdk/spring/azure-spring-boot/src/samples/java/com/azure/spring/aad/webapp/AuthorityController.java
@@ -10,10 +10,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AuthorityController {
+    // BEGIN: readme-sample-admin
     @GetMapping("Admin")
     @ResponseBody
     @PreAuthorize("hasAuthority('APPROLE_Admin')")
     public String admin() {
         return "Admin message";
     }
+    // END: readme-sample-admin
 }
