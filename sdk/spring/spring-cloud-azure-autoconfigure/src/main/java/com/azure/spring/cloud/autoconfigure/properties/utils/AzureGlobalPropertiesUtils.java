59b0a2d6385098105d0c3618769f82d7b7630b59
Xiaolu Dai <xiada@microsoft.com>
2022-01-29 17:20:29 +0800

    fix issues in kv envpostprocessor

---
+++ sdk/spring/spring-cloud-azure-autoconfigure/src/main/java/com/azure/spring/cloud/autoconfigure/properties/utils/AzureGlobalPropertiesUtils.java
@@ -0,0 +1,60 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.spring.cloud.autoconfigure.properties.utils;
+
+import com.azure.spring.cloud.autoconfigure.properties.AzureGlobalProperties;
+import com.azure.spring.core.aware.ClientAware;
+import com.azure.spring.core.aware.ProxyAware;
+import com.azure.spring.core.aware.RetryAware;
+import com.azure.spring.core.properties.AzureProperties;
+import com.azure.spring.core.util.AzurePropertiesUtils;
+import org.springframework.beans.BeanUtils;
+
+/**
+ * Util class for processor {@link AzureGlobalProperties}.
+ */
+public final class AzureGlobalPropertiesUtils {
+
+    private AzureGlobalPropertiesUtils() {
+
+    }
+
+    /**
+     * Load the default value to an Azure Service properties from the global Azure properties.
+     *
+     * @param source The global Azure properties.
+     * @param target The properties of an Azure Service, such as Event Hubs properties. Some common components of the
+     *               service's properties have default value as set to the global properties. For example, the proxy of
+     *               the Event Hubs properties takes the proxy set to the global Azure properties as default.
+     * @param <T> The type of the properties of an Azure Service.
+     * @return The Azure Service's properties.
+     */
+    public static <T extends AzureProperties> T loadProperties(AzureGlobalProperties source, T target) {
+        AzurePropertiesUtils.copyAzureCommonProperties(source, target);
+
+        if (target.getClient() instanceof ClientAware.HttpClient) {
+            BeanUtils.copyProperties(source.getClient().getHttp(), target.getClient());
+
+            ClientAware.HttpClient targetClient = (ClientAware.HttpClient) target.getClient();
+            BeanUtils.copyProperties(source.getClient().getHttp().getLogging(), targetClient.getLogging());
+            targetClient.getLogging().getAllowedHeaderNames().addAll(source.getClient().getHttp().getLogging().getAllowedHeaderNames());
+            targetClient.getLogging().getAllowedQueryParamNames().addAll(source.getClient().getHttp().getLogging().getAllowedQueryParamNames());
+        }
+
+        if (target.getClient() instanceof ClientAware.AmqpClient) {
+            BeanUtils.copyProperties(source.getClient().getAmqp(), target.getClient());
+        }
+
+        if (target.getProxy() instanceof ProxyAware.HttpProxy) {
+            BeanUtils.copyProperties(source.getProxy().getHttp(), target.getProxy());
+        }
+
+        if (target.getRetry() instanceof RetryAware.HttpRetry) {
+            BeanUtils.copyProperties(source.getRetry().getHttp(), target.getRetry());
+        }
+
+        return target;
+    }
+
+}
