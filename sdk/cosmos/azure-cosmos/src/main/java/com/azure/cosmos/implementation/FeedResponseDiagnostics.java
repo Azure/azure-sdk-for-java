fecd738368ee0860f2f1992c452c82440c7752b9
Fabian Meiswinkel <fabian@meiswinkel.com>
2022-02-16 15:31:14 +0100

    Update FeedResponseDiagnostics.java

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/FeedResponseDiagnostics.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/FeedResponseDiagnostics.java
@@ -8,7 +8,6 @@ import com.azure.cosmos.implementation.query.metrics.QueryMetricsTextWriter;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
-
 import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
