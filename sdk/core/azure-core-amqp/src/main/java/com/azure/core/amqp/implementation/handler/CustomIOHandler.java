e2882108276fbaa564641193fed416d5e886d217
Connie <conniey@microsoft.com>
2022-03-01 10:47:34 -0800

    Remove onUnhandled in CustomIOHandler

--- sdk/core/azure-core-amqp/src/main/java/com/azure/core/amqp/implementation/handler/CustomIOHandler.java
+++ sdk/core/azure-core-amqp/src/main/java/com/azure/core/amqp/implementation/handler/CustomIOHandler.java
@@ -33,16 +33,4 @@ public class CustomIOHandler extends IOHandler {
             transport.unbind();
         }
     }
-
-    @Override
-    public void onUnhandled(Event event) {
-        // logger.verbose("Unhandled event: {}, {}", event.getEventType(), event);
-
-        // There appears to be some race condition where it's possible to get a null selector key in proton-j.
-        try {
-            super.onUnhandled(event);
-        } catch (NullPointerException e) {
-            logger.error("Exception occurred when handling event in super.", e);
-        }
-    }
 }
