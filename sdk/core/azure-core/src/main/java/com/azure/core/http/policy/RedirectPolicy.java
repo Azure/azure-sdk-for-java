7b6b02996d7af3922dfaaaa37e6c3a8bb6a3a42f
Kamil Sobol <kasobol@microsoft.com>
2022-03-02 15:01:02 -0800

    redirect policy.

--- sdk/core/azure-core/src/main/java/com/azure/core/http/policy/RedirectPolicy.java
+++ sdk/core/azure-core/src/main/java/com/azure/core/http/policy/RedirectPolicy.java
@@ -7,6 +7,7 @@ import com.azure.core.http.HttpPipelineCallContext;
 import com.azure.core.http.HttpPipelineNextPolicy;
 import com.azure.core.http.HttpRequest;
 import com.azure.core.http.HttpResponse;
+import com.azure.core.util.CoreUtils;
 import reactor.core.publisher.Mono;
 
 import java.util.HashSet;
@@ -45,6 +46,12 @@ public final class RedirectPolicy implements HttpPipelinePolicy {
         return attemptRedirect(context, next, context.getHttpRequest(), 1, new HashSet<>());
     }
 
+    @Override
+    public HttpResponse processSynchronously(HttpPipelineCallContext context, HttpPipelineNextPolicy next) {
+        // Reset the attemptedRedirectUrls for each individual request.
+        return attemptRedirectSync(context, next, context.getHttpRequest(), 1, new HashSet<>());
+    }
+
     /**
      * Function to process through the HTTP Response received in the pipeline
      * and redirect sending the request with new redirect url.
@@ -62,12 +69,35 @@ public final class RedirectPolicy implements HttpPipelinePolicy {
                 if (redirectStrategy.shouldAttemptRedirect(context, httpResponse, redirectAttempt,
                     attemptedRedirectUrls)) {
                     HttpRequest redirectRequestCopy = redirectStrategy.createRedirectRequest(httpResponse);
-                    return httpResponse.getBody()
-                        .ignoreElements()
+                    return CoreUtils.consumeResponseBodyAsync(httpResponse)
                         .then(attemptRedirect(context, next, redirectRequestCopy, redirectAttempt + 1, attemptedRedirectUrls));
                 } else {
                     return Mono.just(httpResponse);
                 }
             });
+    }
+
+    /**
+     * Function to process through the HTTP Response received in the pipeline
+     * and redirect sending the request with new redirect url.
+     */
+    private HttpResponse attemptRedirectSync(final HttpPipelineCallContext context,
+                                               final HttpPipelineNextPolicy next,
+                                               final HttpRequest originalHttpRequest,
+                                               final int redirectAttempt,
+                                               Set<String> attemptedRedirectUrls) {
+        // make sure the context is not modified during retry, except for the URL
+        context.setHttpRequest(originalHttpRequest.copy());
+
+        HttpResponse httpResponse = next.clone().processSynchronously();
+
+        if (redirectStrategy.shouldAttemptRedirect(context, httpResponse, redirectAttempt,
+            attemptedRedirectUrls)) {
+            HttpRequest redirectRequestCopy = redirectStrategy.createRedirectRequest(httpResponse);
+            CoreUtils.consumeResponseBodySync(httpResponse);
+            return attemptRedirectSync(context, next, redirectRequestCopy, redirectAttempt + 1, attemptedRedirectUrls);
+        } else {
+            return httpResponse;
+        }
     }
 }
