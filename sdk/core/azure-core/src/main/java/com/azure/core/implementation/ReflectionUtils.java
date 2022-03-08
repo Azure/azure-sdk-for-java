77bd73be0e3b1224e5cb988da66272573d821db7
Rick Ley <frley@microsoft.com>
2022-03-08 12:23:02 -0800

    Undid accidental changes

--- sdk/core/azure-core/src/main/java/com/azure/core/implementation/ReflectionUtils.java
+++ sdk/core/azure-core/src/main/java/com/azure/core/implementation/ReflectionUtils.java
@@ -9,11 +9,6 @@ import java.lang.invoke.MethodHandles;
  * Utility methods that aid in performing reflective operations.
  */
 final class ReflectionUtils implements ReflectionUtilsApi {
-    // This lookup is specific to the com.azure.core module, specifically this class.
-    private static final MethodHandles.Lookup LOOKUP = MethodHandles.lookup();
-
-    // Convenience pointer to the com.azure.core module.
-    private static final Module CORE_MODULE = ReflectionUtils.class.getModule();
 
     /**
      * Gets the {@link MethodHandles.Lookup} to use when performing reflective operations.
@@ -32,36 +27,11 @@ final class ReflectionUtils implements ReflectionUtilsApi {
      * @throws Throwable If the underlying reflective calls throw an exception.
      */
     public MethodHandles.Lookup getLookupToUse(Class<?> targetClass) throws Throwable {
-        Module responseModule = targetClass.getModule();
-
-        /*
-         * First check if the response class's module is exported to all unnamed modules. If it is we will use
-         * MethodHandles.publicLookup() which is meant for creating MethodHandle instances for publicly accessible
-         * classes.
-         */
-        if (responseModule.isExported("")) {
-            return MethodHandles.publicLookup();
-        }
-
-        /*
-         * Otherwise, we use the MethodHandles.Lookup which is associated to this (com.azure.core) module, and more
-         * specifically, is tied to this class (ReflectionUtils). But, in order to use this lookup we need to ensure
-         * that the com.azure.core module reads the response class's module as the lookup won't have permissions
-         * necessary to create the MethodHandle instance without it.
-         *
-         * This logic is safe due to the fact that any SDK module calling into this code path will already need to open
-         * to com.azure.core as it needs to perform other reflective operations on classes in the module. Adding the
-         * com.azure.core reads is handling specifically required by MethodHandle.
-         */
-        if (!CORE_MODULE.canRead(responseModule)) {
-            CORE_MODULE.addReads(responseModule);
-        }
-
-        return LOOKUP;
+        return MethodHandles.publicLookup();
     }
 
     public int getJavaImplementationMajorVersion() {
-        return 9;
+        return 8;
     }
 
     ReflectionUtils() {
