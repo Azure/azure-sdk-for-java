d7bc60995634b00a7f4b880520fd4326402bf565
Rujun Chen <rujche@microsoft.com>
2022-03-08 10:41:55 +0800

    Revert use "*" to import a bunch classes.

--- sdk/spring/spring-messaging-azure/src/test/java/com/azure/spring/messaging/implementation/config/EnableAzureMessagingTests.java
+++ sdk/spring/spring-messaging-azure/src/test/java/com/azure/spring/messaging/implementation/config/EnableAzureMessagingTests.java
@@ -24,7 +24,11 @@ import org.springframework.stereotype.Component;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  *
