5b2f7441d194f205874887feb60f3c9ccd718ab9
sima-zhu <sizhu@microsoft.com>
2022-03-08 16:30:20 -0800

    test all get markdown

--- eng/common/scripts/get-markdown-files-from-changed-files.ps1
+++ eng/common/scripts/get-markdown-files-from-changed-files.ps1
@@ -6,29 +6,11 @@ param (
 )
 
 $gitDiffChanges = Join-Path $PSScriptRoot "git-diff-changes.ps1"
-$allMarkdownFiles = & $gitDiffChanges -IncludeRegex "*.md"
-# Scan all markdowns and find if it contains the deleted or renamed files.
-$markdownContainLinks = @()
-foreach ($f in $allMarkdownFiles) {
-  $filePath = $f.FullName
-  $content = Get-Content -Path $filePath -Raw
-  foreach ($l in $relativePathLinks) {
-    if ($content -match $l) {
-      $markdownContainLinks += $filePath
-      break
-    }
-  }
-}
+$allMarkdownFiles = & $gitDiffChanges -IncludeRegex '*.md'
 
-# Convert markdowns path of the PR to absolute path.
-$adjustedReadmes = $changedMarkdowns | Foreach-Object { Resolve-Path $_ }
-$markdownContainLinks += $adjustedReadmes
-
-# Get rid of any duplicated ones.
-$allMarkdowns = [string[]]($markdownContainLinks | Sort-Object | Get-Unique)
 
 Write-Host "Here are all markdown files we need to check based on the changed files:"
-foreach ($file in $allMarkdowns) {
+foreach ($file in $allMarkdownFiles ) {
   Write-Host "    $file"
 }
-return $allMarkdowns
+return $allMarkdownFiles 
