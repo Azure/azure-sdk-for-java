50863ba9c92a26ecc6d3d53f98225b85825208a2
sima-zhu <sizhu@microsoft.com>
2022-03-09 11:23:05 -0800

    print more to test

--- eng/common/scripts/git-diff-changes.ps1
+++ eng/common/scripts/git-diff-changes.ps1
@@ -10,12 +10,18 @@
   The branch committish PR targets to merge into.
   .PARAMETER IncludeRegex
   The regex of included files.
+  .PARAMETER ExcludeMode
+  The filter type A(a)dd, D(d)elete, R(r)ename, U(u)pate. 
+  E.g. 'ad' means filter out the newly added file and deleted file 
+  E.g. '' means no filter on file mode.
+  .PARAMETER FilterType
 #>
 [CmdletBinding()]
 param (
   [string] $SourceCommittish = "${env:BUILD_SOURCEVERSION}",
   [string] $TargetCommittish = ("origin/${env:SYSTEM_PULLREQUEST_TARGETBRANCH}" -replace "refs/heads/"),
-  [string] $IncludeRegex = ""
+  [string] $IncludeRegex = "",
+  [string] $FilterType = 'd'
 )
 
 # If ${env:SYSTEM_PULLREQUEST_TARGETBRANCH} is empty, then return empty.
@@ -23,8 +29,18 @@ if ($TargetCommittish -eq "origin/") {
     Write-Host "There is no target branch passed in. "
     return ""
 } 
+$fileExist = (Test-Path "$PSSCriptRoot/../../eng/AddFile.md")
+$fileExists = (Test-Path "$PSSCriptRoot/../../eng/readme-test.md")
+Write-Host "Check file $PSSCriptRoot/../../eng/AddFile.md.  Exist: $fileExist"
+Write-Host "Check file $PSSCriptRoot/../../eng/readme-test.md.  Exist: $fileExists"
+Write-Host "git diff check"
+$fs = git diff origin/main...HEAD --name-only --diff-filter=d
+foreach ($f in $fs) {
+  Write-Host $f.FullName
+}
+Write-Host "-------------------------------------"
 # Git PR diff: https://docs.github.com/pull-requests/collaborating-with-pull-requests/proposing-changes-to-your-work-with-pull-requests/about-comparing-branches-in-pull-requests#three-dot-and-two-dot-git-diff-comparisons
-$command = "git -c core.quotepath=off -c i18n.logoutputencoding=utf-8 diff `"$TargetCommittish...$SourceCommittish`" --name-only --diff-filter=d"
+$command = "git -c core.quotepath=off -c i18n.logoutputencoding=utf-8 diff `"$TargetCommittish...$SourceCommittish`" --name-only --diff-filter=$FilterType"
 if ($IncludeRegex) {
   $command = $command + " -- $IncludeRegex"
 }
