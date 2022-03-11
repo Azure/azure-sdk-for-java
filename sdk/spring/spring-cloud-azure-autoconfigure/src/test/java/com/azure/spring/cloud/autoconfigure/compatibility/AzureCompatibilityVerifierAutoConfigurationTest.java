91ad72b444a92af82f1288c63ea176f7b0752c0a
Muyao Feng <92105726+Netyyyy@users.noreply.github.com>
2022-03-11 15:57:02 +0800

    Add ut of compatible (#27616)
    * add  ut

--- sdk/spring/spring-cloud-azure-autoconfigure/src/test/java/com/azure/spring/cloud/autoconfigure/compatibility/AzureCompatibilityVerifierAutoConfigurationTest.java
+++ sdk/spring/spring-cloud-azure-autoconfigure/src/test/java/com/azure/spring/cloud/autoconfigure/compatibility/AzureCompatibilityVerifierAutoConfigurationTest.java
@@ -5,8 +5,11 @@ package com.azure.spring.cloud.autoconfigure.compatibility;
 
 import com.azure.spring.cloud.autoconfigure.implementation.compatibility.AzureSpringBootVersionVerifier;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootVersion;
 import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -28,4 +31,17 @@ public class AzureCompatibilityVerifierAutoConfigurationTest {
             .run(context -> assertThat(context).hasSingleBean(AzureSpringBootVersionVerifier.class));
     }
 
+    @Test
+    void testCompatibleSpringBootVersionCanSet() {
+        String version = SpringBootVersion.getVersion();
+        this.contextRunner
+            .withPropertyValues(
+                String.format("spring.cloud.azure.compatibility-verifier.compatible-boot-versions=%s", version)
+            )
+            .run(context -> {
+                assertThat(context).hasSingleBean(AzureCompatibilityVerifierProperties.class);
+                AzureCompatibilityVerifierProperties verifierProperties = context.getBean(AzureCompatibilityVerifierProperties.class);
+                assertThat(verifierProperties.getCompatibleBootVersions()).isEqualTo(Arrays.asList(version));
+            });
+    }
 }
