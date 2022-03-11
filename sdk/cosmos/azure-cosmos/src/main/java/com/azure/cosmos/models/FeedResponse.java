425a35a087039721b6e2cf814b23b1322b782743
Fabian Meiswinkel <fabian@meiswinkel.com>
2022-03-11 12:35:06 +0000

    Changing underlying Map when cloning FeedResponse from ConcurrentHashMap to HashMap to allow null values/keys

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/models/FeedResponse.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/models/FeedResponse.java
@@ -94,7 +94,11 @@ public class FeedResponse<T> implements ContinuablePage<String, T> {
         List<T> transformedResults,
         FeedResponse<?> toBeCloned) {
         this.results = transformedResults;
-        this.header = toBeCloned.header != null ? new ConcurrentHashMap<>(toBeCloned.header) : null;
+
+        // NOTE - it is important to use HashMap over ConcurrentHashMap here because some keys/values might be null
+        // and this is not allowed in ConcurrentHashMap - while it is ok in HashMap
+        this.header = toBeCloned.header != null ? new HashMap<>(toBeCloned.header) : null;
+
         this.usageHeaders = toBeCloned.usageHeaders != null ? new HashMap<>(toBeCloned.usageHeaders) : null;
         this.quotaHeaders = toBeCloned.quotaHeaders != null ? new HashMap<>(toBeCloned.quotaHeaders) : null;
         this.useEtagAsContinuation = toBeCloned.useEtagAsContinuation;
