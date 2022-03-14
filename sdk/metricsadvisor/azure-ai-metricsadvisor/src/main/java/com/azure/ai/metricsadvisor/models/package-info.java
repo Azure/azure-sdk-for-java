56b393ae4dc8a73ddbb4bacf0ddfe93ed405d675
Weidong Xu <weidxu@microsoft.com>
2022-03-14 14:37:39 +0800

    add package-info

---
+++ sdk/metricsadvisor/azure-ai-metricsadvisor/src/main/java/com/azure/ai/metricsadvisor/models/package-info.java
@@ -0,0 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+/** Package containing the models for MetricsAdvisor. Microsoft Azure Metrics Advisor REST API (OpenAPI v2). */
+package com.azure.ai.metricsadvisor.models;
