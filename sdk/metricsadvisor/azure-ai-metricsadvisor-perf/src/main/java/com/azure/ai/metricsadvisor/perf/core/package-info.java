0d733696eb9e7ee2ced5550285312c533bd41ab4
Weidong Xu <weidxu@microsoft.com>
2022-03-14 10:31:08 +0800

    clean metricsadvisor

--- sdk/metricsadvisor/azure-ai-metricsadvisor-perf/src/main/java/com/azure/ai/metricsadvisor/perf/core/package-info.java
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-/**
- * Package containing performance base class for Azure MetricsAdvisor SDK for Java.
- */
-package com.azure.ai.metricsadvisor.perf.core;
