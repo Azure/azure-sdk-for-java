2d13ec6824c6807e0e25612d88309e6305a4bae3
xiaofeicao <xiaofeicao@microsoft.com>
2022-03-16 10:58:39 +0800

    make test class public

--- sdk/batch/azure-resourcemanager-batch/src/test/java/com/azure/resourcemanager/batch/BatchTests.java
+++ sdk/batch/azure-resourcemanager-batch/src/test/java/com/azure/resourcemanager/batch/BatchTests.java
@@ -40,7 +40,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
-class BatchTests extends TestBase {
+public class BatchTests extends TestBase {
 
     private static final Random RANDOM = new Random();
 
