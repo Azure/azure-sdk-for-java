7e8e0f54d25037e3b0b11f24ef736f4baa56ef7b
Weidong Xu <weidxu@microsoft.com>
2022-03-24 12:43:35 +0800

    nit update script

--- sdk/resourcemanager/azure-resourcemanager-samples/src/main/resources/create_resources_with_msi.sh
+++ sdk/resourcemanager/azure-resourcemanager-samples/src/main/resources/create_resources_with_msi.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-/usr/bin/yes | sudo apt-get update
-/usr/bin/yes | sudo apt install python3
-/usr/bin/yes | sudo apt install python3-pip
+sudo apt update
+sudo apt install -y python3
+sudo apt install -y python3-pip
+
 sudo pip3 install azure-cli
+
 az login --msi
 az storage account create -n $1 -g $2 -l $3 --sku Premium_LRS
