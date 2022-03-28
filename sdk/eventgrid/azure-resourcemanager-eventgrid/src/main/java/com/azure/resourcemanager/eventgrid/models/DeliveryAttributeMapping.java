482e078a97a02340b59822ea104f907c4bbcec1f
azure-sdk <azuresdk@microsoft.com>
2022-03-28 04:39:13 +0000

    [Automation] Generate Fluent Lite from eventgrid#package-2021-10-preview

--- sdk/eventgrid/azure-resourcemanager-eventgrid/src/main/java/com/azure/resourcemanager/eventgrid/models/DeliveryAttributeMapping.java
+++ sdk/eventgrid/azure-resourcemanager-eventgrid/src/main/java/com/azure/resourcemanager/eventgrid/models/DeliveryAttributeMapping.java
@@ -5,8 +5,6 @@
 package com.azure.resourcemanager.eventgrid.models;
 
 import com.azure.core.annotation.Fluent;
-import com.azure.core.util.logging.ClientLogger;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
@@ -25,8 +23,6 @@ import com.fasterxml.jackson.annotation.JsonTypeName;
 })
 @Fluent
 public class DeliveryAttributeMapping {
-    @JsonIgnore private final ClientLogger logger = new ClientLogger(DeliveryAttributeMapping.class);
-
     /*
      * Name of the delivery attribute or header.
      */
