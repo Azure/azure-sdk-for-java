88c7df88faaf6c2e2438e3834a3eee33f0d9231f
Bill Wert <billwert@microsoft.com>
2022-04-04 19:45:25 -0700

    Remove test for old behavior

--- sdk/identity/azure-identity/src/test/java/com/azure/identity/ManagedIdentityCredentialTest.java
+++ sdk/identity/azure-identity/src/test/java/com/azure/identity/ManagedIdentityCredentialTest.java
@@ -4,7 +4,6 @@
 package com.azure.identity;
 
 import com.azure.core.credential.TokenRequestContext;
-import com.azure.core.exception.ClientAuthenticationException;
 import com.azure.core.util.Configuration;
 import com.azure.identity.implementation.IdentityClient;
 import com.azure.identity.util.TestUtils;
@@ -92,26 +91,6 @@ public class ManagedIdentityCredentialTest {
                 .expectNextMatches(token -> token1.equals(token.getToken())
                         && expiresOn.getSecond() == token.getExpiresAt().getSecond())
                 .verifyComplete();
-    }
-
-    @Test
-    public void testArcUserAssigned() throws Exception {
-        Configuration configuration = Configuration.getGlobalConfiguration().clone();
-
-        // setup
-        String token1 = "token1";
-        String endpoint = "http://localhost";
-        TokenRequestContext request = new TokenRequestContext().addScopes("https://management.azure.com");
-        configuration.put("IDENTITY_ENDPOINT", endpoint);
-        configuration.put("IMDS_ENDPOINT", endpoint);
-
-
-        // test
-        ManagedIdentityCredential credential = new ManagedIdentityCredentialBuilder()
-            .configuration(configuration).clientId(CLIENT_ID).build();
-        StepVerifier.create(credential.getToken(request))
-            .expectErrorMatches(t -> t instanceof ClientAuthenticationException)
-            .verify();
     }
 
     @Test (expected = IllegalStateException.class)
