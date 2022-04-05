cec545e94946d208e29bd309297440b675d0ecfe
Kamil Sobol <kasobol@microsoft.com>
2022-04-05 10:48:46 -0700

    Improve buffered response.

--- sdk/core/azure-core/src/main/java/com/azure/core/implementation/util/ByteArrayContent.java
+++ sdk/core/azure-core/src/main/java/com/azure/core/implementation/util/ByteArrayContent.java
@@ -6,6 +6,7 @@ package com.azure.core.implementation.util;
 import com.azure.core.util.serializer.ObjectSerializer;
 import com.azure.core.util.serializer.TypeReference;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
@@ -61,7 +62,7 @@ public final class ByteArrayContent extends BinaryDataContent {
 
     @Override
     public Flux<ByteBuffer> toFluxByteBuffer() {
-        return Flux.defer(() -> Flux.just(toByteBuffer()));
+        return Mono.fromSupplier(this::toByteBuffer).flux();
     }
 
     @Override
