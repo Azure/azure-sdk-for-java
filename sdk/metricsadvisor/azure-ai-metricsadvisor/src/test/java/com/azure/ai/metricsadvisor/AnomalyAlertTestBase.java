65031f9aa86de4043bdd582f179e0e9e6158515e
samvaity <savaity@microsoft.com>
2022-04-05 13:12:30 -0700

    update to new datafeed and related ids

--- sdk/metricsadvisor/azure-ai-metricsadvisor/src/test/java/com/azure/ai/metricsadvisor/AnomalyAlertTestBase.java
+++ sdk/metricsadvisor/azure-ai-metricsadvisor/src/test/java/com/azure/ai/metricsadvisor/AnomalyAlertTestBase.java
@@ -22,8 +22,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public abstract class AnomalyAlertTestBase extends MetricsAdvisorAdministrationClientTestBase {
 
-    static final String DETECTION_CONFIGURATION_ID = "efaee305-f049-43ec-9f9b-76026d55c14a";
-    static final String ALERT_HOOK_ID = "14b69527-cc37-45ba-8863-4f9857ccacca";
+    static final String DETECTION_CONFIGURATION_ID = "78f3a4e7-fe53-4a05-9f4d-d724ab6c23a7";
+    static final String ALERT_HOOK_ID = "ad2639d2-84dd-4b12-9635-82b8ff14e832";
 
     @Override
     protected void beforeTest() {
