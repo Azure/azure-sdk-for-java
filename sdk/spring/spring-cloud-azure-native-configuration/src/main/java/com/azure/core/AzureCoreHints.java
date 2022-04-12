984ecbad613dafc9177a51fec4b2f77d15f070ad
Moary Chen <moary@126.com>
2022-04-12 19:24:39 +0800

    Fix the problems encountered in the sample test

--- sdk/spring/spring-cloud-azure-native-configuration/src/main/java/com/azure/core/AzureCoreHints.java
+++ sdk/spring/spring-cloud-azure-native-configuration/src/main/java/com/azure/core/AzureCoreHints.java
@@ -4,10 +4,15 @@
 package com.azure.core;
 
 import com.azure.core.credential.TokenCredential;
+import com.azure.core.exception.ResourceModifiedException;
+import com.azure.core.util.Base64Url;
 import com.azure.core.util.logging.LoggingEventBuilder;
+import com.azure.core.util.polling.LongRunningOperationStatus;
 import org.springframework.nativex.hint.InitializationHint;
 import org.springframework.nativex.hint.InitializationTime;
 import org.springframework.nativex.hint.NativeHint;
+import org.springframework.nativex.hint.TypeAccess;
+import org.springframework.nativex.hint.TypeHint;
 import org.springframework.nativex.type.NativeConfiguration;
 
 @NativeHint(
@@ -16,7 +21,15 @@ import org.springframework.nativex.type.NativeConfiguration;
         @InitializationHint(
             initTime = InitializationTime.BUILD,
             types = { LoggingEventBuilder.class })
-    }
+    },
+    types = @TypeHint(
+        types = {
+            Base64Url.class,
+            LongRunningOperationStatus.class,
+            ResourceModifiedException.class
+        },
+        access = { TypeAccess.DECLARED_CONSTRUCTORS, TypeAccess.DECLARED_METHODS }
+    )
 )
 public class AzureCoreHints implements NativeConfiguration {
 }
