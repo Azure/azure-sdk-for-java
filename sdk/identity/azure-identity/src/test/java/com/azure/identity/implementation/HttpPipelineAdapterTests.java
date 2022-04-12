0932ba81de514a927933f0f288944b52df822df8
Bill Wert <billwert@microsoft.com>
2022-04-12 16:49:13 -0700

    Update HttpPipelineAdapterTests

--- sdk/identity/azure-identity/src/test/java/com/azure/identity/implementation/HttpPipelineAdapterTests.java
+++ sdk/identity/azure-identity/src/test/java/com/azure/identity/implementation/HttpPipelineAdapterTests.java
@@ -1,19 +1,16 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
 package com.azure.identity.implementation;
 
 import com.azure.core.http.HttpHeaders;
 import com.azure.core.http.HttpPipeline;
+import com.azure.core.http.HttpPipelineBuilder;
 import com.azure.core.http.HttpResponse;
 import com.microsoft.aad.msal4j.HttpMethod;
 import com.microsoft.aad.msal4j.HttpRequest;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
+import org.mockito.MockedConstruction;
+import org.mockito.Mockito;
 import reactor.core.publisher.Mono;
 
 import java.net.MalformedURLException;
@@ -22,17 +19,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mockConstruction;
 import static org.mockito.Mockito.when;
 
-@RunWith(PowerMockRunner.class)
-@PowerMockIgnore({"com.sun.org.apache.xerces.*", "javax.xml.*", "org.xml.*"})
-@PrepareForTest({HttpPipeline.class, HttpRequest.class, HttpResponse.class, URL.class})
 public class HttpPipelineAdapterTests {
 
     @Test
     public void testSendRequest() throws Exception {
         Mono<String> bodyResponse = Mono.just("dummy-body");
-        HttpPipeline pipeline = PowerMockito.mock(HttpPipeline.class);
+        HttpPipeline pipeline = Mockito.mock(HttpPipeline.class);
         HttpPipelineAdapter pipelineAdapter = new HttpPipelineAdapter(pipeline, new IdentityClientOptions());
         HttpRequest req = mockForSendRequest(bodyResponse, pipeline);
         pipelineAdapter.send(req);
@@ -40,14 +35,15 @@ public class HttpPipelineAdapterTests {
 
     @Test
     public void testSendRequestEmptyBody() throws Exception {
-        HttpPipeline pipeline = PowerMockito.mock(HttpPipeline.class);
+        HttpPipeline pipeline = Mockito.mock(HttpPipeline.class);
         HttpPipelineAdapter pipelineAdapter = new HttpPipelineAdapter(pipeline, new IdentityClientOptions());
         HttpRequest req = mockForSendRequest(Mono.empty(), pipeline);
         pipelineAdapter.send(req);
     }
 
     private HttpRequest mockForSendRequest(Mono<String> bodyResponse, HttpPipeline pipeline) throws MalformedURLException {
-        HttpRequest req = PowerMockito.mock(HttpRequest.class);
+
+        HttpRequest req = Mockito.mock(HttpRequest.class);
         HttpMethod method = HttpMethod.GET;
         when(req.httpMethod()).thenReturn(method);
         URL url = new URL("https://localhost.com/");
@@ -55,7 +51,7 @@ public class HttpPipelineAdapterTests {
         when(req.body()).thenReturn("");
         Map<String, String> a = new HashMap<>();
         when(req.headers()).thenReturn(a);
-        HttpResponse coreResponse = PowerMockito.mock(HttpResponse.class);
+        HttpResponse coreResponse = Mockito.mock(HttpResponse.class);
         when(coreResponse.getBodyAsString()).thenReturn(bodyResponse);
         when(coreResponse.getHeaders()).thenReturn(new HttpHeaders(a));
         when(coreResponse.getStatusCode()).thenReturn(200);
