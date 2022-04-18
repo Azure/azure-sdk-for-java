15318d93261bfc5c7f1cb240dbfc57b502f45625
Hong Li(MSFT) <v-hongli1@microsoft.com>
2022-04-18 17:00:33 +0800

    Rollback pipeline test for Public/UsGov/China Cloud

--- sdk/storage/azure-storage-common/src/main/java/module-info.java
+++ sdk/storage/azure-storage-common/src/main/java/module-info.java
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 module com.azure.storage.common {
+    requires transitive com.azure.core;
 
     exports com.azure.storage.common;
     exports com.azure.storage.common.sas;
