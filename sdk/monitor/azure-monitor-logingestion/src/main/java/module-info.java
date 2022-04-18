756237cf8a12d9172107b92aa26c1b65c87e2a04
Srikanta <srnagar@microsoft.com>
2022-04-18 14:24:06 -0700

    Initial commit for logs ingestion codegen

---
+++ sdk/monitor/azure-monitor-logingestion/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+module com.azure.monitor.logingestion {
+    requires transitive com.azure.core;
+    exports com.azure.monitor.logingestion;
+}
