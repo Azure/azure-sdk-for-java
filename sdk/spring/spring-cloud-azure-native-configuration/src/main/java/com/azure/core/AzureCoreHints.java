f8ada96f628312ecf6b5f9033a0c4a7179545da1
Moary Chen <moary@126.com>
2022-04-19 15:14:23 +0800

    Remove the configuration in spring side due to the graalvm-aot-support has provided the config

--- sdk/spring/spring-cloud-azure-native-configuration/src/main/java/com/azure/core/AzureCoreHints.java
+++ sdk/spring/spring-cloud-azure-native-configuration/src/main/java/com/azure/core/AzureCoreHints.java
@@ -6,10 +6,7 @@ package com.azure.core;
 import com.azure.core.credential.TokenCredential;
 import com.azure.core.exception.ResourceModifiedException;
 import com.azure.core.util.Base64Url;
-import com.azure.core.util.logging.LoggingEventBuilder;
 import com.azure.core.util.polling.LongRunningOperationStatus;
-import org.springframework.nativex.hint.InitializationHint;
-import org.springframework.nativex.hint.InitializationTime;
 import org.springframework.nativex.hint.NativeHint;
 import org.springframework.nativex.hint.TypeAccess;
 import org.springframework.nativex.hint.TypeHint;
@@ -17,11 +14,6 @@ import org.springframework.nativex.type.NativeConfiguration;
 
 @NativeHint(
     trigger = TokenCredential.class,
-    initialization = {
-        @InitializationHint(
-            initTime = InitializationTime.BUILD,
-            types = { LoggingEventBuilder.class })
-    },
     types = @TypeHint(
         types = {
             Base64Url.class,
