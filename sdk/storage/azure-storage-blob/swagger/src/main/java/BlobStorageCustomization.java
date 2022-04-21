c34ebe261ec849b0061f148636412303a31dde05
Rick Ley <frley@microsoft.com>
2022-04-21 14:19:17 -0700

    Removed leftover customization

--- sdk/storage/azure-storage-blob/swagger/src/main/java/BlobStorageCustomization.java
+++ sdk/storage/azure-storage-blob/swagger/src/main/java/BlobStorageCustomization.java
@@ -24,8 +24,6 @@ public class BlobStorageCustomization extends Customization {
         models.getClass("PageList").addAnnotation("@JsonDeserialize(using = PageListDeserializer.class)");
 
         models.getClass("BlobCopySourceTags").rename("BlobCopySourceTagsMode");
-        models.getClass("BlobErrorCode").getProperty("SNAPHOT_OPERATION_RATE_EXCEEDED")
-            .addAnnotation("Deprecated");
 
 
         ClassCustomization blobHttpHeaders = models.getClass("BlobHttpHeaders");
