d0ee6a68778881ea9a08face4a0db19d8a0543a3
Kamil Sobol <kasobol@microsoft.com>
2022-04-28 10:05:58 -0700

    resolve old artifact in revapi.

--- buildSrc/src/main/kotlin/com.azure.revapi-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/com.azure.revapi-conventions.gradle.kts
@@ -6,17 +6,48 @@ import org.revapi.PipelineConfiguration
 import org.revapi.Revapi
 import org.revapi.base.FileArchive
 
+repositories {
+    mavenCentral()
+}
+
+val revapiBaseline by configurations.creating {
+    resolutionStrategy {
+        componentSelection {
+            all {
+                if (candidate.group == "com.azure" && candidate.module == project.name
+                    && !candidate.version.matches(Regex("^\\d+\\.\\d+\\.\\d+\$"))) {
+                    reject("Non GA Version")
+                }
+            }
+        }
+    }
+
+}
+
+dependencies {
+    revapiBaseline("com.azure", project.name, "[1.0.0,)")
+}
+
 class RevApiPlugin : Plugin<Project> {
     override fun apply(project: Project) {
+
         project.task("revapi") {
             dependsOn("jar")
             doLast {
+
                 // TODO this needs further refinement.
-                val newAPI = API.of(FileArchive(project.tasks.getByName<Jar>("jar").archiveFile.get().asFile))
-                    .supportedBy(project.configurations.getByName("compileClasspath").map { f -> FileArchive(f) })
+
+                val newArchiveFile = project.tasks.getByName<Jar>("jar").archiveFile.get().asFile
+                val newArchiveSupportingFiles = project.configurations.getByName("compileClasspath").toList()
+
+                val oldArchiveFile = project.configurations.getByName("revapiBaseline").first()
+                val oldArchiveSupportingFiles = project.configurations.getByName("revapiBaseline").drop(1)
+
+                val newAPI = API.of(FileArchive(newArchiveFile))
+                    .supportedBy(newArchiveSupportingFiles.map { f -> FileArchive(f) })
                     .build()
-                val oldAPI = API.of(FileArchive(project.tasks.getByName<Jar>("jar").archiveFile.get().asFile))
-                    .supportedBy(project.configurations.getByName("compileClasspath").map { f -> FileArchive(f) })
+                val oldAPI = API.of(FileArchive(oldArchiveFile))
+                    .supportedBy(oldArchiveSupportingFiles.map { f -> FileArchive(f) })
                     .build()
 
                 val pipelineConfiguration = PipelineConfiguration.builder()
