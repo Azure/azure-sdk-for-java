1431830c7df23ba92d31a41f58133a7a995b03d7
Rick Ley <frley@microsoft.com>
2022-04-29 11:36:32 -0700

    Get page ranges continuation (#28390)
    * Generated code
    
    * Fixed nextMarker
    
    * Tried to reformat the xml tags
    
    * Tried to add a segment type to reformat xml tags
    
    * Tried another thing
    
    * Got it generating
    
    * Finished feature
    
    * Changelog
    
    * Some apiview feedback
    
    * Starting an attempt to pass context to method
    
    * Sorted out adding context to sync method
    
    * Wrote some tests
    
    * Wrote tests
    
    * Test fixes and recordings
    
    * Added samples
    
    * ci fix
    
    * imports fixes
    
    * Added PageListHelper
    
    * Fixed merge conflict

--- sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/implementation/models/ServicesListBlobContainersSegmentNextResponse.java
+++ sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/implementation/models/ServicesListBlobContainersSegmentNextResponse.java
@@ -29,7 +29,11 @@ public final class ServicesListBlobContainersSegmentNextResponse
         super(request, statusCode, rawHeaders, value, headers);
     }
 
-    /** @return the deserialized response body. */
+    /**
+     * Gets the deserialized response body.
+     *
+     * @return the deserialized response body.
+     */
     @Override
     public BlobContainersSegment getValue() {
         return super.getValue();
