4fc6ee7297fc7d6a317087e99606cdb062e68792
Sean McCullough <44180881+seanmcc-msft@users.noreply.github.com>
2022-05-06 11:14:29 -0500

    Made "Delete if exists file system that was already deleted" @PlaybackOnly

--- sdk/storage/azure-storage-file-datalake/src/test/java/com/azure/storage/file/datalake/FileSystemAPITest.groovy
+++ sdk/storage/azure-storage-file-datalake/src/test/java/com/azure/storage/file/datalake/FileSystemAPITest.groovy
@@ -8,6 +8,7 @@ import com.azure.storage.blob.models.BlobErrorCode
 import com.azure.storage.blob.options.AppendBlobCreateOptions
 import com.azure.storage.common.Utility
 import com.azure.storage.common.test.shared.extensions.RequiredServiceVersion
+import com.azure.storage.common.test.shared.extensions.PlaybackOnly
 import com.azure.storage.file.datalake.models.DataLakeAccessPolicy
 import com.azure.storage.file.datalake.models.DataLakeRequestConditions
 import com.azure.storage.file.datalake.models.DataLakeSignedIdentifier
@@ -498,6 +499,7 @@ class FileSystemAPITest extends APISpec {
         !fsc.getBlobContainerClient().exists()
     }
 
+    @PlaybackOnly
     def "Delete if exists file system that was already deleted"() {
         setup:
         def fsc = primaryDataLakeServiceClient.getFileSystemClient(generateFileSystemName())
