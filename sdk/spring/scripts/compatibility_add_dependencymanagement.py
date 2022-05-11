f8f5e33bcc085d7dce1e976d89a89f25e2842a5a
Muyao Feng <v-muyaofeng@microsoft.com>
2022-05-11 15:34:07 +0800

    add scripts

---
+++ sdk/spring/scripts/compatibility_add_dependencymanagement.py
@@ -0,0 +1,113 @@
+# Python version 3.4 or higher is required to run this script.
+#
+# This script is used to insert dependency management into ./sdk/spring/**/pom*.xml.
+#
+# Here is sample of inserted fragment:
+#    <properties>
+#       <spring-boot-dependencies-version>${spring_boot_dependencies_version}</spring-boot-dependencies-version>
+#       <spring-cloud-dependencies-version>${spring_cloud_dependencies_version}</spring-cloud-dependencies-version>
+#    </properties>
+#
+#    <dependencyManagement>
+#       <dependency>
+#         <groupId>org.springframework.boot</groupId>
+#         <artifactId>spring-boot-dependencies</artifactId>
+#         <version>${spring_boot_dependencies_version}</version>
+#         <type>pom</type>
+#         <scope>import</scope>
+#       </dependency>
+#       <dependency>
+#         <groupId>org.springframework.cloud</groupId>
+#         <artifactId>spring-cloud-dependencies</artifactId>
+#         <version>${spring_cloud_dependencies_version}</version>
+#         <type>pom</type>
+#         <scope>import</scope>
+#       </dependency>
+#    </dependencyManagement>
+#
+# Sample:
+# 1. python .\sdk\spring\scripts\compatibility_add_dependencymanagement.py.py --spring_boot_dependencies_version 2.6.7 --spring_cloud_dependencies_version 2021.0.2
+# 2. python .\sdk\spring\scripts\compatibility_add_dependencymanagement.py.py -b 2.6.7 -c 2021.0.2
+#
+# The script must be run at the root of azure-sdk-for-java.
+
+
+import os
+import time
+import argparse
+
+from log import log
+
+def main():
+    start_time = time.time()
+    change_to_root_dir()
+    args = get_args()
+    log.set_log_level(args.log)
+    log.debug('Current working directory = {}.'.format(os.getcwd()))
+    add_dependency_management_for_all_poms_files_in_directory("./sdk/spring", args.spring_boot_dependencies_version, args.spring_cloud_dependencies_version)
+    elapsed_time = time.time() - start_time
+    log.info('elapsed_time = {}'.format(elapsed_time))
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Insert dependencyManagement in pom files.')
+    parser.add_argument('-b', '--spring_boot_dependencies_version', type=str, required=True)
+    parser.add_argument('-c', '--spring_cloud_dependencies_version', type=str, required=True)
+    parser.add_argument(
+        '--log',
+        type=str,
+        choices=['debug', 'info', 'warn', 'error', 'none'],
+        required=False,
+        default='info',
+        help='Set log level.'
+    )
+    return parser.parse_args()
+
+def change_to_root_dir():
+    os.chdir(os.path.dirname(os.path.realpath(__file__)))
+    os.chdir('../../..')
+
+def add_dependency_management_for_all_poms_files_in_directory(directory, spring_boot_dependencies_version, spring_cloud_dependencies_version):
+    for root, dirs, files in os.walk(directory):
+        for file_name in files:
+            if file_name.startswith('pom') and file_name.endswith('.xml'):
+                file_path = root + os.sep + file_name
+                add_dependency_management_for_file(file_path, spring_boot_dependencies_version, spring_cloud_dependencies_version)
+
+def add_dependency_management_for_file(file_path, spring_boot_dependencies_version, spring_cloud_dependencies_version):
+    log.info("Add dependency management for file: " + file_path)
+    with open(file_path, 'r', encoding='utf-8') as pom_file:
+        pom_file_content = pom_file.read()
+        insert_position = pom_file_content.find('<dependencies>')
+        insert_content = get_dependency_management_content(spring_boot_dependencies_version, spring_cloud_dependencies_version)
+        new_content = pom_file_content[:insert_position] + insert_content + pom_file_content[insert_position:]
+        with open(file_path, 'r+', encoding='utf-8') as updated_pom_file:
+            updated_pom_file.writelines(new_content)
+
+def get_dependency_management_content(spring_boot_dependencies_version, spring_cloud_dependencies_version):
+    return """
+  <properties>
+    <spring-boot-dependencies-version>{}</spring-boot-dependencies-version>
+    <spring-cloud-dependencies-version>{}</spring-cloud-dependencies-version>
+  </properties>
+  <dependencyManagement>
+    <dependencies>
+      <dependency>
+        <groupId>org.springframework.boot</groupId>
+        <artifactId>spring-boot-dependencies</artifactId>
+        <version>{}</version>
+        <type>pom</type>
+        <scope>import</scope>
+      </dependency>
+      <dependency>
+        <groupId>org.springframework.cloud</groupId>
+        <artifactId>spring-cloud-dependencies</artifactId>
+        <version>{}</version>
+        <type>pom</type>
+        <scope>import</scope>
+      </dependency>
+    </dependencies>
+  </dependencyManagement>
+""".format(spring_boot_dependencies_version, spring_cloud_dependencies_version, '${spring-boot-dependencies-version}', '${spring-cloud-dependencies-version}')
+
+if __name__ == '__main__':
+    main()
