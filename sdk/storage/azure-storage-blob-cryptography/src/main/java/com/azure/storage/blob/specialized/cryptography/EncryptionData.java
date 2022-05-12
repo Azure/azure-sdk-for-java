057bb4b66db405b639538cea8745cb70790951fb
Rabab <ibrahimr@microsoft.com>
2022-05-12 14:28:35 -0700

    Reset changes to the patch version.

--- sdk/storage/azure-storage-blob-cryptography/src/main/java/com/azure/storage/blob/specialized/cryptography/EncryptionData.java
+++ sdk/storage/azure-storage-blob-cryptography/src/main/java/com/azure/storage/blob/specialized/cryptography/EncryptionData.java
@@ -92,7 +92,7 @@ final class EncryptionData {
     /**
      * Gets the encryption agent that is used to identify the encryption protocol version and encryption algorithm.
      *
-     * @return an {@link EncryptionAgent}.
+     * @return an {@Link EncryptionAgent}.
      */
     EncryptionAgent getEncryptionAgent() {
         return this.encryptionAgent;
