2a25c8950794585d18360349382a16db03054f7c
Fabian Meiswinkel <fabian@meiswinkel.com>
2022-05-17 22:19:06 +0000

    Removing Class.forName for implementation bridge helpers (#28912)
    * improvement for CosmosExceptionHelper
    
    * update log message
    
    * Removing Class.forName for implementation bridge helpers
    
    * Added changelog
    
    * Update spotbugs-exclude.xml
    
    * Update spotbugs-exclude.xml
    
    * Adding API comments
    
    * Update CHANGELOG.md
    
    Co-authored-by: annie-mac <annie-mac@yindeng2019.fareast.corp.microsoft.com>

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/DirectConnectionConfig.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/DirectConnectionConfig.java
@@ -316,6 +316,12 @@ public final class DirectConnectionConfig {
     // the following helper/accessor only helps to access this class outside of this package.//
     ///////////////////////////////////////////////////////////////////////////////////////////
 
+    /**
+     * Should not be called form user-code. This method is a no-op and is just used internally
+     * to force loading this class
+     */
+    public static void doNothingButEnsureLoadingClass() {}
+
     static {
         ImplementationBridgeHelpers.DirectConnectionConfigHelper.setDirectConnectionConfigAccessor(
             new ImplementationBridgeHelpers.DirectConnectionConfigHelper.DirectConnectionConfigAccessor() {
--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/models/CosmosBulkItemResponse.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/models/CosmosBulkItemResponse.java
@@ -191,6 +191,12 @@ public final class CosmosBulkItemResponse {
     // the following helper/accessor only helps to access this class outside of this package.//
     ///////////////////////////////////////////////////////////////////////////////////////////
 
+    /**
+     * Should not be called form user-code. This method is a no-op and is just used internally
+     * to force loading this class
+     */
+    public static void doNothingButEnsureLoadingClass() {}
+
     static {
         ImplementationBridgeHelpers.CosmosBulkItemResponseHelper.setCosmosBulkItemResponseAccessor(
             new ImplementationBridgeHelpers.CosmosBulkItemResponseHelper.CosmosBulkItemResponseAccessor() {
