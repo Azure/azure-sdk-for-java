fe639132052dedd7a52adb42de08fdadae4c9c2f
Kamil Sobol <kasobol@microsoft.com>
2022-05-18 18:21:57 -0700

    un-flaky this test.

--- sdk/core/azure-core-http-okhttp/src/test/java/com/azure/core/http/okhttp/OkHttpAsyncHttpClientTests.java
+++ sdk/core/azure-core-http-okhttp/src/test/java/com/azure/core/http/okhttp/OkHttpAsyncHttpClientTests.java
@@ -11,6 +11,8 @@ import com.azure.core.http.HttpRequest;
 import com.azure.core.http.HttpResponse;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import okhttp3.ConnectionPool;
+import okhttp3.Dispatcher;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
@@ -216,12 +218,17 @@ public class OkHttpAsyncHttpClientTests {
     @Test
     public void testConcurrentRequests() throws NoSuchAlgorithmException {
         int numRequests = 100; // 100 = 1GB of data read
-        HttpClient client = new OkHttpAsyncClientProvider().createInstance();
+        int concurrency = 10;
+        Dispatcher dispatcher = new Dispatcher();
+        dispatcher.setMaxRequestsPerHost(10); // this is 5 by default.
+        HttpClient client = new OkHttpAsyncHttpClientBuilder()
+            .dispatcher(dispatcher)
+            .build();
         byte[] expectedDigest = digest(LONG_BODY);
         long expectedByteCount = (long) numRequests * LONG_BODY.getBytes(StandardCharsets.UTF_8).length;
 
         Mono<Long> numBytesMono = Flux.range(1, numRequests)
-            .parallel(10)
+            .parallel(concurrency)
             .runOn(Schedulers.boundedElastic())
             .flatMap(n -> Mono.fromCallable(() -> getResponse(client, "/long")).flatMapMany(response -> {
                 MessageDigest md = md5Digest();
