572a80a027c32ebdc7af68a045e667b96cff5870
Muyao Feng <v-muyaofeng@microsoft.com>
2022-05-23 16:54:08 +0800

    test

--- sdk/spring/scripts/compatibility_insert_dependencymanagement.py
+++ sdk/spring/scripts/compatibility_insert_dependencymanagement.py
@@ -10,6 +10,7 @@
 
 import os
 import time
+import json
 
 from log import log
 
@@ -87,8 +88,16 @@ def get_prop_content(pom_file_content):
 
 
 def add_dependency_management_for_file(file_path):
-    spring_cloud_version = os.getenv("SPRING_CLOUD_AZURE_TEST_SUPPORTED_SPRING_CLOUD_VERSION")
-    print(spring_cloud_version)
+    spring_boot_version = os.getenv("SPRING_CLOUD_AZURE_TEST_SUPPORTED_SPRING_BOOT_VERSION")
+    spring_cloud_version = []
+    with open("./sdk/spring/spring-cloud-azure-supported-spring.json", 'r') as file:
+        data = json.load(file)
+    for entry in data:
+        for key in entry:
+            if spring_boot_version == entry[key]:
+                spring_cloud_version = entry["spring-cloud-version"]
+    # spring_cloud_version = os.getenv("SPRING_CLOUD_AZURE_TEST_SUPPORTED_SPRING_CLOUD_VERSION")
+    # print(spring_cloud_version)
     log.info("Add dependency management for file: " + file_path)
     with open(file_path, 'r', encoding = 'utf-8') as pom_file:
         pom_file_content = pom_file.read()
