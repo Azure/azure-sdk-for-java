f8f5dc61f867dbb8c7661806bf82f2d04912fa78
Anushka Singh <anushkasingh16@users.noreply.github.com>
2022-05-24 14:52:18 -0700

    Added a skeleton class that implements CheckpointStore

--- sdk/eventhubs/azure-messaging-eventhubs-checkpointstore-redis/src/Main.java
+++ sdk/eventhubs/azure-messaging-eventhubs-checkpointstore-redis/src/Main.java
@@ -1,5 +1,32 @@
 package com.azure.messaging.eventhubs.checkpointstore.redis;
 
+import com.azure.messaging.eventhubs.CheckpointStore;
+import java.util.List;
+import java.util.Objects;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 /*
 Added this file so that an execuatable jar is produced for the build.
  */
+
+/**
+ * Implementation of CheckpointStore that uses Azure Redis Cache.
+ */
+public class RedisCheckpointStore implements  CheckpointStore{
+    public Flux<PartitionOwnership> claimOwnership(List<PartitionOwnership> requestedPartitionOwnerships){
+        return null;
+    }
+
+    public Flux<Checkpoint> listCheckpoints(String fullyQualifiedNamespace, String eventHubName, String consumerGroup){
+        return null;
+    }
+
+    public Flux<PartitionOwnership> listOwnership(String fullyQualifiedNamespace, String eventHubName, String consumerGroup){
+        return null;
+    }
+
+    public Mono<Void> updateCheckpoint(Checkpoint checkpoint){
+        return null;
+    }
+}
