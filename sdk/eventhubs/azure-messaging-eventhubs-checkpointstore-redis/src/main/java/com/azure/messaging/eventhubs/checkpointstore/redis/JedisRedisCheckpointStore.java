5a3a9140b43baa8da5ad360da0a5b6a58f2d5265
Anushka Singh <anushkasingh16@users.noreply.github.com>
2022-05-31 13:20:40 -0700

    Added JedisPool and isCheckpointValid private method

--- sdk/eventhubs/azure-messaging-eventhubs-checkpointstore-redis/src/main/java/com/azure/messaging/eventhubs/checkpointstore/redis/JedisRedisCheckpointStore.java
+++ sdk/eventhubs/azure-messaging-eventhubs-checkpointstore-redis/src/main/java/com/azure/messaging/eventhubs/checkpointstore/redis/JedisRedisCheckpointStore.java
@@ -7,6 +7,7 @@ import com.azure.messaging.eventhubs.models.Checkpoint;
 import com.azure.messaging.eventhubs.models.PartitionOwnership;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import redis.clients.jedis.JedisPool;
 
 import java.util.List;
 
@@ -14,6 +15,9 @@ import java.util.List;
  * Implementation of {@link CheckpointStore} that uses Azure Redis Cache, specifically Jedis.
  */
 public class JedisRedisCheckpointStore implements CheckpointStore {
+
+    JedisPool jedisPool = new JedisPool(); // Look into other constructors that need config data to initalize JedisPool
+
     /**
      * This method returns the list of partitions that were owned successfully.
      *
@@ -50,11 +54,35 @@ public class JedisRedisCheckpointStore implements CheckpointStore {
     }
 
     /**
+     * This method updates the checkpoint in the Jedis resource for a given partition.
+     *
      * @param checkpoint Checkpoint information for this partition
-     * @return Null
+     * @return The new eTag of the checkpoint that has been successfully added.
      */
     @Override
     public Mono<Void> updateCheckpoint(Checkpoint checkpoint) {
+        //Check if the checkpoint is valid - checkpoint is invalid if it is null or if sequence number is null and offset is null
+        if (!isCheckpointValid(checkpoint)) {
+            //TO DO 1: Throw exception
+            System.out.println("Checkpoint is not valid.");
+        }
+
+        //TO DO 2: Get access to the Jedis resource
+
+        //Getting access to the Partition ID for the current checkpoint
+        String partitionID = checkpoint.getPartitionId();
+        String eventHubName = checkpoint.getEventHubName();
+        String fullyQualifiedNamespace = checkpoint.getFullyQualifiedNamespace();
+        String consumerGroup = checkpoint.getConsumerGroup();
+
+        //TO DO 3: Use the above information to create a format to store each checkpoint and its associated metadata
+
+        //TO DO 4: Add the above metadata to the Jedis Resource
+
         return null;
+    }
+
+    private Boolean isCheckpointValid(Checkpoint checkpoint) {
+        return !(checkpoint == null || (checkpoint.getOffset() == null && checkpoint.getSequenceNumber() == null));
     }
 }
