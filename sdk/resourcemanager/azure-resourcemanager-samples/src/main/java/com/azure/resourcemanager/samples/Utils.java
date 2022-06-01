ed130eefefaf15fcd4223d67e05fe5aea5ec3f25
xiaofeicao <xiaofeicao@microsoft.com>
2022-06-01 19:13:49 +0800

    fix spotbugs

--- sdk/resourcemanager/azure-resourcemanager-samples/src/main/java/com/azure/resourcemanager/samples/Utils.java
+++ sdk/resourcemanager/azure-resourcemanager-samples/src/main/java/com/azure/resourcemanager/samples/Utils.java
@@ -228,6 +228,7 @@ import java.util.Arrays;
 import java.util.Base64;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -3541,7 +3542,9 @@ public final class Utils {
      */
     public static <T> int getSize(Iterable<T> iterable) {
         int res = 0;
-        for (T t : iterable) {
+        Iterator<T> iterator = iterable.iterator();
+        while (iterator.hasNext()) {
+            iterator.next();
             res++;
         }
         return res;
