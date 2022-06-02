7bfcffccc3b3b76f9ac436c150085e28b785ea06
Muyao Feng <v-muyaofeng@microsoft.com>
2022-06-02 16:29:29 +0800

    change as comments

--- sdk/spring/scripts/compatibility_insert_dependencymanagement.py
+++ sdk/spring/scripts/compatibility_insert_dependencymanagement.py
@@ -27,7 +27,7 @@ def main():
     start_time = time.time()
     change_to_root_dir()
     log.debug('Current working directory = {}.'.format(os.getcwd()))
-    add_dependency_management_for_all_poms_files_in_directory("./sdk/spring")
+    add_dependency_management_for_all_poms_files_in_directory("./sdk/spring", get_args().spring_boot_dependencies_version, get_args().spring_cloud_dependencies_version)
     elapsed_time = time.time() - start_time
     log.info('elapsed_time = {}'.format(elapsed_time))
 
@@ -37,12 +37,12 @@ def change_to_root_dir():
     os.chdir('../../..')
 
 
-def add_dependency_management_for_all_poms_files_in_directory(directory):
+def add_dependency_management_for_all_poms_files_in_directory(directory, spring_boot_dependencies_version, spring_cloud_dependencies_version):
     for root, dirs, files in os.walk(directory):
         for file_name in files:
             if file_name.startswith('pom') and file_name.endswith('.xml'):
                 file_path = root + os.sep + file_name
-                add_dependency_management_for_file(file_path)
+                add_dependency_management_for_file(file_path, spring_boot_dependencies_version, spring_cloud_dependencies_version)
 
 
 def contains_repositories(pom_file_content):
@@ -88,15 +88,15 @@ def get_prop_position(pom_file_content):
         return pom_file_content.find("<name>")
 
 
-def get_prop_content(pom_file_content):
+def get_prop_content(pom_file_content, spring_boot_dependencies_version, spring_cloud_dependencies_version):
     if contains_properties(pom_file_content):
-        return get_properties_contend()
+        return get_properties_contend(spring_boot_dependencies_version, spring_cloud_dependencies_version)
     else:
-        return get_properties_contend_with_tag()
+        return get_properties_contend_with_tag(spring_boot_dependencies_version, spring_cloud_dependencies_version)
 
 
-def add_dependency_management_for_file(file_path):
-    spring_cloud_version = get_args().spring_cloud_dependencies_version
+def add_dependency_management_for_file(file_path, spring_boot_dependencies_version, spring_cloud_dependencies_version):
+    spring_cloud_version = spring_cloud_dependencies_version
     log.info("Add dependency management for file: " + file_path)
     with open(file_path, 'r', encoding = 'utf-8') as pom_file:
         pom_file_content = pom_file.read()
@@ -104,7 +104,7 @@ def add_dependency_management_for_file(file_path):
         insert_content = get_dependency_management_content()
         dependency_content = pom_file_content[:insert_position] + insert_content + pom_file_content[insert_position:]
         insert_position = get_prop_position(pom_file_content)
-        insert_content = get_prop_content(pom_file_content)
+        insert_content = get_prop_content(pom_file_content, spring_boot_dependencies_version, spring_cloud_dependencies_version)
         prop_content = dependency_content[:insert_position] + insert_content + dependency_content[insert_position:]
         with open(file_path, 'r+', encoding = 'utf-8') as updated_pom_file:
             updated_pom_file.writelines(prop_content)
@@ -142,20 +142,20 @@ def get_dependency_management_content():
 """
 
 
-def get_properties_contend_with_tag():
+def get_properties_contend_with_tag(spring_boot_dependencies_version, spring_cloud_dependencies_version):
     return """
   <properties>
     {}
   </properties>
   
-  """.format(get_properties_contend())
+  """.format(get_properties_contend(spring_boot_dependencies_version, spring_cloud_dependencies_version))
 
 
-def get_properties_contend():
+def get_properties_contend(spring_boot_dependencies_version, spring_cloud_dependencies_version):
     return """
     <spring.boot.version>{}</spring.boot.version>
     <spring.cloud.version>{}</spring.cloud.version>
-  """.format(get_args().spring_boot_dependencies_version, get_args().spring_cloud_dependencies_version)
+  """.format(spring_boot_dependencies_version, spring_cloud_dependencies_version)
 
 
 if __name__ == '__main__':
