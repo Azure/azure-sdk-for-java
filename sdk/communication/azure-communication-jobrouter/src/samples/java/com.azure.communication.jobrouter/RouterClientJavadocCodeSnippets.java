25c6d35eeafe15b756dabbf4aa49724e0b4e06c2
Charandeep Parisineti <cparisineti@microsoft.com>
2022-06-03 16:55:15 -0700

    Fix build

---
+++ sdk/communication/azure-communication-jobrouter/src/samples/java/com.azure.communication.jobrouter/RouterClientJavadocCodeSnippets.java
@@ -0,0 +1,32 @@
+package com.azure.communication.jobrouter;
+
+public final class RouterClientJavadocCodeSnippets {
+
+    public RouterClient createRouterClient() {
+        String connectionString = "endpoint=https://<RESOURCE_NAME>.communication.azure.com;accesskey=<ACCESS_KEY>";
+
+        // BEGIN: com.azure.communication.jobrouter.routerclient.instantiation
+        // Initialize the router client builder
+        final RouterClientBuilder builder = new RouterClientBuilder()
+            .connectionString(connectionString);
+        // Build the router client
+        RouterClient routerClient = builder.buildClient();
+
+        // END: com.azure.communication.jobrouter.routerclient.instantiation
+        return routerClient;
+    }
+
+    public RouterAsyncClient createRouterAsyncClient() {
+        String connectionString = "endpoint=https://<RESOURCE_NAME>.communication.azure.com;accesskey=<ACCESS_KEY>";
+
+        // BEGIN: com.azure.communication.jobrouter.routerasyncclient.instantiation
+        // Initialize the router client builder
+        final RouterClientBuilder builder = new RouterClientBuilder()
+            .connectionString(connectionString);
+        // Build the router client
+        RouterAsyncClient routerAsyncClient = builder.buildAsyncClient();
+
+        // END: com.azure.communication.jobrouter.routerasyncclient.instantiation
+        return routerAsyncClient;
+    }
+}
