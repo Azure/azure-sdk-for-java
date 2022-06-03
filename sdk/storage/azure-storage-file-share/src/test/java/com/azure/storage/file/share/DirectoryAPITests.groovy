7e263ade3e6e2b395ac6bb36a0d241aeb9502cfe
Kamil Sobol <61715331+kasobol-msft@users.noreply.github.com>
2022-06-03 15:45:25 -0700

    unflaky these tests. (#29211)

--- sdk/storage/azure-storage-file-share/src/test/java/com/azure/storage/file/share/DirectoryAPITests.groovy
+++ sdk/storage/azure-storage-file-share/src/test/java/com/azure/storage/file/share/DirectoryAPITests.groovy
@@ -23,6 +23,7 @@ import spock.lang.Unroll
 import java.time.LocalDateTime
 import java.time.OffsetDateTime
 import java.time.ZoneOffset
+import java.time.temporal.ChronoUnit
 import java.util.stream.Collectors
 
 class DirectoryAPITests extends APISpec {
@@ -491,7 +492,7 @@ class DirectoryAPITests extends APISpec {
         primaryDirectoryClient.setProperties(new FileSmbProperties().setFileChangeTime(changeTime), null)
 
         then:
-        primaryDirectoryClient.getProperties().getSmbProperties().getFileChangeTime() == changeTime
+        primaryDirectoryClient.getProperties().getSmbProperties().getFileChangeTime().truncatedTo(ChronoUnit.MICROS) == changeTime.truncatedTo(ChronoUnit.MICROS)
     }
 
     @Unroll
@@ -959,7 +960,7 @@ class DirectoryAPITests extends APISpec {
         destProperties.getSmbProperties().getNtfsFileAttributes() == EnumSet.of(NtfsFileAttributes.DIRECTORY)
         destProperties.getSmbProperties().getFileCreationTime()
         destProperties.getSmbProperties().getFileLastWriteTime()
-        destProperties.getSmbProperties().getFileChangeTime() == fileChangeTime
+        destProperties.getSmbProperties().getFileChangeTime().truncatedTo(ChronoUnit.MICROS) == fileChangeTime.truncatedTo(ChronoUnit.MICROS)
     }
 
     @RequiredServiceVersion(clazz = ShareServiceVersion.class, min = "V2021_04_10")
