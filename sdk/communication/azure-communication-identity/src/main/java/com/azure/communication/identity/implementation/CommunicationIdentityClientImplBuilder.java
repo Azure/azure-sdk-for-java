ed396ee5409075e929552a3bb20ecf6999152b32
Aigerim <aigerimb@microsoft.com>
2022-06-07 00:40:07 -0700

    [Java] Custom Teams Endpoint (CTE) GA (#26924)
    * client&implementation updates regarding the new cte path
    
    * recordings are updated
    
    * regenerated & updated new client for GA
    
    * refactored & added cte unit tests
    
    * fixed tests, updated recordings, added sanitizer for redaction of sensitive values in the recordings
    
    * renaming CTE params and wrapping them into options class
    
    * exposed TeamsUserExchangeTokenRequest by customization
    
    * fixed swagger pom file
    
    * removed setters of mandatory params & added code snippets for clients
    
    * added code snippets for builder
    
    * changed GetTokenForTeamsUserOptions annotation to Immutable
    
    Co-authored-by: Aigerim Beishenbekova <aigerimb@Aigerims-MacBook-Pro-2.local>

--- sdk/communication/azure-communication-identity/src/main/java/com/azure/communication/identity/implementation/CommunicationIdentityClientImplBuilder.java
+++ sdk/communication/azure-communication-identity/src/main/java/com/azure/communication/identity/implementation/CommunicationIdentityClientImplBuilder.java
@@ -4,51 +4,156 @@
 
 package com.azure.communication.identity.implementation;
 
+import com.azure.core.annotation.Generated;
 import com.azure.core.annotation.ServiceClientBuilder;
+import com.azure.core.client.traits.ConfigurationTrait;
+import com.azure.core.client.traits.EndpointTrait;
+import com.azure.core.client.traits.HttpTrait;
 import com.azure.core.http.HttpClient;
+import com.azure.core.http.HttpHeaders;
 import com.azure.core.http.HttpPipeline;
 import com.azure.core.http.HttpPipelineBuilder;
+import com.azure.core.http.HttpPipelinePosition;
+import com.azure.core.http.policy.AddDatePolicy;
+import com.azure.core.http.policy.AddHeadersFromContextPolicy;
+import com.azure.core.http.policy.AddHeadersPolicy;
 import com.azure.core.http.policy.CookiePolicy;
 import com.azure.core.http.policy.HttpLogOptions;
 import com.azure.core.http.policy.HttpLoggingPolicy;
 import com.azure.core.http.policy.HttpPipelinePolicy;
 import com.azure.core.http.policy.HttpPolicyProviders;
+import com.azure.core.http.policy.RequestIdPolicy;
+import com.azure.core.http.policy.RetryOptions;
 import com.azure.core.http.policy.RetryPolicy;
 import com.azure.core.http.policy.UserAgentPolicy;
+import com.azure.core.util.ClientOptions;
 import com.azure.core.util.Configuration;
+import com.azure.core.util.CoreUtils;
+import com.azure.core.util.builder.ClientBuilderUtil;
 import com.azure.core.util.serializer.JacksonAdapter;
 import com.azure.core.util.serializer.SerializerAdapter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /** A builder for creating a new instance of the CommunicationIdentityClient type. */
 @ServiceClientBuilder(serviceClients = {CommunicationIdentityClientImpl.class})
-public final class CommunicationIdentityClientImplBuilder {
-    private static final String SDK_NAME = "name";
+public final class CommunicationIdentityClientImplBuilder
+        implements HttpTrait<CommunicationIdentityClientImplBuilder>,
+                ConfigurationTrait<CommunicationIdentityClientImplBuilder>,
+                EndpointTrait<CommunicationIdentityClientImplBuilder> {
+    @Generated private static final String SDK_NAME = "name";
 
-    private static final String SDK_VERSION = "version";
+    @Generated private static final String SDK_VERSION = "version";
 
-    private final Map<String, String> properties = new HashMap<>();
+    @Generated private final Map<String, String> properties = new HashMap<>();
+
+    @Generated private final List<HttpPipelinePolicy> pipelinePolicies;
 
     /** Create an instance of the CommunicationIdentityClientImplBuilder. */
+    @Generated
     public CommunicationIdentityClientImplBuilder() {
         this.pipelinePolicies = new ArrayList<>();
     }
 
     /*
-     * The communication resource, for example
-     * https://my-resource.communication.azure.com
+     * The HTTP pipeline to send requests through.
      */
-    private String endpoint;
+    @Generated private HttpPipeline pipeline;
 
-    /**
-     * Sets The communication resource, for example https://my-resource.communication.azure.com.
-     *
-     * @param endpoint the endpoint value.
-     * @return the CommunicationIdentityClientImplBuilder.
+    /** {@inheritDoc}. */
+    @Generated
+    @Override
+    public CommunicationIdentityClientImplBuilder pipeline(HttpPipeline pipeline) {
+        this.pipeline = pipeline;
+        return this;
+    }
+
+    /*
+     * The HTTP client used to send the request.
      */
+    @Generated private HttpClient httpClient;
+
+    /** {@inheritDoc}. */
+    @Generated
+    @Override
+    public CommunicationIdentityClientImplBuilder httpClient(HttpClient httpClient) {
+        this.httpClient = httpClient;
+        return this;
+    }
+
+    /*
+     * The logging configuration for HTTP requests and responses.
+     */
+    @Generated private HttpLogOptions httpLogOptions;
+
+    /** {@inheritDoc}. */
+    @Generated
+    @Override
+    public CommunicationIdentityClientImplBuilder httpLogOptions(HttpLogOptions httpLogOptions) {
+        this.httpLogOptions = httpLogOptions;
+        return this;
+    }
+
+    /*
+     * The client options such as application ID and custom headers to set on a
+     * request.
+     */
+    @Generated private ClientOptions clientOptions;
+
+    /** {@inheritDoc}. */
+    @Generated
+    @Override
+    public CommunicationIdentityClientImplBuilder clientOptions(ClientOptions clientOptions) {
+        this.clientOptions = clientOptions;
+        return this;
+    }
+
+    /*
+     * The retry options to configure retry policy for failed requests.
+     */
+    @Generated private RetryOptions retryOptions;
+
+    /** {@inheritDoc}. */
+    @Generated
+    @Override
+    public CommunicationIdentityClientImplBuilder retryOptions(RetryOptions retryOptions) {
+        this.retryOptions = retryOptions;
+        return this;
+    }
+
+    /** {@inheritDoc}. */
+    @Generated
+    @Override
+    public CommunicationIdentityClientImplBuilder addPolicy(HttpPipelinePolicy customPolicy) {
+        pipelinePolicies.add(customPolicy);
+        return this;
+    }
+
+    /*
+     * The configuration store that is used during construction of the service
+     * client.
+     */
+    @Generated private Configuration configuration;
+
+    /** {@inheritDoc}. */
+    @Generated
+    @Override
+    public CommunicationIdentityClientImplBuilder configuration(Configuration configuration) {
+        this.configuration = configuration;
+        return this;
+    }
+
+    /*
+     * The service endpoint
+     */
+    @Generated private String endpoint;
+
+    /** {@inheritDoc}. */
+    @Generated
+    @Override
     public CommunicationIdentityClientImplBuilder endpoint(String endpoint) {
         this.endpoint = endpoint;
         return this;
@@ -57,7 +162,7 @@ public final class CommunicationIdentityClientImplBuilder {
     /*
      * Api Version
      */
-    private String apiVersion;
+    @Generated private String apiVersion;
 
     /**
      * Sets Api Version.
@@ -65,31 +170,16 @@ public final class CommunicationIdentityClientImplBuilder {
      * @param apiVersion the apiVersion value.
      * @return the CommunicationIdentityClientImplBuilder.
      */
+    @Generated
     public CommunicationIdentityClientImplBuilder apiVersion(String apiVersion) {
         this.apiVersion = apiVersion;
         return this;
     }
 
     /*
-     * The HTTP pipeline to send requests through
-     */
-    private HttpPipeline pipeline;
-
-    /**
-     * Sets The HTTP pipeline to send requests through.
-     *
-     * @param pipeline the pipeline value.
-     * @return the CommunicationIdentityClientImplBuilder.
-     */
-    public CommunicationIdentityClientImplBuilder pipeline(HttpPipeline pipeline) {
-        this.pipeline = pipeline;
-        return this;
-    }
-
-    /*
      * The serializer to serialize an object into a string
      */
-    private SerializerAdapter serializerAdapter;
+    @Generated private SerializerAdapter serializerAdapter;
 
     /**
      * Sets The serializer to serialize an object into a string.
@@ -97,57 +187,9 @@ public final class CommunicationIdentityClientImplBuilder {
      * @param serializerAdapter the serializerAdapter value.
      * @return the CommunicationIdentityClientImplBuilder.
      */
+    @Generated
     public CommunicationIdentityClientImplBuilder serializerAdapter(SerializerAdapter serializerAdapter) {
         this.serializerAdapter = serializerAdapter;
-        return this;
-    }
-
-    /*
-     * The HTTP client used to send the request.
-     */
-    private HttpClient httpClient;
-
-    /**
-     * Sets The HTTP client used to send the request.
-     *
-     * @param httpClient the httpClient value.
-     * @return the CommunicationIdentityClientImplBuilder.
-     */
-    public CommunicationIdentityClientImplBuilder httpClient(HttpClient httpClient) {
-        this.httpClient = httpClient;
-        return this;
-    }
-
-    /*
-     * The configuration store that is used during construction of the service
-     * client.
-     */
-    private Configuration configuration;
-
-    /**
-     * Sets The configuration store that is used during construction of the service client.
-     *
-     * @param configuration the configuration value.
-     * @return the CommunicationIdentityClientImplBuilder.
-     */
-    public CommunicationIdentityClientImplBuilder configuration(Configuration configuration) {
-        this.configuration = configuration;
-        return this;
-    }
-
-    /*
-     * The logging configuration for HTTP requests and responses.
-     */
-    private HttpLogOptions httpLogOptions;
-
-    /**
-     * Sets The logging configuration for HTTP requests and responses.
-     *
-     * @param httpLogOptions the httpLogOptions value.
-     * @return the CommunicationIdentityClientImplBuilder.
-     */
-    public CommunicationIdentityClientImplBuilder httpLogOptions(HttpLogOptions httpLogOptions) {
-        this.httpLogOptions = httpLogOptions;
         return this;
     }
 
@@ -155,7 +197,7 @@ public final class CommunicationIdentityClientImplBuilder {
      * The retry policy that will attempt to retry failed requests, if
      * applicable.
      */
-    private RetryPolicy retryPolicy;
+    @Generated private RetryPolicy retryPolicy;
 
     /**
      * Sets The retry policy that will attempt to retry failed requests, if applicable.
@@ -163,24 +205,9 @@ public final class CommunicationIdentityClientImplBuilder {
      * @param retryPolicy the retryPolicy value.
      * @return the CommunicationIdentityClientImplBuilder.
      */
+    @Generated
     public CommunicationIdentityClientImplBuilder retryPolicy(RetryPolicy retryPolicy) {
         this.retryPolicy = retryPolicy;
-        return this;
-    }
-
-    /*
-     * The list of Http pipeline policies to add.
-     */
-    private final List<HttpPipelinePolicy> pipelinePolicies;
-
-    /**
-     * Adds a custom Http pipeline policy.
-     *
-     * @param customPolicy The custom Http pipeline policy to add.
-     * @return the CommunicationIdentityClientImplBuilder.
-     */
-    public CommunicationIdentityClientImplBuilder addPolicy(HttpPipelinePolicy customPolicy) {
-        pipelinePolicies.add(customPolicy);
         return this;
     }
 
@@ -189,12 +216,13 @@ public final class CommunicationIdentityClientImplBuilder {
      *
      * @return an instance of CommunicationIdentityClientImpl.
      */
+    @Generated
     public CommunicationIdentityClientImpl buildClient() {
-        if (apiVersion == null) {
-            this.apiVersion = "2021-10-31-preview";
-        }
         if (pipeline == null) {
             this.pipeline = createHttpPipeline();
+        }
+        if (apiVersion == null) {
+            this.apiVersion = "2022-06-01";
         }
         if (serializerAdapter == null) {
             this.serializerAdapter = JacksonAdapter.createDefaultSerializerAdapter();
@@ -204,27 +232,47 @@ public final class CommunicationIdentityClientImplBuilder {
         return client;
     }
 
+    @Generated
     private HttpPipeline createHttpPipeline() {
         Configuration buildConfiguration =
                 (configuration == null) ? Configuration.getGlobalConfiguration() : configuration;
         if (httpLogOptions == null) {
             httpLogOptions = new HttpLogOptions();
         }
+        if (clientOptions == null) {
+            clientOptions = new ClientOptions();
+        }
         List<HttpPipelinePolicy> policies = new ArrayList<>();
         String clientName = properties.getOrDefault(SDK_NAME, "UnknownName");
         String clientVersion = properties.getOrDefault(SDK_VERSION, "UnknownVersion");
-        policies.add(
-                new UserAgentPolicy(httpLogOptions.getApplicationId(), clientName, clientVersion, buildConfiguration));
+        String applicationId = CoreUtils.getApplicationId(clientOptions, httpLogOptions);
+        policies.add(new UserAgentPolicy(applicationId, clientName, clientVersion, buildConfiguration));
+        policies.add(new RequestIdPolicy());
+        policies.add(new AddHeadersFromContextPolicy());
+        HttpHeaders headers = new HttpHeaders();
+        clientOptions.getHeaders().forEach(header -> headers.set(header.getName(), header.getValue()));
+        if (headers.getSize() > 0) {
+            policies.add(new AddHeadersPolicy(headers));
+        }
+        policies.addAll(
+                this.pipelinePolicies.stream()
+                        .filter(p -> p.getPipelinePosition() == HttpPipelinePosition.PER_CALL)
+                        .collect(Collectors.toList()));
         HttpPolicyProviders.addBeforeRetryPolicies(policies);
-        policies.add(retryPolicy == null ? new RetryPolicy() : retryPolicy);
+        policies.add(ClientBuilderUtil.validateAndGetRetryPolicy(retryPolicy, retryOptions, new RetryPolicy()));
+        policies.add(new AddDatePolicy());
         policies.add(new CookiePolicy());
-        policies.addAll(this.pipelinePolicies);
+        policies.addAll(
+                this.pipelinePolicies.stream()
+                        .filter(p -> p.getPipelinePosition() == HttpPipelinePosition.PER_RETRY)
+                        .collect(Collectors.toList()));
         HttpPolicyProviders.addAfterRetryPolicies(policies);
         policies.add(new HttpLoggingPolicy(httpLogOptions));
         HttpPipeline httpPipeline =
                 new HttpPipelineBuilder()
                         .policies(policies.toArray(new HttpPipelinePolicy[0]))
                         .httpClient(httpClient)
+                        .clientOptions(clientOptions)
                         .build();
         return httpPipeline;
     }
