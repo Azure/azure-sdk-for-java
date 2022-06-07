a0487a69f71e26d1eb0886f697960d8efb4ff573
Anushka Singh <anushkasingh16@users.noreply.github.com>
2022-06-07 10:37:24 -0700

    Added JSONObject implementation for updateCheckpoints and listCheckpoints

--- sdk/eventhubs/azure-messaging-eventhubs-checkpointstore-redis/src/main/java/module-info.java
+++ sdk/eventhubs/azure-messaging-eventhubs-checkpointstore-redis/src/main/java/module-info.java
@@ -5,4 +5,5 @@ module com.azure.messaging.eventhubs.checkpointstore.redis {
     requires transitive com.azure.core;
     requires redis.clients.jedis;
     requires com.azure.messaging.eventhubs;
+    requires org.json;
 }
