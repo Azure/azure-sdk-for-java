3ac2638f0288f5d33b381abcb014750f8c5d1db5
Charandeep Parisineti <cparisineti@microsoft.com>
2022-06-08 11:37:56 -0700

    Add session records

---
+++ sdk/communication/azure-communication-jobrouter/src/test/java/com.azure.communication.jobrouter/JobRouterClientTestBase.java
@@ -0,0 +1,65 @@
+package com.azure.communication.jobrouter;
+
+import com.azure.communication.common.implementation.CommunicationConnectionString;
+import com.azure.communication.common.implementation.HmacAuthenticationPolicy;
+import com.azure.core.credential.AzureKeyCredential;
+import com.azure.core.http.HttpClient;
+import com.azure.core.http.HttpPipeline;
+import com.azure.core.http.HttpPipelineBuilder;
+import com.azure.core.http.netty.NettyAsyncHttpClientBuilder;
+import com.azure.core.http.policy.*;
+import com.azure.core.test.TestBase;
+import com.azure.core.util.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+
+class JobRouterClientTestBase extends TestBase {
+    protected String getConnectionString() {
+        String connectionString = interceptorManager.isPlaybackMode()
+            ? "connectionString=https://localhost:8080;accesskey=secret"
+            : Configuration.getGlobalConfiguration().get("AZURE_TEST_JOBROUTER_CONNECTION_STRING");
+        Objects.requireNonNull(connectionString);
+        return connectionString;
+    }
+
+    <T> T clientSetup(Function<HttpPipeline, T> clientBuilder) {
+        HttpClient httpClient;
+
+        CommunicationConnectionString connectionString = new CommunicationConnectionString(getConnectionString());
+        AzureKeyCredential credential = new AzureKeyCredential(connectionString.getAccessKey());
+
+        // Closest to API goes first, closest to wire goes last.
+        final List<HttpPipelinePolicy> policies = new ArrayList<>();
+        policies.add(new RequestIdPolicy());
+        policies.add(new AddDatePolicy());
+
+        HttpPolicyProviders.addBeforeRetryPolicies(policies);
+
+        policies.add(new HmacAuthenticationPolicy(credential));
+
+        policies.add(new RetryPolicy());
+
+        HttpPolicyProviders.addAfterRetryPolicies(policies);
+        policies.add(new HttpLoggingPolicy(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BODY_AND_HEADERS)));
+
+        if (interceptorManager.isPlaybackMode()) {
+            httpClient = interceptorManager.getPlaybackClient();
+        } else {
+            httpClient = new NettyAsyncHttpClientBuilder().wiretap(true).build();
+        }
+        policies.add(interceptorManager.getRecordPolicy());
+
+        HttpPipeline pipeline = new HttpPipelineBuilder()
+            .policies(policies.toArray(new HttpPipelinePolicy[0]))
+            .httpClient(httpClient)
+            .build();
+
+        T client;
+        client = clientBuilder.apply(pipeline);
+
+        return Objects.requireNonNull(client);
+    }
+}
---
+++ sdk/communication/azure-communication-jobrouter/src/test/java/com.azure.communication.jobrouter/JobRouterClientTests.java
@@ -0,0 +1,39 @@
+package com.azure.communication.jobrouter;
+
+import com.azure.communication.jobrouter.models.DistributionPolicy;
+import com.azure.communication.jobrouter.models.RoundRobinMode;
+import com.azure.core.http.rest.Response;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JobRouterClientTests extends JobRouterClientTestBase {
+    private RouterClient routerClient;
+
+    @Override
+    protected void beforeTest() {
+        routerClient = clientSetup(httpPipeline -> new RouterClientBuilder()
+            .connectionString(getConnectionString())
+            .pipeline(httpPipeline)
+            .buildClient());
+    }
+
+    @Test
+    public void createDistributionPolicy() {
+        // Setup
+        DistributionPolicy distributionPolicy = new DistributionPolicy();
+        RoundRobinMode roundRobinMode = new RoundRobinMode();
+        roundRobinMode.setMinConcurrentOffers(1);
+        roundRobinMode.setMaxConcurrentOffers(10);
+        distributionPolicy.setMode(roundRobinMode);
+        distributionPolicy.setName("Test_Policy");
+        distributionPolicy.setOfferTtlSeconds(10.0);
+
+        // Action
+        String id = "Contoso_Jobs_Distribution_policy";
+        Response<DistributionPolicy> response = routerClient.upsertDistributionPolicy(id, distributionPolicy);
+
+        // Verify
+        assertEquals(200, response.getStatusCode());
+    }
+}
