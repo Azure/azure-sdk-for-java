5b4f658619db7232a30b247bd8b050e2eb205066
Victor Colin Amador <vicolina@microsoft.com>
2022-06-09 22:47:45 -0700

    Removed unused import.

--- sdk/core/azure-core/src/test/java/com/azure/core/http/policy/RetryPolicyTests.java
+++ sdk/core/azure-core/src/test/java/com/azure/core/http/policy/RetryPolicyTests.java
@@ -28,7 +28,6 @@ import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
