8919ed1b6f9180332a7ecd1428b91b019b1d09ab
Yashika Jain <t-yjain@microsoft.com>
2022-06-13 14:45:33 +0530

    ci and pom updated

---
+++ sdk/purview/azure-analytics-purview-administration/src/test/java/com/azure/analytics/purview/administration/MetadataPolicyClientTests.java
@@ -0,0 +1,49 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.analytics.purview.administration;
+
+import com.azure.core.http.rest.RequestOptions;
+import com.azure.core.util.BinaryData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import java.io.StringReader;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MetadataPolicyClientTests extends PurviewAccountClientTestBase {
+    private MetadataPolicyClient client;
+
+    @Override
+    protected void beforeTest() {
+        client = purviewMetadataClientBuilderSetUp().endpoint(getEndpoint()).buildMetadataPolicyClient();
+    }
+
+    @Test
+    public void testListPolicy() {
+        List<BinaryData> response = client.listAll(null).stream().collect(Collectors.toList());
+        System.out.println(response);
+        Assertions.assertTrue(response.size() > 0);
+    }
+
+    @Test
+    public void testUpdatePolicy() {
+        List<BinaryData> listResponse = client.listAll(null).stream().collect(Collectors.toList());
+        BinaryData item = listResponse.iterator().next();
+
+        JsonReader jsonReader = Json.createReader(new StringReader(item.toString()));
+        JsonObject policy = jsonReader.readObject();
+        String policyId = policy.getString("id");
+
+        RequestOptions requestOptions = new RequestOptions()
+            .addHeader("Content-Type", "application/json")
+            .setBody(item);
+
+        BinaryData response = client.updateWithResponse(policyId, requestOptions).getValue();
+        System.out.println(response);
+    }
+}
---
+++ sdk/purview/azure-analytics-purview-catalog/CHANGELOG.md
@@ -0,0 +1,40 @@
+# Release History
+
+## 1.0.0-beta.4 (Unreleased)
+
+### Features Added
+
+### Breaking Changes
+
+### Bugs Fixed
+
+### Other Changes
+
+## 1.0.0-beta.3 (2022-03-15)
+
+### Breaking Changes
+
+- Removed class `PurviewCatalogClientBuilder`. It was replaced by `CollectionClientBuilder`, `DiscoveryClientBuilder`, `EntityClientBuilder`, `GlossaryClientBuilder`, `LineageClientBuilder`, `RelationshipClientBuilder`, `TypesClientBuilder`.
+- Merged the `Context` parameter into the `RequestOptions` parameter in methods of clients.
+
+### Other Changes
+
+#### Dependency Updates
+
+- Upgraded `azure-core` to `1.26.0`.
+- Upgraded `azure-core-http-netty` to `1.11.8`.
+
+## 1.0.0-beta.2 (2021-10-15)
+
+### Breaking Changes
+
+This is a new version of LLC SDK. Changes are
+
+- A sync client and an async client for each operation group.
+- Add `RequestOptions` and `Context` to client method parameters.
+- Return type of client method is always `Response` except paging and long-running operations.
+- Allow users to set `ServiceVersion` in client builder.
+
+## 1.0.0-beta.1 (2021-05-11)
+
+- Initial beta release for Purview Catalog client library.
