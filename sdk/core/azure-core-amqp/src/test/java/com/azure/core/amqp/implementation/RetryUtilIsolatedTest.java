5e4ab8a4e56a147554bf59512a1ae0f0d228b308
Anu Thomas Chandy <anuamd@hotmail.com>
2022-06-15 14:19:17 -0700

    Adding license header

--- sdk/core/azure-core-amqp/src/test/java/com/azure/core/amqp/implementation/RetryUtilIsolatedTest.java
+++ sdk/core/azure-core-amqp/src/test/java/com/azure/core/amqp/implementation/RetryUtilIsolatedTest.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.azure.core.amqp.implementation;
 
 import com.azure.core.amqp.AmqpRetryMode;
