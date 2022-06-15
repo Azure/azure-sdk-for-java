08c830b7a1e8a2c5be01d8c2fe2e616ff12ecbea
Anushka Singh <anushkasingh16@users.noreply.github.com>
2022-06-15 14:10:13 -0700

    Removed methods  that were not needed, added try-catch for jedis operations

--- sdk/eventhubs/azure-messaging-eventhubs-checkpointstore-redis/src/main/java/com/azure/messaging/eventhubs/checkpointstore/redis/JedisRedisCheckpointStoreBuilder.java
+++ sdk/eventhubs/azure-messaging-eventhubs-checkpointstore-redis/src/main/java/com/azure/messaging/eventhubs/checkpointstore/redis/JedisRedisCheckpointStoreBuilder.java
@@ -6,26 +6,89 @@ import com.azure.core.credential.AzureKeyCredential;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 import redis.clients.jedis.Protocol;
-
-public final class JedisRedisCheckpointStoreBuilder {
-    private JedisPool pool;
+final class JedisRedisCheckpointStoreBuilder {
+    private String hostname = "";
+    private String key;
+    private JedisPool jedisPool;
+    private int operationTimeout = 1000;
+    private int connectionTimeout = 5 * operationTimeout;
+    private int maxPoolConnections = 200;
+    private int maxPoolIdleConnections = 100;
+    private int minPoolIdleConnections = 50;
+    private int maxWait = operationTimeout;
 
     /**
-     * Sets the JedisPool field
-     * @param jedisPool, a JedisPool object initialized using hostname and password of the Azure Redis Cache
-     * @return JedisRedisCheckpointStoreBuilder object
+     * Sets the hostname for the Azure Redis Cache associated with the CheckpointStore
+     * @param endpoint a String representing the hostname for Azure Redis Cache
+     * @return a JedisRedisCheckpointStoreBuilder instance
      */
-    public JedisRedisCheckpointStoreBuilder jedisPool(JedisPool jedisPool){
-        this.pool = jedisPool;
+    public JedisRedisCheckpointStoreBuilder endpoint(String endpoint) {
+        this.hostname = endpoint;
+        return this;
+    }
+    /**
+     * Sets the password for the Azure Redis Cache associated with the CheckpointStore
+     * @param key an AzureKeyCredential representing the key for Azure Redis Cache
+     * @return a JedisRedisCheckpointStoreBuilder instance
+     */
+    public JedisRedisCheckpointStoreBuilder credential(AzureKeyCredential key) {
+        this.key = key.getKey();
         return this;
     }
     /**
      * Builds an instance of the JedisRedisCheckpointStore class
      * @return JedisRedisCheckpointStore object
      */
-    public JedisRedisCheckpointStore build(){
-
-        return new JedisRedisCheckpointStore(pool);
+    public JedisRedisCheckpointStore build() {
+        boolean BLOCK_POOL_WHEN_EXHAUSTED = true;
+        String CLIENT_NAME = "clientName";
+        boolean USE_SSL = true;
+        int SSL_PORT = 6380;
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxTotal(maxPoolConnections);
+        poolConfig.setMaxIdle(maxPoolIdleConnections);
+        poolConfig.setBlockWhenExhausted(BLOCK_POOL_WHEN_EXHAUSTED);
+        poolConfig.setMaxWaitMillis(maxWait);
+        poolConfig.setMinIdle(minPoolIdleConnections);
+        jedisPool = new JedisPool(poolConfig, hostname, SSL_PORT, connectionTimeout, operationTimeout, key, Protocol.DEFAULT_DATABASE, CLIENT_NAME, USE_SSL, null, null, null);
+        return new JedisRedisCheckpointStore(jedisPool);
     }
-
+    /**
+     * Sets the timeout value for JedisPool operations
+     * @param operationTimeout an int representing the operation timeout value for the JedisPool
+     * @return a JedisRedisCheckpointStoreBuilder instance
+     */
+    public JedisRedisCheckpointStoreBuilder operationTimeout(int operationTimeout) {
+        this.operationTimeout = operationTimeout;
+        connectionTimeout = 5 * operationTimeout;
+        maxWait = operationTimeout;
+        return this;
+    }
+    /**
+     * Sets the maximum number of connections that the JedisPool object can create
+     * @param maxPoolConnections an int representing maximum connections the JedisPool can have
+     * @return a JedisRedisCheckpointStoreBuilder instance
+     */
+    public JedisRedisCheckpointStoreBuilder maxPoolConnections(int maxPoolConnections) {
+        this.maxPoolConnections = maxPoolConnections;
+        return this;
+    }
+    /**
+     * Sets the maximum number of idle connections that the JedisPool can have.
+     * @param maxPoolIdleConnections an int representing maximum idle connections the JedisPool can have
+     * @return a JedisRedisCheckpointStoreBuilder instance
+     */
+    public JedisRedisCheckpointStoreBuilder maxPoolIdleConnections(int maxPoolIdleConnections) {
+        this.maxPoolIdleConnections = maxPoolIdleConnections;
+        return this;
+    }
+    /**
+     * Sets the minimum number of idle connections that the JedisPool can have.
+     * @param minPoolIdleConnections an int representing the minimum number of connections that can be idle in a pool
+     * @return a JedisRedisCheckpointStoreBuilder instance
+     */
+    public JedisRedisCheckpointStoreBuilder minPoolIdleConnections(int minPoolIdleConnections) {
+        this.minPoolIdleConnections = minPoolIdleConnections;
+        return this;
+    }
 }
