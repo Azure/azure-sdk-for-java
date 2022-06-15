f02e3b48c2c7ad458ef31eaf77c1cd0c8e94ab57
Rick Ley <frley@microsoft.com>
2022-06-15 14:43:10 -0700

    Client side encryption v2 (#28974)
    * Updated some constants and added the encryptionDataV2 model
    
    * Refactored EncryptionData to be an interface
    
    * Added logic to encrypt using v2
    
    * Updates to encryption logic
    
    * Updated some field names and fixed a bug for larger uploads
    
    * Added FetchEncryptionVersionPolicy
    
    * Added v2 logic to EncryptedBlobRange
    
    * Got full blob downloads and one range test working
    
    * Modification to fix up range tests
    
    * Minor docs notes
    
    * Changes so decryption still happens when range header not specified
    
    * Moved specifying encryption version to the builder to align with other languages
    
    * Some basic cleanup
    
    * More cleanup
    
    * Updated existing tests to account for v2 encryption
    
    * Added encrypted blob range tests
    
    * Switched encryption version to be a parameter on the builder constructor
    
    * Some cleanup and switch to core's serializer
    
    * Made new recordings and updated logic around unencrypted downloads
    
    * Some ci cleanup
    
    * Ci fixes and more recordings
    
    * Fixed a bug in instantiating EncryptedBlobRange
    
    * ci fixes
    
    * Ci and test fixes. Passing request conditions on get props and checking download is not a failure
    
    * Updated recordings and some test fixes
    
    * more recordings
    
    * Fixed some live test
    
    * ci fixes
    
    * Merged the two encryption data types
    
    * UPdated recordings with new schema format
    
    * Updated some live tests
    
    * Moved functionality of FetchEncryptionVersionPolicy into BlobDecryptionPolicy
    
    * Updated recordings
    
    * ci fixes
    
    * Tried fixing ci
    
    * Removed getNBytes from test
    
    * Pr feedback
    
    * Updated keywrapping to prevent downgrade attack
    
    * Updated recordings
    
    * Removed java 9 method
    
    * Unused import
    
    * Fixed bug in parameterized test
    
    * Fixed live tests

--- sdk/storage/azure-storage-blob-cryptography/src/main/java/com/azure/storage/blob/specialized/cryptography/CryptographyConstants.java
+++ sdk/storage/azure-storage-blob-cryptography/src/main/java/com/azure/storage/blob/specialized/cryptography/CryptographyConstants.java
@@ -4,16 +4,22 @@
 package com.azure.storage.blob.specialized.cryptography;
 
 import com.azure.core.util.CoreUtils;
+import com.azure.storage.common.implementation.Constants;
+
 import java.util.Map;
 
 final class CryptographyConstants {
     static final String ENCRYPTION_PROTOCOL_V1 = "1.0";
+
+    static final String ENCRYPTION_PROTOCOL_V2 = "2.0";
 
     static final String AGENT_METADATA_KEY = "EncryptionLibrary";
 
     static final String AES_CBC_PKCS5PADDING = "AES/CBC/PKCS5Padding";
 
     static final String AES_CBC_NO_PADDING = "AES/CBC/NoPadding";
+
+    static final String AES_GCM_NO_PADDING = "AES/GCM/NoPadding";
 
     static final String AES = "AES";
 
@@ -22,6 +28,9 @@ final class CryptographyConstants {
     static final String AGENT_METADATA_VALUE = "JavaTrack2" + USER_AGENT_PROPERTIES.get("version");
 
     static final String ENCRYPTION_DATA_KEY = "encryptiondata";
+
+    static final String ENCRYPTION_METADATA_HEADER = Constants.HeaderConstants.X_MS_META + "-"
+        + ENCRYPTION_DATA_KEY;
 
     static final String ENCRYPTION_MODE = "FullBlob";
 
@@ -32,6 +41,12 @@ final class CryptographyConstants {
     static final String CONTENT_RANGE = "Content-Range";
 
     static final String CONTENT_LENGTH = "Content-Length";
+
+    static final int NONCE_LENGTH = 12;
+
+    static final int TAG_LENGTH = 16;
+
+    static final int GCM_ENCRYPTION_REGION_LENGTH = 4 * Constants.MB;
 
     static final String DECRYPT_UNENCRYPTED_BLOB = "Encryption client is being used but the blob metadata indicates "
         + "that it is not encrypted.";
