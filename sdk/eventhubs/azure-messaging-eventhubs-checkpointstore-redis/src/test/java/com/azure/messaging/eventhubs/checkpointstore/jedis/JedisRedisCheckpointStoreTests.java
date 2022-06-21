754b81eb0bfd8287ccc057516fb028bdde6ed7ca
Anushka Singh <anushkasingh16@users.noreply.github.com>
2022-06-21 15:48:44 -0700

    Removed comments and unused imports

--- sdk/eventhubs/azure-messaging-eventhubs-checkpointstore-redis/src/test/java/com/azure/messaging/eventhubs/checkpointstore/jedis/JedisRedisCheckpointStoreTests.java
+++ sdk/eventhubs/azure-messaging-eventhubs-checkpointstore-redis/src/test/java/com/azure/messaging/eventhubs/checkpointstore/jedis/JedisRedisCheckpointStoreTests.java
@@ -2,10 +2,6 @@
 // Licensed under the MIT License.
 package com.azure.messaging.eventhubs.checkpointstore.jedis;
 
-
-import com.azure.core.credential.AzureKeyCredential;
-import com.azure.core.util.serializer.JacksonAdapter;
-import com.azure.messaging.eventhubs.models.Checkpoint;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockitoAnnotations;
@@ -14,7 +10,6 @@ import org.mockito.MockitoAnnotations;
  * Unit tests for {@link  JedisRedisCheckpointStore}
  */
 public class JedisRedisCheckpointStoreTests {
-    JacksonAdapter jacksonAdapter = new JacksonAdapter();
     @Test
     public void testListCheckpoints() {
 
