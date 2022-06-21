afc9cbead1a6c762ee98d692c78e439729fcf119
Anushka Singh <anushkasingh16@users.noreply.github.com>
2022-06-21 16:56:34 -0700

    Revert "Removed builder class and changed package name"
    This reverts commit 7d92eb6a

--- sdk/eventhubs/azure-messaging-eventhubs-checkpointstore-redis/src/main/java/module-info.java
+++ sdk/eventhubs/azure-messaging-eventhubs-checkpointstore-redis/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-module com.azure.messaging.eventhubs.checkpointstore.jedis {
+module com.azure.messaging.eventhubs.checkpointstore.redis {
     requires transitive com.azure.core;
     requires redis.clients.jedis;
     requires com.azure.messaging.eventhubs;
