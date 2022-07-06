d0c0e264e93beb446331dcf65fec96d4a29658cf
Muyao Feng <v-muyaofeng@microsoft.com>
2022-07-06 11:24:23 +0800

    test cosmos

--- sdk/spring/spring-cloud-azure-integration-tests/src/test/java/com/azure/spring/cloud/integration/tests/cosmos/CosmosIT.java
+++ sdk/spring/spring-cloud-azure-integration-tests/src/test/java/com/azure/spring/cloud/integration/tests/cosmos/CosmosIT.java
@@ -1,10 +1,23 @@
 package com.azure.spring.cloud.integration.tests.cosmos;
 
 
+import com.azure.cosmos.CosmosClient;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 
 
 @ActiveProfiles("cosmos")
 public class CosmosIT {
 
+    @Autowired
+    private static CosmosClient client;
+
+    @Test
+    public void testCosmosOperation() {
+        Assertions.assertThrows(RuntimeException.class, () -> client.getDatabase("users"));
+        client.createDatabase("users");
+        Assertions.assertNotNull(client.getDatabase("users"));
+    }
 }
