5bb7acd5470ca80daab0a4eca0539b3bdc9edfad
Kamil Sobol <61715331+kasobol-msft@users.noreply.github.com>
2022-07-07 13:38:13 -0700

    fix stress tests. (#29845)

--- sdk/storage/azure-storage-perf/src/main/java/com/azure/storage/blob/perf/core/AbstractUploadTest.java
+++ sdk/storage/azure-storage-perf/src/main/java/com/azure/storage/blob/perf/core/AbstractUploadTest.java
@@ -4,23 +4,16 @@
 package com.azure.storage.blob.perf.core;
 
 import com.azure.storage.blob.perf.BlobPerfStressOptions;
-import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
-import static com.azure.perf.test.core.TestDataCreationHelper.createRandomByteBufferFlux;
-
 public abstract class AbstractUploadTest<TOptions extends BlobPerfStressOptions> extends BlobTestBase<TOptions> {
 
+    /**
+     * This ctor makes sure that each upload tests targets different blob.
+     * @param options options
+     */
     public AbstractUploadTest(TOptions options) {
         super(options, BLOB_NAME_PREFIX + UUID.randomUUID());
-    }
-
-    @Override
-    public Mono<Void> setupAsync() {
-        // Upload one blob per test
-        return super.setupAsync()
-            .then(blobAsyncClient.upload(createRandomByteBufferFlux(options.getSize()), null))
-            .then();
     }
 }
