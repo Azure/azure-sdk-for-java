23ca83e97726e7dafbbe2fb4a019372e3aded98e
Muyao Feng <v-muyaofeng@microsoft.com>
2022-07-08 09:44:21 +0800

    test cosmos

--- sdk/spring/spring-cloud-azure-integration-tests/src/test/java/com/azure/spring/cloud/integration/tests/cosmos/CosmosIT.java
+++ sdk/spring/spring-cloud-azure-integration-tests/src/test/java/com/azure/spring/cloud/integration/tests/cosmos/CosmosIT.java
@@ -2,7 +2,6 @@ package com.azure.spring.cloud.integration.tests.cosmos;
 
 
 import com.azure.cosmos.CosmosClient;
-import com.azure.cosmos.CosmosClientBuilder;
 import com.azure.cosmos.CosmosContainer;
 import com.azure.cosmos.CosmosDatabase;
 import com.azure.cosmos.models.CosmosQueryRequestOptions;
@@ -11,32 +10,25 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 
-@SpringBootTest(classes = CosmosIT.class)
+@SpringBootTest
 @ActiveProfiles("cosmos")
 public class CosmosIT {
     private static final Logger LOGGER = LoggerFactory.getLogger(CosmosIT.class);
     private final String databaseName = "products";
     private final String containerName = "users";
     private final String partitionKeyPath = "/country";
-    @Value("${spring.cloud.azure.cosmos.endpoint1}")
-    private String endpoint;
-    @Value("${spring.cloud.azure.cosmos.key1}")
-    private String key;
+
+    @Autowired
+    private CosmosClient client;
 
     @Test
     public void testCosmosOperation() {
         LOGGER.info("CosmosIT begin.");
-        LOGGER.info("Start creating cosmos client");
-        CosmosClient client = new CosmosClientBuilder()
-            .endpoint(endpoint)
-            .key(key)
-            .buildClient();
-        LOGGER.info("Finish creating cosmos client");
         LOGGER.info("Start creating cosmos database");
         client.createDatabaseIfNotExists(databaseName);
         CosmosDatabase database = client.getDatabase(databaseName);
