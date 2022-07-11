ebba17e6c3287caed15dea8f58d6feed0e598def
Kamil Sobol <kasobol@microsoft.com>
2022-07-11 10:58:08 -0700

    more docs.

--- sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/resource/BlobStorageResources.java
+++ sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/resource/BlobStorageResources.java
@@ -5,14 +5,27 @@ import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.common.resource.StorageResource;
 import com.azure.storage.common.resource.StorageResourceContainer;
 
+/**
+ * A class with factories for blob storage resources.
+ */
 public final class BlobStorageResources {
     private BlobStorageResources() {
     }
 
+    /**
+     * Creates {@link StorageResource} representing a blob.
+     * @param blobClient The blob client.
+     * @return A {@link StorageResource} representing a blob.
+     */
     public static StorageResource blob(BlobClient blobClient) {
         return new BlobStorageResource(blobClient);
     }
 
+    /**
+     * Creates {@link StorageResourceContainer} representing a blob container.
+     * @param blobContainerClient The blob container client.
+     * @return A {@link StorageResourceContainer} representing a blob container.
+     */
     public static StorageResourceContainer blobContainer(BlobContainerClient blobContainerClient) {
         return new BlobStorageResourceContainer(blobContainerClient);
     }
--- sdk/storage/azure-storage-file-share/src/main/java/com/azure/storage/file/share/resource/ShareStorageResources.java
+++ sdk/storage/azure-storage-file-share/src/main/java/com/azure/storage/file/share/resource/ShareStorageResources.java
@@ -6,20 +6,38 @@ import com.azure.storage.file.share.ShareClient;
 import com.azure.storage.file.share.ShareDirectoryClient;
 import com.azure.storage.file.share.ShareFileClient;
 
+/**
+ * A class with factories for file share resources.
+ */
 public final class ShareStorageResources {
 
     private ShareStorageResources(){
     }
 
+    /**
+     * Creates {@link StorageResourceContainer} representing a share.
+     * @param shareClient The share client.
+     * @return A {@link StorageResourceContainer} representing a share.
+     */
     public static StorageResourceContainer share(ShareClient shareClient) {
         return new ShareStorageResourceContainer(shareClient);
     }
 
+    /**
+     * Creates {@link StorageResourceContainer} representing a directory on the share.
+     * @param directoryClient The directory client.
+     * @return A {@link StorageResourceContainer} representing a directory on the share.
+     */
     public static StorageResourceContainer directory(ShareDirectoryClient directoryClient) {
         return new ShareDirectoryStorageResourceContainer(directoryClient);
     }
 
-    public static StorageResource file(ShareFileClient file) {
-        return new ShareFileStorageResource(file);
+    /**
+     * Creates {@link StorageResource} representing a file on the share.
+     * @param shareFileClient The share client.
+     * @return A {@link StorageResource} representing a file on the share.
+     */
+    public static StorageResource file(ShareFileClient shareFileClient) {
+        return new ShareFileStorageResource(shareFileClient);
     }
 }
