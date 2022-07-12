4c9181d6c4f0221290c8c787f58978b8b93e22cf
Sameeksha Vaity <savaity@microsoft.com>
2022-07-12 10:28:58 -0700

    update content source

--- sdk/formrecognizer/azure-ai-formrecognizer/src/main/java/com/azure/ai/formrecognizer/administration/models/ContentSource.java
+++ sdk/formrecognizer/azure-ai-formrecognizer/src/main/java/com/azure/ai/formrecognizer/administration/models/ContentSource.java
@@ -23,4 +23,5 @@ import com.fasterxml.jackson.annotation.JsonTypeName;
     @JsonSubTypes.Type(name = "web", value = WebContentSource.class)
 })
 @Immutable
-public abstract class ContentSource { }
+public abstract class ContentSource {
+}
