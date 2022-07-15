475309c49925e3a5c738f67bc84d0367c05897c2
Kamil Sobol <kasobol@microsoft.com>
2022-07-15 11:35:34 -0700

    checkstyle.

--- sdk/storage/azure-storage-common/src/main/java/com/azure/storage/common/datamover/DataMover.java
+++ sdk/storage/azure-storage-common/src/main/java/com/azure/storage/common/datamover/DataMover.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.azure.storage.common.datamover;
 
 import com.azure.storage.common.resource.StorageResource;
--- sdk/storage/azure-storage-common/src/main/java/com/azure/storage/common/datamover/DataMoverBuilder.java
+++ sdk/storage/azure-storage-common/src/main/java/com/azure/storage/common/datamover/DataMoverBuilder.java
@@ -1,7 +1,17 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.azure.storage.common.datamover;
 
+/**
+ * Builds the DataMover
+ */
 public class DataMoverBuilder {
 
+    /**
+     * Builds the data mover.
+     * @return The Data Mover.
+     */
     public DataMover build() {
         return new DataMover();
     }
