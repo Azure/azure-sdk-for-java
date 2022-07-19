e7c2bf9b3333497d558845b1e0ec4e92f4fed1b3
anushkasingh16 <anushkasingh16@users.noreply.github.com>
2022-07-19 13:14:47 -0700

    Added setup and cleanup functions and filled in the errorProcessor/eventProcessor

--- sdk/eventhubs/azure-messaging-eventhubs-track2-perf/src/main/java/com/azure/messaging/eventhubs/perf/EventProcessorJedisTest.java
+++ sdk/eventhubs/azure-messaging-eventhubs-track2-perf/src/main/java/com/azure/messaging/eventhubs/perf/EventProcessorJedisTest.java
@@ -2,13 +2,11 @@
 // Licensed under the MIT License.
 package com.azure.messaging.eventhubs.perf;
 
+import com.azure.messaging.eventhubs.EventProcessorClient;
 import com.azure.messaging.eventhubs.EventProcessorClientBuilder;
 import com.azure.messaging.eventhubs.checkpointstore.jedis.JedisRedisCheckpointStore;
 import com.azure.messaging.eventhubs.models.ErrorContext;
-import com.azure.messaging.eventhubs.models.EventBatchContext;
 import com.azure.messaging.eventhubs.models.EventContext;
-import com.azure.perf.test.core.MockErrorContext;
-import com.azure.perf.test.core.MockEventContext;
 import reactor.core.publisher.Mono;
 import redis.clients.jedis.JedisPool;
 import com.azure.perf.test.core.EventPerfTest;
@@ -19,10 +17,8 @@ import java.util.function.Consumer;
 
 public class EventProcessorJedisTest extends EventPerfTest<EventProcessorJedisOptions> {
 
-    private EventProcessorClientBuilder builder;
-
     //TODO 1: Add expected behavior to all consumer methods
-
+    private EventProcessorClient eventProcessorClient;
     /**
      * Creates an instance of performance test.
      *
@@ -37,24 +33,35 @@ public class EventProcessorJedisTest extends EventPerfTest<EventProcessorJedisOp
         JedisPool jedisPool = new JedisPool(options.getHostName(), 6380, options.getUserName(), options.getPassword());
         JedisRedisCheckpointStore checkpointStore = new JedisRedisCheckpointStore(jedisPool);
 
-        Consumer<ErrorContext> errorProcessor = errorContext -> {
-            System.out.println("Placeholder for processing errors.");
-        };
+        Consumer<ErrorContext> errorProcessor = errorContext -> super.errorRaised(errorContext.getThrowable());;
         Consumer<EventContext> eventProcessor = eventContext -> {
-            System.out.println("Placeholder for processing events.");
+            super.eventRaised();
+            eventContext.updateCheckpoint();
         };
-
-        builder = new EventProcessorClientBuilder()
+        eventProcessorClient = new EventProcessorClientBuilder()
             .connectionString(options.getConnectionString(), options.getEventHubName())
             .consumerGroup(options.getConsumerGroup())
             .checkpointStore(checkpointStore)
             .processError(errorProcessor)
-            .processEvent(eventProcessor);
-
+            .processEvent(eventProcessor)
+            .buildEventProcessorClient();
     }
 
     //TODO 2: setUpAsync
+    @Override
+    public Mono<Void> setupAsync() {
+        return super.setupAsync().then( Mono.defer(() -> {
+            eventProcessorClient.start();
+            return Mono.empty();
+            }));
+    }
 
     //TODO 3: cleanUpAsync
-
+    @Override
+    public Mono<Void> cleanupAsync() {
+        return Mono.defer(() -> {
+            eventProcessorClient.stop();
+            return Mono.empty();
+        }).then(super.cleanupAsync());
+    }
 }
