22ed383f2ef2fa21a2ebf2c0f0d228e3d9d23966
Connie <conniey@microsoft.com>
2022-07-24 10:50:57 -0700

    Using ConcurrentLinkedQueue to avoid ConcurrentModificationException while iterating array.

--- sdk/eventhubs/azure-messaging-eventhubs/src/test/java/com/azure/messaging/eventhubs/implementation/ReactorShimTest.java
+++ sdk/eventhubs/azure-messaging-eventhubs/src/test/java/com/azure/messaging/eventhubs/implementation/ReactorShimTest.java
@@ -10,15 +10,14 @@ import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.LongStream;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 /**
  * Tests for {@link ReactorShim}.
@@ -28,15 +27,15 @@ public class ReactorShimTest {
     /**
      * Test to validate the {@link ReactorShim#windowTimeout(Flux, int, Duration)} facade honor backpressure.
      * <p>
-     * Today the ReactorShim (type local to Event Hubs SDK) is always guaranteed to use Reactor 3.4.19 or above,
-     * which has backpressure aware windowTimeout. In the future, if the ReactorShim moves to azure-core, and if
-     * we decide to have a CI pipeline for the lower Reactor version (below 3.4.19), then this test needs to be
-     * adjusted to execute conditionally (i.e. run only for Reactor version >= 3.4.19 setup).
+     * Today the ReactorShim (type local to Event Hubs SDK) is always guaranteed to use Reactor 3.4.19 or above, which
+     * has backpressure aware windowTimeout. In the future, if the ReactorShim moves to azure-core, and if we decide to
+     * have a CI pipeline for the lower Reactor version (below 3.4.19), then this test needs to be adjusted to execute
+     * conditionally (i.e. run only for Reactor version >= 3.4.19 setup).
      * <p>
-     * This is the replica, with additional asserts, of windowTimeout test that Azure Event Hubs crew contributed
-     * to Reactor while developing the backpressure aware windowTimeout operator, which got integrated into
-     * the Reactor test suite. The test is not using a virtual Timer scheduler (May have to check with the Reactor
-     * team to understand if it is safe to do so for this time-involved operator, and not invalidate any cases).
+     * This is the replica, with additional asserts, of windowTimeout test that Azure Event Hubs crew contributed to
+     * Reactor while developing the backpressure aware windowTimeout operator, which got integrated into the Reactor
+     * test suite. The test is not using a virtual Timer scheduler (May have to check with the Reactor team to
+     * understand if it is safe to do so for this time-involved operator, and not invalidate any cases).
      */
     @Test
     public void windowTimeoutShouldHonorBackpressure() throws InterruptedException {
@@ -68,7 +67,7 @@ public class ReactorShimTest {
         //    Given the Event producer produces at most 4 events per second (due to 250 ms delay between events),
         //    the consumer should receive 3-4 events.
         //
-        final List<Integer> batchSizes = new ArrayList<>(300);
+        final ConcurrentLinkedQueue<Integer> batchSizes = new ConcurrentLinkedQueue<>();
         final int maxWindowSize = 10;
         final int eventConsumeDelayInMillis = 0;
         final Scheduler scheduler = Schedulers.newBoundedElastic(10, 10000, "queued-tasks");
@@ -100,13 +99,14 @@ public class ReactorShimTest {
             final boolean acquired = isCompleted.tryAcquire(durationToPublish.toMillis(), TimeUnit.MILLISECONDS);
             if (acquired) {
                 if (errorReference.get() != null) {
-                    assertFalse(true, "'isCompleted' should have been false because the producer"
+                    fail("'isCompleted' should have been false because the producer"
                         + " should not be terminating, but terminated with an error:" + errorReference.get());
                 } else {
-                    assertFalse(true, "'isCompleted' should have been false because the producer"
+                    fail("'isCompleted' should have been false because the producer"
                         + " should not be terminating, but terminated to completion.");
                 }
             }
+
             for (Integer batchSize : batchSizes) {
                 assertTrue(batchSize <= maxWindowSize, "Unexpected batch size " + batchSize);
             }
