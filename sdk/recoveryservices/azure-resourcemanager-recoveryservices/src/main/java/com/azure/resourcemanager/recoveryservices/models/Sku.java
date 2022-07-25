63158242272d2b1c457f423c0ce2249f5d5ef849
azure-sdk <azuresdk@microsoft.com>
2022-07-25 01:38:47 +0000

    [Automation] Generate Fluent Lite from recoveryservices#package-2022-04

--- sdk/recoveryservices/azure-resourcemanager-recoveryservices/src/main/java/com/azure/resourcemanager/recoveryservices/models/Sku.java
+++ sdk/recoveryservices/azure-resourcemanager-recoveryservices/src/main/java/com/azure/resourcemanager/recoveryservices/models/Sku.java
@@ -12,7 +12,10 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 @Fluent
 public final class Sku {
     /*
-     * The Sku name.
+     * Name of SKU is RS0 (Recovery Services 0th version) and the tier is
+     * standard tier. They do not have affect on backend storage redundancy or
+     * any other vault settings. To manage storage redundancy, use the
+     * backupstorageconfig
      */
     @JsonProperty(value = "name", required = true)
     private SkuName name;
@@ -42,7 +45,9 @@ public final class Sku {
     private String capacity;
 
     /**
-     * Get the name property: The Sku name.
+     * Get the name property: Name of SKU is RS0 (Recovery Services 0th version) and the tier is standard tier. They do
+     * not have affect on backend storage redundancy or any other vault settings. To manage storage redundancy, use the
+     * backupstorageconfig.
      *
      * @return the name value.
      */
@@ -51,7 +56,9 @@ public final class Sku {
     }
 
     /**
-     * Set the name property: The Sku name.
+     * Set the name property: Name of SKU is RS0 (Recovery Services 0th version) and the tier is standard tier. They do
+     * not have affect on backend storage redundancy or any other vault settings. To manage storage redundancy, use the
+     * backupstorageconfig.
      *
      * @param name the name value to set.
      * @return the Sku object itself.
