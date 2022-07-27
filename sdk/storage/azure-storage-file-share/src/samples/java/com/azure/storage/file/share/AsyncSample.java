6208407924b6249bfd19c546c6817b23f7cd9358
Rick Ley <frley@microsoft.com>
2022-07-27 14:03:57 -0700

    Fixed some javadoc samples

--- sdk/storage/azure-storage-file-share/src/samples/java/com/azure/storage/file/share/AsyncSample.java
+++ sdk/storage/azure-storage-file-share/src/samples/java/com/azure/storage/file/share/AsyncSample.java
@@ -30,7 +30,7 @@ public class AsyncSample {
                                                             .buildAsyncClient();
         // Create a share
         String shareName = generateRandomName();
-        fileServiceAsyncClient.createShareWithResponse(shareName, (ShareCreateOptions) null, null).subscribe(
+        fileServiceAsyncClient.createShareWithResponse(shareName, null).subscribe(
             response -> System.out.printf("Successfully created a share with status code: %d.",
                 response.getStatusCode()),
             err -> System.out.println("Failed to create a share. Reasons: " + err.getMessage()),
