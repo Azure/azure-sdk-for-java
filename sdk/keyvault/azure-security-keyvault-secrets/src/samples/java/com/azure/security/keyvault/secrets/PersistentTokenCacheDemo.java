54d04d435e8682db6e2d04fce717d1f730147242
vcolin7 <vicolina@microsoft.com>
2022-07-28 15:40:15 -0500

    Apply suggestions to shared credential sample

--- sdk/keyvault/azure-security-keyvault-secrets/src/samples/java/com/azure/security/keyvault/secrets/PersistentTokenCacheDemo.java
+++ sdk/keyvault/azure-security-keyvault-secrets/src/samples/java/com/azure/security/keyvault/secrets/PersistentTokenCacheDemo.java
@@ -8,27 +8,26 @@ import com.azure.identity.SharedTokenCacheCredential;
 import com.azure.identity.SharedTokenCacheCredentialBuilder;
 
 /**
- * Sample showing how to authenticate to key vault with a shared token cache credential.
+ * Sample showing how to authenticate to Key Vault with a shared token cache credential.
  */
 public class PersistentTokenCacheDemo {
-
     /**
      * Authenticates from shared token cache and gets a secret.
+     *
      * @param args Unused. Arguments to the program.
      */
     public static void main(String[] args) {
-
-        // Wrote to AZURE_USERNAME env variable
+        // Wrote to AZURE_USERNAME env variable.
         SharedTokenCacheCredential defaultCredential = new SharedTokenCacheCredentialBuilder()
-            .clientId("04b07795-8ddb-461a-bbee-02f9e1bf7b46")
+            .clientId("<YOUR_CLIENT_ID>")
             .build();
 
         SecretClient client = new SecretClientBuilder()
-            .vaultUrl("https://persistentcachedemo.vault.azure.net")
+            .vaultUrl("<YOUR_KEY_VAULT_URL>")
             .credential(defaultCredential)
             .buildClient();
 
-        // Try to get a secret! Only works if you are logged in
+        // Try to get a secret! Only works if you are logged in.
         System.out.println("\nWhat is the super secret secret?\n\n");
         KeyVaultSecret secret = client.getSecret("the-secret");
         System.out.println("Secret was found: " + secret.getValue());
