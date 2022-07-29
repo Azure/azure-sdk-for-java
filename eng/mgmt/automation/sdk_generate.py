bc6f6ded47934bfbad4a32294d5adfba1cb51b8a
Zhou Zheng <zhouzheng@microsoft.com>
2022-07-29 11:41:52 +0800

    sdk generation pipeline support java dpg
    Signed-off-by: Zhou Zheng <zhouzheng@microsoft.com>

--- eng/mgmt/automation/sdk_generate.py
+++ eng/mgmt/automation/sdk_generate.py
@@ -6,12 +6,17 @@ import json
 import glob
 import logging
 import argparse
+from typing import List
 
 pwd = os.getcwd()
 os.chdir(os.path.abspath(os.path.dirname(sys.argv[0])))
 from parameters import *
 from utils import set_or_increase_version
-from generate_data import sdk_automation as sdk_automation_data
+from generate_data import (
+    get_sdk_readme_abspath,
+    sdk_automation_readme,
+    update_readme,
+)
 from generate_utils import (
     compare_with_maven_package,
     compile_package,
@@ -19,6 +24,7 @@ from generate_utils import (
     get_and_update_service_from_api_specs,
     get_suffix_from_api_specs,
 )
+
 os.chdir(pwd)
 
 
@@ -40,15 +46,13 @@ def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '--spec-root',
-        default =
-        'https://raw.githubusercontent.com/Azure/azure-rest-api-specs/main/',
+        default='https://raw.githubusercontent.com/Azure/azure-rest-api-specs/main/',
         help = 'Spec root folder',
     )
     parser.add_argument(
         '-r',
         '--readme',
-        help =
-        'Readme path, Sample: "storage" or "specification/storage/resource-manager/readme.md"',
+        help='Readme path, Sample: "storage" or "specification/storage/resource-manager/readme.md"',
     )
     parser.add_argument('-t', '--tag', help = 'Specific tag')
     parser.add_argument('-v', '--version', help = 'Specific sdk version')
@@ -89,12 +93,52 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
+def codegen_sdk_automation(config: dict) -> List[dict]:
+    # priority:
+    # 1. autorestConfig from input
+    # 2. swagger/README.md in sdk repository that matches readme from input
+
+    base_dir = os.path.abspath(os.path.dirname(sys.argv[0]))
+    sdk_root = os.path.abspath(os.path.join(base_dir, SDK_ROOT))
+    spec_root = os.path.abspath(config['specFolder'])
+
+    packages = []
+
+    # find readme.md in spec repository
+    if not config['relatedReadmeMdFile']:
+        return packages
+
+    readme_file_path = config['relatedReadmeMdFile']
+    match = re.search(
+        r'(specification)?/?([^/]+)/data-plane(/.*)*/readme.md',
+        readme_file_path,
+        re.IGNORECASE,
+    )
+    if not match:
+        logging.info(
+            '[Skip] readme path:%s does not format as specification/([^/]+)/data-plane(/.*)*/readme.md',
+            readme_file_path
+        )
+        return packages
+
+    logging.info('[RESOLVE] README from specification %s', readme_file_path)
+    sdk_readme_abspath = get_sdk_readme_abspath(config, readme_file_path)
+
+    if sdk_readme_abspath:
+        spec_readme_abspath = os.path.join(spec_root, readme_file_path)
+        update_readme(sdk_readme_abspath, spec_readme_abspath)
+        sdk_automation_readme(sdk_readme_abspath, packages, sdk_root)
+
+    return packages
+
+
 def sdk_automation(input_file: str, output_file: str):
     base_dir = os.path.abspath(os.path.dirname(sys.argv[0]))
     sdk_root = os.path.abspath(os.path.join(base_dir, SDK_ROOT))
     api_specs_file = os.path.join(base_dir, API_SPECS_FILE)
     with open(input_file, 'r') as fin:
         config = json.load(fin)
+        logging.info(f"sdk_automation input: {config}")
 
     packages = []
     readme = config['relatedReadmeMdFile']
@@ -122,12 +166,12 @@ def sdk_automation(input_file: str, output_file: str):
         tag = None
         if service == 'resources':
             with open(os.path.join(config['specFolder'], readme)) as fin:
-                tag_match = re.search('tag: (package-resources-[\S]+)',
+                tag_match = re.search(r'tag: (package-resources-\S+)',
                                         fin.read())
                 if tag_match:
                     tag = tag_match.group(1)
                 else:
-                    tag = 'package-resources-2020-10'
+                    tag = 'package-resources-2021-01'
 
         module = ARTIFACT_FORMAT.format(service)
         output_folder = OUTPUT_FOLDER_FORMAT.format(service)
@@ -162,23 +206,23 @@ def sdk_automation(input_file: str, output_file: str):
                 'eng/versioning',
                 'pom.xml',
             ],
-            'artifacts': [
-                '{0}/pom.xml'.format(output_folder),
-            ] + [
-                jar for jar in glob.glob('{0}/target/*.jar'.format(
-                    output_folder))
-            ],
-            'result':
-                'succeeded' if succeeded else 'failed',
-            'packageFolder':
-                output_folder,
+            'readmeMd': [readme],
+            'artifacts': ['{0}/pom.xml'.format(output_folder)] +
+                         [jar for jar in glob.glob('{0}/target/*.jar'.format(output_folder))],
+            'apiViewArtifact': next(iter(glob.glob('{0}/target/*-sources.jar'.format(output_folder))), None),
+            'language': 'Java',
+            'result': 'succeeded' if succeeded else 'failed',
+            'packageFolder': output_folder,
         })
 
         update_parameters(pre_suffix)
 
     if not packages:
         # try data-plane codegen
-        packages = sdk_automation_data(config)
+        packages = codegen_sdk_automation(config)
+        for package in packages:
+            if len(package['path']) > 0:
+                package['packageFolder'] = package['path'][0]
 
     with open(output_file, 'w') as fout:
         output = {
@@ -237,8 +281,7 @@ def main():
             compare_with_maven_package(sdk_root, service, stable_version,
                                     current_version, module)
 
-            if args.get('auto_commit_external_change') and args.get(
-                    'user_name') and args.get('user_email'):
+            if args.get('auto_commit_external_change') and args.get('user_name') and args.get('user_email'):
                 pwd = os.getcwd()
                 try:
                     os.chdir(sdk_root)
