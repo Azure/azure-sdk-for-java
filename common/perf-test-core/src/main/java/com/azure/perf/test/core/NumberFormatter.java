2fae1dd26625e5fdd769e651af925ec28fc11daa
Mike Harder <mharder@microsoft.com>
2022-07-28 22:00:11 -0700

    Improve zero, update comments

--- common/perf-test-core/src/main/java/com/azure/perf/test/core/NumberFormatter.java
+++ common/perf-test-core/src/main/java/com/azure/perf/test/core/NumberFormatter.java
@@ -8,20 +8,21 @@ import java.lang.Math;
 public final class NumberFormatter {
     private NumberFormatter() { }
 
-    // Formats a double with the specified minimum number of significant digits.
-    // Digits to the left of the decimal point are never dropped.
+    // Formats a number with a minimum number of significant digits.
+    // Digits to the left of the decimal point are always significant.
     // Examples:
+    // - Format(0, 4) -> "0.000"
     // - Format(12345, 4) -> "12,345"
     // - Format(1.2345, 4) -> "1.235"
-    // - Format(0.00012345, 4) -> "0.0001234"
+    // - Format(0.00012345, 4) -> "0.0001235"
     public static String Format(double value, int minSignificantDigits) {
         if (minSignificantDigits < 0) {
             throw new IllegalArgumentException("minSignificantDigits must be greater than zero");
         }
 
-        // Signficant digits are undefined for the number zero, so hardcode to string "0".
+        // Special case since log(0) is undefined
         if (value == 0) {
-            return "0";
+            return String.format("%." + (minSignificantDigits - 1) + "f", value);
         }
 
         double log = Math.log10(Math.abs(value));
