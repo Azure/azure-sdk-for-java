6390d4ae579e3797fbc544fc02572d905167a1fb
Vinay Gera <vinay_work@hotmail.com>
2022-08-01 07:04:36 -0700

    update

--- sdk/identity/azure-identity/src/samples/Azure-Cache-For-Redis/Jedis/java/com/azure/jedis/sample/HellloWorld.java
+++ sdk/identity/azure-identity/src/samples/Azure-Cache-For-Redis/Jedis/java/com/azure/jedis/sample/HellloWorld.java
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.azure.sample;
+package com.azure.jedis.sample;
 
 import com.azure.core.credential.TokenRequestContext;
 import com.azure.identity.DefaultAzureCredential;
