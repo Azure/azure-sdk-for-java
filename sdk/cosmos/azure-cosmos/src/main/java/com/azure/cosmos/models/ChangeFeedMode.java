1156724de085e8e654c3ef057577e86089b58ae8
Kushagra Thapar <kuthapar@microsoft.com>
2022-08-09 12:18:07 -0700

    Added comments to Change Feed mode

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/models/ChangeFeedMode.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/models/ChangeFeedMode.java
@@ -11,11 +11,13 @@ import com.azure.cosmos.util.Beta;
 @Beta(value = Beta.SinceVersion.V4_35_0, warningText = Beta.PREVIEW_SUBJECT_TO_CHANGE_WARNING)
 public enum ChangeFeedMode {
     /**
-     * Incremental mode
+     * Incremental mode is the latest version change only. This mode only provides changes for Create, Replace and Upsert operations.
+     * Only the most recent change for a given item is included in the change log. Intermediate changes may not be available.
      */
     INCREMENTAL,
     /**
-     * Full Fidelity mode
+     * Full Fidelity model is all version changes. This mode provides changes for Create, Replace, Upsert and Delete operations.
+     * All changes for a given item are included in the changes log.
      */
     FULL_FIDELITY
 }
