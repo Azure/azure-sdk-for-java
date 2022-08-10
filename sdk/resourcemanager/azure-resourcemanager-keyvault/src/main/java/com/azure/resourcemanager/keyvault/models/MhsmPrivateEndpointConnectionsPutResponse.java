01bdc3f9396d308fdcb5a53ee4dfbb78259d175e
Xiaofei Cao <92354331+XiaofeiCao@users.noreply.github.com>
2022-08-10 15:49:57 +0800

    mgmt update keyvault to 2021-10-01 (#30337)
    * codegen
    
    * fix test
    
    * session records
    
    * purge original key before restoring
    
    * changelog
    
    * nit, remove unused import
    
    * related session records
    
    * breaking changes
    
    * remove keys.json and secrets.json due to duplication of data-plane
    
    * add note

--- sdk/resourcemanager/azure-resourcemanager-keyvault/src/main/java/com/azure/resourcemanager/keyvault/models/MhsmPrivateEndpointConnectionsPutResponse.java
+++ sdk/resourcemanager/azure-resourcemanager-keyvault/src/main/java/com/azure/resourcemanager/keyvault/models/MhsmPrivateEndpointConnectionsPutResponse.java
@@ -7,32 +7,36 @@ package com.azure.resourcemanager.keyvault.models;
 import com.azure.core.http.HttpHeaders;
 import com.azure.core.http.HttpRequest;
 import com.azure.core.http.rest.ResponseBase;
-import com.azure.resourcemanager.keyvault.fluent.models.PrivateEndpointConnectionInner;
+import com.azure.resourcemanager.keyvault.fluent.models.MhsmPrivateEndpointConnectionInner;
 
-/** Contains all response data for the delete operation. */
-public final class PrivateEndpointConnectionsDeleteResponse
-    extends ResponseBase<PrivateEndpointConnectionsDeleteHeaders, PrivateEndpointConnectionInner> {
+/** Contains all response data for the put operation. */
+public final class MhsmPrivateEndpointConnectionsPutResponse
+    extends ResponseBase<MhsmPrivateEndpointConnectionsPutHeaders, MhsmPrivateEndpointConnectionInner> {
     /**
-     * Creates an instance of PrivateEndpointConnectionsDeleteResponse.
+     * Creates an instance of MhsmPrivateEndpointConnectionsPutResponse.
      *
-     * @param request the request which resulted in this PrivateEndpointConnectionsDeleteResponse.
+     * @param request the request which resulted in this MhsmPrivateEndpointConnectionsPutResponse.
      * @param statusCode the status code of the HTTP response.
      * @param rawHeaders the raw headers of the HTTP response.
      * @param value the deserialized value of the HTTP response.
      * @param headers the deserialized headers of the HTTP response.
      */
-    public PrivateEndpointConnectionsDeleteResponse(
+    public MhsmPrivateEndpointConnectionsPutResponse(
         HttpRequest request,
         int statusCode,
         HttpHeaders rawHeaders,
-        PrivateEndpointConnectionInner value,
-        PrivateEndpointConnectionsDeleteHeaders headers) {
+        MhsmPrivateEndpointConnectionInner value,
+        MhsmPrivateEndpointConnectionsPutHeaders headers) {
         super(request, statusCode, rawHeaders, value, headers);
     }
 
-    /** @return the deserialized response body. */
+    /**
+     * Gets the deserialized response body.
+     *
+     * @return the deserialized response body.
+     */
     @Override
-    public PrivateEndpointConnectionInner getValue() {
+    public MhsmPrivateEndpointConnectionInner getValue() {
         return super.getValue();
     }
 }
