6aadf1e08e2cce4be399455bea671aeb1c69a3e9
Shili Chen <v-shilichen@microsoft.com>
2022-08-11 08:57:12 +0800

    Fix parameter missing when using OBO process

--- sdk/spring/spring-cloud-azure-autoconfigure/src/test/java/com/azure/spring/cloud/autoconfigure/aad/implementation/oauth2/AadOAuth2ClientConfigurationTests.java
+++ sdk/spring/spring-cloud-azure-autoconfigure/src/test/java/com/azure/spring/cloud/autoconfigure/aad/implementation/oauth2/AadOAuth2ClientConfigurationTests.java
@@ -6,11 +6,18 @@ package com.azure.spring.cloud.autoconfigure.aad.implementation.oauth2;
 import com.azure.spring.cloud.autoconfigure.aad.AadClientRegistrationRepository;
 import com.azure.spring.cloud.autoconfigure.aad.configuration.AadOAuth2ClientConfiguration;
 import com.azure.spring.cloud.autoconfigure.aad.properties.AadAuthenticationProperties;
+import com.nimbusds.jose.jwk.JWK;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.test.context.runner.WebApplicationContextRunner;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.ClientAuthorizationException;
+import org.springframework.security.oauth2.client.JwtBearerOAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizationContext;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.jwt.Jwt;
 
 import java.util.Set;
 
@@ -20,6 +27,9 @@ import static com.azure.spring.cloud.autoconfigure.aad.implementation.WebApplica
 import static com.azure.spring.cloud.autoconfigure.aad.implementation.WebApplicationContextRunnerUtils.webApplicationContextRunner;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
 class AadOAuth2ClientConfigurationTests {
 
@@ -140,5 +150,106 @@ class AadOAuth2ClientConfigurationTests {
                 assertThat(graphScopes).containsOnly("https://graph.microsoft.com/User.Read");
                 assertThat(customScopes).containsOnly("api://52261059-e515-488e-84fd-a09a3f372814/File.Read");
             });
+    }
+
+    @Test
+    void testJwtBearerOAuth2AuthorizedClientProviderAuthExceptionWhenUsingPrivateKeyJwtMethod() {
+        OAuth2ClientAuthenticationJwkResolver jwkResolver = spy(new TestOAuth2ClientAuthenticationJwkResolver());
+        resourceServerWithOboContextRunner()
+            .withBean(OAuth2ClientAuthenticationJwkResolver.class, () -> jwkResolver)
+            .withUserConfiguration(AadOAuth2ClientConfiguration.class)
+            .withPropertyValues(
+                "spring.cloud.azure.active-directory.enabled=true",
+                "spring.cloud.azure.active-directory.credential.client-certificate-path=/test/test.pfx",
+                "spring.cloud.azure.active-directory.credential.client-certificate-password=test",
+                "spring.cloud.azure.active-directory.authorization-clients.graph.scopes=https://graph.microsoft.com/User.Read",
+                "spring.cloud.azure.active-directory.authorization-clients.graph.authorization-grant-type=urn:ietf:params:oauth:grant-type:jwt-bearer",
+                "spring.cloud.azure.active-directory.authorization-clients.graph.scopes=api://52261059-e515-488e-84fd-a09a3f372814/File.Read",
+                "spring.cloud.azure.active-directory.authorization-clients.graph.client-authentication-method=private_key_jwt"
+            )
+            .run(context -> {
+                final JwtBearerOAuth2AuthorizedClientProvider jwtBearerProvider = context.getBean(
+                    JwtBearerOAuth2AuthorizedClientProvider.class);
+                ClientRegistrationRepository clientRepository = context.getBean(
+                    ClientRegistrationRepository.class);
+
+                assertThat(jwtBearerProvider).isNotNull();
+                ObjectProvider<OAuth2ClientAuthenticationJwkResolver> resolvers =
+                    context.getBeanProvider(OAuth2ClientAuthenticationJwkResolver.class);
+                assertThat(resolvers.getIfUnique()).isNotNull();
+
+                OAuth2AuthorizationContext auth2AuthorizationContext = mock(OAuth2AuthorizationContext.class);
+                when(auth2AuthorizationContext.getClientRegistration()).thenReturn(clientRepository.findByRegistrationId("graph"));
+
+                Authentication authentication = mock(Authentication.class);
+                when(auth2AuthorizationContext.getPrincipal()).thenReturn(authentication);
+
+                Jwt jwt = mock(Jwt.class);
+                when(authentication.getPrincipal()).thenReturn(jwt);
+
+                ClientAuthorizationException exception = null;
+                try {
+                    jwtBearerProvider.authorize(auth2AuthorizationContext);
+                } catch (ClientAuthorizationException ex) {
+                    exception = ex;
+                } finally {
+                    assertThat(exception).isNotNull();
+                    assertThat(exception.getMessage())
+                        .isEqualTo("[invalid_key] Failed to resolve JWK signing key for client registration 'graph'.");
+                }
+            });
+    }
+
+    @Test
+    void testJwtBearerOAuth2AuthorizedClientProviderAuthorizeWhenNotUsingPrivateKeyJwtMethod() {
+        resourceServerWithOboContextRunner()
+            .withUserConfiguration(AadOAuth2ClientConfiguration.class)
+            .withPropertyValues(
+                "spring.cloud.azure.active-directory.enabled=true",
+                "spring.cloud.azure.active-directory.authorization-clients.graph.scopes=https://graph.microsoft.com/User.Read",
+                "spring.cloud.azure.active-directory.authorization-clients.graph.authorization-grant-type=on_behalf_of",
+                "spring.cloud.azure.active-directory.authorization-clients.graph.scopes=api://52261059-e515-488e-84fd-a09a3f372814/File.Read"
+            )
+            .run(context -> {
+                final JwtBearerOAuth2AuthorizedClientProvider jwtBearerProvider = context.getBean(
+                    JwtBearerOAuth2AuthorizedClientProvider.class);
+                ClientRegistrationRepository clientRepository = context.getBean(
+                    ClientRegistrationRepository.class);
+
+                assertThat(jwtBearerProvider).isNotNull();
+                ObjectProvider<OAuth2ClientAuthenticationJwkResolver> resolvers =
+                    context.getBeanProvider(OAuth2ClientAuthenticationJwkResolver.class);
+                assertThat(resolvers.getIfUnique()).isNull();
+
+                OAuth2AuthorizationContext auth2AuthorizationContext = mock(OAuth2AuthorizationContext.class);
+                when(auth2AuthorizationContext.getClientRegistration())
+                    .thenReturn(clientRepository.findByRegistrationId("graph"));
+
+                Authentication authentication = mock(Authentication.class);
+                when(auth2AuthorizationContext.getPrincipal()).thenReturn(authentication);
+
+                Jwt jwt = mock(Jwt.class);
+                when(authentication.getPrincipal()).thenReturn(jwt);
+                when(jwt.getTokenValue()).thenReturn("dummy-token");
+
+                ClientAuthorizationException exception = null;
+                try {
+                    jwtBearerProvider.authorize(auth2AuthorizationContext);
+                } catch (ClientAuthorizationException ex) {
+                    exception = ex;
+                } finally {
+                    assertThat(exception).isNotNull();
+                    assertThat(exception.getMessage())
+                        .contains("[invalid_request] AADSTS50027: JWT token is invalid or malformed.");
+                }
+            });
+    }
+
+    class TestOAuth2ClientAuthenticationJwkResolver implements OAuth2ClientAuthenticationJwkResolver {
+
+        @Override
+        public JWK resolve(ClientRegistration clientRegistration) {
+            return null;
+        }
     }
 }
