323fc9b3e3ac77aa04b8c4e83589d15f0aa1a5b9
Franko Morales <fmorales@microsoft.com>
2022-08-12 11:50:38 -0700

    Adding Play Test

--- sdk/communication/azure-communication-callingserver/src/test/java/com/azure/communication/callingserver/EventHandlerUnitTests.java
+++ sdk/communication/azure-communication-callingserver/src/test/java/com/azure/communication/callingserver/EventHandlerUnitTests.java
@@ -5,36 +5,63 @@ package com.azure.communication.callingserver;
 
 import com.azure.communication.callingserver.models.RecordingState;
 import com.azure.communication.callingserver.models.events.CallAutomationEventBase;
+import com.azure.communication.callingserver.models.events.CallConnectedEvent;
+import com.azure.communication.callingserver.models.events.ParticipantsUpdatedEvent;
+import com.azure.communication.callingserver.models.events.PlayCompleted;
 import com.azure.communication.callingserver.models.events.RecordingStateChangedEvent;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class EventHandlerUnitTests {
+    static final String EVENT_PARTICIPANT_UPDATED = "{\"id\":\"61069ef9-5ca9-457f-ac36-e2bb5e8400ca\",\"source\":\"calling/callConnections/401f3500-62bd-46a9-8c09-9e1b06caca01/ParticipantsUpdated\",\"type\":\"Microsoft.Communication.ParticipantsUpdated\",\"data\":{\"participants\":[{\"rawId\":\"8:acs:816df1ca-971b-44d7-b8b1-8fba90748500_00000013-2ff6-dd51-54b7-a43a0d001998\",\"kind\":\"communicationUser\",\"communicationUser\":{\"id\":\"8:acs:816df1ca-971b-44d7-b8b1-8fba90748500_00000013-2ff6-dd51-54b7-a43a0d001998\"}},{\"rawId\":\"8:acs:816df1ca-971b-44d7-b8b1-8fba90748500_00000013-2ff7-1579-99bf-a43a0d0010bc\",\"kind\":\"communicationUser\",\"communicationUser\":{\"id\":\"8:acs:816df1ca-971b-44d7-b8b1-8fba90748500_00000013-2ff7-1579-99bf-a43a0d0010bc\"}}],\"type\":\"participantsUpdated\",\"callConnectionId\":\"401f3500-62bd-46a9-8c09-9e1b06caca01\",\"correlationId\":\"ebd8bf1f-0794-494f-bdda-913042c06ef7\"},\"time\":\"2022-08-12T03:35:07.9129474+00:00\",\"specversion\":\"1.0\",\"datacontenttype\":\"application/json\",\"subject\":\"calling/callConnections/401f3500-62bd-46a9-8c09-9e1b06caca01/ParticipantsUpdated\"}";
+    static final String EVENT_CALL_CONNECTED = "{\"id\":\"46116fb7-27e0-4a99-9478-a659c8fd4815\",\"source\":\"calling/callConnections/401f3500-62bd-46a9-8c09-9e1b06caca01/CallConnected\",\"type\":\"Microsoft.Communication.CallConnected\",\"data\":{\"type\":\"callConnected\",\"callConnectionId\":\"401f3500-62bd-46a9-8c09-9e1b06caca01\",\"correlationId\":\"ebd8bf1f-0794-494f-bdda-913042c06ef7\"},\"time\":\"2022-08-12T03:35:07.8174402+00:00\",\"specversion\":\"1.0\",\"datacontenttype\":\"application/json\",\"subject\":\"calling/callConnections/401f3500-62bd-46a9-8c09-9e1b06caca01/CallConnected\"}";
+
+    @Test
+    public void parseEvent() {
+        CallAutomationEventBase callAutomationEventBase = EventHandler.parseEvent(EVENT_PARTICIPANT_UPDATED);
+
+        assertNotNull(callAutomationEventBase);
+        assertEquals(callAutomationEventBase.getClass(), ParticipantsUpdatedEvent.class);
+        assertNotNull(((ParticipantsUpdatedEvent) callAutomationEventBase).getParticipants());
+    }
+
+    @Test
+    public void parseEventList() {
+        List<CallAutomationEventBase> callAutomationEventBaseList = EventHandler.parseEventList("["
+            + EVENT_CALL_CONNECTED + "," + EVENT_PARTICIPANT_UPDATED + "]");
+
+        assertNotNull(callAutomationEventBaseList);
+        assertEquals(callAutomationEventBaseList.get(0).getClass(), CallConnectedEvent.class);
+        assertEquals(callAutomationEventBaseList.get(1).getClass(), ParticipantsUpdatedEvent.class);
+        assertNotNull(callAutomationEventBaseList.get(0).getCallConnectionId());
+    }
 
     @Test
     public void parseRecordingStateChangedEvent() {
-        String receivedEvent = "[\n" +
-            "    {\n" +
-            "        \"id\": \"bf59843a-888f-47ca-8d1c-885c1f5e71dc\",\n" +
-            "        \"source\": \"calling/recordings/serverCallId/recordingId/recordingId/RecordingStateChanged\",\n" +
-            "        \"type\": \"Microsoft.Communication.CallRecordingStateChanged\",\n" +
-            "        \"data\": {\n" +
-            "            \"type\": \"recordingStateChanged\",\n" +
-            "            \"recordingId\": \"recordingId\",\n" +
-            "            \"state\": \"active\",\n" +
-            "            \"startDateTime\": \"2022-08-11T23:42:45.4394211+00:00\",\n" +
-            "            \"callConnectionId\": \"callConnectionId\",\n" +
-            "            \"serverCallId\": \"serverCallId\",\n" +
-            "            \"correlationId\": \"correlationId\"\n" +
-            "        },\n" +
-            "        \"time\": \"2022-08-11T23:42:45.5346632+00:00\",\n" +
-            "        \"specversion\": \"1.0\",\n" +
-            "        \"datacontenttype\": \"application/json\",\n" +
-            "        \"subject\": \"calling/recordings/serverCallId/recordingId/recordingId/RecordingStateChanged\"\n" +
-            "    }\n" +
-            "]";
+        String receivedEvent = "[\n"
+            + "    {\n"
+            + "        \"id\": \"bf59843a-888f-47ca-8d1c-885c1f5e71dc\",\n"
+            + "        \"source\": \"calling/recordings/serverCallId/recordingId/recordingId/RecordingStateChanged\",\n"
+            + "        \"type\": \"Microsoft.Communication.CallRecordingStateChanged\",\n"
+            + "        \"data\": {\n"
+            + "            \"type\": \"recordingStateChanged\",\n"
+            + "            \"recordingId\": \"recordingId\",\n"
+            + "            \"state\": \"active\",\n"
+            + "            \"startDateTime\": \"2022-08-11T23:42:45.4394211+00:00\",\n"
+            + "            \"callConnectionId\": \"callConnectionId\",\n"
+            + "            \"serverCallId\": \"serverCallId\",\n"
+            + "            \"correlationId\": \"correlationId\"\n"
+            + "        },\n"
+            + "        \"time\": \"2022-08-11T23:42:45.5346632+00:00\",\n"
+            + "        \"specversion\": \"1.0\",\n"
+            + "        \"datacontenttype\": \"application/json\",\n"
+            + "        \"subject\": \"calling/recordings/serverCallId/recordingId/recordingId/RecordingStateChanged\"\n"
+            + "    }\n"
+            + "]";
         CallAutomationEventBase event = EventHandler.parseEvent(receivedEvent);
         assertNotNull(event);
         RecordingStateChangedEvent recordingEvent = (RecordingStateChangedEvent) event;
@@ -42,5 +69,16 @@ public class EventHandlerUnitTests {
         assertEquals("serverCallId", recordingEvent.getServerCallId());
         assertEquals("recordingId", recordingEvent.getRecordingId());
         assertEquals(RecordingState.ACTIVE, recordingEvent.getRecordingState());
+    }
+
+    @Test
+    public void parsePlayCompletedEvent() {
+        String receivedEvent = "";
+        CallAutomationEventBase event = EventHandler.parseEvent(receivedEvent);
+        assertNotNull(event);
+        PlayCompleted playCompleted = (PlayCompleted) event;
+        assertNotNull(playCompleted);
+        assertEquals("serverCallId", playCompleted.getServerCallId());
+        assertEquals(200, playCompleted.getResultInfo().getCode());
     }
 }
