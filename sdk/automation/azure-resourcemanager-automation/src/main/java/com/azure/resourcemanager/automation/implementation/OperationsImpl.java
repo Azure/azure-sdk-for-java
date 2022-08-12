4b94763a7cc94f4f36c1bc18f121401da8e53814
Xiaofei Cao <92354331+XiaofeiCao@users.noreply.github.com>
2022-08-12 12:04:15 +0800

    local code generation (#30428)

--- sdk/automation/azure-resourcemanager-automation/src/main/java/com/azure/resourcemanager/automation/implementation/OperationsImpl.java
+++ sdk/automation/azure-resourcemanager-automation/src/main/java/com/azure/resourcemanager/automation/implementation/OperationsImpl.java
@@ -11,10 +11,9 @@ import com.azure.resourcemanager.automation.fluent.OperationsClient;
 import com.azure.resourcemanager.automation.fluent.models.OperationInner;
 import com.azure.resourcemanager.automation.models.Operation;
 import com.azure.resourcemanager.automation.models.Operations;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public final class OperationsImpl implements Operations {
-    @JsonIgnore private final ClientLogger logger = new ClientLogger(OperationsImpl.class);
+    private static final ClientLogger LOGGER = new ClientLogger(OperationsImpl.class);
 
     private final OperationsClient innerClient;
 
