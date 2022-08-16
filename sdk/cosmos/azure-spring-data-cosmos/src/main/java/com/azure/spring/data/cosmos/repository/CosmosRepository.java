db4cd5f715910458dbe8bccda61b5dd5ba7853bd
Shili Chen <v-shilichen@microsoft.com>
2022-08-16 17:13:17 +0800

    Make the Spring Dta Cosmos compatibility with Spring Data Common 3.0.0.M4

--- sdk/cosmos/azure-spring-data-cosmos/src/main/java/com/azure/spring/data/cosmos/repository/CosmosRepository.java
+++ sdk/cosmos/azure-spring-data-cosmos/src/main/java/com/azure/spring/data/cosmos/repository/CosmosRepository.java
@@ -44,5 +44,111 @@ public interface CosmosRepository<T, ID extends Serializable> extends PagingAndS
      */
     Iterable<T> findAll(PartitionKey partitionKey);
 
+    /**
+     * Saves a given entity. Use the returned instance for further operations as the save operation might have changed the
+     * entity instance completely.
+     *
+     * @param entity must not be {@literal null}.
+     * @return the saved entity; will never be {@literal null}.
+     * @param <S> the saved entity type.
+     * @throws IllegalArgumentException in case the given {@literal entity} is {@literal null}.
+     */
+    <S extends T> S save(S entity);
+
+    /**
+     * Saves all given entities.
+     *
+     * @param entities must not be {@literal null} nor must it contain {@literal null}.
+     * @return the saved entities; will never be {@literal null}. The returned {@literal Iterable} will have the same size
+     *         as the {@literal Iterable} passed as an argument.
+     * @param <S> the saved entity type.
+     * @throws IllegalArgumentException in case the given {@link Iterable entities} or one of its entities is
+     *           {@literal null}.
+     */
+    <S extends T> Iterable<S> saveAll(Iterable<S> entities);
+
+    /**
+     * Retrieves an entity by its id.
+     *
+     * @param id must not be {@literal null}.
+     * @return the entity with the given id or {@literal Optional#empty()} if none found.
+     * @throws IllegalArgumentException if {@literal id} is {@literal null}.
+     */
+    Optional<T> findById(ID id);
+
+    /**
+     * Returns whether an entity with the given id exists.
+     *
+     * @param id must not be {@literal null}.
+     * @return {@literal true} if an entity with the given id exists, {@literal false} otherwise.
+     * @throws IllegalArgumentException if {@literal id} is {@literal null}.
+     */
+    boolean existsById(ID id);
+
+    /**
+     * Returns all instances of the type.
+     *
+     * @return all entities
+     */
+    Iterable<T> findAll();
+
+    /**
+     * Returns all instances of the type {@code T} with the given IDs.
+     * <p>
+     * If some or all ids are not found, no entities are returned for these IDs.
+     * <p>
+     * Note that the order of elements in the result is not guaranteed.
+     *
+     * @param ids must not be {@literal null} nor contain any {@literal null} values.
+     * @return guaranteed to be not {@literal null}. The size can be equal or less than the number of given
+     *         {@literal ids}.
+     * @throws IllegalArgumentException in case the given {@link Iterable ids} or one of its items is {@literal null}.
+     */
+    Iterable<T> findAllById(Iterable<ID> ids);
+
+    /**
+     * Returns the number of entities available.
+     *
+     * @return the number of entities.
+     */
+    long count();
+
+    /**
+     * Deletes the entity with the given id.
+     *
+     * @param id must not be {@literal null}.
+     * @throws IllegalArgumentException in case the given {@literal id} is {@literal null}
+     */
+    void deleteById(ID id);
+
+    /**
+     * Deletes a given entity.
+     *
+     * @param entity must not be {@literal null}.
+     * @throws IllegalArgumentException in case the given entity is {@literal null}.
+     */
+    void delete(T entity);
+
+    /**
+     * Deletes all instances of the type {@code T} with the given IDs.
+     *
+     * @param ids must not be {@literal null}. Must not contain {@literal null} elements.
+     * @throws IllegalArgumentException in case the given {@literal ids} or one of its elements is {@literal null}.
+     * @since 2.5
+     */
+    void deleteAllById(Iterable<? extends ID> ids);
+
+    /**
+     * Deletes the given entities.
+     *
+     * @param entities must not be {@literal null}. Must not contain {@literal null} elements.
+     * @throws IllegalArgumentException in case the given {@literal entities} or one of its entities is {@literal null}.
+     */
+    void deleteAll(Iterable<? extends T> entities);
+
+    /**
+     * Deletes all entities managed by the repository.
+     */
+    void deleteAll();
 }
 
