ff80cb911f0e0c0ec96e2ab1a729ea70843fc54d
Shili Chen <v-shilichen@microsoft.com>
2022-08-16 18:09:53 +0800

    Make the Spring Dta Cosmos compatibility with Spring Data Common 3.0.0.M4

--- sdk/cosmos/azure-spring-data-cosmos/src/main/java/com/azure/spring/data/cosmos/repository/ReactiveCosmosRepository.java
+++ sdk/cosmos/azure-spring-data-cosmos/src/main/java/com/azure/spring/data/cosmos/repository/ReactiveCosmosRepository.java
@@ -3,8 +3,8 @@
 package com.azure.spring.data.cosmos.repository;
 
 import com.azure.cosmos.models.PartitionKey;
-import org.reactivestreams.Publisher;
 import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.data.repository.reactive.ReactiveSortingRepository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -12,7 +12,7 @@ import reactor.core.publisher.Mono;
  * Repository interface with search and delete operation
  */
 @NoRepositoryBean
-public interface ReactiveCosmosRepository<T, K> extends ReactiveSortingRepository<T, K> {
+public interface ReactiveCosmosRepository<T, K> extends ReactiveCrudRepository<T, K>, ReactiveSortingRepository<T, K> {
 
     /**
      * Retrieves an entity by its id and partition key.
@@ -38,178 +38,4 @@ public interface ReactiveCosmosRepository<T, K> extends ReactiveSortingRepositor
      * @return {@link Flux} of items with partition key value
      */
     Flux<T> findAll(PartitionKey partitionKey);
-
-    /**
-     * Saves a given entity. Use the returned instance for further operations as the save operation might have changed the
-     * entity instance completely.
-     *
-     * @param entity must not be {@literal null}.
-     * @return {@link Mono} emitting the saved entity.
-     * @param <S> the saved entity type.
-     * @throws IllegalArgumentException in case the given {@literal entity} is {@literal null}.
-     */
-    <S extends T> Mono<S> save(S entity);
-
-    /**
-     * Saves all given entities.
-     *
-     * @param entities must not be {@literal null}.
-     * @return {@link Flux} emitting the saved entities.
-     * @param <S> the saved entity type.
-     * @throws IllegalArgumentException in case the given {@link Iterable entities} or one of its entities is
-     *           {@literal null}.
-     */
-    <S extends T> Flux<S> saveAll(Iterable<S> entities);
-
-    /**
-     * Saves all given entities.
-     *
-     * @param entityStream must not be {@literal null}.
-     * @return {@link Flux} emitting the saved entities.
-     * @param <S> the saved entity type.
-     * @throws IllegalArgumentException in case the given {@link Publisher entityStream} is {@literal null}.
-     */
-    <S extends T> Flux<S> saveAll(Publisher<S> entityStream);
-
-    /**
-     * Retrieves an entity by its id.
-     *
-     * @param id must not be {@literal null}.
-     * @return {@link Mono} emitting the entity with the given id or {@link Mono#empty()} if none found.
-     * @throws IllegalArgumentException in case the given {@literal id} is {@literal null}.
-     */
-    Mono<T> findById(K id);
-
-    /**
-     * Retrieves an entity by its id supplied by a {@link Publisher}.
-     *
-     * @param id must not be {@literal null}. Uses the first emitted element to perform the find-query.
-     * @return {@link Mono} emitting the entity with the given id or {@link Mono#empty()} if none found.
-     * @throws IllegalArgumentException in case the given {@link Publisher id} is {@literal null}.
-     */
-    Mono<T> findById(Publisher<K> id);
-
-    /**
-     * Returns whether an entity with the given {@literal id} exists.
-     *
-     * @param id must not be {@literal null}.
-     * @return {@link Mono} emitting {@literal true} if an entity with the given id exists, {@literal false} otherwise.
-     * @throws IllegalArgumentException in case the given {@literal id} is {@literal null}.
-     */
-    Mono<Boolean> existsById(K id);
-
-    /**
-     * Returns whether an entity with the given id, supplied by a {@link Publisher}, exists. Uses the first emitted
-     * element to perform the exists-query.
-     *
-     * @param id must not be {@literal null}.
-     * @return {@link Mono} emitting {@literal true} if an entity with the given id exists, {@literal false} otherwise.
-     * @throws IllegalArgumentException in case the given {@link Publisher id} is {@literal null}.
-     */
-    Mono<Boolean> existsById(Publisher<K> id);
-
-    /**
-     * Returns all instances of the type.
-     *
-     * @return {@link Flux} emitting all entities.
-     */
-    Flux<T> findAll();
-
-    /**
-     * Returns all instances of the type {@code T} with the given IDs.
-     * <p>
-     * If some or all ids are not found, no entities are returned for these IDs.
-     * <p>
-     * Note that the order of elements in the result is not guaranteed.
-     *
-     * @param ids must not be {@literal null} nor contain any {@literal null} values.
-     * @return {@link Flux} emitting the found entities. The size can be equal or less than the number of given
-     *         {@literal ids}.
-     * @throws IllegalArgumentException in case the given {@link Iterable ids} or one of its items is {@literal null}.
-     */
-    Flux<T> findAllById(Iterable<K> ids);
-
-    /**
-     * Returns all instances of the type {@code T} with the given IDs supplied by a {@link Publisher}.
-     * <p>
-     * If some or all ids are not found, no entities are returned for these IDs.
-     * <p>
-     * Note that the order of elements in the result is not guaranteed.
-     *
-     * @param idStream must not be {@literal null}.
-     * @return {@link Flux} emitting the found entities.
-     * @throws IllegalArgumentException in case the given {@link Publisher idStream} is {@literal null}.
-     */
-    Flux<T> findAllById(Publisher<K> idStream);
-
-    /**
-     * Returns the number of entities available.
-     *
-     * @return {@link Mono} emitting the number of entities.
-     */
-    Mono<Long> count();
-
-    /**
-     * Deletes the entity with the given id.
-     *
-     * @param id must not be {@literal null}.
-     * @return {@link Mono} signaling when operation has completed.
-     * @throws IllegalArgumentException in case the given {@literal id} is {@literal null}.
-     */
-    Mono<Void> deleteById(K id);
-
-    /**
-     * Deletes the entity with the given id supplied by a {@link Publisher}.
-     *
-     * @param id must not be {@literal null}.
-     * @return {@link Mono} signaling when operation has completed.
-     * @throws IllegalArgumentException in case the given {@link Publisher id} is {@literal null}.
-     */
-    Mono<Void> deleteById(Publisher<K> id);
-
-    /**
-     * Deletes a given entity.
-     *
-     * @param entity must not be {@literal null}.
-     * @return {@link Mono} signaling when operation has completed.
-     * @throws IllegalArgumentException in case the given entity is {@literal null}.
-     */
-    Mono<Void> delete(T entity);
-
-    /**
-     * Deletes all instances of the type {@code T} with the given IDs.
-     *
-     * @param ids must not be {@literal null}.
-     * @return {@link Mono} signaling when operation has completed.
-     * @throws IllegalArgumentException in case the given {@literal ids} or one of its elements is {@literal null}.
-     *           {@literal null}.
-     * @since 2.5
-     */
-    Mono<Void> deleteAllById(Iterable<? extends K> ids);
-
-    /**
-     * Deletes the given entities.
-     *
-     * @param entities must not be {@literal null}.
-     * @return {@link Mono} signaling when operation has completed.
-     * @throws IllegalArgumentException in case the given {@link Iterable entities} or one of its entities is
-     *           {@literal null}.
-     */
-    Mono<Void> deleteAll(Iterable<? extends T> entities);
-
-    /**
-     * Deletes the given entities supplied by a {@link Publisher}.
-     *
-     * @param entityStream must not be {@literal null}.
-     * @return {@link Mono} signaling when operation has completed.
-     * @throws IllegalArgumentException in case the given {@link Publisher entityStream} is {@literal null}.
-     */
-    Mono<Void> deleteAll(Publisher<? extends T> entityStream);
-
-    /**
-     * Deletes all entities managed by the repository.
-     *
-     * @return {@link Mono} signaling when operation has completed.
-     */
-    Mono<Void> deleteAll();
 }
