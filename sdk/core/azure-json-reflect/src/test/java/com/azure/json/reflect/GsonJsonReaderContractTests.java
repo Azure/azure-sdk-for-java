1f475854955f7419d24a3e68463571e47ecd033e
Jack Sandford <jacksandford5757@gmail.com>
2022-08-18 23:11:25 +1200

    refactored GsonJsonReader

--- sdk/core/azure-json-reflect/src/test/java/com/azure/json/reflect/GsonJsonReaderContractTests.java
+++ sdk/core/azure-json-reflect/src/test/java/com/azure/json/reflect/GsonJsonReaderContractTests.java
@@ -7,14 +7,12 @@ import com.azure.json.JsonReader;
 import com.azure.json.contract.JsonReaderContractTests;
 import com.azure.json.reflect.gson.GsonJsonReader;
 
-import java.io.StringReader;
-
 /**
  * Tests {@link GsonJsonReader} against the contract required by {@link JsonReader}.
  */
 public class GsonJsonReaderContractTests extends JsonReaderContractTests {
     @Override
     public JsonReader getJsonReader(String json) {
-        return new GsonJsonReader(new StringReader(json));
+        return GsonJsonReader.fromString(json);
     }
 }
