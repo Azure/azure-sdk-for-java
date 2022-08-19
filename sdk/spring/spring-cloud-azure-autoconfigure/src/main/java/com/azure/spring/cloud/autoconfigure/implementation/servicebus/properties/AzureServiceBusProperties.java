e11c98e67ae35b6055b52d8b90bbefbdf5bff6c6
Strong Liu(刘少壮) <shaozliu@microsoft.com>
2022-08-19 20:06:30 +0800

    Auto start support for Service Bus Processor Client (#30457)

--- sdk/spring/spring-cloud-azure-autoconfigure/src/main/java/com/azure/spring/cloud/autoconfigure/implementation/servicebus/properties/AzureServiceBusProperties.java
+++ sdk/spring/spring-cloud-azure-autoconfigure/src/main/java/com/azure/spring/cloud/autoconfigure/implementation/servicebus/properties/AzureServiceBusProperties.java
@@ -257,6 +257,11 @@ public class AzureServiceBusProperties extends AzureServiceBusCommonProperties
          */
         private Integer maxConcurrentSessions;
 
+        /**
+         * Whether to automatically start the processor after initialization.
+         */
+        private boolean autoStartup = true;
+
         public Integer getMaxConcurrentCalls() {
             return maxConcurrentCalls;
         }
@@ -271,6 +276,14 @@ public class AzureServiceBusProperties extends AzureServiceBusCommonProperties
 
         public void setMaxConcurrentSessions(Integer maxConcurrentSessions) {
             this.maxConcurrentSessions = maxConcurrentSessions;
+        }
+
+        public boolean isAutoStartup() {
+            return autoStartup;
+        }
+
+        public void setAutoStartup(boolean autoStartup) {
+            this.autoStartup = autoStartup;
         }
     }
 
