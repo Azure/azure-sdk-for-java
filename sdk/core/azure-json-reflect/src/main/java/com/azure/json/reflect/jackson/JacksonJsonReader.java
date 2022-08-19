800229e7abc11cc1cbd477e9f348c8d4a9cb4986
Matthew Bowers <bowers.matthew9@gmail.com>
2022-08-19 15:12:41 +1200

    Basic Constructor

--- sdk/core/azure-json-reflect/src/main/java/com/azure/json/reflect/jackson/JacksonJsonReader.java
+++ sdk/core/azure-json-reflect/src/main/java/com/azure/json/reflect/jackson/JacksonJsonReader.java
@@ -6,10 +6,49 @@ import com.azure.json.JsonToken;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
 
 public class JacksonJsonReader extends JsonReader {
+	private static boolean initialized = false;
+	private static MethodHandle jacksonFactoryConstructor;
+	private static MethodHandle jacksonJsonParserInstance;
+	private static final MethodHandles.Lookup publicLookup = MethodHandles.publicLookup();
+	
+	private final Object jacksonParser;
+	
     public JacksonJsonReader(Reader reader) {
-
+    	if (!initialized) {
+    		initializeMethodHandles();
+    	}
+    	try {
+    		jacksonParser = jacksonJsonParserInstance.invoke(reader);
+    	} catch (Throwable e) {
+    		if (e instanceof RuntimeException) {
+    			throw new RuntimeException(e);
+    		} else {
+    			throw new IllegalStateException("Incorrect library present.");
+    		}
+    	}
+    	
+    }
+    
+    private void initializeMethodHandles() {
+    	try {
+    		// The jacksonJsonParser is made via the JsonFactory
+    		Class<?> jacksonJsonFactory = Class.forName("com.fasterxml.jackson.core.JsonFactory");
+    		// The jacksonJsonParser is the equivalent of the Gson JasonReader
+    		Class<?> jacksonJsonParser = Class.forName("com.fasterxml.jackson.core.JsonParser");
+    		
+    		jacksonFactoryConstructor = publicLookup.findConstructor(jacksonJsonFactory, methodType(void.class));
+    		jacksonJsonParserInstance = publicLookup.findVirtual(jacksonJsonFactory, "JsonParser", jacksonJsonParser);
+    		
+    	} catch (IllegalAccessException | NoSuchMethodException | ClassNotFoundException e) {
+    		throw new IllegalStateException("Incorrect library present.");
+    	}
+    	
+    	initialized = true;
     }
 
     @Override
