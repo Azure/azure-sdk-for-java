b8122edf14505f438c3b1af68008d72dd173e448
Alan Zimmer <48699787+alzimmermsft@users.noreply.github.com>
2022-08-24 09:25:45 -0400

    Add azure-xml (#30143)
    Add azure-xml

--- sdk/core/azure-json/src/main/java/com/azure/json/JsonSerializable.java
+++ sdk/core/azure-json/src/main/java/com/azure/json/JsonSerializable.java
@@ -10,20 +10,22 @@ package com.azure.json;
  * class has a static method {@code fromJson(JsonReader)} that deserializes an instance of that class. The contract for
  * reading JSON from {@link JsonReader} is that the initial state of the reader on call will either be a null
  * {@link JsonToken} or be the {@link JsonToken} after the {@link JsonToken#FIELD_NAME} for the object. So, for objects
- * calling out to other {@link JsonSerializable} objects for deserialization, they'll pass the reader pointing to the token
- * after the {@link JsonToken#FIELD_NAME}. This way objects reading JSON will be self-encapsulated for reading properly
- * formatted JSON. And, if an error occurs during deserialization an {@link IllegalStateException} should be thrown.
+ * calling out to other {@link JsonSerializable} objects for deserialization, they'll pass the reader pointing to the
+ * token after the {@link JsonToken#FIELD_NAME}. This way objects reading JSON will be self-encapsulated for reading
+ * properly formatted JSON. And, if an error occurs during deserialization an {@link IllegalStateException} should be
+ * thrown.
  *
- * @param <T> The type of the object that is JSON capable.
+ * @param <T> The type of the object that is JSON serializable.
  */
 public interface JsonSerializable<T extends JsonSerializable<T>> {
     /**
      * Writes the object to the passed {@link JsonWriter}.
      * <p>
      * The contract for writing JSON to {@link JsonWriter} is that the object being written will handle opening and
-     * closing its own JSON object. So, for objects calling out to other {@link JsonSerializable} objects for serialization,
-     * they'll write the field name only then pass the {@link JsonWriter} to the other {@link JsonSerializable} object. This
-     * way objects writing JSON will be self-encapsulated for writing properly formatted JSON.
+     * closing its own JSON object. So, for objects calling out to other {@link JsonSerializable} objects for
+     * serialization, they'll write the field name only then pass the {@link JsonWriter} to the other
+     * {@link JsonSerializable} object. This way objects writing JSON will be self-encapsulated for writing properly
+     * formatted JSON.
      *
      * @param jsonWriter Where the object's JSON will be written.
      * @return The {@link JsonWriter} where the JSON was written.
@@ -41,6 +43,6 @@ public interface JsonSerializable<T extends JsonSerializable<T>> {
      * @return The object that the JSON stream represented, may return null.
      */
     static <T extends JsonSerializable<T>> T fromJson(JsonReader jsonReader) {
-        throw new UnsupportedOperationException("Subclass of JsonCapable must define this factory method.");
+        throw new UnsupportedOperationException("Implementation of JsonSerializable must define this factory method.");
     }
 }
---
+++ sdk/core/azure-xml/src/main/java/com/azure/xml/DefaultXmlWriter.java
@@ -0,0 +1,172 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.xml;
+
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+
+import static javax.xml.XMLConstants.DEFAULT_NS_PREFIX;
+
+/**
+ * Default {@link XmlWriter} implementation based on {@link XMLStreamWriter}.
+ */
+public final class DefaultXmlWriter extends XmlWriter {
+    private static final XMLOutputFactory XML_OUTPUT_FACTORY = XMLOutputFactory.newFactory();
+
+    private final XMLStreamWriter writer;
+
+    /**
+     * Creates an instance of {@link XmlWriter} that writes to the provided {@link OutputStream}.
+     *
+     * @param outputStream The {@link OutputStream} where content will be written.
+     * @return A new instance of {@link XmlWriter}.
+     * @throws RuntimeException If an {@link XmlWriter} cannot be instantiated.
+     */
+    public static XmlWriter toOutputStream(OutputStream outputStream) {
+        try {
+            return new DefaultXmlWriter(XML_OUTPUT_FACTORY.createXMLStreamWriter(
+                new OutputStreamWriter(outputStream, StandardCharsets.UTF_8)));
+        } catch (XMLStreamException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    private DefaultXmlWriter(XMLStreamWriter writer) {
+        this.writer = writer;
+    }
+
+    @Override
+    public XmlWriter writeStartDocument(String version, String encoding) {
+        try {
+            writer.writeStartDocument(encoding, version);
+            return this;
+        } catch (XMLStreamException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public XmlWriter writeStartElement(String namespaceUri, String localName) {
+        try {
+            if (namespaceUri == null) {
+                writer.writeStartElement(localName);
+            } else {
+                writer.writeStartElement(namespaceUri, localName);
+            }
+            return this;
+        } catch (XMLStreamException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public XmlWriter writeStartSelfClosingElement(String namespaceUri, String localName) {
+        try {
+            if (namespaceUri == null) {
+                writer.writeEmptyElement(localName);
+            } else {
+                writer.writeEmptyElement(namespaceUri, localName);
+            }
+            return this;
+        } catch (XMLStreamException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public XmlWriter writeNamespace(String namespaceUri) {
+        if (Objects.equals(writer.getNamespaceContext().getNamespaceURI(DEFAULT_NS_PREFIX), namespaceUri)) {
+            return this;
+        }
+
+        try {
+            writer.setDefaultNamespace(namespaceUri);
+            writer.writeDefaultNamespace(namespaceUri);
+            return this;
+        } catch (XMLStreamException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public XmlWriter writeEndElement() {
+        try {
+            writer.writeEndElement();
+            return this;
+        } catch (XMLStreamException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public XmlWriter writeStringAttribute(String namespaceUri, String localName, String value) {
+        if (value == null) {
+            return this;
+        }
+
+        try {
+            if (namespaceUri == null) {
+                writer.writeAttribute(localName, value);
+            } else {
+                writer.writeAttribute(namespaceUri, localName, value);
+            }
+            return this;
+        } catch (XMLStreamException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public XmlWriter writeString(String value) {
+        if (value == null) {
+            return this;
+        }
+
+        try {
+            writer.writeCharacters(value);
+            return this;
+        } catch (XMLStreamException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public XmlWriter writeCDataString(String value) {
+        if (value == null) {
+            return this;
+        }
+
+        try {
+            writer.writeCData(value);
+            return this;
+        } catch (XMLStreamException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public XmlWriter flush() {
+        try {
+            writer.flush();
+            return this;
+        } catch (XMLStreamException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        try {
+            writer.close();
+        } catch (XMLStreamException e) {
+            throw new IOException(e);
+        }
+    }
+}
