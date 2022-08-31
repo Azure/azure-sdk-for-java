dc290df69d29b536643c0b47f75a8318a89be80a
Rujun Chen <rujche@microsoft.com>
2022-08-31 16:11:45 +0800

    In AadRestOperationConfiguration, use RestTemplateBuilder bean configured in RestTemplateAutoConfiguration.

--- sdk/spring/spring-cloud-azure-autoconfigure/src/main/java/com/azure/spring/cloud/autoconfigure/aad/configuration/AadRestOperationConfiguration.java
+++ sdk/spring/spring-cloud-azure-autoconfigure/src/main/java/com/azure/spring/cloud/autoconfigure/aad/configuration/AadRestOperationConfiguration.java
@@ -3,40 +3,73 @@
 
 package com.azure.spring.cloud.autoconfigure.aad.configuration;
 
-import org.springframework.beans.factory.ObjectProvider;
+import com.azure.spring.cloud.autoconfigure.aad.AadAuthenticationFilterAutoConfiguration;
+import com.azure.spring.cloud.autoconfigure.aadb2c.AadB2cResourceServerAutoConfiguration;
+import com.azure.spring.cloud.autoconfigure.aadb2c.configuration.AadB2cOAuth2ClientConfiguration;
+import com.nimbusds.jose.util.ResourceRetriever;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.boot.autoconfigure.web.client.RestTemplateAutoConfiguration;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
 import org.springframework.security.oauth2.core.http.converter.OAuth2AccessTokenResponseHttpMessageConverter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.web.client.RestOperations;
 
 import java.util.Arrays;
 
 /**
- * Common bean used for Azure AD and Azure AD B2C
+ * Configure RestOperation bean used for accessing Azure AD and Azure AD B2C
  */
 @Configuration(proxyBeanMethods = false)
+@Import(RestTemplateAutoConfiguration.class)
 public class AadRestOperationConfiguration {
 
     /**
-     * Declare RestOperations bean used by various OAuth2AccessTokenResponseClient.
+     * Declare {@link RestOperations} bean which will be used when send http request to Azure AD and Azure AD B2C.
+     * <p>
+     * <p/>
+     * For example:
+     * <ul>
+     *     <li> In {@link ResourceRetriever} declared in {@link AadAuthenticationFilterAutoConfiguration}.
+     *     <li> In kinds of {@link OAuth2AccessTokenResponseClient} bean(like
+     *     {@link DefaultAuthorizationCodeTokenResponseClient}) declared in {@link AadOAuth2ClientConfiguration} and
+     *     {@link AadB2cOAuth2ClientConfiguration}.
+     *     <li> In {@link JwtDecoder} declared in {@link AadResourceServerConfiguration} and
+     *     {@link AadB2cResourceServerAutoConfiguration}
+     * </ul>
      *
-     * @param builderObjectProvider the optional rest template builder bean.
+     * @param builder The RestTemplateBuilder bean, which can be provided in {@link RestTemplateAutoConfiguration}
+     *                if customer not provide this bean.
      * @return RestOperations bean
      */
     @Bean
     @ConditionalOnMissingBean(RestOperations.class)
-    public RestOperations aadAuthRestOperations(ObjectProvider<RestTemplateBuilder> builderObjectProvider) {
-        RestTemplateBuilder builder = builderObjectProvider.getIfAvailable(this::restTemplateBuilder);
+    public RestOperations aadAuthRestOperations(RestTemplateBuilder builder) {
         return builder.build();
     }
 
-    private RestTemplateBuilder restTemplateBuilder() {
-        return new RestTemplateBuilder()
-                .messageConverters(Arrays.asList(
-                        new FormHttpMessageConverter(), new OAuth2AccessTokenResponseHttpMessageConverter()));
+    /**
+     * Declare {@link HttpMessageConverters} bean which can be used in {@link RestTemplateAutoConfiguration}.
+     * <p>
+     * <p/>
+     * {@link FormHttpMessageConverter} and {@link OAuth2AccessTokenResponseHttpMessageConverter} is necessary for
+     * kinds of {@link OAuth2AccessTokenResponseClient} implementations(like
+     * {@link DefaultAuthorizationCodeTokenResponseClient})
+     * .
+     *
+     * @return HttpMessageConverters bean
+     */
+    @Bean
+    @ConditionalOnMissingBean(HttpMessageConverters.class)
+    public HttpMessageConverters httpMessageConverters() {
+        return new HttpMessageConverters(
+                Arrays.asList(new FormHttpMessageConverter(), new OAuth2AccessTokenResponseHttpMessageConverter()));
     }
 
 }
