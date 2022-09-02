ab4db09af82a6b856207216ffe0b81ac15c2853c
Jair <67484440+jairmyree@users.noreply.github.com>
2022-09-02 14:33:58 -0700

    Update sdk/tables/azure-data-tables/src/test/java/com/azure/data/tables/TableServiceAsyncClientTest.java
    Co-authored-by: vcolin7 <vicolina@microsoft.com>

--- sdk/tables/azure-data-tables/src/test/java/com/azure/data/tables/TableServiceAsyncClientTest.java
+++ sdk/tables/azure-data-tables/src/test/java/com/azure/data/tables/TableServiceAsyncClientTest.java
@@ -99,7 +99,7 @@ public class TableServiceAsyncClientTest extends TableServiceClientTestBase {
             .build();
 
         TableServiceAsyncClient malformedClient = getClientBuilder(Configuration.getGlobalConfiguration().get(
-            "TABLES_ENDPOINT","https://tablestests.table.core.windows.com")+"/"+tableName,credential,true)
+            "TABLES_ENDPOINT", "https://tablestests.table.core.windows.com") + "/" + tableName, credential, true)
             .buildAsyncClient();
 
         // Act & Assert
