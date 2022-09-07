89b7341936cc8b210830e52c6d071ce44e844609
Muyao <v-muyaofeng@microsoft.com>
2022-09-07 08:29:09 +0800

    update

--- sdk/spring/spring-cloud-azure-autoconfigure/src/main/java/com/azure/spring/cloud/autoconfigure/implementation/compatibility/AzureSpringBootVersionVerifier.java
+++ sdk/spring/spring-cloud-azure-autoconfigure/src/main/java/com/azure/spring/cloud/autoconfigure/implementation/compatibility/AzureSpringBootVersionVerifier.java
@@ -99,7 +99,8 @@ public class AzureSpringBootVersionVerifier {
         for (String acceptedVersion : acceptedVersions) {
             try {
                 if (this.matchSpringBootVersionFromManifest(acceptedVersion)) {
-                    LOGGER.debug("Current Spring Boot version matching succeeds in Spring Cloud Azure supported versions [{}].", acceptedVersion);
+                    LOGGER.debug("Current Spring Boot version matching succeeds in Spring Cloud Azure supported "
+                        + "versions [{}].", acceptedVersion);
                     return true;
                 }
             } catch (FileNotFoundException e) {
