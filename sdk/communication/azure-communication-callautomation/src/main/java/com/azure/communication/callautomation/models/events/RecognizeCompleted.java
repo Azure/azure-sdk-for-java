b1548c26bd64a4df2564097065d2402d3da84718
Adam Tazi <adamtazi@microsoft.com>
2022-09-13 14:25:26 -0700

    fix recognize events

--- sdk/communication/azure-communication-callautomation/src/main/java/com/azure/communication/callautomation/models/events/RecognizeCompleted.java
+++ sdk/communication/azure-communication-callautomation/src/main/java/com/azure/communication/callautomation/models/events/RecognizeCompleted.java
@@ -11,17 +11,6 @@ import com.azure.core.annotation.Immutable;
 /** The RecognizeCompleted model. */
 @Immutable
 public final class RecognizeCompleted extends CallAutomationEventBase {
-    /*
-     * The operationContext property.
-     */
-    @JsonProperty(value = "operationContext", access = JsonProperty.Access.WRITE_ONLY)
-    private String operationContext;
-
-    /*
-     * Result information defines the code, subcode and message
-     */
-    @JsonProperty(value = "resultInformation", access = JsonProperty.Access.WRITE_ONLY)
-    private ResultInformation resultInformation;
 
     /*
      * Determines the sub-type of the recognize operation.
@@ -36,56 +25,6 @@ public final class RecognizeCompleted extends CallAutomationEventBase {
      */
     @JsonProperty(value = "collectTonesResult", access = JsonProperty.Access.WRITE_ONLY)
     private CollectTonesResult collectTonesResult;
-
-    /*
-     * Used to determine the version of the event.
-     */
-    @JsonProperty(value = "version", access = JsonProperty.Access.WRITE_ONLY)
-    private String version;
-
-    /*
-     * Call connection ID.
-     */
-    @JsonProperty(value = "callConnectionId", access = JsonProperty.Access.WRITE_ONLY)
-    private String callConnectionId;
-
-    /*
-     * Server call ID.
-     */
-    @JsonProperty(value = "serverCallId", access = JsonProperty.Access.WRITE_ONLY)
-    private String serverCallId;
-
-    /*
-     * Correlation ID for event to call correlation. Also called ChainId for
-     * skype chain ID.
-     */
-    @JsonProperty(value = "correlationId", access = JsonProperty.Access.WRITE_ONLY)
-    private String correlationId;
-
-    /*
-     * The public event namespace used as the "type" property in the
-     * CloudEvent.
-     */
-    @JsonProperty(value = "publicEventType", access = JsonProperty.Access.WRITE_ONLY)
-    private String publicEventType;
-
-    /**
-     * Get the operationContext property: The operationContext property.
-     *
-     * @return the operationContext value.
-     */
-    public String getOperationContext() {
-        return this.operationContext;
-    }
-
-    /**
-     * Get the resultInformation property: Result information defines the code, subcode and message.
-     *
-     * @return the resultInformation value.
-     */
-    public ResultInformation getResultInformation() {
-        return this.resultInformation;
-    }
 
     /**
      * Get the recognitionType property: Determines the sub-type of the recognize operation. In case of cancel operation
@@ -106,49 +45,4 @@ public final class RecognizeCompleted extends CallAutomationEventBase {
         return this.collectTonesResult;
     }
 
-    /**
-     * Get the version property: Used to determine the version of the event.
-     *
-     * @return the version value.
-     */
-    public String getVersion() {
-        return this.version;
-    }
-
-    /**
-     * Get the callConnectionId property: Call connection ID.
-     *
-     * @return the callConnectionId value.
-     */
-    public String getCallConnectionId() {
-        return this.callConnectionId;
-    }
-
-    /**
-     * Get the serverCallId property: Server call ID.
-     *
-     * @return the serverCallId value.
-     */
-    public String getServerCallId() {
-        return this.serverCallId;
-    }
-
-    /**
-     * Get the correlationId property: Correlation ID for event to call correlation. Also called ChainId for skype chain
-     * ID.
-     *
-     * @return the correlationId value.
-     */
-    public String getCorrelationId() {
-        return this.correlationId;
-    }
-
-    /**
-     * Get the publicEventType property: The public event namespace used as the "type" property in the CloudEvent.
-     *
-     * @return the publicEventType value.
-     */
-    public String getPublicEventType() {
-        return this.publicEventType;
-    }
 }
--- sdk/communication/azure-communication-callautomation/src/main/java/com/azure/communication/callautomation/models/events/RecognizeFailed.java
+++ sdk/communication/azure-communication-callautomation/src/main/java/com/azure/communication/callautomation/models/events/RecognizeFailed.java
@@ -9,55 +9,5 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 /** The RecognizeFailed model. */
 @Fluent
 public final class RecognizeFailed extends CallAutomationEventBase {
-    /*
-     * Operation context
-     */
-    @JsonProperty(value = "operationContext")
-    private String operationContext;
 
-    /*
-     * Defines the code, sub-code and message for the operation
-     */
-    @JsonProperty(value = "resultInfo")
-    private ResultInformation resultInfo;
-
-    /**
-     * Get the operationContext property: Operation context.
-     *
-     * @return the operationContext value.
-     */
-    public String getOperationContext() {
-        return this.operationContext;
-    }
-
-    /**
-     * Set the operationContext property: Operation context.
-     *
-     * @param operationContext the operationContext value to set.
-     * @return the RecognizeFailed object itself.
-     */
-    public RecognizeFailed setOperationContext(String operationContext) {
-        this.operationContext = operationContext;
-        return this;
-    }
-
-    /**
-     * Get the resultInfo property: Defines the code, sub-code and message for the operation.
-     *
-     * @return the resultInfo value.
-     */
-    public ResultInformation getResultInfo() {
-        return this.resultInfo;
-    }
-
-    /**
-     * Set the resultInfo property: Defines the code, sub-code and message for the operation.
-     *
-     * @param resultInfo the resultInfo value to set.
-     * @return the RecognizeFailed object itself.
-     */
-    public RecognizeFailed setResultInfo(ResultInformation resultInfo) {
-        this.resultInfo = resultInfo;
-        return this;
-    }
 }
