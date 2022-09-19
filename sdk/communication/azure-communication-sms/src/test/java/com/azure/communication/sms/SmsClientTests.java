c4e8b170d19d3ea8a12cf9a5a0e32c30be4df5fe
Liudmila Molkova <limolkova@microsoft.com>
2022-09-19 12:19:13 -0700

    more --add-opens removvals

--- sdk/communication/azure-communication-sms/src/test/java/com/azure/communication/sms/SmsClientTests.java
+++ sdk/communication/azure-communication-sms/src/test/java/com/azure/communication/sms/SmsClientTests.java
@@ -14,7 +14,6 @@ import com.azure.identity.DefaultAzureCredentialBuilder;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
