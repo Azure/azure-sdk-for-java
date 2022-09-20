af2fa105561d9d3a0d5b2b0cae3b698976eb047d
azure-sdk <azuresdk@microsoft.com>
2022-09-20 01:59:49 +0000

    [Automation] Generate Fluent Lite from sqlvirtualmachine#package-preview-2022-07

--- sdk/sqlvirtualmachine/azure-resourcemanager-sqlvirtualmachine/src/main/java/com/azure/resourcemanager/sqlvirtualmachine/models/SqlVirtualMachineUpdate.java
+++ sdk/sqlvirtualmachine/azure-resourcemanager-sqlvirtualmachine/src/main/java/com/azure/resourcemanager/sqlvirtualmachine/models/SqlVirtualMachineUpdate.java
@@ -5,8 +5,6 @@
 package com.azure.resourcemanager.sqlvirtualmachine.models;
 
 import com.azure.core.annotation.Fluent;
-import com.azure.core.util.logging.ClientLogger;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.Map;
@@ -14,8 +12,6 @@ import java.util.Map;
 /** An update to a SQL virtual machine. */
 @Fluent
 public final class SqlVirtualMachineUpdate {
-    @JsonIgnore private final ClientLogger logger = new ClientLogger(SqlVirtualMachineUpdate.class);
-
     /*
      * Resource tags.
      */
