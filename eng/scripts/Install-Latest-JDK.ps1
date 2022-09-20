7d5fc361fbd1d1d396550ce2bd524ed9b10b254a
Alan Zimmer <48699787+alzimmermsft@users.noreply.github.com>
2022-09-20 16:40:59 -0400

    Change check for latest JDK

--- eng/scripts/Install-Latest-JDK.ps1
+++ eng/scripts/Install-Latest-JDK.ps1
@@ -6,6 +6,7 @@ param(
 )
 
 $latestJdkPath = Join-Path -Path $ToolsDirectory -ChildPath $JdkZip
+Write-Host "Latest JDK path: $latestJdkPath"
 if (!(Test-Path -Path $latestJdkPath -PathType leaf)) {
   if (!(Test-Path -Path $ToolsDirectory)) {
     New-Item -Path $ToolsDirectory -ItemType "directory"
@@ -16,14 +17,20 @@ if (!(Test-Path -Path $latestJdkPath -PathType leaf)) {
 
 $latestJdkUnzipPath = Join-Path -Path $ToolsDirectory -ChildPath $JdkName
 tar -xf $latestJdkPath -C $ToolsDirectory
+if (!(Test-Path -Path $latestJdkUnzipPath)) {
+  Write-Host "Failed to unpack JDK to $latestJdkUnzipPath"
+}
 
 $javaHome = Join-Path -Path $latestJdkUnzipPath -ChildPath "bin"
-[System.Environment]::SetEnvironmentVariable("JAVA_HOME", $javaHome, [System.EnvironmentVariableTarget]::Machine)
-[System.Environment]::SetEnvironmentVariable("JAVA_HOME_18_X64", $javaHome, [System.EnvironmentVariableTarget]::Machine)
-$path = [System.Environment]::GetEnvironmentVariable("PATH", [System.EnvironmentVariableTarget]::Machine)
+Write-Host "Latest JDK JAVA_HOME: $javaHome"
+[System.Environment]::SetEnvironmentVariable("JAVA_HOME", $javaHome, [System.EnvironmentVariableTarget]::User)
+[System.Environment]::SetEnvironmentVariable("JAVA_HOME_18_X64", $javaHome, [System.EnvironmentVariableTarget]::User)
+$path = [System.Environment]::GetEnvironmentVariable("PATH", [System.EnvironmentVariableTarget]::User)
 if ($IsWindows) {
   $path = $javaHome + ";" + $path
 } else {
   $path = $javaHome + ":" + $path
 }
-[System.Environment]::SetEnvironmentVariable("PATH", $path, [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable("PATH", $path, [System.EnvironmentVariableTarget]::User)
+
+Write-Host "JAVA_HOME: "
