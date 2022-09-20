6e9d6ac14e27636c2e67659653c6786326b4ba00
Victor Colin Amador <vicolina@microsoft.com>
2022-09-19 18:54:36 -0700

    Added missing changes for sync stack in KeyVaultCredentialPolicy.

--- sdk/keyvault/azure-security-keyvault-administration/src/main/java/com/azure/security/keyvault/administration/implementation/KeyVaultCredentialPolicy.java
+++ sdk/keyvault/azure-security-keyvault-administration/src/main/java/com/azure/security/keyvault/administration/implementation/KeyVaultCredentialPolicy.java
@@ -207,6 +207,7 @@ public class KeyVaultCredentialPolicy extends BearerTokenAuthenticationPolicy {
                 .setTenantId(this.challenge.getTenantId());
 
             setAuthorizationHeaderSync(context, tokenRequestContext);
+            return;
         }
 
         // The body is removed from the initial request because Key Vault supports other authentication schemes which
@@ -216,8 +217,8 @@ public class KeyVaultCredentialPolicy extends BearerTokenAuthenticationPolicy {
 
         // Do not overwrite previous contents if retrying after initial request failed (e.g. timeout).
         if (!context.getData(KEY_VAULT_STASHED_CONTENT_KEY).isPresent()) {
-            if (request.getBody() != null) {
-                context.setData(KEY_VAULT_STASHED_CONTENT_KEY, request.getBody());
+            if (request.getBodyAsBinaryData() != null) {
+                context.setData(KEY_VAULT_STASHED_CONTENT_KEY, request.getBodyAsBinaryData());
                 context.setData(KEY_VAULT_STASHED_CONTENT_LENGTH_KEY,
                     request.getHeaders().getValue(CONTENT_LENGTH_HEADER));
                 request.setHeader(CONTENT_LENGTH_HEADER, "0");
@@ -234,7 +235,7 @@ public class KeyVaultCredentialPolicy extends BearerTokenAuthenticationPolicy {
         Optional<Object> contentLengthOptional = context.getData(KEY_VAULT_STASHED_CONTENT_LENGTH_KEY);
 
         if (request.getBody() == null && contentOptional.isPresent() && contentLengthOptional.isPresent()) {
-            request.setBody(BinaryData.fromObject(contentOptional.get()));
+            request.setBody((BinaryData) (contentOptional.get()));
             request.setHeader(CONTENT_LENGTH_HEADER, (String) contentLengthOptional.get());
         }
 
