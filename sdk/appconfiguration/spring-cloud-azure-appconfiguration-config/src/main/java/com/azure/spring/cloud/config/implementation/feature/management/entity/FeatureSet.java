5867fc18aa1dddde847e78c5ffc3be06a9f33443
Matt Metcalf <mrm9084@gmail.com>
2022-09-21 13:00:26 -0700

    Moving Feature Set to impl folder

--- sdk/appconfiguration/spring-cloud-azure-appconfiguration-config/src/main/java/com/azure/spring/cloud/config/implementation/feature/management/entity/FeatureSet.java
+++ sdk/appconfiguration/spring-cloud-azure-appconfiguration-config/src/main/java/com/azure/spring/cloud/config/implementation/feature/management/entity/FeatureSet.java
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-package com.azure.spring.cloud.config.feature.management.entity;
+package com.azure.spring.cloud.config.implementation.feature.management.entity;
 
 import java.util.HashMap;
 
