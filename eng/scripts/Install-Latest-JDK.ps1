8f96251c949430010e83a62aa8b916a4a2d0f6c5
Alan Zimmer <48699787+alzimmermsft@users.noreply.github.com>
2022-09-21 17:18:01 -0400

    Cache unzipped latest Java directory

--- eng/scripts/Install-Latest-JDK.ps1
+++ eng/scripts/Install-Latest-JDK.ps1
@@ -4,11 +4,10 @@ $jdkUnzipName = "jdk-18.0.2.1+1"
 
 Write-Host "Downloading latest JDK to" (Get-Location)
 
-if (!(Test-Path -Path $jdkZipName -PathType leaf)) {
+if (!(Test-Path -Path $jdkUnzipName -PathType container)) {
   Invoke-WebRequest -URI $jdkUri -OutFile $jdkZipName
+  tar -xvf $jdkZipName
 }
-
-tar -xvf $jdkZipName
 
 $javaHome = (Convert-Path $jdkUnzipName)
 Write-Host "Latest JDK: $javaHome"
