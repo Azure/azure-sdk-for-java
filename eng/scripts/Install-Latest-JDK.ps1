4be2dac915a3f3f109e2de3fefd247db3b7676b9
Alan Zimmer <48699787+alzimmermsft@users.noreply.github.com>
2022-09-21 08:54:51 -0400

    Remove check for OS type

--- eng/scripts/Install-Latest-JDK.ps1
+++ eng/scripts/Install-Latest-JDK.ps1
@@ -14,23 +14,14 @@ $javaHome = Join-Path -Path (Convert-Path $jdkUnzipName) -ChildPath "bin"
 Write-Host "Latest JDK: $javaHome"
 
 Write-Host "Current JAVA_HOME: $Env:JAVA_HOME"
-Write-Host "##vso[task.setvariable variable=JAVA_HOME;]$javaHome"
+$Env:JAVA_HOME = $javaHome
 Write-Host "Updated JAVA_HOME: $Env:JAVA_HOME"
 
-Write-Host "##vso[task.setvariable variable=JAVA_HOME_18_X64;]$javaHome"
+$Env:JAVA_HOME_18_X64 = $javaHome
 
-if ($IsWindows) {
-  $path = $Env:Path
-  $path = $javaHome + ";" + $path
+$path = $Env:PATH
+$path = $javaHome + ";" + $path
 
-  Write-Host "Current Path: $Env:Path"
-  Write-Host "##vso[task.setvariable variable=Path;]$path"
-  Write-Host "Updated Path: $Env:Path"
-} else {
-  $path = $Env:PATH
-  $path = $javaHome + ";" + $path
-
-  Write-Host "Current Path: $Env:PATH"
-  Write-Host "##vso[task.setvariable variable=PATH;]$path"
-  Write-Host "Updated PATH: $Env:PATH"
-}
+Write-Host "Current Path: $Env:PATH"
+$Env:PATH = $path
+Write-Host "Updated PATH: $Env:PATH"
