d03b37690eab2d37f2ecfb4e17de0b3258978f36
Alan Zimmer <48699787+alzimmermsft@users.noreply.github.com>
2022-09-20 21:30:45 -0400

    Simplify everything

--- eng/scripts/Install-Latest-JDK.ps1
+++ eng/scripts/Install-Latest-JDK.ps1
@@ -1,36 +1,25 @@
-param(
-    [Parameter(Mandatory=$true)][string]$ToolsDirectory,
-    [Parameter(Mandatory=$true)][string]$JdkName,
-    [Parameter(Mandatory=$true)][string]$JdkUri,
-    [Parameter(Mandatory=$true)][string]$JdkZip
-)
+$jdkZipName = "OpenJDK18U-jdk_x64_linux_hotspot_18.0.2.1_1.tar.gz"
+$jdkUnzipName = "jdk-18.0.2.1+1"
 
-$latestJdkPath = Join-Path -Path $ToolsDirectory -ChildPath $JdkZip
-Write-Host "Latest JDK path: $latestJdkPath"
-if (!(Test-Path -Path $latestJdkPath -PathType leaf)) {
-  if (!(Test-Path -Path $ToolsDirectory)) {
-    New-Item -Path $ToolsDirectory -ItemType "directory"
-  }
+if (!(Test-Path -Path $jdkZipName -PathType leaf)) {
   $latestJdkUri = "https://github.com/adoptium/temurin18-binaries/releases/download/jdk-18.0.2.1%2B1/$JdkZip"
-  Invoke-WebRequest -URI $JdkUri -OutFile $latestJdkPath
+  Invoke-WebRequest -URI $JdkUri -OutFile $jdkZipName
 }
 
-$latestJdkUnzipPath = Join-Path -Path $ToolsDirectory -ChildPath $JdkName
-tar -xf $latestJdkPath -C $ToolsDirectory
-if (!(Test-Path -Path $latestJdkUnzipPath)) {
-  Write-Host "Failed to unpack JDK to $latestJdkUnzipPath"
-}
+tar -xvf $jdkZipName
 
-$javaHome = Join-Path -Path $latestJdkUnzipPath -ChildPath "bin"
+$javaHome = Join-Path -Path (Convert-Path $jdkUnzipName) -ChildPath "bin"
 Write-Host "Latest JDK JAVA_HOME: $javaHome"
-[System.Environment]::SetEnvironmentVariable("JAVA_HOME", $javaHome, [System.EnvironmentVariableTarget]::User)
-[System.Environment]::SetEnvironmentVariable("JAVA_HOME_18_X64", $javaHome, [System.EnvironmentVariableTarget]::User)
-$path = [System.Environment]::GetEnvironmentVariable("PATH", [System.EnvironmentVariableTarget]::User)
-if ($IsWindows) {
-  $path = $javaHome + ";" + $path
-} else {
-  $path = $javaHome + ":" + $path
-}
-[System.Environment]::SetEnvironmentVariable("PATH", $path, [System.EnvironmentVariableTarget]::User)
 
-Write-Host "JAVA_HOME: "
+[System.Environment]::SetEnvironmentVariable("JAVA_HOME", $javaHome, [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable("JAVA_HOME_18_X64", $javaHome, [System.EnvironmentVariableTarget]::Machine)
+
+if ($IsWindows) {
+  $path = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine)
+  $path = $javaHome + ";" + $path
+  [System.Environment]::SetEnvironmentVariable("Path", $path, [System.EnvironmentVariableTarget]::Machine)
+} else {
+  $path = [System.Environment]::GetEnvironmentVariable("PATH", [System.EnvironmentVariableTarget]::Machine)
+  $path = $javaHome + ":" + $path
+  [System.Environment]::SetEnvironmentVariable("PATH", $path, [System.EnvironmentVariableTarget]::Machine)
+}
