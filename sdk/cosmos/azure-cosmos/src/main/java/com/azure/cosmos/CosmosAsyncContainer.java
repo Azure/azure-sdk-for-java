b5fff267ec45eb9acbc54276e06694394652081d
Fabian Meiswinkel <fabian@meiswinkel.com>
2022-09-22 17:02:00 +0000

    Update CosmosAsyncContainer.java

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/CosmosAsyncContainer.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/CosmosAsyncContainer.java
@@ -61,7 +61,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.core.publisher.SignalType;
 
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
