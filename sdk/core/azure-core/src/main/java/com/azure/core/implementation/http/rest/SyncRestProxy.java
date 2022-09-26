1778950b4b0503132924ae0f1b94af3b0341fcaa
Connie <conniey@microsoft.com>
2022-09-26 11:15:17 -0700

    Fixing inputstream return in Sync client

--- sdk/core/azure-core/src/main/java/com/azure/core/implementation/http/rest/SyncRestProxy.java
+++ sdk/core/azure-core/src/main/java/com/azure/core/implementation/http/rest/SyncRestProxy.java
@@ -19,7 +19,6 @@ import com.azure.core.util.serializer.SerializerAdapter;
 import com.azure.core.util.serializer.SerializerEncoding;
 import reactor.core.publisher.Mono;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Method;
@@ -176,9 +175,7 @@ public class SyncRestProxy extends RestProxyBase {
             }
             result = responseBodyBytes != null ? (responseBodyBytes.length == 0 ? null : responseBodyBytes) : null;
         } else if (TypeUtil.isTypeOrSubTypeOf(entityType, InputStream.class)) {
-            final BinaryData binaryData = response.getSourceResponse().getBodyAsBinaryData();
-
-            result = new ByteArrayInputStream(binaryData.toBytes());
+            result = response.getSourceResponse().getBodyAsInputStream();
         } else if (TypeUtil.isTypeOrSubTypeOf(entityType, BinaryData.class)) {
             // BinaryData
             // The raw response is directly used to create an instance of BinaryData which then provides
