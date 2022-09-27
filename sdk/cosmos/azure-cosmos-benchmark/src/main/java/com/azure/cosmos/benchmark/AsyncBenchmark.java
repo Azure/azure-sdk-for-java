b242041e4673ad9b3e6e0918306a0a05b471eb90
Fabian Meiswinkel <fabian@meiswinkel.com>
2022-09-27 22:21:05 +0000

    Fixing Benchmark build error

--- sdk/cosmos/azure-cosmos-benchmark/src/main/java/com/azure/cosmos/benchmark/AsyncBenchmark.java
+++ sdk/cosmos/azure-cosmos-benchmark/src/main/java/com/azure/cosmos/benchmark/AsyncBenchmark.java
@@ -13,6 +13,7 @@ import com.azure.cosmos.CosmosException;
 import com.azure.cosmos.DirectConnectionConfig;
 import com.azure.cosmos.GatewayConnectionConfig;
 import com.azure.cosmos.implementation.HttpConstants;
+import com.azure.cosmos.models.CosmosClientTelemetryConfig;
 import com.azure.cosmos.models.PartitionKey;
 import com.azure.cosmos.models.ThroughputProperties;
 import com.codahale.metrics.ConsoleReporter;
@@ -90,13 +91,17 @@ abstract class AsyncBenchmark<T> {
             .clientTelemetryEnabled(cfg.isClientTelemetryEnabled());
 
         if (registry != null) {
-            cosmosClientBuilder.clientTelemetryConfig().clientMetrics(registry);
+            CosmosClientTelemetryConfig telemetryConfig = new CosmosClientTelemetryConfig();
+            telemetryConfig.clientMetrics(registry);
+            cosmosClientBuilder.clientTelemetryConfig(telemetryConfig);
         }
 
         registry = configuration.getGraphiteMeterRegistry();
 
         if (registry != null) {
-            cosmosClientBuilder.clientTelemetryConfig().clientMetrics(registry);
+            CosmosClientTelemetryConfig telemetryConfig = new CosmosClientTelemetryConfig();
+            telemetryConfig.clientMetrics(registry);
+            cosmosClientBuilder.clientTelemetryConfig(telemetryConfig);
         }
 
         if (cfg.getConnectionMode().equals(ConnectionMode.DIRECT)) {
