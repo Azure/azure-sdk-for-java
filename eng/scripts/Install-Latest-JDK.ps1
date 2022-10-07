dce92461ff32ae51a4091b2307365b9924e8bcfc
Alan Zimmer <48699787+alzimmermsft@users.noreply.github.com>
2022-10-07 11:23:21 -0400

    Update to Java 19

--- eng/scripts/Install-Latest-JDK.ps1
+++ eng/scripts/Install-Latest-JDK.ps1
@@ -1,6 +1,6 @@
-$jdkUri = "https://github.com/adoptium/temurin18-binaries/releases/download/jdk-18.0.2.1%2B1/OpenJDK18U-jdk_x64_linux_hotspot_18.0.2.1_1.tar.gz"
-$jdkZipName = "OpenJDK18U-jdk_x64_linux_hotspot_18.0.2.1_1.tar.gz"
-$jdkUnzipName = "jdk-18.0.2.1+1"
+$jdkUri = "https://github.com/adoptium/temurin19-binaries/releases/download/jdk-19%2B36/OpenJDK19U-jdk_x64_linux_hotspot_19_36.tar.gz"
+$jdkZipName = "OpenJDK19U-jdk_x64_linux_hotspot_19_36.tar.gz"
+$jdkUnzipName = "jdk-19+36"
 
 Write-Host "Downloading latest JDK to" (Get-Location)
 
@@ -16,4 +16,4 @@ Write-Host "Current JAVA_HOME: $Env:JAVA_HOME"
 Write-Host "##vso[task.setvariable variable=JAVA_HOME;]$javaHome"
 Write-Host "Updated JAVA_HOME: $Env:JAVA_HOME"
 
-Write-Host "##vso[task.setvariable variable=JAVA_HOME_18_X64;]$javaHome"
+Write-Host "##vso[task.setvariable variable=JAVA_HOME_19_X64;]$javaHome"
