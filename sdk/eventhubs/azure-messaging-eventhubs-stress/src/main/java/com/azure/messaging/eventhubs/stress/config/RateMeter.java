6ab2b374b6c22cb2620633bd578f1d8cd35dd778
Kun Liu <97088315+liukun-msft@users.noreply.github.com>
2022-10-11 11:03:20 +0800

    Add stress tests module for Event Hubs (#28958)
    * Add stress tests for Event Hubs
    
    * Update README and pom.xml
    
    * Update configuration to solve bean circular dependency issue
    
    * Update version text
    
    * Clean blank line
    
    * Add stress test module in ci.yml
    
    * Update pom.xml
    
    * Update pom.xml
    
    * Update java doc
    
    * Update links in README
    
    * Update java code base on comments
    
    * Add application insights into dockerfile and update README
    
    * Fix link verification failure issue
    
    * Update class naming and add guid() for bicep file
    
    * Update bicep file
    
    * Rename constants
    
    * Change logger to clientLogger
    
    * Change dependency to alphabet order
    
    * Use spring to inject env variables as scenario options
    
    * Update README
    
    * Set string default value as null
    
    * Change auth rule to namespace level to avoid being created for each EH
    
    * Add parent project in pom.xml
    
    * Update pom.xml version format
    
    * Update pom.xml
    
    * Update dependency version
    
    * Update pom.xml and external_dependencies
    
    * Update pom.xml version
    
    * Update pom.xml and applicationinsights version

---
+++ sdk/eventhubs/azure-messaging-eventhubs-stress/src/main/java/com/azure/messaging/eventhubs/stress/config/RateMeter.java
@@ -0,0 +1,59 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.messaging.eventhubs.stress.config;
+
+import com.azure.core.util.logging.ClientLogger;
+import com.microsoft.applicationinsights.TelemetryClient;
+import com.microsoft.applicationinsights.telemetry.MetricTelemetry;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Send metric telemetry periodically
+ */
+public class RateMeter {
+    private static final ClientLogger LOGGER = new ClientLogger(RateMeter.class);
+
+    private final Map<String, AtomicInteger> rateMap = new ConcurrentHashMap<>();
+    private final TelemetryClient telemetryClient;
+    private final Duration periodicDuration;
+    private final ScheduledExecutorService executorService = new ScheduledThreadPoolExecutor(2);
+
+    public RateMeter(TelemetryClient telemetryClient, Duration periodicDuration) {
+        this.telemetryClient = telemetryClient;
+        this.periodicDuration = periodicDuration;
+        this.runMetrics();
+    }
+
+    public void add(String key, int count) {
+        rateMap.computeIfAbsent(key, name -> new AtomicInteger(0));
+        rateMap.get(key).addAndGet(count);
+    }
+
+    private void runMetrics() {
+        executorService.scheduleAtFixedRate(() -> {
+            final List<MetricTelemetry> metricTelemetryList = new ArrayList<>();
+            rateMap.forEach((key, count) -> {
+                MetricTelemetry metricTelemetry = new MetricTelemetry();
+                metricTelemetry.setValue(count.getAndSet(0));
+                metricTelemetry.setTimestamp(new Date());
+                metricTelemetry.setName(key);
+                metricTelemetryList.add(metricTelemetry);
+            });
+            metricTelemetryList.forEach(metricTelemetry -> {
+                LOGGER.info("Metrics: {Duration: {}, Metric: {}}", periodicDuration, metricTelemetry);
+                telemetryClient.trackMetric(metricTelemetry);
+            });
+        }, periodicDuration.toMillis(), periodicDuration.toMillis(), TimeUnit.MILLISECONDS);
+    }
+}
