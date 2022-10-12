dd231ce9fe88152ed73353daf34315e6cc902ccc
juntuchen <juntuchen@microsoft.com>
2022-10-12 17:24:44 -0400

    fixed some errors

--- sdk/communication/azure-communication-callautomation/src/main/java/com/azure/communication/callautomation/models/events/RecognizeCanceledEvent.java
+++ sdk/communication/azure-communication-callautomation/src/main/java/com/azure/communication/callautomation/models/events/RecognizeCanceledEvent.java
@@ -4,27 +4,8 @@
 package com.azure.communication.callautomation.models.events;
 
 import com.azure.core.annotation.Fluent;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 /** The RecognizeCanceledEvent model. */
 @Fluent
 public final class RecognizeCanceledEvent extends CallAutomationEventBase {
-    /*
-     * Contains the resulting SIP code/sub-code and message from NGC services.
-     */
-    @JsonProperty(value = "resultInformation")
-    private final ResultInformation resultInformation;
-
-    RecognizeCanceledEvent() {
-        this.resultInformation = null;
-    }
-
-    /**
-     * Get the resultInformation property: Contains the resulting SIP code/sub-code and message from NGC services.
-     *
-     * @return the resultInformation value.
-     */
-    public ResultInformation getResultInformation() {
-        return this.resultInformation;
-    }
 }
