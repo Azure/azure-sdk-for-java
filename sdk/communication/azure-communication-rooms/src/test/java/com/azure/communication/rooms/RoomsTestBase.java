b254f13af7bd0f82be50d6817ac4624f3bfa97d2
Aigerim Beishenbekova <aigerimb@microsoft.com>
2022-10-17 21:37:13 +0200

    rooms: updated recording with the latest identity API version

--- sdk/communication/azure-communication-rooms/src/test/java/com/azure/communication/rooms/RoomsTestBase.java
+++ sdk/communication/azure-communication-rooms/src/test/java/com/azure/communication/rooms/RoomsTestBase.java
@@ -7,7 +7,6 @@ import com.azure.communication.common.CommunicationUserIdentifier;
 import com.azure.communication.common.implementation.CommunicationConnectionString;
 import com.azure.communication.identity.CommunicationIdentityClient;
 import com.azure.communication.identity.CommunicationIdentityClientBuilder;
-import com.azure.communication.identity.CommunicationIdentityServiceVersion;
 import com.azure.communication.rooms.models.*;
 import com.azure.core.credential.AccessToken;
 import com.azure.core.credential.AzureKeyCredential;
@@ -136,7 +135,6 @@ public class RoomsTestBase extends TestBase {
         String accessKey = connectionStringObject.getAccessKey();
         builder.endpoint(endpoint)
             .credential(new AzureKeyCredential(accessKey))
-            .serviceVersion(CommunicationIdentityServiceVersion.V2021_03_07)
             .httpClient(httpClient == null ? interceptorManager.getPlaybackClient() : httpClient);
 
         if (getTestMode() == TestMode.RECORD) {
