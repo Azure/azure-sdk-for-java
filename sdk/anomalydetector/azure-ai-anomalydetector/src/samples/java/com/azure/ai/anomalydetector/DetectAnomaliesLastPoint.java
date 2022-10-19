36a0fa605e727fa015023fbb66afebd46f5e965e
Fanling Huang <fanlinghuang@microsoft.com>
2022-10-19 11:39:24 +0800

    update DetectAnomaliesLastPoint sample

--- sdk/anomalydetector/azure-ai-anomalydetector/src/samples/java/com/azure/ai/anomalydetector/DetectAnomaliesLastPoint.java
+++ sdk/anomalydetector/azure-ai-anomalydetector/src/samples/java/com/azure/ai/anomalydetector/DetectAnomaliesLastPoint.java
@@ -3,11 +3,6 @@
 
 package com.azure.ai.anomalydetector;
 
-import com.azure.ai.anomalydetector.models.DetectRequest;
-import com.azure.ai.anomalydetector.models.LastDetectResponse;
-import com.azure.ai.anomalydetector.models.TimeGranularity;
-import com.azure.ai.anomalydetector.models.TimeSeriesPoint;
-import com.azure.ai.anomalydetector.models.ImputeMode;
 import com.azure.core.credential.AzureKeyCredential;
 import com.azure.core.http.ContentType;
 import com.azure.core.http.HttpClient;
@@ -17,8 +12,15 @@ import com.azure.core.http.HttpPipelineBuilder;
 import com.azure.core.http.policy.AddHeadersPolicy;
 import com.azure.core.http.policy.AzureKeyCredentialPolicy;
 import com.azure.core.http.policy.HttpPipelinePolicy;
+import com.azure.core.http.rest.RequestOptions;
+import com.azure.core.http.rest.Response;
+import com.azure.core.util.BinaryData;
 
+import javax.json.Json;
+import javax.json.JsonReader;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -52,42 +54,16 @@ public class DetectAnomaliesLastPoint {
         // Instantiate a client that will be used to call the service.
         AnomalyDetectorClient anomalyDetectorClient = new AnomalyDetectorClientBuilder()
             .pipeline(httpPipeline)
+            .apiVersion("v1.1")
             .endpoint(endpoint)
             .buildClient();
 
-        // Read the time series from csv file and organize the time series into list of TimeSeriesPoint.
-        // The sample csv file has no header, and it contains 2 columns, namely timestamp and value.
-        // The following is a snippet of the sample csv file:
-        //      2018-03-01T00:00:00Z,32858923
-        //      2018-03-02T00:00:00Z,29615278
-        //      2018-03-03T00:00:00Z,22839355
-        //      2018-03-04T00:00:00Z,25948736
-        Path path = Paths.get("./src/samples/java/sample_data/request-data.csv");
-        List<String> requestData = Files.readAllLines(path);
-        List<TimeSeriesPoint> series = requestData.stream()
-            .map(line -> line.trim())
-            .filter(line -> line.length() > 0)
-            .map(line -> line.split(",", 2))
-            .filter(splits -> splits.length == 2)
-            .map(splits -> {
-                TimeSeriesPoint timeSeriesPoint = new TimeSeriesPoint();
-                timeSeriesPoint.setTimestamp(OffsetDateTime.parse(splits[0]));
-                timeSeriesPoint.setValue(Float.parseFloat(splits[1]));
-                return timeSeriesPoint;
-            })
-            .collect(Collectors.toList());
+        InputStream fileInputStream = new FileInputStream("azure-ai-anomalydetector\\src\\samples\\java\\sample_data\\request-data.json");
+        JsonReader reader = Json.createReader(fileInputStream);
+        BinaryData detectBody = BinaryData.fromString(reader.readObject().toString());
+        RequestOptions requestOptions = new RequestOptions();
+        Response<BinaryData> response = anomalyDetectorClient.detectLastPointWithResponse(detectBody, requestOptions);
+        System.out.println(response.getValue().toString());
 
-        System.out.println("Determining if latest data point is an anomaly...");
-        DetectRequest request = new DetectRequest();
-        request.setSeries(series);
-        // Set the granularity to be DAILY since the minimal interval in time of the sample data is one day.
-        request.setGranularity(TimeGranularity.DAILY);
-        request.setImputeMode(ImputeMode.AUTO);
-        LastDetectResponse response = anomalyDetectorClient.detectLastPoint(request);
-        if (response.isAnomaly()) {
-            System.out.println("The latest point was detected as an anomaly.");
-        } else {
-            System.out.println("The latest point was not detected as an anomaly.");
-        }
     }
 }
