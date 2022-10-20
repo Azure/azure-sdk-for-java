2c7f751d42bdbdb8e20b00a97a2e28e1ec411492
Xiaolu Dai <xiada@microsoft.com>
2022-10-20 17:05:18 +0800

    refactor test

--- sdk/spring/spring-cloud-azure-autoconfigure/src/test/java/com/azure/spring/cloud/autoconfigure/data/cosmos/CosmosDataDiagnosticsConfigurationTests.java
+++ sdk/spring/spring-cloud-azure-autoconfigure/src/test/java/com/azure/spring/cloud/autoconfigure/data/cosmos/CosmosDataDiagnosticsConfigurationTests.java
@@ -3,69 +3,43 @@
 
 package com.azure.spring.cloud.autoconfigure.data.cosmos;
 
-import com.azure.cosmos.CosmosAsyncClient;
-import com.azure.cosmos.CosmosClientBuilder;
-import com.azure.spring.cloud.autoconfigure.context.AzureGlobalPropertiesAutoConfiguration;
-import com.azure.spring.cloud.autoconfigure.cosmos.AzureCosmosAutoConfiguration;
-import com.azure.spring.data.cosmos.CosmosFactory;
 import com.azure.spring.data.cosmos.core.ResponseDiagnostics;
 import com.azure.spring.data.cosmos.core.ResponseDiagnosticsProcessor;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockedStatic;
 import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.*;
 
 class CosmosDataDiagnosticsConfigurationTests {
 
-    private static final String ENDPOINT = "https://test.documents.azure.com:443/";
     private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
-        .withConfiguration(AutoConfigurations.of(
-            AzureGlobalPropertiesAutoConfiguration.class,
-            AzureCosmosAutoConfiguration.class,
-            CosmosDataAutoConfiguration.class));
-    CosmosClientBuilder cosmosClientBuilder = mock(CosmosClientBuilder.class);
-    CosmosAsyncClient cosmosAsyncClient = mock(CosmosAsyncClient.class);
+        .withConfiguration(AutoConfigurations.of(TestAutoconfigurationClass.class));
 
     @Test
     void configureWithPopulateQueryMetricsEnabled() {
-        try (MockedStatic<CosmosFactory> mockedStatic = mockStatic(CosmosFactory.class, RETURNS_MOCKS)) {
-            mockedStatic.when(() -> CosmosFactory.createCosmosAsyncClient(cosmosClientBuilder))
-                .thenReturn(cosmosAsyncClient);
-            this.contextRunner
-                .withBean(CosmosClientBuilder.class, () -> cosmosClientBuilder)
-                .withPropertyValues(
-                    "spring.cloud.azure.cosmos.endpoint=" + ENDPOINT,
-                    "spring.cloud.azure.cosmos.database=test",
-                    "spring.cloud.azure.cosmos.populate-query-metrics=true")
-                .run(context -> {
-                    assertThat(context).hasSingleBean(CosmosDataDiagnosticsConfiguration.class);
-                    assertThat(context).hasSingleBean(ResponseDiagnosticsProcessor.class);
-                });
-        }
+        this.contextRunner
+            .withPropertyValues(
+                "spring.cloud.azure.cosmos.populate-query-metrics=true")
+            .run(context -> {
+                assertThat(context).hasSingleBean(CosmosDataDiagnosticsConfiguration.class);
+                assertThat(context).hasSingleBean(ResponseDiagnosticsProcessor.class);
+            });
     }
 
     @Test
     void configureWithPopulateQueryMetricsDisabled() {
-        try (MockedStatic<CosmosFactory> mockedStatic = mockStatic(CosmosFactory.class, RETURNS_MOCKS)) {
-            mockedStatic.when(() -> CosmosFactory.createCosmosAsyncClient(cosmosClientBuilder))
-                .thenReturn(cosmosAsyncClient);
-            this.contextRunner
-                .withBean(CosmosClientBuilder.class, () -> cosmosClientBuilder)
-                .withPropertyValues(
-                    "spring.cloud.azure.cosmos.endpoint=" + ENDPOINT,
-                    "spring.cloud.azure.cosmos.database=test-database",
-                    "spring.cloud.azure.cosmos.populate-query-metrics=false")
-                .run(context -> {
-                    assertThat(context).doesNotHaveBean(ResponseDiagnosticsProcessor.class);
-                    assertThat(context).hasSingleBean(CosmosDataAutoConfiguration.class);
-                });
-        }
+        this.contextRunner
+            .withPropertyValues(
+                "spring.cloud.azure.cosmos.populate-query-metrics=false")
+            .run(context -> {
+                assertThat(context).doesNotHaveBean(CosmosDataDiagnosticsConfiguration.class);
+                assertThat(context).doesNotHaveBean(ResponseDiagnosticsProcessor.class);
+            });
     }
 
     @Test
@@ -90,5 +64,11 @@ class CosmosDataDiagnosticsConfigurationTests {
         @Override
         public void processResponseDiagnostics(ResponseDiagnostics responseDiagnostics) {
         }
+    }
+
+    @Configuration
+    @Import(CosmosDataDiagnosticsConfiguration.class)
+    static class TestAutoconfigurationClass {
+
     }
 }
