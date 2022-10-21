af84ea6f36e81cebf4178a02513228030818c65a
Rujun Chen <rujche@microsoft.com>
2022-10-21 17:13:24 +0800

    Fix 31218 rest template is not well configured when jwk resolver is null

--- sdk/spring/spring-cloud-azure-autoconfigure/src/test/java/com/azure/spring/cloud/autoconfigure/useragent/CosmosUserAgentTests.java
+++ sdk/spring/spring-cloud-azure-autoconfigure/src/test/java/com/azure/spring/cloud/autoconfigure/useragent/CosmosUserAgentTests.java
@@ -9,7 +9,6 @@ import com.azure.cosmos.CosmosClientBuilder;
 import com.azure.spring.cloud.autoconfigure.context.AzureGlobalProperties;
 import com.azure.spring.cloud.autoconfigure.cosmos.AzureCosmosAutoConfiguration;
 import com.azure.spring.cloud.autoconfigure.implementation.cosmos.properties.AzureCosmosProperties;
-import com.azure.spring.cloud.autoconfigure.useragent.util.UserAgentTestUtil;
 import com.azure.spring.cloud.core.implementation.util.AzureSpringIdentifier;
 import com.azure.spring.cloud.service.implementation.cosmos.CosmosClientBuilderFactory;
 import org.junit.jupiter.api.Assertions;
@@ -17,6 +16,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 
+import static com.azure.spring.cloud.core.implementation.util.ReflectionUtils.getField;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 
@@ -28,23 +28,23 @@ class CosmosUserAgentTests {
         final CosmosClient mockCosmosClient = mock(CosmosClient.class);
         final CosmosAsyncClient cosmosAsyncClient = mock(CosmosAsyncClient.class);
         new ApplicationContextRunner()
-            .withConfiguration(AutoConfigurations.of(AzureCosmosAutoConfiguration.class))
-            .withPropertyValues("spring.cloud.azure.cosmos.endpoint=sample")
-            .withBean(AzureGlobalProperties.class, AzureGlobalProperties::new)
-            .withBean(CosmosClient.class, () -> mockCosmosClient)
-            .withBean(CosmosAsyncClient.class, () -> cosmosAsyncClient)
-            .run(context -> {
-                assertThat(context).hasSingleBean(AzureCosmosProperties.class);
-                assertThat(context).hasSingleBean(CosmosClientBuilderFactory.class);
-                assertThat(context).hasSingleBean(CosmosClientBuilder.class);
-                assertThat(context).hasSingleBean(CosmosClient.class);
-                assertThat(context).hasSingleBean(CosmosAsyncClient.class);
+                .withConfiguration(AutoConfigurations.of(AzureCosmosAutoConfiguration.class))
+                .withPropertyValues("spring.cloud.azure.cosmos.endpoint=sample")
+                .withBean(AzureGlobalProperties.class, AzureGlobalProperties::new)
+                .withBean(CosmosClient.class, () -> mockCosmosClient)
+                .withBean(CosmosAsyncClient.class, () -> cosmosAsyncClient)
+                .run(context -> {
+                    assertThat(context).hasSingleBean(AzureCosmosProperties.class);
+                    assertThat(context).hasSingleBean(CosmosClientBuilderFactory.class);
+                    assertThat(context).hasSingleBean(CosmosClientBuilder.class);
+                    assertThat(context).hasSingleBean(CosmosClient.class);
+                    assertThat(context).hasSingleBean(CosmosAsyncClient.class);
 
-                CosmosClientBuilder builder = context.getBean(CosmosClientBuilder.class);
-                String userAgent = (String) UserAgentTestUtil.getPrivateFieldValue(CosmosClientBuilder.class, "userAgentSuffix", builder);
-                Assertions.assertNotNull(userAgent);
-                Assertions.assertEquals(AzureSpringIdentifier.AZURE_SPRING_COSMOS, userAgent);
-            });
+                    CosmosClientBuilder builder = context.getBean(CosmosClientBuilder.class);
+                    String userAgent = (String) getField(CosmosClientBuilder.class, "userAgentSuffix", builder);
+                    Assertions.assertNotNull(userAgent);
+                    Assertions.assertEquals(AzureSpringIdentifier.AZURE_SPRING_COSMOS, userAgent);
+                });
     }
 
 }
