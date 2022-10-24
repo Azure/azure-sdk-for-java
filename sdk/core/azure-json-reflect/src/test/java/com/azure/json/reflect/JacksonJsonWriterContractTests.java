e8f12f17f9ed04c7d867655edeea31d73adbc48a
Jack Sandford <jacksandford5757@gmail.com>
2022-10-24 16:46:34 +1300

    Refactored project to return JsonProviders

--- sdk/core/azure-json-reflect/src/test/java/com/azure/json/reflect/JacksonJsonWriterContractTests.java
+++ sdk/core/azure-json-reflect/src/test/java/com/azure/json/reflect/JacksonJsonWriterContractTests.java
@@ -3,7 +3,6 @@
 
 package com.azure.json.reflect;
 
-import com.azure.json.JsonOptions;
 import com.azure.json.JsonWriter;
 import com.azure.json.contract.JsonWriterContractTests;
 import org.junit.jupiter.api.BeforeEach;
@@ -22,7 +21,7 @@ public class JacksonJsonWriterContractTests extends JsonWriterContractTests {
     @BeforeEach
     public void beforeEach() throws IOException {
         this.outputStream = new ByteArrayOutputStream();
-        this.writer = JsonFactory.getJacksonInstance().getJsonWriter(outputStream, new JsonOptions());
+        this.writer = JsonProviderFactory.getJacksonJsonProvider().createWriter(outputStream);
     }
 
     @Override
