6e2b7296e3c14df72a8ce5973e90ea9177767094
annie-mac <annie-mac@annie-macs-MacBook-Pro.local>
2022-10-26 14:08:04 -0700

    changes

--- sdk/cosmos/azure-cosmos-spark_3_2-12/src/test/scala/com/azure/cosmos/spark/SparkE2EQueryBaseITest.scala
+++ sdk/cosmos/azure-cosmos-spark_3_2-12/src/test/scala/com/azure/cosmos/spark/SparkE2EQueryBaseITest.scala
@@ -13,7 +13,7 @@ import java.sql.Timestamp
 import java.util.UUID
 import scala.collection.mutable
 
-class SparkE2EQueryITest
+class SparkE2EQueryBaseITest
   extends IntegrationSpec
     with SparkWithJustDropwizardAndNoSlf4jMetrics
     with CosmosClient
@@ -868,63 +868,6 @@ class SparkE2EQueryITest
     fieldNames.contains(CosmosTableSchemaInferrer.ResourceIdAttributeName) shouldBe false
     fieldNames.contains(CosmosTableSchemaInferrer.ETagAttributeName) shouldBe false
     fieldNames.contains(CosmosTableSchemaInferrer.AttachmentsAttributeName) shouldBe false
-  }
-
-  "spark query" can "return proper Cosmos specific query plan on explain with nullable properties" in {
-    val cosmosEndpoint = TestConfigurations.HOST
-    val cosmosMasterKey = TestConfigurations.MASTER_KEY
-
-    val id = UUID.randomUUID().toString
-
-    val rawItem = s"""
-                     | {
-                     |   "id" : "$id",
-                     |   "nestedObject" : {
-                     |     "prop1" : 5,
-                     |     "prop2" : "6"
-                     |   }
-                     | }
-                     |""".stripMargin
-
-    val objectNode = objectMapper.readValue(rawItem, classOf[ObjectNode])
-
-    val container = cosmosClient.getDatabase(cosmosDatabase).getContainer(cosmosContainer)
-    container.createItem(objectNode).block()
-
-    val cfg = Map("spark.cosmos.accountEndpoint" -> cosmosEndpoint,
-      "spark.cosmos.accountKey" -> cosmosMasterKey,
-      "spark.cosmos.database" -> cosmosDatabase,
-      "spark.cosmos.container" -> cosmosContainer,
-      "spark.cosmos.read.inferSchema.forceNullableProperties" -> "true",
-      "spark.cosmos.read.partitioning.strategy" -> "Restrictive"
-    )
-
-    val df = spark.read.format("cosmos.oltp").options(cfg).load()
-    val rowsArray = df.where("nestedObject.prop2 = '6'").collect()
-    rowsArray should have size 1
-
-    var output = new java.io.ByteArrayOutputStream()
-    Console.withOut(output) {
-      df.explain()
-    }
-    var queryPlan = output.toString.replaceAll("#\\d+", "#x")
-    logInfo(s"Query Plan: $queryPlan")
-    queryPlan.contains("Cosmos Query: SELECT * FROM r") shouldEqual true
-
-    output = new java.io.ByteArrayOutputStream()
-    Console.withOut(output) {
-      df.where("nestedObject.prop2 = '6'").explain()
-    }
-    queryPlan = output.toString.replaceAll("#\\d+", "#x")
-    logInfo(s"nullable properties Query Plan: $queryPlan")
-    val expected = s"Cosmos Query: SELECT * FROM r WHERE (NOT(IS_NULL(r['nestedObject'])) AND IS_DEFINED(r['nestedObject'])) " +
-      s"AND r['nestedObject']['prop2']=" +
-      s"@param0${System.getProperty("line.separator")} > param: @param0 = 6"
-    logInfo(s"nullable properties $expected, matches the real query plan ${queryPlan.contains(expected)}")
-    queryPlan.contains(expected) shouldEqual true
-
-    val item = rowsArray(0)
-    item.getAs[String]("id") shouldEqual id
   }
 
   "spark query" can "for single logical partition can be configured to use only one Spark partition" in  {
