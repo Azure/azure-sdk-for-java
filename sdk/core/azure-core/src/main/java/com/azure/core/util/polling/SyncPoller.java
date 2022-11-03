f7197ce652a050917269ffb55f3617fa44c43598
Alan Zimmer <48699787+alzimmermsft@users.noreply.github.com>
2022-11-03 12:31:39 -0400

    Full implementation of synchronous polling stategies

--- sdk/core/azure-core/src/main/java/com/azure/core/util/polling/SyncPoller.java
+++ sdk/core/azure-core/src/main/java/com/azure/core/util/polling/SyncPoller.java
@@ -72,7 +72,7 @@ public interface SyncPoller<T, U> {
     PollResponse<T> waitUntil(Duration timeout, LongRunningOperationStatus statusToWaitFor);
 
     /**
-     * Retrieve the final result of the long running operation.
+     * Retrieve the final result of the long-running operation.
      *
      * @return the final result of the long-running operation if there is one.
      */
@@ -102,16 +102,16 @@ public interface SyncPoller<T, U> {
      * Creates default SyncPoller.
      *
      * @param pollInterval the polling interval.
-     * @param syncActivationOperation the operation to synchronously activate (start) the long running operation, this
+     * @param syncActivationOperation the operation to synchronously activate (start) the long-running operation, this
      * operation will be called with a new {@link PollingContext}.
-     * @param pollOperation the operation to poll the current state of long running operation, this parameter is
+     * @param pollOperation the operation to poll the current state of long-running operation, this parameter is
      * required and the operation will be called with current {@link PollingContext}.
-     * @param cancelOperation a {@link Function} that represents the operation to cancel the long running operation if
+     * @param cancelOperation a {@link Function} that represents the operation to cancel the long-running operation if
      * service supports cancellation, this parameter is required and if service does not support cancellation then the
-     * implementer should return Mono.error with an error message indicating absence of cancellation support, the
+     * implementer should throw an exception with an error message indicating absence of cancellation support, the
      * operation will be called with current {@link PollingContext}.
      * @param fetchResultOperation a {@link Function} that represents the  operation to retrieve final result of the
-     * long running operation if service support it, this parameter is required and operation will be called current
+     * long-running operation if service support it, this parameter is required and operation will be called current
      * {@link PollingContext}, if service does not have an api to fetch final result and if final result is same as
      * final poll response value then implementer can choose to simply return value from provided final poll response.
      * @param <T> The type of poll response value.
@@ -138,7 +138,7 @@ public interface SyncPoller<T, U> {
      * @param pollInterval the polling interval
      * @param initialOperation the activation operation to activate (start) the long-running operation. This operation
      * will be invoked at most once across all subscriptions. This parameter is required. If there is no specific
-     * activation work to be done then invocation should return Mono.empty(), this operation will be called with a new
+     * activation work to be done then invocation should return null, this operation will be called with a new
      * {@link PollingContext}.
      * @param strategy a known syncrhonous strategy for polling a long-running operation in Azure
      * @param pollResponseType the {@link TypeReference} of the response type from a polling call, or BinaryData if raw
