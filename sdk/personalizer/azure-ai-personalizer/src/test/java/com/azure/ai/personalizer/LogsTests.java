70b9e247be341a6399fe18029b1554644658c720
Sharath Malladi <msharath@live.com>
2022-11-08 13:17:44 -0800

    fix pipeline tests

--- sdk/personalizer/azure-ai-personalizer/src/test/java/com/azure/ai/personalizer/LogsTests.java
+++ sdk/personalizer/azure-ai-personalizer/src/test/java/com/azure/ai/personalizer/LogsTests.java
@@ -6,7 +6,7 @@ package com.azure.ai.personalizer;
 import com.azure.ai.personalizer.administration.PersonalizerAdministrationClient;
 import com.azure.ai.personalizer.administration.models.PersonalizerLogProperties;
 import com.azure.core.http.HttpClient;
-import org.junit.Ignore;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -23,7 +23,7 @@ public class LogsTests extends PersonalizerTestBase {
 
     @ParameterizedTest(name = DISPLAY_NAME_WITH_ARGUMENTS)
     @MethodSource("com.azure.ai.personalizer.TestUtils#getTestParameters")
-    @Ignore("Deleting the logs on the static test resources will prevent us from running evaluations")
+    @Disabled("Deleting the logs on the static test resources will prevent us from running evaluations")
     public final void deleteLogsTest(HttpClient httpClient, PersonalizerServiceVersion serviceVersion) {
         PersonalizerAdministrationClient client = getAdministrationClient(httpClient, serviceVersion, false);
         deleteLogs(client);
