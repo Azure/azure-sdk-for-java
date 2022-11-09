7d7f607822d602de6449548ba6112c0152221501
Adrian Tang <adtang@microsoft.com>
2022-11-09 12:16:27 -0800

    Reset changes to the patch version.

--- sdk/communication/azure-communication-phonenumbers/src/main/java/com/azure/communication/phonenumbers/PhoneNumbersClient.java
+++ sdk/communication/azure-communication-phonenumbers/src/main/java/com/azure/communication/phonenumbers/PhoneNumbersClient.java
@@ -25,21 +25,7 @@ import com.azure.core.util.logging.ClientLogger;
 import com.azure.core.util.polling.SyncPoller;
 
 /**
- * Synchronous client for Communication service phone number operations.
- *
- * <p><strong>Instantiating a synchronous Phone Numbers Client</strong></p>
- *
- * <!-- src_embed com.azure.communication.phonenumbers.client.instantiation -->
- * <pre>
- * PhoneNumbersClient phoneNumberClient = new PhoneNumbersClientBuilder&#40;&#41;
- *     .endpoint&#40;endpoint&#41;
- *     .credential&#40;keyCredential&#41;
- *     .httpClient&#40;httpClient&#41;
- *     .buildClient&#40;&#41;;
- * </pre>
- * <!-- end com.azure.communication.phonenumbers.client.instantiation -->
- *
- * @see PhoneNumbersClientBuilder
+ * Synchronous client for Communication service phone number operations
  */
 @ServiceClient(builder = PhoneNumbersClientBuilder.class, isAsync = false)
 public final class PhoneNumbersClient {
@@ -55,17 +41,6 @@ public final class PhoneNumbersClient {
 
    /**
      * Gets information about a purchased phone number.
-    *
-    * <p><strong>Code Samples</strong></p>
-    *
-    * <!-- src_embed com.azure.communication.phonenumbers.client.getPurchased -->
-    * <pre>
-    * PurchasedPhoneNumber phoneNumber = phoneNumberClient.getPurchasedPhoneNumber&#40;&quot;+18001234567&quot;&#41;;
-    * System.out.println&#40;&quot;Phone Number Value: &quot; + phoneNumber.getPhoneNumber&#40;&#41;&#41;;
-    * System.out.println&#40;&quot;Phone Number Country Code: &quot; + phoneNumber.getCountryCode&#40;&#41;&#41;;
-    * </pre>
-    * <!-- end com.azure.communication.phonenumbers.client.getPurchased -->
-    *
      * @param phoneNumber The phone number id in E.164 format. The leading plus can be either + or encoded
      *                    as %2B.
      * @return {@link PurchasedPhoneNumber} representing the purchased telephone number.
@@ -79,19 +54,6 @@ public final class PhoneNumbersClient {
 
     /**
      * Gets information about a purchased phone number with response.
-     *
-     * <p><strong>Code Samples</strong></p>
-     *
-     * <!-- src_embed com.azure.communication.phonenumbers.client.getPurchasedWithResponse -->
-     * <pre>
-     * Response&lt;PurchasedPhoneNumber&gt; response = phoneNumberClient
-     *     .getPurchasedPhoneNumberWithResponse&#40;&quot;+18001234567&quot;, Context.NONE&#41;;
-     * PurchasedPhoneNumber phoneNumber = response.getValue&#40;&#41;;
-     * System.out.println&#40;&quot;Phone Number Value: &quot; + phoneNumber.getPhoneNumber&#40;&#41;&#41;;
-     * System.out.println&#40;&quot;Phone Number Country Code: &quot; + phoneNumber.getCountryCode&#40;&#41;&#41;;
-     * </pre>
-     * <!-- end com.azure.communication.phonenumbers.client.getPurchasedWithResponse -->
-     *
      * @param phoneNumber The phone number id in E.164 format. The leading plus can be either + or encoded
      *                    as %2B.
      * @param context A {@link Context} representing the request context.
@@ -108,17 +70,6 @@ public final class PhoneNumbersClient {
     /**
      * Gets the list of the purchased phone numbers.
      *
-     * <p><strong>Code Samples</strong></p>
-     *
-     * <!-- src_embed com.azure.communication.phonenumbers.client.listPurchased -->
-     * <pre>
-     * PagedIterable&lt;PurchasedPhoneNumber&gt; phoneNumbers = phoneNumberClient.listPurchasedPhoneNumbers&#40;&#41;;
-     * PurchasedPhoneNumber phoneNumber = phoneNumbers.iterator&#40;&#41;.next&#40;&#41;;
-     * System.out.println&#40;&quot;Phone Number Value: &quot; + phoneNumber.getPhoneNumber&#40;&#41;&#41;;
-     * System.out.println&#40;&quot;Phone Number Country Code: &quot; + phoneNumber.getCountryCode&#40;&#41;&#41;;
-     * </pre>
-     * <!-- end com.azure.communication.phonenumbers.client.listPurchased -->
-     *
      * @return A {@link PagedIterable} of {@link PurchasedPhoneNumber} instances representing purchased telephone numbers.
      */
     @ServiceMethod(returns = ReturnType.COLLECTION)
@@ -128,17 +79,6 @@ public final class PhoneNumbersClient {
 
     /**
      * Gets the list of the purchased phone numbers with context.
-     *
-     * <p><strong>Code Samples</strong></p>
-     *
-     * <!-- src_embed com.azure.communication.phonenumbers.client.listPurchasedWithContext -->
-     * <pre>
-     * PagedIterable&lt;PurchasedPhoneNumber&gt; phoneNumbers = phoneNumberClient.listPurchasedPhoneNumbers&#40;Context.NONE&#41;;
-     * PurchasedPhoneNumber phoneNumber = phoneNumbers.iterator&#40;&#41;.next&#40;&#41;;
-     * System.out.println&#40;&quot;Phone Number Value: &quot; + phoneNumber.getPhoneNumber&#40;&#41;&#41;;
-     * System.out.println&#40;&quot;Phone Number Country Code: &quot; + phoneNumber.getCountryCode&#40;&#41;&#41;;
-     * </pre>
-     * <!-- end com.azure.communication.phonenumbers.client.listPurchasedWithContext -->
      *
      * @param context A {@link Context} representing the request context.
      * @return A {@link PagedIterable} of {@link PurchasedPhoneNumber} instances representing purchased telephone numbers.
@@ -154,30 +94,6 @@ public final class PhoneNumbersClient {
      *
      * This function returns a Long Running Operation poller that allows you to wait indefinitely until the
      * operation is complete.
-     *
-     * <p><strong>Code Samples</strong></p>
-     *
-     * <!-- src_embed com.azure.communication.phonenumbers.client.beginSearchAvailable -->
-     * <pre>
-     * PhoneNumberCapabilities capabilities = new PhoneNumberCapabilities&#40;&#41;
-     *     .setCalling&#40;PhoneNumberCapabilityType.INBOUND&#41;
-     *     .setSms&#40;PhoneNumberCapabilityType.INBOUND_OUTBOUND&#41;;
-     *
-     * SyncPoller&lt;PhoneNumberOperation, PhoneNumberSearchResult&gt; poller = phoneNumberClient
-     *     .beginSearchAvailablePhoneNumbers&#40;&quot;US&quot;, PhoneNumberType.TOLL_FREE,
-     *         PhoneNumberAssignmentType.APPLICATION, capabilities&#41;;
-     * PollResponse&lt;PhoneNumberOperation&gt; response = poller.waitForCompletion&#40;&#41;;
-     * String searchId = &quot;&quot;;
-     *
-     * if &#40;LongRunningOperationStatus.SUCCESSFULLY_COMPLETED == response.getStatus&#40;&#41;&#41; &#123;
-     *     PhoneNumberSearchResult searchResult = poller.getFinalResult&#40;&#41;;
-     *     searchId = searchResult.getSearchId&#40;&#41;;
-     *     System.out.println&#40;&quot;Searched phone numbers: &quot; + searchResult.getPhoneNumbers&#40;&#41;&#41;;
-     *     System.out.println&#40;&quot;Search expires by: &quot; + searchResult.getSearchExpiresBy&#40;&#41;&#41;;
-     *     System.out.println&#40;&quot;Phone number costs:&quot; + searchResult.getCost&#40;&#41;.getAmount&#40;&#41;&#41;;
-     * &#125;
-     * </pre>
-     * <!-- end com.azure.communication.phonenumbers.client.beginSearchAvailable -->
      *
      * @param countryCode The ISO 3166-2 country code.
      * @param phoneNumberType {@link PhoneNumberType} The phone number type.
@@ -197,31 +113,6 @@ public final class PhoneNumbersClient {
      *
      * This function returns a Long Running Operation poller that allows you to wait indefinitely until the
      * operation is complete.
-     *
-     * <p><strong>Code Samples</strong></p>
-     *
-     * <!-- src_embed com.azure.communication.phonenumbers.client.beginSearchAvailableWithOptions -->
-     * <pre>
-     * PhoneNumberCapabilities capabilities = new PhoneNumberCapabilities&#40;&#41;
-     *     .setCalling&#40;PhoneNumberCapabilityType.INBOUND&#41;
-     *     .setSms&#40;PhoneNumberCapabilityType.INBOUND_OUTBOUND&#41;;
-     * PhoneNumberSearchOptions searchOptions = new PhoneNumberSearchOptions&#40;&#41;.setAreaCode&#40;&quot;800&quot;&#41;.setQuantity&#40;1&#41;;
-     *
-     * SyncPoller&lt;PhoneNumberOperation, PhoneNumberSearchResult&gt; poller = phoneNumberClient
-     *     .beginSearchAvailablePhoneNumbers&#40;&quot;US&quot;, PhoneNumberType.TOLL_FREE,
-     *         PhoneNumberAssignmentType.APPLICATION, capabilities, searchOptions, Context.NONE&#41;;
-     * PollResponse&lt;PhoneNumberOperation&gt; response = poller.waitForCompletion&#40;&#41;;
-     * String searchId = &quot;&quot;;
-     *
-     * if &#40;LongRunningOperationStatus.SUCCESSFULLY_COMPLETED == response.getStatus&#40;&#41;&#41; &#123;
-     *     PhoneNumberSearchResult searchResult = poller.getFinalResult&#40;&#41;;
-     *     searchId = searchResult.getSearchId&#40;&#41;;
-     *     System.out.println&#40;&quot;Searched phone numbers: &quot; + searchResult.getPhoneNumbers&#40;&#41;&#41;;
-     *     System.out.println&#40;&quot;Search expires by: &quot; + searchResult.getSearchExpiresBy&#40;&#41;&#41;;
-     *     System.out.println&#40;&quot;Phone number costs:&quot; + searchResult.getCost&#40;&#41;.getAmount&#40;&#41;&#41;;
-     * &#125;
-     * </pre>
-     * <!-- end com.azure.communication.phonenumbers.client.beginSearchAvailableWithOptions -->
      *
      * @param countryCode The ISO 3166-2 country code.
      * @param phoneNumberType {@link PhoneNumberType} The phone number type.
@@ -245,16 +136,6 @@ public final class PhoneNumbersClient {
      * This function returns a Long Running Operation poller that allows you to wait indefinitely until the
      * operation is complete.
      *
-     * <p><strong>Code Samples</strong></p>
-     *
-     * <!-- src_embed com.azure.communication.phonenumbers.client.beginPurchase -->
-     * <pre>
-     * PollResponse&lt;PhoneNumberOperation&gt; purchaseResponse =
-     *     phoneNumberClient.beginPurchasePhoneNumbers&#40;searchId&#41;.waitForCompletion&#40;&#41;;
-     * System.out.println&#40;&quot;Purchase phone numbers is complete: &quot; + purchaseResponse.getStatus&#40;&#41;&#41;;
-     * </pre>
-     * <!-- end com.azure.communication.phonenumbers.client.beginPurchase -->
-     *
      * @param searchId ID of the search
      * @return A {@link SyncPoller} object with PurchasePhoneNumbersResult.
      * @throws NullPointerException if {@code searchId} is null.
@@ -269,16 +150,6 @@ public final class PhoneNumbersClient {
      *
      * This function returns a Long Running Operation poller that allows you to wait indefinitely until the
      * operation is complete.
-     *
-     * <p><strong>Code Samples</strong></p>
-     *
-     * <!-- src_embed com.azure.communication.phonenumbers.client.beginPurchaseWithContext -->
-     * <pre>
-     * PollResponse&lt;PhoneNumberOperation&gt; purchaseResponse =
-     *     phoneNumberClient.beginPurchasePhoneNumbers&#40;searchId, Context.NONE&#41;.waitForCompletion&#40;&#41;;
-     * System.out.println&#40;&quot;Purchase phone numbers is complete: &quot; + purchaseResponse.getStatus&#40;&#41;&#41;;
-     * </pre>
-     * <!-- end com.azure.communication.phonenumbers.client.beginPurchaseWithContext -->
      *
      * @param searchId ID of the search
      * @param context A {@link Context} representing the request context.
@@ -296,16 +167,6 @@ public final class PhoneNumbersClient {
      * This function returns a Long Running Operation poller that allows you to wait indefinitely until the
      * operation is complete.
      *
-     * <p><strong>Code Samples</strong></p>
-     *
-     * <!-- src_embed com.azure.communication.phonenumbers.client.beginRelease -->
-     * <pre>
-     * PollResponse&lt;PhoneNumberOperation&gt; releaseResponse =
-     *     phoneNumberClient.beginReleasePhoneNumber&#40;&quot;+18001234567&quot;&#41;.waitForCompletion&#40;&#41;;
-     * System.out.println&#40;&quot;Release phone number is complete: &quot; + releaseResponse.getStatus&#40;&#41;&#41;;
-     * </pre>
-     * <!-- end com.azure.communication.phonenumbers.client.beginRelease -->
-     *
      * @param phoneNumber The phone number id in E.164 format. The leading plus can be either + or encoded
      *                    as %2B.
      * @return A {@link SyncPoller} object with ReleasePhoneNumberResult.
@@ -322,15 +183,6 @@ public final class PhoneNumbersClient {
      * This function returns a Long Running Operation poller that allows you to wait indefinitely until the
      * operation is complete.
      *
-     * <p><strong>Code Samples</strong></p>
-     *
-     * <!-- src_embed com.azure.communication.phonenumbers.client.beginReleaseWithContext -->
-     * <pre>
-     * PollResponse&lt;PhoneNumberOperation&gt; releaseResponse =
-     *     phoneNumberClient.beginReleasePhoneNumber&#40;&quot;+18001234567&quot;, Context.NONE&#41;.waitForCompletion&#40;&#41;;
-     * System.out.println&#40;&quot;Release phone number is complete: &quot; + releaseResponse.getStatus&#40;&#41;&#41;;
-     * </pre>
-     * <!-- end com.azure.communication.phonenumbers.client.beginReleaseWithContext -->
      *
      * @param phoneNumber The phone number id in E.164 format. The leading plus can be either + or encoded
      *                    as %2B.
@@ -348,28 +200,6 @@ public final class PhoneNumbersClient {
      *
      * This function returns a Long Running Operation poller that allows you to wait indefinitely until the
      * operation is complete.
-     *
-     * <p><strong>Code Samples</strong></p>
-     *
-     * <!-- src_embed com.azure.communication.phonenumbers.client.beginUpdateCapabilities -->
-     * <pre>
-     * PhoneNumberCapabilities capabilities = new PhoneNumberCapabilities&#40;&#41;;
-     * capabilities
-     *     .setCalling&#40;PhoneNumberCapabilityType.INBOUND&#41;
-     *     .setSms&#40;PhoneNumberCapabilityType.INBOUND_OUTBOUND&#41;;
-     *
-     * SyncPoller&lt;PhoneNumberOperation, PurchasedPhoneNumber&gt; poller =
-     *     phoneNumberClient.beginUpdatePhoneNumberCapabilities&#40;&quot;+18001234567&quot;, capabilities&#41;;
-     * PollResponse&lt;PhoneNumberOperation&gt; response = poller.waitForCompletion&#40;&#41;;
-     *
-     * if &#40;LongRunningOperationStatus.SUCCESSFULLY_COMPLETED == response.getStatus&#40;&#41;&#41; &#123;
-     *     PurchasedPhoneNumber phoneNumber = poller.getFinalResult&#40;&#41;;
-     *     System.out.println&#40;&quot;Phone Number Calling capabilities: &quot; + phoneNumber.getCapabilities&#40;&#41;.getCalling&#40;&#41;&#41;;
-     *     System.out.println&#40;&quot;Phone Number SMS capabilities: &quot; + phoneNumber.getCapabilities&#40;&#41;.getSms&#40;&#41;&#41;;
-     * &#125;
-     * </pre>
-     * <!-- end com.azure.communication.phonenumbers.client.beginUpdateCapabilities -->
-     *
      * @param phoneNumber The phone number id in E.164 format. The leading plus can be either + or encoded
      *                    as %2B.
      * @param capabilities Update capabilities of a purchased phone number.
@@ -386,28 +216,6 @@ public final class PhoneNumbersClient {
      *
      * This function returns a Long Running Operation poller that allows you to wait indefinitely until the
      * operation is complete.
-     *
-     * <p><strong>Code Samples</strong></p>
-     *
-     * <!-- src_embed com.azure.communication.phonenumbers.client.beginUpdateCapabilitiesWithContext -->
-     * <pre>
-     * PhoneNumberCapabilities capabilities = new PhoneNumberCapabilities&#40;&#41;;
-     * capabilities
-     *     .setCalling&#40;PhoneNumberCapabilityType.INBOUND&#41;
-     *     .setSms&#40;PhoneNumberCapabilityType.INBOUND_OUTBOUND&#41;;
-     *
-     * SyncPoller&lt;PhoneNumberOperation, PurchasedPhoneNumber&gt; poller =
-     *     phoneNumberClient.beginUpdatePhoneNumberCapabilities&#40;&quot;+18001234567&quot;, capabilities, Context.NONE&#41;;
-     * PollResponse&lt;PhoneNumberOperation&gt; response = poller.waitForCompletion&#40;&#41;;
-     *
-     * if &#40;LongRunningOperationStatus.SUCCESSFULLY_COMPLETED == response.getStatus&#40;&#41;&#41; &#123;
-     *     PurchasedPhoneNumber phoneNumber = poller.getFinalResult&#40;&#41;;
-     *     System.out.println&#40;&quot;Phone Number Calling capabilities: &quot; + phoneNumber.getCapabilities&#40;&#41;.getCalling&#40;&#41;&#41;;
-     *     System.out.println&#40;&quot;Phone Number SMS capabilities: &quot; + phoneNumber.getCapabilities&#40;&#41;.getSms&#40;&#41;&#41;;
-     * &#125;
-     * </pre>
-     * <!-- end com.azure.communication.phonenumbers.client.beginUpdateCapabilitiesWithContext -->
-     *
      * @param phoneNumber The phone number id in E.164 format. The leading plus can be either + or encoded
      *                    as %2B.
      * @param capabilities Update capabilities of a purchased phone number.
