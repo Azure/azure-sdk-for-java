32c22ef7967887a5301f632f50c84aae67e6c7a3
vcolin7 <victor.y.asi@gmail.com>
2022-11-11 16:19:02 -0600

    Ignoring `releaseKey()` tests test until the service rolls out a fix for an issue with the "version" parameter of a release policy.

--- sdk/keyvault/azure-security-keyvault-keys/src/test/java/com/azure/security/keyvault/keys/KeyClientManagedHsmTest.java
+++ sdk/keyvault/azure-security-keyvault-keys/src/test/java/com/azure/security/keyvault/keys/KeyClientManagedHsmTest.java
@@ -134,6 +134,9 @@ public class KeyClientManagedHsmTest extends KeyClientTest implements KeyClientM
     @ParameterizedTest(name = DISPLAY_NAME_WITH_ARGUMENTS)
     @MethodSource("getTestParameters")
     public void releaseKey(HttpClient httpClient, KeyServiceVersion serviceVersion) {
+        // Ignoring test until the service rolls out a fix for an issue with the "version" parameter of a release
+        // policy.
+        Assumptions.assumeTrue(serviceVersion != KeyServiceVersion.V7_4_PREVIEW_1);
         super.releaseKey(httpClient, serviceVersion);
     }
 }
