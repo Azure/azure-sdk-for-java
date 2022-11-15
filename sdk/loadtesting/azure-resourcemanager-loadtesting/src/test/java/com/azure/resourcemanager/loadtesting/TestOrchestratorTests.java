bd8697bf4937011c47783d0144bff171632adac7
Weidong Xu <weidxu@microsoft.com>
2022-11-15 10:22:09 +0800

    rename test file as suffix with Tests

--- sdk/loadtesting/azure-resourcemanager-loadtesting/src/test/java/com/azure/resourcemanager/loadtesting/TestOrchestratorTests.java
+++ sdk/loadtesting/azure-resourcemanager-loadtesting/src/test/java/com/azure/resourcemanager/loadtesting/TestOrchestratorTests.java
@@ -3,6 +3,8 @@
 
 package com.azure.resourcemanager.loadtesting;
 
+import com.azure.core.http.policy.HttpLogDetailLevel;
+import com.azure.core.http.policy.HttpLogOptions;
 import com.azure.core.management.AzureEnvironment;
 import com.azure.core.management.Region;
 import com.azure.core.management.profile.AzureProfile;
@@ -14,7 +16,7 @@ import com.azure.identity.DefaultAzureCredentialBuilder;
 import java.util.Random;
 import org.junit.jupiter.api.Test;
 
-public class TestOrchestrator extends TestBase {
+public class TestOrchestratorTests extends TestBase {
 
     private static final Random RANDOM = new Random();
     private static final Region LOCATION = Region.US_WEST2;
@@ -40,6 +42,7 @@ public class TestOrchestrator extends TestBase {
         resourceGroupName = Configuration.getGlobalConfiguration().get("AZURE_RESOURCE_GROUP_NAME");
         loadTestManager = LoadTestManager
             .configure()
+            .withLogOptions(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BASIC))
             .authenticate(credential, profile);
     }
 
