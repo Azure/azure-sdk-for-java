38424ea3e6a1b21a10310adce3b3301a65e8652d
yiliu6 <yiliu6@microsoft.com>
2022-11-15 14:22:34 +0800

    move processor-lifecycle-manager from messaging to service module

--- sdk/spring/spring-cloud-azure-service/src/main/java/com/azure/spring/cloud/service/implementation/servicebus/lifecycle/ServiceBusProcessorClientLifecycleManager.java
+++ sdk/spring/spring-cloud-azure-service/src/main/java/com/azure/spring/cloud/service/implementation/servicebus/lifecycle/ServiceBusProcessorClientLifecycleManager.java
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.azure.spring.messaging.servicebus.implementation.core;
+package com.azure.spring.cloud.service.implementation.servicebus.lifecycle;
 
 import com.azure.messaging.servicebus.ServiceBusProcessorClient;
 import org.slf4j.Logger;
