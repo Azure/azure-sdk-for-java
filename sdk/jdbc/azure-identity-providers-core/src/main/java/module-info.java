a801519d41e16f7f4deeea17f250a07d8673f69c
Muyao <muyaofeng@126.com>
2022-11-22 19:19:42 +0800

    move mysql/postgresql/core codes into one module

--- sdk/jdbc/azure-identity-providers-core/src/main/java/module-info.java
+++ sdk/jdbc/azure-identity-providers-core/src/main/java/module-info.java
@@ -1,7 +1,12 @@
+// Licensed under the MIT License.
+// Copyright (c) Microsoft Corporation. All rights reserved.
+
 module com.azure.identity.providers.core {
     requires com.azure.identity;
+    requires static mysql.connector.j;
+    requires static org.postgresql.jdbc;
+    requires transitive java.sql;
 
-    exports com.azure.identity.providers.jdbc.implementation.template to
-        com.azure.identity.providers.mysql,
-        com.azure.identity.providers.postgresql;
+    exports com.azure.identity.providers.mysql;
+    exports com.azure.identity.providers.postgresql;
 }
