d37516c0eeee234b3093fced1acd10b3abfb5b0e
Muyao <muyaofeng@126.com>
2022-11-24 16:05:20 +0800

    change module name to `azure-identity-providers`

--- sdk/jdbc/azure-identity-providers/src/main/java/module-info.java
+++ sdk/jdbc/azure-identity-providers/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 // Licensed under the MIT License.
 // Copyright (c) Microsoft Corporation. All rights reserved.
 
-module com.azure.identity.providers.core {
+module com.azure.identity.providers {
     requires com.azure.identity;
     requires static mysql.connector.j;
     requires static org.postgresql.jdbc;
