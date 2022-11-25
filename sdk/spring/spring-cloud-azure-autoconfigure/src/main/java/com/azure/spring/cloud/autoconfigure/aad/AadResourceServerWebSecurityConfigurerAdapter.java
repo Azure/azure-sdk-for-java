036caa51ba410e268e64f285820ab6b892c01da8
Shili Chen <v-shilichen@microsoft.com>
2022-11-25 17:12:14 +0800

    Enhance the token authentication converter

--- sdk/spring/spring-cloud-azure-autoconfigure/src/main/java/com/azure/spring/cloud/autoconfigure/aad/AadResourceServerWebSecurityConfigurerAdapter.java
+++ sdk/spring/spring-cloud-azure-autoconfigure/src/main/java/com/azure/spring/cloud/autoconfigure/aad/AadResourceServerWebSecurityConfigurerAdapter.java
@@ -10,9 +10,13 @@ import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.util.StringUtils;
+
+import java.util.Collection;
 
 /**
  * Abstract configuration class, used to make JwtConfigurer and AADJwtBearerTokenAuthenticationConverter take effect.
@@ -22,10 +26,31 @@ import org.springframework.util.StringUtils;
 public abstract class AadResourceServerWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    AadResourceServerProperties properties;
+    private AadResourceServerProperties properties;
+
+    private Converter<Jwt, Collection<GrantedAuthority>> jwtGrantedAuthorityConverter;
 
     /**
-     * configure
+     * Creates a new instance with the default configuration.
+     */
+    public AadResourceServerWebSecurityConfigurerAdapter() {
+    }
+
+    /**
+     * Sets the Azure AD properties and custom granted authority converter to creates a new instance,
+     * the custom granted authority converter can be null.
+     *
+     * @param properties the Azure AD properties for Resource Server
+     * @param jwtGrantedAuthorityConverter the custom converter for JWT granted authority
+     */
+    public AadResourceServerWebSecurityConfigurerAdapter(AadResourceServerProperties properties,
+                                                         Converter<Jwt, Collection<GrantedAuthority>> jwtGrantedAuthorityConverter) {
+        this.properties = properties;
+        this.jwtGrantedAuthorityConverter = jwtGrantedAuthorityConverter;
+    }
+
+    /**
+     * Apply the {@link OAuth2ResourceServerConfigurer}  for Azure AD OAuth2 Resource Server scenario.
      *
      * @param http the {@link HttpSecurity} to use
      * @throws Exception Configuration failed
@@ -45,8 +70,21 @@ public abstract class AadResourceServerWebSecurityConfigurerAdapter extends WebS
         if (StringUtils.hasText(properties.getPrincipalClaimName())) {
             converter.setPrincipalClaimName(properties.getPrincipalClaimName());
         }
-        converter.setJwtGrantedAuthoritiesConverter(
-            new AadJwtGrantedAuthoritiesConverter(properties.getClaimToAuthorityPrefixMap()));
+        if (this.jwtGrantedAuthorityConverter != null) {
+            converter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthorityConverter);
+        } else {
+            converter.setJwtGrantedAuthoritiesConverter(
+                new AadJwtGrantedAuthoritiesConverter(properties.getClaimToAuthorityPrefixMap()));
+        }
         return converter;
+    }
+
+    /**
+     * Custom a JWT granted authority converter.
+     * @param jwtGrantedAuthorityConverter the custom converter
+     */
+    public void setJwtGrantedAuthorityConverter(
+        Converter<Jwt, Collection<GrantedAuthority>> jwtGrantedAuthorityConverter) {
+        this.jwtGrantedAuthorityConverter = jwtGrantedAuthorityConverter;
     }
 }
