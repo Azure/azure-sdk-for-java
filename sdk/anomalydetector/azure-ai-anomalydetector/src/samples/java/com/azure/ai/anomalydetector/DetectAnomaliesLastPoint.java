95c30d59f53a4213caccc9cdf45217c8d8eceacc
Fanling Huang <fanlinghuang@microsoft.com>
2022-11-28 21:51:57 +0800

    revise

--- sdk/anomalydetector/azure-ai-anomalydetector/src/samples/java/com/azure/ai/anomalydetector/DetectAnomaliesLastPoint.java
+++ sdk/anomalydetector/azure-ai-anomalydetector/src/samples/java/com/azure/ai/anomalydetector/DetectAnomaliesLastPoint.java
@@ -5,6 +5,9 @@ package com.azure.ai.anomalydetector;
 
 import com.azure.ai.anomalydetector.models.DetectRequest;
 import com.azure.ai.anomalydetector.models.LastDetectResponse;
+import com.azure.ai.anomalydetector.models.TimeGranularity;
+import com.azure.ai.anomalydetector.models.TimeSeriesPoint;
+import com.azure.ai.anomalydetector.models.ImputeMode;
 
 import com.azure.core.credential.AzureKeyCredential;
 import com.azure.core.http.ContentType;
@@ -15,13 +18,14 @@ import com.azure.core.http.HttpPipelineBuilder;
 import com.azure.core.http.policy.AddHeadersPolicy;
 import com.azure.core.http.policy.AzureKeyCredentialPolicy;
 import com.azure.core.http.policy.HttpPipelinePolicy;
-import com.azure.core.util.BinaryData;
 
-import javax.json.Json;
-import javax.json.JsonReader;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 /**
@@ -54,13 +58,39 @@ public class DetectAnomaliesLastPoint {
             .endpoint(endpoint)
             .buildClient();
 
-        InputStream fileInputStream = new FileInputStream("azure-ai-anomalydetector\\src\\samples\\java\\sample_data\\request-data.json");
-        JsonReader reader = Json.createReader(fileInputStream);
-        BinaryData detectBody = BinaryData.fromString(reader.readObject().toString());
+        // Read the time series from csv file and organize the time series into list of TimeSeriesPoint.
+        // The sample csv file has no header, and it contains 2 columns, namely timestamp and value.
+        // The following is a snippet of the sample csv file:
+        //      2018-03-01T00:00:00Z,32858923
+        //      2018-03-02T00:00:00Z,29615278
+        //      2018-03-03T00:00:00Z,22839355
+        //      2018-03-04T00:00:00Z,25948736
+        Path path = Paths.get("azure-ai-anomalydetector/src/samples/java/sample_data/request-data.csv");
+        List<String> requestData = Files.readAllLines(path);
+        List<TimeSeriesPoint> series = requestData.stream()
+            .map(line -> line.trim())
+            .filter(line -> line.length() > 0)
+            .map(line -> line.split(",", 2))
+            .filter(splits -> splits.length == 2)
+            .map(splits -> {
+                TimeSeriesPoint timeSeriesPoint = new TimeSeriesPoint(Float.parseFloat(splits[1]));
+                timeSeriesPoint.setTimestamp(OffsetDateTime.parse(splits[0]));
+                return timeSeriesPoint;
+            })
+            .collect(Collectors.toList());
 
-        DetectRequest detectRequest = detectBody.toObject(DetectRequest.class);
-        LastDetectResponse lastDetectResponse = anomalyDetectorClient.detectUnivariateLastPoint(detectRequest);
-        System.out.println("ExpectedValue: " + lastDetectResponse.getExpectedValue()
-            + ", Severity: " + lastDetectResponse.getSeverity());
+        System.out.println("Determining if latest data point is an anomaly...");
+        DetectRequest request = new DetectRequest(series);
+        request.setGranularity(TimeGranularity.DAILY);
+        request.setImputeMode(ImputeMode.AUTO);
+
+        LastDetectResponse response = anomalyDetectorClient.detectUnivariateLastPoint(request);
+        System.out.println("ExpectedValue: " + response.getExpectedValue()
+            + ", Severity: " + response.getSeverity());
+        if (response.isAnomaly()) {
+            System.out.println("The latest point was detected as an anomaly.");
+        } else {
+            System.out.println("The latest point was not detected as an anomaly.");
+        }
     }
 }
