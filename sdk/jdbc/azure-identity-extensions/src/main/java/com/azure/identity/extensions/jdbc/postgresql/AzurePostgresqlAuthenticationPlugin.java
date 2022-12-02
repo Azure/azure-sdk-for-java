78bc22ce27ac2d9c607028d9a8324055baeff9dd
Muyao <muyaofeng@126.com>
2022-12-02 15:25:31 +0800

    fix javadoc error on jdk8/11

--- sdk/jdbc/azure-identity-extensions/src/main/java/com/azure/identity/extensions/jdbc/postgresql/AzurePostgresqlAuthenticationPlugin.java
+++ sdk/jdbc/azure-identity-extensions/src/main/java/com/azure/identity/extensions/jdbc/postgresql/AzurePostgresqlAuthenticationPlugin.java
@@ -36,15 +36,15 @@ public class AzurePostgresqlAuthenticationPlugin implements AuthenticationPlugin
     /**
      * Callback method to provide the password to use for authentication.
      *
-     * @param type The authentication method that the server is requesting.<br/>
-     *             <br/>
-     *             <p>AzurePostgresqlAuthenticationPlugin is used as an extension to<br/>
+     * @param type The authentication method that the server is requesting.
+     *
+     *             <p>AzurePostgresqlAuthenticationPlugin is used as an extension to
      *             perform authentication with Azure AD,the value here is CLEARTEXT_PASSWORD.</p>
-     *             <br/>
-     *             When PostgreSQL client trying to connect with PostgreSQL server:<br/>
+     *
+     *             When PostgreSQL client trying to connect with PostgreSQL server:
      *             1. Client will send startup packet to server, the server will return the AuthenticationRequestType it accepts,
-     *                If the username is used to perform Azure AD authentication, the server will return CLEARTEXT_PASSWORD.<br/>
-     *             2. Client will do authentication (until AuthenticationOk).<br/>
+     *                If the username is used to perform Azure AD authentication, the server will return CLEARTEXT_PASSWORD.
+     *             2. Client will do authentication (until AuthenticationOk).
      *
      * @return The password to use.
      * @throws PSQLException It will return a PSQLException if the password is null.
