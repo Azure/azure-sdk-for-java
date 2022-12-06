6ffdf4407b6f07113c1d2e87bd42cfa53cba13aa
Jocelyn Schreppler <jaschrep@microsoft.com>
2022-12-06 11:41:18 -0500

    javadocs

--- sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/options/PageBlobUploadPagesOptions.java
+++ sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/options/PageBlobUploadPagesOptions.java
@@ -8,12 +8,20 @@ import com.azure.storage.blob.models.PageBlobRequestConditions;
 import com.azure.storage.blob.models.PageRange;
 import com.azure.storage.common.implementation.StorageImplUtils;
 
+/**
+ * Extended options that may be passed when uploading pages.
+ */
 public class PageBlobUploadPagesOptions {
     private final PageRange pageRange;
     private final BinaryData data;
     private byte[] contentMd5;
     private PageBlobRequestConditions conditions;
 
+    /**
+     * @param pageRange The page range to upload to.
+     * @param data The data to write to the block. Note that this {@code BinaryData} must have defined length
+     * and must be replayable if retries are enabled (the default), see {@link BinaryData#isReplayable()}.
+     */
     public PageBlobUploadPagesOptions(PageRange pageRange, BinaryData data) {
         StorageImplUtils.assertNotNull("pageRange must not be null", pageRange);
         StorageImplUtils.assertNotNull("data must not be null", data);
@@ -21,27 +29,47 @@ public class PageBlobUploadPagesOptions {
         this.data = data;
     }
 
+    /**
+     * @return The block ID to assign the new block.
+     */
     public PageRange getPageRange() {
         return pageRange;
     }
 
+    /**
+     * @return The data to write to the blob.
+     */
     public BinaryData getData() {
         return data;
     }
 
+    /**
+     * @return The MD5 of this instance's data.
+     */
     public byte[] getContentMd5() {
         return contentMd5;
     }
 
+    /**
+     * @param contentMd5 MD5 of this instance's data.
+     * @return The updated options.
+     */
     public PageBlobUploadPagesOptions setContentMd5(byte[] contentMd5) {
         this.contentMd5 = contentMd5;
         return this;
     }
 
+    /**
+     * @return The request conditions for this operation.
+     */
     public PageBlobRequestConditions getConditions() {
         return conditions;
     }
 
+    /**
+     * @param conditions Request conditions for this operation.
+     * @return The updated options.
+     */
     public PageBlobUploadPagesOptions setConditions(PageBlobRequestConditions conditions) {
         this.conditions = conditions;
         return this;
