4b2e3d99f7372b3f1747eb7835dc66d696eb387d
Bill Wert <billwert@microsoft.com>
2022-12-07 11:24:21 -0800

    fixup module for refactoring

--- sdk/core/azure-core-test/src/main/java/module-info.java
+++ sdk/core/azure-core-test/src/main/java/module-info.java
@@ -9,7 +9,7 @@ module com.azure.core.test {
     requires org.junit.jupiter.params;
     requires reactor.test;
     requires java.management;
-    requires testcontainers;
+    requires java.net.http;
 
     exports com.azure.core.test;
     exports com.azure.core.test.annotation;
