62dc0b3789f3f1c587bb6d291ab04e028ad929e5
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2022-12-07 15:23:34 -0500

    Validate Code Generation as Part of CI

---
+++ eng/pipelines/scripts/Invoke-Codegeneration.ps1
@@ -0,0 +1,27 @@
+<#
+.SYNOPSIS
+Invokes all 'Update-Codegeneration.ps1' scripts found within the specified directory.
+
+.DESCRIPTION
+Invokes all 'Update-Codegeneration.ps1' scripts found within the specified directory.
+
+The regenerated code is then compared for differences to ensure that the code generation files are up-to-date.
+
+.PARAMETER Directory
+The directory that will be searched for 'Update-Codegeneration.ps1' scripts. The default is the root directory of the
+Azure SDK for Java repository. CI jobs should use the 'ServiceDirectory', such as /sdk/storage.
+#>
+
+param(
+  [Parameter(Mandatory = $false)]
+  [string]$Directory
+)
+
+$path = ""
+if ($Directory) {
+  $path = $Directory
+}
+
+foreach ($script in (Get-ChildItem -Path $path -Filter "Update-Codegeneration.ps1" -Recurse)) {
+  Invoke-Expression $script.FullName
+}
---
+++ sdk/storage/azure-storage-file-share/swagger/Update-Codegeneration.ps1
@@ -0,0 +1,6 @@
+$originalLocation = (Get-Location).Path
+Set-Location -Path $PSScriptRoot
+
+autorest
+
+Set-Location -Path $originalLocation
