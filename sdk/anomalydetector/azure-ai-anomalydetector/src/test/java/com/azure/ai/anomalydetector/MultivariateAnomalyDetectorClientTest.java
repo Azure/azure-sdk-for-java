2d4373b854568da07a263ae11886347d29d3c500
Fanling Huang <fanlinghuang@microsoft.com>
2022-12-07 13:40:45 +0800

    add copyright

--- sdk/anomalydetector/azure-ai-anomalydetector/src/test/java/com/azure/ai/anomalydetector/MultivariateAnomalyDetectorClientTest.java
+++ sdk/anomalydetector/azure-ai-anomalydetector/src/test/java/com/azure/ai/anomalydetector/MultivariateAnomalyDetectorClientTest.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.azure.ai.anomalydetector;
 
 import com.azure.ai.anomalydetector.models.AnomalyDetectionModel;
