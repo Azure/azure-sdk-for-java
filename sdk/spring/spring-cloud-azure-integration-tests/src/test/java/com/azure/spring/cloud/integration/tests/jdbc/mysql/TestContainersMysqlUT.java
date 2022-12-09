19996b4135b6b3b876609c50486bc2e8f21283b4
Muyao <muyaofeng@126.com>
2022-12-09 09:11:58 +0800

    ignore mac env

--- sdk/spring/spring-cloud-azure-integration-tests/src/test/java/com/azure/spring/cloud/integration/tests/jdbc/mysql/TestContainersMysqlUT.java
+++ sdk/spring/spring-cloud-azure-integration-tests/src/test/java/com/azure/spring/cloud/integration/tests/jdbc/mysql/TestContainersMysqlUT.java
@@ -1,7 +1,6 @@
 package com.azure.spring.cloud.integration.tests.jdbc.mysql;
 
 import com.azure.core.credential.TokenRequestContext;
-import com.azure.cosmos.implementation.guava25.base.Joiner;
 import com.azure.identity.DefaultAzureCredential;
 import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.azure.spring.cloud.autoconfigure.context.AzureGlobalPropertiesAutoConfiguration;
@@ -10,6 +9,10 @@ import com.azure.spring.cloud.autoconfigure.jdbc.AzureJdbcAutoConfiguration;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
@@ -23,19 +26,15 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
-import java.util.ArrayList;
-import java.util.List;
-
 //@DisabledIfEnvironmentVariable(named = "AZURE_MYSQL_IT_SKIPRUNNING", matches = "skipRunning")
 @Testcontainers
-public class TestContainerSMysqlIT {
-    String VALUE = "information_schema,test";
-
+@DisabledOnOs(OS.MAC)
+public class TestContainersMysqlUT {
+    public static final Logger LOGGER = LoggerFactory.getLogger(TestContainersMysqlUT.class);
     private static DockerImageName dockerImageName = DockerImageName.parse("mysql:5.7");
 
     @Container
     public static MySQLContainer mySQLContainer = new MySQLContainer(dockerImageName);
-
 
     @BeforeEach
     void setUp() {
@@ -54,14 +53,8 @@ public class TestContainerSMysqlIT {
     }
 
     @Test
-    void test() {
-        List<String> list = new ArrayList<>();
-        list.add("25");
-        System.out.println(Joiner.on(",").join(list));
-    }
-    @Test
     void testMysqlPassword() {
-        System.out.println("111"+mySQLContainer.getJdbcUrl());
+        LOGGER.info("TestContainersMysqlUT begin.");
         ApplicationContextRunner contextRunner = new ApplicationContextRunner()
             .withPropertyValues("spring.datasource.azure.passwordless-enabled=true")
             .withConfiguration(
@@ -80,6 +73,8 @@ public class TestContainerSMysqlIT {
                 Assertions.assertEquals(sqlRowSet.getString("Number"),"25");
             }
         }));
+        LOGGER.info("TestContainersMysqlUT end.");
+
     }
 
 }
