f40f80dc9ab4f655480baf79809d74967a93b5ea
Muyao <muyaofeng@126.com>
2022-12-13 01:58:26 +0800

    TestContainers for mysql passwordless

---
+++ sdk/spring/spring-cloud-azure-autoconfigure/src/test/java/com/azure/spring/cloud/autoconfigure/jdbc/PasswordlessMySQLTest.java
@@ -0,0 +1,73 @@
+package com.azure.spring.cloud.autoconfigure.jdbc;
+
+import com.azure.core.credential.TokenRequestContext;
+import com.azure.identity.DefaultAzureCredential;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.spring.cloud.autoconfigure.context.AzureGlobalPropertiesAutoConfiguration;
+import com.azure.spring.cloud.autoconfigure.context.AzureTokenCredentialAutoConfiguration;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@Testcontainers
+@EnabledOnOs(OS.LINUX)
+public class PasswordlessMySQLTest {
+    protected final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+        .withPropertyValues("spring.datasource.azure.passwordless-enabled=true")
+        .withConfiguration(AutoConfigurations.of(DataSourceAutoConfiguration.class, JdbcTemplateAutoConfiguration.class,
+            AzureJdbcAutoConfiguration.class, AzureGlobalPropertiesAutoConfiguration.class,
+            AzureTokenCredentialAutoConfiguration.class));
+    private static DockerImageName dockerImageName = DockerImageName.parse("mysql:5.7");
+
+    @Container
+    public static MySQLContainer mySQLContainer = new MySQLContainer(dockerImageName);
+
+    @BeforeEach
+    void setUp() {
+        DefaultAzureCredential build = new DefaultAzureCredentialBuilder().build();
+        // the scopes vary in different Clouds
+        String password = build.getToken(new TokenRequestContext().addScopes("https://ossrdbms-aad.database.windows"
+                                   + ".net/.default"))
+                               .block()
+                               .getToken();
+        mySQLContainer.withUsername("passwordless-test")
+                      .withPassword(password);
+    }
+
+    @DynamicPropertySource
+    static void properties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mySQLContainer::getUsername);
+    }
+
+    @Test
+    void testMysqlPasswordless() {
+        this.contextRunner
+            .run(context -> {
+                String query1 = "CREATE TABLE Test ( PersonID int, Number int);";
+                String query2 = "INSERT INTO Test VALUES (1,25);";
+                String query3 = "SELECT * FROM Test;";
+                JdbcTemplate jdbcTemplate = context.getBean(JdbcTemplate.class);
+                jdbcTemplate.execute(query1);
+                jdbcTemplate.execute(query2);
+                SqlRowSet sqlRowSet = jdbcTemplate.queryForRowSet(query3);
+                while (sqlRowSet.next()) {
+                    Assertions.assertEquals(sqlRowSet.getString("Number"), "25");
+                }
+            });
+    }
+}
