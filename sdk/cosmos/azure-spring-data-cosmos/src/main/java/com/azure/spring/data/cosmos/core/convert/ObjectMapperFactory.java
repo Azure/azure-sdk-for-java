6bd609b6c6f7e21c9cc9f3a0289d88f30c2b9550
Trevor Anderson <trande@microsoft.com>
2022-12-12 12:59:15 -0500

    Attempting to fix precision issue.

--- sdk/cosmos/azure-spring-data-cosmos/src/main/java/com/azure/spring/data/cosmos/core/convert/ObjectMapperFactory.java
+++ sdk/cosmos/azure-spring-data-cosmos/src/main/java/com/azure/spring/data/cosmos/core/convert/ObjectMapperFactory.java
@@ -5,6 +5,8 @@ package com.azure.spring.data.cosmos.core.convert;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.deser.std.NumberDeserializers;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
@@ -16,9 +18,13 @@ public class ObjectMapperFactory {
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     static {
+        SimpleModule module = new SimpleModule();
+        module.addDeserializer(Number.class, new NumberDeserializers.BigDecimalDeserializer() {});
+
         OBJECT_MAPPER.registerModule(new ParameterNamesModule())
                         .registerModule(new Jdk8Module())
-                        .registerModule(new JavaTimeModule());
+                        .registerModule(new JavaTimeModule())
+                        .registerModule(module);
         OBJECT_MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
 
