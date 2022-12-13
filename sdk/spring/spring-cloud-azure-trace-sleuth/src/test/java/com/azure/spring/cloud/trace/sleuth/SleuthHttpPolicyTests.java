962b93a64e4aa67eb1e8f2c8c9b00170f385ce63
Liudmila Molkova <limolkova@microsoft.com>
2022-12-13 13:46:29 -0800

    up

--- sdk/spring/spring-cloud-azure-trace-sleuth/src/test/java/com/azure/spring/cloud/trace/sleuth/SleuthHttpPolicyTests.java
+++ sdk/spring/spring-cloud-azure-trace-sleuth/src/test/java/com/azure/spring/cloud/trace/sleuth/SleuthHttpPolicyTests.java
@@ -34,6 +34,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
