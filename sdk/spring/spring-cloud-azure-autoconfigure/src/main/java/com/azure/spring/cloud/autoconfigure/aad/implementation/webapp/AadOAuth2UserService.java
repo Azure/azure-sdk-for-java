db713741b4fd3e1b2cafbb0bb6dd6d57cf54adc0
zhihaoguo <zhihaoguo@microsoft.com>
2022-12-14 15:08:32 +0800

    refactor

--- sdk/spring/spring-cloud-azure-autoconfigure/src/main/java/com/azure/spring/cloud/autoconfigure/aad/implementation/webapp/AadOAuth2UserService.java
+++ sdk/spring/spring-cloud-azure-autoconfigure/src/main/java/com/azure/spring/cloud/autoconfigure/aad/implementation/webapp/AadOAuth2UserService.java
@@ -16,10 +16,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
-import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.AbstractOAuth2Token;
 import org.springframework.security.oauth2.core.OAuth2AccessToken;
@@ -42,21 +38,17 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static com.azure.spring.cloud.autoconfigure.aad.implementation.AadRestTemplateCreator.createOAuth2ErrorResponseHandledRestTemplate;
 import static com.azure.spring.cloud.autoconfigure.aad.implementation.constants.Constants.DEFAULT_AUTHORITY_SET;
 
 /**
  * This implementation will retrieve group info of user from Microsoft Graph. Then map group to {@link
  * GrantedAuthority}.
  *
- * @see OidcUserService
  * @see OAuth2UserService
  */
 public class AadOAuth2UserService implements OAuth2UserService<OidcUserRequest, OidcUser> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AadOAuth2UserService.class);
-
-    private final OidcUserService oidcUserService;
     private final List<String> allowedGroupNames;
     private final Set<String> allowedGroupIds;
     private final GraphClient graphClient;
@@ -70,7 +62,7 @@ public class AadOAuth2UserService implements OAuth2UserService<OidcUserRequest, 
      * @param restTemplateBuilder the restTemplateBuilder
      */
     public AadOAuth2UserService(AadAuthenticationProperties properties, RestTemplateBuilder restTemplateBuilder) {
-        this(properties, new GraphClient(properties, restTemplateBuilder), restTemplateBuilder);
+        this(properties, new GraphClient(properties, restTemplateBuilder));
     }
 
     /**
@@ -78,51 +70,71 @@ public class AadOAuth2UserService implements OAuth2UserService<OidcUserRequest, 
      *
      * @param properties the AAD authentication properties
      * @param graphClient the graph client
-     * @param restTemplateBuilder the restTemplateBuilder
      */
     public AadOAuth2UserService(AadAuthenticationProperties properties,
                                 GraphClient graphClient,
                                 RestTemplateBuilder restTemplateBuilder) {
+        this(properties, graphClient);
+    }
+
+    private AadOAuth2UserService(AadAuthenticationProperties properties,
+                                GraphClient graphClient) {
         allowedGroupNames = Optional.ofNullable(properties)
-                                    .map(AadAuthenticationProperties::getUserGroup)
-                                    .map(AadAuthenticationProperties.UserGroupProperties::getAllowedGroupNames)
-                                    .orElseGet(Collections::emptyList);
+            .map(AadAuthenticationProperties::getUserGroup)
+            .map(AadAuthenticationProperties.UserGroupProperties::getAllowedGroupNames)
+            .orElseGet(Collections::emptyList);
         allowedGroupIds = Optional.ofNullable(properties)
-                                  .map(AadAuthenticationProperties::getUserGroup)
-                                  .map(AadAuthenticationProperties.UserGroupProperties::getAllowedGroupIds)
-                                  .orElseGet(Collections::emptySet);
-        DefaultOAuth2UserService oAuth2UserService = new DefaultOAuth2UserService();
-        oAuth2UserService.setRestOperations(createOAuth2ErrorResponseHandledRestTemplate(restTemplateBuilder));
-        this.oidcUserService = new OidcUserService();
-        this.oidcUserService.setOauth2UserService(oAuth2UserService);
+            .map(AadAuthenticationProperties::getUserGroup)
+            .map(AadAuthenticationProperties.UserGroupProperties::getAllowedGroupIds)
+            .orElseGet(Collections::emptySet);
         this.graphClient = graphClient;
     }
 
     /**
-     * Returns an {@link OAuth2User} after obtaining the user attributes of the End-User
-     * from the UserInfo Endpoint.
+     * Returns an {@link OAuth2User} instance.
      *
      * @param userRequest the user request
      * @return an {@link OAuth2User}
      * @throws OAuth2AuthenticationException if an error occurs while attempting to obtain
-     * the user attributes from the UserInfo Endpoint
+     *                                       the user attributes from the UserInfo Endpoint
      */
     @Override
     public OidcUser loadUser(OidcUserRequest userRequest) throws OAuth2AuthenticationException {
-        // Delegate to the default implementation for loading a user
-        OidcUser oidcUser = oidcUserService.loadUser(userRequest);
-        OidcIdToken idToken = oidcUser.getIdToken();
-        Set<String> authorityStrings = new HashSet<>();
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
         HttpSession session = attr.getRequest().getSession(true);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         if (authentication != null) {
             LOGGER.debug("User {}'s authorities saved from session: {}.", authentication.getName(), authentication.getAuthorities());
             return (DefaultOidcUser) session.getAttribute(DEFAULT_OIDC_USER);
         }
 
-        authorityStrings.addAll(extractRolesFromIdToken(idToken));
+        DefaultOidcUser defaultOidcUser = getUser(userRequest);
+        session.setAttribute(DEFAULT_OIDC_USER, defaultOidcUser);
+        return defaultOidcUser;
+    }
+
+    private DefaultOidcUser getUser(OidcUserRequest userRequest) {
+        Set<SimpleGrantedAuthority> authorities = buildAuthorities(userRequest);
+        String nameAttributeKey = getNameAttributeKey(userRequest);
+        OidcIdToken idToken = userRequest.getIdToken();
+        DefaultOidcUser defaultOidcUser = new DefaultOidcUser(authorities, idToken, nameAttributeKey);
+        return defaultOidcUser;
+    }
+
+    private String getNameAttributeKey(OidcUserRequest userRequest) {
+        return Optional.of(userRequest)
+                       .map(u -> u.getClientRegistration())
+                       .map(u -> u.getProviderDetails())
+                       .map(u -> u.getUserInfoEndpoint())
+                       .map(u -> u.getUserNameAttributeName())
+                       .filter(StringUtils::hasText)
+                       .orElse(AadJwtClaimNames.NAME);
+    }
+
+    private Set<SimpleGrantedAuthority> buildAuthorities(OidcUserRequest userRequest) {
+        Set<String> authorityStrings = new HashSet<>();
+        authorityStrings.addAll(extractRolesFromIdToken(userRequest.getIdToken()));
         authorityStrings.addAll(extractGroupRolesFromAccessToken(userRequest.getAccessToken()));
         Set<SimpleGrantedAuthority> authorities = authorityStrings.stream()
                                                                   .map(SimpleGrantedAuthority::new)
@@ -131,20 +143,7 @@ public class AadOAuth2UserService implements OAuth2UserService<OidcUserRequest, 
         if (authorities.isEmpty()) {
             authorities = DEFAULT_AUTHORITY_SET;
         }
-        String nameAttributeKey =
-            Optional.of(userRequest)
-                    .map(OAuth2UserRequest::getClientRegistration)
-                    .map(ClientRegistration::getProviderDetails)
-                    .map(ClientRegistration.ProviderDetails::getUserInfoEndpoint)
-                    .map(ClientRegistration.ProviderDetails.UserInfoEndpoint::getUserNameAttributeName)
-                    .filter(StringUtils::hasText)
-                    .orElse(AadJwtClaimNames.NAME);
-        LOGGER.debug("User {}'s authorities extracted by id token and access token: {}.", oidcUser.getClaim(nameAttributeKey), authorities);
-        // Create a copy of oidcUser but use the mappedAuthorities instead
-        DefaultOidcUser defaultOidcUser = new DefaultOidcUser(authorities, idToken, nameAttributeKey);
-
-        session.setAttribute(DEFAULT_OIDC_USER, defaultOidcUser);
-        return defaultOidcUser;
+        return authorities;
     }
 
     /**
