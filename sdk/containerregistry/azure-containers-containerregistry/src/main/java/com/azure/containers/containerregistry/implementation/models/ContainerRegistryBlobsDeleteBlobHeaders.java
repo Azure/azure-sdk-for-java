94e5e4ab4dd2035352c9e2e36cfa13c804795bec
Sameeksha Vaity <savaity@microsoft.com>
2022-12-14 10:36:03 -0800

    Enable sync stack for Container registry (#31584)

--- sdk/containerregistry/azure-containers-containerregistry/src/main/java/com/azure/containers/containerregistry/implementation/models/ContainerRegistryBlobsDeleteBlobHeaders.java
+++ sdk/containerregistry/azure-containers-containerregistry/src/main/java/com/azure/containers/containerregistry/implementation/models/ContainerRegistryBlobsDeleteBlobHeaders.java
@@ -5,6 +5,8 @@
 package com.azure.containers.containerregistry.implementation.models;
 
 import com.azure.core.annotation.Fluent;
+import com.azure.core.http.HttpHeaderName;
+import com.azure.core.http.HttpHeaders;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /** The ContainerRegistryBlobsDeleteBlobHeaders model. */
@@ -15,6 +17,18 @@ public final class ContainerRegistryBlobsDeleteBlobHeaders {
      */
     @JsonProperty(value = "Docker-Content-Digest")
     private String dockerContentDigest;
+
+    private static final HttpHeaderName DOCKER_CONTENT_DIGEST = HttpHeaderName.fromString("Docker-Content-Digest");
+
+    // HttpHeaders containing the raw property values.
+    /**
+     * Creates an instance of ContainerRegistryBlobsDeleteBlobHeaders class.
+     *
+     * @param rawHeaders The raw HttpHeaders that will be used to create the property values.
+     */
+    public ContainerRegistryBlobsDeleteBlobHeaders(HttpHeaders rawHeaders) {
+        this.dockerContentDigest = rawHeaders.getValue("Docker-Content-Digest");
+    }
 
     /**
      * Get the dockerContentDigest property: The Docker-Content-Digest property.
