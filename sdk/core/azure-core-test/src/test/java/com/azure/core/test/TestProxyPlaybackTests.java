72ce5bc1786d262f61a920e3646a239b6711885f
Bill Wert <billwert@microsoft.com>
2022-12-16 14:03:20 -0800

    Add instructions for making a new recording for playback tests.

--- sdk/core/azure-core-test/src/test/java/com/azure/core/test/TestProxyPlaybackTests.java
+++ sdk/core/azure-core-test/src/test/java/com/azure/core/test/TestProxyPlaybackTests.java
@@ -37,6 +37,47 @@ public class TestProxyPlaybackTests extends TestBase {
 
     }
 
+    /*
+     * To make a new recording for this test:
+     * 1) Uncomment this test and comment out the other testPlayback
+     * 2) Set AZURE_TEST_MODE to "RECORD" above
+     * 3) Check in the new recording from target\test-classes\session-records to src\test\resources\session-records\
+     */
+//    @Test
+//    public void testPlayback() {
+//
+//        HttpURLConnectionHttpClient client = new HttpURLConnectionHttpClient();
+//        HttpPipeline pipeline = new HttpPipelineBuilder()
+//            .httpClient(client)
+//            .policies(interceptorManager.getRecordPolicy()).build();
+//        URL url = null;
+//        try {
+//            url = new UrlBuilder().setHost("example.com").setScheme("https").toUrl();
+//        } catch (MalformedURLException e) {
+//            throw new RuntimeException(e);
+//        }
+//        testResourceNamer.now();
+//        testResourceNamer.randomName("playbacktest", 20);
+//        testResourceNamer.now();
+//        testResourceNamer.randomUuid();
+//        testResourceNamer.now();
+//        testResourceNamer.randomName("playbacktest", 20);
+//        testResourceNamer.now();
+//        testResourceNamer.randomUuid();
+//        testResourceNamer.now();
+//        testResourceNamer.randomName("playbacktest", 20);
+//        testResourceNamer.now();
+//        testResourceNamer.randomUuid();
+//        testResourceNamer.now();
+//        testResourceNamer.randomName("playbacktest", 20);
+//        testResourceNamer.now();
+//        testResourceNamer.randomUuid();
+//        HttpRequest request = new HttpRequest(HttpMethod.GET, url);
+//        HttpResponse response = pipeline.sendSync(request, Context.NONE);
+//
+//        assertEquals(response.getStatusCode(), 200);
+//    }
+
     @Test
     public void testPlayback() {
 
