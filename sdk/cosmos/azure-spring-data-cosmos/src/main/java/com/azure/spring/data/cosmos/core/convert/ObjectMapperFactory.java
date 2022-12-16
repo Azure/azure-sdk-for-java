8186db8bb1f7bf94e5e013edf166ce173888fd02
Trevor Anderson <trande@microsoft.com>
2022-12-16 10:33:37 -0500

    Fixing deserialization.

--- sdk/cosmos/azure-spring-data-cosmos/src/main/java/com/azure/spring/data/cosmos/core/convert/ObjectMapperFactory.java
+++ sdk/cosmos/azure-spring-data-cosmos/src/main/java/com/azure/spring/data/cosmos/core/convert/ObjectMapperFactory.java
@@ -19,8 +19,8 @@ public class ObjectMapperFactory {
     static {
         OBJECT_MAPPER.registerModule(new ParameterNamesModule())
                         .registerModule(new Jdk8Module())
-                        .registerModule(new JavaTimeModule());
-                        //.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+                        .registerModule(new JavaTimeModule())
+                        .enable(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS);
         OBJECT_MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
 
