6d470469ed66e0447ca0207878908420011767f7
Bill Wert <billwert@microsoft.com>
2022-12-16 14:04:18 -0800

    Make proxy playback return saved variables

--- sdk/core/azure-core-test/src/main/java/com/azure/core/test/http/TestProxyPlaybackClient.java
+++ sdk/core/azure-core-test/src/main/java/com/azure/core/test/http/TestProxyPlaybackClient.java
@@ -4,13 +4,18 @@ import com.azure.core.http.HttpClient;
 import com.azure.core.http.HttpMethod;
 import com.azure.core.http.HttpRequest;
 import com.azure.core.http.HttpResponse;
+import com.azure.core.test.InterceptorManager;
 import com.azure.core.test.utils.HttpURLConnectionHttpClient;
 import com.azure.core.test.utils.TestProxyUtils;
 import com.azure.core.util.Context;
+import com.azure.core.util.serializer.JacksonAdapter;
+import com.azure.core.util.serializer.SerializerAdapter;
+import com.azure.core.util.serializer.SerializerEncoding;
 import reactor.core.publisher.Mono;
 
-import java.util.Map;
-import java.util.Objects;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class TestProxyPlaybackClient implements HttpClient {
 
@@ -21,15 +26,22 @@ public class TestProxyPlaybackClient implements HttpClient {
 
     private final HttpURLConnectionHttpClient client = new HttpURLConnectionHttpClient();
     private String xRecordingId;
-    private String recordFile;
+    private static final SerializerAdapter SERIALIZER = new JacksonAdapter();
 
-    public void startPlayback(String recordFile, Map<String, String> textReplacementRules) {
+    public Queue<String> startPlayback(String recordFile, Map<String, String> textReplacementRules) {
         // TODO: replacement rules
         HttpRequest request = new HttpRequest(HttpMethod.POST, String.format("%s/playback/start", proxyUrl))
             .setBody(String.format("{\"x-recording-file\": \"%s\"}", recordFile));
-        HttpResponse response = client.sendSync(request, Context.NONE);
-        xRecordingId = response.getHeaderValue("x-recording-id");
+        try (HttpResponse response = client.sendSync(request, Context.NONE)) {
+            xRecordingId = response.getHeaderValue("x-recording-id");
+            String body = response.getBodyAsString().block();
+            return SERIALIZER.<Map<String, String>>deserialize(body, Map.class, SerializerEncoding.JSON).entrySet().stream().sorted(Comparator.comparingInt(e -> Integer.parseInt(e.getKey()))).map(Map.Entry::getValue).collect(Collectors.toCollection(LinkedList::new));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
+
 
 
     public void stopPlayback() {
--- sdk/core/azure-core-test/src/test/java/com/azure/core/test/TestProxyPlaybackTests.java
+++ sdk/core/azure-core-test/src/test/java/com/azure/core/test/TestProxyPlaybackTests.java
@@ -1,13 +1,10 @@
 package com.azure.core.test;
 
-import com.azure.core.http.HttpMethod;
-import com.azure.core.http.HttpPipeline;
-import com.azure.core.http.HttpPipelineBuilder;
-import com.azure.core.http.HttpRequest;
 import com.azure.core.http.HttpClient;
+import com.azure.core.http.HttpMethod;
+import com.azure.core.http.HttpRequest;
 import com.azure.core.http.HttpResponse;
 import com.azure.core.test.implementation.TestingHelpers;
-import com.azure.core.test.utils.HttpURLConnectionHttpClient;
 import com.azure.core.util.Configuration;
 import com.azure.core.util.Context;
 import com.azure.core.util.UrlBuilder;
@@ -17,6 +14,7 @@ import org.junit.jupiter.api.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.OffsetDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -83,15 +81,32 @@ public class TestProxyPlaybackTests extends TestBase {
 
         HttpClient client = interceptorManager.getPlaybackClient();
 
+
         URL url = null;
         try {
             url = new UrlBuilder().setHost("example.com").setScheme("https").toUrl();
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
+
         HttpRequest request = new HttpRequest(HttpMethod.GET, url);
         HttpResponse response = client.sendSync(request, Context.NONE);
-
+        assertEquals(OffsetDateTime.parse("2022-12-16T21:42:13.806425800Z"), testResourceNamer.now());
+        assertEquals("playbacktest69112c", testResourceNamer.randomName("playbacktest", 20));
+        assertEquals(OffsetDateTime.parse("2022-12-16T21:42:13.807425500Z"), testResourceNamer.now());
+        assertEquals("a56e3c54-7325-43ad-a3cc-3314695de323", testResourceNamer.randomUuid());
+        assertEquals(OffsetDateTime.parse("2022-12-16T21:42:13.807425500Z"), testResourceNamer.now());
+        assertEquals("playbacktest828103", testResourceNamer.randomName("playbacktest", 20));
+        assertEquals(OffsetDateTime.parse("2022-12-16T21:42:13.807425500Z"), testResourceNamer.now());
+        assertEquals("095a6bad-1c34-4fdc-8174-b61a5b95c217", testResourceNamer.randomUuid());
+        assertEquals(OffsetDateTime.parse("2022-12-16T21:42:13.807425500Z"), testResourceNamer.now());
+        assertEquals("playbacktest633220", testResourceNamer.randomName("playbacktest", 20));
+        assertEquals(OffsetDateTime.parse("2022-12-16T21:42:13.808425Z"), testResourceNamer.now());
+        assertEquals("4607aedc-e21c-4130-b947-7c5e9be18280", testResourceNamer.randomUuid());
+        assertEquals(OffsetDateTime.parse("2022-12-16T21:42:13.808425Z"), testResourceNamer.now());
+        assertEquals("playbacktest595370", testResourceNamer.randomName("playbacktest", 20));
+        assertEquals(OffsetDateTime.parse("2022-12-16T21:42:13.808425Z"), testResourceNamer.now());
+        assertEquals("a0ba8bd8-ed7c-4c86-83b6-b4bd455da01e", testResourceNamer.randomUuid());
         assertEquals(response.getStatusCode(), 200);
     }
 }
