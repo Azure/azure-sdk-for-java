5fe90e73bfd59124dbdca9670234618fdff0c06d
Haoling Dong <haolingdong@microsoft.com>
2022-12-19 17:43:55 +0800

    add sample on filters

---
+++ sdk/webpubsub/azure-messaging-webpubsub/src/samples/java/com/azure/messaging/webpubsub/BroadcastingWithFilterSample.java
@@ -0,0 +1,31 @@
+package com.azure.messaging.webpubsub;
+
+import com.azure.core.http.rest.RequestOptions;
+import com.azure.core.util.BinaryData;
+import com.azure.core.util.Configuration;
+import com.azure.messaging.webpubsub.models.GetClientAccessTokenOptions;
+import com.azure.messaging.webpubsub.models.WebPubSubClientAccessToken;
+
+public class BroadcastingWithFilterSample {
+    private static final String CONNECTION_STRING = Configuration.getGlobalConfiguration().get("WEB_PUB_SUB_CS");
+
+    public static void main(String[] args) {
+        // build a sync client
+        WebPubSubServiceClient chatHub = new WebPubSubServiceClientBuilder()
+            .connectionString(CONNECTION_STRING)
+            .hub("chat")
+            .buildClient();
+
+        // send a text message to the entire hub with a filter on userId
+        chatHub.sendToAllWithResponse(
+            BinaryData.fromString("Hello World - Broadcast test!"),
+            new RequestOptions().setHeader("Content-Type", "text/plain")
+                .addQueryParam("filter", "userId ne 'user1'"));
+
+        // send a text message to the entire hub with another filter on group
+        chatHub.sendToAllWithResponse(
+            BinaryData.fromString("Hello World - Broadcast test!"),
+            new RequestOptions().setHeader("Content-Type", "text/plain")
+                .addQueryParam("filter", "'GroupA' in groups and not({'GroupB'} in groups)"));
+    }
+}
