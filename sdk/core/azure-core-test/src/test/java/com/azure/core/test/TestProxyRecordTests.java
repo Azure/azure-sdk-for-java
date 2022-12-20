9b07e0aec7aaa19b2a7405f0e57b0e93c5f86bf3
Bill Wert <billwert@microsoft.com>
2022-12-19 18:32:16 -0800

    changes to tests for all underlying changes.

--- sdk/core/azure-core-test/src/test/java/com/azure/core/test/TestProxyRecordTests.java
+++ sdk/core/azure-core-test/src/test/java/com/azure/core/test/TestProxyRecordTests.java
@@ -5,15 +5,14 @@ import com.azure.core.http.HttpPipeline;
 import com.azure.core.http.HttpPipelineBuilder;
 import com.azure.core.http.HttpRequest;
 import com.azure.core.http.HttpResponse;
+import com.azure.core.test.http.TestProxyTestServer;
 import com.azure.core.test.implementation.TestingHelpers;
 import com.azure.core.test.utils.HttpURLConnectionHttpClient;
 import com.azure.core.util.Configuration;
 import com.azure.core.util.Context;
 import com.azure.core.util.UrlBuilder;
-import org.junit.jupiter.api.AfterAll;
-
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 
 
 import java.net.MalformedURLException;
@@ -24,39 +23,81 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SuppressWarnings("deprecation")
 public class TestProxyRecordTests extends TestBase {
-
+    static TestProxyTestServer server;
     @BeforeAll
     public static void setupClass() {
-        Configuration.getGlobalConfiguration().put(TestingHelpers.AZURE_TEST_MODE, "RECORD");
-        Configuration.getGlobalConfiguration().put(TestingHelpers.AZURE_TEST_USE_PROXY, "TRUE");
+        enableTestProxy();
+        server = new TestProxyTestServer();
         TestBase.setupClass();
     }
 
     @AfterAll
     public static void teardownClass() {
         TestBase.teardownClass();
-        Configuration.getGlobalConfiguration().remove(TestingHelpers.AZURE_TEST_MODE);
-        Configuration.getGlobalConfiguration().remove(TestingHelpers.AZURE_TEST_USE_PROXY);
-
+        server.close();
     }
 
 
     @Test
-    public void testRecord() {
-
+    @Tag("Record")
+    public void testBasicRecord() {
         HttpURLConnectionHttpClient client = new HttpURLConnectionHttpClient();
         HttpPipeline pipeline = new HttpPipelineBuilder()
             .httpClient(client)
             .policies(interceptorManager.getRecordPolicy()).build();
         URL url = null;
         try {
-            url = new UrlBuilder().setHost("example.com").setScheme("https").toUrl();
+            url = new UrlBuilder().setHost("localhost").setPort(3000).setScheme("http").toUrl();
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
         testResourceNamer.randomName("test", 10);
         testResourceNamer.now();
         HttpRequest request = new HttpRequest(HttpMethod.GET, url);
+        HttpResponse response = pipeline.sendSync(request, Context.NONE);
+
+        assertEquals(response.getStatusCode(), 200);
+    }
+
+    @Test
+    @Tag("Record")
+    public void testRecordWithPath() {
+        HttpURLConnectionHttpClient client = new HttpURLConnectionHttpClient();
+        HttpPipeline pipeline = new HttpPipelineBuilder()
+            .httpClient(client)
+            .policies(interceptorManager.getRecordPolicy()).build();
+        URL url = null;
+        try {
+            url = new UrlBuilder().setHost("localhost").setPort(3000).setPath("first/path").setScheme("http").toUrl();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+        testResourceNamer.randomName("test", 10);
+        testResourceNamer.now();
+        HttpRequest request = new HttpRequest(HttpMethod.GET, url);
+        HttpResponse response = pipeline.sendSync(request, Context.NONE);
+
+        assertEquals(response.getStatusCode(), 200);
+    }
+
+    @Test
+    @Tag("Record")
+    public void testRecordWithHeaders() {
+        HttpURLConnectionHttpClient client = new HttpURLConnectionHttpClient();
+        HttpPipeline pipeline = new HttpPipelineBuilder()
+            .httpClient(client)
+            .policies(interceptorManager.getRecordPolicy()).build();
+        URL url = null;
+        try {
+            url = new UrlBuilder().setHost("localhost").setPort(3000).setPath("echoheaders").setScheme("http").toUrl();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+        testResourceNamer.randomName("test", 10);
+        testResourceNamer.now();
+        HttpRequest request = new HttpRequest(HttpMethod.GET, url);
+        request.setHeader("header1", "value1");
+        request.setHeader("header2", "value2");
         HttpResponse response = pipeline.sendSync(request, Context.NONE);
 
         assertEquals(response.getStatusCode(), 200);
