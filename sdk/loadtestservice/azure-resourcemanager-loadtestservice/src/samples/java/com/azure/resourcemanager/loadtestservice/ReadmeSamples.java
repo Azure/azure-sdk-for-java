464949a7c3c9bf5dfd33098e29bb6aa8e4490848
Venkat Ragavan <50309787+venkatr21@users.noreply.github.com>
2022-12-21 11:29:29 +0530

    added copyright and license snip

--- sdk/loadtestservice/azure-resourcemanager-loadtestservice/src/samples/java/com/azure/resourcemanager/loadtestservice/ReadmeSamples.java
+++ sdk/loadtestservice/azure-resourcemanager-loadtestservice/src/samples/java/com/azure/resourcemanager/loadtestservice/ReadmeSamples.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.azure.resourcemanager.loadtestservice;
 
 import com.azure.resourcemanager.loadtestservice.models.LoadTestResource;
