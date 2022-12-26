d7c139f0b4093725edbd9f9fcc22f749a0101820
Katherine <96520421+khmic5@users.noreply.github.com>
2022-12-26 14:22:59 +0800

    Azure Maps Weather Update Samples (#32674)
    * Update Azure Maps Weather Samples

--- sdk/maps/azure-maps-weather/src/samples/java/com/azure/maps/weather/samples/GetDailyIndices.java
+++ sdk/maps/azure-maps-weather/src/samples/java/com/azure/maps/weather/samples/GetDailyIndices.java
@@ -3,9 +3,8 @@
 
 package com.azure.maps.weather.samples;
 
+import com.azure.core.credential.AzureKeyCredential;
 import com.azure.core.models.GeoPosition;
-import com.azure.identity.DefaultAzureCredential;
-import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.azure.maps.weather.WeatherAsyncClient;
 import com.azure.maps.weather.WeatherClient;
 import com.azure.maps.weather.WeatherClientBuilder;
@@ -13,47 +12,49 @@ import com.azure.maps.weather.WeatherClientBuilder;
 public class GetDailyIndices {
     public static void main(String[] args) {
         // Authenticates using subscription key
-        // AzureKeyCredential keyCredential = new AzureKeyCredential(System.getenv("SUBSCRIPTION_KEY"));
+        AzureKeyCredential keyCredential = new AzureKeyCredential(System.getenv("SUBSCRIPTION_KEY"));
         // builder.credential(keyCredential);
 
         // Authenticates using Azure AD building a default credential
         // This will look for AZURE_CLIENT_ID, AZURE_TENANT_ID, and AZURE_CLIENT_SECRET env variables
-        DefaultAzureCredential tokenCredential = new DefaultAzureCredentialBuilder().build();
+        // DefaultAzureCredential tokenCredential = new DefaultAzureCredentialBuilder().build();
 
-        WeatherClient client = new WeatherClientBuilder() 
-            .credential(tokenCredential)
+        WeatherClient client = new WeatherClientBuilder()
+            .credential(keyCredential)
             .weatherClientId(System.getenv("MAPS_CLIENT_ID"))
             .buildClient();
 
         // Get Daily Indices -
         // https://docs.microsoft.com/en-us/rest/api/maps/weather/get-daily-indices
-        // Azure Maps Indices API returns index values that will guide end users to plan future activities. 
-        // For example, a health mobile application can notify users that today is good weather for 
-        // running or for other outdoors activities like for playing golf, 
+        // Azure Maps Indices API returns index values that will guide end users to plan future activities.
+        // For example, a health mobile application can notify users that today is good weather for
+        // running or for other outdoors activities like for playing golf,
         // and retail stores can optimize their digital marketing campaigns based on predicted index values.
+        System.out.println("Get Daily Indices Sync Client");
         // BEGIN: com.azure.maps.weather.sync.get_daily_indices
         client.getDailyIndices(new GeoPosition(-79.37849, 43.84745), null, null, null, 11);
         // END: com.azure.maps.weather.sync.get_daily_indices
 
         // Authenticates using subscription key
-        // AzureKeyCredential keyCredential = new AzureKeyCredential(System.getenv("SUBSCRIPTION_KEY"));
+        AzureKeyCredential asyncClientKeyCredential = new AzureKeyCredential(System.getenv("SUBSCRIPTION_KEY"));
         // builder.credential(keyCredential);
 
         // Authenticates using Azure AD building a default credential
         // This will look for AZURE_CLIENT_ID, AZURE_TENANT_ID, and AZURE_CLIENT_SECRET env variables
-        DefaultAzureCredential asyncClientTokenCredential = new DefaultAzureCredentialBuilder().build();
+        // DefaultAzureCredential asyncClientTokenCredential = new DefaultAzureCredentialBuilder().build();
 
         WeatherAsyncClient asyncClient = new WeatherClientBuilder()
-            .credential(asyncClientTokenCredential)
+            .credential(asyncClientKeyCredential)
             .weatherClientId(System.getenv("MAPS_CLIENT_ID"))
             .buildAsyncClient();
 
         // Get Daily Indices -
         // https://docs.microsoft.com/en-us/rest/api/maps/weather/get-daily-indices
-        // Azure Maps Indices API returns index values that will guide end users to plan future activities. 
-        // For example, a health mobile application can notify users that today is good weather for 
-        // running or for other outdoors activities like for playing golf, 
+        // Azure Maps Indices API returns index values that will guide end users to plan future activities.
+        // For example, a health mobile application can notify users that today is good weather for
+        // running or for other outdoors activities like for playing golf,
         // and retail stores can optimize their digital marketing campaigns based on predicted index values.
+        System.out.println("Get Daily Indices Async Client");
         // BEGIN: com.azure.maps.weather.async.get_daily_indices
         asyncClient.getDailyIndices(new GeoPosition(-79.37849, 43.84745), null, null, null, 11);
         // END: com.azure.maps.weather.async.get_daily_indices
