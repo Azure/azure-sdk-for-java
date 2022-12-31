6dd76d4b8dbc4d0caf52fe41a94e8f460a2f06c1
Victor Colin Amador <vicolina@microsoft.com>
2022-12-31 00:44:12 -0800

    Changed `Generate-Patches-For-Automatic-Releases.ps1` to internally call `Update-Artifacts-List-For-Patch-Release.ps1`, update library dependencies and READMEs via version_client.txt and push changes to the patches branch.

--- eng/scripts/Generate-Patches-For-Automatic-Releases.ps1
+++ eng/scripts/Generate-Patches-For-Automatic-Releases.ps1
@@ -5,7 +5,7 @@ param(
     # $(Build.SourcesDirectory) - root of the repository
     [Parameter(Mandatory = $true)][string]$SourcesDirectory,
     # The yml file whose artifacts and additionalModules lists will be updated
-    [Parameter(Mandatory = $true)][string]$YmlToRead
+    [Parameter(Mandatory = $true)][string]$PackagesYmlPath
 )
 
 $StartTime = $( get-date )
@@ -13,29 +13,46 @@ $StartTime = $( get-date )
 . "${PSScriptRoot}/../common/scripts/Helpers/PSModule-Helpers.ps1"
 . "${PSScriptRoot}/bomhelpers.ps1"
 
-# Verify that the SourcesDirectory exists and is a directory
-if (!(Test-Path $SourcesDirectory -PathType Container))
-{
-    LogError("$SourcesDirectory is either not a directory or does not exist.")
-    exit 1
-}
+# Generate the list of artifacts to update for a patch release.
+. "${PSScriptRoot}/Update-Artifacts-List-For-Patch-Release.ps1" -SourcesDirectory $SourcesDirectory -YmlToUpdate $PackagesYmlPath
 
-Install-ModuleIfNotInstalled "powershell-yaml" "0.4.1" | Import-Module
-
-$ymlContent = Get-Content $YmlToRead -Raw
+# Read the package info from the generated YAML file
+$ymlContent = Get-Content $PackagesYmlPath -Raw
 $ymlObject = ConvertFrom-Yaml $ymlContent -Ordered
 $packagesData = $ymlObject["extends"]["parameters"]["artifacts"]
-$branchName = GetBranchName -ArtifactId "patch-for-auto-release"
 $libraryList = $null
+$remoteName = GetRemoteName
+$branchName = GetBranchName -ArtifactId "patches-for-auto-release"
+$currentBranchName = GetCurrentBranchName
 
 # Reset each package to the latest stable release and update CHANGELOG, POM and README for patch release.
-foreach ($packageData in $packagesData)
-{
-    . "${PSScriptRoot}/generatepatch.ps1" -ArtifactIds $packageData["name"] -ServiceDirectoryName $packageData["ServiceDirectory"] -BranchName $branchName
+foreach ($packageData in $packagesData) {
+    . "${PSScriptRoot}/generatepatch.ps1" -ArtifactIds $packageData["name"] -ServiceDirectoryName $packageData["ServiceDirectory"] -BranchName $branchName -PushToRemote $True
     $libraryList += $packageData["groupId"] + ":" + $packageData["name"] + ","
 }
 
 $libraryList = $libraryList.Substring(0, $libraryList.Length - 1)
 
-# Update POMs for all libraries with dependencies on the libraries to patch. Also, update the READMEs of the latter.
-python "${PSScriptRoot}/../versioning/update_versions.py" --update-type library --build-type client --ll $libraryList
+try {
+    # Update POMs for all libraries with dependencies on the libraries to patch. Also, update the READMEs of the latter.
+    python "${PSScriptRoot}/../versioning/update_versions.py" --update-type library --build-type client --ll $libraryList
+
+    Write-Host "git checkout $remoteName/$branchName"
+    git checkout $remoteName/$branchName
+
+    $userName = "azure-sdk"
+    $userEmail = "azuresdk@microsoft.com"
+    $commitMessage = "Updated dependencies in libraries and READMEs via version_client.txt"
+
+    Write-Host "git -c user.name=$userName -c user.email=$userEmail commit -m $commitMessage"
+    git -c user.name = $userName -c user.email = $userEmail commit -m $commitMessage
+
+    Write-Host "git -c user.name=$userName -c user.email=$userEmail push $remoteName $branchName"
+    git -c user.name = $userName -c user.email = $userEmail push $remoteName $branchName
+} catch {
+    LogError "Failed to update dependencies in libraries and READMEs via version_client.txt"
+    exit 1
+} finally {
+    Write-Host "git checkout $currentBranchName"
+    git checkout $currentBranchName
+}
