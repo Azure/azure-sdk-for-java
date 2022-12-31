5fb7e9d914cbd593cc04851c931dd60e0a4cc4a8
Victor Colin Amador <vicolina@microsoft.com>
2022-12-30 19:11:57 -0800

    Added script to generate patches for libraries that will be automatically released based on the contents of `eng/pipelines/patch-release.yml`.

---
+++ eng/scripts/Generate-Patches-For-Automatic-Releases.ps1
@@ -0,0 +1,34 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+param(
+    # $(Build.SourcesDirectory) - root of the repository
+    [Parameter(Mandatory = $true)][string]$SourcesDirectory,
+    # The yml file whose artifacts and additionalModules lists will be updated
+    [Parameter(Mandatory = $true)][string]$YmlToRead
+)
+
+$StartTime = $( get-date )
+. "${PSScriptRoot}/../common/scripts/common.ps1"
+. "${PSScriptRoot}/../common/scripts/Helpers/PSModule-Helpers.ps1"
+. "${PSScriptRoot}/bomhelpers.ps1"
+
+# Verify that the SourcesDirectory exists and is a directory
+if (!(Test-Path $SourcesDirectory -PathType Container))
+{
+    LogError("$SourcesDirectory is either not a directory or does not exist.")
+    exit 1
+}
+
+Install-ModuleIfNotInstalled "powershell-yaml" "0.4.1" | Import-Module
+
+$ymlContent = Get-Content $YmlToRead -Raw
+$ymlObject = ConvertFrom-Yaml $ymlContent -Ordered
+$packagesData = $ymlObject["extends"]["parameters"]["artifacts"]
+$branchName = GetBranchName -ArtifactId "patch-for-auto-release"
+
+# Reset each package to the latest stable release and update CHANGELOG, POM and README for patch release.
+foreach ($packageData in $packagesData)
+{
+    . "${PSScriptRoot}/generatepatch.ps1" -ArtifactIds $packageData["name"] -ServiceDirectoryName $packageData["ServiceDirectory"] -BranchName $branchName
+}
