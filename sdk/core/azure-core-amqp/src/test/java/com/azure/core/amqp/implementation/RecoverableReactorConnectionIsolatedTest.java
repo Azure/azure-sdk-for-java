34c1701813a9b571e640f52caf5121a031affb30
Anu Thomas <anuamd@hotmail.com>
2023-01-04 13:11:06 -0800

    Adding a test to assert retry-attempt resets for each retry-cycle for a new connection

--- sdk/core/azure-core-amqp/src/test/java/com/azure/core/amqp/implementation/RecoverableReactorConnectionIsolatedTest.java
+++ sdk/core/azure-core-amqp/src/test/java/com/azure/core/amqp/implementation/RecoverableReactorConnectionIsolatedTest.java
@@ -26,9 +26,12 @@ import reactor.test.scheduler.VirtualTimeScheduler;
 
 import java.time.Duration;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Deque;
 import java.util.HashMap;
+import java.util.List;
 import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Supplier;
 
 @Execution(ExecutionMode.SAME_THREAD)
@@ -196,6 +199,111 @@ public class RecoverableReactorConnectionIsolatedTest {
                 // assert that all 8 connections were supplied though the max-retry was 4.
                 connectionSupplier.assertInvocationCount(connectionsCount);
             }
+        } finally {
+            connectionSupplier.dispose();
+            recoverableConnection.terminate();
+        }
+    }
+
+    @Test
+    @Execution(ExecutionMode.SAME_THREAD)
+    public void shouldResetRetryAttemptsBeforeRefreshingCache() {
+        // Note: retry-cycle is defined as a series of retry-attempts until it obtains an active connection.
+
+        final int connectionsCountSet1 = 7;
+        final int connectionsCountSet2 = 6;
+        final Deque<ConnectionState> connectionStates = new ArrayDeque<>();
+        // First set of 6 connections with retriable states and one connection with active state.
+        // All must be consumed by the retry-cycle associated with the first request (subscription)
+        // for a connection (i.e. the first time cache is updated).
+        for (int i = 0; i < connectionsCountSet1 - 1; i++) {
+            connectionStates.add(ConnectionState.error(new AmqpException(true, "retriable" + i, null)));
+        }
+        connectionStates.add(ConnectionState.as(EndpointState.ACTIVE));
+
+        // Then, the Second set of 5 connections with a retriable state and one connection with active state.
+        // All must be consumed by the retry-cycle associated with the second request (subscription)
+        // for a connection. (i.e. the second time cache is updated).
+        for (int i = 0; i < connectionsCountSet2 - 1; i++) {
+            connectionStates.add(ConnectionState.error(new AmqpException(true, "retriable" + i, null)));
+        }
+        connectionStates.add(ConnectionState.as(EndpointState.ACTIVE));
+
+        final AmqpRetryOptions retryOptions = new AmqpRetryOptions()
+            .setTryTimeout(OPERATION_TIMEOUT)
+            .setMaxRetries(4);
+
+        // A RetryPolicy to capture "Retry Attempts" (first parameter to 'calculateRetryDelay') to assert.
+        final class TestRetryPolicy extends FixedAmqpRetryPolicy {
+            private final List<Integer> allRetryAttempts = new ArrayList<>();
+            protected TestRetryPolicy(AmqpRetryOptions retryOptions) {
+                super(retryOptions);
+            }
+
+            @Override
+            protected Duration calculateRetryDelay(int retryAttempts, Duration bDelay, Duration bJitter, ThreadLocalRandom rnd) {
+                allRetryAttempts.add(retryAttempts);
+                return super.calculateRetryDelay(retryAttempts, bDelay, bJitter, rnd);
+            }
+
+            List<Integer> getAllRetryAttempts() {
+                return new ArrayList<>(allRetryAttempts);
+            }
+        }
+
+        final TestRetryPolicy retryPolicy = new TestRetryPolicy(retryOptions);
+        final ConnectionSupplier connectionSupplier = new ConnectionSupplier(connectionStates, retryOptions);
+        final RecoverableReactorConnection recoverableConnection = new RecoverableReactorConnection(connectionSupplier,
+            FQDN, ENTITY_PATH, retryPolicy, errorContext, new HashMap<>());
+        try {
+            // The first connection request where the internal retry-cycle inspects the state of all connections
+            // from set1 and emits the 'active' one (i.e., the last one in set1).
+            try (VirtualTimeStepVerifier verifier = new VirtualTimeStepVerifier()) {
+                verifier.create(() -> recoverableConnection.getConnection())
+                    .thenRequest(1)
+                    .thenAwait(VIRTUAL_TIME_SHIFT)
+                    .expectNextMatches(con -> {
+                        Assertions.assertFalse(con.isDisposed());
+                        return true;
+                    })
+                    .verifyComplete();
+            }
+
+            connectionSupplier.assertInvocationCount(connectionsCountSet1);
+            List<Integer> attempts = retryPolicy.getAllRetryAttempts();
+            Assertions.assertEquals(connectionsCountSet1 - 1, attempts.size());
+            // The retry-cycle associated with the first connection request beings with retry-attempt 0
+            Assertions.assertEquals(0, attempts.get(0));
+            // the next retry-attempt in the retry-cycle will be 1 and so on...
+            Assertions.assertEquals(1, attempts.get(1));
+            // the last retry-attempt in the retry-cycle will be 4 since min(attempt, retryOptions.getMaxRetry()) == 4.
+            Assertions.assertEquals(4, attempts.get(connectionsCountSet1 - 2));
+
+            // Close the cached connection by completing connection endpoint,
+            // so the next (second) connection request refreshes cache.
+            connectionSupplier.completeEndpointState();
+
+            // The second connection request where the internal retry-cycle inspects the state of all connections
+            // from set2 and emits the 'active' one (i.e., the last one in set2).
+            try (VirtualTimeStepVerifier verifier = new VirtualTimeStepVerifier()) {
+                verifier.create(() -> recoverableConnection.getConnection())
+                    .thenRequest(1)
+                    .thenAwait(VIRTUAL_TIME_SHIFT)
+                    .expectNextMatches(con -> {
+                        Assertions.assertFalse(con.isDisposed());
+                        return true;
+                    })
+                    .verifyComplete();
+            }
+
+            connectionSupplier.assertInvocationCount(connectionsCountSet1 + connectionsCountSet2);
+            attempts = retryPolicy.getAllRetryAttempts();
+            Assertions.assertEquals(connectionsCountSet1 + connectionsCountSet2 - 2, attempts.size());
+            // Assert that the retry-cycle associated with the second connection request had retry-attempt "reset" to 0.
+            Assertions.assertEquals(0, attempts.get(connectionsCountSet1 - 1));
+            // the next retry-attempt in the retry-cycle will be 1 and so on...
+            Assertions.assertEquals(1, attempts.get(connectionsCountSet1 - 1 + 1));
+
         } finally {
             connectionSupplier.dispose();
             recoverableConnection.terminate();
