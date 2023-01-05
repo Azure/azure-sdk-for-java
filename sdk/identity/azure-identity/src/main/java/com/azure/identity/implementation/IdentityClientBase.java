efa103d855aab83c0489d49007bd2f3f29e81d0a
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2023-01-05 12:51:41 -0500

    Remove AtomicReference

--- sdk/identity/azure-identity/src/main/java/com/azure/identity/implementation/IdentityClientBase.java
+++ sdk/identity/azure-identity/src/main/java/com/azure/identity/implementation/IdentityClientBase.java
@@ -72,7 +72,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 import java.util.regex.Pattern;
@@ -85,7 +84,6 @@ public abstract class IdentityClientBase {
     static final String LINUX_MAC_SWITCHER = "-c";
     static final String WINDOWS_PROCESS_ERROR_MESSAGE = "'az' is not recognized";
     static final Pattern LINUX_MAC_PROCESS_ERROR_MESSAGE = Pattern.compile("(.*)az:(.*)not found");
-    static final AtomicReference<String> DEFAULT_WINDOWS_SYSTEM_ROOT = new AtomicReference<>();
     static final String DEFAULT_WINDOWS_PS_EXECUTABLE = "pwsh.exe";
     static final String LEGACY_WINDOWS_PS_EXECUTABLE = "powershell.exe";
     static final String DEFAULT_LINUX_PS_EXECUTABLE = "pwsh";
@@ -502,14 +500,7 @@ public abstract class IdentityClientBase {
 
     String getSafeWorkingDirectory() {
         if (isWindowsPlatform()) {
-            String windowsSystemRoot = DEFAULT_WINDOWS_SYSTEM_ROOT.updateAndGet(current -> {
-                if (current == null) {
-                    return System.getenv().getOrDefault("SystemRoot", "");
-                }
-
-                return current;
-            });
-
+            String windowsSystemRoot = System.getenv("SystemRoot");
             if (CoreUtils.isNullOrEmpty(windowsSystemRoot)) {
                 return null;
             }
