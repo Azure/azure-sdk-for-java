6ebfc12b5a529f7d2a6b81f23d7ab2671a4b99db
Jair <67484440+jairmyree@users.noreply.github.com>
2023-01-10 15:01:37 -0800

    Removed unused import

--- sdk/search/azure-search-documents/src/test/java/com/azure/search/documents/TestHelpers.java
+++ sdk/search/azure-search-documents/src/test/java/com/azure/search/documents/TestHelpers.java
@@ -35,7 +35,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
