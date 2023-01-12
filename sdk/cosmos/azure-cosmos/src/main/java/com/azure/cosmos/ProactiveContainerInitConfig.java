39e9a72026dfd84a7e4e6a17a6294891232da119
Abhijeet Mohanty <mabhijeet1995@gmail.com>
2023-01-12 12:23:05 -0500

    Updated documentation.

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/ProactiveContainerInitConfig.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/ProactiveContainerInitConfig.java
@@ -36,26 +36,25 @@ public final class ProactiveContainerInitConfig {
      *     opened synchronously when the {@link CosmosClient}/{@link CosmosAsyncClient} is built.
      * </p>
      * <p>
-     *     These proactive connection regions are a subset of the preferred regions configured through the {@link CosmosClientBuilder}.
+     *     These proactive connection regions are a subset of the preferred regions configured through the {@link CosmosClientBuilder}. The first
+     *     {@link ProactiveContainerInitConfig#getNumProactiveConnectionRegions()} read regions from preferred regions are picked. In this context a write-region could also be a read-region but not vice-versa.
      * </p>
      * <p>
-     *     Consider a multi-master account with client configured with preferred regions - "US West" and "US East"
+     *     Consider a multi-master account with client configured with preferred regions - "US West" (write-region) and "US East" (write-region)
      *     <ul>
      *         <li>
      *              If the no. of proactive regions is set to two, connections to "US West" and "US East" are opened proactively.
      *         </li>
      *         <li>
-     *             If the no. of proactive regions is set to one, connections to "US West" is opened proactively.
+     *             If the no. of proactive regions is set to one, connections to "US West" are opened proactively.
      *         </li>
      *     </ul>
-     *     Consider a single-master account with client configured with preferred regions - "US West" and "US East"
+     *     Consider a single-master account with client configured with preferred regions - "US West" (read-region), "US East" (read-region) and
+     *     "West Europe" (write-region)
      *     <ul>
      *         <li>
-     *              If the no. of proactive regions is set to two, connections to "US West" and "US East" are opened proactively.
-     *         </li>
-     *         <li>
-     *             If the no. of proactive regions is set to one, connections to "US West" and "US East" are opened proactively
-     *             considering some customers may want to open connections to the only write region configured for them.
+     *              If the no. of proactive regions is set to two, connections to "US West" and "US East" are opened proactively. If your application
+     *              has workloads which are write-heavy it is important to prioritize write regions in the list of preferred regions.
      *         </li>
      *     </ul>
      * </p>
