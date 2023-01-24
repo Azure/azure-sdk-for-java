eab01519848d9f87f0158ca75b8d825748044bcf
Jair <67484440+jairmyree@users.noreply.github.com>
2023-01-24 10:23:57 -0800

    Removed unused import

--- sdk/tables/azure-data-tables/src/main/java/com/azure/data/tables/TableServiceClient.java
+++ sdk/tables/azure-data-tables/src/main/java/com/azure/data/tables/TableServiceClient.java
@@ -14,7 +14,6 @@ import com.azure.core.http.rest.PagedIterable;
 import com.azure.core.http.rest.PagedResponse;
 import com.azure.core.http.rest.Response;
 import com.azure.core.http.rest.ResponseBase;
-import com.azure.core.http.rest.RestProxy;
 import com.azure.core.http.rest.SimpleResponse;
 import com.azure.core.util.Context;
 import com.azure.core.util.IterableStream;
