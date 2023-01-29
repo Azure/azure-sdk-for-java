ec270f018bddcd2e0025711b6279d91c141e2012
Weidong Xu <weidxu@microsoft.com>
2023-01-29 14:54:37 +0800

    package-info

---
+++ sdk/webpubsub/azure-messaging-webpubsub-client/src/main/java/com/azure/messaging/webpubsub/client/protocol/package-info.java
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+/**
+ * Package containing protocols for WebPubSubClient.
+ */
+package com.azure.messaging.webpubsub.client.protocol;
