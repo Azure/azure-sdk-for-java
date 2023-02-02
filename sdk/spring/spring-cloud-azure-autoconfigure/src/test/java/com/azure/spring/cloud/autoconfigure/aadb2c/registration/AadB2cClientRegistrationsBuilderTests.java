bb2dbc77b004bf67536c977189e849044515d2b4
Shili Chen <v-shilichen@microsoft.com>
2023-02-02 11:27:57 +0800

    Fix code smells

--- sdk/spring/spring-cloud-azure-autoconfigure/src/test/java/com/azure/spring/cloud/autoconfigure/aadb2c/registration/AadB2cClientRegistrationsBuilderTests.java
+++ sdk/spring/spring-cloud-azure-autoconfigure/src/test/java/com/azure/spring/cloud/autoconfigure/aadb2c/registration/AadB2cClientRegistrationsBuilderTests.java
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 package com.azure.spring.cloud.autoconfigure.aadb2c.registration;
 
 public class AadB2cClientRegistrationsBuilderTests {
