ab403d93a07331a5ced94aa08805abec8f9814ab
Muyao <v-muyaofeng@microsoft.com>
2023-02-08 16:14:41 +0800

    fix pl

--- sdk/spring/spring-cloud-azure-stream-binder-eventhubs/src/test/java/com/azure/spring/cloud/stream/binder/eventhubs/implementation/EventHubsMessageChannelBinderTests.java
+++ sdk/spring/spring-cloud-azure-stream-binder-eventhubs/src/test/java/com/azure/spring/cloud/stream/binder/eventhubs/implementation/EventHubsMessageChannelBinderTests.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.azure.spring.cloud.stream.binder.eventhubs.implementation;
 
 import com.azure.spring.cloud.stream.binder.eventhubs.core.implementation.provisioning.EventHubsChannelProvisioner;
