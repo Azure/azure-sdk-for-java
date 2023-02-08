0a48553d0e6d7c12df9c1bcc97c016020165eb48
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2023-02-08 15:45:17 -0500

    Correctly Read JSON When Buffering

---
+++ sdk/core/azure-core/src/main/java/com/azure/core/implementation/util/BinaryDataContentType.java
@@ -0,0 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.core.implementation.util;public class BinaryDataContentType {
+}
