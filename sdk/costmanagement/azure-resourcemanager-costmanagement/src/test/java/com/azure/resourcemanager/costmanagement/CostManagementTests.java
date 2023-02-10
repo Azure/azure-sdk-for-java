798806d62d4bfd9873f29b396f3aecb3da63442e
Weidong Xu <weidxu@microsoft.com>
2023-02-10 11:22:08 +0800

    add test case

---
+++ sdk/costmanagement/azure-resourcemanager-costmanagement/src/test/java/com/azure/resourcemanager/costmanagement/CostManagementTests.java
@@ -0,0 +1,23 @@
+package com.azure.resourcemanager.costmanagement;
+
+import com.azure.core.http.policy.HttpLogDetailLevel;
+import com.azure.core.http.policy.HttpLogOptions;
+import com.azure.core.management.AzureEnvironment;
+import com.azure.core.management.profile.AzureProfile;
+import com.azure.core.test.TestBase;
+import com.azure.core.test.annotation.DoNotRecord;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import org.junit.jupiter.api.Test;
+
+public class CostManagementTests extends TestBase {
+
+    @Test
+    @DoNotRecord(skipInPlayback = true)
+    public void testOperations() {
+        CostManagementManager costManagementManager = CostManagementManager
+            .configure().withLogOptions(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BASIC))
+            .authenticate(new DefaultAzureCredentialBuilder().build(), new AzureProfile(AzureEnvironment.AZURE));
+
+        costManagementManager.operations().list().stream().count();
+    }
+}
