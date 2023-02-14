57d6e29b7b2ccfca4d8220c26a796819f03e55d1
Haoling Dong <haolingdong@microsoft.com>
2023-02-14 10:51:36 +0800

    Tested the script successfully, Revert "Try the fix locally" now.

--- eng/common/scripts/Cadl-Project-Sync.ps1
+++ eng/common/scripts/Cadl-Project-Sync.ps1
@@ -44,10 +44,10 @@ function GetGitRemoteValue([string]$repo) {
         $gitRemotes = (git remote -v)
         foreach ($remote in $gitRemotes) {
             if ($remote.StartsWith("origin")) {
-                if ($remote -match 'https://github.com/\S+') {
+                if ($remote -match 'https://github.com/\S+[\.git]') {
                     $result = "https://github.com/$repo.git"
                     break
-                } elseif ($remote -match "git@github.com:\S+"){
+                } elseif ($remote -match "git@github.com:\S+[\.git]"){
                     $result = "git@github.com:$repo.git"
                     break
                 } else {
