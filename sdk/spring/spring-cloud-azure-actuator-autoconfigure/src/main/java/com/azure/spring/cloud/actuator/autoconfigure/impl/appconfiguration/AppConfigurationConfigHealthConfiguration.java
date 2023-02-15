1dfe5cda8c11955c30af0d38617d4861d91057ae
Matt Metcalf <mrm9084@gmail.com>
2023-02-14 23:57:18 -0800

    Moving files to impl, and adding optional

--- sdk/spring/spring-cloud-azure-actuator-autoconfigure/src/main/java/com/azure/spring/cloud/actuator/autoconfigure/impl/appconfiguration/AppConfigurationConfigHealthConfiguration.java
+++ sdk/spring/spring-cloud-azure-actuator-autoconfigure/src/main/java/com/azure/spring/cloud/actuator/autoconfigure/impl/appconfiguration/AppConfigurationConfigHealthConfiguration.java
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-package com.azure.spring.cloud.actuator.autoconfigure.appconfiguration;
+package com.azure.spring.cloud.actuator.autoconfigure.impl.appconfiguration;
 
 import org.springframework.boot.actuate.autoconfigure.health.ConditionalOnEnabledHealthIndicator;
 import org.springframework.boot.actuate.health.HealthIndicator;
@@ -10,8 +10,8 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.azure.spring.cloud.actuator.appconfiguration.AppConfigurationConfigHealthIndicator;
 import com.azure.spring.cloud.config.AppConfigurationAutoConfiguration.AppConfigurationWatchAutoConfiguration;
+import com.azure.spring.cloud.actuator.appconfiguration.AppConfigurationConfigHealthIndicator;
 import com.azure.spring.cloud.config.AppConfigurationRefresh;
 
 /**
