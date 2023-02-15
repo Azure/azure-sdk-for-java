71ba16fde5eb4764ab2925921f788cc09e3fed47
Matt Metcalf <mrm9084@gmail.com>
2023-02-15 09:40:01 -0800

    Fixing Integration Test

--- sdk/spring/spring-cloud-azure-integration-test-appconfiguration-config/src/test/java/com/azure/spring/cloud/config/CustomClient.java
+++ sdk/spring/spring-cloud-azure-integration-test-appconfiguration-config/src/test/java/com/azure/spring/cloud/config/CustomClient.java
@@ -3,8 +3,9 @@ package com.azure.spring.cloud.config;
 import com.azure.core.credential.TokenCredential;
 import com.azure.data.appconfiguration.ConfigurationClientBuilder;
 import com.azure.identity.EnvironmentCredentialBuilder;
+import com.azure.security.keyvault.secrets.SecretClientBuilder;
 
-public class CustomClient implements ConfigurationClientCustomizer {
+public class CustomClient implements ConfigurationClientCustomizer, SecretClientCustomizer {
 
     TokenCredential buildCredential() {
         return new EnvironmentCredentialBuilder().build();
@@ -12,6 +13,11 @@ public class CustomClient implements ConfigurationClientCustomizer {
 
     @Override
     public void customize(ConfigurationClientBuilder builder, String endpoint) {
+        builder.credential(buildCredential());
+    }
+
+    @Override
+    public void customize(SecretClientBuilder builder, String endpoint) {
         builder.credential(buildCredential());
     }
 
