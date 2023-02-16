ad1c43d9e492d6c176d94710dbbd670b34126782
Fabian Meiswinkel <fabian@meiswinkel.com>
2023-02-16 00:16:10 +0000

    Update CosmosMeterOptions.java

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/clienttelemetry/CosmosMeterOptions.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/clienttelemetry/CosmosMeterOptions.java
@@ -24,7 +24,7 @@ public final class CosmosMeterOptions {
 
         this.name = name;
         this.isEnabled = isEnabled;
-        this.percentiles = percentiles;
+        this.percentiles = percentiles != null ? percentiles.clone() : null;
         this.isHistogramPublishingEnabled = isHistogramPublishingEnabled;
         this.suppressedTagNames = suppressedTagNames;
     }
