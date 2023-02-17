b49033e8280bd966474d2eb13765101cc4007556
Weidong Xu <weidxu@microsoft.com>
2023-02-17 16:27:40 +0800

    mgmt, aks update samples

--- sdk/resourcemanager/azure-resourcemanager/src/samples/java/com/azure/resourcemanager/containerservice/generated/ManagedClustersResetAadProfileSamples.java
+++ sdk/resourcemanager/azure-resourcemanager/src/samples/java/com/azure/resourcemanager/containerservice/generated/ManagedClustersResetAadProfileSamples.java
@@ -9,7 +9,7 @@ import com.azure.resourcemanager.containerservice.models.ManagedClusterAadProfil
 /** Samples for ManagedClusters ResetAadProfile. */
 public final class ManagedClustersResetAadProfileSamples {
     /*
-     * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/stable/2023-01-01/examples/ManagedClustersResetAADProfile.json
+     * x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2023-01-01/examples/ManagedClustersResetAADProfile.json
      */
     /**
      * Sample code: Reset AAD Profile.
