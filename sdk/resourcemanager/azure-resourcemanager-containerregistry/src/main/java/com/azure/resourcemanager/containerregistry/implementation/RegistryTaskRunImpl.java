64c5ff46b3444a5fcecfcf3e391bedad986ee9be
Weidong Xu <weidxu@microsoft.com>
2023-02-21 10:14:05 +0800

    fix null resourceGroupName in refresh

--- sdk/resourcemanager/azure-resourcemanager-containerregistry/src/main/java/com/azure/resourcemanager/containerregistry/implementation/RegistryTaskRunImpl.java
+++ sdk/resourcemanager/azure-resourcemanager-containerregistry/src/main/java/com/azure/resourcemanager/containerregistry/implementation/RegistryTaskRunImpl.java
@@ -22,6 +22,7 @@ import com.azure.resourcemanager.containerregistry.models.RunType;
 import com.azure.resourcemanager.containerregistry.models.SetValue;
 import com.azure.resourcemanager.containerregistry.models.TaskRunRequest;
 import com.azure.resourcemanager.containerregistry.models.Variant;
+import com.azure.resourcemanager.resources.fluentcore.arm.ResourceUtils;
 import com.azure.resourcemanager.resources.fluentcore.utils.ResourceManagerUtils;
 import reactor.core.publisher.Mono;
 
@@ -48,7 +49,11 @@ class RegistryTaskRunImpl implements RegistryTaskRun, RegistryTaskRun.Definition
 
     @Override
     public String resourceGroupName() {
-        return this.resourceGroupName;
+        if (this.resourceGroupName == null) {
+            return ResourceUtils.groupFromResourceId(inner.id());
+        } else {
+            return this.resourceGroupName;
+        }
     }
 
     @Override
@@ -294,7 +299,7 @@ class RegistryTaskRunImpl implements RegistryTaskRun, RegistryTaskRun.Definition
         if (this.fileTaskRunRequest != null) {
             return this
                 .registriesInner
-                .scheduleRunAsync(this.resourceGroupName, this.registryName, this.fileTaskRunRequest)
+                .scheduleRunAsync(this.resourceGroupName(), this.registryName, this.fileTaskRunRequest)
                 .map(
                     runInner -> {
                         self.inner = runInner;
@@ -303,7 +308,7 @@ class RegistryTaskRunImpl implements RegistryTaskRun, RegistryTaskRun.Definition
         } else if (this.encodedTaskRunRequest != null) {
             return this
                 .registriesInner
-                .scheduleRunAsync(this.resourceGroupName, this.registryName, this.encodedTaskRunRequest)
+                .scheduleRunAsync(this.resourceGroupName(), this.registryName, this.encodedTaskRunRequest)
                 .map(
                     runInner -> {
                         self.inner = runInner;
@@ -312,7 +317,7 @@ class RegistryTaskRunImpl implements RegistryTaskRun, RegistryTaskRun.Definition
         } else if (this.dockerTaskRunRequest != null) {
             return this
                 .registriesInner
-                .scheduleRunAsync(this.resourceGroupName, this.registryName, this.dockerTaskRunRequest)
+                .scheduleRunAsync(this.resourceGroupName(), this.registryName, this.dockerTaskRunRequest)
                 .map(
                     runInner -> {
                         self.inner = runInner;
@@ -321,7 +326,7 @@ class RegistryTaskRunImpl implements RegistryTaskRun, RegistryTaskRun.Definition
         } else if (this.taskRunRequest != null) {
             return this
                 .registriesInner
-                .scheduleRunAsync(this.resourceGroupName, this.registryName, this.taskRunRequest)
+                .scheduleRunAsync(this.resourceGroupName(), this.registryName, this.taskRunRequest)
                 .map(
                     runInner -> {
                         self.inner = runInner;
@@ -367,7 +372,7 @@ class RegistryTaskRunImpl implements RegistryTaskRun, RegistryTaskRun.Definition
         return registryManager
             .serviceClient()
             .getRuns()
-            .getAsync(this.resourceGroupName, this.registryName, this.inner.runId())
+            .getAsync(this.resourceGroupName(), this.registryName, this.inner.runId())
             .map(
                 runInner -> {
                     self.inner = runInner;
