eea1e9385f07e58e0b3cc27ed2e3ec81fc7ca7f9
Helen Yang <heya@microsoft.com>
2023-02-22 21:54:20 -0800

    Add copyright

--- sdk/monitor/azure-monitor-opentelemetry-exporter/src/samples/java/com/azure/monitor/opentelemetry/exporter/AzureMonitorMetricExporterSample.java
+++ sdk/monitor/azure-monitor-opentelemetry-exporter/src/samples/java/com/azure/monitor/opentelemetry/exporter/AzureMonitorMetricExporterSample.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.azure.monitor.opentelemetry.exporter;
 
 import com.azure.core.http.HttpClient;
@@ -13,7 +16,6 @@ import io.opentelemetry.sdk.OpenTelemetrySdk;
 import io.opentelemetry.sdk.metrics.SdkMeterProvider;
 import io.opentelemetry.sdk.metrics.export.MetricExporter;
 import io.opentelemetry.sdk.metrics.export.PeriodicMetricReader;
-
 import java.net.InetSocketAddress;
 
 public class AzureMonitorMetricExporterSample {
