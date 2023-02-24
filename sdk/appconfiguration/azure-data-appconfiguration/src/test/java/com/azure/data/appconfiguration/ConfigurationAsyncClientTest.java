b3ce6f237391212cabb25f2ab8e2c234ac18f571
Shawn Fang <45607042+mssfang@users.noreply.github.com>
2023-02-24 11:32:59 -0800

    [AppConfig] Add back the appconfig artifacts in tests.yml (#33665)

--- sdk/appconfiguration/azure-data-appconfiguration/src/test/java/com/azure/data/appconfiguration/ConfigurationAsyncClientTest.java
+++ sdk/appconfiguration/azure-data-appconfiguration/src/test/java/com/azure/data/appconfiguration/ConfigurationAsyncClientTest.java
@@ -1133,14 +1133,13 @@ public class ConfigurationAsyncClientTest extends ConfigurationClientTestBase {
             settings.add(new ConfigurationSetting().setKey(keyPrefix).setValue("myValue" + value).setLabel(labelPrefix));
         }
 
-        List<Mono<Response<ConfigurationSetting>>> results = new ArrayList<>();
         for (ConfigurationSetting setting : settings) {
-            results.add(client.setConfigurationSettingWithResponse(setting, false));
+            StepVerifier.create(client.setConfigurationSetting(setting))
+                .expectNextCount(1)
+                .verifyComplete();
         }
 
         SettingSelector filter = new SettingSelector().setKeyFilter(keyPrefix).setLabelFilter(labelPrefix);
-
-        Flux.merge(results).blockLast();
         StepVerifier.create(client.listRevisions(filter))
             .expectNextCount(numberExpected)
             .verifyComplete();
@@ -1156,16 +1155,15 @@ public class ConfigurationAsyncClientTest extends ConfigurationClientTestBase {
         client = getConfigurationAsyncClient(httpClient, serviceVersion);
         final int numberExpected = 50;
         List<ConfigurationSetting> settings = new ArrayList<>(numberExpected);
-        List<Mono<Response<ConfigurationSetting>>> results = new ArrayList<>();
         for (int value = 0; value < numberExpected; value++) {
             ConfigurationSetting setting = new ConfigurationSetting().setKey(keyPrefix).setValue("myValue" + value).setLabel(labelPrefix);
             settings.add(setting);
-            results.add(client.setConfigurationSettingWithResponse(setting, false));
+            StepVerifier.create(client.setConfigurationSetting(setting))
+                .expectNextCount(1)
+                .verifyComplete();
         }
 
         SettingSelector filter = new SettingSelector().setKeyFilter(keyPrefix).setLabelFilter(labelPrefix);
-
-        Flux.merge(results).blockLast();
 
         List<ConfigurationSetting> configurationSettingList1 = new ArrayList<>();
         List<ConfigurationSetting> configurationSettingList2 = new ArrayList<>();
@@ -1192,7 +1190,7 @@ public class ConfigurationAsyncClientTest extends ConfigurationClientTestBase {
         for (int value = 0; value < numberExpected; value++) {
             ConfigurationSetting setting = new ConfigurationSetting().setKey(keyPrefix).setValue("myValue" + value).setLabel(labelPrefix);
             settings.add(setting);
-            results.add(client.setConfigurationSettingWithResponse(setting, false));
+            StepVerifier.create(client.setConfigurationSetting(setting)).expectNextCount(1).verifyComplete();
         }
 
         SettingSelector filter = new SettingSelector().setKeyFilter(keyPrefix).setLabelFilter(labelPrefix);
