0a854e84208f39d84d47637f646a63abe7287816
Bill Wert <billwert@microsoft.com>
2023-03-03 17:49:47 -0800

    Re-enable test proxy unit tests (#33855)
    * Re-enable test proxy unit tests
    
    The failure was caused by the fact that these tests effectively override the `AZURE_TEST_MODE` variable so they can test record and playback in the same session. The mechanism for doing so is later than when the test proxy started, so the sequence of events is:
    
    ```
    AZURE_TEST_MODE=LIVE
    skip starting test proxy
    AZURE_TEST_MODE=PLAYBACK (within a test)
    try to talk to test proxy.
    ```
    
    This isn't a concern for real tests, as they will have a consistent mode throughout.
    
    * cleanup

--- sdk/core/azure-core-test/src/test/java/com/azure/core/test/TestProxyTests.java
+++ sdk/core/azure-core-test/src/test/java/com/azure/core/test/TestProxyTests.java
@@ -24,9 +24,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -49,7 +49,10 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 /**
  * Test class for testing Test proxy functionality of record, playback and redaction.
  */
-@Disabled
+
+// These tests override the environment variable so they can test playback and record in the same test run.
+// This strategy fails if we are in a LIVE test mode, so we'll just skip these entirely if that's the case.
+@DisabledIfEnvironmentVariable(named = "AZURE_TEST_MODE", matches = "(LIVE|live|Live)")
 public class TestProxyTests extends TestProxyTestBase {
     static TestProxyTestServer server;
     private static final ObjectMapper RECORD_MAPPER = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
