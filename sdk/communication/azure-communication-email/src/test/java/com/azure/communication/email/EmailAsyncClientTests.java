5c2c270fc29994fc7472abb98d935781409d277c
Yogesh Mohanraj <ymohanraj@microsoft.com>
2023-03-06 17:47:09 -0800

    Increasing test timeout

--- sdk/communication/azure-communication-email/src/test/java/com/azure/communication/email/EmailAsyncClientTests.java
+++ sdk/communication/azure-communication-email/src/test/java/com/azure/communication/email/EmailAsyncClientTests.java
@@ -15,7 +15,7 @@ import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@Timeout(value = 5, unit = TimeUnit.MINUTES)
+@Timeout(value = 10, unit = TimeUnit.MINUTES)
 public class EmailAsyncClientTests extends EmailTestBase {
 
     private EmailAsyncClient emailAsyncClient;
