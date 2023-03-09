c48b14171b78aad9b4c1fb31c7e9a8c93fb07760
Rajarshi Sarkar <rsarkar@microsoft.com>
2023-03-08 18:34:38 -0800

    missing javadoc from ExternalStorage abstract base class

--- sdk/communication/azure-communication-callautomation/src/main/java/com/azure/communication/callautomation/models/ExternalStorage.java
+++ sdk/communication/azure-communication-callautomation/src/main/java/com/azure/communication/callautomation/models/ExternalStorage.java
@@ -7,15 +7,27 @@ import com.azure.communication.callautomation.implementation.models.RecordingSto
 import com.azure.core.annotation.Fluent;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/** Abstract base class for different external storage types. */
 @Fluent
 public abstract class ExternalStorage {
     @JsonProperty(value = "storageType")
     private RecordingStorageType storageType;
 
+    /**
+     * Get the storageType property: Defines the type of external storage.
+     *
+     * @return the storageType value.
+     */
     public RecordingStorageType getStorageType() {
         return storageType;
     }
 
+    /**
+     * Set the storageType property: Defines the type of external storage.
+     *
+     * @param storageType the storageType value to set.
+     * @return the ExternalStorage object itself.
+     */
     protected ExternalStorage setStorageType(RecordingStorageType storageType) {
         this.storageType = storageType;
         return this;
