e3259e219375d799e9053254f83d82ee4a74667e
Jocelyn Schreppler <jaschrep@microsoft.com>
2023-03-09 15:46:40 -0500

    Javadocs

--- sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/models/BlobSeekableByteChannelReadResult.java
+++ sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/models/BlobSeekableByteChannelReadResult.java
@@ -12,15 +12,25 @@ public final class BlobSeekableByteChannelReadResult {
     private final SeekableByteChannel channel;
     private final BlobProperties properties;
 
+    /**
+     * @param channel Channel to read the target blob.
+     * @param properties Blob properties of the target blob.
+     */
     public BlobSeekableByteChannelReadResult(SeekableByteChannel channel, BlobProperties properties) {
         this.channel = channel;
         this.properties = properties;
     }
 
+    /**
+     * @return Channel to read the target blob.
+     */
     public SeekableByteChannel getChannel() {
         return channel;
     }
 
+    /**
+     * @return Blob properties of the target blob.
+     */
     public BlobProperties getProperties() {
         return properties;
     }
