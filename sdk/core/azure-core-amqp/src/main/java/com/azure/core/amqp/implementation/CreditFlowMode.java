432cc510f585ea577a2a5b6eb33c45a7004d8c7b
Anu Thomas <anuamd@hotmail.com>
2023-03-11 10:56:29 -0800

    Make the impl package type CreditFlowMode public.

--- sdk/core/azure-core-amqp/src/main/java/com/azure/core/amqp/implementation/CreditFlowMode.java
+++ sdk/core/azure-core-amqp/src/main/java/com/azure/core/amqp/implementation/CreditFlowMode.java
@@ -6,7 +6,7 @@ package com.azure.core.amqp.implementation;
 /**
  * Indicates how to compute the credit and when to send it to the broker via a flow performative.
  */
-enum CreditFlowMode {
+public enum CreditFlowMode {
     /**
      * When the number of total messages emitted 'n' since the last broker flow is greater than or equal
      * to a fraction (e.g. 0.5) of the Prefetch, the message-flux will send a flow for the credit 'n'.
