92f47b8ee92e8161a9d738e56a3e56f3210111a0
xiaofeicao <xiaofeicao@microsoft.com>
2023-03-15 22:02:29 +0800

    update

--- sdk/resourcemanager/azure-resourcemanager-keyvault/src/test/java/com/azure/resourcemanager/keyvault/ManagedHsmTests.java
+++ sdk/resourcemanager/azure-resourcemanager-keyvault/src/test/java/com/azure/resourcemanager/keyvault/ManagedHsmTests.java
@@ -4,60 +4,106 @@
 package com.azure.resourcemanager.keyvault;
 
 import com.azure.core.http.rest.PagedIterable;
+import com.azure.core.util.Context;
+import com.azure.resourcemanager.keyvault.fluent.models.ManagedHsmInner;
 import com.azure.resourcemanager.keyvault.models.CreateMode;
 import com.azure.resourcemanager.keyvault.models.Key;
 import com.azure.resourcemanager.keyvault.models.Keys;
 import com.azure.resourcemanager.keyvault.models.ManagedHsm;
+import com.azure.resourcemanager.keyvault.models.ManagedHsmProperties;
 import com.azure.resourcemanager.keyvault.models.ManagedHsmSku;
+import com.azure.resourcemanager.keyvault.models.ManagedHsmSkuFamily;
+import com.azure.resourcemanager.keyvault.models.ManagedHsmSkuName;
 import com.azure.resourcemanager.keyvault.models.NetworkRuleSet;
 import com.azure.resourcemanager.keyvault.models.ProvisioningState;
+import com.azure.security.keyvault.administration.KeyVaultAccessControlAsyncClient;
+import com.azure.security.keyvault.administration.KeyVaultAccessControlClientBuilder;
+import com.azure.security.keyvault.administration.models.KeyVaultRoleScope;
 import com.azure.security.keyvault.keys.models.KeyType;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
 public class ManagedHsmTests extends KeyVaultManagementTest {
     @Test
     public void canOperateManagedHsmAndKeys() {
-        String resourceGroupName = "rg-name";
-        String hsmName = "my-hsm";
+        ManagedHsm managedHsm = prepareManagedHsm();
 
-        // listByResourceGroups
-        PagedIterable<ManagedHsm> hsms = keyVaultManager.managedHsms()
-            .listByResourceGroup(resourceGroupName);
-        // getByResourceGroup
-        ManagedHsm hsm = keyVaultManager.managedHsms()
-            .getByResourceGroup(resourceGroupName, hsmName);
+        try {
+            // listByResourceGroups
+            PagedIterable<ManagedHsm> hsms = keyVaultManager.managedHsms()
+                .listByResourceGroup(rgName);
+            // getByResourceGroup
+            ManagedHsm hsm = keyVaultManager.managedHsms()
+                .getByResourceGroup(rgName, managedHsm.name());
 
-        // ManagedHsm properties
-        // The Azure Active Directory tenant ID that should be used for authenticating requests to the managed HSM pool.
-        String tenantId  = hsm.tenantId();
-        ManagedHsmSku sku = hsm.sku();
-        // Array of initial administrators object ids for this managed hsm pool.
-        List<String> initialAdminObjectIds = hsm.initialAdminObjectIds();
-        // The URI of the managed hsm pool for performing operations on keys.
-        String hsmUri = hsm.hsmUri();
-        // Property to specify whether the 'soft delete' functionality is enabled for this managed HSM pool.
-        boolean softDelete = hsm.isSoftDeleteEnabled();
-        // softDelete data retention days. It accepts >=7 and <=90.
-        Integer softDeleteRetentionDays = hsm.softDeleteRetentionDays();
-        // Property specifying whether protection against purge is enabled for this managed HSM pool.
-        boolean purgeProtectionEnabled = hsm.isPurgeProtectionEnabled();
-        // he create mode to indicate whether the resource is being created or is being recovered from a deleted resource.
-        CreateMode createMode = hsm.createMode();
-        // Rules governing the accessibility of the key vault from specific network locations.
-        NetworkRuleSet ruleSet = hsm.networkRuleSet();
-        // Provisioning state.
-        ProvisioningState provisioningState = hsm.state();
+            // ManagedHsm properties
+            // The Azure Active Directory tenant ID that should be used for authenticating requests to the managed HSM pool.
+            String tenantId  = hsm.tenantId();
+            ManagedHsmSku sku = hsm.sku();
+            // Array of initial administrators object ids for this managed hsm pool.
+            List<String> initialAdminObjectIds = hsm.initialAdminObjectIds();
+            // The URI of the managed hsm pool for performing operations on keys.
+            String hsmUri = hsm.hsmUri();
+            // Property to specify whether the 'soft delete' functionality is enabled for this managed HSM pool.
+            boolean softDelete = hsm.isSoftDeleteEnabled();
+            // softDelete data retention days. It accepts >=7 and <=90.
+            Integer softDeleteRetentionDays = hsm.softDeleteRetentionDays();
+            // Property specifying whether protection against purge is enabled for this managed HSM pool.
+            boolean purgeProtectionEnabled = hsm.isPurgeProtectionEnabled();
+            // he create mode to indicate whether the resource is being created or is being recovered from a deleted resource.
+            CreateMode createMode = hsm.createMode();
+            // Rules governing the accessibility of the key vault from specific network locations.
+            NetworkRuleSet ruleSet = hsm.networkRuleSet();
+            // Provisioning state.
+            ProvisioningState provisioningState = hsm.state();
 
-        // key operations, same interface as the key vault
-        Keys keys = hsm.keys();
-        String keyName = generateRandomResourceName("key", 10);
-        Key key = keys.define(keyName)
-            .withKeyTypeToCreate(KeyType.RSA)
-            .withKeySize(4096)
-            .create();
+            // key operations, same interface as the key vault
+            Keys keys = hsm.keys();
+            String keyName = generateRandomResourceName("key", 10);
+            Key key = keys.define(keyName)
+                .withKeyTypeToCreate(KeyType.RSA)
+                .withKeySize(4096)
+                .create();
 
-        keys.deleteById(key.id());
+            keys.deleteById(key.id());
+        } finally {
+            keyVaultManager.managedHsms().deleteById(managedHsm.id());
+            keyVaultManager.serviceClient().getManagedHsms().purgeDeleted(managedHsm.name(), managedHsm.regionName());
+        }
+    }
+
+    private ManagedHsm prepareManagedHsm() {
+        String objectId = authorizationManager
+            .users()
+            .getByNameAsync(clientIdFromFile())
+            .block()
+            .id();
+        ManagedHsmInner inner =  keyVaultManager.serviceClient()
+            .getManagedHsms()
+            .createOrUpdate(
+                rgName,
+                generateRandomResourceName("mhsm", 10),
+                new ManagedHsmInner()
+                    .withLocation("westus")
+                    .withSku(
+                        new ManagedHsmSku().withFamily(ManagedHsmSkuFamily.B).withName(ManagedHsmSkuName.STANDARD_B1))
+                    .withProperties(
+                        new ManagedHsmProperties()
+                            .withTenantId(UUID.fromString(authorizationManager.tenantId()))
+                            .withInitialAdminObjectIds(Arrays.asList(objectId))
+                            .withEnableSoftDelete(true)
+                            .withSoftDeleteRetentionInDays(90)
+                            .withEnablePurgeProtection(false)),
+                Context.NONE);
+
+        KeyVaultAccessControlAsyncClient accessControlAsyncClient =
+            new KeyVaultAccessControlClientBuilder()
+                .pipeline(keyVaultManager.httpPipeline())
+                .vaultUrl(inner.properties().hsmUri())
+                .buildAsyncClient();
+        return null;
     }
 }
