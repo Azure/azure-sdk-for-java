704b9c3d49fe99dc4ff70d41931996ac7da96aab
Haoling Dong <haolingdong@microsoft.com>
2023-03-20 14:01:28 +0800

    update TypeSpec-Compare-CurrentToCodegeneration.ps1

--- eng/pipelines/scripts/TypeSpec-Compare-CurrentToCodegeneration.ps1
+++ eng/pipelines/scripts/TypeSpec-Compare-CurrentToCodegeneration.ps1
@@ -1,15 +1,15 @@
-<#
+<#yaml
 .SYNOPSIS
-Sync CADL defintion and generate SDK for RPs under the input directory and have 'cadl-location.yaml'.
+Sync TypeSpec defintion and generate SDK for RPs under the input directory and have 'tsp-location.yaml'.
 
 .DESCRIPTION
-Sync CADL defintion and generate SDK for RPs under the input directory and have 'cadl-location.yaml'.
+Sync TypeSpec defintion and generate SDK for RPs under the input directory and have 'tsp-location.yaml'.
 
 If the regenerated code is different from current code, this will tell the files the differences
 are in, and exit with a failure status.
 
 .PARAMETER Directory
-The directory that will be used to get 'cadl-location.yaml' and generate SDK. The default is the root directory of the
+The directory that will be used to get 'tsp-location.yaml' and generate SDK. The default is the root directory of the
 Azure SDK for Java repository. One can also input service directory like: /sdk/storage, sdk/anomalydetector/azure-ai-anomalydetector.
 #>
 
@@ -32,11 +32,11 @@ Invoking Autorest code regeneration
 "
 
 
-foreach ($cadlLocationPath in (Get-ChildItem -Path $path -Filter "cadl-location.yaml" -Recurse)) {
-  $sdkPath = (get-item $cadlLocationPath).Directory.FullName
+foreach ($tspLocationPath in (Get-ChildItem -Path $path -Filter "tsp-location.yaml" -Recurse)) {
+  $sdkPath = (get-item $tspLocationPath).Directory.FullName
   Write-Host "Generate SDK for $sdkPath"
-  ./eng/common/scripts/Cadl-Project-Sync.ps1 $sdkPath
-  ./eng/common/scripts/Cadl-Project-Generate.ps1 $sdkPath
+  ./eng/common/scripts/TypeSpec-Project-Sync.ps1 $sdkPath
+  ./eng/common/scripts/TypeSpec-Project-Generate.ps1 $sdkPath
 }
 
 Write-Host "
