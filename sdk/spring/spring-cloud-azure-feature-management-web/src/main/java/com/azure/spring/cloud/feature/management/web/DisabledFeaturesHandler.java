3de9b49d8d0f6f9f9073d9957cf7c12fef42da7d
Matt Metcalf <mrm9084@gmail.com>
2023-03-24 13:38:04 -0700

    Updating Web (#34204)

--- sdk/spring/spring-cloud-azure-feature-management-web/src/main/java/com/azure/spring/cloud/feature/management/web/DisabledFeaturesHandler.java
+++ sdk/spring/spring-cloud-azure-feature-management-web/src/main/java/com/azure/spring/cloud/feature/management/web/DisabledFeaturesHandler.java
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-package com.azure.spring.cloud.feature.manager;
+package com.azure.spring.cloud.feature.management.web;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
