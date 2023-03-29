d5655fbfcbab48aeb83d42b355b7a84a64c8d9ac
fariba haghbin <fariba.haghbin@nuance.com>
2023-03-29 19:08:20 -0400

    Remove unused import

--- sdk/communication/azure-communication-callingserver/src/main/java/com/azure/communication/callingserver/CallRecordingAsync.java
+++ sdk/communication/azure-communication-callingserver/src/main/java/com/azure/communication/callingserver/CallRecordingAsync.java
@@ -42,8 +42,6 @@ import reactor.core.publisher.Mono;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousFileChannel;
