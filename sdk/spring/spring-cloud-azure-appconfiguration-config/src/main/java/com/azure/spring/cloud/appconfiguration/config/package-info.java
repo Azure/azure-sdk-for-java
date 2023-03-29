3dce12c63799926ead833ec06410cdeb48d7d68a
Matt Metcalf <mrm9084@gmail.com>
2023-03-29 10:28:20 -0700

    Revert "Moving Changes from main"
    This reverts commit e5ba4a7e64f8523f3f599f03b755be8b6e1067a0.

--- sdk/spring/spring-cloud-azure-appconfiguration-config/src/main/java/com/azure/spring/cloud/appconfiguration/config/package-info.java
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-/**
- * Package contains classes for loading configuration from Azure App Configuration Stores.
- */
-package com.azure.spring.cloud.appconfiguration.config;
