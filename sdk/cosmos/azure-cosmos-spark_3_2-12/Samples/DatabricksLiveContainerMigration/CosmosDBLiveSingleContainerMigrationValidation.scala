46a37c79c00ee2e050e4777c45a40a89bdcde9af
Spence Kile <spence.kile@acuitybrands.com>
2023-04-03 18:26:38 -0400

    Fixes for Source and Target diff Cosmos Accounts

--- sdk/cosmos/azure-cosmos-spark_3_2-12/Samples/DatabricksLiveContainerMigration/CosmosDBLiveSingleContainerMigrationValidation.scala
+++ sdk/cosmos/azure-cosmos-spark_3_2-12/Samples/DatabricksLiveContainerMigration/CosmosDBLiveSingleContainerMigrationValidation.scala
@@ -1,18 +1,27 @@
 // Databricks notebook source
-val cosmosEndpoint = "" //enter your Cosmos DB Account URI
-val cosmosMasterKey = "" //enter your Cosmos DB Account PRIMARY KEY
+val cosmosEndpoint_source = "" //enter your Cosmos DB Account URI
+val cosmosMasterKey_source = "" //enter your Cosmos DB Account PRIMARY KEY
+
+// Databricks notebook target
+val cosmosEndpoint_target = "" //enter your Cosmos DB Account URI
+val cosmosMasterKey_target = "" //enter your Cosmos DB Account PRIMARY KEY
 
 // COMMAND ----------
 
-spark.conf.set("spark.sql.catalog.cosmosCatalog", "com.azure.cosmos.spark.CosmosCatalog")
-spark.conf.set("spark.sql.catalog.cosmosCatalog.spark.cosmos.accountEndpoint", cosmosEndpoint)
-spark.conf.set("spark.sql.catalog.cosmosCatalog.spark.cosmos.accountKey", cosmosMasterKey)
-spark.conf.set("spark.sql.catalog.cosmosCatalog.spark.cosmos.views.repositoryPath", "/viewDefinitions" +  java.util.UUID.randomUUID.toString)
+spark.conf.set("spark.sql.catalog.cosmosCatalogSrc", "com.azure.cosmos.spark.CosmosCatalog")
+spark.conf.set("spark.sql.catalog.cosmosCatalogSrc.spark.cosmos.accountEndpoint", cosmosEndpoint_source)
+spark.conf.set("spark.sql.catalog.cosmosCatalogSrc.spark.cosmos.accountKey", cosmosMasterKey_source)
+spark.conf.set("spark.sql.catalog.cosmosCatalogSrc.spark.cosmos.views.repositoryPath", "/viewDefinitions" +  java.util.UUID.randomUUID.toString)
+
+spark.conf.set("spark.sql.catalog.cosmosCatalogTgt", "com.azure.cosmos.spark.CosmosCatalog")
+spark.conf.set("spark.sql.catalog.cosmosCatalogTgt.spark.cosmos.accountEndpoint", cosmosEndpoint_target)
+spark.conf.set("spark.sql.catalog.cosmosCatalogTgt.spark.cosmos.accountKey", cosmosMasterKey_target)
+spark.conf.set("spark.sql.catalog.cosmosCatalogTgt.spark.cosmos.views.repositoryPath", "/viewDefinitions" +  java.util.UUID.randomUUID.toString)
 
 // COMMAND ----------
 
 // MAGIC %sql
-// MAGIC CREATE TABLE IF NOT EXISTS cosmosCatalog.`database-v4`.SourceView 
+// MAGIC CREATE TABLE IF NOT EXISTS cosmosCatalogSrc.`database-v4`.SourceView 
 // MAGIC   (id STRING, _etag STRING)
 // MAGIC USING cosmos.oltp
 // MAGIC TBLPROPERTIES(isCosmosView = 'True')
@@ -22,12 +31,12 @@ spark.conf.set("spark.sql.catalog.cosmosCatalog.spark.cosmos.views.repositoryPat
 // MAGIC   spark.cosmos.read.inferSchema.enabled = 'False',  
 // MAGIC   spark.cosmos.read.partitioning.strategy = 'Default');
 // MAGIC 
-// MAGIC SELECT * FROM cosmosCatalog.`database-v4`.SourceView
+// MAGIC SELECT * FROM cosmosCatalogSrc.`database-v4`.SourceView
 
 // COMMAND ----------
 
 // MAGIC %sql
-// MAGIC CREATE TABLE IF NOT EXISTS cosmosCatalog.`database-v4`.SinkView 
+// MAGIC CREATE TABLE IF NOT EXISTS cosmosCatalogTgt.`database-v4`.SinkView 
 // MAGIC   (id STRING, _origin_etag STRING)
 // MAGIC USING cosmos.oltp
 // MAGIC TBLPROPERTIES(isCosmosView = 'True')
@@ -37,20 +46,20 @@ spark.conf.set("spark.sql.catalog.cosmosCatalog.spark.cosmos.views.repositoryPat
 // MAGIC   spark.cosmos.read.inferSchema.enabled = 'False',  
 // MAGIC   spark.cosmos.read.partitioning.strategy = 'Default');
 // MAGIC   
-// MAGIC SELECT * FROM cosmosCatalog.`database-v4`.SinkView
+// MAGIC SELECT * FROM cosmosCatalogTgt.`database-v4`.SinkView
 
 // COMMAND ----------
 
 // MAGIC %sql
 // MAGIC -- anti join shows all documents(versions) in the SourceView not present in SinkView
-// MAGIC SELECT * FROM cosmosCatalog.`database-v4`.SourceView src -- change database-v4 to be the value of your source database
-// MAGIC LEFT ANTI JOIN cosmosCatalog.`database-v4`.SinkView sink -- change database-v4 to be the value of your target database
+// MAGIC SELECT * FROM cosmosCatalogSrc.`database-v4`.SourceView src -- change database-v4 to be the value of your source database
+// MAGIC LEFT ANTI JOIN cosmosCatalogTgt.`database-v4`.SinkView sink -- change database-v4 to be the value of your target database
 // MAGIC ON src.id = sink.id and src._etag == sink._origin_etag
 
 // COMMAND ----------
 
 // MAGIC %sql
 // MAGIC -- anti join shows count of all documents(versions) in the SourceView not present in SinkView
-// MAGIC SELECT count(*) FROM cosmosCatalog.`database-v4`.SourceView src  -- change database-v4 to be the value of your source database
-// MAGIC LEFT ANTI JOIN cosmosCatalog.`database-v4`.SinkView sink -- change database-v4 to be the value of your target database
+// MAGIC SELECT count(*) FROM cosmosCatalogSrc.`database-v4`.SourceView src  -- change database-v4 to be the value of your source database
+// MAGIC LEFT ANTI JOIN cosmosCatalogTgt.`database-v4`.SinkView sink -- change database-v4 to be the value of your target database
 // MAGIC ON src.id = sink.id and src._etag == sink._origin_etag
