698941bad2e59fe87407dc33206fcb9c9deab2de
shafang <Shawn.Fang@microsoft.com>
2023-04-04 09:57:37 -0700

    draft

--- sdk/core/azure-core-http-vertx/src/main/java/com/azure/core/http/vertx/VertxAsyncHttpClient.java
+++ sdk/core/azure-core-http-vertx/src/main/java/com/azure/core/http/vertx/VertxAsyncHttpClient.java
@@ -11,6 +11,7 @@ import com.azure.core.util.Context;
 import com.azure.core.util.Contexts;
 import com.azure.core.util.FluxUtil;
 import com.azure.core.util.ProgressReporter;
+import com.azure.core.util.logging.ClientLogger;
 import io.netty.buffer.Unpooled;
 import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
@@ -30,6 +31,8 @@ import java.util.Objects;
  * {@link HttpClient} implementation for the Vert.x {@link io.vertx.core.http.HttpClient}.
  */
 class VertxAsyncHttpClient implements HttpClient {
+    private static final ClientLogger LOGGER = new ClientLogger(VertxAsyncHttpClient.class);
+
     private final Scheduler scheduler;
     final io.vertx.core.http.HttpClient client;
 
@@ -115,5 +118,67 @@ class VertxAsyncHttpClient implements HttpClient {
                         sink::error, vertxHttpRequest::end);
             }
         }));
+    }
+
+    @Override
+    public HttpResponse sendSync(HttpRequest request, Context context) {
+        // boolean eagerlyReadResponse = (boolean) context.getData("azure-eagerly-read-response").orElse(false);
+        ProgressReporter progressReporter = Contexts.with(context).getHttpRequestProgressReporter();
+
+        RequestOptions options = new RequestOptions()
+                                     .setMethod(HttpMethod.valueOf(request.getHttpMethod().name()))
+                                     .setAbsoluteURI(request.getUrl());
+
+        return client.request(options, requestResult -> {
+            if (requestResult.failed()) {
+                throw LOGGER.logExceptionAsError(new RuntimeException(requestResult.cause()));
+            }
+            try {
+                HttpClientRequest vertxHttpRequest = requestResult.result();
+                // Map to Vertx headers
+                request.getHeaders().stream()
+                    .forEach(header -> vertxHttpRequest.putHeader(header.getName(), header.getValuesList()));
+                // Chunking if the size of request is unknown.
+                if (request.getHeaders().get("Content-Length") == null) {
+                    vertxHttpRequest.setChunked(true);
+                }
+
+
+                vertxHttpRequest.response(event -> {
+                    if (event.succeeded()) {
+                        HttpClientResponse vertxHttpResponse = event.result();
+                        return new BufferedVertxHttpResponse(request, vertxHttpResponse, null);
+//                        return vertxHttpResponse.bodyHandler(buffer -> new BufferedVertxHttpResponse(request, vertxHttpResponse, buffer));
+                    } else {
+                        throw LOGGER.logExceptionAsError(new RuntimeException(event.cause()));
+                    }
+                });
+
+
+
+                // TODO (alzimmer)
+                // For now Vertx will always use a buffered request until reliability issues when using streamin can be
+                // resolved.
+                Flux<ByteBuffer> requestBody = request.getBody();
+                if (requestBody == null) {
+                    vertxHttpRequest.end();
+                } else {
+                    if (progressReporter != null) {
+                        requestBody = requestBody.map(buffer -> {
+                            progressReporter.reportProgress(buffer.remaining());
+                            return buffer;
+                        });
+                    }
+
+                    FluxUtil.collectBytesFromNetworkResponse(requestBody, request.getHeaders())
+                        .subscribeOn(scheduler)
+                        .subscribe(bytes -> vertxHttpRequest.write(Buffer.buffer(Unpooled.wrappedBuffer(bytes))),
+                            sink::error, vertxHttpRequest::end);
+                }
+
+            } catch (Exception e) {
+                throw LOGGER.logExceptionAsError(new RuntimeException(e));
+            }
+        });
     }
 }
