b262682a1325a8fdf067a80dbc40f9397fa2a8ef
samvaity <savaity@microsoft.com>
2023-04-06 15:58:14 -0700

    checkstyle

--- sdk/formrecognizer/azure-ai-formrecognizer/src/main/java/com/azure/ai/formrecognizer/documentanalysis/models/DocumentFieldType.java
+++ sdk/formrecognizer/azure-ai-formrecognizer/src/main/java/com/azure/ai/formrecognizer/documentanalysis/models/DocumentFieldType.java
@@ -49,6 +49,8 @@ public final class DocumentFieldType extends ExpandableStringEnum<DocumentFieldT
 
     /** Static value address for DocumentFieldType. */
     public static final DocumentFieldType ADDRESS = fromString("address");
+
+    /** Static value boolean for DocumentFieldType. */
     public static final DocumentFieldType BOOLEAN = fromString("boolean");
 
     /**
