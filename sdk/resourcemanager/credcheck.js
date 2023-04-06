7c16201d9354fcb16f824151c8fe7fd0e19f6dc8
Weidong Xu <weidxu@microsoft.com>
2023-04-06 15:59:17 +0800

    handle another case

--- sdk/resourcemanager/credcheck.js
+++ sdk/resourcemanager/credcheck.js
@@ -20,6 +20,10 @@ function credcheck(dir) {
         /\\"name\\":\\"password\\",\\"value\\":\\"(.*?)\\"/g,
         '\\"name\\":\\"password\\",\\"value\\":\\"***REMOVED***\\"'
     );
+    redactDict.set(
+        /\\"name\\":\\"password2\\",\\"value\\":\\"(.*?)\\"/g,
+        '\\"name\\":\\"password2\\",\\"value\\":\\"***REMOVED***\\"'
+    );
     redactDict.set(/;AccountKey=(.*?)(;|\\")/g, ";AccountKey=***REMOVED***$2");
     redactDict.set(/\\"primaryMasterKey\\":\\"(.*?)\\"/g, '\\"primaryMasterKey\\":\\"***REMOVED***\\"');
     redactDict.set(/\\"primaryReadonlyMasterKey\\":\\"(.*?)\\"/g, '\\"primaryReadonlyMasterKey\\":\\"***REMOVED***\\"');
