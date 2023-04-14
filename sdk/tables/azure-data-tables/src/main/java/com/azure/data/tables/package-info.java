7ca81b2b67fa7024d00adab23a7ec7ce88ff2ec9
Jair Myree <67484440+jairmyree@users.noreply.github.com>
2023-04-14 20:10:48 +0000

    Javadoc Update

--- sdk/tables/azure-data-tables/src/main/java/com/azure/data/tables/package-info.java
+++ sdk/tables/azure-data-tables/src/main/java/com/azure/data/tables/package-info.java
@@ -2,6 +2,11 @@
 // Licensed under the MIT License.
 
 /**
- * Package containing the classes for Tables Clients.
+ * The Azure Tables client library provides access to the database service Azure Tables. The client library supports both Azure Storage and Azure Cosmos tables. This library provides query and CRUD
+ * oprations for tables and entities within the tables. The Azure Tables serivice stores NoSQU data in the cloud, providing a key/attribute store with a schemaless design.
+ * 
+ * <h2>Getting Started</h2>
+ * 
+ * 
  */
 package com.azure.data.tables;
