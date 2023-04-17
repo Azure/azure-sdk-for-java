17a9709289b356a436dc83e0720f21dcd9242c3a
Weidong Xu <weidxu@microsoft.com>
2023-04-17 13:41:48 +0800

    mgmt, ACI upgrade 2023-05 (#34480)

--- sdk/resourcemanager/azure-resourcemanager/src/samples/java/com/azure/resourcemanager/containerinstance/generated/ContainerGroupsStartSamples.java
+++ sdk/resourcemanager/azure-resourcemanager/src/samples/java/com/azure/resourcemanager/containerinstance/generated/ContainerGroupsStartSamples.java
@@ -4,12 +4,10 @@
 
 package com.azure.resourcemanager.containerinstance.generated;
 
-import com.azure.core.util.Context;
-
 /** Samples for ContainerGroups Start. */
 public final class ContainerGroupsStartSamples {
     /*
-     * x-ms-original-file: specification/containerinstance/resource-manager/Microsoft.ContainerInstance/stable/2021-10-01/examples/ContainerGroupsStart.json
+     * x-ms-original-file: specification/containerinstance/resource-manager/Microsoft.ContainerInstance/stable/2023-05-01/examples/ContainerGroupsStart.json
      */
     /**
      * Sample code: ContainerStart.
@@ -17,6 +15,11 @@ public final class ContainerGroupsStartSamples {
      * @param azure The entry point for accessing resource management APIs in Azure.
      */
     public static void containerStart(com.azure.resourcemanager.AzureResourceManager azure) {
-        azure.containerGroups().manager().serviceClient().getContainerGroups().start("demo", "demo1", Context.NONE);
+        azure
+            .containerGroups()
+            .manager()
+            .serviceClient()
+            .getContainerGroups()
+            .start("demo", "demo1", com.azure.core.util.Context.NONE);
     }
 }
