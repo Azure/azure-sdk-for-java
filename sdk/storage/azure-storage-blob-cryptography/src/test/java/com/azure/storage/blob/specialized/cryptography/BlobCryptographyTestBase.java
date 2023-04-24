da077b8b7e029067a4528198ebc483878861eb5a
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2023-04-24 15:56:30 -0400

    Basic setup

---
+++ sdk/storage/azure-storage-blob-cryptography/src/test/java/com/azure/storage/blob/specialized/cryptography/BlobCryptographyTestBase.java
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.storage.blob.specialized.cryptography;
+
+import com.azure.core.test.TestProxyTestBase;
+
+public class BlobCryptographyTestBase extends TestProxyTestBase {
+}
