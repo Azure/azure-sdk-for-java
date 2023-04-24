cf6e7579355d8e0a1701f7668325e5bc7c7665e5
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2023-04-24 15:58:14 -0400

    Revert errant change

--- sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/specialized/BlobOutputStream.java
+++ sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/specialized/BlobOutputStream.java
@@ -108,7 +108,7 @@ public abstract class BlobOutputStream extends StorageOutputStream {
         return new PageBlobOutputStream(client, pageRange, requestConditions);
     }
 
-    protected abstract void commit();
+    abstract void commit();
 
     /**
      * Closes this output stream and releases any system resources associated with this stream. If any data remains in
