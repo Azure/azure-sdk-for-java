65e4c635cd7aca96e4c8a8939b171e1bd1da1515
annie-mac <xinlian@microsoft.com>
2023-04-25 16:28:59 -0700

    skip child leases assignement for split scenario

--- sdk/cosmos/azure-cosmos-tests/src/test/java/com/azure/cosmos/implementation/changefeed/epkversion/PartitionLoadBalancerImplTests.java
+++ sdk/cosmos/azure-cosmos-tests/src/test/java/com/azure/cosmos/implementation/changefeed/epkversion/PartitionLoadBalancerImplTests.java
@@ -8,6 +8,7 @@ import com.azure.cosmos.implementation.changefeed.LeaseContainer;
 import com.azure.cosmos.implementation.changefeed.PartitionController;
 import com.azure.cosmos.implementation.changefeed.PartitionLoadBalancingStrategy;
 import org.mockito.Mockito;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -21,8 +22,17 @@ import java.util.UUID;
 public class PartitionLoadBalancerImplTests {
     private final int PARTITION_LOAD_BALANCER_TIMEOUT = 5000;
 
-    @Test(groups = "unit")
-    public void run_success() throws InterruptedException {
+    @DataProvider(name = "loadBalancingSucceededArgProvider")
+    public static Object[][] loadBalancingSucceededArgProvider() {
+        return new Object[][]{
+            // load balancing call succeeded
+            { false },
+            { true }
+        };
+    }
+
+    @Test(groups = "unit", dataProvider = "loadBalancingSucceededArgProvider")
+    public void run(boolean loadBalancingSucceeded) throws InterruptedException {
         PartitionController partitionControllerMock = Mockito.mock(PartitionController.class);
         LeaseContainer leaseContainerMock = Mockito.mock(LeaseContainer.class);
         PartitionLoadBalancingStrategy partitionLoadBalancingStrategyMock = Mockito.mock(PartitionLoadBalancingStrategy.class);
@@ -32,10 +42,18 @@ public class PartitionLoadBalancerImplTests {
 
         List<Lease> allLeases = Arrays.asList(lease);
         Mockito.when(leaseContainerMock.getAllLeases()).thenReturn(Flux.fromIterable(allLeases));
-        Mockito
-            .when(partitionLoadBalancingStrategyMock.selectLeasesToTake(allLeases))
-            .thenReturn(allLeases)
-            .thenReturn(Arrays.asList());
+
+        if (loadBalancingSucceeded) {
+            Mockito
+                .when(partitionLoadBalancingStrategyMock.selectLeasesToTake(allLeases))
+                .thenReturn(allLeases)
+                .thenReturn(Arrays.asList());
+        } else {
+            Mockito
+                .when(partitionLoadBalancingStrategyMock.selectLeasesToTake(allLeases))
+                .thenThrow(new IllegalArgumentException("Something is wrong"));
+        }
+
         Mockito.when(partitionControllerMock.shutdown()).thenReturn(Mono.empty());
 
         PartitionLoadBalancerImpl partitionLoadBalancerImpl =
@@ -53,45 +71,6 @@ public class PartitionLoadBalancerImplTests {
             .subscribeOn(Schedulers.boundedElastic())
             .subscribe();
         Thread.sleep(Duration.ofSeconds(5).toMillis());
-        Mockito.verify(leaseContainerMock, Mockito.atMost(3)).getAllLeases();
-        partitionLoadBalancerImpl
-            .stop()
-            .timeout(Duration.ofMillis(PARTITION_LOAD_BALANCER_TIMEOUT))
-            .subscribeOn(Schedulers.boundedElastic())
-            .subscribe();
-    }
-
-    @Test(groups = "unit")
-    public void run_failure() throws InterruptedException {
-        PartitionController partitionControllerMock = Mockito.mock(PartitionController.class);
-        LeaseContainer leaseContainerMock = Mockito.mock(LeaseContainer.class);
-        PartitionLoadBalancingStrategy partitionLoadBalancingStrategyMock = Mockito.mock(PartitionLoadBalancingStrategy.class);
-
-
-        ServiceItemLeaseV1 lease = new ServiceItemLeaseV1().withLeaseToken("1");
-        lease.setId("TestLease-" + UUID.randomUUID());
-
-        List<Lease> allLeases = Arrays.asList(lease);
-        Mockito.when(leaseContainerMock.getAllLeases()).thenReturn(Flux.fromIterable(allLeases));
-        Mockito
-            .when(partitionLoadBalancingStrategyMock.selectLeasesToTake(allLeases))
-            .thenThrow(new IllegalArgumentException("Something is wrong"));
-        Mockito.when(partitionControllerMock.shutdown()).thenReturn(Mono.empty());
-
-        PartitionLoadBalancerImpl partitionLoadBalancerImpl =
-            new PartitionLoadBalancerImpl(
-                partitionControllerMock,
-                leaseContainerMock,
-                partitionLoadBalancingStrategyMock,
-                Duration.ofSeconds(2),
-                Schedulers.boundedElastic()
-            );
-
-        partitionLoadBalancerImpl
-            .start()
-            .timeout(Duration.ofMillis(PARTITION_LOAD_BALANCER_TIMEOUT))
-            .subscribeOn(Schedulers.boundedElastic())
-            .subscribe();        Thread.sleep(Duration.ofSeconds(5).toMillis());
         Mockito.verify(leaseContainerMock, Mockito.atMost(3)).getAllLeases();
         partitionLoadBalancerImpl
             .stop()
--- sdk/cosmos/azure-cosmos-tests/src/test/java/com/azure/cosmos/implementation/changefeed/pkversion/PartitionLoadBalancerImplTests.java
+++ sdk/cosmos/azure-cosmos-tests/src/test/java/com/azure/cosmos/implementation/changefeed/pkversion/PartitionLoadBalancerImplTests.java
@@ -8,6 +8,7 @@ import com.azure.cosmos.implementation.changefeed.LeaseContainer;
 import com.azure.cosmos.implementation.changefeed.PartitionController;
 import com.azure.cosmos.implementation.changefeed.PartitionLoadBalancingStrategy;
 import org.mockito.Mockito;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -21,48 +22,17 @@ import java.util.UUID;
 public class PartitionLoadBalancerImplTests {
     private final int PARTITION_LOAD_BALANCER_TIMEOUT = 5000;
 
-    @Test(groups = "unit")
-    public void run_success() throws InterruptedException {
-        PartitionController partitionControllerMock = Mockito.mock(PartitionController.class);
-        LeaseContainer leaseContainerMock = Mockito.mock(LeaseContainer.class);
-        PartitionLoadBalancingStrategy partitionLoadBalancingStrategyMock = Mockito.mock(PartitionLoadBalancingStrategy.class);
-
-        ServiceItemLease lease = new ServiceItemLease().withLeaseToken("1");
-        lease.setId("TestLease-" + UUID.randomUUID());
-
-        List<Lease> allLeases = Arrays.asList(lease);
-        Mockito.when(leaseContainerMock.getAllLeases()).thenReturn(Flux.fromIterable(allLeases));
-        Mockito
-            .when(partitionLoadBalancingStrategyMock.selectLeasesToTake(allLeases))
-            .thenReturn(allLeases)
-            .thenReturn(Arrays.asList());
-        Mockito.when(partitionControllerMock.shutdown()).thenReturn(Mono.empty());
-
-        PartitionLoadBalancerImpl partitionLoadBalancerImpl =
-            new PartitionLoadBalancerImpl(
-                partitionControllerMock,
-                leaseContainerMock,
-                partitionLoadBalancingStrategyMock,
-                Duration.ofSeconds(2),
-                Schedulers.boundedElastic()
-            );
-
-        partitionLoadBalancerImpl
-            .start()
-            .timeout(Duration.ofMillis(PARTITION_LOAD_BALANCER_TIMEOUT))
-            .subscribeOn(Schedulers.boundedElastic())
-            .subscribe();
-        Thread.sleep(Duration.ofSeconds(5).toMillis());
-        Mockito.verify(leaseContainerMock, Mockito.atMost(3)).getAllLeases();
-        partitionLoadBalancerImpl
-            .stop()
-            .timeout(Duration.ofMillis(PARTITION_LOAD_BALANCER_TIMEOUT))
-            .subscribeOn(Schedulers.boundedElastic())
-            .subscribe();
+    @DataProvider(name = "loadBalancingSucceededArgProvider")
+    public static Object[][] loadBalancingSucceededArgProvider() {
+        return new Object[][]{
+            // load balancing call succeeded
+            { false },
+            { true }
+        };
     }
 
-    @Test(groups = "unit")
-    public void run_failure() throws InterruptedException {
+    @Test(groups = "unit", dataProvider = "loadBalancingSucceededArgProvider")
+    public void run(boolean loadBalancingSucceeded) throws InterruptedException {
         PartitionController partitionControllerMock = Mockito.mock(PartitionController.class);
         LeaseContainer leaseContainerMock = Mockito.mock(LeaseContainer.class);
         PartitionLoadBalancingStrategy partitionLoadBalancingStrategyMock = Mockito.mock(PartitionLoadBalancingStrategy.class);
@@ -72,9 +42,18 @@ public class PartitionLoadBalancerImplTests {
 
         List<Lease> allLeases = Arrays.asList(lease);
         Mockito.when(leaseContainerMock.getAllLeases()).thenReturn(Flux.fromIterable(allLeases));
-        Mockito
-            .when(partitionLoadBalancingStrategyMock.selectLeasesToTake(allLeases))
-            .thenThrow(new IllegalArgumentException("Something is wrong"));
+
+        if (loadBalancingSucceeded) {
+            Mockito
+                .when(partitionLoadBalancingStrategyMock.selectLeasesToTake(allLeases))
+                .thenReturn(allLeases)
+                .thenReturn(Arrays.asList());
+        } else {
+            Mockito
+                .when(partitionLoadBalancingStrategyMock.selectLeasesToTake(allLeases))
+                .thenThrow(new IllegalArgumentException("Something is wrong"));
+        }
+
         Mockito.when(partitionControllerMock.shutdown()).thenReturn(Mono.empty());
 
         PartitionLoadBalancerImpl partitionLoadBalancerImpl =
