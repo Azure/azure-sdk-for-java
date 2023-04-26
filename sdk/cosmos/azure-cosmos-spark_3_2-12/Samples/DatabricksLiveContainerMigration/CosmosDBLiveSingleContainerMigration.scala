20d1c0da6043d6682b8a59219ff76740c417bb6d
Theo van Kraay <theo.van@microsoft.com>
2023-04-26 16:28:10 +0100

    Update CosmosDBLiveSingleContainerMigration.scala

--- sdk/cosmos/azure-cosmos-spark_3_2-12/Samples/DatabricksLiveContainerMigration/CosmosDBLiveSingleContainerMigration.scala
+++ sdk/cosmos/azure-cosmos-spark_3_2-12/Samples/DatabricksLiveContainerMigration/CosmosDBLiveSingleContainerMigration.scala
@@ -3,17 +3,17 @@ dbutils.widgets.removeAll()
 
 // COMMAND ----------
 
-// global config
-dbutils.widgets.text("cosmosEndpoint", "") // enter the Cosmos DB Account URI
-dbutils.widgets.text("cosmosMasterKey", "") // enter the Cosmos DB Account PRIMARY KEY
-dbutils.widgets.text("cosmosRegion", "[UK South]") // enter the Cosmos DB Region
-
 // source config
+dbutils.widgets.text("cosmosSourceEndpoint", "") // enter the Cosmos DB Account URI of the source account
+dbutils.widgets.text("cosmosSourceMasterKey", "") // enter the Cosmos DB Account PRIMARY KEY of the source account
+dbutils.widgets.text("cosmosSourcePreferredRegions", "[UK South]") // enter the Cosmos DB Region of the source account
 dbutils.widgets.text("cosmosSourceDatabaseName", "") // enter the name of your source database
 dbutils.widgets.text("cosmosSourceContainerName", "") // enter the name of the container you want to migrate
-dbutils.widgets.text("cosmosSourceContainerThroughputControl", "0.95") // targetThroughputThreshold defines target percentage of available throughput you want the migration to use
+dbutils.widgets.text("cosmosSourceContainerThroughputControl", "0.95") // defines target percentage of available throughput you want the migration to use
 
 // target config
+dbutils.widgets.text("cosmosTargetEndpoint", "") // enter the Cosmos DB Account URI of the target account
+dbutils.widgets.text("cosmosTargetMasterKey", "") // enter the Cosmos DB Account PRIMARY KEY of the target account
 dbutils.widgets.text("cosmosTargetDatabaseName", "") // enter the name of your target database
 dbutils.widgets.text("cosmosTargetContainerName", "") // enter the name of the target container
 dbutils.widgets.text("cosmosTargetContainerPartitionKey", "/pk") // replace "pk" with partition key field used by the target container (keep forward slash "/" at start)
@@ -21,14 +21,15 @@ dbutils.widgets.text("cosmosTargetContainerProvisionedThroughput", "10000") // e
 
 // COMMAND ----------
 
-val cosmosEndpoint = dbutils.widgets.get("cosmosEndpoint")
-val cosmosMasterKey = dbutils.widgets.get("cosmosMasterKey")
-val cosmosRegion = dbutils.widgets.get("cosmosRegion")
-
+val cosmosSourceEndpoint = dbutils.widgets.get("cosmosSourceEndpoint")
+val cosmosSourceMasterKey = dbutils.widgets.get("cosmosSourceMasterKey")
+val cosmosSourcePreferredRegions = dbutils.widgets.get("cosmosSourcePreferredRegions")
 val cosmosSourceDatabaseName = dbutils.widgets.get("cosmosSourceDatabaseName")
 val cosmosSourceContainerName = dbutils.widgets.get("cosmosSourceContainerName") 
 val cosmosSourceContainerThroughputControl = dbutils.widgets.get("cosmosSourceContainerThroughputControl")
 
+val cosmosTargetEndpoint = dbutils.widgets.get("cosmosTargetEndpoint")
+val cosmosTargetMasterKey = dbutils.widgets.get("cosmosTargetMasterKey")
 val cosmosTargetDatabaseName = dbutils.widgets.get("cosmosTargetDatabaseName")
 val cosmosTargetContainerName = dbutils.widgets.get("cosmosTargetContainerName")
 val cosmosTargetContainerPartitionKey = dbutils.widgets.get("cosmosTargetContainerPartitionKey")
@@ -39,22 +40,25 @@ val cosmosTargetContainerProvisionedThroughput = dbutils.widgets.get("cosmosTarg
 import org.apache.spark.sql.SparkSession
 val spark = SparkSession
   .builder()
-  .config("spark.sql.catalog.cosmosCatalog", "com.azure.cosmos.spark.CosmosCatalog")
-  .config("spark.sql.catalog.cosmosCatalog.spark.cosmos.accountEndpoint", cosmosEndpoint)
-  .config("spark.sql.catalog.cosmosCatalog.spark.cosmos.accountKey", cosmosMasterKey)
+  .config("spark.sql.catalog.cosmosCatalogSrc", "com.azure.cosmos.spark.CosmosCatalog")
+  .config("spark.sql.catalog.cosmosCatalogSrc.spark.cosmos.accountEndpoint", cosmosSourceEndpoint)
+  .config("spark.sql.catalog.cosmosCatalogSrc.spark.cosmos.accountKey", cosmosSourceMasterKey)
+  .config("spark.sql.catalog.cosmosCatalogTgt", "com.azure.cosmos.spark.CosmosCatalog")
+  .config("spark.sql.catalog.cosmosCatalogTgt.spark.cosmos.accountEndpoint", cosmosTargetEndpoint)
+  .config("spark.sql.catalog.cosmosCatalogTgt.spark.cosmos.accountKey", cosmosTargetMasterKey)
   .getOrCreate()
 
 // COMMAND ----------
 
 var createThroughputControl = s"""
 /* NOTE: It is important to enable TTL (can be off/-1 by default) on the throughput control container */
-CREATE TABLE IF NOT EXISTS cosmosCatalog.${cosmosSourceDatabaseName}.ThroughputControl -- source database name - ThroughputControl table will be created there
+CREATE TABLE IF NOT EXISTS cosmosCatalogSrc.${cosmosSourceDatabaseName}.ThroughputControl -- source database name - ThroughputControl table will be created there
 USING cosmos.oltp
 OPTIONS(spark.cosmos.database = '${cosmosSourceDatabaseName}') -- reference source database. Do NOT change value partitionKeyPath = '/groupId' below - it must be named '/groupId' for Throughput control feature to work
 TBLPROPERTIES(partitionKeyPath = '/groupId', autoScaleMaxThroughput = '4000', indexingPolicy = 'AllProperties', defaultTtlInSeconds = '-1')
 """
 var createTargetResources= s"""
-CREATE TABLE IF NOT EXISTS cosmosCatalog.$cosmosTargetDatabaseName.$cosmosTargetContainerName -- reference target database and container - it will be created here
+CREATE TABLE IF NOT EXISTS cosmosCatalogTgt.$cosmosTargetDatabaseName.$cosmosTargetContainerName -- reference target database and container - it will be created here
 USING cosmos.oltp 
 TBLPROPERTIES(partitionKeyPath = '$cosmosTargetContainerPartitionKey', autoScaleMaxThroughput = $cosmosTargetContainerProvisionedThroughput, indexingPolicy = 'OnlySystemProperties')
 """
@@ -63,9 +67,9 @@ spark.sql(createTargetResources)
 
 // COMMAND ----------
 
-  val changeFeedCfg = Map("spark.cosmos.accountEndpoint" -> cosmosEndpoint,
+  val changeFeedCfg = Map("spark.cosmos.accountEndpoint" -> cosmosSourceEndpoint,
     "spark.cosmos.applicationName" -> "LiveMigrationRead_",
-    "spark.cosmos.accountKey" -> cosmosMasterKey,
+    "spark.cosmos.accountKey" -> cosmosSourceMasterKey,
     "spark.cosmos.database" -> cosmosSourceDatabaseName,
     "spark.cosmos.container" -> cosmosSourceContainerName,
     "spark.cosmos.read.partitioning.strategy" -> "Default",
@@ -78,15 +82,15 @@ spark.sql(createTargetResources)
     "spark.cosmos.throughputControl.targetThroughputThreshold" -> cosmosSourceContainerThroughputControl, 
     "spark.cosmos.throughputControl.globalControl.database" -> cosmosSourceDatabaseName,
     "spark.cosmos.throughputControl.globalControl.container" -> "ThroughputControl",
-    "spark.cosmos.preferredRegionsList" -> cosmosRegion
+    "spark.cosmos.preferredRegionsList" -> cosmosSourcePreferredRegions
   )
 
   //when running this notebook is stopped (or if a problem causes a crash) change feed processing will be picked up from last processed document
   //if you want to start from beginning, delete this folder or change checkpointLocation value
   val checkpointLocation = "/tmp/LiveMigration_checkpoint"
 
-  val writeCfg = Map("spark.cosmos.accountEndpoint" -> cosmosEndpoint,
-    "spark.cosmos.accountKey" -> cosmosMasterKey,
+  val writeCfg = Map("spark.cosmos.accountEndpoint" -> cosmosTargetEndpoint,
+    "spark.cosmos.accountKey" -> cosmosTargetMasterKey,
     "spark.cosmos.applicationName" -> "LivemigrationWrite_",                     
     "spark.cosmos.database" -> cosmosTargetDatabaseName,
     "spark.cosmos.container" -> cosmosTargetContainerName,
