f56b2a6ad62c8707beefd1d9a7d9edf7388bda72
Shawn Fang <45607042+mssfang@users.noreply.github.com>
2023-04-28 13:23:47 -0700

    Update sdk/openai/azure-ai-openai/src/samples/java/com/azure/ai/openai/GetEmbeddingsAsync.java
    Co-authored-by: Srikanta <51379715+srnagar@users.noreply.github.com>

--- sdk/openai/azure-ai-openai/src/samples/java/com/azure/ai/openai/GetEmbeddingsAsync.java
+++ sdk/openai/azure-ai-openai/src/samples/java/com/azure/ai/openai/GetEmbeddingsAsync.java
@@ -23,7 +23,7 @@ public class GetEmbeddingsAsync {
     public static void main(String[] args) throws InterruptedException {
         String azureOpenaiKey = "{azure-open-ai-key}";
         String endpoint = "{azure-open-ai-endpoint}";
-        String deploymentOrModelId = "{azure_open_ai_deployment_model_id}";
+        String deploymentOrModelId = "{azure-open-ai-deployment-model-id}";
 
         OpenAIAsyncClient client = new OpenAIClientBuilder()
             .endpoint(endpoint)
