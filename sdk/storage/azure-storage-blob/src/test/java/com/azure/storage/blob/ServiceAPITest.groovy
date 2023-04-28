d8615d84dec5d597b86ed44e4f76608f54696cb0
Rabab <ibrahimr@microsoft.com>
2023-04-28 12:51:11 -0700

    removing paralleltransferoptions

--- sdk/storage/azure-storage-blob/src/test/java/com/azure/storage/blob/ServiceAPITest.groovy
+++ sdk/storage/azure-storage-blob/src/test/java/com/azure/storage/blob/ServiceAPITest.groovy
@@ -1224,7 +1224,6 @@ class ServiceAPITest extends APISpec {
         // testing with large dataset that is guaranteed to take longer than the specified timeout (1 second)
         def randomData = getRandomByteArray(256 * Constants.MB)
         def input = new ByteArrayInputStream(randomData)
-//        def pto = new ParallelTransferOptions().setMaxSingleUploadSizeLong(20 * Constants.MB)
 
         when:
         blobClient.uploadWithResponse(new BlobParallelUploadOptions(input), null, null)
