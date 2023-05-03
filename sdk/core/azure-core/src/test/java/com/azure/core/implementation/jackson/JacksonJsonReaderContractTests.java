564ed4960a4bd2175c20d6b152d1bffd44280c5a
Alan Zimmer <48699787+alzimmermsft@users.noreply.github.com>
2023-05-03 12:15:21 -0400

    Add JsonSerializable Support to Azure Core JacksonAdapter (#34742)
    Add JsonSerializable Handling into Core JacksonAdapter

--- sdk/core/azure-core/src/test/java/com/azure/core/implementation/jackson/JacksonJsonReaderContractTests.java
+++ sdk/core/azure-core/src/test/java/com/azure/core/implementation/jackson/JacksonJsonReaderContractTests.java
@@ -1,9 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.azure.core.serializer.json.gson.implementation;
+package com.azure.core.implementation.jackson;
 
-import com.azure.json.JsonOptions;
 import com.azure.json.JsonReader;
 import com.azure.json.contract.JsonReaderContractTests;
 import org.junit.jupiter.api.AfterEach;
@@ -11,14 +10,14 @@ import org.junit.jupiter.api.AfterEach;
 import java.io.IOException;
 
 /**
- * Tests {@link GsonJsonReader} against the contract required by {@link JsonReader}.
+ * Tests {@link JacksonJsonReader} against the contract required by {@link JsonReader}.
  */
-public class GsonJsonReaderContractTests extends JsonReaderContractTests {
+public class JacksonJsonReaderContractTests extends JsonReaderContractTests {
     private JsonReader reader;
 
     @Override
     public JsonReader getJsonReader(String json) throws IOException {
-        this.reader = AzureJsonUtils.createReader(json, new JsonOptions());
+        this.reader = AzureJsonUtils.createReader(json, null);
         return reader;
     }
 
--- sdk/core/azure-core/src/test/java/com/azure/core/implementation/jackson/JacksonJsonWriterContractTests.java
+++ sdk/core/azure-core/src/test/java/com/azure/core/implementation/jackson/JacksonJsonWriterContractTests.java
@@ -1,11 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.azure.json.implementation;
+package com.azure.core.implementation.jackson;
 
-import com.azure.json.JsonOptions;
 import com.azure.json.JsonWriter;
 import com.azure.json.contract.JsonWriterContractTests;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.json.JsonWriteFeature;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
@@ -14,16 +15,19 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 /**
- * Tests {@link DefaultJsonWriter} against the contract required by {@link JsonWriter}.
+ * Tests {@link JacksonJsonWriter} against the contract required by {@link JsonWriter}.
  */
-public class DefaultJsonWriterContractTests extends JsonWriterContractTests {
+public class JacksonJsonWriterContractTests extends JsonWriterContractTests {
+    private static final JsonFactory FACTORY = JsonFactory.builder().build();
+
     private ByteArrayOutputStream outputStream;
     private JsonWriter writer;
 
     @BeforeEach
     public void beforeEach() throws IOException {
         this.outputStream = new ByteArrayOutputStream();
-        this.writer = DefaultJsonWriter.toStream(outputStream, new JsonOptions());
+        this.writer = new JacksonJsonWriter(FACTORY.createGenerator(outputStream)
+            .configure(JsonWriteFeature.WRITE_NAN_AS_STRINGS.mappedFeature(), true));
     }
 
     @AfterEach
