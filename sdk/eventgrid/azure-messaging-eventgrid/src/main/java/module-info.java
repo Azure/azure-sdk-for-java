c33164841ff1335182e5245d3c441111aae0a93b
Bill Wert <billwert@microsoft.com>
2023-05-04 23:07:00 -0700

    builds

--- sdk/eventgrid/azure-messaging-eventgrid/src/main/java/module-info.java
+++ sdk/eventgrid/azure-messaging-eventgrid/src/main/java/module-info.java
@@ -4,9 +4,14 @@
 
 module com.azure.messaging.eventgrid {
     requires transitive com.azure.core;
-    exports com.azure.messaging.eventgrid;
-    exports com.azure.messaging.eventgrid.systemevents;
 
-    opens com.azure.messaging.eventgrid.implementation.models to com.fasterxml.jackson.databind;
-    opens com.azure.messaging.eventgrid.systemevents to com.fasterxml.jackson.databind;
+    exports com.azure.messaging.eventgrid;
+    exports com.azure.messaging.eventgrid.models;
+
+    opens com.azure.messaging.eventgrid to
+            com.azure.core,
+            com.fasterxml.jackson.databind;
+    opens com.azure.messaging.eventgrid.models to
+            com.azure.core,
+            com.fasterxml.jackson.databind;
 }
