cb96d56aca76113be39a7d7ea9bb29262d8b9fb5
Liudmila Molkova <limolkova@microsoft.com>
2023-05-09 17:38:04 -0700

    Follow up on logging tracer provider comments from #34659

--- sdk/core/azure-core-experimental/src/test/java/com/azure/core/experimental/util/tracing/LoggingTracerTests.java
+++ sdk/core/azure-core-experimental/src/test/java/com/azure/core/experimental/util/tracing/LoggingTracerTests.java
@@ -4,10 +4,15 @@
 package com.azure.core.experimental.util.tracing;
 
 import com.azure.core.util.Context;
+import com.azure.core.util.tracing.SpanKind;
+import com.azure.core.util.tracing.StartSpanOptions;
 import com.azure.core.util.tracing.Tracer;
 import com.azure.core.util.tracing.TracerProvider;
+import com.azure.core.util.tracing.TracingLink;
 import org.junit.jupiter.api.Test;
 
+import java.time.Instant;
+import java.util.Collections;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -58,8 +63,29 @@ public class LoggingTracerTests {
             return "00-abcdef0123456789abcdef0123456789-abcdef0123456789-01";
         });
 
-        LoggingTracerProvider.LoggingSpan span = (LoggingTracerProvider.LoggingSpan) spanCtx.getData("span").get();
-        assertEquals("abcdef0123456789abcdef0123456789", span.getTraceId());
+        assertEquals("abcdef0123456789abcdef0123456789", spanCtx.getData("traceId").get());
+        assertEquals("abcdef0123456789", spanCtx.getData("spanId").get());
+        tracer.end(null, null, spanCtx);
+    }
+
+    @Test
+    public void tracingWithLinks() {
+        Tracer tracer = TracerProvider.getDefaultProvider().createTracer("test", null, null,
+            new LoggingTracerProvider.LoggingTracingOptions());
+
+        TracingLink link1 = new TracingLink(new Context("traceId", "00000000000000000000000000000001").addData("spanId", "0000000000000001"),
+            Collections.singletonMap("foo", "bar"));
+        TracingLink link2 = new TracingLink(new Context("traceId", "20000000000000000000000000000000").addData("spanId", "2000000000000000"));
+
+        StartSpanOptions startOptions = new StartSpanOptions(SpanKind.CONSUMER)
+            .setStartTimestamp(Instant.ofEpochSecond(42))
+            .addLink(link1)
+            .addLink(link2);
+
+        LoggingTracerProvider.LoggingSpan parent = new LoggingTracerProvider.LoggingSpan("parent", SpanKind.SERVER, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "bbbbbbbbbbbbbbbb");
+        Context spanCtx = tracer.start("test", startOptions, new Context("span", parent));
+        Object span = spanCtx.getData("span").get();
+        assertInstanceOf(LoggingTracerProvider.LoggingSpan.class, span);
         tracer.end(null, null, spanCtx);
     }
 }
