1fd098f988560e5b976114c792ea79c28affda8e
Matt Metcalf <mrm9084@gmail.com>
2023-05-09 23:58:03 -0700

    Undo Async change

--- sdk/spring/spring-cloud-azure-appconfiguration-config/src/main/java/com/azure/spring/cloud/appconfiguration/config/AppConfigurationRefresh.java
+++ sdk/spring/spring-cloud-azure-appconfiguration-config/src/main/java/com/azure/spring/cloud/appconfiguration/config/AppConfigurationRefresh.java
@@ -5,6 +5,7 @@ package com.azure.spring.cloud.appconfiguration.config;
 import java.util.Map;
 
 import org.springframework.context.ApplicationEventPublisherAware;
+import org.springframework.scheduling.annotation.Async;
 
 import reactor.core.publisher.Mono;
 
@@ -19,6 +20,7 @@ public interface AppConfigurationRefresh extends ApplicationEventPublisherAware 
      * @return Mono with a boolean of if a RefreshEvent was published. If refreshConfigurations is currently being run
      * elsewhere this method will return right away as <b>false</b>.
      */
+    @Async
     Mono<Boolean> refreshConfigurations();
 
     /**
