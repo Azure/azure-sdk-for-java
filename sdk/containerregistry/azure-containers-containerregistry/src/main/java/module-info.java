0262f83230464e35c40a26b7b1adaa72c004b2d3
Liudmila Molkova <limolkova@microsoft.com>
2023-05-10 09:41:44 -0700

    and core

--- sdk/containerregistry/azure-containers-containerregistry/src/main/java/module-info.java
+++ sdk/containerregistry/azure-containers-containerregistry/src/main/java/module-info.java
@@ -10,6 +10,4 @@ module com.azure.containers.containerregistry {
 
     opens com.azure.containers.containerregistry.implementation to com.azure.core;
     opens com.azure.containers.containerregistry.implementation.models to com.azure.core;
-
-    opens com.azure.containers.containerregistry.models to com.azure.core;
 }
