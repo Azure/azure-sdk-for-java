415abb71c1a3d78c004fb0cf5ff34a77e0095a96
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2023-05-12 11:14:37 -0400

    Use OutputStream when writing body to channel

--- sdk/core/azure-core-http-netty/src/main/java/com/azure/core/http/netty/implementation/NettyAsyncHttpResponse.java
+++ sdk/core/azure-core-http-netty/src/main/java/com/azure/core/http/netty/implementation/NettyAsyncHttpResponse.java
@@ -6,7 +6,6 @@ package com.azure.core.http.netty.implementation;
 import com.azure.core.http.HttpHeaderName;
 import com.azure.core.http.HttpRequest;
 import com.azure.core.util.CoreUtils;
-import reactor.core.Exceptions;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
@@ -16,8 +15,10 @@ import reactor.netty.http.client.HttpClientResponse;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousByteChannel;
+import java.nio.channels.Channels;
 import java.nio.channels.WritableByteChannel;
 import java.nio.charset.Charset;
 
@@ -68,9 +69,7 @@ public final class NettyAsyncHttpResponse extends NettyAsyncHttpResponseBase {
 
     @Override
     public Mono<Void> writeBodyToAsync(AsynchronousByteChannel channel) {
-        return Mono.<Void>create(sink -> bodyIntern().retain()
-            .subscribe(new ByteBufFluxAsyncWriteSubscriber(channel, sink)))
-            .doFinally(ignored -> close());
+        return writeBodyTo(Channels.newOutputStream(channel));
     }
 
     @Override
@@ -90,20 +89,36 @@ public final class NettyAsyncHttpResponse extends NettyAsyncHttpResponseBase {
         // complete. This introduces a previously seen, but in a different flavor, race condition where the write
         // operation gets scheduled on one thread and the ByteBuf release happens on another, leaving the write
         // operation racing to complete before the release happens. With all that said, leave this as subscribeOn.
-        bodyIntern().subscribeOn(Schedulers.boundedElastic())
-            .map(nettyBuffer -> {
+        writeBodyTo(Channels.newOutputStream(channel)).block();
+    }
+
+    private Mono<Void> writeBodyTo(OutputStream outputStream) {
+        // Since this uses a synchronous write this doesn't need to retain the ByteBuf as the async version does.
+        // In fact, if retain is used here and there is a cancellation or exception during writing this will likely
+        // leak ByteBufs as it doesn't have the asynchronous stream handling to properly close them when errors happen
+        // during writing.
+        //
+        // This also must use subscribeOn rather than publishOn as subscribeOn will have the entire asynchronous chain
+        // run on the same subscriber where publishOn only affects reactive operations after the publishOn. While this
+        // doesn't seem like it would make much of a difference as this entire call will be blocked it fixes a race
+        // condition. reactor-netty's inbound receive uses a ByteBuf pool to handle the network response and the
+        // returned ByteBufFlux is configured to release the ByteBuf back to the pool when the onNext operation
+        // completes. Unfortunately, when publishOn is used each ByteBuf must be scheduled in the publisher thread using
+        // a future and the publishOn onNext handler will complete once that operation is scheduled, not when it's
+        // complete. This introduces a previously seen, but in a different flavor, race condition where the write
+        // operation gets scheduled on one thread and the ByteBuf release happens on another, leaving the write
+        // operation racing to complete before the release happens. With all that said, leave this as subscribeOn.
+        return bodyIntern().subscribeOn(Schedulers.boundedElastic())
+            .flatMap(byteBuf -> {
                 try {
-                    ByteBuffer nioBuffer = nettyBuffer.nioBuffer();
-                    while (nioBuffer.hasRemaining()) {
-                        channel.write(nioBuffer);
-                    }
-                    return nettyBuffer;
-                } catch (IOException e) {
-                    throw Exceptions.propagate(e);
+                    byteBuf.getBytes(0, outputStream, byteBuf.readableBytes());
+                    return Mono.empty();
+                } catch (IOException ex) {
+                    return Mono.error(ex);
                 }
             })
             .doFinally(ignored -> close())
-            .then().block();
+            .then();
     }
 
     @Override
