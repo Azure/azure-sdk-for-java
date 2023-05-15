e7459b32659a866458a2c8142e8094ea44563ee7
Hong Li(MSFT) <v-hongli1@microsoft.com>
2023-05-15 12:19:23 +0800

    mgmt, local generation for network

--- sdk/resourcemanager/azure-resourcemanager-compute/src/test/java/com/azure/resourcemanager/compute/SharedGalleryImageTests.java
+++ sdk/resourcemanager/azure-resourcemanager-compute/src/test/java/com/azure/resourcemanager/compute/SharedGalleryImageTests.java
@@ -124,11 +124,10 @@ public class SharedGalleryImageTests extends ComputeManagementTest {
                 // Options - End
                 .create();
 
-        System.out.println(galleryImage.location());
-        System.out.println(region.toString());
+        Assertions.fail(galleryImage.location());
         Assertions.assertNotNull(galleryImage);
         Assertions.assertNotNull(galleryImage.innerModel());
-        Assertions.assertTrue(Region.fromName(galleryImage.location()).name().equalsIgnoreCase(region.name()));
+        Assertions.assertTrue(galleryImage.location().equalsIgnoreCase(region.toString()));
         Assertions.assertTrue(galleryImage.osType().equals(OperatingSystemTypes.WINDOWS));
         Assertions.assertTrue(galleryImage.osState().equals(OperatingSystemStateTypes.GENERALIZED));
         Assertions.assertEquals(2, galleryImage.unsupportedDiskTypes().size());
