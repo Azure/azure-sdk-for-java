0545144d0e63df4f594b6140813bdd8685fc6b08
Muyao <v-muyaofeng@microsoft.com>
2023-05-16 18:01:42 +0800

    test

--- sdk/spring/scripts/sync_for_spring3.py
+++ sdk/spring/scripts/sync_for_spring3.py
@@ -28,6 +28,7 @@ from log import log
 
 EXTERNAL_DEPENDENCIES_FILE = 'eng/versioning/external_dependencies.txt'
 SB3_EXTERNAL_DEPENDENCIES_FILE_URL = 'https://raw.githubusercontent.com/Azure/azure-sdk-for-java/feature/spring-boot-3/eng/versioning/external_dependencies.txt'
+OUTPUT_FILE = 'sdk/spring/scripts/conflict_versions.txt'
 SKIP_IDS = [
     'org.eclipse.jgit:org.eclipse.jgit'  # Refs: https://github.com/Azure/azure-sdk-for-java/pull/13956/files#r468368271
 ]
@@ -88,6 +89,7 @@ def three_way_merge(ver_local, ver_remote, ver_sb3_managed):
     conflicting_set = []
     local_missing_set = []
     remote_missing_set = []
+    resolve_dict = {}
     total_keys = ver_remote.keys() | ver_local.keys()
     for k in total_keys:
         if k not in ver_local:
@@ -100,9 +102,15 @@ def three_way_merge(ver_local, ver_remote, ver_sb3_managed):
         conflicting_set.sort()
         len_ver, len_id = cal_len_for_logging_format(conflicting_set, ver_local, ver_remote)
         for k in conflicting_set:
-            log.warn("Conflicting version found for [{:<{}}], local is [{:<{}}], but remote is [{:<{}}], the sb3 managed version is [{:<{}}]"
-                     .format(k, len_id, ver_local[k], len_ver, ver_remote[k], len_ver, ver_sb3_managed.get(k, 'N/A'), len_ver))
-        log.warn("")
+            v_local = ver_local[k]
+            v_remote = ver_remote[k]
+            v_sb3 = ver_sb3_managed.get(k, 'N/A')
+            if v_sb3 != 'N/A' and v_remote == v_sb3 and version_bigger_than(v_remote, v_local):
+                resolve_dict[k] = v_remote
+            else:
+                log.warn("Conflicting version found for [{:<{}}], local is [{:<{}}], but remote is [{:<{}}], the sb3 managed version is [{:<{}}]"
+                         .format(k, len_id, ver_local[k], len_ver, ver_remote[k], len_ver, ver_sb3_managed.get(k, 'N/A'), len_ver))
+                log.warn("")
     if local_missing_set:
         local_missing_set.sort()
         len_ver, len_id = cal_len_for_logging_format(local_missing_set, ver_sb3_managed, ver_remote)
@@ -121,6 +129,10 @@ def three_way_merge(ver_local, ver_remote, ver_sb3_managed):
         for k in remote_missing_set:
             log.warn("Entry existing in local but not remote found for [{:<{}}]".format(k, len_id))
         log.warn("")
+    output_file = open(OUTPUT_FILE, 'w''')
+    for key, value in sorted(resolve_dict.items()):
+        output_file.write('{};{}\n'.format(key, value))
+    output_file.close()
     return local_missing_set, conflicting_set, remote_missing_set
 
 
