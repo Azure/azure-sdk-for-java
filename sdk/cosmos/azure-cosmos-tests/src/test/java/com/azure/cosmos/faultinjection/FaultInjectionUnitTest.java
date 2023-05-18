e1452f223eb7892c817c4e31d1c003f941d90fd1
annie-mac <xinlian@microsoft.com>
2023-05-17 22:06:23 -0700

    refactor

--- sdk/cosmos/azure-cosmos-tests/src/test/java/com/azure/cosmos/faultinjection/FaultInjectionUnitTest.java
+++ sdk/cosmos/azure-cosmos-tests/src/test/java/com/azure/cosmos/faultinjection/FaultInjectionUnitTest.java
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-package com.azure.cosmos.test;
+package com.azure.cosmos.faultinjection;
 
 import com.azure.cosmos.test.faultinjection.FaultInjectionCondition;
 import com.azure.cosmos.test.faultinjection.FaultInjectionConditionBuilder;
@@ -17,7 +17,7 @@ import java.time.Duration;
 
 public class FaultInjectionUnitTest {
 
-    @Test
+    @Test(groups = "unit")
     public void testFaultInjectionBuilder() {
         String ruleId = "rule_id_1";
         FaultInjectionCondition faultInjectionCondition = new FaultInjectionConditionBuilder()
--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/directconnectivity/RntbdTransportClient.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/directconnectivity/RntbdTransportClient.java
@@ -369,10 +369,10 @@ public class RntbdTransportClient extends TransportClient {
             // But the stated behavior may change in later versions (https://github.com/reactor/reactor-core/issues/3235).
             // In order to keep consistent behavior, we internally will always cancel the future.
             //
-            // We should only record cancellation diagnostics if the signal is cancelled and the record is not done yet
+            // Any of the reactor operators can terminate with a cancel signal instead of error signal
+            // For example collectList, Flux.merge, takeUntil
+            // We should only record cancellation diagnostics if the signal is cancelled and the record is cancelled
             if (record.isCancelled()) {
-                // Any of the reactor operators can terminate with a cancel signal instead of error signal
-                // For example collectList, Flux.merge, takeUntil
                 record.cancel(true);
 
                 // When the request got cancelled, in order to capture the details in the diagnostics, fake a OperationCancelledException
