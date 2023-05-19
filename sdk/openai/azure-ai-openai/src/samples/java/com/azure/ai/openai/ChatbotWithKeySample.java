f0f6c01e2e6c57e5947b449acd6c77aa1c220f85
brandom-msft <brandom@microsoft.com>
2023-05-19 14:42:09 -0700

    Revert "removing unreferenced file"
    This reverts commit 58e175ad4b01dedf8885556997c6d1c9e60ba44d.

--- sdk/openai/azure-ai-openai/src/samples/java/com/azure/ai/openai/ChatbotWithKeySample.java
+++ sdk/openai/azure-ai-openai/src/samples/java/com/azure/ai/openai/ChatbotWithKeySample.java
@@ -38,7 +38,7 @@ public class ChatbotWithKeySample {
             }
         }
 
-        // General example of how to get usage info
+        // Genearl example of how to get usage info
         CompletionsUsage usage = completions.getUsage();
         System.out.printf("Usage: number of prompt token is %d, "
                 + "number of completion token is %d, and number of total tokens in request and response is %d.%n",
