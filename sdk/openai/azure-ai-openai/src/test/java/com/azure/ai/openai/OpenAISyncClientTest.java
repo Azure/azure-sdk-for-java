837fef32496e4aea0adac608c0d9c762be575985
Jose Alvarez <jpalvarezl@users.noreply.github.com>
2023-05-30 16:12:10 -0700

    Azure OpenAI adjusting unit tests (#35200)

--- sdk/openai/azure-ai-openai/src/test/java/com/azure/ai/openai/OpenAISyncClientTest.java
+++ sdk/openai/azure-ai-openai/src/test/java/com/azure/ai/openai/OpenAISyncClientTest.java
@@ -5,10 +5,11 @@ package com.azure.ai.openai;
 
 import com.azure.ai.openai.models.ChatCompletions;
 import com.azure.ai.openai.models.ChatCompletionsOptions;
-import com.azure.ai.openai.models.ChatRole;
 import com.azure.ai.openai.models.Completions;
 import com.azure.ai.openai.models.CompletionsOptions;
+import com.azure.ai.openai.models.CompletionsUsage;
 import com.azure.ai.openai.models.Embeddings;
+import com.azure.core.exception.ResourceNotFoundException;
 import com.azure.core.http.HttpClient;
 import com.azure.core.http.rest.RequestOptions;
 import com.azure.core.http.rest.Response;
@@ -19,10 +20,11 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import static com.azure.ai.openai.TestUtils.DISPLAY_NAME_WITH_ARGUMENTS;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class OpenAIClientTest extends OpenAIClientTestBase {
+public class OpenAISyncClientTest extends OpenAIClientTestBase {
     private OpenAIClient client;
 
     private OpenAIClient getOpenAIClient(HttpClient httpClient, OpenAIServiceVersion serviceVersion) {
@@ -33,11 +35,11 @@ public class OpenAIClientTest extends OpenAIClientTestBase {
 
     @ParameterizedTest(name = DISPLAY_NAME_WITH_ARGUMENTS)
     @MethodSource("com.azure.ai.openai.TestUtils#getTestParameters")
-    public void getCompletions(HttpClient httpClient, OpenAIServiceVersion serviceVersion) {
+    public void testGetCompletions(HttpClient httpClient, OpenAIServiceVersion serviceVersion) {
         client = getOpenAIClient(httpClient, serviceVersion);
         getCompletionsRunner((deploymentId, prompt) -> {
             Completions resultCompletions = client.getCompletions(deploymentId, new CompletionsOptions(prompt));
-            assertCompletions(new int[]{0}, null, null, resultCompletions);
+            assertCompletions(1, resultCompletions);
         });
     }
 
@@ -47,45 +49,85 @@ public class OpenAIClientTest extends OpenAIClientTestBase {
         client = getOpenAIClient(httpClient, serviceVersion);
         getCompletionsRunner((deploymentId, prompt) -> {
             IterableStream<Completions> resultCompletions = client.getCompletionsStream(deploymentId, new CompletionsOptions(prompt));
-            resultCompletions.forEach(completions -> {
-                assertNotNull(completions.getId());
-                assertNotNull(completions.getChoices());
-                assertFalse(completions.getChoices().isEmpty());
-                assertNotNull(completions.getChoices().get(0).getText());
-            });
+            assertTrue(resultCompletions.stream().toArray().length > 1);
+            resultCompletions.forEach(OpenAIClientTestBase::assertCompletionsStream);
         });
     }
 
     @ParameterizedTest(name = DISPLAY_NAME_WITH_ARGUMENTS)
     @MethodSource("com.azure.ai.openai.TestUtils#getTestParameters")
-    public void getCompletionsFromPrompt(HttpClient httpClient, OpenAIServiceVersion serviceVersion) {
+    public void testGetCompletionsFromPrompt(HttpClient httpClient, OpenAIServiceVersion serviceVersion) {
         client = getOpenAIClient(httpClient, serviceVersion);
         getCompletionsFromSinglePromptRunner((deploymentId, prompts) -> {
             Completions completions = client.getCompletions(deploymentId, prompts);
-            assertCompletions(new int[]{0}, null, null, completions);
+            assertCompletions(1, completions);
         });
     }
 
     @ParameterizedTest(name = DISPLAY_NAME_WITH_ARGUMENTS)
     @MethodSource("com.azure.ai.openai.TestUtils#getTestParameters")
-    public void getCompletionsWithResponse(HttpClient httpClient, OpenAIServiceVersion serviceVersion) {
+    public void testGetCompletionsWithResponse(HttpClient httpClient, OpenAIServiceVersion serviceVersion) {
         client = getOpenAIClient(httpClient, serviceVersion);
         getCompletionsRunner((deploymentId, prompt) -> {
             Response<BinaryData> response = client.getCompletionsWithResponse(deploymentId,
                 BinaryData.fromObject(new CompletionsOptions(prompt)), new RequestOptions());
-            assertEquals(200, response.getStatusCode());
-            Completions resultCompletions = response.getValue().toObject(Completions.class);
-            assertCompletions(new int[]{0}, null, null, resultCompletions);
+            Completions resultCompletions = assertAndGetValueFromResponse(response, Completions.class, 200);
+            assertCompletions(1, resultCompletions);
         });
     }
 
     @ParameterizedTest(name = DISPLAY_NAME_WITH_ARGUMENTS)
     @MethodSource("com.azure.ai.openai.TestUtils#getTestParameters")
-    public void getChatCompletions(HttpClient httpClient, OpenAIServiceVersion serviceVersion) {
+    public void testGetCompletionsWithResponseBadDeployment(HttpClient httpClient, OpenAIServiceVersion serviceVersion) {
+        client = getOpenAIClient(httpClient, serviceVersion);
+        getCompletionsRunner((_deploymentId, prompt) -> {
+            String deploymentId = "BAD_DEPLOYMENT_ID";
+            ResourceNotFoundException exception = assertThrows(ResourceNotFoundException.class,
+                () -> client.getCompletionsWithResponse(deploymentId,
+                BinaryData.fromObject(new CompletionsOptions(prompt)), new RequestOptions()));
+            assertEquals(404, exception.getResponse().getStatusCode());
+        });
+    }
+
+    @ParameterizedTest(name = DISPLAY_NAME_WITH_ARGUMENTS)
+    @MethodSource("com.azure.ai.openai.TestUtils#getTestParameters")
+    public void testGetCompletionsUsageField(HttpClient httpClient, OpenAIServiceVersion serviceVersion) {
+        client = getOpenAIClient(httpClient, serviceVersion);
+        getCompletionsRunner((modelId, prompt) -> {
+            CompletionsOptions completionsOptions = new CompletionsOptions(prompt);
+            completionsOptions.setMaxTokens(1024);
+            completionsOptions.setN(3);
+            completionsOptions.setLogprobs(1);
+
+            Completions resultCompletions = client.getCompletions(modelId, completionsOptions);
+
+            CompletionsUsage usage = resultCompletions.getUsage();
+            assertCompletions(completionsOptions.getN() * completionsOptions.getPrompt().size(), resultCompletions);
+            assertNotNull(usage);
+            assertTrue(usage.getTotalTokens() > 0);
+            assertEquals(usage.getCompletionTokens() + usage.getPromptTokens(), usage.getTotalTokens());
+        });
+    }
+
+    @ParameterizedTest(name = DISPLAY_NAME_WITH_ARGUMENTS)
+    @MethodSource("com.azure.ai.openai.TestUtils#getTestParameters")
+    public void testGetCompletionsTokenCutoff(HttpClient httpClient, OpenAIServiceVersion serviceVersion) {
+        client = getOpenAIClient(httpClient, serviceVersion);
+        getCompletionsRunner((modelId, prompt) -> {
+            CompletionsOptions completionsOptions = new CompletionsOptions(prompt);
+            completionsOptions.setMaxTokens(3);
+            Completions resultCompletions = client.getCompletions(modelId, completionsOptions);
+            assertCompletions(1, "length", resultCompletions);
+        });
+    }
+
+    @ParameterizedTest(name = DISPLAY_NAME_WITH_ARGUMENTS)
+    @MethodSource("com.azure.ai.openai.TestUtils#getTestParameters")
+    public void testGetChatCompletions(HttpClient httpClient, OpenAIServiceVersion serviceVersion) {
         client = getOpenAIClient(httpClient, serviceVersion);
         getChatCompletionsRunner((deploymentId, chatMessages) -> {
             ChatCompletions resultChatCompletions = client.getChatCompletions(deploymentId, new ChatCompletionsOptions(chatMessages));
-            assertChatCompletions(new int[]{0}, new ChatRole[]{ChatRole.ASSISTANT}, resultChatCompletions);
+            assertChatCompletions(1, resultChatCompletions);
         });
     }
 
@@ -95,31 +137,26 @@ public class OpenAIClientTest extends OpenAIClientTestBase {
         client = getOpenAIClient(httpClient, serviceVersion);
         getChatCompletionsRunner((deploymentId, chatMessages) -> {
             IterableStream<ChatCompletions> resultChatCompletions = client.getChatCompletionsStream(deploymentId, new ChatCompletionsOptions(chatMessages));
-            resultChatCompletions.forEach(chatCompletions -> {
-                assertNotNull(chatCompletions.getId());
-                assertNotNull(chatCompletions.getChoices());
-                assertFalse(chatCompletions.getChoices().isEmpty());
-                assertNotNull(chatCompletions.getChoices().get(0).getDelta());
-            });
+            assertTrue(resultChatCompletions.stream().toArray().length > 1);
+            resultChatCompletions.forEach(OpenAIClientTestBase::assertChatCompletionsStream);
         });
     }
 
     @ParameterizedTest(name = DISPLAY_NAME_WITH_ARGUMENTS)
     @MethodSource("com.azure.ai.openai.TestUtils#getTestParameters")
-    public void getChatCompletionsWithResponse(HttpClient httpClient, OpenAIServiceVersion serviceVersion) {
+    public void testGetChatCompletionsWithResponse(HttpClient httpClient, OpenAIServiceVersion serviceVersion) {
         client = getOpenAIClient(httpClient, serviceVersion);
         getChatCompletionsRunner((deploymentId, chatMessages) -> {
             Response<BinaryData> response = client.getChatCompletionsWithResponse(deploymentId,
                 BinaryData.fromObject(new ChatCompletionsOptions(chatMessages)), new RequestOptions());
-            assertEquals(200, response.getStatusCode());
-            ChatCompletions resultChatCompletions = response.getValue().toObject(ChatCompletions.class);
-            assertChatCompletions(new int[]{0}, new ChatRole[]{ChatRole.ASSISTANT}, resultChatCompletions);
+            ChatCompletions resultChatCompletions = assertAndGetValueFromResponse(response, ChatCompletions.class, 200);
+            assertChatCompletions(1, resultChatCompletions);
         });
     }
 
     @ParameterizedTest(name = DISPLAY_NAME_WITH_ARGUMENTS)
     @MethodSource("com.azure.ai.openai.TestUtils#getTestParameters")
-    public void getEmbeddings(HttpClient httpClient, OpenAIServiceVersion serviceVersion) {
+    public void testGetEmbeddings(HttpClient httpClient, OpenAIServiceVersion serviceVersion) {
         client = getOpenAIClient(httpClient, serviceVersion);
         getEmbeddingRunner((deploymentId, embeddingsOptions) -> {
             Embeddings resultEmbeddings = client.getEmbeddings(deploymentId, embeddingsOptions);
@@ -129,13 +166,12 @@ public class OpenAIClientTest extends OpenAIClientTestBase {
 
     @ParameterizedTest(name = DISPLAY_NAME_WITH_ARGUMENTS)
     @MethodSource("com.azure.ai.openai.TestUtils#getTestParameters")
-    public void getEmbeddingsWithResponse(HttpClient httpClient, OpenAIServiceVersion serviceVersion) {
+    public void testGetEmbeddingsWithResponse(HttpClient httpClient, OpenAIServiceVersion serviceVersion) {
         client = getOpenAIClient(httpClient, serviceVersion);
         getEmbeddingRunner((deploymentId, embeddingsOptions) -> {
             Response<BinaryData> response = client.getEmbeddingsWithResponse(deploymentId,
                 BinaryData.fromObject(embeddingsOptions), new RequestOptions());
-            assertEquals(200, response.getStatusCode());
-            Embeddings resultEmbeddings = response.getValue().toObject(Embeddings.class);
+            Embeddings resultEmbeddings = assertAndGetValueFromResponse(response, Embeddings.class, 200);
             assertEmbeddings(resultEmbeddings);
         });
     }
