b3b8d07bf3e640b03c3e4308f79c9a15e0610514
Liudmila Molkova <limolkova@microsoft.com>
2023-06-05 12:00:54 -0700

    Fix NPE in end span

---
+++ sdk/servicebus/azure-messaging-servicebus/src/test/java/com/azure/messaging/servicebus/implementation/instrumentation/ServiceBusTracerTests.java
@@ -0,0 +1,88 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.messaging.servicebus.implementation.instrumentation;
+
+import com.azure.core.amqp.exception.AmqpErrorCondition;
+import com.azure.core.amqp.exception.AmqpException;
+import com.azure.core.util.Context;
+import com.azure.core.util.tracing.Tracer;
+import com.azure.messaging.servicebus.ServiceBusErrorSource;
+import com.azure.messaging.servicebus.implementation.DispositionStatus;
+import org.apache.qpid.proton.amqp.transport.DeliveryState;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class ServiceBusTracerTests {
+    @Test
+    public void testSpanEndNoError() {
+        Tracer inner = mock(Tracer.class);
+        when(inner.isEnabled()).thenReturn(true);
+
+        ServiceBusTracer tracer = new ServiceBusTracer(inner, "fqdn", "entityPath");
+        tracer.endSpan(null, Context.NONE, null);
+
+        verify(inner, times(1)).end(isNull(), isNull(), same(Context.NONE));
+    }
+
+    @Test
+    public void testSpanEndNoErrorAndScope() {
+        Tracer inner = mock(Tracer.class);
+        when(inner.isEnabled()).thenReturn(true);
+
+        ServiceBusTracer tracer = new ServiceBusTracer(inner, "fqdn", "entityPath");
+        AtomicBoolean closed = new AtomicBoolean();
+        tracer.endSpan(null, Context.NONE, () -> closed.set(true));
+
+        verify(inner, times(1)).end(isNull(), isNull(), same(Context.NONE));
+        assertTrue(closed.get());
+    }
+
+    @Test
+    public void testSpanEndException() {
+        Tracer inner = mock(Tracer.class);
+        when(inner.isEnabled()).thenReturn(true);
+
+        ServiceBusTracer tracer = new ServiceBusTracer(inner, "fqdn", "entityPath");
+
+        Exception ex = new RuntimeException("foo");
+        tracer.endSpan(ex, Context.NONE, null);
+
+        verify(inner, times(1)).end(isNull(), eq(ex), same(Context.NONE));
+    }
+
+    @ParameterizedTest
+    @MethodSource("getAmqpException")
+    public void testSpanEndAmqpException(AmqpException amqpException, String expectedStatus) {
+        Tracer inner = mock(Tracer.class);
+        when(inner.isEnabled()).thenReturn(true);
+
+        ServiceBusTracer tracer = new ServiceBusTracer(inner, "fqdn", "entityPath");
+
+        tracer.endSpan(amqpException, Context.NONE, null);
+
+        verify(inner, times(1)).end(eq(expectedStatus), eq(amqpException), same(Context.NONE));
+    }
+
+    public static Stream<Arguments> getAmqpException() {
+        return Stream.of(
+            Arguments.of(new AmqpException(false, "foo", null, null), null),
+            Arguments.of(new AmqpException(false, AmqpErrorCondition.NOT_FOUND, "foo", null), AmqpErrorCondition.NOT_FOUND.getErrorCondition()),
+            Arguments.of(new AmqpException(false, AmqpErrorCondition.TIMEOUT_ERROR, "", null), AmqpErrorCondition.TIMEOUT_ERROR.getErrorCondition()),
+            Arguments.of(new AmqpException(false, AmqpErrorCondition.SERVER_BUSY_ERROR, null, new RuntimeException("foo"), null), AmqpErrorCondition.SERVER_BUSY_ERROR.getErrorCondition()));
+    }
+}
