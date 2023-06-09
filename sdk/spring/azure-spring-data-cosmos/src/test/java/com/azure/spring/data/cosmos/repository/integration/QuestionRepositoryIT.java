2b3ede3e35a82b515c5b0f15ad163f07d28d6362
Trevor Anderson <trande@microsoft.com>
2023-06-09 12:30:44 -0400

    Revert "findById Optimization (#35261)"
    This reverts commit a7b417ce0909175863f3ec97c8b3112ea428f0a1.

--- sdk/spring/azure-spring-data-cosmos/src/test/java/com/azure/spring/data/cosmos/repository/integration/QuestionRepositoryIT.java
+++ sdk/spring/azure-spring-data-cosmos/src/test/java/com/azure/spring/data/cosmos/repository/integration/QuestionRepositoryIT.java
@@ -3,9 +3,7 @@
 package com.azure.spring.data.cosmos.repository.integration;
 
 import com.azure.spring.data.cosmos.IntegrationTestCollectionManager;
-import com.azure.spring.data.cosmos.common.ResponseDiagnosticsTestUtils;
 import com.azure.spring.data.cosmos.core.CosmosTemplate;
-import com.azure.spring.data.cosmos.core.ResponseDiagnostics;
 import com.azure.spring.data.cosmos.domain.Question;
 import com.azure.spring.data.cosmos.repository.TestRepositoryConfig;
 import com.azure.spring.data.cosmos.repository.repository.QuestionRepository;
@@ -23,8 +21,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestRepositoryConfig.class)
 public class QuestionRepositoryIT {
@@ -32,8 +28,6 @@ public class QuestionRepositoryIT {
     private static final String QUESTION_ID = "question-id";
 
     private static final String QUESTION_URL = "http://xxx.html";
-
-    private static final String NULL_ID = "null-id";
 
     private static final Question QUESTION = new Question(QUESTION_ID, QUESTION_URL);
 
@@ -46,9 +40,6 @@ public class QuestionRepositoryIT {
     @Autowired
     private QuestionRepository repository;
 
-    @Autowired
-    private ResponseDiagnosticsTestUtils responseDiagnosticsTestUtils;
-
     @Before
     public void setUp() {
         collectionManager.ensureContainersCreatedAndEmpty(template, Question.class);
@@ -58,14 +49,14 @@ public class QuestionRepositoryIT {
     @Test
     public void testFindById() {
         final Optional<Question> optional = this.repository.findById(QUESTION_ID);
-        assertThat(responseDiagnosticsTestUtils.getCosmosResponseStatistics()).isNull();
+
         Assert.assertTrue(optional.isPresent());
         Assert.assertEquals(QUESTION, optional.get());
     }
 
     @Test
     public void testFindByIdNull() {
-        final Optional<Question> byId = this.repository.findById(NULL_ID);
+        final Optional<Question> byId = this.repository.findById(QUESTION_URL);
         Assert.assertFalse(byId.isPresent());
     }
 
