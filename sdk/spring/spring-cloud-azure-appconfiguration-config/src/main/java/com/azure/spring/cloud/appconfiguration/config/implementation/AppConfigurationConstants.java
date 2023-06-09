02947eec0ec5fe6acc68bcc464bcb6a37427253a
Alok Ratnaparkhi <alokratnaparkhi@hotmail.com>
2023-06-08 20:34:29 -0500

    Making class as final because it is suggested by CheckStyle guideline.

--- sdk/spring/spring-cloud-azure-appconfiguration-config/src/main/java/com/azure/spring/cloud/appconfiguration/config/implementation/AppConfigurationConstants.java
+++ sdk/spring/spring-cloud-azure-appconfiguration-config/src/main/java/com/azure/spring/cloud/appconfiguration/config/implementation/AppConfigurationConstants.java
@@ -5,7 +5,7 @@ package com.azure.spring.cloud.appconfiguration.config.implementation;
 /**
  * Constants used for processing Azure App Configuration Config info.
  */
-public class AppConfigurationConstants {
+public final class AppConfigurationConstants {
 
     private AppConfigurationConstants() {
         throw new IllegalStateException("You can not initialize AppConfigurationConstants utility class.");
