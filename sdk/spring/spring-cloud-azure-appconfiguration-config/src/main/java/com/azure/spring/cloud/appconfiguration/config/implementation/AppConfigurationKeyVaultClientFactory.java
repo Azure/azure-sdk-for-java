35c3927119053ecd8cb1efc9ccddc0175fc97545
Alok Ratnaparkhi <alokratnaparkhi@hotmail.com>
2023-06-08 20:24:05 -0500

    refactor(AppConfigurationKeyVaultClientFactory): Replace this "Map.containsKey()" with a call to "Map.computeIfAbsent() Description: This commit includes fix for sonar lint open issue #AYZXoioEuI6TRXY4Otq0. Currently, code uses map.containsKey() style code to add the value to key if it does not exist. It is wordy and same goal can be achieved using fewer lines of code. Also, map.containsKey may fail if it encounters null value. This commit uses computeIfAbsent instead of map.containsKey() as suggested by Sonar lint as a compliant solution. Issue Link: https://sonarcloud.io/project/issues?resolved=false&id=stliu_azure-sdk-for-java&open=AYZXoioEuI6TRXY4Otq0&tab=code Files changed: 1) sdk/spring/spring-cloud-azure-appconfiguration-config/src/main/java/com/azure/spring/cloud/appconfiguration/config/implementation/AppConfigurationKeyVaultClientFactory.java -> Added computeIfAbsent and concise lambda expression to reduce no. of lines of code as suggested by Sonar lint as a compliant solution Ref: #Sonar lint issue #AYZXoioEuI6TRXY4Otq0

--- sdk/spring/spring-cloud-azure-appconfiguration-config/src/main/java/com/azure/spring/cloud/appconfiguration/config/implementation/AppConfigurationKeyVaultClientFactory.java
+++ sdk/spring/spring-cloud-azure-appconfiguration-config/src/main/java/com/azure/spring/cloud/appconfiguration/config/implementation/AppConfigurationKeyVaultClientFactory.java
@@ -38,11 +38,10 @@ public class AppConfigurationKeyVaultClientFactory {
     public AppConfigurationSecretClientManager getClient(String host) {
         // Check if we already have a client for this key vault, if not we will make
         // one
-        if (!keyVaultClients.containsKey(host)) {
-            AppConfigurationSecretClientManager client = new AppConfigurationSecretClientManager(host,
-                keyVaultClientProvider, keyVaultSecretProvider, secretClientFactory, credentialsConfigured);
-            keyVaultClients.put(host, client);
-        }
+        keyVaultClients.computeIfAbsent(host, key -> 
+            new AppConfigurationSecretClientManager(host, 
+                    keyVaultClientProvider, keyVaultSecretProvider, secretClientFactory, credentialsConfigured)
+        );
         return keyVaultClients.get(host);
     }
 
