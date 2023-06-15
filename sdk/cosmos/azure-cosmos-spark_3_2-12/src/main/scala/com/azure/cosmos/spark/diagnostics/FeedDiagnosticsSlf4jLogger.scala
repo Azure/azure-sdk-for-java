8bedc2d20e25e35a4b56f8e7182536ed0d05c5ff
Fabian Meiswinkel <fabianm@microsoft.com>
2023-06-15 20:52:11 +0000

    Adding new diagnostics mode in Spark connector including Pk+id tuples

--- sdk/cosmos/azure-cosmos-spark_3_2-12/src/main/scala/com/azure/cosmos/spark/diagnostics/FeedDiagnosticsSlf4jLogger.scala
+++ sdk/cosmos/azure-cosmos-spark_3_2-12/src/main/scala/com/azure/cosmos/spark/diagnostics/FeedDiagnosticsSlf4jLogger.scala
@@ -2,15 +2,45 @@
 // Licensed under the MIT License.
 package com.azure.cosmos.spark.diagnostics
 
+import com.azure.cosmos.implementation.ChangeFeedSparkRowItem
 import com.azure.cosmos.implementation.spark.OperationContext
-import com.azure.cosmos.models.FeedResponse
+import com.azure.cosmos.models.{FeedResponse, PartitionKeyDefinition}
 
 import java.util.concurrent.atomic.AtomicLong
 
-private[spark] class FeedDiagnosticsSlf4jLogger(classType: Class[_])
+private[spark] class FeedDiagnosticsSlf4jLogger(classType: Class[_], includeDetails: Boolean)
   extends DefaultMinimalSlf4jLogger(classType: Class[_]) {
 
   @transient private lazy val inFlightResponsesCounter = new AtomicLong(0)
+
+  private[this] def extractIdentities(response: FeedResponse[_]): Option[String] = {
+
+    if (!response.isInstanceOf[FeedResponse[ChangeFeedSparkRowItem]]) {
+      None
+    } else {
+      val changeFeedResponse = response.asInstanceOf[FeedResponse[ChangeFeedSparkRowItem]]
+      val sb = new StringBuilder()
+      changeFeedResponse.getResults.forEach(item => {
+        if (sb.length > 0) {
+          sb.append(", (")
+        } else {
+          sb.append("(")
+        }
+
+        sb.append(item.lsn)
+        sb.append(", ")
+        item.pkValue match {
+          case Some(pk) => sb.append(pk.toString)
+          case None => sb.append("n/a")
+        }
+        sb.append(", ")
+        sb.append(item.row.getAs[String]("id"))
+        sb.append(")")
+      })
+
+      Some(sb.toString)
+    }
+  }
 
   override def feedResponseReceivedListener
   (
@@ -20,9 +50,18 @@ private[spark] class FeedDiagnosticsSlf4jLogger(classType: Class[_])
 
     val inFlightResponses = inFlightResponsesCounter.incrementAndGet()
 
-    logInfo(
-      s"Received - ItemCount: ${response.getResults.size} CT: ${getContinuationTokenHash(response)}, " +
-        s"In-flight-responses: $inFlightResponses Context: $context")
+    if (isInfoLogEnabled) {
+      if (includeDetails) {
+        logInfo(
+          s"Received - ItemCount: ${response.getResults.size} CT: ${getContinuationTokenHash(response)}, " +
+            s"In-flight-responses: $inFlightResponses Context: $context Items: ${extractIdentities(response)}")
+
+      } else {
+        logInfo(
+          s"Received - ItemCount: ${response.getResults.size} CT: ${getContinuationTokenHash(response)}, " +
+            s"In-flight-responses: $inFlightResponses Context: $context")
+      }
+    }
   }
 
   override def feedResponseProcessedListener
@@ -33,9 +72,17 @@ private[spark] class FeedDiagnosticsSlf4jLogger(classType: Class[_])
 
     val inFlightResponses = inFlightResponsesCounter.decrementAndGet()
 
-    logInfo(
-      s"Processed - ItemCount: ${response.getResults.size} CT: ${getContinuationTokenHash(response)}, " +
-        s"In-flight-responses: $inFlightResponses Context: $context")
+    if (isInfoLogEnabled) {
+      if (includeDetails) {
+        logInfo(
+          s"Processed - ItemCount: ${response.getResults.size} CT: ${getContinuationTokenHash(response)}, " +
+            s"In-flight-responses: $inFlightResponses Context: $context Items: ${extractIdentities(response)}")
+      } else {
+        logInfo(
+          s"Processed - ItemCount: ${response.getResults.size} CT: ${getContinuationTokenHash(response)}, " +
+            s"In-flight-responses: $inFlightResponses Context: $context")
+      }
+    }
   }
 
   def getInFlightResponseCountSnapshot : Long = {
