0c7b8679698f65188113b2806ace2c3c19df2e31
Abhijeet Mohanty <abhmohanty@microsoft.com>
2023-06-19 15:13:16 -0400

    Preliminary changes.

---
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/MetadataRequestConnectionErrorHandler.java
@@ -0,0 +1,43 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.cosmos.implementation;
+
+import com.azure.cosmos.CosmosException;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+import java.util.List;
+
+public class MetadataRequestConnectionErrorHandler {
+
+    private final GlobalEndpointManager globalEndpointManager;
+
+    public MetadataRequestConnectionErrorHandler(GlobalEndpointManager globalEndpointManager) {
+        this.globalEndpointManager = globalEndpointManager;
+    }
+
+    public Mono<Void> markRegionAsUnavailable(RxDocumentServiceRequest request, CosmosException cosmosException) {
+
+        URI locationEndpointToRoute = request.requestContext.locationEndpointToRoute;
+
+        if (Exceptions.isSubStatusCode(cosmosException, HttpConstants.SubStatusCodes.GATEWAY_ENDPOINT_UNAVAILABLE)) {
+            if (request.isReadOnlyRequest()) {
+                this.globalEndpointManager.markEndpointUnavailableForRead(locationEndpointToRoute);
+            } else {
+                this.globalEndpointManager.markEndpointUnavailableForWrite(locationEndpointToRoute);
+            }
+        }
+
+        if (Exceptions.isSubStatusCode(cosmosException, HttpConstants.SubStatusCodes.GATEWAY_ENDPOINT_READ_TIMEOUT)) {
+            if (request.isReadOnlyRequest()) {
+                this.globalEndpointManager.markEndpointUnavailableForRead(locationEndpointToRoute);
+            } else {
+                this.globalEndpointManager.markEndpointUnavailableForWrite(locationEndpointToRoute);
+            }
+        }
+
+        return Mono.empty();
+    }
+
+}
