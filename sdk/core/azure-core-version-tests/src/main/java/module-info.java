25bab7b4433d1d05e91cc3c5db96b12b36a5c76f
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2023-06-21 11:40:00 -0400

    Add requires

--- sdk/core/azure-core-version-tests/src/main/java/module-info.java
+++ sdk/core/azure-core-version-tests/src/main/java/module-info.java
@@ -2,5 +2,8 @@
 // Licensed under the MIT License.
 
 open module com.azure.core.version.tests {
+    requires com.azure.core;
+    requires com.azure.core.serializer.json.jackson;
+
     exports com.azure.core.version.tests;
 }
