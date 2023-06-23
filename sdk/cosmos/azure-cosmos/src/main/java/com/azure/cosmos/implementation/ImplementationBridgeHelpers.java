c9fc9dac19ddb53837688b355a8c67aee888518c
Abhijeet Mohanty <abhmohanty@microsoft.com>
2023-06-23 09:35:37 -0400

    SessionTokenMismatchRetryPolicy optimization through customer supplied region switch hints (#35292)
    * Added fault injection to inject bad session tokens into store results.
    
    * Added fault injection to inject bad session tokens into store results.
    
    * Added fault injection to inject bad session tokens into store results.
    
    * Added fault injection to inject bad session tokens into store results.
    
    * Refactorings.
    
    * Refactorings.
    
    * Refactorings.
    
    * Added preliminary code to configure retry strategy.
    
    * Added preliminary code to configure retry strategy.
    
    * Added preliminary code to configure retry strategy.
    
    * Added preliminary code to configure retry strategy.
    
    * Added preliminary code to configure retry strategy.
    
    * Retry strategy model changes.
    
    * Javadoc updates.
    
    * Refactoring.
    
    * Refactoring.
    
    * Refactoring.
    
    * Added license headers.
    
    * Fixed compilation errors.
    
    * Removed unused imports.
    
    * Wired retry strategy into SessionTokenMismatchRetryPolicy.
    
    * Refactorings.
    
    * Refactorings.
    
    * Refactorings.
    
    * Refactorings.
    
    * Refactorings.
    
    * Refactorings.
    
    * Refactorings.
    
    * Refactorings.
    
    * Integrated region switch logic into SessionTokenMismatchRetryPolicy.
    
    * Added check to see if the region is a possible local region.
    
    * Added check to see if the region is a possible local region.
    
    * Fixed typos.
    
    * Fixed typos.
    
    * Added tests.
    
    * Fixed tests.
    
    * Refactorings.
    
    * Added tests.
    
    * Added tests.
    
    * Fixed tests.
    
    * Fixed tests.
    
    * Fixed javadocs.
    
    * Fix linting errors.
    
    * Added timeout to tests.
    
    * Updated javadoc.
    
    * Updated javadoc.
    
    * Updated javadoc.
    
    * Updated javadoc.
    
    * Updated CHANGELOG.md.
    
    * Updated CHANGELOG.md.
    
    * Added region-switch support for patch operations.
    
    * Updated javadoc.
    
    * Refactorings.
    
    * Refactorings.
    
    * Refactorings.
    
    * Refactorings.
    
    * Addressing review comments.
    
    * Addressing review comments.
    
    * Addressing review comments.
    
    * Addressing review comments.
    
    * Addressing review comments.
    
    * Addressing review comments.
    
    * Addressing review comments.
    
    * Addressing review comments.
    
    * Addressing review comments.
    
    * Addressing review comments.
    
    * Addressing review comments.
    
    * Fixing tests.
    
    * Fixing tests.
    
    * Refactorings.
    
    * Refactorings.
    
    * Addressing review comments.
    
    * Addressing review comments.
    
    * Made CosmosRegionSwitchHint an enum type.
    
    * Made CosmosRegionSwitchHint an enum type.
    
    * Addressing review comments.
    
    * Addressing review comments.
    
    * Addressing review comments.
    
    * Addressing review comments.

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/ImplementationBridgeHelpers.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/ImplementationBridgeHelpers.java
@@ -20,6 +20,7 @@ import com.azure.cosmos.CosmosDiagnosticsHandler;
 import com.azure.cosmos.CosmosDiagnosticsThresholds;
 import com.azure.cosmos.CosmosEndToEndOperationLatencyPolicyConfig;
 import com.azure.cosmos.CosmosException;
+import com.azure.cosmos.CosmosSessionRetryOptions;
 import com.azure.cosmos.DirectConnectionConfig;
 import com.azure.cosmos.GlobalThroughputControlConfig;
 import com.azure.cosmos.ThroughputControlGroupConfig;
@@ -53,6 +54,7 @@ import com.azure.cosmos.models.CosmosItemResponse;
 import com.azure.cosmos.models.CosmosMetricName;
 import com.azure.cosmos.models.CosmosPatchOperations;
 import com.azure.cosmos.models.CosmosQueryRequestOptions;
+import com.azure.cosmos.CosmosRegionSwitchHint;
 import com.azure.cosmos.models.FeedResponse;
 import com.azure.cosmos.models.ModelBridgeInternal;
 import com.azure.cosmos.models.PartitionKey;
@@ -1518,6 +1520,46 @@ public class ImplementationBridgeHelpers {
 
         public interface CosmosContainerProactiveInitConfigAccessor {
             Map<CosmosContainerIdentity, ContainerDirectConnectionMetadata> getContainerPropertiesMap(CosmosContainerProactiveInitConfig cosmosContainerProactiveInitConfig);
+        }
+    }
+
+    public static final class CosmosSessionRetryOptionsHelper {
+        private static final AtomicReference<Boolean> cosmosSessionRetryOptionsClassLoaded = new AtomicReference<>(false);
+        private static final AtomicReference<CosmosSessionRetryOptionsAccessor> accessor = new AtomicReference<>();
+
+        private CosmosSessionRetryOptionsHelper() {}
+
+        public static CosmosSessionRetryOptionsAccessor getCosmosSessionRetryOptionsAccessor() {
+
+            if (!cosmosSessionRetryOptionsClassLoaded.get()) {
+                logger.debug("Initializing cosmosSessionRetryOptionsAccessor...");
+                initializeAllAccessors();
+            }
+
+            CosmosSessionRetryOptionsAccessor snapshot = accessor.get();
+
+            if (snapshot == null) {
+                logger.error("cosmosSessionRetryOptionsAccessor is not initialized yet!");
+                System.exit(9727); // Using a unique status code here to help debug the issue.
+            }
+
+            return snapshot;
+        }
+
+        public static void setCosmosSessionRetryOptionsAccessor(final CosmosSessionRetryOptionsAccessor newAccessor) {
+
+            assert (newAccessor != null);
+
+            if (!accessor.compareAndSet(null, newAccessor)) {
+                logger.debug("CosmosSessionRetryOptionsAccessor already initialized!");
+            } else {
+                logger.debug("Setting CosmosSessionRetryOptionsAccessor...");
+                cosmosSessionRetryOptionsClassLoaded.set(true);
+            }
+        }
+
+        public interface CosmosSessionRetryOptionsAccessor {
+            CosmosRegionSwitchHint getRegionSwitchHint(CosmosSessionRetryOptions sessionRetryOptions);
         }
     }
 }
