cc6376541319ef518db24b78a61f076ac5145abc
Muyao Feng <92105726+Netyyyy@users.noreply.github.com>
2023-06-29 15:42:45 +0800

    revert

--- sdk/spring/spring-cloud-azure-core/src/main/java/com/azure/spring/cloud/core/implementation/util/AzureSpringIdentifier.java
+++ sdk/spring/spring-cloud-azure-core/src/main/java/com/azure/spring/cloud/core/implementation/util/AzureSpringIdentifier.java
@@ -40,7 +40,7 @@ public final class AzureSpringIdentifier {
     private static final Logger LOGGER = LoggerFactory.getLogger(AzureSpringIdentifier.class);
     public static final String VERSION = getVersion();
     public static final String AZURE_SPRING_APP_CONFIG = "az-sp-cfg/" + VERSION;
-    public static final String AZURE_SPRING_EVENT_HUBS = "az-s-eh/" + VERSION;
+    public static final String AZURE_SPRING_EVENT_HUBS = "az-sp-eh/" + VERSION;
 
     public static final String AZURE_SPRING_EVENT_GRID = "az-sp-eg/" + VERSION;
     //Kafka supports set client software name and version individually.
@@ -48,7 +48,7 @@ public final class AzureSpringIdentifier {
     //And given the limitation of Kafka, only . is allowed here.
     public static final String AZURE_SPRING_EVENT_HUBS_KAFKA_OAUTH = ".az-sp-kafka";
     public static final String AZURE_SPRING_KEY_VAULT_SECRETS = "az-sp-kv/" + VERSION;
-    public static final String AZURE_SPRING_KEY_VAULT_CERTIFICATES = "az-sp-kc/" + VERSION;
+    public static final String AZURE_SPRING_KEY_VAULT_CERTIFICATES = "az-sp-kv-ct/" + VERSION;
 
     public static final String AZURE_SPRING_MYSQL_OAUTH = "az-sp-mysql/" + VERSION;
     public static final String AZURE_SPRING_POSTGRESQL_OAUTH = "az-sp-psql/" + VERSION;
