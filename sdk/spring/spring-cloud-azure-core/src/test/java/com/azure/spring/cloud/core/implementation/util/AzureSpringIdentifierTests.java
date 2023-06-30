817d1771f07dbdac1a81273bbc2457f67908ecc4
Muyao <v-muyaofeng@microsoft.com>
2023-06-30 16:38:14 +0800

    fix pl

--- sdk/spring/spring-cloud-azure-core/src/test/java/com/azure/spring/cloud/core/implementation/util/AzureSpringIdentifierTests.java
+++ sdk/spring/spring-cloud-azure-core/src/test/java/com/azure/spring/cloud/core/implementation/util/AzureSpringIdentifierTests.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.azure.spring.cloud.core.implementation.util;
 
 import org.junit.jupiter.api.Assertions;
@@ -7,9 +10,10 @@ public class AzureSpringIdentifierTests {
 
     @Test
     public void testFormatVersion() {
-        Assertions.assertEquals(AzureSpringIdentifier.formatVersion("4.10.0"),"4.10.0");
-        Assertions.assertEquals(AzureSpringIdentifier.formatVersion("4.10.0-beta.1"),"4.10.0-b.1");
-        Assertions.assertEquals(AzureSpringIdentifier.formatVersion("4.10.0-alpha.1"),"4.10.0-a.1");
+        Assertions.assertEquals(AzureSpringIdentifier.formatVersion("4.10.0"), "4.10.0");
+        Assertions.assertEquals(AzureSpringIdentifier.formatVersion("4.9.0-beta.1"), "4.9.0-beta.1");
+        Assertions.assertEquals(AzureSpringIdentifier.formatVersion("4.10.0-beta.1"), "4.10.0-b.1");
+        Assertions.assertEquals(AzureSpringIdentifier.formatVersion("4.10.0-alpha.1"), "4.10.0-a.1");
         Assertions.assertThrows(RuntimeException.class, () -> AzureSpringIdentifier.formatVersion("4.10.0-SNAPSHOT"));
     }
 
