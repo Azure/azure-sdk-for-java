d084ba2391596dab3c0328c25744b3e4927894cc
Jair <67484440+jairmyree@users.noreply.github.com>
2023-06-30 14:30:23 -0700

    Large Log Query Documentation

--- sdk/monitor/azure-monitor-query/src/samples/java/com/azure/monitor/query/LargeQuerySample.java
+++ sdk/monitor/azure-monitor-query/src/samples/java/com/azure/monitor/query/LargeQuerySample.java
@@ -3,6 +3,8 @@
 
 package com.azure.monitor.query;
 
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.azure.core.http.policy.HttpLogDetailLevel;
@@ -10,53 +12,100 @@ import com.azure.core.http.policy.HttpLogOptions;
 import com.azure.core.util.Configuration;
 import com.azure.identity.ClientSecretCredential;
 import com.azure.identity.ClientSecretCredentialBuilder;
-import com.azure.monitor.query.models.LogsBatchQuery;
-import com.azure.monitor.query.models.QueryTimeInterval;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.monitor.query.models.*;
 
 public class LargeQuerySample {
 
 
-    static String workspaceId = "{workspace-id}";
+    // BEGIN: com.azure.monitor.query.LargeQuerySample-setWorkspaceId
+    static final String WORKSPACE_ID = "{workspace-id}";
+    // END: com.azure.monitor.query.LargeQuerySample-setWorkspaceId
+
+
+    static LogsQueryClient client;
 
     public static void main(String[] args) {
-
-        
-
-        ClientSecretCredential tokenCredential = new ClientSecretCredentialBuilder()
-                .clientId(Configuration.getGlobalConfiguration().get("AZURE_MONITOR_CLIENT_ID"))
-                .clientSecret(Configuration.getGlobalConfiguration().get("AZURE_MONITOR_CLIENT_SECRET"))
-                .tenantId(Configuration.getGlobalConfiguration().get("AZURE_TENANT_ID"))
-                .build();
-
-        LogsQueryClient logsQueryClient = new LogsQueryClientBuilder()
-                .credential(tokenCredential)
-                .httpLogOptions(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BODY_AND_HEADERS))
-                .buildClient();
-
-        
-        
+        // BEGIN: com.azure.monitor.query.LargeQuerySample-createLogsQueryClient
+        LogsQueryClient client = new LogsQueryClientBuilder()
+            .credential(new DefaultAzureCredentialBuilder().build())
+            .buildClient();
+        // END: com.azure.monitor.query.LargeQuerySample-createLogsQueryClient
     }
 
-    static LogsBatchQuery getBatchQueryByRowCount(LogsQueryClient client, String query, int maxRowCountPerQuery) {
-        return getBatchQueryByRowCount(client, query, maxRowCountPerQuery, "TimeGenerated");
-    }
 
-    static LogsBatchQuery getBatchQueryByRowCount(LogsQueryClient client, String query, int maxRowCountPerQuery, String timeColumn) {
-        throw new UnsupportedOperationException("Not implemented yet");
-    }
+    // BEGIN: com.azure.monitor.query.LargeQuerySample-createBatchQueryFromLargeQuery
+    static LogsBatchQuery createBatchQueryFromLargeQuery(String originalQuery,
+                                                         List<OffsetDateTime> endpoints,
+                                                         String timeColumn) {
+        LogsBatchQuery batchQuery = new LogsBatchQuery();
 
-    static List<String> getBatchEndpoints(LogsQueryClient client, String originalQuery, QueryTimeInterval timeInterval,
-        int maxRowCountPerQuery, String timeColumn) {
-        
+        for (int i = 0; i < endpoints.size(); i++) {
+            String queryString;
+            if (i == endpoints.size() - 1) {
+                queryString = String.format("%1$s | where %3$s >= datetime('%2$s')", originalQuery, endpoints.get(i), timeColumn);
+            }
+            else {
+                queryString = String.format("%1$s | where %4$s >= datetime('%2$s') and %4$s < datetime('%3$s')", originalQuery, endpoints.get(i), endpoints.get(i + 1), timeColumn);
+            }
+            batchQuery.addWorkspaceQuery(WORKSPACE_ID,queryString, null);
+        }
+
+        return batchQuery;
+    }
+    // END: com.azure.monitor.query.LargeQuerySample-createBatchQueryFromLargeQuery
+
+
+    // BEGIN: com.azure.monitor.query.LargeQuerySample-createBatchQueryFromLargeQueryBySize
+    static LogsBatchQuery createBatchQueryFromLargeQueryByByteSize(String originalQuery,
+                                                                   int maxByteSizePerBatch,
+                                                                   String timeColumn) {
         String findBatchEndpointsQuery = String.format(
-            "%1$s | sort by %2$s desc | extend batch_num = row_cumsum(1) / %3$s | summarize endpoint=mins(%2$s) by batch_num | sort by batch_num asc | project endpoint", 
+            "%1$s | sort by %2$s desc | extend batch_num = row_cumsum(estimate_data_size(*)) / %3$s | summarize endpoint=min(%2$s) by batch_num | sort by batch_num desc | project endpoint",
             originalQuery,
             timeColumn,
-            maxRowCountPerQuery);
+            maxByteSizePerBatch);
 
-        client.queryWorkspace(workspaceId, findBatchEndpointsQuery, timeInterval, String.class);
+        LogsQueryResult result = client.queryWorkspace(WORKSPACE_ID, findBatchEndpointsQuery, QueryTimeInterval.ALL);
+        LogsTable table = result.getTable();
+        List<LogsTableColumn> columns = table.getColumns();
+        List<LogsTableRow> rows = table.getRows();
+        List<OffsetDateTime> endpoints = new ArrayList<>();
 
-        throw new UnsupportedOperationException("Not implemented yet");
-        
-    } 
+        for (LogsTableRow row : rows) {
+            row.getColumnValue("endpoint").ifPresent(rowValue -> {
+                endpoints.add(rowValue.getValueAsDateTime());
+            });
+        }
+
+        return createBatchQueryFromLargeQuery(originalQuery, endpoints, timeColumn);
+    }
+    // END: com.azure.monitor.query.LargeQuerySample-createBatchQueryFromLargeQueryBySize
+
+    // BEGIN: com.azure.monitor.query.LargeQuerySample-createBatchQueryFromLargeQueryByRows
+    static LogsBatchQuery createBatchQueryFromLargeQueryByRowCount(String originalQuery,
+                                                                   int maxRowPerBatch,
+                                                                   String timeColumn) {
+
+        String findBatchEndpointsQuery = String.format(
+            "%1$s | sort by %2$s desc | extend batch_num = row_cumsum(1) / %3$s | summarize endpoint=min(%2$s) by batch_num | sort by batch_num desc | project endpoint",
+            originalQuery,
+            timeColumn,
+            maxRowPerBatch);
+
+        LogsQueryResult result = client.queryWorkspace(WORKSPACE_ID, findBatchEndpointsQuery, QueryTimeInterval.ALL);
+        LogsTable table = result.getTable();
+        List<LogsTableColumn> columns = table.getColumns();
+        List<LogsTableRow> rows = table.getRows();
+        List<OffsetDateTime> endpoints = new ArrayList<>();
+
+        for (LogsTableRow row : rows) {
+            row.getColumnValue("endpoint").ifPresent(rowValue -> {
+                endpoints.add(rowValue.getValueAsDateTime());
+            });
+        }
+
+        return createBatchQueryFromLargeQuery(originalQuery, endpoints, timeColumn);
+    }
+    // END: com.azure.monitor.query.LargeQuerySample-createBatchQueryFromLargeQueryByRows
 }
