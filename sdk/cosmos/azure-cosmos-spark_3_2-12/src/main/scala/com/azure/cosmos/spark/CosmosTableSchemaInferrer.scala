307e238df9babb8cf7a1b92e654bc53ce506ab11
Fabian Meiswinkel <fabian@meiswinkel.com>
2023-07-10 16:05:08 +0200

    Update CosmosTableSchemaInferrer.scala

--- sdk/cosmos/azure-cosmos-spark_3_2-12/src/main/scala/com/azure/cosmos/spark/CosmosTableSchemaInferrer.scala
+++ sdk/cosmos/azure-cosmos-spark_3_2-12/src/main/scala/com/azure/cosmos/spark/CosmosTableSchemaInferrer.scala
@@ -1,5 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. 
+// Licensed under the MIT License.
 package com.azure.cosmos.spark
 
 import com.azure.cosmos.implementation.ImplementationBridgeHelpers
