4b308a2571bf581ae1bd489ac652159640c8b821
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2023-07-17 16:09:15 -0400

    Check out root directories instead of files

---
+++ eng/pipelines/scripts/Set-CredScanCheckoutDirectories.ps1
@@ -0,0 +1,41 @@
+<#
+.SYNOPSIS
+Converts the files changed in a PR to what sparse checkout needs to check out for CredScan running.
+
+.DESCRIPTION
+Given a list of file paths modified in a PR this script will:
+
+- Add a leading '/' to the file path for git sparse checkout.
+- When many files have changed this will compact individual files to whole directory check outs.
+
+At the end of processing this will set the DevOps variable 'SparseCheckoutDirectories'.
+
+.PARAMETER ChangedFiles
+The files changed in the PR.
+#>
+
+param(
+  [Parameter(Mandatory = $true)]
+  $ChangedFiles
+)
+
+# Loop over each file changed and determine which directory to check out and add a leading '/' if needed.
+$changedFiles = ForEach-Object {
+    $changedFile = $_
+    if ($changedFile -matches 'sdk/.*?/') {
+        $changedFile = $matches[0]
+    } elseif ($changedFile -matches '.*/.*?/') {
+        $changedFile = $matches[0]
+    } elseif ($changedFile -notmatch '.*/.*?') {
+        $changedFile = $matches[0]
+    }
+
+    if ($changedFile.StartsWith('/')) {
+        $changedFile
+    } else {
+        '/' + $changedFile
+    }
+}
+
+$tmp = ConvertTo-Json @($changedFiles | Sort-Object | Get-Unique) -Compress
+Write-Host "##vso[task.setvariable variable=SparseCheckoutDirectories;]$tmp"
