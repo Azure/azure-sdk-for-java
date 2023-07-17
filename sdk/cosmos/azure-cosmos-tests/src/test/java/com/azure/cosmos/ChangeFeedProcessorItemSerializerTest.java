2e0fde7aacb45a04a159931768fa75cd4737e773
Kushagra Thapar <kuthapar@microsoft.com>
2023-07-16 20:39:31 -0700

    Added changelog and more tests

--- sdk/cosmos/azure-cosmos-tests/src/test/java/com/azure/cosmos/ChangeFeedProcessorItemSerializerTest.java
+++ sdk/cosmos/azure-cosmos-tests/src/test/java/com/azure/cosmos/ChangeFeedProcessorItemSerializerTest.java
@@ -4,18 +4,14 @@ package com.azure.cosmos;
 
 import com.azure.cosmos.implementation.Utils;
 import com.azure.cosmos.models.ChangeFeedProcessorItem;
-import com.azure.cosmos.rx.changefeed.epkversion.FullFidelityChangeFeedProcessorTest;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.assertj.core.api.Assertions;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 
 public class ChangeFeedProcessorItemSerializerTest {
 
-    private final static Logger logger = LoggerFactory.getLogger(ChangeFeedProcessorItemSerializerTest.class);
     private final ObjectMapper simpleObjectMapper = Utils.getSimpleObjectMapper();
 
     @Test(groups = { "unit" })
@@ -23,6 +19,10 @@ public class ChangeFeedProcessorItemSerializerTest {
         String json = "{\"lsn\":68,\"crts\":1689555410,\"operationType\":\"replace\",\"previousImageLSN\":66}";
         JsonNode jsonNode = simpleObjectMapper.readValue(json, JsonNode.class);
         Assertions.assertThat(jsonNode.get("crts").asText()).isEqualTo("1689555410");
+
+        json = "{\"lsn\":68,\"crts\":1689555412,\"operationType\":\"replace\",\"previousImageLSN\":66}";
+        jsonNode = simpleObjectMapper.readValue(json, JsonNode.class);
+        Assertions.assertThat(jsonNode.get("crts").asText()).isEqualTo("1689555412");
     }
 
     @Test(groups = { "unit" })
@@ -31,6 +31,11 @@ public class ChangeFeedProcessorItemSerializerTest {
         ChangeFeedProcessorItem changeFeedProcessorItem = simpleObjectMapper.readValue(json, ChangeFeedProcessorItem.class);
         JsonNode jsonNode = changeFeedProcessorItem.toJsonNode();
         Assertions.assertThat(jsonNode.get("metadata").get("crts").asText()).isEqualTo("1689561600");
+
+        json = "{\"current\":{\"id\":\"1946c04a-070b-48c0-8e36-517c8d2f92ed\",\"mypk\":\"mypk-1\",\"prop\":\"Johnson\",\"_rid\":\"NopBALG34lcBAAAAAAAAAA==\",\"_self\":\"dbs/NopBAA==/colls/NopBALG34lc=/docs/NopBALG34lcBAAAAAAAAAA==/\",\"_etag\":\"\\\"00000000-0000-0000-b857-fd9e73fb01d9\\\"\",\"_attachments\":\"attachments/\",\"_ts\":1689561604},\"metadata\":{\"lsn\":176,\"crts\":1689561605,\"operationType\":\"create\"}}";
+        changeFeedProcessorItem = simpleObjectMapper.readValue(json, ChangeFeedProcessorItem.class);
+        jsonNode = changeFeedProcessorItem.toJsonNode();
+        Assertions.assertThat(jsonNode.get("metadata").get("crts").asText()).isEqualTo("1689561605");
     }
 
     @Test(groups = { "unit" })
@@ -39,5 +44,10 @@ public class ChangeFeedProcessorItemSerializerTest {
         ChangeFeedProcessorItem changeFeedProcessorItem = simpleObjectMapper.readValue(json, ChangeFeedProcessorItem.class);
         JsonNode jsonNode = changeFeedProcessorItem.toJsonNode();
         Assertions.assertThat(jsonNode.get("metadata").get("crts").asText()).isEqualTo("1689561600");
+
+        json = "{\"current\":{\"id\":\"1946c04a-070b-48c0-8e36-517c8d2f92ed\",\"mypk\":\"mypk-1\",\"prop\":\"Gates\",\"_rid\":\"NopBALG34lcBAAAAAAAAAA==\",\"_self\":\"dbs/NopBAA==/colls/NopBALG34lc=/docs/NopBALG34lcBAAAAAAAAAA==/\",\"_etag\":\"\\\"00000000-0000-0000-b857-fda256f201d9\\\"\",\"_attachments\":\"attachments/\",\"_ts\":1689561604},\"metadata\":{\"lsn\":178,\"crts\":1689561608,\"operationType\":\"replace\",\"previousImageLSN\":176}}";
+        changeFeedProcessorItem = simpleObjectMapper.readValue(json, ChangeFeedProcessorItem.class);
+        jsonNode = changeFeedProcessorItem.toJsonNode();
+        Assertions.assertThat(jsonNode.get("metadata").get("crts").asText()).isEqualTo("1689561608");
     }
 }
