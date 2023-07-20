c21d5f85b94a9fb3abe23e401f3f45b42b625e3e
Aayush Kataria <aayushkataria3011@gmail.com>
2023-07-20 06:35:31 -0700

    Updating address refresh timeouts

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/WebExceptionRetryPolicy.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/WebExceptionRetryPolicy.java
@@ -9,16 +9,18 @@ import com.azure.cosmos.implementation.directconnectivity.WebExceptionUtility;
 import com.azure.cosmos.implementation.http.HttpTimeoutPolicy;
 import com.azure.cosmos.implementation.http.HttpTimeoutPolicyDefault;
 import com.azure.cosmos.implementation.http.ResponseTimeoutAndDelays;
-import io.netty.handler.codec.http.HttpMethod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Mono;
 
+import java.net.URI;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class WebExceptionRetryPolicy implements IRetryPolicy {
     private final static Logger logger = LoggerFactory.getLogger(WebExceptionRetryPolicy.class);
+    // Address Refresh will be re-tried 3 times, please check the if condition carefully :)
+    private final static int MAX_ADDRESS_REFRESH_RETRY_COUNT = 2;
 
     private StopWatch durationTimer = new StopWatch();
     private RetryContext retryContext;
@@ -27,6 +29,8 @@ public class WebExceptionRetryPolicy implements IRetryPolicy {
     private HttpTimeoutPolicy timeoutPolicy;
     private boolean isReadRequest;
     private int retryCountTimeout = 0;
+    private int addressRefreshCount;
+    private URI locationEndpoint;
 
     public WebExceptionRetryPolicy() {
         durationTimer.start();
@@ -57,6 +61,7 @@ public class WebExceptionRetryPolicy implements IRetryPolicy {
         if (WebExceptionUtility.isNetworkFailure(e) && this.isReadRequest &&
             (webException != null && WebExceptionUtility.isReadTimeoutException(webException) &&
                 Exceptions.isSubStatusCode(webException, HttpConstants.SubStatusCodes.GATEWAY_ENDPOINT_READ_TIMEOUT))) {
+
             // if operationType AddressRefresh then just retry
             if (this.request.isAddressRefresh()) {
                 return shouldRetryAddressRefresh();
@@ -88,14 +93,33 @@ public class WebExceptionRetryPolicy implements IRetryPolicy {
             this.request.setResponseTimeout(current.getResponseTimeout());
             this.retryDelay = Duration.ofSeconds(current.getDelayForNextRequestInSeconds());
         }
+        this.locationEndpoint = request.requestContext.locationEndpointToRoute;
     }
 
     private Boolean isOutOfRetries() {
-        return this.durationTimer.getTime(TimeUnit.SECONDS) > this.timeoutPolicy.maximumRetryTimeLimit() ||
-            this.retryCountTimeout >= this.timeoutPolicy.totalRetryCount();
+        return this.retryCountTimeout >= this.timeoutPolicy.totalRetryCount();
     }
 
     private Mono<ShouldRetryResult> shouldRetryAddressRefresh() {
-            return null;
+        if (this.addressRefreshCount++ > MAX_ADDRESS_REFRESH_RETRY_COUNT) {
+            logger
+                .warn(
+                    "shouldRetryAddressRefresh() No more retrying on endpoint {}, operationType = {}, count = {}, " +
+                        "isAddressRefresh = {}",
+                    this.locationEndpoint, this.request.getOperationType(), this.addressRefreshCount, this.request.isAddressRefresh());
+            return Mono.just(ShouldRetryResult.noRetry());
         }
+
+        logger
+            .warn("shouldRetryAddressRefresh() Retrying on endpoint {}, operationType = {}, count = {}, " +
+                    "isAddressRefresh = {}, shouldForcedAddressRefresh = {}, " +
+                    "shouldForceCollectionRoutingMapRefresh = {}",
+                this.locationEndpoint, this.request.getOperationType(), this.addressRefreshCount,
+                this.request.isAddressRefresh(),
+                this.request.shouldForceAddressRefresh(),
+                this.request.forceCollectionRoutingMapRefresh);
+
+        Duration retryDelay = Duration.ZERO;
+        return Mono.just(ShouldRetryResult.retryAfter(retryDelay));
+    }
 }
