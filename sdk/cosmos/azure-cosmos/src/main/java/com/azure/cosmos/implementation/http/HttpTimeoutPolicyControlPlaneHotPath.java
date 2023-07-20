0ad4cbdfcece09229d10c346acdb042c50e09fcc
Aayush Kataria <aayushkataria3011@gmail.com>
2023-07-20 06:16:07 -0700

    updating the timeout policies

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/http/HttpTimeoutPolicyControlPlaneHotPath.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/http/HttpTimeoutPolicyControlPlaneHotPath.java
@@ -18,8 +18,8 @@ public class HttpTimeoutPolicyControlPlaneHotPath extends HttpTimeoutPolicy {
     }
 
     public List<ResponseTimeoutAndDelays> getTimeoutList() {
-        return Collections.unmodifiableList(Arrays.asList(new ResponseTimeoutAndDelays(Duration.ofSeconds(60), 0),
-            new ResponseTimeoutAndDelays(Duration.ofSeconds(60), 1),
-            new ResponseTimeoutAndDelays(Duration.ofSeconds(60), 0)));
+        return Collections.unmodifiableList(Arrays.asList(new ResponseTimeoutAndDelays(Duration.ofMillis(500), 0),
+            new ResponseTimeoutAndDelays(Duration.ofSeconds(5), 1),
+            new ResponseTimeoutAndDelays(Duration.ofSeconds(10), 0)));
     }
 }
