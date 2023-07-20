742da81691d6da5d9cc933211e18e89843ea857c
Shawn Fang <45607042+mssfang@users.noreply.github.com>
2023-07-20 12:40:49 -0700

    test proxy init

--- sdk/purview/azure-analytics-purview-workflow/src/test/java/com/azure/analytics/purview/workflow/ApprovalRequestReassignTests.java
+++ sdk/purview/azure-analytics-purview-workflow/src/test/java/com/azure/analytics/purview/workflow/ApprovalRequestReassignTests.java
@@ -8,9 +8,11 @@ import com.azure.core.http.rest.RequestOptions;
 import com.azure.core.http.rest.Response;
 import com.azure.core.util.BinaryData;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public final class ApprovalRequestReassignTests extends PurviewWorkflowClientTestBase {
+    @Disabled
     @Test
     public void testApprovalRequestReassignTests() {
         BinaryData reassignCommand =
