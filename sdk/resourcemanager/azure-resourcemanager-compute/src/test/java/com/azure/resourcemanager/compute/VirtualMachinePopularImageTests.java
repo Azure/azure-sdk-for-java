9f4613510cbb43380ab889a2bb890b9bafdc1d1e
Weidong Xu <weidxu@microsoft.com>
2023-07-25 09:33:31 +0800

    fix test for deprecated image

--- sdk/resourcemanager/azure-resourcemanager-compute/src/test/java/com/azure/resourcemanager/compute/VirtualMachinePopularImageTests.java
+++ sdk/resourcemanager/azure-resourcemanager-compute/src/test/java/com/azure/resourcemanager/compute/VirtualMachinePopularImageTests.java
@@ -30,7 +30,9 @@ public class VirtualMachinePopularImageTests extends ComputeManagementTest {
 
         rgName = generateRandomResourceName("rg", 10);
         List<Mono<VirtualMachine>> vmMonos = new ArrayList<>();
-        for (KnownWindowsVirtualMachineImage image : KnownWindowsVirtualMachineImage.values()) {
+        for (KnownWindowsVirtualMachineImage image : Arrays.stream(KnownWindowsVirtualMachineImage.values())
+            .filter(image -> image != KnownWindowsVirtualMachineImage.WINDOWS_SERVER_2019_DATACENTER_WITH_CONTAINERS_GEN2)
+            .collect(Collectors.toList())) {
             Mono<VirtualMachine> mono = computeManager.virtualMachines()
                 .define(generateRandomResourceName("vm", 10))
                 .withRegion(Region.US_SOUTH_CENTRAL)
