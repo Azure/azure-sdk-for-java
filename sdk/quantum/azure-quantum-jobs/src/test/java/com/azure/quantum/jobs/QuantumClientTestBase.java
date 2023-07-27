ec0ad2c9282da1d0a5a06453765a7c16a5a7c7f0
XField <58103249+vxfield@users.noreply.github.com>
2023-07-27 11:58:57 -0700

    Remove deprecated Quantum Inspired Optimization from tests and

--- sdk/quantum/azure-quantum-jobs/src/test/java/com/azure/quantum/jobs/QuantumClientTestBase.java
+++ sdk/quantum/azure-quantum-jobs/src/test/java/com/azure/quantum/jobs/QuantumClientTestBase.java
@@ -4,20 +4,25 @@
 package com.azure.quantum.jobs;
 
 import com.azure.core.http.HttpClient;
-import com.azure.core.test.TestBase;
 import com.azure.core.test.TestMode;
+import com.azure.core.test.TestProxyTestBase;
 import com.azure.core.util.Configuration;
-import com.azure.identity.AzureCliCredentialBuilder;
+import com.azure.identity.DefaultAzureCredentialBuilder;
 
-import java.nio.file.Files;
-
-public class QuantumClientTestBase extends TestBase {
-    private final String endpoint = Configuration.getGlobalConfiguration().get("QUANTUM_ENDPOINT");
+public class QuantumClientTestBase extends TestProxyTestBase {
+    private final String location = Configuration.getGlobalConfiguration().get("AZURE_QUANTUM_WORKSPACE_LOCATION");
     private final String subscriptionId = Configuration.getGlobalConfiguration().get(Configuration.PROPERTY_AZURE_SUBSCRIPTION_ID);
     private final String resourceGroup = Configuration.getGlobalConfiguration().get(Configuration.PROPERTY_AZURE_RESOURCE_GROUP);
-    private final String workspaceName = Configuration.getGlobalConfiguration().get("QUANTUM_WORKSPACE");
+    private final String workspaceName = Configuration.getGlobalConfiguration().get("AZURE_QUANTUM_WORKSPACE_NAME");
 
     QuantumClientBuilder getClientBuilder(HttpClient httpClient) {
+
+        System.out.println(String.format("Subscription id: %s", getSubscriptionId()));
+        System.out.println(String.format("Resource group: %s", getResourceGroup()));
+        System.out.println(String.format("Workspace: %s", getWorkspaceName()));
+        System.out.println(String.format("Location: %s", location));
+        System.out.println(String.format("Endpoint: %s", getEndpoint()));
+        System.out.println(String.format("Test mode: %s", getTestMode()));
 
         QuantumClientBuilder builder = new QuantumClientBuilder();
 
@@ -29,10 +34,11 @@ public class QuantumClientTestBase extends TestBase {
             builder.httpClient(interceptorManager.getPlaybackClient());
         } else {
             builder.httpClient(httpClient)
-                .credential(new AzureCliCredentialBuilder().build());
+                .credential(new DefaultAzureCredentialBuilder().build());
         }
 
-        return builder.subscriptionId(getSubscriptionId())
+        return builder
+            .subscriptionId(getSubscriptionId())
             .resourceGroupName(getResourceGroup())
             .workspaceName(getWorkspaceName())
             .host(getEndpoint());
@@ -41,19 +47,25 @@ public class QuantumClientTestBase extends TestBase {
     String getEndpoint() {
         return interceptorManager.isPlaybackMode()
             ? "https://localhost:8080"
-            : endpoint;
+            : String.format("https://%s.quantum.azure.com", location);
     }
 
     String getSubscriptionId() {
-        return testResourceNamer.recordValueFromConfig(subscriptionId);
+        return interceptorManager.isPlaybackMode()
+            ? testResourceNamer.recordValueFromConfig(subscriptionId)
+            : subscriptionId;
     }
 
     String getResourceGroup() {
-        return testResourceNamer.recordValueFromConfig(resourceGroup);
+        return interceptorManager.isPlaybackMode()
+            ? testResourceNamer.recordValueFromConfig(resourceGroup)
+            : resourceGroup;
     }
 
     String getWorkspaceName() {
-        return testResourceNamer.recordValueFromConfig(workspaceName);
+        return interceptorManager.isPlaybackMode()
+            ? testResourceNamer.recordValueFromConfig(workspaceName)
+            : workspaceName;
     }
 
 }
