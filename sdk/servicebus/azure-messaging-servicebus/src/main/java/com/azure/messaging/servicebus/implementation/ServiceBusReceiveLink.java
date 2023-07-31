35652504cccdea475f863289cd89f8520a232fc8
anuchandy <anuamd@hotmail.com>
2023-07-30 18:21:19 -0700

    Adding an API getSessionProperties to internal contract ServiceBusReceiveLink. This API returns a type holding both session-id and the initial locked-until, once the link is active. The client types for session require first reading the non-null session-id, followed by locked-until. Having a single API to retrieve both simplifies the code (e.g., avoid another subscription for locked-until)

--- sdk/servicebus/azure-messaging-servicebus/src/main/java/com/azure/messaging/servicebus/implementation/ServiceBusReceiveLink.java
+++ sdk/servicebus/azure-messaging-servicebus/src/main/java/com/azure/messaging/servicebus/implementation/ServiceBusReceiveLink.java
@@ -29,6 +29,19 @@ public interface ServiceBusReceiveLink extends AmqpReceiveLink, AsyncCloseable {
     Mono<OffsetDateTime> getSessionLockedUntil();
 
     /**
+     * Gets the properties of the session if the link is associated with a session enabled entity.
+     *
+     * <p>The API waits for the link to Active then read the session properties. The API returns
+     * {@link com.azure.core.amqp.exception.AmqpException} if the link has no session id property, session id property
+     * will be absent for entity that is not session enabled.  The AmqpException can also be returned if the link never
+     * gets Active.</p>
+     *
+     * @return The session properties.
+     */
+    // TODO (anu): remove getSessionId and getSessionLockedUntil APIs in favor if this API.
+    Mono<SessionProperties> getSessionProperties();
+
+    /**
      * Updates the disposition status of a message with corresponding lock token.
      *
      * @param lockToken Lock token of message.
@@ -38,4 +51,22 @@ public interface ServiceBusReceiveLink extends AmqpReceiveLink, AsyncCloseable {
      */
     // TODO (anu,connie): remove updateDisposition from this contract as it is now exported by the updated base AmqpReceiveLink.
     Mono<Void> updateDisposition(String lockToken, DeliveryState deliveryState);
+
+    class SessionProperties {
+        private final String sessionId;
+        private final OffsetDateTime sessionLockedUntil;
+
+        public SessionProperties(String sessionId, OffsetDateTime sessionLockedUntil) {
+            this.sessionId = sessionId;
+            this.sessionLockedUntil = sessionLockedUntil;
+        }
+
+        public String getSessionId() {
+            return sessionId;
+        }
+
+        public OffsetDateTime getSessionLockedUntil() {
+            return sessionLockedUntil;
+        }
+    }
 }
