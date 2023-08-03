de92d04c08a8b4bafa4baeadc90ed9c873c6cb44
vivekmore@microsoft.com <vivekmore@microsoft.com>
2023-08-03 12:17:53 +0530

    Updated version order to fix lint error

--- sdk/communication/azure-communication-callautomation/src/main/java/com/azure/communication/callautomation/CallAutomationServiceVersion.java
+++ sdk/communication/azure-communication-callautomation/src/main/java/com/azure/communication/callautomation/CallAutomationServiceVersion.java
@@ -10,13 +10,13 @@ import com.azure.core.util.ServiceVersion;
  */
 public enum CallAutomationServiceVersion implements ServiceVersion {
     /**
-     * Service version {@code 2023-06-15-preview}.
-     */
-    V2023_06_15_PREVIEW("2023-06-15-preview"),
-    /**
      * Service version {@code 2023-03-06}.
      */
-    V2023_03_06("2023-03-06");
+    V2023_03_06("2023-03-06"),
+    /**
+     * Service version {@code 2023-06-15-preview}.
+     */
+    V2023_06_15_PREVIEW("2023-06-15-preview");
 
     private final String version;
 
