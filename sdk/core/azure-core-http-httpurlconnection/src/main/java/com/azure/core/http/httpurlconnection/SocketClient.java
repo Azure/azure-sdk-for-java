e2beef79c2d9d12f1bbb3b1e9fd6b4d08c79635d
Kyle Simmonds <kylesimmonds97@gmail.com>
2023-08-07 18:59:32 +1200

    Response builder added. SimpleServer removed.

--- sdk/core/azure-core-http-httpurlconnection/src/main/java/com/azure/core/http/httpurlconnection/SocketClient.java
+++ sdk/core/azure-core-http-httpurlconnection/src/main/java/com/azure/core/http/httpurlconnection/SocketClient.java
@@ -3,12 +3,9 @@ package com.azure.core.http.httpurlconnection;
 import com.azure.core.http.HttpHeader;
 import com.azure.core.http.HttpHeaderName;
 import com.azure.core.http.HttpHeaders;
-import com.azure.core.http.HttpMethod;
-import com.azure.core.util.BinaryData;
 
 import java.io.*;
 import java.net.*;
-import java.nio.charset.StandardCharsets;
 
 // A socket client to be used to handle PATCH requests
 
@@ -24,64 +21,95 @@ public class SocketClient {
 
     // If no port is specified, set the port to 80
     public SocketClient(String host) {
-        this.host = host;
-        this.port = 80;
+        this(host, 80);
     }
 
     // todo - this should return a HttpResponse
     //  need a response handler to do this
     public void sendPatchRequest(HttpRequest httpRequest) throws IOException {
         String request = buildPatchRequest(httpRequest);
-        try (Socket socket = new Socket(httpRequest.getUrl().getHost(), httpRequest.getUrl().getPort())) {
-            OutputStream outputStream = socket.getOutputStream();
-            outputStream.write(request.getBytes(StandardCharsets.UTF_8));
+        try (Socket socket = new Socket(host, port)) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            OutputStream out = socket.getOutputStream();
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(out);
+            outputStreamWriter.write(request);
+            outputStreamWriter.flush();
+
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = in.readLine()) != null) {
+                sb.append(line);
+                sb.append("\r\n");
+            }
+
+            HttpResponse response = getResponse(httpRequest, sb.toString());
+
+            out.close();
+            in.close();
+            System.out.println(response);
         }
+    }
+
+    // This method extracts an HttpResponse object from the response string
+    // todo - this is clunky. It may be better to pass an inputstream
+    //  to this method and have it read line by line
+    private HttpResponse getResponse(HttpRequest request, String responseString) {
+        // Separate the status line and headers from the body
+        String headerField = responseString.split("\r\n\r\n")[0];
+        byte[] body = responseString.split("\r\n\r\n")[1].getBytes();
+
+        String statusLine = headerField.split("\r\n")[0];
+        // Retrieve the status code from the status line
+        int dotIndex = statusLine.indexOf('.');
+        int statusCode = Integer.parseInt(statusLine.substring(dotIndex+3, dotIndex+5));
+
+        String[] headers = headerField.split("\r\n");
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+
+        for (int i = 1; i < headers.length; i++) {
+            String k = headers[i].split(": ")[0];
+            String v = headers[i].split(": ")[1];
+            // Convert the String key to a HttpHeaderName
+            // Add the key and value to the headers
+            // todo - may need to check that the HttpHeaderName is valid
+            httpHeaders.set(HttpHeaderName.fromString(k), v);
+        }
+        return new HttpResponse(request, httpHeaders, statusCode, body);
     }
 
     // todo - make this a generic request builder or leave as strictly PATCH?
     private String buildPatchRequest(HttpRequest httpRequest) {
         final StringBuilder request = new StringBuilder();
         // Add the status line
-        // todo - hard code this to use PATCH?
         request.append(httpRequest.getHttpMethod())
             .append(" ")
             .append(httpRequest.getUrl().getPath())
-            .append(" HTTP/1.1")
-            .append("\r\n");
+            .append(" HTTP/1.0");
+        System.out.println(request);
         // Add the headers
-        for (HttpHeader headerLine : httpRequest.getHeaders()) {
-            request.append(headerLine.getName())
-                .append(": ")
-                .append(headerLine.getValue())
-                .append("\r\n");
+        // First check if there are any headers to add
+        if (httpRequest.getHeaders().getSize() > 0) {
+            request.append("\r\n");
+            for (HttpHeader headerLine : httpRequest.getHeaders()) {
+                request.append(headerLine.getName())
+                    .append(": ")
+                    .append(headerLine.getValue())
+                    .append("\r\n");
+            }
         }
         // Add the body
+        // Check if there is a body to add
         // todo - add content length check
-        request.append("\r\n");
-        request.append(httpRequest.getBody().toString());
-
+        if (httpRequest.getBody() != null) {
+            request.append("\r\n");
+            request.append(httpRequest.getBody().toString());
+        }
+        // Add end of file
+        // todo - is this only needed when there is no content-length header?
+        request.append("\r\n\r\n");
+        System.out.println(request);
         return request.toString();
-    }
-
-
-    // Testing/development code
-    // Not to be included in final product
-    public static void main(String[] args) throws IOException {
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.set(HttpHeaderName.HOST, "localhost");
-        headers.set(HttpHeaderName.CONTENT_TYPE, "text/html");
-
-        String test = "This is the body of the request.";
-        BinaryData bd = BinaryData.fromBytes(test.getBytes());
-
-        URL url = new URL("http", "localhost", 9000, "/");
-
-        HttpRequest request = new HttpRequest(HttpMethod.PATCH, url, headers, bd);
-
-        SocketClient client = new SocketClient(request.getUrl().getHost(), request.getUrl().getPort());
-        client.sendPatchRequest(request);
-
-        // HttpResponse response = client.sendPatchRequest(request);
     }
 }
