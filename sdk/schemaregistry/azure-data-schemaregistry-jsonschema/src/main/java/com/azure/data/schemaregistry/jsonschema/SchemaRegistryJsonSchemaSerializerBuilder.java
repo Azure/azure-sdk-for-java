225ad3299a156330ea0c597ffb145606d8d25f12
Connie Yau <conniey@microsoft.com>
2023-08-08 13:33:35 -0700

    Add isAutoRegister, serializerAdapater to builder.

--- sdk/schemaregistry/azure-data-schemaregistry-jsonschema/src/main/java/com/azure/data/schemaregistry/jsonschema/SchemaRegistryJsonSchemaSerializerBuilder.java
+++ sdk/schemaregistry/azure-data-schemaregistry-jsonschema/src/main/java/com/azure/data/schemaregistry/jsonschema/SchemaRegistryJsonSchemaSerializerBuilder.java
@@ -5,6 +5,7 @@ package com.azure.data.schemaregistry.jsonschema;
 
 import com.azure.core.util.CoreUtils;
 import com.azure.core.util.logging.ClientLogger;
+import com.azure.core.util.serializer.SerializerAdapter;
 import com.azure.data.schemaregistry.SchemaRegistryAsyncClient;
 
 import java.util.Objects;
@@ -15,9 +16,33 @@ import java.util.Objects;
  * @see SchemaRegistryJsonSchemaSerializer
  */
 public final class SchemaRegistryJsonSchemaSerializerBuilder {
-    private final ClientLogger logger = new ClientLogger(SchemaRegistryJsonSchemaSerializerBuilder.class);
-    private SchemaRegistryAsyncClient schemaRegistryAsyncClient;
+    private static final ClientLogger LOGGER = new ClientLogger(SchemaRegistryJsonSchemaSerializerBuilder.class);
+
+    private Boolean autoRegisterSchemas;
+    private JsonSchemaGenerator jsonSchemaGenerator;
     private String schemaGroup;
+    private SchemaRegistryAsyncClient schemaRegistryAsyncClient;
+    private SerializerAdapter serializerAdapter;
+
+    /**
+     * <p>If true, the serializer will register schemas against Azure Schema Registry service under the specified
+     * group if it fails to find an existing schema to serialize.  See
+     * <a href="https://learn.microsoft.com/azure/event-hubs/schema-registry-overview">Azure Schema Registry
+     * documentation</a> for a description of schema registration behavior.</p>
+     *
+     * <p>If specified false, serializer will query the service for an existing ID given schema content.
+     * Serialization will fail if the schema has not been pre-created.</p>
+     *
+     * Auto-registration is <strong>NOT RECOMMENDED</strong> for production scenarios.
+     *
+     * @param autoRegisterSchemas flag for schema auto-registration
+     *
+     * @return The updated {@link SchemaRegistryJsonSchemaSerializerBuilder} instance.
+     */
+    public SchemaRegistryJsonSchemaSerializerBuilder autoRegisterSchemas(boolean autoRegisterSchemas) {
+        this.autoRegisterSchemas = autoRegisterSchemas;
+        return this;
+    }
 
     /**
      * Specifies schema group for interacting with Azure Schema Registry service.
@@ -31,6 +56,18 @@ public final class SchemaRegistryJsonSchemaSerializerBuilder {
      */
     public SchemaRegistryJsonSchemaSerializerBuilder schemaGroup(String schemaGroup) {
         this.schemaGroup = schemaGroup;
+        return this;
+    }
+
+    /**
+     * JSON serializer adapter to use. If none is supplied, the default is used.
+     *
+     * @param serializerAdapter Serializer to use.
+     *
+     * @return updated {@link SchemaRegistryJsonSchemaSerializerBuilder} instance.
+     */
+    public SchemaRegistryJsonSchemaSerializerBuilder serializer(SerializerAdapter serializerAdapter) {
+        this.serializerAdapter = serializerAdapter;
         return this;
     }
 
@@ -55,6 +92,7 @@ public final class SchemaRegistryJsonSchemaSerializerBuilder {
      * @return updated {@link SchemaRegistryJsonSchemaSerializerBuilder} instance.
      */
     public SchemaRegistryJsonSchemaSerializerBuilder jsonSchemaGenerator(JsonSchemaGenerator jsonSchemaGenerator) {
+        this.jsonSchemaGenerator = jsonSchemaGenerator;
         return this;
     }
 
@@ -63,19 +101,30 @@ public final class SchemaRegistryJsonSchemaSerializerBuilder {
      *
      * @return A new instance of {@link SchemaRegistryJsonSchemaSerializer}.
      *
-     * @throws NullPointerException if {@link #schemaRegistryClient(SchemaRegistryAsyncClient)} is {@code null}
+     * @throws NullPointerException if {@link #schemaRegistryClient(SchemaRegistryAsyncClient)} or
+     *     {@link #jsonSchemaGenerator(JsonSchemaGenerator)} is {@code null}.
+     * @throws IllegalStateException if {@link #autoRegisterSchemas(boolean)} is true but
+     *     {@link #schemaGroup(String)} is not set.
      */
     public SchemaRegistryJsonSchemaSerializer buildSerializer() {
         if (Objects.isNull(schemaRegistryAsyncClient)) {
-            throw logger.logExceptionAsError(new NullPointerException("'schemaRegistryAsyncClient' cannot be null."));
+            throw LOGGER.logExceptionAsError(new NullPointerException("'schemaRegistryAsyncClient' cannot be null."));
         }
 
-        if (CoreUtils.isNullOrEmpty(schemaGroup)) {
-            throw logger.logExceptionAsError(new IllegalStateException("'schemaGroup' cannot be null."));
+        if (Objects.isNull(jsonSchemaGenerator)) {
+            throw LOGGER.logExceptionAsError(new NullPointerException("'jsonSchemaGenerator' cannot be null."));
         }
 
-        final SerializerOptions options = new SerializerOptions(schemaGroup, 100);
+        final boolean isAutoRegister = autoRegisterSchemas != null && autoRegisterSchemas;
 
-        return new SchemaRegistryJsonSchemaSerializer(schemaRegistryAsyncClient, options);
+        if (isAutoRegister && CoreUtils.isNullOrEmpty(schemaGroup)) {
+            throw LOGGER.logExceptionAsError(new IllegalStateException(
+                "'schemaGroup' cannot be null or empty when 'autoRegisterSchema' is true."));
+        }
+
+        final SerializerOptions options = new SerializerOptions(schemaGroup, isAutoRegister, 100);
+
+        return new SchemaRegistryJsonSchemaSerializer(schemaRegistryAsyncClient, jsonSchemaGenerator, options);
     }
 }
+
--- sdk/schemaregistry/azure-data-schemaregistry-jsonschema/src/main/java/com/azure/data/schemaregistry/jsonschema/SerializerOptions.java
+++ sdk/schemaregistry/azure-data-schemaregistry-jsonschema/src/main/java/com/azure/data/schemaregistry/jsonschema/SerializerOptions.java
@@ -10,6 +10,7 @@ import com.azure.core.annotation.Immutable;
  */
 @Immutable
 class SerializerOptions {
+    private final boolean autoRegisterSchemas;
     private final int maxCacheSize;
     private final String schemaGroup;
 
@@ -17,10 +18,12 @@ class SerializerOptions {
      * Creates a new instance.
      *
      * @param schemaGroup Optional schema group when registering a schema is required.
+     * @param autoRegisterSchemas {@code true} to register schema if it does not exist, {@code false} otherwise.
      * @param maxCacheSize The maximum cache size for the serializer.
      */
-    SerializerOptions(String schemaGroup, int maxCacheSize) {
+    SerializerOptions(String schemaGroup, boolean autoRegisterSchemas, int maxCacheSize) {
         this.schemaGroup = schemaGroup;
+        this.autoRegisterSchemas = autoRegisterSchemas;
         this.maxCacheSize = maxCacheSize;
     }
 
