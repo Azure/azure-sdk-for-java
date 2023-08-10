6dc4fe3d280eb8608ae6bb27266a1c6c72fc0cc5
yuwe <yuwe@microsoft.com>
2023-08-10 11:34:28 +0800

    use environment credential

--- sdk/spring/spring-cloud-azure-integration-test-appconfiguration-config/src/test/java/com/azure/spring/cloud/config/connectionstring/CustomSecretClient.java
+++ sdk/spring/spring-cloud-azure-integration-test-appconfiguration-config/src/test/java/com/azure/spring/cloud/config/connectionstring/CustomSecretClient.java
@@ -1,24 +1,13 @@
 package com.azure.spring.cloud.config.connectionstring;
 
-import com.azure.core.credential.TokenCredential;
-import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.azure.security.keyvault.secrets.SecretClientBuilder;
 import com.azure.spring.cloud.appconfiguration.config.SecretClientCustomizer;
 import com.azure.spring.cloud.config.BaseCustomClient;
 import org.springframework.core.env.Environment;
 
 public class CustomSecretClient extends BaseCustomClient implements SecretClientCustomizer {
-    private final Environment environment;
-
     public CustomSecretClient(Environment environment) {
-        this.environment = environment;
-    }
-
-    TokenCredential buildCredential() {
-        String azureClientId = environment.getProperty("AZURE_CLIENT_ID_MANAGED_IDENTITY");
-        return new DefaultAzureCredentialBuilder()
-            .managedIdentityClientId(azureClientId)
-            .build();
+        super(environment);
     }
 
     @Override
