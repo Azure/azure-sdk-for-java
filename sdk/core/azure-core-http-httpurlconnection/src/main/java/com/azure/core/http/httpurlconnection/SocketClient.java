1c6e3d131557a32cd309692bbe7be9da3fdc20bd
Kyle Simmonds <kylesimmonds97@gmail.com>
2023-08-11 07:56:48 +1200

    SSL support added to SocketClient.

--- sdk/core/azure-core-http-httpurlconnection/src/main/java/com/azure/core/http/httpurlconnection/SocketClient.java
+++ sdk/core/azure-core-http-httpurlconnection/src/main/java/com/azure/core/http/httpurlconnection/SocketClient.java
@@ -4,43 +4,89 @@ import com.azure.core.http.HttpHeader;
 import com.azure.core.http.HttpHeaderName;
 import com.azure.core.http.HttpHeaders;
 
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
 import java.io.*;
-import java.net.*;
-import java.util.Arrays;
+import java.net.ProtocolException;
+import java.net.Socket;
 
 // A socket client to be used to handle PATCH requests
 
 public class SocketClient {
 
     private final String host;
-    private final int port;
+    private int port;
 
     public SocketClient(String host, int port) {
         this.host = host;
         this.port = port;
     }
 
-    // If no port is specified, set the port to 80
     public SocketClient(String host) {
-        this(host, 80);
+        this(host, 443);
     }
 
     public HttpResponse sendPatchRequest(HttpRequest httpRequest) throws IOException {
-        String request = buildPatchRequest(httpRequest);
-        try (Socket socket = new Socket(host, port)) {
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-            OutputStream out = socket.getOutputStream();
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(out);
-            outputStreamWriter.write(request);
-            outputStreamWriter.flush();
-
-            HttpResponse response = buildResponse(httpRequest, in);
-
-            out.close();
-            in.close();
-            return response;
+        if (httpRequest.getUrl().getProtocol().equals("https")) {
+            SSLSocketFactory sslSocketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+            try (SSLSocket socket = (SSLSocket) sslSocketFactory.createSocket(host, port)) {
+                return doInputOutput(httpRequest, socket);
+            }
+        } else if (httpRequest.getUrl().getProtocol().equals("http")){
+            // Client is using HTTP, change port number 80
+            // Use a regular socket
+            this.port = 80;
+            try (Socket socket = new Socket(host, port)) {
+                return doInputOutput(httpRequest, socket);
+            }
         }
+        // A protocol other than HTTP or HTTPS has been specified, throw an exception
+        throw new ProtocolException("Only HTTP and HTTPS are supported by this client.");
+    }
+
+    private HttpResponse doInputOutput(HttpRequest httpRequest, Socket socket) throws IOException {
+        String request = buildPatchRequest(httpRequest);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        OutputStream out = socket.getOutputStream();
+        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(out);
+        outputStreamWriter.write(request);
+        outputStreamWriter.flush();
+
+        HttpResponse response = buildResponse(httpRequest, in);
+
+        out.close();
+        in.close();
+        return response;
+    }
+
+    private String buildPatchRequest(HttpRequest httpRequest) {
+        final StringBuilder request = new StringBuilder();
+        // Add the status line
+        request.append("PATCH")
+            .append(" ")
+            .append(httpRequest.getUrl().getPath())
+            .append(" HTTP/1.1")
+            .append("\r\n");
+        // Add the headers
+        // First check if there are any headers to add
+        if (httpRequest.getHeaders().getSize() > 0) {
+            for (HttpHeader headerLine : httpRequest.getHeaders()) {
+                request.append(headerLine.getName())
+                    .append(": ")
+                    .append(headerLine.getValue())
+                    .append("\r\n");
+            }
+        }
+        // Add the body if there is a body to add
+        if (httpRequest.getBody() != null) {
+            request.append("\r\n")
+                .append(httpRequest.getBodyAsBinaryData().toString())
+                .append("\r\n");
+        }
+        // Add carriage return linefeed to mark end of message
+        request.append("\r\n");
+        return request.toString();
     }
 
     // This method reads the response line by line and constructs an HttpResponse object
@@ -71,35 +117,5 @@ public class SocketClient {
         byte[] body = bodyString.toString().getBytes();
 
         return new HttpResponse(request, headers, statusCode, body);
-    }
-
-    private String buildPatchRequest(HttpRequest httpRequest) {
-        final StringBuilder request = new StringBuilder();
-        // Add the status line
-        // todo - hard code the method as PATCH
-        request.append(httpRequest.getHttpMethod())
-            .append(" ")
-            .append(httpRequest.getUrl().getPath())
-            .append(" HTTP/1.0")
-            .append("\r\n");
-        // Add the headers
-        // First check if there are any headers to add
-        if (httpRequest.getHeaders().getSize() > 0) {
-            for (HttpHeader headerLine : httpRequest.getHeaders()) {
-                request.append(headerLine.getName())
-                    .append(": ")
-                    .append(headerLine.getValue())
-                    .append("\r\n");
-            }
-        }
-        // Add the body if there is a body to add
-        if (httpRequest.getBody() != null) {
-            request.append("\r\n")
-                .append(httpRequest.getBody().toString())
-                .append("\r\n");
-        }
-        // Add carriage return linefeed to mark end of message
-        request.append("\r\n");
-        return request.toString();
     }
 }
