d0b274604d42cd0c0990eac9e39ef71378ce7fbe
Connie Yau <conniey@microsoft.com>
2023-08-09 18:04:53 -0700

    Fix script path for test-resources-pre.ps1

--- sdk/confidentialledger/azure-security-confidentialledger/test-resources-pre.ps1
+++ sdk/confidentialledger/azure-security-confidentialledger/test-resources-pre.ps1
@@ -25,7 +25,7 @@ if (!$PSBoundParameters.ContainsKey('ErrorAction')) {
     $ErrorActionPreference = 'Stop'
 }
 
-Import-Module -Name $PSScriptRoot/../../eng/common/scripts/X509Certificate2 -Verbose
+Import-Module -Name $PSScriptRoot/../../../eng/common/scripts/X509Certificate2 -Verbose
 $cert = New-X509Certificate2 -SubjectName 'E=opensource@microsoft.com, CN=Azure SDK, OU=Azure SDK, O=Microsoft, L=Frisco, S=TX, C=US' -ValidDays 365
 
 $pem = (Format-X509Certificate2 -Certificate $cert).ReplaceLineEndings([string]::Empty)
