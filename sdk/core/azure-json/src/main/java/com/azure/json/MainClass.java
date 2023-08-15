278ce389317e93e1cf269f760bd211a34e95ea9a
Zachary Knight <96271345+ZacharyKn1ght@users.noreply.github.com>
2023-08-16 10:03:11 +1200

    Working on examples for demonstrations.

--- sdk/core/azure-json/src/main/java/com/azure/json/MainClass.java
+++ sdk/core/azure-json/src/main/java/com/azure/json/MainClass.java
@@ -1,5 +1,9 @@
 package com.azure.json;
-import com.azure.json.InvalidJsonDataTypeException; 
+import com.azure.json.InvalidJsonDataTypeException;
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * This class acts purely as a playground for testing and playing around with
@@ -14,8 +18,39 @@ import com.azure.json.InvalidJsonDataTypeException;
  * NOTE: can be removed later.
  */
 public class MainClass {
-    public static void main(String[] args) throws InvalidJsonDataTypeException {
+    // Utility static fields
+    public static List<String> firstNames = new ArrayList<>();
+    public static List<String> lastNames = new ArrayList<>();
+    public static List<String> countries = new ArrayList<>();
+    public static final Random randomiser = new Random();
 
+    // Initialising the static members/fields
+    static {
+        // Initialising firstNames List
+        String[] firstNamesExamples = { "James", "Mary", "Robert", "Patricia", "John", "Jennifer", "Michael", "Linda", "David", "Elizabeth", "William", "Barbara", "Richard", "Susan", "Joseph", "Jessica" };
+        Collections.addAll(firstNames, firstNamesExamples);
+        System.out.println(firstNames);
+
+        // Initialising lastNames List
+        String[] lastNamesExamples = { "Anderson", "Jones", "Campbell", "Taylor", "Williams", "Miller", "Johnson", "Wilson", "Simpson", "Harris", "Evans", "Skipper", "McNeill", "Murphy", "Robinson", "Allen", "Knight" };
+        Collections.addAll(lastNames, lastNamesExamples);
+        System.out.println(lastNames);
+
+        // Initialising countries List
+        String[] countryExamples = { "New Zealand", "Australia", "United States", "England", "Ireland", "South Africa", "Canada" };
+        Collections.addAll(countries, countryExamples);
+        System.out.println(countries);
+    }
+
+    public static void main(String[] args) throws InvalidJsonDataTypeException {
+        example_1();
+
+        // example_1_simple_json_object();
+        // example_2_simple_json_array();
+        // example_3_chaining_json_object();
+        // example_4_chaining_json_array();
+
+/*
         int[] numArray = new int[10];
         numArray[0] = 3;
         numArray[5] = 27;
@@ -38,24 +73,116 @@ public class MainClass {
                 .toJson();
         System.out.println(arrayTest);
 
-        
-        // Testing that LinkedHashMap is working as expected 
-        JsonObject testingPropertyOrder = new JsonObject(); 
-        for(int i = 0; i < 10; i++) { 
+        // Testing that LinkedHashMap is working as expected
+        JsonObject testingPropertyOrder = new JsonObject();
+        for(int i = 0; i < 10; i++) {
             testingPropertyOrder.addProperty(
-                "Property"+Integer.toString(i), 
+                "Property"+Integer.toString(i),
                 "value"+Integer.toString(i)
             );
         }
         System.out.println(testingPropertyOrder);
+*/
 
         /*
-
         JsonObject meme2 = new JsonObject();
         meme2.addProperty("DavidTester", "fiveGuys");
         meme2.addProperty("lkoveTester", "fasdsafdsafys");
         String output2 = meme2.toJson();
         System.out.println(output2);
         */
+    }
+
+    // String[] firstNamesExamples = { "James", "Mary", "Robert", "Patricia", "John", "Jennifer", "Michael", "Linda", "David", "Elizabeth", "William", "Barbara", "Richard", "Susan", "Joseph", "Jessica" };
+    // String[] lastNamesExamples = { "Anderson", "Jones", "Campbell", "Taylor", "Williams", "Miller", "Johnson", "Wilson", "Simpson", "Harris", "Evans", "Skipper", "McNeill", "Murphy", "Robinson", "Allen", "Knight" };
+    // String[] countryExamples = { "New Zealand", "Australia", "United States", "England", "Ireland", "South Africa", "Canada" };
+
+    public static void example_1() {
+        JsonObject jsonObj = new JsonObject();
+
+        jsonObj.addProperty("James", "Anderson")
+            .addProperty("Michael", "Campbell")
+            .addProperty("a", "b");
+
+        System.out.println(jsonObj);
+
+        jsonObj.removeProperty("Michael");
+        System.out.println(jsonObj);
+    }
+
+    public static void example_1_simple_json_object() {
+        JsonObject jsonObject = new JsonObject();
+
+        int properties = 10;
+        for(int i = 0; i < properties; i++) {
+            jsonObject.addProperty(
+                firstNames.get(randomiser.nextInt(firstNames.size())),
+                countries.get(randomiser.nextInt(countries.size()))
+            );
+        }
+
+        System.out.println("EXAMPLE 1 - SIMPLE JSON OBJECT");
+        System.out.println("Object:");
+        System.out.println(jsonObject);
+        System.out.println("Getting properties:");
+        System.out.println(jsonObject.getProperty(firstNames.get(randomiser.nextInt(firstNames.size()))));
+    }
+
+    public static void example_2_simple_json_array() {
+        JsonArray countryArray = new JsonArray();
+
+        int elements = 10;
+        for(int i = 0; i < elements; i++) {
+            countryArray.addElement(
+                new JsonString(countries.get(randomiser.nextInt(countries.size())))
+            );
+        }
+
+        System.out.println(countryArray);
+    }
+
+    public static void example_3_chaining_json_object() {
+        JsonObject jsonObject = new JsonObject();
+
+        // Showcasing chaining of JsonObject method calls
+        jsonObject.addProperty(
+            firstNames.get(randomiser.nextInt(firstNames.size())),
+            countries.get(randomiser.nextInt(countries.size()))
+        ).addProperty(
+            firstNames.get(randomiser.nextInt(firstNames.size())),
+            countries.get(randomiser.nextInt(countries.size()))
+        ).addProperty(
+            firstNames.get(randomiser.nextInt(firstNames.size())),
+            countries.get(randomiser.nextInt(countries.size()))
+        ).addProperty(
+            firstNames.get(randomiser.nextInt(firstNames.size())),
+            countries.get(randomiser.nextInt(countries.size()))
+        ).addProperty(
+            firstNames.get(randomiser.nextInt(firstNames.size())),
+            countries.get(randomiser.nextInt(countries.size()))
+        );
+
+        System.out.println(jsonObject);
+    }
+
+    public static void example_4_chaining_json_array() {
+        JsonArray countryArray = new JsonArray();
+
+        // Showcasing chaining of JsonArray method calls
+        countryArray.addElement(new JsonString(countries.get(randomiser.nextInt(countries.size()))))
+            .addElement(new JsonString(countries.get(randomiser.nextInt(countries.size()))))
+            .addElement(new JsonString(countries.get(randomiser.nextInt(countries.size()))))
+            .addElement(new JsonString(countries.get(randomiser.nextInt(countries.size()))))
+            .addElement(new JsonString(countries.get(randomiser.nextInt(countries.size()))))
+            .addElement(new JsonString(countries.get(randomiser.nextInt(countries.size()))))
+            .addElement(new JsonString(countries.get(randomiser.nextInt(countries.size()))))
+            .addElement(new JsonString(countries.get(randomiser.nextInt(countries.size()))))
+            .addElement(new JsonString(countries.get(randomiser.nextInt(countries.size()))));
+
+        System.out.println(countryArray);
+    }
+
+    public static void example_5_get_propert_json_object() {
+
     }
 }
