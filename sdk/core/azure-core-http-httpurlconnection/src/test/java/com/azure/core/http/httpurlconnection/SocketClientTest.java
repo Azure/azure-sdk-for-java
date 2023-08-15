4bfb8327a8181053ab51795a19e729cf67bee8e3
lewisKendall-Jones <60380722+lewisKendall-Jones@users.noreply.github.com>
2023-08-16 03:53:32 +1200

    New tests defined

--- sdk/core/azure-core-http-httpurlconnection/src/test/java/com/azure/core/http/httpurlconnection/SocketClientTest.java
+++ sdk/core/azure-core-http-httpurlconnection/src/test/java/com/azure/core/http/httpurlconnection/SocketClientTest.java
@@ -1,10 +1,10 @@
 package com.azure.core.http.httpurlconnection;
 
 public class SocketClientTest {
+    // To be built upon after HttpUrlConnectionClient's core tests are implemented.
+    // buildPatchRequest tests
 
-    // buildPatchRequest test
+    // sendPatchRequest tests
 
-    // sendPatchRequest test
-
-    // buildResponse test
+    // buildResponse tests
 }
