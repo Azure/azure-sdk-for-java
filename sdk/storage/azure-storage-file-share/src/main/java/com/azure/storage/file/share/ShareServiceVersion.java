486af5b44bccc8b53e2cac63d2621a68740ff914
azure-sdk <azuresdk@microsoft.com>
2023-08-18 13:10:27 -0700

    Reset sources for azure-storage-file-share to the release version 12.19.0.

--- sdk/storage/azure-storage-file-share/src/main/java/com/azure/storage/file/share/ShareServiceVersion.java
+++ sdk/storage/azure-storage-file-share/src/main/java/com/azure/storage/file/share/ShareServiceVersion.java
@@ -93,17 +93,7 @@ public enum ShareServiceVersion implements ServiceVersion {
     /**
      * Service version {@code 2023-01-03}.
      */
-    V2023_01_03("2023-01-03"),
-
-    /**
-     * Service version {@code 2023-05-03}.
-     */
-    V2023_05_03("2023-05-03"),
-
-    /**
-     * Service version {@code 2023-08-03}.
-     */
-    V2023_08_03("2023-08-03");
+    V2023_01_03("2023-01-03");
 
     private final String version;
 
@@ -125,6 +115,6 @@ public enum ShareServiceVersion implements ServiceVersion {
      * @return the latest {@link ShareServiceVersion}
      */
     public static ShareServiceVersion getLatest() {
-        return V2023_08_03;
+        return V2023_01_03;
     }
 }
