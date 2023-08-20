c6c271a15d9d4300de5e9c20627e66cc310f2eef
Jamie <jamie.styx@gmail.com>
2023-08-20 13:31:04 +0900

    added comments in code to increase readability

--- sdk/core/azure-core-http-httpurlconnection/src/main/java/com/azure/core/http/httpurlconnection/AsyncHttpUrlConnectionClient.java
+++ sdk/core/azure-core-http-httpurlconnection/src/main/java/com/azure/core/http/httpurlconnection/AsyncHttpUrlConnectionClient.java
@@ -6,15 +6,32 @@ import reactor.core.publisher.Mono;
 
 import java.util.concurrent.CompletableFuture;
 
+/**
+ * This client provides asynchronous capabilities to send HTTP requests using HttpUrlConnection.
+ * It wraps around the synchronous `HttpUrlConnectionClient` and returns a `CompletableFuture`.
+ */
 public class AsyncHttpUrlConnectionClient {
+
+    // The synchronous HttpUrlConnection client that performs the actual HTTP calls.
     private final HttpUrlConnectionClient httpUrlConnectionClient;
 
+    // Constructor initializes the underlying synchronous HttpUrlConnection client.
     public AsyncHttpUrlConnectionClient() {
         this.httpUrlConnectionClient = new HttpUrlConnectionClient();
     }
 
+    /**
+     * Sends an asynchronous HTTP request.
+     *
+     * @param httpRequest The HTTP request to be sent.
+     * @return A CompletableFuture that represents the HttpResponse. This allows the caller
+     *         to work with the response once it's available without blocking the main thread.
+     */
     public CompletableFuture<HttpResponse> send(HttpRequest httpRequest) {
+        // Convert the synchronous call to asynchronous using Reactor's Mono
         Mono<HttpResponse> responseMono = httpUrlConnectionClient.send(httpRequest);
+
+        // Convert the Mono to CompletableFuture to provide standard Java async capabilities
         return responseMono.toFuture();
     }
 }
