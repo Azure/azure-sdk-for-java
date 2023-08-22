22e724fbc8faa5f224d8b3297f09f4edf54bc4b6
Josh Anderson <jbanderson2001@gmail.com>
2023-08-22 15:45:04 +1200

    Changes made to Async Client

--- sdk/core/azure-core-http-httpurlconnection/src/main/java/com/azure/core/http/httpurlconnection/AsyncHttpUrlConnectionClient.java
+++ sdk/core/azure-core-http-httpurlconnection/src/main/java/com/azure/core/http/httpurlconnection/AsyncHttpUrlConnectionClient.java
@@ -2,8 +2,12 @@ package com.azure.core.http.httpurlconnection;
 
 import com.azure.core.http.HttpRequest;
 import com.azure.core.http.HttpResponse;
+import com.azure.core.util.Context;
+import com.azure.core.util.Contexts;
+import com.azure.core.util.ProgressReporter;
 import reactor.core.publisher.Mono;
 
+import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
 
 /**
@@ -11,13 +15,16 @@ import java.util.concurrent.CompletableFuture;
  * It wraps around the synchronous `HttpUrlConnectionClient` and returns a `CompletableFuture`.
  */
 public class AsyncHttpUrlConnectionClient {
+    private static final String AZURE_EAGERLY_READ_RESPONSE = "azure-eagerly-read-response";
+    private static final String AZURE_IGNORE_RESPONSE_BODY = "azure-ignore-response-body";
+    private static final String AZURE_EAGERLY_CONVERT_HEADERS = "azure-eagerly-convert-headers";
 
     // The synchronous HttpUrlConnection client that performs the actual HTTP calls.
-    private final HttpUrlConnectionClient httpUrlConnectionClient;
+    private final com.azure.core.http.httpurlconnection.HttpUrlConnectionClient httpUrlConnectionClient;
 
     // Constructor initializes the underlying synchronous HttpUrlConnection client.
     public AsyncHttpUrlConnectionClient() {
-        this.httpUrlConnectionClient = new HttpUrlConnectionClient();
+        this.httpUrlConnectionClient = new com.azure.core.http.httpurlconnection.HttpUrlConnectionClient();
     }
 
     /**
@@ -27,12 +34,42 @@ public class AsyncHttpUrlConnectionClient {
      * @return A CompletableFuture that represents the HttpResponse. This allows the caller
      *         to work with the response once it's available without blocking the main thread.
      */
-    public CompletableFuture<HttpResponse> send(HttpRequest httpRequest) {
+    /*public Mono<HttpResponse> send(HttpRequest httpRequest) {
         // Convert the synchronous call to asynchronous using Reactor's Mono
-        Mono<HttpResponse> responseMono = httpUrlConnectionClient.send(httpRequest);
+        // Mono<HttpResponse> responseMono = httpUrlConnectionClient.send(httpRequest);
+        CompletableFuture<HttpResponse> responseFuture = CompletableFuture.supplyAsync(() -> {
+            try{
+
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+        });
 
         // Convert the Mono to CompletableFuture to provide standard Java async capabilities
-        return responseMono.toFuture();
+        //return responseMono.toFuture();
+    }*/
+
+    public Mono<HttpResponse> send(HttpRequest request) {
+        return send(request, Context.NONE);
+    }
+
+    public Mono<HttpResponse> send(HttpRequest request, Context context) {
+        boolean eagerlyReadResponse = (boolean) context.getData(AZURE_EAGERLY_READ_RESPONSE).orElse(false);
+        boolean ignoreResponseBody = (boolean) context.getData(AZURE_IGNORE_RESPONSE_BODY).orElse(false);
+        boolean eagerlyConvertHeaders = (boolean) context.getData(AZURE_EAGERLY_CONVERT_HEADERS).orElse(false);
+
+        ProgressReporter progressReporter = Contexts.with(context).getHttpRequestProgressReporter();
+
+        return Mono.create(sink -> sink.onRequest(value -> {
+            Mono.fromCallable(() -> request)
+                .subscribe(asyncRequest -> {
+                    try {
+                        Mono<HttpResponse> responseMono = httpUrlConnectionClient.send(request);
+                    } catch (Exception ex) {
+                        sink.error(ex);
+                    }
+                }, sink::error);
+        }));
     }
 }
 
