3c61453b842899f66429b2b8a96ffe7b0d381ed5
Josh Anderson <jbanderson2001@gmail.com>
2023-08-31 13:41:09 +1200

    Changes to AsyncClient

--- sdk/core/azure-core-http-httpurlconnection/src/main/java/com/azure/core/http/httpurlconnection/AsyncHttpUrlConnectionClient.java
+++ sdk/core/azure-core-http-httpurlconnection/src/main/java/com/azure/core/http/httpurlconnection/AsyncHttpUrlConnectionClient.java
@@ -30,46 +30,25 @@ public class AsyncHttpUrlConnectionClient {
     /**
      * Sends an asynchronous HTTP request.
      *
-     * @param httpRequest The HTTP request to be sent.
+     * @param /httpRequest The HTTP request to be sent.
      * @return A CompletableFuture that represents the HttpResponse. This allows the caller
      *         to work with the response once it's available without blocking the main thread.
      */
-    /*public Mono<HttpResponse> send(HttpRequest httpRequest) {
-        // Convert the synchronous call to asynchronous using Reactor's Mono
-        // Mono<HttpResponse> responseMono = httpUrlConnectionClient.send(httpRequest);
-        CompletableFuture<HttpResponse> responseFuture = CompletableFuture.supplyAsync(() -> {
-            try{
-
-            } catch (IOException exception) {
-                exception.printStackTrace();
-            }
-        });
-
-        // Convert the Mono to CompletableFuture to provide standard Java async capabilities
-        //return responseMono.toFuture();
-    }*/
 
     public Mono<HttpResponse> send(HttpRequest request) {
         return send(request, Context.NONE);
     }
 
     public Mono<HttpResponse> send(HttpRequest request, Context context) {
-        boolean eagerlyReadResponse = (boolean) context.getData(AZURE_EAGERLY_READ_RESPONSE).orElse(false);
-        boolean ignoreResponseBody = (boolean) context.getData(AZURE_IGNORE_RESPONSE_BODY).orElse(false);
-        boolean eagerlyConvertHeaders = (boolean) context.getData(AZURE_EAGERLY_CONVERT_HEADERS).orElse(false);
+        // boolean eagerlyReadResponse = (boolean) context.getData(AZURE_EAGERLY_READ_RESPONSE).orElse(false);
+        // boolean ignoreResponseBody = (boolean) context.getData(AZURE_IGNORE_RESPONSE_BODY).orElse(false);
+        // boolean eagerlyConvertHeaders = (boolean) context.getData(AZURE_EAGERLY_CONVERT_HEADERS).orElse(false);
 
-        ProgressReporter progressReporter = Contexts.with(context).getHttpRequestProgressReporter();
+        // ProgressReporter progressReporter = Contexts.with(context).getHttpRequestProgressReporter();
 
-        return Mono.create(sink -> sink.onRequest(value -> {
-            Mono.fromCallable(() -> request)
-                .subscribe(asyncRequest -> {
-                    try {
-                        Mono<HttpResponse> responseMono = httpUrlConnectionClient.send(request);
-                    } catch (Exception ex) {
-                        sink.error(ex);
-                    }
-                }, sink::error);
-        }));
+        CompletableFuture<HttpResponse> responseFuture
+            = CompletableFuture.supplyAsync(() -> httpUrlConnectionClient.sendSynchronous(request));
+        return Mono.fromFuture(responseFuture);
     }
 }
 
