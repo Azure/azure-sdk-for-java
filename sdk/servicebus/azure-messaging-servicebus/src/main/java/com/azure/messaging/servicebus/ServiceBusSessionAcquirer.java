cf2421430a897246d6f71961363ee8d79bd547e5
anuchandy <anuamd@hotmail.com>
2023-08-31 11:39:17 -0700

    V2 Session readability improvement: Adding beginLockRenew API in ServiceBusSessionAcquirer.Session and use it in ServiceBusSessionReactorReceiver.

--- sdk/servicebus/azure-messaging-servicebus/src/main/java/com/azure/messaging/servicebus/ServiceBusSessionAcquirer.java
+++ sdk/servicebus/azure-messaging-servicebus/src/main/java/com/azure/messaging/servicebus/ServiceBusSessionAcquirer.java
@@ -5,21 +5,24 @@ package com.azure.messaging.servicebus;
 
 import com.azure.core.amqp.exception.AmqpErrorCondition;
 import com.azure.core.amqp.exception.AmqpException;
-import com.azure.core.amqp.implementation.ReactorConnectionCache;
 import com.azure.core.amqp.implementation.StringUtil;
 import com.azure.core.util.logging.ClientLogger;
 import com.azure.messaging.servicebus.implementation.MessagingEntityType;
-import com.azure.messaging.servicebus.implementation.ServiceBusReactorAmqpConnection;
+import com.azure.messaging.servicebus.implementation.ServiceBusManagementNode;
 import com.azure.messaging.servicebus.implementation.ServiceBusReceiveLink;
 import com.azure.messaging.servicebus.implementation.ServiceBusReceiveLink.SessionProperties;
+import com.azure.messaging.servicebus.implementation.instrumentation.ServiceBusTracer;
 import com.azure.messaging.servicebus.models.ServiceBusReceiveMode;
+import reactor.core.Disposable;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 import reactor.util.retry.Retry;
 
 import java.time.Duration;
+import java.time.OffsetDateTime;
 import java.util.Objects;
 import java.util.concurrent.TimeoutException;
+import java.util.function.Function;
 
 import static com.azure.core.amqp.implementation.ClientConstants.ENTITY_PATH_KEY;
 
@@ -31,22 +34,26 @@ final class ServiceBusSessionAcquirer {
     private final MessagingEntityType entityType;
     private final Duration sessionActiveTimeout;
     private final ServiceBusReceiveMode receiveMode;
-    private final ReactorConnectionCache<ServiceBusReactorAmqpConnection> connectionCache;
+    private final ConnectionCacheWrapper connectionCacheWrapper;
+    private final Mono<ServiceBusManagementNode> sessionManagement;
 
     ServiceBusSessionAcquirer(ClientLogger logger, String identifier, String entityPath,
         MessagingEntityType entityType, ServiceBusReceiveMode receiveMode, Duration sessionActiveTimeout,
-        ReactorConnectionCache<ServiceBusReactorAmqpConnection> connectionCache) {
+        ConnectionCacheWrapper connectionCacheWrapper) {
+        assert connectionCacheWrapper.isV2();
         this.logger = logger;
         this.identifier = identifier;
         this.entityPath = entityPath;
         this.entityType = entityType;
         this.sessionActiveTimeout = sessionActiveTimeout;
         this.receiveMode = receiveMode;
-        this.connectionCache = connectionCache;
+        this.connectionCacheWrapper = connectionCacheWrapper;
+        this.sessionManagement = connectionCacheWrapper.getConnection()
+            .flatMap(connection -> connection.getManagementNode(entityPath, entityType));
     }
 
     boolean isConnectionClosed() {
-        return this.connectionCache.isCurrentConnectionClosed();
+        return this.connectionCacheWrapper.isChannelClosed();
     }
 
     /**
@@ -75,7 +82,7 @@ final class ServiceBusSessionAcquirer {
         return Mono.defer(() -> createSessionReceiveLink(sessionId)
             .flatMap(sessionLink -> sessionLink.getSessionProperties() // Await for sessionLink to "ACTIVE" then reads its properties
                 .flatMap(sessionProperties -> {
-                    return Mono.just(new Session(sessionLink, sessionProperties));
+                    return Mono.just(new Session(sessionLink, sessionProperties, sessionManagement));
                 })
             ))
             .timeout(sessionActiveTimeout)
@@ -120,35 +127,72 @@ final class ServiceBusSessionAcquirer {
 
     private Mono<ServiceBusReceiveLink> createSessionReceiveLink(String sessionId) {
         final String linkName = (sessionId != null) ? sessionId : StringUtil.getRandomString("session-");
-        return connectionCache.get()
+        return connectionCacheWrapper.getConnection()
             .flatMap(connection -> connection.createReceiveLink(linkName, entityPath, receiveMode,
                 null, entityType, identifier, sessionId));
     }
 
     /**
-     * A tuple type to hold properties and AmqpLink to a session.
+     * A type to hold properties and AmqpLink to a session, and allow initiating recurring session lock renewal.
      */
     static final class Session {
-        final ServiceBusReceiveLink link;
-        final SessionProperties properties;
+        private final ServiceBusReceiveLink link;
+        private final SessionProperties properties;
+        private final Mono<ServiceBusManagementNode> sessionManagement;
 
         /**
-         * Create Session tuple to hold properties and AmqpLink to a session.
+         * Create Session instance representing .
          *
          * @param sessionLink the amqp link to the session.
          * @param sessionProperties the session properties.
+         * @param sessionManagement Mono to get management node for session lock renewal.
          */
-        Session(ServiceBusReceiveLink sessionLink, SessionProperties sessionProperties) {
-            this.link = sessionLink;
-            this.properties = sessionProperties;
+        Session(ServiceBusReceiveLink sessionLink, SessionProperties sessionProperties, Mono<ServiceBusManagementNode> sessionManagement) {
+            this.link = Objects.requireNonNull(sessionLink, "sessionLink cannot be null.");
+            this.properties = Objects.requireNonNull(sessionProperties, "sessionProperties cannot be null.");
+            this.sessionManagement = Objects.requireNonNull(sessionManagement, "sessionManagement cannot be null.");
         }
 
-        boolean isDisposed() {
-            return link.isDisposed();
+        /**
+         * Gets the session id.
+         *
+         * @return the session id.
+         */
+        String getId() {
+            return properties.getId();
         }
 
-        Mono<Void> closeAsync() {
-            return link.closeAsync();
+        /**
+         * Gets the link streaming messages from the session and session endpoint events.
+         *
+         * @return the session link.
+         */
+        ServiceBusReceiveLink getLink() {
+            return link;
+        }
+
+        /**
+         * Begin the recurring lock renewal for the session.
+         *
+         * @param tracer the tracer.
+         * @param maxSessionLockRenew the upper bound for the recurring renewal duration.
+         * @return {@link Disposable} that when disposed of, results in stopping the recurring renewal.
+         */
+        Disposable beginLockRenew(ServiceBusTracer tracer, Duration maxSessionLockRenew) {
+            final String sessionId = properties.getId();
+            // Function, when invoked, renews this session lock once.
+            final Function<String, Mono<OffsetDateTime>> lockRenewFunc = __ -> {
+                return sessionManagement.flatMap(mgmt -> {
+                    final Mono<OffsetDateTime> renewLock = mgmt.renewSessionLock(sessionId, link.getLinkName());
+                    return tracer.traceMono("ServiceBus.renewSessionLock", renewLock);
+                });
+            };
+
+            final OffsetDateTime initialLockedUntil = properties.getLockedUntil();
+            // The operation that recurs renewal (with an upper bound of maxSessionLockRenew) using the above 'lockRenewFunc'.
+            final LockRenewalOperation recurringLockRenew = new LockRenewalOperation(sessionId, maxSessionLockRenew,
+                true, lockRenewFunc, initialLockedUntil);
+            return recurringLockRenew;
         }
     }
 }
