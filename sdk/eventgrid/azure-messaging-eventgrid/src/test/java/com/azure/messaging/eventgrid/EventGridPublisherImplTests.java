e9a00d1d96de5930ad6bb904abe93f158536e5c8
Bill Wert <billwert@microsoft.com>
2023-09-06 14:16:56 -0700

    Tests now use test proxy and recordings are pushed to assets repo

--- sdk/eventgrid/azure-messaging-eventgrid/src/test/java/com/azure/messaging/eventgrid/EventGridPublisherImplTests.java
+++ sdk/eventgrid/azure-messaging-eventgrid/src/test/java/com/azure/messaging/eventgrid/EventGridPublisherImplTests.java
@@ -18,6 +18,9 @@ import org.junit.jupiter.api.Test;
 import reactor.test.StepVerifier;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -25,37 +28,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-
-public class EventGridPublisherImplTests extends TestBase {
+public class EventGridPublisherImplTests extends EventGridTestBase {
     private static final Duration DEFAULT_TIMEOUT = Duration.ofSeconds(30);
 
     private HttpPipelineBuilder pipelineBuilder;
 
     private EventGridPublisherClientImplBuilder clientBuilder;
-
-    // Event Grid endpoint for a topic accepting EventGrid schema events
-    private static final String EVENTGRID_ENDPOINT = "AZURE_EVENTGRID_EVENT_ENDPOINT";
-
-    // Event Grid endpoint for a topic accepting CloudEvents schema events
-    private static final String CLOUD_ENDPOINT = "AZURE_EVENTGRID_CLOUDEVENT_ENDPOINT";
-
-    // Event Grid endpoint for a topic accepting custom schema events
-    private static final String CUSTOM_ENDPOINT = "AZURE_EVENTGRID_CUSTOM_ENDPOINT";
-
-    // Event Grid access key for a topic accepting EventGrid schema events
-    private static final String EVENTGRID_KEY = "AZURE_EVENTGRID_EVENT_KEY";
-
-    // Event Grid access key for a topic accepting CloudEvents schema events
-    private static final String CLOUD_KEY = "AZURE_EVENTGRID_CLOUDEVENT_KEY";
-
-    // Event Grid access key for a topic accepting custom schema events
-    private static final String CUSTOM_KEY = "AZURE_EVENTGRID_CUSTOM_KEY";
-
-    private static final String DUMMY_ENDPOINT = "https://www.dummyEndpoint.com/api/events";
-
-    private static final String DUMMY_KEY = "dummyKey";
 
     @Override
     protected void beforeTest() {
@@ -65,34 +44,24 @@ public class EventGridPublisherImplTests extends TestBase {
 
         if (interceptorManager.isPlaybackMode()) {
             pipelineBuilder.httpClient(interceptorManager.getPlaybackClient());
-        } else {
+        } else if (interceptorManager.isRecordMode()) {
             pipelineBuilder.policies(interceptorManager.getRecordPolicy(), new RetryPolicy());
         }
+
+    @Override
+    protected void afterTest() {
+        StepVerifier.resetDefaultTimeout();
     }
 
     @Test
     public void publishEventGridEventsImpl() {
         EventGridPublisherClientImpl egClient = clientBuilder
             .pipeline(pipelineBuilder.policies(
-                new AddHeadersPolicy(new HttpHeaders().put("aeg-sas-key", getKey(EVENTGRID_KEY))))
+                new AddHeadersPolicy(new HttpHeaders().put("aeg-sas-key", getKey(EVENTGRID_KEY).getKey())))
                 .build())
             .buildClient();
 
-        List<EventGridEvent> events = Collections.singletonList(
-            new EventGridEvent()
-                .setId(UUID.randomUUID().toString())
-                .setSubject("Test")
-                .setEventType("Microsoft.MockPublisher.TestEvent")
-                .setData(new HashMap<String, String>() {
-                    {
-                        put("Field1", "Value1");
-                        put("Field2", "Value2");
-                        put("Field3", "Value3");
-                    }
-                })
-                .setDataVersion("1.0")
-                .setEventTime(OffsetDateTime.now())
-        );
+        List<EventGridEvent> events = Collections.singletonList(getEventGridEvent().toImpl());
 
         StepVerifier.create(egClient.publishEventGridEventsWithResponseAsync(getEndpoint(EVENTGRID_ENDPOINT), events))
             .expectNextMatches(voidResponse -> voidResponse.getStatusCode() == 200)
@@ -104,23 +73,11 @@ public class EventGridPublisherImplTests extends TestBase {
     public void publishCloudEventsImpl() {
         EventGridPublisherClientImpl egClient = clientBuilder
             .pipeline(pipelineBuilder.policies(
-                new AddHeadersPolicy(new HttpHeaders().put("aeg-sas-key", getKey(CLOUD_KEY))))
+                new AddHeadersPolicy(new HttpHeaders().put("aeg-sas-key", getKey(CLOUD_KEY).getKey())))
                 .build())
             .buildClient();
 
-        List<CloudEvent> events = Collections.singletonList(
-            new CloudEvent("TestSource", "Microsoft.MockPublisher.TestEvent",
-                BinaryData.fromObject(new HashMap<String, String>() {
-                    {
-                        put("Field1", "Value1");
-                        put("Field2", "Value2");
-                        put("Field3", "Value3");
-                    }
-                }), CloudEventDataFormat.JSON, "application/json")
-                .setId(UUID.randomUUID().toString())
-                .setSubject("Test")
-                .setTime(OffsetDateTime.now())
-        );
+        List<CloudEvent> events = Collections.singletonList(getCloudEvent());
 
         StepVerifier.create(egClient.publishCloudEventEventsWithResponseAsync(getEndpoint(CLOUD_ENDPOINT), events, null))
             .expectNextMatches(voidResponse -> voidResponse.getStatusCode() == 200)
@@ -132,45 +89,18 @@ public class EventGridPublisherImplTests extends TestBase {
     public void publishCustomEventsImpl() {
         EventGridPublisherClientImpl egClient = clientBuilder
             .pipeline(pipelineBuilder.policies(
-                new AddHeadersPolicy(new HttpHeaders().put("aeg-sas-key", getKey(CUSTOM_KEY))))
+                new AddHeadersPolicy(new HttpHeaders().put("aeg-sas-key", getKey(CUSTOM_KEY).getKey())))
                 .build())
             .buildClient();
 
         List<Object> events = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
-            events.add(new HashMap<String, String>() {
-                {
-                    put("id", UUID.randomUUID().toString());
-                    put("time", OffsetDateTime.now().toString());
-                    put("subject", "Test");
-                    put("foo", "bar");
-                    put("type", "Microsoft.MockPublisher.TestEvent");
-                }
-            });
+            events.add(getCustomEvent());
         }
 
         StepVerifier.create(egClient.publishCustomEventEventsWithResponseAsync(getEndpoint(CUSTOM_ENDPOINT), events))
             .expectNextMatches(voidResponse -> voidResponse.getStatusCode() == 200)
             .expectComplete()
             .verify(DEFAULT_TIMEOUT);
-    }
-
-
-    private String getEndpoint(String liveEnvName) {
-        if (interceptorManager.isPlaybackMode()) {
-            return DUMMY_ENDPOINT;
-        }
-        String endpoint = System.getenv(liveEnvName);
-        assertNotNull(endpoint, "System environment variable " + liveEnvName + "is null");
-        return endpoint;
-    }
-
-    private String getKey(String liveEnvName) {
-        if (interceptorManager.isPlaybackMode()) {
-            return DUMMY_KEY;
-        }
-        String key = System.getenv(liveEnvName);
-        assertNotNull(key, "System environment variable " + liveEnvName + "is null");
-        return key;
     }
 }
