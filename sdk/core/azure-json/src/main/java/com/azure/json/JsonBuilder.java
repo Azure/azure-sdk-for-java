b74ff09ecb711c3022ecae84983d1b9e2fc30d6c
Zachary Knight <96271345+ZacharyKn1ght@users.noreply.github.com>
2023-09-07 21:52:22 +1200

    Very important code changes and JavaDocs to JsonBuilder buildOutput method. Including necessary exceptions.

--- sdk/core/azure-json/src/main/java/com/azure/json/JsonBuilder.java
+++ sdk/core/azure-json/src/main/java/com/azure/json/JsonBuilder.java
@@ -6,20 +6,26 @@ import java.io.Reader;
 
 import static com.azure.json.JsonToken.END_DOCUMENT;
 
+/** 
+ * probably unnecessary, but I'm leaving it in for now
+ * The thinking was should we consider hiding all the elements in the implementation
+ * and only exposing the JsonBuilder class to the user? It may be good to prevent people
+ * making random JsonElements without a proper container like Object or Array,
+ * but it may also make it harder to use.
+ */
 public class JsonBuilder {
-
-
-    /** probably unnecessary, but I'm leaving it in for now
-     * The thinking was should we consider hiding all the elements in the implementation
-     * and only exposing the JsonBuilder class to the user? It may be good to prevent people
-     * making random JsonElements without a proper container like Object or Array,
-     * but it may also make it harder to use.
-     * */
-
+    /**
+     * 
+     * @return
+     */
     public JsonArray createArray() {
         return new JsonArray();
     }
 
+    /**
+     * 
+     * @return
+     */
     public JsonObject createObject() {
         return new JsonObject();
     }
@@ -36,64 +42,103 @@ public class JsonBuilder {
         return this.buildOutput(JsonProviders.createReader(json));
     }
 
+    /**
+     * 
+     * @param json
+     * @return
+     * @throws IOException
+     */
     public JsonElement deserialize(byte[] json) throws IOException {
         return this.buildOutput(JsonProviders.createReader(json));
     }
 
+    /**
+     * 
+     * @param json
+     * @return
+     * @throws IOException
+     */
     public JsonElement deserialize(InputStream json) throws IOException {
         return this.buildOutput(JsonProviders.createReader(json));
     }
 
+    /**
+     * 
+     * @param json
+     * @return
+     * @throws IOException
+     */
     public JsonElement deserialize(Reader json) throws IOException {
         return this.buildOutput(JsonProviders.createReader(json));
     }
 
     /**
      * This method is used to build the output JsonElement from the JsonReader
-     * @throws IOException if the JSON string is invalid
+     * 
+     * @param reader
+     * @return
+     * @throws IOException if the JSON string is invalid 
+     * 
+     * TODO: should buildOutput only build one top level JsonArray or JsonObject 
+     * then break out of the while loop? Current implementation enables the 
+     * ability to have multiple top level JsonArray and JsonObjects. For example, 
+     * the following grouped top level JSON objects and JSON arrays would be 
+     * successfully built with our current implementation: 
+     *      {
+     *        "key1":"value1",
+     *        ...,
+     *        "keyN":"valueN"
+     *      }
+     *      {
+     *        "key1":"value1",
+     *        ...,
+     *        "keyN":"valueN"
+     *      }
+     *      ["val1", "val2", ..., "valN"]
      */
-
     private JsonElement buildOutput(JsonReader reader) throws IOException {
-        JsonToken token = reader.nextToken();
+        // Stores resulting deserialised JSON from reader that will be returned. 
         JsonElement output = null;
-        while (token != END_DOCUMENT) {
-            //todo remove this debug line, and remove the comment out cases
-            //System.out.printf("Token: %s%n", token);
-            switch (token) {
-                // I'm leaving in all the options in this switch commented out to make sure I don't miss anything,
-                // but at this point anything other than opening a new object or array is an error.
-                // In theory the reader takes care of this
 
+        JsonToken token = reader.nextToken();
+
+        while (token != END_DOCUMENT) {
+            switch (token) {
+                // Case: deserialising top level JSON array 
                 case START_ARRAY:
                     output = new JsonArray(reader);
                     break;
-//                case END_ARRAY:
-//                    break;
+                // Case: deserialising top level JSON object  
                 case START_OBJECT:
                     output = new JsonObject(reader);
-                    break;
-//                case END_OBJECT:
-//                    break;
-//                case FIELD_NAME:
-//                    break;
-//                case STRING:
-//                    break;
-//                case NUMBER:
-//                    break;
-//                case BOOLEAN:
-//                    break;
-//                case NULL:
-//                    break;
-                case END_DOCUMENT:
-                    break;
-                default:
-                    break;
-                // If we get here then we have an unknown token type - which should not occur...
+                    break; 
+                // Invalid JsonToken token cases: 
+                // NOTE: previous comment mentioned "In theory the reader takes care of this" 
+                case END_ARRAY: 
+                    throw new IOException("Invalid JsonToken.END_ARRAY token read from deserialised JSON. Deserialisation aborted."); 
+                case END_OBJECT: 
+                    throw new IOException("Invalid JsonToken.END_OBJECT token read from deserialised JSON. Deserialisation aborted."); 
+                case FIELD_NAME: 
+                    throw new IOException("Invalid JsonToken.FIELD_NAME token read from deserialised JSON. Deserialisation aborted."); 
+                case STRING: 
+                    throw new IOException("Invalid JsonToken.STRING token read from deserialised JSON. Deserialisation aborted."); 
+                case NUMBER: 
+                    throw new IOException("Invalid JsonToken.NUMBER token read from deserialised JSON. Deserialisation aborted."); 
+                case BOOLEAN: 
+                    throw new IOException("Invalid JsonToken.BOOLEAN token read from deserialised JSON. Deserialisation aborted."); 
+                case NULL: 
+                    throw new IOException("Invalid JsonToken.NULL token read from deserialised JSON. Deserialisation aborted."); 
+                // Case: this should never be the case because of the while condition. 
+                case END_DOCUMENT: 
+                    throw new IOException("Invalid JsonToken.END_DOCUMENT token read from deserialised JSON. Deserialisation aborted."); 
+                // Case: tokens read from JsonReader must be JsonTokens. This 
+                // default case would only succeed if an unknown token type is 
+                // encountered. 
+                default: 
+                    throw new IOException("Invalid token deserialised from JsonReader. Deserialisation aborted."); 
             }
-
             token = reader.nextToken();
         }
-
         return output;
     }
 }
