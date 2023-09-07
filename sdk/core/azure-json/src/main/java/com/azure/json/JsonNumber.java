87572edf96f847f6c2697ff5e6dd02b2d639433a
Zachary Knight <96271345+ZacharyKn1ght@users.noreply.github.com>
2023-09-07 20:31:28 +1200

    Included JavaDoc for JsonNumber serialize method.

--- sdk/core/azure-json/src/main/java/com/azure/json/JsonNumber.java
+++ sdk/core/azure-json/src/main/java/com/azure/json/JsonNumber.java
@@ -15,8 +15,10 @@ public class JsonNumber extends JsonElement {
     /**
      * Default constructor.
      * Default sets numberValue to "0" through the other constructor.
+     * 
      * TODO: may need to remove this due to design guidelines? May only want to
      * have the public JsonNumber(Number value) constructor.
+     * 
      * TODO: may need to double check that 0 is correctly cast to a Number type
      */
     public JsonNumber() { this(0); }
@@ -27,6 +29,7 @@ public class JsonNumber extends JsonElement {
      * Constructor used to explicitly set the number value of the JsonNumber object
      *
      * @param value specifies the number this JsonNumber object represents
+     * 
      * TODO: check for invalid number values or types
      */
     public JsonNumber(Number value) { this.numberValue = value.toString(); }
@@ -57,8 +60,15 @@ public class JsonNumber extends JsonElement {
 
     */
 
+    /** 
+     * @param jsonWriter JsonWriter that the serialized JsonNumber is written to. 
+     * @return JsonWriter state after the serialized JsonNumber has been written 
+     * to it. 
+     * @throws IOException Thrown when JsonWriter.write* calls throw an IOException. 
+     * 
+     * TODO: this needs to be extended to consider all of the number values
+     */
     @Override
-    //todo this needs to be extended to consider all of the number values
     public JsonWriter serialize(JsonWriter jsonWriter) throws IOException {
         jsonWriter.writeInt(Integer.parseInt(this.numberValue));
         return jsonWriter;
