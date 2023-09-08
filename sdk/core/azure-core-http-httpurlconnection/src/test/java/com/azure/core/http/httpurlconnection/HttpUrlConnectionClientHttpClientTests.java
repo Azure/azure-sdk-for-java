7b17deac0e7e854230ce6dd5a41d0d7d3057e245
lewisKendall-Jones <60380722+lewisKendall-Jones@users.noreply.github.com>
2023-09-08 14:33:02 +1200

    Integrated HttpClientTests (extended in HttpUrlConnectionClientHttpClientTests)

---
+++ sdk/core/azure-core-http-httpurlconnection/src/test/java/com/azure/core/http/httpurlconnection/HttpUrlConnectionClientHttpClientTests.java
@@ -0,0 +1,39 @@
+package com.azure.core.http.httpurlconnection;
+
+import com.azure.core.http.HttpClient;
+import com.azure.core.test.HttpClientTestsServer;
+import com.azure.core.test.http.HttpClientTests;
+import com.azure.core.test.http.LocalTestServer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+public class HttpUrlConnectionClientHttpClientTests extends HttpClientTests {
+
+    private static LocalTestServer server;
+    @BeforeAll
+    public static void startTestServer() {
+        server = HttpClientTestsServer.getHttpClientTestsServer();
+        server.start();
+    }
+
+    @AfterAll
+    public static void stopTestServer() {
+        if (server != null) {
+            server.stop();
+        }
+    }
+    @Override
+    protected HttpClient createHttpClient() {
+        return new HttpUrlConnectionClientProvider().createInstance();
+    }
+
+    @Override
+    protected int getPort() {
+        return server.getHttpPort();
+    }
+
+    @Override
+    protected String getServerUri(boolean secure) {
+        return secure ? server.getHttpsUri() : server.getHttpUri();
+    }
+}
