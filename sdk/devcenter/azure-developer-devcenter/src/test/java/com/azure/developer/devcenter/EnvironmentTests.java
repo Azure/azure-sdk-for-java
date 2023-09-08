9956a561dbf7520597f536fcad7cb842a0643b2b
Oleg Demchenko <oldemche@microsoft.com>
2023-09-08 11:58:52 -0700

    Update default variables

--- sdk/devcenter/azure-developer-devcenter/src/test/java/com/azure/developer/devcenter/EnvironmentTests.java
+++ sdk/devcenter/azure-developer-devcenter/src/test/java/com/azure/developer/devcenter/EnvironmentTests.java
@@ -14,8 +14,8 @@ import org.junit.jupiter.api.Assertions;
 class EnvironmentTests extends DevCenterClientTestBase {
     @Test
     public void testCreateEnvironment() {
-        String projectName = Configuration.getGlobalConfiguration().get("DEFAULT_PROJECT_NAME", "sdk-project-hdhjgzht7tgyq");
-        String environmentTypeName = Configuration.getGlobalConfiguration().get("DEFAULT_ENVIRONMENT_TYPE_NAME", "sdk-default-environment-type");
+        String projectName = Configuration.getGlobalConfiguration().get("DEFAULT_PROJECT_NAME", "sdk-project-kjqdmrwdctddo");
+        String environmentTypeName = Configuration.getGlobalConfiguration().get("DEFAULT_ENVIRONMENT_TYPE_NAME", "sdk-environment-type-kzu6evjkw76k6");
         String catalogName = Configuration.getGlobalConfiguration().get("DEFAULT_CATALOG_NAME", "sdk-default-catalog");
         String catalogItemName = Configuration.getGlobalConfiguration().get("DEFAULT_CATALOG_ITEM_NAME", "Sandbox");
 
