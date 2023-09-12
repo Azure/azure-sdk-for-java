96c05937e67d644752adb89032425ec0b3cc131c
Shawn Fang <45607042+mssfang@users.noreply.github.com>
2023-09-12 15:16:02 -0700

    clean up for checkstyles

--- sdk/appconfiguration/azure-data-appconfiguration/src/samples/java/com/azure/data/appconfiguration/FeatureFlagConfigurationSettingSample.java
+++ sdk/appconfiguration/azure-data-appconfiguration/src/samples/java/com/azure/data/appconfiguration/FeatureFlagConfigurationSettingSample.java
@@ -4,7 +4,6 @@
 package com.azure.data.appconfiguration;
 
 import com.azure.core.http.rest.PagedIterable;
-import com.azure.core.util.Configuration;
 import com.azure.data.appconfiguration.models.ConfigurationSetting;
 import com.azure.data.appconfiguration.models.FeatureFlagConfigurationSetting;
 import com.azure.data.appconfiguration.models.FeatureFlagFilter;
@@ -27,7 +26,7 @@ public class FeatureFlagConfigurationSettingSample {
     public static void main(String[] args) {
         // The connection string value can be obtained by going to your App Configuration instance in the Azure portal
         // and navigating to "Access Keys" page under the "Settings" section.
-        String connectionString = Configuration.getGlobalConfiguration().get("AZURE_APPCONFIG_CONNECTION_STRING");;
+        String connectionString = "endpoint={endpoint_value};id={id_value};secret={secret_value}";
         final ConfigurationClient client = new ConfigurationClientBuilder()
                                                .connectionString(connectionString)
                                                .buildClient();
@@ -39,13 +38,9 @@ public class FeatureFlagConfigurationSettingSample {
 
         FeatureFlagFilter percentageFilter = new FeatureFlagFilter("Microsoft.Percentage")
                                                  .addParameter("Value", 30);
-
-        FeatureFlagFilter secondFilter = new FeatureFlagFilter("Microsoft.MemoryPercentage")
-            .addParameter("Value", 70);
-
         FeatureFlagConfigurationSetting featureFlagConfigurationSetting =
             new FeatureFlagConfigurationSetting(key, true)
-                .setClientFilters(Arrays.asList(percentageFilter, secondFilter));
+                .setClientFilters(Arrays.asList(percentageFilter));
 
         // setConfigurationSetting adds or updates a setting to Azure App Configuration store. Alternatively, you can
         // call addConfigurationSetting which only succeeds if the setting does not exist in the store. Or,
@@ -55,46 +50,23 @@ public class FeatureFlagConfigurationSettingSample {
             (FeatureFlagConfigurationSetting) client.setConfigurationSetting(featureFlagConfigurationSetting);
         printFeatureFlagSetting(setting);
 
-//        setting.setValue("{\"id\":\"hello\",\"description\":\"world\",\"enabled\":true,\"conditions\":{\"requirement_type\":\"All\", \"client_filters\":[{\"name\":\"Microsoft.Percentage\",\"parameters\":{\"Value\":30}}]}}");
-//        setting = (FeatureFlagConfigurationSetting) client.setConfigurationSetting(setting);
-//        printFeatureFlagSetting(setting);
-
-//        setting.setDescription("updated description");
-//        setting = (FeatureFlagConfigurationSetting) client.setConfigurationSetting(setting);
-//        printFeatureFlagSetting(setting);
-//
-//
-//        setting.setEnabled(false);
-//        setting = (FeatureFlagConfigurationSetting) client.setConfigurationSetting(setting);
-//        printFeatureFlagSetting(setting);
-//
-//        setting.setKey("newKey");
-//        setting = (FeatureFlagConfigurationSetting) client.setConfigurationSetting(setting);
-//        printFeatureFlagSetting(setting);
-
-
-        setting.setClientFilters(Arrays.asList(percentageFilter));
-        setting = (FeatureFlagConfigurationSetting) client.setConfigurationSetting(setting);
-        printFeatureFlagSetting(setting);
-
-
         System.out.println("[Get-FeatureFlagConfigurationSetting]");
         setting = (FeatureFlagConfigurationSetting) client.getConfigurationSetting(setting);
         printFeatureFlagSetting(setting);
 
-//        System.out.println("[List-FeatureFlagConfigurationSetting]");
-//        PagedIterable<ConfigurationSetting> configurationSettings =
-//            client.listConfigurationSettings(new SettingSelector());
-//        for (ConfigurationSetting configurationSetting : configurationSettings) {
-//            if (configurationSetting instanceof FeatureFlagConfigurationSetting) {
-//                System.out.println("-Listing-FeatureFlagConfigurationSetting");
-//                printFeatureFlagSetting((FeatureFlagConfigurationSetting) configurationSetting);
-//            } else {
-//                System.out.println("-Listing-non-FeatureFlagConfigurationSetting");
-//                System.out.printf("Key: %s, Value: %s%n", configurationSetting.getKey(),
-//                    configurationSetting.getValue());
-//            }
-//        }
+        System.out.println("[List-FeatureFlagConfigurationSetting]");
+        PagedIterable<ConfigurationSetting> configurationSettings =
+            client.listConfigurationSettings(new SettingSelector());
+        for (ConfigurationSetting configurationSetting : configurationSettings) {
+            if (configurationSetting instanceof FeatureFlagConfigurationSetting) {
+                System.out.println("-Listing-FeatureFlagConfigurationSetting");
+                printFeatureFlagSetting((FeatureFlagConfigurationSetting) configurationSetting);
+            } else {
+                System.out.println("-Listing-non-FeatureFlagConfigurationSetting");
+                System.out.printf("Key: %s, Value: %s%n", configurationSetting.getKey(),
+                    configurationSetting.getValue());
+            }
+        }
 
         System.out.println("[Delete-FeatureFlagConfigurationSetting");
         setting = (FeatureFlagConfigurationSetting) client.deleteConfigurationSetting(setting);
