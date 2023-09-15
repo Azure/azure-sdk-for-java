1d7575e8a5996c584f5bd1180d41ccde4c40e8b4
Ajith Thanam <ajitht@microsoft.com>
2023-09-15 16:34:01 -0400

    Recreate events and models

---
+++ sdk/communication/azure-communication-callautomation/src/main/java/com/azure/communication/callautomation/models/DialogStateResult.java
@@ -0,0 +1,64 @@
+package com.azure.communication.callautomation.models;
+
+import com.azure.communication.callautomation.implementation.accesshelpers.DialogStateResponseConstructorProxy;
+import com.azure.communication.callautomation.implementation.converters.CallParticipantConverter;
+import com.azure.communication.callautomation.implementation.models.DialogStateResponse;
+import com.azure.core.annotation.Immutable;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Objects;
+
+@Immutable
+public class DialogStateResult {
+
+    /*
+     * The dialog ID.
+     */
+    @JsonProperty(value = "dialogId")
+    private String dialogId;
+
+    /*
+     * Defines options for dialog.
+     */
+    @JsonProperty(value = "dialogOptions")
+    private StartDialogOptions dialogOptions;
+
+    /*
+     * Determines the type of the dialog.
+     */
+    @JsonProperty(value = "dialogInputType")
+    private DialogInputType dialogInputType;
+
+    /*
+     * The value to identify context of the operation.
+     */
+    @JsonProperty(value = "operationContext")
+    private String operationContext;
+
+    static {
+        DialogStateResponseConstructorProxy.setAccessor(
+            new DialogStateResponseConstructorProxy.DialogStateResponseConstructorAccessor() {
+                @Override
+                public DialogStateResult create(DialogStateResponse internalHeaders) {
+                    return new DialogStateResult(internalHeaders);
+                }
+            });
+    }
+
+    public DialogStateResult(){
+        this.dialogId = null;
+        this.operationContext = null;
+    }
+
+    /**
+     * Constructor of the class
+     *
+     * @param dialogStateResponse The response from the dialog service
+     */
+     DialogStateResult(DialogStateResponse dialogStateResponse) {
+        Objects.requireNonNull(dialogStateResponse, "dialogStateResponse must not be null");
+
+        this.dialogId = dialogStateResponse.getDialogId();
+        this.operationContext = dialogStateResponse.getOperationContext();
+    }
+}
---
+++ sdk/communication/azure-communication-callautomation/src/main/java/com/azure/communication/callautomation/models/StartDialogOptions.java
@@ -0,0 +1,51 @@
+package com.azure.communication.callautomation.models;
+
+import com.azure.communication.callautomation.implementation.models.DialogInputType;
+
+import java.util.Map;
+
+public class StartDialogOptions {
+
+    private String botId;
+    private String dialogId;
+    private DialogInputType dialogInputType;
+    private Map<String, Object> dialogContext;
+    private String operationContext;
+
+    public StartDialogOptions(String botId, DialogInputType dialogInputType, Map<String, Object> dialogContext) {
+        this.botId = botId;
+        this.dialogInputType = dialogInputType;
+        this.dialogContext = dialogContext;
+    }
+
+    public StartDialogOptions(String botId, String dialogId, DialogInputType dialogInputType, Map<String, Object> dialogContext) {
+        this.botId = botId;
+        this.dialogId = dialogId;
+        this.dialogInputType = dialogInputType;
+        this.dialogContext = dialogContext;
+    }
+
+    public String getBotId() {
+        return botId;
+    }
+
+    public String getDialogId() {
+        return dialogId;
+    }
+
+    public DialogInputType getDialogInputType() {
+        return dialogInputType;
+    }
+
+    public Map<String, Object> getDialogContext() {
+        return dialogContext;
+    }
+
+    public String getOperationContext() {
+        return operationContext;
+    }
+
+    public void setOperationContext(String operationContext) {
+        this.operationContext = operationContext;
+    }
+}
