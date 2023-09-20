13eb52ce8e0cf1b2251d33c19d19eb10267681a6
azure-sdk <azuresdk@microsoft.com>
2023-09-20 14:41:10 -0700

    Reset sources for azure-security-attestation to the release version 1.1.16.

--- sdk/attestation/azure-security-attestation/src/test/java/com/azure/security/attestation/AttestationSignersTest.java
+++ sdk/attestation/azure-security-attestation/src/test/java/com/azure/security/attestation/AttestationSignersTest.java
@@ -6,15 +6,11 @@ import com.azure.core.http.HttpClient;
 import com.azure.core.http.rest.Response;
 import com.azure.core.test.TestMode;
 import com.azure.core.util.Context;
-import com.azure.security.attestation.models.AttestationSigner;
 import com.azure.security.attestation.models.AttestationSignerCollection;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import reactor.test.StepVerifier;
-
-import java.security.cert.X509Certificate;
-import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -23,6 +19,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
  * Test for Attestation Signing Certificates APIs.
  */
 public class AttestationSignersTest extends AttestationClientTestBase {
+    private static final String DISPLAY_NAME_WITH_ARGUMENTS = "{displayName} with [{arguments}]";
+
     @ParameterizedTest(name = DISPLAY_NAME_WITH_ARGUMENTS)
     @MethodSource("getAttestationClients")
     void testGetSigningCertificates(HttpClient client, String clientUri) {
@@ -74,40 +72,36 @@ public class AttestationSignersTest extends AttestationClientTestBase {
     /**
      * Verifies the response to the GetSigningCertificates (/certs) API.
      * <p>
-     * Each certificate returned needs to be a valid X.509 certificate. We also verify that self-signed certificates are
-     * signed with the known trusted roots.
+     * Each certificate returned needs to be a valid X.509 certificate.
+     * We also verify that self-signed certificates are signed with the known trusted roots.
      *
      * @param clientUri Base URI for client, used to verify the contents of the certificates.
-     * @param signers AttestationSigners to verify.
+     * @param signers   AttestationSigners to verify.
      */
-    @SuppressWarnings("deprecation")
     private void verifySigningCertificatesResponse(String clientUri, AttestationSignerCollection signers) {
         Assertions.assertTrue(signers.getAttestationSigners().size() > 1);
 
-        for (AttestationSigner signer : signers.getAttestationSigners()) {
+        signers.getAttestationSigners().forEach(signer -> {
             assertNotNull(signer.getKeyId());
             assertNotNull(signer.getCertificates());
             Assertions.assertNotEquals(0, signer.getCertificates().size());
-
-            for (X509Certificate x5c : signer.getCertificates()) {
-                if (!Objects.equals(x5c.getSubjectDN(), x5c.getIssuerDN())) {
-                    continue;
-                }
+            signer.getCertificates().forEach(x5c -> {
                 // If the certificate is self-signed, it should be associated
                 // with either the Microsoft root CA, the VBS self-signed root, or the instance.
-                if (x5c.getIssuerDN().toString().contains("Microsoft Root Certificate Authority")) {
-                    assertEquals("CN=Microsoft Root Certificate Authority 2011, O=Microsoft Corporation, L=Redmond, "
-                        + "ST=Washington, C=US", x5c.getIssuerDN().getName());
-                } else if (x5c.getIssuerDN().toString().contains("AttestationService-LocalTest-ReportSigning")) {
-                    assertEquals("CN=AttestationService-LocalTest-ReportSigning", x5c.getIssuerDN().getName());
-                } else {
-                    // In playback mode, the clientUri is bogus, so it cannot be validated.
-                    if (testContextManager.getTestMode() != TestMode.PLAYBACK) {
-                        assertEquals("CN=" + clientUri, x5c.getSubjectDN().getName());
+                if (x5c.getIssuerDN().equals(x5c.getSubjectDN())) {
+                    if (x5c.getIssuerDN().toString().contains("Microsoft Root Certificate Authority")) {
+                        assertEquals("CN=Microsoft Root Certificate Authority 2011, O=Microsoft Corporation, L=Redmond, ST=Washington, C=US", x5c.getIssuerDN().getName());
+                    } else if (x5c.getIssuerDN().toString().contains("AttestationService-LocalTest-ReportSigning")) {
+                        assertEquals("CN=AttestationService-LocalTest-ReportSigning", x5c.getIssuerDN().getName());
+                    } else {
+                        // In playback mode, the clientUri is bogus, so it cannot be validated.
+                        if (testContextManager.getTestMode() != TestMode.PLAYBACK) {
+                            assertEquals("CN=" + clientUri, x5c.getSubjectDN().getName());
+                        }
                     }
                 }
-            }
-        }
+            });
+        });
     }
 }
 
