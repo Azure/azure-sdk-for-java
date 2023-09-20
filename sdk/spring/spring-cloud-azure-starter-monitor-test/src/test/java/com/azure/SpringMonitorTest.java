8835bda59ea9d4438ecc5e5cfcdbb25eeb13b43e
Jean Bisutti <jean.bisutti@gmail.com>
2023-09-20 10:37:31 +0200

    Add self-diagnostics features to Spring Monitor (#36712)

--- sdk/spring/spring-cloud-azure-starter-monitor-test/src/test/java/com/azure/SpringMonitorTest.java
+++ sdk/spring/spring-cloud-azure-starter-monitor-test/src/test/java/com/azure/SpringMonitorTest.java
@@ -7,6 +7,7 @@ import com.azure.core.http.HttpPipeline;
 import com.azure.core.http.HttpPipelineBuilder;
 import com.azure.core.http.policy.HttpPipelinePolicy;
 import com.azure.monitor.applicationinsights.spring.OpenTelemetryVersionCheckRunner;
+import com.azure.monitor.applicationinsights.spring.selfdiagnostics.SelfDiagnosticsLevel;
 import com.azure.monitor.opentelemetry.exporter.implementation.models.MessageData;
 import com.azure.monitor.opentelemetry.exporter.implementation.models.MonitorDomain;
 import com.azure.monitor.opentelemetry.exporter.implementation.models.RemoteDependencyData;
@@ -23,9 +24,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import reactor.util.annotation.Nullable;
 
 import java.net.MalformedURLException;
@@ -39,12 +41,12 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(
-    classes = {Application.class, SpringMonitorTest.TestConfiguration.class},
+    classes = {Application.class, SpringMonitorTest.TestConfig.class},
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
     properties = {
       "applicationinsights.connection.string=InstrumentationKey=00000000-0000-0000-0000-0FEEDDADBEEF;IngestionEndpoint=https://test.in.applicationinsights.azure.com/;LiveEndpoint=https://test.livediagnostics.monitor.azure.com/"
     })
-public class SpringMonitorTest {
+class SpringMonitorTest {
 
   private static CountDownLatch countDownLatch;
 
@@ -63,8 +65,8 @@ public class SpringMonitorTest {
 
   @Autowired private Resource otelResource;
 
-  @Configuration(proxyBeanMethods = false)
-  static class TestConfiguration {
+  @TestConfiguration
+  static class TestConfig {
 
     @Bean
     HttpPipeline httpPipeline() {
@@ -79,10 +81,15 @@ public class SpringMonitorTest {
           .policies(policy)
           .build();
     }
+    @Bean
+    @Primary
+    SelfDiagnosticsLevel testSelfDiagnosticsLevel() {
+      return SelfDiagnosticsLevel.DEBUG;
+    }
   }
 
   @Test
-  public void applicationContextShouldOnlyContainTheAzureSpanExporter() {
+  void applicationContextShouldOnlyContainTheAzureSpanExporter() {
     List<SpanExporter> spanExporters = otelSpanExportersProvider.getIfAvailable();
     assertThat(spanExporters).hasSize(1);
 
@@ -94,7 +101,7 @@ public class SpringMonitorTest {
   }
 
   @Test
-  public void applicationContextShouldOnlyContainTheAzureLogRecordExporter() {
+  void applicationContextShouldOnlyContainTheAzureLogRecordExporter() {
     List<LogRecordExporter> logRecordExporters = otelLoggerExportersProvider.getIfAvailable();
     assertThat(logRecordExporters).hasSize(1);
 
@@ -106,7 +113,7 @@ public class SpringMonitorTest {
   }
 
   @Test
-  public void applicationContextShouldOnlyContainTheAzureMetricExporter() {
+  void applicationContextShouldOnlyContainTheAzureMetricExporter() {
     List<MetricExporter> metricExporters = otelMetricExportersProvider.getIfAvailable();
     assertThat(metricExporters).hasSize(1);
 
@@ -188,7 +195,7 @@ public class SpringMonitorTest {
   }
 
   @Test
-  public void verifyOpenTelemetryVersion() {
+  void verifyOpenTelemetryVersion() {
     String currentOTelVersion = otelResource.getAttribute(ResourceAttributes.TELEMETRY_SDK_VERSION);
     assertThat(OpenTelemetryVersionCheckRunner.STARTER_OTEL_VERSION)
         .as(
