c8e7079037711acec59397caa758c8b0d8afa988
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2023-09-26 12:03:29 -0400

    Just provide a utility method

--- sdk/core/azure-core/src/main/java/com/azure/core/implementation/concurrent/package-info.java
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-/**
- * This package contains implementation Azure Core APIs for dealing with concurrency.
- */
-package com.azure.core.implementation.concurrent;
