f0b5dd0773a6d32e98dd7fb919f720855efab6ea
Weidong Xu <weidxu@microsoft.com>
2023-09-28 12:48:05 +0800

    test

---
+++ sdk/core/azure-core/src/test/java/com/azure/core/models/PollResultTests.java
@@ -0,0 +1,27 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.core.models;
+
+import com.azure.core.util.BinaryData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PollResultTests {
+
+    @Test
+    public void testDeserialization() {
+        String successResponseBody = "{\"id\":\"Uw_N1zoT53-rhHeaB5UJ_BaWKv8QVanU_Rjmo5FVdRs\",\"status\":\"Succeeded\",\"error\":null}";
+        PollResult pollResult = BinaryData.fromString(successResponseBody).toObject(PollResult.class);
+        Assertions.assertEquals("Uw_N1zoT53-rhHeaB5UJ_BaWKv8QVanU_Rjmo5FVdRs", pollResult.getOperationId());
+        Assertions.assertNull(pollResult.getError());
+
+        // dummy error response
+        String failureResponseBody = "{\"id\":\"Uw_N1zoT53-rhHeaB5UJ_BaWKv8QVanU_Rjmo5FVdRs\",\"status\":\"Failed\",\"error\":{\"code\":\"CODE\",\"message\":\"MESSAGE\"}}";
+        pollResult = BinaryData.fromString(failureResponseBody).toObject(PollResult.class);
+        Assertions.assertEquals("Uw_N1zoT53-rhHeaB5UJ_BaWKv8QVanU_Rjmo5FVdRs", pollResult.getOperationId());
+        Assertions.assertNotNull(pollResult.getError());
+        Assertions.assertEquals("CODE", pollResult.getError().getCode());
+        Assertions.assertEquals("MESSAGE", pollResult.getError().getMessage());
+    }
+}
