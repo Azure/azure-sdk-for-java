1137ba7c4c37798a47876e5a4c444fb8513bfc58
Saya <sh5155@gmail.com>
2023-10-02 13:25:04 +1300

    Add KeyvaultKeys sample tests

---
+++ sdk/android/azure-samples/src/androidTest/java/com/azure/android/sampletesting/KeyvaultKeysTesting.java
@@ -0,0 +1,62 @@
+package com.azure.android.sampletesting;
+
+import static org.junit.Assert.fail;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.azure.android.BuildConfig;
+import com.azure.android.keyvault.keys.HelloWorldKeyvaultKeys;
+import com.azure.android.keyvault.keys.KeyRotationAsyncKeyvaultKeys;
+import com.azure.identity.ClientSecretCredential;
+import com.azure.identity.ClientSecretCredentialBuilder;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class KeyvaultKeysTesting {
+    final String keyvaultEndpoint = "https://android-key-vault.vault.azure.net/";
+
+    ClientSecretCredential clientSecretCredential;
+    @Before
+    public void setup() {
+        // These are obtained by setting system environment variables
+        // on the computer emulating the app
+        clientSecretCredential = new ClientSecretCredentialBuilder()
+            .clientId(BuildConfig.AZURE_CLIENT_ID)
+            .clientSecret(BuildConfig.AZURE_CLIENT_SECRET)
+            .tenantId(BuildConfig.AZURE_TENANT_ID)
+            .build();
+    }
+
+    @Test
+    public void helloWorld() {
+        try {
+            HelloWorldKeyvaultKeys.main(keyvaultEndpoint, clientSecretCredential);
+        } catch (InterruptedException e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void keyRotationAsync() {
+        try {
+            KeyRotationAsyncKeyvaultKeys.main(keyvaultEndpoint, clientSecretCredential);
+        } catch (InterruptedException e) {
+            fail();
+        }
+    }
+
+    /* commented out pending key-id being obtained to put in this
+    @Test
+    public void keyWrapUnwrapOperations() {
+        try {
+            KeyWrapUnwrapOperationsKeyvaultKeys.main(keyvaultEndpoint, clientSecretCredential);
+        } catch (InterruptedException e) {
+            fail();
+        }
+
+    }
+    */
+}
