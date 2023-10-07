32ecc56408f556b9d452dddeca6132cdab7ba1e2
bronii <59108791+bronsonskipper@users.noreply.github.com>
2023-10-07 17:14:38 +1300

    GEN95 JsonArray.java refactor removing if/else block

--- sdk/core/azure-json/src/main/java/com/azure/json/JsonArray.java
+++ sdk/core/azure-json/src/main/java/com/azure/json/JsonArray.java
@@ -80,11 +80,7 @@ public class JsonArray extends JsonElement {
         if (element == null) {
             throw new IllegalArgumentException("Cannot add a null JsonElement to the JsonArray. Use a JsonNull object to represent a valid JSON null value.");
         }
-        if (index > elements.size()) {
-            this.elements.add(element);
-        } else {
-            this.elements.add(index, element);
-        }
+        this.elements.add(index, element);
         return this;
     }
 
