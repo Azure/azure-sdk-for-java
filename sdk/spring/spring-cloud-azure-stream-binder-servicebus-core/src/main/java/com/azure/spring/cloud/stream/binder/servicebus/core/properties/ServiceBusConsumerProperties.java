10c42c63cb8876f09f9e4b4b21475132129a35e7
Shili Chen <v-shilichen@microsoft.com>
2023-10-13 12:46:03 +0800

    refactor the code

--- sdk/spring/spring-cloud-azure-stream-binder-servicebus-core/src/main/java/com/azure/spring/cloud/stream/binder/servicebus/core/properties/ServiceBusConsumerProperties.java
+++ sdk/spring/spring-cloud-azure-stream-binder-servicebus-core/src/main/java/com/azure/spring/cloud/stream/binder/servicebus/core/properties/ServiceBusConsumerProperties.java
@@ -12,7 +12,7 @@ import static com.azure.messaging.servicebus.implementation.ServiceBusConstants.
 /**
  * Service Bus consumer properties.
  */
-public class ServiceBusConsumerProperties extends ProcessorProperties {
+public class ServiceBusConsumerProperties extends ProcessorProperties implements ServiceBusEntityOptionsProvider {
 
     private boolean requeueRejected;
 
@@ -37,10 +37,6 @@ public class ServiceBusConsumerProperties extends ProcessorProperties {
         this.requeueRejected = requeueRejected;
     }
 
-    /**
-     * Get the maxSizeInMegabytes property.
-     * @return the maxSizeInMegabytes value.
-     */
     public Long getMaxSizeInMegabytes() {
         return maxSizeInMegabytes;
     }
@@ -53,10 +49,6 @@ public class ServiceBusConsumerProperties extends ProcessorProperties {
         this.maxSizeInMegabytes = maxSizeInMegabytes;
     }
 
-    /**
-     * Set the defaultMessageTimeToLive property.
-     * @return the defaultMessageTimeToLive value.
-     */
     public Duration getDefaultMessageTimeToLive() {
         return defaultMessageTimeToLive;
     }
