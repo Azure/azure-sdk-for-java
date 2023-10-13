a40e6f51cac209b39887220a5e538863a3dced87
Aayush Kataria <aayushkataria3011@gmail.com>
2023-10-13 13:24:49 -0700

    Adding fixes and test cases for retry policy

---
+++ sdk/cosmos/azure-cosmos-tests/src/test/java/com/azure/cosmos/rx/WebExceptionRetryPolicyE2ETests.java
@@ -0,0 +1,303 @@
+package com.azure.cosmos.rx;
+
+
+import com.azure.cosmos.BridgeInternal;
+import com.azure.cosmos.ConnectionMode;
+import com.azure.cosmos.CosmosAsyncClient;
+import com.azure.cosmos.CosmosAsyncContainer;
+import com.azure.cosmos.CosmosClientBuilder;
+import com.azure.cosmos.CosmosDiagnostics;
+import com.azure.cosmos.CosmosException;
+import com.azure.cosmos.implementation.ClientSideRequestStatistics;
+import com.azure.cosmos.implementation.HttpConstants;
+import com.azure.cosmos.implementation.ImplementationBridgeHelpers;
+import com.azure.cosmos.implementation.OperationType;
+import com.azure.cosmos.implementation.RequestTimeline;
+import com.azure.cosmos.implementation.throughputControl.TestItem;
+
+
+import com.azure.cosmos.models.CosmosChangeFeedRequestOptions;
+import com.azure.cosmos.models.CosmosPatchOperations;
+import com.azure.cosmos.models.CosmosQueryRequestOptions;
+import com.azure.cosmos.models.FeedRange;
+import com.azure.cosmos.models.FeedResponse;
+import com.azure.cosmos.models.PartitionKey;
+import com.azure.cosmos.test.faultinjection.CosmosFaultInjectionHelper;
+import com.azure.cosmos.test.faultinjection.FaultInjectionConditionBuilder;
+import com.azure.cosmos.test.faultinjection.FaultInjectionConnectionType;
+import com.azure.cosmos.test.faultinjection.FaultInjectionOperationType;
+import com.azure.cosmos.test.faultinjection.FaultInjectionResultBuilders;
+import com.azure.cosmos.test.faultinjection.FaultInjectionRule;
+import com.azure.cosmos.test.faultinjection.FaultInjectionRuleBuilder;
+import com.azure.cosmos.test.faultinjection.FaultInjectionServerErrorType;
+import org.testng.SkipException;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Factory;
+import org.testng.annotations.Test;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+import java.util.Collection;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+import static java.time.temporal.ChronoUnit.MILLIS;
+import static java.time.temporal.ChronoUnit.SECONDS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.testng.Assert.fail;
+
+public class WebExceptionRetryPolicyE2ETests extends TestSuiteBase {
+
+    private final static
+    ImplementationBridgeHelpers.CosmosDiagnosticsHelper.CosmosDiagnosticsAccessor diagnosticsAccessor =
+        ImplementationBridgeHelpers.CosmosDiagnosticsHelper.getCosmosDiagnosticsAccessor();
+
+    private CosmosAsyncClient cosmosAsyncClient;
+    private CosmosAsyncContainer cosmosAsyncContainer;
+
+    @Factory(dataProvider = "clientBuildersWithSessionConsistency")
+    public WebExceptionRetryPolicyE2ETests(CosmosClientBuilder clientBuilder) {
+        super(clientBuilder);
+        this.subscriberValidationTimeout = TIMEOUT;
+    }
+
+    @BeforeClass(groups = {"multi-master"}, timeOut = TIMEOUT)
+    public void beforeClass() {
+        this.cosmosAsyncClient = getClientBuilder().buildAsyncClient();
+        this.cosmosAsyncContainer = getSharedMultiPartitionCosmosContainerWithIdAsPartitionKey(cosmosAsyncClient);
+    }
+
+    @AfterClass(groups = {"multi-master"}, timeOut = SHUTDOWN_TIMEOUT, alwaysRun = true)
+    public void afterClass() {
+        safeClose(cosmosAsyncClient);
+    }
+
+    @DataProvider(name = "operationTypeProvider")
+    public static Object[][] operationTypeProvider() {
+        return new Object[][]{
+            // FaultInjectionOperationType, OperationType
+            {FaultInjectionOperationType.READ_ITEM, OperationType.Read}
+        };
+    }
+
+
+    @Test(groups = {"multi-master"}, timeOut = TIMEOUT)
+    public void addressRefreshHttpTimeout() {
+        if (BridgeInternal
+            .getContextClient(this.cosmosAsyncClient)
+            .getConnectionPolicy()
+            .getConnectionMode() != ConnectionMode.DIRECT) {
+            throw new SkipException("addressRefreshHttpTimeout() is only meant for DIRECT mode");
+        }
+
+        TestItem newItem = TestItem.createNewItem();
+        this.cosmosAsyncContainer.createItem(newItem).block();
+
+        // create fault injection rules for address refresh
+        FaultInjectionRule addressRefreshDelayRule = new FaultInjectionRuleBuilder("addressRefreshDelayRule")
+            .condition(
+                new FaultInjectionConditionBuilder()
+                    .operationType(FaultInjectionOperationType.METADATA_REQUEST_ADDRESS_REFRESH)
+                    .build())
+            .result(
+                FaultInjectionResultBuilders.getResultBuilder(FaultInjectionServerErrorType.RESPONSE_DELAY)
+                    .delay(Duration.ofSeconds(14))
+                    .times(4) // make sure it will exhaust the local region retry
+                    .build()
+            )
+            .build();
+
+        // Create gone rules to force address refresh will bound to happen
+        FaultInjectionRule serverGoneRule = new FaultInjectionRuleBuilder("serverGoneRule")
+            .condition(
+                new FaultInjectionConditionBuilder()
+                    .operationType(FaultInjectionOperationType.READ_ITEM)
+                    .build())
+            .result(
+                FaultInjectionResultBuilders.getResultBuilder(FaultInjectionServerErrorType.GONE)
+                    .times(4) // client is on session consistency, make sure exception will be bubbled up to goneAndRetry policy
+                    .build()
+            )
+            .build();
+
+        CosmosFaultInjectionHelper
+            .configureFaultInjectionRules(
+                cosmosAsyncContainer,
+                Arrays.asList(addressRefreshDelayRule, serverGoneRule)).block();
+        try {
+            cosmosAsyncContainer
+                .readItem(newItem.getId(), new PartitionKey(newItem.getId()), TestItem.class)
+                .block();
+            fail("addressRefreshHttpTimeout() should fail due to addressRefresh timeout");
+        } catch (CosmosException e) {
+            System.out.println("dataPlaneRequestHttpTimeout() Diagnostics " + " " + e.getDiagnostics());
+            assertThat(e.getStatusCode()).isEqualTo(HttpConstants.StatusCodes.REQUEST_TIMEOUT);
+            assertThat(e.getSubStatusCode()).isEqualTo(HttpConstants.SubStatusCodes.GATEWAY_ENDPOINT_READ_TIMEOUT);
+            validateAddressRefreshRetryPolicyResponseTimeouts(e.getDiagnostics());
+        } finally {
+            addressRefreshDelayRule.disable();
+            serverGoneRule.disable();
+        }
+    }
+
+    @Test(groups = {"multi-master"}, dataProvider = "operationTypeProvider", timeOut = 8 * TIMEOUT)
+    public void dataPlaneRequestHttpTimeout(
+        FaultInjectionOperationType faultInjectionOperationType,
+        OperationType operationType) {
+
+        if (BridgeInternal
+            .getContextClient(this.cosmosAsyncClient)
+            .getConnectionPolicy()
+            .getConnectionMode() != ConnectionMode.GATEWAY) {
+            throw new SkipException("queryPlanHttpTimeoutWillNotMarkRegionUnavailable() is only meant for GATEWAY mode");
+        }
+
+        TestItem newItem = TestItem.createNewItem();
+        this.cosmosAsyncContainer.createItem(newItem).block();
+        FaultInjectionRule requestHttpTimeoutRule = new FaultInjectionRuleBuilder("requestHttpTimeoutRule" + UUID.randomUUID())
+            .condition(
+                new FaultInjectionConditionBuilder()
+                    .operationType(faultInjectionOperationType)
+                    .connectionType(FaultInjectionConnectionType.GATEWAY)
+                    .build())
+            .result(
+                FaultInjectionResultBuilders.getResultBuilder(FaultInjectionServerErrorType.RESPONSE_DELAY)
+                    .delay(Duration.ofSeconds(66))
+                    .times(4) // make sure it will exhaust the local region retry
+                    .build()
+            )
+            .build();
+
+        CosmosFaultInjectionHelper.configureFaultInjectionRules(this.cosmosAsyncContainer, Arrays.asList(requestHttpTimeoutRule)).block();
+
+
+        try {
+            CosmosDiagnostics cosmosDiagnostics =
+                this.performDocumentOperation(cosmosAsyncContainer, operationType, newItem).block();
+            System.out.println("dataPlaneRequestHttpTimeout() Diagnostics " + " " + cosmosDiagnostics);
+            validateDataPlaneRetryPolicyResponseTimeouts(cosmosDiagnostics);
+        } catch (Exception e) {
+            fail("dataPlaneRequestHttpTimeout() should succeed for operationType " + operationType, e);
+        } finally {
+            requestHttpTimeoutRule.disable();
+        }
+    }
+
+    private void validateDataPlaneRetryPolicyResponseTimeouts(CosmosDiagnostics cosmosDiagnostics) {
+        List<ClientSideRequestStatistics.GatewayStatistics> gatewayStatisticsList = diagnosticsAccessor.getClientSideRequestStatistics(cosmosDiagnostics)
+            .stream()
+            .map(ClientSideRequestStatistics::getGatewayStatisticsList)
+            .flatMap(Collection::stream)
+            .collect(Collectors.toList());
+
+        for (ClientSideRequestStatistics.GatewayStatistics gs : gatewayStatisticsList) {
+            for (RequestTimeline.Event event : gs.getRequestTimeline()) {
+                Duration durationInMillis = event.getDuration();
+                if (durationInMillis != null) {
+                    assertThat(durationInMillis.getSeconds()).isLessThanOrEqualTo(62);
+                }
+            }
+        }
+    }
+
+    private void validateAddressRefreshRetryPolicyResponseTimeouts(CosmosDiagnostics cosmosDiagnostics) {
+
+        List<ClientSideRequestStatistics.AddressResolutionStatistics> addressResolutionStatisticsList = diagnosticsAccessor.getClientSideRequestStatistics(cosmosDiagnostics)
+            .stream()
+            .map(ClientSideRequestStatistics::getAddressResolutionStatistics)
+            .flatMap(m -> m.values().stream())
+            .sorted(Comparator.comparing(ClientSideRequestStatistics.AddressResolutionStatistics::getStartTimeUTC))
+            .collect(Collectors.toList());
+
+        assertThat(MILLIS.between(addressResolutionStatisticsList.get(0).getStartTimeUTC(), addressResolutionStatisticsList.get(0).getEndTimeUTC())).isLessThanOrEqualTo(600);
+        assertThat(MILLIS.between(addressResolutionStatisticsList.get(1).getStartTimeUTC(), addressResolutionStatisticsList.get(1).getEndTimeUTC())).isLessThanOrEqualTo(600);
+        assertThat(SECONDS.between(addressResolutionStatisticsList.get(2).getStartTimeUTC(), addressResolutionStatisticsList.get(2).getEndTimeUTC())).isLessThanOrEqualTo(6);
+        assertThat(SECONDS.between(addressResolutionStatisticsList.get(3).getStartTimeUTC(), addressResolutionStatisticsList.get(3).getEndTimeUTC())).isLessThanOrEqualTo(11);
+    }
+
+    private Mono<CosmosDiagnostics> performDocumentOperation(
+        CosmosAsyncContainer cosmosAsyncContainer,
+        OperationType operationType,
+        TestItem createdItem) {
+        if (operationType == OperationType.Query) {
+            CosmosQueryRequestOptions queryRequestOptions = new CosmosQueryRequestOptions();
+            String query = String.format("SELECT * from c where c.id = '%s'", createdItem.getId());
+            FeedResponse<TestItem> itemFeedResponse =
+                cosmosAsyncContainer.queryItems(query, queryRequestOptions, TestItem.class).byPage().blockFirst();
+            return Mono.just(itemFeedResponse.getCosmosDiagnostics());
+        }
+
+        if (operationType == OperationType.Read
+            || operationType == OperationType.Delete
+            || operationType == OperationType.Replace
+            || operationType == OperationType.Create
+            || operationType == OperationType.Patch
+            || operationType == OperationType.Upsert) {
+
+            if (operationType == OperationType.Read) {
+                return cosmosAsyncContainer
+                    .readItem(
+                        createdItem.getId(),
+                        new PartitionKey(createdItem.getId()),
+                        TestItem.class
+                    )
+                    .map(itemResponse -> itemResponse.getDiagnostics());
+            }
+
+            if (operationType == OperationType.Replace) {
+                return cosmosAsyncContainer
+                    .replaceItem(
+                        createdItem,
+                        createdItem.getId(),
+                        new PartitionKey(createdItem.getId()))
+                    .map(itemResponse -> itemResponse.getDiagnostics());
+            }
+
+            if (operationType == OperationType.Delete) {
+                return cosmosAsyncContainer.deleteItem(createdItem, null).map(itemResponse -> itemResponse.getDiagnostics());
+            }
+
+            if (operationType == OperationType.Create) {
+                return cosmosAsyncContainer.createItem(TestItem.createNewItem()).map(itemResponse -> itemResponse.getDiagnostics());
+            }
+
+            if (operationType == OperationType.Upsert) {
+                return cosmosAsyncContainer.upsertItem(TestItem.createNewItem()).map(itemResponse -> itemResponse.getDiagnostics());
+            }
+
+            if (operationType == OperationType.Patch) {
+                CosmosPatchOperations patchOperations =
+                    CosmosPatchOperations
+                        .create()
+                        .add("newPath", "newPath");
+                return cosmosAsyncContainer
+                    .patchItem(createdItem.getId(), new PartitionKey(createdItem.getId()), patchOperations, TestItem.class)
+                    .map(itemResponse -> itemResponse.getDiagnostics());
+            }
+        }
+
+        if (operationType == OperationType.ReadFeed) {
+            List<FeedRange> feedRanges = cosmosAsyncContainer.getFeedRanges().block();
+            CosmosChangeFeedRequestOptions changeFeedRequestOptions =
+                CosmosChangeFeedRequestOptions.createForProcessingFromBeginning(feedRanges.get(0));
+
+            FeedResponse<TestItem> firstPage = cosmosAsyncContainer
+                .queryChangeFeed(changeFeedRequestOptions, TestItem.class)
+                .byPage()
+                .blockFirst();
+            return Mono.just(firstPage.getCosmosDiagnostics());
+        }
+
+        throw new IllegalArgumentException("The operation type is not supported");
+    }
+}
