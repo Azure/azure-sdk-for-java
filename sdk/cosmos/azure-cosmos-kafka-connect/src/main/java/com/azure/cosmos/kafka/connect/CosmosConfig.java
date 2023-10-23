72b98fd58cac6a0fa994799cd4045014bc9fc5cb
Fabian Meiswinkel <fabianm@microsoft.com>
2023-10-23 07:25:28 +0000

    Fixing linting errors

--- sdk/cosmos/azure-cosmos-kafka-connect/src/main/java/com/azure/cosmos/kafka/connect/CosmosConfig.java
+++ sdk/cosmos/azure-cosmos-kafka-connect/src/main/java/com/azure/cosmos/kafka/connect/CosmosConfig.java
@@ -8,8 +8,18 @@ import org.apache.kafka.common.config.ConfigDef;
 
 import java.util.Map;
 
+/**
+ * Configuration for Cosmos DB Kafka connector
+ */
 public class CosmosConfig extends AbstractConfig {
 
+    /**
+     * Initializes a new instance of the Cosmos DB Kafka Connector configuration
+     * @param definition The configuration definition
+     * @param originals The original config values
+     * @param configProviderProps The configuration overrides for this provider
+     * @param doLog Flag indicating whether the configuration should be logged
+     */
     public CosmosConfig(ConfigDef definition, Map<?, ?> originals, Map<String, ?> configProviderProps, boolean doLog) {
         super(definition, originals, configProviderProps, doLog);
     }
