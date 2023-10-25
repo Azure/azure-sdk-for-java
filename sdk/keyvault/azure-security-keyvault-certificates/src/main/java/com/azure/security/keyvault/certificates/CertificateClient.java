f0ad41099be21ddee4def8f8d6c6700ed8fef86e
vcolin7 <vicolina@microsoft.com>
2023-10-25 11:32:03 -0700

    Update CertificateClient.java

--- sdk/keyvault/azure-security-keyvault-certificates/src/main/java/com/azure/security/keyvault/certificates/CertificateClient.java
+++ sdk/keyvault/azure-security-keyvault-certificates/src/main/java/com/azure/security/keyvault/certificates/CertificateClient.java
@@ -65,10 +65,10 @@ import static com.azure.security.keyvault.certificates.implementation.DeletedCer
 import static com.azure.security.keyvault.certificates.implementation.KeyVaultCertificateWithPolicyHelper.createCertificateWithPolicy;
 
 /**
- * The CertificateClient provides synchronous methods to manage {@link KeyVaultCertificate certifcates} in the Azure Key
- * Vault. The client supports creating, retrieving, updating, merging, deleting, purging, backing up, restoring and
+ * The CertificateClient provides synchronous methods to manage {@link KeyVaultCertificate certifcates} in the key
+ * vault. The client supports creating, retrieving, updating, merging, deleting, purging, backing up, restoring and
  * listing the {@link KeyVaultCertificate certificates}. The client also supports listing
- * {@link DeletedCertificate deleted certificates} for a soft-delete enabled Azure Key Vault.
+ * {@link DeletedCertificate deleted certificates} for a soft-delete enabled key vault.
  *
  * <p>The client further allows creating, retrieving, updating, deleting and listing the
  * {@link CertificateIssuer certificate issuers}. The client also supports creating, listing and deleting
