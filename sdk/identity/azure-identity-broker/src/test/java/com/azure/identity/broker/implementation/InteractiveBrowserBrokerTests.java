f91b839914d9dfd81f18b7b026b29bd79d57296e
Bill Wert <billwert@microsoft.com>
2023-10-26 17:07:46 -0700

    add tests

--- sdk/identity/azure-identity-broker/src/test/java/com/azure/identity/broker/implementation/InteractiveBrowserBrokerTests.java
+++ sdk/identity/azure-identity-broker/src/test/java/com/azure/identity/broker/implementation/InteractiveBrowserBrokerTests.java
@@ -1,9 +1,8 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
 
-package com.azure.identity.broker;
+package com.azure.identity.broker.implementation;
 
-import com.azure.identity.broker.implementation.InteractiveBrowserBroker;
 import com.microsoft.aad.msal4j.IBroker;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
