201db1a9bf632afbffde04e98eed186dee66eb65
anuchandy <anuamd@hotmail.com>
2023-10-30 17:16:05 -0700

    ReceiverUnsettledDeliveries: Using DELIVERY_KEY, LINK_NAME_KEY for logging, Making call-site a logging key-value

--- sdk/core/azure-core-amqp/src/main/java/com/azure/core/amqp/implementation/ClientConstants.java
+++ sdk/core/azure-core-amqp/src/main/java/com/azure/core/amqp/implementation/ClientConstants.java
@@ -24,6 +24,7 @@ public final class ClientConstants {
     public static final String SESSION_NAME_KEY = "sessionName";
     public static final String FULLY_QUALIFIED_NAMESPACE_KEY = "namespace";
     public static final String OPERATION_NAME_KEY = "amqpOperation";
+    public static final String DELIVERY_KEY = "delivery";
     public static final String DELIVERY_STATE_KEY = "deliveryState";
     public static final String DELIVERY_TAG_KEY = "lockToken";
     public static final String ERROR_CONDITION_KEY = "errorCondition";
