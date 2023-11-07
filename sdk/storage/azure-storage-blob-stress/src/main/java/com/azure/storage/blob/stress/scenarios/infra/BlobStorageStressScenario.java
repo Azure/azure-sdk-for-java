67c6dd38ff8dfedc54f1dd8b6157a19e009e56e7
Liudmila Molkova <limolkova@microsoft.com>
2023-11-06 16:21:44 -0800

    add allowed headers

--- sdk/storage/azure-storage-blob-stress/src/main/java/com/azure/storage/blob/stress/scenarios/infra/BlobStorageStressScenario.java
+++ sdk/storage/azure-storage-blob-stress/src/main/java/com/azure/storage/blob/stress/scenarios/infra/BlobStorageStressScenario.java
@@ -1,12 +1,18 @@
 package com.azure.storage.blob.stress.scenarios.infra;
 
 import com.azure.core.http.HttpClient;
+import com.azure.core.http.policy.HttpLogDetailLevel;
+import com.azure.core.http.policy.HttpLogOptions;
 import com.azure.core.util.Configuration;
 import com.azure.storage.blob.BlobServiceAsyncClient;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
 import com.azure.storage.stress.StorageStressScenario;
 import com.azure.storage.stress.StressScenarioBuilder;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public abstract class BlobStorageStressScenario<TBuilder extends StressScenarioBuilder> extends StorageStressScenario {
     private final BlobServiceClient syncClient;
@@ -16,7 +22,19 @@ public abstract class BlobStorageStressScenario<TBuilder extends StressScenarioB
     public BlobStorageStressScenario(TBuilder builder) {
         super(builder);
         String connectionString = Configuration.getGlobalConfiguration().get("STORAGE_CONNECTION_STRING");
-        BlobServiceClientBuilder clientBuilder = new BlobServiceClientBuilder().connectionString(connectionString);
+
+        HttpLogOptions logOptions = new HttpLogOptions()
+            .setLogLevel(HttpLogDetailLevel.HEADERS)
+            .addAllowedHeaderName("x-ms-faultinjector-response-option")
+            .addAllowedHeaderName("Content-Range")
+            .addAllowedHeaderName("Accept-Ranges")
+            .addAllowedHeaderName("x-ms-blob-content-md5")
+            .addAllowedHeaderName("x-ms-error-code")
+            .addAllowedHeaderName("x-ms-range");
+
+        BlobServiceClientBuilder clientBuilder = new BlobServiceClientBuilder()
+            .connectionString(connectionString)
+            .httpLogOptions(logOptions);
         syncNoFaultClient = clientBuilder.buildClient();
 
         if (builder.getFaultInjectingClient() != null) {
