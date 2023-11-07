60080e8c17bd6512ebea0e4394a1ee8a9aba2bca
Bill Wert <billwert@microsoft.com>
2023-11-07 14:47:43 -0800

    Override builder methods to enable fluent building with base class methods.

--- sdk/identity/azure-identity-broker/src/main/java/com/azure/identity/broker/InteractiveBrowserBrokerCredentialBuilder.java
+++ sdk/identity/azure-identity-broker/src/main/java/com/azure/identity/broker/InteractiveBrowserBrokerCredentialBuilder.java
@@ -4,8 +4,25 @@
 package com.azure.identity.broker;
 
 
+import com.azure.core.http.HttpClient;
+import com.azure.core.http.HttpPipeline;
+import com.azure.core.http.ProxyOptions;
+import com.azure.core.http.policy.HttpLogOptions;
+import com.azure.core.http.policy.HttpPipelinePolicy;
+import com.azure.core.http.policy.RetryOptions;
+import com.azure.core.http.policy.RetryPolicy;
+import com.azure.core.util.ClientOptions;
+import com.azure.core.util.Configuration;
+import com.azure.identity.AuthenticationRecord;
+import com.azure.identity.BrowserCustomizationOptions;
 import com.azure.identity.InteractiveBrowserCredential;
 import com.azure.identity.InteractiveBrowserCredentialBuilder;
+import com.azure.identity.TokenCachePersistenceOptions;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.function.Function;
 
 /**
  * Fluent credential builder for instantiating a {@link InteractiveBrowserCredential} configured to use a secure broker.
@@ -24,6 +41,7 @@ public class InteractiveBrowserBrokerCredentialBuilder extends InteractiveBrowse
 
     /**
      * Sets the parent window handle used by the broker. For use on Windows only.
+     *
      * @param windowHandle The window handle of the current application, or 0 for a console application.
      * @return An updated instance of this builder with the interactive browser broker configured.
      */
@@ -35,10 +53,266 @@ public class InteractiveBrowserBrokerCredentialBuilder extends InteractiveBrowse
     /**
      * Enables Microsoft Account (MSA) pass-through. This allows the user to sign in with a Microsoft Account (MSA)
      * instead of a work or school account.
-     * @return The updated InteractiveBrowserCredentialBuilder object.
+     *
+     * @return An updated instance of this builder with enable Legacy MSA Passthrough set to true.
      */
     public InteractiveBrowserBrokerCredentialBuilder enableLegacyMsaPassthrough() {
         this.identityClientOptions.setEnableLegacyMsaPassthrough(true);
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public InteractiveBrowserBrokerCredentialBuilder clientOptions(ClientOptions clientOptions) {
+        super.clientOptions(clientOptions);
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public InteractiveBrowserBrokerCredentialBuilder addPolicy(HttpPipelinePolicy policy) {
+        super.addPolicy(policy);
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public InteractiveBrowserBrokerCredentialBuilder port(int port) {
+        super.port(port);
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public InteractiveBrowserBrokerCredentialBuilder additionallyAllowedTenants(String... additionallyAllowedTenants) {
+        super.additionallyAllowedTenants(additionallyAllowedTenants);
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public InteractiveBrowserBrokerCredentialBuilder authenticationRecord(AuthenticationRecord authenticationRecord) {
+        super.authenticationRecord(authenticationRecord);
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public InteractiveBrowserBrokerCredentialBuilder browserCustomizationOptions(BrowserCustomizationOptions browserCustomizationOptions) {
+        super.browserCustomizationOptions(browserCustomizationOptions);
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public InteractiveBrowserBrokerCredentialBuilder clientId(String clientId) {
+        super.clientId(clientId);
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public InteractiveBrowserBrokerCredentialBuilder configuration(Configuration configuration) {
+        super.configuration(configuration);
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public InteractiveBrowserBrokerCredentialBuilder authorityHost(String authorityHost) {
+        super.authorityHost(authorityHost);
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public InteractiveBrowserBrokerCredentialBuilder disableAutomaticAuthentication() {
+        super.disableAutomaticAuthentication();
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public InteractiveBrowserBrokerCredentialBuilder disableInstanceDiscovery() {
+        super.disableInstanceDiscovery();
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public InteractiveBrowserBrokerCredentialBuilder enableAccountIdentifierLogging() {
+        super.enableAccountIdentifierLogging();
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public InteractiveBrowserBrokerCredentialBuilder enableUnsafeSupportLogging() {
+        super.enableUnsafeSupportLogging();
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public InteractiveBrowserBrokerCredentialBuilder executorService(ExecutorService executorService) {
+        super.executorService(executorService);
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public InteractiveBrowserBrokerCredentialBuilder httpClient(HttpClient client) {
+        super.httpClient(client);
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public InteractiveBrowserBrokerCredentialBuilder httpLogOptions(HttpLogOptions logOptions) {
+        super.httpLogOptions(logOptions);
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public InteractiveBrowserBrokerCredentialBuilder loginHint(String loginHint) {
+        super.loginHint(loginHint);
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public InteractiveBrowserBrokerCredentialBuilder pipeline(HttpPipeline pipeline) {
+        super.pipeline(pipeline);
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public InteractiveBrowserBrokerCredentialBuilder maxRetry(int maxRetry) {
+        super.maxRetry(maxRetry);
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public InteractiveBrowserBrokerCredentialBuilder redirectUrl(String redirectUrl) {
+        super.redirectUrl(redirectUrl);
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public InteractiveBrowserBrokerCredentialBuilder additionallyAllowedTenants(List<String> additionallyAllowedTenants) {
+        super.additionallyAllowedTenants(additionallyAllowedTenants);
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public InteractiveBrowserBrokerCredentialBuilder retryOptions(RetryOptions retryOptions) {
+        super.retryOptions(retryOptions);
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public InteractiveBrowserBrokerCredentialBuilder retryPolicy(RetryPolicy retryPolicy) {
+        super.retryPolicy(retryPolicy);
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public InteractiveBrowserBrokerCredentialBuilder tenantId(String tenantId) {
+        super.tenantId(tenantId);
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public InteractiveBrowserBrokerCredentialBuilder retryTimeout(Function<Duration, Duration> retryTimeout) {
+        super.retryTimeout(retryTimeout);
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public InteractiveBrowserBrokerCredentialBuilder tokenCachePersistenceOptions(TokenCachePersistenceOptions tokenCachePersistenceOptions) {
+        super.tokenCachePersistenceOptions(tokenCachePersistenceOptions);
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public InteractiveBrowserBrokerCredentialBuilder httpPipeline(HttpPipeline httpPipeline) {
+        super.httpPipeline(httpPipeline);
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public InteractiveBrowserBrokerCredentialBuilder proxyOptions(ProxyOptions proxyOptions) {
+        super.proxyOptions(proxyOptions);
         return this;
     }
 }
