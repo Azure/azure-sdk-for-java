289dbb16f558b6e565d69ff08c076cc69263077c
Liudmila Molkova <limolkova@microsoft.com>
2023-11-06 17:18:43 -0800

    clean up tests and add more details to logs

--- sdk/storage/azure-storage-blob-stress/src/main/java/com/azure/storage/blob/stress/scenarios/DownloadToFileStressScenario.java
+++ sdk/storage/azure-storage-blob-stress/src/main/java/com/azure/storage/blob/stress/scenarios/DownloadToFileStressScenario.java
@@ -4,7 +4,6 @@ import com.azure.core.util.BinaryData;
 import com.azure.core.util.Context;
 import com.azure.core.util.logging.ClientLogger;
 import com.azure.storage.blob.options.BlobDownloadToFileOptions;
-import com.azure.storage.blob.stress.BlobStorageStressRunner;
 import com.azure.storage.blob.stress.builders.DownloadToFileScenarioBuilder;
 import com.azure.storage.blob.stress.scenarios.infra.BlobStressScenario;
 import com.azure.storage.stress.RandomInputStream;
@@ -13,6 +12,7 @@ import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.StatusCode;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.context.Scope;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.io.IOException;
@@ -22,25 +22,27 @@ import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Duration;
-import java.util.Queue;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.zip.CRC32;
-
-import static com.azure.storage.stress.HttpFaultInjectingHttpClient.FAULT_TRACKING_CONTEXT_KEY;
 
 public class DownloadToFileStressScenario extends BlobStressScenario<DownloadToFileScenarioBuilder> {
 
     private static final ClientLogger LOGGER = new ClientLogger(DownloadToFileScenarioBuilder.class);
+    private static final Tracer TRACER = GlobalOpenTelemetry.getTracer("DownloadToFileStressScenario");
 
-    private static Path originalDataPath;
-    private static long originalDataChecksum;
+    private final Path originalDataPath;
+
+    private long originalDataChecksum;
 
     private final Path directoryPath;
+    private final long originalDataLength;
 
     public DownloadToFileStressScenario(DownloadToFileScenarioBuilder builder) {
         super(builder, /*singletonBlob*/true, /*initializeBlob*/true);
         this.directoryPath = builder.getDirectoryPath();
+        this.originalDataPath = directoryPath.resolve("original-data-" + UUID.randomUUID());
+        this.originalDataLength = builder.getBlobSize();
     }
 
     @Override
@@ -48,16 +50,14 @@ public class DownloadToFileStressScenario extends BlobStressScenario<DownloadToF
         long endTimeNano = System.nanoTime() + timeout.toNanos();
         long timeoutNano;
 
-        Tracer tracer = GlobalOpenTelemetry.getTracer("test");
         while ((timeoutNano = endTimeNano - System.nanoTime()) > 0) {
             Path downloadPath = directoryPath.resolve(UUID.randomUUID() + ".txt");
-            Span span = tracer.spanBuilder("downloadToFile").startSpan();
-            try (Scope s = span.makeCurrent()) {
-                Queue<String> faultTypes = new ConcurrentLinkedQueue<>();
-                Context context = new Context(FAULT_TRACKING_CONTEXT_KEY, faultTypes);
+            Span span = TRACER.spanBuilder("downloadToFile").startSpan();
+            Scope s = span.makeCurrent();
+            try {
                 BlobDownloadToFileOptions options = new BlobDownloadToFileOptions(downloadPath.toString());
 
-                getSyncBlobClient().downloadToFileWithResponse(options, Duration.ofNanos(timeoutNano), context);
+                getSyncBlobClient().downloadToFileWithResponse(options, Duration.ofNanos(timeoutNano), Context.NONE);
 
                 validateDownloadedContents(downloadPath);
                 logSuccess();
@@ -71,6 +71,7 @@ public class DownloadToFileStressScenario extends BlobStressScenario<DownloadToF
                 logFailure(e.getMessage());
                 span.setStatus(StatusCode.ERROR, e.getMessage());
             } finally {
+                s.close();
                 span.end();
             }
         }
@@ -94,45 +95,58 @@ public class DownloadToFileStressScenario extends BlobStressScenario<DownloadToF
         // Use crc to check for file mismatch, avoiding every parallel test streaming original data from disk
         // If there's a mismatch, the original data can be streamed to check where the fault occurred
         // Data is streamed in the first place to avoid holding potentially gigs in memory
+        int length = 0;
         CRC32 dataCrc = new CRC32();
         try (InputStream file = Files.newInputStream(downloadPath)) {
             byte[] buf = new byte[4 * 1024 * 1024];
             int read;
             while ((read = file.read(buf)) != -1) {
                 dataCrc.update(buf, 0, read);
+                length += read;
             }
         }
         catch (IOException e) {
-            throw new UncheckedIOException(e);
+            throw LOGGER.logExceptionAsError(new UncheckedIOException(e));
         }
 
         long crc = dataCrc.getValue();
         if (crc != originalDataChecksum) {
-            // future: if mismatch, compare against original file
-            throw new RuntimeException("mismatched crc");
+            reportMismatch(crc, length);
         }
     }
 
     private Mono<Void> validateDownloadedContentsAsync(Path downloadPath) {
         CRC32 dataCrc = new CRC32();
-        return BinaryData.fromFile(downloadPath).toFluxByteBuffer()
+        AtomicLong length = new AtomicLong();
+        Flux<Void> check = BinaryData.fromFile(downloadPath).toFluxByteBuffer()
             .map(bb -> {
+                length.addAndGet(bb.remaining());
                 dataCrc.update(bb);
                 return null;
-            })
-            .then(Mono.defer(() -> {
+            });
+
+        return check
+            .doFinally(i -> {
                 long crc = dataCrc.getValue();
                 if (crc != originalDataChecksum) {
-                    // future: if mismatch, compare against original file
-                    return Mono.error(new Exception("mismatched crc"));
+                    reportMismatch(crc, length.get());
                 }
-                return Mono.empty();
-            }));
+            })
+            .then();
+    }
+
+    private void reportMismatch(long actualCrc, long actualLength) {
+        // future: if mismatch, compare against original file
+        throw LOGGER.atError()
+            .addKeyValue("expectedCrc", originalDataChecksum)
+            .addKeyValue("actualCrc", actualCrc)
+            .addKeyValue("expectedLength", originalDataLength)
+            .addKeyValue("actualLength", actualLength)
+            .log(new RuntimeException("mismatched crc"));
     }
 
     @Override
     protected void setupBlob() {
-        originalDataPath = directoryPath.resolve("original-data-" + UUID.randomUUID());
         try (InputStream data = new RandomInputStream(getBlobSize());
              OutputStream file = Files.newOutputStream(originalDataPath);
              OutputStream blob = getSyncBlobClientNoFault().getBlockBlobClient().getBlobOutputStream(true)) {
