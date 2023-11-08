3c306aef3ff238d203b43ff2fd89f3eb3852e969
Victor Colin Amador <vicolina@microsoft.com>
2023-11-08 14:21:53 -0800

    Fixed `HttpBinJSON.toJson()`

--- sdk/generic-sdk-core/generic-core/src/test/java/com/generic/core/models/HttpBinJSON.java
+++ sdk/generic-sdk-core/generic-core/src/test/java/com/generic/core/models/HttpBinJSON.java
@@ -104,50 +104,48 @@ public class HttpBinJSON implements JsonSerializable<HttpBinJSON> {
     public JsonWriter toJson(JsonWriter jsonWriter) throws IOException {
         jsonWriter.writeStartObject();
         jsonWriter.writeStringField("url", url);
-        jsonWriter.writeMapField("headers", headers, (mapEntryWriter, list)
-            -> mapEntryWriter.writeArray(list, JsonWriter::writeString));
-        jsonWriter.writeStartObject("data");
-        jsonWriter.writeEndObject();
+        jsonWriter.writeMapField("headers", headers,
+            (mapEntryWriter, list) -> mapEntryWriter.writeArray(list, JsonWriter::writeString));
+        jsonWriter.writeUntypedField("data", data);
 
-        return jsonWriter;
+        return jsonWriter.writeEndObject();
     }
 
     /**
      * Reads an instance of HttpBinJSON from the JsonReader.
      *
      * @param jsonReader The JsonReader being read.
+     *
      * @return An instance of HttpBinJSON if the JsonReader was pointing to an instance of it, or null if it was
      * pointing to JSON null.
+     *
      * @throws IOException If an error occurs while reading the HttpBinJSON.
      * @throws IllegalStateException If any of the required properties to create HttpBinJSON aren't found.
      */
     public static HttpBinJSON fromJson(JsonReader jsonReader) throws IOException {
-        return jsonReader.readObject(reader -> {
-            String url = null;
-            Map<String, List<String>> headers = null;
-            Object data = null;
+        return jsonReader.readObject(
+            reader -> {
+                HttpBinJSON httpBinJSON = new HttpBinJSON();
 
-            while (reader.nextToken() != JsonToken.END_OBJECT) {
-                String fieldName = reader.getFieldName();
-                reader.nextToken();
+                while (reader.nextToken() != JsonToken.END_OBJECT) {
+                    String fieldName = reader.getFieldName();
+                    reader.nextToken();
 
-                // Example of case-insensitive names and where serialization named don't match field names.
-                if ("url".equalsIgnoreCase(fieldName)) {
-                    url = reader.getString();
-                } else if ("headers".equalsIgnoreCase(fieldName)) {
-                    // Pass the JsonReader to another JsonSerializable to read the inner object.
-                    headers =
-                        reader.readMap(
-                            reader1 -> reader1.readArray(Object::toString));
-                } else if ("data".equalsIgnoreCase(fieldName)) {
-                    data = reader.readUntyped();
-                } else {
-                    reader.skipChildren();
+                    // Example of case-insensitive names and where serialization named don't match field names.
+                    if ("url".equalsIgnoreCase(fieldName)) {
+                        httpBinJSON.url = reader.getString();
+                    } else if ("headers".equalsIgnoreCase(fieldName)) {
+                        // Pass the JsonReader to another JsonSerializable to read the inner object.
+                        httpBinJSON.headers = reader.readMap(reader1 -> reader1.readArray(Object::toString));
+                    } else if ("data".equalsIgnoreCase(fieldName)) {
+                        httpBinJSON.data = reader.readUntyped();
+                    } else {
+                        reader.skipChildren();
+                    }
                 }
-            }
 
-            return new HttpBinJSON().url(url).headers(headers).data(data);
-        });
+                return httpBinJSON;
+            });
     }
 
     public String getHeaderValue(String name) {
