0dc628eeca8fa1d31f9cd8c3d87d30b1bbc4f553
Weidong Xu <weidxu@microsoft.com>
2023-11-14 19:20:23 +0800

    customize ImageGenerationOptions

--- sdk/openai/azure-ai-openai/src/main/java/com/azure/ai/openai/models/ImageGenerationOptions.java
+++ sdk/openai/azure-ai-openai/src/main/java/com/azure/ai/openai/models/ImageGenerationOptions.java
@@ -148,8 +148,7 @@ public final class ImageGenerationOptions {
      *
      * @return the responseFormat value.
      */
-    @Generated
-    public ImageGenerationResponseFormat getResponseFormat() {
+    ImageGenerationResponseFormat getResponseFormat() {
         return this.responseFormat;
     }
 
@@ -160,8 +159,7 @@ public final class ImageGenerationOptions {
      * @param responseFormat the responseFormat value to set.
      * @return the ImageGenerationOptions object itself.
      */
-    @Generated
-    public ImageGenerationOptions setResponseFormat(ImageGenerationResponseFormat responseFormat) {
+    ImageGenerationOptions setResponseFormat(ImageGenerationResponseFormat responseFormat) {
         this.responseFormat = responseFormat;
         return this;
     }
