b3053dec7c5381a1c10ef6ede41a4b8ac405f85d
Liudmila Molkova <limolkova@microsoft.com>
2023-11-14 18:16:20 -0800

    make tracing work with reactor and fix some nits

--- sdk/storage/azure-storage-blob-stress/src/main/java/com/azure/storage/blob/stress/scenarios/DownloadToFileStressScenario.java
+++ sdk/storage/azure-storage-blob-stress/src/main/java/com/azure/storage/blob/stress/scenarios/DownloadToFileStressScenario.java
@@ -3,48 +3,39 @@ package com.azure.storage.blob.stress.scenarios;
 import com.azure.core.util.BinaryData;
 import com.azure.core.util.Context;
 import com.azure.core.util.logging.ClientLogger;
-import com.azure.core.util.logging.LoggingEventBuilder;
 import com.azure.storage.blob.options.BlobDownloadToFileOptions;
 import com.azure.storage.blob.stress.builders.DownloadToFileScenarioBuilder;
 import com.azure.storage.blob.stress.scenarios.infra.BlobStressScenario;
 import com.azure.storage.stress.RandomInputStream;
-import io.opentelemetry.api.GlobalOpenTelemetry;
-import io.opentelemetry.api.trace.Span;
-import io.opentelemetry.api.trace.StatusCode;
-import io.opentelemetry.api.trace.Tracer;
-import io.opentelemetry.context.Scope;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.UncheckedIOException;
+import java.nio.ByteBuffer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Duration;
 import java.util.Base64;
 import java.util.UUID;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.zip.CRC32;
-
-import static com.azure.core.util.tracing.Tracer.PARENT_TRACE_CONTEXT_KEY;
 
 public class DownloadToFileStressScenario extends BlobStressScenario<DownloadToFileScenarioBuilder> {
 
     private static final ClientLogger LOGGER = new ClientLogger(DownloadToFileScenarioBuilder.class);
-    private static final Tracer TRACER = GlobalOpenTelemetry.getTracer("DownloadToFileStressScenario");
-    // TODO: move setUp and originalDataChecksum to DownloadToFileScenarioBuilder
-    private static long originalDataChecksum;
-    private static byte[] originalContentHead = new byte[1024];
-
+    private long originalDataChecksum;
+    private final ByteBuffer originalContentHead;
     private final Path originalDataPath;
     private final Path directoryPath;
+    private final int blobPrintableSize;
 
     public DownloadToFileStressScenario(DownloadToFileScenarioBuilder builder) {
         super(builder, /*initializeBlob*/true);
         this.directoryPath = builder.getDirectoryPath();
         this.originalDataPath = directoryPath.resolve("original-data-" + UUID.randomUUID());
+        this.blobPrintableSize = (int) Math.min(builder.getBlobSize(), 1024);
+        this.originalContentHead = ByteBuffer.allocate(blobPrintableSize);
     }
 
     @Override
@@ -54,14 +45,17 @@ public class DownloadToFileStressScenario extends BlobStressScenario<DownloadToF
 
         while ((timeoutNano = endTimeNano - System.nanoTime()) > 0) {
             Path downloadPath = directoryPath.resolve(UUID.randomUUID() + ".txt");
-            Span span = TRACER.spanBuilder("downloadToFile").startSpan();
-            Scope s = span.makeCurrent();
+            Context span = TRACER.start("downloadToFile", Context.NONE);
+            AutoCloseable s = TRACER.makeSpanCurrent(span);
             try {
                 BlobDownloadToFileOptions options = new BlobDownloadToFileOptions(downloadPath.toString());
 
                 getSyncBlobClient().downloadToFileWithResponse(options, Duration.ofNanos(timeoutNano), Context.NONE);
-                validateDownloadedContents(downloadPath);
-                trackSuccess(span);
+                if (validateDownloadedContents(downloadPath, span)) {
+                    trackSuccess(span);
+                } else {
+                    trackMismatch(span);
+                }
             } catch (Exception e) {
                 if (e.getMessage().contains("Timeout on blocking read")) {
                     // test timed out, so break out of loop instead of counting as a failure
@@ -70,56 +64,57 @@ public class DownloadToFileStressScenario extends BlobStressScenario<DownloadToF
                 trackFailure(span, e);
 
             } finally {
-                s.close();
+                downloadPath.toFile().delete();
+                closeScope(s);
             }
         }
     }
 
-    // do we need to delete the files after exit?
     @Override
     public Mono<Void> runAsync() {
         Path downloadPath = directoryPath.resolve(UUID.randomUUID() + ".txt");
         BlobDownloadToFileOptions options = new BlobDownloadToFileOptions(downloadPath.toString());
 
-        io.opentelemetry.context.Context.root().makeCurrent();
-        Span span = TRACER.spanBuilder("downloadToFileAsync").startSpan();
-        try(Scope s = span.makeCurrent()) {
-            Mono<Void> singleRun = getAsyncBlobClient()
+        Context span = TRACER.start("downloadToFileAsync", Context.NONE);
+        return getAsyncBlobClient()
                 .downloadToFileWithResponse(options)
-                .flatMap(response -> validateDownloadedContentsAsync(downloadPath))
-                    .contextWrite(ctx -> ctx.put(PARENT_TRACE_CONTEXT_KEY, span));
-
-            return singleRun
-                .doOnCancel(() -> {
-                    trackCancellation(span);
-                })
-                .doOnEach(signal -> {
-                    if (signal.isOnComplete()) {
+                .flatMap(ignored -> validateDownloadedContentsAsync(downloadPath, span))
+                .doOnCancel(() -> trackCancellation(span))
+                .doOnError(e -> trackFailure(span, e))
+                .doOnNext(match -> {
+                    if (match) {
                         trackSuccess(span);
-                    } else if (signal.isOnError()) {
-                        trackFailure(span, signal.getThrowable());
+                    } else {
+                        trackMismatch(span);
                     }
                 })
-                .doFinally(i -> downloadPath.toFile().delete());
+                .doFinally(i -> downloadPath.toFile().delete())
+                .contextWrite(reactor.util.context.Context.of("TRACING_CONTEXT", span))
+                .then();
+    }
+
+    private static void closeScope(AutoCloseable scope) {
+        try {
+            scope.close();
+        } catch (Exception e) {
+            // ignore
         }
     }
 
-    private void validateDownloadedContents(Path downloadPath) {
+    private boolean validateDownloadedContents(Path downloadPath, Context span) {
         // Use crc to check for file mismatch, avoiding every parallel test streaming original data from disk
         // If there's a mismatch, the original data can be streamed to check where the fault occurred
         // Data is streamed in the first place to avoid holding potentially gigs in memory
-        int length = 0;
-        byte[] contentHead = new byte[1024];
+        long length = 0;
+        ByteBuffer contentHead = ByteBuffer.allocate(blobPrintableSize);
         CRC32 dataCrc = new CRC32();
         try (InputStream file = Files.newInputStream(downloadPath)) {
             byte[] buf = new byte[4 * 1024 * 1024];
             int read;
-            boolean first = true;
             while ((read = file.read(buf)) != -1) {
                 dataCrc.update(buf, 0, read);
-                if (first) {
-                    System.arraycopy(buf, 0, contentHead, 0, Math.min(read, contentHead.length));
-                    first = false;
+                if (contentHead.hasRemaining()) {
+                    contentHead.put(buf, 0, Math.min(read, contentHead.remaining()));
                 }
                 length += read;
             }
@@ -128,42 +123,51 @@ public class DownloadToFileStressScenario extends BlobStressScenario<DownloadToF
             throw LOGGER.logExceptionAsError(new UncheckedIOException(e));
         }
 
-        long crc = dataCrc.getValue();
-        if (crc != originalDataChecksum) {
-            throw mismatchLogBuilder(crc, length, contentHead)
-                .log(new RuntimeException("mismatched crc"));
-        }
+        return checkMatch(dataCrc, length, contentHead, span);
     }
 
-    private Mono<Void> validateDownloadedContentsAsync(Path downloadPath) {
+    private Mono<Boolean> validateDownloadedContentsAsync(Path downloadPath, Context span) {
         CRC32 dataCrc = new CRC32();
-        AtomicLong length = new AtomicLong();
+        ByteBuffer contentHead = ByteBuffer.allocate(blobPrintableSize);
 
         return BinaryData.fromFile(downloadPath).toFluxByteBuffer()
-            .doOnNext(bb -> {
-                length.addAndGet(bb.remaining());
+            .map(bb -> {
+                long length = bb.remaining();
                 dataCrc.update(bb);
-            })
-            .doFinally(i -> {
-                long crc = dataCrc.getValue();
-                if (crc != originalDataChecksum) {
-                    // TODO: pass actual content here
-                    throw mismatchLogBuilder(crc, length.get(), new byte[0])
-                        .log(new RuntimeException("mismatched crc"));
+                if (contentHead.hasRemaining()) {
+                    bb.flip();
+                    while (contentHead.hasRemaining() && bb.hasRemaining()) {
+                        contentHead.put(bb.get());
+                    }
                 }
+
+                return length;
             })
-            .then();
+            .reduce(0L, Long::sum)
+            .map(l -> checkMatch(dataCrc, l, contentHead, span));
     }
 
-    private LoggingEventBuilder mismatchLogBuilder(long actualCrc, long actualLength, byte[] contentHead) {
+    private boolean checkMatch(CRC32 dataCrc, Long length, ByteBuffer contentHead, Context span) {
+        long crc = dataCrc.getValue();
+        if (crc != originalDataChecksum) {
+            logMismatch(crc, length, contentHead, span);
+            return false;
+        }
+        return true;
+    }
+
+    private void logMismatch(long actualCrc, long actualLength, ByteBuffer contentHead, Context span) {
         // future: if mismatch, compare against original file
-        return LOGGER.atError()
-            .addKeyValue("expectedCrc", originalDataChecksum)
-            .addKeyValue("actualCrc", actualCrc)
-            .addKeyValue("expectedLength", getBlobSize())
-            .addKeyValue("actualLength", actualLength)
-            .addKeyValue("originalContentHead", () -> Base64.getEncoder().encodeToString(originalContentHead))
-            .addKeyValue("actualContentHead", () -> Base64.getEncoder().encodeToString(contentHead));
+        AutoCloseable scope = TRACER.makeSpanCurrent(span);
+        LOGGER.atError()
+                .addKeyValue("expectedCrc", originalDataChecksum)
+                .addKeyValue("actualCrc", actualCrc)
+                .addKeyValue("expectedLength", getBlobSize())
+                .addKeyValue("actualLength", actualLength)
+                .addKeyValue("originalContentHead", () -> Base64.getEncoder().encodeToString(originalContentHead.array()))
+                .addKeyValue("actualContentHead", () -> Base64.getEncoder().encodeToString(contentHead.array()))
+                .log("mismatched crc");
+        closeScope(scope);
     }
 
     @Override
@@ -176,14 +180,12 @@ public class DownloadToFileStressScenario extends BlobStressScenario<DownloadToF
 
             byte[] buf = new byte[4 * 1024 * 1024];
             int read;
-            boolean first = true;
             while ((read = data.read(buf)) != -1) {
                 file.write(buf, 0, read);
                 blob.write(buf, 0, read);
                 dataCrc.update(buf, 0, read);
-                if (first) {
-                    System.arraycopy(buf, 0, originalContentHead, 0, Math.min(read, originalContentHead.length));
-                    first = false;
+                if (originalContentHead.hasRemaining()) {
+                    originalContentHead.put(buf, 0, Math.min(read, originalContentHead.remaining()));
                 }
             }
             file.flush();
