7f943d638e4924068508cd9c5e4781455711530c
hongli750210 <v-hongli1@microsoft.com>
2023-11-16 14:22:48 +0800

    Fixed package org.junit.jupiter.api is not visible.

--- sdk/eventgrid/azure-resourcemanager-eventgrid/src/test/java/module-info.java
+++ sdk/eventgrid/azure-resourcemanager-eventgrid/src/test/java/module-info.java
@@ -6,4 +6,7 @@ open module com.azure.resourcemanager.eventgrid {
     requires com.azure.identity;
     requires com.azure.messaging.eventgrid;
     requires com.azure.messaging.eventhubs;
+    requires org.junit.jupiter.engine;
+    requires org.junit.jupiter.params;
+    requires org.junit.jupiter.api;
 }
