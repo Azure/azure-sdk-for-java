2dce245ebfd097b284c33bfef9234829401b3b12
Charandeep Parisineti <cparisineti@microsoft.com>
2023-11-22 14:13:09 -0800

    Add customization

--- sdk/communication/azure-communication-jobrouter/src/main/java/com/azure/communication/jobrouter/JobRouterClientBuilder.java
+++ sdk/communication/azure-communication-jobrouter/src/main/java/com/azure/communication/jobrouter/JobRouterClientBuilder.java
@@ -8,6 +8,7 @@ import com.azure.communication.jobrouter.implementation.JobRouterClientImpl;
 import com.azure.core.annotation.Generated;
 import com.azure.core.annotation.ServiceClientBuilder;
 import com.azure.core.client.traits.ConfigurationTrait;
+import com.azure.core.client.traits.ConnectionStringTrait;
 import com.azure.core.client.traits.EndpointTrait;
 import com.azure.core.client.traits.HttpTrait;
 import com.azure.core.credential.KeyCredential;
@@ -44,8 +45,9 @@ import java.util.Objects;
  * A builder for creating a new instance of the JobRouterClient type.
  */
 @ServiceClientBuilder(serviceClients = { JobRouterClient.class, JobRouterAsyncClient.class })
-public final class JobRouterClientBuilder implements HttpTrait<JobRouterClientBuilder>,
-    ConfigurationTrait<JobRouterClientBuilder>, EndpointTrait<JobRouterClientBuilder> {
+public final class JobRouterClientBuilder
+    implements HttpTrait<JobRouterClientBuilder>, ConfigurationTrait<JobRouterClientBuilder>,
+    EndpointTrait<JobRouterClientBuilder>, ConnectionStringTrait<JobRouterClientBuilder> {
 
     @Generated
     private static final String SDK_NAME = "name";
