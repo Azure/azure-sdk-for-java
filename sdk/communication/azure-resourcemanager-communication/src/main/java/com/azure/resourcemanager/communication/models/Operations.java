06e3ddd480d4fe200f317c8b4b4eb7409afd4a5e
azure-sdk <azuresdk@microsoft.com>
2023-11-23 11:53:38 +0000

    [Automation] Generate Fluent Lite from communication#package-preview-2023-06

--- sdk/communication/azure-resourcemanager-communication/src/main/java/com/azure/resourcemanager/communication/models/Operations.java
+++ sdk/communication/azure-resourcemanager-communication/src/main/java/com/azure/resourcemanager/communication/models/Operations.java
@@ -7,31 +7,33 @@ package com.azure.resourcemanager.communication.models;
 import com.azure.core.http.rest.PagedIterable;
 import com.azure.core.util.Context;
 
-/** Resource collection API of Operations. */
+/**
+ * Resource collection API of Operations.
+ */
 public interface Operations {
     /**
      * List Operations
-     *
-     * <p>Lists all of the available REST API operations of the Microsoft.Communication provider.
-     *
+     * 
+     * Lists all of the available REST API operations of the Microsoft.Communication provider.
+     * 
      * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
      * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     * @return a list of REST API operations supported by an Azure Resource Provider as paginated response with {@link
-     *     PagedIterable}.
+     * @return a list of REST API operations supported by an Azure Resource Provider as paginated response with
+     * {@link PagedIterable}.
      */
     PagedIterable<Operation> list();
 
     /**
      * List Operations
-     *
-     * <p>Lists all of the available REST API operations of the Microsoft.Communication provider.
-     *
+     * 
+     * Lists all of the available REST API operations of the Microsoft.Communication provider.
+     * 
      * @param context The context to associate with this operation.
      * @throws IllegalArgumentException thrown if parameters fail the validation.
      * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
      * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     * @return a list of REST API operations supported by an Azure Resource Provider as paginated response with {@link
-     *     PagedIterable}.
+     * @return a list of REST API operations supported by an Azure Resource Provider as paginated response with
+     * {@link PagedIterable}.
      */
     PagedIterable<Operation> list(Context context);
 }
