a28acebc1a9823176272ec70d0941903a254dfa7
Harsimar Kaur (from Dev Box) <harskaur@microsoft.com>
2023-11-30 11:19:22 -0800

    using schema url for jmx metric name

--- sdk/monitor/azure-monitor-opentelemetry-exporter/src/main/java/com/azure/monitor/opentelemetry/exporter/implementation/MetricDataMapper.java
+++ sdk/monitor/azure-monitor-opentelemetry-exporter/src/main/java/com/azure/monitor/opentelemetry/exporter/implementation/MetricDataMapper.java
@@ -151,7 +151,11 @@ public class MetricDataMapper {
         pointBuilder.setValue(pointDataValue);
         // TODO (heya) why give it the same name as otel metric?
         //  it seems this field doesn't matter and only _MS.MetricId property matters?
-        pointBuilder.setName(metricData.getName());
+        if (metricData.getInstrumentationScopeInfo().getName().equals("jmx")) {
+            pointBuilder.setName(metricData.getResource().getSchemaUrl());
+        } else {
+            pointBuilder.setName(metricData.getName());
+        }
         metricTelemetryBuilder.setMetricPoint(pointBuilder);
 
         Attributes attributes = pointData.getAttributes();
