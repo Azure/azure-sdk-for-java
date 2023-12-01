0befdab6d876f55f74f55b29264d54b35d0e65bd
Jose Alvarez <jp.alvarezl@gmail.com>
2023-12-01 10:28:01 +0100

    Updated tsp-location commit hash

--- sdk/openai/azure-ai-openai/customization/src/main/java/ChatCompletionsToolCallCustomizations.java
+++ sdk/openai/azure-ai-openai/customization/src/main/java/ChatCompletionsToolCallCustomizations.java
@@ -12,7 +12,7 @@ import java.util.Arrays;
 /**
  * This class contains the customization code to customize the AutoRest generated code for OpenAI.
  */
-public class MyCustomization extends Customization {
+public class ChatCompletionsToolCallCustomizations extends Customization {
 
     @Override
     public void customize(LibraryCustomization customization, Logger logger) {
