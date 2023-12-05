bb02db38e5b3189af07c8efe9a67788bbbb3ec11
azure-sdk <azuresdk@microsoft.com>
2023-12-04 22:48:21 -0800

    Reset sources for azure-communication-sms to the release version 1.1.19.

--- sdk/communication/azure-communication-sms/src/test/java/com/azure/communication/sms/SmsTestBase.java
+++ sdk/communication/azure-communication-sms/src/test/java/com/azure/communication/sms/SmsTestBase.java
@@ -4,22 +4,31 @@
 package com.azure.communication.sms;
 
 import com.azure.communication.common.implementation.CommunicationConnectionString;
+import com.azure.core.credential.AccessToken;
+import com.azure.core.credential.AzureKeyCredential;
 import com.azure.core.credential.TokenCredential;
+import com.azure.core.credential.TokenRequestContext;
 import com.azure.core.http.HttpClient;
+import com.azure.core.test.TestBase;
+import com.azure.core.test.TestMode;
+import com.azure.core.util.Configuration;
+import com.azure.core.util.CoreUtils;
+import com.azure.core.util.logging.ClientLogger;
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.StringJoiner;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import reactor.core.publisher.Mono;
 import com.azure.core.http.HttpPipelineNextPolicy;
 import com.azure.core.http.HttpResponse;
-import com.azure.core.test.TestMode;
-import com.azure.core.test.TestProxyTestBase;
-import com.azure.core.test.models.CustomMatcher;
-import com.azure.core.test.models.TestProxySanitizer;
-import com.azure.core.test.models.TestProxySanitizerType;
-import com.azure.core.test.utils.MockTokenCredential;
-import com.azure.core.util.Configuration;
-import reactor.core.publisher.Mono;
 
-import java.util.Arrays;
+public class SmsTestBase extends TestBase {
+    protected static final TestMode TEST_MODE = initializeTestMode();
 
-public class SmsTestBase extends TestProxyTestBase {
     protected static final String CONNECTION_STRING = Configuration.getGlobalConfiguration()
         .get("COMMUNICATION_LIVETEST_STATIC_CONNECTION_STRING", "endpoint=https://REDACTED.communication.azure.com/;accesskey=QWNjZXNzS2V5");
 
@@ -34,21 +43,45 @@ public class SmsTestBase extends TestProxyTestBase {
 
     protected static final String MESSAGE = "Hello";
 
+    private static final StringJoiner JSON_PROPERTIES_TO_REDACT
+        = new StringJoiner("\":\"|\"", "\"", "\":\"")
+        .add("to");
+
+    private static final Pattern JSON_PROPERTY_VALUE_REDACTION_PATTERN
+        = Pattern.compile(String.format("(?:%s)(.*?)(?:\",|\"})", JSON_PROPERTIES_TO_REDACT.toString()),
+        Pattern.CASE_INSENSITIVE);
+
+    protected SmsClientBuilder getSmsClient(HttpClient httpClient) {
+        CommunicationConnectionString communicationConnectionString = new CommunicationConnectionString(CONNECTION_STRING);
+        String communicationEndpoint = communicationConnectionString.getEndpoint();
+        String communicationAccessKey = communicationConnectionString.getAccessKey(); 
+        
+        SmsClientBuilder builder = new SmsClientBuilder();
+        builder.endpoint(communicationEndpoint)
+            .credential(new AzureKeyCredential(communicationAccessKey))
+            .httpClient(httpClient == null ? interceptorManager.getPlaybackClient() : httpClient);
+        if (getTestMode() == TestMode.RECORD) {
+            List<Function<String, String>> redactors = new ArrayList<>();
+            redactors.add(data -> redact(data, JSON_PROPERTY_VALUE_REDACTION_PATTERN.matcher(data), "REDACTED"));
+            builder.addPolicy(interceptorManager.getRecordPolicy(redactors));
+        }
+        return builder;
+    }
 
     protected SmsClientBuilder getSmsClientWithToken(HttpClient httpClient, TokenCredential tokenCredential) {
         if (getTestMode() == TestMode.PLAYBACK) {
-            tokenCredential = new MockTokenCredential();
+            tokenCredential = new FakeCredentials();
         }
         SmsClientBuilder builder = new SmsClientBuilder();
         builder.endpoint(new CommunicationConnectionString(CONNECTION_STRING).getEndpoint())
             .credential(tokenCredential)
-            .httpClient(interceptorManager.isPlaybackMode() ? interceptorManager.getPlaybackClient() : httpClient);
+            .httpClient(httpClient == null ? interceptorManager.getPlaybackClient() : httpClient);
 
         if (getTestMode() == TestMode.RECORD) {
-            builder.addPolicy(interceptorManager.getRecordPolicy());
+            List<Function<String, String>> redactors = new ArrayList<>();
+            redactors.add(data -> redact(data, JSON_PROPERTY_VALUE_REDACTION_PATTERN.matcher(data), "REDACTED"));
+            builder.addPolicy(interceptorManager.getRecordPolicy(redactors));
         }
-        addTestProxySanitizer();
-        addTestProxyMatcher();
         return builder;
     }
 
@@ -56,32 +89,30 @@ public class SmsTestBase extends TestProxyTestBase {
         SmsClientBuilder builder = new SmsClientBuilder();
         builder
             .connectionString(CONNECTION_STRING)
-            .httpClient(interceptorManager.isPlaybackMode() ? interceptorManager.getPlaybackClient() : httpClient);
+            .httpClient(httpClient == null ? interceptorManager.getPlaybackClient() : httpClient);
 
         if (getTestMode() == TestMode.RECORD) {
-            builder.addPolicy(interceptorManager.getRecordPolicy());
+            List<Function<String, String>> redactors = new ArrayList<>();
+            redactors.add(data -> redact(data, JSON_PROPERTY_VALUE_REDACTION_PATTERN.matcher(data), "REDACTED"));
+            builder.addPolicy(interceptorManager.getRecordPolicy(redactors));
         }
-
-        addTestProxySanitizer();
-        addTestProxyMatcher();
         return builder;
     }
 
-    private void addTestProxySanitizer() {
-        if (!interceptorManager.isLiveMode()) {
-            interceptorManager.addSanitizers(Arrays.asList(new TestProxySanitizer("$..to", null,
-                "REDACTED",
-                TestProxySanitizerType.BODY_KEY), new TestProxySanitizer("$..from", null,
-                "REDACTED",
-                TestProxySanitizerType.BODY_KEY)));
-        }
-    }
-
-    private void addTestProxyMatcher() {
-        if (interceptorManager.isPlaybackMode()) {
-            interceptorManager.addMatchers(Arrays.asList(new CustomMatcher()
-                .setHeadersKeyOnlyMatch(Arrays.asList("x-ms-content-sha256", "x-ms-hmac-string-to-sign-base64"))
-                .setComparingBodies(false)));
+    private static TestMode initializeTestMode() {
+        ClientLogger logger = new ClientLogger(SmsTestBase.class);
+        String azureTestMode = Configuration.getGlobalConfiguration().get("AZURE_TEST_MODE");
+        if (azureTestMode != null) {
+            System.out.println("azureTestMode: " + azureTestMode);
+            try {
+                return TestMode.valueOf(azureTestMode.toUpperCase(Locale.US));
+            } catch (IllegalArgumentException var3) {
+                logger.error("Could not parse '{}' into TestEnum. Using 'Playback' mode.", azureTestMode);
+                return TestMode.PLAYBACK;
+            }
+        } else {
+            logger.info("Environment variable '{}' has not been set yet. Using 'Playback' mode.", "AZURE_TEST_MODE");
+            return TestMode.PLAYBACK;
         }
     }
 
@@ -100,6 +131,24 @@ public class SmsTestBase extends TestProxyTestBase {
                 return Mono.just(bufferedResponse);
             });
     }
+
+    static class FakeCredentials implements TokenCredential {
+        @Override
+        public Mono<AccessToken> getToken(TokenRequestContext tokenRequestContext) {
+            return Mono.just(new AccessToken("someFakeToken", OffsetDateTime.MAX));
+        }
+    }
+
+    private String redact(String content, Matcher matcher, String replacement) {
+        while (matcher.find()) {
+            String captureGroup = matcher.group(1);
+            if (!CoreUtils.isNullOrEmpty(captureGroup)) {
+                content = content.replace(matcher.group(1), replacement);
+            }
+        }
+        return content;
+    }
+
     protected boolean shouldEnableSmsTests() {
         return !Boolean.parseBoolean(SKIP_INT_SMS_TEST);
     }
