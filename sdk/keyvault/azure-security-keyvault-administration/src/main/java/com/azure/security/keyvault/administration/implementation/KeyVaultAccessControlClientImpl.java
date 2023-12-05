0ffa4c64fb491590e0b10b43945dbd4be5c6efe6
azure-sdk <azuresdk@microsoft.com>
2023-12-04 16:25:12 -0800

    Reset sources for azure-security-keyvault-administration to the release version 4.4.2.

--- sdk/keyvault/azure-security-keyvault-administration/src/main/java/com/azure/security/keyvault/administration/implementation/KeyVaultAccessControlClientImpl.java
+++ sdk/keyvault/azure-security-keyvault-administration/src/main/java/com/azure/security/keyvault/administration/implementation/KeyVaultAccessControlClientImpl.java
@@ -6,6 +6,7 @@ package com.azure.security.keyvault.administration.implementation;
 
 import com.azure.core.http.HttpPipeline;
 import com.azure.core.http.HttpPipelineBuilder;
+import com.azure.core.http.policy.CookiePolicy;
 import com.azure.core.http.policy.RetryPolicy;
 import com.azure.core.http.policy.UserAgentPolicy;
 import com.azure.core.util.serializer.JacksonAdapter;
@@ -80,7 +81,9 @@ public final class KeyVaultAccessControlClientImpl {
      */
     public KeyVaultAccessControlClientImpl(String apiVersion) {
         this(
-                new HttpPipelineBuilder().policies(new UserAgentPolicy(), new RetryPolicy()).build(),
+                new HttpPipelineBuilder()
+                        .policies(new UserAgentPolicy(), new RetryPolicy(), new CookiePolicy())
+                        .build(),
                 JacksonAdapter.createDefaultSerializerAdapter(),
                 apiVersion);
     }
