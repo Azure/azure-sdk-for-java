7e60f1f7194819fd0d7cbceea946ef33f9105f6e
Praveen Kuttappan <prmarott@microsoft.com>
2023-12-06 12:25:20 -0500

    Additional API change

--- sdk/template/azure-sdk-template/src/main/java/com/azure/sdk/template/Hello.java
+++ sdk/template/azure-sdk-template/src/main/java/com/azure/sdk/template/Hello.java
@@ -17,7 +17,7 @@ public class Hello {
      * Sample method.
      * @return message
      */
-    public final String getMessage1() {
+    public final String getMessage2() {
         return "hello";
     }
 }
