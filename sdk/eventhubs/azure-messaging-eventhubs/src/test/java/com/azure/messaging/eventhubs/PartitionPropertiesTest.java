a857912312873358d038cba7669325fba9b48f00
Connie <conniey@microsoft.com>
2023-12-06 13:29:52 -0800

    Fix partition properties test.

--- sdk/eventhubs/azure-messaging-eventhubs/src/test/java/com/azure/messaging/eventhubs/PartitionPropertiesTest.java
+++ sdk/eventhubs/azure-messaging-eventhubs/src/test/java/com/azure/messaging/eventhubs/PartitionPropertiesTest.java
@@ -22,10 +22,13 @@ public class PartitionPropertiesTest {
         final String lastEnqueuedOffset = "Last-Enqueued";
         final Instant lastEnqueuedTime = Instant.ofEpochSecond(1560639208);
         final boolean isEmpty = true;
+        final Long beginningReplicationSegment = 10L;
+        final Long lastEnqueuedReplicationSegment = 100L;
 
         // Act
         final PartitionProperties properties = new PartitionProperties(eventHub, id, beginningSequence, endSequence,
-            lastEnqueuedOffset, lastEnqueuedTime, isEmpty);
+            lastEnqueuedOffset, lastEnqueuedTime, isEmpty, beginningReplicationSegment,
+            lastEnqueuedReplicationSegment);
 
         // Assert
         Assertions.assertEquals(eventHub, properties.getEventHubName());
@@ -35,5 +38,8 @@ public class PartitionPropertiesTest {
         Assertions.assertEquals(lastEnqueuedOffset, properties.getLastEnqueuedOffset());
         Assertions.assertEquals(lastEnqueuedTime, properties.getLastEnqueuedTime());
         Assertions.assertEquals(isEmpty, properties.isEmpty());
+
+        Assertions.assertEquals(beginningReplicationSegment, properties.getBeginningReplicationSegment());
+        Assertions.assertEquals(lastEnqueuedReplicationSegment, properties.getLastEnqueuedReplicationSegment());
     }
 }
