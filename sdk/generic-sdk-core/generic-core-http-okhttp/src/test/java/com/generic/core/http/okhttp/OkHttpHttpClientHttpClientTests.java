8ac9aeff7daf4b2f20ab063222abfab0185a9334
vcolin7 <victor.y.asi@gmail.com>
2023-12-06 18:08:23 -0600

    Added an OkHttp implementation of `HttpClientTests`.

---
+++ sdk/generic-sdk-core/generic-core-http-okhttp/src/test/java/com/generic/core/http/okhttp/OkHttpHttpClientHttpClientTests.java
@@ -0,0 +1,48 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.generic.core.http.okhttp;
+
+import com.generic.core.http.client.HttpClient;
+import com.generic.core.http.client.HttpClientTests;
+import com.generic.core.http.client.HttpClientTestsServer;
+import com.generic.core.http.client.LocalTestServer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+
+@Execution(ExecutionMode.SAME_THREAD)
+public class OkHttpHttpClientHttpClientTests extends HttpClientTests {
+    private static LocalTestServer server;
+
+    @BeforeAll
+    public static void startTestServer() {
+        server = HttpClientTestsServer.getHttpClientTestsServer();
+
+        server.start();
+    }
+
+    @AfterAll
+    public static void stopTestServer() {
+        if (server != null) {
+            server.stop();
+        }
+    }
+
+    @Override
+    @Deprecated
+    protected int getPort() {
+        return server.getHttpPort();
+    }
+
+    @Override
+    protected String getServerUri(boolean secure) {
+        return secure ? server.getHttpsUri() : server.getHttpUri();
+    }
+
+    @Override
+    protected HttpClient createHttpClient() {
+        return new OkHttpHttpClientProvider().createInstance();
+    }
+}
