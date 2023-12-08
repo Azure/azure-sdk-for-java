28ad639783420395fbbe2757817b1956f0769407
Alan Zimmer <48699787+alzimmermsft@users.noreply.github.com>
2023-12-08 16:57:25 -0500

    Make a change so I can make a PR (#37999)
    * Make a change so I can make a PR
    
    * Compile fixes

--- sdk/generic-sdk-core/generic-core/src/main/java/com/generic/core/models/ExpandableStringEnum.java
+++ sdk/generic-sdk-core/generic-core/src/main/java/com/generic/core/models/ExpandableStringEnum.java
@@ -10,7 +10,7 @@ import java.util.Collection;
  *
  * @param <T> a specific expandable enum type
  */
-public abstract class ExpandableStringEnum<T extends ExpandableStringEnum<T>> {
+public interface ExpandableStringEnum<T extends ExpandableStringEnum<T>> {
     /**
      * Creates an instance of the specific expandable string enum from a String.
      *
@@ -19,7 +19,7 @@ public abstract class ExpandableStringEnum<T extends ExpandableStringEnum<T>> {
      * @return The expandable string enum instance.
      * @throws RuntimeException wrapping implementation class constructor exception (if any is thrown).
      */
-    public static <T extends ExpandableStringEnum<T>> T fromString(String name) {
+    static <T extends ExpandableStringEnum<T>> T fromString(String name) {
         throw new UnsupportedOperationException("Subtypes must override this method.");
     }
 
@@ -29,16 +29,16 @@ public abstract class ExpandableStringEnum<T extends ExpandableStringEnum<T>> {
      * @param <T> the class of the expandable string enum.
      * @return A collection of all known values for the given {@code clazz}.
      */
-    public static <T extends ExpandableStringEnum<T>> Collection<T> values() {
+    static <T extends ExpandableStringEnum<T>> Collection<T> values() {
         throw new UnsupportedOperationException("Subtypes must override this method.");
     }
 
     @Override
-    public abstract String toString();
+    String toString();
 
     @Override
-    public abstract int hashCode();
+    int hashCode();
 
     @Override
-    public abstract boolean equals(Object obj);
+    boolean equals(Object obj);
 }
