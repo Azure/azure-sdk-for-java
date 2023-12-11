63888aa0a08b5f951009695e78fe212c7498bfd2
Fabian Meiswinkel <fabian@meiswinkel.com>
2023-12-11 14:14:31 +0100

    Update RxDocumentClientImpl.java

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/RxDocumentClientImpl.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/RxDocumentClientImpl.java
@@ -541,12 +541,20 @@ public class RxDocumentClientImpl implements AsyncDocumentClient, IAuthorization
         // this.globalEndpointManager.init() must have been already called
         // hence asserting it
         if (databaseAccount == null) {
-            logger.error("Client initialization failed."
-                + " Check if the endpoint is reachable and if your auth token is valid. More info: https://aka.ms/cosmosdb-tsg-service-unavailable-java");
-            logger.error(" More error details: "+this.globalEndpointManager.getLatestDatabaseRefreshError());
-            throw new RuntimeException("Client initialization failed."
-                + " Check if the endpoint is reachable and if your auth token is valid. More info: https://aka.ms/cosmosdb-tsg-service-unavailable-java."
-                + " More error details: "+this.globalEndpointManager.getLatestDatabaseRefreshError());
+            Throwable databaseRefreshErrorSnapshot = this.globalEndpointManager.getLatestDatabaseRefreshError();
+            if (databaseRefreshErrorSnapshot != null) {
+                logger.error("Client initialization failed. Check if the endpoint is reachable and if your auth token "
+                                 + "is valid. More info: https://aka.ms/cosmosdb-tsg-service-unavailable-java",
+                             databaseRefreshErrorSnapshot
+                );
+                
+                throw new RuntimeException("Client initialization failed. Check if the endpoint is reachable and if your auth token "
+                                               + "is valid. More info: https://aka.ms/cosmosdb-tsg-service-unavailable-java.",
+                                           this.globalEndpointManager.getLatestDatabaseRefreshError());
+            } else {
+                logger.error("Client initialization failed."
+                    + " Check if the endpoint is reachable and if your auth token is valid. More info: https://aka.ms/cosmosdb-tsg-service-unavailable-java");
+            }
         }
 
         this.useMultipleWriteLocations = this.connectionPolicy.isMultipleWriteRegionsEnabled() && BridgeInternal.isEnableMultipleWriteLocations(databaseAccount);
