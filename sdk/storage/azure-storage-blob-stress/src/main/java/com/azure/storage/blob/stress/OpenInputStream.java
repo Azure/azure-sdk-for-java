7a41750e0b9f0cfaf27a095b2ea9c2e320265ea6
Rabab Ibrahim <ibrahimr@microsoft.com>
2023-12-13 13:37:15 -0800

    Adding all download scenarios for BlobClient (#37956)

---
+++ sdk/storage/azure-storage-blob-stress/src/main/java/com/azure/storage/blob/stress/OpenInputStream.java
@@ -0,0 +1,60 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.storage.blob.stress;
+
+import com.azure.core.util.BinaryData;
+import com.azure.core.util.Context;
+import com.azure.core.util.logging.ClientLogger;
+import com.azure.storage.blob.BlobAsyncClient;
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.stress.utils.OriginalContent;
+import com.azure.storage.blob.stress.utils.TelemetryHelper;
+import com.azure.storage.stress.StorageStressOptions;
+import reactor.core.publisher.Mono;
+
+import java.io.InputStream;
+
+import static com.azure.core.util.FluxUtil.monoError;
+
+public class OpenInputStream extends BlobScenarioBase<StorageStressOptions> {
+    private static final ClientLogger LOGGER = new ClientLogger(OpenInputStream.class);
+    private static final TelemetryHelper TELEMETRY_HELPER = new TelemetryHelper(OpenInputStream.class);
+    private static final OriginalContent ORIGINAL_CONTENT = new OriginalContent();
+    private final BlobClient syncClient;
+    private final BlobAsyncClient asyncNoFaultClient;
+
+
+    public OpenInputStream(StorageStressOptions options) {
+        super(options, TELEMETRY_HELPER);
+        this.syncClient = getSyncContainerClient().getBlobClient(options.getBlobName());
+        this.asyncNoFaultClient = getAsyncContainerClientNoFault().getBlobAsyncClient(options.getBlobName());
+    }
+
+    @Override
+    protected boolean runInternal(Context span) {
+        try (InputStream stream = syncClient.openInputStream()) {
+            return ORIGINAL_CONTENT.checkMatch(BinaryData.fromStream(stream), span).block().booleanValue();
+        } catch (Exception e) {
+            LOGGER.error("Failed to open input stream", e);
+            return false;
+        }
+    }
+
+    @Override
+    protected Mono<Boolean> runInternalAsync(Context context) {
+        return monoError(LOGGER, new RuntimeException("openInputStream() does not exist on the async client"));
+    }
+
+    @Override
+    public Mono<Void> globalSetupAsync() {
+        return super.globalSetupAsync()
+            .then(ORIGINAL_CONTENT.setupBlob(asyncNoFaultClient, options.getSize()));
+    }
+
+    @Override
+    public Mono<Void> globalCleanupAsync() {
+        return asyncNoFaultClient.delete()
+            .then(super.globalCleanupAsync());
+    }
+}
