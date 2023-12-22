2950594652b2a4771a1995e6a4364e9eee1f834a
Haoling Dong <87355844+haolingdong-msft@users.noreply.github.com>
2023-12-22 16:52:24 +0800

    revert change on sdk_init.sh

--- eng/mgmt/automation/sdk_init.sh
+++ eng/mgmt/automation/sdk_init.sh
@@ -3,12 +3,6 @@
 apt-get install python3.8
 apt-get install -y --upgrade python3-pip python3-setuptools
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-nvm install v18.15.0
-nvm alias default node
-
 cat << EOF > $1
 {}
 EOF
