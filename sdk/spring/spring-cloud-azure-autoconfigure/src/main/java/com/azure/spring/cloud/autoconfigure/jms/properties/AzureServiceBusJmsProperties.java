c4bee39c269e79d4731d931f09edb4847787cec2
Muyao <v-muyaofeng@microsoft.com>
2023-12-27 14:26:52 +0800

    update comments

--- sdk/spring/spring-cloud-azure-autoconfigure/src/main/java/com/azure/spring/cloud/autoconfigure/jms/properties/AzureServiceBusJmsProperties.java
+++ sdk/spring/spring-cloud-azure-autoconfigure/src/main/java/com/azure/spring/cloud/autoconfigure/jms/properties/AzureServiceBusJmsProperties.java
@@ -313,7 +313,7 @@ public class AzureServiceBusJmsProperties implements InitializingBean, Passwordl
         if (null == pricingTier || !pricingTier.matches("(?i)premium|standard")) {
             String errMessage = "'spring.jms.servicebus.pricing-tier' is not valid.";
             if ("basic".equalsIgnoreCase(pricingTier)) {
-                errMessage = "basic tier is not supported by Service Bus JMS. Please use standard or premium tier instead.";
+                errMessage = "The basic tier is not supported by Service Bus JMS. Please use standard or premium tier instead.";
             }
             throw new IllegalArgumentException(errMessage);
         }
