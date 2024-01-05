a1ea452cac76c0d34b847eebe0f5842c298f3921
Alan Zimmer <48699787+alzimmermsft@users.noreply.github.com>
2024-01-05 17:03:10 -0500

    New ExpandableStringEnum concept

--- sdk/core/azure-core/src/main/java/com/azure/core/models/CloudEventDataFormat.java
+++ sdk/core/azure-core/src/main/java/com/azure/core/models/CloudEventDataFormat.java
@@ -5,7 +5,12 @@ package com.azure.core.models;
 
 import com.azure.core.util.BinaryData;
 import com.azure.core.util.ExpandableStringEnum;
+import com.azure.core.util.SimpleCache;
 import com.fasterxml.jackson.annotation.JsonCreator;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Representation of the data format for a {@link CloudEvent}.
@@ -16,6 +21,14 @@ import com.fasterxml.jackson.annotation.JsonCreator;
  * @see CloudEvent#CloudEvent(String, String, BinaryData, com.azure.core.models.CloudEventDataFormat, String)
  */
 public final class CloudEventDataFormat extends ExpandableStringEnum<CloudEventDataFormat> {
+    private static final Map<String, CloudEventDataFormat> CONSTANTS = new ConcurrentHashMap<>();
+
+    // Should there be two caches? One for the constant values and one for the runtime values?
+    // Or should any value not in the constants be a new instance and not cached?
+    private static final SimpleCache<String, CloudEventDataFormat> RUNTIME = new SimpleCache<>();
+
+    private final String value;
+
     /**
      * Creates a new instance of {@link CloudEventDataFormat} without a {@link #toString()} value.
      * <p>
@@ -26,17 +39,23 @@ public final class CloudEventDataFormat extends ExpandableStringEnum<CloudEventD
      */
     @Deprecated
     public CloudEventDataFormat() {
+        this.value = null;
+    }
+
+    @Deprecated
+    private CloudEventDataFormat(String value) {
+        this.value = value;
     }
 
     /**
      * Bytes format.
      */
-    public static final CloudEventDataFormat BYTES = fromString("BYTES", CloudEventDataFormat.class);
+    public static final CloudEventDataFormat BYTES = fromStringConstant("BYTES");
 
     /**
      * JSON format.
      */
-    public static final CloudEventDataFormat JSON = fromString("JSON", CloudEventDataFormat.class);
+    public static final CloudEventDataFormat JSON = fromStringConstant("JSON");
 
     /**
      * Creates or gets a CloudEventDataFormat from its string representation.
@@ -46,6 +65,49 @@ public final class CloudEventDataFormat extends ExpandableStringEnum<CloudEventD
      */
     @JsonCreator
     public static CloudEventDataFormat fromString(String name) {
-        return fromString(name, CloudEventDataFormat.class);
+        if (name == null) {
+            return null;
+        }
+
+        CloudEventDataFormat format = CONSTANTS.get(name);
+        if (format != null) {
+            return format;
+        }
+
+        format = RUNTIME.get(name);
+        if (format != null) {
+            return format;
+        }
+
+        return RUNTIME.computeIfAbsent(name, CloudEventDataFormat::new);
+    }
+
+    // This method should only be called by constants.
+    private static CloudEventDataFormat fromStringConstant(String name) {
+        return CONSTANTS.computeIfAbsent(name, CloudEventDataFormat::new);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(CloudEventDataFormat.class, value);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+
+        if (!(obj instanceof CloudEventDataFormat)) {
+            return false;
+        }
+
+        CloudEventDataFormat other = (CloudEventDataFormat) obj;
+        return Objects.equals(value, other.value);
     }
 }
