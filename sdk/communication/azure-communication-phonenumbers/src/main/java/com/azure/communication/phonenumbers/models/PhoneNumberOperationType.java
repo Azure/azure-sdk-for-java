9de20fde55f70da7a0ae1f3715047ae2d9488a00
Meghesh Patil (from Dev Box) <mepatil@microsoft.com>
2024-01-04 17:30:01 -0800

    removed generated annotation

--- sdk/communication/azure-communication-phonenumbers/src/main/java/com/azure/communication/phonenumbers/models/PhoneNumberOperationType.java
+++ sdk/communication/azure-communication-phonenumbers/src/main/java/com/azure/communication/phonenumbers/models/PhoneNumberOperationType.java
@@ -4,13 +4,11 @@
 
 package com.azure.communication.phonenumbers.models;
 
-import com.azure.communication.phonenumbers.CodeCoverageAnnotation.Generated;
 import com.azure.core.util.ExpandableStringEnum;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import java.util.Collection;
 
 /** Defines values for PhoneNumberOperationType. */
-@Generated
 public final class PhoneNumberOperationType extends ExpandableStringEnum<PhoneNumberOperationType> {
     /** Static value purchase for PhoneNumberOperationType. */
     public static final PhoneNumberOperationType PURCHASE = fromString("purchase");
