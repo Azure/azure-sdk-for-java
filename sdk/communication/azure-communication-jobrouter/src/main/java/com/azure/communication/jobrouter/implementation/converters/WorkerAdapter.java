9126244d7cb3fe61fec0289ecf46ff73eb91336b
Charandeep Parisineti <cparisineti@microsoft.com>
2024-01-11 12:34:52 -0800

    Fix build

--- sdk/communication/azure-communication-jobrouter/src/main/java/com/azure/communication/jobrouter/implementation/converters/WorkerAdapter.java
+++ sdk/communication/azure-communication-jobrouter/src/main/java/com/azure/communication/jobrouter/implementation/converters/WorkerAdapter.java
@@ -20,14 +20,14 @@ public class WorkerAdapter {
      * @return RouterWorker
      */
     public static RouterWorkerInternal convertCreateWorkerOptionsToRouterWorker(CreateWorkerOptions createWorkerOptions) {
-        Map<String, Object> labels = createWorkerOptions.getLabels() != null ?
-            createWorkerOptions.getLabels().entrySet()
+        Map<String, Object> labels = createWorkerOptions.getLabels() != null
+            ? createWorkerOptions.getLabels().entrySet()
             .stream()
             .collect(Collectors.toMap(entry -> entry.getKey(),
                 entry -> RouterValueAdapter.getValue(entry.getValue()))) : null;
 
-        Map<String, Object> tags = createWorkerOptions.getTags() != null ?
-            createWorkerOptions.getTags().entrySet()
+        Map<String, Object> tags = createWorkerOptions.getTags() != null
+            ? createWorkerOptions.getTags().entrySet()
             .stream()
             .collect(Collectors.toMap(entry -> entry.getKey(),
                 entry -> RouterValueAdapter.getValue(entry.getValue()))) : null;
