cbcea5812465d5369f62765a34938debd2803b99
Liudmila Molkova <limolkova@microsoft.com>
2024-01-11 15:49:29 -0800

    lint

--- sdk/eventhubs/azure-messaging-eventhubs/CHANGELOG.md
+++ sdk/eventhubs/azure-messaging-eventhubs/CHANGELOG.md
@@ -1,6 +1,6 @@
 # Release History
 
-## 5.17.0-beta.1 (Unreleased)
+## 5.18.0-beta.1 (Unreleased)
 
 ### Features Added
 
--- sdk/servicebus/azure-messaging-servicebus/src/test/java/com/azure/messaging/servicebus/ServiceBusReceiverAsyncClientTest.java
+++ sdk/servicebus/azure-messaging-servicebus/src/test/java/com/azure/messaging/servicebus/ServiceBusReceiverAsyncClientTest.java
@@ -84,7 +84,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
