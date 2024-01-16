48cc0213e537dfc489df5f371b22500f05a4a4e8
Xilong Feng (WICRESOFT NORTH AMERICA LTD) <v-xilongfeng@microsoft.com>
2024-01-16 11:14:41 +0800

    Fix Cognitive Search Samples Issue

--- sdk/search/azure-search-documents/src/samples/java/com/azure/search/documents/indexes/CreateIndexWithFieldBuilderExample.java
+++ sdk/search/azure-search-documents/src/samples/java/com/azure/search/documents/indexes/CreateIndexWithFieldBuilderExample.java
@@ -33,11 +33,11 @@ public class CreateIndexWithFieldBuilderExample {
         List<SearchField> indexFields = SearchIndexClient.buildSearchFields(Hotel.class, new FieldBuilderOptions());
         String indexName = "hotels";
         List<SearchField> searchFieldList = new ArrayList<>();
-searchFieldList.add(new SearchField("hotelId", SearchFieldDataType.STRING)
+        searchFieldList.add(new SearchField("hotelId", SearchFieldDataType.STRING)
 	                .setKey(true)
 	                .setFilterable(true)
 	                .setSortable(true));
-SearchIndex newIndex = new SearchIndex(indexName, indexFields)
+        SearchIndex newIndex = new SearchIndex(indexName, indexFields)
 	                .setFields(searchFieldList);
 
         // Create index.
