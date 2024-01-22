451dc2607a194c47414848607e97a723e3002549
Vinay Gera <vigera@microsoft.com>
2024-01-22 10:04:36 -0800

    init web app and test resources setup

---
+++ sdk/identity/identity-webapp/src/main/java/com/azure/ManagedIdentityServlet.java
@@ -0,0 +1,17 @@
+package com.azure;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/mitest")
+public class ManagedIdentityServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body><h1>Hello, Managed Identity!</h1></body></html>");
+    }
+}
