60b2081d80c94fef46eb6a1d0c67c550b4d3b794
Annie Liang <64233642+xinlian12@users.noreply.github.com>
2024-01-22 17:20:18 -0800

    Update sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/models/ChangeFeedProcessorOptions.java
    Co-authored-by: Kushagra Thapar <kushuthapar@gmail.com>

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/models/ChangeFeedProcessorOptions.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/models/ChangeFeedProcessorOptions.java
@@ -382,7 +382,7 @@ public final class ChangeFeedProcessorOptions {
      * only use this config if you want to limit the RU that can be used for your change feed processing.
      * By using this config, it can slow down the process and cause the lag.
      * 
-     * For direct mode, please configure the throughput control group with the (total RU you would allow for changeFeed processing).
+     * For direct mode, please configure the throughput control group with the total RU you would allow for changeFeed processing.
      * For gateway mode, please configure the throughput control group with the (total RU you would allow for changeFeed processing / total CFP Instances).
      *
      * @param feedPollThroughputControlGroupConfig the throughput control for change feed requests for the monitored collection
