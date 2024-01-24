aefc2fe712da170b12aeda4a594c33753426c9d0
Jair Myree <jair.myree@gmail.com>
2024-01-24 09:56:01 -0800

    Update sdk/monitor/azure-monitor-ingestion/src/main/java/com/azure/monitor/ingestion/LogsIngestionClient.java
    Co-authored-by: Scott Addie <10702007+scottaddie@users.noreply.github.com>

--- sdk/monitor/azure-monitor-ingestion/src/main/java/com/azure/monitor/ingestion/LogsIngestionClient.java
+++ sdk/monitor/azure-monitor-ingestion/src/main/java/com/azure/monitor/ingestion/LogsIngestionClient.java
@@ -87,7 +87,7 @@ import static com.azure.monitor.ingestion.implementation.Utils.registerShutdownH
  *         {@link LogsIngestionClient#upload(String, String, Iterable, LogsUploadOptions) upload(String, String, Iterable, LogsUploadOptions)} - Uploads logs to Azure Monitor with options to configure the upload request.
  *     </li>
  *     <li>
- *         {@link LogsIngestionClient#uploadWithResponse(String, String, BinaryData, RequestOptions) uploadWithResponse(String, String, BinaryData, RequestOptions)} - Uploads logs to Azure Monitor with options to configure the HTTP request.
+ *         {@link LogsIngestionClient#uploadWithResponse(String, String, BinaryData, RequestOptions) uploadWithResponse(String, String, BinaryData, RequestOptions)} - Uploads logs to a Log Analytics workspace with options to configure the HTTP request.
  *     </li>
  * </ul>
  *
