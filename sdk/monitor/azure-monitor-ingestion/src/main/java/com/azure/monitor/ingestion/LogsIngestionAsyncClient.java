af9f90e98e88b1253eacd2f36309ecdb6c10ed56
Jair Myree <jair.myree@gmail.com>
2024-01-24 14:01:07 -0800

    Update sdk/monitor/azure-monitor-ingestion/src/main/java/com/azure/monitor/ingestion/LogsIngestionAsyncClient.java
    Co-authored-by: Scott Addie <10702007+scottaddie@users.noreply.github.com>

--- sdk/monitor/azure-monitor-ingestion/src/main/java/com/azure/monitor/ingestion/LogsIngestionAsyncClient.java
+++ sdk/monitor/azure-monitor-ingestion/src/main/java/com/azure/monitor/ingestion/LogsIngestionAsyncClient.java
@@ -83,7 +83,7 @@ import static com.azure.monitor.ingestion.implementation.Utils.gzipRequest;
  *         {@link LogsIngestionAsyncClient#upload(String, String, Iterable) upload(String, String, Iterable)} - Uploads logs to a Log Analytics workspace.
  *     </li>
  *     <li>
- *         {@link LogsIngestionAsyncClient#upload(String, String, Iterable, LogsUploadOptions) upload(String, String, Iterable, LogsUploadOptions)} - Uploads logs to Azure Monitor with options to configure the upload request.
+ *         {@link LogsIngestionAsyncClient#upload(String, String, Iterable, LogsUploadOptions) upload(String, String, Iterable, LogsUploadOptions)} - Uploads logs to a Log Analytics workspace with options to configure the upload request.
  *     </li>
  *     <li>
  *         {@link LogsIngestionAsyncClient#uploadWithResponse(String, String, BinaryData, RequestOptions) uploadWithResponse(String, String, BinaryData, RequestOptions)} - Uploads logs to Azure Monitor with options to configure the HTTP request.
