2676134d87e1f98a4367fe24782ac5564a9adbc8
Victor Colin Amador <vicolina@microsoft.com>
2024-01-24 15:36:54 -0800

    Fixed build issues.

--- sdk/generic-sdk-core/generic-core-http-okhttp/src/test/java/com/generic/core/http/okhttp/OkHttpHttpClientTests.java
+++ sdk/generic-sdk-core/generic-core-http-okhttp/src/test/java/com/generic/core/http/okhttp/OkHttpHttpClientTests.java
@@ -7,7 +7,6 @@ import com.generic.core.http.client.HttpClient;
 import com.generic.core.http.models.HttpMethod;
 import com.generic.core.http.models.HttpRequest;
 import com.generic.core.http.models.HttpResponse;
-import com.generic.core.implementation.test.LocalTestServer;
 import com.generic.core.models.Header;
 import com.generic.core.models.HeaderName;
 import com.generic.core.models.Headers;
