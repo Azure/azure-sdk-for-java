ee0606248fe89f3caa56fccd982ebfb52e41092f
Liudmila Molkova <limolkova@microsoft.com>
2024-01-25 13:42:12 -0800

    Update EH stress tests (#38417)
    * Update EH stress tests: metrics, forwarding case, scenarios

--- sdk/eventhubs/azure-messaging-eventhubs-stress/src/main/java/com/azure/messaging/eventhubs/stress/scenarios/EventForwarder.java
+++ sdk/eventhubs/azure-messaging-eventhubs-stress/src/main/java/com/azure/messaging/eventhubs/stress/scenarios/EventForwarder.java
@@ -3,18 +3,29 @@
 
 package com.azure.messaging.eventhubs.stress.scenarios;
 
+import com.azure.core.amqp.AmqpRetryOptions;
 import com.azure.messaging.eventhubs.EventData;
+import com.azure.messaging.eventhubs.EventHubClientBuilder;
+import com.azure.messaging.eventhubs.EventHubProducerAsyncClient;
 import com.azure.messaging.eventhubs.EventProcessorClient;
 import com.azure.messaging.eventhubs.LoadBalancingStrategy;
 import com.azure.messaging.eventhubs.models.EventBatchContext;
 import com.azure.messaging.eventhubs.models.EventPosition;
+import com.azure.messaging.eventhubs.models.SendOptions;
 import com.azure.messaging.eventhubs.stress.util.SimulatedFailure;
 import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.trace.Span;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -23,13 +34,11 @@ import static com.azure.messaging.eventhubs.stress.util.TestUtils.createMessageP
 import static com.azure.messaging.eventhubs.stress.util.TestUtils.getProcessorBuilder;
 
 /**
- * Test for EventProcessorClient
+ * Test for EventProcessorClient that forwards messages
  */
-@Component("EventProcessor")
-public class EventProcessor extends EventHubsScenario {
+@Component("EventForwarder")
+public class EventForwarder extends EventHubsScenario {
     private final AtomicBoolean stopping = new AtomicBoolean(false);
-    @Value("${PROCESS_CALLBACK_DURATION_MAX_IN_MS:50}")
-    private int processMessageDurationMaxInMs;
 
     @Value("${MAX_BATCH_SIZE:10}")
     private int maxBatchSize;
@@ -43,16 +52,27 @@ public class EventProcessor extends EventHubsScenario {
     @Value("${ENABLE_CHECKPOINT:true}")
     private boolean enableCheckpoint;
 
-    @Value("${PROCESSOR_INSTANCES_COUNT:2}")
+    @Value("${FORWARD_EVENTHUBS_CONNECTION_STRING:#{null}}")
+    private String forwardConnectionString;
+
+    @Value("${FORWARD_EVENT_HUB_NAME:#{null}}")
+    private String forwardEventHubName;
+
+    @Value("${FORWARD_PARTITIONS_COUNT:8}")
+    private int forwardPartitionsCount;
+
+    @Value("${PROCESSOR_INSTANCES_COUNT:1}")
     private int processorInstancesCount;
 
     @Value("${PROCESSOR_FAILURE_RATIO:0}")
     private double processorFailureRatio;
 
     private byte[] expectedPayload;
+    private EventHubProducerAsyncClient producerClient;
 
     @Override
     public void run() {
+        producerClient = getForwardProducer();
         expectedPayload = createMessagePayload(options.getMessageSize()).toBytes();
         Duration maxWaitTime = maxWaitTimeInMs > 0 ? Duration.ofMillis(maxWaitTimeInMs) : null;
 
@@ -79,35 +99,50 @@ public class EventProcessor extends EventHubsScenario {
         }
     }
 
+    private EventHubProducerAsyncClient getForwardProducer() {
+        return toClose(new EventHubClientBuilder()
+            .connectionString(forwardConnectionString)
+            .eventHubName(forwardEventHubName)
+            .retryOptions(new AmqpRetryOptions().setTryTimeout(Duration.ofSeconds(10)))
+            .buildAsyncProducerClient());
+    }
+
     @SuppressWarnings("try")
     private void processBatch(EventBatchContext batchContext) {
         if (stopping.get()) {
             return;
         }
 
-        for (EventData eventData : batchContext.getEvents()) {
-            checkEvent(eventData, batchContext.getPartitionContext().getPartitionId());
-        }
+        Map<String, List<EventData>> batches = regroupEvents(batchContext);
+        Flux.fromStream(batches.entrySet().stream().filter(e -> e.getValue() != null && !e.getValue().isEmpty()))
+            .flatMap(entry -> {
+                if (ThreadLocalRandom.current().nextDouble() < processorFailureRatio) {
+                    return Mono.error(new SimulatedFailure());
+                }
 
-        blockingWait(getWaitTime());
-        if (processorFailureRatio > 0) {
-            double failChance = ThreadLocalRandom.current().nextDouble();
-            if (failChance < processorFailureRatio) {
-                throw new SimulatedFailure();
-            }
-        }
+                return producerClient.send(entry.getValue(), new SendOptions().setPartitionKey(entry.getKey()))
+                       .doOnCancel(() -> telemetryHelper.recordError("cancelled", "sendBatch", entry.getKey()))
+                       .doOnError(err -> telemetryHelper.recordError(err, "sendBatch", entry.getKey()));
+            })
+            .parallel(batches.entrySet().size(), 1)
+            .runOn(Schedulers.boundedElastic())
+            .then()
+            .block();
 
         if (enableCheckpoint) {
             batchContext.updateCheckpoint();
         }
     }
 
-    private Duration getWaitTime() {
-        if (processMessageDurationMaxInMs != 0) {
-            return Duration.ofMillis(ThreadLocalRandom.current().nextInt(processMessageDurationMaxInMs));
-        }
+    private Map<String, List<EventData>> regroupEvents(EventBatchContext batchContext) {
+        Map<String, List<EventData>> batches = new HashMap<>();
+        for (EventData eventData : batchContext.getEvents()) {
+            checkEvent(eventData, batchContext.getPartitionContext().getPartitionId());
 
-        return Duration.ZERO;
+            int forwardPartitionId = ThreadLocalRandom.current().nextInt(forwardPartitionsCount);
+            batches.computeIfAbsent(String.valueOf(forwardPartitionId), k -> new ArrayList<>()).add(eventData);
+        }
+        return batches;
     }
 
     private void checkEvent(EventData message, String partitionId) {
@@ -126,7 +161,8 @@ public class EventProcessor extends EventHubsScenario {
         span.setAttribute(AttributeKey.longKey("maxWaitTimeInMs"), maxWaitTimeInMs);
         span.setAttribute(AttributeKey.booleanKey("enableCheckpoint"), enableCheckpoint);
         span.setAttribute(AttributeKey.longKey("prefetchCount"), prefetchCount);
-        span.setAttribute(AttributeKey.longKey("processMessageDurationMaxInMs"), processMessageDurationMaxInMs);
+        span.setAttribute(AttributeKey.stringKey("forwardEventHubName"), forwardEventHubName);
+        span.setAttribute(AttributeKey.longKey("forwardPartitionsCount"), forwardPartitionsCount);
         span.setAttribute(AttributeKey.longKey("processorInstancesCount"), processorInstancesCount);
         span.setAttribute(AttributeKey.doubleKey("processorFailureRatio"), processorFailureRatio);
     }
