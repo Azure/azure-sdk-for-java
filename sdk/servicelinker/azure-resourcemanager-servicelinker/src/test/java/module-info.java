3fdc3ca4ca847bd8c9744db311439c33d1ab3a2c
Alan Zimmer <48699787+alzimmermsft@users.noreply.github.com>
2024-01-31 17:13:26 -0500

    Fix compilation cases

--- sdk/servicelinker/azure-resourcemanager-servicelinker/src/test/java/module-info.java
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-open module com.azure.resourcemanager.servicelinker {
-    requires com.azure.resourcemanager;
-    requires com.azure.identity;
-}
