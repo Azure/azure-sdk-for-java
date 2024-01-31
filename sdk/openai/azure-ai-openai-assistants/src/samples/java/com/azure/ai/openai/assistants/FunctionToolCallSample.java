e197d7878510e89ad8c70133b500d2375101e92e
José Álvarez <josealvar@microsoft.com>
2024-01-31 17:49:41 +0100

    WIP

--- sdk/openai/azure-ai-openai-assistants/src/samples/java/com/azure/ai/openai/assistants/FunctionToolCallSample.java
+++ sdk/openai/azure-ai-openai-assistants/src/samples/java/com/azure/ai/openai/assistants/FunctionToolCallSample.java
@@ -3,5 +3,12 @@
 
 package com.azure.ai.openai.assistants;
 
+import com.azure.core.util.Configuration;
+
 public class FunctionToolCallSample {
+
+    public static void main(String[] args) throws InterruptedException {
+        String apiKey = Configuration.getGlobalConfiguration().get("NON_AZURE_OPENAI_KEY");
+        String deploymentOrModelId = "gpt-4-1106-preview";
+    }
 }
