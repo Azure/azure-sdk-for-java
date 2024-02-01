c8afa7ad3fb366989e26329de298d5437e9dcc84
Jose Alvarez <jpalvarezl@users.noreply.github.com>
2024-02-01 19:09:06 +0100

    [OpenAI] Assistants Function Tool Call tests and sample (#38566)
    * extracted class for cleanup
    
    * Cleaned up and corrected logic for polling repeat
    
    * Function definitions in helper class seemingly complete
    
    * Assistant creation works
    
    * Added sync tests for functions
    
    * Added tests for Functions
    
    * Removed unused imports
    
    * Added license statement to source files
    
    * WIP
    
    * Added test files recordings
    
    * Fixed Azure tests
    
    * Style checks passing
    
    * Added function sample file
    
    * Style checks

--- sdk/openai/azure-ai-openai-assistants/src/test/java/com/azure/ai/openai/assistants/AssistantsClientTestBase.java
+++ sdk/openai/azure-ai-openai-assistants/src/test/java/com/azure/ai/openai/assistants/AssistantsClientTestBase.java
@@ -21,7 +21,9 @@ import com.azure.ai.openai.assistants.models.ThreadDeletionStatus;
 import com.azure.ai.openai.assistants.models.ThreadInitializationMessage;
 import com.azure.ai.openai.assistants.models.ThreadMessage;
 import com.azure.ai.openai.assistants.models.ThreadRun;
+import com.azure.ai.openai.assistants.models.ToolDefinition;
 import com.azure.ai.openai.assistants.models.UploadFileRequest;
+import com.azure.ai.openai.assistants.implementation.FunctionsToolCallHelper;
 import com.azure.core.credential.AzureKeyCredential;
 import com.azure.core.credential.KeyCredential;
 import com.azure.core.http.HttpClient;
@@ -40,6 +42,7 @@ import reactor.test.StepVerifier;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
@@ -194,6 +197,23 @@ public abstract class AssistantsClientTestBase extends TestProxyTestBase {
             .setTools(Arrays.asList(new RetrievalToolDefinition()));
 
         testRunner.accept(uploadRequest, assistantOptions);
+    }
+
+    void createFunctionToolCallRunner(BiConsumer<AssistantCreationOptions, AssistantThreadCreationOptions> testRunner) {
+        FunctionsToolCallHelper functionsToolCallHelper = new FunctionsToolCallHelper();
+        List<ToolDefinition> toolDefinition = Arrays.asList(
+            functionsToolCallHelper.getAirlinePriceToDestinationForSeasonDefinition(),
+            functionsToolCallHelper.getFavoriteVacationDestinationDefinition(),
+            functionsToolCallHelper.getPreferredAirlineForSeasonDefinition()
+        );
+        AssistantCreationOptions assistantOptions = new AssistantCreationOptions(GPT_4_1106_PREVIEW)
+            .setName("Java SDK Function Tool Call Test")
+            .setInstructions("You are a helpful assistant that can help fetch data from files you know about.")
+            .setTools(toolDefinition);
+
+        AssistantThreadCreationOptions threadCreationOptions = new AssistantThreadCreationOptions();
+
+        testRunner.accept(assistantOptions, threadCreationOptions);
     }
 
     void uploadAssistantTextFileRunner(Consumer<UploadFileRequest> testRunner) {
--- sdk/openai/azure-ai-openai-assistants/src/test/java/com/azure/ai/openai/assistants/AzureRetrievalAsyncTest.java
+++ sdk/openai/azure-ai-openai-assistants/src/test/java/com/azure/ai/openai/assistants/AzureRetrievalAsyncTest.java
@@ -8,11 +8,11 @@ import com.azure.ai.openai.assistants.models.AssistantThread;
 import com.azure.ai.openai.assistants.models.AssistantThreadCreationOptions;
 import com.azure.ai.openai.assistants.models.MessageRole;
 import com.azure.ai.openai.assistants.models.MessageTextContent;
-import com.azure.ai.openai.assistants.models.OpenAIFile;
 import com.azure.ai.openai.assistants.models.OpenAIPageableListOfThreadMessage;
 import com.azure.ai.openai.assistants.models.RunStatus;
 import com.azure.ai.openai.assistants.models.ThreadMessage;
 import com.azure.ai.openai.assistants.models.ThreadRun;
+import com.azure.ai.openai.assistants.implementation.AsyncUtils;
 import com.azure.core.http.HttpClient;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -42,59 +42,51 @@ public class AzureRetrievalAsyncTest extends AssistantsClientTestBase {
             StepVerifier.create(client.uploadFile(uploadFileRequest)
                 .flatMap(openAIFile -> {
                     // Create assistant
-                    CleanUp cleanUp = new CleanUp();
+                    AsyncUtils cleanUp = new AsyncUtils();
                     assistantCreationOptions.setFileIds(Arrays.asList(openAIFile.getId()));
-                    cleanUp.file = openAIFile;
+                    cleanUp.setFile(openAIFile);
                     return client.createAssistant(assistantCreationOptions).zipWith(Mono.just(cleanUp));
                 }).flatMap(tuple -> {
                     Assistant assistant = tuple.getT1();
-                    CleanUp cleanUp = tuple.getT2();
-                    cleanUp.assistant = assistant;
+                    AsyncUtils cleanUp = tuple.getT2();
+                    cleanUp.setAssistant(assistant);
 
                     return client.createThread(new AssistantThreadCreationOptions())
                         .zipWith(Mono.just(cleanUp));
                 }).flatMap(tuple -> {
                     AssistantThread thread = tuple.getT1();
-                    CleanUp cleanUp = tuple.getT2();
-                    cleanUp.thread = thread;
+                    AsyncUtils cleanUp = tuple.getT2();
+                    cleanUp.setThread(thread);
 
                     return client.createMessage(
                         thread.getId(),
                         MessageRole.USER,
                         "Can you give me the documented codes for 'banana' and 'orange'?"
                     ).flatMap(_message ->
-                        client.createRun(cleanUp.thread, cleanUp.assistant)
+                        client.createRun(cleanUp.getThread(), cleanUp.getAssistant())
                             .flatMap(createdRun ->
-                                client.getRun(cleanUp.thread.getId(), createdRun.getId()).zipWith(Mono.just(cleanUp))
-                                    .repeat()
-                                    .delayElements(Duration.ofMillis(500))
-                                    .takeWhile(tuple2 -> {
+                                client.getRun(cleanUp.getThread().getId(), createdRun.getId()).zipWith(Mono.just(cleanUp))
+                                    .repeatWhen(completed -> completed.delayElements(Duration.ofMillis(500)))
+                                    .takeUntil(tuple2 -> {
                                         ThreadRun run = tuple2.getT1();
 
-                                        return run.getStatus() == RunStatus.IN_PROGRESS
-                                            || run.getStatus() == RunStatus.QUEUED;
+                                        return run.getStatus() != RunStatus.IN_PROGRESS
+                                            && run.getStatus() != RunStatus.QUEUED;
                                     })
                                     .last()
                             )
                     );
                 }).flatMap(tuple -> {
-                    // we do one last request, that gets the Run with the Status that broke the above loop
                     ThreadRun run = tuple.getT1();
-                    CleanUp cleanUp = tuple.getT2();
-
-                    return client.getRun(cleanUp.thread.getId(), run.getId()).zipWith(Mono.just(cleanUp));
-                })
-                .flatMap(tuple -> {
-                    ThreadRun run = tuple.getT1();
-                    CleanUp cleanUp = tuple.getT2();
+                    AsyncUtils cleanUp = tuple.getT2();
 
                     assertEquals(RunStatus.COMPLETED, run.getStatus());
-                    assertEquals(cleanUp.assistant.getId(), run.getAssistantId());
+                    assertEquals(cleanUp.getAssistant().getId(), run.getAssistantId());
 
-                    return client.listMessages(cleanUp.thread.getId()).zipWith(Mono.just(cleanUp));
+                    return client.listMessages(cleanUp.getThread().getId()).zipWith(Mono.just(cleanUp));
                 }).map(tuple -> {
                     OpenAIPageableListOfThreadMessage messageList = tuple.getT1();
-                    CleanUp cleanUp = tuple.getT2();
+                    AsyncUtils cleanUp = tuple.getT2();
 
                     assertEquals(2, messageList.getData().size());
                     ThreadMessage firstMessage = messageList.getData().get(0);
@@ -108,18 +100,12 @@ public class AzureRetrievalAsyncTest extends AssistantsClientTestBase {
 
                     return cleanUp;
                 })
-                .flatMap(cleanUp -> client.deleteAssistant(cleanUp.assistant.getId())
-                    .flatMap(_unused -> client.deleteFile(cleanUp.file.getId()))
-                    .flatMap(_unused -> client.deleteThread(cleanUp.thread.getId()))))
+                .flatMap(cleanUp -> client.deleteAssistant(cleanUp.getAssistant().getId())
+                    .flatMap(_unused -> client.deleteFile(cleanUp.getFile().getId()))
+                    .flatMap(_unused -> client.deleteThread(cleanUp.getThread().getId()))))
                 // last deletion asserted
                 .assertNext(threadDeletionStatus -> assertTrue(threadDeletionStatus.isDeleted()))
                 .verifyComplete();
         });
-    }
-
-    private class CleanUp {
-        Assistant assistant;
-        AssistantThread thread;
-        OpenAIFile file;
     }
 }
