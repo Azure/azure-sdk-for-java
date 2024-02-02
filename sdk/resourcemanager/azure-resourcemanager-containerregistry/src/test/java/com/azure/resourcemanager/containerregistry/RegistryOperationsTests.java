78be46d23e3283e63fee1c45149c520ff722c02d
hongli750210 <v-hongli1@microsoft.com>
2024-02-02 09:58:28 +0800

    Fixed for Ability to create Container Registry with zone redundancy (issue#38488)

---
+++ sdk/resourcemanager/azure-resourcemanager-containerregistry/src/test/java/com/azure/resourcemanager/containerregistry/RegistryOperationsTests.java
@@ -0,0 +1,34 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.resourcemanager.containerregistry;
+
+import com.azure.core.management.Region;
+import com.azure.resourcemanager.containerregistry.models.Registry;
+import com.azure.resourcemanager.containerregistry.models.ZoneRedundancy;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RegistryOperationsTests extends RegistryTest {
+
+    @Override
+    protected void cleanUpResources() {
+        resourceManager.resourceGroups().beginDeleteByName(rgName);
+    }
+
+    @Test
+    public void canCreateContainerRegisterWithZoneRedundancy() {
+        final String acrName = generateRandomResourceName("acr", 10);
+        Registry registry =
+            registryManager
+                .containerRegistries()
+                .define(acrName)
+                .withRegion(Region.US_EAST)
+                .withNewResourceGroup(rgName)
+                .withPremiumSku()
+                .withRegistryNameAsAdminUser()
+                .withZoneRedundancy()
+                .create();
+        Assertions.assertEquals(ZoneRedundancy.ENABLED, registry.zoneRedundancy());
+    }
+}
