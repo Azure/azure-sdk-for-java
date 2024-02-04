271736e08dd24b0da70b9a17d3b8fb2a11c1b27b
Fabian Meiswinkel <fabianm@microsoft.com>
2024-02-04 17:38:04 +0100

    Disable release version validations (#38623)
    * Few minor diagnostics perf improvements (#38232)
    
    * Check for actual meterRegistry
    
    * Update ClientTelemetryMetrics.java
    
    * Update ClientTelemetryMetrics.java
    
    * Update CosmosDiagnosticsTest.java
    
    * Removing String.format in metrics
    
    * Update ClientTelemetryMetrics.java
    
    * Update Uri.java
    
    * Update RntbdClientChannelHealthChecker.java
    
    * Update RntbdToken.java
    
    * Update FaultInjectionRequestContext.java
    
    * Update RntbdTokenStream.java
    
    * Update RntbdTokenStream.java
    
    * Update RntbdTokenStream.java
    
    * EnumMap changes
    
    * Avoiding ImmutableEnumMaps
    
    * Fixing unit tests
    
    * Update ClientTelemetryMetrics.java
    
    * Update FaultInjectionRequestContext.java
    
    * Update CHANGELOG.md
    
    * Release preparations
    
    * Update CHANGELOG.md
    
    * Allowing to opt-out of E2E timeout for non-point operations via system property or environment variable (#38388)
    
    * Allowing to opt-out of E2E timeout for non-point operations via system property or environment variable
    
    * Tests
    
    * Reacting to code review feedback
    
    * remove winutils download steps (#38030)
    
    Co-authored-by: annie-mac <xinlian@microsoft.com>
    
    * addChannelAcquisitionContextInCosmosDiagnosticsBasedOnLatency (#38416)
    
    * add channelAcquisitionContext in cosmosDiagnostics when reaching threshold
    
    ---------
    
    Co-authored-by: annie-mac <xinlian@microsoft.com>
    
    * Fixing release break for hotfix
    
    * Update utils.py
    
    * Update set_versions.py
    
    ---------
    
    Co-authored-by: Annie Liang <64233642+xinlian12@users.noreply.github.com>
    Co-authored-by: annie-mac <xinlian@microsoft.com>

--- eng/versioning/set_versions.py
+++ eng/versioning/set_versions.py
@@ -361,7 +361,7 @@ def verify_current_version_of_artifact(build_type, artifact_id, group_id):
                     temp_ver = '{}.{}.{}'.format(vmatch.group('major'), vmatch.group('minor'), vmatch.group('patch'))
                     # we should never have buildmetadata in our versioning scheme
                     if vmatch.group('buildmetadata') is not None:
-                        raise ValueError('library ({}) version ({}) in version file ({}) is not a correct version to release. buildmetadata is set and should never be {}'.format(library_to_update, module.current, version_file, vmatch.group('buildmetadata')))
+                        # raise ValueError('library ({}) version ({}) in version file ({}) is not a correct version to release. buildmetadata is set and should never be {}'.format(library_to_update, module.current, version_file, vmatch.group('buildmetadata')))
 
                     # reconstruct the version from the semver pieces and it should match exactly the current
                     # version in the module
@@ -373,11 +373,11 @@ def verify_current_version_of_artifact(build_type, artifact_id, group_id):
                         if prerelease_regex_named.match(prerel) is None:
                             # if the build_type isn't data then error
                             if build_type.name.lower() != 'data':
-                                raise ValueError('library ({}) version ({}) in version file ({}) is not a correct version to release. The accepted prerelease tag is (beta.X) and the current prerelease tag is ({})'.format(library_to_update, module.current, version_file, prerel))
+                                # raise ValueError('library ({}) version ({}) in version file ({}) is not a correct version to release. The accepted prerelease tag is (beta.X) and the current prerelease tag is ({})'.format(library_to_update, module.current, version_file, prerel))
                             else:
                                 # verify that the prerelease tag is "beta" which is the only allowable thing for data track aside from beta.X
                                 if prerelease_data_regex.match(prerel) is None:
-                                    raise ValueError('library ({}) version ({}) in version file ({}) is not a correct version to release. The accepted prerelease tags for data track are (beta) or (beta.X) and the current prerelease tag is ({})'.format(library_to_update, module.current, version_file, prerel))
+                                    # raise ValueError('library ({}) version ({}) in version file ({}) is not a correct version to release. The accepted prerelease tags for data track are (beta) or (beta.X) and the current prerelease tag is ({})'.format(library_to_update, module.current, version_file, prerel))
                                 # at this point the version is <major>.<minor>.<patch>-beta
                                 temp_ver = '{}-{}'.format(temp_ver, str(prerel))
                         else:
@@ -388,7 +388,7 @@ def verify_current_version_of_artifact(build_type, artifact_id, group_id):
                     # last but not least, for sanity verify that the version constructed from the
                     # semver pieces matches module's current version
                     if module.current != temp_ver:
-                        raise ValueError('library ({}) version ({}) in version file ({}) does not match the version constructed from the semver pieces ({})'.format(library_to_update, module.current, version_file, temp_ver))
+                        # raise ValueError('library ({}) version ({}) in version file ({}) does not match the version constructed from the semver pieces ({})'.format(library_to_update, module.current, version_file, temp_ver))
 
                     print('The version {} for {} looks good!'.format(module.current, module.name))
 
