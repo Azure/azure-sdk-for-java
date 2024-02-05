443fe1ac47454ae5b05719125661f3c3047ef250
Sanjana Kapur <sanjanakapur@microsoft.com>
2024-02-04 20:20:18 -0800

    Remove setPoolInfo method from BatchJob

--- sdk/batch/azure-compute-batch/src/main/java/com/azure/compute/batch/models/BatchJob.java
+++ sdk/batch/azure-compute-batch/src/main/java/com/azure/compute/batch/models/BatchJob.java
@@ -508,17 +508,6 @@ public final class BatchJob {
     }
 
     /**
-     * Set the poolInfo property: Specifies how a Job should be assigned to a Pool.
-     *
-     * @param poolInfo the poolInfo value to set.
-     * @return the BatchJob object itself.
-     */
-    public BatchJob setPoolInfo(BatchPoolInfo poolInfo) {
-        this.poolInfo = poolInfo;
-        return this;
-    }
-
-    /**
      * Get the onAllTasksComplete property: The action the Batch service should take when all Tasks in the Job are in
      * the completed state. The default is noaction.
      *
