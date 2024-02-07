9505f517a0b271fad5d0fb42a7c553b9d94c2835
Victor Colin Amador <vicolina@microsoft.com>
2024-02-06 22:41:38 -0800

    Added a new Response type.

---
+++ sdk/generic-sdk-core/generic-core/src/main/java/com/generic/core/http/models/NewResponse.java
@@ -0,0 +1,81 @@
+package com.generic.core.http.models;
+
+import com.generic.core.models.BinaryData;
+import com.generic.core.models.Headers;
+
+import java.io.Closeable;
+
+public abstract class NewResponse<T> implements Closeable {
+    protected final T body;
+    protected final HttpRequest request;
+    protected final int statusCode;
+    protected BinaryData bodyBinaryData = null;
+
+    /**
+     * Creates a {@link NewResponse}.
+     *
+     * @param request The request which resulted in this response.
+     * @param statusCode The status code of the HTTP response.
+     * @param body The deserialized value of the HTTP response.
+     */
+    protected NewResponse(HttpRequest request, int statusCode, T body) {
+        this.request = request;
+        this.statusCode = statusCode;
+        this.body = body;
+    }
+
+    /**
+     * Gets the HTTP response status code.
+     *
+     * @return The status code of the HTTP response.
+     */
+    public abstract int getStatusCode();
+
+    /**
+     * Gets the headers from the HTTP response.
+     *
+     * @return The HTTP response headers.
+     */
+    public Headers getHeaders() {
+        return this.request.getHeaders();
+    };
+
+    /**
+     * Gets the HTTP request which resulted in this response.
+     *
+     * @return The HTTP request.
+     */
+    public HttpRequest getRequest() {
+        return this.request;
+    }
+
+    /**
+     * Gets the deserialized value of the HTTP response.
+     *
+     * @return The deserialized value of the HTTP response.
+     */
+    public abstract T getBody();
+
+    /**
+     * Gets the {@link BinaryData} that represents the body of the response.
+     *
+     * <p>Subclasses should override this method.</p>
+     *
+     * @return The {@link BinaryData} response body.
+     */
+    public BinaryData getBodyAsBinaryData() {
+        // We shouldn't create multiple binary data instances for a single stream.
+        if (bodyBinaryData == null && body != null) {
+            bodyBinaryData = BinaryData.fromObject(body);
+        }
+
+        return bodyBinaryData;
+    }
+
+    /**
+     * Closes the response content stream, if any.
+     */
+    @Override
+    public void close() {
+    }
+}
