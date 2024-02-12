11171d6b3ecaab178fad66b3e97bdf5a6d2b8823
Abhijeet Mohanty <abhmohanty@microsoft.com>
2024-02-12 17:02:13 -0500

    Adding new APIs to ChangeFeedProcessorContext.java.

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/ChangeFeedProcessorContext.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/ChangeFeedProcessorContext.java
@@ -26,12 +26,20 @@ public interface ChangeFeedProcessorContext {
     @Beta(value = Beta.SinceVersion.V4_51_0, warningText = Beta.PREVIEW_SUBJECT_TO_CHANGE_WARNING)
     String getLeaseToken();
 
-    @Beta()
+    /**
+     * Gets the request charge as request units (RU) consumed in obtaining the change feed batch.
+     * <br/>
+     * For more information about the RU and factors that can impact the effective charges please visit
+     * <a href="https://docs.microsoft.com/en-us/azure/cosmos-db/request-units">Request Units in Azure Cosmos DB</a>
+     *
+     * @return the request charge.
+     */
+    @Beta(value = Beta.SinceVersion.V4_56_0, warningText = Beta.PREVIEW_SUBJECT_TO_CHANGE_WARNING)
     double getRequestCharge();
 
-    @Beta()
+    @Beta(value = Beta.SinceVersion.V4_56_0, warningText = Beta.PREVIEW_SUBJECT_TO_CHANGE_WARNING)
     String getSessionToken();
 
-    @Beta()
+    @Beta(value = Beta.SinceVersion.V4_56_0, warningText = Beta.PREVIEW_SUBJECT_TO_CHANGE_WARNING)
     CosmosDiagnostics getDiagnostics();
 }
