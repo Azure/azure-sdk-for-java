89e55e40e7d3bf09fc1bdfbf31cb2e8279aeb807
heyams <heya@microsoft.com>
2024-02-16 16:40:46 -0800

    Keep upstream copyright

--- sdk/monitor/azure-monitor-opentelemetry-exporter/src/main/java/com/azure/monitor/opentelemetry/exporter/implementation/AttributeKeyTemplate.java
+++ sdk/monitor/azure-monitor-opentelemetry-exporter/src/main/java/com/azure/monitor/opentelemetry/exporter/implementation/AttributeKeyTemplate.java
@@ -1,6 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package com.azure.monitor.opentelemetry.exporter.implementation;
 
 import io.opentelemetry.api.common.AttributeKey;
