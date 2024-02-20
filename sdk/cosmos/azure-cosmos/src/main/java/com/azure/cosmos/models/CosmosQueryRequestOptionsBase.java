343939f69d408189d87e20f401fbef7cf9d67700
Fabian Meiswinkel <fabianm@microsoft.com>
2024-02-21 00:29:00 +0100

    azure-cosmos Release 2024-02-20 API review fixes (#38844)
    * Added Api to allow Spark apps to retrieve CosmosAsyncClinet used by the spark connector
    
    * Update CosmosAsyncClientCache.scala
    
    * Added changelog entries
    
    * azure cosmos release 2024-02-20
    
    * Included Spark 3.4 into release
    
    * azure-cosmos release 2024-02-20 - API review fixes

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/models/CosmosQueryRequestOptionsBase.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/models/CosmosQueryRequestOptionsBase.java
@@ -88,14 +88,13 @@ public abstract class CosmosQueryRequestOptionsBase<T extends CosmosQueryRequest
      *
      * @return the consistency level.
      */
-
     public ConsistencyLevel getConsistencyLevel() {
         return consistencyLevel;
     }
 
     /**
      * Sets the consistency level required for the request. The effective consistency level
-     * can only be reduce for read/query requests. So when the Account's default consistency level
+     * can only be reduced for read/query requests. So when the Account's default consistency level
      * is for example Session you can specify on a request-by-request level for individual requests
      * that Eventual consistency is sufficient - which could reduce the latency and RU charges for this
      * request but will not guarantee session consistency (read-your-own-write) anymore
