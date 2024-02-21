bda57368ccae3bcaef45d5d92a35dedbd8f1246f
Alan Zimmer <48699787+alzimmermsft@users.noreply.github.com>
2024-02-21 12:17:44 -0500

    Only serialize as JsonSerializable if type declares toJson and fromJson

--- sdk/core/azure-core/src/main/java/com/azure/core/implementation/jackson/JsonSerializableDeserializer.java
+++ sdk/core/azure-core/src/main/java/com/azure/core/implementation/jackson/JsonSerializableDeserializer.java
@@ -3,8 +3,9 @@
 
 package com.azure.core.implementation.jackson;
 
-import com.azure.core.implementation.ReflectiveInvoker;
+import com.azure.core.implementation.ReflectionSerializable;
 import com.azure.core.implementation.ReflectionUtils;
+import com.azure.core.implementation.ReflectiveInvoker;
 import com.azure.core.util.logging.ClientLogger;
 import com.azure.json.JsonReader;
 import com.azure.json.JsonSerializable;
@@ -13,28 +14,40 @@ import com.fasterxml.jackson.databind.BeanDescription;
 import com.fasterxml.jackson.databind.DeserializationConfig;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.deser.BeanDeserializerModifier;
+import com.fasterxml.jackson.databind.deser.ResolvableDeserializer;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
 import java.io.IOException;
 
-final class JsonSerializableDeserializer extends JsonDeserializer<JsonSerializable<?>> {
+final class JsonSerializableDeserializer extends StdDeserializer<Object> implements ResolvableDeserializer {
+    private static final long serialVersionUID = -3097250410847767845L;
+
     private static final ClientLogger LOGGER = new ClientLogger(JsonSerializableDeserializer.class);
 
-    private static final Module MODULE = new SimpleModule().setDeserializerModifier(new BeanDeserializerModifier() {
-        @SuppressWarnings("unchecked")
-        @Override
-        public JsonDeserializer<?> modifyDeserializer(DeserializationConfig config, BeanDescription beanDesc,
-            JsonDeserializer<?> deserializer) {
-            return (JsonSerializable.class.isAssignableFrom(beanDesc.getBeanClass()))
-                ? new JsonSerializableDeserializer((Class<? extends JsonSerializable<?>>) beanDesc.getBeanClass())
-                : deserializer;
-        }
-    });
+    /**
+     * The default mapperAdapter for the current type.
+     */
+    private final JsonDeserializer<?> defaultDeserializer;
 
     private final Class<? extends JsonSerializable<?>> jsonSerializableType;
     private final ReflectiveInvoker readJson;
+
+    @SuppressWarnings("unchecked")
+    private JsonSerializableDeserializer(Class<?> t, JsonDeserializer<?> defaultDeserializer) {
+        super(t);
+        this.defaultDeserializer = defaultDeserializer;
+
+        this.jsonSerializableType = (Class<? extends JsonSerializable<?>>) t;
+        try {
+            this.readJson = ReflectionUtils.getMethodInvoker(t, t.getDeclaredMethod("fromJson", JsonReader.class));
+        } catch (Exception e) {
+            throw LOGGER.logExceptionAsError(new IllegalStateException(e));
+        }
+    }
 
     /**
      * Gets a module wrapping this deserializer as an adapter for the Jackson ObjectMapper.
@@ -42,22 +55,15 @@ final class JsonSerializableDeserializer extends JsonDeserializer<JsonSerializab
      * @return A module to be plugged into Jackson ObjectMapper.
      */
     public static Module getModule() {
-        return MODULE;
-    }
-
-    /**
-     * Creates an instance of {@link JsonSerializableDeserializer}.
-     *
-     * @param jsonSerializableType The type implementing {@link JsonSerializable} being deserialized.
-     */
-    JsonSerializableDeserializer(Class<? extends JsonSerializable<?>> jsonSerializableType) {
-        this.jsonSerializableType = jsonSerializableType;
-        try {
-            this.readJson = ReflectionUtils.getMethodInvoker(jsonSerializableType,
-                jsonSerializableType.getDeclaredMethod("fromJson", JsonReader.class));
-        } catch (Exception e) {
-            throw LOGGER.logExceptionAsError(new IllegalStateException(e));
-        }
+        return new SimpleModule().setDeserializerModifier(new BeanDeserializerModifier() {
+            @Override
+            public JsonDeserializer<?> modifyDeserializer(DeserializationConfig config, BeanDescription beanDesc,
+                JsonDeserializer<?> serializer) {
+                return ReflectionSerializable.supportsJsonSerializable(beanDesc.getBeanClass())
+                    ? new JsonSerializableDeserializer(beanDesc.getBeanClass(), serializer)
+                    : serializer;
+            }
+        });
     }
 
     @Override
@@ -68,5 +74,10 @@ final class JsonSerializableDeserializer extends JsonDeserializer<JsonSerializab
             IOException ioException = (e instanceof IOException) ? (IOException) e : new IOException(e);
             throw LOGGER.logThrowableAsError(ioException);
         }
+    }
+
+    @Override
+    public void resolve(DeserializationContext ctxt) throws JsonMappingException {
+        ((ResolvableDeserializer) defaultDeserializer).resolve(ctxt);
     }
 }
--- sdk/core/azure-core/src/main/java/com/azure/core/implementation/jackson/JsonSerializableSerializer.java
+++ sdk/core/azure-core/src/main/java/com/azure/core/implementation/jackson/JsonSerializableSerializer.java
@@ -3,19 +3,35 @@
 
 package com.azure.core.implementation.jackson;
 
+import com.azure.core.implementation.ReflectionSerializable;
 import com.azure.json.JsonSerializable;
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.BeanDescription;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.SerializationConfig;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.jsontype.TypeSerializer;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.BeanSerializerModifier;
+import com.fasterxml.jackson.databind.ser.ResolvableSerializer;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 
 import java.io.IOException;
 
-@SuppressWarnings("rawtypes")
-final class JsonSerializableSerializer extends JsonSerializer<JsonSerializable> {
-    private static final Module MODULE
-        = new SimpleModule().addSerializer(JsonSerializable.class, new JsonSerializableSerializer());
+final class JsonSerializableSerializer extends StdSerializer<Object> implements ResolvableSerializer {
+    private static final long serialVersionUID = 6099245008569550678L;
+
+    /**
+     * The default mapperAdapter for the current type.
+     */
+    private final JsonSerializer<?> defaultSerializer;
+
+    private JsonSerializableSerializer(Class<?> t, JsonSerializer<?> defaultSerializer) {
+        super(t, false);
+        this.defaultSerializer = defaultSerializer;
+    }
 
     /**
      * Gets a module wrapping this serializer as an adapter for the Jackson ObjectMapper.
@@ -23,19 +39,32 @@ final class JsonSerializableSerializer extends JsonSerializer<JsonSerializable> 
      * @return A module to be plugged into Jackson ObjectMapper.
      */
     public static Module getModule() {
-        return MODULE;
+        return new SimpleModule().setSerializerModifier(new BeanSerializerModifier() {
+            @Override
+            public JsonSerializer<?> modifySerializer(SerializationConfig config, BeanDescription beanDesc,
+                JsonSerializer<?> serializer) {
+                return ReflectionSerializable.supportsJsonSerializable(beanDesc.getBeanClass())
+                    ? new JsonSerializableSerializer(beanDesc.getBeanClass(), serializer)
+                    : serializer;
+            }
+        });
     }
 
     @Override
-    public void serialize(JsonSerializable value, JsonGenerator gen, SerializerProvider serializers)
-        throws IOException {
+    public void serialize(Object value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
         // Do not use this JacksonJsonWriter in a try-with-resources block as closing it closes the underlying
         // JsonGenerator which could cause problems in further serialization done.
-        new JacksonJsonWriter(gen).writeJson(value);
+        new JacksonJsonWriter(gen).writeJson((JsonSerializable<?>) value);
     }
 
     @Override
-    public Class<JsonSerializable> handledType() {
-        return JsonSerializable.class;
+    public void resolve(SerializerProvider provider) throws JsonMappingException {
+        ((ResolvableSerializer) defaultSerializer).resolve(provider);
+    }
+
+    @Override
+    public void serializeWithType(Object value, JsonGenerator gen, SerializerProvider serializers,
+        TypeSerializer typeSer) throws IOException {
+        serialize(value, gen, serializers);
     }
 }
