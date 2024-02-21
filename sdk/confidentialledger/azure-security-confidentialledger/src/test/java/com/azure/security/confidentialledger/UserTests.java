36194baf32c824f8f62dad7e313b4389726095c5
azure-sdk <azuresdk@microsoft.com>
2024-02-20 21:40:50 -0800

    Reset sources for azure-security-confidentialledger to the release version 1.0.16.

--- sdk/confidentialledger/azure-security-confidentialledger/src/test/java/com/azure/security/confidentialledger/UserTests.java
+++ sdk/confidentialledger/azure-security-confidentialledger/src/test/java/com/azure/security/confidentialledger/UserTests.java
@@ -7,17 +7,19 @@ package com.azure.security.confidentialledger;
 import com.azure.core.http.rest.RequestOptions;
 import com.azure.core.http.rest.Response;
 import com.azure.core.util.BinaryData;
+import com.azure.core.util.Configuration;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public final class UserTests extends ConfidentialLedgerClientTestBase {
     @Test
-    public void testGetUserTests() {
-        String userAad = ConfidentialLedgerEnvironment.getConfidentialLedgerAdminOid();
+    public void testGetUserTests() throws Exception {
+        String userAad = Configuration.getGlobalConfiguration().get("USER_AAD", "d958292f-5b70-4b66-9502-562217cc7eaa");
         RequestOptions requestOptions = new RequestOptions();
 
         Response<BinaryData> response = confidentialLedgerClient.getUserWithResponse(userAad, requestOptions);
@@ -37,5 +39,6 @@ public final class UserTests extends ConfidentialLedgerClientTestBase {
         }
 
         Assertions.assertEquals(responseBodyJson.get("assignedRole").asText(), "Administrator");
+        Assertions.assertEquals(responseBodyJson.get("userId").asText(), userAad);
     }
 }
