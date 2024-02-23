189a4d07892cc45cc394d69bf1193e17491f5c9c
XiaofeiCao <xiaofeicao@microsoft.com>
2024-02-23 17:05:36 +0800

    test case

---
+++ sdk/resourcemanager/azure-resourcemanager/src/test/java/com/azure/resourcemanager/StorageAccountCmkTests.java
@@ -0,0 +1,153 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.resourcemanager;
+
+import com.azure.core.credential.TokenCredential;
+import com.azure.core.http.HttpClient;
+import com.azure.core.http.HttpPipeline;
+import com.azure.core.http.policy.HttpLogOptions;
+import com.azure.core.http.policy.HttpPipelinePolicy;
+import com.azure.core.http.policy.RetryPolicy;
+import com.azure.core.management.Region;
+import com.azure.core.management.profile.AzureProfile;
+import com.azure.core.test.annotation.DoNotRecord;
+import com.azure.resourcemanager.keyvault.models.Key;
+import com.azure.resourcemanager.keyvault.models.KeyPermissions;
+import com.azure.resourcemanager.keyvault.models.Vault;
+import com.azure.resourcemanager.msi.models.Identity;
+import com.azure.resourcemanager.resources.fluentcore.utils.HttpPipelineProvider;
+import com.azure.resourcemanager.resources.fluentcore.utils.ResourceManagerUtils;
+import com.azure.resourcemanager.storage.models.StorageAccount;
+import com.azure.resourcemanager.storage.models.StorageAccountSkuType;
+import com.azure.resourcemanager.test.ResourceManagerTestProxyTestBase;
+import com.azure.resourcemanager.test.utils.TestDelayProvider;
+import com.azure.resourcemanager.test.utils.TestIdentifierProvider;
+import com.azure.security.keyvault.keys.models.KeyType;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+public class StorageAccountCmkTests extends ResourceManagerTestProxyTestBase {
+    private AzureResourceManager azureResourceManager;
+
+    @Override
+    protected HttpPipeline buildHttpPipeline(TokenCredential credential,
+                                             AzureProfile profile,
+                                             HttpLogOptions httpLogOptions,
+                                             List<HttpPipelinePolicy> policies,
+                                             HttpClient httpClient) {
+        return HttpPipelineProvider.buildHttpPipeline(
+            credential,
+            profile,
+            null,
+            httpLogOptions,
+            null,
+            new RetryPolicy("Retry-After", ChronoUnit.SECONDS),
+            policies,
+            httpClient);
+    }
+
+    @Override
+    protected void initializeClients(HttpPipeline httpPipeline, AzureProfile profile) {
+        ResourceManagerUtils.InternalRuntimeContext.setDelayProvider(new TestDelayProvider(!isPlaybackMode()));
+        ResourceManagerUtils.InternalRuntimeContext internalContext = new ResourceManagerUtils.InternalRuntimeContext();
+        internalContext.setIdentifierFunction(name -> new TestIdentifierProvider(testResourceNamer));
+        azureResourceManager = buildManager(AzureResourceManager.class, httpPipeline, profile);
+        setInternalContext(internalContext, azureResourceManager);
+    }
+
+    @Override
+    protected void cleanUpResources() {}
+
+    @Test
+    // involves key vault key
+    @DoNotRecord(skipInPlayback = true)
+    public void testCmkAndMmk() {
+        String vaultName = generateRandomResourceName("vt", 15);
+        String msiName1 = generateRandomResourceName("msi", 15);
+        String msiName2 = generateRandomResourceName("msi", 15);
+        String saName = generateRandomResourceName("sa", 15);
+        String rgName = generateRandomResourceName("jvmgmt", 15);
+        Region region = Region.US_EAST;
+        try {
+            // create key vault with access policy
+            Vault vault = azureResourceManager.vaults()
+                .define(vaultName)
+                .withRegion(region)
+                .withNewResourceGroup(rgName)
+                .withEmptyAccessPolicy()
+                .create();
+            // create two MSIs
+            Identity identity1 = azureResourceManager.identities()
+                .define(msiName1)
+                .withRegion(region)
+                .withExistingResourceGroup(rgName)
+                .create();
+            Identity identity2 = azureResourceManager.identities()
+                .define(msiName2)
+                .withRegion(region)
+                .withExistingResourceGroup(rgName)
+                .create();
+            // create key vault key
+            Key key = vault.keys().define("key1")
+                .withKeyTypeToCreate(KeyType.RSA)
+                .withKeySize(4096)
+                .create();
+
+            // create Storage Account with default MMK
+            StorageAccount storageAccount = azureResourceManager.storageAccounts()
+                .define(saName)
+                .withRegion(region)
+                .withExistingResourceGroup(rgName)
+                .withGeneralPurposeAccountKindV2()
+                .withSku(StorageAccountSkuType.STANDARD_RAGRS)
+                .withSystemAssignedManagedServiceIdentity()
+                .create();
+
+            // assign access policy to the three MSIs
+            vault.update()
+                .defineAccessPolicy()   // access policy for this sample client to generate key
+                    .forServicePrincipal(clientIdFromFile())
+                    .allowKeyAllPermissions()
+                    .attach()
+                .defineAccessPolicy()
+                    .forObjectId(storageAccount.systemAssignedManagedServiceIdentityPrincipalId())
+                    .allowKeyPermissions(KeyPermissions.GET, KeyPermissions.WRAP_KEY, KeyPermissions.UNWRAP_KEY)
+                    .attach()
+                .defineAccessPolicy()
+                    .forObjectId(identity1.id())
+                    .allowKeyPermissions(KeyPermissions.GET, KeyPermissions.WRAP_KEY, KeyPermissions.UNWRAP_KEY)
+                    .attach()
+                .defineAccessPolicy()
+                    .forObjectId(identity2.id())
+                    .allowKeyPermissions(KeyPermissions.GET, KeyPermissions.WRAP_KEY, KeyPermissions.UNWRAP_KEY)
+                    .attach()
+                .apply();
+            // update Storage Account to CMK with system-assigned MSI
+            storageAccount.update()
+                .withEncryptionKeyFromKeyVault(vault.vaultUri(), key.name(), "")
+                .apply();
+            // update Storage Account to CMK with user-assigned MSI
+            storageAccount.update()
+                .withExistingUserAssignedManagedServiceIdentity(identity1.id())
+                .withEncryptionKeyFromKeyVault(vault.vaultUri(), key.name(), "", identity1.id())
+                .apply();
+            // update Storage Account to CMK with another user-assigned MSI
+            storageAccount.update()
+                .withoutUserAssignedManagedServiceIdentity(identity1.id())
+                .withExistingUserAssignedManagedServiceIdentity(identity2.id())
+                .withEncryptionKeyFromKeyVault(vault.vaultUri(), key.name(), "", identity2.id())
+                .apply();
+            // update Storage Account to CMK with system-assigned MSI
+            storageAccount.update()
+                .withEncryptionKeyFromKeyVault(vault.vaultUri(), key.name(), "")
+                .apply();
+
+        } finally {
+            azureResourceManager.resourceGroups().deleteByName(rgName);
+        }
+    }
+}
