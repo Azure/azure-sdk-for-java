6bdd4ec0efdc119558b277dcf7262c6b0222e253
Arif Ibrahim Mohamed <arifibrahim4@gmail.com>
2024-02-27 12:30:03 -0500

    Checkstype fix for sample codes (#38965)

--- sdk/communication/azure-communication-messages/src/samples/java/com/azure/communication/messages/NotificationMessageSample.java
+++ sdk/communication/azure-communication-messages/src/samples/java/com/azure/communication/messages/NotificationMessageSample.java
@@ -1,6 +1,20 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.azure.communication.messages;
 
-import com.azure.communication.messages.models.*;
+import com.azure.communication.messages.models.MessageTemplate;
+import com.azure.communication.messages.models.MessageTemplateBindings;
+import com.azure.communication.messages.models.MessageTemplateDocument;
+import com.azure.communication.messages.models.MessageTemplateImage;
+import com.azure.communication.messages.models.MessageTemplateQuickAction;
+import com.azure.communication.messages.models.MessageTemplateText;
+import com.azure.communication.messages.models.MessageTemplateValue;
+import com.azure.communication.messages.models.MessageTemplateVideo;
+import com.azure.communication.messages.models.MediaNotificationContent;
+import com.azure.communication.messages.models.TextNotificationContent;
+import com.azure.communication.messages.models.TemplateNotificationContent;
+import com.azure.communication.messages.models.SendMessageResult;
 import com.azure.communication.messages.models.channels.WhatsAppMessageButtonSubType;
 import com.azure.communication.messages.models.channels.WhatsAppMessageTemplateBindings;
 import com.azure.communication.messages.models.channels.WhatsAppMessageTemplateBindingsButton;
@@ -17,10 +31,10 @@ public class NotificationMessageSample {
     private static final String CONNECTION_STRING = System.getenv("ACS_CONNECTION_STRING");
     private static final String CHANNEL_ID = "77ffd898-ec44-42cd-b560-57a8903d05c7";
     private static final String RECIPIENT_IDENTIFIER = System.getenv("RECIPIENT_IDENTIFIER");
-    private static final List<String> recipients = new ArrayList<>();
+    private static final List<String> TO_LIST = new ArrayList<>();
 
     public static void main(String[] args) {
-        recipients.add(RECIPIENT_IDENTIFIER);
+        TO_LIST.add(RECIPIENT_IDENTIFIER);
         sendTemplateMessageWithDocument();
     }
 
@@ -51,15 +65,15 @@ public class NotificationMessageSample {
         //Update template parameter binding
         List<WhatsAppMessageTemplateBindingsComponent> components = new ArrayList<>();
         components.add(new WhatsAppMessageTemplateBindingsComponent("Days"));
-        MessageTemplateBindings bindings =new WhatsAppMessageTemplateBindings()
+        MessageTemplateBindings bindings = new WhatsAppMessageTemplateBindings()
             .setBody(components);
         template.setBindings(bindings);
 
         NotificationMessagesClient client = createClientWithTokenCredential();
         SendMessageResult result = client.send(
-            new TemplateNotificationContent(CHANNEL_ID, recipients, template));
+            new TemplateNotificationContent(CHANNEL_ID, TO_LIST, template));
 
-        result.getReceipts().forEach(r -> System.out.println("Message sent to:"+r.getTo() + " and message id:"+ r.getMessageId()));
+        result.getReceipts().forEach(r -> System.out.println("Message sent to:" + r.getTo() + " and message id:" + r.getMessageId()));
     }
 
     /*
@@ -103,8 +117,8 @@ public class NotificationMessageSample {
 
         // Add parameter binding for template buttons in a list
         List<WhatsAppMessageTemplateBindingsButton> templateButtonBindings = new ArrayList<>();
-        templateButtonBindings.add( new WhatsAppMessageTemplateBindingsButton(WhatsAppMessageButtonSubType.QUICK_REPLY, "Yes"));
-        templateButtonBindings.add( new WhatsAppMessageTemplateBindingsButton(WhatsAppMessageButtonSubType.QUICK_REPLY, "No"));
+        templateButtonBindings.add(new WhatsAppMessageTemplateBindingsButton(WhatsAppMessageButtonSubType.QUICK_REPLY, "Yes"));
+        templateButtonBindings.add(new WhatsAppMessageTemplateBindingsButton(WhatsAppMessageButtonSubType.QUICK_REPLY, "No"));
 
         MessageTemplateBindings templateBindings = new WhatsAppMessageTemplateBindings()
             .setBody(templateBodyBindings) // Set the parameter binding for template body
@@ -116,9 +130,9 @@ public class NotificationMessageSample {
 
         NotificationMessagesClient client = createClientWithConnectionString();
         SendMessageResult result = client.send(
-            new TemplateNotificationContent(CHANNEL_ID, recipients, messageTemplate));
+            new TemplateNotificationContent(CHANNEL_ID, TO_LIST, messageTemplate));
 
-        result.getReceipts().forEach(r -> System.out.println("Message sent to:"+r.getTo() + " and message id:"+ r.getMessageId()));
+        result.getReceipts().forEach(r -> System.out.println("Message sent to:" + r.getTo() + " and message id:" + r.getMessageId()));
     }
 
     /*
@@ -177,9 +191,9 @@ public class NotificationMessageSample {
 
         NotificationMessagesClient client = createClientWithConnectionString();
         SendMessageResult result = client.send(
-            new TemplateNotificationContent(CHANNEL_ID, recipients, template));
+            new TemplateNotificationContent(CHANNEL_ID, TO_LIST, template));
 
-        result.getReceipts().forEach(r -> System.out.println("Message sent to:"+r.getTo() + " and message id:"+ r.getMessageId()));
+        result.getReceipts().forEach(r -> System.out.println("Message sent to:" + r.getTo() + " and message id:" + r.getMessageId()));
     }
 
     /*
@@ -230,9 +244,9 @@ public class NotificationMessageSample {
 
         NotificationMessagesClient client = createClientWithConnectionString();
         SendMessageResult result = client.send(
-            new TemplateNotificationContent(CHANNEL_ID, recipients, template));
+            new TemplateNotificationContent(CHANNEL_ID, TO_LIST, template));
 
-        result.getReceipts().forEach(r -> System.out.println("Message sent to:"+r.getTo() + " and message id:"+ r.getMessageId()));
+        result.getReceipts().forEach(r -> System.out.println("Message sent to:" + r.getTo() + " and message id:" + r.getMessageId()));
     }
 
     /*
@@ -285,9 +299,9 @@ public class NotificationMessageSample {
 
         NotificationMessagesClient client = createClientWithConnectionString();
         SendMessageResult result = client.send(
-            new TemplateNotificationContent(CHANNEL_ID, recipients, template));
+            new TemplateNotificationContent(CHANNEL_ID, TO_LIST, template));
 
-        result.getReceipts().forEach(r -> System.out.println("Message sent to:"+r.getTo() + " and message id:"+ r.getMessageId()));
+        result.getReceipts().forEach(r -> System.out.println("Message sent to:" + r.getTo() + " and message id:" + r.getMessageId()));
     }
 
     /*
@@ -297,9 +311,9 @@ public class NotificationMessageSample {
     private static void sendTextMessage() {
         NotificationMessagesClient client = createClientWithAzureKeyCredential();
         SendMessageResult result = client.send(
-            new TextNotificationContent(CHANNEL_ID, recipients, "Hello from ACS messaging"));
+            new TextNotificationContent(CHANNEL_ID, TO_LIST, "Hello from ACS messaging"));
 
-        result.getReceipts().forEach(r -> System.out.println("Message sent to:"+r.getTo() + " and message id:"+ r.getMessageId()));
+        result.getReceipts().forEach(r -> System.out.println("Message sent to:" + r.getTo() + " and message id:"+ r.getMessageId()));
     }
 
     /*
@@ -312,9 +326,9 @@ public class NotificationMessageSample {
 
         NotificationMessagesClient client = createClientWithConnectionString();
         SendMessageResult result = client.send(
-            new MediaNotificationContent(CHANNEL_ID, recipients, mediaUrl));
+            new MediaNotificationContent(CHANNEL_ID, TO_LIST, mediaUrl));
 
-        result.getReceipts().forEach(r -> System.out.println("Message sent to:"+r.getTo() + " and message id:"+ r.getMessageId()));
+        result.getReceipts().forEach(r -> System.out.println("Message sent to:" + r.getTo() + " and message id:" + r.getMessageId()));
     }
 
     private static NotificationMessagesClient createClientWithConnectionString() {
