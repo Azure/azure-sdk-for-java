83b5d0af45cddfba20b4c1df59a0e93626c9f961
Fabian Meiswinkel <fabianm@microsoft.com>
2024-02-28 18:12:12 +0000

    Update CosmosItemSerializer.java

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/CosmosItemSerializer.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/CosmosItemSerializer.java
@@ -11,9 +11,53 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.util.Map;
 
+/**
+ * The {@link CosmosItemSerializer} allows customizing the serialization of Cosmos Items - either to transform payload (for
+ * example wrap/unwrap in custom envelopes) or use custom serialization settings or json serializer stacks.
+ */
 public abstract class CosmosItemSerializer {
     private final static ObjectMapper objectMapper = Utils.getSimpleObjectMapper();
-    public final static CosmosItemSerializer DEFAULT_SERIALIZER = new CosmosItemSerializer() {
+
+    /**
+     * Gets the default Cosmos item serializer. This serializer is used by default when no custom serializer is
+     * specified on request options or the {@link CosmosClientBuilder}
+     */
+    public final static CosmosItemSerializer DEFAULT_SERIALIZER = new DefaultCosmosItemSerializer();
+
+    /**
+     * Used to instantiate subclasses
+     */
+    protected CosmosItemSerializer() {
+    }
+
+    /**
+     * Used to serialize a POJO into a json tree
+     * @param item the POJO to be serialized
+     * @return the json tree that will be used as payload in Cosmos DB items
+     * @param <T> The type of the POJO
+     */
+    public abstract <T> Map<String, Object> serialize(T item);
+
+    /**
+     * Used to deserialize the json tree stored in the Cosmos DB item as a POJO
+     * @param jsonNodeMap the json tree from the Cosmos DB item
+     * @param classType The type of the POJO
+     * @return The deserialized POJO
+     * @param <T> The type of the POJO
+     */
+    public abstract  <T> T deserialize(Map<String, Object> jsonNodeMap, Class<T> classType);
+
+    private static class DefaultCosmosItemSerializer extends CosmosItemSerializer {
+        DefaultCosmosItemSerializer() {
+            super();
+        }
+
+        /**
+         * Used to serialize a POJO into a json tree
+         * @param item the POJO to be serialized
+         * @return the json tree that will be used as payload in Cosmos DB items
+         * @param <T> The type of the POJO
+         */
         @Override
         @SuppressWarnings("unchecked")
         public <T> Map<String, Object> serialize(T item) {
@@ -33,6 +77,13 @@ public abstract class CosmosItemSerializer {
             return new JsonNodeMap(jsonNode);
         }
 
+        /**
+         * Used to deserialize the json tree stored in the Cosmos DB item as a POJO
+         * @param jsonNodeMap the json tree from the Cosmos DB item
+         * @param classType The type of the POJO
+         * @return The deserialized POJO
+         * @param <T> The type of the POJO
+         */
         @Override
         public <T> T deserialize(Map<String, Object> jsonNodeMap, Class<T> classType) {
             if (jsonNodeMap == null) {
@@ -52,12 +103,5 @@ public abstract class CosmosItemSerializer {
                 throw new IllegalStateException(String.format("Unable to parse JSON %s as %s", jsonNode, classType.getName()), e);
             }
         }
-    };
-
-    protected CosmosItemSerializer() {
     }
-
-    public abstract <T> Map<String, Object> serialize(T item);
-
-    public abstract  <T> T deserialize(Map<String, Object> jsonNode, Class<T> clazz);
 }
