9c1ecf26799696faa39a1cc0ad8e4eb427bd8ab0
Ben Broderick Phillips <bebroder@microsoft.com>
2024-03-13 20:03:07 -0400

    fix addtl modules json

--- eng/pipelines/scripts/generate-project-list.ps1
+++ eng/pipelines/scripts/generate-project-list.ps1
@@ -1,19 +1,22 @@
-if (!$env:ARTIFACTSJSON) {
+# Check if empty or still a literal devops variable reference $(<var name>)
+if (!$env:ARTIFACTSJSON -or $env:ARTIFACTSJSON -like '*ArtifactsJson*') {
   throw "ArtifactsJson devops variable was not set"
 }
-if (!$env:ADDITIONALMODULESJSON) {
-  throw "AdditionalModulesJson devops variable was not set"
-}
 $artifacts = $env:ARTIFACTSJSON | ConvertFrom-Json
-$additionalModules = $env:ADDITIONALMODULESJSON | ConvertFrom-Json
 
 $projectList = @()
 foreach ($artifact in $artifacts) {
   $projectList += "$($artifact.groupId):$($artifact.name)"
 }
-foreach ($artifact in $additionalModules) {
-  $projectList += "$($artifact.groupId):$($artifact.name)"
+
+# Check if empty or still a literal devops variable reference $(<var name>)
+if (!$env:ADDITIONALMODULESJSON -or $env:ADDITIONALMODULESJSON -like '*AdditionalModulesJson*') {
+  $additionalModules = $env:ADDITIONALMODULESJSON | ConvertFrom-Json
+  foreach ($artifact in $additionalModules) {
+    $projectList += "$($artifact.groupId):$($artifact.name)"
+  }
 }
+
 $projects = $projectList -join ','
 
 Write-Host "ProjectList = $projects"
