049b38f86f91ec599fb6715645a8a78cde306379
azure-sdk <azuresdk@microsoft.com>
2024-03-20 10:58:08 -0700

    Reset sources for azure-communication-chat to the release version 1.4.1.

--- sdk/communication/azure-communication-chat/src/main/java/com/azure/communication/chat/models/ChatMessageType.java
+++ sdk/communication/azure-communication-chat/src/main/java/com/azure/communication/chat/models/ChatMessageType.java
@@ -8,33 +8,21 @@ import com.azure.core.util.ExpandableStringEnum;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import java.util.Collection;
 
-/**
- * Defines values for ChatMessageType.
- */
+/** Defines values for ChatMessageType. */
 public final class ChatMessageType extends ExpandableStringEnum<ChatMessageType> {
-    /**
-     * Static value text for ChatMessageType.
-     */
+    /** Static value text for ChatMessageType. */
     public static final ChatMessageType TEXT = fromString("text");
 
-    /**
-     * Static value html for ChatMessageType.
-     */
+    /** Static value html for ChatMessageType. */
     public static final ChatMessageType HTML = fromString("html");
 
-    /**
-     * Static value topicUpdated for ChatMessageType.
-     */
+    /** Static value topicUpdated for ChatMessageType. */
     public static final ChatMessageType TOPIC_UPDATED = fromString("topicUpdated");
 
-    /**
-     * Static value participantAdded for ChatMessageType.
-     */
+    /** Static value participantAdded for ChatMessageType. */
     public static final ChatMessageType PARTICIPANT_ADDED = fromString("participantAdded");
 
-    /**
-     * Static value participantRemoved for ChatMessageType.
-     */
+    /** Static value participantRemoved for ChatMessageType. */
     public static final ChatMessageType PARTICIPANT_REMOVED = fromString("participantRemoved");
 
     /**
@@ -48,9 +36,7 @@ public final class ChatMessageType extends ExpandableStringEnum<ChatMessageType>
         return fromString(name, ChatMessageType.class);
     }
 
-    /**
-     * @return known ChatMessageType values.
-     */
+    /** @return known ChatMessageType values. */
     public static Collection<ChatMessageType> values() {
         return values(ChatMessageType.class);
     }
