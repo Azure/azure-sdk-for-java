13acc000983959f4d9325b12553fb3f134a0eaf8
Muyao <v-muyaofeng@microsoft.com>
2024-03-20 19:36:52 +0800

    add BindingHandlerMappingsProvider

---
+++ sdk/spring/spring-cloud-azure-stream-binder-servicebus/src/main/java/com/azure/spring/cloud/stream/binder/servicebus/implementation/config/ExtendedBindingHandlerMappingsProviderConfiguration.java
@@ -0,0 +1,25 @@
+package com.azure.spring.cloud.stream.binder.servicebus.implementation.config;
+
+import org.springframework.boot.context.properties.source.ConfigurationPropertyName;
+import org.springframework.cloud.stream.config.BindingHandlerAdvise;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class ExtendedBindingHandlerMappingsProviderConfiguration {
+
+	@Bean
+	public BindingHandlerAdvise.MappingsProvider kafkaExtendedPropertiesDefaultMappingsProvider() {
+		return () -> {
+			Map<ConfigurationPropertyName, ConfigurationPropertyName> mappings = new HashMap<>();
+			mappings.put(
+					ConfigurationPropertyName.of("spring.cloud.stream.servicebus.bindings"),
+					ConfigurationPropertyName.of("spring.cloud.stream.servicebus.default"));
+			return mappings;
+		};
+	}
+
+}
