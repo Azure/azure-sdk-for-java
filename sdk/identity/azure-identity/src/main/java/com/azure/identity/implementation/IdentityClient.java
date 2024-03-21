39521a08f81f80f7abcc14f409763a6a788f80fc
Bill Wert <billwert@microsoft.com>
2024-03-20 18:27:36 -0700

    remove block in async path

--- sdk/identity/azure-identity/src/main/java/com/azure/identity/implementation/IdentityClient.java
+++ sdk/identity/azure-identity/src/main/java/com/azure/identity/implementation/IdentityClient.java
@@ -825,28 +825,30 @@ public class IdentityClient extends IdentityClientBase {
 
         // If the broker is enabled, try to get the token for the default account by passing
         // a null account to MSAL. If that fails, show the dialog.
-        Mono<MsalToken> token = null;
-        if (options.isBrokerEnabled() && options.useDefaultBrokerAccount()) {
-            token = getPublicClientInstance(request).getValue().flatMap(pc ->
-                Mono.fromFuture(() ->
-                        acquireTokenFromPublicClientSilently(request, pc, null, false)).
+
+        return getPublicClientInstance(request).getValue().flatMap(pc -> {
+            if (options.isBrokerEnabled() && options.useDefaultBrokerAccount()) {
+                return Mono.fromFuture(() ->
+                    acquireTokenFromPublicClientSilently(request, pc, null, false)).
                     // if something bad happened we fall back.
                     onErrorResume(t -> Mono.empty()).
-                    map(MsalToken::new));
-        }
-        if (token == null || token.hasElement().block() == Boolean.FALSE) {
+                    map(MsalToken::new);
+            } else {
+                return Mono.empty();
+            }
+        }).
+        switchIfEmpty(Mono.defer(() -> {
             InteractiveRequestParameters.InteractiveRequestParametersBuilder builder =
                 buildInteractiveRequestParameters(request, loginHint, redirectUri);
 
             SynchronizedAccessor<PublicClientApplication> publicClient = getPublicClientInstance(request);
 
-            Mono<IAuthenticationResult> acquireToken = publicClient.getValue()
-                .flatMap(pc -> Mono.fromFuture(() -> pc.acquireToken(builder.build())));
-
-            return acquireToken.onErrorMap(t -> new ClientAuthenticationException(
-                "Failed to acquire token with Interactive Browser Authentication.", null, t)).map(MsalToken::new);
-        }
-        return token;
+            return publicClient.getValue()
+                .flatMap(pc -> Mono.fromFuture(() -> pc.acquireToken(builder.build())))
+                .onErrorMap(t -> new ClientAuthenticationException(
+                    "Failed to acquire token with Interactive Browser Authentication.", null, t))
+                .map(MsalToken::new);
+        }));
     }
 
     /**
