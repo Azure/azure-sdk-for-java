c6e7b37ca77aea3c6d835e611df67daeacbef050
Victor Colin Amador <vicolina@microsoft.com>
2024-03-20 18:06:15 -0700

    Removed `HttpClientProviders` and moved its `createInstance()` method to `HttpClient`.

--- sdk/generic-sdk-core/generic-core/src/main/java/com/generic/core/http/client/HttpClient.java
+++ sdk/generic-sdk-core/generic-core/src/main/java/com/generic/core/http/client/HttpClient.java
@@ -5,6 +5,8 @@ package com.generic.core.http.client;
 
 import com.generic.core.http.Response;
 import com.generic.core.http.models.HttpRequest;
+import com.generic.core.implementation.util.Providers;
+import com.generic.core.util.configuration.Configuration;
 
 /**
  * A generic interface for sending HTTP requests and getting responses.
@@ -20,11 +22,34 @@ public interface HttpClient {
     Response<?> send(HttpRequest request);
 
     /**
-     * Creates a new {@link HttpClient} instance.
+     * Creates a new default {@link HttpClient} instance.
      *
-     * @return A new {@link HttpClient} instance.
+     * @return A new default {@link HttpClient} instance.
      */
     static HttpClient createDefault() {
-        return HttpClientProviders.createInstance();
+        return new DefaultHttpClientBuilder().build();
+    }
+
+    /**
+     * Creates a new instance of the {@link HttpClient} that the default {@link HttpClientProvider} is configured to
+     * create.
+     *
+     * @return A new {@link HttpClient} instance, entirely unrelated to all other instances that were created
+     * previously.
+     */
+    static HttpClient createInstance() {
+        final String NO_DEFAULT_PROVIDER_MESSAGE = "A request was made to load the default HttpClient provider "
+            + "but one could not be found on the classpath. If you are using a dependency manager, consider including "
+            + "a dependency on azure-core-http-netty or azure-core-http-okhttp. Depending on your existing "
+            + "dependencies, you have the choice of Netty or OkHttp implementations. Additionally, refer to "
+            + "https://aka.ms/azsdk/java/docs/custom-httpclient to learn about writing your own implementation.";
+
+        final Providers<HttpClientProvider, HttpClient> HTTP_CLIENT_PROVIDERS =
+            new Providers<>(HttpClientProvider.class,
+                Configuration.getGlobalConfiguration().get(Configuration.PROPERTY_HTTP_CLIENT_IMPLEMENTATION),
+                NO_DEFAULT_PROVIDER_MESSAGE);
+
+        return HTTP_CLIENT_PROVIDERS.create(HttpClientProvider::createInstance, new DefaultHttpClientBuilder().build(),
+            null);
     }
 }
