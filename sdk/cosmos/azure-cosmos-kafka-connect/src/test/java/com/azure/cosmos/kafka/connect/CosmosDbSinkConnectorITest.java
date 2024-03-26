754c06c7f60392aaf289b54a3b39483796c3f173
annie-mac <xinlian@microsoft.com>
2024-03-25 21:23:23 -0700

    add proto test cases

--- sdk/cosmos/azure-cosmos-kafka-connect/src/test/java/com/azure/cosmos/kafka/connect/CosmosDbSinkConnectorITest.java
+++ sdk/cosmos/azure-cosmos-kafka-connect/src/test/java/com/azure/cosmos/kafka/connect/CosmosDbSinkConnectorITest.java
@@ -10,6 +10,7 @@ import com.azure.cosmos.kafka.connect.implementation.CosmosClientStore;
 import com.azure.cosmos.kafka.connect.implementation.sink.CosmosSinkConfig;
 import com.fasterxml.jackson.databind.JsonNode;
 import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import io.confluent.kafka.serializers.protobuf.KafkaProtobufSerializer;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -159,6 +160,89 @@ public class CosmosDbSinkConnectorITest extends KafkaCosmosIntegrationTestSuiteB
                     .setProp(testItem.getProp())
                     .build();
                 ProducerRecord<GenericRecord, GenericRecord> testItemRecord = new ProducerRecord<>(singlePartitionContainerName, keyAvro, testItemAvro);
+                kafkaProducer.send(testItemRecord).get();
+                recordValueIds.add(testItem.getId());
+            }
+
+            // Wait for some time for the sink connector to process all records
+            Thread.sleep(5000);
+            // read from the container and verify all the items are created
+            String query = "select * from c";
+            List<String> createdItemIds = container.queryItems(query, TestItem.class)
+                .byPage()
+                .flatMapIterable(response -> response.getResults())
+                .map(TestItem::getId)
+                .collectList()
+                .block();
+            assertThat(createdItemIds.size()).isEqualTo(recordValueIds.size());
+            assertThat(createdItemIds.containsAll(recordValueIds)).isTrue();
+
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (client != null) {
+                logger.info("cleaning container {}", singlePartitionContainerName);
+                cleanUpContainer(client, databaseName, singlePartitionContainerName);
+                client.close();
+            }
+
+            // IMPORTANT: remove the connector after use
+            if (kafkaCosmosConnectContainer != null) {
+                kafkaCosmosConnectContainer.deleteConnector(connectorName);
+            }
+        }
+    }
+
+    @Test(groups = { "kafka-integration"}, timeOut = 10 * TIMEOUT)
+    public void sinkToSingleContainerWithProtoDeSe() throws InterruptedException {
+        Map<String, String> sinkConnectorConfig = new HashMap<>();
+        System.out.println("schema registry url:" + schemaRegistryContainer.getInternalBaseUrl());
+        System.out.println(KafkaCosmosTestConfigurations.SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO);
+
+        // TODO: use a different topic name as the previous tests
+        sinkConnectorConfig.put("topics", singlePartitionContainerName);
+        sinkConnectorConfig.put("value.converter", "io.confluent.connect.protobuf.ProtobufConverter");
+        // TODO[Public Preview]: add tests for with schema
+        sinkConnectorConfig.put("value.converter.schemas.enable", "true");
+        sinkConnectorConfig.put("value.converter.schema.registry.url", schemaRegistryContainer.getInternalBaseUrl());
+        sinkConnectorConfig.put("key.converter", StringConverter.class.getName());
+        sinkConnectorConfig.put("connector.class", "com.azure.cosmos.kafka.connect.CosmosDBSinkConnector");
+        sinkConnectorConfig.put("kafka.connect.cosmos.accountEndpoint", KafkaCosmosTestConfigurations.HOST);
+        sinkConnectorConfig.put("kafka.connect.cosmos.accountKey", KafkaCosmosTestConfigurations.MASTER_KEY);
+        sinkConnectorConfig.put("kafka.connect.cosmos.applicationName", "Test");
+        sinkConnectorConfig.put("kafka.connect.cosmos.sink.database.name", databaseName);
+        sinkConnectorConfig.put("kafka.connect.cosmos.sink.containers.topicMap", singlePartitionContainerName + "#" + singlePartitionContainerName);
+
+        // Create topic ahead of time
+        kafkaCosmosConnectContainer.createTopic(singlePartitionContainerName, 1);
+
+        CosmosSinkConfig sinkConfig = new CosmosSinkConfig(sinkConnectorConfig);
+        CosmosAsyncClient client = CosmosClientStore.getCosmosClient(sinkConfig.getAccountConfig());
+        CosmosAsyncContainer container = client.getDatabase(databaseName).getContainer(singlePartitionContainerName);
+
+        String connectorName = "simpleTest-" + UUID.randomUUID();
+        try {
+            // register the sink connector
+            kafkaCosmosConnectContainer.registerConnector(connectorName, sinkConnectorConfig);
+
+            Properties producerProperties = kafkaCosmosConnectContainer.getProducerProperties();
+            producerProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+            producerProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaProtobufSerializer.class.getName());
+            KafkaProducer<String, TestItemProtoOuterClass.TestItemProto> kafkaProducer = new KafkaProducer<>(producerProperties);
+
+            // first create few records in the topic
+            logger.info("Creating sink records...");
+            List<String> recordValueIds = new ArrayList<>();
+            for (int i = 0; i < 10; i++) {
+                TestItem testItem = TestItem.createNewItem();
+
+                TestItemProtoOuterClass.TestItemProto testItemProto = TestItemProtoOuterClass.TestItemProto.newBuilder()
+                    .setId(testItem.getId())
+                    .setMypk(testItem.getMypk())
+                    .setProp(testItem.getProp())
+                    .build();
+
+                ProducerRecord<String, TestItemProtoOuterClass.TestItemProto> testItemRecord = new ProducerRecord<>(singlePartitionContainerName, testItem.getId(), testItemProto);
                 kafkaProducer.send(testItemRecord).get();
                 recordValueIds.add(testItem.getId());
             }
