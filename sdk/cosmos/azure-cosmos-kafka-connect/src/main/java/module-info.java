9afd22bcbd80a0b1682fa745be82976e67821107
Annie Liang <64233642+xinlian12@users.noreply.github.com>
2024-03-25 17:38:37 -0700

    revertKafkaRelatedChange (#39392)
    * Revert "KafkaV2SinkConnector (#38973)"
    
    This reverts commit 6c983ab4f67008d8203fb12638f02f944ee358c5.
    
    * Revert "UsingTestContainerForKafkaIntegrationTests (#38884)"
    
    This reverts commit 12bec49fdf4b629b7a20c89b7fd22e18bff7b3d0.
    
    * Revert "KafkaV2SourceConnector (#38748)"
    
    This reverts commit 30835d90059b9991894a1a5da6f7cdfb3efe8f57.
    
    * revert one more change
    
    * revert change
    
    ---------
    
    Co-authored-by: annie-mac <xinlian@microsoft.com>

--- sdk/cosmos/azure-cosmos-kafka-connect/src/main/java/module-info.java
+++ sdk/cosmos/azure-cosmos-kafka-connect/src/main/java/module-info.java
@@ -6,11 +6,10 @@ module com.azure.cosmos.kafka.connect {
 
     requires transitive com.azure.cosmos;
     requires kafka.clients;
-    requires connect.api;
-    requires json.path;
 
 
     // public API surface area
     exports com.azure.cosmos.kafka.connect;
+
     uses com.azure.core.util.tracing.Tracer;
 }
