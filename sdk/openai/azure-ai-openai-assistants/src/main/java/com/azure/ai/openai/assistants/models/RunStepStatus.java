30777ae9f8eaf7bc3b735a26da02865a93280d81
azure-sdk <azuresdk@microsoft.com>
2024-03-27 04:10:15 +0000

    [Automation] Generate SDK based on TypeSpec 0.15.5

--- sdk/openai/azure-ai-openai-assistants/src/main/java/com/azure/ai/openai/assistants/models/RunStepStatus.java
+++ sdk/openai/azure-ai-openai-assistants/src/main/java/com/azure/ai/openai/assistants/models/RunStepStatus.java
@@ -5,7 +5,6 @@ package com.azure.ai.openai.assistants.models;
 
 import com.azure.core.annotation.Generated;
 import com.azure.core.util.ExpandableStringEnum;
-import com.fasterxml.jackson.annotation.JsonCreator;
 import java.util.Collection;
 
 /**
@@ -60,7 +59,6 @@ public final class RunStepStatus extends ExpandableStringEnum<RunStepStatus> {
      * @return the corresponding RunStepStatus.
      */
     @Generated
-    @JsonCreator
     public static RunStepStatus fromString(String name) {
         return fromString(name, RunStepStatus.class);
     }
--- sdk/openai/azure-ai-openai/src/main/java/com/azure/ai/openai/models/ImageGenerationOptions.java
+++ sdk/openai/azure-ai-openai/src/main/java/com/azure/ai/openai/models/ImageGenerationOptions.java
@@ -5,21 +5,23 @@ package com.azure.ai.openai.models;
 
 import com.azure.core.annotation.Fluent;
 import com.azure.core.annotation.Generated;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.azure.json.JsonReader;
+import com.azure.json.JsonSerializable;
+import com.azure.json.JsonToken;
+import com.azure.json.JsonWriter;
+import java.io.IOException;
 
 /**
  * Represents the request data used to generate images.
  */
 @Fluent
-public final class ImageGenerationOptions {
+public final class ImageGenerationOptions implements JsonSerializable<ImageGenerationOptions> {
 
     /*
      * A description of the desired images.
      */
     @Generated
-    @JsonProperty(value = "prompt")
-    private String prompt;
+    private final String prompt;
 
     /*
      * The number of images to generate.
@@ -27,7 +29,6 @@ public final class ImageGenerationOptions {
      * Dall-e-3 models only support a value of 1.
      */
     @Generated
-    @JsonProperty(value = "n")
     private Integer n;
 
     /*
@@ -36,14 +37,12 @@ public final class ImageGenerationOptions {
      * Dall-e-3 models support 1024x1024, 1792x1024, or 1024x1792.
      */
     @Generated
-    @JsonProperty(value = "size")
     private ImageSize size;
 
     /*
      * A unique identifier representing your end-user, which can help to monitor and detect abuse.
      */
     @Generated
-    @JsonProperty(value = "user")
     private String user;
 
     /**
@@ -52,8 +51,7 @@ public final class ImageGenerationOptions {
      * @param prompt the prompt value to set.
      */
     @Generated
-    @JsonCreator
-    public ImageGenerationOptions(@JsonProperty(value = "prompt") String prompt) {
+    public ImageGenerationOptions(String prompt) {
         this.prompt = prompt;
     }
 
@@ -120,8 +118,7 @@ public final class ImageGenerationOptions {
     }
 
     /**
-     * Get the user property: A unique identifier representing your end-user, which can help to monitor and detect
-     * abuse.
+     * Get the user property: A unique identifier representing your end-user, which can help to monitor and detect abuse.
      *
      * @return the user value.
      */
@@ -131,8 +128,7 @@ public final class ImageGenerationOptions {
     }
 
     /**
-     * Set the user property: A unique identifier representing your end-user, which can help to monitor and detect
-     * abuse.
+     * Set the user property: A unique identifier representing your end-user, which can help to monitor and detect abuse.
      *
      * @param user the user value to set.
      * @return the ImageGenerationOptions object itself.
@@ -147,7 +143,6 @@ public final class ImageGenerationOptions {
      * The format in which image generation response items should be presented.
      */
     @Generated
-    @JsonProperty(value = "response_format")
     private ImageGenerationResponseFormat responseFormat;
 
     /**
@@ -173,12 +168,10 @@ public final class ImageGenerationOptions {
     }
 
     /*
-     * The model name or Azure OpenAI model deployment name to use for image generation. If not specified, dall-e-2
-     * will be
+     * The model name or Azure OpenAI model deployment name to use for image generation. If not specified, dall-e-2 will be
      * inferred as a default.
      */
     @Generated
-    @JsonProperty(value = "model")
     private String model;
 
     /*
@@ -186,7 +179,6 @@ public final class ImageGenerationOptions {
      * Only configurable with dall-e-3 models.
      */
     @Generated
-    @JsonProperty(value = "quality")
     private ImageGenerationQuality quality;
 
     /*
@@ -194,12 +186,10 @@ public final class ImageGenerationOptions {
      * Only configurable with dall-e-3 models.
      */
     @Generated
-    @JsonProperty(value = "style")
     private ImageGenerationStyle style;
 
     /**
-     * Get the model property: The model name or Azure OpenAI model deployment name to use for image generation. If not
-     * specified, dall-e-2 will be
+     * Get the model property: The model name or Azure OpenAI model deployment name to use for image generation. If not specified, dall-e-2 will be
      * inferred as a default.
      *
      * @return the model value.
@@ -210,8 +200,7 @@ public final class ImageGenerationOptions {
     }
 
     /**
-     * Set the model property: The model name or Azure OpenAI model deployment name to use for image generation. If not
-     * specified, dall-e-2 will be
+     * Set the model property: The model name or Azure OpenAI model deployment name to use for image generation. If not specified, dall-e-2 will be
      * inferred as a default.
      *
      * @param model the model value to set.
@@ -269,5 +258,78 @@ public final class ImageGenerationOptions {
     public ImageGenerationOptions setStyle(ImageGenerationStyle style) {
         this.style = style;
         return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Generated
+    @Override
+    public JsonWriter toJson(JsonWriter jsonWriter) throws IOException {
+        jsonWriter.writeStartObject();
+        jsonWriter.writeStringField("prompt", this.prompt);
+        jsonWriter.writeStringField("model", this.model);
+        jsonWriter.writeNumberField("n", this.n);
+        jsonWriter.writeStringField("size", this.size == null ? null : this.size.toString());
+        jsonWriter.writeStringField("response_format",
+            this.responseFormat == null ? null : this.responseFormat.toString());
+        jsonWriter.writeStringField("quality", this.quality == null ? null : this.quality.toString());
+        jsonWriter.writeStringField("style", this.style == null ? null : this.style.toString());
+        jsonWriter.writeStringField("user", this.user);
+        return jsonWriter.writeEndObject();
+    }
+
+    /**
+     * Reads an instance of ImageGenerationOptions from the JsonReader.
+     *
+     * @param jsonReader The JsonReader being read.
+     * @return An instance of ImageGenerationOptions if the JsonReader was pointing to an instance of it, or null if it was pointing to JSON null.
+     * @throws IllegalStateException If the deserialized JSON object was missing any required properties.
+     * @throws IOException If an error occurs while reading the ImageGenerationOptions.
+     */
+    @Generated
+    public static ImageGenerationOptions fromJson(JsonReader jsonReader) throws IOException {
+        return jsonReader.readObject(reader -> {
+            String prompt = null;
+            String model = null;
+            Integer n = null;
+            ImageSize size = null;
+            ImageGenerationResponseFormat responseFormat = null;
+            ImageGenerationQuality quality = null;
+            ImageGenerationStyle style = null;
+            String user = null;
+            while (reader.nextToken() != JsonToken.END_OBJECT) {
+                String fieldName = reader.getFieldName();
+                reader.nextToken();
+                if ("prompt".equals(fieldName)) {
+                    prompt = reader.getString();
+                } else if ("model".equals(fieldName)) {
+                    model = reader.getString();
+                } else if ("n".equals(fieldName)) {
+                    n = reader.getNullable(JsonReader::getInt);
+                } else if ("size".equals(fieldName)) {
+                    size = ImageSize.fromString(reader.getString());
+                } else if ("response_format".equals(fieldName)) {
+                    responseFormat = ImageGenerationResponseFormat.fromString(reader.getString());
+                } else if ("quality".equals(fieldName)) {
+                    quality = ImageGenerationQuality.fromString(reader.getString());
+                } else if ("style".equals(fieldName)) {
+                    style = ImageGenerationStyle.fromString(reader.getString());
+                } else if ("user".equals(fieldName)) {
+                    user = reader.getString();
+                } else {
+                    reader.skipChildren();
+                }
+            }
+            ImageGenerationOptions deserializedImageGenerationOptions = new ImageGenerationOptions(prompt);
+            deserializedImageGenerationOptions.model = model;
+            deserializedImageGenerationOptions.n = n;
+            deserializedImageGenerationOptions.size = size;
+            deserializedImageGenerationOptions.responseFormat = responseFormat;
+            deserializedImageGenerationOptions.quality = quality;
+            deserializedImageGenerationOptions.style = style;
+            deserializedImageGenerationOptions.user = user;
+            return deserializedImageGenerationOptions;
+        });
     }
 }
