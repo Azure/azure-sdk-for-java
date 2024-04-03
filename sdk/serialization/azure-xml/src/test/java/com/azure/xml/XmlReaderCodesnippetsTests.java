ed8e9d464ed84f5906a0f77f8c334eae128b2835
Alan Zimmer <48699787+alzimmermsft@users.noreply.github.com>
2024-04-03 15:02:47 -0400

    Prepare azure-xml for initial GA

--- sdk/serialization/azure-xml/src/test/java/com/azure/xml/XmlReaderCodesnippetsTests.java
+++ sdk/serialization/azure-xml/src/test/java/com/azure/xml/XmlReaderCodesnippetsTests.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 
 import javax.xml.stream.XMLStreamException;
 
+import java.util.Objects;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
@@ -22,6 +24,23 @@ public class XmlReaderCodesnippetsTests {
 
             // Get the value of the attribute "attribute" as an Integer in a way that allows for the attribute to be
             // missing or have a null value.
+            Objects.equals(1234, reader.getNullableAttribute(null, "attribute", Integer::parseInt));
+
+            // This attribute doesn't exist, so null is returned without causing a NumberFormatException (which is what
+            // Integer.parseInt throws on a null string being passed).
+            Objects.isNull(reader.getNullableAttribute(null, "nonExistentAttribute", Integer::parseInt));
+        } catch (XMLStreamException ex) {
+            // Do something with the exception
+        }
+        // END: com.azure.xml.XmlReader.getNullableAttribute#String-String-ReadValueCallback
+
+        // The real test
+        try (XmlReader reader = XmlReader.fromString("<root><element attribute=\"1234\"/></root>")) {
+            reader.nextElement(); // Progress to <root>
+            reader.nextElement(); // Progress to <element>
+
+            // Get the value of the attribute "attribute" as an Integer in a way that allows for the attribute to be
+            // missing or have a null value.
             assertEquals(1234, (int) reader.getNullableAttribute(null, "attribute", Integer::parseInt));
 
             // This attribute doesn't exist, so null is returned without causing a NumberFormatException (which is what
@@ -30,12 +49,30 @@ public class XmlReaderCodesnippetsTests {
         } catch (XMLStreamException ex) {
             // Do something with the exception
         }
-        // END: com.azure.xml.XmlReader.getNullableAttribute#String-String-ReadValueCallback
     }
 
     @Test
     public void getNullableElement() {
         // BEGIN: com.azure.xml.XmlReader.getNullableElement#ReadValueCallback
+        try (XmlReader reader = XmlReader.fromString("<root><element>1234</element><emptyElement/></root>")) {
+            reader.nextElement(); // Progress to <root>
+            reader.nextElement(); // Progress to <element>
+
+            // Get the value of the element "element" as an Integer in a way that allows for the element to be missing
+            // or have a null value.
+            Objects.equals(1234, reader.getNullableElement(Integer::parseInt)); // 1234
+
+            reader.nextElement(); // Progress to <emptyElement>
+
+            // This element doesn't exist, so null is returned without causing a NumberFormatException (which is what
+            // Integer.parseInt throws on a null string being passed).
+            Objects.isNull(reader.getNullableElement(Integer::parseInt));
+        } catch (XMLStreamException ex) {
+            // Do something with the exception
+        }
+        // END: com.azure.xml.XmlReader.getNullableElement#ReadValueCallback
+
+        // The real test
         try (XmlReader reader = XmlReader.fromString("<root><element>1234</element><emptyElement/></root>")) {
             reader.nextElement(); // Progress to <root>
             reader.nextElement(); // Progress to <element>
@@ -52,6 +89,5 @@ public class XmlReaderCodesnippetsTests {
         } catch (XMLStreamException ex) {
             // Do something with the exception
         }
-        // END: com.azure.xml.XmlReader.getNullableElement#ReadValueCallback
     }
 }
