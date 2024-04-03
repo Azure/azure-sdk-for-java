eb2139e58e488bcc2e078612c37fb6c3669d4ff8
Weidong Xu <weidxu@microsoft.com>
2024-04-03 10:11:31 +0800

    autorest swagger/README-recordingstoragetype.md --java --use=@autorest/java@4.1.27 --use=@autorest/modelerfour@4.27.0 --debug --skip-formatting

--- sdk/communication/azure-communication-callautomation/swagger/src/main/java/CallautomationCustomizations.java
+++ sdk/communication/azure-communication-callautomation/swagger/src/main/java/CallautomationCustomizations.java
@@ -14,7 +14,7 @@ public class CallautomationCustomizations extends Customization {
     private static final String BASE_PACKAGE = "com.azure.communication.callautomation.";
     private static final String IMPLEMENTATION_MODELS_PACKAGE = BASE_PACKAGE + "implementation.models";
 
-    public static final String CALL_AUTOMATION_CLASS_NAME = "";
+    public static final String CALL_AUTOMATION_CLASS_NAME = "RecordingStorageTypeInternal";
 
     @Override
     public void customize(LibraryCustomization customization, Logger logger) {
@@ -22,10 +22,11 @@ public class CallautomationCustomizations extends Customization {
         try {
             customizeCallAutomation(customization);
         } catch (IllegalArgumentException e) {
-            logger.info("Yes failed");
+            logger.info("Yes failed " + e.getMessage());
         } catch (Exception e) {
-            logger.info("No terrible");
+            logger.info("No terrible " + e.getMessage());
         }
+        logger.info("Customization pass");
     }
 
     private void customizeCallAutomation(LibraryCustomization customization) {
