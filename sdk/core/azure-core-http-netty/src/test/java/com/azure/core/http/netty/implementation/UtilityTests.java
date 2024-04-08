3b9ccef9a4d56d924e3ce61ff5b8690b88d5de24
Alan Zimmer <48699787+alzimmermsft@users.noreply.github.com>
2024-04-08 12:40:42 -0400

    Fix Netty version mismatch from being logged due to incorrect POM configuration

--- sdk/core/azure-core-http-netty/src/test/java/com/azure/core/http/netty/implementation/UtilityTests.java
+++ sdk/core/azure-core-http-netty/src/test/java/com/azure/core/http/netty/implementation/UtilityTests.java
@@ -1,5 +1,17 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 package com.azure.core.http.netty.implementation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class UtilityTests {
+    @Test
+    public void validateNettyVersions() {
+        StringBuilder logger = new StringBuilder();
+        Utility.validateNettyVersions(logger::append);
+
+        assertEquals(0, logger.length(), "Unexpected Netty version mismatch logs.");
+    }
 }
