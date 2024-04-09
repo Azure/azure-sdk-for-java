e81f7f96f7f9a6ee98e3bbd889c1409cce5e3b1c
Liudmila Molkova <limolkova@microsoft.com>
2024-04-08 20:09:58 -0700

    log and rethrow runtime exception

--- sdk/core/azure-core-tracing-opentelemetry/src/main/java/com/azure/core/tracing/opentelemetry/OpenTelemetryTracer.java
+++ sdk/core/azure-core-tracing-opentelemetry/src/main/java/com/azure/core/tracing/opentelemetry/OpenTelemetryTracer.java
@@ -76,12 +76,13 @@ public class OpenTelemetryTracer implements com.azure.core.util.tracing.Tracer {
 
             // Shading plugins rewrite string literals that look like package name, so we have to trick it
             // to resolve the actual io.opentelemetry.context.Context.
-            // (If we're running in agent, this code was rewritten to use io.opentelemetry.javaagent.shaded.io.opentelemetry.context.Context)
             applicationContextClass = Class.forName("#io.opentelemetry.context.Context".substring(1));
             convertAppToAgentContextMethod = agentCtxStorageClass.getMethod("getAgentContext", applicationContextClass);
-        } catch (Throwable t) {
+        } catch (ReflectiveOperationException t) {
             // it's expected if we're not running in the agent
-            LOGGER.verbose("Failed to resolve agent context storage", t);
+            LOGGER.verbose("Failed to resolve AgentContextStorage.getAgentContext or one of its dependencies", t);
+        } catch (RuntimeException t) {
+            throw LOGGER.logThrowableAsError(t);
         }
     }
 
