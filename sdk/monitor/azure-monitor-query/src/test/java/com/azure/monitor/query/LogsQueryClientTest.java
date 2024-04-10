22f8b2f796e0454459306d219b14bce93923226c
jairmyree <jairmyree@microsoft.com>
2024-04-10 13:38:39 -0700

    Updating test to check reading with azure-json

--- sdk/monitor/azure-monitor-query/src/test/java/com/azure/monitor/query/LogsQueryClientTest.java
+++ sdk/monitor/azure-monitor-query/src/test/java/com/azure/monitor/query/LogsQueryClientTest.java
@@ -18,6 +18,7 @@ import com.azure.core.util.BinaryData;
 import com.azure.core.util.Context;
 import com.azure.core.util.serializer.TypeReference;
 import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.json.JsonOptions;
 import com.azure.json.JsonProviders;
 import com.azure.json.JsonReader;
 import com.azure.monitor.query.models.LogsBatchQuery;
@@ -285,9 +286,10 @@ public class LogsQueryClientTest extends TestProxyTestBase {
             Map<String, Object> statisticsMap = jsonReader.readMap(JsonReader::readUntyped);
             assertNotNull(statisticsMap);
             Object query = statisticsMap.get("query");
-            if (query instanceof Map) {
-                Map<String, Object> queryMap = (Map<String, Object>) query;
+            if (query instanceof Map<?, ?>) {
+                Map<?,?> queryMap = (Map<?,?>) query;
                 assertNotNull(queryMap.get("executionTime"));
+                assertNotNull(queryMap.get("resourceUsage"));
             } else {
                 Assertions.fail("Failed to read the statistics data.");
             }
