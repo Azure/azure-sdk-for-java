342c0c1144440195684a87e9e09ff066428a8855
jairmyree <jairmyree@microsoft.com>
2024-04-10 10:05:23 -0700

    Updating whitespacing

--- sdk/monitor/azure-monitor-query/src/test/java/com/azure/monitor/query/LogsQueryClientTest.java
+++ sdk/monitor/azure-monitor-query/src/test/java/com/azure/monitor/query/LogsQueryClientTest.java
@@ -356,7 +356,7 @@ public class LogsQueryClientTest extends TestProxyTestBase {
 
         BinaryData visualization = queryResults.getVisualization();
 
-        try ( JsonReader reader = JsonProviders.createReader(visualization.toStream());) {
+        try (JsonReader reader = JsonProviders.createReader(visualization.toStream());) {
             Map<String, Object> map = reader.readMap(innerReader -> {
                 return reader.readUntyped();
             });
