30d614b5f2508d80f20006498ff59dcc497d352b
Alan Zimmer <48699787+alzimmermsft@users.noreply.github.com>
2024-04-16 17:20:43 -0400

    Few more CI issue fixes

--- sdk/client-core/core/src/main/java/io/clientcore/core/http/client/HttpClientProvider.java
+++ sdk/client-core/core/src/main/java/io/clientcore/core/http/client/HttpClientProvider.java
@@ -13,7 +13,7 @@ import io.clientcore.core.util.configuration.Configuration;
 public abstract class HttpClientProvider {
     static final String NO_DEFAULT_PROVIDER_MESSAGE = "A request was made to load the default HttpClient provider "
         + "but one could not be found on the classpath. If you are using a dependency manager, consider including "
-        + "a dependency on io.clientcore:http-okhttp. Additionally, refer to "
+        + "a dependency on io.clientcore:http-okhttp3 or io.clientcore:http-jdk-httpclient. Additionally, refer to "
         + "https://aka.ms/azsdk/java/docs/custom-httpclient to learn about writing your own implementation.";
 
     static HttpClient sharedHttpClient;
