acf014f7e241237e59181ed48fb660323536fb61
Victor Colin Amador <vicolina@microsoft.com>
2024-04-17 14:48:09 -0700

    Added deprecation suppression to avoid compiler failure.

--- sdk/client-core/core/src/main/java/io/clientcore/core/http/models/HttpRequest.java
+++ sdk/client-core/core/src/main/java/io/clientcore/core/http/models/HttpRequest.java
@@ -136,6 +136,7 @@ public class HttpRequest {
      * @throws NullPointerException if {@code url} is {@code null}.
      * @throws IllegalArgumentException If {@code url} cannot be parsed into a valid {@link URL}.
      */
+    @SuppressWarnings("deprecation")
     public HttpRequest setUrl(String url) {
         try {
             this.url = new URL(Objects.requireNonNull(url, "'url' cannot be null"));
