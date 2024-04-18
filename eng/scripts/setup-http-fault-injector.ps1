efd6e176b29be18c4e10c30d2cbc9f754e6758fe
Rabab <ibrahimr@microsoft.com>
2024-04-18 11:54:22 -0700

    removing changes to http-fault-injector script to decouple the changes

--- eng/scripts/setup-http-fault-injector.ps1
+++ eng/scripts/setup-http-fault-injector.ps1
@@ -3,9 +3,6 @@ param(
   [string]$SourcesDirectory = $env:BUILD_SOURCESDIRECTORY
 )
 
-# Save the current directory
-$originalPath = Get-Location
-
 dotnet tool install azure.sdk.tools.httpfaultinjector --global --prerelease --add-source https://pkgs.dev.azure.com/azure-sdk/public/_packaging/azure-sdk-for-net/nuget/v3/index.json
 dotnet tool update azure.sdk.tools.httpfaultinjector --global --prerelease --add-source https://pkgs.dev.azure.com/azure-sdk/public/_packaging/azure-sdk-for-net/nuget/v3/index.json
 http-fault-injector --version
@@ -14,27 +11,20 @@ Write-Host "##vso[task.setvariable variable=ASPNETCORE_Kestrel__Certificates__De
 Write-Host "##vso[task.setvariable variable=ASPNETCORE_Kestrel__Certificates__Default__Password]password"
 Write-Host "##vso[task.setvariable variable=PROXY_MANUAL_START]true"
 
-# Trust http-fault-injector self-signed certificate
-$securityPath = $null
-if (Test-Path "$env:JAVA_HOME/jre/lib/security") {
-    $securityPath = "$env:JAVA_HOME/jre/lib/security"
-} elseif (Test-Path "$env:JAVA_HOME/lib/security") {
-    $securityPath = "$env:JAVA_HOME/lib/security"
-}
-
-if ($securityPath) {
-    Set-Location $securityPath
-    dotnet dev-certs https --export-path http-fault-injector.pfx
-
-    if ($IsWindows) {
-        keytool -keystore cacerts -importcert -noprompt -trustcacerts -alias HttpFaultInject -file http-fault-injector.pfx -storepass changeit
-    } else {
-        sudo keytool -keystore cacerts -importcert -noprompt -trustcacerts -alias HttpFaultInject -file http-fault-injector.pfx -storepass changeit
-    }
-
-    # Revert back to the original directory
-    Set-Location $originalPath
+# Trust http-fault-injector self-siigned certificate
+if (Test-Path $env:JAVA_HOME/jre/lib/security) {
+    Set-Location $env:JAVA_HOME/jre/lib/security
+} elseif (Test-Path $env:JAVA_HOME/lib/security) {
+    Set-Location $env:JAVA_HOME/lib/security
 } else {
     Write-Error "JDK directory structure is unknown and unsupported. JAVA_HOME is '$env:JAVA_HOME'"
     exit 1
+}
+
+dotnet dev-certs https --export-path http-fault-injector.pfx
+
+if ($IsWindows) {
+    keytool -keystore cacerts -importcert -noprompt -trustcacerts -alias HttpFaultInject -file http-fault-injector.pfx -storepass changeit
+} else {
+    sudo keytool -keystore cacerts -importcert -noprompt -trustcacerts -alias HttpFaultInject -file http-fault-injector.pfx -storepass changeit
 }
