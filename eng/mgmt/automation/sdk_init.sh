eff6818ed9835f3eb6d1cd2df47160e7e68a831b
Weidong Xu <weidxu@microsoft.com>
2024-04-22 13:21:32 +0800

    install tsp-client

--- eng/mgmt/automation/sdk_init.sh
+++ eng/mgmt/automation/sdk_init.sh
@@ -3,6 +3,9 @@
 apt-get install python3.8
 apt-get install -y --upgrade python3-pip python3-setuptools
 
+# install tsp-client globally (local install may interfere with tooling)
+npm install -g @azure-tools/typespec-client-generator-cli
+
 cat << EOF > $1
 {}
 EOF
