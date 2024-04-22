b3b794187ebaac027b4e5e1e7fe701f3bbedca20
Weidong Xu <weidxu@microsoft.com>
2024-04-22 13:30:20 +0800

    print apt-get stderr to stdout

--- eng/mgmt/automation/sdk_init.sh
+++ eng/mgmt/automation/sdk_init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-apt-get install python3.8
-apt-get install -y --upgrade python3-pip python3-setuptools
+apt-get install python3.8 2>&1
+apt-get install -y --upgrade python3-pip python3-setuptools 2>&1
 
 # install tsp-client globally (local install may interfere with tooling)
 npm install -g @azure-tools/typespec-client-generator-cli
