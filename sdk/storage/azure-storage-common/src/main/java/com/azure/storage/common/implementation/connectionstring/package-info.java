9232e6d10a6fad8a19654fb3d10405e062c10d8c
azure-sdk <azuresdk@microsoft.com>
2024-04-23 09:30:07 -0700

    Reset sources for azure-storage-common to the release version 12.24.3.

--- sdk/storage/azure-storage-common/src/main/java/com/azure/storage/common/implementation/connectionstring/package-info.java
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-/**
- * Package containing classes for parsing connection strings.
- */
-package com.azure.storage.common.implementation.connectionstring;
