9b684aff0df46d9986107a8795df1fb82c172d3c
azure-sdk <azuresdk@microsoft.com>
2024-04-23 09:30:29 -0700

    Reset sources for azure-storage-internal-avro to the release version 12.10.3.

--- sdk/storage/azure-storage-internal-avro/src/main/java/com/azure/storage/internal/avro/implementation/schema/file/package-info.java
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-/**
- * Package containing classes for Avro file schema.
- */
-package com.azure.storage.internal.avro.implementation.schema.file;
