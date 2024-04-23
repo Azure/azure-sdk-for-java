4d64017883b24097c9e21098790c614406ba6377
Shawn Fang <shafang@microsoft.com>
2024-04-23 13:00:50 -0700

    remove entire nio and changefeed packages

--- sdk/storage/azure-storage-blob-changefeed/src/main/java/com/azure/storage/blob/changefeed/implementation/models/package-info.java
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-/**
- * Package containing implementation models for Blob Changefeed.
- */
-package com.azure.storage.blob.changefeed.implementation.models;
