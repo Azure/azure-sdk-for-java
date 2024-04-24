7697af61939d9372d10021d797659caba0fb2472
José Álvarez <josealvar@microsoft.com>
2024-04-24 08:43:03 +0200

    Rewrote factory with ifs instead

--- sdk/openai/azure-ai-openai-assistants/src/main/java/com/azure/ai/openai/assistants/implementation/OpenAISSETypeFactory.java
+++ sdk/openai/azure-ai-openai-assistants/src/main/java/com/azure/ai/openai/assistants/implementation/OpenAISSETypeFactory.java
@@ -24,20 +24,20 @@ import static com.azure.ai.openai.assistants.implementation.models.AssistantStre
 
 public class OpenAISSETypeFactory {
 
-    public <T extends JsonSerializable<T>> deserializeEvent(BinaryData event, AssistantStreamEvent eventType) {
-        return switch (eventType.toString()) {
-            case THREAD_CREATED -> event.toObject(ThreadRun.class);
-            case THREAD_RUN_CREATED -> event.toObject(ThreadRun.class);
-            case THEARD_RUN_QUEUED -> event.toObject(ThreadRun.class);
-            case THREAD_RUN_IN_PROGRESS -> event.toObject(ThreadRun.class);
-            case THREAD_RUN_STEP_CREATED -> event.toObject(RunStep.class);
-            case THREAD_RUN_STEP_IN_PROGRESS -> event.toObject(RunStep.class);
-            case THREAD_MESSAGE_CREATED -> event.toObject(ThreadMessage.class);
-            case THREAD_MESSAGE_IN_PROGRESS -> event.toObject(ThreadMessage.class);
-            case THREAD_MESSAGE_DELTA -> event.toObject(MessageDeltaChunk.class);
-            case THREAD_MESSAGE_COMPLETED -> event.toObject(ThreadMessage.class);
-            case THREAD_RUN_COMPLETED -> event.toObject(ThreadRun.class);
-            default -> throw new IllegalArgumentException("Unknown event type: " + eventType);
-        };
+    public <T extends JsonSerializable<T>> T deserializeEvent(BinaryData event, AssistantStreamEvent eventType) {
+        if (THREAD_CREATED.equals(eventType) || THREAD_RUN_CREATED.equals(eventType) ||
+            THEARD_RUN_QUEUED.equals(eventType) || THREAD_RUN_IN_PROGRESS.equals(eventType) ||
+            THREAD_RUN_COMPLETED.equals(eventType)) {
+            return event.toObject(ThreadRun.class);
+        } else if (THREAD_RUN_STEP_CREATED.equals(eventType) || THREAD_RUN_STEP_IN_PROGRESS.equals(eventType)) {
+            return event.toObject(RunStep.class);
+        } else if (THREAD_MESSAGE_CREATED.equals(eventType) || THREAD_MESSAGE_IN_PROGRESS.equals(eventType)
+            || THREAD_MESSAGE_COMPLETED.equals(eventType)) {
+            return event.toObject(ThreadMessage.class);
+        } else if(THREAD_MESSAGE_DELTA.equals(eventType)) {
+            return event.toObject(MessageDeltaChunk.class);
+        } else {
+            throw new IllegalArgumentException("Unknown event type: " + eventType);
+        }
     }
 }
