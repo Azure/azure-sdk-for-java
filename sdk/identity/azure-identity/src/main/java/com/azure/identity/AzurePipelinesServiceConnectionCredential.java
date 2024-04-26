d5e09018e789bf30e7c196ba495aa1c7b4096dd9
Bill Wert <billwert@microsoft.com>
2024-04-26 15:24:23 -0700

    new AzurePipelinesServiceConnectionCredential

---
+++ sdk/identity/azure-identity/src/main/java/com/azure/identity/AzurePipelinesServiceConnectionCredential.java
@@ -0,0 +1,34 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.identity;
+
+import com.azure.core.credential.AccessToken;
+import com.azure.core.credential.TokenCredential;
+import com.azure.core.credential.TokenRequestContext;
+import com.azure.identity.implementation.IdentityClientOptions;
+import reactor.core.publisher.Mono;
+
+/**
+ * The {@link AzurePipelinesServiceConnectionCredential} acquires a token using the Azure Devops Pipeline service connection.
+ */
+public class AzurePipelinesServiceConnectionCredential implements TokenCredential {
+
+    private final String serviceConnectionId;
+
+    private final IdentityClientOptions identityClientOptions;
+
+    AzurePipelinesServiceConnectionCredential(String serviceConnectionId, IdentityClientOptions identityClientOptions) {
+        this.serviceConnectionId = serviceConnectionId;
+        this.identityClientOptions = identityClientOptions;
+    }
+    @Override
+    public Mono<AccessToken> getToken(TokenRequestContext request) {
+        return null;
+    }
+
+    @Override
+    public AccessToken getTokenSync(TokenRequestContext request) {
+        return null;
+    }
+}
