24ec39474c05bacab405e764546972d5ceb261b0
Xiaofei Cao <92354331+XiaofeiCao@users.noreply.github.com>
2024-04-26 16:58:46 +0800

    mpg, generate.py support generating from tsp (#39832)
    * generate script
    
    generation script
    
    generate.py
    
    * use tsp-client

--- eng/mgmt/automation/generate_utils.py
+++ eng/mgmt/automation/generate_utils.py
@@ -299,7 +299,15 @@ def update_spec(spec: str, subspec: str) -> str:
     return spec
 
 
-def generate_typespec_project(tsp_project: str, sdk_root: str, spec_root: str, head_sha: str, repo_url: str):
+def generate_typespec_project(
+    tsp_project: str,
+    sdk_root: str,
+    spec_root: str = None,
+    head_sha: str = "",
+    repo_url: str = ""):
+
+    if not tsp_project:
+        return False
 
     succeeded = False
     sdk_folder = None
@@ -307,15 +315,25 @@ def generate_typespec_project(tsp_project: str, sdk_root: str, spec_root: str, h
     module = None
     require_sdk_integration = False
 
-    tsp_dir = os.path.join(spec_root, tsp_project)
-
     try:
-        repo = remove_prefix(repo_url, 'https://github.com/')
-        cmd = ['npx', 'tsp-client', 'init', '--debug',
-               '--tsp-config', tsp_dir,
-               '--commit', head_sha,
-               '--repo', repo,
-               '--local-spec-repo', tsp_dir]
+        url_match = re.match(
+            r'^https://github.com/(?P<repo>[^/]*/azure-rest-api-specs(-pr)?)/blob/(?P<commit>[0-9a-f]{40})/(?P<path>.*)/tspconfig.yaml$',
+            tsp_project,
+            re.IGNORECASE
+        )
+        if url_match:
+            # generate from remote url
+            cmd = ['npx', 'tsp-client', 'init', '--debug',
+                   '--tsp-config', tsp_project]
+        else:
+            # sdk automation
+            tsp_dir = os.path.join(spec_root, tsp_project) if spec_root else tsp_project
+            repo = remove_prefix(repo_url, 'https://github.com/')
+            cmd = ['npx', 'tsp-client', 'init', '--debug',
+                   '--tsp-config', tsp_dir,
+                   '--commit', head_sha,
+                   '--repo', repo,
+                   '--local-spec-repo', tsp_dir]
         check_call(cmd, sdk_root)
 
         sdk_folder = find_sdk_folder(sdk_root)
