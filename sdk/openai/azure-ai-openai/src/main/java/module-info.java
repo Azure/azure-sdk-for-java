2cee6cd787a7018cf8e492d31127d49cb95aa54a
Shawn Fang <shafang@microsoft.com>
2024-04-29 23:33:08 -0700

    Unable to remove FunctionCallPreset.java

--- sdk/openai/azure-ai-openai/src/main/java/module-info.java
+++ sdk/openai/azure-ai-openai/src/main/java/module-info.java
@@ -8,5 +8,6 @@ module com.azure.ai.openai {
     exports com.azure.ai.openai;
     exports com.azure.ai.openai.models;
     opens com.azure.ai.openai.models to com.azure.core;
+    opens com.azure.ai.openai.implementation.models to com.azure.core;
     opens com.azure.ai.openai.implementation to com.azure.core;
 }
