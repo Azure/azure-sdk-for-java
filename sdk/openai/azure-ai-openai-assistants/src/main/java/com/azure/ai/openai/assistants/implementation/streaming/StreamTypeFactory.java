ac7eb419917d40307755adf6b84887fd16953e30
Jose Alvarez <josealvar@microsoft.com>
2024-05-03 13:51:25 +0200

    Added all the possible branches for event types

--- sdk/openai/azure-ai-openai-assistants/src/main/java/com/azure/ai/openai/assistants/implementation/streaming/StreamTypeFactory.java
+++ sdk/openai/azure-ai-openai-assistants/src/main/java/com/azure/ai/openai/assistants/implementation/streaming/StreamTypeFactory.java
@@ -22,15 +22,22 @@ import static com.azure.ai.openai.assistants.implementation.models.AssistantStre
 import static com.azure.ai.openai.assistants.implementation.models.AssistantStreamEvent.THREAD_MESSAGE_COMPLETED;
 import static com.azure.ai.openai.assistants.implementation.models.AssistantStreamEvent.THREAD_MESSAGE_CREATED;
 import static com.azure.ai.openai.assistants.implementation.models.AssistantStreamEvent.THREAD_MESSAGE_DELTA;
+import static com.azure.ai.openai.assistants.implementation.models.AssistantStreamEvent.THREAD_MESSAGE_INCOMPLETE;
 import static com.azure.ai.openai.assistants.implementation.models.AssistantStreamEvent.THREAD_MESSAGE_IN_PROGRESS;
+import static com.azure.ai.openai.assistants.implementation.models.AssistantStreamEvent.THREAD_RUN_CANCELLED;
+import static com.azure.ai.openai.assistants.implementation.models.AssistantStreamEvent.THREAD_RUN_CANCELLING;
 import static com.azure.ai.openai.assistants.implementation.models.AssistantStreamEvent.THREAD_RUN_COMPLETED;
 import static com.azure.ai.openai.assistants.implementation.models.AssistantStreamEvent.THREAD_RUN_CREATED;
+import static com.azure.ai.openai.assistants.implementation.models.AssistantStreamEvent.THREAD_RUN_EXPIRED;
+import static com.azure.ai.openai.assistants.implementation.models.AssistantStreamEvent.THREAD_RUN_FAILED;
 import static com.azure.ai.openai.assistants.implementation.models.AssistantStreamEvent.THREAD_RUN_IN_PROGRESS;
 import static com.azure.ai.openai.assistants.implementation.models.AssistantStreamEvent.THREAD_RUN_QUEUED;
 import static com.azure.ai.openai.assistants.implementation.models.AssistantStreamEvent.THREAD_RUN_REQUIRES_ACTION;
+import static com.azure.ai.openai.assistants.implementation.models.AssistantStreamEvent.THREAD_RUN_STEP_CANCELLED;
 import static com.azure.ai.openai.assistants.implementation.models.AssistantStreamEvent.THREAD_RUN_STEP_COMPLETED;
 import static com.azure.ai.openai.assistants.implementation.models.AssistantStreamEvent.THREAD_RUN_STEP_CREATED;
 import static com.azure.ai.openai.assistants.implementation.models.AssistantStreamEvent.THREAD_RUN_STEP_DELTA;
+import static com.azure.ai.openai.assistants.implementation.models.AssistantStreamEvent.THREAD_RUN_STEP_EXPIRED;
 import static com.azure.ai.openai.assistants.implementation.models.AssistantStreamEvent.THREAD_RUN_STEP_FAILED;
 import static com.azure.ai.openai.assistants.implementation.models.AssistantStreamEvent.THREAD_RUN_STEP_IN_PROGRESS;
 
@@ -43,14 +50,17 @@ public final class StreamTypeFactory {
         if (THREAD_CREATED.equals(event)) {
             return new StreamThreadCreation(eventJson.toObject(AssistantThread.class));
         } else if  (THREAD_RUN_CREATED.equals(event) ||  THREAD_RUN_QUEUED.equals(event) ||
-                THREAD_RUN_IN_PROGRESS.equals(event) || THREAD_RUN_COMPLETED.equals(event)) {
+                THREAD_RUN_IN_PROGRESS.equals(event) || THREAD_RUN_COMPLETED.equals(event) ||
+                THREAD_RUN_CANCELLED.equals(event) || THREAD_RUN_CANCELLING.equals(event) ||
+                THREAD_RUN_FAILED.equals(event) || THREAD_RUN_EXPIRED.equals(event)) {
             return new StreamThreadRunCreation(eventJson.toObject(ThreadRun.class));
         } else if (THREAD_RUN_STEP_CREATED.equals(event) || THREAD_RUN_STEP_IN_PROGRESS.equals(event) ||
-                THREAD_RUN_STEP_COMPLETED.equals(event) || THREAD_RUN_STEP_FAILED.equals(event)) {
+                THREAD_RUN_STEP_COMPLETED.equals(event) || THREAD_RUN_STEP_FAILED.equals(event) ||
+                THREAD_RUN_STEP_CANCELLED.equals(event) || THREAD_RUN_STEP_EXPIRED.equals(event)) {
             return new StreamRunCreation(eventJson.toObject(RunStep.class));
         } else if (THREAD_MESSAGE_CREATED.equals(event) || THREAD_MESSAGE_IN_PROGRESS.equals(event)) {
             return new StreamMessageCreation(eventJson.toObject(ThreadMessage.class));
-        } else if (THREAD_MESSAGE_COMPLETED.equals(event)) {
+        } else if (THREAD_MESSAGE_COMPLETED.equals(event) || THREAD_MESSAGE_INCOMPLETE.equals(event)) {
             return new StreamMessageCompletion(eventJson.toObject(ThreadMessage.class));
         } else if (THREAD_MESSAGE_DELTA.equals(event)) {
             return new StreamMessageUpdate(eventJson.toObject(MessageDeltaChunk.class));
