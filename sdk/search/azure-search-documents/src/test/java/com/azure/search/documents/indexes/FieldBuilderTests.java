3019cb4b560668c8c062dc6a8f22db17bcb1829d
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2024-05-07 13:12:58 -0400

    Fix linting

--- sdk/search/azure-search-documents/src/test/java/com/azure/search/documents/indexes/FieldBuilderTests.java
+++ sdk/search/azure-search-documents/src/test/java/com/azure/search/documents/indexes/FieldBuilderTests.java
@@ -25,7 +25,6 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.OffsetDateTime;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
