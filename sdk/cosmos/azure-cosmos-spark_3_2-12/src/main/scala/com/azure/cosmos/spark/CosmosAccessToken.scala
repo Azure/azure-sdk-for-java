e93870402966d6f971810f8f8a193fdb03b92869
Fabian Meiswinkel <fabianm@microsoft.com>
2024-05-08 14:15:42 +0000

    Added scala doc

--- sdk/cosmos/azure-cosmos-spark_3_2-12/src/main/scala/com/azure/cosmos/spark/CosmosAccessToken.scala
+++ sdk/cosmos/azure-cosmos-spark_3_2-12/src/main/scala/com/azure/cosmos/spark/CosmosAccessToken.scala
@@ -4,4 +4,9 @@ package com.azure.cosmos.spark
 
 import java.time.OffsetDateTime
 
+/**
+ * Class representing access tokens
+ * @param token the actual token
+ * @param Offset the expiration time of the token
+ */
 case class CosmosAccessToken(token: String, Offset: OffsetDateTime)
