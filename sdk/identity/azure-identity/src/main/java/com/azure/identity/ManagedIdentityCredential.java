572133d45ab8d1a7dcdaf2aa3f350319dd236bb3
Vinay Gera <vigera@microsoft.com>
2024-05-13 11:32:06 -0700

    update MI cred env var

--- sdk/identity/azure-identity/src/main/java/com/azure/identity/ManagedIdentityCredential.java
+++ sdk/identity/azure-identity/src/main/java/com/azure/identity/ManagedIdentityCredential.java
@@ -91,6 +91,8 @@ public final class ManagedIdentityCredential implements TokenCredential {
     static final String PROPERTY_IDENTITY_SERVER_THUMBPRINT = "IDENTITY_SERVER_THUMBPRINT";
     static final String AZURE_FEDERATED_TOKEN_FILE = "AZURE_FEDERATED_TOKEN_FILE";
 
+    static final String USE_AZURE_IDENTITY_CLIENT_LIBRARY_LEGACY_MI = "USE_AZURE_IDENTITY_CLIENT_LIBRARY_LEGACY_MI";
+
 
     /**
      * Creates an instance of the ManagedIdentityCredential with the client ID of a
@@ -133,7 +135,7 @@ public final class ManagedIdentityCredential implements TokenCredential {
                 .identityClientOptions(updateIdentityClientOptions(ManagedIdentityType.AKS,
                     identityClientOptions, configuration))
                 .build());
-        } else if (configuration.contains("USE_LEGACY_AZURE_MANAGED_IDENTITY_AUTH_CODE")) {
+        } else if (configuration.contains(USE_AZURE_IDENTITY_CLIENT_LIBRARY_LEGACY_MI)) {
             if (configuration.contains(Configuration.PROPERTY_MSI_ENDPOINT)) {
                 managedIdentityServiceCredential = new AppServiceMsiCredential(clientId, clientBuilder
                     .identityClientOptions(updateIdentityClientOptions(ManagedIdentityType.APP_SERVICE,
