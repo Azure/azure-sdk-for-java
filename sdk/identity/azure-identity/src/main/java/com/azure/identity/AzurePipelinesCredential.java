f9e5d66a0f3c1141f2da19a25ec9ce307ffb670d
Bill Wert <billwert@microsoft.com>
2024-05-17 17:18:56 -0700

    first cut at AzurePipelinesCredential

--- sdk/identity/azure-identity/src/main/java/com/azure/identity/AzurePipelinesCredential.java
+++ sdk/identity/azure-identity/src/main/java/com/azure/identity/AzurePipelinesCredential.java
@@ -6,29 +6,55 @@ package com.azure.identity;
 import com.azure.core.credential.AccessToken;
 import com.azure.core.credential.TokenCredential;
 import com.azure.core.credential.TokenRequestContext;
+import com.azure.core.http.HttpClient;
+import com.azure.core.http.HttpHeaderName;
+import com.azure.core.http.HttpMethod;
+import com.azure.core.http.HttpPipeline;
+import com.azure.core.http.HttpRequest;
+import com.azure.core.http.HttpResponse;
+import com.azure.identity.implementation.ClientAssertionCredentialHelper;
+import com.azure.identity.implementation.IdentityClientBase;
 import com.azure.identity.implementation.IdentityClientOptions;
 import reactor.core.publisher.Mono;
+
+import java.io.IOException;
+import java.net.URL;
 
 /**
  * The {@link AzurePipelinesCredential} acquires a token using the Azure Devops Pipeline service connection.
  */
 public class AzurePipelinesCredential implements TokenCredential {
+    private final ClientAssertionCredentialHelper clientAssertionCredentialHelper;
 
-    private final String serviceConnectionId;
+    AzurePipelinesCredential(String clientId, String tenantId, String requestUrl, String systemAccessToken, IdentityClientOptions identityClientOptions) {
 
-    private final IdentityClientOptions identityClientOptions;
 
-    AzurePipelinesCredential(String serviceConnectionId, IdentityClientOptions identityClientOptions) {
-        this.serviceConnectionId = serviceConnectionId;
-        this.identityClientOptions = identityClientOptions;
+        clientAssertionCredentialHelper = new ClientAssertionCredentialHelper(clientId, tenantId, identityClientOptions, () -> {
+            HttpClient client = identityClientOptions.getHttpClient();
+            if (client == null ) {
+                HttpClient.createDefault();
+            }
+            HttpPipeline pipeline = IdentityClientBase.setupPipeline(client, identityClientOptions);
+            try {
+                URL url = new URL(requestUrl);
+                HttpRequest request = new HttpRequest(HttpMethod.POST, url);
+                request.setHeader(HttpHeaderName.AUTHORIZATION, "Bearer " + systemAccessToken);
+                request.setHeader(HttpHeaderName.CONTENT_TYPE, "application/json");
+                try (HttpResponse response = pipeline.sendSync(request, null)) {
+                    return response.getBodyAsString().block();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
     @Override
     public Mono<AccessToken> getToken(TokenRequestContext request) {
-        return null;
+        return clientAssertionCredentialHelper.getToken(request);
     }
 
     @Override
     public AccessToken getTokenSync(TokenRequestContext request) {
-        return null;
+        return clientAssertionCredentialHelper.getTokenSync(request);
     }
 }
--- sdk/identity/azure-identity/src/main/java/com/azure/identity/AzurePipelinesCredentialBuilder.java
+++ sdk/identity/azure-identity/src/main/java/com/azure/identity/AzurePipelinesCredentialBuilder.java
@@ -3,12 +3,16 @@
 
 package com.azure.identity;
 
+import com.azure.core.util.Configuration;
+
+import java.util.Objects;
+
 /**
  * The {@link AzurePipelinesCredentialBuilder} provides a fluent builder for {@link AzurePipelinesCredential}.
  */
 public class AzurePipelinesCredentialBuilder extends AadCredentialBuilderBase<AzurePipelinesCredentialBuilder> {
     private String serviceConnectionId;
-
+    private final static String OIDC_API_VERSION = "7.1-preview.1";
     /**
      * Sets the service connection id for the Azure Devops Pipeline service connection. The service connection id is
      * retrieved from the Serivce Connection in the portal.
@@ -26,6 +30,24 @@ public class AzurePipelinesCredentialBuilder extends AadCredentialBuilderBase<Az
      * @return an instance of the {@link AzurePipelinesCredential}.
      */
     public AzurePipelinesCredential build() {
-        return new AzurePipelinesCredential(serviceConnectionId, identityClientOptions.clone());
+        Objects.requireNonNull(serviceConnectionId);
+        Objects.requireNonNull(this.clientId);
+        Objects.requireNonNull(this.tenantId);
+
+        String teamFoundationCollectionUri = Configuration.getGlobalConfiguration().get("SYSTEM_TEAMFOUNDATIONCOLLECTIONURI");
+        String teamProjectId = Configuration.getGlobalConfiguration().get("SYSTEM_TEAMPROJECTID");
+        String planId = Configuration.getGlobalConfiguration().get("SYSTEM_PLANID");
+        String jobId = Configuration.getGlobalConfiguration().get("SYSTEM_JOBID");
+        String systemAccessToken = Configuration.getGlobalConfiguration().get("SYSTEM_ACCESSTOKEN");
+
+        Objects.requireNonNull(teamFoundationCollectionUri);
+        Objects.requireNonNull(teamProjectId);
+        Objects.requireNonNull(planId);
+        Objects.requireNonNull(jobId);
+        Objects.requireNonNull(systemAccessToken);
+
+        String requestUrl = String.format("%s%s/_apis/distributedtask/hubs/build/plans/%s/jobs/%s/oidctoken?api-version=%s&serviceConnectionId=%s",
+            teamFoundationCollectionUri, teamProjectId, planId, jobId, OIDC_API_VERSION, serviceConnectionId);
+        return new AzurePipelinesCredential(this.clientId, this.tenantId, requestUrl, systemAccessToken, identityClientOptions.clone());
     }
 }
