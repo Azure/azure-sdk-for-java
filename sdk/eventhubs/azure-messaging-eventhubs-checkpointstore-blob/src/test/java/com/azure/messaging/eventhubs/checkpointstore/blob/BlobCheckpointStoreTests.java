7b2e8c3879c44121fc0b94f0c7d22878c715bb50
Connie Yau <conniey@microsoft.com>
2024-05-21 12:05:42 -0700

    Fix test assertion

--- sdk/eventhubs/azure-messaging-eventhubs-checkpointstore-blob/src/test/java/com/azure/messaging/eventhubs/checkpointstore/blob/BlobCheckpointStoreTests.java
+++ sdk/eventhubs/azure-messaging-eventhubs-checkpointstore-blob/src/test/java/com/azure/messaging/eventhubs/checkpointstore/blob/BlobCheckpointStoreTests.java
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
+ // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
 package com.azure.messaging.eventhubs.checkpointstore.blob;
@@ -196,7 +196,7 @@ public class BlobCheckpointStoreTests {
                 assertEquals(eventHubName, checkpoint.getEventHubName());
                 assertEquals(consumerGroup, checkpoint.getConsumerGroup());
                 assertEquals(1L, checkpoint.getSequenceNumber());
-                assertEquals("230L", checkpoint.getOffset());
+                assertEquals("230", checkpoint.getOffset());
             }).verifyComplete();
     }
 
