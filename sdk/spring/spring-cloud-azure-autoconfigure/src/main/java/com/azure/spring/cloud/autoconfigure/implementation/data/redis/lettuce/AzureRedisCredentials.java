6ae796fb991fe62c176b58607dfa8e1f1d9162f5
Xiaolu Dai <xiada@microsoft.com>
2024-05-21 17:56:05 +0800

    Support Lettuce passwordless autoconfiguration

---
+++ sdk/spring/spring-cloud-azure-autoconfigure/src/main/java/com/azure/spring/cloud/autoconfigure/implementation/data/redis/lettuce/AzureRedisCredentials.java
@@ -0,0 +1,48 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.spring.cloud.autoconfigure.implementation.data.redis.lettuce;
+
+import com.azure.identity.extensions.implementation.template.AzureAuthenticationTemplate;
+import com.azure.spring.cloud.core.properties.PasswordlessProperties;
+import io.lettuce.core.RedisCredentials;
+
+import java.util.Objects;
+
+public class AzureRedisCredentials implements RedisCredentials {
+
+    private final AzureAuthenticationTemplate azureAuthenticationTemplate;
+    private final String username;
+
+    /**
+     * Create instance of Azure Redis Credentials
+     * @param username the username to be used for authentication.
+     */
+    public AzureRedisCredentials(String username, PasswordlessProperties passwordlessProperties) {
+        Objects.requireNonNull(username, "Username is required");
+        Objects.requireNonNull(passwordlessProperties, "PasswordlessProperties is required");
+        this.username = username;
+        azureAuthenticationTemplate = new AzureAuthenticationTemplate();
+        azureAuthenticationTemplate.init(passwordlessProperties.toPasswordlessProperties());
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean hasUsername() {
+        return username != null;
+    }
+
+    @Override
+    public char[] getPassword() {
+        return azureAuthenticationTemplate.getTokenAsPassword().toCharArray();
+    }
+
+    @Override
+    public boolean hasPassword() {
+        return true;
+    }
+}
