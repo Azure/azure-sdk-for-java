e3520a3db85146c91b9d596ffb5c82e18e7a2539
Bill Wert <billwert@microsoft.com>
2024-05-23 12:55:00 -0700

    fix linter error

--- sdk/identity/azure-identity/src/test/java/com/azure/identity/recorded/IdentityTestBase.java
+++ sdk/identity/azure-identity/src/test/java/com/azure/identity/recorded/IdentityTestBase.java
@@ -12,7 +12,6 @@ import com.azure.core.test.models.*;
 import com.azure.core.util.Configuration;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
