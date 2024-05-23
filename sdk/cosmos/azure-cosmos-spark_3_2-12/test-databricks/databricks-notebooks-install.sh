22d4660fab071044d579cb4cca66b9e9a2226315
Fabian Meiswinkel <fabianm@microsoft.com>
2024-05-23 20:10:57 +0000

    Update databricks-notebooks-install.sh

--- sdk/cosmos/azure-cosmos-spark_3_2-12/test-databricks/databricks-notebooks-install.sh
+++ sdk/cosmos/azure-cosmos-spark_3_2-12/test-databricks/databricks-notebooks-install.sh
@@ -9,8 +9,9 @@ TENANTID=$6
 RESOURCEGROUPNAME=$7
 CLIENTID=$8
 CLIENTSECRET=$9
-COSMOSCONTAINERNAME=${10}
-COSMOSDATABASENAME=${11}
+CLIENTCERTPEM=${10}
+COSMOSCONTAINERNAME=${11}
+COSMOSDATABASENAME=${12}
 [[ -z "$CLUSTER_NAME" ]] && exit 1
 [[ -z "$NOTEBOOKSFOLDER" ]] && exit 1
 [[ -z "$COSMOSENDPOINT" ]] && exit 1
@@ -20,6 +21,7 @@ COSMOSDATABASENAME=${11}
 [[ -z "$RESOURCEGROUPNAME" ]] && exit 1
 [[ -z "$CLIENTID" ]] && exit 1
 [[ -z "$CLIENTSECRET" ]] && exit 1
+[[ -z "$CLIENTCERTPEM" ]] && exit 1
 [[ -z "$COSMOSCONTAINERNAME" ]] && exit 1
 [[ -z "$COSMOSDATABASENAME" ]] && exit 1
 
@@ -44,7 +46,7 @@ do
 	fi
 
 	echo "Creating run for job $JOB_ID"
-	RUN_ID=$(databricks jobs run-now --job-id $JOB_ID --notebook-params "{\"cosmosEndpoint\": \"$COSMOSENDPOINT\",\"cosmosMasterKey\": \"$COSMOSKEY\",\"subscriptionId\": \"$SUBSCRIPTIONID\",\"tenantId\": \"$TENANTID\",\"resourceGroupName\": \"$RESOURCEGROUPNAME\",\"clientId\": \"$CLIENTID\",\"clientSecret\": \"$CLIENTSECRET\",\"cosmosContainerName\": \"$COSMOSCONTAINERNAME\", \"cosmosDatabaseName\": \"$COSMOSDATABASENAME\"}" | jq -r '.run_id')
+	RUN_ID=$(databricks jobs run-now --job-id $JOB_ID --notebook-params "{\"cosmosEndpoint\": \"$COSMOSENDPOINT\",\"cosmosMasterKey\": \"$COSMOSKEY\",\"subscriptionId\": \"$SUBSCRIPTIONID\",\"tenantId\": \"$TENANTID\",\"resourceGroupName\": \"$RESOURCEGROUPNAME\",\"clientId\": \"$CLIENTID\",\"clientSecret\": \"$CLIENTSECRET\",\"clientCertPem\": \"$CLIENTCERTPEM\",\"cosmosContainerName\": \"$COSMOSCONTAINERNAME\", \"cosmosDatabaseName\": \"$COSMOSDATABASENAME\"}" | jq -r '.run_id')
 
 	if [[ -z "$RUN_ID" ]]
 	then
