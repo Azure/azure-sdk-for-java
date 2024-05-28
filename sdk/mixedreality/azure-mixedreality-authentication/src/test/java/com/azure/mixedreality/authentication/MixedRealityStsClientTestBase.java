21cbba988160aae396c4917e9fb88e4e4b1dd04f
azure-sdk <azuresdk@microsoft.com>
2024-05-28 16:53:10 -0700

    Reset sources for azure-mixedreality-authentication to the release version 1.2.23.

--- sdk/mixedreality/azure-mixedreality-authentication/src/test/java/com/azure/mixedreality/authentication/MixedRealityStsClientTestBase.java
+++ sdk/mixedreality/azure-mixedreality-authentication/src/test/java/com/azure/mixedreality/authentication/MixedRealityStsClientTestBase.java
@@ -10,30 +10,22 @@ import com.azure.core.http.HttpPipeline;
 import com.azure.core.http.HttpPipelineBuilder;
 import com.azure.core.http.policy.BearerTokenAuthenticationPolicy;
 import com.azure.core.http.policy.HttpPipelinePolicy;
-import com.azure.core.test.TestProxyTestBase;
-import com.azure.core.test.models.CustomMatcher;
-import com.azure.core.test.models.BodilessMatcher;
-import com.azure.core.test.models.TestProxyRequestMatcher;
-import com.azure.core.test.models.TestProxySanitizer;
-import com.azure.core.test.models.TestProxySanitizerType;
+import com.azure.core.test.TestBase;
+import com.azure.core.test.TestMode;
 import com.azure.core.util.Configuration;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
-public class MixedRealityStsClientTestBase extends TestProxyTestBase {
-    public static final String INVALID_DUMMY_TOKEN = "eyJhbGciOiJub25lIiwidHlwIjoiSldUIn0.eyJlbWFpbCI6IkJvYkBjb250b"
-        + "3NvLmNvbSIsImdpdmVuX25hbWUiOiJCb2IiLCJpc3MiOiJodHRwOi8vRGVmYXVsdC5Jc3N1ZXIuY29tIiwiYXVkIjoiaHR0cDovL0RlZm"
-        + "F1bHQuQXVkaWVuY2UuY29tIiwiaWF0IjoiMTYwNzk3ODY4MyIsIm5iZiI6IjE2MDc5Nzg2ODMiLCJleHAiOiIxNjA3OTc4OTgzIn0.";
+public class MixedRealityStsClientTestBase extends TestBase {
     private final String accountDomain = Configuration.getGlobalConfiguration().get("MIXEDREALITY_ACCOUNT_DOMAIN");
     private final String accountId = Configuration.getGlobalConfiguration().get("MIXEDREALITY_ACCOUNT_ID");
     private final String accountKey = Configuration.getGlobalConfiguration().get("MIXEDREALITY_ACCOUNT_KEY");
 
     // NOT REAL ACCOUNT DETAILS
     private final String playbackAccountDomain = "mixedreality.azure.com";
-    private final String playbackAccountId = "f5b3e69f-1e1b-46a5-a718-aea58a7a0f8e";
+    private final String playbackAccountId = "68321d5a-7978-4ceb-b880-0f49751daae9";
     private final String playbackAccountKey = "NjgzMjFkNWEtNzk3OC00Y2ViLWI4ODAtMGY0OTc1MWRhYWU5";
 
     HttpPipeline getHttpPipeline(HttpClient httpClient) {
@@ -48,29 +40,13 @@ public class MixedRealityStsClientTestBase extends TestProxyTestBase {
         final List<HttpPipelinePolicy> policies = new ArrayList<>();
         policies.add(new BearerTokenAuthenticationPolicy(credential, authenticationScope));
 
-        if (interceptorManager.isRecordMode() || interceptorManager.isPlaybackMode()) {
-            List<TestProxySanitizer> customSanitizers = new ArrayList<>();
-            customSanitizers.add(new TestProxySanitizer("$..AccessToken", null, INVALID_DUMMY_TOKEN,
-                TestProxySanitizerType.BODY_KEY));
-            interceptorManager.addSanitizers(customSanitizers);
-        }
-
-        if (interceptorManager.isRecordMode()) {
+        if (getTestMode() == TestMode.RECORD) {
             policies.add(interceptorManager.getRecordPolicy());
         }
 
-        if (interceptorManager.isPlaybackMode()) {
-            List<TestProxyRequestMatcher> customMatchers = new ArrayList<>();
-            customMatchers.add(new BodilessMatcher());
-            customMatchers.add(new CustomMatcher().setExcludedHeaders(Collections.singletonList("X-MRC-CV")));
-            interceptorManager.addMatchers(customMatchers);
-        }
-
-
-
         HttpPipeline pipeline = new HttpPipelineBuilder()
             .policies(policies.toArray(new HttpPipelinePolicy[0]))
-            .httpClient(interceptorManager.isPlaybackMode() ? interceptorManager.getPlaybackClient() : httpClient)
+            .httpClient(httpClient == null ? interceptorManager.getPlaybackClient() : httpClient)
             .build();
 
         return pipeline;
