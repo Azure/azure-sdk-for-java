dbd6bf19bdccde0f8dcdee607a7c760c8142739b
Sameeksha Vaity <savaity@microsoft.com>
2024-05-30 15:19:47 -0700

    Update CancelTranslationTests.java with RecordWithoutRequestBody

--- sdk/translation/azure-ai-translation-document/src/test/java/com/azure/ai/translation/document/CancelTranslationTests.java
+++ sdk/translation/azure-ai-translation-document/src/test/java/com/azure/ai/translation/document/CancelTranslationTests.java
@@ -27,28 +27,31 @@ import com.azure.ai.translation.document.models.BatchRequest;
 import com.azure.ai.translation.document.models.SourceInput;
 import com.azure.ai.translation.document.models.TargetInput;
 import com.azure.ai.translation.document.models.TranslationStatus;
+import com.azure.core.test.annotation.RecordWithoutRequestBody;
 import com.azure.core.util.polling.SyncPoller;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CancelTranslationTests extends DocumentTranslationClientTestBase {
 
+    @RecordWithoutRequestBody
     @Test
     public void testCancelTranslation() {
-        
+
         String sourceUrl = createSourceContainer(ONE_TEST_DOCUMENTS);
         SourceInput sourceInput = TestHelper.CreateSourceInput(sourceUrl, null, null, null);
-        
+
         String targetUrl = createTargetContainer(null);
-        String targetLanguageCode = "fr"; 
+        String targetLanguageCode = "fr";
         TargetInput targetInput = TestHelper.CreateTargetInput(targetUrl, targetLanguageCode, null, null, null);
-        List<TargetInput> targetInputs = new ArrayList<>(); 
+        List<TargetInput> targetInputs = new ArrayList<>();
         targetInputs.add(targetInput);
         BatchRequest batchRequest = new BatchRequest(sourceInput, targetInputs);
         SyncPoller<TranslationStatus, Void> poller =
-                 getDocumentTranslationClient().beginStartTranslation(TestHelper.GetStartTranslationDetails(batchRequest));   
+                 getDocumentTranslationClient().beginStartTranslation(TestHelper.GetStartTranslationDetails(batchRequest));
 
         //Cancel Translation
         String translationId = poller.poll().getValue().getId();
@@ -63,7 +66,7 @@ public class CancelTranslationTests extends DocumentTranslationClientTestBase {
         String status = translationStatus.getStatus().toString();
         System.out.println("Translation status is : " + status);
         Assertions.assertTrue(status.equals("Cancelled") || status.equals("Cancelling") || status.equals("NotStarted"));
-        
+
     }
-    
+
 }
