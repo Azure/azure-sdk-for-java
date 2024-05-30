9bd1bfd2e8d240e68baa4e68343984ef7aab2d2a
Jose Alvarez <josealvar@microsoft.com>
2024-05-30 14:27:18 +0200

    Code regen

--- sdk/openai/azure-ai-openai-assistants/src/main/java/com/azure/ai/openai/assistants/models/CreateRunOptions.java
+++ sdk/openai/azure-ai-openai-assistants/src/main/java/com/azure/ai/openai/assistants/models/CreateRunOptions.java
@@ -361,7 +361,7 @@ public final class CreateRunOptions implements JsonSerializable<CreateRunOptions
      * An alternative to sampling with temperature, called nucleus sampling, where the model
      * considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens
      * comprising the top 10% probability mass are considered.
-     *
+     * 
      * We generally recommend altering this or temperature but not both.
      */
     @Generated
