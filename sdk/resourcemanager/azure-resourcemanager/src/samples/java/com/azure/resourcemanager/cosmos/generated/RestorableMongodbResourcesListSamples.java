4b5c5c09885948b18e32b520193d9026bd831eca
hongli750210 <v-hongli1@microsoft.com>
2024-06-05 13:23:47 +0800

    mgmt, local generation for Cosmos DB 2024-05-15 Stable

--- sdk/resourcemanager/azure-resourcemanager/src/samples/java/com/azure/resourcemanager/cosmos/generated/RestorableMongodbResourcesListSamples.java
+++ sdk/resourcemanager/azure-resourcemanager/src/samples/java/com/azure/resourcemanager/cosmos/generated/RestorableMongodbResourcesListSamples.java
@@ -9,8 +9,7 @@ package com.azure.resourcemanager.cosmos.generated;
  */
 public final class RestorableMongodbResourcesListSamples {
     /*
-     * x-ms-original-file: specification/cosmos-db/resource-manager/Microsoft.DocumentDB/stable/2023-11-15/examples/
-     * CosmosDBRestorableMongodbResourceList.json
+     * x-ms-original-file: specification/cosmos-db/resource-manager/Microsoft.DocumentDB/stable/2024-05-15/examples/CosmosDBRestorableMongodbResourceList.json
      */
     /**
      * Sample code: CosmosDBRestorableMongodbResourceList.
@@ -18,7 +17,11 @@ public final class RestorableMongodbResourcesListSamples {
      * @param azure The entry point for accessing resource management APIs in Azure.
      */
     public static void cosmosDBRestorableMongodbResourceList(com.azure.resourcemanager.AzureResourceManager azure) {
-        azure.cosmosDBAccounts().manager().serviceClient().getRestorableMongodbResources().list("WestUS",
-            "d9b26648-2f53-4541-b3d8-3044f4f9810d", "WestUS", "06/01/2022 4:56", com.azure.core.util.Context.NONE);
+        azure.cosmosDBAccounts()
+            .manager()
+            .serviceClient()
+            .getRestorableMongodbResources()
+            .list("WestUS", "d9b26648-2f53-4541-b3d8-3044f4f9810d", "WestUS", "06/01/2022 4:56",
+                com.azure.core.util.Context.NONE);
     }
 }
--- sdk/resourcemanager/azure-resourcemanager/src/samples/java/com/azure/resourcemanager/cosmos/generated/SqlResourcesRetrieveContinuousBackupInformationSamples.java
+++ sdk/resourcemanager/azure-resourcemanager/src/samples/java/com/azure/resourcemanager/cosmos/generated/SqlResourcesRetrieveContinuousBackupInformationSamples.java
@@ -11,8 +11,7 @@ import com.azure.resourcemanager.cosmos.models.ContinuousBackupRestoreLocation;
  */
 public final class SqlResourcesRetrieveContinuousBackupInformationSamples {
     /*
-     * x-ms-original-file: specification/cosmos-db/resource-manager/Microsoft.DocumentDB/stable/2023-11-15/examples/
-     * CosmosDBSqlContainerBackupInformation.json
+     * x-ms-original-file: specification/cosmos-db/resource-manager/Microsoft.DocumentDB/stable/2024-05-15/examples/CosmosDBSqlContainerBackupInformation.json
      */
     /**
      * Sample code: CosmosDBSqlContainerBackupInformation.
@@ -20,8 +19,11 @@ public final class SqlResourcesRetrieveContinuousBackupInformationSamples {
      * @param azure The entry point for accessing resource management APIs in Azure.
      */
     public static void cosmosDBSqlContainerBackupInformation(com.azure.resourcemanager.AzureResourceManager azure) {
-        azure.cosmosDBAccounts().manager().serviceClient().getSqlResources().retrieveContinuousBackupInformation(
-            "rgName", "ddb1", "databaseName", "containerName",
-            new ContinuousBackupRestoreLocation().withLocation("North Europe"), com.azure.core.util.Context.NONE);
+        azure.cosmosDBAccounts()
+            .manager()
+            .serviceClient()
+            .getSqlResources()
+            .retrieveContinuousBackupInformation("rgName", "ddb1", "databaseName", "containerName",
+                new ContinuousBackupRestoreLocation().withLocation("North Europe"), com.azure.core.util.Context.NONE);
     }
 }
