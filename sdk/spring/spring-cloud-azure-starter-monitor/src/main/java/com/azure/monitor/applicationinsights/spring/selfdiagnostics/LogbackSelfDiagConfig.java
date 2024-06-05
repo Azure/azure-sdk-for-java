9012838d6157edbdfacdf51913b01d5cc3d74702
Jean Bisutti <jean.bisutti@gmail.com>
2024-06-05 14:39:41 +0200

    Remove unused import

--- sdk/spring/spring-cloud-azure-starter-monitor/src/main/java/com/azure/monitor/applicationinsights/spring/selfdiagnostics/LogbackSelfDiagConfig.java
+++ sdk/spring/spring-cloud-azure-starter-monitor/src/main/java/com/azure/monitor/applicationinsights/spring/selfdiagnostics/LogbackSelfDiagConfig.java
@@ -6,7 +6,6 @@ package com.azure.monitor.applicationinsights.spring.selfdiagnostics;
 import ch.qos.logback.classic.Level;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
