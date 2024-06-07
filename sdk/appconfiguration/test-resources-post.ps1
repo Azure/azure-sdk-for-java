07296921e862d71ed4dd305fa4ac48bb98f6c118
Vinay Gera <vigera@microsoft.com>
2024-06-06 19:36:24 -0700

    update test setup

---
+++ sdk/appconfiguration/test-resources-post.ps1
@@ -0,0 +1,26 @@
+# cspell:ignore Drevapi, Djacoco, Dcodesnippet, Dcheckstyle, Dspotbugs, Dspotless, Dcheckstyle, Dfunction, Dresource, vmcontainer, OIDC, oidc, usermgdid, azfunc, kubelet
+
+param (
+    [hashtable] $DeploymentOutputs
+)
+function getVariable {
+    param (
+        $name
+    )
+    if ($null -ne $DeploymentOutputs -and $DeploymentOutputs.ContainsKey($name)) {
+        return $DeploymentOutputs[$name]
+    }
+    if (test-path "env:\$name") {
+        return (Get-Item "env:\$name").Value
+    }
+    Write-Error "Could not find value for $name"
+    exit 1
+}
+
+$azIdentityRoot = "$PSScriptRoot/../identity/azure-identity/pom.xml" | Resolve-Path
+
+mvn clean install -DskipTests "-Drevapi.skip=true" "-Dcheckstyle.skip=true" "-Dcodesnippet.skip=true" "-Dspotbugs.skip=true" "-Dmaven.javadoc.skip=true" "-Dspotless.check.skip=true" "-Dspotless.apply.skip=true" "-Djacoco.skip=true" -f $azIdentityRootPom | Write-Host
+
+
+
+
