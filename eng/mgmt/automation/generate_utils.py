fc5ecef839dc575530fb37e751093c15c9e73f72
xiaofeicao <xiaofeicao@microsoft.com>
2024-06-16 16:34:38 +0800

    try shell=True
    tsp sdkautomation shell=True

--- eng/mgmt/automation/generate_utils.py
+++ eng/mgmt/automation/generate_utils.py
@@ -362,36 +362,15 @@ def generate_typespec_project(
         tspconfig_valid = True
         if url_match:
             # generate from remote url
-            tsp_cmd = [
-                "npx" + (".cmd" if is_windows() else ""),
-                "tsp-client",
-                "init",
-                "--debug",
-                "--tsp-config",
-                tsp_project,
-            ]
+            tsp_cmd = f"npx tsp-client init --debug --tsp-config {tsp_project}"
         else:
             # sdk automation
             tsp_dir = os.path.join(spec_root, tsp_project) if spec_root else tsp_project
             tspconfig_valid = validate_tspconfig(tsp_dir)
             repo = remove_prefix(repo_url, "https://github.com/")
-            tsp_cmd = [
-                "npx" + (".cmd" if is_windows() else ""),
-                "tsp-client",
-                "init",
-                "--debug",
-                "--tsp-config",
-                tsp_dir,
-                "--commit",
-                head_sha,
-                "--repo",
-                repo,
-                "--local-spec-repo",
-                tsp_dir,
-            ]
-
+            tsp_cmd = f"npx tsp-client init --debug --tsp-config {tsp_dir} --commit {head_sha} --repo {repo} --local-spec-repo {tsp_dir}"
         if tspconfig_valid:
-            check_call(tsp_cmd, sdk_root)
+            check_call(tsp_cmd, sdk_root, shell=True)
 
             sdk_folder = find_sdk_folder(sdk_root)
             logging.info("SDK folder: " + sdk_folder)
@@ -422,7 +401,7 @@ def generate_typespec_project(
                     drop_changes(sdk_root)
                     remove_generated_source_code(sdk_folder, f"{group_id}.{service}")
                     # regenerate
-                    check_call(tsp_cmd, sdk_root)
+                    check_call(tsp_cmd, sdk_root, shell=True)
                 succeeded = True
     except subprocess.CalledProcessError as error:
         error_message = (
