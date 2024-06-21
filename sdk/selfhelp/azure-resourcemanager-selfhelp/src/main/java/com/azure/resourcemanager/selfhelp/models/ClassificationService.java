c6a18843c3a033f177c2119de8fe58a5b471190d
azure-sdk <azuresdk@microsoft.com>
2024-06-21 07:08:27 +0000

    [Automation] Generate Fluent Lite from Swagger help#package-2024-03-01-preview

--- sdk/selfhelp/azure-resourcemanager-selfhelp/src/main/java/com/azure/resourcemanager/selfhelp/models/ClassificationService.java
+++ sdk/selfhelp/azure-resourcemanager-selfhelp/src/main/java/com/azure/resourcemanager/selfhelp/models/ClassificationService.java
@@ -5,30 +5,31 @@
 package com.azure.resourcemanager.selfhelp.models;
 
 import com.azure.core.annotation.Fluent;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.azure.json.JsonReader;
+import com.azure.json.JsonSerializable;
+import com.azure.json.JsonToken;
+import com.azure.json.JsonWriter;
+import java.io.IOException;
 import java.util.List;
 
 /**
  * Service Classification result object.
  */
 @Fluent
-public final class ClassificationService {
+public final class ClassificationService implements JsonSerializable<ClassificationService> {
     /*
      * Azure resource Id of the service.
      */
-    @JsonProperty(value = "serviceId", access = JsonProperty.Access.WRITE_ONLY)
     private String serviceId;
 
     /*
      * Localized name of the azure service.
      */
-    @JsonProperty(value = "displayName", access = JsonProperty.Access.WRITE_ONLY)
     private String displayName;
 
     /*
      * List of applicable ARM resource types for this service.
      */
-    @JsonProperty(value = "resourceTypes")
     private List<String> resourceTypes;
 
     /**
@@ -81,5 +82,47 @@ public final class ClassificationService {
      * @throws IllegalArgumentException thrown if the instance is not valid.
      */
     public void validate() {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public JsonWriter toJson(JsonWriter jsonWriter) throws IOException {
+        jsonWriter.writeStartObject();
+        jsonWriter.writeArrayField("resourceTypes", this.resourceTypes,
+            (writer, element) -> writer.writeString(element));
+        return jsonWriter.writeEndObject();
+    }
+
+    /**
+     * Reads an instance of ClassificationService from the JsonReader.
+     * 
+     * @param jsonReader The JsonReader being read.
+     * @return An instance of ClassificationService if the JsonReader was pointing to an instance of it, or null if it
+     * was pointing to JSON null.
+     * @throws IOException If an error occurs while reading the ClassificationService.
+     */
+    public static ClassificationService fromJson(JsonReader jsonReader) throws IOException {
+        return jsonReader.readObject(reader -> {
+            ClassificationService deserializedClassificationService = new ClassificationService();
+            while (reader.nextToken() != JsonToken.END_OBJECT) {
+                String fieldName = reader.getFieldName();
+                reader.nextToken();
+
+                if ("serviceId".equals(fieldName)) {
+                    deserializedClassificationService.serviceId = reader.getString();
+                } else if ("displayName".equals(fieldName)) {
+                    deserializedClassificationService.displayName = reader.getString();
+                } else if ("resourceTypes".equals(fieldName)) {
+                    List<String> resourceTypes = reader.readArray(reader1 -> reader1.getString());
+                    deserializedClassificationService.resourceTypes = resourceTypes;
+                } else {
+                    reader.skipChildren();
+                }
+            }
+
+            return deserializedClassificationService;
+        });
     }
 }
