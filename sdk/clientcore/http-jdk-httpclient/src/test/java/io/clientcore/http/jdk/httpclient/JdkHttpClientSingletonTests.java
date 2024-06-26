c4f43a43f0f45687db54924bbbe8284d4e1f5212
Victor Colin Amador <vicolina@microsoft.com>
2024-06-26 13:57:37 -0700

    Updated tests.

--- sdk/clientcore/http-jdk-httpclient/src/test/java/io/clientcore/http/jdk/httpclient/JdkHttpClientSingletonTests.java
+++ sdk/clientcore/http-jdk-httpclient/src/test/java/io/clientcore/http/jdk/httpclient/JdkHttpClientSingletonTests.java
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package io.clientcore.http.okhttp3;
+package io.clientcore.http.jdk.httpclient;
 
 import io.clientcore.core.http.client.HttpClient;
 import org.junit.jupiter.api.Test;
@@ -12,19 +12,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @Execution(ExecutionMode.SAME_THREAD) // To avoid a race condition because of singleton http client.
-public class OkHttpHttpClientSingletonTests {
+public class JdkHttpClientSingletonTests {
     @Test
     public void testGetSharedInstance() {
-        HttpClient client1 = new OkHttpHttpClientProvider().getSharedInstance();
-        HttpClient client2 = new OkHttpHttpClientProvider().getSharedInstance();
+        HttpClient client1 = new JdkHttpClientProvider().getSharedInstance();
+        HttpClient client2 = new JdkHttpClientProvider().getSharedInstance();
 
         assertEquals(client1, client2);
     }
 
     @Test
     public void testGetNewInstance() {
-        HttpClient client1 = new OkHttpHttpClientProvider().getNewInstance();
-        HttpClient client2 = new OkHttpHttpClientProvider().getNewInstance();
+        HttpClient client1 = new JdkHttpClientProvider().getNewInstance();
+        HttpClient client2 = new JdkHttpClientProvider().getNewInstance();
 
         assertNotEquals(client1, client2);
     }
