3db39b64d3cfb254ae81b7ef0de4dfbcf1b3448f
azure-sdk <azuresdk@microsoft.com>
2024-06-27 16:06:54 -0700

    Reset sources for azure-data-schemaregistry-apacheavro to the release version 1.1.17.

--- sdk/schemaregistry/azure-data-schemaregistry-apacheavro/src/samples/java/com/azure/data/schemaregistry/apacheavro/generatedtestsources/PlayingCardSuit.java
+++ sdk/schemaregistry/azure-data-schemaregistry-apacheavro/src/samples/java/com/azure/data/schemaregistry/apacheavro/generatedtestsources/PlayingCardSuit.java
@@ -9,7 +9,5 @@ public enum PlayingCardSuit implements org.apache.avro.generic.GenericEnumSymbol
   SPADES, HEARTS, DIAMONDS, CLUBS  ;
   public static final org.apache.avro.Schema SCHEMA$ = new org.apache.avro.Schema.Parser().parse("{\"type\":\"enum\",\"name\":\"PlayingCardSuit\",\"namespace\":\"com.azure.data.schemaregistry.apacheavro.generatedtestsources\",\"symbols\":[\"SPADES\",\"HEARTS\",\"DIAMONDS\",\"CLUBS\"]}");
   public static org.apache.avro.Schema getClassSchema() { return SCHEMA$; }
-
-  @Override
   public org.apache.avro.Schema getSchema() { return SCHEMA$; }
 }
