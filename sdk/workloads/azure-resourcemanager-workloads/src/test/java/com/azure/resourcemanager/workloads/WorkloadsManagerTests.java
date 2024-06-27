f6b8a987fcbd3db55a2e2987b636ce7df426514b
hongli750210 <v-hongli1@microsoft.com>
2024-06-27 16:54:53 +0800

    mgmt, update test on workloads

--- sdk/workloads/azure-resourcemanager-workloads/src/test/java/com/azure/resourcemanager/workloads/WorkloadsManagerTests.java
+++ sdk/workloads/azure-resourcemanager-workloads/src/test/java/com/azure/resourcemanager/workloads/WorkloadsManagerTests.java
@@ -10,10 +10,10 @@ import com.azure.core.management.AzureEnvironment;
 import com.azure.core.management.Region;
 import com.azure.core.management.profile.AzureProfile;
 import com.azure.core.test.TestBase;
-import com.azure.core.test.annotation.DoNotRecord;
+import com.azure.core.test.annotation.LiveOnly;
 import com.azure.core.util.Configuration;
 import com.azure.core.util.CoreUtils;
-import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.identity.AzurePowerShellCredentialBuilder;
 import com.azure.resourcemanager.loganalytics.LogAnalyticsManager;
 import com.azure.resourcemanager.loganalytics.models.PublicNetworkAccessType;
 import com.azure.resourcemanager.loganalytics.models.WorkspaceCapping;
@@ -44,7 +44,7 @@ public class WorkloadsManagerTests extends TestBase {
 
     @Override
     public void beforeTest() {
-        final TokenCredential credential = new DefaultAzureCredentialBuilder().build();
+        final TokenCredential credential = new AzurePowerShellCredentialBuilder().build();
         final AzureProfile profile = new AzureProfile(AzureEnvironment.AZURE);
 
         logAnalyticsManager = LogAnalyticsManager
@@ -89,7 +89,7 @@ public class WorkloadsManagerTests extends TestBase {
     }
 
     @Test
-    @DoNotRecord(skipInPlayback = true)
+    @LiveOnly
     public void testCreateMonitor() {
         Monitor monitor = null;
         String randomPadding = randomPadding();
