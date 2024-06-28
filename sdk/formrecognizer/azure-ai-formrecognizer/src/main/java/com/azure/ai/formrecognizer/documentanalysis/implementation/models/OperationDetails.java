77296f53cbfc84e5f1a86eef7fed3af477b8d458
Sameeksha Vaity (from Dev Box) <savaity@microsoft.com>
2024-06-28 14:09:08 -0700

    regen with 4.1.27

--- sdk/formrecognizer/azure-ai-formrecognizer/src/main/java/com/azure/ai/formrecognizer/documentanalysis/implementation/models/OperationDetails.java
+++ sdk/formrecognizer/azure-ai-formrecognizer/src/main/java/com/azure/ai/formrecognizer/documentanalysis/implementation/models/OperationDetails.java
@@ -239,30 +239,30 @@ public class OperationDetails implements JsonSerializable<OperationDetails> {
     public static OperationDetails fromJson(JsonReader jsonReader) throws IOException {
         return jsonReader.readObject(reader -> {
             String discriminatorValue = null;
-            JsonReader readerToUse = reader.bufferObject();
-
-            readerToUse.nextToken(); // Prepare for reading
-            while (readerToUse.nextToken() != JsonToken.END_OBJECT) {
-                String fieldName = readerToUse.getFieldName();
-                readerToUse.nextToken();
-                if ("kind".equals(fieldName)) {
-                    discriminatorValue = readerToUse.getString();
-                    break;
-                } else {
-                    readerToUse.skipChildren();
+            try (JsonReader readerToUse = reader.bufferObject()) {
+                readerToUse.nextToken(); // Prepare for reading
+                while (readerToUse.nextToken() != JsonToken.END_OBJECT) {
+                    String fieldName = readerToUse.getFieldName();
+                    readerToUse.nextToken();
+                    if ("kind".equals(fieldName)) {
+                        discriminatorValue = readerToUse.getString();
+                        break;
+                    } else {
+                        readerToUse.skipChildren();
+                    }
                 }
-            }
-            // Use the discriminator value to determine which subtype should be deserialized.
-            if ("documentModelBuild".equals(discriminatorValue)) {
-                return DocumentModelBuildOperationDetails.fromJson(readerToUse.reset());
-            } else if ("documentModelCompose".equals(discriminatorValue)) {
-                return DocumentModelComposeOperationDetails.fromJson(readerToUse.reset());
-            } else if ("documentModelCopyTo".equals(discriminatorValue)) {
-                return DocumentModelCopyToOperationDetails.fromJson(readerToUse.reset());
-            } else if ("documentClassifierBuild".equals(discriminatorValue)) {
-                return DocumentClassifierBuildOperationDetails.fromJson(readerToUse.reset());
-            } else {
-                return fromJsonKnownDiscriminator(readerToUse.reset());
+                // Use the discriminator value to determine which subtype should be deserialized.
+                if ("documentModelBuild".equals(discriminatorValue)) {
+                    return DocumentModelBuildOperationDetails.fromJson(readerToUse.reset());
+                } else if ("documentModelCompose".equals(discriminatorValue)) {
+                    return DocumentModelComposeOperationDetails.fromJson(readerToUse.reset());
+                } else if ("documentModelCopyTo".equals(discriminatorValue)) {
+                    return DocumentModelCopyToOperationDetails.fromJson(readerToUse.reset());
+                } else if ("documentClassifierBuild".equals(discriminatorValue)) {
+                    return DocumentClassifierBuildOperationDetails.fromJson(readerToUse.reset());
+                } else {
+                    return fromJsonKnownDiscriminator(readerToUse.reset());
+                }
             }
         });
     }
