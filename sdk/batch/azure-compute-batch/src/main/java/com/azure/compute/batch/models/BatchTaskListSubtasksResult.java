577c058243659e74b544012637941133a18ef3fc
Connie Yau <conniey@microsoft.com>
2024-07-01 12:50:21 -0700

    Fix BatchTaskListSubtasksResult

--- sdk/batch/azure-compute-batch/src/main/java/com/azure/compute/batch/models/BatchTaskListSubtasksResult.java
+++ sdk/batch/azure-compute-batch/src/main/java/com/azure/compute/batch/models/BatchTaskListSubtasksResult.java
@@ -5,18 +5,22 @@ package com.azure.compute.batch.models;
 
 import com.azure.core.annotation.Generated;
 import com.azure.core.annotation.Immutable;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.azure.json.JsonReader;
+import com.azure.json.JsonSerializable;
+import com.azure.json.JsonToken;
+import com.azure.json.JsonWriter;
+
+import java.io.IOException;
 import java.util.List;
 
 /** The result of listing the subtasks of a Task. */
 @Immutable
-public final class BatchTaskListSubtasksResult {
+public final class BatchTaskListSubtasksResult implements JsonSerializable<BatchTaskListSubtasksResult> {
 
     /*
      * The list of subtasks.
      */
     @Generated
-    @JsonProperty(value = "value")
     private List<BatchSubtask> value;
 
     /** Creates an instance of BatchTaskListSubtasksResult class. */
@@ -31,5 +35,32 @@ public final class BatchTaskListSubtasksResult {
     @Generated
     public List<BatchSubtask> getValue() {
         return this.value;
+    }
+
+    @Override
+    public JsonWriter toJson(JsonWriter jsonWriter) throws IOException {
+        jsonWriter.writeStartObject();
+        jsonWriter.writeArrayField("value", this.value, (writer, element) -> element.toJson(writer));
+        jsonWriter.writeEndObject();
+        return jsonWriter;
+    }
+
+
+    public static BatchTaskListSubtasksResult fromJson(JsonReader jsonReader) throws IOException {
+        return jsonReader.readObject(reader -> {
+            BatchTaskListSubtasksResult result = new BatchTaskListSubtasksResult();
+            while (reader.nextToken() != JsonToken.END_OBJECT) {
+                String fieldName = reader.getFieldName();
+                reader.nextToken();
+
+                if ("value".equals(fieldName)) {
+                    result.value = reader.readArray(r -> BatchSubtask.fromJson(r));
+                } else {
+                    reader.skipChildren();
+                }
+            }
+
+            return result;
+        });
     }
 }
