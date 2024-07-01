440c0a642670dfcb4a1432ad22d0cb660741b8c5
azure-sdk <azuresdk@microsoft.com>
2024-07-01 10:22:46 -0700

    Reset sources for azure-communication-sms to the release version 1.1.24.

--- sdk/communication/azure-communication-sms/src/main/java/com/azure/communication/sms/models/SmsSendOptions.java
+++ sdk/communication/azure-communication-sms/src/main/java/com/azure/communication/sms/models/SmsSendOptions.java
@@ -5,37 +5,29 @@
 package com.azure.communication.sms.models;
 
 import com.azure.core.annotation.Fluent;
-import com.azure.json.JsonReader;
-import com.azure.json.JsonSerializable;
-import com.azure.json.JsonToken;
-import com.azure.json.JsonWriter;
-import java.io.IOException;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-/**
- * Optional configuration for sending SMS messages.
- */
+/** The SmsSendOptions model. */
 @Fluent
-public final class SmsSendOptions implements JsonSerializable<SmsSendOptions> {
+public final class SmsSendOptions {
     /*
-     * Enable this flag to receive a delivery report for this message on the Azure Resource EventGrid.
+     * Enable this flag to receive a delivery report for this message on the
+     * Azure Resource EventGrid.
      */
+    @JsonProperty(value = "enableDeliveryReport", required = true)
     private boolean deliveryReportEnabled;
 
     /*
-     * Use this field to provide metadata that will then be sent back in the corresponding Delivery Report.
+     * Use this field to provide metadata that will then be sent back in the
+     * corresponding Delivery Report.
      */
+    @JsonProperty(value = "tag")
     private String tag;
-
-    /**
-     * Creates an instance of SmsSendOptions class.
-     */
-    public SmsSendOptions() {
-    }
 
     /**
      * Get the deliveryReportEnabled property: Enable this flag to receive a delivery report for this message on the
      * Azure Resource EventGrid.
-     * 
+     *
      * @return the deliveryReportEnabled value.
      */
     public boolean isDeliveryReportEnabled() {
@@ -45,7 +37,7 @@ public final class SmsSendOptions implements JsonSerializable<SmsSendOptions> {
     /**
      * Set the deliveryReportEnabled property: Enable this flag to receive a delivery report for this message on the
      * Azure Resource EventGrid.
-     * 
+     *
      * @param deliveryReportEnabled the deliveryReportEnabled value to set.
      * @return the SmsSendOptions object itself.
      */
@@ -57,7 +49,7 @@ public final class SmsSendOptions implements JsonSerializable<SmsSendOptions> {
     /**
      * Get the tag property: Use this field to provide metadata that will then be sent back in the corresponding
      * Delivery Report.
-     * 
+     *
      * @return the tag value.
      */
     public String getTag() {
@@ -67,52 +59,12 @@ public final class SmsSendOptions implements JsonSerializable<SmsSendOptions> {
     /**
      * Set the tag property: Use this field to provide metadata that will then be sent back in the corresponding
      * Delivery Report.
-     * 
+     *
      * @param tag the tag value to set.
      * @return the SmsSendOptions object itself.
      */
     public SmsSendOptions setTag(String tag) {
         this.tag = tag;
         return this;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public JsonWriter toJson(JsonWriter jsonWriter) throws IOException {
-        jsonWriter.writeStartObject();
-        jsonWriter.writeBooleanField("enableDeliveryReport", this.deliveryReportEnabled);
-        jsonWriter.writeStringField("tag", this.tag);
-        return jsonWriter.writeEndObject();
-    }
-
-    /**
-     * Reads an instance of SmsSendOptions from the JsonReader.
-     * 
-     * @param jsonReader The JsonReader being read.
-     * @return An instance of SmsSendOptions if the JsonReader was pointing to an instance of it, or null if it was
-     * pointing to JSON null.
-     * @throws IllegalStateException If the deserialized JSON object was missing any required properties.
-     * @throws IOException If an error occurs while reading the SmsSendOptions.
-     */
-    public static SmsSendOptions fromJson(JsonReader jsonReader) throws IOException {
-        return jsonReader.readObject(reader -> {
-            SmsSendOptions deserializedSmsSendOptions = new SmsSendOptions();
-            while (reader.nextToken() != JsonToken.END_OBJECT) {
-                String fieldName = reader.getFieldName();
-                reader.nextToken();
-
-                if ("enableDeliveryReport".equals(fieldName)) {
-                    deserializedSmsSendOptions.deliveryReportEnabled = reader.getBoolean();
-                } else if ("tag".equals(fieldName)) {
-                    deserializedSmsSendOptions.tag = reader.getString();
-                } else {
-                    reader.skipChildren();
-                }
-            }
-
-            return deserializedSmsSendOptions;
-        });
     }
 }
