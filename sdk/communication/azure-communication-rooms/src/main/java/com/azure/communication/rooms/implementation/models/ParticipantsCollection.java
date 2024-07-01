c6fd84cc957c58d79f062aeb2b246c0dcfef9970
Sameeksha Vaity (from Dev Box) <savaity@microsoft.com>
2024-07-01 16:12:12 -0700

    azure-json migration

--- sdk/communication/azure-communication-rooms/src/main/java/com/azure/communication/rooms/implementation/models/ParticipantsCollection.java
+++ sdk/communication/azure-communication-rooms/src/main/java/com/azure/communication/rooms/implementation/models/ParticipantsCollection.java
@@ -5,30 +5,37 @@
 package com.azure.communication.rooms.implementation.models;
 
 import com.azure.core.annotation.Fluent;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.azure.json.JsonReader;
+import com.azure.json.JsonSerializable;
+import com.azure.json.JsonToken;
+import com.azure.json.JsonWriter;
+import java.io.IOException;
 import java.util.List;
 
-/** A collection of participants in a room. */
+/**
+ * A collection of participants in a room.
+ */
 @Fluent
-public final class ParticipantsCollection {
+public final class ParticipantsCollection implements JsonSerializable<ParticipantsCollection> {
     /*
      * A collection of participants.
      */
-    @JsonProperty(value = "value", required = true)
     private List<RoomParticipant> value;
 
     /*
      * If there are more participants that can be retrieved, the next link will be populated.
      */
-    @JsonProperty(value = "nextLink")
     private String nextLink;
 
-    /** Creates an instance of ParticipantsCollection class. */
-    public ParticipantsCollection() {}
+    /**
+     * Creates an instance of ParticipantsCollection class.
+     */
+    public ParticipantsCollection() {
+    }
 
     /**
      * Get the value property: A collection of participants.
-     *
+     * 
      * @return the value value.
      */
     public List<RoomParticipant> getValue() {
@@ -37,7 +44,7 @@ public final class ParticipantsCollection {
 
     /**
      * Set the value property: A collection of participants.
-     *
+     * 
      * @param value the value value to set.
      * @return the ParticipantsCollection object itself.
      */
@@ -47,8 +54,9 @@ public final class ParticipantsCollection {
     }
 
     /**
-     * Get the nextLink property: If there are more participants that can be retrieved, the next link will be populated.
-     *
+     * Get the nextLink property: If there are more participants that can be retrieved, the next link will be
+     * populated.
+     * 
      * @return the nextLink value.
      */
     public String getNextLink() {
@@ -56,13 +64,52 @@ public final class ParticipantsCollection {
     }
 
     /**
-     * Set the nextLink property: If there are more participants that can be retrieved, the next link will be populated.
-     *
+     * Set the nextLink property: If there are more participants that can be retrieved, the next link will be
+     * populated.
+     * 
      * @param nextLink the nextLink value to set.
      * @return the ParticipantsCollection object itself.
      */
     public ParticipantsCollection setNextLink(String nextLink) {
         this.nextLink = nextLink;
         return this;
+    }
+
+    @Override
+    public JsonWriter toJson(JsonWriter jsonWriter) throws IOException {
+        jsonWriter.writeStartObject();
+        jsonWriter.writeArrayField("value", this.value, (writer, element) -> writer.writeJson(element));
+        jsonWriter.writeStringField("nextLink", this.nextLink);
+        return jsonWriter.writeEndObject();
+    }
+
+    /**
+     * Reads an instance of ParticipantsCollection from the JsonReader.
+     * 
+     * @param jsonReader The JsonReader being read.
+     * @return An instance of ParticipantsCollection if the JsonReader was pointing to an instance of it, or null if it
+     * was pointing to JSON null.
+     * @throws IllegalStateException If the deserialized JSON object was missing any required properties.
+     * @throws IOException If an error occurs while reading the ParticipantsCollection.
+     */
+    public static ParticipantsCollection fromJson(JsonReader jsonReader) throws IOException {
+        return jsonReader.readObject(reader -> {
+            ParticipantsCollection deserializedParticipantsCollection = new ParticipantsCollection();
+            while (reader.nextToken() != JsonToken.END_OBJECT) {
+                String fieldName = reader.getFieldName();
+                reader.nextToken();
+
+                if ("value".equals(fieldName)) {
+                    List<RoomParticipant> value = reader.readArray(reader1 -> RoomParticipant.fromJson(reader1));
+                    deserializedParticipantsCollection.value = value;
+                } else if ("nextLink".equals(fieldName)) {
+                    deserializedParticipantsCollection.nextLink = reader.getString();
+                } else {
+                    reader.skipChildren();
+                }
+            }
+
+            return deserializedParticipantsCollection;
+        });
     }
 }
--- sdk/communication/azure-communication-rooms/src/main/java/module-info.java
+++ sdk/communication/azure-communication-rooms/src/main/java/module-info.java
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 module com.azure.communication.rooms {
+    requires com.azure.json;
     requires transitive com.azure.communication.common;
 
     // public API surface area
@@ -9,10 +10,10 @@ module com.azure.communication.rooms {
     exports com.azure.communication.rooms.models;
 
     opens com.azure.communication.rooms
-        to com.fasterxml.jackson.databind, com.azure.core;
+        to com.azure.core;
     opens com.azure.communication.rooms.models
-        to com.fasterxml.jackson.databind, com.azure.core;
+        to com.azure.core;
     opens com.azure.communication.rooms.implementation.models
-        to com.fasterxml.jackson.databind, com.azure.core;
+        to com.azure.core;
 }
 
