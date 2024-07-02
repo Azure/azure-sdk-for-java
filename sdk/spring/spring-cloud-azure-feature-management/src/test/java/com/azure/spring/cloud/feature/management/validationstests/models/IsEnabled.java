6c80093c6c1280feec7d95f5c5a91689c48753e3
Yu Wei <yuwe@microsoft.com>
2024-07-02 13:24:21 +0800

    update the schema of test case

--- sdk/spring/spring-cloud-azure-feature-management/src/test/java/com/azure/spring/cloud/feature/management/validationstests/models/IsEnabled.java
+++ sdk/spring/spring-cloud-azure-feature-management/src/test/java/com/azure/spring/cloud/feature/management/validationstests/models/IsEnabled.java
@@ -6,18 +6,30 @@ public class IsEnabled {
     private String result;
     private String exception;
 
+    /**
+     * @return result
+     * */
     public String getResult() {
         return result;
     }
 
-    public String getException() {
-        return exception;
-    }
-
+    /**
+     * @param result the result of validation test case
+     * */
     public void setResult(String result) {
         this.result = result;
     }
 
+    /**
+     * @return exception
+     * */
+    public String getException() {
+        return exception;
+    }
+
+    /**
+     * @param exception the exception message throws when run test case
+     * */
     public void setException(String exception) {
         this.exception = exception;
     }
--- sdk/spring/spring-cloud-azure-feature-management/src/test/java/com/azure/spring/cloud/feature/management/validationstests/models/Variant.java
+++ sdk/spring/spring-cloud-azure-feature-management/src/test/java/com/azure/spring/cloud/feature/management/validationstests/models/Variant.java
@@ -6,18 +6,30 @@ public class Variant {
     private String result;
     private String exception;
 
+    /**
+     * @return result
+     * */
     public String getResult() {
         return result;
     }
 
+    /**
+     * @param result the result of variant feature flag
+     * */
     public void setResult(String result) {
         this.result = result;
     }
 
+    /**
+     * @return exception
+     * */
     public String getException() {
         return exception;
     }
 
+    /**
+     * @param exception the exception message throws when run variant test case
+     * */
     public void setException(String exception) {
         this.exception = exception;
     }
