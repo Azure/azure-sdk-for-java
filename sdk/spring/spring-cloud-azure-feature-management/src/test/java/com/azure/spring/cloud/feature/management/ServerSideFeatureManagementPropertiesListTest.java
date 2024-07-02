b06515b37d9714dc7629f5890b746bcd2e7d03ce
Yu Wei <yuwe@microsoft.com>
2024-07-02 14:32:05 +0800

    code style

--- sdk/spring/spring-cloud-azure-feature-management/src/test/java/com/azure/spring/cloud/feature/management/ServerSideFeatureManagementPropertiesListTest.java
+++ sdk/spring/spring-cloud-azure-feature-management/src/test/java/com/azure/spring/cloud/feature/management/ServerSideFeatureManagementPropertiesListTest.java
@@ -5,7 +5,6 @@ package com.azure.spring.cloud.feature.management;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
