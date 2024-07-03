a41d2ac198f54bd674cadb67fbdb2aa32b00fb61
Shawn Fang <shafang@microsoft.com>
2024-07-03 14:55:48 -0700

    revert `TextAnalyticsErrorCode implements Serializable` back

--- sdk/textanalytics/azure-ai-textanalytics/src/main/java/com/azure/ai/textanalytics/models/TextAnalyticsErrorCode.java
+++ sdk/textanalytics/azure-ai-textanalytics/src/main/java/com/azure/ai/textanalytics/models/TextAnalyticsErrorCode.java
@@ -6,13 +6,15 @@ package com.azure.ai.textanalytics.models;
 import com.azure.core.annotation.Immutable;
 import com.azure.core.util.ExpandableStringEnum;
 
+import java.io.Serializable;
 import java.util.Collection;
 
 /**
  * Defines values for TextAnalyticsErrorCode.
  */
 @Immutable
-public final class TextAnalyticsErrorCode extends ExpandableStringEnum<TextAnalyticsErrorCode> {
+public final class TextAnalyticsErrorCode extends ExpandableStringEnum<TextAnalyticsErrorCode> implements Serializable {
+    private static final long serialVersionUID = 21436310107606058L;
     /**
      * Enum value InvalidRequest.
      */
