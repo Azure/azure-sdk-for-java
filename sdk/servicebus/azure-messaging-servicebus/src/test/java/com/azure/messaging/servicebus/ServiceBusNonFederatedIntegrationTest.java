6de2456c5c34cd77ba0419553cb9f8cd694fe79b
anuchandy <anuamd@hotmail.com>
2024-07-08 16:30:22 -0700

    use assumeTrue not assertTrue

--- sdk/servicebus/azure-messaging-servicebus/src/test/java/com/azure/messaging/servicebus/ServiceBusNonFederatedIntegrationTest.java
+++ sdk/servicebus/azure-messaging-servicebus/src/test/java/com/azure/messaging/servicebus/ServiceBusNonFederatedIntegrationTest.java
@@ -33,13 +33,12 @@ import static org.junit.jupiter.api.Assumptions.assumeTrue;
 @Tag("integration")
 @Execution(ExecutionMode.SAME_THREAD)
 public class ServiceBusNonFederatedIntegrationTest extends IntegrationTestBase {
-    private static final ClientLogger LOGGER = new ClientLogger(ServiceBusNonFederatedIntegrationTest.class);
     private static final Duration TIMEOUT = Duration.ofSeconds(20);
     private static final Duration DEFAULT_TIMEOUT = Duration.ofSeconds(10);
     private static final String TEST_MESSAGE = "SSLorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vehicula posuere lobortis. Aliquam finibus volutpat dolor, faucibus pellentesque ipsum bibendum vitae. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut sit amet urna hendrerit, dapibus justo a, sodales justo. Mauris finibus augue id pulvinar congue. Nam maximus luctus ipsum, at commodo ligula euismod ac. Phasellus vitae lacus sit amet diam porta placerat. \nUt sodales efficitur sapien ut posuere. Morbi sed tellus est. Proin eu erat purus. Proin massa nunc, condimentum id iaculis dignissim, consectetur et odio. Cras suscipit sem eu libero aliquam tincidunt. Nullam ut arcu suscipit, eleifend velit in, cursus libero. Ut eleifend facilisis odio sit amet feugiat. Phasellus at nunc sit amet elit sagittis commodo ac in nisi. Fusce vitae aliquam quam. Integer vel nibh euismod, tempus elit vitae, pharetra est. Duis vulputate enim a elementum dignissim. Morbi dictum enim id elit scelerisque, in elementum nulla pharetra. \nAenean aliquet aliquet condimentum. Proin dapibus dui id libero tempus feugiat. Sed commodo ligula a lectus mattis, vitae tincidunt velit auctor. Fusce quis semper dui. Phasellus eu efficitur sem. Ut non sem sit amet enim condimentum venenatis id dictum massa. Nullam sagittis lacus a neque sodales, et ultrices arcu mattis. Aliquam erat volutpat. \nAenean fringilla quam elit, id mattis purus vestibulum nec. Praesent porta eros in dapibus molestie. Vestibulum orci libero, tincidunt et turpis eget, condimentum lobortis enim. Fusce suscipit ante et mauris consequat cursus nec laoreet lorem. Maecenas in sollicitudin diam, non tincidunt purus. Nunc mauris purus, laoreet eget interdum vitae, placerat a sapien. In mi risus, blandit eu facilisis nec, molestie suscipit leo. Pellentesque molestie urna vitae dui faucibus bibendum. \nDonec quis ipsum ultricies, imperdiet ex vel, scelerisque eros. Ut at urna arcu. Vestibulum rutrum odio dolor, vitae cursus nunc pulvinar vel. Donec accumsan sapien in malesuada tempor. Maecenas in condimentum eros. Sed vestibulum facilisis massa a iaculis. Etiam et nibh felis. Donec maximus, sem quis vestibulum gravida, turpis risus congue dolor, pharetra tincidunt lectus nisi at velit.";
 
-    public ServiceBusNonFederatedIntegrationTest() {
-        super(LOGGER);
+    ServiceBusNonFederatedIntegrationTest() {
+        super(new ClientLogger(ServiceBusNonFederatedIntegrationTest.class));
     }
 
     @Test
@@ -119,7 +118,7 @@ public class ServiceBusNonFederatedIntegrationTest extends IntegrationTestBase {
         final TestMode mode = super.getTestMode();
         if (mode == TestMode.PLAYBACK) {
             // AMQP traffic never gets recorded so there is no PLAYBACK supported.
-            assertTrue(false, "Skipping integration tests in playback mode.");
+            assumeTrue(false, "Skipping integration tests in playback mode.");
             return;
         }
 
@@ -130,6 +129,6 @@ public class ServiceBusNonFederatedIntegrationTest extends IntegrationTestBase {
                 return;
             }
         }
-        assertTrue(false, "Not running integration in record mode (missing authentication set up).");
+        assumeTrue(false, "Not running integration in record mode (missing authentication set up).");
     }
 }
