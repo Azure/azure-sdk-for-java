e6f1ad9e9159a206abfb0baf4f556b403037ef28
Victor Colin Amador <vicolina@microsoft.com>
2024-07-08 00:19:30 -0700

    Trying to fix test resource deployment issue.

--- sdk/digitaltwins/azure-digitaltwins-core/src/test/resources/prerequisite/setup.ps1
+++ sdk/digitaltwins/azure-digitaltwins-core/src/test/resources/prerequisite/setup.ps1
@@ -104,15 +104,17 @@ az deployment group create --resource-group $ResourceGroup --name $($DigitalTwin
 # Even though the output variable names are all capital letters in the script, ARM turns them into a strange casing
 # and we have to use that casing in order to get them from the deployment outputs.
 $dtHostName = az deployment group show -g $ResourceGroup -n $($DigitalTwinName.ToLower()) --query 'properties.outputs.digitaltwinS_URL.value' --output tsv
-
 $outputfileDir = (Get-Item -Path $PSScriptRoot).Parent.Parent.Parent.Parent.Parent.Fullname
 $outputFile = Join-Path -Path $outputfileDir -ChildPath "test-resources.json.env"
+$tenantId = "72f988bf-86f1-41af-91ab-2d7cd011db47"
 
 Add-Type -AssemblyName System.Security
 
 $environmentText = @"
 {
-    "DIGITALTWINS_URL": "$dtHostName"
+    "DIGITALTWINS_URL": "$dtHostName",
+    "DIGITALTWINS_CLIENT_ID": "$appId",
+    "DIGITALTWINS_TENANT_ID":  "$tenantId"
 }
 "@
 
