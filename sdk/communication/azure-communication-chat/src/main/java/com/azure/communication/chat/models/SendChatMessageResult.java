667817fb95f881d13d0fcfd7c48c4d77fd4781ce
Anu Thomas Chandy <anuamd@hotmail.com>
2024-07-10 08:25:55 -0700

    migrate azure-communication-chat to azure-json (#40936)
    * Convert the handwritten models to azure-json
    
    * regenerate the protocol layer for azure-json
    
    * Adjust ChatAsyncClient to use newly generated service client
    
    * remove jackson annotations from the hand written models
    
    * migrate chat tests to use azure-json
    
    * refresh protocol layer with autorest-4.1.33
    
    * update revapi.json for azure json migration

--- sdk/communication/azure-communication-chat/src/main/java/com/azure/communication/chat/models/SendChatMessageResult.java
+++ sdk/communication/azure-communication-chat/src/main/java/com/azure/communication/chat/models/SendChatMessageResult.java
@@ -5,17 +5,20 @@
 package com.azure.communication.chat.models;
 
 import com.azure.core.annotation.Fluent;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.azure.json.JsonReader;
+import com.azure.json.JsonSerializable;
+import com.azure.json.JsonToken;
+import com.azure.json.JsonWriter;
+import java.io.IOException;
 
 /**
  * Result of the send message operation.
  */
 @Fluent
-public final class SendChatMessageResult {
+public final class SendChatMessageResult implements JsonSerializable<SendChatMessageResult> {
     /*
      * A server-generated message id.
      */
-    @JsonProperty(value = "id", required = true)
     private String id;
 
     /**
@@ -42,5 +45,42 @@ public final class SendChatMessageResult {
     public SendChatMessageResult setId(String id) {
         this.id = id;
         return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public JsonWriter toJson(JsonWriter jsonWriter) throws IOException {
+        jsonWriter.writeStartObject();
+        jsonWriter.writeStringField("id", this.id);
+        return jsonWriter.writeEndObject();
+    }
+
+    /**
+     * Reads an instance of SendChatMessageResult from the JsonReader.
+     * 
+     * @param jsonReader The JsonReader being read.
+     * @return An instance of SendChatMessageResult if the JsonReader was pointing to an instance of it, or null if it
+     * was pointing to JSON null.
+     * @throws IllegalStateException If the deserialized JSON object was missing any required properties.
+     * @throws IOException If an error occurs while reading the SendChatMessageResult.
+     */
+    public static SendChatMessageResult fromJson(JsonReader jsonReader) throws IOException {
+        return jsonReader.readObject(reader -> {
+            SendChatMessageResult deserializedSendChatMessageResult = new SendChatMessageResult();
+            while (reader.nextToken() != JsonToken.END_OBJECT) {
+                String fieldName = reader.getFieldName();
+                reader.nextToken();
+
+                if ("id".equals(fieldName)) {
+                    deserializedSendChatMessageResult.id = reader.getString();
+                } else {
+                    reader.skipChildren();
+                }
+            }
+
+            return deserializedSendChatMessageResult;
+        });
     }
 }
