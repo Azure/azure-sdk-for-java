a9d97fc4642f4f0c396791d9c9d4171242fdd00c
Moary Chen <v-shilichen@microsoft.com>
2024-07-10 14:51:19 +0800

    Remove unused import

--- sdk/spring/spring-cloud-azure-autoconfigure/src/main/java/com/azure/spring/cloud/autoconfigure/implementation/keyvault/AzureKeyVaultConfiguration.java
+++ sdk/spring/spring-cloud-azure-autoconfigure/src/main/java/com/azure/spring/cloud/autoconfigure/implementation/keyvault/AzureKeyVaultConfiguration.java
@@ -6,7 +6,6 @@ package com.azure.spring.cloud.autoconfigure.implementation.keyvault;
 import com.azure.spring.cloud.autoconfigure.implementation.AzureServiceConfigurationBase;
 import com.azure.spring.cloud.autoconfigure.implementation.context.properties.AzureGlobalProperties;
 import com.azure.spring.cloud.autoconfigure.implementation.keyvault.common.AzureKeyVaultProperties;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
