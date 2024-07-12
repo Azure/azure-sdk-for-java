c435ac68ce9f8d2f817813cdb17c2fb698c2dcda
Anu Thomas Chandy <anuamd@hotmail.com>
2024-07-12 09:51:41 -0700

    migrate azure-mixedreality-remoterendering to azure-json  (#41122)
    * nit; remove unnecessary comma in test-resource.json
    
    * update autorest version to use
    
    * refresh the auto-gen protocol layer to use az json
    
    * adjust the test
    
    * adjust module-info for azure json
    
    * update assets.json post the recording
    
    * update the playback dummy account id
    
    * Address existing java-doc warnings

--- sdk/remoterendering/azure-mixedreality-remoterendering/src/main/java/com/azure/mixedreality/remoterendering/implementation/models/CreateConversionSettings.java
+++ sdk/remoterendering/azure-mixedreality-remoterendering/src/main/java/com/azure/mixedreality/remoterendering/implementation/models/CreateConversionSettings.java
@@ -4,37 +4,80 @@
 
 package com.azure.mixedreality.remoterendering.implementation.models;
 
-import com.azure.core.annotation.Fluent;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.azure.core.annotation.Immutable;
+import com.azure.json.JsonReader;
+import com.azure.json.JsonSerializable;
+import com.azure.json.JsonToken;
+import com.azure.json.JsonWriter;
+import java.io.IOException;
 
-/** Request to start a conversion. */
-@Fluent
-public final class CreateConversionSettings {
+/**
+ * Request to start a conversion.
+ */
+@Immutable
+public final class CreateConversionSettings implements JsonSerializable<CreateConversionSettings> {
     /*
-     * Conversion settings describe the origin of input files and destination
-     * of output files.
+     * Conversion settings describe the origin of input files and destination of output files.
      */
-    @JsonProperty(value = "settings", required = true)
-    private ConversionSettings settings;
+    private final ConversionSettings settings;
 
     /**
      * Creates an instance of CreateConversionSettings class.
-     *
+     * 
      * @param settings the settings value to set.
      */
-    @JsonCreator
-    public CreateConversionSettings(@JsonProperty(value = "settings", required = true) ConversionSettings settings) {
+    public CreateConversionSettings(ConversionSettings settings) {
         this.settings = settings;
     }
 
     /**
      * Get the settings property: Conversion settings describe the origin of input files and destination of output
      * files.
-     *
+     * 
      * @return the settings value.
      */
     public ConversionSettings getSettings() {
         return this.settings;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public JsonWriter toJson(JsonWriter jsonWriter) throws IOException {
+        jsonWriter.writeStartObject();
+        jsonWriter.writeJsonField("settings", this.settings);
+        return jsonWriter.writeEndObject();
+    }
+
+    /**
+     * Reads an instance of CreateConversionSettings from the JsonReader.
+     * 
+     * @param jsonReader The JsonReader being read.
+     * @return An instance of CreateConversionSettings if the JsonReader was pointing to an instance of it, or null if
+     * it was pointing to JSON null.
+     * @throws IllegalStateException If the deserialized JSON object was missing any required properties.
+     * @throws IOException If an error occurs while reading the CreateConversionSettings.
+     */
+    public static CreateConversionSettings fromJson(JsonReader jsonReader) throws IOException {
+        return jsonReader.readObject(reader -> {
+            boolean settingsFound = false;
+            ConversionSettings settings = null;
+            while (reader.nextToken() != JsonToken.END_OBJECT) {
+                String fieldName = reader.getFieldName();
+                reader.nextToken();
+
+                if ("settings".equals(fieldName)) {
+                    settings = ConversionSettings.fromJson(reader);
+                    settingsFound = true;
+                } else {
+                    reader.skipChildren();
+                }
+            }
+            if (settingsFound) {
+                return new CreateConversionSettings(settings);
+            }
+            throw new IllegalStateException("Missing required property: settings");
+        });
     }
 }
