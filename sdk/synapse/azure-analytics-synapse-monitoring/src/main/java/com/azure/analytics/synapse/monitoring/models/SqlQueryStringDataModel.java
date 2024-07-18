f2faf42a252fdcc66e1ad7b5741dfb7b3c8acedf
Alan Zimmer <48699787+alzimmermsft@users.noreply.github.com>
2024-07-18 08:28:35 -0400

    Migrate Synapse Monitoring to stream-style serialization (#41186)
    Migrate Synapse Monitoring to stream-style serialization

--- sdk/synapse/azure-analytics-synapse-monitoring/src/main/java/com/azure/analytics/synapse/monitoring/models/SqlQueryStringDataModel.java
+++ sdk/synapse/azure-analytics-synapse-monitoring/src/main/java/com/azure/analytics/synapse/monitoring/models/SqlQueryStringDataModel.java
@@ -5,20 +5,31 @@
 package com.azure.analytics.synapse.monitoring.models;
 
 import com.azure.core.annotation.Fluent;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.azure.json.JsonReader;
+import com.azure.json.JsonSerializable;
+import com.azure.json.JsonToken;
+import com.azure.json.JsonWriter;
+import java.io.IOException;
 
-/** The SqlQueryStringDataModel model. */
+/**
+ * The SqlQueryStringDataModel model.
+ */
 @Fluent
-public final class SqlQueryStringDataModel {
+public final class SqlQueryStringDataModel implements JsonSerializable<SqlQueryStringDataModel> {
     /*
      * The query property.
      */
-    @JsonProperty(value = "query")
     private String query;
 
     /**
+     * Creates an instance of SqlQueryStringDataModel class.
+     */
+    public SqlQueryStringDataModel() {
+    }
+
+    /**
      * Get the query property: The query property.
-     *
+     * 
      * @return the query value.
      */
     public String getQuery() {
@@ -27,12 +38,48 @@ public final class SqlQueryStringDataModel {
 
     /**
      * Set the query property: The query property.
-     *
+     * 
      * @param query the query value to set.
      * @return the SqlQueryStringDataModel object itself.
      */
     public SqlQueryStringDataModel setQuery(String query) {
         this.query = query;
         return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public JsonWriter toJson(JsonWriter jsonWriter) throws IOException {
+        jsonWriter.writeStartObject();
+        jsonWriter.writeStringField("query", this.query);
+        return jsonWriter.writeEndObject();
+    }
+
+    /**
+     * Reads an instance of SqlQueryStringDataModel from the JsonReader.
+     * 
+     * @param jsonReader The JsonReader being read.
+     * @return An instance of SqlQueryStringDataModel if the JsonReader was pointing to an instance of it, or null if it
+     * was pointing to JSON null.
+     * @throws IOException If an error occurs while reading the SqlQueryStringDataModel.
+     */
+    public static SqlQueryStringDataModel fromJson(JsonReader jsonReader) throws IOException {
+        return jsonReader.readObject(reader -> {
+            SqlQueryStringDataModel deserializedSqlQueryStringDataModel = new SqlQueryStringDataModel();
+            while (reader.nextToken() != JsonToken.END_OBJECT) {
+                String fieldName = reader.getFieldName();
+                reader.nextToken();
+
+                if ("query".equals(fieldName)) {
+                    deserializedSqlQueryStringDataModel.query = reader.getString();
+                } else {
+                    reader.skipChildren();
+                }
+            }
+
+            return deserializedSqlQueryStringDataModel;
+        });
     }
 }
