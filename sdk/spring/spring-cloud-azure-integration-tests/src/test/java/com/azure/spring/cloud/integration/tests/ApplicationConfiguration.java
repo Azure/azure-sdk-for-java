ed68f98f80d1016e2f424554ae24c1c2d6d8f902
rujche <rujche@microsoft.com>
2024-07-22 13:45:05 +0800

    Add "subscribeOn" in AzurePipelinesCredential.

--- sdk/spring/spring-cloud-azure-integration-tests/src/test/java/com/azure/spring/cloud/integration/tests/ApplicationConfiguration.java
+++ sdk/spring/spring-cloud-azure-integration-tests/src/test/java/com/azure/spring/cloud/integration/tests/ApplicationConfiguration.java
@@ -6,6 +6,7 @@ import com.azure.core.credential.TokenCredential;
 import com.azure.core.util.Configuration;
 import com.azure.identity.AzureCliCredentialBuilder;
 import com.azure.identity.AzureDeveloperCliCredentialBuilder;
+import com.azure.identity.AzurePipelinesCredential;
 import com.azure.identity.AzurePipelinesCredentialBuilder;
 import com.azure.identity.AzurePowerShellCredentialBuilder;
 import com.azure.identity.ChainedTokenCredentialBuilder;
@@ -13,6 +14,7 @@ import com.azure.identity.EnvironmentCredentialBuilder;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import reactor.core.scheduler.Schedulers;
 
 import static com.azure.spring.cloud.autoconfigure.implementation.context.AzureContextUtils.DEFAULT_TOKEN_CREDENTIAL_BEAN_NAME;
 import static org.springframework.util.StringUtils.hasText;
@@ -34,7 +36,15 @@ public class ApplicationConfiguration {
             .addLast(new EnvironmentCredentialBuilder().build())
             .addLast(new AzureCliCredentialBuilder().build())
             .addLast(new AzureDeveloperCliCredentialBuilder().build());
+        TokenCredential createAzurePipelinesCredential = createAzurePipelinesCredential();
+        if (createAzurePipelinesCredential != null) {
+            builder.addLast(createAzurePipelinesCredential);
+        }
+        builder.addLast(new AzurePowerShellCredentialBuilder().build());
+        return builder.build();
+    }
 
+    private TokenCredential createAzurePipelinesCredential() {
         Configuration config = Configuration.getGlobalConfiguration();
         String serviceConnectionId = config.get("AZURESUBSCRIPTION_SERVICE_CONNECTION_ID");
         String clientId = config.get("AZURESUBSCRIPTION_CLIENT_ID");
@@ -44,17 +54,15 @@ public class ApplicationConfiguration {
             && hasText(clientId)
             && hasText(tenantId)
             && hasText(systemAccessToken)) {
-
-            builder.addLast(new AzurePipelinesCredentialBuilder()
+            AzurePipelinesCredential pipelinesCredential = new AzurePipelinesCredentialBuilder()
                 .systemAccessToken(systemAccessToken)
                 .clientId(clientId)
                 .tenantId(tenantId)
                 .serviceConnectionId(serviceConnectionId)
-                .build());
+                .build();
+            return request -> pipelinesCredential.getToken(request).subscribeOn(Schedulers.boundedElastic());
         }
-
-        builder.addLast(new AzurePowerShellCredentialBuilder().build());
-        return builder.build();
+        return null;
     }
 
 }
