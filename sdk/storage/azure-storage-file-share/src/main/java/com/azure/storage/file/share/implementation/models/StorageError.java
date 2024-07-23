6904980abf6db4303b4501f7c96c416597f2697a
Isabelle <ibrandes@microsoft.com>
2024-07-23 11:12:21 -0700

    adding storageerror copy blob parameters back as it doesn't effect api views

--- sdk/storage/azure-storage-file-share/src/main/java/com/azure/storage/file/share/implementation/models/StorageError.java
+++ sdk/storage/azure-storage-file-share/src/main/java/com/azure/storage/file/share/implementation/models/StorageError.java
@@ -24,6 +24,21 @@ public final class StorageError implements XmlSerializable<StorageError> {
     private String message;
 
     /*
+     * The CopySourceStatusCode property.
+     */
+    private Long copySourceStatusCode;
+
+    /*
+     * The CopySourceErrorCode property.
+     */
+    private String copySourceErrorCode;
+
+    /*
+     * The CopySourceErrorMessage property.
+     */
+    private String copySourceErrorMessage;
+
+    /*
      * The AuthenticationErrorDetail property.
      */
     private String authenticationErrorDetail;
@@ -36,7 +51,7 @@ public final class StorageError implements XmlSerializable<StorageError> {
 
     /**
      * Get the message property: The Message property.
-     * 
+     *
      * @return the message value.
      */
     public String getMessage() {
@@ -45,7 +60,7 @@ public final class StorageError implements XmlSerializable<StorageError> {
 
     /**
      * Set the message property: The Message property.
-     * 
+     *
      * @param message the message value to set.
      * @return the StorageError object itself.
      */
@@ -55,8 +70,68 @@ public final class StorageError implements XmlSerializable<StorageError> {
     }
 
     /**
+     * Get the copySourceStatusCode property: The CopySourceStatusCode property.
+     *
+     * @return the copySourceStatusCode value.
+     */
+    public Long getCopySourceStatusCode() {
+        return this.copySourceStatusCode;
+    }
+
+    /**
+     * Set the copySourceStatusCode property: The CopySourceStatusCode property.
+     *
+     * @param copySourceStatusCode the copySourceStatusCode value to set.
+     * @return the StorageError object itself.
+     */
+    public StorageError setCopySourceStatusCode(Long copySourceStatusCode) {
+        this.copySourceStatusCode = copySourceStatusCode;
+        return this;
+    }
+
+    /**
+     * Get the copySourceErrorCode property: The CopySourceErrorCode property.
+     *
+     * @return the copySourceErrorCode value.
+     */
+    public String getCopySourceErrorCode() {
+        return this.copySourceErrorCode;
+    }
+
+    /**
+     * Set the copySourceErrorCode property: The CopySourceErrorCode property.
+     *
+     * @param copySourceErrorCode the copySourceErrorCode value to set.
+     * @return the StorageError object itself.
+     */
+    public StorageError setCopySourceErrorCode(String copySourceErrorCode) {
+        this.copySourceErrorCode = copySourceErrorCode;
+        return this;
+    }
+
+    /**
+     * Get the copySourceErrorMessage property: The CopySourceErrorMessage property.
+     *
+     * @return the copySourceErrorMessage value.
+     */
+    public String getCopySourceErrorMessage() {
+        return this.copySourceErrorMessage;
+    }
+
+    /**
+     * Set the copySourceErrorMessage property: The CopySourceErrorMessage property.
+     *
+     * @param copySourceErrorMessage the copySourceErrorMessage value to set.
+     * @return the StorageError object itself.
+     */
+    public StorageError setCopySourceErrorMessage(String copySourceErrorMessage) {
+        this.copySourceErrorMessage = copySourceErrorMessage;
+        return this;
+    }
+
+    /**
      * Get the authenticationErrorDetail property: The AuthenticationErrorDetail property.
-     * 
+     *
      * @return the authenticationErrorDetail value.
      */
     public String getAuthenticationErrorDetail() {
@@ -65,7 +140,7 @@ public final class StorageError implements XmlSerializable<StorageError> {
 
     /**
      * Set the authenticationErrorDetail property: The AuthenticationErrorDetail property.
-     * 
+     *
      * @param authenticationErrorDetail the authenticationErrorDetail value to set.
      * @return the StorageError object itself.
      */
@@ -84,13 +159,16 @@ public final class StorageError implements XmlSerializable<StorageError> {
         rootElementName = CoreUtils.isNullOrEmpty(rootElementName) ? "StorageError" : rootElementName;
         xmlWriter.writeStartElement(rootElementName);
         xmlWriter.writeStringElement("Message", this.message);
+        xmlWriter.writeNumberElement("CopySourceStatusCode", this.copySourceStatusCode);
+        xmlWriter.writeStringElement("CopySourceErrorCode", this.copySourceErrorCode);
+        xmlWriter.writeStringElement("CopySourceErrorMessage", this.copySourceErrorMessage);
         xmlWriter.writeStringElement("AuthenticationErrorDetail", this.authenticationErrorDetail);
         return xmlWriter.writeEndElement();
     }
 
     /**
      * Reads an instance of StorageError from the XmlReader.
-     * 
+     *
      * @param xmlReader The XmlReader being read.
      * @return An instance of StorageError if the XmlReader was pointing to an instance of it, or null if it was
      * pointing to XML null.
@@ -119,6 +197,12 @@ public final class StorageError implements XmlSerializable<StorageError> {
 
                 if ("Message".equals(elementName.getLocalPart())) {
                     deserializedStorageError.message = reader.getStringElement();
+                } else if ("CopySourceStatusCode".equals(elementName.getLocalPart())) {
+                    deserializedStorageError.copySourceStatusCode = reader.getNullableElement(Long::parseLong);
+                } else if ("CopySourceErrorCode".equals(elementName.getLocalPart())) {
+                    deserializedStorageError.copySourceErrorCode = reader.getStringElement();
+                } else if ("CopySourceErrorMessage".equals(elementName.getLocalPart())) {
+                    deserializedStorageError.copySourceErrorMessage = reader.getStringElement();
                 } else if ("AuthenticationErrorDetail".equals(elementName.getLocalPart())) {
                     deserializedStorageError.authenticationErrorDetail = reader.getStringElement();
                 } else {
