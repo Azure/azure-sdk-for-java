57a937ed3db116bda5036638e3c299e24015fbb0
Moary Chen <v-shilichen@microsoft.com>
2024-07-23 16:40:31 +0800

    Revert

--- sdk/spring/scripts/compatibility_update_supported_version_matrix_json.py
+++ sdk/spring/scripts/compatibility_update_supported_version_matrix_json.py
@@ -21,6 +21,8 @@ from _constants import SPRING_BOOT_MAJOR_2_VERSION_NAME, SPRING_BOOT_MAJOR_3_VER
 
 def get_args():
     parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--target-version-prefix', type=str)
+    parser.add_argument('-n', '--non-target-version-prefix-list', type=str, nargs='*')
     parser.add_argument(
         '-sbmvn',
         '--spring-boot-major-version-number',
@@ -51,23 +53,40 @@ def update_supported_version_matrix_json_file(filepath, suppoerted_spring_boot_v
         json.dump(data, file, indent=2)
 
 
-def get_supported_spring_boot_versions():
+def get_supported_spring_boot_version(target_version_prefix, non_target_version_prefix_list):
     supported_version_list = []
     filepath = "https://raw.githubusercontent.com/Azure/azure-sdk-for-java/main/sdk/spring/pipeline/spring-cloud-azure-supported-spring.json"
     data = requests.get(filepath).json()
     for entry in data:
         for key in entry:
             if entry[key] == "SUPPORTED":
-                if entry["spring-boot-version"].startswith(get_args().spring_boot_major_version_number + "."):
+                if is_target_version(entry["spring-boot-version"], target_version_prefix,
+                                     non_target_version_prefix_list):
                     supported_version_list.append(entry["spring-boot-version"])
     return supported_version_list
+
+
+def is_target_version(spring_boot_version, target_version_prefix, non_target_version_prefix_list):
+    if not spring_boot_version.startswith(get_args().spring_boot_major_version_number + "."):
+        return False
+
+    if target_version_prefix is not None and not spring_boot_version.startswith(target_version_prefix):
+        return False
+
+    if non_target_version_prefix_list is not None:
+        for entry in non_target_version_prefix_list:
+            if spring_boot_version.startswith(entry):
+                return False
+    return True
 
 
 def main():
     start_time = time.time()
     change_to_repo_root_dir()
     log.debug('Current working directory = {}.'.format(os.getcwd()))
-    update_supported_version_matrix_json_file(get_args().matrix_config_path, get_supported_spring_boot_versions())
+    supported_spring_boot_version = get_supported_spring_boot_version(
+        get_args().target_version_prefix, get_args().non_target_version_prefix_list)
+    update_supported_version_matrix_json_file(get_args().matrix_config_path, supported_spring_boot_version)
     elapsed_time = time.time() - start_time
     log.info('elapsed_time = {}'.format(elapsed_time))
 
