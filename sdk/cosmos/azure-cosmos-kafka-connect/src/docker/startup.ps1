504a983303cfa99b7b812bac1a0853a6c76eb5d7
Tomas Varon <70857381+tvaron3@users.noreply.github.com>
2024-07-24 10:18:01 -0700

    revert ps script

--- sdk/cosmos/azure-cosmos-kafka-connect/src/docker/startup.ps1
+++ sdk/cosmos/azure-cosmos-kafka-connect/src/docker/startup.ps1
@@ -1,29 +1,20 @@
 #!/usr/bin/env pwsh
 $ErrorActionPreference='Stop'
 cd $PSScriptRoot
-Write-Host "Shutting down Docker Compose orchestration..."
-docker-compose down
 
 Write-Host "Deleting prior Cosmos DB connectors..."
-rm -rf "$PSScriptRoot/connectors"
-New-Item -Path "$PSScriptRoot" -ItemType "directory" -Name "connectors" -Force | Out-Null
-cd $PSScriptRoot/../..
+rm -rf "$PSScriptRoot/src/test/connectorPlugins/connectors"
+New-Item -Path "$PSScriptRoot/src/test/connectorPlugins" -ItemType "directory" -Name "connectors" -Force | Out-Null
 
 Write-Host "Rebuilding Cosmos DB connectors..."
 mvn clean package -DskipTests -Dmaven.javadoc.skip
-copy target\*-jar-with-dependencies.jar $PSScriptRoot/connectors
-cd $PSScriptRoot
+copy target\*-jar-with-dependencies.jar $PSScriptRoot/src/test/connectorPlugins/connectors
+cd $PSScriptRoot/src/test/connectorPlugins
 
 Write-Host "Adding custom Insert UUID SMT"
-cd $PSScriptRoot/connectors
+cd $PSScriptRoot/src/test/connectorPlugins/connectors
 git clone https://github.com/confluentinc/kafka-connect-insert-uuid.git insertuuid -q && cd insertuuid
 mvn clean package -DskipTests=true
-copy target\*.jar $PSScriptRoot/connectors
-rm -rf "$PSScriptRoot/connectors/insertuuid"
+copy target\*.jar $PSScriptRoot/src/test/connectorPlugins/connectors
+rm -rf "$PSScriptRoot/src/test/connectorPlugins/connectors/insertuuid"
 cd $PSScriptRoot
-
-Write-Host "Building Cosmos DB Kafka Connect Docker image"
-docker build . -t cosmosdb-kafka-connect:latest
-
-Write-Host "Starting Docker Compose..."
-docker-compose up -d
