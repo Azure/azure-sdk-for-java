201e2bd95a9fce8500229ed0c04f10000c1dcebb
Shawn Fang <shafang@microsoft.com>
2024-07-25 16:05:48 -0700

    typespec validation

--- sdk/purview/azure-analytics-purview-datamap/src/main/java/module-info.java
+++ sdk/purview/azure-analytics-purview-datamap/src/main/java/module-info.java
@@ -4,7 +4,6 @@
 
 module com.azure.analytics.purview.datamap {
     requires transitive com.azure.core;
-
     exports com.azure.analytics.purview.datamap;
     exports com.azure.analytics.purview.datamap.models;
     opens com.azure.analytics.purview.datamap.models to com.azure.core;
