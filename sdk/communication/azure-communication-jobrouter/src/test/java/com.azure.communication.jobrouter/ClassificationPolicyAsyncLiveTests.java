5e2cf0eb1beee419dd4c36e07288202fb46f8f60
azure-sdk <azuresdk@microsoft.com>
2024-07-26 13:16:16 -0700

    Reset sources for azure-communication-jobrouter to the release version 1.1.5.

--- sdk/communication/azure-communication-jobrouter/src/test/java/com.azure.communication.jobrouter/ClassificationPolicyAsyncLiveTests.java
+++ sdk/communication/azure-communication-jobrouter/src/test/java/com.azure.communication.jobrouter/ClassificationPolicyAsyncLiveTests.java
@@ -4,17 +4,13 @@
 package com.azure.communication.jobrouter;
 
 import com.azure.communication.jobrouter.models.ClassificationPolicy;
-import com.azure.communication.jobrouter.models.ConditionalWorkerSelectorAttachment;
 import com.azure.communication.jobrouter.models.CreateClassificationPolicyOptions;
 import com.azure.communication.jobrouter.models.CreateDistributionPolicyOptions;
-import com.azure.communication.jobrouter.models.CreateJobWithClassificationPolicyOptions;
 import com.azure.communication.jobrouter.models.CreateQueueOptions;
 import com.azure.communication.jobrouter.models.DistributionPolicy;
 import com.azure.communication.jobrouter.models.LabelOperator;
 import com.azure.communication.jobrouter.models.LongestIdleMode;
-import com.azure.communication.jobrouter.models.PassThroughWorkerSelectorAttachment;
 import com.azure.communication.jobrouter.models.QueueSelectorAttachment;
-import com.azure.communication.jobrouter.models.RouterJob;
 import com.azure.communication.jobrouter.models.RouterQueue;
 import com.azure.communication.jobrouter.models.RouterQueueSelector;
 import com.azure.communication.jobrouter.models.RouterValue;
@@ -24,32 +20,29 @@ import com.azure.communication.jobrouter.models.StaticRouterRule;
 import com.azure.communication.jobrouter.models.StaticWorkerSelectorAttachment;
 import com.azure.communication.jobrouter.models.WorkerSelectorAttachment;
 import com.azure.core.http.HttpClient;
-import com.azure.core.http.rest.Response;
-import com.azure.core.util.BinaryData;
+import com.azure.core.test.annotation.LiveOnly;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.Duration;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ClassificationPolicyAsyncLiveTests extends JobRouterTestBase {
+    private JobRouterAdministrationAsyncClient administrationAsyncClient;
 
     @ParameterizedTest
     @MethodSource("com.azure.core.test.TestBase#getHttpClients")
+    @LiveOnly // Remove after azure-core-test 1.26.0-beta.1 is released.
     public void createClassificationPolicy(HttpClient httpClient) {
         // Setup
-        JobRouterAsyncClient routerAsyncClient = getRouterAsyncClient(httpClient);
-        JobRouterAdministrationAsyncClient administrationAsyncClient = getRouterAdministrationAsyncClient(httpClient);
-        String testName = "classification-policy-test-async";
-        String distributionPolicyId = String.format("%s-%s-DistributionPolicy", JAVA_LIVE_TESTS, testName);
+        administrationAsyncClient = getRouterAdministrationAsyncClient(httpClient);
+        String distributionPolicyId = String.format("%s-DistributionPolicy", JAVA_LIVE_TESTS);
+
         String distributionPolicyName = String.format("%s-Name", distributionPolicyId);
 
         CreateDistributionPolicyOptions createDistributionPolicyOptions = new CreateDistributionPolicyOptions(
@@ -57,121 +50,77 @@ public class ClassificationPolicyAsyncLiveTests extends JobRouterTestBase {
             Duration.ofSeconds(10),
             new LongestIdleMode()
                 .setMinConcurrentOffers(1)
-                .setMaxConcurrentOffers(10))
+                .setMaxConcurrentOffers(10)
+        )
             .setName(distributionPolicyName);
 
         DistributionPolicy distributionPolicy = administrationAsyncClient.createDistributionPolicy(createDistributionPolicyOptions).block();
 
-        String queueId = String.format("%s-%s-Queue", JAVA_LIVE_TESTS, testName);
-        String fallbackQueueId = String.format("%s-%s-FallbackQueue", JAVA_LIVE_TESTS, testName);
-        String queueName = String.format("%s-Name", queueId);
+        String queueId = String.format("%s-Queue", JAVA_LIVE_TESTS);
 
-        Map<String, RouterValue> queueLabels = new HashMap<>();
-        queueLabels.put("Label_1", new RouterValue("Value_1"));
+        String queueName = String.format("%s-Name", queueId);
+        Map<String, RouterValue> queueLabels = new HashMap<String, RouterValue>() {
+            {
+                put("Label_1", new RouterValue("Value_1"));
+            }
+        };
 
         CreateQueueOptions createQueueOptions = new CreateQueueOptions(queueId, distributionPolicyId)
             .setLabels(queueLabels)
             .setName(queueName);
 
         RouterQueue jobQueue = administrationAsyncClient.createQueue(createQueueOptions).block();
-        RouterQueue fallbackQueue = administrationAsyncClient.createQueue(
-            new CreateQueueOptions(fallbackQueueId, distributionPolicyId)).block();
 
-        String classificationPolicyId = String.format("%s-%s-ClassificationPolicy", JAVA_LIVE_TESTS, testName);
+        String classificationPolicyId = String.format("%s-ClassificationPolicy", JAVA_LIVE_TESTS);
         String classificationPolicyName = String.format("%s-Name", classificationPolicyId);
 
+        /**
+         * Create queue selectors.
+         */
         StaticQueueSelectorAttachment staticQueueSelector = new StaticQueueSelectorAttachment(
-            new RouterQueueSelector("Id", LabelOperator.EQUAL, new RouterValue(queueId)));
+            new RouterQueueSelector("queueId", LabelOperator.EQUAL)
+                .setValue(new RouterValue(queueId)));
 
-        List<QueueSelectorAttachment> queueSelectors = new ArrayList<>();
-        queueSelectors.add(staticQueueSelector);
-        queueSelectors.add(new StaticQueueSelectorAttachment(
-            new RouterQueueSelector("Name", LabelOperator.NOT_EQUAL, new RouterValue(true))));
+        List<QueueSelectorAttachment> queueSelectors = new ArrayList<QueueSelectorAttachment>() {
+            {
+                add(staticQueueSelector);
+            }
+        };
 
+
+        /**
+         * Create worker selectors.
+         */
         StaticWorkerSelectorAttachment staticWorkerSelector = new StaticWorkerSelectorAttachment(
-            new RouterWorkerSelector("key", LabelOperator.EQUAL, new RouterValue("value"))
-                .setExpedite(true).setExpiresAfter(Duration.ofSeconds(10)));
+            new RouterWorkerSelector("key", LabelOperator.EQUAL)
+                .setValue(new RouterValue("value")));
 
-        List<WorkerSelectorAttachment> workerSelectors = new ArrayList<>();
-        workerSelectors.add(staticWorkerSelector);
-        workerSelectors.add(new ConditionalWorkerSelectorAttachment(new StaticRouterRule().setValue(new RouterValue(true)),
-            Collections.singletonList(new RouterWorkerSelector("Name", LabelOperator.NOT_EQUAL, new RouterValue(true)))));
-        workerSelectors.add(new PassThroughWorkerSelectorAttachment("Key", LabelOperator.NOT_EQUAL));
+        List<WorkerSelectorAttachment> workerSelectors = new ArrayList<WorkerSelectorAttachment>() {
+            {
+                add(staticWorkerSelector);
+            }
+        };
 
+        /**
+         * Create classification policy
+         */
         CreateClassificationPolicyOptions createClassificationPolicyOptions = new CreateClassificationPolicyOptions(
             classificationPolicyId)
             .setName(classificationPolicyName)
             .setPrioritizationRule(new StaticRouterRule().setValue(new RouterValue(1)))
             .setWorkerSelectors(workerSelectors)
             .setQueueSelectors(queueSelectors)
-            .setFallbackQueueId(fallbackQueue.getId());
-
-        String jobId = String.format("%s-%s-Job", JAVA_LIVE_TESTS, testName);
-        String channelId = String.format("%s-%s-Channel", JAVA_LIVE_TESTS, testName);
+            .setFallbackQueueId(jobQueue.getId());
 
         // Action
-        ClassificationPolicy policy = administrationAsyncClient.createClassificationPolicy(createClassificationPolicyOptions).block();
-        RouterJob job = routerAsyncClient.createJobWithClassificationPolicy(
-            new CreateJobWithClassificationPolicyOptions(jobId, channelId, classificationPolicyId)
-                .setLabels(Collections.singletonMap("Key", new RouterValue("abc"))))
-            .block();
-
-        sleepIfRunningAgainstService(5000);
+        ClassificationPolicy result = administrationAsyncClient.createClassificationPolicy(createClassificationPolicyOptions).block();
 
         // Verify
-        assertEquals(classificationPolicyId, policy.getId());
-        assertEquals(classificationPolicyName, policy.getName());
-        assertEquals(StaticRouterRule.class, policy.getPrioritizationRule().getClass());
-        assertNotNull(policy.getEtag());
-        assertEquals(1, ((StaticRouterRule) policy.getPrioritizationRule()).getValue().getIntValue());
-        assertEquals(3, policy.getWorkerSelectorAttachments().size());
-        assertEquals(Duration.ofSeconds(10), ((StaticWorkerSelectorAttachment) policy.getWorkerSelectorAttachments().get(0)).getWorkerSelector().getExpiresAfter());
-        assertEquals(2, policy.getQueueSelectorAttachments().size());
-        assertEquals(fallbackQueueId, policy.getFallbackQueueId());
-
-        Response<BinaryData> binaryData = administrationAsyncClient.getClassificationPolicyWithResponse(policy.getId(), null).block();
-        ClassificationPolicy deserialized = binaryData.getValue().toObject(ClassificationPolicy.class);
-
-        assertEquals(classificationPolicyId, deserialized.getId());
-        assertEquals(classificationPolicyName, deserialized.getName());
-        assertEquals(StaticRouterRule.class, deserialized.getPrioritizationRule().getClass());
-        assertEquals(policy.getEtag(), deserialized.getEtag());
-        assertEquals(1, ((StaticRouterRule) deserialized.getPrioritizationRule()).getValue().getIntValue());
-        assertEquals(3, deserialized.getWorkerSelectorAttachments().size());
-        assertEquals(Duration.ofSeconds(10), ((StaticWorkerSelectorAttachment) deserialized.getWorkerSelectorAttachments().get(0)).getWorkerSelector().getExpiresAfter());
-        assertEquals(2, deserialized.getQueueSelectorAttachments().size());
-        assertEquals(fallbackQueueId, deserialized.getFallbackQueueId());
-
-        job = routerAsyncClient.getJob(job.getId()).block();
-
-        assertEquals(jobId, job.getId());
-        assertEquals(classificationPolicyId, job.getClassificationPolicyId());
-        assertEquals(queueId, job.getQueueId());
-        assertEquals(channelId, job.getChannelId());
-        assertEquals(1, job.getPriority());
-        assertEquals(3, job.getAttachedWorkerSelectors().size());
-
-        deserialized.setPrioritizationRule(null);
-        deserialized.setQueueSelectorAttachments(new ArrayList<>());
-        ClassificationPolicy updatedPolicy = administrationAsyncClient.updateClassificationPolicy(
-            deserialized.getId(), deserialized).block();
-
-        assertEquals(classificationPolicyId, updatedPolicy.getId());
-        assertEquals(classificationPolicyName, updatedPolicy.getName());
-        assertEquals(StaticRouterRule.class, updatedPolicy.getPrioritizationRule().getClass());
-        assertNotEquals(policy.getEtag(), updatedPolicy.getEtag());
-        assertEquals(1, ((StaticRouterRule) updatedPolicy.getPrioritizationRule()).getValue().getIntValue());
-        assertEquals(3, updatedPolicy.getWorkerSelectorAttachments().size());
-        assertEquals(Duration.ofSeconds(10), ((StaticWorkerSelectorAttachment) updatedPolicy.getWorkerSelectorAttachments().get(0)).getWorkerSelector().getExpiresAfter());
-        assertEquals(0, updatedPolicy.getQueueSelectorAttachments().size());
-        assertEquals(fallbackQueueId, updatedPolicy.getFallbackQueueId());
+        assertEquals(classificationPolicyId, result.getId());
 
         // Cleanup
-        routerAsyncClient.cancelJob(job.getId()).block();
-        routerAsyncClient.deleteJob(job.getId()).block();
         administrationAsyncClient.deleteClassificationPolicy(classificationPolicyId).block();
         administrationAsyncClient.deleteQueue(queueId).block();
-        administrationAsyncClient.deleteQueue(fallbackQueueId).block();
         administrationAsyncClient.deleteDistributionPolicy(distributionPolicyId).block();
     }
 }
--- sdk/communication/azure-communication-jobrouter/src/test/java/com.azure.communication.jobrouter/DistributionPolicyLiveTests.java
+++ sdk/communication/azure-communication-jobrouter/src/test/java/com.azure.communication.jobrouter/DistributionPolicyLiveTests.java
@@ -5,35 +5,26 @@ package com.azure.communication.jobrouter;
 
 import com.azure.communication.jobrouter.models.BestWorkerMode;
 import com.azure.communication.jobrouter.models.CreateDistributionPolicyOptions;
-import com.azure.communication.jobrouter.models.DistributionModeKind;
 import com.azure.communication.jobrouter.models.DistributionPolicy;
 import com.azure.communication.jobrouter.models.FunctionRouterRule;
 import com.azure.communication.jobrouter.models.FunctionRouterRuleCredential;
 import com.azure.communication.jobrouter.models.LongestIdleMode;
 import com.azure.communication.jobrouter.models.RoundRobinMode;
-import com.azure.communication.jobrouter.models.RouterValue;
-import com.azure.communication.jobrouter.models.ScoringRuleOptions;
-import com.azure.communication.jobrouter.models.ScoringRuleParameterSelector;
-import com.azure.communication.jobrouter.models.StaticRouterRule;
 import com.azure.core.http.HttpClient;
-import com.azure.core.http.rest.Response;
-import com.azure.core.util.BinaryData;
+import com.azure.core.test.annotation.LiveOnly;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class DistributionPolicyLiveTests extends JobRouterTestBase {
     private JobRouterAdministrationClient routerAdminClient;
 
     @ParameterizedTest
     @MethodSource("com.azure.core.test.TestBase#getHttpClients")
+    @LiveOnly // Remove after azure-core-test 1.26.0-beta.1 is released.
     public void createDistributionPolicyBestWorkerDefaultScoringRule(HttpClient httpClient) {
         // Setup
         routerAdminClient = getRouterAdministrationClient(httpClient);
@@ -46,13 +37,7 @@ public class DistributionPolicyLiveTests extends JobRouterTestBase {
             new BestWorkerMode()
                 .setMinConcurrentOffers(1)
                 .setMaxConcurrentOffers(10)
-                .setScoringRule(new StaticRouterRule().setValue(new RouterValue(5)))
-                .setBypassSelectors(true)
-                .setScoringRuleOptions(new ScoringRuleOptions()
-                    .setBatchScoringEnabled(true)
-                    .setScoringParameters(Collections.singletonList(ScoringRuleParameterSelector.JOB_LABELS))
-                    .setBatchSize(30)
-                    .setDescendingOrder(true)))
+        )
             .setName(bestWorkerModeDistributionPolicyName);
 
         // Action
@@ -60,54 +45,6 @@ public class DistributionPolicyLiveTests extends JobRouterTestBase {
 
         // Verify
         assertEquals(bestWorkerModeDistributionPolicyId, result.getId());
-        assertNotNull(result.getEtag());
-        assertEquals(Duration.ofSeconds(10), result.getOfferExpiresAfter());
-        assertEquals(DistributionModeKind.BEST_WORKER, result.getMode().getKind());
-        assertEquals(bestWorkerModeDistributionPolicyName, result.getName());
-        assertEquals(1, result.getMode().getMinConcurrentOffers());
-        assertEquals(10, result.getMode().getMaxConcurrentOffers());
-        assertEquals(true, result.getMode().isBypassSelectors());
-        assertEquals(5, ((StaticRouterRule) ((BestWorkerMode) result.getMode()).getScoringRule()).getValue().getIntValue());
-        assertEquals(30, ((BestWorkerMode) result.getMode()).getScoringRuleOptions().getBatchSize());
-        assertEquals(true, ((BestWorkerMode) result.getMode()).getScoringRuleOptions().isDescendingOrder());
-        assertEquals(true, ((BestWorkerMode) result.getMode()).getScoringRuleOptions().isBatchScoringEnabled());
-        assertEquals(1, ((BestWorkerMode) result.getMode()).getScoringRuleOptions().getScoringParameters().size());
-
-        Response<BinaryData> binaryResponse = routerAdminClient.getDistributionPolicyWithResponse(result.getId(), null);
-        DistributionPolicy deserialized = binaryResponse.getValue().toObject(DistributionPolicy.class);
-
-        assertEquals(bestWorkerModeDistributionPolicyId, deserialized.getId());
-        assertEquals(result.getEtag(), deserialized.getEtag());
-        assertEquals(Duration.ofSeconds(10), deserialized.getOfferExpiresAfter());
-        assertEquals(DistributionModeKind.BEST_WORKER, deserialized.getMode().getKind());
-        assertEquals(bestWorkerModeDistributionPolicyName, deserialized.getName());
-        assertEquals(1, deserialized.getMode().getMinConcurrentOffers());
-        assertEquals(10, deserialized.getMode().getMaxConcurrentOffers());
-        assertEquals(true, deserialized.getMode().isBypassSelectors());
-        assertEquals(5, ((StaticRouterRule) ((BestWorkerMode) deserialized.getMode()).getScoringRule()).getValue().getIntValue());
-        assertEquals(30, ((BestWorkerMode) deserialized.getMode()).getScoringRuleOptions().getBatchSize());
-        assertEquals(true, ((BestWorkerMode) deserialized.getMode()).getScoringRuleOptions().isDescendingOrder());
-        assertEquals(true, ((BestWorkerMode) deserialized.getMode()).getScoringRuleOptions().isBatchScoringEnabled());
-        assertEquals(1, ((BestWorkerMode) deserialized.getMode()).getScoringRuleOptions().getScoringParameters().size());
-
-        ((BestWorkerMode) deserialized.getMode()).getScoringRuleOptions().setScoringParameters(new ArrayList<>());
-        deserialized.setOfferExpiresAfter(Duration.ofMinutes(5));
-        DistributionPolicy updatedPolicy = routerAdminClient.updateDistributionPolicy(
-            deserialized.getId(), deserialized);
-
-        assertEquals(bestWorkerModeDistributionPolicyId, updatedPolicy.getId());
-        assertNotEquals(result.getEtag(), updatedPolicy.getEtag());
-        assertEquals(Duration.ofMinutes(5), updatedPolicy.getOfferExpiresAfter());
-        assertEquals(DistributionModeKind.BEST_WORKER, updatedPolicy.getMode().getKind());
-        assertEquals(bestWorkerModeDistributionPolicyName, updatedPolicy.getName());
-        assertEquals(1, updatedPolicy.getMode().getMinConcurrentOffers());
-        assertEquals(10, updatedPolicy.getMode().getMaxConcurrentOffers());
-        assertEquals(true, updatedPolicy.getMode().isBypassSelectors());
-        assertEquals(5, ((StaticRouterRule) ((BestWorkerMode) updatedPolicy.getMode()).getScoringRule()).getValue().getIntValue());
-        assertEquals(30, ((BestWorkerMode) updatedPolicy.getMode()).getScoringRuleOptions().getBatchSize());
-        assertEquals(true, ((BestWorkerMode) updatedPolicy.getMode()).getScoringRuleOptions().isDescendingOrder());
-        assertEquals(true, ((BestWorkerMode) updatedPolicy.getMode()).getScoringRuleOptions().isBatchScoringEnabled());
-        assertEquals(0, ((BestWorkerMode) updatedPolicy.getMode()).getScoringRuleOptions().getScoringParameters().size());
 
         // Cleanup
         routerAdminClient.deleteDistributionPolicy(bestWorkerModeDistributionPolicyId);
@@ -115,6 +52,7 @@ public class DistributionPolicyLiveTests extends JobRouterTestBase {
 
     @ParameterizedTest
     @MethodSource("com.azure.core.test.TestBase#getHttpClients")
+    @LiveOnly // Remove after azure-core-test 1.26.0-beta.1 is released.
     public void createDistributionPolicyBestWorkerAzureFunctionRule(HttpClient httpClient) {
         // Setup
         routerAdminClient = getRouterAdministrationClient(httpClient);
@@ -132,7 +70,8 @@ public class DistributionPolicyLiveTests extends JobRouterTestBase {
             new BestWorkerMode()
                 .setScoringRule(azureFunctionRule)
                 .setMinConcurrentOffers(1)
-                .setMaxConcurrentOffers(10))
+                .setMaxConcurrentOffers(10)
+        )
             .setName(bestWorkerModeDistributionPolicyName);
 
         // Action
@@ -147,6 +86,7 @@ public class DistributionPolicyLiveTests extends JobRouterTestBase {
 
     @ParameterizedTest
     @MethodSource("com.azure.core.test.TestBase#getHttpClients")
+    @LiveOnly // Remove after azure-core-test 1.26.0-beta.1 is released.
     public void createDistributionPolicyLongestIdle(HttpClient httpClient) {
         // Setup
         routerAdminClient = getRouterAdministrationClient(httpClient);
@@ -158,7 +98,8 @@ public class DistributionPolicyLiveTests extends JobRouterTestBase {
             Duration.ofSeconds(10),
             new LongestIdleMode()
                 .setMinConcurrentOffers(1)
-                .setMaxConcurrentOffers(10))
+                .setMaxConcurrentOffers(10)
+            )
             .setName(longestIdleModeDistributionPolicyName);
 
         // Action
@@ -173,6 +114,7 @@ public class DistributionPolicyLiveTests extends JobRouterTestBase {
 
     @ParameterizedTest
     @MethodSource("com.azure.core.test.TestBase#getHttpClients")
+    @LiveOnly // Remove after azure-core-test 1.26.0-beta.1 is released.
     public void createDistributionPolicyRoundRobin(HttpClient httpClient) {
         // Setup
         routerAdminClient = getRouterAdministrationClient(httpClient);
@@ -184,7 +126,8 @@ public class DistributionPolicyLiveTests extends JobRouterTestBase {
             Duration.ofSeconds(10),
             new RoundRobinMode()
                 .setMinConcurrentOffers(1)
-                .setMaxConcurrentOffers(10))
+                .setMaxConcurrentOffers(10)
+        )
             .setName(roundRobinModeDistributionPolicyName);
 
         // Action
