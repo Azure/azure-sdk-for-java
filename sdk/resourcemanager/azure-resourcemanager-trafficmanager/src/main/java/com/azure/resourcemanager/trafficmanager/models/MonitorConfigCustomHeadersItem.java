31f31747b5bc008bed71cc672bb6fd55aca990f1
XiaofeiCao <xiaofeicao@microsoft.com>
2024-07-29 16:11:06 +0800

    regen trafficmanager

--- sdk/resourcemanager/azure-resourcemanager-trafficmanager/src/main/java/com/azure/resourcemanager/trafficmanager/models/MonitorConfigCustomHeadersItem.java
+++ sdk/resourcemanager/azure-resourcemanager-trafficmanager/src/main/java/com/azure/resourcemanager/trafficmanager/models/MonitorConfigCustomHeadersItem.java
@@ -5,30 +5,36 @@
 package com.azure.resourcemanager.trafficmanager.models;
 
 import com.azure.core.annotation.Fluent;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.azure.json.JsonReader;
+import com.azure.json.JsonSerializable;
+import com.azure.json.JsonToken;
+import com.azure.json.JsonWriter;
+import java.io.IOException;
 
-/** Custom header name and value. */
+/**
+ * Custom header name and value.
+ */
 @Fluent
-public final class MonitorConfigCustomHeadersItem {
+public final class MonitorConfigCustomHeadersItem implements JsonSerializable<MonitorConfigCustomHeadersItem> {
     /*
      * Header name.
      */
-    @JsonProperty(value = "name")
     private String name;
 
     /*
      * Header value.
      */
-    @JsonProperty(value = "value")
     private String value;
 
-    /** Creates an instance of MonitorConfigCustomHeadersItem class. */
+    /**
+     * Creates an instance of MonitorConfigCustomHeadersItem class.
+     */
     public MonitorConfigCustomHeadersItem() {
     }
 
     /**
      * Get the name property: Header name.
-     *
+     * 
      * @return the name value.
      */
     public String name() {
@@ -37,7 +43,7 @@ public final class MonitorConfigCustomHeadersItem {
 
     /**
      * Set the name property: Header name.
-     *
+     * 
      * @param name the name value to set.
      * @return the MonitorConfigCustomHeadersItem object itself.
      */
@@ -48,7 +54,7 @@ public final class MonitorConfigCustomHeadersItem {
 
     /**
      * Get the value property: Header value.
-     *
+     * 
      * @return the value value.
      */
     public String value() {
@@ -57,7 +63,7 @@ public final class MonitorConfigCustomHeadersItem {
 
     /**
      * Set the value property: Header value.
-     *
+     * 
      * @param value the value value to set.
      * @return the MonitorConfigCustomHeadersItem object itself.
      */
@@ -68,9 +74,49 @@ public final class MonitorConfigCustomHeadersItem {
 
     /**
      * Validates the instance.
-     *
+     * 
      * @throws IllegalArgumentException thrown if the instance is not valid.
      */
     public void validate() {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public JsonWriter toJson(JsonWriter jsonWriter) throws IOException {
+        jsonWriter.writeStartObject();
+        jsonWriter.writeStringField("name", this.name);
+        jsonWriter.writeStringField("value", this.value);
+        return jsonWriter.writeEndObject();
+    }
+
+    /**
+     * Reads an instance of MonitorConfigCustomHeadersItem from the JsonReader.
+     * 
+     * @param jsonReader The JsonReader being read.
+     * @return An instance of MonitorConfigCustomHeadersItem if the JsonReader was pointing to an instance of it, or
+     * null if it was pointing to JSON null.
+     * @throws IOException If an error occurs while reading the MonitorConfigCustomHeadersItem.
+     */
+    public static MonitorConfigCustomHeadersItem fromJson(JsonReader jsonReader) throws IOException {
+        return jsonReader.readObject(reader -> {
+            MonitorConfigCustomHeadersItem deserializedMonitorConfigCustomHeadersItem
+                = new MonitorConfigCustomHeadersItem();
+            while (reader.nextToken() != JsonToken.END_OBJECT) {
+                String fieldName = reader.getFieldName();
+                reader.nextToken();
+
+                if ("name".equals(fieldName)) {
+                    deserializedMonitorConfigCustomHeadersItem.name = reader.getString();
+                } else if ("value".equals(fieldName)) {
+                    deserializedMonitorConfigCustomHeadersItem.value = reader.getString();
+                } else {
+                    reader.skipChildren();
+                }
+            }
+
+            return deserializedMonitorConfigCustomHeadersItem;
+        });
     }
 }
