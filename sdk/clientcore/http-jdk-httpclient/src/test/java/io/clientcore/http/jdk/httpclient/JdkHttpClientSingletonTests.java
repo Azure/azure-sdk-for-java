bf2e5edd008ae5d5ced9614305b18bb3e985d862
Victor Colin Amador <vicolina@microsoft.com>
2024-07-29 17:46:41 -0700

    Disabled JDK Singleton client tests for Java versions under 11.

--- sdk/clientcore/http-jdk-httpclient/src/test/java/io/clientcore/http/jdk/httpclient/JdkHttpClientSingletonTests.java
+++ sdk/clientcore/http-jdk-httpclient/src/test/java/io/clientcore/http/jdk/httpclient/JdkHttpClientSingletonTests.java
@@ -5,10 +5,13 @@ package io.clientcore.http.jdk.httpclient;
 
 import io.clientcore.core.http.client.HttpClient;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledForJreRange;
+import org.junit.jupiter.api.condition.JRE;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+@DisabledForJreRange(max = JRE.JAVA_11)
 public class JdkHttpClientSingletonTests {
     @Test
     public void testGetSharedInstance() {
