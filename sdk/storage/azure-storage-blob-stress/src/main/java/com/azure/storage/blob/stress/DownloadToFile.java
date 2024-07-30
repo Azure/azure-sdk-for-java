05cbf0f8480b77e393432705db2be9d5e98c5381
Rabab <ibrahimr@microsoft.com>
2024-07-29 23:40:40 -0700

    testing why java never finishes executing

--- sdk/storage/azure-storage-blob-stress/src/main/java/com/azure/storage/blob/stress/DownloadToFile.java
+++ sdk/storage/azure-storage-blob-stress/src/main/java/com/azure/storage/blob/stress/DownloadToFile.java
@@ -79,8 +79,19 @@ public class DownloadToFile extends BlobScenarioBase<StorageStressOptions> {
 
     @Override
     public Mono<Void> cleanupAsync() {
+        LOGGER.info("before calling deleteIfExists");
         return asyncNoFaultClient.deleteIfExists()
-            .then(super.cleanupAsync());
+            .doFirst(() -> {
+                // Log before cleanup
+                System.out.println("Cleaning up...");
+                LOGGER.info("Before calling cleanup on blob {}", asyncNoFaultClient.getBlobUrl());
+            })
+            .then(super.cleanupAsync())
+            .doFinally(aVoid -> {
+                // Log on successful completion
+                System.out.println("Cleanup completed successfully.");
+                LOGGER.info("Deleted blob {}", asyncNoFaultClient.getBlobUrl());
+            });
     }
 
     private Path getTempPath(String prefix) {
