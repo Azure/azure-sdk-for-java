198f6949ea52e5ca1eb946d849108b82c5db3804
Jean Bisutti <jean.bisutti@gmail.com>
2024-07-31 14:51:42 +0200

    Fix linting

--- sdk/spring/spring-cloud-azure-starter-monitor/src/test/java/com/azure/monitor/applicationinsights/spring/SpringMonitorPropertyFileTest.java
+++ sdk/spring/spring-cloud-azure-starter-monitor/src/test/java/com/azure/monitor/applicationinsights/spring/SpringMonitorPropertyFileTest.java
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 package com.azure.monitor.applicationinsights.spring;
 
 import com.azure.core.util.CoreUtils;
@@ -10,7 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class SpringMonitorPropertyFileTest {
 
     @Test
-    void should_contain_project_properties() {
+    void shouldContainProjectProperties() {
         Map<String, String> properties =
             CoreUtils.getProperties("azure-spring-cloud-azure-starter-monitor.properties");
 
