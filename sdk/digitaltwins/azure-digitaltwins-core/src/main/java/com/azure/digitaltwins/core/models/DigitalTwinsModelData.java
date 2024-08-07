307e0fc934cba241ea23391f87ceb258f89e0b7a
Alan Zimmer <48699787+alzimmermsft@users.noreply.github.com>
2024-08-07 16:42:04 -0400

    Migrate azure-digitaltwins-core to azure-json (#40716)
    Migrate azure-digitaltwins-core to azure-json

--- sdk/digitaltwins/azure-digitaltwins-core/src/main/java/com/azure/digitaltwins/core/models/DigitalTwinsModelData.java
+++ sdk/digitaltwins/azure-digitaltwins-core/src/main/java/com/azure/digitaltwins/core/models/DigitalTwinsModelData.java
@@ -4,55 +4,52 @@
 package com.azure.digitaltwins.core.models;
 
 import com.azure.core.annotation.Fluent;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.azure.json.JsonReader;
+import com.azure.json.JsonSerializable;
+import com.azure.json.JsonToken;
+import com.azure.json.JsonWriter;
 
+import java.io.IOException;
 import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
 /**
  * The DigitalTwinsModelData representing the model and its corresponding metadata.
  */
 @Fluent
-public final class DigitalTwinsModelData {
+public final class DigitalTwinsModelData implements JsonSerializable<DigitalTwinsModelData> {
 
     /*
-     * A language map that contains the localized display names as specified in
-     * the model definition.
+     * A language map that contains the localized display names as specified in the model definition.
      */
-    @JsonProperty(value = "displayName")
-    private Map<String, String> displayName;
+    private final Map<String, String> displayName;
 
     /*
-     * A language map that contains the localized descriptions as specified in
-     * the model definition.
+     * A language map that contains the localized descriptions as specified in the model definition.
      */
-    @JsonProperty(value = "description")
-    private Map<String, String> description;
+    private final Map<String, String> description;
 
     /*
      * The id of the model as specified in the model definition.
      */
-    @JsonProperty(value = "id", required = true)
-    private String id;
+    private final String id;
 
     /*
      * The time the model was uploaded to the service.
      */
-    @JsonProperty(value = "uploadTime")
-    private OffsetDateTime uploadTime;
+    private final OffsetDateTime uploadTime;
 
     /*
-     * Indicates if the model is decommissioned. Decommissioned models cannot
-     * be referenced by newly created digital twins.
+     * Indicates if the model is decommissioned. Decommissioned models cannot be referenced by newly created digital
+     * twins.
      */
-    @JsonProperty(value = "decommissioned")
-    private boolean decommissioned;
+    private final boolean decommissioned;
 
     /*
      * The model definition that conforms to Digital Twins Definition Language (DTDL) v2.
      */
-    @JsonProperty(value = "model")
-    private String dtdlModel;
+    private final String dtdlModel;
 
     /**
      * Construct a new DigitalTwinsModelData instance. This class should only be constructed internally since the
@@ -65,12 +62,8 @@ public final class DigitalTwinsModelData {
      * @param uploadedOn The time when this model was uploaded.
      * @param decommissioned If this model has been decommissioned.
      */
-    public DigitalTwinsModelData(String modelId,
-                                 String dtdlModel,
-                                 Map<String, String> displayName,
-                                 Map<String, String> description,
-                                 OffsetDateTime uploadedOn,
-                                 boolean decommissioned) {
+    public DigitalTwinsModelData(String modelId, String dtdlModel, Map<String, String> displayName,
+        Map<String, String> description, OffsetDateTime uploadedOn, boolean decommissioned) {
         this.displayName = displayName;
         this.description = description;
         this.id = modelId;
@@ -134,5 +127,67 @@ public final class DigitalTwinsModelData {
      */
     public String getDtdlModel() {
         return this.dtdlModel;
+    }
+
+    @Override
+    public JsonWriter toJson(JsonWriter jsonWriter) throws IOException {
+        return jsonWriter.writeStartObject()
+            .writeMapField("displayName", displayName, JsonWriter::writeString)
+            .writeMapField("description", description, JsonWriter::writeString)
+            .writeStringField("id", id)
+            .writeStringField("uploadTime",
+                uploadTime == null ? null : DateTimeFormatter.ISO_OFFSET_DATE_TIME.format(uploadTime))
+            .writeBooleanField("decommissioned", decommissioned)
+            .writeStringField("dtdlModel", dtdlModel)
+            .writeEndObject();
+    }
+
+    /**
+     * Reads an instance of DigitalTwinsModelData from the JsonReader.
+     *
+     * @param jsonReader The JsonReader being read.
+     * @return An instance of DigitalTwinsModelData if the JsonReader was pointing to an instance of it, or null
+     * if it was pointing to JSON null.
+     * @throws IllegalStateException If the required property 'id' is missing.
+     * @throws IOException If an error occurs while reading the DigitalTwinsModelData.
+     */
+    public static DigitalTwinsModelData fromJson(JsonReader jsonReader) throws IOException {
+        return jsonReader.readObject(reader -> {
+            Map<String, String> displayName = null;
+            Map<String, String> description = null;
+            boolean idFound = false;
+            String id = null;
+            OffsetDateTime uploadTime = null;
+            boolean decommissioned = false;
+            String dtdlModel = null;
+
+            while (reader.nextToken() != JsonToken.END_OBJECT) {
+                String fieldName = reader.getFieldName();
+                reader.nextToken();
+
+                if ("displayName".equals(fieldName)) {
+                    displayName = reader.readMap(JsonReader::getString);
+                } else if ("description".equals(fieldName)) {
+                    description = reader.readMap(JsonReader::getString);
+                } else if ("id".equals(fieldName)) {
+                    id = reader.getString();
+                    idFound = true;
+                } else if ("uploadTime".equals(fieldName)) {
+                    uploadTime = reader.getNullable(nonNullReader -> OffsetDateTime.parse(nonNullReader.getString()));
+                } else if ("decommissioned".equals(fieldName)) {
+                    decommissioned = reader.getBoolean();
+                } else if ("dtdlModel".equals(fieldName)) {
+                    dtdlModel = reader.getString();
+                } else {
+                    reader.skipChildren();
+                }
+            }
+
+            if (!idFound) {
+                throw new IllegalStateException("Missing required property 'id'.");
+            }
+
+            return new DigitalTwinsModelData(id, dtdlModel, displayName, description, uploadTime, decommissioned);
+        });
     }
 }
