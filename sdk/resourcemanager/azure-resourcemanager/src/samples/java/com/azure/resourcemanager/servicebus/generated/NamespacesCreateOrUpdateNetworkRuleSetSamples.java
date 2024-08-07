59be45b5d695aadaff24dda63647e25d2ed2bc02
Xiaofei Cao <92354331+XiaofeiCao@users.noreply.github.com>
2024-08-07 11:49:23 +0800

    mgmt azure-json, migrate azure-resourcemanager-servicebus (#41409)
    * servicebus config
    
    * regen
    
    * manual code
    
    duration serializer
    
    resource
    
    resource
    
    fix api-specs comment
    
    fix DurationSerializer
    
    revapi.json

--- sdk/resourcemanager/azure-resourcemanager/src/samples/java/com/azure/resourcemanager/servicebus/generated/NamespacesCreateOrUpdateNetworkRuleSetSamples.java
+++ sdk/resourcemanager/azure-resourcemanager/src/samples/java/com/azure/resourcemanager/servicebus/generated/NamespacesCreateOrUpdateNetworkRuleSetSamples.java
@@ -4,65 +4,51 @@
 
 package com.azure.resourcemanager.servicebus.generated;
 
-import com.azure.core.util.Context;
 import com.azure.resourcemanager.servicebus.fluent.models.NetworkRuleSetInner;
 import com.azure.resourcemanager.servicebus.models.DefaultAction;
+import com.azure.resourcemanager.servicebus.models.NetworkRuleIpAction;
 import com.azure.resourcemanager.servicebus.models.NWRuleSetIpRules;
 import com.azure.resourcemanager.servicebus.models.NWRuleSetVirtualNetworkRules;
-import com.azure.resourcemanager.servicebus.models.NetworkRuleIpAction;
 import com.azure.resourcemanager.servicebus.models.Subnet;
 import java.util.Arrays;
 
-/** Samples for Namespaces CreateOrUpdateNetworkRuleSet. */
+/**
+ * Samples for Namespaces CreateOrUpdateNetworkRuleSet.
+ */
 public final class NamespacesCreateOrUpdateNetworkRuleSetSamples {
     /*
-     * x-ms-original-file: specification/servicebus/resource-manager/Microsoft.ServiceBus/stable/2021-11-01/examples/NameSpaces/VirtualNetworkRule/SBNetworkRuleSetCreate.json
+     * x-ms-original-file:
+     * specification/servicebus/resource-manager/Microsoft.ServiceBus/stable/2021-11-01/examples/NameSpaces/
+     * VirtualNetworkRule/SBNetworkRuleSetCreate.json
      */
     /**
      * Sample code: NameSpaceNetworkRuleSetCreate.
-     *
+     * 
      * @param azure The entry point for accessing resource management APIs in Azure.
      */
     public static void nameSpaceNetworkRuleSetCreate(com.azure.resourcemanager.AzureResourceManager azure) {
-        azure
-            .serviceBusNamespaces()
+        azure.serviceBusNamespaces()
             .manager()
             .serviceClient()
             .getNamespaces()
-            .createOrUpdateNetworkRuleSetWithResponse(
-                "ResourceGroup",
-                "sdk-Namespace-6019",
-                new NetworkRuleSetInner()
-                    .withDefaultAction(DefaultAction.DENY)
-                    .withVirtualNetworkRules(
-                        Arrays
-                            .asList(
-                                new NWRuleSetVirtualNetworkRules()
-                                    .withSubnet(
-                                        new Subnet()
-                                            .withId(
-                                                "/subscriptions/854d368f-1828-428f-8f3c-f2affa9b2f7d/resourcegroups/alitest/providers/Microsoft.Network/virtualNetworks/myvn/subnets/subnet2"))
-                                    .withIgnoreMissingVnetServiceEndpoint(true),
-                                new NWRuleSetVirtualNetworkRules()
-                                    .withSubnet(
-                                        new Subnet()
-                                            .withId(
-                                                "/subscriptions/854d368f-1828-428f-8f3c-f2affa9b2f7d/resourcegroups/alitest/providers/Microsoft.Network/virtualNetworks/myvn/subnets/subnet3"))
-                                    .withIgnoreMissingVnetServiceEndpoint(false),
-                                new NWRuleSetVirtualNetworkRules()
-                                    .withSubnet(
-                                        new Subnet()
-                                            .withId(
-                                                "/subscriptions/854d368f-1828-428f-8f3c-f2affa9b2f7d/resourcegroups/alitest/providers/Microsoft.Network/virtualNetworks/myvn/subnets/subnet6"))
-                                    .withIgnoreMissingVnetServiceEndpoint(false)))
-                    .withIpRules(
-                        Arrays
-                            .asList(
-                                new NWRuleSetIpRules().withIpMask("1.1.1.1").withAction(NetworkRuleIpAction.ALLOW),
-                                new NWRuleSetIpRules().withIpMask("1.1.1.2").withAction(NetworkRuleIpAction.ALLOW),
-                                new NWRuleSetIpRules().withIpMask("1.1.1.3").withAction(NetworkRuleIpAction.ALLOW),
-                                new NWRuleSetIpRules().withIpMask("1.1.1.4").withAction(NetworkRuleIpAction.ALLOW),
-                                new NWRuleSetIpRules().withIpMask("1.1.1.5").withAction(NetworkRuleIpAction.ALLOW))),
-                Context.NONE);
+            .createOrUpdateNetworkRuleSetWithResponse("ResourceGroup", "sdk-Namespace-6019", new NetworkRuleSetInner()
+                .withDefaultAction(DefaultAction.DENY)
+                .withVirtualNetworkRules(Arrays.asList(
+                    new NWRuleSetVirtualNetworkRules().withSubnet(new Subnet().withId(
+                        "/subscriptions/854d368f-1828-428f-8f3c-f2affa9b2f7d/resourcegroups/alitest/providers/Microsoft.Network/virtualNetworks/myvn/subnets/subnet2"))
+                        .withIgnoreMissingVnetServiceEndpoint(true),
+                    new NWRuleSetVirtualNetworkRules().withSubnet(new Subnet().withId(
+                        "/subscriptions/854d368f-1828-428f-8f3c-f2affa9b2f7d/resourcegroups/alitest/providers/Microsoft.Network/virtualNetworks/myvn/subnets/subnet3"))
+                        .withIgnoreMissingVnetServiceEndpoint(false),
+                    new NWRuleSetVirtualNetworkRules().withSubnet(new Subnet().withId(
+                        "/subscriptions/854d368f-1828-428f-8f3c-f2affa9b2f7d/resourcegroups/alitest/providers/Microsoft.Network/virtualNetworks/myvn/subnets/subnet6"))
+                        .withIgnoreMissingVnetServiceEndpoint(false)))
+                .withIpRules(
+                    Arrays.asList(new NWRuleSetIpRules().withIpMask("1.1.1.1").withAction(NetworkRuleIpAction.ALLOW),
+                        new NWRuleSetIpRules().withIpMask("1.1.1.2").withAction(NetworkRuleIpAction.ALLOW),
+                        new NWRuleSetIpRules().withIpMask("1.1.1.3").withAction(NetworkRuleIpAction.ALLOW),
+                        new NWRuleSetIpRules().withIpMask("1.1.1.4").withAction(NetworkRuleIpAction.ALLOW),
+                        new NWRuleSetIpRules().withIpMask("1.1.1.5").withAction(NetworkRuleIpAction.ALLOW))),
+                com.azure.core.util.Context.NONE);
     }
 }
--- sdk/resourcemanager/azure-resourcemanager/src/samples/java/com/azure/resourcemanager/servicebus/generated/NamespacesGetAuthorizationRuleSamples.java
+++ sdk/resourcemanager/azure-resourcemanager/src/samples/java/com/azure/resourcemanager/servicebus/generated/NamespacesGetAuthorizationRuleSamples.java
@@ -4,24 +4,26 @@
 
 package com.azure.resourcemanager.servicebus.generated;
 
-import com.azure.core.util.Context;
-
-/** Samples for Namespaces GetAuthorizationRule. */
+/**
+ * Samples for Namespaces GetAuthorizationRule.
+ */
 public final class NamespacesGetAuthorizationRuleSamples {
     /*
-     * x-ms-original-file: specification/servicebus/resource-manager/Microsoft.ServiceBus/stable/2021-11-01/examples/NameSpaces/SBNameSpaceAuthorizationRuleGet.json
+     * x-ms-original-file:
+     * specification/servicebus/resource-manager/Microsoft.ServiceBus/stable/2021-11-01/examples/NameSpaces/
+     * SBNameSpaceAuthorizationRuleGet.json
      */
     /**
      * Sample code: NameSpaceAuthorizationRuleGet.
-     *
+     * 
      * @param azure The entry point for accessing resource management APIs in Azure.
      */
     public static void nameSpaceAuthorizationRuleGet(com.azure.resourcemanager.AzureResourceManager azure) {
-        azure
-            .serviceBusNamespaces()
+        azure.serviceBusNamespaces()
             .manager()
             .serviceClient()
             .getNamespaces()
-            .getAuthorizationRuleWithResponse("ArunMonocle", "sdk-Namespace-6914", "sdk-AuthRules-1788", Context.NONE);
+            .getAuthorizationRuleWithResponse("ArunMonocle", "sdk-Namespace-6914", "sdk-AuthRules-1788",
+                com.azure.core.util.Context.NONE);
     }
 }
