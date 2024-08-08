247a89b5d9aec5c3128c0e8fde59b783744f9eb0
Chuong Nguyen <chuongnguyen@microsoft.com>
2024-08-08 18:36:15 +0700

    fix lint issues

--- sdk/webpubsub/azure-messaging-webpubsub/src/main/java/com/azure/messaging/webpubsub/WebPubSubServiceClient.java
+++ sdk/webpubsub/azure-messaging-webpubsub/src/main/java/com/azure/messaging/webpubsub/WebPubSubServiceClient.java
@@ -350,13 +350,13 @@ public final class WebPubSubServiceClient {
      * }
      * }</pre>
      *
-     * @param groups         Target group names. Rejected by server on status code 400 if this parameter is null.
-     * @param filter         The filter to apply to the connections.
+     * @param groups Target group names. Rejected by server on status code 400 if this parameter is null.
+     * @param filter The filter to apply to the connections.
      * @param requestOptions The options to configure the HTTP request before HTTP client sends it.
-     * @throws HttpResponseException         thrown if the request is rejected by server.
+     * @throws HttpResponseException thrown if the request is rejected by server.
      * @throws ClientAuthenticationException thrown if the request is rejected by server on status code 401.
-     * @throws ResourceNotFoundException     thrown if the request is rejected by server on status code 404.
-     * @throws ResourceModifiedException     thrown if the request is rejected by server on status code 409.
+     * @throws ResourceNotFoundException thrown if the request is rejected by server on status code 404.
+     * @throws ResourceModifiedException thrown if the request is rejected by server on status code 409.
      */
     @ServiceMethod(returns = ReturnType.SINGLE)
     public void addConnectionsToGroups(List<String> groups, String filter, RequestOptions requestOptions) {
