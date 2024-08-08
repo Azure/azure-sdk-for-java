69b51e207364a4e8e05e3ca1bfc6724f8e20f8f2
Han Chiang <jimmy4834@hotmail.com>
2024-08-08 09:09:59 +0000

    Codegen and address breaking change

--- sdk/face/azure-ai-vision-face/src/test/java/com/azure/ai/vision/face/tests/commands/liveness/ILivenessWithVerifySessionSyncCommands.java
+++ sdk/face/azure-ai-vision-face/src/test/java/com/azure/ai/vision/face/tests/commands/liveness/ILivenessWithVerifySessionSyncCommands.java
@@ -3,14 +3,14 @@
 
 package com.azure.ai.vision.face.tests.commands.liveness;
 
-import com.azure.ai.vision.face.models.CreateLivenessSessionContent;
+import com.azure.ai.vision.face.models.CreateLivenessWithVerifySessionJsonContent;
 import com.azure.ai.vision.face.models.CreateLivenessWithVerifySessionResult;
 import com.azure.ai.vision.face.models.LivenessWithVerifySession;
 import com.azure.core.util.BinaryData;
 
 public interface ILivenessWithVerifySessionSyncCommands {
     CreateLivenessWithVerifySessionResult createLivenessWithVerifySessionSync(
-        CreateLivenessSessionContent content, BinaryData verifyImage);
+        CreateLivenessWithVerifySessionJsonContent content, BinaryData verifyImage);
     LivenessWithVerifySession getLivenessWithVerifySessionResultSync(String sessionId);
     void deleteLivenessWithVerifySessionSync(String sessionId);
 }
