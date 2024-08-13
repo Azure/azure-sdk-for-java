bb1407b80f18b5cb4a4f8ced352e7399b4d1040f
azure-sdk <azuresdk@microsoft.com>
2024-08-13 09:10:16 +0000

    [Automation] Generate SDK based on TypeSpec 0.19.1

--- sdk/standbypool/azure-resourcemanager-standbypool/src/main/java/com/azure/resourcemanager/standbypool/fluent/models/StandbyVirtualMachinePoolResourceInner.java
+++ sdk/standbypool/azure-resourcemanager-standbypool/src/main/java/com/azure/resourcemanager/standbypool/fluent/models/StandbyVirtualMachinePoolResourceInner.java
@@ -31,9 +31,9 @@ public final class StandbyVirtualMachinePoolResourceInner extends Resource {
     private SystemData systemData;
 
     /*
-     * Fully qualified resource Id for the resource.
+     * The type of the resource.
      */
-    private String id;
+    private String type;
 
     /*
      * The name of the resource.
@@ -41,9 +41,9 @@ public final class StandbyVirtualMachinePoolResourceInner extends Resource {
     private String name;
 
     /*
-     * The type of the resource.
+     * Fully qualified resource Id for the resource.
      */
-    private String type;
+    private String id;
 
     /**
      * Creates an instance of StandbyVirtualMachinePoolResourceInner class.
@@ -82,13 +82,13 @@ public final class StandbyVirtualMachinePoolResourceInner extends Resource {
     }
 
     /**
-     * Get the id property: Fully qualified resource Id for the resource.
+     * Get the type property: The type of the resource.
      * 
-     * @return the id value.
+     * @return the type value.
      */
     @Override
-    public String id() {
-        return this.id;
+    public String type() {
+        return this.type;
     }
 
     /**
@@ -102,13 +102,13 @@ public final class StandbyVirtualMachinePoolResourceInner extends Resource {
     }
 
     /**
-     * Get the type property: The type of the resource.
+     * Get the id property: Fully qualified resource Id for the resource.
      * 
-     * @return the type value.
+     * @return the id value.
      */
     @Override
-    public String type() {
-        return this.type;
+    public String id() {
+        return this.id;
     }
 
     /**
