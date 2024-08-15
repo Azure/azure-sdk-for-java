a719650801205966ad4b1c69bdfe7551f4f8a73b
Rabab <ibrahimr@microsoft.com>
2024-08-15 11:49:31 -0700

    adding a private static class in the Factory class due to SIC_INNER_SHOULD_BE_STATIC_ANON lint warning

--- sdk/storage/azure-storage-internal-avro/src/main/java/com/azure/storage/internal/avro/implementation/AvroReaderSyncFactory.java
+++ sdk/storage/azure-storage-internal-avro/src/main/java/com/azure/storage/internal/avro/implementation/AvroReaderSyncFactory.java
@@ -11,49 +11,53 @@ import java.nio.ByteBuffer;
 public class AvroReaderSyncFactory {
 
     /**
+     * An internal static inner class that implements AvroSyncReader.
+     */
+    private static class InternalAvroSyncReader implements AvroSyncReader {
+        private final AvroSyncParser parser;
+        private final ByteBuffer avroHeader;
+        private final ByteBuffer avroBody;
+        private final long offset;
+        private final long thresholdIndex;
+
+        InternalAvroSyncReader(AvroSyncParser parser, ByteBuffer avroHeader, ByteBuffer avroBody, long offset, long thresholdIndex) {
+            this.parser = parser;
+            this.avroHeader = avroHeader;
+            this.avroBody = avroBody;
+            this.offset = offset;
+            this.thresholdIndex = thresholdIndex;
+        }
+
+        @Override
+        public Iterable<AvroObject> read() {
+            // Parse the header
+            avroHeader.position(0); // Ensure the header buffer is at the start
+            parser.parse(avroHeader);
+
+            // Prepare parser to read the body at an offset, only if necessary
+            parser.prepareParserToReadBody(offset, thresholdIndex);
+
+            // Set the position of the body buffer to the offset
+            avroBody.position((int) offset);
+
+            // Return iterable of parsed objects from the body
+            return parser.parse(avroBody);
+        }
+    }
+
+    /**
      * Gets a new instance of {@link AvroSyncReader} with support for offset and thresholdIndex.
-     *
-     * @param avroHeader The ByteBuffer containing the Avro header.
-     * @param avroBody The ByteBuffer that starts at the offset and represents the start of a block.
-     * @param offset The position in the ByteBuffer from where to start parsing.
-     * @param thresholdIndex The minimum index of the objects to be returned.
-     * @return An AvroSyncReader.
      */
     public AvroSyncReader getAvroReader(ByteBuffer avroHeader, ByteBuffer avroBody, long offset, long thresholdIndex) {
-        return new AvroSyncReader() {
-            private final AvroSyncParser parser = new AvroSyncParser(true); // assuming true means handling partial reads
-
-            @Override
-            public Iterable<AvroObject> read() {
-                // Parse the header
-                avroHeader.position(0); // Ensure the header buffer is at the start
-                parser.parse(avroHeader);
-
-                // Prepare parser to read the body at an offset, only if necessary
-                parser.prepareParserToReadBody(offset, thresholdIndex);
-
-                // Set the position of the body buffer to the offset
-                avroBody.position((int) offset);
-
-                // Return iterable of parsed objects from the body
-                return parser.parse(avroBody);
-            }
-        };
+        AvroSyncParser parser = new AvroSyncParser(true);
+        return new InternalAvroSyncReader(parser, avroHeader, avroBody, offset, thresholdIndex);
     }
 
     /**
      * Gets a new instance of {@link AvroSyncReader}.
-     *
-     * @param data The ByteBuffer that contains the Avro data.
-     * @return An AvroReader.
      */
     public AvroSyncReader getAvroReader(ByteBuffer data) {
-        return new AvroSyncReader() {
-            private final AvroSyncParser parser = new AvroSyncParser(false);
-            @Override
-            public Iterable<AvroObject> read() {
-                return parser.parse(data);
-            }
-        };
+        AvroSyncParser parser = new AvroSyncParser(false);
+        return () -> parser.parse(data);
     }
 }
