63016a485385b230faadfd116c845944b878cb0a
Vinay Gera <vigera@microsoft.com>
2024-08-22 13:35:08 -0700

    spotless fix

--- sdk/core/azure-core-experimental/src/main/java/com/azure/core/experimental/credential/PopTokenRequestContext.java
+++ sdk/core/azure-core-experimental/src/main/java/com/azure/core/experimental/credential/PopTokenRequestContext.java
@@ -28,12 +28,11 @@ public final class PopTokenRequestContext extends TokenRequestContext {
     private String proofOfPossessionNonce;
     private HttpRequest request;
 
-
     /**
      * Creates a new instance of the PopTokenRequestContext.
      */
-    public PopTokenRequestContext() {}
-
+    public PopTokenRequestContext() {
+    }
 
     /**
      * Adds scopes required for the token.
