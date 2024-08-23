e541a6de469ae4b4f45f2ae0df10a2a898f5784b
Fabian Meiswinkel <fabianm@microsoft.com>
2024-08-23 00:05:57 +0000

    Update BulkWriterNoProgressException.scala

--- sdk/cosmos/azure-cosmos-spark_3_2-12/src/main/scala/com/azure/cosmos/spark/BulkWriterNoProgressException.scala
+++ sdk/cosmos/azure-cosmos-spark_3_2-12/src/main/scala/com/azure/cosmos/spark/BulkWriterNoProgressException.scala
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 package com.azure.cosmos.spark
 
 import com.azure.cosmos.models.CosmosItemOperation
