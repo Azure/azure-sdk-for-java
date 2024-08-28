353e4b4f58a7416bb5a7df2a7b4dde8043022c1d
Shawn Fang <shafang@microsoft.com>
2024-08-28 12:10:14 -0700

    different suffix `/8S8` or `/8Sw` doesn't matter

--- sdk/openai/azure-ai-openai/src/main/java/com/azure/ai/openai/implementation/EmbeddingsUtils.java
+++ sdk/openai/azure-ai-openai/src/main/java/com/azure/ai/openai/implementation/EmbeddingsUtils.java
@@ -54,14 +54,6 @@ public final class EmbeddingsUtils {
         byte[] byteArray = byteBuffer.array();
 
         // Encode byte array to Base64
-        String base64String = Base64.getEncoder().withoutPadding().encodeToString(byteArray);
-
-        // TODO: Remove this workaround once you find the encoding schema can do it
-        // Ensure the encoded string ends with "/8S8"
-        if (!base64String.endsWith("/8S8")) {
-            base64String = base64String.substring(0, base64String.length() - 4) + "/8S8";
-        }
-
-        return base64String;
+        return Base64.getEncoder().withoutPadding().encodeToString(byteArray);
     }
 }
--- sdk/openai/azure-ai-openai/src/test/java/com/azure/ai/openai/implementation/EmbeddingUtilsTests.java
+++ sdk/openai/azure-ai-openai/src/test/java/com/azure/ai/openai/implementation/EmbeddingUtilsTests.java
@@ -38,15 +38,24 @@ public class EmbeddingUtilsTests {
                 0.00942275f, -0.0010887914f, -4.0955696E-4f, -0.008831308f, -0.028631182f, 0.0030176996f, 0.009295053f,
                 0.01654694f, 0.0064756204f, 0.019436942f, 0.014718847f, -0.025754621f, -0.009913378f, 3.3152518E7f);
         String embedding = "Cw/juyfdrrsak0I8vdDMvGBIyrys\u002BCI9CYElvFANGrwVDFi8fNwivJphP7wT8gA8fs1mvHWW/jvumiY8FvSku7n3uzxcNca6Y610PJYlKLxqVp87mSdMPBLhoDvyEDE8pmDYu8Zqu7nPyrY7jLRMvPwehjykb5Q7YA7XOwKv57taTXm85O/Xuyxkmbw6roU7mYpSO/eXm7yPGfc8mNz4u1DkBjzhYRo8x7WOus651rkxsRC88YvqvJnERTtHShg8c42HPHEx1Ds5Op88UidxPFv70ry5ayK8A\u002B/8S8";
+
         // Act
         List<Float> floats = EmbeddingsUtils.convertBase64ToFloatList(embedding);
         String convertFloatListToBase64 = EmbeddingsUtils.convertFloatListToBase64(expectedFloatList);
+        List<Float> reconvertedBackToFloats = EmbeddingsUtils.convertBase64ToFloatList(convertFloatListToBase64);
 
         // Assert
         for (int i = 0; i < expectedFloatList.size(); i++) {
             assertEquals(expectedFloatList.get(i).floatValue(), floats.get(i).floatValue());
         }
-        assertEquals(embedding, convertFloatListToBase64);
+
+        // Testing if the converted Based64 can be converted back to floats correctly, as the converted Base64 String
+        // has a different suffix, '/8Sw', which is different from the original base64 string, '/8S8'.
+        // Since decoding the string results in the same expected float list, the conversion is correct.
+        // And it is not expected to compare those Base64 strings.
+        for (int i = 0; i < expectedFloatList.size(); i++) {
+            assertEquals(expectedFloatList.get(i).floatValue(), reconvertedBackToFloats.get(i).floatValue());
+        }
     }
 
     @Test
