f04726d0f84e241d24ab0db33fbd4f8025705ede
glenn <glharper@microsoft.com>
2024-08-28 11:52:54 -0400

    add customization to unfinalize base classes

--- sdk/ai/azure-ai-inference/customization/src/main/java/InferenceCustomizations.java
+++ sdk/ai/azure-ai-inference/customization/src/main/java/InferenceCustomizations.java
@@ -5,6 +5,10 @@ import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.FieldDeclaration;
 import org.slf4j.Logger;
 
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * This class contains the customization code to customize the AutoRest generated code for Azure AI Inference.
  */
@@ -14,25 +18,17 @@ public class InferenceCustomizations extends Customization {
     public void customize(LibraryCustomization customization, Logger logger) {
         // remove unused class (no reference to them, after partial-update)
         customization.getRawEditor().removeFile("src/main/java/com/azure/ai/inference/implementation/models/CompleteOptions.java");
-        //customizeEmbeddingEncodingFormatClass(customization, logger);
-        //customizeEmbeddingsOptions(customization, logger);
+        customizeChatCompletionsBaseClasses(customization, logger);
     }
 
-    /*
-    private void customizeEmbeddingEncodingFormatClass(LibraryCustomization customization, Logger logger) {
-        logger.info("Customizing the EmbeddingEncodingFormat class");
-        ClassCustomization embeddingEncodingFormatClass = customization.getPackage("com.azure.ai.openai.models").getClass("EmbeddingEncodingFormat");
-        embeddingEncodingFormatClass.getConstructor("EmbeddingEncodingFormat").setModifier(0);
-        embeddingEncodingFormatClass.setModifier(0);
+    private void customizeChatCompletionsBaseClasses(LibraryCustomization customization, Logger logger) {
+        List<String> classList = Arrays.asList("ChatCompletionsNamedToolSelection", "ChatCompletionsToolCall", "ChatCompletionsToolDefinition");
+        for (String className : classList) {
+            logger.info("Customizing the {} class", className);
+            ClassCustomization namedToolSelectionClass = customization.getPackage("com.azure.ai.inference.models").getClass(className);
+            namedToolSelectionClass.setModifier(Modifier.PUBLIC);
+        }
     }
-
-    private void customizeEmbeddingsOptions(LibraryCustomization customization, Logger logger) {
-        logger.info("Customizing the EmbeddingsOptions class");
-        ClassCustomization embeddingsOptionsClass = customization.getPackage("com.azure.ai.openai.models").getClass("EmbeddingsOptions");
-        embeddingsOptionsClass.getMethod("getEncodingFormat").setModifier(0);
-        embeddingsOptionsClass.getMethod("setEncodingFormat").setModifier(0);
-    }
-     */
 
     private static String joinWithNewline(String... lines) {
         return String.join("\n", lines);
