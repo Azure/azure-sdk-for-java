3658006cf5efa56edede5c1994219d051ba0b032
Eddú Meléndez Gonzales <eddu.melendez@gmail.com>
2024-08-28 19:43:17 -0500

    Add Storage Blob and Queue Service Connection from Docker Compose (#41321)

---
+++ sdk/spring/spring-cloud-azure-docker-compose/CHANGELOG.md
@@ -0,0 +1,11 @@
+# Release History
+
+## 5.15.0-beta.1 (Unreleased)
+
+### Features Added
+
+### Breaking Changes
+
+### Bugs Fixed
+
+### Other Changes
---
+++ sdk/spring/spring-cloud-azure-docker-compose/src/test/java/com/azure/spring/cloud/docker/compose/implementation/service/connection/storage/StorageQueueDockerComposeConnectionDetailsFactoryTests.java
@@ -0,0 +1,44 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.spring.cloud.docker.compose.implementation.service.connection.storage;
+
+import com.azure.spring.cloud.autoconfigure.implementation.context.AzureGlobalPropertiesAutoConfiguration;
+import com.azure.spring.cloud.autoconfigure.implementation.storage.queue.AzureStorageQueueAutoConfiguration;
+import com.azure.storage.queue.QueueClient;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(properties = {
+    "spring.docker.compose.skip.in-tests=false",
+    "spring.docker.compose.file=classpath:com/azure/spring/cloud/docker/compose/implementation/service/connection/storage/storage-compose.yaml",
+    "spring.docker.compose.stop.command=down",
+    "spring.cloud.azure.storage.queue.queue-name=devstoreaccount1/tc-queue"
+})
+class StorageQueueDockerComposeConnectionDetailsFactoryTests {
+
+    @Autowired
+    private QueueClient queueClient;
+
+    @Test
+    void test() {
+        String message = "Hello World!";
+        this.queueClient.create();
+        this.queueClient.sendMessage(message);
+        var messageItem = this.queueClient.receiveMessage();
+        assertThat(messageItem.getBody().toString()).isEqualTo(message);
+    }
+
+    @Configuration
+    @ImportAutoConfiguration(classes = {
+        AzureGlobalPropertiesAutoConfiguration.class,
+        AzureStorageQueueAutoConfiguration.class})
+    static class Config {
+    }
+
+}
