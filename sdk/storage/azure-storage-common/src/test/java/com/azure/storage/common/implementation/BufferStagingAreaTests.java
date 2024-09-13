841f4c5df5ada774925bceb766b795cc4d08b261
Rabab <ibrahimr@microsoft.com>
2024-09-12 18:11:40 -0700

    removing unused import

--- sdk/storage/azure-storage-common/src/test/java/com/azure/storage/common/implementation/BufferStagingAreaTests.java
+++ sdk/storage/azure-storage-common/src/test/java/com/azure/storage/common/implementation/BufferStagingAreaTests.java
@@ -22,7 +22,6 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BufferStagingAreaTests {
     static Tuple2<ByteBuffer[], ByteBuffer[]> generateData(int numBuffs, int minBuffSize, int maxBuffSize) {
