86ab1ea3fe7fe41f72f4ab0d4b7dba10ad0242be
Weidong Xu <weidxu@microsoft.com>
2024-09-14 14:08:03 +0800

    customization

--- sdk/face/azure-ai-vision-face/customization/src/main/java/FaceCustomizations.java
+++ sdk/face/azure-ai-vision-face/customization/src/main/java/FaceCustomizations.java
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+import com.azure.autorest.customization.ClassCustomization;
 import com.azure.autorest.customization.Customization;
 import com.azure.autorest.customization.LibraryCustomization;
 import org.slf4j.Logger;
@@ -15,6 +16,18 @@ public class FaceCustomizations extends Customization {
 
     @Override
     public void customize(LibraryCustomization customization, Logger logger) {
-        // NOOP
+        ClassCustomization classCustomization = customization.getClass(
+                "com.azure.ai.vision.face.administration",
+                "FaceAdministrationClientBuilder");
+
+        classCustomization.removeAnnotation("@ServiceClientBuilder");
+        classCustomization.addAnnotation("@ServiceClientBuilder(\n" +
+                "    serviceClients = {\n" +
+                "        FaceAdministrationClient.class,\n" +
+                "        FaceAdministrationAsyncClient.class,\n" +
+                "        LargeFaceListClient.class,\n" +
+                "        LargePersonGroupClient.class,\n" +
+                "        LargeFaceListAsyncClient.class,\n" +
+                "        LargePersonGroupAsyncClient.class })");
     }
 }
