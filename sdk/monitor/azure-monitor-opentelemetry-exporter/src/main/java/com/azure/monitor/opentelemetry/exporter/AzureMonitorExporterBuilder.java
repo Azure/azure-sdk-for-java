1675d38f8cc05af42cfd2c5d044ef40eb27428de
Jean Bisutti <jean.bisutti@gmail.com>
2024-09-17 11:18:01 +0200

    Update sdk/monitor/azure-monitor-opentelemetry-exporter/src/main/java/com/azure/monitor/opentelemetry/exporter/AzureMonitorExporterBuilder.java
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>

--- sdk/monitor/azure-monitor-opentelemetry-exporter/src/main/java/com/azure/monitor/opentelemetry/exporter/AzureMonitorExporterBuilder.java
+++ sdk/monitor/azure-monitor-opentelemetry-exporter/src/main/java/com/azure/monitor/opentelemetry/exporter/AzureMonitorExporterBuilder.java
@@ -182,7 +182,7 @@ public final class AzureMonitorExporterBuilder implements ConnectionStringTrait<
      *
      * <p><strong>Note:</strong> It is important to understand the precedence order of the HttpTrait APIs. In
      * particular, if an {@link HttpPipeline} is specified, this takes precedence over all other APIs in the trait, and
-     * they will be ignored. If no {@link HttpPipeline} is specified, a HTTP pipeline will be constructed internally
+     * they will be ignored. If no {@link HttpPipeline} is specified, an HTTP pipeline will be constructed internally
      * based on the settings provided to this trait. Additionally, there may be other APIs in types that implement this
      * trait that are also ignored if an {@link HttpPipeline} is specified, so please be sure to refer to the
      * documentation of types that implement this trait to understand the full set of implications.</p>
