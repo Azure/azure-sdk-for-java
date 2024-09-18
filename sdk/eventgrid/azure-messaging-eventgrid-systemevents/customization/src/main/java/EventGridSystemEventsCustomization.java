23b49e2820b6b0653378af8cacf761ff9d5c12a9
Bill Wert <billwert@microsoft.com>
2024-09-17 17:20:05 -0700

    add files

---
+++ sdk/eventgrid/azure-messaging-eventgrid-systemevents/customization/src/main/java/EventGridSystemEventsCustomization.java
@@ -0,0 +1,30 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import com.azure.autorest.customization.Customization;
+import com.azure.autorest.customization.Editor;
+import com.azure.autorest.customization.LibraryCustomization;
+import org.slf4j.Logger;
+
+import java.util.List;
+
+/**
+ * This class contains the customization code to customize the AutoRest generated code for Event Grid.
+ */
+public class EventGridSystemEventsCustomization extends Customization {
+    private final String newLine = System.lineSeparator();
+    @Override
+    public void customize(LibraryCustomization customization, Logger logger) {
+
+        Editor editor = customization.getRawEditor();
+        List<String> lines = editor.getFileLines("src/main/java/module-info.java");
+        StringBuilder sb = new StringBuilder();
+        lines.forEach(line -> {
+            if (!line.trim().equals("exports com.azure.messaging.eventgrid.systemevents;")) {
+                sb.append(line).append(newLine);
+            }
+        });
+        editor.replaceFile("src/main/java/module-info.java", sb.toString());
+    }
+
+}
