8b2d98918da4ad82092050789bd80a7b3f9a0bc6
azure-sdk <azuresdk@microsoft.com>
2024-09-18 04:45:32 +0000

    Reset sources for azure-containers-containerregistry to the release version 1.2.11.

--- sdk/containerregistry/azure-containers-containerregistry/src/test/java/com/azure/containers/containerregistry/ContainerRegistryCredentialPolicyTests.java
+++ sdk/containerregistry/azure-containers-containerregistry/src/test/java/com/azure/containers/containerregistry/ContainerRegistryCredentialPolicyTests.java
@@ -7,7 +7,6 @@ import com.azure.containers.containerregistry.implementation.authentication.Cont
 import com.azure.containers.containerregistry.implementation.authentication.ContainerRegistryTokenRequestContext;
 import com.azure.containers.containerregistry.implementation.authentication.ContainerRegistryTokenService;
 import com.azure.core.credential.AccessToken;
-import com.azure.core.http.HttpHeaderName;
 import com.azure.core.http.HttpHeaders;
 import com.azure.core.http.HttpMethod;
 import com.azure.core.http.HttpPipelineCallContext;
@@ -18,13 +17,11 @@ import com.azure.core.http.HttpResponse;
 import com.azure.core.test.SyncAsyncExtension;
 import com.azure.core.test.annotation.SyncAsyncTest;
 import com.azure.core.test.http.MockHttpResponse;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import reactor.core.publisher.Mono;
-import reactor.test.StepVerifier;
 
 import java.time.OffsetDateTime;
 
@@ -39,9 +36,11 @@ import static org.mockito.Mockito.when;
 
 public class ContainerRegistryCredentialPolicyTests {
 
+    public static final String WWW_AUTHENTICATE = "WWW-Authenticate";
     public static final String AUTHENTICATE_HEADER = "Bearer realm=\"https://mytest.azurecr.io/oauth2/token\",service=\"mytest.azurecr.io\",scope=\"registry:catalog:*\",error=\"invalid_token\"";
     public static final Integer UNAUTHORIZED = 401;
     public static final Integer SUCCESS = 200;
+    public static final String AUTHORIZATION = "Authorization";
     public static final String BEARER = "Bearer";
     public static final String SERVICENAME = "mytest.azurecr.io";
     public static final String SCOPENAME = "registry:catalog:*";
@@ -67,12 +66,20 @@ public class ContainerRegistryCredentialPolicyTests {
         HttpPipelineCallContext context = mock(HttpPipelineCallContext.class);
         when(context.getHttpRequest()).thenReturn(request);
 
-        MockHttpResponse unauthorizedResponseWithHeader = new MockHttpResponse(request, UNAUTHORIZED,
-            new HttpHeaders().set(HttpHeaderName.WWW_AUTHENTICATE, AUTHENTICATE_HEADER));
+        MockHttpResponse unauthorizedResponseWithHeader = new MockHttpResponse(
+            mock(HttpRequest.class),
+            UNAUTHORIZED,
+            new HttpHeaders().set(WWW_AUTHENTICATE, AUTHENTICATE_HEADER)
+        );
 
-        MockHttpResponse unauthorizedResponseWithoutHeader = new MockHttpResponse(request, UNAUTHORIZED);
+        MockHttpResponse unauthorizedResponseWithoutHeader = new MockHttpResponse(
+            mock(HttpRequest.class),
+            UNAUTHORIZED);
 
-        MockHttpResponse successResponse = new MockHttpResponse(request, SUCCESS);
+        MockHttpResponse successResponse = new MockHttpResponse(
+            mock(HttpRequest.class),
+            SUCCESS
+        );
 
         HttpPipelineNextPolicy mockNextClone = mock(HttpPipelineNextPolicy.class);
         when(mockNextClone.process()).thenReturn(Mono.just(successResponse));
@@ -131,12 +138,12 @@ public class ContainerRegistryCredentialPolicyTests {
         ContainerRegistryCredentialsPolicy policy = new ContainerRegistryCredentialsPolicy(this.service, "foo");
         ContainerRegistryCredentialsPolicy spyPolicy = Mockito.spy(policy);
 
-        // Validate that the onChallenge ran successfully.
-        StepVerifier.create(spyPolicy.authorizeRequestOnChallenge(this.callContext, this.unauthorizedHttpResponse))
-            .assertNext(Assertions::assertTrue)
-            .verifyComplete();
+        Boolean onChallenge = spyPolicy.authorizeRequestOnChallenge(this.callContext, this.unauthorizedHttpResponse).block();
 
-        String tokenValue = this.callContext.getHttpRequest().getHeaders().getValue(HttpHeaderName.AUTHORIZATION);
+        // Validate that the onChallenge ran successfully.
+        assertTrue(onChallenge);
+
+        String tokenValue = this.callContext.getHttpRequest().getHeaders().getValue(AUTHORIZATION);
         assertFalse(tokenValue.isEmpty());
         assertTrue(tokenValue.startsWith(BEARER));
         assertTrue(tokenValue.endsWith(tokenValue));
@@ -155,12 +162,12 @@ public class ContainerRegistryCredentialPolicyTests {
         ContainerRegistryCredentialsPolicy policy = new ContainerRegistryCredentialsPolicy(this.service, "foo");
         ContainerRegistryCredentialsPolicy spyPolicy = Mockito.spy(policy);
 
-        boolean onChallenge = spyPolicy.authorizeRequestOnChallengeSync(this.callContext, this.unauthorizedHttpResponse);
+        Boolean onChallenge = spyPolicy.authorizeRequestOnChallengeSync(this.callContext, this.unauthorizedHttpResponse);
 
         // Validate that the onChallenge ran successfully.
         assertTrue(onChallenge);
 
-        String tokenValue = this.callContext.getHttpRequest().getHeaders().getValue(HttpHeaderName.AUTHORIZATION);
+        String tokenValue = this.callContext.getHttpRequest().getHeaders().getValue(AUTHORIZATION);
         assertFalse(tokenValue.isEmpty());
         assertTrue(tokenValue.startsWith(BEARER));
         assertTrue(tokenValue.endsWith(tokenValue));
