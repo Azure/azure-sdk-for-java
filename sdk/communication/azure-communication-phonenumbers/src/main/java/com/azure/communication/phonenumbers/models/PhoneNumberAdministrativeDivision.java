a4137876a5ea99002d4aaf52508bb77b2ff181bc
azure-sdk <azuresdk@microsoft.com>
2024-09-18 17:24:49 +0000

    Reset sources for azure-communication-phonenumbers to the release version 1.1.16.

--- sdk/communication/azure-communication-phonenumbers/src/main/java/com/azure/communication/phonenumbers/models/PhoneNumberAdministrativeDivision.java
+++ sdk/communication/azure-communication-phonenumbers/src/main/java/com/azure/communication/phonenumbers/models/PhoneNumberAdministrativeDivision.java
@@ -5,37 +5,29 @@
 package com.azure.communication.phonenumbers.models;
 
 import com.azure.core.annotation.Immutable;
-import com.azure.json.JsonReader;
-import com.azure.json.JsonSerializable;
-import com.azure.json.JsonToken;
-import com.azure.json.JsonWriter;
-import java.io.IOException;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-/**
- * Represents an administrative division. e.g. state or province.
- */
+/** The PhoneNumberAdministrativeDivision model. */
 @Immutable
-public final class PhoneNumberAdministrativeDivision implements JsonSerializable<PhoneNumberAdministrativeDivision> {
+public final class PhoneNumberAdministrativeDivision {
     /*
-     * Represents the localized name of the administrative division of the locality. e.g. state or province localized name.
+     * Represents the localized name of the administrative division of the
+     * locality. e.g. state or province localized name.
      */
+    @JsonProperty(value = "localizedName", required = true, access = JsonProperty.Access.WRITE_ONLY)
     private String localizedName;
 
     /*
-     * Represents the abbreviated name of the administrative division of the locality. e.g. state or province abbreviation such as WA (Washington).
+     * Represents the abbreviated name of the administrative division of the
+     * locality. e.g. state or province abbreviation such as WA (Washington).
      */
+    @JsonProperty(value = "abbreviatedName", required = true, access = JsonProperty.Access.WRITE_ONLY)
     private String abbreviatedName;
-
-    /**
-     * Creates an instance of PhoneNumberAdministrativeDivision class.
-     */
-    public PhoneNumberAdministrativeDivision() {
-    }
 
     /**
      * Get the localizedName property: Represents the localized name of the administrative division of the locality.
      * e.g. state or province localized name.
-     * 
+     *
      * @return the localizedName value.
      */
     public String getLocalizedName() {
@@ -45,49 +37,10 @@ public final class PhoneNumberAdministrativeDivision implements JsonSerializable
     /**
      * Get the abbreviatedName property: Represents the abbreviated name of the administrative division of the locality.
      * e.g. state or province abbreviation such as WA (Washington).
-     * 
+     *
      * @return the abbreviatedName value.
      */
     public String getAbbreviatedName() {
         return this.abbreviatedName;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public JsonWriter toJson(JsonWriter jsonWriter) throws IOException {
-        jsonWriter.writeStartObject();
-        return jsonWriter.writeEndObject();
-    }
-
-    /**
-     * Reads an instance of PhoneNumberAdministrativeDivision from the JsonReader.
-     * 
-     * @param jsonReader The JsonReader being read.
-     * @return An instance of PhoneNumberAdministrativeDivision if the JsonReader was pointing to an instance of it, or
-     * null if it was pointing to JSON null.
-     * @throws IllegalStateException If the deserialized JSON object was missing any required properties.
-     * @throws IOException If an error occurs while reading the PhoneNumberAdministrativeDivision.
-     */
-    public static PhoneNumberAdministrativeDivision fromJson(JsonReader jsonReader) throws IOException {
-        return jsonReader.readObject(reader -> {
-            PhoneNumberAdministrativeDivision deserializedPhoneNumberAdministrativeDivision
-                = new PhoneNumberAdministrativeDivision();
-            while (reader.nextToken() != JsonToken.END_OBJECT) {
-                String fieldName = reader.getFieldName();
-                reader.nextToken();
-
-                if ("localizedName".equals(fieldName)) {
-                    deserializedPhoneNumberAdministrativeDivision.localizedName = reader.getString();
-                } else if ("abbreviatedName".equals(fieldName)) {
-                    deserializedPhoneNumberAdministrativeDivision.abbreviatedName = reader.getString();
-                } else {
-                    reader.skipChildren();
-                }
-            }
-
-            return deserializedPhoneNumberAdministrativeDivision;
-        });
     }
 }
