d6c308cf71314955e3015161060c2208f40692fa
azure-sdk <azuresdk@microsoft.com>
2024-09-18 17:24:52 +0000

    Reset sources for azure-developer-loadtesting to the release version 1.0.16.

--- sdk/loadtesting/azure-developer-loadtesting/src/samples/java/com/azure/developer/loadtesting/HelloWorld.java
+++ sdk/loadtesting/azure-developer-loadtesting/src/samples/java/com/azure/developer/loadtesting/HelloWorld.java
@@ -3,49 +3,44 @@
 
 package com.azure.developer.loadtesting;
 
-import com.azure.core.exception.ClientAuthenticationException;
-import com.azure.core.exception.ResourceNotFoundException;
+import java.io.File;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
 import com.azure.core.http.rest.PagedIterable;
 import com.azure.core.http.rest.Response;
 import com.azure.core.util.BinaryData;
 import com.azure.core.util.polling.PollResponse;
 import com.azure.core.util.polling.SyncPoller;
 import com.azure.identity.DefaultAzureCredentialBuilder;
-import com.azure.json.JsonProviders;
-import com.azure.json.JsonReader;
-
-import java.io.File;
-import java.io.IOException;
-import java.time.Duration;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Sample demonstrates how to create and successfully run a test.
  */
-@SuppressWarnings("unchecked")
 public final class HelloWorld {
     /**
-     * Authenticates with the load testing resource and shows how to list tests, test files and test runs for a given
-     * resource.
+     * Authenticates with the load testing resource and shows how to list tests, test files and test runs
+     * for a given resource.
      *
      * @param args Unused. Arguments to the program.
      *
-     * @throws ClientAuthenticationException - when the credentials have insufficient permissions for load test
-     * resource.
+     * @throws ClientAuthenticationException - when the credentials have insufficient permissions for load test resource.
      * @throws ResourceNotFoundException - when test with `testId` does not exist when listing files.
      */
     public static void main(String[] args) {
         // Initialize the clients
         LoadTestAdministrationClient adminClient = new LoadTestAdministrationClientBuilder()
-            .credential(new DefaultAzureCredentialBuilder().build())
-            .endpoint("<endpoint>")
-            .buildClient();
+                .credential(new DefaultAzureCredentialBuilder().build())
+                .endpoint("<endpoint>")
+                .buildClient();
         LoadTestRunClient testRunClient = new LoadTestRunClientBuilder()
-            .credential(new DefaultAzureCredentialBuilder().build())
-            .endpoint("<endpoint>")
-            .buildClient();
+                .credential(new DefaultAzureCredentialBuilder().build())
+                .endpoint("<endpoint>")
+                .buildClient();
 
         // Constants and parameters
         final String testId = "6758667a-a57c-47e5-9cef-9b1f1432daca";
@@ -57,33 +52,33 @@ public final class HelloWorld {
          * BEGIN: Create test
          */
         // construct Test object using nested String:Object Maps
-        Map<String, Object> testMap = new HashMap<>();
+        Map<String, Object> testMap = new HashMap<String, Object>();
         testMap.put("displayName", "Sample Display Name");
         testMap.put("description", "Sample Description");
 
         // loadTestConfig describes the number of test engines to generate load
-        Map<String, Object> loadTestConfigMap = new HashMap<>();
+        Map<String, Object> loadTestConfigMap = new HashMap<String, Object>();
         loadTestConfigMap.put("engineInstances", 1);
         testMap.put("loadTestConfiguration", loadTestConfigMap);
 
         // environmentVariables are plain-text data passed to test engines
-        Map<String, Object> envVarMap = new HashMap<>();
+        Map<String, Object> envVarMap = new HashMap<String, Object>();
         envVarMap.put("a", "b");
         envVarMap.put("x", "y");
         testMap.put("environmentVariables", envVarMap);
 
         // secrets are secure data sent using Azure Key Vault
-        Map<String, Object> secretMap = new HashMap<>();
-        Map<String, Object> sampleSecretMap = new HashMap<>();
+        Map<String, Object> secretMap = new HashMap<String, Object>();
+        Map<String, Object> sampleSecretMap = new HashMap<String, Object>();
         sampleSecretMap.put("value", "https://samplevault.vault.azure.net/secrets/samplesecret/f113f91fd4c44a368049849c164db827");
         sampleSecretMap.put("type", "AKV_SECRET_URI");
         secretMap.put("sampleSecret", sampleSecretMap);
         testMap.put("secrets", secretMap);
 
         // passFailCriteria define the conditions to conclude the test as success
-        Map<String, Object> passFailMap = new HashMap<>();
-        Map<String, Object> passFailMetrics = new HashMap<>();
-        Map<String, Object> samplePassFailMetric = new HashMap<>();
+        Map<String, Object> passFailMap = new HashMap<String, Object>();
+        Map<String, Object> passFailMetrics = new HashMap<String, Object>();
+        Map<String, Object> samplePassFailMetric = new HashMap<String, Object>();
         samplePassFailMetric.put("clientmetric", "response_time_ms");
         samplePassFailMetric.put("aggregate", "percentage");
         samplePassFailMetric.put("condition", ">");
@@ -98,7 +93,6 @@ public final class HelloWorld {
 
         // receive response with BinaryData content
         Response<BinaryData> testOutResponse = adminClient.createOrUpdateTestWithResponse(testId, test, null);
-
         System.out.println(testOutResponse.getValue().toString());
         /*
          * END: Create test
@@ -112,9 +106,7 @@ public final class HelloWorld {
 
         // receive response with BinaryData content
         // NOTE: file name should be passed as input argument `testFileName`. File name in local path is ignored
-        PollResponse<BinaryData> fileUrlOut = adminClient.beginUploadTestFile(testId, testFileName, fileData, null)
-            .waitForCompletion(Duration.ofMinutes(2));
-
+        PollResponse<BinaryData> fileUrlOut = adminClient.beginUploadTestFile(testId, testFileName, fileData, null).waitForCompletion(Duration.ofMinutes(2));
         System.out.println(fileUrlOut.getValue().toString());
         /*
          * END: Upload test file
@@ -124,7 +116,7 @@ public final class HelloWorld {
          * BEGIN: Start test run
          */
         // construct Test Run object using nested String:Object Maps
-        Map<String, Object> testRunMap = new HashMap<>();
+        Map<String, Object> testRunMap = new HashMap<String, Object>();
         testRunMap.put("testId", testId);
         testRunMap.put("displayName", "SDK-Created-TestRun");
 
@@ -133,7 +125,6 @@ public final class HelloWorld {
 
         // receive response with BinaryData content
         SyncPoller<BinaryData, BinaryData> testRunPoller = testRunClient.beginTestRun(testRunId, testRun, null);
-
         System.out.println(testRunPoller.poll().getValue().toString());
         /*
          * END: Start test run
@@ -149,7 +140,6 @@ public final class HelloWorld {
         }
 
         Response<BinaryData> stoppedTestRunOut = testRunClient.stopTestRunWithResponse(testRunId, null);
-
         System.out.println(stoppedTestRunOut.getValue().toString());
         /*
          * END: Stop test run
@@ -160,18 +150,16 @@ public final class HelloWorld {
          */
         // wait for test to reach terminal state
         PollResponse<BinaryData> testRunOut = testRunPoller.poll();
+        JsonNode testRunJson = null;
         String testStatus = null, startDateTime = null, endDateTime = null;
-
         while (!testRunOut.getStatus().isComplete()) {
             testRunOut = testRunPoller.poll();
-
             // parse JSON and read status value
-            try (JsonReader jsonReader = JsonProviders.createReader(testRunOut.getValue().toBytes())) {
-                Map<String, Object> jsonTree = jsonReader.readMap(JsonReader::readUntyped);
-
-                testStatus = jsonTree.get("status").toString();
+            try {
+                testRunJson = new ObjectMapper().readTree(testRunOut.getValue().toString());
+                testStatus = testRunJson.get("status").asText();
                 System.out.println("Status of test run: " +  testStatus);
-            } catch (IOException e) {
+            } catch (JsonProcessingException e) {
                 e.printStackTrace();
                 // handle error condition
             }
@@ -184,13 +172,11 @@ public final class HelloWorld {
             }
         }
 
-        try (JsonReader jsonReader = JsonProviders.createReader(testRunPoller.getFinalResult().toBytes())) {
-            Map<String, Object> jsonTree = jsonReader.readMap(JsonReader::readUntyped);
-
-            startDateTime = jsonTree.get("startDateTime").toString();
-            endDateTime = jsonTree.get("endDateTime").toString();
-            System.out.println("Status of test run: " +  testStatus);
-        } catch (IOException e) {
+        try {
+            testRunJson = new ObjectMapper().readTree(testRunPoller.getFinalResult().toString());
+            startDateTime = testRunJson.get("startDateTime").asText();
+            endDateTime = testRunJson.get("endDateTime").asText();
+        } catch (JsonProcessingException e) {
             e.printStackTrace();
             // handle error condition
         }
@@ -198,14 +184,11 @@ public final class HelloWorld {
         // get list of all metric namespaces and pick the first one
         Response<BinaryData> metricNamespacesOut = testRunClient.getMetricNamespacesWithResponse(testRunId, null);
         String metricNamespace = null;
-
         // parse JSON and read first value
-        try (JsonReader jsonReader = JsonProviders.createReader(metricNamespacesOut.getValue().toBytes())) {
-            Map<String, Object> jsonTree = jsonReader.readMap(JsonReader::readUntyped);
-
-            List<Map<String, Object>> metricNamespaces = (List<Map<String, Object>>) jsonTree.get("value");
-            metricNamespace = metricNamespaces.get(0).get("metricNamespaceName").toString();
-        } catch (IOException e) {
+        try {
+            JsonNode metricNamespacesJson = new ObjectMapper().readTree(metricNamespacesOut.getValue().toString());
+            metricNamespace = metricNamespacesJson.get("value").get(0).get("metricNamespaceName").asText();
+        } catch (JsonProcessingException e) {
             e.printStackTrace();
             // handle error condition
         }
@@ -213,23 +196,17 @@ public final class HelloWorld {
         // get list of all metric definitions and pick the first one
         Response<BinaryData> metricDefinitionsOut = testRunClient.getMetricDefinitionsWithResponse(testRunId, metricNamespace, null);
         String metricName = null;
-
         // parse JSON and read first value
-        try (JsonReader jsonReader = JsonProviders.createReader(metricDefinitionsOut.getValue().toBytes())) {
-            Map<String, Object> jsonTree = jsonReader.readMap(JsonReader::readUntyped);
-
-            List<Object> metricDefinitions = (List<Object>) jsonTree.get("value");
-            Map<String, Object> firstMetricDefinition = (Map<String, Object>) metricDefinitions.get(0);
-            Map<String, Object> name = (Map<String, Object>) firstMetricDefinition.get("name");
-            metricName = name.get("value").toString();
-        } catch (IOException e) {
+        try {
+            JsonNode metricDefinitionsJson = new ObjectMapper().readTree(metricDefinitionsOut.getValue().toString());
+            metricName = metricDefinitionsJson.get("value").get(0).get("name").get("value").asText();
+        } catch (JsonProcessingException e) {
             e.printStackTrace();
             // handle error condition
         }
 
         // fetch client metrics using metric namespace and metric name
         PagedIterable<BinaryData> clientMetricsOut = testRunClient.listMetrics(testRunId, metricName, metricNamespace, startDateTime + '/' + endDateTime, null);
-
         clientMetricsOut.forEach((clientMetric) -> {
             System.out.println(clientMetric.toString());
         });
