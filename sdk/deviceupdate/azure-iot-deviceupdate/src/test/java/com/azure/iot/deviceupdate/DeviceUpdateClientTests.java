a3963f2600fe40962b0726416798e552fe2df818
azure-sdk <azuresdk@microsoft.com>
2024-09-18 17:46:04 +0000

    Reset sources for azure-iot-deviceupdate to the release version 1.0.21.

--- sdk/deviceupdate/azure-iot-deviceupdate/src/test/java/com/azure/iot/deviceupdate/DeviceUpdateClientTests.java
+++ sdk/deviceupdate/azure-iot-deviceupdate/src/test/java/com/azure/iot/deviceupdate/DeviceUpdateClientTests.java
@@ -1,40 +1,49 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-
 package com.azure.iot.deviceupdate;
 
+import com.azure.core.credential.AccessToken;
 import com.azure.core.exception.HttpResponseException;
 import com.azure.core.http.HttpClient;
 import com.azure.core.http.policy.HttpLogDetailLevel;
 import com.azure.core.http.policy.HttpLogOptions;
 import com.azure.core.http.rest.PagedFlux;
 import com.azure.core.http.rest.Response;
-import com.azure.core.test.TestProxyTestBase;
+import com.azure.core.test.TestBase;
+import com.azure.core.test.TestMode;
 import com.azure.core.util.BinaryData;
 import com.azure.core.util.polling.PollerFlux;
+import com.azure.identity.DefaultAzureCredentialBuilder;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
 
-import static com.azure.iot.deviceupdate.TestUtils.getCredential;
+import java.time.OffsetDateTime;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
-public class DeviceUpdateClientTests extends TestProxyTestBase {
-    private DeviceUpdateAsyncClient createClient() {
-        DeviceUpdateClientBuilder clientBuilder = new DeviceUpdateClientBuilder()
-            .endpoint(TestData.ACCOUNT_ENDPOINT)
-            .instanceId(TestData.INSTANCE_ID)
-            .httpClient(HttpClient.createDefault())
-            .httpLogOptions(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BODY_AND_HEADERS));
-        if (interceptorManager.isPlaybackMode()) {
-            clientBuilder.httpClient(interceptorManager.getPlaybackClient());
-        } else if (interceptorManager.isRecordMode()) {
-            clientBuilder.addPolicy(interceptorManager.getRecordPolicy());
-        }
+public class DeviceUpdateClientTests extends TestBase {
 
-        return clientBuilder.credential(getCredential(getTestMode())).buildAsyncClient();
+    private DeviceUpdateAsyncClient createClient() {
+        DeviceUpdateClientBuilder builder =
+            new DeviceUpdateClientBuilder()
+                .endpoint(TestData.ACCOUNT_ENDPOINT)
+                .instanceId(TestData.INSTANCE_ID)
+                .httpClient(HttpClient.createDefault())
+                .httpLogOptions(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BODY_AND_HEADERS));
+        if (getTestMode() == TestMode.PLAYBACK) {
+            builder.httpClient(interceptorManager.getPlaybackClient())
+                .credential(request -> Mono.just(new AccessToken("this_is_a_token", OffsetDateTime.MAX)));
+        } else if (getTestMode() == TestMode.RECORD) {
+            builder.addPolicy(interceptorManager.getRecordPolicy())
+                .credential(new DefaultAzureCredentialBuilder().build());
+        } else if (getTestMode() == TestMode.LIVE) {
+            builder.credential(new DefaultAzureCredentialBuilder().build());
+        }
+        return builder.buildAsyncClient();
     }
 
     @Test
