69431993c488d9ab180985a65efe55f6ac02f9b6
Bill Wert <billwert@microsoft.com>
2024-09-19 15:35:40 -0700

    Add failure case test

--- sdk/identity/azure-identity/src/test/java/com/azure/identity/AzurePipelinesCredentialTest.java
+++ sdk/identity/azure-identity/src/test/java/com/azure/identity/AzurePipelinesCredentialTest.java
@@ -4,16 +4,19 @@
 package com.azure.identity;
 
 import com.azure.core.credential.TokenRequestContext;
+import com.azure.core.exception.ClientAuthenticationException;
 import com.azure.core.test.TestProxyTestBase;
 import com.azure.core.test.annotation.LiveOnly;
 import com.azure.core.util.Configuration;
+import com.azure.core.util.logging.ClientLogger;
 import org.junit.jupiter.api.Test;
 import reactor.test.StepVerifier;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class AzurePipelinesCredentialTest extends TestProxyTestBase {
-
+    static ClientLogger LOGGER = new ClientLogger(AzurePipelinesCredentialTest.class);
     static String clientId = Configuration.getGlobalConfiguration().get("AZURE_SERVICE_CONNECTION_CLIENT_ID");
     static String tenantId = Configuration.getGlobalConfiguration().get("AZURE_SERVICE_CONNECTION_TENANT_ID");
 
@@ -51,5 +54,25 @@ public class AzurePipelinesCredentialTest extends TestProxyTestBase {
 
         // Act & Assert
         assertNotNull(credential.getTokenSync(new TokenRequestContext().addScopes("https://vault.azure.net/.default")));
+    }
+
+    @Test
+    @LiveOnly
+    public void testWithInvlaidSystemAccessToken() {
+        AzurePipelinesCredential credential = new AzurePipelinesCredentialBuilder()
+            .clientId(clientId)
+            .tenantId(tenantId)
+            .serviceConnectionId(serviceConnectionId)
+            .systemAccessToken("INVALID")
+            .build();
+
+        try {
+            credential.getTokenSync(new TokenRequestContext().addScopes("https://vault.azure.net/.default"));
+        } catch (ClientAuthenticationException e) {
+            LOGGER.verbose("Body: " + e.getResponse().getBodyAsString().block());
+            LOGGER.verbose("Status code: " + e.getResponse().getStatusCode());
+            fail();
+        }
+
     }
 }
