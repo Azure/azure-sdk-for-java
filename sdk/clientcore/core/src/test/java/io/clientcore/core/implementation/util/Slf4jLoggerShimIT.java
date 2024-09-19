f9114ad43d39bf6f7b23abaabcdbf9227ee2004a
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2024-09-19 16:09:52 -0400

    Attempt to alleviate flaky test

--- sdk/clientcore/core/src/test/java/io/clientcore/core/implementation/util/Slf4jLoggerShimIT.java
+++ sdk/clientcore/core/src/test/java/io/clientcore/core/implementation/util/Slf4jLoggerShimIT.java
@@ -2,9 +2,13 @@
 // Licensed under the MIT License.
 package io.clientcore.core.implementation.util;
 
-import io.clientcore.core.models.SocketConnection;
 import io.clientcore.core.util.ClientLogger;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.junit.jupiter.api.parallel.Isolated;
 import org.slf4j.Logger;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -15,7 +19,38 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 /**
  * Integration tests for {@link Slf4jLoggerShim} verifying the reflective invocation behaviors.
  */
+@Isolated("Mutates System.properties")
+@Execution(ExecutionMode.SAME_THREAD)
 public class Slf4jLoggerShimIT {
+    private static final String LOG_LEVEL_PREFIX = "org.slf4j.simpleLogger.log.";
+    private String slf4jLoggerShimITLogLevel;
+    private String slf4jLoggerShimLogLevel;
+
+    @BeforeEach
+    public void setupLogLevels() {
+        slf4jLoggerShimITLogLevel = System.setProperty(
+            LOG_LEVEL_PREFIX + "io.clientcore.core.implementation.util.Slf4jLoggerShimIT", "debug");
+        slf4jLoggerShimLogLevel = System.setProperty(
+            LOG_LEVEL_PREFIX + "io.clientcore.core.implementation.util.Slf4jLoggerShim", "info");
+    }
+
+    @AfterEach
+    public void resetLogLevels() {
+        if (slf4jLoggerShimITLogLevel == null) {
+            System.clearProperty(LOG_LEVEL_PREFIX + "io.clientcore.core.implementation.util.Slf4jLoggerShimIT");
+        } else {
+            System.setProperty(LOG_LEVEL_PREFIX + "io.clientcore.core.implementation.util.Slf4jLoggerShimIT",
+                slf4jLoggerShimITLogLevel);
+        }
+
+        if (slf4jLoggerShimLogLevel == null) {
+            System.clearProperty(LOG_LEVEL_PREFIX + "io.clientcore.core.implementation.util.Slf4jLoggerShim");
+        } else {
+            System.setProperty(LOG_LEVEL_PREFIX + "io.clientcore.core.implementation.util.Slf4jLoggerShim",
+                slf4jLoggerShimLogLevel);
+        }
+    }
+
     @Test
     public void slf4jOnClasspathEnablesNonDefaultLogger() {
         Object logger = Slf4jLoggerShim.createLogger(Slf4jLoggerShimIT.class.getName());
@@ -35,7 +70,7 @@ public class Slf4jLoggerShimIT {
         assertTrue(shim.canLogAtLevel(ClientLogger.LogLevel.WARNING));
         assertTrue(shim.canLogAtLevel(ClientLogger.LogLevel.ERROR));
 
-        shim = new Slf4jLoggerShim(SocketConnection.class);
+        shim = new Slf4jLoggerShim(Slf4jLoggerShim.class);
 
         // But the default log level should be INFO for everything else.
         assertFalse(shim.canLogAtLevel(ClientLogger.LogLevel.VERBOSE));
