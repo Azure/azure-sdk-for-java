012a19409f406c0c8b8dcb26bd27f0334a8521da
anuchandy <anuamd@hotmail.com>
2024-09-20 09:36:32 -0700

    Remove unused callback scheduling in session handler

--- sdk/core/azure-core-amqp/src/main/java/com/azure/core/amqp/implementation/handler/SessionHandler.java
+++ sdk/core/azure-core-amqp/src/main/java/com/azure/core/amqp/implementation/handler/SessionHandler.java
@@ -4,7 +4,6 @@
 package com.azure.core.amqp.implementation.handler;
 
 import com.azure.core.amqp.exception.AmqpErrorContext;
-import com.azure.core.amqp.exception.AmqpException;
 import com.azure.core.amqp.exception.SessionErrorContext;
 import com.azure.core.amqp.implementation.AmqpMetricsProvider;
 import com.azure.core.amqp.implementation.ExceptionUtil;
@@ -15,14 +14,11 @@ import org.apache.qpid.proton.engine.EndpointState;
 import org.apache.qpid.proton.engine.Event;
 import org.apache.qpid.proton.engine.Session;
 
-import java.io.IOException;
 import java.time.Duration;
 import java.util.Locale;
-import java.util.concurrent.RejectedExecutionException;
 
 import static com.azure.core.amqp.implementation.AmqpLoggingUtils.addErrorCondition;
 import static com.azure.core.amqp.implementation.ClientConstants.SESSION_ID_KEY;
-import static com.azure.core.amqp.implementation.ClientConstants.ERROR_DESCRIPTION_KEY;
 import static com.azure.core.amqp.implementation.ClientConstants.SESSION_NAME_KEY;
 
 /**
@@ -30,8 +26,6 @@ import static com.azure.core.amqp.implementation.ClientConstants.SESSION_NAME_KE
  */
 public class SessionHandler extends Handler {
     private final String sessionName;
-    private final Duration openTimeout;
-    private final ReactorDispatcher reactorDispatcher;
     private final AmqpMetricsProvider metricsProvider;
 
     /**
@@ -48,8 +42,6 @@ public class SessionHandler extends Handler {
         Duration openTimeout, AmqpMetricsProvider metricProvider) {
         super(connectionId, hostname);
         this.sessionName = sessionName;
-        this.openTimeout = openTimeout;
-        this.reactorDispatcher = reactorDispatcher;
         this.metricsProvider = metricProvider;
     }
 
@@ -76,27 +68,6 @@ public class SessionHandler extends Handler {
         addErrorCondition(logger.atVerbose(), e.getSession().getCondition()).addKeyValue(SESSION_NAME_KEY, sessionName)
             .addKeyValue(SESSION_ID_KEY, getId())
             .log("onSessionLocalOpen");
-
-        final Session session = e.getSession();
-
-        try {
-            reactorDispatcher.invoke(this::onSessionTimeout, this.openTimeout);
-        } catch (IOException | RejectedExecutionException ioException) {
-            logger.atInfo()
-                .addKeyValue(SESSION_NAME_KEY, sessionName)
-                .addKeyValue("reactorDispatcherError", ioException.getMessage())
-                .log("onSessionLocalOpen");
-
-            session.close();
-
-            final String message = String.format(Locale.US,
-                "onSessionLocalOpen connectionId[%s], entityName[%s], underlying IO of"
-                    + " reactorDispatcher faulted with error: %s",
-                getConnectionId(), sessionName, ioException.getMessage());
-            final Throwable exception = new AmqpException(false, message, ioException, getErrorContext());
-
-            onError(exception);
-        }
     }
 
     @Override
@@ -170,25 +141,5 @@ public class SessionHandler extends Handler {
             .addKeyValue(SESSION_ID_KEY, getId())
             .log("onSessionFinal.");
         close();
-    }
-
-    private void onSessionTimeout() {
-        logger.atWarning()
-            .addKeyValue(SESSION_NAME_KEY, sessionName)
-            .addKeyValue(ERROR_DESCRIPTION_KEY, "timeout")
-            .log("onSessionTimeout");
-
-        // It is supposed to close a local session to handle timeout exception.
-        // However, closing the session can result in NPE because of proton-j bug (https://issues.apache
-        // .org/jira/browse/PROTON-1939).
-        // And the bug will cause the reactor thread to stop processing pending tasks scheduled on the reactor and
-        // as a result task won't be completed at all.
-
-        // TODO: handle timeout error once the proton-j bug is fixed.
-        // if (!sessionCreated && !sessionOpenErrorDispatched) {
-        // logger.warning(
-        // "SessionTimeoutHandler.onEvent - connectionId[{}], entityName[{}], session open timed out.",
-        // this.connectionId, this.entityName);
-        // }
     }
 }
