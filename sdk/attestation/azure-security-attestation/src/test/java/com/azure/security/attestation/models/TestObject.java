93a9ba2df34601208362938dd88aae4570e931ef
Alan Zimmer <48699787+alzimmermsft@users.noreply.github.com>
2024-09-27 09:36:25 -0400

    Enable Spotless on Agrifood, Anomaly Detector, AOT, and Attestation data-plane (#41962)
    Enable Spotless on Agrifood, Anomaly Detector, AOT, and Attestation data-plane

--- sdk/attestation/azure-security-attestation/src/test/java/com/azure/security/attestation/models/TestObject.java
+++ sdk/attestation/azure-security-attestation/src/test/java/com/azure/security/attestation/models/TestObject.java
@@ -24,61 +24,72 @@ final class TestObject {
 
     @JsonProperty(value = "int")
     private int integer;
+
     public TestObject setInteger(int v) {
         integer = v;
         return this;
     }
+
     public int getInteger() {
         return integer;
     }
 
     @JsonProperty(value = "exp")
     private long expiration;
+
     public TestObject setExpiresOn(OffsetDateTime expirationTime) {
         this.expiration = expirationTime.toEpochSecond();
         return this;
     }
+
     public OffsetDateTime getExpiresOn() {
         return OffsetDateTime.ofInstant(Instant.EPOCH.plusSeconds(expiration), ZoneOffset.UTC);
     }
 
     @JsonProperty(value = "iat")
     private long issuedOn;
+
     public TestObject setIssuedOn(OffsetDateTime issuedOn) {
         this.issuedOn = issuedOn.toEpochSecond();
         return this;
     }
+
     public OffsetDateTime getIssuedOn() {
         return OffsetDateTime.ofInstant(Instant.EPOCH.plusSeconds(issuedOn), ZoneOffset.UTC);
     }
 
     @JsonProperty(value = "nbf")
     private long notBefore;
+
     public TestObject setNotBefore(OffsetDateTime notBefore) {
         this.notBefore = notBefore.toEpochSecond();
         return this;
     }
+
     public OffsetDateTime getNotBefore() {
         return OffsetDateTime.ofInstant(Instant.EPOCH.plusSeconds(notBefore), ZoneOffset.UTC);
     }
 
-
     @JsonProperty(value = "intArray")
     private int[] integerArray;
+
     public TestObject setIntegerArray(int[] v) {
         integerArray = v.clone();
         return this;
     }
+
     public int[] getIntegerArray() {
         return integerArray;
     }
 
     @JsonProperty(value = "iss")
     String issuer;
+
     public TestObject setIssuer(String iss) {
         issuer = iss;
         return this;
     }
+
     public String getIssuer() {
         return issuer;
     }
