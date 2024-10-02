f7a2e27f4365127d25a0b48a6e057a25961fc361
koen-mertens <138520871+koen-mertens@users.noreply.github.com>
2024-10-02 23:50:38 +0200

    Radiology insights ga (#41098)
    * SDK regenerated + test implementation
    
    * changes for using defaultAzureCredential
    
    * Trying to fix build
    
    * Merging in changes from previous fork
    
    * Updating assets
    
    * Fixing version
    
    * Regenerate SDK
    
    * patch client to input data instead of job
    
    * changes to samples with new input
    
    * Update version_client.txt
    
    * update version and release date
    
    * Update tsp-location.yaml
    
    * Regenerated SDK
    
    * Update RadiologyInsightsClientImpl.java
    
    * Update module-info.java
    
    * regenerate SDK after emitter fixes
    
    * Update module-info.java
    
    * Update RadiologyInsightsAsyncClient.java
    
    * Update RadiologyInsightsAsyncClient.java
    
    * remove unused imports
    
    * Update RadiologyInsightsClientTestBase.java
    
    * Update RadiologyInsightsClientTestBase.java
    
    * Update CHANGELOG.md
    
    * fix comments by reviewers
    
    * regen
    
    * modify handwritten code
    
    * modify sample
    
    * regen
    
    ---------
    
    Co-authored-by: hvanhoe <111971082+hvanhoe@users.noreply.github.com>
    Co-authored-by: Weidong Xu <weidxu@microsoft.com>

--- sdk/healthinsights/azure-health-insights-radiologyinsights/CHANGELOG.md
+++ sdk/healthinsights/azure-health-insights-radiologyinsights/CHANGELOG.md
@@ -1,14 +1,32 @@
 # Release History
 
-## 1.0.0-beta.2 (Unreleased)
+## 1.0.0 (2024-09-11)
+
+- GA release
 
 ### Features Added
 
+- Added sample code for all Inference types
+- Added tests for all Inference types
+
 ### Breaking Changes
 
-### Bugs Fixed
-
-### Other Changes
+- Request changes:
+    - POST call replaced with PUT (this change is taken care of automatically by the SDK)
+    - Method `RadiologyInsightsClient.beginInferRadiologyInsights()` expects a String specifying a unique job id as well as a RadiologyInsightsJob (wrapping a RadiologyInsightsData object) instead of a RadiologyInsightsData object
+    - Method `RadiologyInsightsAsyncClient.beginInferRadiologyInsights()` expects a String specifying a unique job id as well as a RadiologyInsightsJob (wrapping a RadiologyInsightsData object) instead of a RadiologyInsightsData object
+    - Class `Encounter` renamed into `PatientEncounter`
+    - Class `FhirR4Extendible` renamed into `OrderedProcedure`
+    - Method `PatientDocument.setCreatedDateTime()` renamed into `PatientDocument.setCreatedAt()`
+    - Method `PatientRecord.setPatientInfo()` renamed into `PatientRecord.setPatientDetails()`
+- Response changes:
+    - Class `FhirR4Extendible1` renamed into `RadiologyInsightsInference`
+    - Method `FollowupCommunicationInference.getDateTime()` renamed into `FollowupCommunicationInference.getCommunicatedAt()`
+    - Method `FollowupCommunicationInference.isWasAcknowledged()` renamed into `FollowupCommunicationInference.isAcknowledged()`
+    - Method `FollowupRecommendationInference.getEffectiveDateTime()` renamed into `FollowupRecommendationInference.getEffectiveAt()`
+    - Method `RadiologyInsightsJob.getCreatedDateTime()` renamed into `RadiologyInsightsJob.getCreatedAt()`
+    - Method `RadiologyInsightsJob.getExpirationDateTime()` renamed into `RadiologyInsightsJob.getExpiresAt()`
+    - Method `RadiologyInsightsJob.getLastUpdateDateTime()` renamed into `RadiologyInsightsJob.getUpdatedAt()`
 
 ## 1.0.0-beta.1 (2024-02-15)
 
--- sdk/healthinsights/azure-health-insights-radiologyinsights/src/main/java/com/azure/health/insights/radiologyinsights/models/RadiologyInsightsInferenceResult.java
+++ sdk/healthinsights/azure-health-insights-radiologyinsights/src/main/java/com/azure/health/insights/radiologyinsights/models/RadiologyInsightsInferenceResult.java
@@ -5,8 +5,11 @@ package com.azure.health.insights.radiologyinsights.models;
 
 import com.azure.core.annotation.Generated;
 import com.azure.core.annotation.Immutable;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.azure.json.JsonReader;
+import com.azure.json.JsonSerializable;
+import com.azure.json.JsonToken;
+import com.azure.json.JsonWriter;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -14,20 +17,18 @@ import java.util.List;
  * 'result' will contain an instance of RadiologyInsightsInferenceResult.
  */
 @Immutable
-public final class RadiologyInsightsInferenceResult {
+public final class RadiologyInsightsInferenceResult implements JsonSerializable<RadiologyInsightsInferenceResult> {
 
     /*
      * Results for the patients given in the request.
      */
     @Generated
-    @JsonProperty(value = "patientResults")
     private final List<RadiologyInsightsPatientResult> patientResults;
 
     /*
      * The version of the model used for inference, expressed as the model date.
      */
     @Generated
-    @JsonProperty(value = "modelVersion")
     private final String modelVersion;
 
     /**
@@ -37,10 +38,7 @@ public final class RadiologyInsightsInferenceResult {
      * @param modelVersion the modelVersion value to set.
      */
     @Generated
-    @JsonCreator
-    private RadiologyInsightsInferenceResult(
-        @JsonProperty(value = "patientResults") List<RadiologyInsightsPatientResult> patientResults,
-        @JsonProperty(value = "modelVersion") String modelVersion) {
+    private RadiologyInsightsInferenceResult(List<RadiologyInsightsPatientResult> patientResults, String modelVersion) {
         this.patientResults = patientResults;
         this.modelVersion = modelVersion;
     }
@@ -63,5 +61,47 @@ public final class RadiologyInsightsInferenceResult {
     @Generated
     public String getModelVersion() {
         return this.modelVersion;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Generated
+    @Override
+    public JsonWriter toJson(JsonWriter jsonWriter) throws IOException {
+        jsonWriter.writeStartObject();
+        jsonWriter.writeArrayField("patientResults", this.patientResults,
+            (writer, element) -> writer.writeJson(element));
+        jsonWriter.writeStringField("modelVersion", this.modelVersion);
+        return jsonWriter.writeEndObject();
+    }
+
+    /**
+     * Reads an instance of RadiologyInsightsInferenceResult from the JsonReader.
+     *
+     * @param jsonReader The JsonReader being read.
+     * @return An instance of RadiologyInsightsInferenceResult if the JsonReader was pointing to an instance of it, or
+     * null if it was pointing to JSON null.
+     * @throws IllegalStateException If the deserialized JSON object was missing any required properties.
+     * @throws IOException If an error occurs while reading the RadiologyInsightsInferenceResult.
+     */
+    @Generated
+    public static RadiologyInsightsInferenceResult fromJson(JsonReader jsonReader) throws IOException {
+        return jsonReader.readObject(reader -> {
+            List<RadiologyInsightsPatientResult> patientResults = null;
+            String modelVersion = null;
+            while (reader.nextToken() != JsonToken.END_OBJECT) {
+                String fieldName = reader.getFieldName();
+                reader.nextToken();
+                if ("patientResults".equals(fieldName)) {
+                    patientResults = reader.readArray(reader1 -> RadiologyInsightsPatientResult.fromJson(reader1));
+                } else if ("modelVersion".equals(fieldName)) {
+                    modelVersion = reader.getString();
+                } else {
+                    reader.skipChildren();
+                }
+            }
+            return new RadiologyInsightsInferenceResult(patientResults, modelVersion);
+        });
     }
 }
