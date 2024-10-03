c151e22f4d36c3903162a2eb8dc436a084a1e73d
Isabelle <ibrandes@microsoft.com>
2024-10-03 15:14:23 -0700

    adding missed recordings and fixing style

--- sdk/storage/azure-storage-blob-cryptography/src/main/java/com/azure/storage/blob/specialized/cryptography/EncryptedBlobLength.java
+++ sdk/storage/azure-storage-blob-cryptography/src/main/java/com/azure/storage/blob/specialized/cryptography/EncryptedBlobLength.java
@@ -5,8 +5,6 @@ package com.azure.storage.blob.specialized.cryptography;
 
 import com.azure.core.util.logging.ClientLogger;
 
-import java.util.function.Function;
-
 import static com.azure.storage.blob.specialized.cryptography.CryptographyConstants.ENCRYPTION_PROTOCOL_V1;
 import static com.azure.storage.blob.specialized.cryptography.CryptographyConstants.ENCRYPTION_PROTOCOL_V2;
 import static com.azure.storage.blob.specialized.cryptography.CryptographyConstants.ENCRYPTION_PROTOCOL_V2_1;
@@ -20,22 +18,22 @@ final class EncryptedBlobLength {
     private static final ClientLogger LOGGER = new ClientLogger(EncryptedBlobLength.class);
 
     static Long computeAdjustedBlobLength(EncryptionData encryptionData, Long encryptedLength) {
-            switch (encryptionData.getEncryptionAgent().getProtocol()) {
-                /*
-                 Technically, the total unencrypted length may be different for v1,
-                 but because this helper method is only used for partitioning ranged downloads,
-                 the size does not need to be adjusted for v1.
-                 */
-                case ENCRYPTION_PROTOCOL_V1:
-                    return encryptedLength;
-                case ENCRYPTION_PROTOCOL_V2:
-                case ENCRYPTION_PROTOCOL_V2_1:
-                    long regionLength = encryptionData.getEncryptedRegionInfo().getDataLength();
-                    long region = (long) Math.ceil((double) encryptedLength / (double) (regionLength + NONCE_LENGTH + TAG_LENGTH));
-                    long offset = (NONCE_LENGTH + TAG_LENGTH) * region;
-                    return encryptedLength - offset;
-                default:
-                    throw LOGGER.logExceptionAsError(new IllegalArgumentException("Unexpected protocol version"));
-            }
+        switch (encryptionData.getEncryptionAgent().getProtocol()) {
+            /*
+             Technically, the total unencrypted length may be different for v1,
+             but because this helper method is only used for partitioning ranged downloads,
+             the size does not need to be adjusted for v1.
+             */
+            case ENCRYPTION_PROTOCOL_V1:
+                return encryptedLength;
+            case ENCRYPTION_PROTOCOL_V2:
+            case ENCRYPTION_PROTOCOL_V2_1:
+                long regionLength = encryptionData.getEncryptedRegionInfo().getDataLength();
+                long region = (long) Math.ceil((double) encryptedLength / (double) (regionLength + NONCE_LENGTH + TAG_LENGTH));
+                long offset = (NONCE_LENGTH + TAG_LENGTH) * region;
+                return encryptedLength - offset;
+            default:
+                throw LOGGER.logExceptionAsError(new IllegalArgumentException("Unexpected protocol version"));
+        }
     }
 }
