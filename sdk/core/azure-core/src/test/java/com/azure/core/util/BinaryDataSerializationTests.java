8eb7bcff7903d6a62847dd03e0cf8aa67af0ff34
Weidong Xu <weidxu@microsoft.com>
2024-10-12 15:04:03 +0800

    test case for demonstration

--- sdk/core/azure-core/src/test/java/com/azure/core/util/BinaryDataSerializationTests.java
+++ sdk/core/azure-core/src/test/java/com/azure/core/util/BinaryDataSerializationTests.java
@@ -16,7 +16,7 @@ public class BinaryDataSerializationTests {
     private static final TypeReference<List<BinaryData>> TYPE_REFERENCE_LIST_BINARY_DATA = new TypeReference<List<BinaryData>>() {
     };
 
-    private static final String jsonString = "[1, \"hello\"]";
+    private static final String jsonString = "[1,\"hello\"]";
 
     @Test
     public void testBinaryDataInCollectionDeserialization() {
@@ -26,17 +26,37 @@ public class BinaryDataSerializationTests {
 
         Assertions.assertArrayEquals("1".getBytes(StandardCharsets.UTF_8), list.get(0).toBytes());
         Assertions.assertArrayEquals("\"hello\"".getBytes(StandardCharsets.UTF_8), list.get(1).toBytes());
+        // array lengths differ, Actual   :5, it is "hello" without quote marks
     }
 
     @Test
     public void testBinaryDataInCollectionSerialization() {
         List<BinaryData> list = new ArrayList<>();
-        list.add(BinaryData.fromString("1"));
-        list.add(BinaryData.fromString("\"hello\""));
+        list.add(BinaryData.fromObject(1));
+        list.add(BinaryData.fromObject("hello"));
+
+        Assertions.assertArrayEquals("1".getBytes(StandardCharsets.UTF_8), list.get(0).toBytes());
 
         BinaryData data = BinaryData.fromObject(list);
 
         String json = data.toString();
         Assertions.assertEquals(jsonString, json);
+    }
+
+    @Test
+    public void testBinaryDataInCollectionSerialization2() {
+        List<BinaryData> list = new ArrayList<>();
+        list.add(BinaryData.fromString("1"));
+        list.add(BinaryData.fromString("\"hello\""));
+
+        Assertions.assertArrayEquals("1".getBytes(StandardCharsets.UTF_8), list.get(0).toBytes());
+
+        BinaryData data = BinaryData.fromObject(list);
+
+        String json = data.toString();
+        Assertions.assertEquals(jsonString, json);
+        // Actual   :["1","\"hello\""]
+
+        // Actual   :["MQ==","ImhlbGxvIg=="], if BinaryData.fromBytes
     }
 }
