d8dce73b3c290d56cedc6eeabba83e4eda0a54a1
azure-sdk <azuresdk@microsoft.com>
2024-10-14 05:16:06 +0000

    [Automation] Generate Fluent Lite from Swagger azurearcdata#package-2021-08-01

--- sdk/azurearcdata/azure-resourcemanager-azurearcdata/src/samples/java/com/azure/resourcemanager/azurearcdata/generated/SqlManagedInstancesListByResourceGroupSamples.java
+++ sdk/azurearcdata/azure-resourcemanager-azurearcdata/src/samples/java/com/azure/resourcemanager/azurearcdata/generated/SqlManagedInstancesListByResourceGroupSamples.java
@@ -4,18 +4,22 @@
 
 package com.azure.resourcemanager.azurearcdata.generated;
 
-/** Samples for SqlManagedInstances ListByResourceGroup. */
+/**
+ * Samples for SqlManagedInstances ListByResourceGroup.
+ */
 public final class SqlManagedInstancesListByResourceGroupSamples {
     /*
-     * x-ms-original-file: specification/azurearcdata/resource-manager/Microsoft.AzureArcData/stable/2021-08-01/examples/ListByResourceGroupSqlManagedInstance.json
+     * x-ms-original-file:
+     * specification/azurearcdata/resource-manager/Microsoft.AzureArcData/stable/2021-08-01/examples/
+     * ListByResourceGroupSqlManagedInstance.json
      */
     /**
      * Sample code: Gets all SQL Instance in a resource group.
-     *
+     * 
      * @param manager Entry point to AzureArcDataManager.
      */
-    public static void getsAllSQLInstanceInAResourceGroup(
-        com.azure.resourcemanager.azurearcdata.AzureArcDataManager manager) {
+    public static void
+        getsAllSQLInstanceInAResourceGroup(com.azure.resourcemanager.azurearcdata.AzureArcDataManager manager) {
         manager.sqlManagedInstances().listByResourceGroup("testrg", com.azure.core.util.Context.NONE);
     }
 }
