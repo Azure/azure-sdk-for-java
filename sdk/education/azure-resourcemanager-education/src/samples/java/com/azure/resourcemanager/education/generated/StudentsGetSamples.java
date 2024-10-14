7e80905b7a29db1b1611559a3dc9dcec9039512d
azure-sdk <azuresdk@microsoft.com>
2024-10-14 05:17:12 +0000

    [Automation] Generate Fluent Lite from Swagger education#package-2021-12-01-preview

--- sdk/education/azure-resourcemanager-education/src/samples/java/com/azure/resourcemanager/education/generated/StudentsGetSamples.java
+++ sdk/education/azure-resourcemanager-education/src/samples/java/com/azure/resourcemanager/education/generated/StudentsGetSamples.java
@@ -4,22 +4,22 @@
 
 package com.azure.resourcemanager.education.generated;
 
-import com.azure.core.util.Context;
-
-/** Samples for Students Get. */
+/**
+ * Samples for Students Get.
+ */
 public final class StudentsGetSamples {
     /*
-     * x-ms-original-file: specification/education/resource-manager/Microsoft.Education/preview/2021-12-01-preview/examples/Student.json
+     * x-ms-original-file:
+     * specification/education/resource-manager/Microsoft.Education/preview/2021-12-01-preview/examples/Student.json
      */
     /**
      * Sample code: Student.
-     *
+     * 
      * @param manager Entry point to EducationManager.
      */
     public static void student(com.azure.resourcemanager.education.EducationManager manager) {
-        manager
-            .students()
-            .getWithResponse(
-                "{billingAccountName}", "{billingProfileName}", "{invoiceSectionName}", "{studentAlias}", Context.NONE);
+        manager.students()
+            .getWithResponse("{billingAccountName}", "{billingProfileName}", "{invoiceSectionName}", "{studentAlias}",
+                com.azure.core.util.Context.NONE);
     }
 }
