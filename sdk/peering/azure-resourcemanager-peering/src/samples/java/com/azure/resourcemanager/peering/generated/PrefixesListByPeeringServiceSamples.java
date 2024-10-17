1bf000ab25345ff7f3a07ed4151d3801e8a702e0
azure-sdk <azuresdk@microsoft.com>
2024-10-17 03:58:37 +0000

    [Automation] Generate Fluent Lite from Swagger peering#package-2021-01-01

--- sdk/peering/azure-resourcemanager-peering/src/samples/java/com/azure/resourcemanager/peering/generated/PrefixesListByPeeringServiceSamples.java
+++ sdk/peering/azure-resourcemanager-peering/src/samples/java/com/azure/resourcemanager/peering/generated/PrefixesListByPeeringServiceSamples.java
@@ -4,18 +4,22 @@
 
 package com.azure.resourcemanager.peering.generated;
 
-/** Samples for Prefixes ListByPeeringService. */
+/**
+ * Samples for Prefixes ListByPeeringService.
+ */
 public final class PrefixesListByPeeringServiceSamples {
     /*
-     * x-ms-original-file: specification/peering/resource-manager/Microsoft.Peering/stable/2021-01-01/examples/ListPrefixesByPeeringService.json
+     * x-ms-original-file:
+     * specification/peering/resource-manager/Microsoft.Peering/stable/2021-01-01/examples/ListPrefixesByPeeringService.
+     * json
      */
     /**
      * Sample code: List all the prefixes associated with the peering service.
-     *
+     * 
      * @param manager Entry point to PeeringManager.
      */
-    public static void listAllThePrefixesAssociatedWithThePeeringService(
-        com.azure.resourcemanager.peering.PeeringManager manager) {
+    public static void
+        listAllThePrefixesAssociatedWithThePeeringService(com.azure.resourcemanager.peering.PeeringManager manager) {
         manager.prefixes().listByPeeringService("rgName", "peeringServiceName", null, com.azure.core.util.Context.NONE);
     }
 }
