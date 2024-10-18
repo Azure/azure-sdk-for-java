247d5d3f6b199ad9eabd44b68e5398e75d7eaff0
azure-sdk <azuresdk@microsoft.com>
2024-10-18 21:39:49 +0000

    Reset sources for azure-mixedreality-remoterendering to the release version 1.1.33.

--- sdk/remoterendering/azure-mixedreality-remoterendering/src/main/java/module-info.java
+++ sdk/remoterendering/azure-mixedreality-remoterendering/src/main/java/module-info.java
@@ -5,7 +5,7 @@ module com.azure.mixedreality.remoterendering {
     requires transitive com.azure.core;
     requires com.azure.mixedreality.authentication;
 
-    opens com.azure.mixedreality.remoterendering.implementation.models to com.azure.core;
+    opens com.azure.mixedreality.remoterendering.implementation.models to com.fasterxml.jackson.databind, com.azure.core;
 
     exports com.azure.mixedreality.remoterendering;
     exports com.azure.mixedreality.remoterendering.models;
