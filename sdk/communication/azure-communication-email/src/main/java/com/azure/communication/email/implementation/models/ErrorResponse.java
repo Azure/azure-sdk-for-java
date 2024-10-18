434dae9d0a8112b1274c5add2bf311f933508f14
azure-sdk <azuresdk@microsoft.com>
2024-10-18 21:39:09 +0000

    Reset sources for azure-communication-email to the release version 1.0.17.

--- sdk/communication/azure-communication-email/src/main/java/com/azure/communication/email/implementation/models/ErrorResponse.java
+++ sdk/communication/azure-communication-email/src/main/java/com/azure/communication/email/implementation/models/ErrorResponse.java
@@ -5,34 +5,28 @@
 package com.azure.communication.email.implementation.models;
 
 import com.azure.core.annotation.Fluent;
-import com.azure.json.JsonReader;
-import com.azure.json.JsonSerializable;
-import com.azure.json.JsonToken;
-import com.azure.json.JsonWriter;
-import java.io.IOException;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * Error response
- * 
- * Common error response for all Azure Resource Manager APIs to return error details for failed operations. (This also
- * follows the OData error response format.).
+ *
+ * <p>Common error response for all Azure Resource Manager APIs to return error details for failed operations. (This
+ * also follows the OData error response format.).
  */
 @Fluent
-public final class ErrorResponse implements JsonSerializable<ErrorResponse> {
+public final class ErrorResponse {
     /*
      * The error object.
      */
+    @JsonProperty(value = "error")
     private ErrorDetail error;
 
-    /**
-     * Creates an instance of ErrorResponse class.
-     */
-    public ErrorResponse() {
-    }
+    /** Creates an instance of ErrorResponse class. */
+    public ErrorResponse() {}
 
     /**
      * Get the error property: The error object.
-     * 
+     *
      * @return the error value.
      */
     public ErrorDetail getError() {
@@ -41,48 +35,12 @@ public final class ErrorResponse implements JsonSerializable<ErrorResponse> {
 
     /**
      * Set the error property: The error object.
-     * 
+     *
      * @param error the error value to set.
      * @return the ErrorResponse object itself.
      */
     public ErrorResponse setError(ErrorDetail error) {
         this.error = error;
         return this;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public JsonWriter toJson(JsonWriter jsonWriter) throws IOException {
-        jsonWriter.writeStartObject();
-        jsonWriter.writeJsonField("error", this.error);
-        return jsonWriter.writeEndObject();
-    }
-
-    /**
-     * Reads an instance of ErrorResponse from the JsonReader.
-     * 
-     * @param jsonReader The JsonReader being read.
-     * @return An instance of ErrorResponse if the JsonReader was pointing to an instance of it, or null if it was
-     * pointing to JSON null.
-     * @throws IOException If an error occurs while reading the ErrorResponse.
-     */
-    public static ErrorResponse fromJson(JsonReader jsonReader) throws IOException {
-        return jsonReader.readObject(reader -> {
-            ErrorResponse deserializedErrorResponse = new ErrorResponse();
-            while (reader.nextToken() != JsonToken.END_OBJECT) {
-                String fieldName = reader.getFieldName();
-                reader.nextToken();
-
-                if ("error".equals(fieldName)) {
-                    deserializedErrorResponse.error = ErrorDetail.fromJson(reader);
-                } else {
-                    reader.skipChildren();
-                }
-            }
-
-            return deserializedErrorResponse;
-        });
     }
 }
