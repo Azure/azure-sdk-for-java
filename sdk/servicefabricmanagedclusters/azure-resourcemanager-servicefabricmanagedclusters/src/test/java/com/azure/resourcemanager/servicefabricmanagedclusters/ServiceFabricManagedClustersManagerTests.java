f1a7f60b0c5c2660a32761a4327229da71d842f2
Hong Li(MSFT) <74638143+v-hongli1@users.noreply.github.com>
2024-10-18 10:49:10 +0800

    Fix for register resource provider

--- sdk/servicefabricmanagedclusters/azure-resourcemanager-servicefabricmanagedclusters/src/test/java/com/azure/resourcemanager/servicefabricmanagedclusters/ServiceFabricManagedClustersManagerTests.java
+++ sdk/servicefabricmanagedclusters/azure-resourcemanager-servicefabricmanagedclusters/src/test/java/com/azure/resourcemanager/servicefabricmanagedclusters/ServiceFabricManagedClustersManagerTests.java
@@ -15,12 +15,17 @@ import com.azure.core.util.Configuration;
 import com.azure.core.util.CoreUtils;
 import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.azure.resourcemanager.resources.ResourceManager;
+import com.azure.resourcemanager.resources.fluentcore.utils.ResourceManagerUtils;
+import com.azure.resourcemanager.resources.models.Provider;
 import com.azure.resourcemanager.servicefabricmanagedclusters.models.ManagedCluster;
 import com.azure.resourcemanager.servicefabricmanagedclusters.models.Sku;
 import com.azure.resourcemanager.servicefabricmanagedclusters.models.SkuName;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.UUID;
 
@@ -37,14 +42,18 @@ public class ServiceFabricManagedClustersManagerTests extends TestProxyTestBase 
         final TokenCredential credential = new DefaultAzureCredentialBuilder().build();
         final AzureProfile profile = new AzureProfile(AzureEnvironment.AZURE);
 
-        serviceFabricManagedClustersManager = ServiceFabricManagedClustersManager.configure()
+        serviceFabricManagedClustersManager = ServiceFabricManagedClustersManager
+            .configure()
             .withLogOptions(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BASIC))
             .authenticate(credential, profile);
 
-        resourceManager = ResourceManager.configure()
+        resourceManager = ResourceManager
+            .configure()
             .withLogOptions(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BASIC))
             .authenticate(credential, profile)
             .withDefaultSubscription();
+
+        canRegisterProviders(Arrays.asList("Microsoft.ServiceFabric"));
 
         // use AZURE_RESOURCE_GROUP_NAME if run in LIVE CI
         String testResourceGroup = Configuration.getGlobalConfiguration().get("AZURE_RESOURCE_GROUP_NAME");
@@ -52,7 +61,10 @@ public class ServiceFabricManagedClustersManagerTests extends TestProxyTestBase 
         if (testEnv) {
             resourceGroupName = testResourceGroup;
         } else {
-            resourceManager.resourceGroups().define(resourceGroupName).withRegion(REGION).create();
+            resourceManager.resourceGroups()
+                .define(resourceGroupName)
+                .withRegion(REGION)
+                .create();
         }
     }
 
@@ -72,7 +84,8 @@ public class ServiceFabricManagedClustersManagerTests extends TestProxyTestBase 
             String adminUser = "user" + randomPadding();
             String adminPassWord = UUID.randomUUID().toString().replace("-", "@").substring(0, 13);
             // @embedmeStart
-            managedCluster = serviceFabricManagedClustersManager.managedClusters()
+            managedCluster = serviceFabricManagedClustersManager
+                .managedClusters()
                 .define(clusterName)
                 .withRegion(REGION)
                 .withExistingResourceGroup(resourceGroupName)
@@ -86,13 +99,8 @@ public class ServiceFabricManagedClustersManagerTests extends TestProxyTestBase 
             // @embedmeEnd
             managedCluster.refresh();
             Assertions.assertEquals(clusterName, managedCluster.name());
-            Assertions.assertEquals(clusterName,
-                serviceFabricManagedClustersManager.managedClusters().getById(managedCluster.id()).name());
-            Assertions.assertTrue(serviceFabricManagedClustersManager.managedClusters()
-                .listByResourceGroup(resourceGroupName)
-                .stream()
-                .findAny()
-                .isPresent());
+            Assertions.assertEquals(clusterName, serviceFabricManagedClustersManager.managedClusters().getById(managedCluster.id()).name());
+            Assertions.assertTrue(serviceFabricManagedClustersManager.managedClusters().listByResourceGroup(resourceGroupName).stream().findAny().isPresent());
         } finally {
             if (managedCluster != null) {
                 serviceFabricManagedClustersManager.managedClusters().deleteById(managedCluster.id());
@@ -102,5 +110,24 @@ public class ServiceFabricManagedClustersManagerTests extends TestProxyTestBase 
 
     private static String randomPadding() {
         return String.format("%05d", Math.abs(RANDOM.nextInt() % 100000));
+    }
+
+    /**
+     * Check and register service resources
+     *
+     * @param providerNamespaces the resource provider names
+     */
+    private void canRegisterProviders(List<String> providerNamespaces) {
+        providerNamespaces.forEach(providerNamespace -> {
+            Provider provider = resourceManager.providers().getByName(providerNamespace);
+            if (!"Registered".equalsIgnoreCase(provider.registrationState())
+                && !"Registering".equalsIgnoreCase(provider.registrationState())) {
+                provider = resourceManager.providers().register(providerNamespace);
+            }
+            while (!"Registered".equalsIgnoreCase(provider.registrationState())) {
+                ResourceManagerUtils.sleep(Duration.ofSeconds(5));
+                provider = resourceManager.providers().getByName(provider.namespace());
+            }
+        });
     }
 }
