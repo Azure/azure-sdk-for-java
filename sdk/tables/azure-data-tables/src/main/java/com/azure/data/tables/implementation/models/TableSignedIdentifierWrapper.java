5527866a0cb8c972c386102292c0252855d969b9
Alan Zimmer <48699787+alzimmermsft@users.noreply.github.com>
2024-10-21 15:32:34 -0400

    Use Swagger transforms and code customizations to remove translation layer (#42497)

--- sdk/tables/azure-data-tables/src/main/java/com/azure/data/tables/implementation/models/TableSignedIdentifierWrapper.java
+++ sdk/tables/azure-data-tables/src/main/java/com/azure/data/tables/implementation/models/TableSignedIdentifierWrapper.java
@@ -4,38 +4,38 @@
 
 package com.azure.data.tables.implementation.models;
 
-import java.util.List;
-
 import com.azure.core.util.CoreUtils;
+import com.azure.data.tables.models.TableSignedIdentifier;
 import com.azure.xml.XmlReader;
 import com.azure.xml.XmlSerializable;
 import com.azure.xml.XmlToken;
 import com.azure.xml.XmlWriter;
 import java.util.ArrayList;
+import java.util.List;
 import javax.xml.namespace.QName;
 import javax.xml.stream.XMLStreamException;
 
 /**
- * A wrapper around List&lt;SignedIdentifier&gt; which provides top-level metadata for serialization.
+ * A wrapper around List&lt;TableSignedIdentifier&gt; which provides top-level metadata for serialization.
  */
-public final class SignedIdentifierWrapper implements XmlSerializable<SignedIdentifierWrapper> {
-    private final List<SignedIdentifier> signedIdentifiers;
+public final class TableSignedIdentifierWrapper implements XmlSerializable<TableSignedIdentifierWrapper> {
+    private final List<TableSignedIdentifier> signedIdentifiers;
 
     /**
-     * Creates an instance of SignedIdentifierWrapper.
+     * Creates an instance of TableSignedIdentifierWrapper.
      * 
      * @param signedIdentifiers the list.
      */
-    public SignedIdentifierWrapper(List<SignedIdentifier> signedIdentifiers) {
+    public TableSignedIdentifierWrapper(List<TableSignedIdentifier> signedIdentifiers) {
         this.signedIdentifiers = signedIdentifiers;
     }
 
     /**
-     * Get the List&lt;SignedIdentifier&gt; contained in this wrapper.
+     * Get the List&lt;TableSignedIdentifier&gt; contained in this wrapper.
      * 
-     * @return the List&lt;SignedIdentifier&gt;.
+     * @return the List&lt;TableSignedIdentifier&gt;.
      */
-    public List<SignedIdentifier> items() {
+    public List<TableSignedIdentifier> items() {
         return signedIdentifiers;
     }
 
@@ -49,22 +49,22 @@ public final class SignedIdentifierWrapper implements XmlSerializable<SignedIden
         rootElementName = CoreUtils.isNullOrEmpty(rootElementName) ? "SignedIdentifiers" : rootElementName;
         xmlWriter.writeStartElement(rootElementName);
         if (signedIdentifiers != null) {
-            for (SignedIdentifier element : signedIdentifiers) {
+            for (TableSignedIdentifier element : signedIdentifiers) {
                 xmlWriter.writeXml(element, "SignedIdentifier");
             }
         }
         return xmlWriter.writeEndElement();
     }
 
-    public static SignedIdentifierWrapper fromXml(XmlReader xmlReader) throws XMLStreamException {
+    public static TableSignedIdentifierWrapper fromXml(XmlReader xmlReader) throws XMLStreamException {
         return fromXml(xmlReader, null);
     }
 
-    public static SignedIdentifierWrapper fromXml(XmlReader xmlReader, String rootElementName)
+    public static TableSignedIdentifierWrapper fromXml(XmlReader xmlReader, String rootElementName)
         throws XMLStreamException {
         rootElementName = CoreUtils.isNullOrEmpty(rootElementName) ? "SignedIdentifiers" : rootElementName;
         return xmlReader.readObject(rootElementName, reader -> {
-            List<SignedIdentifier> items = null;
+            List<TableSignedIdentifier> items = null;
 
             while (reader.nextElement() != XmlToken.END_ELEMENT) {
                 QName elementName = reader.getElementName();
@@ -74,12 +74,12 @@ public final class SignedIdentifierWrapper implements XmlSerializable<SignedIden
                         items = new ArrayList<>();
                     }
 
-                    items.add(SignedIdentifier.fromXml(reader));
+                    items.add(TableSignedIdentifier.fromXml(reader));
                 } else {
                     reader.nextElement();
                 }
             }
-            return new SignedIdentifierWrapper(items);
+            return new TableSignedIdentifierWrapper(items);
         });
     }
 }
