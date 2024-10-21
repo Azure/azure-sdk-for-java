2308afb583b9a68f0664673eff39a9aaa420c2cf
XiaofeiCao <xiaofeicao@microsoft.com>
2024-10-21 14:15:24 +0800

    remove disable

--- sdk/redisenterprise/azure-resourcemanager-redisenterprise/src/test/java/com/azure/resourcemanager/redisenterprise/RedisEnterpriseManagerTests.java
+++ sdk/redisenterprise/azure-resourcemanager-redisenterprise/src/test/java/com/azure/resourcemanager/redisenterprise/RedisEnterpriseManagerTests.java
@@ -23,7 +23,6 @@ import com.azure.resourcemanager.redisenterprise.models.TlsVersion;
 import com.azure.resourcemanager.resources.ResourceManager;
 import com.azure.resourcemanager.resources.fluentcore.policy.ProviderRegistrationPolicy;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.Random;
@@ -70,8 +69,6 @@ public class RedisEnterpriseManagerTests extends TestProxyTestBase {
 
     @Test
     @LiveOnly
-    @Disabled("Waiting Azure Redis team to register Microsoft.Cache for TME subscription.")
-    // todo(xiaofei) enable when registration is done
     public void testCreate() {
         Cluster cluster = null;
         try {
