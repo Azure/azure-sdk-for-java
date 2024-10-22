0a68acba4cd95dc5e17d3a40975493bf2099f422
Moary Chen <v-shilichen@microsoft.com>
2024-10-22 16:31:10 +0800

    Decouple application context for AzureRedisCredentials

--- sdk/spring/spring-cloud-azure-autoconfigure/src/main/java/com/azure/spring/cloud/autoconfigure/implementation/data/redis/lettuce/AzureRedisCredentials.java
+++ sdk/spring/spring-cloud-azure-autoconfigure/src/main/java/com/azure/spring/cloud/autoconfigure/implementation/data/redis/lettuce/AzureRedisCredentials.java
@@ -4,64 +4,41 @@
 package com.azure.spring.cloud.autoconfigure.implementation.data.redis.lettuce;
 
 import com.azure.core.credential.TokenCredential;
-import com.azure.identity.extensions.implementation.credential.TokenCredentialProviderOptions;
 import com.azure.identity.extensions.implementation.credential.provider.TokenCredentialProvider;
-import com.azure.identity.extensions.implementation.enums.AuthProperty;
 import com.azure.identity.extensions.implementation.template.AzureAuthenticationTemplate;
 import com.azure.spring.cloud.core.properties.PasswordlessProperties;
-import com.azure.spring.cloud.service.implementation.identity.credential.provider.SpringTokenCredentialProvider;
 import com.nimbusds.jwt.JWT;
 import com.nimbusds.jwt.JWTParser;
 import io.lettuce.core.RedisCredentials;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.util.StringUtils;
 
 import java.util.Objects;
 import java.util.Properties;
-
-import static com.azure.spring.cloud.service.implementation.identity.credential.provider.SpringTokenCredentialProvider.PASSWORDLESS_TOKEN_CREDENTIAL_BEAN_NAME;
 
 public class AzureRedisCredentials implements RedisCredentials {
 
     public static final Logger LOGGER = LoggerFactory.getLogger(AzureRedisCredentials.class);
     private final AzureAuthenticationTemplate azureAuthenticationTemplate;
     private final String username;
-    private static final String SPRING_CLOUD_AZURE_REDIS_PREFIX = "spring.data.redis.azure";
-    private String passwordlessPropertiesPrefix = SPRING_CLOUD_AZURE_REDIS_PREFIX;
-    private GenericApplicationContext applicationContext;
 
     /**
      * Create instance of Azure Redis Credentials
      */
     public AzureRedisCredentials(String username, PasswordlessProperties passwordlessProperties) {
-        this(null, null, username, passwordlessProperties);
+        Objects.requireNonNull(passwordlessProperties, "PasswordlessProperties is required");
+        azureAuthenticationTemplate = new AzureAuthenticationTemplate();
+        azureAuthenticationTemplate.init(passwordlessProperties.toPasswordlessProperties());
+        this.username = resolveUsername(azureAuthenticationTemplate, username);
     }
 
-    public AzureRedisCredentials(GenericApplicationContext applicationContext,
-                                 String username,
-                                 PasswordlessProperties passwordlessProperties) {
-        this(null, applicationContext, username, passwordlessProperties);
-    }
-
-    public AzureRedisCredentials(String passwordlessPropertiesPrefix,
-                                 GenericApplicationContext applicationContext,
-                                 String username,
+    public AzureRedisCredentials(String username,
+                                 TokenCredentialProvider tokenCredentialProvider,
                                  PasswordlessProperties passwordlessProperties) {
         Objects.requireNonNull(passwordlessProperties, "PasswordlessProperties is required");
-        this.applicationContext = applicationContext;
-        azureAuthenticationTemplate = new AzureAuthenticationTemplate();
+        azureAuthenticationTemplate = new AzureAuthenticationTemplate(tokenCredentialProvider, null);
         Properties properties = passwordlessProperties.toPasswordlessProperties();
-        if (applicationContext == null) {
-            LOGGER.debug("The applicationContext is not available, unable to obtain the Redis passwordless "
-                + "token credential bean, will use default credential.");
-        } else {
-            enhancePasswordlessProperties(properties, passwordlessProperties);
-        }
-        if (StringUtils.hasText(passwordlessPropertiesPrefix)) {
-            this.passwordlessPropertiesPrefix = passwordlessPropertiesPrefix;
-        }
         azureAuthenticationTemplate.init(properties);
         this.username = resolveUsername(azureAuthenticationTemplate, username);
     }
@@ -88,31 +65,6 @@ public class AzureRedisCredentials implements RedisCredentials {
         } catch (Exception e) {
             throw new IllegalArgumentException("Failed to parse the token, can't get the username from the token", e);
         }
-    }
-
-    private void enhancePasswordlessProperties(Properties properties, PasswordlessProperties passwordlessProperties) {
-        if (!passwordlessProperties.isPasswordlessEnabled()) {
-            if (!passwordlessProperties.isPasswordlessEnabled()) {
-                LOGGER.debug("Feature passwordless authentication is not enabled({}.passwordless-enabled=false), "
-                    + "skip enhancing Redis properties.", passwordlessPropertiesPrefix);
-                return;
-            }
-        }
-
-        String tokenCredentialBeanName = passwordlessProperties.getCredential().getTokenCredentialBeanName();
-        if (StringUtils.hasText(tokenCredentialBeanName)) {
-            AuthProperty.TOKEN_CREDENTIAL_BEAN_NAME.setProperty(properties, tokenCredentialBeanName);
-        } else {
-            TokenCredentialProvider tokenCredentialProvider = TokenCredentialProvider.createDefault(new TokenCredentialProviderOptions(properties));
-            TokenCredential tokenCredential = tokenCredentialProvider.get();
-
-            tokenCredentialBeanName = PASSWORDLESS_TOKEN_CREDENTIAL_BEAN_NAME + "." + passwordlessPropertiesPrefix;
-            AuthProperty.TOKEN_CREDENTIAL_BEAN_NAME.setProperty(properties, tokenCredentialBeanName);
-            applicationContext.registerBean(tokenCredentialBeanName, TokenCredential.class, () -> tokenCredential);
-        }
-
-        AuthProperty.TOKEN_CREDENTIAL_PROVIDER_CLASS_NAME.setProperty(properties, SpringTokenCredentialProvider.class.getName());
-        AuthProperty.AUTHORITY_HOST.setProperty(properties, passwordlessProperties.getProfile().getEnvironment().getActiveDirectoryEndpoint());
     }
 
     @Override
