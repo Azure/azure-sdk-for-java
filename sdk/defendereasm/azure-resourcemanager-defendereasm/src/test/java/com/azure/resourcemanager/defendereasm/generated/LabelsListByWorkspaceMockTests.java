6d2fc2f70a3c0e87ae08634c3e5aaab054addf63
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2024-10-24 12:55:22 -0400

    Enable Spotless on Defender EASM

--- sdk/defendereasm/azure-resourcemanager-defendereasm/src/test/java/com/azure/resourcemanager/defendereasm/generated/LabelsListByWorkspaceMockTests.java
+++ sdk/defendereasm/azure-resourcemanager-defendereasm/src/test/java/com/azure/resourcemanager/defendereasm/generated/LabelsListByWorkspaceMockTests.java
@@ -31,37 +31,27 @@ public final class LabelsListByWorkspaceMockTests {
         HttpResponse httpResponse = Mockito.mock(HttpResponse.class);
         ArgumentCaptor<HttpRequest> httpRequest = ArgumentCaptor.forClass(HttpRequest.class);
 
-        String responseStr =
-            "{\"value\":[{\"properties\":{\"displayName\":\"tehfiqscjeypvh\",\"color\":\"rkgqhcjrefo\",\"provisioningState\":\"ProvisioningResources\"},\"id\":\"qsl\",\"name\":\"yyvxyqjpkcattpn\",\"type\":\"jcrcczsqpjhvmda\"}]}";
+        String responseStr
+            = "{\"value\":[{\"properties\":{\"displayName\":\"tehfiqscjeypvh\",\"color\":\"rkgqhcjrefo\",\"provisioningState\":\"ProvisioningResources\"},\"id\":\"qsl\",\"name\":\"yyvxyqjpkcattpn\",\"type\":\"jcrcczsqpjhvmda\"}]}";
 
         Mockito.when(httpResponse.getStatusCode()).thenReturn(200);
         Mockito.when(httpResponse.getHeaders()).thenReturn(new HttpHeaders());
-        Mockito
-            .when(httpResponse.getBody())
+        Mockito.when(httpResponse.getBody())
             .thenReturn(Flux.just(ByteBuffer.wrap(responseStr.getBytes(StandardCharsets.UTF_8))));
-        Mockito
-            .when(httpResponse.getBodyAsByteArray())
+        Mockito.when(httpResponse.getBodyAsByteArray())
             .thenReturn(Mono.just(responseStr.getBytes(StandardCharsets.UTF_8)));
-        Mockito
-            .when(httpClient.send(httpRequest.capture(), Mockito.any()))
-            .thenReturn(
-                Mono
-                    .defer(
-                        () -> {
-                            Mockito.when(httpResponse.getRequest()).thenReturn(httpRequest.getValue());
-                            return Mono.just(httpResponse);
-                        }));
+        Mockito.when(httpClient.send(httpRequest.capture(), Mockito.any())).thenReturn(Mono.defer(() -> {
+            Mockito.when(httpResponse.getRequest()).thenReturn(httpRequest.getValue());
+            return Mono.just(httpResponse);
+        }));
 
-        EasmManager manager =
-            EasmManager
-                .configure()
-                .withHttpClient(httpClient)
-                .authenticate(
-                    tokenRequestContext -> Mono.just(new AccessToken("this_is_a_token", OffsetDateTime.MAX)),
-                    new AzureProfile("", "", AzureEnvironment.AZURE));
+        EasmManager manager = EasmManager.configure()
+            .withHttpClient(httpClient)
+            .authenticate(tokenRequestContext -> Mono.just(new AccessToken("this_is_a_token", OffsetDateTime.MAX)),
+                new AzureProfile("", "", AzureEnvironment.AZURE));
 
-        PagedIterable<LabelResource> response =
-            manager.labels().listByWorkspace("dcsi", "ynnaam", com.azure.core.util.Context.NONE);
+        PagedIterable<LabelResource> response
+            = manager.labels().listByWorkspace("dcsi", "ynnaam", com.azure.core.util.Context.NONE);
 
         Assertions.assertEquals("tehfiqscjeypvh", response.iterator().next().displayName());
         Assertions.assertEquals("rkgqhcjrefo", response.iterator().next().color());
