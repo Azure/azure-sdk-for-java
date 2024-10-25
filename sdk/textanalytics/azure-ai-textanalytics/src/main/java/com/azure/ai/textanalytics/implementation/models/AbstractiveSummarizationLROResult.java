3a8c00490a759ed1f5e83d7b8eb9b53219474fb2
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2024-10-25 08:55:58 -0400

    Regenerate Text Analytics

--- sdk/textanalytics/azure-ai-textanalytics/src/main/java/com/azure/ai/textanalytics/implementation/models/AbstractiveSummarizationLROResult.java
+++ sdk/textanalytics/azure-ai-textanalytics/src/main/java/com/azure/ai/textanalytics/implementation/models/AbstractiveSummarizationLROResult.java
@@ -5,6 +5,7 @@
 package com.azure.ai.textanalytics.implementation.models;
 
 import com.azure.core.annotation.Fluent;
+import com.azure.core.util.CoreUtils;
 import com.azure.json.JsonReader;
 import com.azure.json.JsonToken;
 import com.azure.json.JsonWriter;
@@ -125,8 +126,8 @@ public final class AbstractiveSummarizationLROResult extends AnalyzeTextLROResul
                 reader.nextToken();
 
                 if ("lastUpdateDateTime".equals(fieldName)) {
-                    deserializedAbstractiveSummarizationLROResult.setLastUpdateDateTime(
-                        reader.getNullable(nonNullReader -> OffsetDateTime.parse(nonNullReader.getString())));
+                    deserializedAbstractiveSummarizationLROResult.setLastUpdateDateTime(reader
+                        .getNullable(nonNullReader -> CoreUtils.parseBestOffsetDateTime(nonNullReader.getString())));
                 } else if ("status".equals(fieldName)) {
                     deserializedAbstractiveSummarizationLROResult.setStatus(State.fromString(reader.getString()));
                 } else if ("taskName".equals(fieldName)) {
