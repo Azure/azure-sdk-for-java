d549059bfc4fc932e0ace6fdbf787c8c3ee362dc
hongli750210 <v-hongli1@microsoft.com>
2024-10-29 16:39:35 +0800

    Register provider via policy For PostgreSqlFlexibleServer.

--- sdk/postgresql/azure-resourcemanager-postgresql/src/test/java/com/azure/resourcemanager/postgresql/PostgreSqlManagerTests.java
+++ sdk/postgresql/azure-resourcemanager-postgresql/src/test/java/com/azure/resourcemanager/postgresql/PostgreSqlManagerTests.java
@@ -16,7 +16,6 @@ import com.azure.core.util.CoreUtils;
 import com.azure.identity.AzurePowerShellCredentialBuilder;
 import com.azure.resourcemanager.postgresql.models.*;
 import com.azure.resourcemanager.resources.ResourceManager;
-import com.azure.resourcemanager.resources.fluentcore.policy.ProviderRegistrationPolicy;
 import io.netty.util.internal.StringUtil;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -37,14 +36,14 @@ public class PostgreSqlManagerTests extends TestProxyTestBase {
         final TokenCredential credential = new AzurePowerShellCredentialBuilder().build();
         final AzureProfile profile = new AzureProfile(AzureEnvironment.AZURE);
 
-        resourceManager = ResourceManager.configure()
-            .withLogOptions(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BASIC))
-            .authenticate(credential, profile).withDefaultSubscription();
-
         postgreSqlManager = PostgreSqlManager.configure()
             .withLogOptions(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BASIC))
-            .withPolicy(new ProviderRegistrationPolicy(resourceManager))
             .authenticate(credential, profile);
+
+        resourceManager = ResourceManager.configure()
+            .withLogOptions(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BASIC))
+            .authenticate(credential, profile)
+            .withDefaultSubscription();
 
         // use AZURE_RESOURCE_GROUP_NAME if run in LIVE CI
         String testResourceGroup = Configuration.getGlobalConfiguration().get("AZURE_RESOURCE_GROUP_NAME");
