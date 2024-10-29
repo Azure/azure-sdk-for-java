f750c87c70ac0a5e8309952e72355664cb40ff2b
hongli750210 <v-hongli1@microsoft.com>
2024-10-29 15:33:35 +0800

    Register provider via policy For CognitiveServices.

--- sdk/cognitiveservices/azure-resourcemanager-cognitiveservices/src/test/java/com/azure/resourcemanager/cognitiveservices/CognitiveServicesManagerTests.java
+++ sdk/cognitiveservices/azure-resourcemanager-cognitiveservices/src/test/java/com/azure/resourcemanager/cognitiveservices/CognitiveServicesManagerTests.java
@@ -38,7 +38,8 @@ public class CognitiveServicesManagerTests extends TestProxyTestBase {
 
         resourceManager = ResourceManager.configure()
             .withLogOptions(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BASIC))
-            .authenticate(credential, profile).withDefaultSubscription();
+            .authenticate(credential, profile)
+            .withDefaultSubscription();
 
         cognitiveServicesManager = CognitiveServicesManager.configure()
             .withLogOptions(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BASIC))
