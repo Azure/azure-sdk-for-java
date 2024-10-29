ae769e4e01d0381ac24cac290035cd8d5f795c92
hongli750210 <v-hongli1@microsoft.com>
2024-10-29 10:28:25 +0800

    Register provider via policy For Chaos.

--- sdk/chaos/azure-resourcemanager-chaos/src/test/java/com/azure/resourcemanager/chaos/ChaosManagerTests.java
+++ sdk/chaos/azure-resourcemanager-chaos/src/test/java/com/azure/resourcemanager/chaos/ChaosManagerTests.java
@@ -19,6 +19,7 @@ import com.azure.resourcemanager.chaos.models.Target;
 import com.azure.resourcemanager.keyvault.KeyVaultManager;
 import com.azure.resourcemanager.keyvault.models.SkuName;
 import com.azure.resourcemanager.resources.ResourceManager;
+import com.azure.resourcemanager.resources.fluentcore.policy.ProviderRegistrationPolicy;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -40,18 +41,19 @@ public class ChaosManagerTests extends TestProxyTestBase {
         final TokenCredential credential = new AzurePowerShellCredentialBuilder().build();
         final AzureProfile profile = new AzureProfile(AzureEnvironment.AZURE);
 
+        resourceManager = ResourceManager.configure()
+            .withLogOptions(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BASIC))
+            .authenticate(credential, profile).withDefaultSubscription();
+
         chaosManager = ChaosManager.configure()
             .withLogOptions(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BODY_AND_HEADERS))
+            .withPolicy(new ProviderRegistrationPolicy(resourceManager))
             .authenticate(credential, profile);
 
         keyVaultManager = KeyVaultManager.configure()
             .withLogOptions(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BASIC))
+            .withPolicy(new ProviderRegistrationPolicy(resourceManager))
             .authenticate(credential, profile);
-
-        resourceManager = ResourceManager.configure()
-            .withLogOptions(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BASIC))
-            .authenticate(credential, profile)
-            .withDefaultSubscription();
 
         // use AZURE_RESOURCE_GROUP_NAME if run in LIVE CI
         String testResourceGroup = Configuration.getGlobalConfiguration().get("AZURE_RESOURCE_GROUP_NAME");
