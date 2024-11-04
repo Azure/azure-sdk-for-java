4860ded043f7dc0e478069b5d1777eb47fc3764f
hongli750210 <v-hongli1@microsoft.com>
2024-11-04 14:51:38 +0800

    Register provider via policy For PostgreSqlFlexibleServer.

--- sdk/postgresqlflexibleserver/azure-resourcemanager-postgresqlflexibleserver/src/test/java/com/azure/resourcemanager/postgresqlflexibleserver/PostgreSqlManagerTests.java
+++ sdk/postgresqlflexibleserver/azure-resourcemanager-postgresqlflexibleserver/src/test/java/com/azure/resourcemanager/postgresqlflexibleserver/PostgreSqlManagerTests.java
@@ -42,7 +42,7 @@ import java.util.UUID;
 
 public class PostgreSqlManagerTests extends TestProxyTestBase {
     private static final Random RANDOM = new Random();
-    private static final Region REGION = Region.US_EAST;
+    private static final Region REGION = Region.EUROPE_WEST;
     private String resourceGroupName = "rg" + randomPadding();
     private PostgreSqlManager postgreSqlManager;
     private ResourceManager resourceManager;
