7a4deda4e4d67153d07d8071ffcb6bb0d4cd444c
Daniel Seiler <xiphoseer@mailbox.org>
2024-11-06 17:16:28 +0100

    [azure-spring-data-cosmos] Turn java.math.Big{Decimal,Integer} into simple types (#40239)
    * turn java.math.Big{Decimal,Integer} into simple types
    
    * update CHANGELOG.md
    
    * Adding integration tests.
    
    * Update CHANGELOG.md
    
    * Update BigJavaMathTypes.java
    
    Adding header
    
    ---------
    
    Co-authored-by: Trevor Anderson <trande@microsoft.com>
    Co-authored-by: Trevor Anderson <63077034+trande4884@users.noreply.github.com>

--- sdk/spring/azure-spring-data-cosmos/src/main/java/com/azure/spring/data/cosmos/core/mapping/CosmosSimpleTypes.java
+++ sdk/spring/azure-spring-data-cosmos/src/main/java/com/azure/spring/data/cosmos/core/mapping/CosmosSimpleTypes.java
@@ -5,6 +5,8 @@ package com.azure.spring.data.cosmos.core.mapping;
 
 import org.springframework.data.mapping.model.SimpleTypeHolder;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
@@ -20,6 +22,8 @@ final class CosmosSimpleTypes {
     static {
         Set<Class<?>> simpleTypes = new HashSet<>();
         simpleTypes.add(UUID.class);
+        simpleTypes.add(BigDecimal.class);
+        simpleTypes.add(BigInteger.class);
 
         COSMOS_SIMPLE_TYPES = Collections.unmodifiableSet(simpleTypes);
     }
