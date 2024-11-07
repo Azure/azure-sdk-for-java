c171ef43fe8155d9b6d4cdb7a83259d95ac7b66c
Alexa Thomases <athomases@microsoft.com>
2024-11-07 10:38:49 -0800

    Customization cont.

---
+++ sdk/healthdataaiservices/azure-health-deidentification/customization/src/main/java/ListJobsCustomization.java
@@ -0,0 +1,54 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import com.azure.autorest.customization.ClassCustomization;
+import com.azure.autorest.customization.Customization;
+import com.azure.autorest.customization.JavadocCustomization;
+import com.azure.autorest.customization.LibraryCustomization;
+import com.azure.autorest.customization.PackageCustomization;
+import com.github.javaparser.ParseProblemException;
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.Modifier;
+import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.body.Parameter;
+import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.stmt.CatchClause;
+import com.github.javaparser.ast.stmt.Statement;
+import com.github.javaparser.ast.stmt.TryStmt;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
+import com.github.javaparser.ast.type.Type;
+import org.slf4j.Logger;
+
+import java.util.function.Consumer;
+
+/**
+ * This customizes the generated Azure Deidentification client. The following changes are made by this customization:
+ * <li>Mark the listJobs() overload with continuationToken parameter in DeidentificationClient as private.</li>
+ * <li>Mark the listJobs() overload with continuationToken parameter in DeidentificationAsyncClient as privater.</li>
+ * <li>Mark the listJobDocuments() overload with continuationToken parameter in DeidentificationClient as private.</li>
+ * <li>Mark the listJobDocuments() overload with continuationToken parameter in DeidentificationAsyncClient as private.</li>
+ */
+public class ListJobsCustomization extends Customization {
+
+    @Override
+    public void customize(LibraryCustomization libraryCustomization, Logger logger) {
+        PackageCustomization models = libraryCustomization.getPackage("com.azure.health.deidentification");
+
+        ClassCustomization deidentificationClientClass = models.getClass("DeidentificationClient");
+        customizeClassByMarkingContinuationTokenOverloadsPrivate(deidentificationClientClass);
+
+        ClassCustomization deidentificationAsyncClientClass = models.getClass("DeidentificationAsyncClient");
+        customizeClassByMarkingContinuationTokenOverloadsPrivate(deidentificationAsyncClientClass);
+    }
+
+    private static void customizeClassByMarkingContinuationTokenOverloadsPrivate(ClassCustomization classCustomization) {
+        classCustomization.customizeAst(ast -> {
+            ClassOrInterfaceDeclaration clazz = ast.getClassByName(classCustomization.getClassName()).get();
+
+            clazz.getMethodsBySignature("listJobs", "String").get(0).setModifiers(Modifier.Keyword.PRIVATE);
+            clazz.getMethodsBySignature("listJobDocuments", "String", "String").get(0).setModifiers(Modifier.Keyword.PRIVATE);
+        });
+    }
+}
