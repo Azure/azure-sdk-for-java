f4045eb9b8e679171348f067f0ea2cfcd2e5bce5
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2024-11-15 09:42:22 -0500

    Merge Swagger and TypeSpec code generation into one script

--- eng/scripts/Compare-CurrentToCodegeneration.ps1
+++ eng/scripts/Compare-CurrentToCodegeneration.ps1
@@ -1,44 +1,54 @@
 <#
 .SYNOPSIS
 Invokes all 'Update-Codegeneration.ps1' scripts found within the specified directory and compares it against current
-code.
+code and syncs TypeSpec defintion and generates SDK for RPs under the input directory and have 'tsp-location.yaml'.
 
 .DESCRIPTION
 Invokes all 'Update-Codegeneration.ps1' scripts found within the specified directory and compares it against current
-code.
+code and syncs TypeSpec defintion and generates SDK for RPs under the input directory and have 'tsp-location.yaml'.
 
 If the regenerated code is different than the current code this will tell the differences, the files the differences
 are in, and exit with a failure status.
 
 .PARAMETER Directory
-The directory that will be searched for 'Update-Codegeneration.ps1' scripts. The default is the root directory of the
-Azure SDK for Java repository. CI jobs should use the 'ServiceDirectory', such as /sdk/storage.
+The directory that will be searched for 'Update-Codegeneration.ps1' scripts and 'tsp-location.yaml'. The default is
+the root directory of the Azure SDK for Java repository. One can also input service directory like:
+'-Directory /sdk/storage' or '-Directory sdk/anomalydetector/azure-ai-anomalydetector'.
 #>
 
 param(
-  [Parameter(Mandatory = $false)]
-  [string]$Directory
+    [Parameter(Mandatory = $false)]
+    [string]$Directory
 )
+
+function Reset-Repository {
+    # Clean up generated code, so that next step will not be affected.
+    git reset --hard
+    git clean -fd .
+}
 
 $path = ""
 if ($Directory) {
-  $path = $Directory
+    $path = $Directory
 }
 
 $swaggers = Get-ChildItem -Path $path -Filter "Update-Codegeneration.ps1" -Recurse
-if ($swaggers.Count -eq 0) {
-  Write-Host "
+$tspYamls = Get-ChildItem -Path $path -Filter "tsp-location.yaml" -Recurse
+if ($swaggers.Count -eq 0 -and $tspYamls.Count -eq 0) {
+    Write-Host "
 
-  ===========================================
-  No Swagger files to regenerate
-  ===========================================
+===========================================
+No Swagger or TypeSpec files to regenerate
+===========================================
 
-  "
-  exit 0
+"
+    exit 0
 }
 
-
-Write-Host "
+# Stores SDKs that failed to regenerate code successfully
+$failedSdk = $null
+if ($swaggers.Count -gt 0) {
+    Write-Host "
 
 ===================================
 Invoking Autorest code regeneration
@@ -46,8 +56,49 @@ Invoking Autorest code regeneration
 
 "
 
-foreach ($script in $swaggers) {
-  Invoke-Expression $script.FullName
+    foreach ($script in $swaggers) {
+        Invoke-Expression $script.FullName
+        if ($LastExitCode -ne 0) {
+            $failedSdk += $script.Directory.FullName
+        }
+    }
+}
+
+if ($tspYamls.Count -gt 0) {
+    Write-Host "
+
+===========================================
+Installing typespec-client-generator-cli
+===========================================
+
+"
+
+    npm install -g @azure-tools/typespec-client-generator-cli
+
+    Write-Host "
+
+===========================================
+Invoking tsp-client update
+===========================================
+
+"
+    foreach ($tspLocationPath in $tpsYamls) {
+        $sdkPath = (get-item $tspLocationPath).Directory.FullName
+        Write-Host "Generate SDK for $sdkPath"
+        Push-Location
+        Set-Location -Path $sdkPath
+        tsp-client update
+        if ($LastExitCode -ne 0) {
+            $failedSdk += $sdkPath
+        }
+        Pop-Location
+    }
+}
+
+if ($failedSdk.Length -gt 0) {
+    Write-Host "Code generation failed for following modules: $failedSdk"
+    Reset-Repository
+    exit 1
 }
 
 Write-Host "
@@ -59,7 +110,7 @@ Verify no diff
 "
 
 # prevent warning related to EOL differences which triggers an exception for some reason
-& git -c core.safecrlf=false diff --ignore-space-at-eol --exit-code -- "*.java"
+git -c core.safecrlf=false diff --ignore-space-at-eol --exit-code -- "*.java" ":(exclude)**/src/test/**" ":(exclude)**/src/samples/**" ":(exclude)**/src/main/**/implementation/**"
 
 if ($LastExitCode -ne 0) {
   $status = git status -s | Out-String
@@ -67,5 +118,13 @@ if ($LastExitCode -ne 0) {
 The following files are out of date:
 $status
 "
+  Reset-Repository
   exit 1
 }
+
+# Delete out TypeSpec temporary folders if they still exist.
+Get-ChildItem -Path $path -Filter TempTypeSpecFiles -Recurse -Directory | ForEach-Object {
+  Remove-Item -Path $_.FullName -Recurse -Force
+}
+
+Reset-Repository
