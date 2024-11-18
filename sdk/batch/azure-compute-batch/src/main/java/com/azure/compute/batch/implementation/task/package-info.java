9ef3488c66ee9d3688f56c13cc03bc1b19cf5b9f
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2024-11-18 13:17:39 -0500

    Migrate Batch

---
+++ sdk/batch/azure-compute-batch/src/main/java/com/azure/compute/batch/implementation/task/package-info.java
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+/**
+ * Package containing the classes for Batch task implementation.
+ */
+package com.azure.compute.batch.implementation.task;
