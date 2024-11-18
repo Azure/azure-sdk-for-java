7792e0b1abd8e5ed6bfdafb535c74f009e304f2b
Connie Yau <conniey@microsoft.com>
2024-11-18 11:45:27 -0800

    Add tests for OffsetDateTimeDescribedType

--- sdk/servicebus/azure-messaging-servicebus/src/main/java/com/azure/messaging/servicebus/implementation/MessageUtils.java
+++ sdk/servicebus/azure-messaging-servicebus/src/main/java/com/azure/messaging/servicebus/implementation/MessageUtils.java
@@ -391,7 +391,7 @@ public final class MessageUtils {
             long tickTime = (long) described - EPOCH_TICKS;
             int nano = (int) ((tickTime % TICK_PER_SECOND) * TIME_LENGTH_DELTA);
             long seconds = tickTime / TICK_PER_SECOND;
-            return (T) OffsetDateTime.ofInstant(Instant.ofEpochSecond(seconds, nano), ZoneId.systemDefault());
+            return (T) OffsetDateTime.ofInstant(Instant.ofEpochSecond(seconds, nano), ZoneOffset.UTC);
         } else if (ServiceBusConstants.DURATION_SYMBOL.equals(descriptor)) {
             return (T) Duration.ofNanos(((long) described) * TIME_LENGTH_DELTA);
         }
