9bc80c5b24c9af900f9adf750ecd45f05dc033d6
azure-sdk <azuresdk@microsoft.com>
2024-11-21 19:17:01 +0000

    Reset sources for azure-communication-rooms to the release version 1.1.7.

--- sdk/communication/azure-communication-rooms/src/main/java/com/azure/communication/rooms/implementation/converters/package-info.java
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-/**
- * Package containing converters for Azure Communication Room Service.
- */
-package com.azure.communication.rooms.implementation.converters;
