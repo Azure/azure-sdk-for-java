fec71cd2d239572dc501b9044bc35b585482f24d
XiaofeiCao <xiaofeicao@microsoft.com>
2024-11-26 17:01:06 +0800

    remove getAzureCloud

--- sdk/core/azure-core-management/src/test/java/com/azure/core/management/AzureProfileTests.java
+++ sdk/core/azure-core-management/src/test/java/com/azure/core/management/AzureProfileTests.java
@@ -13,22 +13,17 @@ public class AzureProfileTests {
         // normal case
         AzureProfile azurePublicCloud = new AzureProfile(AzureCloud.AZURE_PUBLIC_CLOUD);
         Assertions.assertEquals(AzureEnvironment.AZURE, azurePublicCloud.getEnvironment());
-        Assertions.assertEquals(AzureCloud.AZURE_PUBLIC_CLOUD, azurePublicCloud.getAzureCloud());
 
         AzureProfile azureChinaCloud = new AzureProfile(AzureCloud.AZURE_CHINA_CLOUD);
         Assertions.assertEquals(AzureEnvironment.AZURE_CHINA, azureChinaCloud.getEnvironment());
-        Assertions.assertEquals(AzureCloud.AZURE_CHINA_CLOUD, azureChinaCloud.getAzureCloud());
 
         AzureProfile azureUSGovernment = new AzureProfile(AzureCloud.AZURE_US_GOVERNMENT);
         Assertions.assertEquals(AzureEnvironment.AZURE_US_GOVERNMENT, azureUSGovernment.getEnvironment());
-        Assertions.assertEquals(AzureCloud.AZURE_US_GOVERNMENT, azureUSGovernment.getAzureCloud());
 
         // exception case
         // exception when initializing using custom AzureCloud
         Assertions.assertThrows(IllegalArgumentException.class, () -> new AzureProfile(AzureCloud.fromString("Custom")));
 
         AzureProfile customEnvironment = new AzureProfile(new AzureEnvironment(new HashMap<>()));
-        // exception when getting AzureCloud from custom AzureEnvironment
-        Assertions.assertThrows(IllegalArgumentException.class, customEnvironment::getAzureCloud);
     }
 }
