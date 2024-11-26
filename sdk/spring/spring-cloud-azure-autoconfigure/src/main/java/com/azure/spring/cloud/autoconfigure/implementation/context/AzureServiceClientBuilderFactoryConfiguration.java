63ae5c034a707ec7eadc25ad90681ece00f2c984
Moary Chen <v-shilichen@microsoft.com>
2024-11-26 14:24:55 +0800

    Fix warn logs: Mark the bean post processor related beans as internal to avoid the log 'Bean xxx of type xxx is not eligible for getting processed by all BeanPostProcessors'

---
+++ sdk/spring/spring-cloud-azure-autoconfigure/src/main/java/com/azure/spring/cloud/autoconfigure/implementation/context/AzureServiceClientBuilderFactoryConfiguration.java
@@ -0,0 +1,28 @@
+package com.azure.spring.cloud.autoconfigure.implementation.context;
+
+import com.azure.spring.cloud.core.implementation.factory.AbstractAzureServiceClientBuilderFactory;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Role;
+
+/**
+ * {@code @Configuration} class that registers a {@link AzureServiceClientBuilderFactoryPostProcessor}
+ * bean capable of processing the {@link AbstractAzureServiceClientBuilderFactory } bean.
+ *
+ * @since 5.19.0
+ */
+@Configuration(proxyBeanMethods = false)
+@Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+class AzureServiceClientBuilderFactoryConfiguration {
+
+    /**
+     * The BeanPostProcessor to apply the default token credential and resolver to all service client builder factories.
+     * @return the BPP.
+     */
+    @Bean
+    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+    static AzureServiceClientBuilderFactoryPostProcessor builderFactoryBeanPostProcessor() {
+        return new AzureServiceClientBuilderFactoryPostProcessor();
+    }
+}
