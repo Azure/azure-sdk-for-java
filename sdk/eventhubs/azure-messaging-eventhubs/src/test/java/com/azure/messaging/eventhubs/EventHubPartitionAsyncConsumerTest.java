f629b9fdb455004ec18a825d491792798ba0d6b6
Connie Yau <conniey@microsoft.com>
2024-11-26 13:12:19 -0800

    Fix test breaks using OffsetString

--- sdk/eventhubs/azure-messaging-eventhubs/src/test/java/com/azure/messaging/eventhubs/EventHubPartitionAsyncConsumerTest.java
+++ sdk/eventhubs/azure-messaging-eventhubs/src/test/java/com/azure/messaging/eventhubs/EventHubPartitionAsyncConsumerTest.java
@@ -88,10 +88,11 @@ class EventHubPartitionAsyncConsumerTest {
 
     private MessageFluxWrapper linkProcessor;
     private EventHubPartitionAsyncConsumer consumer;
+    private AutoCloseable closeable;
 
     @BeforeEach
-    void setup() {
-        MockitoAnnotations.initMocks(this);
+    public void setup() {
+        this.closeable = MockitoAnnotations.openMocks(this);
 
         when(retryPolicy.getRetryOptions()).thenReturn(new AmqpRetryOptions());
 
@@ -103,7 +104,11 @@ class EventHubPartitionAsyncConsumerTest {
     }
 
     @AfterEach
-    void teardown() {
+    public void tearDown() throws Exception {
+        if (closeable != null) {
+            closeable.close();
+        }
+
         Mockito.framework().clearInlineMock(this);
 
         if (consumer != null) {
@@ -165,8 +170,8 @@ class EventHubPartitionAsyncConsumerTest {
             CONSUMER_GROUP, PARTITION_ID, currentPosition, false);
 
         final Message message3 = mock(Message.class);
-        final Long secondOffset = 54L;
-        final Long lastOffset = 65L;
+        final String secondOffset = "54L";
+        final String lastOffset = "65L";
         final AmqpAnnotatedMessage annotatedMessage1
             = new AmqpAnnotatedMessage(AmqpMessageBody.fromData("Foo".getBytes(StandardCharsets.UTF_8)));
         final AmqpAnnotatedMessage annotatedMessage2
@@ -175,7 +180,7 @@ class EventHubPartitionAsyncConsumerTest {
             = new AmqpAnnotatedMessage(AmqpMessageBody.fromData("Baz".getBytes(StandardCharsets.UTF_8)));
 
         final EventData event1 = new EventData(annotatedMessage1,
-            getSystemProperties(annotatedMessage1, 25L, 14L, null), Context.NONE);
+            getSystemProperties(annotatedMessage1, "25L", 14L, null), Context.NONE);
         final EventData event2 = new EventData(annotatedMessage2,
             getSystemProperties(annotatedMessage2, secondOffset, 21L, 10L), Context.NONE);
         final EventData event3 = new EventData(annotatedMessage3,
@@ -202,7 +207,7 @@ class EventHubPartitionAsyncConsumerTest {
         // Assert that we have the current offset.
         final EventPosition firstPosition = currentPosition.get().get();
         Assertions.assertNotNull(firstPosition);
-        Assertions.assertEquals(secondOffset, Long.parseLong(firstPosition.getOffset()));
+        Assertions.assertEquals(secondOffset, firstPosition.getOffset());
         Assertions.assertFalse(firstPosition.isInclusive());
 
         StepVerifier.create(consumer.receive()).expectComplete().verify(DEFAULT_TIMEOUT);
@@ -224,8 +229,8 @@ class EventHubPartitionAsyncConsumerTest {
             CONSUMER_GROUP, PARTITION_ID, currentPosition, false);
 
         final Message message3 = mock(Message.class);
-        final long secondOffset = 54L;
-        final long lastOffset = 65L;
+        final String secondOffset = "54L";
+        final String lastOffset = "65L";
         final AmqpAnnotatedMessage annotatedMessage1
             = new AmqpAnnotatedMessage(AmqpMessageBody.fromData("Foo".getBytes(StandardCharsets.UTF_8)));
         final AmqpAnnotatedMessage annotatedMessage2
@@ -234,7 +239,7 @@ class EventHubPartitionAsyncConsumerTest {
             = new AmqpAnnotatedMessage(AmqpMessageBody.fromData("Baz".getBytes(StandardCharsets.UTF_8)));
 
         final EventData event1 = new EventData(annotatedMessage1,
-            getSystemProperties(annotatedMessage1, 25L, 14L, null), Context.NONE);
+            getSystemProperties(annotatedMessage1, "25L", 14L, null), Context.NONE);
         final EventData event2 = new EventData(annotatedMessage2,
             getSystemProperties(annotatedMessage2, secondOffset, 21L, 2L), Context.NONE);
         final EventData event3 = new EventData(annotatedMessage3,
@@ -313,10 +318,11 @@ class EventHubPartitionAsyncConsumerTest {
         }, FluxSink.OverflowStrategy.BUFFER);
     }
 
-    private static SystemProperties getSystemProperties(AmqpAnnotatedMessage amqpAnnotatedMessage, long offset,
+    private static SystemProperties getSystemProperties(AmqpAnnotatedMessage amqpAnnotatedMessage, String offsetString,
         long sequenceNumber, Long replicationSegment) {
 
-        amqpAnnotatedMessage.getMessageAnnotations().put(AmqpMessageConstant.OFFSET_ANNOTATION_NAME.getValue(), offset);
+        amqpAnnotatedMessage.getMessageAnnotations()
+            .put(AmqpMessageConstant.OFFSET_ANNOTATION_NAME.getValue(), offsetString);
         amqpAnnotatedMessage.getMessageAnnotations()
             .put(AmqpMessageConstant.SEQUENCE_NUMBER_ANNOTATION_NAME.getValue(), sequenceNumber);
         amqpAnnotatedMessage.getMessageAnnotations()
@@ -324,7 +330,8 @@ class EventHubPartitionAsyncConsumerTest {
         amqpAnnotatedMessage.getMessageAnnotations()
             .put(AmqpMessageConstant.REPLICATION_SEGMENT_ANNOTATION_NAME.getValue(), replicationSegment);
 
-        return new SystemProperties(amqpAnnotatedMessage, offset, TEST_DATE, sequenceNumber, null, null);
+        return new SystemProperties(amqpAnnotatedMessage, offsetString, TEST_DATE, sequenceNumber,
+            null, null);
     }
 
     private MessageFluxWrapper createLinkProcessor(boolean isV2) {
