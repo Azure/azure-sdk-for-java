6047199b483aad36f3d794522cf711457ec9d589
Harsimar Kaur (from Dev Box) <harskaur@microsoft.com>
2024-12-04 12:07:40 -0800

    move error tracker & validator functionality to a dif branch

--- sdk/monitor/azure-monitor-opentelemetry-autoconfigure/src/main/java/com/azure/monitor/opentelemetry/autoconfigure/implementation/quickpulse/filtering/DuplicateMetricIdException.java
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-package com.azure.monitor.opentelemetry.autoconfigure.implementation.quickpulse.filtering;
-
-public class DuplicateMetricIdException extends Exception {
-    public DuplicateMetricIdException(String message) {
-        super(message);
-    }
-}
--- sdk/monitor/azure-monitor-opentelemetry-autoconfigure/src/main/java/com/azure/monitor/opentelemetry/autoconfigure/implementation/quickpulse/filtering/UnexpectedFilterCreateException.java
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-package com.azure.monitor.opentelemetry.autoconfigure.implementation.quickpulse.filtering;
-
-public class UnexpectedFilterCreateException extends Exception {
-    public UnexpectedFilterCreateException(String message) {
-        super(message);
-    }
-}
