da2a72e58a3e5b39375eae17857057f08d0f8f79
Trevor Anderson <63077034+trande4884@users.noreply.github.com>
2024-12-05 14:22:07 -0500

    Update sdk/spring/azure-spring-data-cosmos/src/main/java/com/azure/spring/data/cosmos/exception/CosmosExceptionUtils.java
    Co-authored-by: Fabian Meiswinkel <fabian@meiswinkel.com>

--- sdk/spring/azure-spring-data-cosmos/src/main/java/com/azure/spring/data/cosmos/exception/CosmosExceptionUtils.java
+++ sdk/spring/azure-spring-data-cosmos/src/main/java/com/azure/spring/data/cosmos/exception/CosmosExceptionUtils.java
@@ -66,7 +66,7 @@ public class CosmosExceptionUtils {
                 case HttpConstants.StatusCodes.PRECONDITION_FAILED -> throw new CosmosPreconditionFailedException(message, cosmosException);
                 case HttpConstants.StatusCodes.REQUEST_ENTITY_TOO_LARGE -> throw new CosmosRequestEntityTooLargeException(message, cosmosException);
                 case HttpConstants.StatusCodes.TOO_MANY_REQUESTS -> throw new CosmosRequestRateTooLargeException(message, cosmosException);
-                case HttpConstants.StatusCodes.RETRY_WITH -> throw new CosmosRetryWithException(message, unwrappedThrowable);
+                case HttpConstants.StatusCodes.RETRY_WITH -> throw new CosmosRetryWithException(message, cosmosException);
                 case HttpConstants.StatusCodes.SERVICE_UNAVAILABLE -> throw new CosmosServiceUnavailableException(message, unwrappedThrowable);
                 case HttpConstants.StatusCodes.UNAUTHORIZED -> throw new CosmosUnauthorizedException(message, unwrappedThrowable);
                 default -> throw new CosmosAccessException(message, cosmosException);
