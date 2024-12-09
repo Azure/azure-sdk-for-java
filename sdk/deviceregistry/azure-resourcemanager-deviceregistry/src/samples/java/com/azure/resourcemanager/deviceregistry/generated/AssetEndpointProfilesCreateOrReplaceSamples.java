de09cc5789fd6d4cfcb4d014ec5cc579d8a87aac
azure-sdk <azuresdk@microsoft.com>
2024-12-09 07:29:38 +0000

    [Automation] External Change

--- sdk/deviceregistry/azure-resourcemanager-deviceregistry/src/samples/java/com/azure/resourcemanager/deviceregistry/generated/AssetEndpointProfilesCreateOrReplaceSamples.java
+++ sdk/deviceregistry/azure-resourcemanager-deviceregistry/src/samples/java/com/azure/resourcemanager/deviceregistry/generated/AssetEndpointProfilesCreateOrReplaceSamples.java
@@ -5,9 +5,9 @@
 package com.azure.resourcemanager.deviceregistry.generated;
 
 import com.azure.resourcemanager.deviceregistry.models.AssetEndpointProfileProperties;
+import com.azure.resourcemanager.deviceregistry.models.Authentication;
+import com.azure.resourcemanager.deviceregistry.models.AuthenticationMethod;
 import com.azure.resourcemanager.deviceregistry.models.ExtendedLocation;
-import com.azure.resourcemanager.deviceregistry.models.UserAuthentication;
-import com.azure.resourcemanager.deviceregistry.models.UserAuthenticationMode;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,7 +16,33 @@ import java.util.Map;
  */
 public final class AssetEndpointProfilesCreateOrReplaceSamples {
     /*
-     * x-ms-original-file: 2023-11-01-preview/Create_AssetEndpointProfile.json
+     * x-ms-original-file: 2024-11-01/Create_AssetEndpointProfile_With_DiscoveredAepRef.json
+     */
+    /**
+     * Sample code: Create_AssetEndpointProfile_With_DiscoveredAepRef.
+     * 
+     * @param manager Entry point to DeviceRegistryManager.
+     */
+    public static void createAssetEndpointProfileWithDiscoveredAepRef(
+        com.azure.resourcemanager.deviceregistry.DeviceRegistryManager manager) {
+        manager.assetEndpointProfiles()
+            .define("my-assetendpointprofile")
+            .withRegion("West Europe")
+            .withExistingResourceGroup("myResourceGroup")
+            .withExtendedLocation(new ExtendedLocation().withType("CustomLocation")
+                .withName(
+                    "/subscriptions/00000000-0000-0000-0000-000000000000/resourcegroups/myResourceGroup/providers/microsoft.extendedlocation/customlocations/location1"))
+            .withTags(mapOf("site", "building-1"))
+            .withProperties(
+                new AssetEndpointProfileProperties().withTargetAddress("https://www.example.com/myTargetAddress")
+                    .withEndpointProfileType("myEndpointProfileType")
+                    .withAuthentication(new Authentication().withMethod(AuthenticationMethod.ANONYMOUS))
+                    .withDiscoveredAssetEndpointProfileRef("discoveredAssetEndpointProfile1"))
+            .create();
+    }
+
+    /*
+     * x-ms-original-file: 2024-11-01/Create_AssetEndpointProfile.json
      */
     /**
      * Sample code: Create_AssetEndpointProfile.
@@ -35,7 +61,8 @@ public final class AssetEndpointProfilesCreateOrReplaceSamples {
             .withTags(mapOf("site", "building-1"))
             .withProperties(
                 new AssetEndpointProfileProperties().withTargetAddress("https://www.example.com/myTargetAddress")
-                    .withUserAuthentication(new UserAuthentication().withMode(UserAuthenticationMode.ANONYMOUS)))
+                    .withEndpointProfileType("myEndpointProfileType")
+                    .withAuthentication(new Authentication().withMethod(AuthenticationMethod.ANONYMOUS)))
             .create();
     }
 
