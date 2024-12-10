099ea875ec105cd035018bf2626ae19aecc438cb
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2024-12-10 14:30:29 -0500

    Update Storage to remove default or removed Autorest configs

--- sdk/storage/azure-storage-file-share/swagger/src/main/java/ShareStorageCustomization.java
+++ sdk/storage/azure-storage-file-share/swagger/src/main/java/ShareStorageCustomization.java
@@ -8,7 +8,6 @@ import com.azure.autorest.customization.PackageCustomization;
 import com.github.javaparser.ParseProblemException;
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.NodeList;
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.body.Parameter;
 import com.github.javaparser.ast.stmt.BlockStmt;
@@ -37,132 +36,7 @@ public class ShareStorageCustomization extends Customization {
 
         models.getClass("AccessRight").rename("ShareFileHandleAccessRights");
 
-        customizeShareFileRangeList(models.getClass("ShareFileRangeList"));
-        customizeFilesAndDirectoriesListSegment(
-            customization.getPackage("com.azure.storage.file.share.implementation.models")
-                .getClass("FilesAndDirectoriesListSegment"));
-
         updateImplToMapInternalException(customization.getPackage("com.azure.storage.file.share.implementation"));
-
-        addMissingHashMapImport(customization.getPackage("com.azure.storage.file.share.implementation.models"));
-    }
-
-    // ShareFileRangeList has special serialization behaviors which Swagger cannot define correctly. It has a single
-    // outer XML element "<Ranges>" which can contain a list of both "<Range>" and "<ClearRange>" elements,
-    // intermixed. Swagger isn't capable of defining this, so it ends up thinking there are two separate lists wrapped
-    // with "<Ranges>" and "<ClearRanges>". We need to manually correct this.
-    private static void customizeShareFileRangeList(ClassCustomization classCustomization) {
-        classCustomization.customizeAst(ast -> {
-            ClassOrInterfaceDeclaration clazz = ast.getClassByName(classCustomization.getClassName()).get();
-
-            clazz.getMethodsBySignature("toXml", "XmlWriter", "String").get(0)
-                .setBody(StaticJavaParser.parseBlock(String.join("\n",
-                    "{",
-                    "rootElementName = CoreUtils.isNullOrEmpty(rootElementName) ? \"Ranges\" : rootElementName;",
-                    "xmlWriter.writeStartElement(rootElementName);",
-                    "if (this.ranges != null) {",
-                    "    for (FileRange element : this.ranges) {",
-                    "        xmlWriter.writeXml(element, \"Range\");",
-                    "    }",
-                    "}",
-                    "if (this.clearRanges != null) {",
-                    "    for (ClearRange element : this.clearRanges) {",
-                    "        xmlWriter.writeXml(element, \"ClearRange\");",
-                    "    }",
-                    "}",
-                    "return xmlWriter.writeEndElement();",
-                    "}"
-                )));
-
-            clazz.getMethodsBySignature("fromXml", "XmlReader", "String").get(0)
-                .setBody(StaticJavaParser.parseBlock(String.join("\n",
-                    "{",
-                    "String finalRootElementName = CoreUtils.isNullOrEmpty(rootElementName) ? \"Ranges\" : rootElementName;",
-                    "return xmlReader.readObject(finalRootElementName, reader -> {",
-                    "    ShareFileRangeList deserializedShareFileRangeList = new ShareFileRangeList();",
-                    "    while (reader.nextElement() != XmlToken.END_ELEMENT) {",
-                    "        QName elementName = reader.getElementName();",
-                    "",
-                    "        if (\"Range\".equals(elementName.getLocalPart())) {",
-                    "            if (deserializedShareFileRangeList.ranges == null) {",
-                    "                deserializedShareFileRangeList.ranges = new ArrayList<>();",
-                    "            }",
-                    "            deserializedShareFileRangeList.ranges.add(FileRange.fromXml(reader, \"Range\"));",
-                    "        } else if (\"ClearRange\".equals(elementName.getLocalPart())) {",
-                    "            if (deserializedShareFileRangeList.clearRanges == null) {",
-                    "                deserializedShareFileRangeList.clearRanges = new ArrayList<>();",
-                    "            }",
-                    "            deserializedShareFileRangeList.clearRanges.add(ClearRange.fromXml(reader, \"ClearRange\"));",
-                    "        } else {",
-                    "            reader.skipElement();",
-                    "        }",
-                    "    }",
-                    "",
-                    "    return deserializedShareFileRangeList;",
-                    "});",
-                    "}"
-                )));
-        });
-    }
-
-    // FilesAndDirectoriesListSegment has special serialization behaviors which Swagger cannot define correctly. It has
-    // a single outer XML element "<Entries>" which can contain a list of both "<Directory>" and "<File>" elements,
-    // intermixed. Swagger isn't capable of defining this, so it ends up thinking there are two separate lists wrapped
-    // with "<Directories>" and "<Files>". We need to manually correct this.
-    private static void customizeFilesAndDirectoriesListSegment(ClassCustomization classCustomization) {
-        classCustomization.customizeAst(ast -> {
-            ClassOrInterfaceDeclaration clazz = ast.getClassByName(classCustomization.getClassName()).get();
-
-            clazz.getMethodsBySignature("toXml", "XmlWriter", "String").get(0)
-                .setBody(StaticJavaParser.parseBlock(String.join("\n",
-                    "{",
-                    "rootElementName = CoreUtils.isNullOrEmpty(rootElementName) ? \"Entries\" : rootElementName;",
-                    "xmlWriter.writeStartElement(rootElementName);",
-                    "if (this.directoryItems != null) {",
-                    "    for (DirectoryItem element : this.directoryItems) {",
-                    "        xmlWriter.writeXml(element, \"Directory\");",
-                    "    }",
-                    "}",
-                    "if (this.fileItems != null) {",
-                    "    for (FileItem element : this.fileItems) {",
-                    "        xmlWriter.writeXml(element, \"File\");",
-                    "    }",
-                    "}",
-                    "return xmlWriter.writeEndElement();",
-                    "}"
-                )));
-
-            clazz.getMethodsBySignature("fromXml", "XmlReader", "String").get(0)
-                .setBody(StaticJavaParser.parseBlock(String.join("\n",
-                    "{",
-                    "String finalRootElementName = CoreUtils.isNullOrEmpty(rootElementName) ? \"Entries\" : rootElementName;",
-                    "return xmlReader.readObject(finalRootElementName, reader -> {",
-                    "    FilesAndDirectoriesListSegment deserializedFilesAndDirectoriesListSegment",
-                    "        = new FilesAndDirectoriesListSegment();",
-                    "    while (reader.nextElement() != XmlToken.END_ELEMENT) {",
-                    "        QName elementName = reader.getElementName();",
-                    "",
-                    "        if (\"Directory\".equals(elementName.getLocalPart())) {",
-                    "            if (deserializedFilesAndDirectoriesListSegment.directoryItems == null) {",
-                    "                deserializedFilesAndDirectoriesListSegment.directoryItems = new ArrayList<>();",
-                    "            }",
-                    "            deserializedFilesAndDirectoriesListSegment.directoryItems",
-                    "                .add(DirectoryItem.fromXml(reader, \"Directory\"));",
-                    "        } else if (\"File\".equals(elementName.getLocalPart())) {",
-                    "            if (deserializedFilesAndDirectoriesListSegment.fileItems == null) {",
-                    "                deserializedFilesAndDirectoriesListSegment.fileItems = new ArrayList<>();",
-                    "            }",
-                    "            deserializedFilesAndDirectoriesListSegment.fileItems.add(FileItem.fromXml(reader, \"File\"));",
-                    "        } else {",
-                    "            reader.skipElement();",
-                    "        }",
-                    "    }",
-                    "",
-                    "    return deserializedFilesAndDirectoriesListSegment;",
-                    "});",
-                    "}"
-                )));
-        });
     }
 
     /**
@@ -266,12 +140,5 @@ public class ShareStorageCustomization extends Customization {
 
         // Replace the last statement with the try-catch block.
         method.setBody(new BlockStmt(new NodeList<>(tryCatchMap)));
-    }
-
-    // Temporary fix to a bug in Autorest.
-    private static void addMissingHashMapImport(PackageCustomization implementationModels) {
-        for (String className : Arrays.asList("FilesDownloadHeaders", "FilesGetPropertiesHeaders", "DirectoriesGetPropertiesHeaders", "SharesGetPropertiesHeaders")) {
-            implementationModels.getClass(className).addImports("java.util.HashMap");
-        }
     }
 }
