ea0d42f253a6e9cd2bd796e30a9196bc8bccbbc6
azure-sdk <azuresdk@microsoft.com>
2024-12-11 06:01:29 +0000

    [Automation] Generate Fluent Lite from Swagger iothub#package-2023-06

--- sdk/iothub/azure-resourcemanager-iothub/src/main/java/com/azure/resourcemanager/iothub/models/AuthenticationType.java
+++ sdk/iothub/azure-resourcemanager-iothub/src/main/java/com/azure/resourcemanager/iothub/models/AuthenticationType.java
@@ -5,20 +5,25 @@
 package com.azure.resourcemanager.iothub.models;
 
 import com.azure.core.util.ExpandableStringEnum;
-import com.fasterxml.jackson.annotation.JsonCreator;
 import java.util.Collection;
 
-/** Specifies authentication type being used for connecting to the storage account. */
+/**
+ * Specifies authentication type being used for connecting to the storage account.
+ */
 public final class AuthenticationType extends ExpandableStringEnum<AuthenticationType> {
-    /** Static value keyBased for AuthenticationType. */
+    /**
+     * Static value keyBased for AuthenticationType.
+     */
     public static final AuthenticationType KEY_BASED = fromString("keyBased");
 
-    /** Static value identityBased for AuthenticationType. */
+    /**
+     * Static value identityBased for AuthenticationType.
+     */
     public static final AuthenticationType IDENTITY_BASED = fromString("identityBased");
 
     /**
      * Creates a new instance of AuthenticationType value.
-     *
+     * 
      * @deprecated Use the {@link #fromString(String)} factory method.
      */
     @Deprecated
@@ -27,18 +32,17 @@ public final class AuthenticationType extends ExpandableStringEnum<Authenticatio
 
     /**
      * Creates or finds a AuthenticationType from its string representation.
-     *
+     * 
      * @param name a name to look for.
      * @return the corresponding AuthenticationType.
      */
-    @JsonCreator
     public static AuthenticationType fromString(String name) {
         return fromString(name, AuthenticationType.class);
     }
 
     /**
      * Gets known AuthenticationType values.
-     *
+     * 
      * @return known AuthenticationType values.
      */
     public static Collection<AuthenticationType> values() {
