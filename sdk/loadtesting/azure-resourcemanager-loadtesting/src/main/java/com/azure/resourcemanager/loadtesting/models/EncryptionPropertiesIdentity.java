d9ff34301388f2e7c938e351dd08bfdd0f9e2100
azure-sdk <azuresdk@microsoft.com>
2024-12-12 04:23:31 +0000

    [Automation] Generate Fluent Lite from Swagger loadtestservice#package-2022-12-01

--- sdk/loadtesting/azure-resourcemanager-loadtesting/src/main/java/com/azure/resourcemanager/loadtesting/models/EncryptionPropertiesIdentity.java
+++ sdk/loadtesting/azure-resourcemanager-loadtesting/src/main/java/com/azure/resourcemanager/loadtesting/models/EncryptionPropertiesIdentity.java
@@ -5,37 +5,39 @@
 package com.azure.resourcemanager.loadtesting.models;
 
 import com.azure.core.annotation.Fluent;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.azure.json.JsonReader;
+import com.azure.json.JsonSerializable;
+import com.azure.json.JsonToken;
+import com.azure.json.JsonWriter;
+import java.io.IOException;
 
 /**
  * All identity configuration for Customer-managed key settings defining which identity should be used to auth to Key
  * Vault.
  */
 @Fluent
-public class EncryptionPropertiesIdentity {
+public final class EncryptionPropertiesIdentity implements JsonSerializable<EncryptionPropertiesIdentity> {
     /*
-     * Managed identity type to use for accessing encryption key Url
+     * Managed identity type to use for accessing encryption key Url.
      */
-    @JsonProperty(value = "type")
     private Type type;
 
     /*
-     * user assigned identity to use for accessing key encryption key Url. Ex:
+     * User assigned identity to use for accessing key encryption key Url. Ex:
      * /subscriptions/fa5fc227-a624-475e-b696-cdd604c735bc/resourceGroups/<resource
-     * group>/providers/Microsoft.ManagedIdentity/userAssignedIdentities/myId
+     * group>/providers/Microsoft.ManagedIdentity/userAssignedIdentities/myId.
      */
-    @JsonProperty(value = "resourceId")
-    @JsonInclude(value = JsonInclude.Include.ALWAYS)
     private String resourceId;
 
-    /** Creates an instance of EncryptionPropertiesIdentity class. */
+    /**
+     * Creates an instance of EncryptionPropertiesIdentity class.
+     */
     public EncryptionPropertiesIdentity() {
     }
 
     /**
      * Get the type property: Managed identity type to use for accessing encryption key Url.
-     *
+     * 
      * @return the type value.
      */
     public Type type() {
@@ -44,7 +46,7 @@ public class EncryptionPropertiesIdentity {
 
     /**
      * Set the type property: Managed identity type to use for accessing encryption key Url.
-     *
+     * 
      * @param type the type value to set.
      * @return the EncryptionPropertiesIdentity object itself.
      */
@@ -54,10 +56,10 @@ public class EncryptionPropertiesIdentity {
     }
 
     /**
-     * Get the resourceId property: user assigned identity to use for accessing key encryption key Url. Ex:
+     * Get the resourceId property: User assigned identity to use for accessing key encryption key Url. Ex:
      * /subscriptions/fa5fc227-a624-475e-b696-cdd604c735bc/resourceGroups/&lt;resource
      * group&gt;/providers/Microsoft.ManagedIdentity/userAssignedIdentities/myId.
-     *
+     * 
      * @return the resourceId value.
      */
     public String resourceId() {
@@ -65,10 +67,10 @@ public class EncryptionPropertiesIdentity {
     }
 
     /**
-     * Set the resourceId property: user assigned identity to use for accessing key encryption key Url. Ex:
+     * Set the resourceId property: User assigned identity to use for accessing key encryption key Url. Ex:
      * /subscriptions/fa5fc227-a624-475e-b696-cdd604c735bc/resourceGroups/&lt;resource
      * group&gt;/providers/Microsoft.ManagedIdentity/userAssignedIdentities/myId.
-     *
+     * 
      * @param resourceId the resourceId value to set.
      * @return the EncryptionPropertiesIdentity object itself.
      */
@@ -79,9 +81,48 @@ public class EncryptionPropertiesIdentity {
 
     /**
      * Validates the instance.
-     *
+     * 
      * @throws IllegalArgumentException thrown if the instance is not valid.
      */
     public void validate() {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public JsonWriter toJson(JsonWriter jsonWriter) throws IOException {
+        jsonWriter.writeStartObject();
+        jsonWriter.writeStringField("type", this.type == null ? null : this.type.toString());
+        jsonWriter.writeStringField("resourceId", this.resourceId);
+        return jsonWriter.writeEndObject();
+    }
+
+    /**
+     * Reads an instance of EncryptionPropertiesIdentity from the JsonReader.
+     * 
+     * @param jsonReader The JsonReader being read.
+     * @return An instance of EncryptionPropertiesIdentity if the JsonReader was pointing to an instance of it, or null
+     * if it was pointing to JSON null.
+     * @throws IOException If an error occurs while reading the EncryptionPropertiesIdentity.
+     */
+    public static EncryptionPropertiesIdentity fromJson(JsonReader jsonReader) throws IOException {
+        return jsonReader.readObject(reader -> {
+            EncryptionPropertiesIdentity deserializedEncryptionPropertiesIdentity = new EncryptionPropertiesIdentity();
+            while (reader.nextToken() != JsonToken.END_OBJECT) {
+                String fieldName = reader.getFieldName();
+                reader.nextToken();
+
+                if ("type".equals(fieldName)) {
+                    deserializedEncryptionPropertiesIdentity.type = Type.fromString(reader.getString());
+                } else if ("resourceId".equals(fieldName)) {
+                    deserializedEncryptionPropertiesIdentity.resourceId = reader.getString();
+                } else {
+                    reader.skipChildren();
+                }
+            }
+
+            return deserializedEncryptionPropertiesIdentity;
+        });
     }
 }
