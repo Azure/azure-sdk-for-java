58bc550c5ef693e65c84377eaee062a047cc0cb6
vcolin7 <victor.y.asi@gmail.com>
2024-12-14 13:40:48 -0600

    Added tsp-location.yaml and AdminCustomizations.java

---
+++ sdk/keyvault/azure-security-keyvault-administration/swagger/src/main/java/AdminCustomizations.java
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import com.azure.autorest.customization.Customization;
+
+/**
+ * Contains customizations for Azure Key Vault Administrationó code generation.
+ */
+public class RbacCustomizations extends Customization {
+
+}
