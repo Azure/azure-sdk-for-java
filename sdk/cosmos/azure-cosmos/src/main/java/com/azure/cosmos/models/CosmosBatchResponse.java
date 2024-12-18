62ca4967689b23993ee7831811bda87dc52d4856
Abhijeet Mohanty <abhmohanty@microsoft.com>
2024-12-18 12:23:32 -0500

    Adding javadoc.

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/models/CosmosBatchResponse.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/models/CosmosBatchResponse.java
@@ -26,6 +26,10 @@ public final class CosmosBatchResponse {
     private final List<CosmosBatchOperationResult> results;
     private final int subStatusCode;
     private final CosmosDiagnostics cosmosDiagnostics;
+    private long opCountPerEvaluation;
+    private long retriedOpCountPerEvaluation;
+    private long globalOpCount;
+    private int targetMaxMicroBatchSize;
 
     /**
      * Initializes a new instance of the {@link CosmosBatchResponse} class.
@@ -188,6 +192,71 @@ public final class CosmosBatchResponse {
 
         return this.cosmosDiagnostics.getDuration();
     }
+
+    /**
+     * Get operation count per evaluation
+     * @return Operation count per evaluation
+     * */
+    public long getOpCountPerEvaluation() {
+        return opCountPerEvaluation;
+    }
+
+    /**
+     * Set operation count per evaluation
+     * @param opCountPerEvaluation  Operation count per evaluation
+     * */
+    public void setOpCountPerEvaluation(long opCountPerEvaluation) {
+        this.opCountPerEvaluation = opCountPerEvaluation;
+    }
+
+    /**
+     * Get global operation count
+     * @return Global operation count
+     * */
+    public long getGlobalOpCount() {
+        return this.globalOpCount;
+    }
+
+    /**
+     * Set global operation count
+     * @param globalOpCount  Global operation count
+     * */
+    public void setGlobalOpCount(long globalOpCount) {
+        this.globalOpCount = globalOpCount;
+    }
+
+    /**
+     * Get retried operation count per evaluation
+     * @return retried operation count per evaluation
+     * */
+    public long getRetriedOpCountPerEvaluation() {
+        return retriedOpCountPerEvaluation;
+    }
+
+    /**
+     * Set retried operation count per evaluation
+     * @param retriedOpCountPerEvaluation  retried operation count per evaluation
+     * */
+    public void setRetriedOpCountPerEvaluation(long retriedOpCountPerEvaluation) {
+        this.retriedOpCountPerEvaluation = retriedOpCountPerEvaluation;
+    }
+
+    /**
+     * Get target max micro batch size
+     * @return target max micro batch size
+     * */
+    public int getTargetMaxMicroBatchSize() {
+        return this.targetMaxMicroBatchSize;
+    }
+
+    /**
+     * Set target max micro batch size
+     * @param targetMaxMicroBatchSize  target max micro batch size
+     * */
+    public void setTargetMaxMicroBatchSize(int targetMaxMicroBatchSize) {
+        this.targetMaxMicroBatchSize = targetMaxMicroBatchSize;
+    }
+
 
     void addAll(List<? extends CosmosBatchOperationResult> collection) {
         this.results.addAll(collection);
--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/models/CosmosMetricName.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/models/CosmosMetricName.java
@@ -113,6 +113,37 @@ public final class CosmosMetricName {
     public static final CosmosMetricName REQUEST_SUMMARY_DIRECT_ACTUAL_ITEM_COUNT = new CosmosMetricName(
         nameOf("req.rntbd.actualItemCount"),
         CosmosMetricCategory.REQUEST_SUMMARY);
+    /**
+     * Op count per evaluation - relevant for batch operations executed by bulk executor
+     * NOTE: No percentiles or histogram supported
+     */
+    public static final CosmosMetricName REQUEST_SUMMARY_DIRECT_OP_COUNT_PER_EVALUATION = new CosmosMetricName(
+        nameOf("req.rntbd.opCountPerEvaluation"),
+        CosmosMetricCategory.REQUEST_SUMMARY);
+
+    /**
+     * Op retried count per evaluation - relevant for batch operations executed by bulk executor
+     * NOTE: No percentiles or histogram supported
+     */
+    public static final CosmosMetricName REQUEST_SUMMARY_DIRECT_OP_RETRIED_COUNT_PER_EVALUATION = new CosmosMetricName(
+        nameOf("req.rntbd.opRetriedCountPerEvaluation"),
+        CosmosMetricCategory.REQUEST_SUMMARY);
+
+    /**
+     * Global op count - relevant for batch operations executed by bulk executor
+     * NOTE: No percentiles or histogram supported
+     */
+    public static final CosmosMetricName REQUEST_SUMMARY_DIRECT_GLOBAL_OP_COUNT = new CosmosMetricName(
+        nameOf("req.rntbd.globalOpCount"),
+        CosmosMetricCategory.REQUEST_SUMMARY);
+
+    /**
+     * Target max micro batch size - relevant for batch operations executed by bulk executor
+     * NOTE: No percentiles or histogram supported
+     */
+    public static final CosmosMetricName REQUEST_SUMMARY_DIRECT_TARGET_MAX_MICRO_BATCH_SIZE = new CosmosMetricName(
+        nameOf("req.rntbd.targetMaxMicroBatchSize"),
+        CosmosMetricCategory.REQUEST_SUMMARY);
 
     /**
      * Number of requests (Counter)
@@ -143,6 +174,22 @@ public final class CosmosMetricName {
      */
     public static final CosmosMetricName REQUEST_SUMMARY_GATEWAY_ACTUAL_ITEM_COUNT = new CosmosMetricName(
         nameOf("req.gw.actualItemCount"),
+        CosmosMetricCategory.REQUEST_SUMMARY);
+
+    public static final CosmosMetricName REQUEST_SUMMARY_GATEWAY_OP_COUNT_PER_EVALUATION = new CosmosMetricName(
+        nameOf("req.gw.opCountPerEvaluation"),
+        CosmosMetricCategory.REQUEST_SUMMARY);
+
+    public static final CosmosMetricName REQUEST_SUMMARY_GATEWAY_OP_RETRIED_COUNT_PER_EVALUATION = new CosmosMetricName(
+        nameOf("req.gw.opRetriedCountPerEvaluation"),
+        CosmosMetricCategory.REQUEST_SUMMARY);
+
+    public static final CosmosMetricName REQUEST_SUMMARY_GATEWAY_GLOBAL_OP_COUNT = new CosmosMetricName(
+        nameOf("req.gw.globalOpCount"),
+        CosmosMetricCategory.REQUEST_SUMMARY);
+
+    public static final CosmosMetricName REQUEST_SUMMARY_GATEWAY_TARGET_MAX_MICRO_BATCH_SIZE = new CosmosMetricName(
+        nameOf("req.gw.targetMaxMicroBatchSize"),
         CosmosMetricCategory.REQUEST_SUMMARY);
 
     /**
@@ -389,10 +436,18 @@ public final class CosmosMetricName {
         map.put(nameOf("req.rntbd.backendlatency"), CosmosMetricName.REQUEST_SUMMARY_DIRECT_BACKEND_LATENCY);
         map.put(nameOf("req.rntbd.rus"), CosmosMetricName.REQUEST_SUMMARY_DIRECT_REQUEST_CHARGE);
         map.put(nameOf("req.rntbd.actualitemcount"), CosmosMetricName.REQUEST_SUMMARY_DIRECT_ACTUAL_ITEM_COUNT);
+        map.put(nameOf("req.rntbd.opCountPerEvaluation"), CosmosMetricName.REQUEST_SUMMARY_DIRECT_OP_COUNT_PER_EVALUATION);
+        map.put(nameOf("req.rntbd.opRetriedCountPerEvaluation"), CosmosMetricName.REQUEST_SUMMARY_DIRECT_OP_RETRIED_COUNT_PER_EVALUATION);
+        map.put(nameOf("req.rntbd.globalOpCount"), CosmosMetricName.REQUEST_SUMMARY_DIRECT_GLOBAL_OP_COUNT);
+        map.put(nameOf("req.rntbd.targetMaxMicroBatchSize"), CosmosMetricName.REQUEST_SUMMARY_DIRECT_TARGET_MAX_MICRO_BATCH_SIZE);
         map.put(nameOf("req.gw.requests"), CosmosMetricName.REQUEST_SUMMARY_GATEWAY_REQUESTS);
         map.put(nameOf("req.gw.latency"), CosmosMetricName.REQUEST_SUMMARY_GATEWAY_LATENCY);
         map.put(nameOf("req.gw.rus"), CosmosMetricName.REQUEST_SUMMARY_GATEWAY_REQUEST_CHARGE);
         map.put(nameOf("req.gw.actualitemcount"), CosmosMetricName.REQUEST_SUMMARY_GATEWAY_ACTUAL_ITEM_COUNT);
+        map.put(nameOf("req.gw.opCountPerEvaluation"), CosmosMetricName.REQUEST_SUMMARY_GATEWAY_OP_COUNT_PER_EVALUATION);
+        map.put(nameOf("req.gw.opRetriedCountPerEvaluation"), CosmosMetricName.REQUEST_SUMMARY_GATEWAY_OP_RETRIED_COUNT_PER_EVALUATION);
+        map.put(nameOf("req.gw.globalOpCount"), CosmosMetricName.REQUEST_SUMMARY_GATEWAY_GLOBAL_OP_COUNT);
+        map.put(nameOf("req.gw.targetMaxMicroBatchSize"), CosmosMetricName.REQUEST_SUMMARY_GATEWAY_TARGET_MAX_MICRO_BATCH_SIZE);
         map.put(nameOf("req.reqpayloadsize"), CosmosMetricName.REQUEST_SUMMARY_SIZE_REQUEST);
         map.put(nameOf("req.rsppayloadsize"), CosmosMetricName.REQUEST_SUMMARY_SIZE_RESPONSE);
         map.put(nameOf("req.rntbd.timeline"), CosmosMetricName.REQUEST_DETAILS_DIRECT_TIMELINE);
