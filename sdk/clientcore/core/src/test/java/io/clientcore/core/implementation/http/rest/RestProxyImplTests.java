c9f403cfbee6a4f3391d427153c486d8a048a4da
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2024-12-19 12:16:39 -0500

    Cleanup RestProxy code

--- sdk/clientcore/core/src/test/java/io/clientcore/core/implementation/http/rest/RestProxyImplTests.java
+++ sdk/clientcore/core/src/test/java/io/clientcore/core/implementation/http/rest/RestProxyImplTests.java
@@ -16,21 +16,37 @@ import io.clientcore.core.http.models.HttpRequest;
 import io.clientcore.core.http.models.Response;
 import io.clientcore.core.http.pipeline.HttpPipeline;
 import io.clientcore.core.http.pipeline.HttpPipelineBuilder;
+import io.clientcore.core.implementation.util.JsonSerializer;
 import io.clientcore.core.util.Context;
 import io.clientcore.core.util.binarydata.BinaryData;
-import io.clientcore.core.implementation.util.JsonSerializer;
+import org.junit.jupiter.api.Named;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
 
+import static io.clientcore.core.util.TestUtils.assertArraysEqual;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Tests {@link RestProxy}.
  */
 public class RestProxyImplTests {
+    private static final String SAMPLE = "sample";
+    private static final byte[] EXPECTED = SAMPLE.getBytes(StandardCharsets.UTF_8);
+
     @ServiceInterface(name = "myService", host = "https://azure.com")
     interface TestInterface {
         @HttpRequestInformation(method = HttpMethod.POST, path = "my/uri/path", expectedStatusCodes = { 200 })
@@ -88,5 +104,126 @@ public class RestProxyImplTests {
                 }
             };
         }
+    }
+
+    @ParameterizedTest
+    @MethodSource("expectedBodyLengthDataProvider")
+    public void expectedBodyLength(HttpRequest httpRequest) {
+        BinaryData binaryData = RestProxyImpl.validateLength(httpRequest);
+
+        assertNotNull(binaryData);
+        assertArraysEqual(EXPECTED, binaryData.toBytes());
+    }
+
+    public static Stream<Arguments> expectedBodyLengthDataProvider() throws Exception {
+        return dataProvider(EXPECTED.length);
+    }
+
+    @Test
+    public void emptyRequestBody() {
+        HttpRequest httpRequest = new HttpRequest(HttpMethod.GET, "http://localhost");
+
+        assertNull(RestProxyImpl.validateLength(httpRequest));
+    }
+
+    @ParameterizedTest
+    @MethodSource("unexpectedBodyLengthTooSmallDataProvider")
+    public void unexpectedBodyLengthTooSmall(HttpRequest httpRequest) {
+        assertThrows(IllegalStateException.class, () -> validateAndCollectRequest(httpRequest), "Request body "
+            + "emitted " + EXPECTED.length + " bytes, less than the expected " + (EXPECTED.length + 1) + " bytes.");
+    }
+
+    public static Stream<Arguments> unexpectedBodyLengthTooSmallDataProvider() throws Exception {
+        return dataProvider(EXPECTED.length + 1);
+    }
+
+    @ParameterizedTest
+    @MethodSource("unexpectedBodyLengthTooLargeDataProvider")
+    public void unexpectedBodyLengthTooLarge(HttpRequest httpRequest) {
+        assertThrows(IllegalStateException.class, () -> validateAndCollectRequest(httpRequest), "Request body "
+            + "emitted " + EXPECTED.length + " bytes, more than the expected " + (EXPECTED.length - 1) + " bytes.");
+    }
+
+    public static Stream<Arguments> unexpectedBodyLengthTooLargeDataProvider() throws Exception {
+        return dataProvider(EXPECTED.length - 1);
+    }
+
+    @Test
+    public void multipleToBytesToCheckBodyLength() {
+        HttpRequest httpRequest
+            = new HttpRequest(HttpMethod.GET, "http://localhost").setBody(BinaryData.fromBytes(EXPECTED));
+        httpRequest.getHeaders().set(HttpHeaderName.CONTENT_LENGTH, String.valueOf(EXPECTED.length));
+
+        BinaryData binaryData = RestProxyImpl.validateLength(httpRequest);
+
+        assertNotNull(binaryData);
+        assertArraysEqual(EXPECTED, binaryData.toBytes());
+        assertArraysEqual(EXPECTED, binaryData.toBytes());
+    }
+
+    private static Stream<Arguments> dataProvider(int contentLength) throws Exception {
+        Path file = Files.createTempFile(RestProxyImpl.class.getSimpleName(), null);
+        file.toFile().deleteOnExit();
+        Files.write(file, EXPECTED);
+
+        return Stream.of(
+            Arguments.of(Named.of("bytes",
+                createHttpRequest("http://localhost", BinaryData.fromBytes(EXPECTED), contentLength))),
+            Arguments.of(Named.of("string",
+                createHttpRequest("http://localhost", BinaryData.fromString(SAMPLE), contentLength))),
+            Arguments.of(
+                Named.of("stream", createHttpRequest("http://localhost", BinaryData.fromFile(file), contentLength))),
+            Arguments
+                .of(Named.of("file", createHttpRequest("http://localhost", BinaryData.fromFile(file), contentLength))));
+    }
+
+    private static HttpRequest createHttpRequest(String uri, BinaryData body, int contentLength) {
+        HttpRequest httpRequest = new HttpRequest(HttpMethod.GET, uri).setBody(body);
+        httpRequest.getHeaders().set(HttpHeaderName.CONTENT_LENGTH, String.valueOf(contentLength));
+        return httpRequest;
+    }
+
+    @Test
+    public void userProvidedLengthShouldNotBeTrustedTooLarge() throws IOException {
+        try (InputStream byteArrayInputStream = new ByteArrayInputStream(EXPECTED)) {
+            HttpRequest httpRequest = new HttpRequest(HttpMethod.GET, "http://localhost")
+                .setBody(BinaryData.fromStream(byteArrayInputStream, EXPECTED.length - 1L));
+            httpRequest.getHeaders().set(HttpHeaderName.CONTENT_LENGTH, String.valueOf(EXPECTED.length - 1L));
+
+            IllegalStateException thrown = assertThrows(IllegalStateException.class,
+                () -> validateAndCollectRequest(httpRequest), "Expected validateLength() to throw, but it didn't");
+            assertEquals("Request body emitted " + EXPECTED.length + " bytes, more than the expected "
+                + (EXPECTED.length - 1) + " bytes.", thrown.getMessage());
+        }
+    }
+
+    @Test
+    public void userProvidedLengthShouldNotBeTrustedTooSmall() throws IOException {
+        try (InputStream byteArrayInputStream = new ByteArrayInputStream(EXPECTED)) {
+            HttpRequest httpRequest = new HttpRequest(HttpMethod.GET, "http://localhost")
+                .setBody(BinaryData.fromStream(byteArrayInputStream, EXPECTED.length + 1L));
+            httpRequest.getHeaders().set(HttpHeaderName.CONTENT_LENGTH, String.valueOf(EXPECTED.length + 1L));
+
+            IllegalStateException thrown = assertThrows(IllegalStateException.class,
+                () -> validateAndCollectRequest(httpRequest), "Expected validateLength() to throw, but it didn't");
+
+            assertEquals("Request body emitted " + EXPECTED.length + " bytes, less than the expected "
+                + (EXPECTED.length + 1) + " bytes.", thrown.getMessage());
+        }
+    }
+
+    @Test
+    public void expectedBodyLength() throws IOException {
+        try (InputStream byteArrayInputStream = new ByteArrayInputStream(EXPECTED)) {
+            HttpRequest httpRequest = new HttpRequest(HttpMethod.GET, "http://localhost")
+                .setBody(BinaryData.fromStream(byteArrayInputStream, (long) EXPECTED.length));
+            httpRequest.getHeaders().set(HttpHeaderName.CONTENT_LENGTH, String.valueOf(EXPECTED.length));
+
+            assertArraysEqual(EXPECTED, validateAndCollectRequest(httpRequest));
+        }
+    }
+
+    private static byte[] validateAndCollectRequest(HttpRequest request) {
+        return RestProxyImpl.validateLength(request).toBytes();
     }
 }
