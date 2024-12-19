63bf526f6241ed8d13f0ccaaa8d3134a02720c69
Hong Li(MSFT) <74638143+v-hongli1@users.noreply.github.com>
2024-12-19 14:17:17 +0800

    Update Javadoc of premium SDK for azure-resourcemanager-appplatform (#43458)

--- sdk/resourcemanager/azure-resourcemanager-appplatform/src/main/java/com/azure/resourcemanager/appplatform/models/RuntimeVersion.java
+++ sdk/resourcemanager/azure-resourcemanager-appplatform/src/main/java/com/azure/resourcemanager/appplatform/models/RuntimeVersion.java
@@ -21,6 +21,15 @@ public final class RuntimeVersion extends ExpandableStringEnum<RuntimeVersion> {
     public static final RuntimeVersion NET_CORE_31 = fromString("NetCore_31");
 
     /**
+     * Creates a new instance of RuntimeVersion value.
+     *
+     * @deprecated Use the {@link #fromString(String)} factory method.
+     */
+    @Deprecated
+    public RuntimeVersion() {
+    }
+
+    /**
      * Creates or finds a RuntimeVersion from its string representation.
      *
      * @param name a name to look for.
@@ -30,7 +39,11 @@ public final class RuntimeVersion extends ExpandableStringEnum<RuntimeVersion> {
         return fromString(name, RuntimeVersion.class);
     }
 
-    /** @return known RuntimeVersion values. */
+    /**
+     * Gets known RuntimeVersion values.
+     *
+     * @return known RuntimeVersion values.
+     */
     public static Collection<RuntimeVersion> values() {
         return values(RuntimeVersion.class);
     }
