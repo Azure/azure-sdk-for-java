f492237dd3d43bf0cc8711b3358ed682925569db
Harsimar Kaur (from Dev Box) <harskaur@microsoft.com>
2024-12-19 16:06:09 -0800

    remove unused classes

--- sdk/monitor/azure-monitor-opentelemetry-autoconfigure/src/main/java/com/azure/monitor/opentelemetry/autoconfigure/implementation/quickpulse/filtering/MetricFailureToCreateException.java
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-package com.azure.monitor.opentelemetry.autoconfigure.implementation.quickpulse.filtering;
-
-public class MetricFailureToCreateException extends Exception {
-    public MetricFailureToCreateException(String message) {
-        super(message);
-    }
-}
