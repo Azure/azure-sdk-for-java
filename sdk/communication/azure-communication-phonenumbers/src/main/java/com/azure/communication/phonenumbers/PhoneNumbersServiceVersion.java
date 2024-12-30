1565674a1e1322d3a52e19768b1feb0dd2bd07ab
guoqing2023 <guoqing@microsoft.com>
2024-12-30 11:45:47 -0600

    Add number insight GA

--- sdk/communication/azure-communication-phonenumbers/src/main/java/com/azure/communication/phonenumbers/PhoneNumbersServiceVersion.java
+++ sdk/communication/azure-communication-phonenumbers/src/main/java/com/azure/communication/phonenumbers/PhoneNumbersServiceVersion.java
@@ -17,7 +17,12 @@ public enum PhoneNumbersServiceVersion implements ServiceVersion {
     /**
      * Number Lookup update to public preview {@code 2024-03-01-preview}
      */
-    V2024_03_01_PREVIEW("2024-03-01-preview");
+    V2024_03_01_PREVIEW("2024-03-01-preview"),
+
+    /**
+     * Number Lookup GA {@code 2025-02-11}
+     */
+    V2025_02_11("2025-02-11");
 
     private final String version;
 
@@ -42,6 +47,6 @@ public enum PhoneNumbersServiceVersion implements ServiceVersion {
      */
     public static PhoneNumbersServiceVersion getLatest() {
 
-        return V2024_03_01_PREVIEW;
+        return V2025_02_11;
     }
 }
