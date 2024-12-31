14b694f29d57ff3852e967689bdcf938e7dc19d2
vcolin7 <victor.y.asi@gmail.com>
2024-12-31 13:51:44 -0600

    Updated CertificatesCustomizations.java and re-generated.

--- sdk/keyvault/azure-security-keyvault-certificates/swagger/src/main/java/CertificatesCustomizations.java
+++ sdk/keyvault/azure-security-keyvault-certificates/swagger/src/main/java/CertificatesCustomizations.java
@@ -23,19 +23,13 @@ public class CertificatesCustomizations extends Customization {
         removeFiles(rawEditor);
 
         // Customize the CertificateClientImpl class.
-        PackageCustomization implPackageCustomization =
-            libraryCustomization.getPackage("com.azure.security.keyvault.certificates.implementation");
-        ClassCustomization implClientClassCustomization = implPackageCustomization.getClass("CertificateClientImpl");
-        customizeClientImpl(implClientClassCustomization);
+        ClassCustomization classCustomization = libraryCustomization
+            .getPackage("com.azure.security.keyvault.certificates.implementation")
+            .getClass("CertificateClientImpl");
+        String classPath =
+            "src/main/java/com/azure/security/keyvault/certificates/implementation/CertificateClientImpl.java";
 
-        customizeModuleInfo(rawEditor);
-        customizePackageInfos(rawEditor);
-
-        // Change the names of generated
-        /*PackageCustomization modelsPackageCustomization =
-            libraryCustomization.getPackage("com.azure.security.keyvault.certificates.models");
-
-        customizeAlgorithms(modelsPackageCustomization);*/
+        replaceInFile(classCustomization, classPath, "KeyVault", "Certificate");
     }
 
     private static void removeFiles(Editor editor) {
@@ -45,90 +39,6 @@ public class CertificatesCustomizations extends Customization {
         editor.removeFile("src/main/java/com/azure/security/keyvault/certificates/CertificateAsyncClient.java");
         editor.removeFile("src/main/java/com/azure/security/keyvault/certificates/CertificateClient.java");
         editor.removeFile("src/main/java/com/azure/security/keyvault/certificates/CertificateClientBuilder.java");
-    }
-
-    private static void renameFiles(Editor editor) {
-        // Uncomment the following line once the TSP spec includes all service versions.
-        /*editor.renameFile("src/main/java/com/azure/security/keyvault/certificates/implementation/KeyVaultServiceVersion.java",
-            "src/main/java/com/azure/security/keyvault/certificates/CertificateServiceVersion.java");*/
-    }
-
-    private static void customizeClientImpl(ClassCustomization classCustomization) {
-        // Remove the KeyVaultServiceVersion import since we will use CertificateServiceVersion for now. We'll remove
-        // this once the TSP spec includes all service versions.
-        classCustomization.customizeAst(ast ->
-            replaceImport(ast, "com.azure.security.keyvault.certificates.KeyVaultServiceVersion",
-            "com.azure.security.keyvault.certificates.CertificateServiceVersion"));
-
-        String classPath =
-            "src/main/java/com/azure/security/keyvault/certificates/implementation/CertificateClientImpl.java";
-
-        replaceInFile(classCustomization, classPath, "KeyVault", "Certificate");
-    }
-
-    /*private static void customizeAlgorithms(PackageCustomization packageCustomization) {
-        packageCustomization.getClass("CertificateKeyCurveName")
-            .customizeAst(ast -> ast.getClassByName("CertificateKeyCurveName")
-                .ifPresent(clazz -> {
-                    clazz.getFieldByName("P256K").ifPresent(field -> field.getVariable(0).setName("P_256K"));
-                    clazz.getFieldByName("P256").ifPresent(field -> field.getVariable(0).setName("P_256"));
-                    clazz.getFieldByName("P384").ifPresent(field -> field.getVariable(0).setName("P_384"));
-                    clazz.getFieldByName("P521").ifPresent(field -> field.getVariable(0).setName("P_521"));
-                }));
-    }*/
-
-    private static void customizeModuleInfo(Editor editor) {
-        editor.replaceFile("src/main/java/module-info.java",
-            joinWithNewline(
-                "// Copyright (c) Microsoft Corporation. All rights reserved.",
-                "// Licensed under the MIT License.",
-                "",
-                "module com.azure.security.keyvault.certificates {",
-                "    requires transitive com.azure.core;",
-                "    requires com.azure.json;",
-                "",
-                "    exports com.azure.security.keyvault.certificates;",
-                "    exports com.azure.security.keyvault.certificates.models;",
-                "",
-                "    opens com.azure.security.keyvault.certificates to com.azure.core;",
-                "    opens com.azure.security.keyvault.certificates.implementation to com.azure.core;",
-                "    opens com.azure.security.keyvault.certificates.implementation.models to com.azure.core;",
-                "    opens com.azure.security.keyvault.certificates.models to com.azure.core;",
-                "}",
-                ""
-            ));
-    }
-
-    private static void customizePackageInfos(Editor editor) {
-        editor.replaceFile("src/main/java/com/azure/security/keyvault/certificates/implementation/package-info.java",
-            joinWithNewline(
-                "// Copyright (c) Microsoft Corporation. All rights reserved.",
-                "// Licensed under the MIT License.",
-                "// Code generated by Microsoft (R) TypeSpec Code Generator.",
-                "",
-                "/**",
-                " * Package containing the implementations for CertificateClient. The key vault client performs "
-                    + "cryptographic key operations",
-                " * and vault operations against the Key Vault service.",
-                " */",
-                "package com.azure.security.keyvault.certificates.implementation;",
-                ""
-            ));
-
-        editor.replaceFile("src/main/java/com/azure/security/keyvault/certificates/implementation/models/package-info.java",
-            joinWithNewline(
-                "// Copyright (c) Microsoft Corporation. All rights reserved.",
-                "// Licensed under the MIT License.",
-                "// Code generated by Microsoft (R) TypeSpec Code Generator.",
-                "",
-                "/**",
-                " * Package containing the data models for CertificateClient. The key vault client performs "
-                    + "cryptographic key operations and",
-                " * vault operations against the Key Vault service.",
-                " */",
-                "package com.azure.security.keyvault.certificates.implementation.models;",
-                ""
-            ));
     }
 
     /**
@@ -164,37 +74,5 @@ public class CertificatesCustomizations extends Customization {
         for (Range range : ranges) {
             editor.replace(classPath, range.getStart(), range.getEnd(), "CertificateServiceVersion");
         }*/
-    }
-
-    private static void replaceImport(CompilationUnit ast, String originalImport, String newImport) {
-        NodeList<ImportDeclaration> nodeList = ast.getImports();
-
-        for (ImportDeclaration importDeclaration : nodeList) {
-            if (importDeclaration.getNameAsString().equals(originalImport)) {
-                importDeclaration.setName(newImport);
-
-                break;
-            }
-        }
-
-        ast.setImports(nodeList);
-    }
-
-    private static void removeImport(CompilationUnit ast, String importStatement) {
-        NodeList<ImportDeclaration> nodeList = ast.getImports();
-
-        for (ImportDeclaration importDeclaration : nodeList) {
-            if (importDeclaration.getNameAsString().equals(importStatement)) {
-                nodeList.remove(importDeclaration);
-
-                break;
-            }
-        }
-
-        ast.setImports(nodeList);
-    }
-
-    private static String joinWithNewline(String... lines) {
-        return String.join("\n", lines);
     }
 }
