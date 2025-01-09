89ffa0d3b3c9fca66b5296760129e23af80d0110
Weidong Xu <weidxu@microsoft.com>
2025-01-09 15:38:57 +0800

    more PagedIterable tests

--- sdk/clientcore/core/src/test/java/io/clientcore/core/http/models/PagedIterableTests.java
+++ sdk/clientcore/core/src/test/java/io/clientcore/core/http/models/PagedIterableTests.java
@@ -3,16 +3,114 @@
 
 package io.clientcore.core.http.models;
 
+import io.clientcore.core.util.binarydata.BinaryData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PagedIterableTests {
 
+    private final HttpHeaders httpHeaders = new HttpHeaders();
+    private final HttpRequest httpRequest = new HttpRequest(HttpMethod.GET, "http://localhost");
+    private final BinaryData responseBody = BinaryData.empty();
+
+    // tests with mocked PagedResponse
+    private List<PagedResponse<Integer>> pagedResponses;
+
+    @ParameterizedTest
+    @ValueSource(ints = { 0, 5 })
+    public void streamByPage(int numberOfPages) {
+        PagedIterable<Integer> pagedIterable = getIntegerPagedIterable(numberOfPages);
+        List<PagedResponse<Integer>> pages = pagedIterable.streamByPage().collect(Collectors.toList());
+
+        assertEquals(numberOfPages, pages.size());
+        assertEquals(pagedResponses, pages);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { 0, 5 })
+    public void iterateByPage(int numberOfPages) {
+        PagedIterable<Integer> pagedIterable = getIntegerPagedIterable(numberOfPages);
+        List<PagedResponse<Integer>> pages = new ArrayList<>();
+        pagedIterable.iterableByPage().iterator().forEachRemaining(pages::add);
+
+        assertEquals(numberOfPages, pages.size());
+        assertEquals(pagedResponses, pages);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { 0, 5 })
+    public void streamByT(int numberOfPages) {
+        PagedIterable<Integer> pagedIterable = getIntegerPagedIterable(numberOfPages);
+        List<Integer> values = pagedIterable.stream().collect(Collectors.toList());
+
+        assertEquals(numberOfPages * 3, values.size());
+        assertEquals(Stream.iterate(0, i -> i + 1).limit(numberOfPages * 3L).collect(Collectors.toList()), values);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { 0, 5 })
+    public void iterateByT(int numberOfPages) {
+        PagedIterable<Integer> pagedIterable = getIntegerPagedIterable(numberOfPages);
+        List<Integer> values = new ArrayList<>();
+        pagedIterable.iterator().forEachRemaining(values::add);
+
+        assertEquals(numberOfPages * 3, values.size());
+        assertEquals(Stream.iterate(0, i -> i + 1).limit(numberOfPages * 3L).collect(Collectors.toList()), values);
+    }
+
+    private PagedIterable<Integer> getIntegerPagedIterable(int numberOfPages) {
+        createPagedResponse(numberOfPages);
+
+        return new PagedIterable<>(pagingOptions -> pagedResponses.isEmpty() ? null : pagedResponses.get(0),
+            (pagingOptions, nextLink) -> getNextPageSync(nextLink, pagedResponses));
+    }
+
+    private void createPagedResponse(int numberOfPages) {
+        pagedResponses = IntStream.range(0, numberOfPages)
+            .boxed()
+            .map(i -> createPagedResponse(httpRequest, httpHeaders, numberOfPages, this::getItems, i))
+            .collect(Collectors.toList());
+    }
+
+    private <T> PagedResponse<T> createPagedResponse(HttpRequest httpRequest, HttpHeaders headers, int numberOfPages,
+        Function<Integer, List<T>> valueSupplier, int i) {
+        return new PagedResponse<>(httpRequest, 200, headers, responseBody, valueSupplier.apply(i), null,
+            (i < numberOfPages - 1) ? String.valueOf(i + 1) : null, null, null, null);
+    }
+
+    private PagedResponse<Integer> getNextPageSync(String nextLink, List<PagedResponse<Integer>> pagedResponses) {
+
+        if (nextLink == null || nextLink.isEmpty()) {
+            return null;
+        }
+
+        int parsedToken = Integer.parseInt(nextLink);
+        if (parsedToken >= pagedResponses.size()) {
+            return null;
+        }
+
+        return pagedResponses.get(parsedToken);
+    }
+
+    private List<Integer> getItems(int i) {
+        return IntStream.range(i * 3, i * 3 + 3).boxed().collect(Collectors.toList());
+    }
+
+    // tests with mocked HttpResponse
     @Test
-    public void testPagedIterable() {
+    public void testPagedIterableNextLink() {
         PagedIterable<TodoItem> pagedIterable = this.list();
         verifyIteratorSize(pagedIterable.iterableByPage().iterator(), 2);
         verifyIteratorSize(pagedIterable.iterator(), 3);
@@ -68,12 +166,13 @@ public class PagedIterableTests {
 
     private Response<TodoPage> listSync(PagingOptions pagingOptions) {
         // mock request on first page
-        return new HttpResponse<>(null, 200, null, new TodoPage(List.of(new TodoItem(), new TodoItem()), "nextLink1"));
+        return new HttpResponse<>(httpRequest, 200, httpHeaders,
+            new TodoPage(List.of(new TodoItem(), new TodoItem()), "nextLink1"));
     }
 
     private Response<TodoPage> listNextSync(String nextLink) {
         // mock request on next page
         Assertions.assertEquals("nextLink1", nextLink);
-        return new HttpResponse<>(null, 200, null, new TodoPage(List.of(new TodoItem()), null));
+        return new HttpResponse<>(httpRequest, 200, httpHeaders, new TodoPage(List.of(new TodoItem()), null));
     }
 }
