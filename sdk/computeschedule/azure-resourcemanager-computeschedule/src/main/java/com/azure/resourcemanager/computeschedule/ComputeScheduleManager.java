e572608747b1411aea3b6e04ab78812ca5244bce
azure-sdk <azuresdk@microsoft.com>
2025-01-22 09:56:29 +0000

    [Automation] Generate Fluent Lite from TypeSpec computeschedule

--- sdk/computeschedule/azure-resourcemanager-computeschedule/src/main/java/com/azure/resourcemanager/computeschedule/ComputeScheduleManager.java
+++ sdk/computeschedule/azure-resourcemanager-computeschedule/src/main/java/com/azure/resourcemanager/computeschedule/ComputeScheduleManager.java
@@ -209,7 +209,7 @@ public final class ComputeScheduleManager {
                 .append("-")
                 .append("com.azure.resourcemanager.computeschedule")
                 .append("/")
-                .append("1.0.0-beta.1");
+                .append("1.0.0-beta.2");
             if (!Configuration.getGlobalConfiguration().get("AZURE_TELEMETRY_DISABLED", false)) {
                 userAgentBuilder.append(" (")
                     .append(Configuration.getGlobalConfiguration().get("java.version"))
