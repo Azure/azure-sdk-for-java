7664542f24fe9f7cadad04a0168c74cb558625d0
Bill Wert <billwert@microsoft.com>
2025-01-24 17:39:29 -0800

    remove deprecated Media events

--- sdk/eventgrid/azure-messaging-eventgrid-systemevents/customization/src/main/java/EventGridSystemEventsCustomization.java
+++ sdk/eventgrid/azure-messaging-eventgrid-systemevents/customization/src/main/java/EventGridSystemEventsCustomization.java
@@ -26,11 +26,9 @@ public class EventGridSystemEventsCustomization extends Customization {
     @Override
     public void customize(LibraryCustomization customization, Logger logger) {
         customizeModuleInfo(customization, logger);
-        customizeMediaLiveEventChannelArchiveHeartbeatEventData(customization, logger);
         customizeAcsRouterEvents(customization, logger);
         customizeAcsRecordingFileStatusUpdatedEventDataDuration(customization, logger);
         customizeStorageDirectoryDeletedEventData(customization, logger);
-        customizeMediaLiveEventIngestHeartbeatEventData(customization);
         customizeAcsMessageEventData(customization, logger);
     }
 
@@ -52,30 +50,6 @@ public class EventGridSystemEventsCustomization extends Customization {
             }
         });
         editor.replaceFile("src/main/java/module-info.java", sb.toString());
-    }
-
-    /**
-     * Customize the MediaLiveEventChannelArchiveHeartbeatEventData.getChannelLatency method to return a Duration object.
-     * @param customization The LibraryCustomization object.
-     * @param logger The logger object.
-     */
-    public void customizeMediaLiveEventChannelArchiveHeartbeatEventData(LibraryCustomization customization, Logger logger) {
-        PackageCustomization packageModels = customization.getPackage("com.azure.messaging.eventgrid.systemevents");
-        ClassCustomization classCustomization = packageModels.getClass("MediaLiveEventChannelArchiveHeartbeatEventData");
-        classCustomization.addStaticBlock("static final ClientLogger LOGGER = new ClientLogger(MediaLiveEventChannelArchiveHeartbeatEventData.class);");
-        logger.info("Fixing getChannelLatency");
-        classCustomization.customizeAst(ast -> {
-            ast.addImport("java.time.Duration");
-            ast.addImport("com.azure.core.util.logging.ClientLogger");
-            ClassOrInterfaceDeclaration clazz = ast.getClassByName("MediaLiveEventChannelArchiveHeartbeatEventData").get();
-            clazz.getMethodsByName("getChannelLatency").forEach(method -> {
-                method.setType("Duration");
-                method.setBody(parseBlock("{ if (\"n/a\".equals(this.channelLatency)) { return null; } Long channelLatencyLong; try { channelLatencyLong = Long.parseLong(this.channelLatency); } catch (NumberFormatException ex) { LOGGER.logExceptionAsError(ex); return null; } return Duration.ofMillis(channelLatencyLong); }"));
-                method.setJavadocComment(new Javadoc(new JavadocDescription(List.of(new JavadocSnippet("Gets the duration of channel latency."))))
-                    .addBlockTag("return", "the duration of channel latency."));
-            });
-
-        });
     }
 
     public void customizeAcsRouterEvents(LibraryCustomization customization, Logger logger) {
@@ -123,27 +97,6 @@ public class EventGridSystemEventsCustomization extends Customization {
         PackageCustomization packageModels = customization.getPackage("com.azure.messaging.eventgrid.systemevents");
         ClassCustomization classCustomization = packageModels.getClass("StorageDirectoryDeletedEventData");
         classCustomization.getMethod("getRecursive").rename("isRecursive").setReturnType("Boolean", "Boolean.getBoolean(%s)");
-    }
-
-    public void customizeMediaLiveEventIngestHeartbeatEventData(LibraryCustomization customization) {
-        PackageCustomization packageModels = customization.getPackage("com.azure.messaging.eventgrid.systemevents");
-        ClassCustomization classCustomization = packageModels.getClass("MediaLiveEventIngestHeartbeatEventData");
-        classCustomization.addStaticBlock("static final ClientLogger LOGGER = new ClientLogger(MediaLiveEventIngestHeartbeatEventData.class);", Arrays.asList("com.azure.core.util.logging.ClientLogger"));
-
-        classCustomization.customizeAst(ast -> {
-            ast.addImport("java.time.OffsetDateTime");
-            ClassOrInterfaceDeclaration clazz = ast.getClassByName("MediaLiveEventIngestHeartbeatEventData").get();
-            clazz.getMethodsByName("getIngestDriftValue").forEach(method -> {
-                method.setType("Integer");
-                method.setBody(parseBlock("{ if (\"n/a\".equals(this.ingestDriftValue)) { return null; } try { return Integer.parseInt(this.ingestDriftValue); } catch (NumberFormatException ex) { LOGGER.logExceptionAsError(ex); return null; } }"));
-            });
-
-            clazz.getMethodsByName("getLastFragmentArrivalTime").forEach(method -> {
-                method.setType("OffsetDateTime");
-                method.setBody(parseBlock("{ return OffsetDateTime.parse(this.lastFragmentArrivalTime); }"));
-            });
-
-        });
     }
 
     public void customizeAcsMessageEventData(LibraryCustomization customization, Logger logger) {
