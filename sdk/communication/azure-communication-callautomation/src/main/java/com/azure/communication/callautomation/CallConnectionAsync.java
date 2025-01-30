581b01013a060c0fd4dd5663f225fcfff4c050cd
Vinothini Dharmaraj <v-vdharmaraj@microsoft.com>
2025-01-30 14:59:02 -0800

    removing the microsoft app identifier which was added default merge from main

--- sdk/communication/azure-communication-callautomation/src/main/java/com/azure/communication/callautomation/CallConnectionAsync.java
+++ sdk/communication/azure-communication-callautomation/src/main/java/com/azure/communication/callautomation/CallConnectionAsync.java
@@ -35,7 +35,6 @@ import com.azure.communication.callautomation.models.TransferCallResult;
 import com.azure.communication.callautomation.models.TransferCallToParticipantOptions;
 import com.azure.communication.common.CommunicationIdentifier;
 import com.azure.communication.common.CommunicationUserIdentifier;
-import com.azure.communication.common.MicrosoftTeamsAppIdentifier;
 import com.azure.communication.common.MicrosoftTeamsUserIdentifier;
 import com.azure.communication.common.PhoneNumberIdentifier;
 import com.azure.core.annotation.ReturnType;
@@ -254,10 +253,6 @@ public final class CallConnectionAsync {
         } else if (targetParticipant instanceof MicrosoftTeamsUserIdentifier) {
             return transferCallToParticipantWithResponse(
                 new TransferCallToParticipantOptions((MicrosoftTeamsUserIdentifier) targetParticipant))
-                    .flatMap(FluxUtil::toMono);
-        } else if (targetParticipant instanceof MicrosoftTeamsAppIdentifier) {
-            return transferCallToParticipantWithResponse(
-                new TransferCallToParticipantOptions((MicrosoftTeamsAppIdentifier) targetParticipant))
                     .flatMap(FluxUtil::toMono);
         } else {
             throw logger.logExceptionAsError(new IllegalArgumentException("targetParticipant type is invalid."));
