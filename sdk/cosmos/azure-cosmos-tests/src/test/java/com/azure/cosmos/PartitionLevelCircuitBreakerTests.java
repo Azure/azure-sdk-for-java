d43a3b844827f24096d99eed7988563319942ee5
Abhijeet Mohanty <abhmohanty@microsoft.com>
2025-02-01 23:30:49 -0500

    Fixing CI / live tests pipeline.

--- sdk/cosmos/azure-cosmos-tests/src/test/java/com/azure/cosmos/PartitionLevelCircuitBreakerTests.java
+++ sdk/cosmos/azure-cosmos-tests/src/test/java/com/azure/cosmos/PartitionLevelCircuitBreakerTests.java
@@ -61,7 +61,6 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.lang.reflect.Field;
-import java.net.URI;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collections;
