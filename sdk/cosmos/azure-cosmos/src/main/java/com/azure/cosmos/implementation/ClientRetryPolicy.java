2d03b8b678d75d7e9b8803c689be66231ff0415a
Abhijeet Mohanty <abhmohanty@microsoft.com>
2025-02-03 17:47:54 -0500

    Addressing review comments.

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/ClientRetryPolicy.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/ClientRetryPolicy.java
@@ -217,12 +217,8 @@ public class ClientRetryPolicy extends DocumentClientRetryPolicy {
         // a different region for Document reads
         // For Document writes being retried on a different region there is no guarantee of idempotency as isAddressRefresh property
         // on RxDocumentServiceRequest instance is not set back to false after address resolution completes so hard to stay what stage timed out - address resolution or the Document write
-        if (!isMetaDataRequest && request.isReadOnly()) {
-            return true;
-        }
-
-        return false;
-    }
+        return request.isReadOnly();
+      }
 
     private ShouldRetryResult shouldRetryOnSessionNotAvailable(RxDocumentServiceRequest request) {
         this.sessionTokenRetryCount++;
