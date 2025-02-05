bf72f924d06bc7f7693cb5a2e4c5bf21b0e14e7b
Matt Metcalf <mrm9084@gmail.com>
2025-02-05 11:01:27 -0800

    Fixing Feature flag loading after both redesigns

--- sdk/spring/spring-cloud-azure-appconfiguration-config/src/main/java/com/azure/spring/cloud/appconfiguration/config/implementation/AppConfigurationFeatureManagementPropertySource.java
+++ sdk/spring/spring-cloud-azure-appconfiguration-config/src/main/java/com/azure/spring/cloud/appconfiguration/config/implementation/AppConfigurationFeatureManagementPropertySource.java
@@ -2,12 +2,17 @@
 // Licensed under the MIT License.
 package com.azure.spring.cloud.appconfiguration.config.implementation;
 
+import java.util.List;
+
 import org.springframework.core.env.EnumerablePropertySource;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 
 /**
  * Azure App Configuration PropertySource unique per Store Label(Profile) combo.
  *
- * <p>
+ * <p>O
  * i.e. If connecting to 2 stores and have 2 labels set 4 AppConfigurationPropertySources need to be created.
  * </p>
  */
@@ -19,6 +24,8 @@ class AppConfigurationFeatureManagementPropertySource extends EnumerableProperty
 
     private static final String FEATURE_FLAG_KEY = FEATURE_MANAGEMENT_KEY + ".feature_flags";
 
+    private static final ObjectMapper MAPPER = JsonMapper.builder().build();
+
     AppConfigurationFeatureManagementPropertySource(FeatureFlagClient featureFlagLoader) {
         super(FEATURE_MANAGEMENT_KEY, featureFlagLoader);
         this.featureFlagLoader = featureFlagLoader;
@@ -26,14 +33,18 @@ class AppConfigurationFeatureManagementPropertySource extends EnumerableProperty
 
     @Override
     public String[] getPropertyNames() {
-        String[] names = { FEATURE_FLAG_KEY };
+        if (featureFlagLoader != null && featureFlagLoader.getProperties().size() > 0) {
+            String[] names = { FEATURE_FLAG_KEY };
+            return names;
+        }
+        String[] names = { };
         return names;
     }
 
     @Override
     public Object getProperty(String name) {
         if (FEATURE_FLAG_KEY.equals(name)) {
-            return featureFlagLoader.getProperties();
+            return MAPPER.convertValue(featureFlagLoader.getProperties().values().toArray(), List.class);
         }
         return null;
     }
