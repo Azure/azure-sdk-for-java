64d510040e0f0f662c2b96a8fb97f8f4a7de5c07
XiaofeiCao <xiaofeicao@microsoft.com>
2025-02-05 15:32:37 +0800

    javadoc

--- sdk/core/azure-core-management/src/main/java/com/azure/core/management/implementation/polling/SyncPollOperation.java
+++ sdk/core/azure-core-management/src/main/java/com/azure/core/management/implementation/polling/SyncPollOperation.java
@@ -23,6 +23,9 @@ import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
+/**
+ * Synchronous poll operation for Azure resource manager (ARM) long-running-operation (LRO).
+ */
 public class SyncPollOperation {
     private static final LongRunningOperationStatus LRO_CANCELLED
         = LongRunningOperationStatus.fromString("Cancelled", true);
