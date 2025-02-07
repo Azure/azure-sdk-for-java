63635f43dabe5ed7e7944dcc7d9fa556411d5aa2
azure-sdk <azuresdk@microsoft.com>
2025-02-07 08:38:59 +0000

    [Automation] Generate Fluent Lite from TypeSpec deviceregistry

--- sdk/deviceregistry/azure-resourcemanager-deviceregistry/src/main/java/com/azure/resourcemanager/deviceregistry/DeviceRegistryManager.java
+++ sdk/deviceregistry/azure-resourcemanager-deviceregistry/src/main/java/com/azure/resourcemanager/deviceregistry/DeviceRegistryManager.java
@@ -221,7 +221,7 @@ public final class DeviceRegistryManager {
                 .append("-")
                 .append("com.azure.resourcemanager.deviceregistry")
                 .append("/")
-                .append("1.0.0-beta.1");
+                .append("1.0.0");
             if (!Configuration.getGlobalConfiguration().get("AZURE_TELEMETRY_DISABLED", false)) {
                 userAgentBuilder.append(" (")
                     .append(Configuration.getGlobalConfiguration().get("java.version"))
