45f06c09011b689173943b66655ebecfd06fdb16
Charlie Chen <yuchungchen@microsoft.com>
2025-02-11 00:38:32 +0800

    Add javadoc for CurrentConditionDuration

--- sdk/maps/azure-maps-weather/src/main/java/com/azure/maps/weather/models/CurrentConditionDuration.java
+++ sdk/maps/azure-maps-weather/src/main/java/com/azure/maps/weather/models/CurrentConditionDuration.java
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 package com.azure.maps.weather.models;
 
 import com.azure.core.util.ExpandableEnum;
@@ -9,6 +11,9 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * Defines values for CurrentConditionDuration.
+ */
 public class CurrentConditionDuration implements ExpandableEnum<Integer> {
     private static final Map<Integer, CurrentConditionDuration> VALUES = new ConcurrentHashMap<>();
 
