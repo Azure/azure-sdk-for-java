81a8866cc2a6e73698af1b68ed6ba07f20c70743
Fabian Meiswinkel <fabianm@microsoft.com>
2025-02-11 02:48:30 +0000

    Hack to make the dotnet-benchmark a simple repro for test against local proxy

---
+++ sdk/cosmos/azure-cosmos-dotnet-benchmark/src/main/java/com/azure/cosmos/dotnet/benchmark/ThinClientReproMain.java
@@ -0,0 +1,45 @@
+package com.azure.cosmos.dotnet.benchmark;
+
+import com.azure.cosmos.ConsistencyLevel;
+import com.azure.cosmos.CosmosAsyncClient;
+import com.azure.cosmos.CosmosAsyncContainer;
+import com.azure.cosmos.CosmosClientBuilder;
+import com.azure.cosmos.CosmosException;
+import com.azure.cosmos.models.CosmosItemResponse;
+import com.azure.cosmos.models.PartitionKey;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import java.util.UUID;
+
+public class ThinClientReproMain {
+    public static void main(String[] args) {
+        try {
+            System.setProperty("COSMOS.THINCLIENT_ENABLED", "true");
+            System.setProperty("COSMOS.HTTP2_ENABLED", "true");
+
+            CosmosAsyncClient client = new CosmosClientBuilder()
+                .key(System.getProperty("COSMOS.KEY"))
+                .endpoint(System.getProperty("COSMOS.ENDPOINT"))
+                .gatewayMode()
+                .consistencyLevel(ConsistencyLevel.SESSION)
+                .buildAsyncClient();
+
+            CosmosAsyncContainer container = client.getDatabase("NehaTestDb").getContainer("NehaTestContainer");
+            ObjectMapper mapper = new ObjectMapper();
+            ObjectNode doc = mapper.createObjectNode();
+            String idValue = UUID.randomUUID().toString();
+            doc.put("id", idValue);
+            System.out.println("Document to be ingested - " + doc.toPrettyString());
+            CosmosItemResponse<ObjectNode> createResponse = container.createItem(doc).block();
+            System.out.println("CREATE DIAGNOSTICS: " + createResponse.getDiagnostics());
+            CosmosItemResponse<ObjectNode> response = container.readItem(idValue, new PartitionKey(idValue), ObjectNode.class).block();
+            System.out.println("READ DIAGNOSTICS: " + response.getDiagnostics());
+            ObjectNode readDoc = response.getItem();
+
+            System.out.println("Document read - " + readDoc.toPrettyString());
+        } catch (CosmosException cosmosException) {
+            System.out.println("COSMOS ERROR: " + cosmosException);
+        }
+    }
+}
