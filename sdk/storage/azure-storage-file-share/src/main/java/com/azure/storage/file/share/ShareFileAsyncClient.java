4072d9ce6d3076d009916f7829063b423fee9929
Isabelle <ibrandes@microsoft.com>
2025-02-12 10:41:54 -0800

    comments and formatting

--- sdk/storage/azure-storage-file-share/src/main/java/com/azure/storage/file/share/ShareFileAsyncClient.java
+++ sdk/storage/azure-storage-file-share/src/main/java/com/azure/storage/file/share/ShareFileAsyncClient.java
@@ -3400,11 +3400,39 @@ public class ShareFileAsyncClient {
             .map(ModelHelper::createHardLinkResponse);
     }
 
+    /**
+     * NFS only. Creates a symbolic link to a file specified by the path.
+     * <!-- src_embed com.azure.storage.file.share.ShareFileAsyncClient.createSymbolicLink#String -->
+     * <pre>
+     * symbolicLinkClient.createSymbolicLink&#40;sourceClient.getFilePath&#40;&#41;&#41;
+     *     .subscribe&#40;response -&gt; System.out.printf&#40;&quot;Link count is %s.&quot;,
+     *         response.getPosixProperties&#40;&#41;.getLinkCount&#40;&#41;&#41;&#41;;
+     * </pre>
+     * <!-- end com.azure.storage.file.share.ShareFileAsyncClient.createSymbolicLink#String -->
+     *
+     * @param linkText The absolute or relative path of the file to be linked to.
+     * @return A {@link Mono} containing a {@link ShareFileInfo} describing the state of the symbolic link.
+     */
     public Mono<ShareFileInfo> createSymbolicLink(String linkText) {
         return createSymbolicLinkWithResponse(new ShareFileCreateSymbolicLinkOptions(linkText))
             .flatMap(FluxUtil::toMono);
     }
 
+    /**
+     * NFS only. Creates a symbolic link to a file specified by the path.
+     * <!-- src_embed com.azure.storage.file.share.ShareFileAsyncClient.createSymbolicLinkWithResponse#ShareFileCreateSymbolicLinkOptions -->
+     * <pre>
+     * ShareFileCreateSymbolicLinkOptions options = new ShareFileCreateSymbolicLinkOptions&#40;sourceClient.getFilePath&#40;&#41;&#41;;
+     * symbolicLinkClient.createSymbolicLinkWithResponse&#40;options&#41;
+     *     .subscribe&#40;response -&gt; System.out.printf&#40;&quot;Link count is %s.&quot;,
+     *         response.getValue&#40;&#41;.getPosixProperties&#40;&#41;.getLinkCount&#40;&#41;&#41;&#41;;
+     * </pre>
+     * <!-- end com.azure.storage.file.share.ShareFileAsyncClient.createSymbolicLinkWithResponse#ShareFileCreateSymbolicLinkOptions -->
+     *
+     * @param options {@link ShareFileCreateSymbolicLinkOptions}
+     * @return A {@link Mono} containing a {@link Response} whose {@link Response#getValue() value} contains {@link ShareFileInfo}
+     * describing the state of the symbolic link.
+     */
     public Mono<Response<ShareFileInfo>> createSymbolicLinkWithResponse(ShareFileCreateSymbolicLinkOptions options) {
         try {
             StorageImplUtils.assertNotNull("options", options);
@@ -3415,27 +3443,57 @@ public class ShareFileAsyncClient {
         }
     }
 
-    Mono<Response<ShareFileInfo>> createSymbolicLinkWithResponse(String linkText, ShareRequestConditions requestConditions, Context context) {
+    Mono<Response<ShareFileInfo>> createSymbolicLinkWithResponse(String linkText,
+        ShareRequestConditions requestConditions, Context context) {
         context = context == null ? Context.NONE : context;
         return this.azureFileStorageClient.getFiles()
-            .createSymbolicLinkWithResponseAsync(shareName, filePath, linkText, null, null,null, null, null,
+            .createSymbolicLinkWithResponseAsync(shareName, filePath, linkText, null, null, null, null, null,
                 requestConditions.getLeaseId(), null, null, context)
             .map(ModelHelper::createSymbolicLinkResponse);
     }
 
-    public Mono<ShareFileSymbolicLinkInfo> getSymbolicLink(){
+    /**
+     * Reads the value of the symbolic link. Only applicable if this {@link ShareFileAsyncClient} is pointed
+     * at an NFS symbolic link.
+     * <!-- src_embed com.azure.storage.file.share.ShareFileAsyncClient.getSymbolicLink -->
+     * <pre>
+     * symbolicLinkClient.getSymbolicLink&#40;&#41;
+     *     .subscribe&#40;response -&gt; &#123;
+     *         System.out.printf&#40;&quot;Link text is %s.&quot;, response.getLinkText&#40;&#41;&#41;;
+     *     &#125;&#41;;
+     * </pre>
+     * <!-- end com.azure.storage.file.share.ShareFileAsyncClient.getSymbolicLink -->
+     *
+     * @return A {@link Mono} containing a {@link ShareFileSymbolicLinkInfo} describing the symbolic link.
+     */
+    public Mono<ShareFileSymbolicLinkInfo> getSymbolicLink() {
         return getSymbolicLinkWithResponse().flatMap(FluxUtil::toMono);
     }
 
-    public Mono<Response<ShareFileSymbolicLinkInfo>> getSymbolicLinkWithResponse(){
-        try{
+    /**
+     * Reads the value of the symbolic link. Only applicable if this {@link ShareFileAsyncClient} is pointed
+     * at an NFS symbolic link.
+     * <!-- src_embed com.azure.storage.file.share.ShareFileAsyncClient.getSymbolicLinkWithResponse -->
+     * <pre>
+     * symbolicLinkClient.getSymbolicLinkWithResponse&#40;&#41;
+     *     .subscribe&#40;response -&gt; &#123;
+     *         System.out.printf&#40;&quot;Link text is %s.&quot;, response.getValue&#40;&#41;.getLinkText&#40;&#41;&#41;;
+     *     &#125;&#41;;
+     * </pre>
+     * <!-- end com.azure.storage.file.share.ShareFileAsyncClient.getSymbolicLinkWithResponse -->
+     *
+     * @return A {@link Mono} containing a {@link Response} whose {@link Response#getValue() value} contains a
+     * {@link ShareFileSymbolicLinkInfo} describing the symbolic link.
+     */
+    public Mono<Response<ShareFileSymbolicLinkInfo>> getSymbolicLinkWithResponse() {
+        try {
             return withContext(context -> getSymbolicLinkWithResponse(context));
         } catch (RuntimeException ex) {
             return monoError(LOGGER, ex);
         }
     }
 
-    Mono<Response<ShareFileSymbolicLinkInfo>> getSymbolicLinkWithResponse(Context context){
+    Mono<Response<ShareFileSymbolicLinkInfo>> getSymbolicLinkWithResponse(Context context) {
         context = context == null ? Context.NONE : context;
         return this.azureFileStorageClient.getFiles()
             .getSymbolicLinkWithResponseAsync(shareName, filePath, null, null, null, context)
