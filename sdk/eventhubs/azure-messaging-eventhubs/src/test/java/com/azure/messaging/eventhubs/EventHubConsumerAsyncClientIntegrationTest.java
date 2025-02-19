ce2417e654e25b604ff85ebf10e7dc526ce90d82
Connie Yau <conniey@microsoft.com>
2025-02-19 11:33:32 -0800

    Fix test breaks.

--- sdk/eventhubs/azure-messaging-eventhubs/src/test/java/com/azure/messaging/eventhubs/EventHubConsumerAsyncClientIntegrationTest.java
+++ sdk/eventhubs/azure-messaging-eventhubs/src/test/java/com/azure/messaging/eventhubs/EventHubConsumerAsyncClientIntegrationTest.java
@@ -167,7 +167,7 @@ public class EventHubConsumerAsyncClientIntegrationTest extends IntegrationTestB
         // Act & Assert
         try (EventHubConsumerAsyncClient consumer = builder.buildAsyncConsumerClient()) {
             final AtomicReference<LastEnqueuedEventProperties> lastViewed
-                = new AtomicReference<>(new LastEnqueuedEventProperties(null, null, null, null));
+                = new AtomicReference<>(new LastEnqueuedEventProperties(null, (String) null, null, null));
 
             StepVerifier.create(consumer.receiveFromPartition(partitionId, EventPosition.latest(), options).take(10))
                 .assertNext(event -> verifyLastRetrieved(lastViewed, event.getLastEnqueuedEventProperties(), true))
@@ -339,7 +339,7 @@ public class EventHubConsumerAsyncClientIntegrationTest extends IntegrationTestB
         final EventHubConsumerAsyncClient consumer = toClose(builder.prefetchCount(1).buildAsyncConsumerClient());
 
         final AtomicReference<LastEnqueuedEventProperties> lastViewed
-            = new AtomicReference<>(new LastEnqueuedEventProperties(null, null, null, null));
+            = new AtomicReference<>(new LastEnqueuedEventProperties(null, (String) null, null, null));
 
         // Act & Assert
         try {
