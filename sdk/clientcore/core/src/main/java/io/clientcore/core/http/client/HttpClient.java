8d95c66f7b59b2efefd5627ad5948ee463a40e76
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2025-02-19 17:58:28 -0500

    Add async story to ClientCore

--- sdk/clientcore/core/src/main/java/io/clientcore/core/http/client/HttpClient.java
+++ sdk/clientcore/core/src/main/java/io/clientcore/core/http/client/HttpClient.java
@@ -6,21 +6,48 @@ package io.clientcore.core.http.client;
 import io.clientcore.core.http.models.HttpRequest;
 import io.clientcore.core.http.models.Response;
 import io.clientcore.core.implementation.http.client.DefaultHttpClientProvider;
+import io.clientcore.core.utils.SharedExecutorService;
 
 import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 
 /**
  * A generic interface for sending HTTP requests and getting responses.
  */
 public interface HttpClient {
     /**
-     * Sends the provided request synchronously with contextual information.
+     * Sends the provided request.
      *
      * @param request The HTTP request to send.
      * @return The response.
      * @throws IOException If an I/O error occurs during sending the request or receiving the response.
      */
     Response<?> send(HttpRequest request) throws IOException;
+
+    /**
+     * Sends the provided request asynchronously.
+     * <p>
+     * If an I/O error occurs while sending the request or receiving the response, the returned
+     * {@link CompletableFuture} will complete exceptionally.
+     *
+     * @param request The HTTP request to send.
+     * @return A CompletableFuture that will complete with the response.
+     */
+    default CompletableFuture<Response<?>> sendAsync(HttpRequest request) {
+        // TODO (alzimmer): Few thoughts here.
+        //  Is this API checked with IOException as the synchronous API is? Or is it mentioned in docs that getting the
+        //  result of the CompletableFuture may throw IOException if one occurred while sending the request?
+        //  Does this API need to accept an ExecutorService to manage which thread the CompletableFuture is executed on?
+        //  Or, does this return an extension type of CompletableFuture that can set the ExecutorService after the fact?
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                return send(request);
+            } catch (IOException e) {
+                throw new CompletionException(e);
+            }
+        }, SharedExecutorService.getInstance());
+    }
 
     /**
      * Get a new instance of the {@link HttpClient} that the {@link HttpClientProvider} loaded from the classpath is
