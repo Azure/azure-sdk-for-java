f1cc96fb610ebe03d83fccfe7f564443820cbcb0
vcolin7 <victor.y.asi@gmail.com>
2025-02-21 19:17:01 -0800

    Added codesnippets.

--- sdk/keyvault/azure-security-keyvault-keys/src/main/java/com/azure/security/keyvault/keys/KeyClient.java
+++ sdk/keyvault/azure-security-keyvault-keys/src/main/java/com/azure/security/keyvault/keys/KeyClient.java
@@ -850,7 +850,7 @@ public final class KeyClient {
      * {@link KeyVaultKey retrieved key}.</p>
      * <!-- src_embed com.azure.security.keyvault.keys.KeyClient.getKey#String-String -->
      * <pre>
-     * String keyVersion = &quot;6A385B124DEF4096AF1361A85B16C204&quot;;
+     * String keyVersion = &quot;&lt;key-version&gt;&quot;;
      * KeyVaultKey keyWithVersion = keyClient.getKey&#40;&quot;keyName&quot;, keyVersion&#41;;
      *
      * System.out.printf&#40;&quot;Retrieved key with name: %s and: id %s%n&quot;, keyWithVersion.getName&#40;&#41;,
@@ -884,7 +884,7 @@ public final class KeyClient {
      * {@link KeyVaultKey retrieved key}.</p>
      * <!-- src_embed com.azure.security.keyvault.keys.KeyClient.getKeyWithResponse#String-String-Context -->
      * <pre>
-     * String keyVersion = &quot;6A385B124DEF4096AF1361A85B16C204&quot;;
+     * String keyVersion = &quot;&lt;key-version&gt;&quot;;
      * Response&lt;KeyVaultKey&gt; getKeyResponse =
      *     keyClient.getKeyWithResponse&#40;&quot;keyName&quot;, keyVersion, new Context&#40;&quot;key1&quot;, &quot;value1&quot;&#41;&#41;;
      *
@@ -1841,7 +1841,7 @@ public final class KeyClient {
      * <p>Releases a {@link KeyVaultKey key}. Prints out the signed object that contains the release key.</p>
      * <!-- src_embed com.azure.security.keyvault.keys.KeyClient.releaseKey#String-String-String -->
      * <pre>
-     * String myKeyVersion = &quot;6A385B124DEF4096AF1361A85B16C204&quot;;
+     * String myKeyVersion = &quot;&lt;key-version&gt;&quot;;
      * String myTargetAttestationToken = &quot;someAttestationToken&quot;;
      * ReleaseKeyResult releaseKeyVersionResult =
      *     keyClient.releaseKey&#40;&quot;keyName&quot;, myKeyVersion, myTargetAttestationToken&#41;;
@@ -1876,7 +1876,7 @@ public final class KeyClient {
      * {@link Response HTTP Response} details and the signed object that contains the release key.</p>
      * <!-- src_embed com.azure.security.keyvault.keys.KeyClient.releaseKeyWithResponse#String-String-String-ReleaseKeyOptions-Context -->
      * <pre>
-     * String releaseKeyVersion = &quot;6A385B124DEF4096AF1361A85B16C204&quot;;
+     * String releaseKeyVersion = &quot;&lt;key-version&gt;&quot;;
      * String someTargetAttestationToken = &quot;someAttestationToken&quot;;
      * ReleaseKeyOptions releaseKeyOptions = new ReleaseKeyOptions&#40;&#41;
      *     .setAlgorithm&#40;KeyExportEncryptionAlgorithm.RSA_AES_KEY_WRAP_256&#41;
@@ -2156,6 +2156,24 @@ public final class KeyClient {
      * <p>Gets the latest version of the {@link KeyVaultKey key} in the key vault, including its attestation
      * information. Prints out the details of the {@link KeyVaultKey retrieved key}.</p>
      * <!-- src_embed com.azure.security.keyvault.keys.KeyClient.getKeyAttestation#String -->
+     * <pre>
+     * KeyVaultKey keyVaultKey = keyClient.getKeyAttestation&#40;&quot;keyName&quot;&#41;;
+     *
+     * System.out.printf&#40;&quot;Retrieved key with name: %s and: id %s%n&quot;, keyVaultKey.getName&#40;&#41;,
+     *     keyVaultKey.getId&#40;&#41;&#41;;
+     *
+     * KeyAttestation keyAttestationInfo = keyVaultKey.getProperties&#40;&#41;.getKeyAttestation&#40;&#41;;
+     *
+     * System.out.printf&#40;&quot;Attestation information details: %n&quot;
+     *         + &quot;Certificate PEM file: %s%n&quot;
+     *         + &quot;Private key attestation: %s%n&quot;
+     *         + &quot;Public key attestation: %s%n&quot;
+     *         + &quot;Version: %s&quot;,
+     *     Base64Url.encode&#40;keyAttestationInfo.getCertificatePemFile&#40;&#41;&#41;,
+     *     Base64Url.encode&#40;keyAttestationInfo.getPrivateKeyAttestation&#40;&#41;&#41;,
+     *     Base64Url.encode&#40;keyAttestationInfo.getPublicKeyAttestation&#40;&#41;&#41;,
+     *     keyAttestationInfo.getVersion&#40;&#41;&#41;;
+     * </pre>
      * <!-- end com.azure.security.keyvault.keys.KeyClient.getKeyAttestation#String -->
      *
      * @param name The name of the {@link KeyVaultKey key}, cannot be {@code null}.
@@ -2181,6 +2199,26 @@ public final class KeyClient {
      * <p>Gets a specific version of the {@link KeyVaultKey key} in the key vault, including its attestation
      * information. Prints out the details of the {@link KeyVaultKey retrieved key}.</p>
      * <!-- src_embed com.azure.security.keyvault.keys.KeyClient.getKeyAttestationWithResponse#String-String-Context -->
+     * <pre>
+     * String keyVersion = &quot;&lt;key-version&gt;&quot;;
+     * Response&lt;KeyVaultKey&gt; getKeyResponse =
+     *     keyClient.getKeyAttestationWithResponse&#40;&quot;keyName&quot;, keyVersion, new Context&#40;&quot;key1&quot;, &quot;value1&quot;&#41;&#41;;
+     * KeyVaultKey keyVaultKey = getKeyResponse.getValue&#40;&#41;;
+     *
+     * System.out.printf&#40;&quot;Retrieved key with name: %s and: id %s%n&quot;, keyVaultKey.getName&#40;&#41;, keyVaultKey.getId&#40;&#41;&#41;;
+     *
+     * KeyAttestation keyAttestationInfo = keyVaultKey.getProperties&#40;&#41;.getKeyAttestation&#40;&#41;;
+     *
+     * System.out.printf&#40;&quot;Attestation information details: %n&quot;
+     *         + &quot;Certificate PEM file: %s%n&quot;
+     *         + &quot;Private key attestation: %s%n&quot;
+     *         + &quot;Public key attestation: %s%n&quot;
+     *         + &quot;Version: %s&quot;,
+     *     Base64Url.encode&#40;keyAttestationInfo.getCertificatePemFile&#40;&#41;&#41;,
+     *     Base64Url.encode&#40;keyAttestationInfo.getPrivateKeyAttestation&#40;&#41;&#41;,
+     *     Base64Url.encode&#40;keyAttestationInfo.getPublicKeyAttestation&#40;&#41;&#41;,
+     *     keyAttestationInfo.getVersion&#40;&#41;&#41;;
+     * </pre>
      * <!-- end com.azure.security.keyvault.keys.KeyClient.getKeyAttestationWithResponse#String-String-Context -->
      *
      * @param name The name of the {@link KeyVaultKey key}, cannot be {@code null}.
