0087d9584d2bf7a4a534ce666f81c59df3f37605
azure-sdk <azuresdk@microsoft.com>
2025-02-25 05:46:14 +0000

    Reset sources for azure-communication-rooms to the release version 1.1.8.

--- sdk/communication/azure-communication-rooms/src/main/java/com/azure/communication/rooms/models/AddOrUpdateParticipantsResult.java
+++ sdk/communication/azure-communication-rooms/src/main/java/com/azure/communication/rooms/models/AddOrUpdateParticipantsResult.java
@@ -7,9 +7,5 @@ package com.azure.communication.rooms.models;
  * Represents the result of an addOrUpdate operation for participants in a room.
  */
 public final class AddOrUpdateParticipantsResult {
-    /**
-     * Creates an instance of {@link AddOrUpdateParticipantsResult}.
-     */
-    public AddOrUpdateParticipantsResult() {
-    }
+
 }
