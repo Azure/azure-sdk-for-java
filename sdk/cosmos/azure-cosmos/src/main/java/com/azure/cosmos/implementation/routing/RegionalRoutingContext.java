8db98bb153a3f13ac9151384592cd9b834cc8356
Abhijeet Mohanty <abhmohanty@microsoft.com>
2025-02-26 13:01:30 -0500

    Fixing CI pipeline.

---
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/routing/RegionalRoutingContext.java
@@ -0,0 +1,32 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.cosmos.implementation.routing;
+
+import java.net.URI;
+import java.util.Objects;
+
+public class RegionalRoutingContext {
+    private final URI gatewayRegionalEndpoint;
+
+    public RegionalRoutingContext(URI gatewayRegionalEndpoint) {
+        this.gatewayRegionalEndpoint = gatewayRegionalEndpoint;
+    }
+
+    public URI getGatewayRegionalEndpoint() {
+        return this.gatewayRegionalEndpoint;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RegionalRoutingContext that = (RegionalRoutingContext) o;
+        return this.gatewayRegionalEndpoint.equals(that.gatewayRegionalEndpoint);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.gatewayRegionalEndpoint);
+    }
+}
