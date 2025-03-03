ec7a099bd358cbab63d3b4b2199f9c14f4737d39
Abhijeet Mohanty <abhmohanty@microsoft.com>
2025-03-03 10:04:24 -0500

    Addressing RegionalRoutingContext.

---
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/routing/RegionalRoutingContext.java
@@ -0,0 +1,38 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.cosmos.implementation.routing;
+
+import java.net.URI;
+import java.util.Objects;
+
+public class RegionalRoutingContext {
+
+    // IMPORTANT:
+    // Please reevaluate overridden equals() implementation
+    // when adding additional properties to this class
+    private final URI gatewayRegionalEndpoint;
+    private final String gatewayRegionalEndpointAsString;
+
+    public RegionalRoutingContext(URI gatewayRegionalEndpoint) {
+        this.gatewayRegionalEndpoint = gatewayRegionalEndpoint;
+        this.gatewayRegionalEndpointAsString = gatewayRegionalEndpoint.toString();
+    }
+
+    public URI getGatewayRegionalEndpoint() {
+        return this.gatewayRegionalEndpoint;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RegionalRoutingContext that = (RegionalRoutingContext) o;
+        return this.gatewayRegionalEndpoint.equals(that.gatewayRegionalEndpoint);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.gatewayRegionalEndpointAsString);
+    }
+}
