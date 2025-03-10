fa640078ccc76c1c5bc8b441bb36b9c3c8a44d6d
Moary Chen <v-shilichen@microsoft.com>
2025-03-10 14:39:16 +0800

    Update sdk/keyvault/azure-security-keyvault-jca/src/main/java/com/azure/security/keyvault/jca/implementation/utils/HttpUtil.java
    Co-authored-by: Xiaolu Dai <31124698+saragluna@users.noreply.github.com>

--- sdk/keyvault/azure-security-keyvault-jca/src/main/java/com/azure/security/keyvault/jca/implementation/utils/HttpUtil.java
+++ sdk/keyvault/azure-security-keyvault-jca/src/main/java/com/azure/security/keyvault/jca/implementation/utils/HttpUtil.java
@@ -145,7 +145,7 @@ public final class HttpUtil {
                 HttpEntity entity = response.getEntity();
                 result = entity != null ? EntityUtils.toString(entity) : null;
             } else {
-                LOGGER.log(WARNING, "Can not get response successfully because return http status code is " + status
+                LOGGER.log(WARNING, "Fail to get response from Key Vault because return http status code is " + status
                     + ". "
                     + "It can be caused by missing permissions or roles. To know how to add permissions or roles, "
                     + "see https://github.com/Azure/azure-sdk-for-java/tree/main/sdk/keyvault/azure-security-keyvault-jca#prerequisites.");
