e6a2647e2052366ac94da6332eb1f08f26b1baee
Scott Beddall <scbedd@microsoft.com>
2025-03-13 16:47:14 -0700

    more changes

--- eng/common/scripts/job-matrix/Create-PrJobMatrix.ps1
+++ eng/common/scripts/job-matrix/Create-PrJobMatrix.ps1
@@ -160,7 +160,7 @@ function GeneratePRMatrixForBatch {
     # we only need to modify the generated job name if there is more than one matrix config + batch
     $matrixSuffixNecessary = $matrixBatchesByConfig.Keys.Count -gt 1
 
-    Write-Host "We are iterating over the batches for matrix config count $matrixBatchCount"
+    Write-Host "We are iterating over the batches for matrix config count $matrixBatchCount. Is a matrix suffix necessary: $matrixSuffixNecessary"
     # if we are doing direct packages (or a full indirect matrix), we need to walk the batches and duplicate the matrix config for each batch, fully assigning
     # the each batch's packages to the matrix config. This will generate a _non-sparse_ matrix for the incoming packages
     if ($directBatch -or $FullSparseMatrix) {
@@ -171,6 +171,8 @@ function GeneratePRMatrixForBatch {
         $namesForBatch = ($batch | ForEach-Object { $_.ArtifactName }) -join ","
 
         foreach ($matrixOutputItem in $matrixResults) {
+          Write-Host "Dumping direct matrix item before modifying name"
+          Write-Host ($matrixOutputItem | Out-String)
           # we need to clone this, as each item is an object with possible children
           $outputItem = $matrixOutputItem | ConvertTo-Json -Depth 100 | ConvertFrom-Json -AsHashtable
           # we just need to iterate across them, grab the parameters hashtable, and add the new key
@@ -197,6 +199,8 @@ function GeneratePRMatrixForBatch {
       $batchSuffixNecessary = $packageBatches.Length -gt 0
       $batchCounter = 1
       foreach ($batch in $packageBatches) {
+        Write-Host "Dumping indirect matrix item before modifying name"
+        Write-Host $batch | Out-String
         $namesForBatch = ($batch | ForEach-Object { $_.ArtifactName }) -join ","
         $outputItem = QueuePop -queue ([ref]$matrixResults)
 
