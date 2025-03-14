6513d3a2ddf2d2282928c8417335e25d3f336d58
Sameeksha Vaity (from Dev Box) <savaity@microsoft.com>
2025-03-14 15:14:57 -0700

    remove rest proxy code and move shared test to annotation-processor-test module

---
+++ sdk/clientcore/annotation-processor-test/src/test/java/io/clientcore/annotation/processor/test/implementation/client/HttpClientProvidersIT.java
@@ -0,0 +1,28 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package io.clientcore.annotation.processor.test.implementation.client;
+
+import io.clientcore.core.http.client.HttpClient;
+import io.clientcore.core.implementation.http.client.JdkHttpClient;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledForJreRange;
+import org.junit.jupiter.api.condition.JRE;
+
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+
+/**
+ * Tests for the {@link HttpClient} class.
+ * <p>
+ * Now that the default HttpClient, and related code, are using multi-release JARs this must be an integration test as
+ * the full JAR must be available to use the multi-release code.
+ */
+@DisabledForJreRange(max = JRE.JAVA_11)
+public class HttpClientProvidersIT {
+    @Test
+    public void testNoProvider() {
+        HttpClient httpClient = HttpClient.getSharedInstance();
+
+        assertInstanceOf(JdkHttpClient.class, httpClient);
+    }
+}
