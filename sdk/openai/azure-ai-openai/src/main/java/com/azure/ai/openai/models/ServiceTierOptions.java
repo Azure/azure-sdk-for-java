9bb871d6278ba2d521a1e1f17ea57ae482dd3f23
Jose Alvarez <jp.alvarezl@gmail.com>
2025-03-19 16:27:22 +0100

    Alternative spelling

--- sdk/openai/azure-ai-openai/src/main/java/com/azure/ai/openai/models/ServiceTierOptions.java
+++ sdk/openai/azure-ai-openai/src/main/java/com/azure/ai/openai/models/ServiceTierOptions.java
@@ -10,8 +10,8 @@ import java.util.Collection;
  * Specifies the latency tier to use for processing the request.
  * This parameter is relevant for customers subscribed to the scale tier service:
  *  - If set to 'auto', and the Project is Scale tier enabled, the system will utilize scale tier credits until they are exhausted.
- *  - If set to 'auto', and the Project is not Scale tier enabled, the request will be processed using the default service tier with a lower uptime SLA and no latency guarentee.
- *  - If set to 'default', the request will be processed using the default service tier with a lower uptime SLA and no latency guarentee.
+ *  - If set to 'auto', and the Project is not Scale tier enabled, the request will be processed using the default service tier with a lower uptime SLA and no latency guaranty.
+ *  - If set to 'default', the request will be processed using the default service tier with a lower uptime SLA and no latency guaranty.
  *  - When not set, the default behavior is 'auto'.
  * When this parameter is set, the response body will include the `service_tier` utilized.
  */
