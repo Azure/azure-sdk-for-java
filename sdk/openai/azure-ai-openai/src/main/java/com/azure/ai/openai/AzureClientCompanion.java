965f1c79b9efa3480fb805975a36ee47171c0bbe
Jose Alvarez <jp.alvarezl@gmail.com>
2025-03-27 11:16:55 +0100

    Project compiles

--- sdk/openai/azure-ai-openai/src/main/java/com/azure/ai/openai/AzureClientCompanion.java
+++ sdk/openai/azure-ai-openai/src/main/java/com/azure/ai/openai/AzureClientCompanion.java
@@ -1,6 +1,13 @@
 package com.azure.ai.openai;
 
+import com.openai.client.OpenAIClient;
+
 // Placeholder class
 public class AzureClientCompanion {
 
+    private final OpenAIClient client;
+
+    public AzureClientCompanion(OpenAIClient client) {
+        this.client = client;
+    }
 }
