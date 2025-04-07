7e90fae1eb002da8599585801b649ba9c01674cb
Sanjana Kapur <sanjanakapur@microsoft.com>
2025-04-07 16:05:26 -0700

    Timeout changed from int32 to duration

--- sdk/batch/azure-compute-batch/src/main/java/com/azure/compute/batch/models/BatchNodeFilesListOptions.java
+++ sdk/batch/azure-compute-batch/src/main/java/com/azure/compute/batch/models/BatchNodeFilesListOptions.java
@@ -5,6 +5,7 @@ package com.azure.compute.batch.models;
 
 import com.azure.core.annotation.Fluent;
 import com.azure.core.annotation.Generated;
+import java.time.Duration;
 
 /**
  * Optional parameters for List Node Files operation.
@@ -17,7 +18,7 @@ public final class BatchNodeFilesListOptions {
      * value is larger than 30, the default will be used instead.".
      */
     @Generated
-    private Integer timeOutInSeconds;
+    private Long timeOutInSeconds;
 
     /*
      * The maximum number of items to return in the response. A maximum of 1000
@@ -53,21 +54,11 @@ public final class BatchNodeFilesListOptions {
      * @return the timeOutInSeconds value.
      */
     @Generated
-    public Integer getTimeOutInSeconds() {
-        return this.timeOutInSeconds;
-    }
-
-    /**
-     * Set the timeOutInSeconds property: The maximum time that the server can spend processing the request, in seconds.
-     * The default is 30 seconds. If the value is larger than 30, the default will be used instead.".
-     *
-     * @param timeOutInSeconds the timeOutInSeconds value to set.
-     * @return the BatchNodeFilesListOptions object itself.
-     */
-    @Generated
-    public BatchNodeFilesListOptions setTimeOutInSeconds(Integer timeOutInSeconds) {
-        this.timeOutInSeconds = timeOutInSeconds;
-        return this;
+    public Duration getTimeOutInSeconds() {
+        if (this.timeOutInSeconds == null) {
+            return null;
+        }
+        return Duration.ofSeconds(this.timeOutInSeconds);
     }
 
     /**
@@ -137,6 +128,23 @@ public final class BatchNodeFilesListOptions {
     @Generated
     public BatchNodeFilesListOptions setRecursive(Boolean recursive) {
         this.recursive = recursive;
+        return this;
+    }
+
+    /**
+     * Set the timeOutInSeconds property: The maximum time that the server can spend processing the request, in seconds.
+     * The default is 30 seconds. If the value is larger than 30, the default will be used instead.".
+     *
+     * @param timeOutInSeconds the timeOutInSeconds value to set.
+     * @return the BatchNodeFilesListOptions object itself.
+     */
+    @Generated
+    public BatchNodeFilesListOptions setTimeOutInSeconds(Duration timeOutInSeconds) {
+        if (timeOutInSeconds == null) {
+            this.timeOutInSeconds = null;
+        } else {
+            this.timeOutInSeconds = timeOutInSeconds.getSeconds();
+        }
         return this;
     }
 }
