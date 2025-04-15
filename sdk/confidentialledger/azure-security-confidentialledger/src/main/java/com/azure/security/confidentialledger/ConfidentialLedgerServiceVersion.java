7ecbc4f8a0e35798c11cc5190ffcd0f02e193cb7
amruthashree <aanekondamat@microsoft.com>
2025-04-14 21:05:32 -0500

    Fixing test failures

--- sdk/confidentialledger/azure-security-confidentialledger/src/main/java/com/azure/security/confidentialledger/ConfidentialLedgerServiceVersion.java
+++ sdk/confidentialledger/azure-security-confidentialledger/src/main/java/com/azure/security/confidentialledger/ConfidentialLedgerServiceVersion.java
@@ -11,6 +11,10 @@ import com.azure.core.util.ServiceVersion;
  */
 public enum ConfidentialLedgerServiceVersion implements ServiceVersion {
     /**
+     * Enum value 2022-05-13.
+     */
+    V2022_05_13("2022-05-13"),
+    /**
      * Enum value 2024-12-09-preview.
      */
     V2024_12_09_PREVIEW("2024-12-09-preview");
