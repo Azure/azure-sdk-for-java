a3417899feb97581f8cc0d08f6bb73b2025efffd
Fabian Meiswinkel <fabianm@microsoft.com>
2025-04-17 18:31:42 +0000

    Fixing LINTing errors

--- sdk/cosmos/azure-cosmos-kafka-connect/src/main/java/com/azure/cosmos/kafka/connect/implementation/source/MetadataEntityTypes.java
+++ sdk/cosmos/azure-cosmos-kafka-connect/src/main/java/com/azure/cosmos/kafka/connect/implementation/source/MetadataEntityTypes.java
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 package com.azure.cosmos.kafka.connect.implementation.source;
 
 final class MetadataEntityTypes {
--- sdk/cosmos/azure-cosmos-kafka-connect/src/main/java/com/azure/cosmos/kafka/connect/implementation/source/UnifiedMetadataSchemaConstants.java
+++ sdk/cosmos/azure-cosmos-kafka-connect/src/main/java/com/azure/cosmos/kafka/connect/implementation/source/UnifiedMetadataSchemaConstants.java
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 package com.azure.cosmos.kafka.connect.implementation.source;
 
 import org.apache.kafka.connect.data.Schema;
