39d263478619740638bcac7e1e0c3193bac86d47
Sofia Robles Sandoval <sofiar@microsoft.com>
2025-04-30 11:20:21 -0600

    Fix linting issues

--- sdk/communication/azure-communication-phonenumbers/src/main/java/com/azure/communication/phonenumbers/PhoneNumbersClient.java
+++ sdk/communication/azure-communication-phonenumbers/src/main/java/com/azure/communication/phonenumbers/PhoneNumbersClient.java
@@ -1074,12 +1074,12 @@ public final class PhoneNumbersClient {
      * status code.
      * 
      * @param request The request object containing the reservation ID, phone numbers to add, and phone numbers to remove.
-     * @param context       A {@link Context} representing the request context.
+     * @param context A {@link Context} representing the request context.
      * @return represents a reservation for phone numbers along with
      *         {@link Response}.
      */
     @ServiceMethod(returns = ReturnType.SINGLE)
-    public Response<PhoneNumbersReservation> createOrUpdateReservation(CreateOrUpdateReservationOptions request,
+    public Response<PhoneNumbersReservation> createOrUpdateReservationWithResponse(CreateOrUpdateReservationOptions request,
         Context context) {
         String reservationId
             = request.getReservationId() != null ? request.getReservationId() : UUID.randomUUID().toString();
--- sdk/communication/azure-communication-phonenumbers/src/test/java/com/azure/communication/phonenumbers/PhoneNumbersClientIntegrationTest.java
+++ sdk/communication/azure-communication-phonenumbers/src/test/java/com/azure/communication/phonenumbers/PhoneNumbersClientIntegrationTest.java
@@ -45,7 +45,6 @@ import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
