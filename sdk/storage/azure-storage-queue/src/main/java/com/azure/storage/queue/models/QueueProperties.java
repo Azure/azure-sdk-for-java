7e22359ab1c6c31d69b4bf0d3597871a53448d2b
gunjansingh-msft <gunjansingh@microsoft.com>
2025-05-13 20:47:12 +0530

    QueueProperties feature changes

--- sdk/storage/azure-storage-queue/src/main/java/com/azure/storage/queue/models/QueueProperties.java
+++ sdk/storage/azure-storage-queue/src/main/java/com/azure/storage/queue/models/QueueProperties.java
@@ -14,6 +14,7 @@ import java.util.Map;
 public final class QueueProperties {
     private final Map<String, String> metadata;
     private final int approximateMessagesCount;
+    private final long approximateMessagesCountLong;
 
     /**
      * Creates an instance that contains properties of a queue.
@@ -21,9 +22,10 @@ public final class QueueProperties {
      * @param metadata Metadata associated with the queue.
      * @param approximateMessagesCount Approximate number of messages contained in the queue.
      */
-    public QueueProperties(Map<String, String> metadata, int approximateMessagesCount) {
+    public QueueProperties(Map<String, String> metadata, long approximateMessagesCount) {
         this.metadata = metadata;
-        this.approximateMessagesCount = approximateMessagesCount;
+        this.approximateMessagesCount = Math.toIntExact(approximateMessagesCount);
+        this.approximateMessagesCountLong = approximateMessagesCount;
     }
 
     /**
@@ -42,5 +44,14 @@ public final class QueueProperties {
      */
     public int getApproximateMessagesCount() {
         return approximateMessagesCount;
+    }
+
+    /**
+     * Gets the approximate number of messages contained in the queue at the time of properties retrieval.
+     *
+     * @return the approximate number of messages contained in the queue at the time of properties retrieval.
+     */
+    public long getApproximateMessagesCountLong() {
+        return approximateMessagesCountLong;
     }
 }
