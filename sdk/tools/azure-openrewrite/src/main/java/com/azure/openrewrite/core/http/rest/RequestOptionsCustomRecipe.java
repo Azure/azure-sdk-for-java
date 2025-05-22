a414d5227a541f62ab09feb0fdeb035e3de3ff7b
Jair Myree <jairmyree@microsoft.com>
2025-05-22 12:01:27 -0700

    Updates to RequestOptions (in progress)

--- sdk/tools/azure-openrewrite/src/main/java/com/azure/openrewrite/core/http/rest/RequestOptionsCustomRecipe.java
+++ sdk/tools/azure-openrewrite/src/main/java/com/azure/openrewrite/core/http/rest/RequestOptionsCustomRecipe.java
@@ -1,13 +1,17 @@
 package com.azure.openrewrite.core.http.rest;
 
+import org.openrewrite.Cursor;
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.Recipe;
 import org.openrewrite.java.JavaTemplate;
 import org.openrewrite.java.JavaVisitor;
 import org.openrewrite.java.MethodMatcher;
+import org.openrewrite.java.tree.Flag;
 import org.openrewrite.java.tree.J;
 
 import com.azure.openrewrite.util.ConfiguredParserJavaTemplateBuilder;
+import org.openrewrite.java.tree.JavaType;
+import org.openrewrite.java.tree.Space;
 
 /**
  * A custom OpenRewrite recipe to migrate the use of RequestOptions.
@@ -45,6 +49,7 @@ public class RequestOptionsCustomRecipe extends Recipe {
     @Override
     public JavaVisitor<ExecutionContext> getVisitor() {
         ConfiguredParserJavaTemplateBuilder templateBuilder = ConfiguredParserJavaTemplateBuilder.defaultBuilder();
+        final String FLUENT_KEY = "NewClassFluent";
         return new JavaVisitor<ExecutionContext>() {
 
             @Override
@@ -56,16 +61,113 @@ public class RequestOptionsCustomRecipe extends Recipe {
                 MethodMatcher methodMatcher;
                 JavaTemplate replacementTemplate;
 
+
                 methodMatcher = new MethodMatcher("com.azure.core.http.rest.RequestOptions <constructor>()");
                 if (methodMatcher.matches((J.NewClass) n)) {
-                    replacementTemplate = templateBuilder.getJavaTemplateBuilder("RequestContext.none()")
-                            .imports("io.clientcore.core.http.models.RequestContext")
-                            .build();
+                    boolean isFluent = ctx.getMessage(FLUENT_KEY) != null
+                        && (Boolean) ctx.getMessage(FLUENT_KEY);
 
-                    n = replacementTemplate.apply(updateCursor(newClass), newClass.getCoordinates().replace());
+                    if (isFluent) {
+                        // If the new class is a fluent API, we need to replace it with a fluent API
+                        replacementTemplate = templateBuilder.getJavaTemplateBuilder("RequestContext.builder()")
+                                .imports("io.clientcore.core.http.models.RequestContext")
+                                .build();
+                        ctx.putMessage(FLUENT_KEY, null);
+                        n = replacementTemplate.apply(getCursor(), newClass.getCoordinates().replace());
+                    } else {
+                        // If the new class is not a fluent API, we need to replace it with a non-fluent API
+                        replacementTemplate = templateBuilder.getJavaTemplateBuilder("RequestContext.none()")
+                                .imports("io.clientcore.core.http.models.RequestContext")
+                                .build();
+                        n = replacementTemplate.apply(updateCursor(newClass), newClass.getCoordinates().replace());
+                    }
+
                 }
 
                 return n;
+            }
+
+            @Override
+            public J visitMethodInvocation(J.MethodInvocation method, ExecutionContext ctx) {
+                // replace RequestOptions constructor with RequestContext static method
+                // Before: com.azure.core.http.rest.RequestOptions <constructor>()
+                // After: io.clientcore.core.http.models.RequestContext none()
+                J.MethodInvocation m = (J.MethodInvocation) super.visitMethodInvocation(method, ctx);
+                MethodMatcher methodMatcher;
+                JavaTemplate replacementTemplate;
+                boolean isFluent = isFluentChainMethod(method);
+                Recipe formatter = new org.openrewrite.java.format.TabsAndIndents();
+                doAfterVisit(formatter.getVisitor());
+
+                JavaType.Method methodType = method.getMethodType();
+                if (methodType != null &&
+                    !isStaticMethod(method) &&
+                    methodType.getReturnType() == methodType.getDeclaringType() &&
+                    (method.getSelect() instanceof J.Identifier)) {
+                    J.Assignment reassignment = doVariableReassignment(m);
+                    return reassignment;
+                }
+
+                if (method.getSelect() != null && method.getSelect() instanceof J.NewClass) {
+                    ctx.putMessage(FLUENT_KEY, true);
+                    J.MethodInvocation invokeBuilder = (J.MethodInvocation) visit(method.getSelect(), ctx);
+                    m = m.withSelect(invokeBuilder);
+                    return m;
+                }
+
+                if (isFluent) {
+                    m = invokeBuildOnBuilderChain(m, updateCursor(m));
+                    return m;
+                }
+
+
+                return m;
+            }
+
+            private boolean isStaticMethod(J.MethodInvocation methodInvocation) {
+                // Returns true if the method is a static method.
+                // This is determined by checking if the method has a return type of the same class as the method being called.
+                JavaType.Method methodType = methodInvocation.getMethodType();
+                if (methodType == null) {
+                    return false;
+                }
+
+                return methodType.getFlags().contains(Flag.Static);
+            }
+
+            private J.Assignment doVariableReassignment(J.MethodInvocation methodInvocation) {
+                J.Identifier variableName = (J.Identifier) methodInvocation.getSelect();
+                String code = String.format("%s = %s", variableName.getSimpleName(), methodInvocation.toString());
+                JavaTemplate replacementTemplate = templateBuilder.getJavaTemplateBuilder(code)
+                    .imports("io.clientcore.core.http.models.RequestContext")
+                    .build();
+                return replacementTemplate.apply(updateCursor(methodInvocation), methodInvocation.getCoordinates().replace());
+            }
+
+            private boolean isFluentChainMethod(J.MethodInvocation methodInvocation) {
+                // Returns true if the method is part a fluent API chain.
+                // This is determined by checking if the method has a return type of the same class as the method being called.
+                JavaType.Method methodType = methodInvocation.getMethodType();
+                if (methodType == null) {
+                    return false;
+                }
+                return methodType.getReturnType() == methodType.getDeclaringType() && methodInvocation.getSelect() instanceof J.MethodInvocation;
+            }
+
+            // assumes that the method is a fluent API. Do not call on non-fluent APIs.
+            private J.MethodInvocation invokeBuildOnBuilderChain(J.MethodInvocation methodInvocation, Cursor cursor) {
+                // Returns true if the method is the end of a fluent call chain.
+                // This is determined by checking if the method has a return type of the same class as the method being called.
+
+                if (methodInvocation.getSelect() instanceof J.MethodInvocation && !(cursor.getParentTreeCursor().getValue() instanceof J.MethodInvocation)) {
+                    JavaTemplate replacementTemplate = templateBuilder.getJavaTemplateBuilder("build()")
+                            .imports("io.clientcore.core.http.models.RequestContext")
+                            .contextSensitive()
+                            .build();
+                    J.MethodInvocation invokeBuild = replacementTemplate.apply(updateCursor(methodInvocation), methodInvocation.getCoordinates().replace());
+                    return invokeBuild.withSelect(methodInvocation);
+                }
+                return methodInvocation;
             }
         };
     }
--- sdk/tools/azure-openrewrite/src/test/resources/migrationExamples/azure-core/sample16/v1/RequestOptionsUse.java
+++ sdk/tools/azure-openrewrite/src/test/resources/migrationExamples/azure-core/sample16/v1/RequestOptionsUse.java
@@ -1,3 +1,4 @@
+import com.azure.core.http.HttpHeaderName;
 import com.azure.core.http.rest.RequestOptions;
 
 public class RequestOptionsUse {
@@ -5,6 +6,21 @@ public class RequestOptionsUse {
 
         // Sample 1: Basic usage
         RequestOptions options1 = new RequestOptions();
+        options1.addHeader("Custom-Header", "CustomValue");
+        options1.setHeader("Another-Header", "AnotherValue");
+
+        options1.addHeader(HttpHeaderName.CONTENT_TYPE, "application/json");
+        options1.setHeader(HttpHeaderName.ACCEPT, "application/json");
+
+        options1
+            .addQueryParam("queryParam1", "value1")
+            .addQueryParam("queryParam2", "value2")
+            .addQueryParam("queryParam3", "value3");
+
+        RequestOptions options2 = new RequestOptions()
+            .addHeader(HttpHeaderName.CONTENT_TYPE, "application/json")
+            .setHeader(HttpHeaderName.ACCEPT, "application/json")
+            .addQueryParam("queryParam1", "value1");
 
     }
 }
