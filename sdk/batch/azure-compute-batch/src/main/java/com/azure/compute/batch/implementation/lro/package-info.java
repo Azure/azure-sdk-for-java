0bce24652c1c96ab8fe12fce893425dc54c3b73f
Sanjana Kapur <sanjanakapur@microsoft.com>
2025-05-22 17:02:42 -0700

    Update Javadocs and address comments

---
+++ sdk/batch/azure-compute-batch/src/main/java/com/azure/compute/batch/implementation/lro/package-info.java
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+/**
+ * Package containing classes for long-running operation (LRO) polling implementations
+ * used internally by the Azure Batch client library.
+ */
+package com.azure.compute.batch.implementation.lro;
