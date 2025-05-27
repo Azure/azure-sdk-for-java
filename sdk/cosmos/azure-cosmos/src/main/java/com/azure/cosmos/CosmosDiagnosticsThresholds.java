94429321adfd4a5afd22a1aa2670b9c9ace8c73d
Fabian Meiswinkel <fabianm@microsoft.com>
2025-05-27 18:03:53 +0000

    Update CosmosDiagnosticsThresholds.java

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/CosmosDiagnosticsThresholds.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/CosmosDiagnosticsThresholds.java
@@ -76,6 +76,16 @@ public final class CosmosDiagnosticsThresholds {
      * Creates an instance of the CosmosDiagnosticsThresholds class with default values
      */
     public CosmosDiagnosticsThresholds() {
+
+        StringBuilder sb = new StringBuilder();
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+
+        for (StackTraceElement element : stackTrace) {
+            sb.append(element.toString()).append("\n");
+        }
+
+        LOGGER.info("CTOR CosmosDiagnosticsThresholds - {}", sb);
+
         this.pointOperationLatencyThreshold = DEFAULT_POINT_OPERATION_LATENCY_THRESHOLD;
         this.nonPointOperationLatencyThreshold = DEFAULT_NON_POINT_OPERATION_LATENCY_THRESHOLD;
         this.requestChargeThreshold = DEFAULT_REQUEST_CHARGE_THRESHOLD;
@@ -102,6 +112,14 @@ public final class CosmosDiagnosticsThresholds {
             "Argument 'pointOperationLatencyThreshold' must not be null.");
 
         this.pointOperationLatencyThreshold = pointOperationLatencyThreshold;
+        StringBuilder sb = new StringBuilder();
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+
+        for (StackTraceElement element : stackTrace) {
+            sb.append(element.toString()).append("\n");
+        }
+
+        LOGGER.info("setPointOperationLatencyThreshold {} - {}", pointOperationLatencyThreshold,  sb);
 
         return this;
     }
