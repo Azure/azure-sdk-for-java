2e68e3ae4847e39543eec7bef8d3b22b04f84052
Fabian Meiswinkel <fabianm@microsoft.com>
2025-05-29 14:10:58 +0000

    Update Http2ResponseHeaderCleanerHandler.java

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/http/Http2ResponseHeaderCleanerHandler.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/http/Http2ResponseHeaderCleanerHandler.java
@@ -6,45 +6,40 @@ package com.azure.cosmos.implementation.http;
 import com.azure.cosmos.implementation.HttpConstants;
 import com.azure.cosmos.implementation.apachecommons.lang.StringUtils;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http2.Http2Headers;
 import io.netty.handler.codec.http2.Http2HeadersFrame;
-import io.netty.handler.codec.http2.Http2SettingsAckFrame;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Http2ResponseHeaderCleanerHandler extends ChannelInboundHandlerAdapter {
+public class Http2ResponseHeaderCleanerHandler extends SimpleChannelInboundHandler<Http2HeadersFrame> {
 
     private static final Logger logger = LoggerFactory.getLogger(Http2ResponseHeaderCleanerHandler.class);
+
+    public Http2ResponseHeaderCleanerHandler() {
+        super(Http2HeadersFrame.class, false);
+    }
+
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, Http2HeadersFrame headersFrame) throws Exception {
+        Http2Headers headers = headersFrame.headers();
 
-        if (msg instanceof Http2HeadersFrame) {
-            Http2HeadersFrame headersFrame = (Http2HeadersFrame) msg;
-            Http2Headers headers = headersFrame.headers();
+        headers.forEach(entry -> {
+            CharSequence key = entry.getKey();
+            CharSequence value = entry.getValue();
 
-            headers.forEach(entry -> {
-                CharSequence key = entry.getKey();
-                CharSequence value = entry.getValue();
+            // Based on the tests, only 'x-ms-serviceversion' header has extra value,
+            // so only check this specific header here
+            if (StringUtils.equalsIgnoreCase(key, HttpConstants.HttpHeaders.SERVER_VERSION)) {
+                // Check for leading whitespace or other prohibited characters
+                if (StringUtils.isNotEmpty(value) && (value.charAt(0) == ' ' || value.charAt(value.length() - 1) == ' ')) {
+                    // Clean up the header value by trimming or handling as needed
+                    logger.trace("There are extra white space for key {} with value {}", key, value);
 
-                // Based on the tests, only 'x-ms-serviceversion' header has extra value,
-                // so only check this specific header here
-                if (StringUtils.equalsIgnoreCase(key, HttpConstants.HttpHeaders.SERVER_VERSION)) {
-                    // Check for leading whitespace or other prohibited characters
-                    if (StringUtils.isNotEmpty(value) && (value.charAt(0) == ' ' || value.charAt(value.length() - 1) == ' ')) {
-                        // Clean up the header value by trimming or handling as needed
-                        logger.trace("There are extra white space for key {} with value {}", key, value);
-
-                        // TODO[Http2]: for now just trim the spaces, explore other options for example escape the whitespace
-                        headers.set(key, value.toString().trim());
-                    }
+                    // TODO[Http2]: for now just trim the spaces, explore other options for example escape the whitespace
+                    headers.set(key, value.toString().trim());
                 }
-            });
-        //} else if (msg instanceof Http2SettingsAckFrame) {
-            // We ignore the Http2SettingsAckFrame
-        } else {
-            // Pass the message to the next handler in the pipeline
-            ctx.fireChannelRead(msg);
-        }
+            }
+        });
     }
 }
