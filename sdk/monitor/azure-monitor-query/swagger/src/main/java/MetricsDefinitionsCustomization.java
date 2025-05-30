c9d7202269ad53ae7310678b4efd75f0c50fca1d
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2025-05-30 09:16:11 -0400

    Fix a few things

--- sdk/monitor/azure-monitor-query/swagger/src/main/java/MetricsDefinitionsCustomization.java
+++ sdk/monitor/azure-monitor-query/swagger/src/main/java/MetricsDefinitionsCustomization.java
@@ -3,19 +3,31 @@
 
 import com.azure.autorest.customization.Customization;
 import com.azure.autorest.customization.LibraryCustomization;
+import com.github.javaparser.StaticJavaParser;
 import org.slf4j.Logger;
 
 public class MetricsDefinitionsCustomization extends Customization {
 
     @Override
     public void customize(LibraryCustomization libraryCustomization, Logger logger) {
-        String replace = libraryCustomization.getRawEditor().getFileContent("src/main/java/com/azure/monitor/query/implementation" +
-                        "/metricsdefinitions/AzureMonitorMetricsDefinitionsAPIBuilder.java")
-                .replace("policies.add(new BearerTokenAuthenticationPolicy(tokenCredential, String.format(\"%s/" +
-                                ".default\", host)));",
-                        "String localHost = (host != null) ? host : \"https://management.azure.com\";\n" +
-                                "policies.add(new BearerTokenAuthenticationPolicy(tokenCredential, String.format(\"%s/.default\", localHost)));");
-        libraryCustomization.getRawEditor().replaceFile("src/main/java/com/azure/monitor/query/implementation" +
-                "/metricsdefinitions/AzureMonitorMetricsDefinitionsAPIBuilder.java", replace);
+        libraryCustomization.getClass("com.azure.monitor.query.implementation.metricsdefinitions", "AzureMonitorMetricsDefinitionsAPIBuilder")
+            .customizeAst(ast -> ast.getClassByName("AzureMonitorMetricsDefinitionsAPIBuilder").ifPresent(clazz -> {
+                // Update createHttpPipeline to better handle Managed Identity.
+                clazz.getMethodsByName("createHttpPipeline").forEach(method -> method.getBody().ifPresent(body -> {
+                    String bodyStr = body.toString().replace(
+                        "policies.add(new BearerTokenAuthenticationPolicy(tokenCredential, String.format(\"%s/.default\", host)));",
+                        "String localHost = (host != null) ? host : \"https://management.azure.com\";"
+                            + "policies.add(new BearerTokenAuthenticationPolicy(tokenCredential, String.format(\"%s/.default\", localHost)));");
+                    method.setBody(StaticJavaParser.parseBlock(bodyStr));
+                }));
+
+                // Update validateClient to not validate subscriptionId was set as there isn't a way to do so using the
+                // public builder.
+                clazz.getMethodsByName("validateClient").forEach(method -> method.getBody().ifPresent(body -> {
+                    String bodyStr = body.toString().replace(
+                        "Objects.requireNonNull(subscriptionId, \"'subscriptionId' cannot be null.\");", "");
+                    method.setBody(StaticJavaParser.parseBlock(bodyStr));
+                }));
+            }));
     }
 }
