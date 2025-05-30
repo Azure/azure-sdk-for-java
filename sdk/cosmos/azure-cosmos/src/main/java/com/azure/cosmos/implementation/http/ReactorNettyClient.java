eaf38fce53c944d5fc23d2fc02a39f7750361fb9
Fabian Meiswinkel <fabianm@microsoft.com>
2025-05-30 11:36:09 +0000

    Update ReactorNettyClient.java

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/http/ReactorNettyClient.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/http/ReactorNettyClient.java
@@ -4,6 +4,7 @@ package com.azure.cosmos.implementation.http;
 
 import com.azure.cosmos.Http2ConnectionConfig;
 import com.azure.cosmos.implementation.Configs;
+import com.azure.cosmos.implementation.CosmosSchedulers;
 import com.azure.cosmos.implementation.ImplementationBridgeHelpers;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.Channel;
@@ -271,10 +272,12 @@ public class ReactorNettyClient implements HttpClient {
             .uri(request.uri().toASCIIString())
             .send(bodySendDelegate(request))
             .responseConnection((reactorNettyResponse, reactorNettyConnection) -> {
-                HttpResponse httpResponse = new ReactorNettyHttpResponse(reactorNettyResponse,
-                    reactorNettyConnection).withRequest(request);
-                responseReference.set((ReactorNettyHttpResponse) httpResponse);
-                return Mono.just(httpResponse);
+                return Mono.fromCallable(() -> {
+                    HttpResponse httpResponse = new ReactorNettyHttpResponse(reactorNettyResponse,
+                        reactorNettyConnection).withRequest(request);
+                    responseReference.set((ReactorNettyHttpResponse) httpResponse);
+                    return httpResponse;
+                }).subscribeOn(CosmosSchedulers.TRANSPORT_RESPONSE_BOUNDED_ELASTIC);
             })
             .contextWrite(Context.of(REACTOR_NETTY_REQUEST_RECORD_KEY, request.reactorNettyRequestRecord()))
             .doOnCancel(() -> {
