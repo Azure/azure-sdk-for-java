81e1190864f8982bacc5373996c3ccec0bcc5538
Fabian Meiswinkel <fabianm@microsoft.com>
2025-05-30 19:27:32 +0000

    Update RxGatewayStoreModel.java

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/RxGatewayStoreModel.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/RxGatewayStoreModel.java
@@ -369,8 +369,7 @@ public class RxGatewayStoreModel implements RxStoreModel, HttpTransportSerialize
                                                                       RxDocumentServiceRequest request,
                                                                       HttpRequest httpRequest) {
 
-        // .publishOn(CosmosSchedulers.TRANSPORT_RESPONSE_BOUNDED_ELASTIC)
-        return httpResponseMono.flatMap(httpResponse -> {
+        return httpResponseMono.publishOn(CosmosSchedulers.TRANSPORT_RESPONSE_BOUNDED_ELASTIC).flatMap(httpResponse -> {
 
             // header key/value pairs
             HttpHeaders httpResponseHeaders = httpResponse.headers();
@@ -378,9 +377,9 @@ public class RxGatewayStoreModel implements RxStoreModel, HttpTransportSerialize
 
             Mono<ByteBuf> contentObservable = httpResponse
                 .body()
-                .switchIfEmpty(Mono.just(Unpooled.EMPTY_BUFFER));
-                //.map(bodyByteBuf -> bodyByteBuf.retain())
-                //.publishOn(CosmosSchedulers.TRANSPORT_RESPONSE_BOUNDED_ELASTIC);
+                .switchIfEmpty(Mono.just(Unpooled.EMPTY_BUFFER))
+                .map(bodyByteBuf -> bodyByteBuf.retain())
+                .publishOn(CosmosSchedulers.TRANSPORT_RESPONSE_BOUNDED_ELASTIC);
 
             return contentObservable
                 .map(content -> {
