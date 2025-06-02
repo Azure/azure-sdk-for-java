0e1bbb60bb6f7566e0103d222927f10c11199fc0
Isabelle <ibrandes@microsoft.com>
2025-06-02 10:23:51 -0700

    updating module info to contain more packages

--- sdk/storage/azure-storage-common/src/main/java/module-info.java
+++ sdk/storage/azure-storage-common/src/main/java/module-info.java
@@ -26,6 +26,7 @@ module com.azure.storage.common {
         com.azure.data.tables, com.azure.storage.blob, com.azure.storage.blob.cryptography,
         com.azure.storage.file.share, com.azure.storage.file.datalake, com.azure.storage.queue;
 
-    exports com.azure.storage.common.implementation.structuredmessage
-        to com.azure.storage.blob, com.azure.storage.file.share, com.azure.storage.file.datalake;
+    exports com.azure.storage.common.implementation.structuredmessage to
+        com.azure.data.tables, com.azure.storage.blob, com.azure.storage.blob.cryptography,
+        com.azure.storage.file.share, com.azure.storage.file.datalake, com.azure.storage.queue;
 }
