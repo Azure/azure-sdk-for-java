5227ad01ea57f2b3eb6a0b03972aaf007831d2e8
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2025-06-02 17:59:52 -0400

    Update more TypeSpec code customizations

--- sdk/eventgrid/azure-messaging-eventgrid-namespaces/customization/src/main/java/EventGridCustomization.java
+++ sdk/eventgrid/azure-messaging-eventgrid-namespaces/customization/src/main/java/EventGridCustomization.java
@@ -1,12 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import com.azure.autorest.customization.ClassCustomization;
 import com.azure.autorest.customization.Customization;
 import com.azure.autorest.customization.LibraryCustomization;
 import com.azure.autorest.customization.PackageCustomization;
-import com.github.javaparser.ast.body.EnumConstantDeclaration;
-import com.github.javaparser.ast.body.EnumDeclaration;
+import com.github.javaparser.ast.Node;
 import org.slf4j.Logger;
 
 import java.util.Arrays;
@@ -18,29 +16,19 @@ public class EventGridCustomization extends Customization {
     @Override
     public void customize(LibraryCustomization customization, Logger logger) {
         removeExtraFiles(customization, logger);
-        removeSendEvents(customization, logger);
+        removeSendEvents(customization);
         customizeEventGridClientImplImports(customization, logger);
     }
 
-    private void removeSendEvents(LibraryCustomization customization, Logger logger) {
+    private void removeSendEvents(LibraryCustomization customization) {
         PackageCustomization packageCustomization = customization.getPackage("com.azure.messaging.eventgrid.namespaces");
-        ClassCustomization classCustomization = packageCustomization.getClass("EventGridSenderClient");
-        classCustomization.customizeAst(compilationUnit -> {
-            compilationUnit.getClassByName("EventGridSenderClient").get().getMethods().forEach(method -> {
-                if (method.getNameAsString().equals("sendEvents")) {
-                    method.remove();
-                }
-            });
-        });
+        packageCustomization.getClass("EventGridSenderClient").customizeAst(ast ->
+            ast.getClassByName("EventGridSenderClient").ifPresent(clazz ->
+                clazz.getMethodsByName("sendEvents").forEach(Node::remove)));
 
-        classCustomization = packageCustomization.getClass("EventGridSenderAsyncClient");
-        classCustomization.customizeAst(compilationUnit -> {
-            compilationUnit.getClassByName("EventGridSenderAsyncClient").get().getMethods().forEach(method -> {
-                if (method.getNameAsString().equals("sendEvents")) {
-                    method.remove();
-                }
-            });
-        });
+        packageCustomization.getClass("EventGridSenderAsyncClient").customizeAst(ast ->
+            ast.getClassByName("EventGridSenderAsyncClient").ifPresent(clazz ->
+                clazz.getMethodsByName("sendEvents").forEach(Node::remove)));
     }
 
     public void removeExtraFiles(LibraryCustomization customization, Logger logger) {
@@ -51,23 +39,20 @@ public class EventGridCustomization extends Customization {
     }
 
     public void customizeEventGridClientImplImports(LibraryCustomization customization, Logger logger) {
-
         Arrays.asList("com.azure.messaging.eventgrid.namespaces",
             "com.azure.messaging.eventgrid.namespaces.models",
             "com.azure.messaging.eventgrid.namespaces.implementation.models").forEach(p -> {
             logger.info("Working on " + p);
             PackageCustomization packageCustomization = customization.getPackage(p);
-            packageCustomization.listClasses().forEach(c -> {
-                c.customizeAst(comp -> {
-                    if (comp.getImports().removeIf(i -> i.getNameAsString().equals("com.azure.messaging.eventgrid.namespaces.models.CloudEvent"))) {
-                        logger.info("Removed CloudEvent import from " + c.getClassName());
-                        comp.addImport("com.azure.core.models.CloudEvent");
-                    }
-                    if (comp.getImports().removeIf(i -> i.getNameAsString().equals("com.azure.messaging.eventgrid.namespaces.implementation.models.PublishResult"))) {
-                        logger.info("Removed PublishResult import from " + c.getClassName());
-                    }
-                });
-            });
+            packageCustomization.listClasses().forEach(c -> c.customizeAst(comp -> {
+                if (comp.getImports().removeIf(i -> i.getNameAsString().equals("com.azure.messaging.eventgrid.namespaces.models.CloudEvent"))) {
+                    logger.info("Removed CloudEvent import from " + c.getClassName());
+                    comp.addImport("com.azure.core.models.CloudEvent");
+                }
+                if (comp.getImports().removeIf(i -> i.getNameAsString().equals("com.azure.messaging.eventgrid.namespaces.implementation.models.PublishResult"))) {
+                    logger.info("Removed PublishResult import from " + c.getClassName());
+                }
+            }));
         });
     }
 }
