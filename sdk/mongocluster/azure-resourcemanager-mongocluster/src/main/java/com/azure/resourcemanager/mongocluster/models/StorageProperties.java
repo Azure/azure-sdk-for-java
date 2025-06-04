f1814e95453e07cd154650e3c6ced9ff75060ac7
azure-sdk <azuresdk@microsoft.com>
2025-06-04 07:03:56 +0000

    Configurations:  'specification/mongocluster/DocumentDB.MongoCluster.Management/tspconfig.yaml', API Version: 2025-04-01-preview, SDK Release Type: beta, and CommitSHA: '99130fc37612a5118195f5b8e51a118b57fb67c6' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs'

--- sdk/mongocluster/azure-resourcemanager-mongocluster/src/main/java/com/azure/resourcemanager/mongocluster/models/StorageProperties.java
+++ sdk/mongocluster/azure-resourcemanager-mongocluster/src/main/java/com/azure/resourcemanager/mongocluster/models/StorageProperties.java
@@ -22,6 +22,21 @@ public final class StorageProperties implements JsonSerializable<StorageProperti
      */
     private Long sizeGb;
 
+    /*
+     * The type of storage to provision the cluster servers with.
+     */
+    private StorageType type;
+
+    /*
+     * The IOPs of the storage assigned to each server. Only applicable if the type is 'PremiumSSDv2'.
+     */
+    private Long iops;
+
+    /*
+     * The throughput of the storage assigned to each server. Only applicable if the type is 'PremiumSSDv2'.
+     */
+    private Long throughput;
+
     /**
      * Creates an instance of StorageProperties class.
      */
@@ -49,6 +64,70 @@ public final class StorageProperties implements JsonSerializable<StorageProperti
     }
 
     /**
+     * Get the type property: The type of storage to provision the cluster servers with.
+     * 
+     * @return the type value.
+     */
+    public StorageType type() {
+        return this.type;
+    }
+
+    /**
+     * Set the type property: The type of storage to provision the cluster servers with.
+     * 
+     * @param type the type value to set.
+     * @return the StorageProperties object itself.
+     */
+    public StorageProperties withType(StorageType type) {
+        this.type = type;
+        return this;
+    }
+
+    /**
+     * Get the iops property: The IOPs of the storage assigned to each server. Only applicable if the type is
+     * 'PremiumSSDv2'.
+     * 
+     * @return the iops value.
+     */
+    public Long iops() {
+        return this.iops;
+    }
+
+    /**
+     * Set the iops property: The IOPs of the storage assigned to each server. Only applicable if the type is
+     * 'PremiumSSDv2'.
+     * 
+     * @param iops the iops value to set.
+     * @return the StorageProperties object itself.
+     */
+    public StorageProperties withIops(Long iops) {
+        this.iops = iops;
+        return this;
+    }
+
+    /**
+     * Get the throughput property: The throughput of the storage assigned to each server. Only applicable if the type
+     * is 'PremiumSSDv2'.
+     * 
+     * @return the throughput value.
+     */
+    public Long throughput() {
+        return this.throughput;
+    }
+
+    /**
+     * Set the throughput property: The throughput of the storage assigned to each server. Only applicable if the type
+     * is 'PremiumSSDv2'.
+     * 
+     * @param throughput the throughput value to set.
+     * @return the StorageProperties object itself.
+     */
+    public StorageProperties withThroughput(Long throughput) {
+        this.throughput = throughput;
+        return this;
+    }
+
+    /**
      * Validates the instance.
      * 
      * @throws IllegalArgumentException thrown if the instance is not valid.
@@ -63,6 +142,9 @@ public final class StorageProperties implements JsonSerializable<StorageProperti
     public JsonWriter toJson(JsonWriter jsonWriter) throws IOException {
         jsonWriter.writeStartObject();
         jsonWriter.writeNumberField("sizeGb", this.sizeGb);
+        jsonWriter.writeStringField("type", this.type == null ? null : this.type.toString());
+        jsonWriter.writeNumberField("iops", this.iops);
+        jsonWriter.writeNumberField("throughput", this.throughput);
         return jsonWriter.writeEndObject();
     }
 
@@ -83,6 +165,12 @@ public final class StorageProperties implements JsonSerializable<StorageProperti
 
                 if ("sizeGb".equals(fieldName)) {
                     deserializedStorageProperties.sizeGb = reader.getNullable(JsonReader::getLong);
+                } else if ("type".equals(fieldName)) {
+                    deserializedStorageProperties.type = StorageType.fromString(reader.getString());
+                } else if ("iops".equals(fieldName)) {
+                    deserializedStorageProperties.iops = reader.getNullable(JsonReader::getLong);
+                } else if ("throughput".equals(fieldName)) {
+                    deserializedStorageProperties.throughput = reader.getNullable(JsonReader::getLong);
                 } else {
                     reader.skipChildren();
                 }
