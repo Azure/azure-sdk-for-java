2a2c81da478cac25796a9714d1d57aeeb37b8b99
Fabian Meiswinkel <fabianm@microsoft.com>
2025-06-05 21:17:52 +0000

    Refactoring sampling diagnostics logger usage

--- sdk/cosmos/azure-cosmos-benchmark/src/main/java/com/azure/cosmos/benchmark/AsyncBenchmark.java
+++ sdk/cosmos/azure-cosmos-benchmark/src/main/java/com/azure/cosmos/benchmark/AsyncBenchmark.java
@@ -173,7 +173,7 @@ abstract class AsyncBenchmark<T> {
                 cfg.getPointOperationThreshold(),
                 cfg.getNonPointOperationThreshold());
             telemetryConfig.diagnosticsHandler(
-                new SamplingDiagnosticsLogger(10, 10_000)
+                new CosmosSamplingDiagnosticsLogger(10, 10_000)
             );
         }
 
@@ -663,126 +663,5 @@ abstract class AsyncBenchmark<T> {
         else return null;
     }
 
-    private static class SamplingDiagnosticsLogger implements CosmosDiagnosticsHandler {
 
-        private final int maxLogCount;
-        private final AtomicInteger logCountInSamplingInterval;
-        private final ScheduledExecutorService executor;
-
-        public SamplingDiagnosticsLogger(int maxLogCount, int samplingIntervalInMs) {
-            checkArgument(maxLogCount > 0, "Argument 'maxLogCount must be a positive integer.");
-
-            this.logCountInSamplingInterval = new AtomicInteger(0);
-            this.maxLogCount = maxLogCount;
-            logger.info("MaxLogCount: {}, samplingIntervalInMs: {}", this.maxLogCount, samplingIntervalInMs);
-            executor = Executors.newSingleThreadScheduledExecutor(new CosmosDaemonThreadFactory("AsyncBenchmark_logSampling"));
-            executor.scheduleAtFixedRate(() -> {
-                    int snapshot = this.logCountInSamplingInterval.getAndSet(0);
-                    if (snapshot != 0) {
-                        logger.info("Resetting number of logs ({}-0)...", snapshot);
-                    }
-                },
-                samplingIntervalInMs,
-                samplingIntervalInMs,
-                TimeUnit.MILLISECONDS);
-        }
-
-        private final static Logger logger = LoggerFactory.getLogger(SamplingDiagnosticsLogger.class);
-
-        /**
-         * Decides whether to log diagnostics for an operation and emits the logs when needed
-         *
-         * @param diagnosticsContext the Cosmos DB diagnostic context with metadata for the operation
-         * @param traceContext the Azure trace context
-         */
-        @Override
-        public final void handleDiagnostics(CosmosDiagnosticsContext diagnosticsContext, Context traceContext) {
-            checkNotNull(diagnosticsContext, "Argument 'diagnosticsContext' must not be null.");
-
-            boolean shouldLog = shouldLog(diagnosticsContext);
-            if (shouldLog) {
-                int previousLogCount = this.logCountInSamplingInterval.getAndIncrement();
-
-                if (previousLogCount <= this.maxLogCount) {
-                    logger.info(
-                        "Account: {} -> DB: {}, Col:{}, StatusCode: {}:{} Diagnostics: {}",
-                        diagnosticsContext.getAccountName(),
-                        diagnosticsContext.getDatabaseName(),
-                        diagnosticsContext.getContainerName(),
-                        diagnosticsContext.getStatusCode(),
-                        diagnosticsContext.getSubStatusCode(),
-                        diagnosticsContext.toJson());
-                } else if (previousLogCount == this.maxLogCount + 1) {
-                    logger.info("Already logged {} diagnostics - stopping until sampling interval is reset.", this.maxLogCount);
-                }
-            }
-        }
-
-        /**
-         * Decides whether to log diagnostics for an operation
-         *
-         * @param diagnosticsContext the diagnostics context
-         * @return a flag indicating whether to log the operation or not
-         */
-        protected boolean shouldLog(CosmosDiagnosticsContext diagnosticsContext) {
-
-            if (!diagnosticsContext.isCompleted()) {
-                return false;
-            }
-
-            return diagnosticsContext.isFailure() ||
-                diagnosticsContext.isThresholdViolated() ||
-                logger.isDebugEnabled();
-        }
-
-        /**
-         * Logs the operation. This method can be overridden for example to emit logs to a different target than log4j
-         *
-         * @param ctx the diagnostics context
-         */
-        protected void log(CosmosDiagnosticsContext ctx) {
-            if (ctx.isFailure()) {
-                if (logger.isErrorEnabled()) {
-                    logger.error(
-                        "Account: {} -> DB: {}, Col:{}, StatusCode: {}:{} Diagnostics: {}",
-                        ctx.getAccountName(),
-                        ctx.getDatabaseName(),
-                        ctx.getContainerName(),
-                        ctx.getStatusCode(),
-                        ctx.getSubStatusCode(),
-                        ctx.toJson());
-                }
-            } else if (ctx.isThresholdViolated()) {
-                if (logger.isInfoEnabled()) {
-                    logger.info(
-                        "Account: {} -> DB: {}, Col:{}, StatusCode: {}:{} Diagnostics: {}",
-                        ctx.getAccountName(),
-                        ctx.getDatabaseName(),
-                        ctx.getContainerName(),
-                        ctx.getStatusCode(),
-                        ctx.getSubStatusCode(),
-                        ctx.toJson());
-                }
-            } else if (logger.isTraceEnabled()) {
-                logger.trace(
-                    "Account: {} -> DB: {}, Col:{}, StatusCode: {}:{} Diagnostics: {}",
-                    ctx.getAccountName(),
-                    ctx.getDatabaseName(),
-                    ctx.getContainerName(),
-                    ctx.getStatusCode(),
-                    ctx.getSubStatusCode(),
-                    ctx.toJson());
-            } else if (logger.isDebugEnabled()) {
-                logger.debug(
-                    "Account: {} -> DB: {}, Col:{}, StatusCode: {}:{}, Latency: {}, Request charge: {}",
-                    ctx.getAccountName(),
-                    ctx.getDatabaseName(),
-                    ctx.getContainerName(),
-                    ctx.getStatusCode(),
-                    ctx.getSubStatusCode(),
-                    ctx.getDuration(),
-                    ctx.getTotalRequestCharge());
-            }
-        }
-    }
 }
