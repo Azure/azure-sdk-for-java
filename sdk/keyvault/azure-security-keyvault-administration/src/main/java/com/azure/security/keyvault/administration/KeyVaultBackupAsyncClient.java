e2567a22f4f835f8df4b48529f3554d4f8cc6b45
vcolin7 <victor.y.asi@gmail.com>
2025-06-12 23:06:37 -0700

    Ran spotless

--- sdk/keyvault/azure-security-keyvault-administration/src/main/java/com/azure/security/keyvault/administration/KeyVaultBackupAsyncClient.java
+++ sdk/keyvault/azure-security-keyvault-administration/src/main/java/com/azure/security/keyvault/administration/KeyVaultBackupAsyncClient.java
@@ -319,8 +319,8 @@ public final class KeyVaultBackupAsyncClient {
 
         return (pollingContext) -> {
             try {
-                return withContext(context ->
-                    preBackupWithResponse(blobStorageUrl, sasToken, context)).map(Response::getValue);
+                return withContext(context -> preBackupWithResponse(blobStorageUrl, sasToken, context))
+                    .map(Response::getValue);
             } catch (RuntimeException e) {
                 return monoError(LOGGER, e);
             }
@@ -410,8 +410,8 @@ public final class KeyVaultBackupAsyncClient {
 
         return (pollingContext) -> {
             try {
-                return withContext(context ->
-                    backupWithResponse(blobStorageUrl, sasToken, context)).map(Response::getValue);
+                return withContext(context -> backupWithResponse(blobStorageUrl, sasToken, context))
+                    .map(Response::getValue);
             } catch (RuntimeException e) {
                 return monoError(LOGGER, e);
             }
@@ -582,8 +582,8 @@ public final class KeyVaultBackupAsyncClient {
 
         return (pollingContext) -> {
             try {
-                return withContext(context ->
-                    preRestoreWithResponse(folderUrl, sasToken, context)).map(Response::getValue);
+                return withContext(context -> preRestoreWithResponse(folderUrl, sasToken, context))
+                    .map(Response::getValue);
             } catch (RuntimeException e) {
                 return monoError(LOGGER, e);
             }
@@ -682,8 +682,8 @@ public final class KeyVaultBackupAsyncClient {
 
         return (pollingContext) -> {
             try {
-                return withContext(context ->
-                    restoreWithResponse(folderUrl, sasToken, context)).map(Response::getValue);
+                return withContext(context -> restoreWithResponse(folderUrl, sasToken, context))
+                    .map(Response::getValue);
             } catch (RuntimeException e) {
                 return monoError(LOGGER, e);
             }
@@ -845,8 +845,8 @@ public final class KeyVaultBackupAsyncClient {
 
         return (pollingContext) -> {
             try {
-                return withContext(context ->
-                    selectiveKeyRestoreWithResponse(keyName, folderUrl, sasToken, context)).map(Response::getValue);
+                return withContext(context -> selectiveKeyRestoreWithResponse(keyName, folderUrl, sasToken, context))
+                    .map(Response::getValue);
             } catch (RuntimeException e) {
                 return monoError(LOGGER, e);
             }
