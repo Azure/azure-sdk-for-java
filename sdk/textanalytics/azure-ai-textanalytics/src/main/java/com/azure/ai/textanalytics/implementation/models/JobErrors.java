d183fb64427195e88f07a0b199d40637efa08b01
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2025-06-16 11:22:46 -0400

    Update Text Analytics

--- sdk/textanalytics/azure-ai-textanalytics/src/main/java/com/azure/ai/textanalytics/implementation/models/JobErrors.java
+++ sdk/textanalytics/azure-ai-textanalytics/src/main/java/com/azure/ai/textanalytics/implementation/models/JobErrors.java
@@ -5,6 +5,7 @@
 package com.azure.ai.textanalytics.implementation.models;
 
 import com.azure.core.annotation.Fluent;
+import com.azure.core.annotation.Generated;
 import com.azure.json.JsonReader;
 import com.azure.json.JsonSerializable;
 import com.azure.json.JsonToken;
@@ -20,11 +21,13 @@ public final class JobErrors implements JsonSerializable<JobErrors> {
     /*
      * The errors property.
      */
+    @Generated
     private List<Error> errors;
 
     /**
      * Creates an instance of JobErrors class.
      */
+    @Generated
     public JobErrors() {
     }
 
@@ -33,6 +36,7 @@ public final class JobErrors implements JsonSerializable<JobErrors> {
      * 
      * @return the errors value.
      */
+    @Generated
     public List<Error> getErrors() {
         return this.errors;
     }
@@ -43,6 +47,7 @@ public final class JobErrors implements JsonSerializable<JobErrors> {
      * @param errors the errors value to set.
      * @return the JobErrors object itself.
      */
+    @Generated
     public JobErrors setErrors(List<Error> errors) {
         this.errors = errors;
         return this;
@@ -51,6 +56,7 @@ public final class JobErrors implements JsonSerializable<JobErrors> {
     /**
      * {@inheritDoc}
      */
+    @Generated
     @Override
     public JsonWriter toJson(JsonWriter jsonWriter) throws IOException {
         jsonWriter.writeStartObject();
@@ -66,6 +72,7 @@ public final class JobErrors implements JsonSerializable<JobErrors> {
      * to JSON null.
      * @throws IOException If an error occurs while reading the JobErrors.
      */
+    @Generated
     public static JobErrors fromJson(JsonReader jsonReader) throws IOException {
         return jsonReader.readObject(reader -> {
             JobErrors deserializedJobErrors = new JobErrors();
