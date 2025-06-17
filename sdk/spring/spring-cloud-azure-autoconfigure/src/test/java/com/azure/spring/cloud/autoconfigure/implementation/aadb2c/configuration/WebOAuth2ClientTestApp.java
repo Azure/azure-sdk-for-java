e9b373449ebc6d0cc8af90d081ad9891cf34549f
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2025-06-17 12:18:14 -0400

    More fixes

--- sdk/spring/spring-cloud-azure-autoconfigure/src/test/java/com/azure/spring/cloud/autoconfigure/implementation/aadb2c/configuration/WebOAuth2ClientTestApp.java
+++ sdk/spring/spring-cloud-azure-autoconfigure/src/test/java/com/azure/spring/cloud/autoconfigure/implementation/aadb2c/configuration/WebOAuth2ClientTestApp.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAu
 import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@SuppressWarnings({"removal", "deprecation"})
 @EnableWebSecurity
 @Import(OAuth2ClientAutoConfiguration.class)
 public class WebOAuth2ClientTestApp {
