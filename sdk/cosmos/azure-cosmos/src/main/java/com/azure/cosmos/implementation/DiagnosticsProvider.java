9a4389e1aab6f8cbbd0fd41c2bf4edfb998c58da
lloydmeta <lloydmeta@gmail.com>
2025-06-18 22:09:57 +0900

    [o11y] Fix some attributes to match Otel Semantic Conventions
    * db.cosmosdb.sub_status_code is now an int
      * Added `azure.cosmosdb.response.sub_status_code` to reflect this too
        since it's the non-deprecated label
    * db.cosmosdb.request_charge is now a double
      * Added `azure.cosmosdb.operation.request_charge` to reflect this too
    
    Sources
    * https://github.com/open-telemetry/semantic-conventions/blob/723789b86a803492d03191b458138fff6e7a6b9f/docs/registry/attributes/db.md#db-cosmosdb-request-charge
    * https://opentelemetry.io/docs/specs/semconv/registry/attributes/azure/#azure-cosmosdb-operation-request-charge
    
    Signed-off-by: lloydmeta <lloydmeta@gmail.com>

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/DiagnosticsProvider.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/DiagnosticsProvider.java
@@ -1536,11 +1536,19 @@ public final class DiagnosticsProvider {
                 context);
             tracer.setAttribute(
                 "db.cosmosdb.sub_status_code",
-                Integer.toString(cosmosCtx.getSubStatusCode()),
+                cosmosCtx.getSubStatusCode(),
+                context);
+            tracer.setAttribute(
+                "azure.cosmosdb.response.sub_status_code",
+                cosmosCtx.getSubStatusCode(),
                 context);
             tracer.setAttribute(
                 "db.cosmosdb.request_charge",
-                Float.toString(cosmosCtx.getTotalRequestCharge()),
+                (double) cosmosCtx.getTotalRequestCharge(),
+                context);
+            tracer.setAttribute(
+                "azure.cosmosdb.operation.request_charge",
+                (double) cosmosCtx.getTotalRequestCharge(),
                 context);
             tracer.setAttribute("db.cosmosdb.request_content_length",cosmosCtx.getMaxRequestPayloadSizeInBytes(), context);
             tracer.setAttribute("db.cosmosdb.max_response_content_length_bytes",cosmosCtx.getMaxResponsePayloadSizeInBytes(), context);
