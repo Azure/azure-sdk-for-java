89301cdc9403afe7934c26c07164eeaee8d53440
jairmyree <jairmyree@microsoft.com>
2025-06-20 17:53:30 -0700

    Change overview generation script

--- eng/pipelines/scripts/generate_overview_from_readme.py
+++ eng/pipelines/scripts/generate_overview_from_readme.py
@@ -46,13 +46,12 @@ def generate_overview(readme_file, version, overview_file_path):
 
         # Before passing the README contents to markdown2 clean out the codesnippet tags on the java code fences.
         # Clean ```java com.azure.core.aCodeSnippetTag to ```java, without doing this markdown2 won't properly process
-        # the contents of the code fence. Replace '&' with '&amp;' to avoid issues with the HTML parser.
+        # the contents of the code fence. 
         cleaned_readme_content_lines = []
         for line in raw_readme_content_lines:
             cleaned_readme_content_lines.append(re.sub(pattern="``` *java +[a-zA-Z0-9.#\-_]*", repl="```java", string=line, flags=re.UNICODE))
 
         readme_content = ''.join(cleaned_readme_content_lines)
-        # readme_content = re.sub(pattern='\\B&\\B', repl='&amp;', string=readme_content, flags=re.UNICODE)
 
         # markdown2.markdown will create html from the readme.md file. The fenced-code-blocks
         # extras being passed into the markdown call is necessary to deal with the embedded
