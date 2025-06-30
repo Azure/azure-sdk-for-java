f8f39cf1e58280985906432bf97c6f39b5975a54
tvaron3 <tomas.varon1802@gmail.com>
2025-06-29 21:29:20 -0700

    account data resolver fabric

---
+++ sdk/cosmos/azure-cosmos-spark-account-data-resolver-sample/src/main/scala/com/azure/cosmos/spark/samples/FabricAccountDataResolver.scala
@@ -0,0 +1,66 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+package com.azure.cosmos.spark.samples
+
+import com.azure.cosmos.spark.AccountDataResolver
+import com.azure.cosmos.spark.CosmosAccessToken
+
+import java.time.OffsetDateTime
+import scala.collection.concurrent.TrieMap
+
+class FabricAccountDataResolver extends AccountDataResolver with BasicLoggingTrait {
+    private val tokenProviders : TrieMap[ConfigParameters, Option[List[String] => CosmosAccessToken]] =
+            new TrieMap[ConfigParameters, Option[List[String] => CosmosAccessToken]]()
+    private final val AUDIENCE = "https://cosmos.azure.com/.default"
+
+
+  override def getAccountDataConfig(configs: Map[String, String]): Map[String, String] = {
+    val configParameters = ConfigParameters.apply(configs)
+    if (isEnabled(configParameters)) {
+      configs +
+        ("spark.cosmos.auth.type" -> "AccessToken")
+    } else {
+      configs
+    }
+  }
+
+  private def isEnabled(configs: ConfigParameters): Boolean = {
+    configs.isEnabledConfigValue.isDefined && configs.isEnabledConfigValue.get.toBoolean
+  }
+
+  override def getAccessTokenProvider(configs: Map[String, String]): Option[List[String] => CosmosAccessToken] = {
+    val configParameters = ConfigParameters.apply(configs)
+    tokenProviders.getOrElseUpdate(
+      configParameters,
+      getAccessTokenProviderImpl(configParameters)
+    )
+  }
+
+  private def getAccessTokenProviderImpl(configs: ConfigParameters): Option[List[String] => CosmosAccessToken] = {
+    if (isEnabled(configs)) {
+      logInfo(s"FabricAccountDataResolver is enabled")
+      Some((tokenRequestContextStrings: List[String]) => {
+        val accessToken = mssparkutils.credentials.getToken(AUDIENCE)
+        CosmosAccessToken(accessToken, OffsetDateTime.now().plusHours(2))
+      })
+    } else {
+      logInfo(s"FabricAccountDataResolver is disabled")
+      None
+    }
+  }
+
+  private[this] object FabricConfigNames {
+    val Audience = "cosmos.auth.fabric.audience"
+    val CustomAuthEnabled = "cosmos.auth.fabric.enabled"
+  }
+
+  private case class ConfigParameters(audience: Option[String], isEnabledConfigValue: Option[String])
+  private object ConfigParameters {
+    def apply(configs: Map[String, String]): ConfigParameters = {
+      new ConfigParameters(
+        configs.get(FabricConfigNames.Audience),
+        configs.get(FabricConfigNames.CustomAuthEnabled)
+      )
+    }
+  }
+}
