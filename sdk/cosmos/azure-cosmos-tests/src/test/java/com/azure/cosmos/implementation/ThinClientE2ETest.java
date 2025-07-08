fd827d1820d15c1ed37ef97ddd2b736da922d0cb
Fabian Meiswinkel <fabianm@microsoft.com>
2025-07-08 22:01:53 +0000

    Update ThinClientE2ETest.java

--- sdk/cosmos/azure-cosmos-tests/src/test/java/com/azure/cosmos/implementation/ThinClientE2ETest.java
+++ sdk/cosmos/azure-cosmos-tests/src/test/java/com/azure/cosmos/implementation/ThinClientE2ETest.java
@@ -50,8 +50,8 @@ public class ThinClientE2ETest {
         CosmosAsyncClient client = null;
         try {
             // If running locally, uncomment these lines
-            System.setProperty("COSMOS.THINCLIENT_ENABLED", "true");
-            System.setProperty("COSMOS.HTTP2_ENABLED", "true");
+            // System.setProperty("COSMOS.THINCLIENT_ENABLED", "true");
+            // System.setProperty("COSMOS.HTTP2_ENABLED", "true");
 
             client = new CosmosClientBuilder()
                 .endpoint(TestConfigurations.HOST)
@@ -98,8 +98,8 @@ public class ThinClientE2ETest {
         CosmosAsyncClient client = null;
         try {
             // If running locally, uncomment these lines
-            System.setProperty("COSMOS.THINCLIENT_ENABLED", "true");
-            System.setProperty("COSMOS.HTTP2_ENABLED", "true");
+            // System.setProperty("COSMOS.THINCLIENT_ENABLED", "true");
+            // System.setProperty("COSMOS.HTTP2_ENABLED", "true");
 
             client = new CosmosClientBuilder()
                 .endpoint(TestConfigurations.HOST)
@@ -139,8 +139,8 @@ public class ThinClientE2ETest {
         CosmosAsyncClient client = null;
         try {
             // If running locally, uncomment these lines
-            System.setProperty("COSMOS.THINCLIENT_ENABLED", "true");
-            System.setProperty("COSMOS.HTTP2_ENABLED", "true");
+            // System.setProperty("COSMOS.THINCLIENT_ENABLED", "true");
+            // System.setProperty("COSMOS.HTTP2_ENABLED", "true");
 
             client = new CosmosClientBuilder()
                 .endpoint(TestConfigurations.HOST)
@@ -213,8 +213,8 @@ public class ThinClientE2ETest {
         CosmosAsyncClient client = null;
         try {
             // if running locally, uncomment these lines
-            System.setProperty("COSMOS.THINCLIENT_ENABLED", "true");
-            System.setProperty("COSMOS.HTTP2_ENABLED", "true");
+            // System.setProperty("COSMOS.THINCLIENT_ENABLED", "true");
+            // System.setProperty("COSMOS.HTTP2_ENABLED", "true");
 
             client  = new CosmosClientBuilder()
                 .endpoint(TestConfigurations.HOST)
