995b11415c4fb39799a45a3ca30f233dd97f23e0
Sanjana Kapur <sanjanakapur@microsoft.com>
2025-07-11 21:47:38 -0700

    Fix Shared Key Test

--- sdk/batch/azure-compute-batch/src/test/java/com/azure/compute/batch/SharedKeyTests.java
+++ sdk/batch/azure-compute-batch/src/test/java/com/azure/compute/batch/SharedKeyTests.java
@@ -50,7 +50,7 @@ public class SharedKeyTests extends BatchClientTestBase {
     public void testPoolCRUD() {
         // Generate a jobId that is unique per test mode (sync vs async)
         String testModeSuffix = SyncAsyncExtension.execute(() -> "sync", () -> Mono.just("async"));
-        String poolId = sharedKeyPoolPrefix + "-" + testModeSuffix;
+        String poolId = sharedKeyPoolPrefix + "-" + testModeSuffix + "-" + System.currentTimeMillis();
         try {
             /*
              * Creating Pool
@@ -158,8 +158,12 @@ public class SharedKeyTests extends BatchClientTestBase {
             /*
              * Deleting Pool
              */
-            SyncAsyncExtension.execute(() -> batchClientWithSharedKey.deletePool(poolId),
-                () -> batchAsyncClientWithSharedKey.deletePool(poolId));
+            try {
+                SyncAsyncExtension.execute(() -> batchClientWithSharedKey.deletePool(poolId),
+                    () -> batchAsyncClientWithSharedKey.deletePool(poolId));
+            } catch (Exception e) {
+                // Ignore here
+            }
         }
     }
 }
