b38faee577faaf07997d2a3ab739a8e7dfaf948d
Sun Yuhan <sunyuhan1998@users.noreply.github.com>
2025-07-22 12:06:11 +0800

    fix: Changed the visibility of the interface `getImageGenerationsWithResponse` to public
    Signed-off-by: Sun Yuhan <sunyuhan1998@users.noreply.github.com>

--- sdk/openai/azure-ai-openai/src/main/java/com/azure/ai/openai/OpenAIAsyncClient.java
+++ sdk/openai/azure-ai-openai/src/main/java/com/azure/ai/openai/OpenAIAsyncClient.java
@@ -917,7 +917,7 @@ public final class OpenAIAsyncClient {
      * Gets transcribed text and associated metadata from provided spoken audio data. Audio will be transcribed in the
      * written language corresponding to the language it was spoken in.
      * <p><strong>Response Body Schema</strong></p>
-     * 
+     *
      * <pre>
      * {@code
      * {
@@ -1309,7 +1309,7 @@ public final class OpenAIAsyncClient {
      * Gets transcribed text and associated metadata from provided spoken audio data. Audio will be transcribed in the
      * written language corresponding to the language it was spoken in.
      * <p><strong>Response Body Schema</strong></p>
-     * 
+     *
      * <pre>
      * {@code
      * String
@@ -1340,7 +1340,7 @@ public final class OpenAIAsyncClient {
     /**
      * Gets English language transcribed text and associated metadata from provided spoken audio data.
      * <p><strong>Response Body Schema</strong></p>
-     * 
+     *
      * <pre>
      * {@code
      * {
@@ -1392,7 +1392,7 @@ public final class OpenAIAsyncClient {
     /**
      * Gets English language transcribed text and associated metadata from provided spoken audio data.
      * <p><strong>Response Body Schema</strong></p>
-     * 
+     *
      * <pre>
      * {@code
      * String
@@ -1521,7 +1521,7 @@ public final class OpenAIAsyncClient {
      * completion of {@link Mono}.
      */
     @ServiceMethod(returns = ReturnType.SINGLE)
-    Mono<Response<BinaryData>> getImageGenerationsWithResponse(String deploymentOrModelName,
+    public Mono<Response<BinaryData>> getImageGenerationsWithResponse(String deploymentOrModelName,
         BinaryData imageGenerationOptions, RequestOptions requestOptions) {
         return this.openAIServiceClient != null
             ? this.openAIServiceClient.getImageGenerationsWithResponseAsync(deploymentOrModelName,
