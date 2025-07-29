6ed53a9027edb69b1577e8dcccc1a1be146c84f5
browndav <browndav@microsoft.com>
2025-07-29 17:01:54 -0400

    fix javadocs to match nonencoded name for url

--- sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/BlobContainerClient.java
+++ sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/BlobContainerClient.java
@@ -150,7 +150,7 @@ public final class BlobContainerClient {
      * Initializes a new BlobClient object by concatenating blobName to the end of ContainerAsyncClient's URL. The new
      * BlobClient uses the same request policy pipeline as the ContainerAsyncClient.
      *
-     * @param blobName A {@code String} representing the name of the blob. This should not be URL encoded.
+     * @param blobName A {@code String} representing the name of the blob. This should not be encoded.
      * <p><strong>Code Samples</strong></p>
      *
      * <!-- src_embed com.azure.storage.blob.BlobContainerClient.getBlobClient#String -->
@@ -176,7 +176,7 @@ public final class BlobContainerClient {
      * </pre>
      * <!-- end com.azure.storage.blob.BlobContainerClient.getBlobClient#String-String -->
      *
-     * @param blobName A {@code String} representing the name of the blob. This should not be URL encoded.
+     * @param blobName A {@code String} representing the name of the blob. This should not be encoded.
      * @param snapshot the snapshot identifier for the blob.
      * @return A new {@link BlobClient} object which references the blob with the specified name in this container.
      */
@@ -189,7 +189,7 @@ public final class BlobContainerClient {
      * Initializes a new BlobClient object by concatenating blobName to the end of ContainerAsyncClient's URL. The new
      * BlobClient uses the same request policy pipeline as the ContainerAsyncClient.
      *
-     * @param blobName A {@code String} representing the name of the blob. This should not be URL encoded.
+     * @param blobName A {@code String} representing the name of the blob. This should not be encoded.
      * @param versionId the version identifier for the blob, pass {@code null} to interact with the latest blob version.
      * @return A new {@link BlobClient} object which references the blob with the specified name in this container.
      */
