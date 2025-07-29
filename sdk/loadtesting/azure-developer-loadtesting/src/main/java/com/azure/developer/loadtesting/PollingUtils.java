6888585d60694c3a0b15e0088808d4635abe8772
azure-sdk <azuresdk@microsoft.com>
2025-07-29 20:39:25 +0000

    Reset sources for azure-developer-loadtesting to the release version 1.0.23.

--- sdk/loadtesting/azure-developer-loadtesting/src/main/java/com/azure/developer/loadtesting/PollingUtils.java
+++ sdk/loadtesting/azure-developer-loadtesting/src/main/java/com/azure/developer/loadtesting/PollingUtils.java
@@ -5,12 +5,6 @@ package com.azure.developer.loadtesting;
 import com.azure.core.util.BinaryData;
 import com.azure.core.util.polling.LongRunningOperationStatus;
 import com.azure.core.util.polling.PollResponse;
-import com.azure.developer.loadtesting.models.FileValidationStatus;
-import com.azure.developer.loadtesting.models.LoadTestRun;
-import com.azure.developer.loadtesting.models.TestFileInfo;
-import com.azure.developer.loadtesting.models.TestProfileRun;
-import com.azure.developer.loadtesting.models.TestProfileRunStatus;
-import com.azure.developer.loadtesting.models.TestRunStatus;
 import com.azure.json.JsonProviders;
 import com.azure.json.JsonReader;
 import reactor.core.publisher.Mono;
@@ -29,7 +23,7 @@ final class PollingUtils {
     }
 
     static PollResponse<BinaryData> getValidationStatus(BinaryData fileBinary) throws RuntimeException {
-        String validationStatus;
+        String validationStatus, fileType;
 
         try (JsonReader jsonReader = JsonProviders.createReader(fileBinary.toBytes())) {
             Map<String, Object> jsonTree = jsonReader.readMap(JsonReader::readUntyped);
@@ -70,25 +64,6 @@ final class PollingUtils {
         }
 
         return new PollResponse<>(lroStatus, fileBinary);
-    }
-
-    static PollResponse<TestFileInfo> getValidationStatus(TestFileInfo testFile) throws RuntimeException {
-        LongRunningOperationStatus lroStatus;
-
-        if (FileValidationStatus.VALIDATION_NOT_REQUIRED.equals(testFile.getValidationStatus())
-            || FileValidationStatus.VALIDATION_SUCCESS.equals(testFile.getValidationStatus())) {
-            lroStatus = LongRunningOperationStatus.SUCCESSFULLY_COMPLETED;
-        } else if (FileValidationStatus.VALIDATION_FAILURE.equals(testFile.getValidationStatus())) {
-            lroStatus = LongRunningOperationStatus.FAILED;
-        } else if (FileValidationStatus.VALIDATION_INITIATED.equals(testFile.getValidationStatus())) {
-            lroStatus = LongRunningOperationStatus.IN_PROGRESS;
-        } else if (FileValidationStatus.NOT_VALIDATED.equals(testFile.getValidationStatus())) {
-            lroStatus = LongRunningOperationStatus.SUCCESSFULLY_COMPLETED;
-        } else {
-            lroStatus = LongRunningOperationStatus.NOT_STARTED;
-        }
-
-        return new PollResponse<>(lroStatus, testFile);
     }
 
     static PollResponse<BinaryData> getTestRunStatus(BinaryData testRunBinary) throws RuntimeException {
@@ -132,85 +107,5 @@ final class PollingUtils {
         }
 
         return new PollResponse<>(lroStatus, testRunBinary);
-    }
-
-    static PollResponse<LoadTestRun> getTestRunStatus(LoadTestRun loadTestRun) throws RuntimeException {
-
-        LongRunningOperationStatus lroStatus;
-
-        if (TestRunStatus.NOT_STARTED.equals(loadTestRun.getStatus())) {
-            lroStatus = LongRunningOperationStatus.NOT_STARTED;
-        } else if (TestRunStatus.DONE.equals(loadTestRun.getStatus())) {
-            lroStatus = LongRunningOperationStatus.SUCCESSFULLY_COMPLETED;
-        } else if (TestRunStatus.FAILED.equals(loadTestRun.getStatus())) {
-            lroStatus = LongRunningOperationStatus.FAILED;
-        } else if (TestRunStatus.CANCELLED.equals(loadTestRun.getStatus())) {
-            lroStatus = LongRunningOperationStatus.USER_CANCELLED;
-        } else {
-            lroStatus = LongRunningOperationStatus.IN_PROGRESS;
-        }
-
-        return new PollResponse<>(lroStatus, loadTestRun);
-    }
-
-    static PollResponse<BinaryData> getTestProfileRunStatus(BinaryData testProfileRunBinary) throws RuntimeException {
-        String status;
-
-        try (JsonReader jsonReader = JsonProviders.createReader(testProfileRunBinary.toBytes())) {
-            Map<String, Object> jsonTree = jsonReader.readMap(JsonReader::readUntyped);
-
-            status = jsonTree.get("status").toString();
-        } catch (IOException e) {
-            throw new RuntimeException("Encountered exception while retrieving test profile run status", e);
-        }
-
-        LongRunningOperationStatus lroStatus;
-
-        switch (status) {
-            case "NOTSTARTED":
-                lroStatus = LongRunningOperationStatus.NOT_STARTED;
-
-                break;
-
-            case "DONE":
-                lroStatus = LongRunningOperationStatus.SUCCESSFULLY_COMPLETED;
-
-                break;
-
-            case "FAILED":
-                lroStatus = LongRunningOperationStatus.FAILED;
-
-                break;
-
-            case "CANCELLED":
-                lroStatus = LongRunningOperationStatus.USER_CANCELLED;
-
-                break;
-
-            default:
-                lroStatus = LongRunningOperationStatus.IN_PROGRESS;
-
-                break;
-        }
-
-        return new PollResponse<>(lroStatus, testProfileRunBinary);
-    }
-
-    static PollResponse<TestProfileRun> getTestProfileRunStatus(TestProfileRun testProfileRun) throws RuntimeException {
-        LongRunningOperationStatus lroStatus;
-
-        if (TestProfileRunStatus.NOT_STARTED.equals(testProfileRun.getStatus())) {
-            lroStatus = LongRunningOperationStatus.NOT_STARTED;
-        } else if (TestProfileRunStatus.DONE.equals(testProfileRun.getStatus())) {
-            lroStatus = LongRunningOperationStatus.SUCCESSFULLY_COMPLETED;
-        } else if (TestProfileRunStatus.FAILED.equals(testProfileRun.getStatus())) {
-            lroStatus = LongRunningOperationStatus.FAILED;
-        } else if (TestProfileRunStatus.CANCELLED.equals(testProfileRun.getStatus())) {
-            lroStatus = LongRunningOperationStatus.USER_CANCELLED;
-        } else {
-            lroStatus = LongRunningOperationStatus.IN_PROGRESS;
-        }
-
-        return new PollResponse<>(lroStatus, testProfileRun);
     }
 }
