96ad913a3399d7ed19268f9e503b77f8b0fa3eb8
azure-sdk <azuresdk@microsoft.com>
2025-07-29 20:40:14 +0000

    Reset sources for azure-communication-email to the release version 1.0.22.

--- sdk/communication/azure-communication-email/CHANGELOG.md
+++ sdk/communication/azure-communication-email/CHANGELOG.md
@@ -1,21 +1,5 @@
 # Release History
 
-## 1.2.0-beta.2 (Unreleased)
-
-### Features Added
-
-### Breaking Changes
-
-### Bugs Fixed
-
-### Other Changes
-
-## 1.2.0-beta.1 (2025-07-16)
-
-### Features Added
-
-- Added `beginSend(String operationId)` method to `EmailClient` and `EmailAsyncClient` to create a poller from an existing operation ID. This allows customers to check the status of a previously started email send operation.
-
 ## 1.0.22 (2025-06-19)
 
 ### Other Changes
@@ -25,6 +9,7 @@
 - Upgraded `azure-communication-common` from `1.3.10` to version `1.4.0`.
 - Upgraded `azure-core-http-netty` from `1.15.11` to version `1.15.12`.
 - Upgraded `azure-core` from `1.55.3` to version `1.55.4`.
+
 
 ## 1.0.21 (2025-03-24)
 
@@ -91,13 +76,6 @@
 - Upgraded `azure-core-http-netty` from `1.15.2` to version `1.15.3`.
 
 
-## 1.1.0-beta.1 (2024-08-14)
-
-### Features Added
-
-- Consumers can now provide a value for the `ContentId` property when sending emails with attachments.
-  This allows consumers to reference attachments in the email body using the `cid` scheme. The `ContentId` property can be set on the `EmailAttachment` object.
-
 ## 1.0.15 (2024-07-26)
 
 ### Other Changes
@@ -148,9 +126,8 @@
 
 #### Dependency Updates
 
-- Upgraded `azure-core-http-netty` from `1.14.0` to version `1.14.1`.
 - Upgraded `azure-core` from `1.46.0` to version `1.47.0`.
-- Upgraded `azure-communication-common` from `1.3.0` to version `1.3.1`.
+- Upgraded `azure-core-http-netty` from `1.14.0` to version `1.14.1`.
 
 
 ## 1.0.10 (2024-02-20)
--- sdk/communication/azure-communication-email/src/main/java/com/azure/communication/email/models/EmailAddress.java
+++ sdk/communication/azure-communication-email/src/main/java/com/azure/communication/email/models/EmailAddress.java
@@ -5,39 +5,37 @@
 package com.azure.communication.email.models;
 
 import com.azure.core.annotation.Fluent;
-import com.azure.json.JsonReader;
-import com.azure.json.JsonSerializable;
-import com.azure.json.JsonToken;
-import com.azure.json.JsonWriter;
-import java.io.IOException;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-/**
- * An object representing the email address and its display name.
- */
+/** An object representing the email address and its display name. */
 @Fluent
-public final class EmailAddress implements JsonSerializable<EmailAddress> {
+public final class EmailAddress {
     /*
      * Email address.
      */
-    private final String address;
+    @JsonProperty(value = "address", required = true)
+    private String address;
 
     /*
      * Email display name.
      */
+    @JsonProperty(value = "displayName")
     private String displayName;
 
     /**
      * Creates an instance of EmailAddress class.
-     * 
+     *
      * @param address the address value to set.
      */
-    public EmailAddress(String address) {
+    @JsonCreator
+    public EmailAddress(@JsonProperty(value = "address", required = true) String address) {
         this.address = address;
     }
 
     /**
      * Get the address property: Email address.
-     * 
+     *
      * @return the address value.
      */
     public String getAddress() {
@@ -46,7 +44,7 @@ public final class EmailAddress implements JsonSerializable<EmailAddress> {
 
     /**
      * Get the displayName property: Email display name.
-     * 
+     *
      * @return the displayName value.
      */
     public String getDisplayName() {
@@ -55,60 +53,12 @@ public final class EmailAddress implements JsonSerializable<EmailAddress> {
 
     /**
      * Set the displayName property: Email display name.
-     * 
+     *
      * @param displayName the displayName value to set.
      * @return the EmailAddress object itself.
      */
     public EmailAddress setDisplayName(String displayName) {
         this.displayName = displayName;
         return this;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public JsonWriter toJson(JsonWriter jsonWriter) throws IOException {
-        jsonWriter.writeStartObject();
-        jsonWriter.writeStringField("address", this.address);
-        jsonWriter.writeStringField("displayName", this.displayName);
-        return jsonWriter.writeEndObject();
-    }
-
-    /**
-     * Reads an instance of EmailAddress from the JsonReader.
-     * 
-     * @param jsonReader The JsonReader being read.
-     * @return An instance of EmailAddress if the JsonReader was pointing to an instance of it, or null if it was
-     * pointing to JSON null.
-     * @throws IllegalStateException If the deserialized JSON object was missing any required properties.
-     * @throws IOException If an error occurs while reading the EmailAddress.
-     */
-    public static EmailAddress fromJson(JsonReader jsonReader) throws IOException {
-        return jsonReader.readObject(reader -> {
-            boolean addressFound = false;
-            String address = null;
-            String displayName = null;
-            while (reader.nextToken() != JsonToken.END_OBJECT) {
-                String fieldName = reader.getFieldName();
-                reader.nextToken();
-
-                if ("address".equals(fieldName)) {
-                    address = reader.getString();
-                    addressFound = true;
-                } else if ("displayName".equals(fieldName)) {
-                    displayName = reader.getString();
-                } else {
-                    reader.skipChildren();
-                }
-            }
-            if (addressFound) {
-                EmailAddress deserializedEmailAddress = new EmailAddress(address);
-                deserializedEmailAddress.displayName = displayName;
-
-                return deserializedEmailAddress;
-            }
-            throw new IllegalStateException("Missing required property: address");
-        });
     }
 }
