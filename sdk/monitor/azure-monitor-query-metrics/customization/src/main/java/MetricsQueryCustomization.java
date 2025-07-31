a91b9dd3fb76139214a19f6c79bfcb6ed80b1539
Jair Myree <jairmyree@microsoft.com>
2025-07-30 18:36:42 -0700

    regen TypeSpec - need to update the tsp-location after specs PR merges

--- sdk/monitor/azure-monitor-query-metrics/customization/src/main/java/MetricsQueryCustomization.java
+++ sdk/monitor/azure-monitor-query-metrics/customization/src/main/java/MetricsQueryCustomization.java
@@ -16,12 +16,18 @@ public class MetricsQueryCustomization extends Customization {
      * Customizes the MonitorQueryMetricsClientBuilder by adding audience support for authorization scopes.
      * This customization adds a MetricsQueryAudience field and setter to the builder, and modifies
      * the HTTP pipeline creation to use the audience when configuring bearer token authentication.
+     * This customization also updates the module-info.java file to export the metrics models package.
      *
      * @param libraryCustomization The library customization object for modifying generated code.
      * @param logger The logger for recording customization activities.
      */
     @Override
     public void customize(LibraryCustomization libraryCustomization, Logger logger) {
+        String original = libraryCustomization.getRawEditor().getFileContent("src/main/java/module-info.java");
+        String replace = original.replace("exports com.azure.monitor.query.metrics.implementation;", "");
+
+        libraryCustomization.getRawEditor().replaceFile("src/main/java/module-info.java", replace);
+
         libraryCustomization.getClass("com.azure.monitor.query.metrics.implementation", "MonitorQueryMetricsClientBuilder")
             .customizeAst(ast -> {
                 ast.addImport("com.azure.monitor.query.metrics.models.MetricsQueryAudience");
