63f8e1145aa988331d34ec858dc2713af9302524
Racherazzle <128776618+Racherazzle@users.noreply.github.com>
2025-08-06 12:07:16 +1200

    correctly changes the first import statement

--- sdk/tools/azure-openrewrite/src/test/resources/migrationExamples/azure-data-appconfiguration/sample1/v2/AadAuthentication.java
+++ sdk/tools/azure-openrewrite/src/test/resources/migrationExamples/azure-data-appconfiguration/sample1/v2/AadAuthentication.java
@@ -1,25 +1,26 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.azure.data.appconfiguration;
+package com.azure.v2.data.appconfiguration;
 
-import com.azure.data.appconfiguration.models.ConfigurationSetting;
-import com.azure.identity.DefaultAzureCredential;
-import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.v2.data.appconfiguration.models.ConfigurationSetting;
+import com.azure.v2.identity.AzureCliCredential;
+import com.azure.v2.identity.AzureCliCredentialBuilder;
+import io.clientcore.core.utils.configuration.Configuration;
 
 /**
- * Sample demonstrates how to use AAD token to build a configuration client.
+ * Sample demonstrates how to use Entra token to build a configuration client.
  */
 public class AadAuthentication {
     /**
-     * Sample for how to use AAD token Authentication.
+     * Sample for how to use Entra token Authentication.
      *
      * @param args Unused. Arguments to the program.
      */
     public static void main(String[] args) {
         // The endpoint can be obtained by going to your App Configuration instance in the Azure portal
         // and navigating to "Overview" page. Looking for the "Endpoint" keyword.
-        String endpoint = "{endpoint_value}";
+        String endpoint = Configuration.getGlobalConfiguration().get("AZ_CONFIG_ENDPOINT");
 
         // Default token credential could be obtained from Identity service.
         // It tries to create a valid credential in the following order:
@@ -27,10 +28,10 @@ public class AadAuthentication {
         //      ManagedIdentityCredential
         //      SharedTokenCacheCredential
         //      Fails if none of the credentials above could be created.
-        DefaultAzureCredential tokenCredential = new DefaultAzureCredentialBuilder().build();
+        AzureCliCredential tokenCredential = new AzureCliCredentialBuilder().build();
 
         final ConfigurationClient client = new ConfigurationClientBuilder()
-            .credential(tokenCredential) // AAD authentication
+            .credential(tokenCredential) // Entra authentication
             .endpoint(endpoint)
             .buildClient();
 
@@ -41,13 +42,13 @@ public class AadAuthentication {
         System.out.println("Beginning of synchronous sample...");
 
         ConfigurationSetting setting = client.setConfigurationSetting(key, null, value);
-        System.out.printf("[SetConfigurationSetting] Key: %s, Value: %s", setting.getKey(), setting.getValue());
+        System.out.printf("[SetConfigurationSetting] Key: %s, Value: %s%n", setting.getKey(), setting.getValue());
 
         setting = client.getConfigurationSetting(key, null, null);
-        System.out.printf("[GetConfigurationSetting] Key: %s, Value: %s", setting.getKey(), setting.getValue());
+        System.out.printf("[GetConfigurationSetting] Key: %s, Value: %s%n", setting.getKey(), setting.getValue());
 
         setting = client.deleteConfigurationSetting(key, null);
-        System.out.printf("[DeleteConfigurationSetting] Key: %s, Value: %s", setting.getKey(), setting.getValue());
+        System.out.printf("[DeleteConfigurationSetting] Key: %s, Value: %s%n", setting.getKey(), setting.getValue());
 
         System.out.println("End of synchronous sample.");
     }
