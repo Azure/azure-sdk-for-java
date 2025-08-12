566a2a068646d225609afd404d1b0b09d66be881
Sanjana Kapur <sanjanakapur@microsoft.com>
2025-08-12 03:14:10 -0700

    Fix bug in error handling logic

--- sdk/batch/azure-compute-batch/src/main/java/com/azure/compute/batch/JobDeletePollerAsync.java
+++ sdk/batch/azure-compute-batch/src/main/java/com/azure/compute/batch/JobDeletePollerAsync.java
@@ -5,7 +5,7 @@ package com.azure.compute.batch;
 
 import com.azure.compute.batch.models.BatchJob;
 import com.azure.compute.batch.models.BatchJobState;
-import com.azure.core.exception.ResourceNotFoundException;
+import com.azure.core.exception.HttpResponseException;
 import com.azure.core.http.rest.RequestOptions;
 import com.azure.core.util.Context;
 import com.azure.core.util.polling.LongRunningOperationStatus;
@@ -68,8 +68,10 @@ public final class JobDeletePollerAsync {
 
                 return new PollResponse<>(status, job);
             })
-                .onErrorResume(ResourceNotFoundException.class,
-                    ex -> Mono.just(new PollResponse<>(LongRunningOperationStatus.SUCCESSFULLY_COMPLETED, null)));
+                .onErrorResume(HttpResponseException.class,
+                    ex -> ex.getResponse() != null && ex.getResponse().getStatusCode() == 404
+                        ? Mono.just(new PollResponse<>(LongRunningOperationStatus.SUCCESSFULLY_COMPLETED, null))
+                        : Mono.error(ex));
         };
     }
 
