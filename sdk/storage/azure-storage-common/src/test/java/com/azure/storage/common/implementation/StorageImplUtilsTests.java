4a06ec5d79550c16d77589e36e898f329fc27202
browndav <browndav@microsoft.com>
2025-08-28 19:13:57 -0400

    create test to ensure getCause() is not null and returns an exception type

---
+++ sdk/storage/azure-storage-common/src/test/java/com/azure/storage/common/implementation/StorageImplUtilsTests.java
@@ -0,0 +1,102 @@
+// StorageImplUtilsTests.java
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+package com.azure.storage.common.implementation;
+
+import com.azure.core.exception.HttpResponseException;
+import com.azure.core.http.HttpResponse;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+import java.util.function.Supplier;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StorageImplUtilsTests {
+
+    @Test
+    public void sendRequestThrowsTimeoutException() throws IOException {
+        // These are all of the types of Exceptions that can be thrown by Future.get()
+        List<Class<? extends Exception>> exceptions = Arrays.asList(TimeoutException.class, RuntimeException.class,
+            ExecutionException.class, InterruptedException.class);
+
+        for (Class<? extends Exception> exception : exceptions) {
+            try {
+                //Arrange
+                Supplier<?> timeoutExceptionSupplier = generateSupplier(exception);
+                CallableExceptionOperation operation = new CallableExceptionOperation(timeoutExceptionSupplier);
+
+                // Act
+                StorageImplUtils.sendRequest(operation, Duration.ofSeconds(120), BlobStorageException.class);
+            } catch (RuntimeException e) {
+                //Assert
+                assertNotNull(e.getCause());
+                assertInstanceOf(exception, e.getCause());
+            }
+        }
+    }
+
+
+    //
+    private static class CallableExceptionOperation implements Callable<Object> {
+        private final Supplier<?> exceptionSupplier;
+
+        /**
+         * Creates a new CallableExceptionOperation with the specified exception supplier.
+         *
+         * @param exceptionSupplier a {@link Supplier} that provides exception instances to be thrown
+         *                         when {@link #call()} is invoked. The supplier should return instances
+         *                         of supported exception types: {@link RuntimeException},
+         *                         {@link TimeoutException}, {@link ExecutionException}, or
+         *                         {@link InterruptedException}.
+         */
+        public CallableExceptionOperation(Supplier<?> exceptionSupplier) {
+            this.exceptionSupplier = exceptionSupplier;
+        }
+
+        @Override
+        public Object call() throws Exception {
+            Object e = exceptionSupplier.get();
+
+            if (e instanceof RuntimeException) {
+                throw (RuntimeException) e;
+            } else if (e instanceof TimeoutException) {
+                throw (TimeoutException) e;
+            } else if (e instanceof ExecutionException) {
+                throw (ExecutionException) e;
+            } else if (e instanceof InterruptedException) {
+                throw (InterruptedException) e;
+            }
+            return new Object();
+        }
+    }
+
+    private static Supplier<?> generateSupplier(Class<? extends Exception> exceptionType) {
+        return () -> {
+            try {
+                if (exceptionType == ExecutionException.class) {
+                    // ExecutionException requires a cause
+                    return new ExecutionException("Test execution exception", new RuntimeException("Test cause"));
+                } else {
+                    // For other exceptions, use no-arg constructor
+                    return exceptionType.getConstructor().newInstance();
+                }
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to create exception instance", e);
+            }
+        };
+    }
+
+    private static final class BlobStorageException extends HttpResponseException {
+
+        public BlobStorageException(HttpResponse response) {
+            super(response);
+        }
+    }
+}
