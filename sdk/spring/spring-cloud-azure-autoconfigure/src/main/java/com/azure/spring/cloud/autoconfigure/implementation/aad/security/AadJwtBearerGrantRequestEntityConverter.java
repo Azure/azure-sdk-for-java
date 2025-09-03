849fd05f08af1509ce7de35a6366dbae7c1cefd3
muyao <muyaofeng@126.com>
2025-09-03 10:58:16 +0800

    replace JwtBearerGrantRequestEntityConverter

--- sdk/spring/spring-cloud-azure-autoconfigure/src/main/java/com/azure/spring/cloud/autoconfigure/implementation/aad/security/AadJwtBearerGrantRequestEntityConverter.java
+++ sdk/spring/spring-cloud-azure-autoconfigure/src/main/java/com/azure/spring/cloud/autoconfigure/implementation/aad/security/AadJwtBearerGrantRequestEntityConverter.java
@@ -3,8 +3,9 @@
 
 package com.azure.spring.cloud.autoconfigure.implementation.aad.security;
 
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.oauth2.client.endpoint.DefaultOAuth2TokenRequestParametersConverter;
 import org.springframework.security.oauth2.client.endpoint.JwtBearerGrantRequest;
-import org.springframework.security.oauth2.client.endpoint.JwtBearerGrantRequestEntityConverter;
 import org.springframework.util.MultiValueMap;
 
 /**
@@ -13,12 +14,14 @@ import org.springframework.util.MultiValueMap;
  * @since 4.3.0
  * @see <a href="https://docs.microsoft.com/azure/active-directory/develop/v2-oauth2-on-behalf-of-flow">OAuth 2.0 On-Behalf-Of</a>
  */
-@SuppressWarnings({"deprecation", "removal"})
-public class AadJwtBearerGrantRequestEntityConverter extends JwtBearerGrantRequestEntityConverter {
+public class AadJwtBearerGrantRequestEntityConverter implements Converter<JwtBearerGrantRequest, MultiValueMap<String, String>> {
+
+    private final DefaultOAuth2TokenRequestParametersConverter<JwtBearerGrantRequest> delegate =
+        new DefaultOAuth2TokenRequestParametersConverter<>();
 
     @Override
-    protected MultiValueMap<String, String> createParameters(JwtBearerGrantRequest jwtBearerGrantRequest) {
-        MultiValueMap<String, String> parameters = super.createParameters(jwtBearerGrantRequest);
+    public MultiValueMap<String, String> convert(JwtBearerGrantRequest jwtBearerGrantRequest) {
+        MultiValueMap<String, String> parameters = delegate.convert(jwtBearerGrantRequest);
         parameters.add("requested_token_use", "on_behalf_of");
         return parameters;
     }
