86a81b6142bffbd1767cade67adf5c0fdca14940
Vinay Gera <vigera@microsoft.com>
2025-09-03 22:47:25 -0700

    test variant new command

--- sdk/identity/azure-identity/src/main/java/com/azure/identity/implementation/util/PowerShellUtil.java
+++ sdk/identity/azure-identity/src/main/java/com/azure/identity/implementation/util/PowerShellUtil.java
@@ -14,12 +14,50 @@ import com.azure.core.util.CoreUtils;
  * Utility class for powershell auth related ops .
  */
 public class PowerShellUtil {
+    // public static String getPwshCommand(String tenantId, String scope, String sep) {
+    //     return "$ErrorActionPreference = 'Stop'" + sep
+    //         + "$ProgressPreference = 'SilentlyContinue'" + sep
+    //         + "$VerbosePreference = 'SilentlyContinue'" + sep
+    //         + "$WarningPreference = 'SilentlyContinue'" + sep
+    //         + "$InformationPreference = 'SilentlyContinue'" + sep
+    //         + "[version]$minimumVersion = '2.2.0'" + sep
+    //         + "$m = Import-Module Az.Accounts -MinimumVersion $minimumVersion -PassThru -ErrorAction SilentlyContinue" + sep
+    //         + "if (! $m) {" + sep
+    //         + "    Write-Output 'VersionTooOld'" + sep
+    //         + "    exit" + sep
+    //         + "}" + sep
+    //         + "$params = @{ 'ResourceUrl' = '" + scope + "'; 'WarningAction' = 'Ignore' }" + sep
+    //         + "$tenantId = '" + tenantId + "'" + sep
+    //         + "if ($tenantId.Length -gt 0) {" + sep
+    //         + "    $params['TenantId'] = $tenantId" + sep
+    //         + "}" + sep
+    //         + "if ($m.Version -ge [version]'2.17.0' -and $m.Version -lt [version]'5.0.0') {" + sep
+    //         + "    $params['AsSecureString'] = $true" + sep
+    //         + "}" + sep
+    //         + "$token = Get-AzAccessToken @params" + sep
+    //         + "$tokenValue = $token.Token" + sep
+    //         + "if ($tokenValue -is [System.Security.SecureString]) {" + sep
+    //         + "    if ($PSVersionTable.PSVersion.Major -lt 7) {" + sep
+    //         + "        $ssPtr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($tokenValue)" + sep
+    //         + "        try {" + sep
+    //         + "            $tokenValue = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($ssPtr)" + sep
+    //         + "        }" + sep
+    //         + "        finally {" + sep
+    //         + "            [System.Runtime.InteropServices.Marshal]::ZeroFreeBSTR($ssPtr)" + sep
+    //         + "        }" + sep
+    //         + "    } else {" + sep
+    //         + "        $tokenValue = $tokenValue | ConvertFrom-SecureString -AsPlainText" + sep
+    //         + "    }" + sep
+    //         + "}" + sep
+    //         + "$customToken = [PSCustomObject]@{" + sep
+    //         + "    Token = $tokenValue" + sep
+    //         + "    ExpiresOn = $token.ExpiresOn" + sep
+    //         + "}" + sep
+    //         + "$customToken | ConvertTo-Json -Compress";
+    // }
+
     public static String getPwshCommand(String tenantId, String scope, String sep) {
         return "$ErrorActionPreference = 'Stop'" + sep
-            + "$ProgressPreference = 'SilentlyContinue'" + sep
-            + "$VerbosePreference = 'SilentlyContinue'" + sep
-            + "$WarningPreference = 'SilentlyContinue'" + sep
-            + "$InformationPreference = 'SilentlyContinue'" + sep
             + "[version]$minimumVersion = '2.2.0'" + sep
             + "$m = Import-Module Az.Accounts -MinimumVersion $minimumVersion -PassThru -ErrorAction SilentlyContinue" + sep
             + "if (! $m) {" + sep
@@ -49,11 +87,10 @@ public class PowerShellUtil {
             + "        $tokenValue = $tokenValue | ConvertFrom-SecureString -AsPlainText" + sep
             + "    }" + sep
             + "}" + sep
-            + "$customToken = [PSCustomObject]@{" + sep
-            + "    Token = $tokenValue" + sep
-            + "    ExpiresOn = $token.ExpiresOn" + sep
-            + "}" + sep
-            + "$customToken | ConvertTo-Json -Compress";
+            + "$customToken = New-Object -TypeName PSObject" + sep
+            + "$customToken | Add-Member -MemberType NoteProperty -Name Token -Value $tokenValue" + sep
+            + "$customToken | Add-Member -MemberType NoteProperty -Name ExpiresOn -Value $token.ExpiresOn" + sep
+            + "$customToken | ConvertTo-Json -Compress -Depth 10";
     }
 
     /**
