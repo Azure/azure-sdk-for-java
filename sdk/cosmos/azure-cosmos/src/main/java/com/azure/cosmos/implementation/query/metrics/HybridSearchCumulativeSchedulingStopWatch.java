e9b9edad78b4410a851a65b31b5fd902dac9ef18
Aayush Kataria <aayushkataria3011@gmail.com>
2025-09-04 15:01:06 -0700

    Resolving comments

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/query/metrics/HybridSearchCumulativeSchedulingStopWatch.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/query/metrics/HybridSearchCumulativeSchedulingStopWatch.java
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 package com.azure.cosmos.implementation.query.metrics;
 
 import com.azure.cosmos.implementation.apachecommons.lang.time.StopWatch;
