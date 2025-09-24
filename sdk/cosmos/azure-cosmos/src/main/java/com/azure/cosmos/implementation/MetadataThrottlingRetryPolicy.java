9ebd824fc8d4885e8d613d897917744975e4ebfa
annie-mac <xinlian@microsoft.com>
2025-09-24 14:40:02 -0700

    resolve comments

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/MetadataThrottlingRetryPolicy.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/MetadataThrottlingRetryPolicy.java
@@ -9,10 +9,9 @@ import java.time.Duration;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class MetadataThrottlingRetryPolicy extends ResourceThrottleRetryPolicy {
-    private final static int RANDOM_SALT_IN_MS = 5;
+    private final static int RANDOM_SALT_IN_MS = 100;
     private final static int MAX_ATTEMPT_COUNT = Integer.MAX_VALUE;
     private final static Duration MAX_RETRY_WAIT_TIME = Duration.ofSeconds((Integer.MAX_VALUE / 1000) - 1);
-    private static final ThreadLocalRandom random = ThreadLocalRandom.current();
 
     public MetadataThrottlingRetryPolicy(RetryContext retryContext) {
         super(MAX_ATTEMPT_COUNT, MAX_RETRY_WAIT_TIME, retryContext, false);
@@ -23,7 +22,9 @@ public class MetadataThrottlingRetryPolicy extends ResourceThrottleRetryPolicy {
         return super.shouldRetry(exception)
             .flatMap(shouldRetryResult -> {
                 if (shouldRetryResult.shouldRetry) {
-                    shouldRetryResult.withRandomSalt(random.nextInt(RANDOM_SALT_IN_MS));
+                    // ResourceThrottleRetryPolicy will retry based on the back-off returned from server
+                    // for metadata request, in order to spread the requests more, adding some random salt from sdk side
+                    shouldRetryResult.withRandomSalt(ThreadLocalRandom.current().nextInt(RANDOM_SALT_IN_MS + 1));
                 }
 
                 return Mono.just(shouldRetryResult);
