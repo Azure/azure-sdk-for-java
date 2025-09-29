89df0da4f744ac07c80e1ba700e431d38dd227d9
ray chen <raychen@microsoft.com>
2025-09-29 18:30:26 +0000

    Testing eng/common changes for duplicate artifact names

--- eng/common/scripts/Validate-All-Packages.ps1
+++ eng/common/scripts/Validate-All-Packages.ps1
@@ -1,50 +1,252 @@
 [CmdletBinding()]
 Param (
   [Parameter(Mandatory=$True)]
-  [array]$ArtifactList,
+  [array] $PackageInfoFiles,
   [Parameter(Mandatory=$True)]
   [string]$ArtifactPath,
   [Parameter(Mandatory=$True)]
   [string]$RepoRoot,
   [Parameter(Mandatory=$True)]
   [string]$APIKey,
-  [string]$ConfigFileDir,
   [string]$BuildDefinition,
   [string]$PipelineUrl,
   [string]$APIViewUri  = "https://apiview.dev/AutoReview/GetReviewStatus",
   [bool] $IsReleaseBuild = $false
 )
 
+# Validate-All-Packages.ps1 folds in the code that was originally in Validate-Package.ps1
+# since Validate-Package.ps1 was only called from Validate-All-Packages.ps1. This replaces
+# script calls with function calls and also allows calling CheckAzLoginAndDevOpsExtensionInstall
+# and CheckDevOpsAccess once for all of the PackageInfo files being processed instead of once
+# per artifact in Validate-Package.ps1 and then again in Update-DevOps-Release-WorkItem.ps1
+
 Set-StrictMode -Version 3
 . (Join-Path $PSScriptRoot common.ps1)
+. ${PSScriptRoot}\Helpers\ApiView-Helpers.ps1
+. ${PSScriptRoot}\Helpers\DevOps-WorkItem-Helpers.ps1
 
-function ProcessPackage($PackageName, $ConfigFileDir)
+# Function to validate change log
+function ValidateChangeLog($changeLogPath, $versionString, $validationStatus)
 {
-    Write-Host "Artifact path: $($ArtifactPath)"
-    Write-Host "Package Name: $($PackageName)"
-    Write-Host "Config File directory: $($ConfigFileDir)"
-
-    &$EngCommonScriptsDir/Validate-Package.ps1 `
-        -PackageName $PackageName `
-        -ArtifactPath $ArtifactPath `
-        -RepoRoot $RepoRoot `
-        -APIViewUri $APIViewUri `
-        -APIKey $APIKey `
-        -BuildDefinition $BuildDefinition `
-        -PipelineUrl $PipelineUrl `
-        -ConfigFileDir $ConfigFileDir
-    if ($LASTEXITCODE -ne 0) {
-        Write-Error "Failed to validate package $PackageName"
-        exit 1
+    try
+    {
+        $ChangeLogStatus = [PSCustomObject]@{
+            IsValid = $false
+            Message = ""
+        }
+        $changeLogFullPath = Join-Path $RepoRoot $changeLogPath
+        Write-Host "Path to change log: [$changeLogFullPath]"
+        if (Test-Path $changeLogFullPath)
+        {
+            Confirm-ChangeLogEntry -ChangeLogLocation $changeLogFullPath -VersionString $versionString -ForRelease $true -ChangeLogStatus $ChangeLogStatus -SuppressErrors $true
+            $validationStatus.Status = if ($ChangeLogStatus.IsValid) { "Success" } else { "Failed" }
+            $validationStatus.Message = $ChangeLogStatus.Message
+        }
+        else {
+            $validationStatus.Status = "Failed"
+            $validationStatus.Message = "Change log is not found in [$changeLogPath]. Change log file must be present in package root directory."
+        }
+    }
+    catch
+    {
+        Write-Host "Current directory: $(Get-Location)"
+        $validationStatus.Status = "Failed"
+        $validationStatus.Message = $_.Exception.Message
     }
 }
 
-# Check if package config file is present. This file has package version, SDK type etc info.
-if (-not $ConfigFileDir) {
-    $ConfigFileDir = Join-Path -Path $ArtifactPath "PackageInfo"
-}
-foreach ($artifact in $ArtifactList)
+# Function to verify API review status
+function VerifyAPIReview($packageName, $packageVersion, $language)
 {
-    Write-Host "Processing $($artifact.name)"
-    ProcessPackage -PackageName $artifact.name -ConfigFileDir $ConfigFileDir
+    $APIReviewValidation = [PSCustomObject]@{
+        Name = "API Review Approval"
+        Status = "Pending"
+        Message = ""
+    }
+    $PackageNameValidation = [PSCustomObject]@{
+        Name = "Package Name Approval"
+        Status = "Pending"
+        Message = ""
+    }
+
+    try
+    {
+        $apiStatus = [PSCustomObject]@{
+            IsApproved = $false
+            Details = ""
+        }
+        $packageNameStatus = [PSCustomObject]@{
+            IsApproved = $false
+            Details = ""
+        }
+        Write-Host "Checking API review status for package $packageName with version $packageVersion. language [$language]."
+        Check-ApiReviewStatus $packageName $packageVersion $language $APIViewUri $APIKey $apiStatus $packageNameStatus
+
+        Write-Host "API review approval details: $($apiStatus.Details)"
+        Write-Host "Package name approval details: $($packageNameStatus.Details)"
+        #API review approval status
+        $APIReviewValidation.Message = $apiStatus.Details
+        $APIReviewValidation.Status = if ($apiStatus.IsApproved) { "Approved" } else { "Pending" }
+
+        # Package name approval status
+        $PackageNameValidation.Status = if ($packageNameStatus.IsApproved) { "Approved" } else { "Pending" }
+        $PackageNameValidation.Message = $packageNameStatus.Details
+    }
+    catch
+    {
+        Write-Warning "Failed to get API review status. Error: $_"
+        $PackageNameValidation.Status = "Failed"
+        $PackageNameValidation.Message = $_.Exception.Message
+        $APIReviewValidation.Status = "Failed"
+        $APIReviewValidation.Message = $_.Exception.Message
+    }
+
+    return [PSCustomObject]@{
+        ApiviewApproval = $APIReviewValidation
+        PackageNameApproval = $PackageNameValidation
+    }
+}
+
+
+# Function to check if a package version has already shipped
+function IsVersionShipped($packageName, $packageVersion)
+{
+    Write-Host "Checking if a version is already shipped for package $packageName with version $packageVersion."
+    $parsedNewVersion = [AzureEngSemanticVersion]::new($packageVersion)
+    $versionMajorMinor = "" + $parsedNewVersion.Major + "." + $parsedNewVersion.Minor
+    $workItem = FindPackageWorkItem -lang $LanguageDisplayName -packageName $packageName -version $versionMajorMinor -includeClosed $true -outputCommand $false
+    if ($workItem)
+    {
+        # Check if the package version is already shipped
+        $shippedVersionSet = ParseVersionSetFromMDField $workItem.fields["Custom.ShippedPackages"]
+        if ($shippedVersionSet.ContainsKey($packageVersion)) {
+            return $true
+        }
+    }
+    else {
+        Write-Host "No work item found for package [$packageName]. Creating new work item for package."
+    }
+    return $false
+}
+
+# Function to create or update package work item in Azure DevOps
+function CreateUpdatePackageWorkItem($pkgInfo)
+{
+    $versionString = $pkgInfo.Version
+    $packageName = $pkgInfo.Name
+    $plannedDate = $pkgInfo.ReleaseStatus
+    $setReleaseState = $true
+    if (!$plannedDate -or $plannedDate -eq "Unreleased")
+    {
+        $setReleaseState = $false
+        $plannedDate = "unknown"
+    }
+
+    # Create or update package work item
+    $succeeded = Update-DevOpsReleaseWorkItem `
+                    -language $LanguageDisplayName `
+                    -packageName $packageName `
+                    -version $versionString `
+                    -plannedDate $plannedDate `
+                    -packageRepoPath $pkgInfo.serviceDirectory `
+                    -packageType $pkgInfo.SDKType `
+                    -packageNewLibrary $pkgInfo.IsNewSDK `
+                    -serviceName "unknown" `
+                    -packageDisplayName "unknown" `
+                    -inRelease $IsReleaseBuild
+
+    if (-not $succeeded) {
+        Write-Host "Update of the Devops Release WorkItem failed."
+    }
+    return $succeeded
+}
+
+CheckAzLoginAndDevOpsExtensionInstall
+
+CheckDevOpsAccess
+
+Write-Host "Artifact path: $($ArtifactPath)"
+
+foreach ($packageInfoFile in $PackageInfoFiles)
+{
+    $packageInfo = Get-Content $packageInfoFile | ConvertFrom-Json
+
+    Write-Host "Processing packageInfoFile: $packageInfoFile"
+    # The ArtifactName maps to the PackageInfo
+    Write-Host "Processing $($packageInfo.Name)"
+    Write-Host "Is Release Build: $IsReleaseBuild"
+
+# JRS everything below needs to be updated to pull information from the PackageInfo which was
+# was created above
+    # Read package property file and identify all packages to process
+    # Note: For Java PackageInfo.Name will soon be GroupId+ArtifactName
+    $pkgName = $packageInfo.Name
+    $changeLogPath = $packageInfo.ChangeLogPath
+    $versionString = $packageInfo.Version
+    Write-Host "Checking if we need to create or update work item for package $pkgName with version $versionString."
+    $isShipped = IsVersionShipped $pkgName $versionString
+    if ($isShipped) {
+        Write-Host "Package work item already exists for version [$versionString] that is marked as shipped. Skipping the update of package work item."
+        exit 0
+    }
+
+    Write-Host "Validating package $pkgName with version $versionString."
+
+    # Change log validation
+    $changeLogStatus = [PSCustomObject]@{
+        Name = "Change Log Validation"
+        Status = "Success"
+        Message = ""
+    }
+    ValidateChangeLog $changeLogPath $versionString $changeLogStatus
+
+    # API review and package name validation
+    $fullPackageName = $pkgName
+    # If there's a groupId that means this is Java and pkgName = GroupId+ArtifactName
+    # but the VerifyAPIReview requires GroupId:ArtifactName
+    if ($packageInfo.PSObject.Members.Name -contains "Group") {
+        $groupId = $packageInfo.Group
+        if ($groupId) {
+            $fullPackageName = "$($groupId):$($packageInfo.ArtifactName)"
+        }
+    }
+    Write-Host "Checking API review status for package $fullPackageName"
+    $apireviewDetails = VerifyAPIReview $fullPackageName $packageInfo.Version $Language
+
+    $pkgValidationDetails= [PSCustomObject]@{
+        Name = $pkgName
+        Version = $packageInfo.Version
+        ChangeLogValidation = $changeLogStatus
+        APIReviewValidation = $apireviewDetails.ApiviewApproval
+        PackageNameValidation = $apireviewDetails.PackageNameApproval
+    }
+
+    $output = ConvertTo-Json $pkgValidationDetails
+    Write-Host "Output: $($output)"
+
+    # Create json token file in artifact path
+    $tokenFile = Join-Path $ArtifactPath "$pkgName-Validation.json"
+    $output | Out-File -FilePath $tokenFile -Encoding utf8
+
+    # Create DevOps work item
+    $updatedWi = CreateUpdatePackageWorkItem $packageInfo
+
+    # Update validation status in package work item
+    if ($updatedWi) {
+        Write-Host "Updating validation status in package work item."
+        $updatedWi = UpdateValidationStatus $pkgValidationDetails $BuildDefinition $PipelineUrl
+    }
+
+    # Fail the build if any validation is not successful for a release build
+    Write-Host "Change log status:" $changelogStatus.Status
+    Write-Host "API Review status:" $apireviewDetails.ApiviewApproval.Status
+    Write-Host "Package Name status:" $apireviewDetails.PackageNameApproval.Status
+
+    if ($IsReleaseBuild)
+    {
+        if (!$updatedWi -or $changelogStatus.Status -ne "Success" -or $apireviewDetails.ApiviewApproval.Status -ne "Approved" -or $apireviewDetails.PackageNameApproval.Status -ne "Approved") {
+            Write-Error "At least one of the Validations above failed for package $pkgName with version $versionString."
+            exit 1
+        }
+    }
 }
