f0d0b274dcb913077ac28872d9cfd52f32199534
Hong Li(MSFT) <74638143+v-hongli1@users.noreply.github.com>
2025-10-13 15:14:19 +0800

    mgmt, local generation for Microsoft.Network 2024-10-01 (#46945)
    * Generate codes
    
    * Update changelog
    
    * Fix test exception
    
    * Update assets
    
    * remove uppercase setting of Network Security Group Name
    
    * fix without action
    
    * fix without action
    
    * fix without action by TreeMap
    
    * fix for discussion_r2425270949

--- sdk/resourcemanager/azure-resourcemanager/src/samples/java/com/azure/resourcemanager/network/generated/VirtualNetworkGatewaysDeleteSamples.java
+++ sdk/resourcemanager/azure-resourcemanager/src/samples/java/com/azure/resourcemanager/network/generated/VirtualNetworkGatewaysDeleteSamples.java
@@ -10,7 +10,7 @@ package com.azure.resourcemanager.network.generated;
 public final class VirtualNetworkGatewaysDeleteSamples {
     /*
      * x-ms-original-file:
-     * specification/network/resource-manager/Microsoft.Network/stable/2024-07-01/examples/VirtualNetworkGatewayDelete.
+     * specification/network/resource-manager/Microsoft.Network/stable/2024-10-01/examples/VirtualNetworkGatewayDelete.
      * json
      */
     /**
