97e43a839dada6cd7e6b59ac40908f519c665ea2
Fabian Meiswinkel <fabianm@microsoft.com>
2025-10-14 09:36:38 +0000

    Update databricks-notebooks-install.sh

--- sdk/cosmos/azure-cosmos-spark_3_2-12/test-databricks/databricks-notebooks-install.sh
+++ sdk/cosmos/azure-cosmos-spark_3_2-12/test-databricks/databricks-notebooks-install.sh
@@ -51,7 +51,7 @@ do
 	echo "Creating run for job $JOB_ID"
 	DBG_JSON="\"{\\\"job_id\\\": $JOB_ID, \\\"notebook_params\\\": {\\\"cosmosEndpoint\\\": \\\"$COSMOSENDPOINT\\\",\\\"cosmosMasterKey\\\": \\\"$COSMOSKEY\\\",\\\"subscriptionId\\\": \\\"$SUBSCRIPTIONID\\\",\\\"tenantId\\\": \\\"$TENANTID\\\",\\\"resourceGroupName\\\": \\\"$RESOURCEGROUPNAME\\\",\\\"clientId\\\": \\\"$CLIENTID\\\",\\\"clientSecret\\\": \\\"$CLIENTSECRET\\\",\\\"cosmosContainerName\\\": \\\"$COSMOSCONTAINERNAME\\\", \\\"cosmosDatabaseName\\\": \\\"$COSMOSDATABASENAME\\\"}}\""
 	echo "DBG_JSON: $DBG_JSON"
-	RUN_ID=$(databricks jobs run-now --json $DBG_JSON | jq -r '.run_id')
+	RUN_ID=$(databricks jobs run-now --json "$DBG_JSON" | jq -r '.run_id')
 
 	if [[ -z "$RUN_ID" ]]
 	then
