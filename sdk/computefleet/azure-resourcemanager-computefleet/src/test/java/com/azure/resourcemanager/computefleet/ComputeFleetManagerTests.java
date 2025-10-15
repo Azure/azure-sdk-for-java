2cb026527e2aebd7eb8750718a9b8f30ec1fcc65
Weidong Xu <weidxu@microsoft.com>
2025-10-15 12:44:09 +0800

    workaround for computefleet test failure

--- sdk/computefleet/azure-resourcemanager-computefleet/src/test/java/com/azure/resourcemanager/computefleet/ComputeFleetManagerTests.java
+++ sdk/computefleet/azure-resourcemanager-computefleet/src/test/java/com/azure/resourcemanager/computefleet/ComputeFleetManagerTests.java
@@ -109,6 +109,9 @@ public class ComputeFleetManagerTests extends TestProxyTestBase {
     @Test
     @LiveOnly
     public void testCreateComputeFleet() {
+        // trigger the auth, to mitigate https://github.com/Azure/azure-sdk-for-java/issues/46858
+        resourceManager.providers().getByName("Microsoft.HDInsight");
+
         Fleet fleet = null;
         try {
             String fleetName = "fleet" + randomPadding();
