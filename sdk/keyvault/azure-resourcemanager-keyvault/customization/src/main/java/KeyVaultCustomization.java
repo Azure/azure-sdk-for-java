bc146aeced3836520c9f976022c0bcf468eac84b
XiaofeiCao <xiaofeicao@microsoft.com>
2025-10-24 13:23:08 +0800

    customization

---
+++ sdk/keyvault/azure-resourcemanager-keyvault/customization/src/main/java/KeyVaultCustomization.java
@@ -0,0 +1,29 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import com.azure.autorest.customization.ClassCustomization;
+import com.azure.autorest.customization.Customization;
+import com.azure.autorest.customization.LibraryCustomization;
+import com.azure.autorest.customization.PackageCustomization;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
+import org.slf4j.Logger;
+
+public class KeyVaultCustomization extends Customization {
+    @Override
+    public void customize(LibraryCustomization customization, Logger logger) {
+        PackageCustomization fluentModelsPackage = customization.getPackage("com.azure.resourcemanager.keyvault.fluent.models");
+        customizeResourceBaseClass(fluentModelsPackage.getClass("VaultInner"));
+        customizeResourceBaseClass(fluentModelsPackage.getClass("ManagedHsmInner"));
+    }
+
+    private static void customizeResourceBaseClass(ClassCustomization customization) {
+        customization.customizeAst(ast -> {
+            ast.getClassByName(customization.getClassName()).ifPresent(clazz -> {
+                String resourceClassName = "com.azure.core.management.Resource";
+                ast.addImport(resourceClassName);
+                clazz.getExtendedTypes().clear();
+                clazz.addExtendedType(new ClassOrInterfaceType(null, "Resource"));
+            });
+        });
+    }
+}
