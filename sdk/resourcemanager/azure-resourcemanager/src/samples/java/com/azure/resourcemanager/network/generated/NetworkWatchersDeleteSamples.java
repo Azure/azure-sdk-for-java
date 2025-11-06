995c65e08dc4734a4fed65de04e3ec9fef9ab1d8
Hui Zhu <v-huizhu2@microsoft.com>
2025-11-06 10:34:19 +0800

    Update example file paths to reflect the 2025-01-01 API version

--- sdk/resourcemanager/azure-resourcemanager/src/samples/java/com/azure/resourcemanager/network/generated/NetworkWatchersDeleteSamples.java
+++ sdk/resourcemanager/azure-resourcemanager/src/samples/java/com/azure/resourcemanager/network/generated/NetworkWatchersDeleteSamples.java
@@ -10,7 +10,7 @@ package com.azure.resourcemanager.network.generated;
 public final class NetworkWatchersDeleteSamples {
     /*
      * x-ms-original-file:
-     * specification/network/resource-manager/Microsoft.Network/stable/2024-10-01/examples/NetworkWatcherDelete.json
+     * specification/network/resource-manager/Microsoft.Network/stable/2025-01-01/examples/NetworkWatcherDelete.json
      */
     /**
      * Sample code: Delete network watcher.
