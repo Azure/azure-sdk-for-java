3d5939b2b2d1245645a88f15e69449132b8a062b
Jose Alvarez <jp.alvarezl@gmail.com>
2025-11-06 14:56:03 +0100

    Fixed bad spelling enum names

--- sdk/ai/azure-ai-agents/customizations/src/main/java/AgentsCustomizations.java
+++ sdk/ai/azure-ai-agents/customizations/src/main/java/AgentsCustomizations.java
@@ -10,11 +10,29 @@ public class AgentsCustomizations extends Customization {
 
     @Override
     public void customize(LibraryCustomization libraryCustomization, Logger logger) {
-//        removeConversationsClientBuilder(libraryCustomization, logger);
+        renameImageGenSizeEnums(libraryCustomization, logger);
     }
 
-    private void removeConversationsClientBuilder(LibraryCustomization customization, Logger logger) {
-        logger.info("Removing ConversationsClientBuilder class");
-        customization.getRawEditor().removeFile("src/main/java/com/azure/ai/agents/ConversationsClientBuilder.java");
+    /**
+     * Customization for enum values that are originally numbers and get transliterated by the emitter.
+     * @param customization
+     * @param logger
+     */
+    private void renameImageGenSizeEnums(LibraryCustomization customization, Logger logger) {
+        logger.info("Renaming enum ImageGenToolSize variants");
+        customization.getClass("com.azure.ai.agents.models", "ImageGenToolSize").customizeAst(ast -> ast.getEnumByName("ImageGenToolSize")
+                .ifPresent(clazz -> clazz.getEntries().stream()
+                        .filter(entry -> "ONE_ZERO_TWO_FOURX_ONE_ZERO_TWO_FOUR".equals(entry.getName().getIdentifier()))
+                        .forEach(entry -> entry.setName("SIZE_1024_X_1024"))));
+
+        customization.getClass("com.azure.ai.agents.models", "ImageGenToolSize").customizeAst(ast -> ast.getEnumByName("ImageGenToolSize")
+                .ifPresent(clazz -> clazz.getEntries().stream()
+                        .filter(entry -> "ONE_ZERO_TWO_FOURX_ONE_FIVE_THREE_SIX".equals(entry.getName().getIdentifier()))
+                        .forEach(entry -> entry.setName("SIZE_1024_X_1536"))));
+
+        customization.getClass("com.azure.ai.agents.models", "ImageGenToolSize").customizeAst(ast -> ast.getEnumByName("ImageGenToolSize")
+                .ifPresent(clazz -> clazz.getEntries().stream()
+                        .filter(entry -> "ONE_FIVE_THREE_SIXX_ONE_ZERO_TWO_FOUR".equals(entry.getName().getIdentifier()))
+                        .forEach(entry -> entry.setName("SIZE_1536_X_1024"))));
     }
 }
