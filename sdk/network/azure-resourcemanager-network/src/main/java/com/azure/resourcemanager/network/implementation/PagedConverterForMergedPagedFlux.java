5dd1d08a588e2b9baf62342155c1b9c6da486a71
Weidong Xu <weidxu@microsoft.com>
2025-11-11 13:52:01 +0800

    comment

--- sdk/network/azure-resourcemanager-network/src/main/java/com/azure/resourcemanager/network/implementation/PagedConverterForMergedPagedFlux.java
+++ sdk/network/azure-resourcemanager-network/src/main/java/com/azure/resourcemanager/network/implementation/PagedConverterForMergedPagedFlux.java
@@ -23,6 +23,8 @@ final class PagedConverterForMergedPagedFlux {
 
     /**
      * Applies map transform to elements of PagedFlux.
+     * <p>
+     * Use this mapPage (not the one in PagedConverter), when the PagedFlux is produced by PagedConverter.mergePagedFlux
      *
      * @param pagedFlux the input of PagedFlux.
      * @param mapper the map transform of element T to element S.
