48e898cc8156f4b723433beeda6e578105f2739b
Anannya Patra <40665106+anannya03@users.noreply.github.com>
2025-11-11 00:18:50 -0800

    Remove unused import statements in IdentitySslUtil

--- sdk/identity/azure-identity/src/main/java/com/azure/identity/implementation/util/IdentitySslUtil.java
+++ sdk/identity/azure-identity/src/main/java/com/azure/identity/implementation/util/IdentitySslUtil.java
@@ -12,7 +12,6 @@ import javax.net.ssl.SSLSession;
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
-
 import java.security.KeyManagementException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
