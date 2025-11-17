ee589a08791d281d245133fde7cccf2363bf43d7
Isaiah Dickerson <idickerson@microsoft.com>
2025-11-17 15:16:19 -0800

    address checkstyle issue

--- sdk/search/azure-search-documents/src/main/java/com/azure/search/documents/knowledgebases/package-info.java
+++ sdk/search/azure-search-documents/src/main/java/com/azure/search/documents/knowledgebases/package-info.java
@@ -1,4 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 /**
  * For more information about Azure AI Search KnowledgeBases, supported features, and usage guidelines, see the Azure documentation and the Azure SDK for Java guidelines.
  * 
