f22be56e541d8b4eb7676bd719a820e6b848ca13
muyao <muyaofeng@126.com>
2025-11-20 09:41:39 +0800

    fix

--- sdk/spring/spring-cloud-azure-starter-monitor-test/src/test/java/com/azure/spring/cloud/autoconfigure/monitor/SpringMonitorTest.java
+++ sdk/spring/spring-cloud-azure-starter-monitor-test/src/test/java/com/azure/spring/cloud/autoconfigure/monitor/SpringMonitorTest.java
@@ -17,7 +17,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.resttestclient.TestRestTemplate;
 import org.springframework.context.annotation.Bean;
 import reactor.util.annotation.Nullable;
 
