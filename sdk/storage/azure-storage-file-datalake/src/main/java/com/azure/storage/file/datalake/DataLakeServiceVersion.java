e0ac8b96b576c2743bf5e3b39792dc588d649476
Isabelle <141270045+ibrandes@users.noreply.github.com>
2025-12-03 11:29:29 -0800

    adding new service version (#47408)

--- sdk/storage/azure-storage-file-datalake/src/main/java/com/azure/storage/file/datalake/DataLakeServiceVersion.java
+++ sdk/storage/azure-storage-file-datalake/src/main/java/com/azure/storage/file/datalake/DataLakeServiceVersion.java
@@ -152,7 +152,12 @@ public enum DataLakeServiceVersion implements ServiceVersion {
     /**
      * Service version {@code 2026-02-06}.
      */
-    V2026_02_06("2026-02-06");
+    V2026_02_06("2026-02-06"),
+
+    /**
+     * Service version {@code 2026-04-06}.
+     */
+    V2026_04_06("2026-04-06");
 
     private final String version;
 
@@ -174,6 +179,6 @@ public enum DataLakeServiceVersion implements ServiceVersion {
      * @return the latest {@link DataLakeServiceVersion}
      */
     public static DataLakeServiceVersion getLatest() {
-        return V2026_02_06;
+        return V2026_04_06;
     }
 }
