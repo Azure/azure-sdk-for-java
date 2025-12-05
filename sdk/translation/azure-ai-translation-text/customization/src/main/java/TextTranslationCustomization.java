8c21c662ec1d734fd86a4c7ab9585509de9cf3b6
Jiarui Guo <jiarguo@microsoft.com>
2025-12-05 15:30:06 -0800

    customization to remove V3_0

--- sdk/translation/azure-ai-translation-text/customization/src/main/java/TextTranslationCustomization.java
+++ sdk/translation/azure-ai-translation-text/customization/src/main/java/TextTranslationCustomization.java
@@ -5,9 +5,9 @@ import com.github.javaparser.ast.type.ClassOrInterfaceType;
 import org.slf4j.Logger;
 
 /**
- * This class contains the customization code to customize the TypeSpec generated code for Text Translation Client Builder.
+ * This class contains the customization code to customize the TypeSpec generated code for Text Translation.
  */
-public class TextTranslationClientBuilderCustomization extends Customization {
+public class TextTranslationCustomization extends Customization {
 
     @Override
     public void customize(LibraryCustomization customization, Logger logger) {
@@ -36,5 +36,17 @@ public class TextTranslationClientBuilderCustomization extends Customization {
                 }
             });
         });
+
+        logger.info("Customizing the TextTranslationServiceVersion enum - removing V3_0");
+        customization.getClass("com.azure.ai.translation.text", "TextTranslationServiceVersion")
+            .customizeAst(ast -> ast.getEnumByName("TextTranslationServiceVersion")
+                .ifPresent(enumDeclaration -> {
+                    enumDeclaration.getEntries().stream()
+                        .filter(entry -> entry.getNameAsString().equals("V3_0"))
+                        .findFirst()
+                        .ifPresent(enumConstant -> enumConstant.remove());
+                }
+            )
+        );
     }
 }
