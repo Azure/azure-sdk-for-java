e69b48647e5ffd0a6baf2258a7621ea93f496d72
Hui Zhu <v-huizhu2@microsoft.com>
2025-12-09 12:19:59 +0800

    test(redis): disable public network access update test
    - Disabled test case for updating public network access due to internal policy restrictions
    - Added @Disabled annotation with explanation about local authentication methods not being allowed
    - Preserved existing test structure and comments for future reference

--- sdk/redis/azure-resourcemanager-redis/src/test/java/com/azure/resourcemanager/redis/RedisCacheOperationsTests.java
+++ sdk/redis/azure-resourcemanager-redis/src/test/java/com/azure/resourcemanager/redis/RedisCacheOperationsTests.java
@@ -207,7 +207,7 @@ public class RedisCacheOperationsTests extends RedisManagementTest {
         // com.microsoft.azure.CloudException: One of the SAS URIs provided could not be used for the following reason:
         // The SAS token is poorly formatted.
         /*premiumCache.exportData(storageAccount.name(),"snapshot1");
-
+        
         premiumCache.importData(Arrays.asList("snapshot1"));*/
     }
 
@@ -367,6 +367,7 @@ public class RedisCacheOperationsTests extends RedisManagementTest {
     }
 
     @Test
+    @Disabled("This request was denied due to internal policy. Local authentication methods are not allowed.")
     public void canUpdatePublicNetworkAccess() {
         resourceManager.resourceGroups().define(rgNameSecond).withRegion(Region.US_CENTRAL);
 
