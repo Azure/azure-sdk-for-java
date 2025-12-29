0a22036c06e67a917966c7b9d4ada02bdde1f949
Abhijeet Mohanty <abhmohanty@microsoft.com>
2025-12-29 11:23:19 +0400

    Intercept 404-0s to remap to 404-1003s when parent resource for bulk operations.

--- sdk/cosmos/azure-cosmos-tests/src/test/java/com/azure/cosmos/CosmosNotFoundTests.java
+++ sdk/cosmos/azure-cosmos-tests/src/test/java/com/azure/cosmos/CosmosNotFoundTests.java
@@ -8,14 +8,20 @@ import com.azure.cosmos.implementation.HttpConstants;
 import com.azure.cosmos.implementation.ImplementationBridgeHelpers;
 import com.azure.cosmos.implementation.OperationType;
 import com.azure.cosmos.implementation.TestConfigurations;
+import com.azure.cosmos.models.CosmosBulkOperations;
 import com.azure.cosmos.models.CosmosContainerProperties;
+import com.azure.cosmos.models.CosmosItemOperation;
+import com.azure.cosmos.models.PartitionKey;
 import com.azure.cosmos.models.ThroughputProperties;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Factory;
 import org.testng.annotations.Test;
+import reactor.core.publisher.Flux;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -113,6 +119,52 @@ public class CosmosNotFoundTests extends FaultInjectionTestBase {
         }
     }
 
+    @Test(groups = {"fast"}, timeOut = TIMEOUT)
+    public void performBulkOnNonExistentContainer() {
+
+        CosmosAsyncClient asyncClientToUse = getClientBuilder()
+            .endpoint(TestConfigurations.HOST)
+            .key(TestConfigurations.MASTER_KEY)
+            .buildAsyncClient();
+
+        // Try to read the item from a non-existent container
+        String nonExistentContainerId = "NonExistentContainer_" + UUID.randomUUID();
+        CosmosAsyncContainer nonExistentContainer = asyncClientToUse
+            .getDatabase(existingAsyncContainer.getDatabase().getId())
+            .getContainer(nonExistentContainerId);
+
+        try {
+
+            List<CosmosItemOperation> cosmosItemOperations = new ArrayList<>();
+
+            CosmosItemOperation cosmosItemOperation = CosmosBulkOperations.getReadItemOperation(
+                this.objectToCreate.getId(),
+                new PartitionKey(this.createdItemPk),
+                TestObject.class);
+
+            cosmosItemOperations.add(cosmosItemOperation);
+
+            Flux<CosmosItemOperation> operationsFlux = Flux.fromIterable(cosmosItemOperations);
+
+            nonExistentContainer.executeBulkOperations(operationsFlux).blockLast();
+        } catch (CosmosException ce) {
+
+            // Verify status code is 404 (Not Found)
+            assertThat(ce.getStatusCode())
+                .as("Status code should be 404 (Not Found)")
+                .isEqualTo(HttpConstants.StatusCodes.NOTFOUND);
+
+            // Verify sub-status code is either 0 or 1003
+            assertThat(ce.getSubStatusCode())
+                .as("Sub-status code should be 1003")
+                .isIn(
+                    HttpConstants.SubStatusCodes.OWNER_RESOURCE_NOT_EXISTS
+                );
+        } finally {
+            safeClose(asyncClientToUse);
+        }
+    }
+
     @Test(groups = {"thin-client-multi-region"}, dataProvider = "operationTypeProvider", timeOut = TIMEOUT)
     public void performDocumentOperationOnNonExistentContainerGatewayModeV2(OperationType operationType) {
         logger.info("Running test: Read item from non-existent container in Gateway Connection Mode");
@@ -147,6 +199,58 @@ public class CosmosNotFoundTests extends FaultInjectionTestBase {
 
             // Verify sub-status code is 1003
             assertThat(diagnosticsContext.getSubStatusCode())
+                .as("Sub-status code should be 1003")
+                .isIn(
+                    HttpConstants.SubStatusCodes.OWNER_RESOURCE_NOT_EXISTS
+                );
+        } finally {
+            safeClose(v2GatewayAsyncClient);
+            System.clearProperty("COSMOS.THINCLIENT_ENABLED");
+        }
+    }
+
+    @Test(groups = {"thin-client-multi-region"}, timeOut = TIMEOUT)
+    public void performBulkOnNonExistentContainerGatewayModeV2() {
+        logger.info("Running test: Read item from non-existent container in Gateway Connection Mode");
+
+        // Try to read the item from a non-existent container using Gateway mode client
+        String nonExistentContainerId = "NonExistentContainer_" + UUID.randomUUID();
+
+        System.setProperty("COSMOS.THINCLIENT_ENABLED", "true");
+
+        CosmosAsyncClient v2GatewayAsyncClient = new CosmosClientBuilder()
+            .endpoint(TestConfigurations.HOST)
+            .key(TestConfigurations.MASTER_KEY)
+            .gatewayMode(new GatewayConnectionConfig().setHttp2ConnectionConfig(new Http2ConnectionConfig().setEnabled(true)))
+            .buildAsyncClient();
+
+        CosmosAsyncContainer nonExistentContainer = v2GatewayAsyncClient
+            .getDatabase(existingAsyncContainer.getDatabase().getId())
+            .getContainer(nonExistentContainerId);
+
+        try {
+
+            List<CosmosItemOperation> cosmosItemOperations = new ArrayList<>();
+
+            CosmosItemOperation cosmosItemOperation = CosmosBulkOperations.getReadItemOperation(
+                this.objectToCreate.getId(),
+                new PartitionKey(this.createdItemPk),
+                TestObject.class);
+
+            cosmosItemOperations.add(cosmosItemOperation);
+
+            Flux<CosmosItemOperation> operationsFlux = Flux.fromIterable(cosmosItemOperations);
+
+            nonExistentContainer.executeBulkOperations(operationsFlux).blockLast();
+        } catch (CosmosException ce) {
+
+            // Verify status code is 404 (Not Found)
+            assertThat(ce.getStatusCode())
+                .as("Status code should be 404 (Not Found)")
+                .isEqualTo(HttpConstants.StatusCodes.NOTFOUND);
+
+            // Verify sub-status code is either 0 or 1003
+            assertThat(ce.getSubStatusCode())
                 .as("Sub-status code should be 1003")
                 .isIn(
                     HttpConstants.SubStatusCodes.OWNER_RESOURCE_NOT_EXISTS
@@ -228,6 +332,64 @@ public class CosmosNotFoundTests extends FaultInjectionTestBase {
         }
     }
 
+    @Test(groups = {"fast"}, timeOut = TIMEOUT)
+    public void performBulkOnDeletedContainer() throws InterruptedException {
+
+        // Create a dedicated container for this test
+        String testContainerId = "CosmosNotFoundTestsContainer_" + UUID.randomUUID();
+        CosmosContainerProperties containerProperties = new CosmosContainerProperties(testContainerId, "/mypk");
+        testAsyncDatabase.createContainer(containerProperties, ThroughputProperties.createManualThroughput(400)).block();
+
+        CosmosAsyncClient clientToUse = getClientBuilder()
+            .endpoint(TestConfigurations.HOST)
+            .key(TestConfigurations.MASTER_KEY)
+            .buildAsyncClient();
+
+        CosmosAsyncContainer containerToUse = clientToUse.getDatabase(testAsyncDatabase.getId()).getContainer(testContainerId);
+
+        Thread.sleep(5000);
+
+        // Create an item in the container
+        TestObject testObject = TestObject.create(this.createdItemPk);
+        containerToUse.createItem(testObject).block();
+
+        // Create a different client instance to delete the container
+        CosmosAsyncClient deletingAsyncClient = getClientBuilder()
+            .endpoint(TestConfigurations.HOST)
+            .key(TestConfigurations.MASTER_KEY)
+            .buildAsyncClient();
+
+        try {
+            // Delete the container using the different client instance
+            CosmosAsyncContainer containerToDelete = deletingAsyncClient.getDatabase(testAsyncDatabase.getId()).getContainer(testContainerId);
+            containerToDelete.delete().block();
+
+            Thread.sleep(5000);
+
+            // Try to read the item from the deleted container using the original client
+
+            List<CosmosItemOperation> cosmosItemOperations = new ArrayList<>();
+
+            CosmosItemOperation cosmosItemOperation = CosmosBulkOperations.getReadItemOperation(
+                this.objectToCreate.getId(),
+                new PartitionKey(this.createdItemPk),
+                TestObject.class);
+
+            cosmosItemOperations.add(cosmosItemOperation);
+
+            Flux<CosmosItemOperation> operationsFlux = Flux.fromIterable(cosmosItemOperations);
+
+            containerToUse.executeBulkOperations(operationsFlux).blockLast();
+        } catch (CosmosException ce) {
+            assertThat(ce.getSubStatusCode())
+                .as("Sub-status code should be 0")
+                .isIn(HttpConstants.SubStatusCodes.UNKNOWN);
+        } finally {
+            safeClose(clientToUse);
+            safeClose(deletingAsyncClient);
+        }
+    }
+
     @Test(groups = {"thin-client-multi-region"}, dataProvider = "operationTypeProvider", timeOut = TIMEOUT)
     public void performDocumentOperationOnDeletedContainerWithGatewayV2(OperationType operationType) throws InterruptedException {
         logger.info("Running test: Read item from deleted container - Gateway V2 Connection Mode");
@@ -288,6 +450,74 @@ public class CosmosNotFoundTests extends FaultInjectionTestBase {
                     HttpConstants.SubStatusCodes.OWNER_RESOURCE_NOT_EXISTS
                 );
 
+        } finally {
+            safeClose(gatewayV2AsyncClientToUse);
+            safeClose(containerDeletingAsyncClient);
+
+            System.clearProperty("COSMOS.THINCLIENT_ENABLED");
+        }
+    }
+
+    @Test(groups = {"thin-client-multi-region"}, timeOut = TIMEOUT)
+    public void performBulkOnDeletedContainerWithGatewayV2() throws InterruptedException {
+        logger.info("Running test: Read item from deleted container - Gateway V2 Connection Mode");
+
+        // Create a dedicated container for this test
+        String testContainerId = "CosmosNotFoundTestsContainer_" + UUID.randomUUID();
+        CosmosContainerProperties containerProperties = new CosmosContainerProperties(testContainerId, "/mypk");
+        testAsyncDatabase.createContainer(containerProperties, ThroughputProperties.createManualThroughput(400)).block();
+
+        System.setProperty("COSMOS.THINCLIENT_ENABLED", "true");
+        Http2ConnectionConfig http2ConnectionConfig = new Http2ConnectionConfig().setEnabled(true);
+        GatewayConnectionConfig gatewayConnectionConfig = new GatewayConnectionConfig();
+        gatewayConnectionConfig.setHttp2ConnectionConfig(http2ConnectionConfig);
+
+        CosmosAsyncClient gatewayV2AsyncClientToUse = new CosmosClientBuilder()
+            .endpoint(TestConfigurations.HOST)
+            .key(TestConfigurations.MASTER_KEY)
+            .gatewayMode(gatewayConnectionConfig)
+            .buildAsyncClient();
+        CosmosAsyncClient containerDeletingAsyncClient = new CosmosClientBuilder()
+            .endpoint(TestConfigurations.HOST)
+            .key(TestConfigurations.MASTER_KEY)
+            .gatewayMode()
+            .buildAsyncClient();
+
+        CosmosAsyncContainer containerToUse = gatewayV2AsyncClientToUse.getDatabase(testAsyncDatabase.getId()).getContainer(testContainerId);
+
+        Thread.sleep(5000);
+
+        // Create an item in the container
+        TestObject testObject = TestObject.create(this.createdItemPk);
+        containerToUse.createItem(testObject).block();
+
+        try {
+            // Delete the container using the different client instance
+            CosmosAsyncContainer asyncContainerToDelete = containerDeletingAsyncClient.getDatabase(testAsyncDatabase.getId()).getContainer(testContainerId);
+            asyncContainerToDelete.delete().block();
+
+            Thread.sleep(5000);
+
+            // Try to read the item from the deleted container using the original client
+
+            List<CosmosItemOperation> cosmosItemOperations = new ArrayList<>();
+
+            CosmosItemOperation cosmosItemOperation = CosmosBulkOperations.getReadItemOperation(
+                this.objectToCreate.getId(),
+                new PartitionKey(this.createdItemPk),
+                TestObject.class);
+
+            cosmosItemOperations.add(cosmosItemOperation);
+
+            Flux<CosmosItemOperation> operationsFlux = Flux.fromIterable(cosmosItemOperations);
+
+            containerToUse.executeBulkOperations(operationsFlux).blockLast();
+        } catch (CosmosException ce) {
+            assertThat(ce.getSubStatusCode())
+                .as("Sub-status code should be 1003")
+                .isIn(
+                    HttpConstants.SubStatusCodes.OWNER_RESOURCE_NOT_EXISTS
+                );
         } finally {
             safeClose(gatewayV2AsyncClientToUse);
             safeClose(containerDeletingAsyncClient);
