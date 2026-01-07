db41e2c55c727feaa070efc0015e110ac9d21bd3
Fabian Meiswinkel <fabianm@microsoft.com>
2026-01-07 01:42:05 +0000

    Update CosmosItemIdentityHelper.scala

--- sdk/cosmos/azure-cosmos-spark_3/src/main/scala/com/azure/cosmos/spark/CosmosItemIdentityHelper.scala
+++ sdk/cosmos/azure-cosmos-spark_3/src/main/scala/com/azure/cosmos/spark/CosmosItemIdentityHelper.scala
@@ -6,6 +6,7 @@ package com.azure.cosmos.spark
 import com.azure.cosmos.implementation.routing.PartitionKeyInternal
 import com.azure.cosmos.implementation.{ImplementationBridgeHelpers, Utils}
 import com.azure.cosmos.models.{CosmosItemIdentity, PartitionKey}
+import com.fasterxml.jackson.databind.JsonNode
 
 import java.util
 
@@ -32,7 +33,20 @@ private[spark] object CosmosItemIdentityHelper {
       case cosmosItemIdentityStringRegx(idValue, pkValue) =>
         val partitionKeyValue = Utils.parse(pkValue, classOf[Object])
         partitionKeyValue match {
-          case arrayList: util.ArrayList[Object] => Some(createCosmosItemIdentityWithMultiHashPartitionKey(idValue, arrayList.toArray))
+          case arrayList: util.ArrayList[_] => 
+            // Convert Jackson JsonNode objects to their primitive values
+            // This is necessary because Utils.parse returns JsonNode instances when deserializing from JSON
+            // In Scala 2.13, the deprecated JavaConverters behaves differently, so we need explicit conversion
+            val pkValuesArray = new Array[Object](arrayList.size())
+            var i = 0
+            while (i < arrayList.size()) {
+              pkValuesArray(i) = arrayList.get(i) match {
+                case node: JsonNode => convertJsonNodeToPrimitive(node)
+                case other => other.asInstanceOf[Object]
+              }
+              i += 1
+            }
+            Some(createCosmosItemIdentityWithMultiHashPartitionKey(idValue, pkValuesArray))
           case _ => Some(new CosmosItemIdentity(new PartitionKey(partitionKeyValue), idValue))
         }
       case _ => None
@@ -44,8 +58,25 @@ private[spark] object CosmosItemIdentityHelper {
       ImplementationBridgeHelpers
         .PartitionKeyHelper
         .getPartitionKeyAccessor
-        .toPartitionKey(PartitionKeyInternal.fromObjectArray(pkValuesArray, false))
+        .toPartitionKey(PartitionKeyInternal.fromObjectArray(pkValuesArray, true))
 
     new CosmosItemIdentity(partitionKey, idValue)
+  }
+  
+  private[this] def convertJsonNodeToPrimitive(node: JsonNode): Object = {
+    if (node.isTextual) {
+      node.asText()
+    } else if (node.isBoolean) {
+      Boolean.box(node.asBoolean())
+    } else if (node.isInt || node.isLong) {
+      Long.box(node.asLong())
+    } else if (node.isDouble || node.isFloat) {
+      Double.box(node.asDouble())
+    } else if (node.isNull) {
+      null
+    } else {
+      // For any other type, return the node itself and let the partition key logic handle it
+      node
+    }
   }
 }
