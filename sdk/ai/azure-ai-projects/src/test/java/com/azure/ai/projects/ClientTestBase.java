c9c07bb7abed48bc6e21a1a1af39687aeb317ebd
Jose Alvarez <josealvar@microsoft.com>
2026-01-13 15:39:43 +0100

    Using valid version

--- sdk/ai/azure-ai-projects/src/test/java/com/azure/ai/projects/ClientTestBase.java
+++ sdk/ai/azure-ai-projects/src/test/java/com/azure/ai/projects/ClientTestBase.java
@@ -51,7 +51,7 @@ public class ClientTestBase extends TestProxyTestBase {
         String version = Configuration.getGlobalConfiguration().get("SERVICE_VERSION");
         AIProjectsServiceVersion serviceVersion = version != null
             ? AIProjectsServiceVersion.valueOf(version)
-            : AIProjectsServiceVersion.V2025_05_15_PREVIEW;
+            : AIProjectsServiceVersion.V2025_11_15_PREVIEW;
         builder.serviceVersion(serviceVersion);
         return builder;
     }
