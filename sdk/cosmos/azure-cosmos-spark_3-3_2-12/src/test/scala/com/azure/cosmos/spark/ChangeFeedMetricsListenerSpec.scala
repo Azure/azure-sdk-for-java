8fb735dc39c77a542a7c71e5f65a9b8f8e6037d6
Fabian Meiswinkel <fabianm@microsoft.com>
2026-01-15 23:19:06 +0000

    Fixing build issues

--- sdk/cosmos/azure-cosmos-spark_3-3_2-12/src/test/scala/com/azure/cosmos/spark/ChangeFeedMetricsListenerSpec.scala
+++ sdk/cosmos/azure-cosmos-spark_3-3_2-12/src/test/scala/com/azure/cosmos/spark/ChangeFeedMetricsListenerSpec.scala
@@ -1,5 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+// scalastyle:off magic.number
+// scalastyle:off multiple.string.literals
 
 package com.azure.cosmos.spark
 
