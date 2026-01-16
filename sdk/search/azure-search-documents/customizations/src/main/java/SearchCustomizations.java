70f686a9ff85d5332aa5d5242233a79fe5a6be3a
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2026-01-16 15:41:05 -0500

    Initial regeneration using TypeSpec

---
+++ sdk/search/azure-search-documents/customizations/src/main/java/SearchCustomizations.java
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import com.azure.autorest.customization.Customization;
+import com.azure.autorest.customization.LibraryCustomization;
+import org.slf4j.Logger;
+
+/**
+ * Contains customizations for Azure AI Search code generation.
+ */
+public class SearchCustomizations extends Customization {
+    @Override
+    public void customize(LibraryCustomization libraryCustomization, Logger logger) {
+    }
+}
--- sdk/search/azure-search-documents/src/main/java/com/azure/search/documents/options/package-info.java
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-/**
- * Package containing options model classes used by Azure Search Documents.
- */
-package com.azure.search.documents.options;
