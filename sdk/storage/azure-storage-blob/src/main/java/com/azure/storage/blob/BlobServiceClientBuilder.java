d7f8858db2fb56678734193722aed10e95b4868a
Isabelle <ibrandes@microsoft.com>
2026-01-25 14:00:48 -0800

    adding support to blob service client

--- sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/BlobServiceClientBuilder.java
+++ sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/BlobServiceClientBuilder.java
@@ -311,8 +311,8 @@ public final class BlobServiceClientBuilder implements TokenCredentialTrait<Blob
     @Override
     public BlobServiceClientBuilder credential(TokenCredential credential) {
         this.tokenCredential = Objects.requireNonNull(credential, "'credential' cannot be null.");
+        // Only clear conflicting credentials, allow SAS to coexist for delegated user scenarios
         this.storageSharedKeyCredential = null;
-        this.sasToken = null;
         return this;
     }
 
@@ -326,8 +326,8 @@ public final class BlobServiceClientBuilder implements TokenCredentialTrait<Blob
      */
     public BlobServiceClientBuilder sasToken(String sasToken) {
         this.sasToken = Objects.requireNonNull(sasToken, "'sasToken' cannot be null.");
+        // Only clear conflicting credentials, allow SAS to coexist for delegated user scenarios
         this.storageSharedKeyCredential = null;
-        this.tokenCredential = null;
         return this;
     }
 
@@ -341,6 +341,8 @@ public final class BlobServiceClientBuilder implements TokenCredentialTrait<Blob
     @Override
     public BlobServiceClientBuilder credential(AzureSasCredential credential) {
         this.azureSasCredential = Objects.requireNonNull(credential, "'credential' cannot be null.");
+        // Allow token credential to coexist for delegated user scenarios, only clear conflicting ones
+        this.storageSharedKeyCredential = null;
         return this;
     }
 
