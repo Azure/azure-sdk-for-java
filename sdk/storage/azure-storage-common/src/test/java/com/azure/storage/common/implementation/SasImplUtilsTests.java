863b7b68c54029b11543fb5ab7572b2a1d5278d3
browndav <browndav@microsoft.com>
2026-01-27 11:33:54 -0500

    remove static from SasImplUtilsTests so that beforeeach works

--- sdk/storage/azure-storage-common/src/test/java/com/azure/storage/common/implementation/SasImplUtilsTests.java
+++ sdk/storage/azure-storage-common/src/test/java/com/azure/storage/common/implementation/SasImplUtilsTests.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;;
 
 public class SasImplUtilsTests {
 
-    private static Map<String, String> requestHeaders;
+    private Map<String, String> requestHeaders;
 
     @BeforeEach
     public void setup() {
