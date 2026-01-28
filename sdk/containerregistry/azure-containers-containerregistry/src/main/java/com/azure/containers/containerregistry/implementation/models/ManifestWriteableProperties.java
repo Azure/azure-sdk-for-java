3cdc564a0f139ff4e939fd33724f349c56fe4c99
azure-sdk <azuresdk@microsoft.com>
2026-01-28 16:09:37 +0000

    Reset sources for azure-containers-containerregistry to the release version 1.2.21.

--- sdk/containerregistry/azure-containers-containerregistry/src/main/java/com/azure/containers/containerregistry/implementation/models/ManifestWriteableProperties.java
+++ sdk/containerregistry/azure-containers-containerregistry/src/main/java/com/azure/containers/containerregistry/implementation/models/ManifestWriteableProperties.java
@@ -5,66 +5,54 @@
 package com.azure.containers.containerregistry.implementation.models;
 
 import com.azure.core.annotation.Fluent;
-import com.azure.core.annotation.Generated;
 import com.azure.json.JsonReader;
 import com.azure.json.JsonSerializable;
 import com.azure.json.JsonToken;
 import com.azure.json.JsonWriter;
 import java.io.IOException;
 
-/**
- * Changeable attributes.
- */
+/** Changeable attributes. */
 @Fluent
 public final class ManifestWriteableProperties implements JsonSerializable<ManifestWriteableProperties> {
     /*
      * Delete enabled
      */
-    @Generated
     private Boolean deleteEnabled;
 
     /*
      * Write enabled
      */
-    @Generated
     private Boolean writeEnabled;
 
     /*
      * List enabled
      */
-    @Generated
     private Boolean listEnabled;
 
     /*
      * Read enabled
      */
-    @Generated
     private Boolean readEnabled;
 
-    /**
-     * Creates an instance of ManifestWriteableProperties class.
-     */
-    @Generated
+    /** Creates an instance of ManifestWriteableProperties class. */
     public ManifestWriteableProperties() {
     }
 
     /**
      * Get the deleteEnabled property: Delete enabled.
-     * 
+     *
      * @return the deleteEnabled value.
      */
-    @Generated
     public Boolean isDeleteEnabled() {
         return this.deleteEnabled;
     }
 
     /**
      * Set the deleteEnabled property: Delete enabled.
-     * 
+     *
      * @param deleteEnabled the deleteEnabled value to set.
      * @return the ManifestWriteableProperties object itself.
      */
-    @Generated
     public ManifestWriteableProperties setDeleteEnabled(Boolean deleteEnabled) {
         this.deleteEnabled = deleteEnabled;
         return this;
@@ -72,21 +60,19 @@ public final class ManifestWriteableProperties implements JsonSerializable<Manif
 
     /**
      * Get the writeEnabled property: Write enabled.
-     * 
+     *
      * @return the writeEnabled value.
      */
-    @Generated
     public Boolean isWriteEnabled() {
         return this.writeEnabled;
     }
 
     /**
      * Set the writeEnabled property: Write enabled.
-     * 
+     *
      * @param writeEnabled the writeEnabled value to set.
      * @return the ManifestWriteableProperties object itself.
      */
-    @Generated
     public ManifestWriteableProperties setWriteEnabled(Boolean writeEnabled) {
         this.writeEnabled = writeEnabled;
         return this;
@@ -94,21 +80,19 @@ public final class ManifestWriteableProperties implements JsonSerializable<Manif
 
     /**
      * Get the listEnabled property: List enabled.
-     * 
+     *
      * @return the listEnabled value.
      */
-    @Generated
     public Boolean isListEnabled() {
         return this.listEnabled;
     }
 
     /**
      * Set the listEnabled property: List enabled.
-     * 
+     *
      * @param listEnabled the listEnabled value to set.
      * @return the ManifestWriteableProperties object itself.
      */
-    @Generated
     public ManifestWriteableProperties setListEnabled(Boolean listEnabled) {
         this.listEnabled = listEnabled;
         return this;
@@ -116,30 +100,24 @@ public final class ManifestWriteableProperties implements JsonSerializable<Manif
 
     /**
      * Get the readEnabled property: Read enabled.
-     * 
+     *
      * @return the readEnabled value.
      */
-    @Generated
     public Boolean isReadEnabled() {
         return this.readEnabled;
     }
 
     /**
      * Set the readEnabled property: Read enabled.
-     * 
+     *
      * @param readEnabled the readEnabled value to set.
      * @return the ManifestWriteableProperties object itself.
      */
-    @Generated
     public ManifestWriteableProperties setReadEnabled(Boolean readEnabled) {
         this.readEnabled = readEnabled;
         return this;
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Generated
     @Override
     public JsonWriter toJson(JsonWriter jsonWriter) throws IOException {
         jsonWriter.writeStartObject();
@@ -152,13 +130,12 @@ public final class ManifestWriteableProperties implements JsonSerializable<Manif
 
     /**
      * Reads an instance of ManifestWriteableProperties from the JsonReader.
-     * 
+     *
      * @param jsonReader The JsonReader being read.
      * @return An instance of ManifestWriteableProperties if the JsonReader was pointing to an instance of it, or null
-     * if it was pointing to JSON null.
+     *     if it was pointing to JSON null.
      * @throws IOException If an error occurs while reading the ManifestWriteableProperties.
      */
-    @Generated
     public static ManifestWriteableProperties fromJson(JsonReader jsonReader) throws IOException {
         return jsonReader.readObject(reader -> {
             ManifestWriteableProperties deserializedManifestWriteableProperties = new ManifestWriteableProperties();
--- sdk/containerregistry/azure-containers-containerregistry/src/main/java/com/azure/containers/containerregistry/models/ArtifactOperatingSystem.java
+++ sdk/containerregistry/azure-containers-containerregistry/src/main/java/com/azure/containers/containerregistry/models/ArtifactOperatingSystem.java
@@ -4,125 +4,77 @@
 
 package com.azure.containers.containerregistry.models;
 
-import com.azure.core.annotation.Generated;
 import com.azure.core.util.ExpandableStringEnum;
 import java.util.Collection;
 
-/**
- * Defines values for ArtifactOperatingSystem.
- */
+/** Defines values for ArtifactOperatingSystem. */
 public final class ArtifactOperatingSystem extends ExpandableStringEnum<ArtifactOperatingSystem> {
-    /**
-     * Static value aix for ArtifactOperatingSystem.
-     */
-    @Generated
+    /** Static value aix for ArtifactOperatingSystem. */
     public static final ArtifactOperatingSystem AIX = fromString("aix");
 
-    /**
-     * Static value android for ArtifactOperatingSystem.
-     */
-    @Generated
+    /** Static value android for ArtifactOperatingSystem. */
     public static final ArtifactOperatingSystem ANDROID = fromString("android");
 
-    /**
-     * Static value darwin for ArtifactOperatingSystem.
-     */
-    @Generated
+    /** Static value darwin for ArtifactOperatingSystem. */
     public static final ArtifactOperatingSystem DARWIN = fromString("darwin");
 
-    /**
-     * Static value dragonfly for ArtifactOperatingSystem.
-     */
-    @Generated
+    /** Static value dragonfly for ArtifactOperatingSystem. */
     public static final ArtifactOperatingSystem DRAGONFLY = fromString("dragonfly");
 
-    /**
-     * Static value freebsd for ArtifactOperatingSystem.
-     */
-    @Generated
+    /** Static value freebsd for ArtifactOperatingSystem. */
     public static final ArtifactOperatingSystem FREE_BSD = fromString("freebsd");
 
-    /**
-     * Static value illumos for ArtifactOperatingSystem.
-     */
-    @Generated
+    /** Static value illumos for ArtifactOperatingSystem. */
     public static final ArtifactOperatingSystem ILLUMOS = fromString("illumos");
 
-    /**
-     * Static value ios for ArtifactOperatingSystem.
-     */
-    @Generated
+    /** Static value ios for ArtifactOperatingSystem. */
     public static final ArtifactOperatingSystem IOS = fromString("ios");
 
-    /**
-     * Static value js for ArtifactOperatingSystem.
-     */
-    @Generated
+    /** Static value js for ArtifactOperatingSystem. */
     public static final ArtifactOperatingSystem JS = fromString("js");
 
-    /**
-     * Static value linux for ArtifactOperatingSystem.
-     */
-    @Generated
+    /** Static value linux for ArtifactOperatingSystem. */
     public static final ArtifactOperatingSystem LINUX = fromString("linux");
 
-    /**
-     * Static value netbsd for ArtifactOperatingSystem.
-     */
-    @Generated
+    /** Static value netbsd for ArtifactOperatingSystem. */
     public static final ArtifactOperatingSystem NET_BSD = fromString("netbsd");
 
-    /**
-     * Static value openbsd for ArtifactOperatingSystem.
-     */
-    @Generated
+    /** Static value openbsd for ArtifactOperatingSystem. */
     public static final ArtifactOperatingSystem OPEN_BSD = fromString("openbsd");
 
-    /**
-     * Static value plan9 for ArtifactOperatingSystem.
-     */
-    @Generated
+    /** Static value plan9 for ArtifactOperatingSystem. */
     public static final ArtifactOperatingSystem PLAN9 = fromString("plan9");
 
-    /**
-     * Static value solaris for ArtifactOperatingSystem.
-     */
-    @Generated
+    /** Static value solaris for ArtifactOperatingSystem. */
     public static final ArtifactOperatingSystem SOLARIS = fromString("solaris");
 
-    /**
-     * Static value windows for ArtifactOperatingSystem.
-     */
-    @Generated
+    /** Static value windows for ArtifactOperatingSystem. */
     public static final ArtifactOperatingSystem WINDOWS = fromString("windows");
 
     /**
      * Creates a new instance of ArtifactOperatingSystem value.
-     * 
+     *
      * @deprecated Use the {@link #fromString(String)} factory method.
      */
-    @Generated
     @Deprecated
     public ArtifactOperatingSystem() {
     }
 
     /**
      * Creates or finds a ArtifactOperatingSystem from its string representation.
-     * 
+     *
      * @param name a name to look for.
      * @return the corresponding ArtifactOperatingSystem.
      */
-    @Generated
     public static ArtifactOperatingSystem fromString(String name) {
         return fromString(name, ArtifactOperatingSystem.class);
     }
 
     /**
      * Gets known ArtifactOperatingSystem values.
-     * 
+     *
      * @return known ArtifactOperatingSystem values.
      */
-    @Generated
     public static Collection<ArtifactOperatingSystem> values() {
         return values(ArtifactOperatingSystem.class);
     }
