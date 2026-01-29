7285b8fe2768c838c3545886391fa3060b251f1f
azure-sdk <azuresdk@microsoft.com>
2026-01-29 21:31:50 +0800

    Reset sources for azure-communication-rooms to the release version 1.2.5.

--- sdk/communication/azure-communication-rooms/src/main/java/com/azure/communication/rooms/implementation/ParticipantsImpl.java
+++ sdk/communication/azure-communication-rooms/src/main/java/com/azure/communication/rooms/implementation/ParticipantsImpl.java
@@ -60,7 +60,7 @@ public final class ParticipantsImpl {
      * service to perform REST calls.
      */
     @Host("{endpoint}")
-    @ServiceInterface(name = "AzureCommunicationRoomServiceParticipants")
+    @ServiceInterface(name = "AzureCommunicationRo")
     public interface ParticipantsService {
         @Get("/rooms/{roomId}/participants")
         @ExpectedResponses({ 200 })
@@ -219,7 +219,8 @@ public final class ParticipantsImpl {
      */
     @ServiceMethod(returns = ReturnType.COLLECTION)
     public PagedIterable<RoomParticipant> list(String roomId) {
-        return new PagedIterable<>(() -> listSinglePage(roomId), nextLink -> listNextSinglePage(nextLink));
+        return new PagedIterable<>(() -> listSinglePage(roomId, Context.NONE),
+            nextLink -> listNextSinglePage(nextLink));
     }
 
     /**
@@ -250,7 +251,9 @@ public final class ParticipantsImpl {
      */
     @ServiceMethod(returns = ReturnType.SINGLE)
     public Mono<Response<Object>> updateWithResponseAsync(String roomId, String updateParticipantsRequest) {
-        return FluxUtil.withContext(context -> updateWithResponseAsync(roomId, updateParticipantsRequest, context));
+        final String accept = "application/json";
+        return FluxUtil.withContext(context -> service.update(this.client.getEndpoint(), roomId,
+            this.client.getApiVersion(), updateParticipantsRequest, accept, context));
     }
 
     /**
@@ -339,15 +342,16 @@ public final class ParticipantsImpl {
     }
 
     /**
-     * Get participants in a room.
-     * 
      * Get the next page of items.
      * 
-     * @param nextLink The URL to get the next list of items.
+     * @param nextLink The URL to get the next list of items
+     * 
+     * The nextLink parameter.
      * @throws IllegalArgumentException thrown if parameters fail the validation.
      * @throws CommunicationErrorResponseException thrown if the request is rejected by server.
      * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     * @return participants in a room along with {@link PagedResponse} on successful completion of {@link Mono}.
+     * @return a collection of participants in a room along with {@link PagedResponse} on successful completion of
+     * {@link Mono}.
      */
     @ServiceMethod(returns = ReturnType.SINGLE)
     public Mono<PagedResponse<RoomParticipant>> listNextSinglePageAsync(String nextLink) {
@@ -358,16 +362,17 @@ public final class ParticipantsImpl {
     }
 
     /**
-     * Get participants in a room.
-     * 
      * Get the next page of items.
      * 
-     * @param nextLink The URL to get the next list of items.
+     * @param nextLink The URL to get the next list of items
+     * 
+     * The nextLink parameter.
      * @param context The context to associate with this operation.
      * @throws IllegalArgumentException thrown if parameters fail the validation.
      * @throws CommunicationErrorResponseException thrown if the request is rejected by server.
      * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     * @return participants in a room along with {@link PagedResponse} on successful completion of {@link Mono}.
+     * @return a collection of participants in a room along with {@link PagedResponse} on successful completion of
+     * {@link Mono}.
      */
     @ServiceMethod(returns = ReturnType.SINGLE)
     public Mono<PagedResponse<RoomParticipant>> listNextSinglePageAsync(String nextLink, Context context) {
@@ -378,15 +383,15 @@ public final class ParticipantsImpl {
     }
 
     /**
-     * Get participants in a room.
-     * 
      * Get the next page of items.
      * 
-     * @param nextLink The URL to get the next list of items.
+     * @param nextLink The URL to get the next list of items
+     * 
+     * The nextLink parameter.
      * @throws IllegalArgumentException thrown if parameters fail the validation.
      * @throws CommunicationErrorResponseException thrown if the request is rejected by server.
      * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     * @return participants in a room along with {@link PagedResponse}.
+     * @return a collection of participants in a room along with {@link PagedResponse}.
      */
     @ServiceMethod(returns = ReturnType.SINGLE)
     public PagedResponse<RoomParticipant> listNextSinglePage(String nextLink) {
@@ -398,16 +403,16 @@ public final class ParticipantsImpl {
     }
 
     /**
-     * Get participants in a room.
-     * 
      * Get the next page of items.
      * 
-     * @param nextLink The URL to get the next list of items.
+     * @param nextLink The URL to get the next list of items
+     * 
+     * The nextLink parameter.
      * @param context The context to associate with this operation.
      * @throws IllegalArgumentException thrown if parameters fail the validation.
      * @throws CommunicationErrorResponseException thrown if the request is rejected by server.
      * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
-     * @return participants in a room along with {@link PagedResponse}.
+     * @return a collection of participants in a room along with {@link PagedResponse}.
      */
     @ServiceMethod(returns = ReturnType.SINGLE)
     public PagedResponse<RoomParticipant> listNextSinglePage(String nextLink, Context context) {
