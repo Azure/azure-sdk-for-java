d0efd3712db406cedb1d31f40600e358e118a8e1
azure-sdk <azuresdk@microsoft.com>
2026-01-29 18:54:45 +0800

    Reset sources for azure-communication-jobrouter to the release version 1.1.17.

--- sdk/communication/azure-communication-jobrouter/src/main/java/com/azure/communication/jobrouter/implementation/converters/package-info.java
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-/**
- * Package containing converters for Azure Communication JobRouter Service.
- */
-package com.azure.communication.jobrouter.implementation.converters;
