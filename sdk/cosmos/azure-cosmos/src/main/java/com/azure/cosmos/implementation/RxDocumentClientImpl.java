207423bc7672613669a4e6939c78fdb278c8985a
Bhaskar Mallapragada <mbhaskar@gmail.com>
2026-01-29 14:17:39 -0800

    Dedicatedgateway shardkey request option (#47796)
    * Adding shardkey option to DedicatedGatewayRequestOptions
    * Adding tests
    * Adding validation tests for shardkey
    * Using Pattern instead of String.matches
    Test import cleanup
    * Adding validation for length
    * Fixing serialization lint by adding correct serialVersionUID to fix serialization lint error. Needed to use the old class serialVersionUID to fix the issue. This should be harmless as we introduced a new param, so when deserialized with old class object, the new property will be ignored.

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/RxDocumentClientImpl.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/RxDocumentClientImpl.java
@@ -2024,6 +2024,10 @@ public class RxDocumentClientImpl implements AsyncDocumentClient, IAuthorization
                 headers.put(HttpConstants.HttpHeaders.DEDICATED_GATEWAY_PER_REQUEST_BYPASS_CACHE,
                     String.valueOf(options.getDedicatedGatewayRequestOptions().isIntegratedCacheBypassed()));
             }
+            if (options.getDedicatedGatewayRequestOptions().getShardKey() != null) {
+                headers.put(HttpConstants.HttpHeaders.DEDICATED_GATEWAY_PER_REQUEST_SHARD_KEY,
+                    options.getDedicatedGatewayRequestOptions().getShardKey());
+            }
         }
 
         return headers;
