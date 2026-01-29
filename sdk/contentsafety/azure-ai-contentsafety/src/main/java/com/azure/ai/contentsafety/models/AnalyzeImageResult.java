962f48eb9d76c3eb268452fc4d0a2de1681da4be
azure-sdk <azuresdk@microsoft.com>
2026-01-29 16:03:34 +0000

    Reset sources for azure-ai-contentsafety to the release version 1.0.16.

--- sdk/contentsafety/azure-ai-contentsafety/src/main/java/com/azure/ai/contentsafety/models/AnalyzeImageResult.java
+++ sdk/contentsafety/azure-ai-contentsafety/src/main/java/com/azure/ai/contentsafety/models/AnalyzeImageResult.java
@@ -5,24 +5,22 @@ package com.azure.ai.contentsafety.models;
 
 import com.azure.core.annotation.Generated;
 import com.azure.core.annotation.Immutable;
-import com.azure.json.JsonReader;
-import com.azure.json.JsonSerializable;
-import com.azure.json.JsonToken;
-import com.azure.json.JsonWriter;
-import java.io.IOException;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 /**
  * The image analysis response.
  */
 @Immutable
-public final class AnalyzeImageResult implements JsonSerializable<AnalyzeImageResult> {
+public final class AnalyzeImageResult {
 
     /*
      * Analysis result for categories.
      */
     @Generated
-    private final List<ImageCategoriesAnalysis> categoriesAnalysis;
+    @JsonProperty(value = "categoriesAnalysis")
+    private List<ImageCategoriesAnalysis> categoriesAnalysis;
 
     /**
      * Creates an instance of AnalyzeImageResult class.
@@ -30,7 +28,9 @@ public final class AnalyzeImageResult implements JsonSerializable<AnalyzeImageRe
      * @param categoriesAnalysis the categoriesAnalysis value to set.
      */
     @Generated
-    private AnalyzeImageResult(List<ImageCategoriesAnalysis> categoriesAnalysis) {
+    @JsonCreator
+    private AnalyzeImageResult(
+        @JsonProperty(value = "categoriesAnalysis") List<ImageCategoriesAnalysis> categoriesAnalysis) {
         this.categoriesAnalysis = categoriesAnalysis;
     }
 
@@ -42,43 +42,5 @@ public final class AnalyzeImageResult implements JsonSerializable<AnalyzeImageRe
     @Generated
     public List<ImageCategoriesAnalysis> getCategoriesAnalysis() {
         return this.categoriesAnalysis;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Generated
-    @Override
-    public JsonWriter toJson(JsonWriter jsonWriter) throws IOException {
-        jsonWriter.writeStartObject();
-        jsonWriter.writeArrayField("categoriesAnalysis", this.categoriesAnalysis,
-            (writer, element) -> writer.writeJson(element));
-        return jsonWriter.writeEndObject();
-    }
-
-    /**
-     * Reads an instance of AnalyzeImageResult from the JsonReader.
-     *
-     * @param jsonReader The JsonReader being read.
-     * @return An instance of AnalyzeImageResult if the JsonReader was pointing to an instance of it, or null if it was
-     * pointing to JSON null.
-     * @throws IllegalStateException If the deserialized JSON object was missing any required properties.
-     * @throws IOException If an error occurs while reading the AnalyzeImageResult.
-     */
-    @Generated
-    public static AnalyzeImageResult fromJson(JsonReader jsonReader) throws IOException {
-        return jsonReader.readObject(reader -> {
-            List<ImageCategoriesAnalysis> categoriesAnalysis = null;
-            while (reader.nextToken() != JsonToken.END_OBJECT) {
-                String fieldName = reader.getFieldName();
-                reader.nextToken();
-                if ("categoriesAnalysis".equals(fieldName)) {
-                    categoriesAnalysis = reader.readArray(reader1 -> ImageCategoriesAnalysis.fromJson(reader1));
-                } else {
-                    reader.skipChildren();
-                }
-            }
-            return new AnalyzeImageResult(categoriesAnalysis);
-        });
     }
 }
