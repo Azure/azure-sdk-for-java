08e2acf0db5a233ee5e18ffdf0000dde98fe925e
azure-sdk <azuresdk@microsoft.com>
2026-01-29 21:32:31 +0800

    Reset sources for azure-data-schemaregistry to the release version 1.5.9.

--- sdk/schemaregistry/azure-data-schemaregistry/src/main/java/com/azure/data/schemaregistry/implementation/models/SchemaGroups.java
+++ sdk/schemaregistry/azure-data-schemaregistry/src/main/java/com/azure/data/schemaregistry/implementation/models/SchemaGroups.java
@@ -5,7 +5,6 @@
 package com.azure.data.schemaregistry.implementation.models;
 
 import com.azure.core.annotation.Fluent;
-import com.azure.core.annotation.Generated;
 import com.azure.json.JsonReader;
 import com.azure.json.JsonSerializable;
 import com.azure.json.JsonToken;
@@ -21,19 +20,16 @@ public final class SchemaGroups implements JsonSerializable<SchemaGroups> {
     /*
      * Array of schema groups.
      */
-    @Generated
     private List<String> groups;
 
     /*
      * URl to next batch of schema groups
      */
-    @Generated
     private String nextLink;
 
     /**
      * Creates an instance of SchemaGroups class.
      */
-    @Generated
     public SchemaGroups() {
     }
 
@@ -42,7 +38,6 @@ public final class SchemaGroups implements JsonSerializable<SchemaGroups> {
      * 
      * @return the groups value.
      */
-    @Generated
     public List<String> getGroups() {
         return this.groups;
     }
@@ -53,7 +48,6 @@ public final class SchemaGroups implements JsonSerializable<SchemaGroups> {
      * @param groups the groups value to set.
      * @return the SchemaGroups object itself.
      */
-    @Generated
     public SchemaGroups setGroups(List<String> groups) {
         this.groups = groups;
         return this;
@@ -64,7 +58,6 @@ public final class SchemaGroups implements JsonSerializable<SchemaGroups> {
      * 
      * @return the nextLink value.
      */
-    @Generated
     public String getNextLink() {
         return this.nextLink;
     }
@@ -75,16 +68,11 @@ public final class SchemaGroups implements JsonSerializable<SchemaGroups> {
      * @param nextLink the nextLink value to set.
      * @return the SchemaGroups object itself.
      */
-    @Generated
     public SchemaGroups setNextLink(String nextLink) {
         this.nextLink = nextLink;
         return this;
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Generated
     @Override
     public JsonWriter toJson(JsonWriter jsonWriter) throws IOException {
         jsonWriter.writeStartObject();
@@ -101,7 +89,6 @@ public final class SchemaGroups implements JsonSerializable<SchemaGroups> {
      * pointing to JSON null.
      * @throws IOException If an error occurs while reading the SchemaGroups.
      */
-    @Generated
     public static SchemaGroups fromJson(JsonReader jsonReader) throws IOException {
         return jsonReader.readObject(reader -> {
             SchemaGroups deserializedSchemaGroups = new SchemaGroups();
