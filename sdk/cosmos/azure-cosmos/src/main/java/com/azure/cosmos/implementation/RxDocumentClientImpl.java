ffc4ceb36b56db371d8c6fe51f4eaa34e2ec6f0e
Annie Liang <64233642+xinlian12@users.noreply.github.com>
2026-01-30 10:07:55 -0800

    [SparkConnector]AdjustConcurrencyControlForTransactionalBatch (#47803)
    * adjust concurrency control for transactional batch
    
    ---------
    
    Co-authored-by: Annie Liang <anniemac@Annies-MacBook-Pro.local>

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/RxDocumentClientImpl.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/RxDocumentClientImpl.java
@@ -5192,7 +5192,8 @@ public class RxDocumentClientImpl implements AsyncDocumentClient, IAuthorization
                                                          ServerBatchRequest serverBatchRequest,
                                                          RequestOptions options,
                                                          boolean disableAutomaticIdGeneration,
-                                                         boolean disableStaledResourceExceptionHandling) {
+                                                         boolean disableStaledResourceExceptionHandling,
+                                                         boolean disableRetryForThrottledBatchRequest) {
         AtomicReference<RxDocumentServiceRequest> requestReference = new AtomicReference<>();
 
         Consumer<CosmosException> gwModeE2ETimeoutDiagnosticHandler
@@ -5208,7 +5209,11 @@ public class RxDocumentClientImpl implements AsyncDocumentClient, IAuthorization
         ScopedDiagnosticsFactory scopedDiagnosticsFactory = new ScopedDiagnosticsFactory(this, false);
         scopedDiagnosticsFactory.setGwModeE2ETimeoutDiagnosticsHandler(gwModeE2ETimeoutDiagnosticHandler);
 
-        DocumentClientRetryPolicy documentClientRetryPolicy = this.resetSessionTokenRetryPolicy.getRequestPolicy(scopedDiagnosticsFactory);
+        DocumentClientRetryPolicy documentClientRetryPolicy =
+            this.resetSessionTokenRetryPolicy.getRequestPolicy(
+                scopedDiagnosticsFactory,
+                disableRetryForThrottledBatchRequest);
+
         if (!disableStaledResourceExceptionHandling) {
             documentClientRetryPolicy = new StaleResourceRetryPolicy(
                 this.collectionCache,
