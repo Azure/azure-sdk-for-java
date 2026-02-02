4a7cbc9265756ac06f4602a96b6a803d44f5a8b2
browndav <browndav@microsoft.com>
2026-02-02 11:49:56 -0500

    create shell for credentialvalidatortest

---
+++ sdk/storage/azure-storage-common/src/test/java/com/azure/storage/common/implementation/credentials/CredentialValidatorTest.java
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.storage.common.implementation.credentials;
+
+import com.azure.core.util.logging.ClientLogger;
+
+public class CredentialValidatorTest {
+    private static final ClientLogger LOGGER = new ClientLogger(CredentialValidatorTest.class);
+
+}
