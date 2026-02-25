bb1e6a31818d9e4f91eb4b7e653762d5570e1976
Fabian Meiswinkel <fabianm@microsoft.com>
2026-02-25 18:45:27 +0000

    Renamed methods

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/Exceptions.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/Exceptions.java
@@ -75,7 +75,7 @@ public class Exceptions {
         return statusCode == HttpConstants.StatusCodes.INTERNAL_SERVER_ERROR && (subStatusCode >= 20_000 && subStatusCode < 21_000);
     }
 
-    public static boolean isTransientExceptionPossiblyCausingNoisyLogs(int statusCode, int subStatusCode) {
+    public static boolean isCommonlyExpectedExceptionPossiblyCausingNoisyLogs(int statusCode, int subStatusCode) {
         if (statusCode == HttpConstants.StatusCodes.TOO_MANY_REQUESTS
             && subStatusCode == HttpConstants.SubStatusCodes.USER_REQUEST_RATE_TOO_LARGE) {
             return true;
