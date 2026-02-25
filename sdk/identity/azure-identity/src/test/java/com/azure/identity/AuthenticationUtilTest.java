6a965766d0cd61e08e0db69c5502100eab5195ef
anannya03 <aaniee09@gmail.com>
2026-02-24 17:19:21 -0800

    modify the tests

--- sdk/identity/azure-identity/src/test/java/com/azure/identity/AuthenticationUtilTest.java
+++ sdk/identity/azure-identity/src/test/java/com/azure/identity/AuthenticationUtilTest.java
@@ -3,31 +3,32 @@
 
 package com.azure.identity;
 
+import com.azure.core.http.HttpClient;
+import com.azure.core.http.HttpRequest;
+import com.azure.core.test.http.MockHttpResponse;
 import com.azure.core.test.utils.MockTokenCredential;
+import com.azure.core.util.Context;
+
+import org.apache.tools.ant.taskdefs.condition.Http;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.util.function.Supplier;
 
 import static com.azure.identity.AuthenticationUtil.getBearerTokenSupplier;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import com.azure.core.http.HttpClient;
-import com.azure.core.test.http.MockHttpResponse;
-import static org.mockito.Mockito.mockStatic;
-import org.mockito.MockedStatic;
-import reactor.core.publisher.Mono;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 public class AuthenticationUtilTest {
 
     @Test
     public void testGetBearerTokenSupplier() {
-        HttpClient mockHttpClient = request -> Mono.just(new MockHttpResponse(request, 200));
-
-        try (MockedStatic<HttpClient> httpClientMock = mockStatic(HttpClient.class)) {
-            httpClientMock.when(HttpClient::createDefault).thenReturn(mockHttpClient);
-
-            MockTokenCredential credential = new MockTokenCredential();
-            Supplier<String> supplier = getBearerTokenSupplier(credential, "scope");
-            assertEquals("mockToken", supplier.get());
-        }
+        HttpClient mockHttpClient = Mockito.mock(HttpClient.class);
+        when(mockHttpClient.sendSync(any(HttpRequest.class), any(Context.class)))
+            .thenAnswer(invocation -> new MockHttpResponse(invocation.getArgument(0, HttpRequest.class), 200));
+        MockTokenCredential credential = new MockTokenCredential();
+        Supplier<String> supplier = getBearerTokenSupplier(credential, mockHttpClient, "scope");
+        assertEquals("mockToken", supplier.get());
     }
 }
