480a1f0747d9077fc26ebdc95bab3bf6b2f25832
Rujun Chen <rujche@microsoft.com>
2026-02-27 16:32:45 +0800

    add empty line to create a commit

--- sdk/spring/spring-cloud-azure-docker-compose/src/test/java/com/azure/spring/cloud/docker/compose/implementation/service/connection/storage/StorageQueueDockerComposeConnectionDetailsFactoryTests.java
+++ sdk/spring/spring-cloud-azure-docker-compose/src/test/java/com/azure/spring/cloud/docker/compose/implementation/service/connection/storage/StorageQueueDockerComposeConnectionDetailsFactoryTests.java
@@ -25,6 +25,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @EnabledOnOs(OS.LINUX)
 class StorageQueueDockerComposeConnectionDetailsFactoryTests {
 
+
     @Autowired
     private QueueClient queueClient;
 
