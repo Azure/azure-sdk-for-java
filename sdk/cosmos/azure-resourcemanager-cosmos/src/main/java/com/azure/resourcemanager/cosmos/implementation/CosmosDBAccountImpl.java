b660a75e6f12f298bb91d5cd8e48fe04ad52369b
azure-sdk <azuresdk@microsoft.com>
2026-05-05 20:04:48 +0000

    Reset sources for azure-resourcemanager-cosmos to the release version 2.54.2.

--- sdk/cosmos/azure-resourcemanager-cosmos/src/main/java/com/azure/resourcemanager/cosmos/implementation/CosmosDBAccountImpl.java
+++ sdk/cosmos/azure-resourcemanager-cosmos/src/main/java/com/azure/resourcemanager/cosmos/implementation/CosmosDBAccountImpl.java
@@ -807,12 +807,6 @@ class CosmosDBAccountImpl
     }
 
     @Override
-    public CosmosDBAccountImpl enableLocalAuth() {
-        this.innerModel().withDisableLocalAuth(false);
-        return this;
-    }
-
-    @Override
     public CosmosDBAccountImpl enableAutomaticFailover() {
         this.innerModel().withEnableAutomaticFailover(true);
         return this;
