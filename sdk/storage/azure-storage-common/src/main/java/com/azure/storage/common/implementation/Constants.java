e3e8e666a4c3297dca3efd905235b3cc32633cec
azure-sdk <azuresdk@microsoft.com>
2026-05-05 23:05:30 +0000

    Reset sources for azure-storage-common to the release version 12.32.2.

--- sdk/storage/azure-storage-common/src/main/java/com/azure/storage/common/implementation/Constants.java
+++ sdk/storage/azure-storage-common/src/main/java/com/azure/storage/common/implementation/Constants.java
@@ -88,7 +88,7 @@ public final class Constants {
     public static final String PROPERTY_AZURE_STORAGE_SAS_SERVICE_VERSION = "AZURE_STORAGE_SAS_SERVICE_VERSION";
 
     public static final String SAS_SERVICE_VERSION
-        = Configuration.getGlobalConfiguration().get(PROPERTY_AZURE_STORAGE_SAS_SERVICE_VERSION, "2026-06-06");
+        = Configuration.getGlobalConfiguration().get(PROPERTY_AZURE_STORAGE_SAS_SERVICE_VERSION, "2026-02-06");
 
     public static final String ADJUSTED_BLOB_LENGTH_KEY = "adjustedBlobLength";
 
@@ -220,7 +220,7 @@ public final class Constants {
          * @deprecated For SAS Service Version use {@link Constants#SAS_SERVICE_VERSION}.
          */
         @Deprecated
-        public static final String TARGET_STORAGE_VERSION = "2026-06-06";
+        public static final String TARGET_STORAGE_VERSION = "2026-02-06";
 
         /**
          * Error code returned from the service.
@@ -283,31 +283,6 @@ public final class Constants {
      * RESERVED FOR INTERNAL USE.
      */
     public static final class UrlConstants {
-
-        /**
-         * DNS subdomain label for the Blob service ({@code account}.blob.{suffix}).
-         */
-        public static final String BLOB_URI_SUBDOMAIN = "blob";
-
-        /**
-         * DNS subdomain label for the Azure Files service ({@code account}.file.{suffix}).
-         */
-        public static final String FILE_URI_SUBDOMAIN = "file";
-
-        /**
-         * DNS subdomain label for the Queue service ({@code account}.queue.{suffix}).
-         */
-        public static final String QUEUE_URI_SUBDOMAIN = "queue";
-
-        /**
-         * DNS subdomain label for the Table service ({@code account}.table.{suffix}).
-         */
-        public static final String TABLE_URI_SUBDOMAIN = "table";
-
-        /**
-         * DNS subdomain label for Data Lake Storage ({@code account}.dfs.{suffix}).
-         */
-        public static final String DFS_URI_SUBDOMAIN = "dfs";
 
         /**
          * The snapshot parameters.
@@ -390,16 +365,6 @@ public final class Constants {
         public static final String SAS_ENCRYPTION_SCOPE = "ses";
 
         /**
-         * The SAS request headers parameter.
-         */
-        public static final String SAS_REQUEST_HEADERS = "srh";
-
-        /**
-         * The SAS request query parameters parameter.
-         */
-        public static final String SAS_REQUEST_QUERY_PARAMETERS = "srq";
-
-        /**
          * The SAS cache control parameter.
          */
         public static final String SAS_CACHE_CONTROL = "rscc";
@@ -455,11 +420,6 @@ public final class Constants {
         public static final String SAS_SIGNED_KEY_VERSION = "skv";
 
         /**
-         * The SAS signed delegated user tenant id parameter for user delegation SAS.
-         */
-        public static final String SAS_SIGNED_KEY_DELEGATED_USER_TENANT_ID = "skdutid";
-
-        /**
          * The SAS preauthorized agent object id parameter for user delegation SAS.
          */
         public static final String SAS_PREAUTHORIZED_AGENT_OBJECT_ID = "saoid";
@@ -488,5 +448,4 @@ public final class Constants {
             // Private to prevent construction.
         }
     }
-
 }
