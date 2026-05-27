0548c836bf19effe4dbc36f753ba19f3a728ed0d
ReneOv-MSFT <142273469+ReneOv-MSFT@users.noreply.github.com>
2026-05-27 10:04:00 -0700

    Remove unused import

--- sdk/batch/azure-compute-batch/src/main/java/com/azure/compute/batch/implementation/task/TaskManager.java
+++ sdk/batch/azure-compute-batch/src/main/java/com/azure/compute/batch/implementation/task/TaskManager.java
@@ -3,7 +3,6 @@
 package com.azure.compute.batch.implementation.task;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
