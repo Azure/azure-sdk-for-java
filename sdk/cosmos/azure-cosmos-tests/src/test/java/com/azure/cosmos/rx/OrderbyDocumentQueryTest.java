ccabf826d82b69b8574aa2db7456fe0cc81bf200
Fabian Meiswinkel <fabianm@microsoft.com>
2026-06-02 21:59:25 +0000

    Fixing flaky query tests due to container creation being async

--- sdk/cosmos/azure-cosmos-tests/src/test/java/com/azure/cosmos/rx/OrderbyDocumentQueryTest.java
+++ sdk/cosmos/azure-cosmos-tests/src/test/java/com/azure/cosmos/rx/OrderbyDocumentQueryTest.java
@@ -688,6 +688,7 @@ public class OrderbyDocumentQueryTest extends TestSuiteBase {
                 }))
             .block();
         roundTripsContainer = createdDatabase.getContainer(containerName);
+        waitForCollectionToBeAvailableToRead();
         setupRoundTripContainer();
 
         List<Map<String, Object>> keyValuePropsList = new ArrayList<>();
