bfbc0912d72a33bf83b928e6d054a3b73f9c7974
azure-sdk <azuresdk@microsoft.com>
2026-06-12 09:19:19 +0000

    Configurations:  'specification/containerservice/resource-manager/Microsoft.ContainerService/aks/tspconfig.yaml', API Version: 2026-04-02-preview, SDK Release Type: beta, and CommitSHA: 'a6a17efb2376ccaf81f05080b4da8774f6e551a9' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6428461 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

--- sdk/containerservice/azure-resourcemanager-containerservice/src/samples/java/com/azure/resourcemanager/containerservice/generated/ManagedNamespacesListByManagedClusterSamples.java
+++ sdk/containerservice/azure-resourcemanager-containerservice/src/samples/java/com/azure/resourcemanager/containerservice/generated/ManagedNamespacesListByManagedClusterSamples.java
@@ -9,7 +9,7 @@ package com.azure.resourcemanager.containerservice.generated;
  */
 public final class ManagedNamespacesListByManagedClusterSamples {
     /*
-     * x-ms-original-file: 2026-04-01/ManagedNamespacesList.json
+     * x-ms-original-file: 2026-04-02-preview/ManagedNamespacesList.json
      */
     /**
      * Sample code: List namespaces by Managed Cluster.
