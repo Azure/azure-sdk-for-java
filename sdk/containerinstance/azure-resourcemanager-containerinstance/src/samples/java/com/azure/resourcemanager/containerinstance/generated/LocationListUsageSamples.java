0afbcc5fbcc9f16bddb9939427b5933edd6e912c
azure-sdk <azuresdk@microsoft.com>
2026-07-23 02:29:55 +0000

    Configurations:  'specification/containerinstance/resource-manager/Microsoft.ContainerInstance/ContainerInstance/tspconfig.yaml', SDK Release Type: beta, and CommitSHA: '8c66db435b08db558d154f98c998f3d9672dab58' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6605343 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

--- sdk/containerinstance/azure-resourcemanager-containerinstance/src/samples/java/com/azure/resourcemanager/containerinstance/generated/LocationListUsageSamples.java
+++ sdk/containerinstance/azure-resourcemanager-containerinstance/src/samples/java/com/azure/resourcemanager/containerinstance/generated/LocationListUsageSamples.java
@@ -9,7 +9,7 @@ package com.azure.resourcemanager.containerinstance.generated;
  */
 public final class LocationListUsageSamples {
     /*
-     * x-ms-original-file: 2025-09-01/ContainerGroupUsage.json
+     * x-ms-original-file: 2026-08-01-preview/ContainerGroupUsage.json
      */
     /**
      * Sample code: ContainerUsage.
