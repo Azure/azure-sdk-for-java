c766d261fbabd4a6ec2db282da9a50acacc107a2
azure-sdk <azuresdk@microsoft.com>
2026-08-25 14:34:18 +0000

    Configurations:  'specification/cloudhealth/resource-manager/Microsoft.CloudHealth/CloudHealth/tspconfig.yaml', SDK Release Type: beta, and CommitSHA: 'ef5352bc9eb986c683703bc0f7907804837a1e00' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6742992 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

--- sdk/cloudhealth/azure-resourcemanager-cloudhealth/src/samples/java/com/azure/resourcemanager/cloudhealth/generated/HealthModelsUpdateSamples.java
+++ sdk/cloudhealth/azure-resourcemanager-cloudhealth/src/samples/java/com/azure/resourcemanager/cloudhealth/generated/HealthModelsUpdateSamples.java
@@ -13,7 +13,7 @@ import java.util.Map;
  */
 public final class HealthModelsUpdateSamples {
     /*
-     * x-ms-original-file: 2026-05-01-preview/HealthModels_Update.json
+     * x-ms-original-file: 2026-09-01-preview/HealthModels_Update.json
      */
     /**
      * Sample code: HealthModels_Update.
