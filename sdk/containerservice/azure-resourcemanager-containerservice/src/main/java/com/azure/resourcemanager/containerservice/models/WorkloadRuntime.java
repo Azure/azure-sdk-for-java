3708c8ed11b5e5bb65adc170753380ab5b044f3a
azure-sdk <azuresdk@microsoft.com>
2026-09-01 01:30:39 +0000

    Configurations:  'specification/containerservice/resource-manager/Microsoft.ContainerService/aks/tspconfig.yaml', API Version: 2026-06-02-preview, SDK Release Type: beta, and CommitSHA: '036696689876be9b9eb9680494011244e41950d8' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6768963 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

--- sdk/containerservice/azure-resourcemanager-containerservice/src/main/java/com/azure/resourcemanager/containerservice/models/WorkloadRuntime.java
+++ sdk/containerservice/azure-resourcemanager-containerservice/src/main/java/com/azure/resourcemanager/containerservice/models/WorkloadRuntime.java
@@ -22,6 +22,14 @@ public final class WorkloadRuntime extends ExpandableStringEnum<WorkloadRuntime>
     public static final WorkloadRuntime WASM_WASI = fromString("WasmWasi");
 
     /**
+     * Nodes can use (Kata + Cloud Hypervisor + Hyper-V) to enable Nested VM-based pods (Preview). Due to the use
+     * Hyper-V, AKS node OS itself is a nested VM (the root OS) of Hyper-V. Thus it can only be used with VM series that
+     * support Nested Virtualization such as Dv3 series. This naming convention will be deprecated in future releases in
+     * favor of KataVmIsolation.
+     */
+    public static final WorkloadRuntime KATA_MSHV_VM_ISOLATION = fromString("KataMshvVmIsolation");
+
+    /**
      * Nodes can use (Kata + Cloud Hypervisor + Hyper-V) to enable Nested VM-based pods. Due to the use Hyper-V, AKS
      * node OS itself is a nested VM (the root OS) of Hyper-V. Thus it can only be used with VM series that support
      * Nested Virtualization such as Dv3 series.
