e10e0dc4e2c37cd99cbbee0a82850e9fe1baae16
Hua Liang <huliang@microsoft.com>
2026-09-15 12:11:17 -0700

    Fix Content Safety Java generation and coverage checks

--- sdk/contentsafety/azure-ai-contentsafety/src/samples/java/com/azure/ai/contentsafety/generated/AnalyzeImage.java
+++ sdk/contentsafety/azure-ai-contentsafety/src/samples/java/com/azure/ai/contentsafety/generated/AnalyzeImage.java
@@ -9,7 +9,6 @@ import com.azure.ai.contentsafety.ContentSafetyClientBuilder;
 import com.azure.ai.contentsafety.models.AnalyzeImageOptions;
 import com.azure.ai.contentsafety.models.AnalyzeImageResult;
 import com.azure.ai.contentsafety.models.ContentSafetyImageData;
-import com.azure.core.util.BinaryData;
 import com.azure.core.util.Configuration;
 import com.azure.identity.DefaultAzureCredentialBuilder;
 
@@ -21,8 +20,7 @@ public class AnalyzeImage {
                 .buildClient();
         // BEGIN:com.azure.ai.contentsafety.generated.analyze-image.analyze-image
         AnalyzeImageResult response = contentSafetyClient
-            .analyzeImage(new AnalyzeImageOptions(
-                new ContentSafetyImageData().setContent(BinaryData.fromBytes("Y29udGVudDE=".getBytes()))));
+            .analyzeImage(new AnalyzeImageOptions(new ContentSafetyImageData().setContent("Y29udGVudDE=".getBytes())));
         // END:com.azure.ai.contentsafety.generated.analyze-image.analyze-image
     }
 }
