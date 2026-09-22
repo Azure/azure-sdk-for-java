712301a211afe2299b9704707daebd1435ac236d
browndav <browndav@microsoft.com>
2026-09-22 10:14:26 -0400

    fix linting issues

--- sdk/storage/azure-storage-common/src/test/java/com/azure/storage/common/implementation/StorageImplUtilsTests.java
+++ sdk/storage/azure-storage-common/src/test/java/com/azure/storage/common/implementation/StorageImplUtilsTests.java
@@ -23,7 +23,6 @@ import reactor.core.publisher.Mono;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertSame;
