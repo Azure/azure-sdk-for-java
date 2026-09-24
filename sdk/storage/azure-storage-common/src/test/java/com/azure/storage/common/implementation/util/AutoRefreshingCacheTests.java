71cd327a1084da90eb58d1fa272d59b59302fd43
browndav <browndav@microsoft.com>
2026-09-24 13:38:27 -0400

    Remove duplicate import introduced by cache test merge

--- sdk/storage/azure-storage-common/src/test/java/com/azure/storage/common/implementation/util/AutoRefreshingCacheTests.java
+++ sdk/storage/azure-storage-common/src/test/java/com/azure/storage/common/implementation/util/AutoRefreshingCacheTests.java
@@ -21,7 +21,6 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
