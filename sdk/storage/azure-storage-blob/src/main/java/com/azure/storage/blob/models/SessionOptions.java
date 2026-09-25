79abe1d80ddd74a603a0a81fdcf4fc2481130402
browndav <browndav@microsoft.com>
2026-09-25 13:06:39 -0400

    remove unsused containerName var from SessionOptions

--- sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/models/SessionOptions.java
+++ sdk/storage/azure-storage-blob/src/main/java/com/azure/storage/blob/models/SessionOptions.java
@@ -17,7 +17,6 @@ package com.azure.storage.blob.models;
 public final class SessionOptions {
 
     private SessionMode sessionMode = SessionMode.DISABLED;
-    private String containerName;
     private String accountName;
     private SessionProvider sessionProvider;
 
